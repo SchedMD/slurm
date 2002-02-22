cc9aefa6645ae5f752fd80654ce3688eb95b4d8e
Mark Grondona <mgrondona@llnl.gov>
2002-02-22 16:05:22 +0000

    o run aclocal to generate aclocal.m4  o added some comments describing what this script does  o add message to run ./configure to finish slurm configuration.

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -3,6 +3,14 @@
 # $Id$
 # $Source$
 #
+# Run this script to generate aclocal.m4, config.h.in, 
+# Makefile.in's, and ./configure...
+# 
+# To specify extra flags to aclocal (include dirs for example),
+# set ACLOCAL_FLAGS
+#
+echo "running aclocal $ACLOCAL_FLAGS ... "
+aclocal $ACLOCAL_FLAGS
 echo "running autoheader ... "
 autoheader
 echo "running automake --add-missing ... "
@@ -11,3 +19,4 @@ echo "running autoconf ... "
 autoconf
 echo "removing stale config.status and config.log"
 rm -f config.status config.log
+echo "now run ./configure to configure slurm for your environment."
