c7f378b576215cefe1f5c22f702dfb205cd2939b
Mark Grondona <mgrondona@llnl.gov>
2002-04-13 00:49:54 +0000

    formatting changes

--- src/common/xassert.h
+++ src/common/xassert.h
@@ -1,10 +1,10 @@
 /* $Id$ */
 
 /*
-** xassert: assert type macro with configurable handling
-**          If NDEBUG is defined, do nothing.
-**          If not, and expression is zero, log an error message and abort.
-*/
+ * xassert: assert type macro with configurable handling
+ *          If NDEBUG is defined, do nothing.
+ *          If not, and expression is zero, log an error message and abort.
+ */
 
 #ifndef _XASSERT_H
 #define _XASSERT_H	1
@@ -27,14 +27,12 @@
      } _STMT_END 
 
 /*  This prints the assertion failed message to the slurm log facility
-**  (see log.h) and aborts the calling program
-**  (messages go to stderr if log is not initialized)
-*/
+ *  (see log.h) and aborts the calling program
+ *  (messages go to stderr if log is not initialized)
+ */
 extern void __xassert_failed(char *, const char *, int, const char *) 
 	    __NORETURN_ATTR;
 
 #endif /* NDEBUG. */
 
 #endif /* !__XASSERT_H */
-
-/* vim: set sw=4 ts=4 expandtabs */
