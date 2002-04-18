777d37e932bd3c4f8ff6055572944d52bb90b949
Mark Grondona <mgrondona@llnl.gov>
2002-04-18 00:21:10 +0000

    o added try_xrealloc() which returns <= 0 when malloc fails  o let xrealloc() and try_xrealloc() accept NULL first args  o added documentation to xmalloc.h

--- src/common/xmalloc.c
+++ src/common/xmalloc.c
@@ -1,12 +1,12 @@
 /* $Id$ */
 
 /* xmalloc.c: enhanced malloc routines
-**
-** Mark Grondona <mgrondona@llnl.gov>
-** 
-** Started with Jim Garlick's xmalloc and tied into slurm log facility.
-** Also added ability to print file, line, and function of caller.
-*/
+ *
+ * Mark Grondona <mgrondona@llnl.gov>
+ * 
+ * Started with Jim Garlick's xmalloc and tied into slurm log facility.
+ * Also added ability to print file, line, and function of caller.
+ */
 
 #if HAVE_CONFIG_H
 #  include "config.h"
@@ -24,23 +24,26 @@
 #if	HAVE_UNSAFE_MALLOC
 #  include <pthread.h>
    static pthread_mutex_t malloc_lock = PTHREAD_MUTEX_INITIALIZER;
-#  define MALLOC_LOCK()	pthread_mutex_lock(&malloc_lock)
+#  define MALLOC_LOCK()		pthread_mutex_lock(&malloc_lock)
 #  define MALLOC_UNLOCK()	pthread_mutex_unlock(&malloc_lock)
 #else
 #  define MALLOC_LOCK()	
 #  define MALLOC_UNLOCK()	
 #endif
 
-
 static void malloc_assert_failed(char *, const char *, int, 
-        const char *, const char *); 
+                                 const char *, const char *); 
 
+#if NDEBUG
+#  define xmalloc_assert  ((void)0)
+#else
+#  define xmalloc_assert(expr)  _STMT_START {  			    \
+          (expr) ? ((void)(0)) :                                    \
+          malloc_assert_failed(__STRING(expr), file, line, func,    \
+                               __CURRENT_FUNC__)                    \
+          } _STMT_END
+#endif /* NDEBUG */
 
-#define xmalloc_assert(expr, _file, _line, _func)  _STMT_START {    \
-        (expr) ? ((void)(0)) :                                      \
-        malloc_assert_failed(__STRING(expr), _file, _line, _func,   \
-                             __CURRENT_FUNC__)                      \
-        } _STMT_END
 
 /*
  * "Safe" version of malloc().
@@ -52,7 +55,7 @@ void *_xmalloc(size_t size, const char *file, int line, const char *func)
 	void *new;
 	int *p;
 
-	xmalloc_assert(size > 0 && size <= INT_MAX, file, line, func);
+	xmalloc_assert(size > 0 && size <= INT_MAX);
 	MALLOC_LOCK();
 	p = (int *)malloc(size + 2*sizeof(int));
 	MALLOC_UNLOCK();
@@ -64,7 +67,7 @@ void *_xmalloc(size_t size, const char *file, int line, const char *func)
 		exit(1);
 	}
 	p[0] = XMALLOC_MAGIC;	/* add "secret" magic cookie */
-	p[1] = size;		/* store size in buffer */
+	p[1] = (int)size;	/* store size in buffer */
 
 	new = &p[2];
 	memset(new, 0, size);
@@ -79,7 +82,7 @@ void *_try_xmalloc(size_t size, const char *file, int line, const char *func)
 	void *new;
 	int *p;
 
-	xmalloc_assert(size > 0 && size <= INT_MAX, file, line, func);
+	xmalloc_assert(size > 0 && size <= INT_MAX);
 	MALLOC_LOCK();
 	p = (int *)malloc(size + 2*sizeof(int));
 	MALLOC_UNLOCK();
@@ -87,7 +90,7 @@ void *_try_xmalloc(size_t size, const char *file, int line, const char *func)
 		return NULL;
 	}
 	p[0] = XMALLOC_MAGIC;	/* add "secret" magic cookie */
-	p[1] = size;		/* store size in buffer */
+	p[1] = (int)size;	/* store size in buffer */
 
 	new = &p[2];
 	memset(new, 0, size);
@@ -103,26 +106,94 @@ void *_try_xmalloc(size_t size, const char *file, int line, const char *func)
 void _xrealloc(void **item, size_t newsize, 
 	       const char *file, int line, const char *func)
 {
-	int *p = (int *)*item - 2;
+	int *p = NULL;
 
-	xmalloc_assert(*item != NULL, file, line, func);
-	xmalloc_assert(newsize > 0 && newsize <= INT_MAX, file, line, func);
+	/* xmalloc_assert(*item != NULL, file, line, func); */
+	xmalloc_assert(newsize >= 0 && (int)newsize <= INT_MAX);
 
-	/* magic cookie still there? */
-	xmalloc_assert(p[0] == XMALLOC_MAGIC, file, line, func);		
+	if (*item != NULL) {
+		p = (int *)*item - 2;
 
-	MALLOC_LOCK();
-	p = (int *)realloc(p, newsize + 2*sizeof(int));
-	MALLOC_UNLOCK();
-	if (!p) {
-		fprintf(stderr, "%s:%d: %s: xrealloc(%d) failed\n", 
-				file, line, func, (int)newsize);
-		exit(1);
+		/* magic cookie still there? */
+		xmalloc_assert(p[0] == XMALLOC_MAGIC);		
+
+		MALLOC_LOCK();
+		p = (int *)realloc(p, newsize + 2*sizeof(int));
+		MALLOC_UNLOCK();
+
+		if (p == NULL) 
+			goto error;
+
+		xmalloc_assert(p[0] == XMALLOC_MAGIC);
+
+	} else {
+		/* Initalize new memory */
+		MALLOC_LOCK();
+		p = (int *)malloc(newsize + 2*sizeof(int));
+		MALLOC_UNLOCK();
+
+		if (p == NULL) 
+			goto error;
+
+		memset(&p[2], 0, newsize);
+		p[0] = XMALLOC_MAGIC;
 	}
-	xmalloc_assert(p[0] == XMALLOC_MAGIC, file, line, func);
+
+	p[1] = (int)newsize;
+	*item = &p[2];
+	return;
+
+  error:
+	fprintf(stderr, "%s:%d: %s: xrealloc(%d) failed\n", 
+		file, line, func, (int)newsize);
+	abort();
+}
+
+/* 
+ * same as above, but return <= 0 on malloc() failure instead of aborting.
+ * `*item' will be unchanged.
+ */
+int _try_xrealloc(void **item, size_t newsize, 
+	          const char *file, int line, const char *func)
+{
+	int *p = NULL;
+
+	/* xmalloc_assert(*item != NULL, file, line, func); */
+	xmalloc_assert(newsize >= 0 && (int)newsize <= INT_MAX);
+
+	if (*item != NULL) {
+		p = (int *)*item - 2;
+
+		/* magic cookie still there? */
+		xmalloc_assert(p[0] == XMALLOC_MAGIC);		
+
+		MALLOC_LOCK();
+		p = (int *)realloc(p, newsize + 2*sizeof(int));
+		MALLOC_UNLOCK();
+
+		if (p == NULL) 
+			return 0;
+
+		xmalloc_assert(p[0] == XMALLOC_MAGIC);
+
+	} else {
+		/* Initalize new memory */
+		MALLOC_LOCK();
+		p = (int *)malloc(newsize + 2*sizeof(int));
+		MALLOC_UNLOCK();
+
+		if (p == NULL) 
+			return 0;
+
+		memset(&p[2], 0, newsize);
+		p[0] = XMALLOC_MAGIC;
+	}
+
 	p[1] = newsize;
 	*item = &p[2];
+	return 1;
 }
+
 
 /*
  * Return the size of a buffer.
@@ -131,8 +202,8 @@ void _xrealloc(void **item, size_t newsize,
 int _xsize(void *item, const char *file, int line, const char *func)
 {
 	int *p = (int *)item - 2;
-	xmalloc_assert(item != NULL, file, line, func);	
-	xmalloc_assert(p[0] == XMALLOC_MAGIC, file, line, func); 
+	xmalloc_assert(item != NULL);	
+	xmalloc_assert(p[0] == XMALLOC_MAGIC); 
 	return p[1];
 }
 
@@ -147,7 +218,7 @@ void _xfree(void **item, const char *file, int line, const char *func)
 
 	if (*item != NULL) {
 		/* magic cookie still there? */
-		xmalloc_assert(p[0] == XMALLOC_MAGIC, file, line, func);	
+		xmalloc_assert(p[0] == XMALLOC_MAGIC);	
 		MALLOC_LOCK();
 		free(p);
 		MALLOC_UNLOCK();
--- src/common/xmalloc.h
+++ src/common/xmalloc.h
@@ -1,12 +1,47 @@
 /* $Id$ */
 
 /* 
-** enhanced malloc routines for slurm
-** 
-**  o attempt to report file, line, and calling function on error
-**  o use configurable slurm log facility for reporting error
-**
-*/
+ * xmalloc: enhanced malloc routines for slurm
+ * 
+ *  o default: never return if errors are encountered.
+ *
+ *  o attempt to report file, line, and calling function on assertion failure
+ *
+ *  o use configurable slurm log facility for reporting errors
+ *
+ * Description:
+ *
+ * void *xmalloc(size_t size);
+ * void *try_xmalloc(size_t size);
+ * void xrealloc(void *p, size_t newsize);
+ * int  try_xrealloc(void *p, size_t newsize);
+ * void xfree(void *p);
+ * int  xsize(void *p);
+ *
+ * xmalloc(size) allocates size bytes and returns a pointer to the allocated
+ * memory. The memory is set to zero. xmalloc() will not return unless
+ * there are no errors. The memory must be freed using xfree().
+ *
+ * try_xmalloc(size) is the same as above, but a NULL pointer is returned
+ * when there is an error allocating the memory.
+ *
+ * xrealloc(p, newsize) changes the size of the block pointed to by p to the
+ * value of newsize. Newly allocated memory is not zeroed. If p is NULL, 
+ * xrealloc() performs the same function as  `p = xmalloc(newsize)'. If p 
+ * is not NULL, it is required to have been initialized with a call to 
+ * [try_]xmalloc() or [try_]xrealloc().
+ *
+ * try_xrealloc(p, newsize) is the same as above, but returns <= 0 if the
+ * there is an error allocating the requested memory.
+ * 
+ * xfree(p) frees the memory block pointed to by p. The memory must have been
+ * initialized with a call to [try_]xmalloc() or [try_]xrealloc().
+ *
+ * xsize(p) returns the current size of the memory allocation pointed to by
+ * p. The memory must have been allocated with [try_]xmalloc() or 
+ * [try_]xrealloc().
+ *
+ */
 
 #ifndef _XMALLOC_H
 #define _XMALLOC_H
@@ -18,21 +53,31 @@
 #include "macros.h"
 
 #define xmalloc(__sz) 		\
-         _xmalloc (__sz, __FILE__, __LINE__, __CURRENT_FUNC__)
+	_xmalloc (__sz, __FILE__, __LINE__, __CURRENT_FUNC__)
+
 #define try_xmalloc(__sz)	\
-	 _try_xmalloc(__sz, __FILE__, __LINE__, __CURRENT_FUNC__)
+	_try_xmalloc(__sz, __FILE__, __LINE__, __CURRENT_FUNC__)
+
 #define xfree(__p)		\
-         _xfree((void **)&(__p), __FILE__, __LINE__, __CURRENT_FUNC__)
+	_xfree((void **)&(__p), __FILE__, __LINE__, __CURRENT_FUNC__)
+
 #define xrealloc(__p, __sz) 	\
-         _xrealloc((void **)&(__p), __sz, __FILE__, __LINE__, __CURRENT_FUNC__)
+        _xrealloc((void **)&(__p), __sz, __FILE__, __LINE__, __CURRENT_FUNC__)
+
+#define try_xrealloc(__p, __sz) \
+	_try_xrealloc((void **)&(__p), __sz, __FILE__, __LINE__, \
+		      __CURRENT_FUNC__)
+
 #define xsize(__p)		\
-	 _xsize((void *)__p, __FILE__, __LINE__, __CURRENT_FUNC__)
+	_xsize((void *)__p, __FILE__, __LINE__, __CURRENT_FUNC__)
 
 void *_xmalloc(size_t size, const char *file, int line, const char *func);
 void *_try_xmalloc(size_t size, const char *file, int line, const char *func);
 void _xfree(void **p, const char *file, int line, const char *func);
 void _xrealloc(void **p, size_t newsize, 
 	       const char *file, int line, const char *func);
+int  _try_xrealloc(void **p, size_t newsize,
+		   const char *file, int line, const char *func);
 int _xsize(void *p, const char *file, int line, const char *func);
 
 #define XMALLOC_MAGIC 0x42
