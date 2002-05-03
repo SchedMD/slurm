ff2ef75e7d94c96fcbe00973a6c4b9bb0fe8218e
Moe Jette <jette1@llnl.gov>
2002-05-03 23:01:06 +0000

    Change in comment of api/allocate.c Added string pack/unpack functions and set of tests. - Jette

--- trunk/src/common/pack.h
+++ trunk/src/common/pack.h
@@ -10,6 +10,9 @@ void	_unpack32(uint32_t *valp, void **bufp, int *lenp);
 void	_pack16(uint16_t val, void **bufp, int *lenp);
 void	_unpack16(uint16_t *valp, void **bufp, int *lenp);
 
+void	_packstr(char *valp, uint32_t size_val, void **bufp, int *lenp);
+void	_unpackstr(char **valp, uint32_t *size_valp, void **bufp, int *lenp);
+
 #define pack32(val,bufp,lenp) do {			\
 	assert(sizeof(val) == sizeof(uint32_t)); 	\
 	assert((bufp) != NULL && *(bufp) != NULL);	\
@@ -42,6 +45,24 @@ void	_unpack16(uint16_t *valp, void **bufp, int *lenp);
         assert((lenp) != NULL);				\
         assert(*(lenp) >= sizeof(*(valp)));		\
 	_unpack16(valp,bufp,lenp);			\
+} while (0)
+
+#define packstr(valp,size_val,bufp,lenp) do {		\
+	assert(size_val == 0 || valp != NULL);		\
+	assert(sizeof(size_val) == sizeof(uint32_t));	\
+	assert((bufp) != NULL && *(bufp) != NULL);	\
+        assert((lenp) != NULL);				\
+        assert(*(lenp) >= (sizeof(size_val)+size_val));	\
+	_packstr(valp,size_val,bufp,lenp);		\
+} while (0)
+
+#define unpackstr(valp,size_valp,bufp,lenp) do {	\
+	assert(valp != NULL);				\
+	assert(sizeof(size_valp) == sizeof(uint32_t *));\
+	assert((bufp) != NULL && *(bufp) != NULL);	\
+        assert((lenp) != NULL);				\
+        assert(*(lenp) >= sizeof(uint32_t));		\
+	_unpackstr(valp,size_valp,bufp,lenp);		\
 } while (0)
 
 #endif /* _PACK_INCLUDED */
