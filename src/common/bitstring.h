cb35def20795beb81fb85be2b583504dd726abfa
Mark Grondona <mgrondona@llnl.gov>
2002-05-06 22:33:21 +0000

    o conditionally include config.h  o include inittypes.h if available (for systems w/out stdint.h)

--- src/common/bitstring.h
+++ src/common/bitstring.h
@@ -15,10 +15,21 @@
  * bitstrings are zero origin
  */
 
-#include <stdint.h>
-
 #ifndef _BITSTRING_H_
 #define	_BITSTRING_H_
+
+#if HAVE_CONFIG_H
+#  include <config.h>
+#  if HAVE_INTTYPES_H
+#    include <inttypes.h>
+#  else
+#    if HAVE_STDINT_H
+#      include <stdint.h>
+#    endif
+#  endif  /* HAVE_INTTYPES_H */
+#else	/* !HAVE_CONFIG_H */
+#  include <inttypes.h>
+#endif  /*  HAVE_CONFIG_H */
 
 #define BITSTR_SHIFT_WORD8	3
 #define BITSTR_SHIFT_WORD32	5
