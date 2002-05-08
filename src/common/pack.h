10de818aa1ae6dd193538fe5b37186dc9b419eb5
Mark Grondona <mgrondona@llnl.gov>
2002-05-08 16:54:26 +0000

    o changed name of (un)packstr to more general (un)packmem, (un)packstr*    are now macros that call the *mem functions  o removed unneeded size argument from packstr

--- src/common/pack.h
+++ src/common/pack.h
@@ -13,7 +13,9 @@
 #    include <stdint.h>
 #  endif
 #endif  /* HAVE_INTTYPES_H */
-#endif  /* HAVE_CONFIG_H   */
+#else   /* !HAVE_CONFIG_H */
+#include <stdint.h>
+#endif  /* HAVE_CONFIG_H */
 
 #include <assert.h>
 
@@ -23,9 +25,9 @@ void	_unpack32(uint32_t *valp, void **bufp, int *lenp);
 void	_pack16(uint16_t val, void **bufp, int *lenp);
 void	_unpack16(uint16_t *valp, void **bufp, int *lenp);
 
-void	_packstr(char *valp, uint16_t size_val, void **bufp, int *lenp);
-void	_unpackstr_ptr(char **valp, uint16_t *size_valp, void **bufp, int *lenp);
-void	_unpackstr_xmalloc(char **valp, uint16_t *size_valp, void **bufp, int *lenp);
+void	_packmem(char *valp, uint16_t size_val, void **bufp, int *lenp);
+void	_unpackmem_ptr(char **valp, uint16_t *size_valp, void **bufp, int *lenp);
+void	_unpackmem_xmalloc(char **valp, uint16_t *size_valp, void **bufp, int *lenp);
 
 #define pack32(val,bufp,lenp) do {			\
 	assert(sizeof(val) == sizeof(uint32_t)); 	\
@@ -61,31 +63,43 @@ void	_unpackstr_xmalloc(char **valp, uint16_t *size_valp, void **bufp, int *lenp
 	_unpack16(valp,bufp,lenp);			\
 } while (0)
 
-#define packstr(valp,size_val,bufp,lenp) do {		\
+#define packmem(valp,size_val,bufp,lenp) do {		\
 	assert(size_val == 0 || valp != NULL);		\
-	assert(sizeof(size_val) == sizeof(uint16_t));	\
 	assert((bufp) != NULL && *(bufp) != NULL);	\
         assert((lenp) != NULL);				\
         assert(*(lenp) >= (sizeof(size_val)+size_val));	\
-	_packstr(valp,size_val,bufp,lenp);		\
+	_packmem(valp,(uint16_t)size_val,bufp,lenp);	\
 } while (0)
 
-#define unpackstr_ptr(valp,size_valp,bufp,lenp) do {	\
+#define packstr(str,bufp,lenp) do {			\
+	uint16_t _size = (uint16_t)(strlen(str)+1);	\
+        assert(_size == 0 || str != NULL);		\
+	assert((bufp) != NULL && *(bufp) != NULL);	\
+        assert((lenp) != NULL);				\
+        assert(*(lenp) >= (sizeof(_size)+_size));	\
+	_packmem(str,(uint16_t)_size,bufp,lenp);	\
+} while (0)				
+
+#define unpackmem_ptr(valp,size_valp,bufp,lenp) do {	\
 	assert(valp != NULL);				\
 	assert(sizeof(size_valp) == sizeof(uint16_t *));\
 	assert((bufp) != NULL && *(bufp) != NULL);	\
         assert((lenp) != NULL);				\
         assert(*(lenp) >= sizeof(uint32_t));		\
-	_unpackstr_ptr(valp,size_valp,bufp,lenp);	\
+	_unpackmem_ptr(valp,(uint16_t *)size_valp,bufp,lenp);\
 } while (0)
 
-#define unpackstr_xmalloc(valp,size_valp,bufp,lenp) do {\
+#define unpackstr_ptr		unpackmem_ptr
+
+#define unpackmem_xmalloc(valp,size_valp,bufp,lenp) do {\
 	assert(valp != NULL);				\
 	assert(sizeof(size_valp) == sizeof(uint16_t *));\
 	assert((bufp) != NULL && *(bufp) != NULL);	\
         assert((lenp) != NULL);				\
         assert(*(lenp) >= sizeof(uint32_t));		\
-	_unpackstr_xmalloc(valp,size_valp,bufp,lenp);	\
+	_unpackmem_xmalloc(valp,(uint16_t *)size_valp,bufp,lenp);\
 } while (0)
+
+#define unpackstr_xmalloc	unpackmem_xmalloc
 
 #endif /* _PACK_INCLUDED */
