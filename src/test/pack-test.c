00f975dd9c5e399a1ddc941a1490f6d9809abc22
Mark Grondona <mgrondona@llnl.gov>
2002-05-08 17:07:01 +0000

    o test pack of null string

--- src/test/pack-test.c
+++ src/test/pack-test.c
@@ -26,21 +26,20 @@ int main (int argc, char *argv[])
         uint32_t test32 = 5678, out32, byte_cnt;
         char testbytes[] = "TEST BYTES", *outbytes;
 	char teststring[] = "TEST STRING",  *outstring = NULL;
+	char *nullstr = NULL;
 
         bufp = buffer;
         len_buf = sizeof(buffer);
         pack16(test16, &bufp, &len_buf);
         pack32(test32, &bufp, &len_buf);
-        packstr(testbytes, (uint32_t)(strlen(testbytes)+1), &bufp, &len_buf);
-        packstr(teststring, (uint32_t)(strlen(teststring)+1), &bufp, &len_buf);
+        packstr(testbytes, &bufp, &len_buf);
+        packstr(teststring, &bufp, &len_buf);
+	packstr(nullstr, &bufp, &len_buf);
         printf("wrote %d bytes\n", len_buf);
 
         bufp = buffer;
         len_buf = sizeof(buffer);
         unpack16(&out16, &bufp, &len_buf);
-        unpack32(&out32, &bufp, &len_buf);
-        unpackstr_ptr(&outbytes, &byte_cnt, &bufp, &len_buf);
-        unpackstr_xmalloc(&outstring, &byte_cnt, &bufp, &len_buf);
 
         if (out16 != test16) {
                 printf("un/pack16 failed\n");
@@ -48,22 +47,39 @@ int main (int argc, char *argv[])
 	} else
 		passed++;
 
+        unpack32(&out32, &bufp, &len_buf);
+
         if (out32 != test32) {
                 printf("un/pack32 failed\n");
+		failed++;
 	} else
 		passed++;
+
+        unpackstr_ptr(&outbytes, &byte_cnt, &bufp, &len_buf);
 
         if (strcmp(testbytes, outbytes) != 0) {
                 printf("un/packstr_ptr failed\n");
+		failed++;
 	} else
 		passed++;
 
+        unpackstr_xmalloc(&outstring, &byte_cnt, &bufp, &len_buf);
+
         if (strcmp(teststring, outstring) != 0) {
                 printf("un/packstr_xmalloc failed\n");
+		failed++;
 	} else {
 		passed++;
 		xfree(outstring);
 	}
+
+	unpackstr_xmalloc(&nullstr, &byte_cnt, &bufp, &len_buf);
+
+	if (nullstr != NULL) {
+		printf("un/packstr of null string failed\n");
+		failed++;
+	} else 
+		passed++;
 
         printf("%d tests passed, %d failed.\n", passed, failed);
 
