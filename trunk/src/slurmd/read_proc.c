f66042ed245e0db5cce9e9e0ec1e79ce4517b8a1
Moe Jette <jette1@llnl.gov>
2002-05-15 00:40:01 +0000

    Convert slurmd code to linux kernel coding standards.

--- trunk/src/slurmd/read_proc.c
+++ trunk/src/slurmd/read_proc.c
@@ -23,307 +23,309 @@
 #include "slurm.h"
 #define SESSION_RECS 50
 
-int Hertz = 0;
+int hertz = 0;
 
-int Iteration = -1;
-int Session_Record_Count = 0;
-struct Session_Record {
-    int Uid;
-    int Session;
-    long unsigned Time;		/* Total system and user time, all processes */
-    long RSS;			/* Total Resident Set Size, all processes */
-    int Iteration;		/* Defunct records have value -1 */
-    int Processes;		/* Count of processes */
+int iteration = -1;
+int sess_rec_cnt = 0;
+struct sess_record {
+    int uid;
+    int session;
+    long unsigned time;		/* Total system and user time, all processes */
+    long resident_set_size;	/* Total Resident Set Size, all processes */
+    int iteration;		/* Defunct records have value -1 */
+    int processes;		/* Count of processes */
 };
-struct Session_Record *Session_Ptr;
+struct sess_record *session_ptr;
 
 #define BUF_SIZE 1024
 #define DEBUG_MODULE 1
 #define DEBUG_SYSTEM 1
 
-int Dump_Proc(int uid, int sid);
-int Init_Proc();
-int Parse_Proc_Stat(char* Proc_Stat, int *Session, long unsigned *Time, long *RSS);
-int Read_Proc();
+int dump_proc(int uid, int sid);
+int init_proc();
+int parse_proc_stat(char* proc_stat, int *session, 
+		long unsigned *time, long *resident_set_size);
+int read_proc();
 
 #if DEBUG_MODULE
 /* main is used here for testing purposes only */
-main(int argc, char * argv[]) {
-    int Error_Code, i, iterations, uid;
-    char Out_Line[BUF_SIZE];
+int 
+main(int argc, char * argv[]) 
+{
+	int error_code, i, iterations, uid;
+	if ((argc < 2) || (argc > 3)) {
+		printf ("Usage: %s <iterations> [<uid>]\n", argv[0]);
+		exit (0);
+	} 
+	if (argc == 3) 
+		uid = atoi (argv[2]);
+	else
+		uid = -1;
+	iterations = atoi (argv[1]);
+	error_code = init_proc ();
+	if (error_code != 0) {
+		printf("Error %d from init_proc\n", error_code);
+		exit(1);
+	} 
 
-    if ((argc < 2) || (argc > 3)) {
-	printf("Usage: %s <iterations> [<uid>]\n", argv[0]);
-	exit(0);
-    } /* if */
-    if (argc == 3) 
-	uid = atoi(argv[2]);
-    else
-	uid = -1;
-    iterations = atoi(argv[1]);
-    Error_Code = Init_Proc();
-    if (Error_Code != 0) {
-	printf("Error %d from Init_Proc\n", Error_Code);
-	exit(1);
-    } /* if */
-
-    for (i=0; i<iterations; i++) {
-	if (i > 0) {
-	    sleep(10);
-	    printf("\n\n");
-	 } /* if */
-	Error_Code = Read_Proc();
-	if (Error_Code != 0) {
-	    printf("Error %d from Read_Proc\n", Error_Code);
-	    exit(1);
-	 } /* if */
-	Error_Code = Dump_Proc(uid, -1);
-	if (Error_Code != 0) {
-	    printf("Error %d from Dump_Proc\n", Error_Code);
-	    exit(1);
-	 } /* if */
-    } /* for (i */
-
-    exit(0);
-} /* main */
+	for (i=0; i<iterations; i++) {
+		if (i > 0) {
+			sleep (10);
+			printf("\n\n");
+		} 
+		error_code = read_proc ();
+		if (error_code != 0) {
+			printf ("Error %d from read_proc\n", error_code);
+			exit (1);
+		} 
+		error_code = dump_proc (uid, -1);
+		if (error_code != 0) {
+			printf ("Error %d from dump_proc\n", error_code);
+			exit (1);
+		} 
+	}
+	exit (0);
+}
 #endif
 
 
 /*
- * Dump_Proc - Print the contents of the Process table
+ * dump_proc - Print the contents of the Process table
  * Input: uid - optional UID filter, enter -1 if no uid filter
- * 	sid - optional Session ID filter, enter -1 if no SID filter
+ * 	sid - optional session ID filter, enter -1 if no SID filter
  * Output: Return code is zero or errno
  */
-int Dump_Proc(int uid, int sid) {
-    struct Session_Record *S_Ptr;
+int 
+dump_proc(int uid, int sid) 
+{
+	struct sess_record *s_ptr;
 
-    for (S_Ptr=Session_Ptr; S_Ptr<(Session_Ptr+Session_Record_Count); S_Ptr++) {
-	if (S_Ptr->Iteration == -1) continue;   /* Defunct */
-	if ((uid != -1) && (uid != S_Ptr->Uid)) continue;
-	if ((sid != -1) && (sid != S_Ptr->Session)) continue;
-	printf("Uid=%d Session=%d Time=%lu RSS=%ld Iteration=%d Processes=%d\n", 
-		S_Ptr->Uid, S_Ptr->Session, S_Ptr->Time, S_Ptr->RSS, S_Ptr->Iteration, S_Ptr->Processes);
-    } /* for */
-    return 0;
-} /* Dump_Proc */
+	for (s_ptr=session_ptr; s_ptr<(session_ptr+sess_rec_cnt); s_ptr++) {
+		if (s_ptr->iteration == -1) 
+			continue;   /* Defunct */
+		if ((uid != -1) && (uid != s_ptr->uid)) 
+			continue;
+		if ((sid != -1) && (sid != s_ptr->session))
+			continue;
+		printf ("uid=%d session=%d time=%lu resident_set_size=%ld iteration=%d processes=%d\n", 
+			s_ptr->uid, s_ptr->session, s_ptr->time, 
+			s_ptr->resident_set_size, s_ptr->iteration, s_ptr->processes);
+	}
+	return 0;
+}
 
 
 /*
- * Init_Proc - Initialize the Session_Record data structure OR increase size as needed
+ * init_proc - Initialize the sess_record data structure OR increase size as needed
  * Input: none
  * Output: Return code is zero or errno
  */
-int Init_Proc() {
-    struct Session_Record *S_Ptr;
+int 
+init_proc() 
+{
+	struct sess_record *s_ptr;
 
-    if (Session_Record_Count == 0)
-	Session_Ptr = (struct Session_Record *)malloc(sizeof(struct Session_Record) * SESSION_RECS);
-    else
-	Session_Ptr = (struct Session_Record *)realloc(Session_Ptr, sizeof(struct Session_Record) * (Session_Record_Count+SESSION_RECS));
+	if (sess_rec_cnt == 0)
+		session_ptr = (struct sess_record *) 
+			malloc (sizeof (struct sess_record) * SESSION_RECS);
+	else
+		session_ptr = (struct sess_record *)
+			realloc (session_ptr, sizeof (struct sess_record) * 
+				(sess_rec_cnt+SESSION_RECS));
 
-    if (Session_Ptr == NULL) {
-#if DEBUG_SYSTEM
-	fprintf(stderr, "Init_Proc: unable to allocate memory\n");
-#else
-	syslog(LOG_ALARM, "Init_Proc: unable to allocate memory\n");
-#endif
-	exit(1);
-    } /* if */
+	if (session_ptr == NULL) {
+		error ("init_proc: unable to allocate memory\n");
+		exit(1);
+	} 
 
-    for (S_Ptr=(Session_Ptr+Session_Record_Count); S_Ptr<(Session_Ptr+Session_Record_Count+SESSION_RECS); S_Ptr++) {
-	S_Ptr->Iteration = -1;
-    } /* for */
-    Session_Record_Count += SESSION_RECS;
-    return 0;
-} /* Init_Proc */
+	for (s_ptr= (session_ptr+sess_rec_cnt); 
+	     s_ptr<(session_ptr+sess_rec_cnt+SESSION_RECS); s_ptr++) {
+		s_ptr->iteration = -1;
+	} 
+	sess_rec_cnt += SESSION_RECS;
+	return 0;
+}
 
 
 /*
- * Parse_Proc_Stat - Break out all of a process' information from the stat file
- * Input: Proc_Stat - Process status info read from /proc/<pid>/stat
- *	Session - Location into which the Session ID is written
- *	Time - Location into which total user and system time (in seconds) is written
- *	RSS - Location into which the Resident Set Size is written
+ * parse_proc_stat - Break out all of a process' information from the stat file
+ * Input: proc_stat - Process status info read from /proc/<pid>/stat
+ *	session - Location into which the session ID is written
+ *	time - Location into which total user and system time (in seconds) is written
+ *	resident_set_size - Location into which the Resident Set Size is written
  * Output: Exit code is zero or errno
- *	Fill in contents of Session, Time, and RSS
+ *	Fill in contents of session, time, and resident_set_size
  */
-int Parse_Proc_Stat(char* Proc_Stat, int *Session, long unsigned *Time, long *RSS) {
-    int Pid, PPid, PGrp, TTY, TPGID;
-    char Cmd[16], State[1];
-    long unsigned Flags, Min_Flt, CMin_Flt, Maj_Flt, CMaj_Flt, UTime, STime;
-    long CuTime, CsTime, Priority, Nice, Timeout, It_Real_Value;
-    long unsigned Start_Time, VSize;
-    long unsigned RSS_RLim, Start_Code, End_Code, Start_Stack, KStk_Esp, KStk_Eip;
-    long unsigned WChan, NSwap, CnSwap;
-    int  LProc;
-    int num;
-    char *str_ptr;
+int 
+parse_proc_stat(char* proc_stat, int *session, long unsigned *time, 
+	long *resident_set_size) {
+	int pid, ppid, pgrp, tty, tpgid;
+	char cmd[16], state[1];
+	long unsigned flags, min_flt, cmin_flt, maj_flt, cmaj_flt, utime, stime;
+	long cutime, cstime, priority, nice, timeout, it_real_value;
+	long unsigned start_time, vsize;
+	long unsigned resident_set_size_rlim, start_code, end_code;
+	long unsigned start_stack, kstk_esp, kstk_eip;
+	long unsigned w_chan, n_swap, sn_swap;
+	int  l_proc;
+	int num;
+	char *str_ptr;
     
-    str_ptr = (char *)strrchr(Proc_Stat, ')');		/* split into "PID (cmd" and "<rest>" */
-    *str_ptr = '\0';			/* replace trailing ')' with NUL */
-    /* parse these two strings separately, skipping the leading "(". */
-    memset(Cmd, 0, sizeof(Cmd));
-    sscanf(Proc_Stat, "%d (%15c", &Pid, Cmd);   /* comm[16] in kernel */
-    num = sscanf(str_ptr + 2,			/* skip space after ')' too */
-       "%c "
-       "%d %d %d %d %d "
-       "%lu %lu %lu %lu %lu %lu %lu "
-       "%ld %ld %ld %ld %ld %ld "
-       "%lu %lu "
-       "%ld "
-       "%lu %lu %lu %lu %lu %lu "
-       "%*s %*s %*s %*s " /* discard, no RT signals & Linux 2.1 used hex */
-       "%lu %lu %lu %*d %d",
-       &State,
-       &PPid, &PGrp, Session, &TTY, &TPGID,
-       &Flags, &Min_Flt, &CMin_Flt, &Maj_Flt, &CMaj_Flt, &UTime, &STime, 
-       &CuTime, &CsTime, &Priority, &Nice, &Timeout, &It_Real_Value,
-       &Start_Time, &VSize,
-       RSS,
-       &RSS_RLim, &Start_Code, &End_Code, &Start_Stack, &KStk_Esp, &KStk_Eip,
-/*     &Signal, &Blocked, &SigIgnore, &SigCatch,   */ /* can't use */
-       &WChan, &NSwap, &CnSwap /* , &Exit_Signal  */, &LProc);
-    *Time = (UTime + STime) / Hertz;
-} /* Parse_Proc_Stat */
+	/* split into "PID (cmd" and "<rest>" */
+	str_ptr = (char *)strrchr(proc_stat, ')'); 
+	*str_ptr = '\0';		/* replace trailing ')' with NULL */
+	/* parse these two strings separately, skipping the leading "(". */
+	memset (cmd, 0, sizeof(cmd));
+	sscanf (proc_stat, "%d (%15c", &pid, cmd);   /* comm[16] in kernel */
+	num = sscanf(str_ptr + 2,		/* skip space after ')' too */
+		"%c "
+		"%d %d %d %d %d "
+		"%lu %lu %lu %lu %lu %lu %lu "
+		"%ld %ld %ld %ld %ld %ld "
+		"%lu %lu "
+		"%ld "
+		"%lu %lu %lu "
+		"%lu %lu %lu "
+		"%*s %*s %*s %*s " /* discard, no RT signals & Linux 2.1 used hex */
+		"%lu %lu %lu %*d %d",
+		state,
+		&ppid, &pgrp, session, &tty, &tpgid,
+		&flags, &min_flt, &cmin_flt, &maj_flt, &cmaj_flt, &utime, &stime, 
+		&cutime, &cstime, &priority, &nice, &timeout, &it_real_value,
+		&start_time, &vsize,
+		resident_set_size,
+		&resident_set_size_rlim, &start_code, &end_code, 
+		&start_stack, &kstk_esp, &kstk_eip,
+/*		&signal, &blocked, &sig_ignore, &sig_catch,   */ /* can't use */
+		&w_chan, &n_swap, &sn_swap /* , &Exit_signal  */, &l_proc);
+	*time = (utime + stime) / hertz;
+	return 0;
+}
 
 /* 
- * Read_Proc - Read into a table key information about every process on the system
+ * read_proc - Read into a table key information about every process on the system
  * Input: none
  * Output: Return code is zero or errno
  */
-int Read_Proc() {
-    DIR *Proc_FS;
-    struct dirent *Proc_Ent;
-    int Proc_FD, Proc_Stat_Size, found, n;
-    char Proc_Name[22], *Proc_Stat;
-    struct stat Buffer;
-    int Uid, Session;
-    long RSS;
-    long unsigned Time;
-    struct Session_Record *S_Ptr, *Sess_Free;
+int 
+read_proc() 
+{
+	DIR *proc_fs;
+	struct dirent *proc_ent;
+	int proc_fd, proc_stat_size, found, n;
+	char proc_name[22], *proc_stat;
+	struct stat buffer;
+	int uid, session;
+	long resident_set_size;
+	long unsigned time;
+	struct sess_record *s_ptr, *sess_free;
 
-    /* Initialization */
-    if (Hertz == 0) {
-	Hertz = sysconf(_SC_CLK_TCK);
-	if (Hertz == 0) {
-#if DEBUG_SYSTEM
-	    fprintf(stderr, "Read_Proc: unable to get clock rate\n");
-#else
-	    syslog(LOG_ALERT, "Read_Proc: unable to get clock rate\n");
-#endif
-	    Hertz = 100;
-	} /* if */
-    } /* if */
-    Proc_Stat_Size = BUF_SIZE;
-    Proc_Stat = (char *)malloc(Proc_Stat_Size);
-    if (Proc_Stat == NULL) {
-#if DEBUG_SYSTEM
-	fprintf(stderr, "Read_Proc: unable to allocate memory\n");
-#else
-	syslog(LOG_ALERT, "Read_Proc: unable to allocate memory\n");
-#endif
-	return ENOMEM;
-    } /* if */
-    Proc_FS = opendir("/proc");
-    if (Proc_FS == NULL) {
-#if DEBUG_SYSTEM
-	fprintf(stderr, "Read_Proc: opendir unable to open /proc, errno=%d\n", errno);
-#else
-	syslog(LOG_ALERT, "Read_Proc: opendir unable to open /proc, errno=%d\n", errno);
-#endif
-    } /* if */
-    Iteration++;
-
-    /* Read the entries */
-    while ((Proc_Ent = readdir(Proc_FS)) != (struct dirent *)NULL) {
-	if (Proc_Ent->d_name[0] < '0') continue;	/* Not "real" process ID */
-	if (Proc_Ent->d_name[0] > '9') continue;	/* Not "real" process ID */
-	if (strlen(Proc_Ent->d_name) > 10) {
-	    /* If you see this, make Proc_Name longer and change this value */
-#if DEBUG_SYSTEM
-	    fprintf(stderr, "Read_Proc: process ID number too long\n");
-#else
-	    syslog(LOG_ERR, "Read_Proc: process ID number too long\n");
-#endif  
-	    continue;
-	} /* if */
-	sprintf(Proc_Name, "/proc/%s/stat", Proc_Ent->d_name);
-	Proc_FD = open(Proc_Name, O_RDONLY, 0);
-	if (Proc_FD == -1) continue;  /* process is now gone */
-	while ((n = read(Proc_FD, Proc_Stat, Proc_Stat_Size)) > 0) {
-	    if (n < (Proc_Stat_Size-1)) break;
-	    Proc_Stat_Size += BUF_SIZE;
-	    Proc_Stat = (char *)realloc(Proc_Stat, Proc_Stat_Size);
-	    if (Proc_Stat == NULL) {
-#if DEBUG_SYSTEM
-		fprintf(stderr, "Read_Proc: unable to allocate memory\n");
-#else
-		syslog(LOG_ALERT, "Read_Proc: unable to allocate memory\n");
-#endif
-		close(Proc_FD);
-		closedir(Proc_FS);
+	/* Initialization */
+	if (hertz == 0) {
+		hertz = sysconf(_SC_CLK_TCK);
+		if (hertz == 0) {
+			error ("read_proc: unable to get clock rate\n");
+			hertz = 100;
+		} 
+	} 
+	proc_stat_size = BUF_SIZE;
+	proc_stat = (char *) malloc(proc_stat_size);
+	if (proc_stat == NULL) {
+		error ("read_proc: unable to allocate memory\n");
 		return ENOMEM;
-	    } /* if */
-	    if (lseek(Proc_FD, (off_t) 0, SEEK_SET) != 0) break;
+	} 
+	proc_fs = opendir("/proc");
+	if (proc_fs == NULL)
+		error ("read_proc: opendir unable to open /proc, errno=%d\n", errno);
+	iteration++;
+
+	/* Read the entries */
+	while ((proc_ent = readdir(proc_fs)) != (struct dirent *)NULL) {
+		if (proc_ent->d_name[0] < '0') 
+			continue;	/* Not "real" process ID */
+		if (proc_ent->d_name[0] > '9') 
+			continue;	/* Not "real" process ID */
+		if (strlen(proc_ent->d_name) > 10) {
+			/* make proc_name longer and change this value */
+			error ("read_proc: process ID number too long\n");
+			continue;
+		} 
+		sprintf (proc_name, "/proc/%s/stat", proc_ent->d_name);
+		proc_fd = open (proc_name, O_RDONLY, 0);
+		if (proc_fd == -1) 
+			continue;  /* process is now gone */
+		while ((n = read(proc_fd, proc_stat, proc_stat_size)) > 0) {
+			if (n < (proc_stat_size-1)) break;
+			proc_stat_size += BUF_SIZE;
+			proc_stat = (char *)realloc(proc_stat, proc_stat_size);
+			if (proc_stat == NULL) {
+				error ("read_proc: unable to allocate memory\n");
+				close(proc_fd);
+				closedir(proc_fs);
+				return ENOMEM;
+			} 
+			if (lseek(proc_fd, (off_t) 0, SEEK_SET) != 0) 
+				break;
+		}
+		fstat(proc_fd, &buffer);
+		close(proc_fd);
+		if (n <= 0) 
+			continue;
+		uid = buffer.st_uid;
+		parse_proc_stat (proc_stat, &session, &time, &resident_set_size);
+		found = 0;
+		sess_free = NULL;
+		for (s_ptr=session_ptr; s_ptr<(session_ptr+sess_rec_cnt); s_ptr++) {
+			if (s_ptr->iteration == -1) {
+				if (sess_free == NULL) 
+					sess_free = s_ptr;
+				continue;
+			} 
+			if (s_ptr->session != session) 
+				continue;
+			if (s_ptr->iteration != iteration) {
+				s_ptr->iteration = iteration;
+				s_ptr->processes = 0;
+				s_ptr->resident_set_size       = 0;
+				s_ptr->time      = 0;
+			} 
+			s_ptr->processes += 1;
+			s_ptr->resident_set_size       += resident_set_size;
+			s_ptr->time      += time;
+			found = 1;
+			break;
+		} 
+		if (found == 0) {
+			if (sess_free == NULL) {
+				if (init_proc() != 0)
+					exit(1); /* no memory, we're dead */
+				for (s_ptr=session_ptr; 
+				     s_ptr<(session_ptr+sess_rec_cnt); s_ptr++) {
+					if (s_ptr->iteration != -1) 
+						continue;
+					sess_free = s_ptr;
+					break;
+				} 
+				if (sess_free == NULL) {
+					error ("read_proc: Internal error\n");
+					exit(1);
+				} 
+			} 
+			sess_free->iteration = iteration;
+			sess_free->processes = 1;
+			sess_free->resident_set_size       = resident_set_size;
+			sess_free->session   = session;
+			sess_free->time      = time;
+			sess_free->uid       = uid;
+		} 
 	} /* while */
-	fstat(Proc_FD, &Buffer);
-	close(Proc_FD);
-	if (n <= 0) continue;
-	Uid = Buffer.st_uid;
-	Parse_Proc_Stat(Proc_Stat, &Session, &Time, &RSS);
-	found = 0;
-	Sess_Free = NULL;
-	for (S_Ptr=Session_Ptr; S_Ptr<(Session_Ptr+Session_Record_Count); S_Ptr++) {
-	    if (S_Ptr->Iteration == -1) {
-		if (Sess_Free == NULL) Sess_Free = S_Ptr;
-		continue;
-	    } /* if */
-	    if (S_Ptr->Session != Session) continue;
-	    if (S_Ptr->Iteration != Iteration) {
-		S_Ptr->Iteration = Iteration;
-		S_Ptr->Processes = 0;
-		S_Ptr->RSS       = 0;
-		S_Ptr->Time      = 0;
-	    } /* if */
-	    S_Ptr->Processes += 1;
-	    S_Ptr->RSS       += RSS;
-	    S_Ptr->Time      += Time;
-	    found = 1;
-	    break;
-	} /* for */
-	if (found == 0) {
-	    if (Sess_Free == NULL) {
-		if (Init_Proc() != 0) exit(1); /* no memory, we're dead */
-		for (S_Ptr=Session_Ptr; S_Ptr<(Session_Ptr+Session_Record_Count); S_Ptr++) {
-		    if (S_Ptr->Iteration != -1) continue;
-		    Sess_Free = S_Ptr;
-		    break;
-		} /* for */
-		if (Sess_Free == NULL) {
-#if DEBUG_SYSTEM
-		    fprintf(stderr, "Read_Proc: Internal error\n");
-#else
-		    syslog(LOG_ERR, "Read_Proc: Internal error\n");
-#endif  
-		    exit(1);
-		} /* if */
-	    } /* if */
-	    Sess_Free->Iteration = Iteration;
-	    Sess_Free->Processes = 1;
-	    Sess_Free->RSS       = RSS;
-	    Sess_Free->Session   = Session;
-	    Sess_Free->Time      = Time;
-	    Sess_Free->Uid       = Uid;
-	} /* if */
-    } /* while */
 
-    /* Termination */
-    free(Proc_Stat);
-    closedir(Proc_FS);
-    for (S_Ptr=Session_Ptr; S_Ptr<(Session_Ptr+Session_Record_Count); S_Ptr++) {
-	if (S_Ptr->Iteration != Iteration) S_Ptr->Iteration=-1;   /* Defunct */
-    } /* for */
-    return 0;
-} /* Read_Proc */
-
+	/* Termination */
+	free(proc_stat);
+	closedir(proc_fs);
+	for (s_ptr=session_ptr; s_ptr<(session_ptr+sess_rec_cnt); s_ptr++) {
+		if (s_ptr->iteration != iteration) 
+			s_ptr->iteration=-1;   /* Defunct */
+	} 
+	return 0;
+}
