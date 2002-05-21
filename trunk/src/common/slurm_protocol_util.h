dc174acce90fa88b90ab595e1363cf800edfe2e7
tewk <tewk@unknown>
2002-05-21 23:16:00 +0000

    added set_slurm_address_htons prototype

--- trunk/src/common/slurm_protocol_util.h
+++ trunk/src/common/slurm_protocol_util.h
@@ -5,4 +5,5 @@
 #include <stdint.h>
 uint32_t check_header_version( header_t * header) ;
 void init_header ( header_t * header , slurm_message_type_t message_type , uint16_t flags ) ;
+void set_slurm_addr_hton ( slurm_addr * slurm_address , uint16_t port , uint32_t ip_address ) ;
 #endif
