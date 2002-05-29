d98a3645aac235c9455f9150de92fce0aa59adb6
tewk <tewk@unknown>
2002-05-29 17:43:20 +0000

    fixed for new abstract communication implementation

--- trunk/src/test/slurm_protocol_message_server.c
+++ trunk/src/test/slurm_protocol_message_server.c
@@ -8,12 +8,14 @@
 int32_t main ( int32_t argc , char * argv[] )
 {
 	/* declare file descriptors */
+	slurm_fd listen_socket ;
 	slurm_fd worker_socket ;
 
 	/* declare address structures */
-	slurm_addr worker_address ;
+	slurm_addr listen_address ;
 	slurm_addr peer_address ;
-	slurm_msg_type_t msg_type ;
+
+	slurm_msg_type_t msg_type;
 
 	uint32_t buffer_len = 1024 ;
 	char buf_temp [ buffer_len ] ;
@@ -23,17 +25,18 @@ int32_t main ( int32_t argc , char * argv[] )
 	uint32_t length_io ;
 		
 	/* init address sturctures */
-	slurm_set_addr_uint ( & worker_address , 7000 , 0x7f000001 ) ;
+	slurm_set_addr_uint ( & listen_address , 7001 , 0x7f000001 ) ;
 	/* open and listen on socket */
-	worker_socket = slurm_init_msg_engine ( & worker_address ) ;
+	listen_socket = slurm_init_msg_engine ( & listen_address ) ;
+	worker_socket = slurm_accept_msg_conn (  listen_socket , & peer_address ) ;
 
-	length_io = slurm_receive_buffer ( worker_socket , & peer_address, & msg_type , buffer , buffer_len ) ;
-	printf ( "Bytes Recieved %i\n", length_io ) ;
 
 	msg_type = 1 ;
-	slurm_set_addr_uint ( & peer_address , 7001 , 0x7f000001 ) ;
 	length_io = slurm_send_node_buffer ( worker_socket , & peer_address, msg_type , test_send , test_send_len ) ;
 	printf ( "Bytes Sent %i\n", length_io ) ;
+	
+	length_io = slurm_receive_buffer ( worker_socket , & peer_address, & msg_type ,  buffer , buffer_len ) ;
+	printf ( "Bytes Recieved %i\n", length_io ) ;
 
 	slurm_shutdown_msg_engine ( worker_socket ) ;
 
