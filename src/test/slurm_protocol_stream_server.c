5a0ca18452fcc234d21061cfdac188c1b5680d4d
tewk <tewk@unknown>
2002-05-30 17:39:54 +0000

    removed stdint depedency

--- src/test/slurm_protocol_stream_server.c
+++ src/test/slurm_protocol_stream_server.c
@@ -1,11 +1,10 @@
 #include <netinet/in.h>
 #include <src/common/slurm_protocol_api.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
-int32_t main ( int32_t argc , char * argv[] )
+int main ( int argc , char * argv[] )
 {
 	/* declare file descriptors */
 	slurm_fd listen_socket ;
@@ -15,12 +14,12 @@ int32_t main ( int32_t argc , char * argv[] )
 	slurm_addr listen_address ;
 	slurm_addr worker_address ;
 
-	uint32_t buffer_len = 1024 ;
+	unsigned int buffer_len = 1024 ;
 	char buf_temp [ buffer_len ] ;
 	char * buffer = buf_temp ;
 	char * test_send = "This is a test of simple socket communication" ;
-	uint32_t test_send_len = strlen ( test_send ) ;
-	uint32_t length_io ;
+	unsigned int test_send_len = strlen ( test_send ) ;
+	unsigned int length_io ;
 		
 	/* init address sturctures */
 	slurm_set_addr_uint ( & listen_address , 7000, 0x7f000001 ) ;
