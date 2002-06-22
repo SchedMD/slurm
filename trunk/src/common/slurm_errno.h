5425bed07730572b10892c73a58d8f3b8318a699
tewk <tewk@unknown>
2002-06-22 20:35:40 +0000

    general slurm error codes

---
+++ trunk/src/common/slurm_errno.h
@@ -0,0 +1,6 @@
+#ifndef _SLURM_ERRNO_H
+#define _SLURM_ERRNO_H
+#include <src/common/slurm_protocol_errno.h>
+#define ESLRUMD_PIPE_ERROR_ON_TASK_SPAWN 		90000
+#define ESLURMD_KILL_TASK_FAILED			90001
+#endif
