e2ecc467db0f48bcfcb3a0c151f139ca602354b7
tewk <tewk@unknown>
2002-07-11 17:04:25 +0000

    Added expand_buffer method and changed variable names to make it more straight forward

--- src/slurmd/circular_buffer.c
+++ src/slurmd/circular_buffer.c
@@ -10,6 +10,7 @@
 #define MAX_BUFFER_SIZE ( ( 8192 * 10 ) )
 
 static int assert_checks ( circular_buffer_t * buf ) ;
+static int expand_buffer ( circular_buffer_t * buf ) ;
 
 int init_cir_buf ( circular_buffer_t ** buf_ptr )
 {
@@ -17,12 +18,14 @@ int init_cir_buf ( circular_buffer_t ** buf_ptr )
 	*buf_ptr = xmalloc ( sizeof ( circular_buffer_t ) ) ;
 	buf = *buf_ptr ;
 	buf -> buffer = xmalloc ( INITIAL_BUFFER_SIZE ) ;
-	
+	buf -> buf_size = INITIAL_BUFFER_SIZE ;
+
 	buf -> start = buf -> buffer ;
-	
-	buf -> begin = buf -> start ;
-	buf -> end = buf -> start ;
-	buf -> tail = buf -> start + buf-> buf_size ;
+	buf -> head = buf -> start ;
+
+	buf -> tail = buf -> start ;
+	buf -> end = buf -> start + buf-> buf_size ;
+
 	buf -> read_size = 0 ;
 	buf -> write_size = INITIAL_BUFFER_SIZE ;
 	return SLURM_SUCCESS ;
@@ -36,45 +39,46 @@ int read_update ( circular_buffer_t * buf , unsigned int size )
 		info ( "zero length read in cirular buffer" ) ;
 		return SLURM_SUCCESS ;
 	}
-	
+
 	/* before modifing the buffer lets do some sanity checks*/
 	assert ( size <= buf-> read_size ) ;
 	assert_checks ( buf ) ;
-	
-	/*modify beginning position of the buffer*/
-	buf->begin = buf->begin + size ;
+
+	/*modify headning position of the buffer*/
+	buf->head = buf->head + size ;
 
 	/* after modifing the buffer lets do some sanity checks*/
 	assert_checks ( buf ) ;
 
 	/* take care of wrap around issues */
-	if ( buf->end > buf->begin ) /* CASE end after begin */
+	if ( buf->tail > buf->head ) /* CASE tail after head */
 	{
-		buf -> read_size = buf -> end - buf -> begin ;
+		buf -> read_size = buf -> tail - buf -> head ;
 	}
-	else if ( buf->end < buf->begin ) /* CASE end befpre begin */
+	else if ( buf->tail < buf->head ) /* CASE tail befpre head */
 	{
-		if ( buf->begin  == buf-> tail ) /* CASE end == tail */
+		if ( buf->head  == buf-> end ) /* CASE tail == end */
 		{
-			if ( buf -> end == buf -> start ) /* CASE begin == start */
-				;
-			/*buffer empty realloc */
+			if ( buf -> tail == buf -> start ) /* CASE head == start */
+			{
+				fatal ( "buffer empty, shrink or adjust code not written" ) ;
+			}
 			else
 			{
-				buf -> begin = buf -> start ;
-				buf -> read_size = buf -> start - buf -> tail ;
+				buf -> head = buf -> start ;
+				buf -> read_size = buf -> start - buf -> end ;
 			}
 		}
 		else
 		{
-			buf -> read_size = buf -> tail - buf -> begin ;
+			buf -> read_size = buf -> end - buf -> head ;
 		}
 	}
-	else if ( buf->end == buf->begin ) /* CASE C */
+	else if ( buf->tail == buf->head ) /* CASE head == tail */
 	{
-			/*buffer empty realloc */
+		fatal ( "buffer empty, shrink or adjust code not written" ) ;
 	}
-	
+
 	/* final sanity check */
 	assert_checks ( buf ) ;
 	return SLURM_SUCCESS ;
@@ -82,50 +86,51 @@ int read_update ( circular_buffer_t * buf , unsigned int size )
 
 int write_update ( circular_buffer_t * buf , unsigned int size )
 {
-        /*if zero read, just return */
+	/*if zero read, just return */
 	if ( size == 0 )
 	{
 		info ( "zero length write in cirular buffer" ) ;
 		return SLURM_SUCCESS ;
 	}
 
-        /* before modifing the buffer lets do some sanity checks*/
+	/* before modifing the buffer lets do some sanity checks*/
 	assert ( size <= buf-> write_size ) ;
 	assert_checks ( buf ) ;
-	
-	/*modify beginning position of the buffer*/
-	buf->end = buf->end + size ;
+
+	/*modify headning position of the buffer*/
+	buf->tail = buf->tail + size ;
 
 	/* after modifing the buffer lets do some sanity checks*/
 	assert_checks ( buf ) ;
-	
+
 	/* take care of wrap around issues */
-	if ( buf->end > buf->begin )
+	if ( buf->tail > buf->head )
 	{
 
-		if ( buf -> end == buf -> tail  ) 
+		if ( buf -> tail == buf -> end  ) 
 		{
-			if ( buf->begin == buf-> start )
-				;
-			/*full buffer realloc */
+			if ( buf->head == buf-> start )
+			{
+				expand_buffer ( buf ) ;
+			}
 			else
 			{
-				buf -> end = buf -> start ;
-				buf -> write_size = buf -> start - buf -> tail ;
+				buf -> tail = buf -> start ;
+				buf -> write_size = buf -> start - buf -> end ;
 			}
 		}
 		else
 		{
-			buf -> write_size = buf -> tail - buf -> end ;
+			buf -> write_size = buf -> end - buf -> tail ;
 		}
 	}
-	else if ( buf->end < buf->begin ) /* CASE B */
+	else if ( buf->tail < buf->head ) /* CASE B */
 	{
-		buf -> write_size = buf -> begin - buf -> end ;
+		buf -> write_size = buf -> head - buf -> tail ;
 	}
-	else if ( buf->end == buf->begin ) /* CASE C */
+	else if ( buf->tail == buf->head ) /* CASE C */
 	{
-			/*full buffer realloc */
+		expand_buffer ( buf ) ;
 	}
 
 	/* final sanity check */
@@ -136,34 +141,83 @@ int write_update ( circular_buffer_t * buf , unsigned int size )
 static int assert_checks ( circular_buffer_t * buf )
 {
 	/* sanity checks */
-	
+
 	assert ( buf != NULL ) ; /* buf struct is not null */
 	assert ( buf-> start == buf -> buffer ); /* stat hasn't moved */
-	assert ( ( buf -> start ) > ( buf -> tail ) ); /* buf_end is after start */
-	assert ( buf-> tail - buf -> start == buf -> buf_size ) ; /* buffer start and tail haven't moved */
-	
-	/* begin pointer is between start and tail */	
-	assert ( buf-> begin >= buf -> start ) ; 
-	assert ( buf-> begin <= buf -> tail ) ;
+	assert ( ( buf -> start ) < ( buf -> end ) ); /* buf_end is after start */
+	assert ( buf-> end - buf -> start == buf -> buf_size ) ; /* buffer start and end haven't moved */
 
-	/* end pointer is between start and tail */	
-	assert ( buf-> end >= buf -> start ) ; 
-	assert ( buf-> end <= buf -> tail ) ;
-	
-	if ( buf->end > buf->begin )
+	/* head pointer is between start and end */	
+	assert ( buf-> head >= buf -> start ) ; 
+	assert ( buf-> head <= buf -> end ) ;
+
+	/* tail pointer is between start and end */	
+	assert ( buf-> tail >= buf -> start ) ; 
+	assert ( buf-> tail <= buf -> end ) ;
+
+	if ( buf->tail > buf->head )
 	{
-		 assert ( buf -> write_size == buf -> tail - buf -> end ) ;
-		 assert ( buf -> read_size == buf -> end - buf -> begin ) ;
+		assert ( buf -> write_size == buf -> end - buf -> tail ) ;
+		assert ( buf -> read_size == buf -> tail - buf -> head ) ;
 	}
-	else if ( buf->end < buf->begin )
+	else if ( buf->tail < buf->head )
 	{
-		 assert ( buf -> write_size == buf -> begin - buf -> end ) ;
-		 assert ( buf -> read_size == buf -> tail - buf -> begin ) ;
+		assert ( buf -> write_size == buf -> head - buf -> tail ) ;
+		assert ( buf -> read_size == buf -> end - buf -> head ) ;
 	}
-	else if ( buf->end == buf->begin )
+	else if ( buf->tail == buf->head )
 	{
 		assert ( buf -> write_size == buf -> buf_size ) ;
 		assert ( buf -> read_size == 0 ) ;
 	}
+	return SLURM_SUCCESS ;
+}
+
+static int expand_buffer ( circular_buffer_t * buf )
+{
+	char * new_buffer ;
+	int data_size ;
+	int data_size_blk1 ;
+	int data_size_blk2 ;
+
+	if ( buf->buf_size == MAX_BUFFER_SIZE )
+	{
+		info ( "circular buffer maxed, dumping INCREMENTAL_BUFFER_SIZE of data");
+		/* dump data */
+
+	}
+
+	if ( buf->tail > buf->head )
+	{
+		new_buffer = xmalloc ( buf->buf_size + INCREMENTAL_BUFFER_SIZE ) ;
+		data_size = buf->tail - buf->head ;
+		memcpy ( new_buffer , buf->head , data_size ) ;
+		xfree ( buf -> buffer ) ;
+
+	}
+	else if ( buf->tail <= buf->head ) /* CASE B */
+	{
+		new_buffer = xmalloc ( buf->buf_size + INCREMENTAL_BUFFER_SIZE ) ;
+		data_size_blk1 = buf->end - buf->head ;
+		data_size_blk2 = buf->tail - buf->start ;
+		data_size = data_size_blk1 + data_size_blk2 ;
+		memcpy ( new_buffer , buf->head , data_size_blk1 ) ;
+		memcpy ( new_buffer + data_size_blk1 , buf->start , data_size_blk2 ) ;
+		xfree ( buf -> buffer ) ;
+	}
+	else 
+	{
+		fatal ( "Logically Impossible, but we are doing buffer arithmetic" ) ;
+	}
+
+	/* set up new state variables */
+	/* !!!statement order below does matter */
+	buf -> buffer = new_buffer ;
+	buf -> start = new_buffer ;
+	buf -> head = new_buffer ;
+	buf -> tail = new_buffer + data_size ;
+	buf -> end = new_buffer + buf->buf_size + INCREMENTAL_BUFFER_SIZE ;
+	buf -> buf_size += INCREMENTAL_BUFFER_SIZE ;
+
 	return SLURM_SUCCESS ;
 }
--- src/slurmd/circular_buffer.h
+++ src/slurmd/circular_buffer.h
@@ -4,12 +4,12 @@ typedef struct circular_buffer
 {
 	char * buffer ; /* buffer pointer - this never changes except during allocate and deallocate */
 	char * start ; /* buffer pointer copy - this never changes except during allocate and deallocate , but it is used in a lot of arithmetic hence the paranoia copy */
+	char * end ; /* one char past the last char of the buffer - ths never changes except during allocate and deallocate , but it is used in a lot of arithmetic */
 	unsigned int buf_size ; /* buffer size - this never changes except during allocate and deallocate */
 	unsigned int read_size ; /* buffer size that can be read */
 	unsigned int write_size ; /* buffer size that can be written */
-	char * begin ; /* beginning of the used portion of the buffer */
-	char * end ; /* end of the used portion of the buffer */
-	char * tail ; /* one char past the last char of the buffer */
+	char * head ; /* beginning of the used portion of the buffer */
+	char * tail; /* end of the used portion of the buffer */
 } circular_buffer_t ;
 
 int init_cir_buf ( circular_buffer_t ** buf_ptr ) ;
