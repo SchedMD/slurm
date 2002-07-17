2332326e2f7108a3b159f2ea4a8140f1783ca1fc
tewk <tewk@unknown>
2002-07-17 16:45:52 +0000

    fixed #ifndef in slurm.h added preliminary task_launch-test for slurmd

---
+++ testsuite/slurm_unit/slurmd/task_launch-test.c
@@ -0,0 +1,41 @@
+#include <src/common/slurm_protocol_api.h>
+
+int main ( int argc , char* argv[] )
+{
+	slurm_msg_t request_msg ;
+	slurm_msg_t response_msg ;
+        launch_tasks_msg_t launch_tasks_msg ;
+	slurm_addr      io_pipe_addrs[2] ;
+	int             gids[1] ;
+	gids[1] = 9999 ;
+
+	request_msg . msg_type = REQUEST_LAUNCH_TASKS ;
+	request_msg . data = & launch_tasks_msg ;
+
+	slurm_set_addr_char ( io_pipe_addrs , 7071 , "localhost" ) ;
+	slurm_set_addr_char ( io_pipe_addrs + 1 , 7072 , "localhost" ) ;
+	launch_tasks_msg . job_id = 1000 ;
+	launch_tasks_msg . job_step_id = 2000 ;
+	launch_tasks_msg . uid = 8737 ;
+	launch_tasks_msg . credentials = NULL ;
+	launch_tasks_msg . tasks_to_launch = 1 ;
+	launch_tasks_msg . envc = 0 ;
+	launch_tasks_msg . env = NULL ;
+	launch_tasks_msg . cwd = "." ;
+	launch_tasks_msg . cmd_line = "./testme" ;
+	launch_tasks_msg . streams = io_pipe_addrs ;
+	launch_tasks_msg . global_task_ids = gids ;
+
+	slurm_send_only_node_msg ( & request_msg ) ;
+
+	switch ( response_msg . msg_type ) 
+	{
+		case RESPONSE_SLURM_RC :
+			break ;
+		case RESPONSE_LAUNCH_TASKS :
+			break ;
+		default:
+			break ;
+	}
+	return SLURM_SUCCESS ;
+}
