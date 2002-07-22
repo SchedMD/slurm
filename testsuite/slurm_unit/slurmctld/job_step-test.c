540f7386e8d959433c79a80e88427a6d89f774fc
tewk <tewk@unknown>
2002-07-22 18:31:01 +0000

    Fixed test scripts to compile
    job_script is no longer a member of the job_rec struct

--- testsuite/slurm_unit/slurmctld/job_step-test.c
+++ testsuite/slurm_unit/slurmctld/job_step-test.c
@@ -46,7 +46,6 @@ main( int argc, char* argv[])
 	job_mesg. partition = "batch\0";
 	job_mesg. priority = 100;
 	job_mesg. req_nodes = "lx[3000-3003]\0";
-	job_mesg. job_script = "/bin/hostname\0";
 	job_mesg. shared = 0;
 	job_mesg. time_limit = 200;
 	job_mesg. num_procs = 1000;
