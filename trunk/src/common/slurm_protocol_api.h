94e2b5e35199f13a01746ee4d4e2a3b5f41e3c7a
tewk <tewk@unknown>
2002-07-22 19:51:35 +0000

    Added slurm_get_stream_addr which provides get_sockname functionality

--- trunk/src/common/slurm_protocol_api.h
+++ trunk/src/common/slurm_protocol_api.h
@@ -125,6 +125,7 @@ slurm_fd inline slurm_open_stream ( slurm_addr * slurm_address ) ;
 
 size_t inline slurm_write_stream ( slurm_fd open_fd , char * buffer , size_t size ) ;
 size_t inline slurm_read_stream ( slurm_fd open_fd , char * buffer , size_t size ) ;
+int inline slurm_get_stream_addr  ( slurm_fd open_fd , slurm_addr * source_address ) ;
 int inline slurm_close_stream ( slurm_fd open_fd ) ;
 	
 
@@ -135,7 +136,6 @@ int inline slurm_close_stream ( slurm_fd open_fd ) ;
 int slurm_receive_buffer ( slurm_fd open_fd , slurm_addr * source_address , slurm_msg_type_t * msg_type , char * data_buffer , size_t buf_len ) ;
 int slurm_send_controller_buffer ( slurm_fd open_fd , slurm_msg_type_t msg_type , char * data_buffer , size_t buf_len ) ;
 int slurm_send_node_buffer ( slurm_fd open_fd , slurm_addr * destination_address , slurm_msg_type_t msg_type , char * data_buffer , size_t buf_len ) ;
-
 /* Address Conversion Functions */
 
 void inline slurm_set_addr_uint ( slurm_addr * slurm_address , uint16_t port , uint32_t ip_address ) ;
