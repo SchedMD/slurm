13f43b65f6bbc60682958e41b58dee3b1273ce50
tewk <tewk@unknown>
2002-07-24 18:05:46 +0000

    Now submits a null credential

--- testsuite/slurm_unit/slurmd/task_mgr-test.c
+++ testsuite/slurm_unit/slurmd/task_mgr-test.c
@@ -11,15 +11,15 @@ int main ( int argc , char ** argv )
 	char arg0[] = "./testme" ;
 	char arg1[] = "" ;
 	char * args[] = { arg0 , arg1 } ;
+	slurm_job_credential_t cred = { 1,1,"test",0 , {0,0,0,0,0,0,0,0} } ;
 	
 	gids[1] = 9999 ;
-	
 	slurm_set_addr_char ( & io_pipe_addrs , 7071 , "localhost" ) ;
 	//kill_tasks_msg_t kill_tasks_msg ;
 	launch_tasks_msg . job_id = 1000 ;
 	launch_tasks_msg . job_step_id = 2000 ; 
 	launch_tasks_msg . uid = 8207 ;
-	launch_tasks_msg . credential = NULL ;
+	launch_tasks_msg . credential = & cred ;
 	launch_tasks_msg . tasks_to_launch = 1 ;
 	launch_tasks_msg . envc = 0 ;
 	launch_tasks_msg . env = NULL ;
