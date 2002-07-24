0c8403a072f54ef3f7938cf52a868f4c15b335de
tewk <tewk@unknown>
2002-07-24 00:51:22 +0000

    Fixed bug in src/common/slurm_protocol_pack.c, missing a break statement modified srun_emu.pl to follow new task launch design updated port number in testsuite/slurm_unit/slurmd/task_launch-test.c

--- trunk/testsuite/slurm_unit/slurmd/srun_emu.pl
+++ trunk/testsuite/slurm_unit/slurmd/srun_emu.pl
@@ -1,41 +1,24 @@
 #!/usr/bin/perl
 use IO::Socket;
-
-$pid = fork();
-if ($pid)
+my $sock1 = new IO::Socket::INET (
+		LocalHost => 'localhost',
+		LocalPort => '7071',
+		Proto => 'tcp',
+		Listen => 1,
+		Reuse => 1,
+		);
+while ( true )
 {
-#stdout
-	my $sock1 = new IO::Socket::INET (
-			LocalHost => 'localhost',
-			LocalPort => '7071',
-			Proto => 'tcp',
-			Listen => 1,
-			Reuse => 1,
-			);
 	my $new_sock1 = $sock1->accept();
-	while(<$new_sock1>) 
+	$pid = fork ();
+	if ($pid)
 	{
-		print STDOUT $_;
-	}
-	close($sock1);
-	print "CLOSED STD OUT SOCKET" ;
 
-}
-else
-{
-#stderr
-	my $sock2 = new IO::Socket::INET (
-			LocalHost => 'localhost',
-			LocalPort => '7072',
-			Proto => 'tcp',
-			Listen => 1,
-			Reuse => 1,
-			);
-	my $new_sock2 = $sock2->accept();
-	while(<$new_sock2>) 
-	{
-		print STDERR $_;
+		while(<$new_sock1>) 
+		{
+			print STDOUT $_;
+		}
+		close($sock1);
+		print "CLOSED STD OUT SOCKET" ;
 	}
-	close($sock2);
-	print "CLOSED STD ERR SOCKET" ;
 }
