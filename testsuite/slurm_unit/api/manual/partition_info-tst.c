f0a0ef5885e8347467ad196297409110ad196b4e
Moe Jette <jette1@llnl.gov>
2002-07-26 02:38:34 +0000

    Resync tests with API updates.

--- testsuite/slurm_unit/api/manual/partition_info-tst.c
+++ testsuite/slurm_unit/api/manual/partition_info-tst.c
@@ -20,10 +20,10 @@ main (int argc, char *argv[])
 		return (error_code);
 	}
 
-	note("Updated at %lx, record count %d\n",
+	note("Updated at %ld, record count %d\n",
 		(time_t) part_info_ptr->last_update, part_info_ptr->record_count);
 
-	slurm_print_partition_info ( stdout, part_info_ptr );
-	slurm_free_partition_info (part_info_ptr);
+	slurm_print_partition_info_msg ( stdout, part_info_ptr );
+	slurm_free_partition_info_msg (part_info_ptr);
 	return (0);
 }
