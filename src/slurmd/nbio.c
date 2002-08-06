022f6f0c5048f8b54ef1ad682b21df79e4a3ce5f
tewk <tewk@unknown>
2002-08-06 21:17:11 +0000

    got nbio to compile

--- src/slurmd/nbio.c
+++ src/slurmd/nbio.c
@@ -18,23 +18,24 @@
 #include <src/slurmd/task_mgr.h>
 #include <src/slurmd/shmem_struct.h>
 #include <src/slurmd/circular_buffer.h>
+#include <src/slurmd/reconnect_utils.h>
 #include <src/slurmd/io.h>
 #include <src/slurmd/pipes.h>
 
 typedef enum 
 {
-	IN_OUT_FD ;
-	SIG_ERR_FD ;
-	CHILD_IN_WR_FD ;
-	CHILD_OUT_RD_FD ;
-	CHILD_ERR_RD_RD ;
+	IN_OUT_FD ,
+	SIG_ERR_FD ,
+	CHILD_IN_WR_FD ,
+	CHILD_OUT_RD_FD ,
+	CHILD_ERR_RD_FD 
 } nbio_fd_t ;
 
 typedef enum
 {
-	RD_SET ;
-	WR_SET ;
-	ER_SET ;
+	RD_SET ,
+	WR_SET ,
+	ER_SET 
 } nbio_set_t ;
 
 typedef struct nbio_attr
@@ -47,8 +48,19 @@ typedef struct nbio_attr
 	circular_buffer_t * out_cir_buf ;
 	circular_buffer_t * err_cir_buf ;
 	int nbio_flags [5] ;
+	int reconnect_flags [2] ;
+	time_t reconnect_timers [2] ;
 	int max_fd ;
 } nbio_attr_t ;
+
+int nbio_set_init ( nbio_attr_t * nbio_attr , slurm_fd_set * set_ptr ) ;
+int memcpy_sets ( slurm_fd_set * init_set , slurm_fd_set * next_set ) ;
+int write_task_socket ( circular_buffer_t * cir_buf, task_start_t * task_start, slurm_fd write_fd , char * const name ) ;
+int read_task_pipe ( circular_buffer_t * cir_buf, task_start_t * task_start, slurm_fd write_fd , char * const name ) ;
+int write_task_pipe ( circular_buffer_t * cir_buf, task_start_t * task_start, slurm_fd write_fd , char * const name ) ;
+int read_task_socket ( circular_buffer_t * cir_buf, task_start_t * task_start, slurm_fd read_fd , char * const name ) ;
+int error_task_pipe ( ) ;
+int error_task_socket ( ) ; 
 
 int init_nbio_attr ( nbio_attr_t * nbio_attr , task_start_t * task_start )
 {
@@ -56,13 +68,14 @@ int init_nbio_attr ( nbio_attr_t * nbio_attr , task_start_t * task_start )
 	nbio_attr -> task_start = task_start ;
 	nbio_attr -> fd[IN_OUT_FD] = task_start -> sockets[STDIN_OUT_SOCK];
 	nbio_attr -> fd[SIG_ERR_FD] = task_start -> sockets[SIG_STDERR_SOCK];
-	nbio_attr -> fd[CHILD_IN_WR_FD] = task_start -> pipes[CHILD_IN_WR];
-	nbio_attr -> fd[CHILD_OUT_RD_FD] = task_start -> pipes[CHILD_OUT_RD];
-	nbio_attr -> fd[CHILD_ERR_RD_FD] = task_start -> pipes[CHILD_ERR_RD];
-	init_circular_buffer ( & cir_buf ) ;
-	init_circular_buffer ( & cir_buf ) ;
-	init_circular_buffer ( & cir_buf ) ;
+	nbio_attr -> fd[CHILD_IN_WR_FD] = task_start -> pipes[CHILD_IN_WR_PIPE];
+	nbio_attr -> fd[CHILD_OUT_RD_FD] = task_start -> pipes[CHILD_OUT_RD_PIPE];
+	nbio_attr -> fd[CHILD_ERR_RD_FD] = task_start -> pipes[CHILD_ERR_RD_PIPE];
+	init_circular_buffer ( & nbio_attr -> in_cir_buf ) ;
+	init_circular_buffer ( & nbio_attr -> out_cir_buf ) ;
+	init_circular_buffer ( & nbio_attr -> err_cir_buf ) ;
 	nbio_set_init (  nbio_attr , nbio_attr -> init_set ) ;
+	return SLURM_SUCCESS ;
 }
 
 void * do_nbio ( void * arg )
@@ -75,50 +88,69 @@ void * do_nbio ( void * arg )
 
 	while ( true ) 
 	{
-		int rc = slurm_select ( nbio_attr . max_fd , nbio_attr . init_set[RD_SET] , nbio_attr . init_set[WR_SET] , nbio_attr . init_set[ER_SET] , NULL ) ;
+		int rc = slurm_select ( nbio_attr . max_fd , & nbio_attr . init_set[RD_SET] , & nbio_attr . init_set[WR_SET] , & nbio_attr . init_set[ER_SET] , NULL ) ;
 
-		nbio_set_init ( & nbio_attr , nbio_attr -> next_set )
+		nbio_set_init ( & nbio_attr , nbio_attr . next_set ) ;
 
-		if ( slurm_FD_ISSET ( set_ptr[ER_SET] , nbio_attr . fd [CHILD_IN_WR_FD] ) ) { error_task_pipe ( ) }
-		if ( slurm_FD_ISSET ( set_ptr[ER_SET] , nbio_attr . fd [CHILD_OUT_RD_FD] ) ) { error_task_pipe ( ) }
-		if ( slurm_FD_ISSET ( set_ptr[ER_SET] , nbio_attr . fd [CHILD_ERR_RD_RD] ) ) { error_task_pipe ( ) }
-		if ( slurm_FD_ISSET ( set_ptr[ER_SET] , nbio_attr . fd [IN_OUT_FD] ) ) { error_task_socket ( ) }
-		if ( slurm_FD_ISSET ( set_ptr[ER_SET] , nbio_attr . fd [SIG_ERR_FD] ) ) { error_task_socket ( ) }
+		if ( slurm_FD_ISSET (  nbio_attr . fd [CHILD_IN_WR_FD] , & nbio_attr . init_set [ER_SET] ) ) 
+		{ 
+			error_task_pipe ( ) ;
+			break ;
+		}
+		if ( slurm_FD_ISSET ( nbio_attr . fd [CHILD_OUT_RD_FD] , & nbio_attr . init_set [ER_SET] ) ) 
+		{ 
+			error_task_pipe ( ) ;
+			break ;
+		}
+		if ( slurm_FD_ISSET ( nbio_attr . fd [CHILD_ERR_RD_FD] , & nbio_attr . init_set [ER_SET] ) ) 
+		{
+			error_task_pipe ( ) ;
+			break ;
+		}
+		if ( slurm_FD_ISSET ( nbio_attr . fd [IN_OUT_FD] , & nbio_attr . init_set [ER_SET] ) ) 
+		{ 
+			error_task_socket ( ) ;
+		}
+		if ( slurm_FD_ISSET ( nbio_attr . fd [SIG_ERR_FD] , & nbio_attr . init_set [ER_SET] ) ) 
+		{ 
+			error_task_socket ( ) ;
+		}
 
-		if ( slurm_FD_ISSET ( init_set [RD_SET] , nbio_attr . fd [IN_OUT_FD] ) ) 
+		if ( slurm_FD_ISSET ( nbio_attr . fd [IN_OUT_FD] , & nbio_attr . init_set [RD_SET] ) ) 
 		{ 
 			read_task_socket ( nbio_attr . in_cir_buf , nbio_attr . task_start , nbio_attr . fd [IN_OUT_FD] , "stdin" ); 
-			slurm_FD_SET ( next_set [WR_SET] , nbio_attr . fd [CHILD_IN_WR_FD] ); 
+			slurm_FD_SET ( nbio_attr . fd [CHILD_IN_WR_FD] , & nbio_attr . next_set [WR_SET] ); 
 		}
-		if ( slurm_FD_ISSET ( init_set [RD_SET] , nbio_attr . fd [CHILD_OUT_RD_FD] ) ) 
+		if ( slurm_FD_ISSET ( nbio_attr . fd [CHILD_OUT_RD_FD] , & nbio_attr . init_set [RD_SET] ) ) 
 		{ 
 			read_task_pipe ( nbio_attr . out_cir_buf , nbio_attr . task_start , nbio_attr . fd [CHILD_OUT_RD_FD] , "stdout" ); 
-			slurm_FD_SET ( next_set [WR_SET] , nbio_attr . fd [IN_OUT_FD] ); 
+			slurm_FD_SET ( nbio_attr . fd [IN_OUT_FD] , & nbio_attr . next_set [WR_SET] ); 
 		}
-		if ( slurm_FD_ISSET ( init_set [RD_SET] , nbio_attr . fd [CHILD_ERR_RD_RD] ) ) 
+		if ( slurm_FD_ISSET ( nbio_attr . fd [CHILD_ERR_RD_FD] , & nbio_attr . init_set [RD_SET] ) ) 
 		{ 
 			read_task_pipe ( nbio_attr . err_cir_buf , nbio_attr . task_start , nbio_attr . fd [CHILD_ERR_RD_FD] , "stderr" ); 
-			slurm_FD_SET ( next_set [WR_SET] , nbio_attr .  fd [SIG_ERR_FD] ); 
+			slurm_FD_SET ( nbio_attr .  fd [SIG_ERR_FD] , & nbio_attr . next_set [WR_SET] ); 
 		}
 
-		if ( slurm_FD_ISSET ( init_set [WR_SET] , nbio_attr -> fd [CHILD_IN_WR_FD] ) ) 
+		if ( slurm_FD_ISSET ( nbio_attr . fd [CHILD_IN_WR_FD] , & nbio_attr . next_set [WR_SET] ) ) 
 		{ 
 			write_task_pipe ( nbio_attr . in_cir_buf , nbio_attr . task_start , nbio_attr . fd [CHILD_IN_WR_FD] , "stdin" );
-			slurm_FD_CLR ( next_set [WR_SET] , nbio_attr . fd [CHILD_IN_WR_FD] ); 
+			slurm_FD_CLR ( nbio_attr . fd [CHILD_IN_WR_FD] , & nbio_attr . next_set [WR_SET] ); 
 		}
-		if ( slurm_FD_ISSET ( init_set [WR_SET] , nbio_attr -> fd [IN_OUT_FD] ) ) 
+		if ( slurm_FD_ISSET ( nbio_attr . fd [IN_OUT_FD] , & nbio_attr . next_set [WR_SET] ) ) 
 		{ 
-			write_task_socket ( nbio_attr . out_cir_buf , nbio_attr . task_start , nbio_attr . fd [CHILD_OUT_WR_FD] , "stdout" );
-			slurm_FD_SET ( next_set [WR_SET] , nbio_attr . fd [IN_OUT_FD] ); 
+			write_task_socket ( nbio_attr . out_cir_buf , nbio_attr . task_start , nbio_attr . fd [IN_OUT_FD] , "stdout" );
+			slurm_FD_CLR ( nbio_attr . fd [IN_OUT_FD] , & nbio_attr . next_set [WR_SET] ); 
 		}
-		if ( slurm_FD_ISSET ( init_set [WR_SET] , nbio_attr -> fd [SIG_ERR_FD] ) ) 
+		if ( slurm_FD_ISSET ( nbio_attr . fd [SIG_ERR_FD] , & nbio_attr . next_set [WR_SET] ) ) 
 		{ 
-			write_task_socket ( nbio_attr . err_cir_buf , nbio_attr . task_start , nbio_attr . fd [CHILD_ERR_WR_FD] , "stderr" ); 
-			slurm_FD_SET ( next_set [WR_SET] , nbio_attr .  fd [SIG_ERR_FD] ); 
+			write_task_socket ( nbio_attr . err_cir_buf , nbio_attr . task_start , nbio_attr . fd [SIG_ERR_FD] , "stderr" ); 
+			slurm_FD_CLR ( nbio_attr .  fd [SIG_ERR_FD] , & nbio_attr . next_set [WR_SET] ); 
 		}
 
-		memcpy_sets ( init_set , next_set ) ;
+		memcpy_sets ( nbio_attr . init_set , nbio_attr . next_set ) ;
 	}
+	return SLURM_SUCCESS ;
 }
 
 int memcpy_sets ( slurm_fd_set * init_set , slurm_fd_set * next_set )
@@ -129,9 +161,10 @@ int memcpy_sets ( slurm_fd_set * init_set , slurm_fd_set * next_set )
 	{
 		memcpy ( & init_set[i] , & next_set[i] , sizeof ( slurm_fd_set ) ) ;
 	}
+	return SLURM_SUCCESS ;
 }
 
-int read_task_pipe ( circular_buffer_t * cir_buf, task_start_t * task_start, slurm_fd write_fd , char * const name )
+int read_task_pipe ( circular_buffer_t * cir_buf, task_start_t * task_start, slurm_fd read_fd , char * const name )
 {
 	int bytes_read ;
 	int local_errno ;
@@ -174,8 +207,8 @@ int write_task_pipe ( circular_buffer_t * cir_buf, task_start_t * task_start, sl
 	if ( ( bytes_written = write_EINTR ( write_fd , cir_buf->head , cir_buf->read_size ) ) <= 0 )
 	{
 		local_errno = errno ;
-		info ( "error sending %s stream for task %i, %m errno: %i , bytes read %i ", 
-				name , task_start -> launch_msg -> global_task_ids[ task_start -> local_task_id ] , local_errno , bytes_read ) ;
+		info ( "error sending %s stream for task %i, %m errno: %i , bytes written %i ", 
+				name , task_start -> launch_msg -> global_task_ids[ task_start -> local_task_id ] , local_errno , bytes_written ) ;
 		slurm_seterrno_ret ( ESLURMD_PIPE_DISCONNECT ) ;
 	}
 	else
@@ -282,7 +315,7 @@ int write_task_socket ( circular_buffer_t * cir_buf, task_start_t * task_start, 
 		}
 		else
 		{
-			info ( "bytes_read: %i don't know what to do with this return code ", bytes_read ) ;
+			info ( "bytes_read: %i don't know what to do with this return code ", sock_bytes_written ) ;
 		}
 	}
 	else
@@ -294,10 +327,12 @@ int write_task_socket ( circular_buffer_t * cir_buf, task_start_t * task_start, 
 
 int error_task_pipe ( ) 
 {
+	return SLURM_SUCCESS ;
 }
 
 int error_task_socket ( ) 
 {
+	return SLURM_SUCCESS ;
 }
 
 int nbio_set_init ( nbio_attr_t * nbio_attr , slurm_fd_set * set_ptr )
@@ -305,15 +340,15 @@ int nbio_set_init ( nbio_attr_t * nbio_attr , slurm_fd_set * set_ptr )
 	int i ;
 	for ( i=0 ; i < 3 ; i++ )
 	{
-		FD_ZERO ( nbio_attr -> init_set[i] ) ;
+		FD_ZERO ( & set_ptr[i] ) ;
 	}
-	slurm_FD_SET ( set_ptr[RD_SET] , nbio_attr -> fd [IN_OUT_FD] ) ;
-	slurm_FD_SET ( set_ptr[RD_SET] , nbio_attr -> fd [CHILD_OUT_RD_FD] ) ;
-	slurm_FD_SET ( set_ptr[RD_SET] , nbio_attr -> fd [CHILD_ERR_RD_RD] ) ;
+	slurm_FD_SET ( nbio_attr -> fd [IN_OUT_FD] , & set_ptr [RD_SET] ) ;
+	slurm_FD_SET ( nbio_attr -> fd [CHILD_OUT_RD_FD] , & set_ptr [RD_SET] ) ;
+	slurm_FD_SET ( nbio_attr -> fd [CHILD_ERR_RD_FD] , & set_ptr [RD_SET] ) ;
 	
 	for ( i=0 ; i < 5 ; i++ )
 	{
-		slurm_FD_SET ( set_ptr[ER_SET] , nbio_attr -> fd [i] ) ;
+		slurm_FD_SET ( nbio_attr -> fd [i] , & set_ptr[ER_SET] ) ;
 	}
 
 	nbio_attr -> max_fd  = 0 ;
