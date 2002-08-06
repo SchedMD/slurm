76a8e958fbb8b0436cb61d05ac84f197baf0405f
jce <jce@unknown>
2002-08-06 05:35:15 +0000

    added squeue.h

---
+++ trunk/src/squeue/squeue.h
@@ -0,0 +1,75 @@
+/****************************************************************************\
+ *  * slurm_protocol_defs.h - definitions used for RPCs
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Kevin Tew <tew1@llnl.gov>.
+ *  UCRL-CODE-2002-040.
+ *
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+
+#ifndef __SQUEUE_H__
+
+#if HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <ctype.h>
+#include <stdio.h>
+
+#if HAVE_INTTYPES_H
+#  include <inttypes.h>
+#else  /* !HAVE_INTTYPES_H */
+#  if HAVE_STDINT_H
+#    include <stdint.h>
+#  endif
+#endif  /* HAVE_INTTYPES_H */
+
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <src/api/slurm.h>
+#include <src/common/hostlist.h>
+#include <src/common/log.h>
+#include <src/common/slurm_protocol_api.h>
+#include <src/common/xmalloc.h>
+#include <src/common/list.h>
+
+#include "print.h"
+
+struct squeue_parameters {
+	bool job_flag;
+	bool step_flag;
+	char* jobs;
+	char* steps;
+	uint16_t step;
+	uint32_t job;
+	enum job_states state;
+	int verbose;
+	char* format;
+	char* partitions;
+};
+
+int parse_state( char* str, enum job_states* states );
+int parse_command_line( int argc, char* argv[] );
+void print_options();
+
+#endif
