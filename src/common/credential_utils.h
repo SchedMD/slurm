9d8712137e763a188d6b395f7e46ee5b20d79a5a
Mark Grondona <mgrondona@llnl.gov>
2002-09-10 22:59:54 +0000

    o code cleanup throughout

--- src/common/credential_utils.h
+++ src/common/credential_utils.h
@@ -13,15 +13,14 @@
 #include <src/common/slurm_protocol_api.h>
 #include <src/common/signature_utils.h>
 
-typedef struct credential_state
-{
-	int job_id ;			/* job_id that this credential cooresponds to */
-	short int revoked ;		/* boolean true/false */
-	short int procs_allocated ;	/* number of credential procs running */
-	short int total_procs ;		/* number of procs in credential */
-	time_t revoke_time ;		/* time of revoke - this is informational only not used */
-	time_t expiration ;		/* expiration date set at credential creation time */
-} credential_state_t ;
+typedef struct credential_state {
+	int job_id;		/* job_id that this credential cooresponds to */
+	short int revoked;	/* boolean true/false */
+	short int procs_allocated;	/* number of credential procs running */
+	short int total_procs;	/* number of procs in credential */
+	time_t revoke_time;	/* time of revoke - this is informational only not used */
+	time_t expiration;	/* expiration date set at credential creation time */
+} credential_state_t;
 
 /* time to wait after expiration_time before removing credential_state from credential_state_list*/
 #define EXPIRATION_WINDOW 600
@@ -31,13 +30,13 @@ typedef struct credential_state
  * called from slurmd_init initializes the List structure pointed to by list
  * IN list	- type List 
  */
-int initialize_credential_state_list ( List * list ) ;
+int initialize_credential_state_list(List * list);
 
 /* destroy_credential_state_list
  * destroys a initialized list
  * IN list	- type List 
  */
-int destroy_credential_state_list ( List list ) ;
+int destroy_credential_state_list(List list);
 
 /* verify_credential
  * given a credential message and a verify_ctx containing the public key
@@ -47,7 +46,9 @@ int destroy_credential_state_list ( List list ) ;
  * IN credential		- credential to verify
  * IN credential_state_list	- list to add credential state object to 
  */
-int verify_credential ( slurm_ssl_key_ctx_t * verfify_ctx , slurm_job_credential_t * credential , List credential_state_list ) ;
+int verify_credential(slurm_ssl_key_ctx_t * verfify_ctx,
+		      slurm_job_credential_t * credential,
+		      List credential_state_list);
 
 /* sign_credential
  * signs a credential before transmit
@@ -55,13 +56,14 @@ int verify_credential ( slurm_ssl_key_ctx_t * verfify_ctx , slurm_job_credential
  * IN sign_ctx			- slurm ssl private key ctx
  * IN credential		- credential to sign 
  */
-int sign_credential ( slurm_ssl_key_ctx_t * sign_ctx , slurm_job_credential_t * credential ) ; 
+int sign_credential(slurm_ssl_key_ctx_t * sign_ctx,
+		    slurm_job_credential_t * credential);
 
 /* revoke_credential
  * expires a credential in the credential_state_list
  * IN revoke_msg		- revoke rpc message
  * IN list			- list to revoke credential state object in
  */
-int revoke_credential ( revoke_credential_msg_t * revoke_msg , List list ) ;
+int revoke_credential(revoke_credential_msg_t * revoke_msg, List list);
 
 #endif
