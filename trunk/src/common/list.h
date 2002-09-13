af879777b66e9239374cae18584a3f9ee38b6a98
Chris Dunlap <cdunlap@llnl.gov>
2002-09-13 21:35:25 +0000

    Added UC copyright & GPL license info.

--- trunk/src/common/list.h
+++ trunk/src/common/list.h
@@ -1,7 +1,28 @@
-/******************************************************************************\
+/*****************************************************************************\
  *  $Id: list.h,v 1.6 2001/12/15 14:33:49 dun Exp $
- *    by Chris Dunlap <cdunlap@llnl.gov>
-\******************************************************************************/
+ *****************************************************************************
+ *  Copyright (C) 2001-2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Chris Dunlap <cdunlap@llnl.gov>.
+ *  UCRL-CODE-2002-009.
+ *  
+ *  This file is part of ConMan, a remote console management program.
+ *  For details, see <http://www.llnl.gov/linux/conman/>.
+ *  
+ *  ConMan is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  ConMan is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with ConMan; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
 
 
 #ifndef _LIST_H
