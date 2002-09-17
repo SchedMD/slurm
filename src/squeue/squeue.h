09bcd633138785f270f268b928eec39d887149b5
Moe Jette <jette1@llnl.gov>
2002-09-17 19:05:18 +0000

    Added copyright notices.

--- src/squeue/squeue.h
+++ src/squeue/squeue.h
@@ -1,9 +1,9 @@
 /****************************************************************************\
- *  * slurm_protocol_defs.h - definitions used for RPCs
+ *  squeue.h - definitions used for printing job queue state
  *****************************************************************************
  *  Copyright (C) 2002 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
- *  Written by Kevin Tew <tew1@llnl.gov>.
+ *  Written by Joey Ekstrom <ekstrom1@llnl.gov>
  *  UCRL-CODE-2002-040.
  *
  *  This file is part of SLURM, a resource management program.
