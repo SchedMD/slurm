5e86f4b2e1b47eab7b2cc132df22ab64041fec69
Mark Grondona <mgrondona@llnl.gov>
2002-09-20 18:13:28 +0000

    o some cleanup but mostly reformatting

--- trunk/src/slurmd/reconnect_utils.c
+++ trunk/src/slurmd/reconnect_utils.c
@@ -22,105 +22,106 @@
 #include <src/slurmd/pipes.h>
 #include <src/slurmd/reconnect_utils.h>
 
-int connect_io_stream (  task_start_t * task_start , int out_or_err )
+int connect_io_stream(task_start_t * task_start, int out_or_err)
 {
-	int local_errno ;
-	if ( ( task_start->sockets[out_or_err] = slurm_open_stream ( & ( task_start -> io_streams_dest ) ) ) == SLURM_PROTOCOL_ERROR )
-	{
-		local_errno = errno ;	
-		info ( "error opening socket to srun to pipe %s %m errno: %i" , out_or_err ? "stdout" : "stderr" , local_errno ) ;
-		return SLURM_PROTOCOL_ERROR ;
-	}
-	else
-	{
-		return send_io_stream_header ( task_start , out_or_err) ;
-	}
-	
+	if ((task_start->sockets[out_or_err] =
+	     slurm_open_stream(&(task_start->io_streams_dest))) ==
+	    SLURM_PROTOCOL_ERROR) {
+		error("connect_io_stream: <%s>: %m", 
+				out_or_err ? "stdout" : "stderr");
+		return SLURM_PROTOCOL_ERROR;
+	} else 
+		return send_io_stream_header(task_start, out_or_err);
 }
 
-int send_io_stream_header ( task_start_t * task_start , int out_or_err ) 
+int send_io_stream_header(task_start_t * task_start, int out_or_err)
 {
-	slurm_io_stream_header_t io_header ;
-	char buffer[sizeof(slurm_io_stream_header_t)] ;
-	char * buf_ptr = buffer ;
-	int buf_size = sizeof(slurm_io_stream_header_t) ;
-	int size = sizeof(slurm_io_stream_header_t) ;
+	slurm_io_stream_header_t io_header;
+	int buf_size = sizeof(slurm_io_stream_header_t);
+	char buffer[buf_size];
+	char *buf_ptr = buffer;
+	int size = buf_size;
 
+	if (out_or_err == STDIN_OUT_SOCK) {
+		init_io_stream_header(&io_header,
+				      task_start->launch_msg->credential->
+				      signature,
+				      task_start->launch_msg->
+				      global_task_ids[task_start->local_task_id],
+				      SLURM_IO_STREAM_INOUT);
+		pack_io_stream_header(&io_header, (void **) &buf_ptr, &size);
+		return slurm_write_stream(task_start->
+					  sockets[STDIN_OUT_SOCK], buffer,
+					  buf_size - size);
+	} else {
 
-	if( out_or_err == STDIN_OUT_SOCK )
-	{
-		init_io_stream_header ( & io_header , 
-				task_start -> launch_msg -> credential -> signature , 
-				task_start -> launch_msg -> global_task_ids[task_start -> local_task_id ] , 
-				SLURM_IO_STREAM_INOUT 
-				) ;
-		pack_io_stream_header ( & io_header , (void ** ) & buf_ptr , & size ) ;
-		return slurm_write_stream (  task_start->sockets[STDIN_OUT_SOCK] , buffer , buf_size - size ) ;
-	}
-	else
-	{
-
-		init_io_stream_header ( & io_header , 
-				task_start -> launch_msg -> credential -> signature , 
-				task_start -> launch_msg -> global_task_ids[task_start -> local_task_id ] , 
-				SLURM_IO_STREAM_SIGERR 
-				) ;
-		pack_io_stream_header ( & io_header , (void ** ) & buf_ptr , & size ) ;
-		return slurm_write_stream (  task_start->sockets[SIG_STDERR_SOCK] , buffer , buf_size - size ) ;
+		init_io_stream_header(&io_header,
+				      task_start->launch_msg->credential->
+				      signature,
+				      task_start->launch_msg->
+				      global_task_ids[task_start->
+						      local_task_id],
+				      SLURM_IO_STREAM_SIGERR);
+		pack_io_stream_header(&io_header, (void **) &buf_ptr,
+				      &size);
+		return slurm_write_stream(task_start->
+					  sockets[SIG_STDERR_SOCK], buffer,
+					  buf_size - size);
 	}
 }
 
 ssize_t read_EINTR(int fd, void *buf, size_t count)
 {
-	ssize_t bytes_read ;
-	while ( true )
-	{
-		if ( ( bytes_read = read ( fd, buf, count ) ) <= 0 )
-		{
-			if ( ( bytes_read == SLURM_PROTOCOL_ERROR ) && ( errno == EINTR ) )
-			{
-				debug ( "read_EINTR: bytes_read: %i , fd: %i %m errno: %i", bytes_read , fd , errno ) ;
-				continue ;
+	ssize_t bytes_read;
+	while (true) {
+		if ((bytes_read = read(fd, buf, count)) <= 0) {
+			if ((bytes_read == SLURM_PROTOCOL_ERROR)
+			    && (errno == EINTR)) {
+				debug
+				    ("read_EINTR: bytes_read: %i , fd: %i %m errno: %i",
+				     bytes_read, fd, errno);
+				continue;
 			}
 		}
-		return bytes_read ;
+		return bytes_read;
 	}
 }
 
 ssize_t write_EINTR(int fd, void *buf, size_t count)
 {
-	ssize_t bytes_written ;
-	while ( true )
-	{
-		if ( ( bytes_written = write ( fd, buf, count ) ) <= 0 )
-		{
-			if ( ( bytes_written == SLURM_PROTOCOL_ERROR ) && ( errno == EINTR ) )
-			{
-				debug ( "write_EINTR: bytes_written: %i , fd: %i %m errno: %i", bytes_written , fd , errno ) ;
-				continue ;
+	ssize_t bytes_written;
+	while (true) {
+		if ((bytes_written = write(fd, buf, count)) <= 0) {
+			if ((bytes_written == SLURM_PROTOCOL_ERROR)
+			    && (errno == EINTR)) {
+				debug
+				    ("write_EINTR: bytes_written: %i , fd: %i %m errno: %i",
+				     bytes_written, fd, errno);
+				continue;
 			}
 		}
-		return bytes_written ;
+		return bytes_written;
 	}
 }
 
-struct timeval timeval_diff ( struct timeval * last , struct timeval * first )
+struct timeval timeval_diff(struct timeval *last, struct timeval *first)
 {
-	struct timeval temp ;
-	double lastd = last -> tv_sec * 1000000 + last -> tv_usec ;
-	double firstd = first -> tv_sec * 1000000 + first -> tv_usec ;
-	double diffd = lastd - firstd ;
-	temp . tv_sec = diffd / 1000000 ;
-	temp . tv_usec = ( long long ) diffd % 1000000 ;
-	return temp ;
+	struct timeval temp;
+	double lastd = last->tv_sec * 1000000 + last->tv_usec;
+	double firstd = first->tv_sec * 1000000 + first->tv_usec;
+	double diffd = lastd - firstd;
+	temp.tv_sec = diffd / 1000000;
+	temp.tv_usec = (long long) diffd % 1000000;
+	return temp;
 }
 
-double timeval_diffd ( struct timeval * last , struct timeval * first , struct timeval * remaining )
+double timeval_diffd(struct timeval *last, struct timeval *first,
+		     struct timeval *remaining)
 {
-	double lastd = last -> tv_sec * 1000000 + last -> tv_usec ;
-	double firstd = first -> tv_sec * 1000000 + first -> tv_usec ;
-	double diffd = lastd - firstd ;
-	remaining -> tv_sec = diffd / 1000000 ;
-	remaining -> tv_usec = ( long long ) diffd % 1000000 ;
-	return diffd ;
+	double lastd = last->tv_sec * 1000000 + last->tv_usec;
+	double firstd = first->tv_sec * 1000000 + first->tv_usec;
+	double diffd = lastd - firstd;
+	remaining->tv_sec = diffd / 1000000;
+	remaining->tv_usec = (long long) diffd % 1000000;
+	return diffd;
 }
--- trunk/src/slurmd/reconnect_utils.h
+++ trunk/src/slurmd/reconnect_utils.h
@@ -4,13 +4,12 @@
 /* connect_io_stream
  * called by the io_threads to establish a connection to srun
  */
-int connect_io_stream (  task_start_t * task_start , int out_or_err ) ;
+int connect_io_stream(task_start_t * task_start, int out_or_err);
 
 /* connect_io_stream
  * called by connect_io_stream to send stream identification info
  */
-int send_io_stream_header ( task_start_t * task_start , int out_or_err ) ;
-ssize_t read_EINTR(int fd, void *buf, size_t count) ;
-ssize_t write_EINTR(int fd, void *buf, size_t count) ;
-#endif 
-
+int send_io_stream_header(task_start_t * task_start, int out_or_err);
+ssize_t read_EINTR(int fd, void *buf, size_t count);
+ssize_t write_EINTR(int fd, void *buf, size_t count);
+#endif
