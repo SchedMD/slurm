6a62248850c5438a41b482a6d88d285e55197dbc
Moe Jette <jette1@llnl.gov>
2002-09-24 01:30:17 +0000

    No code changes. Just added the LLNL copyright/disclaimer to files.

--- src/slurmd/locks.h
+++ src/slurmd/locks.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- * locks.h - definitions for semaphore functions for slurmd (locks.c)
+ *  locks.h - definitions for semaphore functions for slurmd (locks.c)
  *****************************************************************************
  *  Copyright (C) 2002 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
