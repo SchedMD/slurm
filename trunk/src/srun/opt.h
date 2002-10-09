ebd938a2bd414794954f6a9e8a846b61f7b5d841
Moe Jette <jette1@llnl.gov>
2002-10-09 20:11:30 +0000

    Add logic to recognize --batch or -b option on execute line. No processing beyond that in place yet.

--- trunk/src/srun/opt.h
+++ trunk/src/srun/opt.h
@@ -1,6 +1,28 @@
-/* $Id$ */
-
-/* Options for srun */
+/*****************************************************************************\
+ *  opt.h - definitions for srun option processing
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Mark Grondona <grondona1@llnl.gov>, et. al.
+ *  UCRL-CODE-2002-040.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
 
 #ifndef _HAVE_OPT_H
 #define _HAVE_OPT_H
@@ -36,7 +58,8 @@ enum modes {
 	MODE_NORMAL	= 1,
 	MODE_IMMEDIATE	= 2,
 	MODE_ATTACH	= 3,
-	MODE_ALLOCATE	= 4
+	MODE_ALLOCATE	= 4,
+	MODE_BATCH	= 5
 };
 
 enum modes mode;
@@ -98,6 +121,7 @@ typedef struct srun_options {
 	bool labelio;		/* --label-output, -l		*/
 	bool allocate;		/* --allocate, 	   -A		*/
 	bool overcommit;	/* --overcommit,   -O		*/
+	bool batch;		/* --batch,   -b		*/
 
 	/* constraint options */
 	int mincpus;		/* --mincpus=n			*/
