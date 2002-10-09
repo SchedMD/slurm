ac06d61504991db3aaff442ebb1fdc8e0fa9cd68
Moe Jette <jette1@llnl.gov>
2002-10-09 22:38:39 +0000

    Basic infrastructure in place for batch job submit.

--- src/srun/srun.c
+++ src/srun/srun.c
@@ -33,14 +33,22 @@
 #  include <pthread.h>
 #endif
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <linux/limits.h>
+#include <fcntl.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <signal.h>
+#include <unistd.h>
 
+#include <src/api/slurm.h>
 
 #include <src/common/log.h>
 #include <src/common/xstring.h>
-#include <src/api/slurm.h>
+#include <src/common/xmalloc.h>
 #include <src/common/slurm_protocol_api.h>
 
 #include <src/srun/opt.h>
@@ -56,6 +64,10 @@
 
 typedef resource_allocation_response_msg_t allocation_resp;
 
+#define	TYPE_NOT_TEXT	0
+#define	TYPE_TEXT	1
+#define	TYPE_SCRIPT	2
+
 /*
  * forward declaration of static funcs
  */
@@ -65,6 +77,11 @@ static void		 create_job_step(job_t *job);
 static void		 sigterm_handler(int signum);
 void *                   sig_thr(void *arg);
 void 			 fwd_signal(job_t *job, int signo);
+static char 		*build_script (char *pathname, int file_type);
+static char 		*find_file_path (char *fname);
+static char 		*get_shell (void);
+static int 		 is_file_text (char *fname);
+static int		 run_batch_job (void);
 
 #if HAVE_LIBELAN3
 #  include <src/common/qsw.h> 
@@ -101,7 +118,11 @@ main(int ac, char **av)
 	/* now global "opt" should be filled in and available,
 	 * create a job from opt
 	 */
-	if (opt.no_alloc) {
+	if (opt.batch) {
+		if (run_batch_job())
+			exit (1);
+		exit (0);
+	} else if (opt.no_alloc) {
 		printf("do not allocate resources\n");
 		job = job_create(NULL); 
 #if HAVE_LIBELAN3
@@ -115,7 +136,7 @@ main(int ac, char **av)
 		if (_verbose || _debug)
 			print_job_information(resp);
 		else
-			printf("jobid %d\n", resp->job_id); 
+			printf("jobid %u\n", resp->job_id); 
 
 		job = job_create(resp); 
 		create_job_step(job);
@@ -452,4 +473,259 @@ fwd_signal(job_t *job, int signo)
 					job->host[i]);
 	}
 
+}
+
+
+/* submit a batch job and return error code */
+static int
+run_batch_job(void)
+{
+	int i, file_type, rc, retries;
+	job_desc_msg_t job;
+	submit_response_msg_t *resp;
+	extern char **environ;
+	char *pathname, *job_script;
+
+	pathname = find_file_path (remote_argv[0]);
+	if (pathname == NULL)
+		return 1;
+	file_type = is_file_text (pathname);
+	if (file_type == TYPE_NOT_TEXT) {
+		error ("file %s is not script", pathname);
+		xfree (pathname);
+		return 1;
+	}
+	job_script = build_script (pathname, file_type);
+	if (job_script == NULL) {
+		error ("unable to build script from file %s", pathname);
+		xfree (pathname);
+		return 1;
+	}
+	xfree (pathname);
+
+	slurm_init_job_desc_msg(&job);
+
+	job.contiguous     = opt.contiguous;
+	job.features       = opt.constraints;
+
+	job.name           = opt.job_name;
+
+	job.partition      = opt.partition;
+
+	if (opt.mincpus > -1)
+		job.min_procs = opt.mincpus;
+	if (opt.realmem > -1)
+		job.min_memory = opt.realmem;
+	if (opt.tmpdisk > -1)
+		job.min_tmp_disk = opt.tmpdisk;
+
+	job.req_nodes      = opt.nodelist;
+
+	job.num_procs      = opt.nprocs;
+
+	if (opt.nodes > -1)
+		job.num_nodes = opt.nodes;
+
+	job.user_id        = opt.uid;
+
+/* 	job.kill_on_node_fail	= int; FIXME */
+/*	job.time_limit		= int; FIXME */
+/*	job.shared		= int; FIXME */
+	job.environment		= environ;
+	for (i=0; ; i++) {
+		if (environ[i] == NULL) {
+			job.env_size	= (i - 1);
+			break;
+		}
+	}
+	job.script		= job_script;
+	if (opt.efname)
+		job.stderr	= opt.efname;
+	if (opt.ifname)
+		job.stdin	= opt.ifname;
+	if (opt.ofname)
+		job.stdout	= opt.ofname;
+	job.work_dir		= opt.cwd;
+
+	retries = 0;
+	while ((rc = slurm_submit_batch_job(&job, &resp)) == SLURM_FAILURE) {
+		if ((slurm_get_errno() == ESLURM_ERROR_ON_DESC_TO_RECORD_COPY) &&
+		    (retries < MAX_RETRIES)) {
+			if (retries == 0)
+				error ("Slurm controller not responding, sleeping and retrying");
+			else
+				debug ("Slurm controller not responding, sleeping and retrying");
+
+			sleep (++retries);
+		}
+		else {
+			error("Unable to submit batch job resources: %s", 
+					slurm_strerror(errno));
+			return 1;
+		}			
+	}
+
+	
+	if (rc == 0) {
+		printf ("jobid %u\n",resp->job_id);
+		slurm_free_submit_response_response_msg (resp);
+	}
+	xfree (job_script);
+	return rc;
+}
+
+/* find_file_path - given a filename, return the full path to a regular file 
+ *	of that name that can be read or NULL otherwise
+ * NOTE: The calling function must xfree the return value (if set) 
+ */
+char *
+find_file_path (char *fname)
+{
+	int modes;
+	char *pathname;
+	struct stat stat_buf;
+
+	if (fname == NULL)
+		return NULL;
+
+	pathname = xmalloc (PATH_MAX);
+
+	/* generate a fully qualified pathname */
+	if (fname[0] == '/') {
+		if ((strlen (fname) + 1) > PATH_MAX) {
+			error ("Supplied filename too long: %s", fname);
+			goto cleanup;
+		}
+		strcpy (pathname, fname);
+	} else {
+		getcwd (pathname, PATH_MAX);
+		if ((strlen (pathname) + strlen (fname) + 2) > PATH_MAX) {
+			error ("Supplied filename too long: %s", fname);
+			goto cleanup;
+		}
+		strcat (pathname, "/");
+		strcat (pathname, fname);
+	}
+
+	/* determine if the file is accessable */
+	if (stat (pathname, &stat_buf) < 0) {
+		error ("Unable to stat file %s: %m", pathname);
+		goto cleanup;
+	}
+
+	if (S_ISREG (stat_buf.st_mode) == 0) {
+		error ("%s is not a regular file", pathname);
+		goto cleanup;
+	}
+
+	if (stat_buf.st_uid == getuid())
+		modes = (stat_buf.st_mode >> 6) & 0x7;
+	else if (stat_buf.st_gid == getgid())
+		modes = (stat_buf.st_mode >> 3) & 0x7;
+	else
+		modes =  stat_buf.st_mode       & 0x7;
+
+	if ((modes & 0x4) == 0) {
+		error ("%s can not be read", pathname);
+		goto cleanup;
+	}
+
+	return pathname;
+
+    cleanup:
+	xfree (pathname);
+	return NULL;
+}
+
+/* get_shell - return a string containing the default shell for this user
+ * NOTE: This function is NOT reentrant (see getpwuid_r if needed) */
+char *
+get_shell (void)
+{
+	struct passwd *pw_ent_ptr;
+
+	pw_ent_ptr = getpwuid (getuid ());
+	return pw_ent_ptr->pw_shell;
+}
+
+/* is_file_text - determine if specified file is a script
+ *	return 0 if the specified file can not be read or does not contain text
+ *	returns 2 if file contains text starting with "#!", otherwise
+ *	returns 1 if file contains text, but lacks "#!" header */
+int
+is_file_text (char *fname)
+{
+	int buf_size, fd, i;
+	int rc = 1;	/* initially assume the file contains text */
+	char buffer[128];
+
+	fd = open(fname, O_RDONLY);
+	if (fd < 0) {
+		error ("Unable to open file %s: %m", fname);
+		return 0;
+	}
+
+	buf_size = read (fd, buffer, sizeof (buffer));
+	if (buf_size < 0) {
+		error ("Unable to read file %s: %m", fname);
+		rc = 0;
+	}
+	(void) close (fd);
+
+	for (i=0; i<buf_size; i++) {
+		if (((buffer[i] >= 0x00) && (buffer[i] <= 0x06)) ||
+		    ((buffer[i] >= 0x0e) && (buffer[i] <= 0x1f)) ||
+		     (buffer[i] >= 0x7f)) {
+			rc = 0;
+			break;
+		}
+	}
+
+	if ((rc == 1) && (buf_size > 2)) {
+		if ((buffer[0] == '#') && (buffer[1] == '!'))
+			rc = 2;
+	}
+
+	return rc;
+}
+
+/* allocate and build a string containing a script for a batch job */
+char *
+build_script (char *fname, int file_type)
+{
+	char *buffer, *shell;
+	int buf_size, buf_used = 0, fd, data_size, i;
+
+	fd = open(fname, O_RDONLY);
+	if (fd < 0) {
+		error ("Unable to open file %s: %m", fname);
+		return NULL;
+	}
+
+	buf_size = 8096;
+	buffer = xmalloc (buf_size);
+	buf_used = 0;
+	if (file_type != TYPE_SCRIPT) {
+		shell = get_shell();
+		strcpy (buffer, "#!");
+		strcat (buffer, shell);
+		buf_used = strlen(buffer);
+	}
+
+	while (1) {
+		i = buf_size - buf_used;
+		if (i < 1024) {
+			buf_size += 8096;
+			xrealloc (buffer, buf_size);
+			i = buf_size - buf_used;
+		}
+		data_size = read (fd, &buffer[buf_used], i);
+		if (data_size <= 0)
+			break;
+		buf_used += i;
+	}
+	buffer[buf_used] = '\000';
+	(void) close (fd);
+
+	return buffer;
 }
