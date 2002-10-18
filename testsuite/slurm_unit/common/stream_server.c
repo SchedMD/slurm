fe4191125fd98cedd11d924cfdd1579716447c4e
Moe Jette <jette1@llnl.gov>
2002-10-18 16:47:05 +0000

    Minor code clean-up, comments, error checking. No fundamental changes to logic.

--- testsuite/slurm_unit/common/stream_server.c
+++ testsuite/slurm_unit/common/stream_server.c
@@ -1,3 +1,6 @@
+/* Slurm stream server test, read stdin until "quit" is entered, anything else is sent 
+ * out to the specified SLURM port, anything received on that port is printed */
+#include <src/common/log.h>
 #include <netinet/in.h>
 #include <src/common/slurm_protocol_api.h>
 #include <stdlib.h>
@@ -12,11 +15,11 @@ void* read2stdout_thread( void* arg )
 	unsigned int buffer_len = 1024*1024 ;
 	int bytes_read ;
 	char buffer[ buffer_len ] ;
-	slurm_fd fd = (slurm_fd) arg ;
+	slurm_fd *fd = (slurm_fd *) arg ;
 
 	while(1)
 	{
-		if ( ( bytes_read = slurm_read_stream( fd, buffer, buffer_len ) ) < 0 )
+		if ( ( bytes_read = slurm_read_stream( *fd, buffer, buffer_len ) ) < 0 )
 			break;
 		buffer[ bytes_read ] = '\0' ;
 		printf( "%s", buffer );
@@ -33,8 +36,11 @@ void stdout2socket_loop( slurm_fd fd )
 	int curr_size = 0 ;
 	while (1)
 	{
-		if( (bytes_read = read(0, current, buffer_len - curr_size ) ) < 0 )
+		if( (bytes_read = read(STDIN_FILENO, current, buffer_len-curr_size ) ) < 0 )
+		{
+			printf("read error\n");
 			break;
+		}
 		
 		curr_size += bytes_read ;
 		if ( '\n' == buffer[ curr_size - 1 ] )
@@ -42,7 +48,7 @@ void stdout2socket_loop( slurm_fd fd )
 			if ( strncmp( "quit", buffer, 4 ) == 0 )
 				break;
 			buffer[ curr_size ] = '\0' ;
-			if ( slurm_write_stream( fd, buffer, curr_size ) < 0 )
+			if ( slurm_write_stream( fd, buffer, curr_size ) < 0 )  /* fails */
 			{
 				printf("Could not send\n");
 				break;
@@ -56,7 +62,7 @@ void stdout2socket_loop( slurm_fd fd )
 
 int main ( int argc , char * argv[] )
 {
-	/* declare file descriptors */
+	log_options_t log_opts = { 1, LOG_LEVEL_DEBUG3,  LOG_LEVEL_INFO, LOG_LEVEL_QUIET } ;
 	slurm_fd listen_socket ;
 	slurm_fd worker_socket ;
 
@@ -66,17 +72,28 @@ int main ( int argc , char * argv[] )
 
 	/* read thread */
 	pthread_t read_pth ;
-	int16_t port = atoi( argv[1]) ;
+	int16_t port;
 
-		
+	if (argc > 1)
+		port = atoi( argv[1] ) ;
+
+	if ((argc < 2) || (port < 1)) {
+		printf("Usage: %s <port_number>\n", argv[0] );
+		exit( 1 );
+	}
+
+	
 	/* init address sturctures */
+	log_init(argv[0], log_opts, SYSLOG_FACILITY_DAEMON, NULL);
 	slurm_set_addr_uint ( & listen_address , port, SLURM_INADDR_ANY ) ;
+
 	/* open and listen on socket */
 	listen_socket = slurm_listen_stream ( & listen_address ) ;
+
 	/* accept socket */
 	worker_socket = slurm_accept_stream ( listen_socket , & worker_address ) ;
 
-	if ( pthread_create( &read_pth, NULL, &read2stdout_thread, (void*) worker_socket ) ) 	
+	if ( pthread_create( &read_pth, NULL, &read2stdout_thread, (void*) &worker_socket ) ) 	
 	{
 		printf("Could not create read_thread: error=%d\n", errno );
 		exit( errno );
