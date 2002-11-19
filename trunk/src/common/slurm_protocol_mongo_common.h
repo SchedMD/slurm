db40cc67fe490c471e356eec42f984f18f0a5bd2
Moe Jette <jette1@llnl.gov>
2002-11-19 19:20:33 +0000

    Add disclaimer, code clean-up.

--- trunk/src/common/slurm_protocol_mongo_common.h
+++ trunk/src/common/slurm_protocol_mongo_common.h
@@ -1,8 +1,36 @@
+/*****************************************************************************\
+ *  slurm_protocol_mongo_common.h - alternative communications protocol to 
+ *	TCP sockets. As of 11/18/2002 it is unclear that this communications
+ * 	protocol will be fully developed.
+ ****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Kevin Tew <tew1@llnl.gov> 
+ *  UCRL-CODE-2002-040.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+
 #ifndef _SLURM_PROTOCOL_MONGO_COMMON_H
 #define _SLURM_PROTOCOL_MONGO_COMMON_H
 
 #if HAVE_CONFIG_H
-#  include <config.h>
+#  include "config.h"
 #  if HAVE_INTTYPES_H
 #    include <inttypes.h>
 #  else
