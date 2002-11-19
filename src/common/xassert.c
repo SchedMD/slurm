46abe210048f8f8cf4eee312d206ad636b95e5bb
Moe Jette <jette1@llnl.gov>
2002-11-19 19:14:37 +0000

    Added disclaimers, code clean-up, no logic changes.

--- src/common/xassert.c
+++ src/common/xassert.c
@@ -6,7 +6,7 @@
 */
 
 #if HAVE_CONFIG_H
-#  include <config.h>
+#  include "config.h"
 #endif
 
 #include <stdlib.h>
--- src/common/xmalloc.h
+++ src/common/xmalloc.h
@@ -1,14 +1,33 @@
 /* $Id$ */
-
-/* 
- * xmalloc: enhanced malloc routines for slurm
- * 
- *  o default: never return if errors are encountered.
- *
- *  o attempt to report file, line, and calling function on assertion failure
- *
- *  o use configurable slurm log facility for reporting errors
- *
+/*****************************************************************************\
+ *  xmalloc.h - enhanced malloc routines for slurm
+ *  - default: never return if errors are encountered.
+ *  - attempt to report file, line, and calling function on assertion failure
+ *  - use configurable slurm log facility for reporting errors
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Jim Garlick <garlick1@llnl.gov> and 
+ *	Mark Grondona <mgrondona@llnl.gov>
+ *  UCRL-CODE-2002-040.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ *****************************************************************************
  * Description:
  *
  * void *xmalloc(size_t size);
@@ -41,7 +60,7 @@
  * p. The memory must have been allocated with [try_]xmalloc() or 
  * [try_]xrealloc().
  *
- */
+\*****************************************************************************/
 
 #ifndef _XMALLOC_H
 #define _XMALLOC_H
