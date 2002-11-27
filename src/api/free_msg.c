cdb53141147bc5bd2ad4d3317fd39c0945b136f8
Moe Jette <jette1@llnl.gov>
2002-11-27 23:45:02 +0000

    Code clean-up. No logic changes.

--- src/api/free_msg.c
+++ src/api/free_msg.c
@@ -47,6 +47,10 @@ static void _slurm_free_partition_info_members (partition_info_t * part);
 static void _free_all_step_info (job_step_info_response_msg_t *msg);
 static void _slurm_free_job_step_info_members (job_step_info_t * msg);
 
+#define X_FREE(_X)			\
+	do {				\
+		if (_X) xfree (_X);	\
+	} while (0)
 
 /*
  * slurm_free_resource_allocation_response_msg - free slurm resource
@@ -58,14 +62,10 @@ void slurm_free_resource_allocation_response_msg (
 				resource_allocation_response_msg_t * msg)
 {
 	if (msg) {
-		if (msg->node_list)
-			xfree(msg->node_list);
-		if (msg->cpus_per_node)
-			xfree(msg->cpus_per_node);
-		if (msg->cpu_count_reps)
-			xfree(msg->cpu_count_reps);
-		if (msg->node_addr)
-			xfree(msg->node_addr);
+		X_FREE(msg->node_list);
+		X_FREE(msg->cpus_per_node);
+		X_FREE(msg->cpu_count_reps);
+		X_FREE(msg->node_addr);
 		xfree(msg);
 	}
 }
@@ -81,16 +81,11 @@ void slurm_free_resource_allocation_and_run_response_msg (
 			resource_allocation_and_run_response_msg_t * msg)
 {
 	if (msg) {
-		if (msg->node_list)
-			xfree(msg->node_list);
-		if (msg->cpus_per_node)
-			xfree(msg->cpus_per_node);
-		if (msg->cpu_count_reps)
-			xfree(msg->cpu_count_reps);
-		if (msg->credentials)
-			xfree(msg->credentials);
-		if (msg->node_addr)
-			xfree(msg->node_addr);
+		X_FREE(msg->node_list);
+		X_FREE(msg->cpus_per_node);
+		X_FREE(msg->cpu_count_reps);
+		X_FREE(msg->node_addr);
+		X_FREE(msg->credentials);
 #		ifdef HAVE_LIBELAN3
 		qsw_free_jobinfo(msg->qsw_job);
 #		endif
@@ -109,8 +104,7 @@ void slurm_free_job_step_create_response_msg(
 		job_step_create_response_msg_t * msg)
 {
 	if (msg) {
-		if (msg->credentials)
-			xfree(msg->credentials);
+		X_FREE(msg->credentials);
 
 #		ifdef HAVE_LIBELAN3
 		qsw_free_jobinfo(msg->qsw_job);
@@ -143,42 +137,24 @@ void slurm_free_submit_response_response_msg(submit_response_msg_t * msg)
 void slurm_free_ctl_conf(slurm_ctl_conf_info_msg_t * config_ptr)
 {
 	if (config_ptr) {
-		if (config_ptr->backup_addr)
-			xfree(config_ptr->backup_addr);
-		if (config_ptr->backup_controller)
-			xfree(config_ptr->backup_controller);
-		if (config_ptr->control_addr)
-			xfree(config_ptr->control_addr);
-		if (config_ptr->control_machine)
-			xfree(config_ptr->control_machine);
-		if (config_ptr->epilog)
-			xfree(config_ptr->epilog);
-		if (config_ptr->job_credential_private_key)
-			xfree(config_ptr->job_credential_private_key);
-		if (config_ptr->job_credential_public_certificate)
-			xfree(config_ptr->job_credential_public_certificate);
-		if (config_ptr->prioritize)
-			xfree(config_ptr->prioritize);
-		if (config_ptr->prolog)
-			xfree(config_ptr->prolog);
-		if (config_ptr->slurm_user_name)
-			xfree(config_ptr->slurm_user_name);
-		if (config_ptr->slurmctld_pidfile)
-			xfree(config_ptr->slurmctld_pidfile);
-		if (config_ptr->slurmctld_logfile)
-			xfree(config_ptr->slurmctld_logfile);
-		if (config_ptr->slurmd_logfile)
-			xfree(config_ptr->slurmd_logfile);
-		if (config_ptr->slurmd_pidfile)
-			xfree(config_ptr->slurmd_pidfile);
-		if (config_ptr->slurmd_spooldir)
-			xfree(config_ptr->slurmd_spooldir);
-		if (config_ptr->slurm_conf)
-			xfree(config_ptr->slurm_conf);
-		if (config_ptr->state_save_location)
-			xfree(config_ptr->state_save_location);
-		if (config_ptr->tmp_fs)
-			xfree(config_ptr->tmp_fs);
+		X_FREE(config_ptr->backup_addr);
+		X_FREE(config_ptr->backup_controller);
+		X_FREE(config_ptr->control_addr);
+		X_FREE(config_ptr->control_machine);
+		X_FREE(config_ptr->epilog);
+		X_FREE(config_ptr->job_credential_private_key);
+		X_FREE(config_ptr->job_credential_public_certificate);
+		X_FREE(config_ptr->prioritize);
+		X_FREE(config_ptr->prolog);
+		X_FREE(config_ptr->slurm_user_name);
+		X_FREE(config_ptr->slurmctld_pidfile);
+		X_FREE(config_ptr->slurmctld_logfile);
+		X_FREE(config_ptr->slurmd_logfile);
+		X_FREE(config_ptr->slurmd_pidfile);
+		X_FREE(config_ptr->slurmd_spooldir);
+		X_FREE(config_ptr->slurm_conf);
+		X_FREE(config_ptr->state_save_location);
+		X_FREE(config_ptr->tmp_fs);
 		xfree(config_ptr);
 	}
 }
@@ -215,18 +191,12 @@ static void _free_all_job_info(job_info_msg_t *msg)
 static void _slurm_free_job_info_members(job_info_t * job)
 {
 	if (job) {
-		if (job->nodes)
-			xfree(job->nodes);
-		if (job->partition)
-			xfree(job->partition);
-		if (job->name)
-			xfree(job->name);
-		if (job->node_inx)
-			xfree(job->node_inx);
-		if (job->req_nodes)
-			xfree(job->req_nodes);
-		if (job->features)
-			xfree(job->features);
+		X_FREE(job->nodes);
+		X_FREE(job->partition);
+		X_FREE(job->name);
+		X_FREE(job->node_inx);
+		X_FREE(job->req_nodes);
+		X_FREE(job->features);
 		xfree(job->req_node_inx);
 	}
 }
@@ -265,10 +235,8 @@ static void _free_all_step_info (job_step_info_response_msg_t *msg)
 static void _slurm_free_job_step_info_members (job_step_info_t * msg)
 {
 	if (msg != NULL) {
-		if (msg->partition != NULL)
-			xfree(msg->partition);
-		if (msg->nodes != NULL)
-			xfree(msg->nodes);
+		X_FREE(msg->partition);
+		X_FREE(msg->nodes);
 	}
 }
 
@@ -304,12 +272,9 @@ static void _free_all_node_info(node_info_msg_t *msg)
 static void _slurm_free_node_info_members(node_info_t * node)
 {
 	if (node) {
-		if (node->name)
-			xfree(node->name);
-		if (node->features)
-			xfree(node->features);
-		if (node->partition)
-			xfree(node->partition);
+		X_FREE(node->name);
+		X_FREE(node->features);
+		X_FREE(node->partition);
 	}
 }
 
@@ -348,14 +313,10 @@ static void  _free_all_partitions(partition_info_msg_t *msg)
 static void _slurm_free_partition_info_members(partition_info_t * part)
 {
 	if (part) {
-		if (part->name)
-			xfree(part->name);
-		if (part->allow_groups)
-			xfree(part->allow_groups);
-		if (part->nodes)
-			xfree(part->nodes);
-		if (part->node_inx)
-			xfree(part->node_inx);
+		X_FREE(part->name);
+		X_FREE(part->allow_groups);
+		X_FREE(part->nodes);
+		X_FREE(part->node_inx);
 	}
 }
 
