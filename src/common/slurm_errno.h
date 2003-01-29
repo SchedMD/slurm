e49b2ac02fd1a5bda2d7be0fd9986d3691dfd2e9
Moe Jette <jette1@llnl.gov>
2003-01-29 01:26:35 +0000

    Modify header files for C++ support (see http://www.gnu.org/software/libtool/manual.html#SEC38

--- src/common/slurm_errno.h
+++ src/common/slurm_errno.h
@@ -27,6 +27,33 @@
 #ifndef _SLURM_ERRNO_H
 #define _SLURM_ERRNO_H
 
+/* BEGIN_C_DECLS should be used at the beginning of your declarations,
+   so that C++ compilers don't mangle their names.  Use _END_C_DECLS at
+   the end of C declarations. */
+#undef BEGIN_C_DECLS
+#undef END_C_DECLS
+#ifdef __cplusplus
+# define BEGIN_C_DECLS	extern "C" {
+# define END_C_DECLS	}
+#else
+# define BEGIN_C_DECLS	/* empty */
+# define END_C_DECLS	/* empty */
+#endif
+
+/* PARAMS is a macro used to wrap function prototypes, so that compilers
+   that don't understand ANSI C prototypes still work, and ANSI C
+   compilers can issue warnings about type mismatches.  */
+#undef PARAMS
+#if defined (__STDC__) || defined (_AIX) \
+	|| (defined (__mips) && defined (_SYSTYPE_SVR4)) \
+	|| defined(WIN32) || defined(__cplusplus)
+# define PARAMS(protos)	protos
+#else
+# define PARAMS(protos)	()
+#endif
+
+BEGIN_C_DECLS
+
 #include <errno.h>
 
 /* set errno to the specified value - then return -1 */ 
@@ -162,15 +189,17 @@ enum {
 };
 
 /* look up an errno value */
-char * slurm_strerror(int errnum);
+char * slurm_strerror PARAMS((int errnum));
 
 /* set an errno value */
-void slurm_seterrno(int errnum);
+void slurm_seterrno PARAMS((int errnum));
 
 /* get an errno value */
-inline int slurm_get_errno();
+inline int slurm_get_errno PARAMS((void));
 
 /* print message: error string for current errno value */
-void slurm_perror(char *msg);
+void slurm_perror PARAMS((char *msg));
+
+END_C_DECLS
 
 #endif /* !_SLURM_ERRNO_H */
