278a62ec70c606c69ee3736f36d5e434e2a3ce36
Moe Jette <jette1@llnl.gov>
2003-01-30 01:54:30 +0000

    Link and test manual api tests against public libraries and headers.

--- trunk/testsuite/slurm_unit/api/manual/partition_info-tst.c
+++ trunk/testsuite/slurm_unit/api/manual/partition_info-tst.c
@@ -21,7 +21,8 @@ main (int argc, char *argv[])
 	}
 
 	note("Updated at %ld, record count %d\n",
-		(time_t) part_info_ptr->last_update, part_info_ptr->record_count);
+		(time_t) part_info_ptr->last_update, 
+		part_info_ptr->record_count);
 
 	slurm_print_partition_info_msg ( stdout, part_info_ptr );
 	slurm_free_partition_info_msg (part_info_ptr);
