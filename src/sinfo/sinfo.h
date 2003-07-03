77559b0ff896840fa13e60975462eeeec94ba14b
Moe Jette <jette1@llnl.gov>
2003-07-03 18:15:00 +0000

    Major changes in sinfo, output format changes and environment variables added. Major restructuring of the sinfo code.

--- src/sinfo/sinfo.h
+++ src/sinfo/sinfo.h
@@ -22,9 +22,10 @@
  *  You should have received a copy of the GNU General Public License along
  *  with SLURM; if not, write to the Free Software Foundation, Inc.,
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
-\*****************************************************************************/
+\****************************************************************************/
 
-#ifndef __SINFO_H__
+#ifndef _SINFO_H
+#define _SINFO_H
 
 #if HAVE_CONFIG_H
 #  include "config.h"
@@ -32,6 +33,10 @@
 
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #if HAVE_INTTYPES_H
 #  include <inttypes.h>
@@ -41,47 +46,85 @@
 #  endif
 #endif  /* HAVE_INTTYPES_H */
 
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
-
 #include <slurm/slurm.h>
 
 #include "src/common/hostlist.h"
 #include "src/common/list.h"
 #include "src/common/log.h"
+#include "src/common/macros.h"
 #include "src/common/slurm_protocol_api.h"
 #include "src/common/xmalloc.h"
 
+/* Collection of data for printing reports. Like data is combined here */
+typedef struct {
+	uint16_t node_state;
 
-struct sinfo_parameters {
-	char* partition;
+	uint32_t nodes_alloc;
+	uint32_t nodes_idle;
+	uint32_t nodes_other;
+	uint32_t nodes_tot;
+	uint32_t min_cpus;
+	uint32_t max_cpus;
+	uint32_t min_disk;
+	uint32_t max_disk;
+	uint32_t min_mem;
+	uint32_t max_mem;
+	uint32_t min_weight;
+	uint32_t max_weight;
+
+	char *features;
+
+	hostlist_t nodes;
+
+	/* part_info contains partition, avail, max_time, job_size, 
+	 * root, share, groups */
+	partition_info_t* part_info;
+} sinfo_data_t;
+
+/* Identify what fields must match for a node's information to be 
+ * combined into a single sinfo_data entry based upon output format */
+struct sinfo_match_flags {
+	bool avail_flag;
+	bool features_flag;
+	bool groups_flag;
+	bool job_size_flag;
+	bool max_time_flag;
+	bool partition_flag;
+	bool root_flag;
+	bool share_flag;
 	bool state_flag;
-	enum node_states state;
-	bool node_flag;
-	char* nodes;
-	bool summarize;
+};
+
+/* Input parameters */
+struct sinfo_parameters {
+	bool exact_match;
 	bool long_output;
 	bool line_wrap;
-	int verbose;
+	bool no_header;
+	bool node_field_flag;
+	bool node_flag;
+	bool state_flag;
+	bool summarize;
+	struct sinfo_match_flags match_flags;
+
+	char* format;
+	char* nodes;
+	char* partition;
+
 	int iterate;
-	bool exact_match;
-};
-
-struct node_state_summary {
-	hostlist_t nodes;
+	int node_field_size;
 	enum node_states state;
-	uint32_t node_count;
+	int verbose;
+
+	List  format_list;
 };
 
-struct partition_summary {
-	partition_info_t* info;
-	List states;
-};
+extern struct sinfo_parameters params;
 
-int parse_state( char* str, enum job_states* states );
-int parse_command_line( int argc, char* argv[] );
-void print_options( void );
+int  parse_command_line( int argc, char* argv[] );
+int  parse_state( char* str, enum job_states* states );
+void print_date(void);
+int  print_sinfo_entry(sinfo_data_t *sinfo_data , List format);
+int  print_sinfo_list(List sinfo_list, List format);
 
 #endif
