c2c2e94e536c79883200f2ab31aab019dac88d04
Moe Jette <jette1@llnl.gov>
2003-08-15 01:44:37 +0000

    Pass pathnames of slurm commands to bash script to permit testing of alternate systems in parallel.

--- qa/trunk/test9.7.bash
+++ qa/trunk/test9.7.bash
@@ -1,15 +1,30 @@
 #!/bin/bash
 # Simple SLURM stress test
-# Usage: <prog> <sleep_time> <iterations>
-# Default is 1 second sleep and 3 iterations
+# Usage: <prog> <exec1> <exec2> <exec3> <sleep_time> <iterations>
+# Default is sinfo, srun, squeue, 1 second sleep and 3 iterations
 if [ $# -gt 0 ]; then
-	sleep_time=$1
+	exec1=$1
+else
+	exec1="sinfo"
+fi
+if [ $# -gt 1 ]; then
+	exec2=$2
+else
+	exec2="srun"
+fi
+if [ $# -gt 2 ]; then
+	exec3=$3
+else
+	exec3="squeue"
+fi
+if [ $# -gt 3 ]; then
+	sleep_time=$4
 else
 	sleep_time=1
 fi
 
-if [ $# -gt 1 ]; then
-	iterations=$2
+if [ $# -gt 4 ]; then
+	iterations=$5
 else
 	iterations=3
 fi
@@ -21,17 +36,17 @@ touch $log
 while [ $inx -le $iterations ]
 do
 	echo "########## LOOP $inx ########## " >>$log 2>&1
-	sinfo                                   >>$log 2>&1
+	$exec1                                  >>$log 2>&1
 	if [ $? -ne 0 ]; then
 		exit_code=$?
 	fi
 	sleep $sleep_time
-	srun -N1-$inx -c1 -l hostname           >>$log 2>&1
+	$exec2 -N1-$inx -c1 -l hostname         >>$log 2>&1
 	if [ $? -ne 0 ]; then
 		exit_code=$?
 	fi
 	sleep $sleep_time
-	squeue                                  >>$log 2>&1
+	$exec3                                  >>$log 2>&1
 	if [ $? -ne 0 ]; then
 		exit_code=$?
 	fi
