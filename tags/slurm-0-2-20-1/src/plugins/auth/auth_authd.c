0fcffec67c970b1e0fd5a811bef48c058e7e2f68
no author <no@author>
2003-10-06 23:45:01 +0000

    This commit was manufactured by cvs2svn to create tag 'slurm-0-2-20-1'.

---
+++ tags/slurm-0-2-20-1/src/plugins/auth/auth_authd.c
@@ -0,0 +1,516 @@
+/*****************************************************************************\
+ *  auth_authd - plugin for Brent Chun's authd
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Kevin Tew <tew1@llnl.gov> et. al.
+ *  UCRL-CODE-2002-040.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#  if HAVE_SSL
+#    include <openssl/rsa.h>
+#    include <openssl/pem.h>
+#    include <openssl/err.h>
+#  endif /* HAVE_SSL*/
+#  if STDC_HEADERS
+#    include <stdio.h>
+#    include <string.h>
+#  endif /* STDC_HEADERS */
+#  if HAVE_UNISTD_H
+#    include <unistd.h>
+#  endif /* HAVE_UNISTD_H */
+#else /* ! HAVE_CONFIG_H */
+#  include <stdio.h>
+#  include <unistd.h>
+#  include <string.h>
+#  include <openssl/rsa.h>
+#  include <openssl/pem.h>
+#  include <openssl/err.h>
+#  include <auth.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <pwd.h>
+#include <grp.h>
+#include <auth.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+
+/* Need these regardless of how main SLURM transport is abstracted. */
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#ifndef UNIX_PATH_MAX
+#define UNIX_PATH_MAX 108  /* Cribbed from linux/un.h */
+#endif
+
+#include <slurm/slurm_errno.h>
+#include "src/common/slurm_auth.h"
+#include "src/common/xmalloc.h"
+#include "src/common/xassert.h"
+#include "src/common/pack.h"
+#include "src/common/log.h"
+#include "src/common/arg_desc.h"
+
+const char plugin_name[]	= "Brent Chun's authd authentication plugin";
+const char plugin_type[]	= "auth/authd";
+const uint32_t plugin_version = 90;
+
+/*
+ * Where to find the timeout in the argument vector.  This is set
+ * during initialization and should not change.
+ */
+static int timeout_idx = -1;
+
+/* Default timeout. */
+static const int AUTHD_TTL = 2;
+
+typedef struct _slurm_auth_credential {
+	credentials cred;
+	signature sig;
+	int cr_errno;
+} slurm_auth_credential_t;
+
+/* Plugin-global errno. */
+static int plugin_errno = SLURM_SUCCESS;
+
+/*
+ * Errno values.  If you add something here you have to add its
+ * corresponding error string in slurm_auth_errstr() below.
+ */
+enum {
+	SLURM_AUTH_UNPACK = SLURM_AUTH_FIRST_LOCAL_ERROR,
+	SLURM_AUTH_EXPIRED
+};
+
+/*
+ * These come from /usr/include/auth.h which should be installed
+ * as part of the authd installation.
+ */
+static char *cli_path = AUTH_SOCK_PATH;
+static char *svr_path = AUTHD_SOCK_PATH;
+static char *pub_key_file = AUTH_PUB_KEY;
+
+/*
+ * Write bytes reliably to a file descriptor.
+ */
+static int
+write_bytes( int fd, char *buf, size_t size )
+{
+	ssize_t bytes_remaining, bytes_written;
+	char *ptr;
+
+	bytes_remaining = size;
+	ptr = buf;
+	while ( bytes_remaining > 0 ) {
+		bytes_written = write( fd, ptr, size );
+		if ( bytes_written < 0 ) return -1;
+		bytes_remaining -= bytes_written;
+		ptr += bytes_written;
+	}
+	return 0;
+}
+
+/*
+ * Read bytes reliably from a file descriptor.
+ */
+static int
+read_bytes( int fd, char *buf, size_t size )
+{
+	ssize_t bytes_remaining, bytes_read;
+	char *ptr;
+
+	bytes_remaining = size;
+	ptr = buf;
+	while ( bytes_remaining > 0 ) {
+		bytes_read = read( fd, ptr, size );
+		if ( bytes_read < 0 ) return -1;
+		bytes_remaining -= bytes_read;
+		ptr += bytes_read;
+	}
+	return 0;
+}
+
+/*
+ * These two cribbed from auth.c in the authd distribution.  They would
+ * normally be available in the authd library, but the library relies on
+ * Brent Chun's enormous and irrelevant convenience library, and we only
+ * need to make one call to that library here.  So we inline the code
+ * from his library and sever the dependency.
+ */
+static int
+slurm_auth_get_signature( credentials *cred, signature *sig )
+{
+	int sock;
+	char cli_name[ UNIX_PATH_MAX ];
+	struct sockaddr_un cli_addr;
+	struct sockaddr_un svr_addr;
+	socklen_t addr_len = sizeof( struct sockaddr_un );
+
+	if ( ( sock = socket( AF_UNIX, SOCK_STREAM, 0 ) ) < 0 ) {
+		return -1;
+	}
+
+	cli_addr.sun_family = AF_UNIX;	
+	memset( cli_addr.sun_path, 0, UNIX_PATH_MAX );
+	sprintf( cli_name, cli_path, getpid() );
+	strcpy( &cli_addr.sun_path[ 1 ], cli_name );
+	if ( bind( sock, (struct sockaddr *) &cli_addr, addr_len ) < 0 ) {
+		error( "authd plugin: cannot bind socket to authd" );
+		close( sock );
+		return -1;
+	}
+
+	svr_addr.sun_family = AF_UNIX;
+	memset( svr_addr.sun_path, 0, UNIX_PATH_MAX );
+	strcpy( &svr_addr.sun_path[ 1 ], svr_path );
+	if ( connect( sock, (struct sockaddr *) &svr_addr, addr_len ) < 0 ) {
+		error( "suthd plugin: cannot connect to authd" );
+		close( sock );
+		return -1;
+	}
+	
+	if ( write_bytes( sock, (char *) cred, sizeof( credentials ) ) < 0 ) {
+		error( "authd plugin: cannot write to authd" );
+		close( sock );
+		return -1;
+	}
+	if ( read_bytes( sock, (char *) sig, sizeof( signature ) ) < 0 ) {
+		error( "authd plugin: cannot read from authd" );
+		close( sock );
+		return -1;
+	}
+	close( sock );
+	return 0;	
+}
+
+static int
+slurm_auth_verify_signature( credentials *cred, signature *sig )
+{
+	int rc_error = 0;
+	RSA *pub_key = NULL;
+	FILE *f = NULL;
+
+	if ( ( f = fopen( pub_key_file, "r" ) ) == NULL ) {		
+		rc_error = -1;
+		error( "authd plugin: cannot open public key file %s", pub_key_file );
+		goto cleanup;
+	}
+
+	if ( ( pub_key = PEM_read_RSA_PUBKEY( f, NULL, NULL, NULL ) ) == NULL ) {
+		error( "authd plugin: cannot read RSA public key" );
+		rc_error = -1;
+		goto cleanup;
+	}
+
+	ERR_load_crypto_strings();
+	if ( RSA_verify( 0,
+					 (unsigned char *) cred, sizeof( credentials ),
+					 sig->data, AUTH_RSA_SIGLEN,
+					 pub_key ) == 0 ) {
+		rc_error = -1;
+		error( "authd plugin: cannot verify signature" );
+		goto cleanup;
+	}
+
+ cleanup:
+	if ( pub_key != NULL ) RSA_free( pub_key );
+	if ( f != NULL ) fclose( f );
+	return rc_error;
+}
+
+
+int init( void )
+{
+	const arg_desc_t *desc;
+	
+	verbose( "authd authentication module initializing" );
+
+	if ( ( desc = slurm_auth_get_arg_desc() ) == NULL ) {
+		error( "unable to query SLURM for argument vector layout" );
+		return SLURM_ERROR;
+	}
+
+	if ( ( timeout_idx = arg_idx_by_name( desc, ARG_TIMEOUT ) ) < 0 ) {
+		error( "Required argument 'Timeout' not provided" );
+		return SLURM_ERROR;
+	}
+	
+	return SLURM_SUCCESS;
+}
+
+
+slurm_auth_credential_t *
+slurm_auth_create( void *argv[] )
+{
+	int ttl;	
+	slurm_auth_credential_t *cred;
+
+	if ( argv == NULL ) {
+		plugin_errno = SLURM_AUTH_MEMORY;
+		return NULL;
+	}
+
+	cred = (slurm_auth_credential_t *)
+		xmalloc( sizeof( slurm_auth_credential_t ) );
+	cred->cr_errno = SLURM_SUCCESS;
+	cred->cred.uid = geteuid();
+	cred->cred.gid = getegid();
+
+	cred->cred.valid_from = time( NULL );
+
+	ttl = timeout_idx >= 0 ? (int) argv[ timeout_idx ] : AUTHD_TTL;
+	/*
+	 * In debug mode read the time-to-live from an environment
+	 * variable.
+	 */
+#ifndef NDEBUG
+	{
+		char *env = getenv( "SLURM_AUTHD_TTL" );
+		if ( env ) {
+			ttl = atoi( env );
+			if ( ttl <= 0 ) ttl = AUTHD_TTL;
+		}
+	}
+#endif /*NDEBUG*/
+
+	cred->cred.valid_to = cred->cred.valid_from + ttl;
+
+	/* Sign the credential. */
+	if ( slurm_auth_get_signature( &cred->cred, &cred->sig ) < 0 ) {
+		plugin_errno = SLURM_AUTH_INVALID;
+		xfree( cred );
+		return NULL;
+	}
+	
+	return cred;
+}
+
+int
+slurm_auth_destroy( slurm_auth_credential_t *cred )
+{
+	if ( cred == NULL ) {
+		plugin_errno = SLURM_AUTH_BADARG;
+		return SLURM_ERROR;
+	}
+	xfree( cred );
+	return SLURM_SUCCESS;
+}
+
+int
+slurm_auth_verify( slurm_auth_credential_t *cred, void *argv[] )
+{
+	int rc;
+	time_t now;
+
+	if ( ( cred == NULL ) || ( argv == NULL ) ) {
+		plugin_errno = SLURM_AUTH_BADARG;
+		return SLURM_ERROR;
+	}
+	
+	rc = slurm_auth_verify_signature( &cred->cred, &cred->sig );
+	if ( rc < 0 ) {
+		cred->cr_errno = SLURM_AUTH_INVALID;
+		return SLURM_ERROR;
+	}
+
+	now = time( NULL );
+	if ( ( now < cred->cred.valid_from ) || ( now > cred->cred.valid_to ) ) {
+		cred->cr_errno = SLURM_AUTH_EXPIRED;
+		return SLURM_ERROR;
+	}
+
+	/* XXX check to see if user is valid on the system. */
+	
+	return SLURM_SUCCESS;
+}
+
+
+uid_t
+slurm_auth_get_uid( slurm_auth_credential_t *cred )
+{
+	if ( cred == NULL ) {
+		plugin_errno = SLURM_AUTH_BADARG;
+		return SLURM_AUTH_NOBODY;
+	}
+	return cred->cred.uid;
+}
+
+
+gid_t
+slurm_auth_get_gid( slurm_auth_credential_t *cred )
+{
+	if ( cred == NULL ) {
+		plugin_errno = SLURM_AUTH_BADARG;
+		return SLURM_AUTH_NOBODY;
+	}
+	return cred->cred.gid;
+}
+
+
+int
+slurm_auth_pack( slurm_auth_credential_t *cred, Buf buf )
+{
+	uint16_t sig_size = sizeof( signature );
+
+	if ( ( cred == NULL ) || ( buf == NULL ) ) {
+		plugin_errno = SLURM_AUTH_BADARG;
+		return SLURM_ERROR;
+	}
+
+	/*
+	 * Marshall the plugin type and version for runtime sanity check.
+	 * Add the terminating zero so we get it for free at the
+	 * other end.
+	 */
+	packmem( (char *) plugin_type, strlen( plugin_type ) + 1, buf );
+	pack32( plugin_version, buf );
+	
+	pack32( cred->cred.uid, buf );
+	pack32( cred->cred.gid, buf );
+	pack_time( cred->cred.valid_from, buf );
+	pack_time( cred->cred.valid_to, buf );
+	packmem( cred->sig.data, sig_size, buf );
+
+	return SLURM_SUCCESS;
+}
+
+
+slurm_auth_credential_t *
+slurm_auth_unpack( Buf buf )
+{
+	slurm_auth_credential_t *cred;
+	uint16_t sig_size; /* ignored */
+	uint32_t version;	
+	char *data;
+
+	if ( buf == NULL ) {
+		plugin_errno = SLURM_AUTH_BADARG;
+		return NULL;
+	}
+
+	
+	/* Check the plugin type. */
+	if ( unpackmem_ptr( &data, &sig_size, buf ) != SLURM_SUCCESS ) {
+		plugin_errno = SLURM_AUTH_UNPACK;
+		return NULL;
+	}
+	if ( strcmp( data, plugin_type ) != 0 ) {
+		plugin_errno = SLURM_AUTH_MISMATCH;
+		return NULL;
+	}
+
+	if ( unpack32( &version, buf ) != SLURM_SUCCESS ) {
+		plugin_errno = SLURM_AUTH_UNPACK;
+		return NULL;
+	}
+	if( version != plugin_version ) {
+		plugin_errno = SLURM_AUTH_MISMATCH;
+		return NULL;
+	}
+
+	/* Allocate a credential. */
+	cred = (slurm_auth_credential_t *)
+		xmalloc( sizeof( slurm_auth_credential_t ) );
+	cred->cr_errno = SLURM_SUCCESS;
+
+	if ( unpack32( &cred->cred.uid, buf ) != SLURM_SUCCESS ) {
+		plugin_errno = SLURM_AUTH_UNPACK;
+		goto unpack_error;
+	}
+	if ( unpack32( &cred->cred.gid, buf ) != SLURM_SUCCESS ) {
+		plugin_errno = SLURM_AUTH_UNPACK;
+		goto unpack_error;
+	}
+	if ( unpack_time( &cred->cred.valid_from, buf ) != SLURM_SUCCESS ) {
+		plugin_errno = SLURM_AUTH_UNPACK;
+		goto unpack_error;
+	}
+	if ( unpack_time( &cred->cred.valid_to, buf ) != SLURM_SUCCESS ) {
+		plugin_errno = SLURM_AUTH_UNPACK;
+		goto unpack_error;
+	}
+	if ( unpackmem_ptr( &data, &sig_size, buf ) != SLURM_SUCCESS ) {
+		plugin_errno = SLURM_AUTH_UNPACK;
+		goto unpack_error;
+	}
+	memcpy( cred->sig.data, data, sizeof( signature ) );
+
+	return cred;
+
+ unpack_error:
+	xfree( cred );
+	return NULL;
+}
+
+
+int
+slurm_auth_print( slurm_auth_credential_t *cred, FILE *fp )
+{
+	if ( cred == NULL ) {
+		plugin_errno = SLURM_AUTH_BADARG;
+		return SLURM_ERROR;
+	}
+
+	verbose( "BEGIN AUTHD CREDENTIAL\n" );
+	verbose( "   UID: %d", cred->cred.uid );
+	verbose( "   GID: %d", cred->cred.gid );
+	verbose( "   Valid from: %s", ctime( &cred->cred.valid_from ) );
+	verbose( "   Valid to: %s", ctime( &cred->cred.valid_to ) );
+	verbose( "   Signature: 0x%02x%02x%02x%02x ...\n",
+			 cred->sig.data[ 0 ], cred->sig.data[ 1 ],
+			 cred->sig.data[ 2 ], cred->sig.data[ 3 ] );
+	verbose( "END AUTHD CREDENTIAL\n" );
+
+	return SLURM_SUCCESS;	
+}
+
+
+int
+slurm_auth_errno( slurm_auth_credential_t *cred )
+{
+	if ( cred == NULL )
+		return plugin_errno;
+	else
+		return cred->cr_errno;
+}
+
+
+const char *
+slurm_auth_errstr( int slurm_errno )
+{
+	static struct {
+		int err;
+		char *msg;
+	} tbl[] = {
+		{ SLURM_AUTH_UNPACK, "cannot unpack authentication type" },
+		{ SLURM_AUTH_EXPIRED, "the credential has expired" },
+		{ 0, NULL }
+	};
+
+	int i;
+
+	for ( i = 0; ; ++i ) {
+		if ( tbl[ i ].msg == NULL ) return "unknown error";
+		if ( tbl[ i ].err == slurm_errno ) return tbl[ i ].msg;
+	}
+}
---
+++ tags/slurm-0-2-20-1/src/scancel/opt.c
@@ -0,0 +1,497 @@
+/*****************************************************************************\
+ *  opt.c - options processing for scancel
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Mark Grondona <grondona1@llnl.gov>, et. al.
+ *  UCRL-CODE-2002-040.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+
+#if HAVE_POPT_H
+#include <popt.h>
+#else
+#include <src/popt/popt.h>
+#endif
+
+#include <pwd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#ifdef HAVE_STRINGS_H
+#  include <strings.h>
+#endif
+
+#include <src/common/log.h>
+#include <src/common/slurm_protocol_defs.h>
+#include <src/common/xmalloc.h>
+#include <src/common/xstring.h>
+#include <src/scancel/scancel.h>
+
+#define __DEBUG 0
+
+#define SIZE(a) (sizeof(a)/sizeof(a[0]))
+
+/*---[ popt definitions ]------------------------------------------------*/
+
+/* generic OPT_ definitions -- mainly for use with env vars 
+ * (not to be confused with POPT_* definitions)
+ */
+#define OPT_NONE	0x00
+#define OPT_INT		0x01
+#define OPT_STRING	0x02
+
+/* specific options, used with popt (and env var processing) */
+#define OPT_INTERACTIVE	0x03
+#define OPT_NAME	0x04
+#define OPT_PARTITION	0x05
+#define OPT_STATE	0x06
+#define OPT_USER	0x07
+#define OPT_VERBOSE	0x08
+#define OPT_VERSION	0x09
+#define OPT_SIGNAL	0x0a
+
+
+#ifndef POPT_TABLEEND
+#  define POPT_TABLEEND { NULL, '\0', 0, 0, 0, NULL, NULL }
+#endif
+
+struct poptOption options[] = {
+	{"interactive", 'i', POPT_ARG_NONE, NULL, OPT_INTERACTIVE,
+	 "confirm each job cancelation", },
+	{"name", 'n', POPT_ARG_STRING, NULL, OPT_NAME,
+	 "name of job", "name"},
+	{"partition", 'p', POPT_ARG_STRING, NULL, OPT_PARTITION,
+	 "name of job's partition", "name"},
+	{"signal", 's', POPT_ARG_STRING, NULL, OPT_SIGNAL,
+	 "signal name or number", "name | integer"},
+	{"state", 't', POPT_ARG_STRING, NULL, OPT_STATE,
+	 "name of job's state", "PENDING | RUNNING"},
+	{"user", 'u', POPT_ARG_STRING, NULL, OPT_USER,
+	 "name of job's owner", "name"},
+        {"verbose", 'v', POPT_ARG_NONE, NULL, OPT_VERBOSE,
+	 "verbose operation (multiple -v's increase verbosity)", },
+	{"version", 'V', POPT_ARG_NONE, NULL, OPT_VERSION,
+	 "report the current version", },
+	POPT_AUTOHELP
+	POPT_TABLEEND
+};
+
+struct signv {
+	char *name;
+	uint16_t val;
+} sys_signame[ ] = {
+	{ "HUP",	SIGHUP  },
+	{ "INT",	SIGINT  },
+	{ "QUIT",	SIGQUIT },
+	{ "ABRT",	SIGABRT },
+	{ "KILL",	SIGKILL },
+	{ "ALRM",	SIGALRM },
+	{ "TERM",	SIGTERM },
+	{ "USR1",	SIGUSR1 },
+	{ "USR2",	SIGUSR2 },
+	{ "CONT",	SIGCONT },
+	{ "STOP",	SIGSTOP },
+	{ "TSTP",	SIGTSTP },
+	{ "TTIN",	SIGTTIN },
+	{ "TTOU",	SIGTTOU }
+};
+
+/*---[ end popt definitions ]---------------------------------------------*/
+
+/* forward declarations of static functions 
+ *
+ */
+
+/* 
+ * fill in default options 
+ */
+static void opt_default(void);
+
+/* set options based upon env vars 
+ */
+static void opt_env(void);
+
+/* set options based upon commandline args
+ */
+static void opt_args(int, char **);
+
+/* verify options sanity 
+ */
+static bool opt_verify(void);
+
+static void print_version (void);
+
+/* translate job state name to number
+ */
+static enum job_states xlate_state_name(const char *state_name);
+
+/* translate name name to number
+ */
+static uint16_t xlate_signal_name(const char *signal_name);
+
+/* list known options and their settings 
+ */
+#if	__DEBUG
+static void opt_list(void);
+#endif
+
+static void xlate_job_step_ids(const char **rest);
+
+/*---[ end forward declarations of static functions ]---------------------*/
+
+int initialize_and_process_args(int argc, char *argv[])
+{
+	/* initialize option defaults */
+	opt_default();
+
+	/* initialize options with env vars */
+	opt_env();
+
+	/* initialize options with argv */
+	opt_args(argc, argv);
+
+#if	__DEBUG
+	opt_list();
+#endif
+	return 1;
+
+}
+
+static enum job_states xlate_state_name(const char *state_name)
+{
+	enum job_states i;
+	char *state_names;
+
+	for (i=0; i<JOB_END; i++) {
+		if ((strcasecmp(state_name,job_state_string(i)) == 0) ||
+		    (strcasecmp(state_name,job_state_string_compact(i)) == 0)){
+			return i;
+		}
+	}
+	if ((strcasecmp(state_name, 
+			job_state_string(JOB_COMPLETING)) == 0) ||
+	    (strcasecmp(state_name, 
+			job_state_string_compact(JOB_COMPLETING)) == 0)) {
+		return JOB_COMPLETING;
+	}	
+
+	fprintf (stderr, "Invalid job state specified: %s", state_name);
+	state_names = xstrdup(job_state_string(0));
+	for (i=1; i<JOB_END; i++) {
+		xstrcat(state_names, ",");
+		xstrcat(state_names, job_state_string(i));
+	}
+	xstrcat(state_names, ",");
+	xstrcat(state_names, job_state_string(JOB_COMPLETING));
+	fprintf (stderr, "Valid job states include: %s\n", state_names);
+	xfree (state_names);
+	exit (1);
+}
+
+
+static uint16_t xlate_signal_name(const char *signal_name)
+{
+	uint16_t sig_num;
+	char *end_ptr, *sig_names;
+	int i;
+
+	sig_num = (uint16_t) strtol(signal_name, &end_ptr, 10);
+	if ((*end_ptr == '\0') || (sig_num != 0))
+		return sig_num;
+	
+	for (i=0; i<SIZE(sys_signame); i++) {
+		if (strcasecmp(sys_signame[i].name, signal_name) == 0) {
+			xfree(sig_names);
+			return sys_signame[i].val;
+		}
+		if (i == 0)
+			sig_names = xstrdup(sys_signame[i].name);
+		else {
+			xstrcat(sig_names, ",");
+			xstrcat(sig_names, sys_signame[i].name);
+		}			
+	}
+	fprintf (stderr, "Invalid job signal: %s\n", signal_name);
+	fprintf (stderr, "Valid signals include: %s\n", sig_names);
+	xfree(sig_names);
+	exit(1);
+}
+
+static void print_version (void)
+{
+	printf("%s %s\n", PACKAGE, SLURM_VERSION);
+}
+
+/*
+ * opt_default(): used by initialize_and_process_args to set defaults
+ */
+static void opt_default()
+{
+	opt.interactive	= false;
+	opt.job_cnt	= 0;
+	opt.job_name	= NULL;
+	opt.partition	= NULL;
+	opt.signal	= SIGKILL;
+	opt.state	= JOB_END;
+	opt.user_name	= NULL;
+	opt.user_id	= 0;
+	opt.verbose	= false;
+}
+
+/*
+ * opt_env(): used by initialize_and_process_args to set options via
+ *            environment variables. See comments above for how to
+ *            extend srun to process different vars
+ */
+static void opt_env()
+{
+	char *val;
+
+	if ( (val=getenv("SCANCEL_INTERACTIVE")) ) {
+		if (strcasecmp(val, "true") == 0)
+			opt.interactive = true;
+		else if (strcasecmp(val, "T") == 0)
+			opt.interactive = true;
+		else if (strcasecmp(val, "false") == 0)
+			opt.interactive = false;
+		else if (strcasecmp(val, "F") == 0)
+			opt.interactive = false;
+		else
+			error ("Unrecognized SCANCEL_INTERACTIVE value: %s",
+				val);
+	}
+
+	if ( (val=getenv("SCANCEL_NAME")) ) {
+		opt.job_name = xstrdup(val);
+	}
+
+	if ( (val=getenv("SCANCEL_PARTITION")) ) {
+		opt.partition = xstrdup(val);
+	}
+
+	if ( (val=getenv("SCANCEL_STATE")) ) {
+		opt.state = true;
+			error ("Unrecognized SCANCEL_STATE value: %s",
+				val);
+	}
+
+	if ( (val=getenv("SCANCEL_USER")) ) {
+		opt.user_name = xstrdup(val);
+	}
+
+	if ( (val=getenv("SCANCEL_VERBOSE")) ) {
+		if (strcasecmp(val, "true") == 0)
+			opt.verbose = true;
+		else if (strcasecmp(val, "T") == 0)
+			opt.verbose = true;
+		else if (strcasecmp(val, "false") == 0)
+			opt.verbose = false;
+		else if (strcasecmp(val, "F") == 0)
+			opt.verbose = false;
+		else
+			error ("Unrecognized SCANCEL_VERBOSE value: %s",
+				val);
+	}
+}
+
+/*
+ * opt_args() : set options via commandline args and popt
+ */
+static void opt_args(int ac, char **av)
+{
+	int rc;
+	const char **rest;
+	const char *arg;
+	poptContext optctx;
+
+	optctx = poptGetContext("scancel", ac, (const char **) av, options,
+				POPT_CONTEXT_POSIXMEHARDER);
+
+	poptSetOtherOptionHelp(optctx, "[OPTIONS] [job_id.step_id]");
+
+	poptReadDefaultConfig(optctx, 0);
+
+	/* first pass through args to see if attach or allocate mode
+	 * are set
+	 */
+	while ((rc = poptGetNextOpt(optctx)) > 0) {
+		arg = poptGetOptArg(optctx);
+
+		switch (rc) {
+		case OPT_INTERACTIVE:
+			opt.interactive = true;
+			break;
+
+		case OPT_NAME:
+			opt.job_name = xstrdup(arg);
+			break;
+
+		case OPT_PARTITION:
+			opt.partition = xstrdup(arg);
+			break;
+
+		case OPT_SIGNAL:
+			opt.signal = xlate_signal_name(arg);
+			break;
+
+		case OPT_STATE:
+			opt.state = xlate_state_name(arg);
+			break;
+
+		case OPT_USER:
+			opt.user_name = xstrdup(arg);
+			break;
+
+		case OPT_VERBOSE:
+			opt.verbose++;
+			break;
+
+		case OPT_VERSION:
+			print_version();
+			exit(0);
+			break;
+
+		default:
+			break;
+			/* do nothing */
+		}
+	}
+
+	if (rc < -1) {
+		const char *bad_opt;
+		bad_opt = poptBadOption(optctx, POPT_BADOPTION_NOALIAS);
+		error("bad argument %s: %s", bad_opt, poptStrerror(rc));
+		error("Try \"scancel --help\" for more information\n");
+		exit(1);
+	}
+
+	rest = poptGetArgs(optctx);
+	xlate_job_step_ids(rest);
+
+	if (!opt_verify()) {
+		poptPrintUsage(optctx, stderr, 0);
+		exit(1);
+	}
+
+	poptFreeContext(optctx);
+
+}
+
+static void
+xlate_job_step_ids(const char **rest)
+{
+	int i;
+	long tmp_l;
+	char *next_str;
+
+	opt.job_cnt = 0;
+
+	if (rest != NULL) {
+		while (rest[opt.job_cnt] != NULL)
+			opt.job_cnt++;
+	}
+
+	opt.job_id  = xmalloc(opt.job_cnt * sizeof(uint32_t));
+	opt.step_id = xmalloc(opt.job_cnt * sizeof(uint32_t));
+
+	for (i=0; i<opt.job_cnt; i++) {
+		tmp_l = strtol(rest[i], &next_str, 10);
+		if (tmp_l <= 0) {
+			error ("Invalid job_id %s", rest[i]);
+			exit (1);
+		}
+		opt.job_id[i] = tmp_l;
+
+		if (next_str[0] == '.') {
+			tmp_l = strtol(&next_str[1], &next_str, 10);
+			if (tmp_l < 0) {
+				error ("Invalid job id %s", rest[i]);
+				exit (1);
+			}
+			opt.step_id[i] = tmp_l;
+		} else 
+			opt.step_id[i] = NO_VAL;
+
+		if (next_str[0] != '\0') {
+			error ("Invalid job ID %s", rest[i]);
+			exit (1);
+		}
+	}
+}
+
+
+/* 
+ * opt_verify : perform some post option processing verification
+ *
+ */
+static bool
+opt_verify(void)
+{
+	bool verified = true;
+	struct passwd *passwd_ptr;
+
+	if (opt.user_name) {	/* translate to user_id */
+		passwd_ptr = getpwnam (opt.user_name);
+		if (passwd_ptr == NULL) {
+			error ("Invalid user name: %s", opt.user_name);
+			return false;
+		} else {
+			opt.user_id = passwd_ptr->pw_uid;
+		}
+	}
+
+	if ((opt.job_name == NULL) &&
+	    (opt.partition == NULL) &&
+	    (opt.state == JOB_END) &&
+	    (opt.user_name == NULL) &&
+	    (opt.job_cnt == 0))
+		verified = false;	/* no job specification */
+
+	return verified;
+}
+
+#if	__DEBUG
+
+#define tf_(b) (b == true) ? "true" : "false"
+
+static void 
+opt_list(void)
+{
+	int i;
+
+	info("interactive    : %s", tf_(opt.interactive));
+	info("job_name       : %s", opt.job_name);
+	info("partition      : %s", opt.partition);
+	info("signal         : %u", opt.signal);
+	info("state          : %s", job_state_string(opt.state));
+	info("user_id        : %u", opt.user_id);
+	info("user_name      : %s", opt.user_name);
+	info("verbose        : %d", opt.verbose);
+
+	for (i=0; i<opt.job_cnt; i++) {
+		info("job_steps      : %u.%u ", opt.job_id[i], opt.step_id[i]);
+	}
+}
+
+#endif				/* __DEBUG */
+
