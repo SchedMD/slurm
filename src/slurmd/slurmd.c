e43739348092c05fe6e2aee15cc61f32edcd5940
Mark Grondona <mgrondona@llnl.gov>
2003-10-09 22:35:25 +0000

    o ignore SIGCHLD in main slurmd -- reset to default handling in slurmd    manager process.

--- src/slurmd/slurmd.c
+++ src/slurmd/slurmd.c
@@ -174,6 +174,7 @@ main (int argc, char *argv[])
 	xsignal(SIGTERM, &_term_handler);
 	xsignal(SIGINT,  &_term_handler);
 	xsignal(SIGHUP,  &_hup_handler );
+	xsignal(SIGCHLD, SIG_IGN);
 
 	_msg_engine();
 
