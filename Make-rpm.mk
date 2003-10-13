1cb61d3baa6cb192d416a8fca3e0ece1689d3f57
Mark Grondona <mgrondona@llnl.gov>
2003-10-13 16:11:51 +0000

    o change rpm -ba to rpmbuild -ba .

--- Make-rpm.mk
+++ Make-rpm.mk
@@ -117,8 +117,8 @@ rpm-internal: tar-internal
 	    <$$spec >$$tmp/SPECS/$$proj.spec; \
 	if ! test -s $$tmp/SPECS/$$proj.spec; then \
 	  echo "ERROR: Cannot create $$proj.spec." 1>&2; exit 1; fi; \
-	rpm --showrc | egrep "_(gpg|pgp)_nam" >/dev/null && sign="--sign"; \
-	if ! rpm -ba --define "_tmppath $$tmp/TMP" --define "_topdir $$tmp" \
+	rpmbuild --showrc | egrep "_(gpg|pgp)_nam" >/dev/null && sign="--sign"; \
+	if ! rpmbuild -ba --define "_tmppath $$tmp/TMP" --define "_topdir $$tmp" \
           $$sign --quiet $$rpmargs $$tmp/SPECS/$$proj.spec \
                                                    >$$tmp/rpm.log 2>&1; then \
 	    cat $$tmp/rpm.log; exit 1; fi; \
