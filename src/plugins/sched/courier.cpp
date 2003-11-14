94f91c6297393d11934dd11edc0c0455c89e3d54
jwindley <jwindley@unknown>
2003-11-14 00:56:31 +0000

    Fix buffer underrun

--- src/plugins/sched/courier.cpp
+++ src/plugins/sched/courier.cpp
@@ -67,7 +67,7 @@ courier_t::write_bytes( char * const buf, const size_t size )
 	ptr = buf;
 	while ( bytes_remaining > 0 ) {
 		bytes_written = write( m_fd, ptr, size );
-		if ( bytes_written <= 0 ) return 0;
+		if ( bytes_written < 0 ) return 0;
 		bytes_remaining -= bytes_written;
 		ptr += bytes_written;
 	}
@@ -88,7 +88,7 @@ courier_t::read_bytes( char *buf, const size_t size )
 	ptr = buf;
 	while ( bytes_remaining > 0 ) {
 		bytes_read = read( m_fd, ptr, bytes_remaining );
-		if ( bytes_read <= 0 ) return 0;
+		if ( bytes_read < 0 ) return 0;
 		bytes_remaining -= bytes_read;
 		ptr += bytes_read;
 	}
