088b14cda4770f1140c0e1b477606d4421ce0a0b
Mark Grondona <mgrondona@llnl.gov>
2003-11-18 22:41:47 +0000

    o reorder some operations that depend on location of spooldir to    make slurmd startup more robust.  o clean up code in _msg_engine()

--- branches/slurm-0-2-branch/src/slurmd/slurmd.c
+++ branches/slurm-0-2-branch/src/slurmd/slurmd.c
@@ -31,6 +31,7 @@
 
 #include <fcntl.h>
 #include <string.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <sys/stat.h>
 #include <sys/time.h>
@@ -140,6 +141,10 @@ main (int argc, char *argv[])
 
 	log_init(argv[0], conf->log_opts, LOG_DAEMON, conf->logfile);
 
+	xsignal(SIGTERM, &_term_handler);
+	xsignal(SIGINT,  &_term_handler);
+	xsignal(SIGHUP,  &_hup_handler );
+
 	/* 
 	 * Run slurmd_init() here in order to report early errors
 	 * (with shared memory and public keyfile)
@@ -176,9 +181,6 @@ main (int argc, char *argv[])
         if (send_registration_msg(SLURM_SUCCESS) < 0) 
 		error("Unable to register with slurm controller");
 
-	xsignal(SIGTERM, &_term_handler);
-	xsignal(SIGINT,  &_term_handler);
-	xsignal(SIGHUP,  &_hup_handler );
 
 	_install_fork_handlers();
 	list_install_fork_handlers();
@@ -203,34 +205,31 @@ main (int argc, char *argv[])
 	return 0;
 }
 
+
 static void
 _msg_engine()
 {
 	slurm_fd sock;
 	slurm_addr cli;
 
-	while (1) {
-		if (_shutdown)
-			break;
-  again:
-		if ((sock = slurm_accept_msg_conn(conf->lfd, &cli)) < 0) {
-			if (errno == EINTR) {
-				if (_shutdown) {
-					verbose("got shutdown request");
-					break;
-				}
-				if (_reconfig) {
-					_reconfigure();
-					verbose("got reconfigure request");
-				}
-				goto again;
-			}
-			error("accept: %m");
+	while (!_shutdown) {
+		if ((sock = slurm_accept_msg_conn(conf->lfd, &cli)) >= 0) {
+			_handle_connection(sock, &cli);
 			continue;
 		}
-		if (sock > 0)
-			_handle_connection(sock, &cli);
+		/*
+		 *  Otherwise, accept() failed.
+		 */
+		if (errno == EINTR) {
+			if (_reconfig) {
+				verbose("got reconfigure request");
+				_reconfigure();
+			}
+			continue;
+		} 
+		error("accept: %m");
 	}
+	verbose("got shutdown request");
 	slurm_shutdown_msg_engine(conf->lfd);
 	return;
 }
@@ -642,12 +641,6 @@ _slurmd_init()
 		return SLURM_FAILURE;
 
 	/* 
-	 * Restore any saved revoked credential information
-	 */
-	if (_restore_cred_state(conf->vctx))
-		return SLURM_FAILURE;
-
-	/* 
 	 * Create slurmd spool directory if necessary.
 	 */
 	if (_set_slurmd_spooldir() < 0) {
@@ -655,6 +648,12 @@ _slurmd_init()
 		return SLURM_FAILURE;
 	}
 
+
+	/* 
+	 * Restore any saved revoked credential information
+	 */
+	if (_restore_cred_state(conf->vctx))
+		return SLURM_FAILURE;
 	/*
 	 * Cleanup shared memory if so configured
 	 */
@@ -731,7 +730,7 @@ static int
 _slurmd_fini()
 {
 	save_cred_state(conf->vctx);
-	shm_fini();
+	shm_fini(); 
 	return SLURM_SUCCESS;
 }
 
