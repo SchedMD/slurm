e66f0c9e7448d532a5291cd3a74183a6f13eeb87
Moe Jette <jette1@llnl.gov>
2003-11-18 18:34:48 +0000

    Don't update the job database time stamp on job purge. It was triggering state saves, backfill scheduler runs, etc. when there was not a significant state change that should trigger those events.

--- trunk/src/slurmctld/job_mgr.c
+++ trunk/src/slurmctld/job_mgr.c
@@ -2435,7 +2435,7 @@ void purge_old_job(void)
 	i = list_delete_all(job_list, &_list_find_job_old, "");
 	if (i) {
 		debug2("purge_old_job: purged %d old job records", i);
-		last_job_update = time(NULL);
+/*		last_job_update = time(NULL);	don't worry about state save */
 	}
 }
 
