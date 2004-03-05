161b954591bc341cc64c46f5aa5025afe06a4b25
no author <no@author>
2004-03-05 01:26:18 +0000

    This commit was manufactured by cvs2svn to create tag 'slurm-0-3-0-0-pre7'.

---
+++ tags/slurm-0-3-0-0-pre7/src/common/xassert.c
@@ -0,0 +1,22 @@
+/* $Id$ */
+
+/* 
+** xassert: replacement for assert which sends error to log instead of stderr
+**
+*/
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "src/common/log.h"
+
+void __xassert_failed(char *expr, const char *file, int line, char *func)
+{
+	error("%s:%d: %s(): Assertion (%s) failed.\n", file, line, func, expr);
+	log_flush();
+	abort();
+}
