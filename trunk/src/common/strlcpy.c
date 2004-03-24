489f32bac8b71552e4ceb293eea4b4bf84f5f17b
Moe Jette <jette1@llnl.gov>
2004-03-24 01:01:56 +0000

    A long line somehow was split into two lines. Put it back together.

--- trunk/src/common/strlcpy.c
+++ trunk/src/common/strlcpy.c
@@ -28,8 +28,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: strlcpy.c,v 1.5 2001/05/13 15:40:16 deraadt Exp
-$";
+static char *rcsid = "$OpenBSD: strlcpy.c,v 1.5 2001/05/13 15:40:16 deraadt Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
