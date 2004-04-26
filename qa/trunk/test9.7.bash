b106a09e5b7c43ceb00352de9f54ffd92b4b19bd
Moe Jette <jette1@llnl.gov>
2004-04-26 22:03:05 +0000

    Added disclaimer and copyright notices to tests. No changes in logic.

--- qa/trunk/test9.7.bash
+++ qa/trunk/test9.7.bash
@@ -1,7 +1,31 @@
 #!/bin/bash
+############################################################################
 # Simple SLURM stress test
 # Usage: <prog> <exec1> <exec2> <exec3> <sleep_time> <iterations>
 # Default is sinfo, srun, squeue, 1 second sleep and 3 iterations
+############################################################################
+# Copyright (C) 2002 The Regents of the University of California.
+# Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+# Written by Moe Jette <jette1@llnl.gov>
+# UCRL-CODE-2002-040.
+# 
+# This file is part of SLURM, a resource management program.
+# For details, see <http://www.llnl.gov/linux/slurm/>.
+#  
+# SLURM is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+# 
+# SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with SLURM; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+############################################################################
 if [ $# -gt 0 ]; then
 	exec1=$1
 else
