600a916e01c97190a20d9916db894085cab4b1e9
Moe Jette <jette1@llnl.gov>
2004-08-24 23:12:11 +0000

    Treat "Shared" parameter value other than NO as error on Blue Gene systems.

--- src/slurmctld/read_config.c
+++ src/slurmctld/read_config.c
@@ -596,6 +596,13 @@ static int _parse_part_spec(char *in_line)
 			goto cleanup;
 		}
 		xfree(shared_str);
+#ifdef HAVE_BGL
+		if (shared_val != SHARED_NO) {
+			error("Illegal Shared parameter value for partition %s",
+				partition_name);
+			shared_val = SHARED_NO;
+		} 
+#endif
 	}
 
 	if (state_str) {
