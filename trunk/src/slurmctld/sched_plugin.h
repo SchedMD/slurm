ae9be52894d858d2f48b43ef6b0703c1f2b3b6e7
Moe Jette <jette1@llnl.gov>
2004-08-25 00:14:08 +0000

    Add copyright notices.

--- trunk/src/slurmctld/sched_plugin.h
+++ trunk/src/slurmctld/sched_plugin.h
@@ -1,3 +1,29 @@
+/*****************************************************************************\
+ *  sched_plugin.h - Define scheduler plugin functions.
+ *****************************************************************************
+ *  Copyright (C) 2004 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Jay Windley <jwindley@lnxi.gov>
+ *  UCRL-CODE-2002-040.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+
 #ifndef __SLURM_CONTROLLER_SCHED_PLUGIN_API_H__
 #define __SLURM_CONTROLLER_SCHED_PLUGIN_API_H__
 
