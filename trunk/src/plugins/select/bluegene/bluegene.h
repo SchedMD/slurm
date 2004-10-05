576d97a41a8d67e11023743602d9a65844e41add
Moe Jette <jette1@llnl.gov>
2004-10-05 19:14:17 +0000

    Perform some clean-up of comments and code. No changes in logic.

--- trunk/src/plugins/select/bluegene/bluegene.h
+++ trunk/src/plugins/select/bluegene/bluegene.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- *  bluegene.h - header for bgl node allocation plugin. 
+ *  bluegene.h - header for blue gene node allocation plugin. 
  *****************************************************************************
  *  Copyright (C) 2004 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
@@ -53,7 +53,7 @@ typedef struct bgl_record {
 	lifecycle_type_t part_lifecycle;/* either STATIC or DYNAMIC	*/
 	hostlist_t hostlist;		/* expanded form of hosts */
 	bitstr_t *bitmap;		/* bitmap of nodes for this partition */
-	struct partition* alloc_part;       /* the allocated partition   */
+	struct partition* alloc_part;	/* the allocated partition   */
 	int size;			/* node count for the partitions */
 	rm_partition_t part_type;	/* Mesh or Torus or NAV		*/
 	rm_partition_mode_t node_use;	/* either COPROCESSOR or VIRTUAL */
@@ -91,15 +91,15 @@ extern void fini_bgl(void);
  */
 extern int create_static_partitions(List part_list);
 
-/** */
+/* Try to find resources for a given job request */
 extern int submit_job(struct job_record *job_ptr, bitstr_t *bitmap,
 	       int min_nodes, int max_nodes);
-/** */
+
+/* sort a list of bgl_records by size (node count) */
 extern void sort_bgl_record_inc_size(List records);
-/** */
 extern void sort_bgl_record_dec_size(List records);
 
-/** */
+/* Log a bgl_record's contents */
 extern void print_bgl_record(bgl_record_t* record);
 
 /* Return strings representing blue gene data types */
@@ -109,6 +109,5 @@ extern char* convert_node_use(rm_partition_mode_t pt);
 
 /* bluegene_agent - detached thread periodically updates status of bluegene nodes */
 extern void *bluegene_agent(void *args);
-
 
 #endif /* _BLUEGENE_H_ */
