e3d71eec1fb3c8e05c7414f8684a2437a6a048fd
Moe Jette <jette1@llnl.gov>
2005-06-13 21:52:46 +0000

    Fix bug that was resetting BGL job geometry on unrelated field update.

--- src/api/init_msg.c
+++ src/api/init_msg.c
@@ -80,7 +80,7 @@ void slurm_init_job_desc_msg(job_desc_msg_t * job_desc_msg)
 	job_desc_msg->work_dir    = NULL;
 	job_desc_msg->host        = NULL;
 	job_desc_msg->port        = 0;
-#if SYSTEM_DIMENSION
+#if SYSTEM_DIMENSIONS
 {
 	int i;
 	for (i=0; i<SYSTEM_DIMENSIONS; i++)
