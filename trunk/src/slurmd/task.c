7dae09f6c4c8eb66dadba3d65d875dd9e204bae9
Moe Jette <jette1@llnl.gov>
2005-08-26 21:50:49 +0000

    Fix mpich-gm bugs, thanks to Takao Hatazaki (HP).

--- trunk/src/slurmd/task.c
+++ trunk/src/slurmd/task.c
@@ -170,7 +170,7 @@ exec_task(slurmd_job_t *job, int i, int waitfd)
 	
 	t = job->task[i];
 	job->envtp->procid = t->gtid;
-	job->envtp->gmpi = t->gtid;
+	job->envtp->gmpi = getenvp(job->env, "SLURM_GMPI") ? t->gtid : -1;
 	
 	
 	setup_env(job->envtp);
