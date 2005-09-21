a34b5aba124a803a8cb775869733461483f60446
Moe Jette <jette1@llnl.gov>
2005-09-21 23:16:48 +0000

    switch from localtime() to localtime_r() for better thread safety.

--- branches/slurm-0-6-branch/src/common/xstring.c
+++ branches/slurm-0-6-branch/src/common/xstring.c
@@ -149,8 +149,7 @@ void _xstrftimecat(char **buf, const char *fmt)
 {
 	char p[256];		/* output truncated to 256 chars */
 	time_t t;
-	struct tm *tm_ptr = NULL;
-	static pthread_mutex_t localtime_lock = PTHREAD_MUTEX_INITIALIZER;
+	struct tm tm;
 
 	const char default_fmt[] = "%m/%d/%Y %H:%M:%S %Z";
 
@@ -160,13 +159,11 @@ void _xstrftimecat(char **buf, const char *fmt)
 	if (time(&t) == (time_t) -1) 
 		fprintf(stderr, "time() failed\n");
 
-	pthread_mutex_lock(&localtime_lock);
-	if (!(tm_ptr = localtime(&t)))
-		fprintf(stderr, "localtime() failed\n");
+	if (!localtime_r(&t, &tm))
+		fprintf(stderr, "localtime_r() failed\n");
 
-	strftime(p, sizeof(p), fmt, tm_ptr);
+	strftime(p, sizeof(p), fmt, &tm);
 
-	pthread_mutex_unlock(&localtime_lock);
 
 	_xstrcat(buf, p);
 }
