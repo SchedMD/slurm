c09981553ba4d664ed4e0b27882ef8c1c1f54f64
Moe Jette <jette1@llnl.gov>
2005-09-21 17:04:01 +0000

    Add $Id$ to header.

--- branches/slurm-0-6-branch/src/plugins/select/bluegene/bgl_switch_connections.c
+++ branches/slurm-0-6-branch/src/plugins/select/bluegene/bgl_switch_connections.c
@@ -1,6 +1,8 @@
 /*****************************************************************************\
  *  bgl_switch_connections.c - Blue Gene switch management functions, 
  *  establish switch connections
+ *
+ *  $Id$
  *****************************************************************************
  *  Copyright (C) 2004 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
