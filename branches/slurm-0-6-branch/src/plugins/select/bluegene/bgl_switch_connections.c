7b57feaa6f217bd3586add9a61d4b4c55bab7245
Danny Auble <da@llnl.gov>
2005-09-22 16:24:52 +0000

    found extra free, checking in fix

--- branches/slurm-0-6-branch/src/plugins/select/bluegene/bgl_switch_connections.c
+++ branches/slurm-0-6-branch/src/plugins/select/bluegene/bgl_switch_connections.c
@@ -501,18 +501,19 @@ extern int configure_partition_switches(bgl_record_t * bgl_record)
 			}
 
 			if (!strcasecmp((char *)bpid, (char *)curr_bpid)) {
-				free(curr_bpid);
 				coord_switch[found_bpid] = curr_switch;
 				found_bpid++;
-				if(found_bpid==PA_SYSTEM_DIMENSIONS)
+				if(found_bpid==PA_SYSTEM_DIMENSIONS) {
+					free(curr_bpid);
 					break;
+				}
 			}
 			
 			//BGLBridgeAPI doesn't strdup SwitchBPID
-			//free(curr_bpid);
+			free(curr_bpid);
 		}
 
-		//free(bpid);
+		free(bpid);
 
 		if(found_bpid==PA_SYSTEM_DIMENSIONS) {
 						
