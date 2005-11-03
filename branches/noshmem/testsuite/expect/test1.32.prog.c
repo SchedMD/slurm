7dea634cbec1038c85966bf4a6b543e147c29d0e
Christopher J. Morrone <morrone2@llnl.gov>
2005-11-03 22:06:48 +0000

    Quiet compile warning

--- branches/noshmem/testsuite/expect/test1.32.prog.c
+++ branches/noshmem/testsuite/expect/test1.32.prog.c
@@ -26,6 +26,7 @@
 \*****************************************************************************/
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
