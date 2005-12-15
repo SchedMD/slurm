9fba4560e5251d74d1e0ad7679058f5507f6f029
Moe Jette <jette1@llnl.gov>
2005-12-15 17:47:04 +0000

    Add missing interator initialization.

--- src/slurmctld/step_mgr.c
+++ src/slurmctld/step_mgr.c
@@ -446,6 +446,7 @@ try_again:
 		nodes_idle = bit_alloc (bit_size (nodes_avail) );
 		if ((nodes_picked == NULL) || (nodes_idle == NULL))
 			fatal("bit_alloc malloc failure");
+		step_iterator = list_iterator_create (job_ptr->step_list);
 		while ((step_p = (struct step_record *) list_next (step_iterator)))
 			bit_or (nodes_idle, step_p->step_node_bitmap); 
 		list_iterator_destroy (step_iterator);
