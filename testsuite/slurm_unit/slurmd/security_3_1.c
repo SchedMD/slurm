7a9c025473c633d183fd06ee344c1a936a27460a
Moe Jette <jette1@llnl.gov>
2005-12-20 19:41:43 +0000

    Add copyright notices, no code changes.

--- testsuite/slurm_unit/slurmd/security_3_1.c
+++ testsuite/slurm_unit/slurmd/security_3_1.c
@@ -1,3 +1,31 @@
+/*****************************************************************************\
+ *  security_3_1.c - Spawn a bogus batch job launch request (not from 
+ *  privileged user). Make sure that slurmd rejects the request and logs it.
+ *  Much of this code is lifted from slurmctld/job_scheduler.c:_launch_job
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Morris Jette <jette1@llnl.gov> et.al.
+ *  UCRL-CODE-2002-040.
+ *
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -15,10 +43,6 @@ static int  _report_results(slurm_msg_t *response_msg_ptr);
 static int  _send_launch_msg(batch_job_launch_msg_t *launch_msg_ptr);
 static void _usage(char *prog_name);
 
-/* Spawn a bogus batch job launch request (not from privileged user).
- * Make sure that slurmd rejects the request and logs it. 
- * Much of this code is lifted from slurmctld/job_scheduler.c:_launch_job
- */
 int main(int argc, char *argv[])
 {
 	batch_job_launch_msg_t launch_msg;
