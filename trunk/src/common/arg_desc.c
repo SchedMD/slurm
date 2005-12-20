3b3b55f855b2500a88cf5440126688bec332e2ca
Moe Jette <jette1@llnl.gov>
2005-12-20 18:42:54 +0000

    Add missing copyright notices, UCRL-CODE numbers.

--- trunk/src/common/arg_desc.c
+++ trunk/src/common/arg_desc.c
@@ -1,3 +1,27 @@
+/****************************************************************************\
+ *  arg_desc.c
+ *****************************************************************************
+ *  Copyright (C) 2005 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  UCRL-CODE-2002-040.
+ *
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\****************************************************************************/
 #include <string.h>
 #include "src/common/arg_desc.h"
 #include "src/common/macros.h"
