fedfcf7ba0da9c8628f9fa970cc2a1bb248e24af
Danny Auble <da@llnl.gov>
2006-02-11 00:03:07 +0000

    more of the same

--- src/plugins/select/bluegene/block_allocator/block_allocator.h
+++ src/plugins/select/bluegene/block_allocator/block_allocator.h
@@ -219,6 +219,7 @@ extern List bp_map_list;
 extern char letters[62];
 extern char colors[6];
 extern int DIM_SIZE[BA_SYSTEM_DIMENSIONS];
+extern pthread_mutex_t api_file_mutex;
 
 /* destroy a bg_info_record_t */
 extern void destroy_bg_info_record(void* object);
