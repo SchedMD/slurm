29b46d8687671139a715ab88b8dcfbccdd5a35d3
Moe Jette <jette1@llnl.gov>
2006-03-01 17:46:41 +0000

    Added basic "sbcast" support, still needs message fanout logic.

--- src/slurmd/slurmd/req.c
+++ src/slurmd/slurmd/req.c
@@ -41,6 +41,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <utime.h>
 
 #include "src/common/hostlist.h"
 #include "src/common/log.h"
@@ -91,6 +92,7 @@ static void _rpc_update_time(slurm_msg_t *, slurm_addr *);
 static void _rpc_shutdown(slurm_msg_t *msg, slurm_addr *cli_addr);
 static void _rpc_reconfig(slurm_msg_t *msg, slurm_addr *cli_addr);
 static void _rpc_pid2jid(slurm_msg_t *msg, slurm_addr *);
+static int  _rpc_file_bcast(slurm_msg_t *msg, slurm_addr *);
 static int  _rpc_ping(slurm_msg_t *, slurm_addr *);
 static int  _run_prolog(uint32_t jobid, uid_t uid, char *bg_part_id);
 static int  _run_epilog(uint32_t jobid, uid_t uid, char *bg_part_id);
@@ -212,6 +214,11 @@ slurmd_req(slurm_msg_t *msg, slurm_addr *cli)
 					getpid(), rc);
 			slurm_free_jobacct_msg(msg->data);
 		}
+		break;
+	case REQUEST_FILE_BCAST:
+		rc = _rpc_file_bcast(msg, cli);
+		slurm_send_rc_msg(msg, rc);
+		slurm_free_file_bcast_msg(msg->data);
 		break;
 	default:
 		error("slurmd_req: invalid request msg type %d\n",
@@ -1083,6 +1090,99 @@ static void  _rpc_pid2jid(slurm_msg_t *msg, slurm_addr *cli)
 	}
 }
 
+static int
+_rpc_file_bcast(slurm_msg_t *msg, slurm_addr *cli)
+{
+	file_bcast_msg_t *req = msg->data;
+	int fd, flags, offset, inx, rc;
+	uid_t req_uid = g_slurm_auth_get_uid(msg->cred);
+	uid_t req_gid = g_slurm_auth_get_gid(msg->cred);
+	pid_t child;
+
+#if 0
+	info("last_block=%u force=%u modes=%o",
+		req->last_block, req->force, req->modes);
+	info("uid=%u gid=%u atime=%lu mtime=%lu block_len=%u",
+		req->uid, req->gid, req->atime, req->mtime, req->block_len);
+	/* when the file being transferred is binary, the following line
+	 * can break the terminal output for slurmd */
+	/* info("req->data=%s, @ %lu", req->data, (unsigned long) &req->data); */
+#endif
+
+	info("sbcast req_uid=%u fname=%s block_no=%u", 
+		req_uid, req->fname, req->block_no);
+	child = fork();
+	if (child == -1) {
+		error("sbcast: fork failure");
+		return errno;
+	} else if (child > 0) {
+		waitpid(child, &rc, 0);
+		return WEXITSTATUS(rc);
+	}
+
+	/* The child actually performs the I/O and exits with 
+	 * a return code, do not return! */
+	if (setgid(req_gid) < 0) {
+		error("sbcast: uid:%u setgid(%u): %s", req_uid, req_gid, 
+			strerror(errno));
+		exit(errno);
+	}
+	if (setuid(req_uid) < 0) {
+		error("sbcast: getuid(%u): %s", req_uid, strerror(errno));
+		exit(errno);
+	}
+
+	flags = O_WRONLY;
+	if (req->block_no == 1) {
+		flags |= O_CREAT;
+		if (req->force)
+			flags |= O_TRUNC;
+		else
+			flags |= O_EXCL;
+	} else
+		flags |= O_APPEND;
+
+	fd = open(req->fname, flags, 0700);
+	if (fd == -1) {
+		error("sbcast: uid:%u can't open `%s`: %s",
+			req_uid, req->fname, strerror(errno));
+		exit(errno);
+	}
+
+	offset = 0;
+	while (req->block_len - offset) {
+		inx = write(fd, &req->data[offset], (req->block_len - offset));
+		if (inx == -1) {
+			if ((errno == EINTR) || (errno == EAGAIN))
+				continue;
+			error("sbcast: uid:%u can't write `%s`: %s",
+				req_uid, req->fname, strerror(errno));
+			close(fd);
+			exit(errno);
+		}
+		offset += inx;
+	}
+	if (req->last_block && fchmod(fd, (req->modes & 0777))) {
+		error("sbcast: uid:%u can't chmod `%s`: %s",
+			req_uid, req->fname, strerror(errno));
+	}
+	if (req->last_block && fchown(fd, req->uid, req->gid)) {
+		error("sbcast: uid:%u can't chown `%s`: %s",
+			req_uid, req->fname, strerror(errno));
+	}
+	close(fd);
+	fd = 0;
+	if (req->last_block && req->atime) {
+		struct utimbuf time_buf;
+		time_buf.actime  = req->atime;
+		time_buf.modtime = req->mtime;
+		if (utime(req->fname, &time_buf)) {
+			error("sbcast: uid:%u can't utime `%s`: %s",
+				req_uid, req->fname, strerror(errno));
+		}
+	}
+	exit(SLURM_SUCCESS);
+}
 
 static void 
 _rpc_reattach_tasks(slurm_msg_t *msg, slurm_addr *cli)
