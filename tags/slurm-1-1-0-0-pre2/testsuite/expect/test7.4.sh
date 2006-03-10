f5c4b272f722da5cb09f7e90eeb0bb508cadfd20
Moe Jette <jette1@llnl.gov>
2006-03-10 20:04:30 +0000

    Has a bad slurm.spec. replace it.

--- tags/slurm-1-1-0-0-pre2/testsuite/expect/test7.4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Usage: <squeue path> <node list>
-# Returns SLURM job id allocated  to that node for that user
-#
-node=`head -n 1 $2`
-user=`id -un`
-jobid=`$1 -h -o %i -u $user -n $node`
-echo $jobid
