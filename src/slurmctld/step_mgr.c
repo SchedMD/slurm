a7a42cf001091f5a7a5c57ca8367b4ca4a293855
Moe Jette <jette1@llnl.gov>
2006-04-06 19:39:35 +0000

    Report the invalid task count that prevents a step from running.

--- src/slurmctld/step_mgr.c
+++ src/slurmctld/step_mgr.c
@@ -673,8 +673,10 @@ step_create(job_step_create_request_msg_t *step_specs,
 	}
 	
 	if ((step_specs->num_tasks < 1)
-	|| (step_specs->num_tasks > (node_count*MAX_TASKS_PER_NODE)))
+	||  (step_specs->num_tasks > (node_count*MAX_TASKS_PER_NODE))) {
+		info("step has invalid task count: %u", step_specs->num_tasks);
 		return ESLURM_BAD_TASK_COUNT;
+	}
 
 	step_ptr = create_step_record (job_ptr);
 	if (step_ptr == NULL)
