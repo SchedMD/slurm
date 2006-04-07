8bab8bf2c9da21af4310f6dfc5c7b556a33f4b7f
Christopher J. Morrone <morrone2@llnl.gov>
2006-04-07 02:20:54 +0000

    Patch from Mark Grondona
    "[PATCH] srun reports wrong hostname at task exit"
    
    This patch adds a hostid[] array to the step_layout structure (it
    seems that is where it should be), and a couple calls to translate
    taskid to hostid or hostname. The job->hostid[] array is then removed
    and its users moved to the new method (there were just a few)

--- branches/slurm-1.0/src/srun/reattach.c
+++ branches/slurm-1.0/src/srun/reattach.c
@@ -439,7 +439,6 @@ int reattach()
 	job->jobid  = s->jobid;
 	job->stepid = s->stepid;
 	job->step_layout->tids   = xmalloc(job->nhosts * sizeof(uint32_t *));
-	job->hostid = xmalloc(s->ntasks   * sizeof(uint32_t *));
 
 	if (job->stepid == NO_VAL) {
 		char *new_argv0 = NULL;
