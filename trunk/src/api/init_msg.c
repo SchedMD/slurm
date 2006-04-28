d40b239f9c508003f78b4f7bdabbf58e78949789
Moe Jette <jette1@llnl.gov>
2006-04-28 16:06:04 +0000

    svn merge -r7893:7901 https://eris.llnl.gov/svn/slurm/branches/slurm-1.0

--- trunk/src/api/init_msg.c
+++ trunk/src/api/init_msg.c
@@ -79,6 +79,7 @@ void slurm_init_job_desc_msg(job_desc_msg_t * job_desc_msg)
 	job_desc_msg->user_id     = NO_VAL;
 	job_desc_msg->work_dir    = NULL;
 	job_desc_msg->host        = NULL;
+	job_desc_msg->mail_type   = 0;
 	job_desc_msg->mail_user   = NULL;
 	job_desc_msg->port        = 0;
 	job_desc_msg->begin_time  = 0;
