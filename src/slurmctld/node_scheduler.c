6c08077b491f18b044523c0fe22151faa0ba16fc
Danny Auble <da@llnl.gov>
2006-05-18 19:30:26 +0000

    typo fix from max_node to max_nodes

--- src/slurmctld/node_scheduler.c
+++ src/slurmctld/node_scheduler.c
@@ -888,9 +888,9 @@ extern int select_nodes(struct job_record *job_ptr, bool test_only)
 	else
 		shared = job_ptr->details->shared;
 
-	if (max_node < min_nodes)
+	if (max_nodes < min_nodes) {
 		error_code = ESLURM_REQUESTED_PART_CONFIG_UNAVAILABLE;
-	else {
+	} else {
 		error_code = _pick_best_nodes(node_set_ptr, node_set_size,
 				      &select_bitmap, job_ptr,
 				      min_nodes, max_nodes, req_nodes,
