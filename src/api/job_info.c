57c01853de52292351761400e96b5e3e6b68d4b2
Moe Jette <jette1@llnl.gov>
2006-08-10 21:36:34 +0000

    Major clean-up of da's logic to write data to string, convert to use xstrcat() function rather than equivalent in-line code.

--- src/api/job_info.c
+++ src/api/job_info.c
@@ -104,13 +104,10 @@ slurm_sprint_job_info ( job_info_t * job_ptr, int one_liner )
 	char time_str[32], select_buf[128];
 	struct group *group_info = NULL;
 	char tmp1[7], tmp3[7];
-	char tmp2[100];
+	char tmp_line[128];
 	uint16_t quarter = (uint16_t) NO_VAL;
 	uint16_t nodecard = (uint16_t) NO_VAL;
-	char *out = xmalloc(HUGE_BUF);
-	int len = 0;
-	int lentmp2 = 0;
-	int tmplen = 0;
+	char *out = NULL;
 	
 #ifdef HAVE_BG
 	char *nodelist = "BP_List";
@@ -125,536 +122,226 @@ slurm_sprint_job_info ( job_info_t * job_ptr, int one_liner )
 #endif	
 
 	/****** Line 1 ******/
-	sprintf ( tmp2, "JobId=%u ", job_ptr->job_id);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out, "%s", tmp2);
-	len += lentmp2;
-
-	sprintf ( tmp2, "UserId=%s(%u) ", 
+	snprintf(tmp_line, sizeof(tmp_line), "JobId=%u ", job_ptr->job_id);
+	out = xstrdup(tmp_line);
+	snprintf(tmp_line, sizeof(tmp_line), "UserId=%s(%u) ", 
 		uid_to_string((uid_t) job_ptr->user_id), job_ptr->user_id);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
-	group_info = getgrgid( (gid_t) job_ptr->group_id );
-	if ( group_info && group_info->gr_name[ 0 ] )
-		sprintf ( tmp2, "GroupId=%s(%u)",
+	xstrcat(out, tmp_line);
+	group_info = getgrgid((gid_t) job_ptr->group_id );
+	if ( group_info && group_info->gr_name[ 0 ] ) {
+		snprintf(tmp_line, sizeof(tmp_line), "GroupId=%s(%u)",
 			 group_info->gr_name, job_ptr->group_id );
-	else
-		sprintf ( tmp2, "GroupId=(%u)", job_ptr->group_id );
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
+	} else {
+		snprintf(tmp_line, sizeof(tmp_line), "GroupId=(%u)", 
+			job_ptr->group_id );
 	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	xstrcat(out, tmp_line);
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
+		xstrcat(out, "\n   ");
 
 	/****** Line 2 ******/
-	sprintf ( tmp2, "Name=%s", job_ptr->name);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	snprintf(tmp_line, sizeof(tmp_line), "Name=%s", job_ptr->name);
+	xstrcat(out, tmp_line);
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
+		xstrcat(out, "\n   ");
 
 	/****** Line 3 ******/
-	sprintf ( tmp2, "Priority=%u Partition=%s BatchFlag=%u", 
-		  job_ptr->priority, job_ptr->partition, 
-		  job_ptr->batch_flag);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	snprintf(tmp_line, sizeof(tmp_line), 
+		"Priority=%u Partition=%s BatchFlag=%u", 
+		job_ptr->priority, job_ptr->partition, 
+		job_ptr->batch_flag);
+	xstrcat(out, tmp_line);
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
+		xstrcat(out, "\n   ");
 
 	/****** Line 4 ******/
-	sprintf ( tmp2, "AllocNode:Sid=%s:%u TimeLimit=", 
-		  job_ptr->alloc_node, job_ptr->alloc_sid);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	snprintf(tmp_line, sizeof(tmp_line),
+		"AllocNode:Sid=%s:%u TimeLimit=", 
+		job_ptr->alloc_node, job_ptr->alloc_sid);
+	xstrcat(out, tmp_line);
 	if (job_ptr->time_limit == INFINITE)
-		sprintf ( tmp2, "UNLIMITED");
+		sprintf(tmp_line, "UNLIMITED");
 	else if (job_ptr->time_limit == NO_VAL)
-		sprintf ( tmp2, "Partition_Limit");
+		sprintf(tmp_line, "Partition_Limit");
 	else
-		sprintf ( tmp2, "%u", job_ptr->time_limit);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+		sprintf(tmp_line, "%u", job_ptr->time_limit);
+	xstrcat(out, tmp_line);
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+		xstrcat(out, "\n   ");
 
 	/****** Line 5 ******/
-	slurm_make_time_str ((time_t *)&job_ptr->start_time, time_str,
+	slurm_make_time_str((time_t *)&job_ptr->start_time, time_str,
 		sizeof(time_str));
-	sprintf ( tmp2, "JobState=%s StartTime=%s EndTime=",
-		  job_state_string(job_ptr->job_state), time_str);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	snprintf(tmp_line, sizeof(tmp_line),
+		"JobState=%s StartTime=%s EndTime=",
+		job_state_string(job_ptr->job_state), time_str);
+	xstrcat(out, tmp_line);
 	if ((job_ptr->time_limit == INFINITE) && 
 	    (job_ptr->end_time > time(NULL)))
-		sprintf ( tmp2, "NONE");
+		sprintf(tmp_line, "NONE");
 	else {
 		slurm_make_time_str ((time_t *)&job_ptr->end_time, time_str,
 			sizeof(time_str));
-		sprintf ( tmp2, "%s", time_str);
+		sprintf(tmp_line, "%s", time_str);
 	}
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	xstrcat(out, tmp_line);
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
+		xstrcat(out, "\n   ");
 
 	/****** Line 6 ******/
-	sprintf ( tmp2, "%s=%s", nodelist, job_ptr->nodes);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	snprintf(tmp_line, sizeof(tmp_line), "%s=%s", nodelist, job_ptr->nodes);
+	xstrcat(out, tmp_line);
 	if(job_ptr->nodes) {
 		if(quarter != (uint16_t) NO_VAL) {
 			if(nodecard != (uint16_t) NO_VAL) 
-				sprintf ( tmp2, ".%d.%d", quarter, nodecard);
+				sprintf(tmp_line, ".%d.%d", quarter, nodecard);
 			else
-				sprintf ( tmp2, ".%d", quarter);
-				lentmp2 = strlen(tmp2);
-				tmplen += lentmp2;
-				if(tmplen>HUGE_BUF) {
-					j = len + HUGE_BUF;
-					xrealloc(out, j);
-				}
-				sprintf ( out+len, "%s", tmp2);
-				len += lentmp2;
+				sprintf(tmp_line, ".%d", quarter);
+			xstrcat(out, tmp_line);
 		} 
 	}
-
-	sprintf ( tmp2, " %sIndices=", nodelist);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	sprintf(tmp_line, " %sIndices=", nodelist);
+	xstrcat(out, tmp_line);
 	for (j = 0; job_ptr->node_inx; j++) {
 		if (j > 0)
-			sprintf ( tmp2, ",%d", job_ptr->node_inx[j]);
+			sprintf(tmp_line, ",%d", job_ptr->node_inx[j]);
 		else
-			sprintf ( tmp2, "%d", job_ptr->node_inx[j]);
-		lentmp2 = strlen(tmp2);
-		tmplen += lentmp2;
-		if(tmplen>HUGE_BUF) {
-			j = len + HUGE_BUF;
-			xrealloc(out, j);
-		}
-		sprintf ( out+len, "%s", tmp2);
-		len += lentmp2;
-		
+			sprintf(tmp_line, "%d", job_ptr->node_inx[j]);
+		xstrcat(out, tmp_line);
 		if (job_ptr->node_inx[j] == -1)
 			break;
 	}
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
+		xstrcat(out, "\n   ");
 
 	/****** Line 7 ******/
 	convert_to_kilo(job_ptr->num_procs, tmp1);
 	convert_to_kilo(job_ptr->num_nodes, tmp3);
 #ifdef HAVE_BG
-	sprintf ( tmp2, "ReqProcs=%s MinBPs=%s ", tmp1, tmp3);
+	sprintf(tmp_line, "ReqProcs=%s MinBPs=%s ", tmp1, tmp3);
 #else
-	sprintf ( tmp2, "ReqProcs=%s MinNodes=%s ", tmp1, tmp3);
+	sprintf(tmp_line, "ReqProcs=%s MinNodes=%s ", tmp1, tmp3);
 #endif
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	xstrcat(out, tmp_line);
 	convert_to_kilo(job_ptr->shared, tmp1);
 	convert_to_kilo(job_ptr->contiguous, tmp3);
-	sprintf ( tmp2, "Shared=%s Contiguous=%s ", tmp1, tmp3);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	sprintf(tmp_line, "Shared=%s Contiguous=%s ", tmp1, tmp3);
+	xstrcat(out, tmp_line);
 	convert_to_kilo(job_ptr->cpus_per_task, tmp1);
-	sprintf ( tmp2, "CPUs/task=%s", tmp1);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	sprintf (tmp_line, "CPUs/task=%s", tmp1);
+	xstrcat(out, tmp_line);
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
+		xstrcat(out, "\n   ");
 
 	/****** Line 8 ******/
 	convert_to_kilo(job_ptr->min_procs, tmp1);
 	convert_to_kilo(job_ptr->min_memory, tmp3);
-	sprintf ( tmp2, "MinProcs=%s MinMemory=%s ", tmp1, tmp3);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	snprintf(tmp_line, sizeof(tmp_line), "MinProcs=%s MinMemory=%s ", 
+		tmp1, tmp3);
+	xstrcat(out, tmp_line);
 	convert_to_kilo(job_ptr->min_tmp_disk, tmp1);
-	sprintf ( tmp2, "Features=%s MinTmpDisk=%s", job_ptr->features, tmp1);
+	snprintf(tmp_line, sizeof(tmp_line), "Features=%s MinTmpDisk=%s", 
+		job_ptr->features, tmp1);
+	xstrcat(out, tmp_line);
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
+		xstrcat(out, "\n   ");
 
 	/****** Line 9 ******/
-	sprintf ( tmp2, "Dependency=%u Account=%s Reason=%s Network=%s",
+	snprintf(tmp_line, sizeof(tmp_line), 
+		"Dependency=%u Account=%s Reason=%s Network=%s",
 		job_ptr->dependency, job_ptr->account,
 		job_reason_string(job_ptr->wait_reason), job_ptr->network);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	xstrcat(out, tmp_line);
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+		xstrcat(out, "\n   ");
 
 	/****** Line 10 ******/
-	sprintf ( tmp2, "Req%s=%s ", nodelist, job_ptr->req_nodes);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
-	sprintf ( tmp2, "Req%sIndices=", nodelist);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	snprintf(tmp_line, sizeof(tmp_line), "Req%s=%s ", 
+		nodelist, job_ptr->req_nodes);
+	xstrcat(out, tmp_line);
+	snprintf(tmp_line, sizeof(tmp_line), "Req%sIndices=", nodelist);
+	xstrcat(out, tmp_line);
 	for (j = 0; job_ptr->req_node_inx; j++) {
 		if (j > 0)
-			sprintf ( tmp2, ",%d", job_ptr->req_node_inx[j]);
+			sprintf(tmp_line, ",%d", job_ptr->req_node_inx[j]);
 		else
-			sprintf ( tmp2, "%d", job_ptr->req_node_inx[j]);
-		lentmp2 = strlen(tmp2);
-		tmplen += lentmp2;
-		if(tmplen>HUGE_BUF) {
-			j = len + HUGE_BUF;
-			xrealloc(out, j);
-		}
-		sprintf ( out+len, "%s", tmp2);
-		len += lentmp2;
-		
+			sprintf(tmp_line, "%d", job_ptr->req_node_inx[j]);
+		xstrcat(out, tmp_line);
 		if (job_ptr->req_node_inx[j] == -1)
 			break;
 	}
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
+		xstrcat(out, "\n   ");
 
 	/****** Line 11 ******/
-	sprintf ( tmp2, "Exc%s=%s ", nodelist, job_ptr->exc_nodes);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
-	sprintf ( tmp2, "Exc%sIndices=", nodelist);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	snprintf(tmp_line, sizeof(tmp_line), "Exc%s=%s ", 
+		nodelist, job_ptr->exc_nodes);
+	xstrcat(out, tmp_line);
+	snprintf(tmp_line, sizeof(tmp_line), "Exc%sIndices=", nodelist);
+	xstrcat(out, tmp_line);
 	for (j = 0; job_ptr->exc_node_inx; j++) {
 		if (j > 0)
-			sprintf ( tmp2, ",%d", job_ptr->exc_node_inx[j]);
+			sprintf(tmp_line, ",%d", job_ptr->exc_node_inx[j]);
 		else
-			sprintf ( tmp2, "%d", job_ptr->exc_node_inx[j]);
-		lentmp2 = strlen(tmp2);
-		tmplen += lentmp2;
-		if(tmplen>HUGE_BUF) {
-			j = len + HUGE_BUF;
-			xrealloc(out, j);
-		}
-		sprintf ( out+len, "%s", tmp2);
-		len += lentmp2;
-		
+			sprintf(tmp_line, "%d", job_ptr->exc_node_inx[j]);
+		xstrcat(out, tmp_line);
 		if (job_ptr->exc_node_inx[j] == -1)
 			break;
 	}
 	if (one_liner)
-		sprintf ( tmp2, " ");
+		xstrcat(out, " ");
 	else
-		sprintf ( tmp2, "\n   ");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
+		xstrcat(out, "\n   ");
 
 	/****** Line 12 ******/
-	slurm_make_time_str ((time_t *)&job_ptr->submit_time, time_str, 
+	slurm_make_time_str((time_t *)&job_ptr->submit_time, time_str, 
 		sizeof(time_str));
-	sprintf ( tmp2, "SubmitTime=%s ", time_str);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-
+	sprintf(tmp_line, "SubmitTime=%s ", time_str);
+	xstrcat(out, tmp_line);
 	if (job_ptr->suspend_time) {
 		slurm_make_time_str ((time_t *)&job_ptr->suspend_time, 
 			time_str, sizeof(time_str));
 	} else {
 		strncpy(time_str, "None", sizeof(time_str));
 	}
-	sprintf ( tmp2, "SuspendTime=%s PreSusTime=%ld", 
+	sprintf(tmp_line, "SuspendTime=%s PreSusTime=%ld", 
 		  time_str, (long int)job_ptr->pre_sus_time);
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
+	xstrcat(out, tmp_line);
 
 	/****** Line 13 (optional) ******/
 	select_g_sprint_jobinfo(job_ptr->select_jobinfo,
 		select_buf, sizeof(select_buf), SELECT_PRINT_MIXED);
 	if (select_buf[0] != '\0') {
 		if (one_liner)
-			sprintf ( tmp2, " ");
+			xstrcat(out, " ");
 		else
-			sprintf ( tmp2, "\n   ");
-		lentmp2 = strlen(tmp2);
-		tmplen += lentmp2;
-		if(tmplen>HUGE_BUF) {
-			j = len + HUGE_BUF;
-			xrealloc(out, j);
-		}
-		sprintf ( out+len, "%s", tmp2);
-		len += lentmp2;
-
-		sprintf ( tmp2, "%s", select_buf);
-		lentmp2 = strlen(tmp2);
-		tmplen += lentmp2;
-		if(tmplen>HUGE_BUF) {
-			j = len + HUGE_BUF;
-			xrealloc(out, j);
-		}
-		sprintf ( out+len, "%s", tmp2);
-		len += lentmp2;
+			xstrcat(out, "\n   ");
+		xstrcat(out, select_buf);
 	}
 
-	sprintf ( tmp2, "\n\n");
-	lentmp2 = strlen(tmp2);
-	tmplen += lentmp2;
-	if(tmplen>HUGE_BUF) {
-		j = len + HUGE_BUF;
-		xrealloc(out, j);
-	}
-	sprintf ( out+len, "%s", tmp2);
-	len += lentmp2;
-	
+	sprintf(tmp_line, "\n\n");
+	xstrcat(out, select_buf);
+
 	return out;
 
 }
