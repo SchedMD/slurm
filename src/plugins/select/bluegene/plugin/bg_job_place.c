d8a51b1538521622d0dc2be16412aa78c7ad5c5e
Danny Auble <da@llnl.gov>
2006-08-24 17:27:14 +0000

    fixed convert_to_kilo misses

--- src/plugins/select/bluegene/plugin/bg_job_place.c
+++ src/plugins/select/bluegene/plugin/bg_job_place.c
@@ -243,7 +243,7 @@ try_again:
 			/* We use the proccessor count per partition here
 			   mostly to see if we can run on a smaller partition. 
 			 */
-			convert_to_kilo(proc_cnt, tmp_char);
+			convert_num_unit((float)proc_cnt, tmp_char, UNIT_NONE);
 			debug("block %s CPU count (%s) not suitable",
 			      record->bg_block_id, 
 			      tmp_char);
@@ -258,7 +258,8 @@ try_again:
 		if ((record->bp_count < min_nodes)
 		    ||  (req_nodes != 0 && record->bp_count > req_nodes)
 		    ||  (record->bp_count < target_size)) {
-			convert_to_kilo(record->node_cnt, tmp_char);
+			convert_num_unit((float)record->node_cnt, tmp_char,
+					 UNIT_NONE);
 			debug("block %s node count (%s) not suitable",
 			      record->bg_block_id,
 			      tmp_char);
