38e9aac97b4b5416717a18b1444de85021fe4cc0
Moe Jette <jette1@llnl.gov>
2006-08-29 18:15:22 +0000

    Add a bunch of logic to dump node info;

--- branches/wiki2/src/plugins/sched/wiki2/get_nodes.c
+++ branches/wiki2/src/plugins/sched/wiki2/get_nodes.c
@@ -25,71 +25,141 @@
 \*****************************************************************************/
 
 #include "./msg.h"
+#include "src/slurmctld/slurmctld.h"
 
-/* RET 0 on success, -1 on failure */
+static void	_dump_node(struct node_record *node_ptr);
+static char *	_get_node_state(uint16_t state);
+
+/*
+ * get_nodes - get information on specific node(s) changed since some time
+ * cmd_ptr IN - CMD=GETNODES ARG=[<UPDATETIME>:<NODEID>[:<NODEID>]...]
+ *                               [<UPDATETIME>:ALL]
+ * fd IN - file in which to write response
+ * RET 0 on success, -1 on failure
+ *
+ * Response format
+ * ARG=<cnt>#<NODEID>;STATE=<state>;CMEMORY=<mb>;CDISK=<mb>;CPROC=<cpus>;
+ *                    FEATURE=<feature:feature>;PARTITION=<part>[#<NODEID;...];
+ */
 extern int	get_nodes(char *cmd_ptr, slurm_fd fd)
 {
-#if 0
-	char *arg_ptr, *task_ptr, *node_ptr, *tmp_char;
-	int i;
-	uint32_t jobid;
-	hostlist_t hl;
-	char host_string[1024];
-	static char reply_msg[128];
+/* FIXME */
+int err;
+int *err_code = &err;
+char **err_msg = &cmd_ptr;
+
+	char *arg_ptr, *tmp_char;
+	time_t update_time;
 
 	arg_ptr = strstr(cmd_ptr, "ARG=");
 	if (arg_ptr == NULL) {
 		*err_code = 300;
-		*err_msg = "STARTJOB lacks ARG";
-		error("wiki: STARTJOB lacks ARG");
+		*err_msg = "GETNODES lacks ARG";
+		error("wiki: GETNODES lacks ARG");
 		return -1;
 	}
-	jobid = strtol(arg_ptr+4, &tmp_char, 10);
-	if (!isspace(tmp_char[0])) {
+	update_time = (uint32_t) strtol(arg_ptr+4, &tmp_char, 10);
+	if (tmp_char[0] != ':') {
 		*err_code = 300;
 		*err_msg = "Invalid ARG value";
-		error("wiki: STARTJOB has invalid jobid");
+		error("wiki: GETNODES has invalid ARG value");
 		return -1;
 	}
-
-	task_ptr = strstr(cmd_ptr, "TASKLIST=");
-	if (task_ptr == NULL) {
+	tmp_char++;
+	if (strncmp(tmp_char, "ALL", 3) == 0) {
+		/* report all nodes */
+	} else {
 		*err_code = 300;
-		*err_msg = "STARTJOB lacks TASKLIST";
-		error("wiki: STARTJOB lacks TASKLIST");
+		*err_msg = "Support for individual node data not available";
+		error("wiki: GETNODES list individual nodes");
 		return -1;
 	}
-	node_ptr = task_ptr + 9;
-	for (i=0; node_ptr[i]!='\0'; i++) {
-		if (node_ptr[i] == ':')
-			node_ptr[i] = ',';
-	}
-	hl = hostlist_create(node_ptr);
-	i = hostlist_ranged_string(hl, sizeof(host_string), host_string);
-	hostlist_destroy(hl);
-	if (i < 0) {
-		*err_code = 300;
-		*err_msg = "STARTJOB has invalid TASKLIST";
-		error("wiki: STARTJOB has invalid TASKLIST");
-		return -1;
-	}
-	if (sched_set_nodelist(jobid, host_string) != SLURM_SUCCESS) {
-		*err_code = 734;
-		*err_msg = "failed to assign nodes";
-		error("wiki: failed to assign nodes to job %u", jobid);
-		return -1;
-	}
-
-	if (sched_start_job(jobid, (uint32_t) 1) != SLURM_SUCCESS) {
-		*err_code = 730;
-		*err_msg = "failed to start job";
-		error("wiki: failed to start job %u", jobid);
-		return -1;
-	}
-
-	snprintf(reply_msg, sizeof(reply_msg), 
-		"job %u started successfully", jobid);
-	*err_msg = reply_msg;
-#endif
 	return 0;
+}
+
+static void	_dump_node(struct node_record *node_ptr)
+{
+	char tmp[512];
+	int i;
+	uint32_t cpu_cnt;
+
+	if (!node_ptr)
+		return;
+
+	snprintf(tmp, sizeof(tmp), "%s;STATE=%s;",
+		node_ptr->name, 
+		_get_node_state(node_ptr->node_state));
+
+	if (slurmctld_conf.fast_schedule) {
+		cpu_cnt = node_ptr->config_ptr->cpus;
+		snprintf(tmp, sizeof(tmp),
+			"CMEMORY=%u;CDISK=%u;CPROC=%u;",
+			node_ptr->config_ptr->real_memory,
+			node_ptr->config_ptr->tmp_disk,
+			node_ptr->config_ptr->cpus);
+	} else {
+		cpu_cnt = node_ptr->cpus;
+		snprintf(tmp, sizeof(tmp),
+			"CMEMORY=%u;CDISK=%u;CPROC=%u;",
+			node_ptr->real_memory,
+			node_ptr->tmp_disk,
+			node_ptr->cpus);
+	}
+
+	if (node_ptr->config_ptr
+	&&  node_ptr->config_ptr->feature) {
+		snprintf(tmp, sizeof(tmp), "FEATURES=%s;",
+			node_ptr->config_ptr->feature);
+		/* comma separated to colon */
+		for (i=0; (tmp[i] != '\0'); i++) {
+			if ((tmp[i] == ',')
+			||  (tmp[i] == '|'))
+				tmp[i] = ':';
+		}
+	}
+		
+	for (i=0; i<node_ptr->part_cnt; i++) {
+		char *header;
+		uint32_t cpu_avail;
+		if (i == 0)
+			header = "CCLASS=";
+		else
+			header = ",";
+		snprintf(tmp, sizeof(tmp), "%s%s:%u", 
+			header,
+			node_ptr->part_pptr[i]->name,
+			cpu_cnt);
+/* FIXME: Modify to support shared nodes and consumable resources */
+		if (node_ptr->node_state == NODE_STATE_IDLE)
+			cpu_avail = cpu_cnt;
+		else
+			cpu_avail = 0;
+		if (i == 0)
+			header = "ACLASS=";
+		else
+			header = ",";
+		snprintf(tmp, sizeof(tmp), "%s%s:%u",
+			header,
+			node_ptr->part_pptr[i]->name,
+			cpu_cnt);
+	}
+}
+
+static char *	_get_node_state(uint16_t state)
+{
+	uint16_t base_state = state & NODE_STATE_FLAGS;
+
+	if (state & NODE_STATE_DRAIN)
+		return "Draining";
+	if (state & NODE_STATE_COMPLETING)
+		return "Busy";
+
+	if (base_state == NODE_STATE_DOWN)
+		return "Down";
+	if (base_state == NODE_STATE_ALLOCATED)
+		return "Running";
+	if (base_state == NODE_STATE_IDLE)
+		return "Idle";
+	
+	return "Unknown";
 }
