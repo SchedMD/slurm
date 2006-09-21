62a4feb0589a7d159687266e2d146524033bcb16
Danny Auble <da@llnl.gov>
2006-09-21 16:47:41 +0000

    making sure jobs are submitted correctly with the --contiguous flag in srun

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -1440,9 +1440,8 @@ extern int job_allocate(job_desc_msg_t * job_specs, int immediate, int will_run,
 
 	no_alloc = test_only || too_fragmented || 
 			(!top_prio) || (!independent);
-	
 	error_code = select_nodes(job_ptr, no_alloc);
-	
+
 	if ((error_code == ESLURM_NODES_BUSY)
 	||  (error_code == ESLURM_JOB_HELD)
 	||  (error_code == ESLURM_REQUESTED_PART_CONFIG_UNAVAILABLE)) {
