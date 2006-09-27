79f4cea076a110a1148f425a0ca12c36fb9d7d52
Moe Jette <jette1@llnl.gov>
2006-09-27 20:33:27 +0000

    In sched/wiki2: for job info include required HOSTLIST if applicable.

--- branches/slurm-1.1/src/plugins/sched/wiki2/job_will_run.c
+++ branches/slurm-1.1/src/plugins/sched/wiki2/job_will_run.c
@@ -253,10 +253,9 @@ extern char *	bitmap2wiki_node_name(bitstr_t *bitmap)
 	for (i = 0; i < node_record_count; i++) {
 		if (bit_test (bitmap, i) == 0)
 			continue;
-		if (first == 0) {
+		if (first == 0)
 			xstrcat(buf, ":");
-			first = 0;
-		}
+		first = 0;
 		xstrcat(buf, node_record_table_ptr[i].name);
 	}
 	return buf;
