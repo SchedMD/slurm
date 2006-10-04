d4206a8896ed71758ed925c456b78f215ee6295c
Moe Jette <jette1@llnl.gov>
2006-10-04 23:12:47 +0000

    Expand explanation of hostlist. No change in logic.

--- trunk/src/common/hostlist.h
+++ trunk/src/common/hostlist.h
@@ -96,16 +96,24 @@ int set_grid(int start, int end, int count);
  *
  * Create a new hostlist from a string representation. 
  *
- * The string representation (str) may contain one or more hostnames or
- * bracketed hostlists separated by either `,' or whitespace. A bracketed 
- * hostlist is denoted by a common prefix followed by a list of numeric 
- * ranges contained within brackets: e.g. "tux[0-5,12,20-25]" 
+ * The string representation may contain one or more hostnames or
+ * bracketed hostlists separated by either `,' or whitespace (e.g. 
+ * "alpha,beta,gamma"). A bracketed hostlist is denoted by a common 
+ * prefix followed by a list of numeric ranges contained within brackets
+ * (e.g. "tux[0-5,12,20-25]"). Note that the numeric ranges can include 
+ * one or more leading zeros to indicate the numeric portion has a 
+ * fixed number of digits (e.g. "linux[0000-1023]"). 
  *
- * To support systems with 3-D topography, a rectangular prism may 
- * be described using two three digit numbers separated by "x": e.g. 
- * "bgl[123x456]". This selects all nodes between 1 and 4 inclusive 
- * in the first dimension, between 2 and 5 in the second, and between 
- * 3 and 6 in the third dimension for a total of 4*4*4=64 nodes
+ * To support the BlueGene system's 3-D topology, a node name prefix
+ * is followed by three digits identifying the node's position in 
+ * the X, Y and Z positions respectively. For example "bgl123" represents
+ * the node or midplane with an X position of 1, Y of 2, and Z of 3.
+ * A rectangular prism may be described using two endpoint locations
+ * separated by "x" (e.g. "bgl[123x456]" selects all nodes with X 
+ * positions between 1 and 4 inclusive, Y between 2 and 5, and Z between 
+ * 3 and 6 for a total of 4*4*4=64 nodes). Two or more rectangular 
+ * prisms may be specified using comma separators within the brackets
+ * (e.g. "bgl[000x133,400x533]").
  *
  * Note: if this module is compiled with WANT_RECKLESS_HOSTRANGE_EXPANSION
  * defined, a much more loose interpretation of host ranges is used. 
