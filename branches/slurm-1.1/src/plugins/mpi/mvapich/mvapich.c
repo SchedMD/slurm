ed21c094409b21e195aef4e61e72f5491b2fb202
Danny Auble <da@llnl.gov>
2006-11-03 18:08:30 +0000

    bad parens on a check fixed

--- branches/slurm-1.1/src/plugins/mpi/mvapich/mvapich.c
+++ branches/slurm-1.1/src/plugins/mpi/mvapich/mvapich.c
@@ -15,7 +15,7 @@
  *  any later version.
  *
  *  In addition, as a special exception, the copyright holders give permission 
- *  to link the code of portions of this program with the OpenSSL library under 
+ *  to link the code of portions of this program with the OpenSSL library under
  *  certain conditions as described in each individual source file, and 
  *  distribute linked combinations including the two. You must obey the GNU 
  *  General Public License in all respects for all of the code used other than 
@@ -338,11 +338,10 @@ static void mvapich_bcast_hostids (void)
 		if (fd_write_n (mvi->fd, hostids, len) < 0)
 			error ("mvapich: write hostid rank %d: %m", mvi->rank);
 
-		if (rc = fd_read_n (mvi->fd, &co, sizeof (int)) <= 0) {
+		if ((rc = fd_read_n (mvi->fd, &co, sizeof (int))) <= 0) {
 			close (mvi->fd);
 			connect_once = 0;
-		}
-		else
+		} else
 			mvi->do_poll = 1;
 	}
 
