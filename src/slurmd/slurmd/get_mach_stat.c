a8fb816a07224e6d728898976880be0ea95aceb0
Danny Auble <da@llnl.gov>
2006-11-14 21:13:56 +0000

    patch from Anne-Marie.Wunderlin@bull.com slurm-1.2.0-0.pre6  slurmd segmentation fault

--- src/slurmd/slurmd/get_mach_stat.c
+++ src/slurmd/slurmd/get_mach_stat.c
@@ -534,14 +534,14 @@ get_cpuinfo(uint16_t numproc,
 			mincpuid = MIN(mincpuid, val);
 		} else if (chk_cpuinfo_uint16(buffer, "physical id", &val)) {
 			cpuinfo[curcpu].physid = val;
-			if (cpuinfo[val].physcnt == 0) numphys++;
-			cpuinfo[val].physcnt++;
+			if (cpuinfo[curcpu].physcnt == 0) numphys++;
+			cpuinfo[curcpu].physcnt++;
 			maxphysid = MAX(maxphysid, val);
 			minphysid = MIN(minphysid, val);
 		} else if (chk_cpuinfo_uint16(buffer, "core id", &val)) {
 			cpuinfo[curcpu].coreid = val;
-			if (cpuinfo[val].corecnt == 0) numcores++;
-			cpuinfo[val].corecnt++;
+			if (cpuinfo[curcpu].corecnt == 0) numcores++;
+			cpuinfo[curcpu].corecnt++;
 			maxcoreid = MAX(maxcoreid, val);
 			mincoreid = MIN(mincoreid, val);
 		} else if (chk_cpuinfo_uint16(buffer, "siblings", &val)) {
