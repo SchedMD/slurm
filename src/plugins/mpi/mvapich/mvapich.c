adb32159fbd3a0aadbb4c3c15af89afdfce0f86b
Moe Jette <jette1@llnl.gov>
2006-11-22 18:24:48 +0000

    Add timersub macro as needed on AIX.

--- src/plugins/mpi/mvapich/mvapich.c
+++ src/plugins/mpi/mvapich/mvapich.c
@@ -72,6 +72,19 @@
 
 #include "src/plugins/mpi/mvapich/mvapich.h"
 
+/* NOTE: AIX lacks timersub */
+#ifndef timersub
+#  define timersub(a, b, result)					\
+	do {								\
+		(result)->tv_sec = (a)->tv_sec - (b)->tv_sec;		\
+		(result)->tv_usec = (a)->tv_usec - (b)->tv_usec;	\
+		if ((result)->tv_usec < 0) {				\
+			--(result)->tv_sec;				\
+			(result)->tv_usec += 1000000;			\
+		}							\
+	} while (0)
+#endif
+
 /*
  *  Arguments passed to mvapich support thread.
  */
