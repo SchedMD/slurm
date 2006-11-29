5ed581b67114777cc6b18d4e6bad5f2cd7981892
Moe Jette <jette1@llnl.gov>
2006-11-29 17:18:57 +0000

    Add test for sched/wiki (not wiki2)

--- testsuite/expect/test7.8.prog.c
+++ testsuite/expect/test7.8.prog.c
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- *  test7.7.prog.c - Test of sched/wiki2 plugin
+ *  test7.8.prog.c - Test of sched/wiki plugin
  *****************************************************************************
  *  Copyright (C) 2006 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
@@ -31,12 +31,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#include "./test7.7.crypto.c"
-
 /* global variables */
-char *auth_key, *control_addr;
-int   e_port, sched_port;
-long  job_id;
+char *control_addr;
+int   sched_port;
+long  job_id1, job_id2;
 
 static int _conn_wiki_port(char *host, int port)
 {
@@ -63,35 +61,6 @@ static int _conn_wiki_port(char *host, int port)
 		perror("connect");
 		exit(1);
 	}
-	return sock_fd;
-}
-
-static int _conn_event_port(char *host, int port)
-{
-	int sock_fd;
-	struct sockaddr_in wiki_addr;
-	struct hostent *hptr;
-
-	hptr = gethostbyname(host);
-	if (hptr == NULL) {
-		perror("gethostbyname");
-		exit(1);
-	}
-	if ((sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-		perror("socket");
-		exit(1);
-	}
-	bzero((char *) &wiki_addr, sizeof(wiki_addr));
-	wiki_addr.sin_family = AF_INET;
-	wiki_addr.sin_port   = htons(port);
-	memcpy(&wiki_addr.sin_addr.s_addr, hptr->h_addr, hptr->h_length);
-	if (bind(sock_fd, (struct sockaddr *) &wiki_addr,
-			sizeof(wiki_addr))) {
-		printf("WARNING: bind to port %i failed, may not be real error\n",
-			port);
-		return -1;
-	}
-	listen(sock_fd, 1);
 	return sock_fd;
 }
 
@@ -179,17 +148,11 @@ static char *_recv_msg(int fd)
 static void _xmit(char *msg)
 {
 	int msg_len = strlen(msg);
-	char *out_msg, *in_msg, sum[20], *sc_ptr;
+	char *out_msg, *in_msg, *sc_ptr;
 	int wiki_fd = _conn_wiki_port(control_addr, sched_port);
 	int sc;
 
-	out_msg = calloc(1, (msg_len+100));
-	if (out_msg == NULL) {
-		perror("malloc");
-		exit(1);
-	}
-	checksum(sum, auth_key, msg);
-	sprintf(out_msg, "%s %s", sum, msg);
+	out_msg = msg;
 	printf("send:%s\n", out_msg);
 	_send_msg(wiki_fd, out_msg, strlen(out_msg));
 	in_msg = _recv_msg(wiki_fd);
@@ -204,30 +167,6 @@ static void _xmit(char *msg)
 	close(wiki_fd);
 }
 
-static void _event_mgr(void)
-{
-	int accept_fd, event_fd;
-	int accept_addr_len = sizeof(struct sockaddr);
-	size_t cnt;
-	char in_msg[5];
-	struct sockaddr_in accept_addr;
-
-	if ((event_fd = _conn_event_port(control_addr, e_port)) < 0)
-		return;
-	printf("READY_FOR_EVENT\n");
-	if ((accept_fd = accept(event_fd, (struct sockaddr *) &accept_addr,
-			&accept_addr_len)) < 0) {
-		perror("accept");
-		exit(1);
-	}
-	close(event_fd);
-
-	cnt = _read_bytes(accept_fd, in_msg, sizeof(in_msg));
-	if (cnt > 0)
-		printf("event recv:%s\n\n", in_msg);
-	close(accept_fd);
-}
-
 static void _get_jobs(void)
 {
 	time_t now = time(NULL);
@@ -237,18 +176,6 @@ static void _get_jobs(void)
 	snprintf(out_msg, sizeof(out_msg),
 		"TS=%u AUTH=root DT=%s",
 		(uint32_t) now, "CMD=GETJOBS ARG=0:ALL");
-	_xmit(out_msg);
-
-	/* Dump volitile data */
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=GETJOBS ARG=%u:ALL",
-		(uint32_t) now, (uint32_t) 1);
-	_xmit(out_msg);
-
-	/* Dump state only */
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=GETJOBS ARG=%u:ALL",
-		(uint32_t) now, (uint32_t) (now+2));
 	_xmit(out_msg);
 }
 
@@ -261,18 +188,6 @@ static void _get_nodes(void)
 	snprintf(out_msg, sizeof(out_msg),
 		"TS=%u AUTH=root DT=%s", 
 		(uint32_t) now, "CMD=GETNODES ARG=0:ALL");
-	_xmit(out_msg);
-
-	/* Dump volitile data */
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=GETNODES ARG=%u:ALL",
-		(uint32_t) now, (uint32_t) 1);
-	_xmit(out_msg);
-
-	/* Dump state only */
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=GETNODES ARG=%u:ALL",
-		(uint32_t) now, (uint32_t) (now+2));
 	_xmit(out_msg);
 }
 
@@ -299,120 +214,27 @@ static void _start_job(long my_job_id)
 	_xmit(out_msg);
 }
 
-static void _suspend_job(long my_job_id)
-{
-	time_t now = time(NULL);
-	char out_msg[128];
-
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=SUSPENDJOB ARG=%ld",
-		(uint32_t) now, my_job_id);
-	_xmit(out_msg);
-}
-
-static void _signal_job(long my_job_id)
-{
-	time_t now = time(NULL);
-	char out_msg[128];
-
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=SIGNALJOB ARG=%ld VALUE=URG",
-		(uint32_t) now, my_job_id);
-	_xmit(out_msg);
-}
-
-static void _modify_job(long my_job_id)
-{
-	time_t now = time(NULL);
-	char out_msg[256];
-
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=JOBMODIFY ARG=%ld "
-		/* PARTITION=pdebug" */
-		"TIMELIMIT=10 BANK=test_bank",
-		(uint32_t) now, my_job_id);
-	_xmit(out_msg);
-}
-static void _resume_job(long my_job_id)
-{
-	time_t now = time(NULL);
-	char out_msg[128];
-
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=RESUMEJOB ARG=%ld",
-		(uint32_t) now, my_job_id);
-	_xmit(out_msg);
-}
-
-static void _job_requeue(long my_job_id)
-{
-	time_t now = time(NULL);
-	char out_msg[128];
-
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=JOBREQUEUE ARG=%ld",
-		(uint32_t) now, my_job_id);
-	_xmit(out_msg);
-}
-
-static void _job_will_run(long my_job_id)
-{
-	time_t now = time(NULL);
-	char out_msg[128];
-
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=JOBWILLRUN ARG=%ld %s",
-		(uint32_t) now, my_job_id,
-		"TASKLIST=");	/* put desired node list here */
-	_xmit(out_msg);
-}
-
-static void _initialize(void)
-{
-	time_t now = time(NULL);
-	char out_msg[128];
-
-	snprintf(out_msg, sizeof(out_msg),
-		"TS=%u AUTH=root DT=CMD=INITIALIZE ARG=USEHOSTEXP=T EPORT=%u",
-		(uint32_t) now, e_port);
-	_xmit(out_msg);
-}
-
 int main(int argc, char * argv[])
 {
-	if (argc < 4) {
-		printf("Usage: %s, auth_key control_addr e_port "
-			"job_id sched_port\n", argv[0]);
+	if (argc != 5) {
+		printf("Usage: %s, control_addr job_id1 job_id2 sched_port\n", 
+			argv[0]);
 		exit(1);
 	}
 
-	auth_key     = argv[1];
-	control_addr = argv[2];
-	e_port       = atoi(argv[3]);
-	job_id       = atoi(argv[4]);
-	sched_port   = atoi(argv[5]);
-	printf("auth_key=%s control_addr=%s e_port=%d job_id=%d sched_port=%d\n", 
-		auth_key, control_addr, e_port, job_id, sched_port);
+	control_addr = argv[1];
+	job_id1      = atoi(argv[2]);
+	job_id2      = atoi(argv[3]);
+	sched_port   = atoi(argv[4]);
+	printf("control_addr=%s job_id=%ld,%ld sched_port=%d\n", 
+		control_addr, job_id1, job_id2, sched_port);
 
-	_initialize();
 	_get_jobs();
 	_get_nodes();
-	_job_will_run(job_id);
-	_start_job(job_id);
-	_suspend_job(job_id);
-	_resume_job(job_id);
-	_modify_job(job_id);
-	_signal_job(job_id);
-	if (e_port)
-		_event_mgr();
-	else {
-		printf("READY\n");
-		sleep(3);
-	}
-	_cancel_job(job_id+1);
-	_job_requeue(job_id);	/* Put job back into HELD state */
+	_start_job(job_id1);
+	_cancel_job(job_id2);
 	sleep(5);
-	_start_job(job_id);
+	_start_job(job_id1);
 	_get_jobs();
 
 	printf("SUCCESS\n");
