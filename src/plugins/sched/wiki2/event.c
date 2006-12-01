988c22bd2e6ff420ecc52ab5ee3f9012e963a1dc
Moe Jette <jette1@llnl.gov>
2006-12-01 23:47:35 +0000

    Restructure wiki event notification logic to re-open connection only as needed.

--- src/plugins/sched/wiki2/event.c
+++ src/plugins/sched/wiki2/event.c
@@ -44,28 +44,12 @@ static slurm_addr	moab_event_addr,  moab_event_addr_bu;
 static int		event_addr_set = 0;
 static slurm_fd		event_fd = (slurm_fd) -1;
 
-/*
- * event_notify - Notify Moab of some event
- * msg IN - event type, NULL to close connection
- * RET 0 on success, -1 on failure
- */
-extern int	event_notify(char *msg)
+/* Open event_fd as needed
+ * RET 0 on success, -1 on failure */
+static int _open_fd(time_t now)
 {
-	time_t now = time(NULL);
-	int rc;
-
-	if (e_port == 0) {
-		/* Event notification disabled */
+	if (event_fd != -1)
 		return 0;
-	}
-
-	if (job_aggregation_time
-	&&  (difftime(now, last_notify_time) < job_aggregation_time)) {
-		info("wiki event notification already sent recently");
-		return 0;
-	}
-
-	pthread_mutex_lock(&event_mutex);
 
 	/* Identify address for socket connection.
 	 * Done only on first call, then cached. */
@@ -96,33 +80,71 @@ extern int	event_notify(char *msg)
 				e_host_bu, e_port);
 		}
 	}
-	if (event_fd == -1) {
-		pthread_mutex_unlock(&event_mutex);
-		/* Don't retry again for a while (10 mins)
-		 * to avoid long delays from ETIMEDOUT */
-		last_notify_time = now + 600;
+	if (event_fd == -1)
 		return -1;
-	}
 
 	/* We can't have the controller block on the following write() */
 	fd_set_nonblocking(event_fd);
+	return 0;
+}
 
-	/* Always send "1234\0" as the message
-	 * (we do not care if all of the message is sent, just that
-	 * some of it went through to wake up Moab)
-	 */
-	if (write(event_fd, "1234", 5) > 0) {
-		info("wiki event_notification sent: %s", msg);
-		last_notify_time = now;
-		rc = 0;
-	} else {
+/*
+ * event_notify - Notify Moab of some event
+ * msg IN - event type, NULL to close connection
+ * RET 0 on success, -1 on failure
+ */
+extern int	event_notify(char *msg)
+{
+	time_t now = time(NULL);
+	int rc, retry = 2;
+
+	if (e_port == 0) {
+		/* Event notification disabled */
+		return 0;
+	}
+
+	if (job_aggregation_time
+	&&  (difftime(now, last_notify_time) < job_aggregation_time)) {
+		info("wiki event notification already sent recently");
+		return 0;
+	}
+
+	pthread_mutex_lock(&event_mutex);
+	while (retry) {
+		if ((event_fd == -1) && ((rc = _open_fd(now)) == -1)) {
+			/* Can't even open socket.
+			 * Don't retry again for a while (10 mins)
+			 * to avoid long delays from ETIMEDOUT */
+			last_notify_time = now + 600;
+			break;
+		}
+
+		/* Always send "1234\0" as the message
+		 * (we do not care if all of the message is sent, 
+		 * just that some of it went through to wake up Moab)
+		 */
+		if (write(event_fd, "1234", 5) > 0) {
+			info("wiki event_notification sent: %s", msg);
+			last_notify_time = now;
+			rc = 0;
+			break;	/* success */
+		}
+
 		error("wiki event notification failure: %m");
+		rc = -1;
+		retry--;
+		if ((errno == EAGAIN) || (errno == EINTR))
+			continue;
+
 		/* close socket, re-open later */
 		(void) slurm_shutdown_msg_engine(event_fd);
 		event_fd = -1;
-		rc = -1;
-	}
 
+		if (errno != EPIPE)
+			break;
+		/* If Moab closed the socket we get an EPIPE, 
+		 * retry once */
+	}
 	pthread_mutex_unlock(&event_mutex);
 
 	return rc;
