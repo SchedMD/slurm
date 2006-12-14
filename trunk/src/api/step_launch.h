21d975e4b0fdfbad3bf1f9b7a6b0b4f074cf5f7c
Christopher J. Morrone <morrone2@llnl.gov>
2006-12-14 04:01:00 +0000

    Remove references to slaunch.

--- trunk/src/api/step_launch.h
+++ trunk/src/api/step_launch.h
@@ -61,7 +61,8 @@ struct step_launch_state {
 	/* message thread variables */
 	eio_handle_t *msg_handle;
 	pthread_t msg_thread;
-	/* set to -1 if slaunch message handler should not attempt to handle */
+	/* set to -1 if step launch message handler should not attempt
+	   to handle */
 	int slurmctld_socket_fd;
 	uint16_t num_resp_port;
 	uint16_t *resp_port; /* array of message response ports */
