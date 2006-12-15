136f53638278d99f9b3962110f0bb91f925effda
Moe Jette <jette1@llnl.gov>
2006-12-15 18:37:34 +0000

    svn merge -r10546:10563 https://eris.llnl.gov/svn/slurm/branches/slurm-1.1

--- trunk/src/slurmctld/backup.c
+++ trunk/src/slurmctld/backup.c
@@ -359,8 +359,8 @@ static int _background_process_msg(slurm_msg_t * msg)
 			debug3("Ignoring RPC: REQUEST_CONTROL");
 			error_code = ESLURM_DISABLED;
 		} else {
-			error("Invalid RPC received %d from uid %u", 
-			      msg->msg_type, uid);
+			error("Invalid RPC received %d while in standby mode", 
+			      msg->msg_type);
 			error_code = ESLURM_IN_STANDBY_MODE;
 		}
 	}
