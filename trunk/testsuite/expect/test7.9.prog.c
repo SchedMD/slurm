ceca19cd18558032548da97c053e72f7c4961da3
Christopher J. Morrone <morrone2@llnl.gov>
2006-12-19 22:32:48 +0000

    Add missing include of stdlib.h.

--- trunk/testsuite/expect/test7.9.prog.c
+++ trunk/testsuite/expect/test7.9.prog.c
@@ -24,6 +24,7 @@
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 \*****************************************************************************/
 #include <stdio.h>
+#include <stdlib.h> /* exit() prototype is here */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
