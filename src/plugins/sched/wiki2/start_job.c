c2445d2244508cf2b27f82e353b2baab997b8f37
Moe Jette <jette1@llnl.gov>
2007-01-17 21:50:04 +0000

    Restructure logic for cleaner merge with slurm v1.2

--- src/plugins/sched/wiki2/start_job.c
+++ src/plugins/sched/wiki2/start_job.c
@@ -190,12 +190,17 @@ static int	_start_job(uint32_t jobid, char *hostlist,
 
 			/* restore job state */
 			job_ptr->priority = 0;
+			if (job_ptr->details) {
+				/* Details get cleared on job abort; happens
+				 * if the request is sufficiently messed up.
+				 * This happens when Moab tries to start a
+				 * a job on invalid nodes (wrong partition). */
+				xfree(job_ptr->details->req_nodes);
+				FREE_NULL_BITMAP(job_ptr->details->req_node_bitmap);
+			}
 			if (job_ptr->job_state == JOB_FAILED)
 				wait_string = "Invalid request, job aborted";
 			else if (job_ptr->details) {
-				xfree(job_ptr->details->req_nodes);
-				if (job_ptr->details->req_node_bitmap)
-					FREE_NULL_BITMAP(job_ptr->details->req_node_bitmap);
 				wait_reason = job_ptr->details->wait_reason;
 				wait_string = job_reason_string(wait_reason);
 				job_ptr->details->wait_reason = WAIT_HELD;
