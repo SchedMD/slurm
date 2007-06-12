6f2ac67466686183e11c755352a8a701a969e988
Moe Jette <jette1@llnl.gov>
2007-06-12 17:40:08 +0000

    svn merge -r11687:11691 https://eris.llnl.gov/svn/slurm/branches/slurm-1.2

--- src/scontrol/scontrol.h
+++ src/scontrol/scontrol.h
@@ -98,6 +98,7 @@ extern int one_liner;	/* one record per line if =1 */
 extern int quiet_flag;	/* quiet=1, verbose=-1, normal=0 */
 
 extern int	scontrol_checkpoint(char *op, char *job_step_id_str);
+extern int	scontrol_encode_hostlist(char *hostlist);
 extern int 	scontrol_load_jobs (job_info_msg_t ** job_buffer_pptr);
 extern int 	scontrol_load_nodes (node_info_msg_t ** node_buffer_pptr, 
 			uint16_t show_flags);
