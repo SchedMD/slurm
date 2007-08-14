0f30f9330e9ab4c4fa774585d7449530aec1d607
Moe Jette <jette1@llnl.gov>
2007-08-14 22:38:42 +0000

    Increase default MessageTimeout (in slurm.conf) from 5 to 10 secs.

--- branches/slurm-1.2/src/common/read_config.h
+++ branches/slurm-1.2/src/common/read_config.h
@@ -64,7 +64,7 @@ extern char *default_plugstack;
 #define DEFAULT_MAX_JOB_COUNT       2000
 #define DEFAULT_MIN_JOB_AGE         300
 #define DEFAULT_MPI_DEFAULT         "none"
-#define DEFAULT_MSG_TIMEOUT         5
+#define DEFAULT_MSG_TIMEOUT         10
 #ifdef HAVE_AIX		/* AIX specific default configuration parameters */
 #  define DEFAULT_CHECKPOINT_TYPE   "checkpoint/aix"
 #  define DEFAULT_PROCTRACK_TYPE    "proctrack/aix"
