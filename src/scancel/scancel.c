8c9395771f69521bae66cabc4b8f74f8f6788a90
Moe Jette <jette1@llnl.gov>
2007-08-27 16:21:36 +0000

    Change behavior of "scancel -s KILL <jobid>" to send SIGKILL to all job     steps rather than cancelling the job. This now matches the behavior of     all other signals. "scancel <jobid>" still cancels the job and all steps.

--- src/scancel/scancel.c
+++ src/scancel/scancel.c
@@ -229,17 +229,20 @@ static void
 _cancel_job_id (uint32_t job_id, uint16_t sig)
 {
 	int error_code = SLURM_SUCCESS, i;
+	bool sig_set = true;
 
-	if (sig == (uint16_t)-1)
+	if (sig == (uint16_t)-1) {
 		sig = SIGKILL;
+		sig_set = false;
+	}
 
 	for (i=0; i<MAX_CANCEL_RETRY; i++) {
-		if (sig == SIGKILL)
+		if (!sig_set)
 			verbose("Terminating job %u", job_id);
 		else
 			verbose("Signal %u to job %u", sig, job_id);
 
-		if ((sig == SIGKILL) || opt.ctld) {
+		if ((!sig_set) || opt.ctld) {
 			error_code = slurm_kill_job (job_id, sig,
 						     (uint16_t)opt.batch);
 		} else {
