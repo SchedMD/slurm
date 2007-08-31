4a54d9221b747d0d4606f5976caf467eddd99e7f
Moe Jette <jette1@llnl.gov>
2007-08-31 17:57:29 +0000

    In select/cons_res with SelectTypeParameters=CR_Socket_Memory, fix bug in     memory allocation tracking, mem.patch from Chris Holmes, HP.

--- branches/slurm-1.2/src/plugins/select/cons_res/select_cons_res.c
+++ branches/slurm-1.2/src/plugins/select/cons_res/select_cons_res.c
@@ -2443,7 +2443,6 @@ extern int select_p_update_nodeinfo(struct job_record *job_ptr)
 					error("Job %u Host %s too many allocated sockets %u",
 					      job->job_id, this_node->node_ptr->name, 
 					      this_node->alloc_sockets);
-				this_node->alloc_memory += job->alloc_memory[i];
 				break;
 			case CR_CORE_MEMORY:
 				this_node->alloc_memory += job->alloc_memory[i];
