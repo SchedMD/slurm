553313ecbbe17cb9af021338d5d5a1dfc5cd4195
Moe Jette <jette1@llnl.gov>
2007-09-17 21:36:13 +0000

    In sched/wiki, enable use of wiki.conf parameter ExcludePartitions to     directly schedule selected partitions without Maui control.

--- src/plugins/sched/wiki/msg.c
+++ src/plugins/sched/wiki/msg.c
@@ -54,6 +54,7 @@ char     auth_key[KEY_SIZE] = "";
 char     e_host[E_HOST_SIZE] = "";
 char     e_host_bu[E_HOST_SIZE] = "";
 uint16_t e_port = 0;
+struct   part_record *exclude_part_ptr[EXC_PART_CNT];
 uint16_t job_aggregation_time = 10;	/* Default value is 10 seconds */
 int      init_prio_mode = PRIO_HOLD;
 uint16_t kill_wait;
@@ -228,15 +229,20 @@ extern int parse_wiki_config(void)
 		{"EHost", S_P_STRING},
 		{"EHostBackup", S_P_STRING},
 		{"EPort", S_P_UINT16},
+		{"ExcludePartitions", S_P_STRING},
 		{"JobAggregationTime", S_P_UINT16},
 		{"JobPriority", S_P_STRING}, 
 		{NULL} };
 	s_p_hashtbl_t *tbl;
+	char *exclude_partitions;
 	char *key = NULL, *priority_mode = NULL, *wiki_conf;
 	struct stat buf;
 	slurm_ctl_conf_t *conf;
+	int i;
 
 	/* Set default values */
+	for (i=0; i<EXC_PART_CNT; i++)
+		exclude_part_ptr[i] = NULL;
 	conf = slurm_conf_lock();
 	strncpy(e_host, conf->control_addr, sizeof(e_host));
 	if (conf->backup_addr) {
@@ -275,6 +281,25 @@ extern int parse_wiki_config(void)
 	}
 	s_p_get_uint16(&e_port, "EPort", tbl);
 	s_p_get_uint16(&job_aggregation_time, "JobAggregationTime", tbl); 
+
+	if (s_p_get_string(&exclude_partitions, "ExcludePartitions", tbl)) {
+		char *tok, *tok_p;
+		tok = strtok_r(exclude_partitions, ",", &tok_p);
+		i = 0;
+		while (tok) {
+			if (i >= EXC_PART_CNT) {
+				error("ExcludePartitions has too many entries "
+				      "skipping %s and later entries");
+				break;
+			}	
+			exclude_part_ptr[i] = find_part_record(tok);
+			if (exclude_part_ptr[i])
+				i++;
+			else
+				error("ExcludePartitions %s not found", tok);
+			tok = strtok_r(NULL, ",", &tok_p);
+		}
+	}
 
 	if (s_p_get_string(&priority_mode, "JobPriority", tbl)) {
 		if (strcasecmp(priority_mode, "hold") == 0)
