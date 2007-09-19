89c2ec3b3a0482a28ad1d366c306418c0ff52ebb
Moe Jette <jette1@llnl.gov>
2007-09-19 19:35:03 +0000

    Add missing header to avoid compiler warning

--- trunk/src/slurmctld/controller.c
+++ trunk/src/slurmctld/controller.c
@@ -68,6 +68,7 @@
 #include "src/common/node_select.h"
 #include "src/common/pack.h"
 #include "src/common/read_config.h"
+#include "src/common/slurm_jobacct_gather.h"
 #include "src/common/slurm_jobacct_storage.h"
 #include "src/common/slurm_auth.h"
 #include "src/common/slurm_jobcomp.h"
