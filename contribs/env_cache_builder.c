ae2247c8954b327f46590bb5409da6da66d894ab
Moe Jette <jette1@llnl.gov>
2007-10-30 19:11:32 +0000

    first cut at logic to build environment variable cache files for users.

--- contribs/env_cache_builder.c
+++ contribs/env_cache_builder.c
@@ -1,15 +1,18 @@
 /*****************************************************************************\
- *  This program is used to identify users for whom a pseudo-login takes 
- *  more than SU_WAIT_MSEC to complete. Either enter specific user names 
- *  on the execute line (e.g.. "time_login alice bob") or provide no input 
- *  on the execute line to test all users in the /etc/passwd file with a 
- *  UID greater than 100 (avoiding various system users). 
+ *  This program is used to build an environment variable cache file for use
+ *  with the srun/sbatch --get-user-env option, which is used by Moab to launch
+ *  user jobs. srun/sbatch will first attempt to load the user's current 
+ *  environment by executing "su - <user> -c env". If that fails to complete
+ *  in a relatively short period of time (currently 8 seconds), srun/sbatch
+ *  will attempt to load the user's environment from a cache file located 
+ *  in the directory StateSaveLocation with a name of the sort "env_<user>".
+ *  If that fails, then abort the job request.
  *
- *  Users indentified for whom the pseudo-login takes too long will not 
- *  have their environment variables set by Moab on job submit, which 
- *  relies upon the srun "--get-user-env" option to get this information.
- *  See SLURM's env_array_user_default() code in src/common/env.c.
- *  This option is presently used only by Moab.
+ *  This program can accept a space delimited list of individual users to have
+ *  cache files created (e.g. "cache_build alice bob chuck"). If no argument
+ *  is given, cache files will be created for all users.
+ *
+ *  This program must execute as user root. 
  *****************************************************************************
  *  Copyright (C) 2007 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
@@ -54,9 +57,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define DEBUG 0
 #define SU_WAIT_MSEC 8000
 static void _parse_line(char *in_line, char **user_name, int *user_id);
-static long int  _time_login(char *user_name);
+static long int _build_cache(char *user_name);
 
 main (int argc, char **argv)
 {
@@ -66,13 +70,15 @@ main (int argc, char **argv)
 	long int delta_t;
 
 	if (geteuid() != (uid_t)0) {
-		printf("need to run as user root\n");
+		printf("Need to run as user root\n");
 		exit(1);
 	}
 
 	for (i=1; i<argc; i++) {
-		delta_t = _time_login(argv[i]);
+		delta_t = _build_cache(argv[i]);
+#if DEBUG
 		printf("user %-8s time %ld usec\n", argv[i], delta_t);
+#endif
 	}
 	if (i > 1)
 		exit(0);
@@ -87,10 +93,12 @@ main (int argc, char **argv)
 		_parse_line(in_line, &user_name, &user_id);
 		if (user_id <= 100)
 			continue;
-		delta_t = _time_login(user_name);
+		delta_t = _build_cache(user_name);
+#if DEBUG
 		if (delta_t < ((SU_WAIT_MSEC * 0.8) * 1000))
 			continue;
 		printf("user %-8s time %ld usec\n", user_name, delta_t);
+#endif
 	}
 	fclose(passwd_fd);
 }
@@ -105,18 +113,20 @@ static void _parse_line(char *in_line, char **user_name, int *user_id)
 	if (tok)
 		*user_id = atoi(tok);
 	else {
-		perror("strtok");
+		printf("error parsing /etc/passwd: %s\n", in_line);
 		*user_id = 0;
 	}
 }
 
-static long int _time_login(char *user_name)
+static long int _build_cache(char *user_name)
 {
 	FILE *su;
 	char line[BUFSIZ];
 	char name[BUFSIZ];
 	char value[BUFSIZ];
-	int fildes[2], found, fval, rc, timeleft;
+	char *starttoken = "XXXXSLURMSTARTPARSINGHEREXXXX";
+	char *stoptoken  = "XXXXSLURMSTOPPARSINGHEREXXXXX";
+	int fildes[2], found, fval, len, rc, timeleft;
 	pid_t child;
 	struct timeval begin, now;
 	struct pollfd ufds;
@@ -138,8 +148,10 @@ static long int _time_login(char *user_name)
 		dup2(fildes[1], 1);
 		close(2);
 		open("/dev/null", O_WRONLY);
-		execl("/bin/su", "su", "-", user_name, "-c", 
-			"echo; echo; echo HELLO", NULL);
+		snprintf(line, sizeof(line),
+			 "echo; echo; echo; echo %s; env; echo %s",
+			 starttoken, stoptoken);
+		execl("/bin/su", "su", "-", user_name, "-c", line, NULL);
 		exit(1);
 	}
 
@@ -151,17 +163,28 @@ static long int _time_login(char *user_name)
 	gettimeofday(&begin, NULL);
 	ufds.fd = fildes[0];
 	ufds.events = POLLIN;
+
+	/* First look for the start token in the output */
+	len = strlen(starttoken);
 	found = 0;
 	while (!found) {
 		gettimeofday(&now, NULL);
-		timeleft = SU_WAIT_MSEC;
+		timeleft = SU_WAIT_MSEC * 10;
 		timeleft -= (now.tv_sec -  begin.tv_sec)  * 1000;
 		timeleft -= (now.tv_usec - begin.tv_usec) / 1000;
-		if (timeleft <= 0)
+		if (timeleft <= 0) {
+#if DEBUG
+			printf("timeout1\n");
+#endif
 			break;
+		}
 		if ((rc = poll(&ufds, 1, timeleft)) <= 0) {
-			if (rc == 0)	/* timeout */
+			if (rc == 0) {
+#if DEBUG
+				printf("timeout2\n");
+#endif
 				break;
+			}
 			if ((errno == EINTR) || (errno == EAGAIN))
 				continue;
 			perror("poll");
@@ -172,18 +195,74 @@ static long int _time_login(char *user_name)
 			break;
 		}
 		while (fgets(line, BUFSIZ, su)) {
-			if (!strncmp(line, "HELLO", 5)) {
+			if (!strncmp(line, starttoken, len)) {
 				found = 1;
 				break;
 			}
 		}
 	}
+	if (!found) {
+		printf("Failed to get current user environment variables "
+			"for %s\n", user_name);
+		close(fildes[0]);
+		gettimeofday(&now, NULL);
+		delta_t  = now.tv_sec -  begin.tv_sec * 1000000;
+		delta_t += now.tv_usec - begin.tv_usec;
+		if (delta_t < (SU_WAIT_MSEC * 1000))
+			return (SU_WAIT_MSEC * 1000);
+		return delta_t;
+	}
+
+	len = strlen(stoptoken);
+	found = 0;
+	while (!found) {
+		gettimeofday(&now, NULL);
+		timeleft = SU_WAIT_MSEC * 10;
+		timeleft -= (now.tv_sec -  begin.tv_sec)  * 1000;
+		timeleft -= (now.tv_usec - begin.tv_usec) / 1000;
+		if (timeleft <= 0) {
+#if DEBUG
+			printf("timeout3\n");
+#endif
+			break;
+		}
+		if ((rc = poll(&ufds, 1, timeleft)) <= 0) {
+			if (rc == 0) {
+#if DEBUG
+				printf("timeout4\n");
+#endif
+				break;
+			}
+			if ((errno == EINTR) || (errno == EAGAIN))
+				continue;
+			perror("poll");
+			break;
+		}
+		if (!(ufds.revents & POLLIN)) {
+			perror("POLLERR|POLLHUP");
+			break;
+		}
+		/* stop at the line containing the stoptoken string */
+		if ((fgets(line, BUFSIZ, su) == 0) ||
+		    (!strncmp(line, stoptoken, len))) {
+			found = 1;
+			break;
+		}
+
+		/* write "line" to the cache file */
+/*printf("p2:%s\n",line); */
+	}
 	close(fildes[0]);
 	waitpid(-1, NULL, WNOHANG);
 
+	gettimeofday(&now, NULL);
 	delta_t  = (now.tv_sec  - begin.tv_sec)  * 1000000;
 	delta_t +=  now.tv_usec - begin.tv_usec;
-	if (!found && (delta_t < (SU_WAIT_MSEC * 1000)))
-		return (SU_WAIT_MSEC * 1000);
+	if (!found) {
+		printf("Failed to get current user environment variables "
+			"for %s\n", user_name);
+		if (delta_t < (SU_WAIT_MSEC * 1000))
+			return (SU_WAIT_MSEC * 1000);
+	}
 	return delta_t;
 }
