059f1121000a161d9d095e5252f00ab9a9df4c82
Moe Jette <jette1@llnl.gov>
2007-11-20 23:11:49 +0000

    svn merge -r12651:12656 https://eris.llnl.gov/svn/slurm/branches/slurm-1.2

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -3119,15 +3119,15 @@ void reset_job_bitmaps(void)
 		job_ptr->part_ptr = part_ptr;
 
 		FREE_NULL_BITMAP(job_ptr->node_bitmap);
-		if ((job_ptr->nodes_completing)
-		    &&  (node_name2bitmap(job_ptr->nodes_completing,
-					  false,  &job_ptr->node_bitmap))) {
+		if ((job_ptr->nodes_completing) &&
+		     (node_name2bitmap(job_ptr->nodes_completing,
+				     false,  &job_ptr->node_bitmap))) {
 			error("Invalid nodes (%s) for job_id %u",
 			      job_ptr->nodes_completing,
 			      job_ptr->job_id);
 			job_fail = true;
-		} else if ((job_ptr->nodes)
-			   &&  (node_name2bitmap(job_ptr->nodes, false,
+		} else if ((job_ptr->node_bitmap == NULL)  && job_ptr->nodes &&
+			   (node_name2bitmap(job_ptr->nodes, false,
 						 &job_ptr->node_bitmap))) {
 			error("Invalid nodes (%s) for job_id %u", 
 		    	      job_ptr->nodes, job_ptr->job_id);
@@ -4877,12 +4877,11 @@ extern void update_job_nodes_completing(void)
 
 	job_iterator = list_iterator_create(job_list);
 	while ((job_ptr = (struct job_record *) list_next(job_iterator))) {
-		if ((job_ptr->job_state & JOB_COMPLETING) == 0)
+		if (((job_ptr->job_state & JOB_COMPLETING) == 0) ||
+		    (job_ptr->node_bitmap == NULL))
 			continue;
-		if (job_ptr->nodes_completing)  /* no change */
-			continue;
-		node_name2bitmap(job_ptr->nodes_completing,
-				 false,  &job_ptr->node_bitmap);
+		xfree(job_ptr->nodes_completing);
+		job_ptr->nodes_completing = bitmap2node_name(job_ptr->node_bitmap);
 	}
 	list_iterator_destroy(job_iterator);
 }
