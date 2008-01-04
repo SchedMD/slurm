7aa28bebf69abfc401de903198d40fb9c43c076e
Moe Jette <jette1@llnl.gov>
2008-01-04 20:59:57 +0000

    fix bad test preventing select/cons_res from running jobs

--- trunk/src/plugins/select/cons_res/select_cons_res.c
+++ trunk/src/plugins/select/cons_res/select_cons_res.c
@@ -2349,7 +2349,7 @@ extern int select_p_job_test(struct job_record *job_ptr, bitstr_t * bitmap,
 
 	if (mode == SELECT_MODE_TEST_ONLY)
 		test_only = true;
-	else if (mode == SELECT_MODE_TEST_ONLY)
+	else if (mode == SELECT_MODE_RUN_NOW)
 		test_only = false;
 	else	/* SELECT_MODE_WILL_RUN */
 		return EINVAL;	/* not yet supported */
