a53a69e2665a98a2795bf1e2f9c62e866491589f
Moe Jette <jette1@llnl.gov>
2008-02-01 22:26:15 +0000

    Change default node tmp_disk size to zero (for diskless nodes). Change hostname logic to use uname() rather than gethostname() and permit     hostnames to contain ".".

--- trunk/src/common/read_config.c
+++ trunk/src/common/read_config.c
@@ -42,14 +42,15 @@
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
+#include <pthread.h>
 #include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/utsname.h>
 #include <time.h>
 #include <unistd.h>
-#include <pthread.h>
 
 #include <slurm/slurm.h>
 
@@ -347,7 +348,7 @@ static int parse_nodename(void **dest, slurm_parser_enum_t type,
 
 		if (!s_p_get_uint32(&n->tmp_disk, "TmpDisk", tbl)
 		    && !s_p_get_uint32(&n->tmp_disk, "TmpDisk", dflt))
-			n->tmp_disk = 1;
+			n->tmp_disk = 0;
 
 		if (!s_p_get_uint32(&n->weight, "Weight", tbl)
 		    && !s_p_get_uint32(&n->weight, "Weight", dflt))
@@ -1010,27 +1011,18 @@ extern int slurm_conf_get_cpus_sct(const char *node_name,
  * (e.g. "linux123.foo.bar" becomes "linux123") 
  * OUT name
  */
-int
+extern int
 gethostname_short (char *name, size_t len)
 {
-	int error_code, name_len;
-	char *dot_ptr, path_name[1024];
+	struct utsname buf;
 
-	error_code = gethostname (path_name, sizeof(path_name));
-	if (error_code)
-		return error_code;
+	if (uname(&buf))
+		return errno;
 
-	dot_ptr = strchr (path_name, '.');
-	if (dot_ptr == NULL)
-		dot_ptr = path_name + strlen(path_name);
-	else
-		dot_ptr[0] = '\0';
-
-	name_len = (dot_ptr - path_name);
-	if (name_len > len)
+	if (strlen(buf.nodename) >= len)
 		return ENAMETOOLONG;
 
-	strcpy (name, path_name);
+	strcpy(name, buf.nodename);
 	return 0;
 }
 
