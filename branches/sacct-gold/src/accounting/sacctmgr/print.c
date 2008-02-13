ffcaf808e7714e300882d829316e13fed152c69d
Danny Auble <da@llnl.gov>
2008-02-13 00:28:43 +0000

    renam

--- branches/sacct-gold/src/accounting/sacctmgr/print.c
+++ branches/sacct-gold/src/accounting/sacctmgr/print.c
@@ -1,18 +1,123 @@
-void print_header(void)
+/*****************************************************************************\
+ *  print.c - definitions for all printing functions.
+ *****************************************************************************
+ *  Copyright (C) 2002-2008 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Danny Auble <da@llnl.gov>
+ *  UCRL-CODE-226842.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#include "sacctmgr.h"
+#define FORMAT_STRING_SIZE 32
+
+extern void print_header(void)
 {
-	int	i,j;
-	for (i=0; i<nprintfields; i++) {
-		if (i)
-			printf(" ");
-		j=printfields[i];
-		(fields[j].print_routine)(HEADLINE, 0);
+/* 	int	i,j; */
+/* 	for (i=0; i<nprintfields; i++) { */
+/* 		if (i) */
+/* 			printf(" "); */
+/* 		j=printfields[i]; */
+/* 		(fields[j].print_routine)(HEADLINE, 0); */
+/* 	} */
+/* 	printf("\n"); */
+/* 	for (i=0; i<nprintfields; i++) { */
+/* 		if (i) */
+/* 			printf(" "); */
+/* 		j=printfields[i]; */
+/* 		(fields[j].print_routine)(UNDERSCORE, 0); */
+/* 	} */
+/* 	printf("\n"); */
+}
+extern int  print_str(char *str, int width, bool right, bool cut_output)
+{
+	char format[64];
+	int printed = 0;
+
+	if (right == true && width != 0)
+		snprintf(format, 64, "%%%ds", width);
+	else if (width != 0)
+		snprintf(format, 64, "%%.%ds", width);
+	else {
+		format[0] = '%';
+		format[1] = 's';
+		format[2] = '\0';
 	}
-	printf("\n");
-	for (i=0; i<nprintfields; i++) {
-		if (i)
-			printf(" ");
-		j=printfields[i];
-		(fields[j].print_routine)(UNDERSCORE, 0);
+
+	if ((width == 0) || (cut_output == false)) {
+		if ((printed = printf(format, str)) < 0)
+			return printed;
+	} else {
+		char temp[width + 1];
+		snprintf(temp, width + 1, format, str);
+		if ((printed = printf("%s",temp)) < 0)
+			return printed;
 	}
-	printf("\n");
+
+	while (printed++ < width)
+		printf(" ");
+
+	return printed;
+}
+extern void print_date(void)
+{
+	time_t now;
+
+	now = time(NULL);
+	printf("%s", ctime(&now));
+
+}
+
+extern int print_secs(long time, int width, bool right, bool cut_output)
+{
+	char str[FORMAT_STRING_SIZE];
+	long days, hours, minutes, seconds;
+
+	seconds =  time % 60;
+	minutes = (time / 60)   % 60;
+	hours   = (time / 3600) % 24;
+	days    =  time / 86400;
+
+	if (days) 
+		snprintf(str, FORMAT_STRING_SIZE,
+			 "%ld-%2.2ld:%2.2ld:%2.2ld",
+		         days, hours, minutes, seconds);
+	else if (hours)
+		snprintf(str, FORMAT_STRING_SIZE,
+			 "%ld:%2.2ld:%2.2ld",
+		         hours, minutes, seconds);
+	else
+		snprintf(str, FORMAT_STRING_SIZE,
+			 "%ld:%2.2ld",
+		         minutes, seconds);
+
+	_print_str(str, width, right, cut_output);
+	return SLURM_SUCCESS;
 }
