0aae3e160d1aeafc0a964952ddd69aada8658d40
Moe Jette <jette1@llnl.gov>
2008-02-26 19:11:11 +0000

    Add logic that can be used to log the default Munge socket name

--- branches/slurm.dbd/src/plugins/auth/munge/auth_munge.c
+++ branches/slurm.dbd/src/plugins/auth/munge/auth_munge.c
@@ -160,9 +160,20 @@ slurm_auth_create( void *argv[], char *socket )
 		return NULL;
 	}
 
+#if 0
+	/* This logic can be used to determine what socket is used by default.
+	 * A typical name is "/var/run/munge/munge.socket.2" */
+{
+	char *old_socket;
+	if (munge_ctx_get(ctx, MUNGE_OPT_SOCKET, &old_socket) != EMUNGE_SUCCESS)
+		error("munge_ctx_get failure");
+	else
+		info("Default Munge socket is %s", old_socket);
+}
+#endif
 	if (socket &&
 	    (munge_ctx_set(ctx, MUNGE_OPT_SOCKET, socket) != EMUNGE_SUCCESS)) {
-		error("munge_ctx_get failure");
+		error("munge_ctx_set failure");
 		munge_ctx_destroy(ctx);
 		return NULL;
 	}
@@ -459,7 +470,7 @@ _decode_cred(slurm_auth_credential_t *c, char *socket)
 	}
 	if (socket &&
 	    (munge_ctx_set(ctx, MUNGE_OPT_SOCKET, socket) != EMUNGE_SUCCESS)) {
-		error("munge_ctx_get failure");
+		error("munge_ctx_set failure");
 		munge_ctx_destroy(ctx);
 		return SLURM_ERROR;
 	}
