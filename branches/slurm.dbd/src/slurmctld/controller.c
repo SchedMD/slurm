a34be99ce4d463aee7a1d4d78530ebeec44beba2
Moe Jette <jette1@llnl.gov>
2008-02-26 17:36:57 +0000

    validate accounting plugins at startup

--- branches/slurm.dbd/src/slurmctld/controller.c
+++ branches/slurm.dbd/src/slurmctld/controller.c
@@ -297,7 +297,13 @@ int main(int argc, char *argv[])
 			SLURM_SUCCESS )
 		fatal( "failed to initialize checkpoint plugin" );
 	if (slurm_select_init() != SLURM_SUCCESS )
-		fatal( "failed to initialize node selection plugin state");
+		fatal( "failed to initialize node selection plugin");
+	if (slurm_clusteracct_storage_init() != SLURM_SUCCESS )
+		fatal( "failed to initialize clusteracct_storage plugin");
+	if (slurm_jobacct_gather_init() != SLURM_SUCCESS )
+		fatal( "failed to initialize jobacct_gather plugin");
+	if (slurm_jobacct_storage_init() != SLURM_SUCCESS )
+		fatal( "failed to initialize jobacct_storage plugin");
 
 	while (1) {
 		/* initialization for each primary<->backup switch */
@@ -464,6 +470,7 @@ int main(int argc, char *argv[])
 	 * unplug after other data structures are purged */
 	g_slurm_jobcomp_fini();
 	jobacct_storage_g_fini();
+	slurm_clusteracct_storage_fini();
 	slurm_jobacct_gather_fini();
 	slurm_sched_fini();
 	slurm_select_fini();
