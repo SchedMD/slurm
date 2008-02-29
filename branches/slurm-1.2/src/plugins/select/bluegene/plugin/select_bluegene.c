3e1d37ec662fde76bebc9fd5f70d323a1524da0f
Danny Auble <da@llnl.gov>
2008-02-29 17:30:38 +0000

    update to make squeue display the correct cnode count for bluegene systems

--- branches/slurm-1.2/src/plugins/select/bluegene/plugin/select_bluegene.c
+++ branches/slurm-1.2/src/plugins/select/bluegene/plugin/select_bluegene.c
@@ -1164,6 +1164,9 @@ extern int select_p_alter_node_cnt(enum select_node_cnt type, void *data)
 		
 		/* this means it is greater or equal to one bp */
 		if(tmp > 0) {
+			select_g_set_jobinfo(job_desc->select_jobinfo,
+					     SELECT_DATA_NODE_CNT,
+					     &job_desc->min_nodes);
 			job_desc->min_nodes = tmp;
 			job_desc->num_procs = procs_per_node * tmp;
 		} else { 
@@ -1179,6 +1182,10 @@ extern int select_p_alter_node_cnt(enum select_node_cnt type, void *data)
 				job_desc->min_nodes = 
 					bluegene_bp_node_cnt;
 			
+			select_g_set_jobinfo(job_desc->select_jobinfo,
+					     SELECT_DATA_NODE_CNT,
+					     &job_desc->min_nodes);
+
 			tmp = bluegene_bp_node_cnt/job_desc->min_nodes;
 			
 			job_desc->num_procs = procs_per_node/tmp;
