fc76be0bba8bfbd64152c05fcaa42844fdaf5c36
Moe Jette <jette1@llnl.gov>
2008-03-14 22:52:50 +0000

    flag work to be done

--- trunk/src/slurmctld/controller.c
+++ trunk/src/slurmctld/controller.c
@@ -379,6 +379,8 @@ int main(int argc, char *argv[])
 		clusteracct_storage_g_register_ctld(
 					slurmctld_conf.cluster_name, 
 					slurmctld_conf.slurmctld_port);
+		/* FIXME: wait for response and treat SLURM_ERROR as fatal
+		 * if accounting_storage_enforce is set */
 
 		/*
 		 * create attached thread for signal handling
