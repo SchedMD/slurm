4aafa53052a3855d4a4eacb16e4dd4f7888210c9
Moe Jette <jette1@llnl.gov>
2008-05-06 19:59:00 +0000

    tweak tests for bgl operation (srun disabled)

--- testsuite/slurm_unit/slurmctld/security_2_2b.sh
+++ testsuite/slurm_unit/slurmctld/security_2_2b.sh
@@ -8,9 +8,12 @@ file_orig=`${slurm_bin}scontrol show config | awk '{ if ( $1 ~ /SLURM_CONFIG_FIL
 grep -iv AuthType <$file_orig >tmp.$$
 echo "AuthType=auth/dummy" >>tmp.$$
 
+echo "#!/bin/sh"     >tmp2.$$
+echo "id"           >>tmp2.$$
+
 # Run srun using this config file
 export SLURM_CONF=tmp.$$ 
-${slurm_bin}srun /usr/bin/id
+${slurm_bin}sbatch tmp2.$$
 
 # Clean up
-rm tmp.$$
+rm -f tmp.$$ tmp2.$$
