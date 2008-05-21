6a6a19f712ea3bc9d0bcf120dd87e94e580eb3fe
Danny Auble <da@llnl.gov>
2008-05-21 18:48:10 +0000

    svn merge -r14094:14099 https://eris.llnl.gov/svn/slurm/branches/slurm-1.2

--- trunk/src/slurmctld/read_config.c
+++ trunk/src/slurmctld/read_config.c
@@ -801,12 +801,12 @@ int read_slurm_conf(int recover)
 
 	_build_bitmaps_pre_select();
 	if ((select_g_node_init(node_record_table_ptr, node_record_count)
-			!= SLURM_SUCCESS) ||
-	    (select_g_block_init(part_list) != SLURM_SUCCESS) ||
-	    (select_g_state_restore(state_save_dir) != SLURM_SUCCESS) ||
-	    (select_g_job_init(job_list) != SLURM_SUCCESS)) { 
-		error("failed to initialize node selection plugin state");
-		abort();
+	     != SLURM_SUCCESS) 
+	    || (select_g_block_init(part_list) != SLURM_SUCCESS) 
+	    || (select_g_state_restore(state_save_dir) != SLURM_SUCCESS) 
+	    || (select_g_job_init(job_list) != SLURM_SUCCESS)) {
+		fatal("failed to initialize node selection plugin state, "
+		      "Clean start required.");
 	}
 	xfree(state_save_dir);
 	reset_job_bitmaps();		/* must follow select_g_job_init() */
