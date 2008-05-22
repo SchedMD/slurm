4652795facfd1dc448a672f4882135433924d219
Moe Jette <jette1@llnl.gov>
2008-05-22 23:01:06 +0000

    Reopen slurmctld log file after reconfigure or SIGHUP is received.

--- src/slurmctld/controller.c
+++ src/slurmctld/controller.c
@@ -661,6 +661,7 @@ static void *_slurmctld_signal_hand(void *no_data)
 			break;
 		case SIGHUP:	/* kill -1 */
 			info("Reconfigure signal (SIGHUP) received");
+			slurm_reconfigure();
 			break;
 		case SIGABRT:	/* abort */
 			info("SIGABRT received");
@@ -1357,6 +1358,7 @@ static void _update_cred_key(void)
  * NOTE: READ lock_slurmctld config before entry */
 void update_logging(void) 
 {
+info("update_logging");
 	/* Preserve execute line arguments (if any) */
 	if (debug_level) {
 		slurmctld_conf.slurmctld_debug = MIN(
