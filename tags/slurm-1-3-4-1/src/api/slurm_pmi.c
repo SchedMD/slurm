252d3524438e9975aa70359ef147fc97552f0c39
Moe Jette <jette1@llnl.gov>
2008-06-23 22:52:11 +0000

    tag v1.3.4

---
+++ tags/slurm-1-3-4-1/src/api/slurm_pmi.c
@@ -0,0 +1,402 @@
+/****************************************************************************\
+ *  slurm_pmi.c - PMI support functions internal to SLURM
+ *****************************************************************************
+ *  Copyright (C) 2005-2006 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Morris Jette <jette1@llnl.gov>.
+ *  LLNL-CODE-402394.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under 
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#include <stdlib.h>
+#include <sys/time.h>
+#include <slurm/slurm.h>
+#include <slurm/slurm_errno.h>
+
+#include "src/api/slurm_pmi.h"
+#include "src/common/slurm_protocol_api.h"
+#include "src/common/slurm_protocol_defs.h"
+#include "src/common/forward.h"
+#include "src/common/read_config.h"
+#include "src/common/xmalloc.h"
+#include "src/common/fd.h"
+#include "src/common/slurm_auth.h"
+
+#define DEFAULT_PMI_TIME 500
+#define MAX_RETRIES      5
+
+int pmi_fd = -1;
+int pmi_time = 0;
+uint16_t srun_port = 0;
+slurm_addr srun_addr;
+
+static void _delay_rpc(int pmi_rank, int pmi_size);
+static int  _forward_comm_set(struct kvs_comm_set *kvs_set_ptr);
+static int  _get_addr(void);
+static void _set_pmi_time(void);
+
+/* Delay an RPC to srun in order to avoid overwhelming the srun command.
+ * The delay is based upon the number of tasks, this task's rank, and PMI_TIME.
+ * This logic depends upon synchronized clocks across the cluster. */
+static void _delay_rpc(int pmi_rank, int pmi_size)
+{
+	struct timeval tv1, tv2;
+	uint32_t cur_time;	/* current time in usec (just 9 digits) */
+	uint32_t tot_time;	/* total time expected for all RPCs */
+	uint32_t offset_time;	/* relative time within tot_time */
+	uint32_t target_time;	/* desired time to issue the RPC */
+	uint32_t delta_time, error_time;
+	int retries = 0;
+
+	_set_pmi_time();
+
+again:	if (gettimeofday(&tv1, NULL)) {
+		usleep(pmi_rank * pmi_time);
+		return;
+	}
+
+	cur_time = (tv1.tv_sec % 1000) + tv1.tv_usec;
+	tot_time = pmi_size * pmi_time;
+	offset_time = cur_time % tot_time;
+	target_time = pmi_rank * pmi_time;
+	if (target_time < offset_time)
+		delta_time = target_time - offset_time + tot_time;
+	else
+		delta_time = target_time - offset_time;
+	if (usleep(delta_time)) {
+		if (errno == EINVAL)
+			usleep(900000);
+		/* errno == EINTR */
+		goto again;
+	}
+
+	/* Verify we are active at the right time. If current time is different
+	 * from target by more than 15*pmi_time, then start over. If PMI_TIME 
+	 * is set appropriately, then srun should have no more than 30 RPCs
+	 * in the queue at one time in the worst case. */
+	if (gettimeofday(&tv2, NULL))
+		return;
+	tot_time = (tv2.tv_sec - tv1.tv_sec) * 1000000;
+	tot_time += tv2.tv_usec;
+	tot_time -= tv1.tv_usec;
+	if (tot_time >= delta_time)
+		error_time = tot_time - delta_time;
+	else
+		error_time = delta_time - tot_time;
+	if (error_time > (15*pmi_time)) {	/* too far off */
+#if 0
+		info("delta=%u tot=%u err=%u", 
+			delta_time, tot_time, error_time);
+#endif
+		if ((++retries) <= 2)
+			goto again;
+	}
+}
+
+static int _get_addr(void)
+{
+	char *env_host, *env_port;
+
+	if (srun_port)
+		return SLURM_SUCCESS;
+
+	env_host = getenv("SLURM_SRUN_COMM_HOST");
+	env_port = getenv("SLURM_SRUN_COMM_PORT");
+	if (!env_host || !env_port)
+		return SLURM_ERROR;
+
+	srun_port = (uint16_t) atol(env_port);
+	slurm_set_addr(&srun_addr, srun_port, env_host);
+	return SLURM_SUCCESS;
+}
+
+static void _set_pmi_time(void)
+{
+	char *tmp, *endptr;
+
+	if (pmi_time)
+		return;
+
+	tmp = getenv("PMI_TIME");
+	if (tmp == NULL) {
+		pmi_time = DEFAULT_PMI_TIME;
+		return;
+	}
+
+	pmi_time = strtol(tmp, &endptr, 10);
+	if ((pmi_time < 0) || (endptr[0] != '\0')) {
+		error("Invalid PMI_TIME: %s", tmp);
+		pmi_time = DEFAULT_PMI_TIME;
+	}
+}
+
+/* Transmit PMI Keyval space data */
+int slurm_send_kvs_comm_set(struct kvs_comm_set *kvs_set_ptr, 
+		int pmi_rank, int pmi_size)
+{
+	slurm_msg_t msg_send;
+	int rc, retries = 0, timeout = 0;
+
+	if (kvs_set_ptr == NULL)
+		return EINVAL;
+
+	if ((rc = _get_addr()) != SLURM_SUCCESS)
+		return rc; 
+	_set_pmi_time();
+
+	slurm_msg_t_init(&msg_send);
+	msg_send.address = srun_addr;
+	msg_send.msg_type = PMI_KVS_PUT_REQ;
+	msg_send.data = (void *) kvs_set_ptr;
+	
+	/* Send the RPC to the local srun communcation manager.
+	 * Since the srun can be sent thousands of messages at 
+	 * the same time and refuse some connections, retry as 
+	 * needed. Spread out messages by task's rank. Also 
+	 * increase the timeout if many tasks since the srun 
+	 * command is very overloaded.
+	 * We also increase the timeout (default timeout is
+	 * 10 secs). */
+	_delay_rpc(pmi_rank, pmi_size);
+	if      (pmi_size > 4000)	/* 240 secs */
+		timeout = slurm_get_msg_timeout() * 24000;
+	else if (pmi_size > 1000)	/* 120 secs */
+		timeout = slurm_get_msg_timeout() * 12000;
+	else if (pmi_size > 100)	/* 50 secs */
+		timeout = slurm_get_msg_timeout() * 5000;
+	else if (pmi_size > 10)		/* 20 secs */
+		timeout = slurm_get_msg_timeout() * 2000;
+
+	while (slurm_send_recv_rc_msg_only_one(&msg_send, &rc, timeout) < 0) {
+		if (retries++ > MAX_RETRIES) {
+			error("slurm_send_kvs_comm_set: %m");
+			return SLURM_ERROR;
+		} else
+			debug("send_kvs retry %d", retries);
+		_delay_rpc(pmi_rank, pmi_size);
+	}
+
+	return rc;
+}
+
+/* Wait for barrier and get full PMI Keyval space data */
+int  slurm_get_kvs_comm_set(struct kvs_comm_set **kvs_set_ptr, 
+		int pmi_rank, int pmi_size)
+{
+	int rc, srun_fd, retries = 0, timeout = 0;
+	slurm_msg_t msg_send, msg_rcv;
+	slurm_addr slurm_addr, srun_reply_addr;
+	char hostname[64];
+	uint16_t port;
+	kvs_get_msg_t data;
+	char *env_pmi_ifhn;
+
+	if (kvs_set_ptr == NULL)
+		return EINVAL;
+	*kvs_set_ptr = NULL;	/* initialization */
+
+	if ((rc = _get_addr()) != SLURM_SUCCESS) {
+		error("_get_addr: %m");
+		return rc;
+	}
+
+	_set_pmi_time();
+
+	if (pmi_fd < 0) {
+		if ((pmi_fd = slurm_init_msg_engine_port(0)) < 0) {
+			error("slurm_init_msg_engine_port: %m");
+			return SLURM_ERROR;
+		}
+		fd_set_blocking(pmi_fd);
+	}
+	if (slurm_get_stream_addr(pmi_fd, &slurm_addr) < 0) {
+		error("slurm_get_stream_addr: %m");
+		return SLURM_ERROR;
+	}
+	/* hostname is not set here, so slurm_get_addr fails
+	slurm_get_addr(&slurm_addr, &port, hostname, sizeof(hostname)); */
+	port = ntohs(slurm_addr.sin_port); 
+	if ((env_pmi_ifhn = getenv("SLURM_PMI_RESP_IFHN"))) {
+		strncpy(hostname, env_pmi_ifhn, sizeof(hostname));
+		hostname[sizeof(hostname)-1] = 0;
+	} else
+		gethostname_short(hostname, sizeof(hostname));
+
+	data.task_id = pmi_rank;
+	data.size = pmi_size;
+	data.port = port;
+	data.hostname = hostname;
+	slurm_msg_t_init(&msg_send);
+	slurm_msg_t_init(&msg_rcv);
+	msg_send.address = srun_addr;
+	msg_send.msg_type = PMI_KVS_GET_REQ;
+	msg_send.data = &data;
+
+	/* Send the RPC to the local srun communcation manager.
+	 * Since the srun can be sent thousands of messages at 
+	 * the same time and refuse some connections, retry as 
+	 * needed. Wait until all key-pairs have been sent by
+	 * all tasks then spread out messages by task's rank.
+	 * Also increase the message timeout if many tasks 
+	 * since the srun command can get very overloaded (the
+	 * default timeout is 10 secs).
+	 */
+	_delay_rpc(pmi_rank, pmi_size);
+	if      (pmi_size > 4000)	/* 240 secs */
+		timeout = slurm_get_msg_timeout() * 24000;
+	else if (pmi_size > 1000)	/* 120 secs */
+		timeout = slurm_get_msg_timeout() * 12000;
+	else if (pmi_size > 100)	/* 60 secs */
+		timeout = slurm_get_msg_timeout() * 6000;
+	else if (pmi_size > 10)		/* 20 secs */
+		timeout = slurm_get_msg_timeout() * 2000;
+
+	while (slurm_send_recv_rc_msg_only_one(&msg_send, &rc, timeout) < 0) {
+		if (retries++ > MAX_RETRIES) {
+			error("slurm_get_kvs_comm_set: %m");
+			return SLURM_ERROR;
+		} else
+			debug("get kvs retry %d", retries);
+		_delay_rpc(pmi_rank, pmi_size);
+	}
+	if (rc != SLURM_SUCCESS) {
+		error("slurm_get_kvs_comm_set error_code=%d", rc);
+		return rc;
+	}
+
+	/* get the message after all tasks reach the barrier */
+	srun_fd = slurm_accept_msg_conn(pmi_fd, &srun_reply_addr);
+	if (srun_fd < 0) {
+		error("slurm_accept_msg_conn: %m");
+		return errno;
+	}
+
+	while ((rc = slurm_receive_msg(srun_fd, &msg_rcv, timeout)) != 0) {
+		if (errno == EINTR)
+			continue;
+		error("slurm_receive_msg: %m");
+		slurm_close_accepted_conn(srun_fd);
+		return errno;
+	}
+	if(msg_rcv.auth_cred)
+		(void)g_slurm_auth_destroy(msg_rcv.auth_cred);
+	
+	if (msg_rcv.msg_type != PMI_KVS_GET_RESP) {
+		error("slurm_get_kvs_comm_set msg_type=%d", msg_rcv.msg_type);
+		slurm_close_accepted_conn(srun_fd);
+		return SLURM_UNEXPECTED_MSG_ERROR;
+	}
+	if (slurm_send_rc_msg(&msg_rcv, SLURM_SUCCESS) < 0)
+		error("slurm_send_rc_msg: %m");
+	
+	slurm_close_accepted_conn(srun_fd);
+	*kvs_set_ptr = msg_rcv.data;
+
+	rc = _forward_comm_set(*kvs_set_ptr);
+	return rc;
+}
+
+/* Forward keypair info to other tasks as required.
+ * Clear message forward structure upon completion. 
+ * The messages are forwarded sequentially. */
+static int _forward_comm_set(struct kvs_comm_set *kvs_set_ptr)
+{
+	int i, rc = SLURM_SUCCESS;
+	int tmp_host_cnt = kvs_set_ptr->host_cnt;
+	slurm_msg_t msg_send;
+	int msg_rc;
+
+	kvs_set_ptr->host_cnt = 0;
+	for (i=0; i<tmp_host_cnt; i++) {
+		if (kvs_set_ptr->kvs_host_ptr[i].port == 0)
+			continue;	/* empty */
+		slurm_msg_t_init(&msg_send);
+		msg_send.msg_type = PMI_KVS_GET_RESP;
+		msg_send.data = (void *) kvs_set_ptr;
+		slurm_set_addr(&msg_send.address,
+			kvs_set_ptr->kvs_host_ptr[i].port,
+			kvs_set_ptr->kvs_host_ptr[i].hostname);
+		if (slurm_send_recv_rc_msg_only_one(&msg_send,
+				&msg_rc, 0) < 0) {
+			error("Could not forward msg to %s",
+				kvs_set_ptr->kvs_host_ptr[i].hostname);
+			msg_rc = 1;
+		}
+		rc = MAX(rc, msg_rc);
+		xfree(kvs_set_ptr->kvs_host_ptr[i].hostname);
+	}
+	xfree(kvs_set_ptr->kvs_host_ptr);
+	return rc;
+}
+
+static void _free_kvs_comm(struct kvs_comm *kvs_comm_ptr)
+{
+	int i;
+
+	if (kvs_comm_ptr == NULL)
+		return;
+
+	for (i=0; i<kvs_comm_ptr->kvs_cnt; i++) {
+		xfree(kvs_comm_ptr->kvs_keys[i]);
+		xfree(kvs_comm_ptr->kvs_values[i]);
+	}
+	xfree(kvs_comm_ptr->kvs_name);
+	xfree(kvs_comm_ptr->kvs_keys);
+	xfree(kvs_comm_ptr->kvs_values);
+	xfree(kvs_comm_ptr);
+}
+
+/* Free kvs_comm_set returned by slurm_get_kvs_comm_set() */
+void slurm_free_kvs_comm_set(struct kvs_comm_set *kvs_set_ptr)
+{
+	int i;
+
+	if (kvs_set_ptr == NULL)
+		return;
+
+	for (i=0; i<kvs_set_ptr->host_cnt; i++)
+		xfree(kvs_set_ptr->kvs_host_ptr[i].hostname);
+	xfree(kvs_set_ptr->kvs_host_ptr);
+
+	for (i=0; i<kvs_set_ptr->kvs_comm_recs; i++)
+		_free_kvs_comm(kvs_set_ptr->kvs_comm_ptr[i]);
+	xfree(kvs_set_ptr->kvs_comm_ptr);
+	xfree(kvs_set_ptr);
+}
+
+/* Finalization processing */
+void slurm_pmi_finalize(void)
+{
+	if (pmi_fd >= 0) {
+		slurm_shutdown_msg_engine(pmi_fd);
+		pmi_fd = -1;
+	}
+	srun_port = 0;
+}
---
+++ tags/slurm-1-3-4-1/src/database/mysql_common.c
@@ -0,0 +1,438 @@
+/*****************************************************************************\
+ *  mysql_common.c - common functions for the the mysql storage plugin.
+ *****************************************************************************
+ *
+ *  Copyright (C) 2004-2007 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Danny Auble <da@llnl.gov>
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+ *
+ *  This file is patterned after jobcomp_linux.c, written by Morris Jette and
+ *  Copyright (C) 2002 The Regents of the University of California.
+\*****************************************************************************/
+
+#include "mysql_common.h"
+#include "src/common/xmalloc.h"
+#include "src/common/timers.h"
+#include "src/common/slurm_protocol_api.h"
+
+pthread_mutex_t mysql_lock = PTHREAD_MUTEX_INITIALIZER;
+
+#ifdef HAVE_MYSQL
+
+static int _clear_results(MYSQL *mysql_db)
+{
+	MYSQL_RES *result = NULL;
+	int rc = 0;
+	do {
+		/* did current statement return data? */
+		if((result = mysql_store_result(mysql_db)))
+			mysql_free_result(result);
+		
+		/* more results? -1 = no, >0 = error, 0 = yes (keep looping) */
+		if ((rc = mysql_next_result(mysql_db)) > 0)
+			error("Could not execute statement %d %s\n",
+			      mysql_errno(mysql_db),
+			      mysql_error(mysql_db));
+	} while (rc == 0);
+	
+	return SLURM_SUCCESS;
+}
+
+static MYSQL_RES *_get_first_result(MYSQL *mysql_db)
+{
+	MYSQL_RES *result = NULL;
+	int rc = 0;
+	do {
+		/* did current statement return data? */
+		if((result = mysql_store_result(mysql_db)))
+			return result;
+		
+		/* more results? -1 = no, >0 = error, 0 = yes (keep looping) */
+		if ((rc = mysql_next_result(mysql_db)) > 0)
+			debug3("error: Could not execute statement %d\n", rc);
+		
+	} while (rc == 0);
+	
+	return NULL;
+}
+
+static MYSQL_RES *_get_last_result(MYSQL *mysql_db)
+{
+	MYSQL_RES *result = NULL;
+	MYSQL_RES *last_result = NULL;
+	int rc = 0;
+	do {
+		/* did current statement return data? */
+		if((result = mysql_store_result(mysql_db))) {
+			if(last_result)
+				mysql_free_result(last_result);
+			last_result = result;
+		}
+		/* more results? -1 = no, >0 = error, 0 = yes (keep looping) */
+		if ((rc = mysql_next_result(mysql_db)) > 0)
+			debug3("error: Could not execute statement %d\n", rc);
+	} while (rc == 0);
+	
+	return last_result;
+}
+
+static int _mysql_make_table_current(MYSQL *mysql_db, char *table_name,
+				     storage_field_t *fields)
+{
+	char *query = NULL;
+	MYSQL_RES *result = NULL;
+	MYSQL_ROW row;
+	int i = 0;
+	List columns = NULL;
+	ListIterator itr = NULL;
+	char *col = NULL;
+	DEF_TIMERS;
+
+	query = xstrdup_printf("show columns from %s", table_name);
+
+	if(!(result = mysql_db_query_ret(mysql_db, query, 0))) {
+		xfree(query);
+		return SLURM_ERROR;
+	}
+	xfree(query);
+	columns = list_create(slurm_destroy_char);
+	while((row = mysql_fetch_row(result))) {
+		col = xstrdup(row[0]); //Field
+		list_append(columns, col);
+	}
+	mysql_free_result(result);
+	itr = list_iterator_create(columns);
+	query = xstrdup_printf("alter table %s", table_name);
+	START_TIMER;
+	while(fields[i].name) {
+		int found = 0;
+		list_iterator_reset(itr);
+		while((col = list_next(itr))) {
+			if(!strcmp(col, fields[i].name)) {
+				xstrfmtcat(query, " modify %s %s,",
+					   fields[i].name,
+					   fields[i].options);
+				list_delete_item(itr);
+				found = 1;
+				break;
+			}
+		}
+		if(!found) {
+			if(i) {
+				info("adding column %s after %s",
+				     fields[i].name,
+				     fields[i-1].name);
+				xstrfmtcat(query, " add %s %s after %s,",
+					   fields[i].name,
+					   fields[i].options,
+					   fields[i-1].name);
+			} else {
+				info("adding column %s at the beginning",
+				     fields[i].name,
+				     fields[i-1].name);
+				xstrfmtcat(query, " add %s %s first,",
+					   fields[i].name,
+					   fields[i].options);
+			}
+		}
+
+		i++;
+	}
+	list_iterator_destroy(itr);
+	list_destroy(columns);
+	query[strlen(query)-1] = ';';
+	if(mysql_db_query(mysql_db, query)) {
+		xfree(query);
+		return SLURM_ERROR;
+	}
+	xfree(query);
+	
+	END_TIMER2("make table current");
+	return SLURM_SUCCESS;
+}
+
+static int _create_db(char *db_name, mysql_db_info_t *db_info)
+{
+	char create_line[50];
+	MYSQL *mysql_db = NULL;
+
+//	slurm_mutex_lock(&mysql_lock);
+	if(!(mysql_db = mysql_init(mysql_db)))
+		fatal("mysql_init failed: %s", mysql_error(mysql_db));
+	
+	if(mysql_real_connect(mysql_db, db_info->host, db_info->user,
+			      db_info->pass, NULL, db_info->port, NULL, 0)) {
+		snprintf(create_line, sizeof(create_line),
+			 "create database %s", db_name);
+		if(mysql_query(mysql_db, create_line)) {
+			fatal("mysql_real_query failed: %d %s\n%s",
+			      mysql_errno(mysql_db),
+			      mysql_error(mysql_db), create_line);
+		}
+		mysql_close_db_connection(&mysql_db);
+	} else {
+		info("Connection failed to host = %s "
+		     "user = %s pass = %s port = %u",
+		     db_info->host, db_info->user,
+		     db_info->pass, db_info->port);
+		slurm_mutex_unlock(&mysql_lock);
+		fatal("mysql_real_connect failed: %d %s\n",
+		      mysql_errno(mysql_db),
+		      mysql_error(mysql_db));
+	}
+//	slurm_mutex_unlock(&mysql_lock);
+	return SLURM_SUCCESS;
+}
+
+extern int *destroy_mysql_db_info(mysql_db_info_t *db_info)
+{
+	if(db_info) {
+		xfree(db_info->host);
+		xfree(db_info->user);
+		xfree(db_info->pass);
+		xfree(db_info);
+	}
+	return SLURM_SUCCESS;
+}
+
+extern int mysql_get_db_connection(MYSQL **mysql_db, char *db_name,
+				   mysql_db_info_t *db_info)
+{
+	int rc = SLURM_SUCCESS;
+	bool storage_init = false;
+
+	if(!(*mysql_db = mysql_init(*mysql_db)))
+		fatal("mysql_init failed: %s", mysql_error(*mysql_db));
+	else {
+#ifdef MYSQL_OPT_RECONNECT
+{
+		my_bool reconnect = 1;
+		/* make sure reconnect is on */
+		mysql_options(*mysql_db, MYSQL_OPT_RECONNECT, &reconnect);
+}
+#endif
+		while(!storage_init) {
+			if(!mysql_real_connect(*mysql_db, db_info->host,
+					       db_info->user, db_info->pass,
+					       db_name, db_info->port,
+					       NULL, CLIENT_MULTI_STATEMENTS)) {
+				if(mysql_errno(*mysql_db) == ER_BAD_DB_ERROR) {
+					debug("Database %s not created.  "
+					      "Creating", db_name);
+					_create_db(db_name, db_info);
+				} else {
+					fatal("mysql_real_connect failed: "
+					      "%d %s",
+					      mysql_errno(*mysql_db),
+					      mysql_error(*mysql_db));
+				}
+			} else {
+				storage_init = true;
+			}
+		}
+	}
+	return rc;
+}
+
+extern int mysql_close_db_connection(MYSQL **mysql_db)
+{
+	if(mysql_db && *mysql_db) {
+		if(mysql_thread_safe())
+			mysql_thread_end();
+		mysql_close(*mysql_db);
+		*mysql_db = NULL;
+	}
+
+	return SLURM_SUCCESS;
+}
+
+extern int mysql_cleanup()
+{
+	debug3("starting mysql cleaning up");
+
+#ifdef mysql_library_end
+	mysql_library_end();
+#else
+	mysql_server_end();
+#endif
+
+	debug3("finished mysql cleaning up");
+	return SLURM_SUCCESS;
+}
+
+extern int mysql_db_query(MYSQL *mysql_db, char *query)
+{
+	if(!mysql_db)
+		fatal("You haven't inited this storage yet.");
+	slurm_mutex_lock(&mysql_lock);
+
+	/* clear out the old results so we don't get a 2014 error */
+	_clear_results(mysql_db);		
+//try_again:
+	if(mysql_query(mysql_db, query)) {
+		/* if(mysql_errno(mysql_db) == CR_SERVER_GONE_ERROR) { */
+/* 			/\* FIX ME: this means the connection went away *\/ */
+/* 		} */
+
+		error("mysql_query failed: %d %s\n%s",
+		      mysql_errno(mysql_db),
+		      mysql_error(mysql_db), query);
+		errno = mysql_errno(mysql_db);
+		slurm_mutex_unlock(&mysql_lock);
+		return SLURM_ERROR;
+	}
+	slurm_mutex_unlock(&mysql_lock);
+
+	return SLURM_SUCCESS;
+}
+
+extern int mysql_db_ping(MYSQL *mysql_db)
+{
+	/* clear out the old results so we don't get a 2014 error */
+	_clear_results(mysql_db);		
+	return mysql_ping(mysql_db);
+}
+
+extern int mysql_db_commit(MYSQL *mysql_db)
+{
+	//slurm_mutex_lock(&mysql_lock);
+
+	/* clear out the old results so we don't get a 2014 error */
+	_clear_results(mysql_db);		
+	if(mysql_commit(mysql_db)) {
+		error("mysql_commit failed: %d %s",
+		      mysql_errno(mysql_db),
+		      mysql_error(mysql_db));
+		errno = mysql_errno(mysql_db);
+		//slurm_mutex_unlock(&mysql_lock);
+		return SLURM_ERROR;
+	}
+	//slurm_mutex_unlock(&mysql_lock);
+
+	return SLURM_SUCCESS;
+}
+
+extern int mysql_db_rollback(MYSQL *mysql_db)
+{
+	//slurm_mutex_lock(&mysql_lock);
+
+	/* clear out the old results so we don't get a 2014 error */
+	_clear_results(mysql_db);		
+	if(mysql_rollback(mysql_db)) {
+		error("mysql_commit failed: %d %s",
+		      mysql_errno(mysql_db),
+		      mysql_error(mysql_db));
+		errno = mysql_errno(mysql_db);
+		//slurm_mutex_unlock(&mysql_lock);
+		return SLURM_ERROR;
+	}
+	//mysql_db_query(mysql_db, "unlock tables;");
+	//slurm_mutex_unlock(&mysql_lock);
+
+	return SLURM_SUCCESS;
+
+}
+
+extern MYSQL_RES *mysql_db_query_ret(MYSQL *mysql_db, char *query, bool last)
+{
+	MYSQL_RES *result = NULL;
+	
+	if(mysql_db_query(mysql_db, query) != SLURM_ERROR)  {
+		if(last)
+			result = _get_last_result(mysql_db);
+		else
+			result = _get_first_result(mysql_db);
+		if(!result && mysql_field_count(mysql_db)) {
+			/* should have returned data */
+			error("We should have gotten a result: %s", 
+			      mysql_error(mysql_db));
+		}
+	}
+
+	return result;
+}
+
+extern int mysql_insert_ret_id(MYSQL *mysql_db, char *query)
+{
+	int new_id = 0;
+	
+	if(mysql_db_query(mysql_db, query) != SLURM_ERROR)  {
+		new_id = mysql_insert_id(mysql_db);
+		if(!new_id) {
+			/* should have new id */
+			error("We should have gotten a new id: %s", 
+			      mysql_error(mysql_db));
+		}
+	}
+
+	return new_id;
+	
+}
+
+extern int mysql_db_create_table(MYSQL *mysql_db, char *table_name,
+				 storage_field_t *fields, char *ending)
+{
+	char *query = NULL;
+	int i = 0;
+	storage_field_t *first_field = fields;
+
+	if(!fields || !fields->name) {
+		error("Not creating an empty table");
+		return SLURM_ERROR;
+	}
+
+	query = xstrdup_printf("create table if not exists %s (%s %s",
+			       table_name, fields->name, fields->options);
+	i=1;
+	fields++;
+		
+	while(fields && fields->name) {
+		xstrfmtcat(query, ", %s %s", fields->name, fields->options);
+		fields++;
+		i++;
+	}
+	xstrcat(query, ending);
+
+	/* make sure we can do a rollback */
+	xstrcat(query, " engine='innodb'");
+
+	if(mysql_db_query(mysql_db, query) == SLURM_ERROR) {
+		xfree(query);
+		return SLURM_ERROR;
+	}
+	xfree(query);	
+	
+	return _mysql_make_table_current(mysql_db, table_name, first_field);
+}
+
+
+#endif
+
