cdafbd6a05f069fe53614ded8c6d7bbb3346d7be
Moe Jette <jette1@llnl.gov>
2008-07-01 18:11:10 +0000

    Log to job's output when it is cancelled or reaches it's time limit (ported     from existing code in slurm v1.3).

--- branches/slurm-1.2/src/slurmd/slurmstepd/req.c
+++ branches/slurm-1.2/src/slurmd/slurmstepd/req.c
@@ -711,6 +711,26 @@ _handle_signal_container(int fd, slurmd_job_t *job, uid_t uid)
 		goto done;
 	}
 
+	if (job->nodeid == 0) {
+		static int msg_sent = 0;
+		char *entity;
+		if (job->stepid == SLURM_BATCH_SCRIPT)
+			entity = "JOB";
+		else
+			entity = "STEP";
+		/* Not really errors,
+		 * but we want messages displayed by default */
+		if (msg_sent)
+			;
+		else if (sig == SIGXCPU) {
+			error("*** %s CANCELLED DUE TO TIME LIMIT ***", entity);
+			msg_sent = 1;
+		} else if ((sig == SIGTERM) || (sig == SIGKILL)) {
+			error("*** %s CANCELLED ***", entity);
+			msg_sent = 1;
+		}
+	}
+
 	/*
 	 * Signal the container
 	 */
