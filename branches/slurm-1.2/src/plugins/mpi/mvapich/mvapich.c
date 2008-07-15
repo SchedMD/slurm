13f19cc52d0e2a78e8dfa6fbcb7cb492a8108a6b
Mark Grondona <mgrondona@llnl.gov>
2008-07-15 22:32:08 +0000

    Fix handling of EINTR and EAGAIN in calls to poll(2). Fixes errors of the form:
     srun: error: mvapich: poll: Interrupted system call
     srun: error: mvapich: Fatal error. Killing job
    
    esp. when running srun under a debugger.

--- branches/slurm-1.2/src/plugins/mpi/mvapich/mvapich.c
+++ branches/slurm-1.2/src/plugins/mpi/mvapich/mvapich.c
@@ -408,6 +408,20 @@ static void mvapich_poll_destroy (struct mvapich_poll *mp)
 	xfree (mp);
 }
 
+
+/*
+ *  Call poll(2) on mvapich_poll object, handling EAGAIN and EINTR errors.
+ */
+static int mvapich_poll_internal (struct mvapich_poll *mp)
+{
+	int n;
+	while ((n = poll (mp->fds, mp->nfds, startup_timeout (mp->st))) < 0) {
+		if (errno != EINTR && errno != EAGAIN)
+			return (-1);
+	}
+	return (n);
+}
+
 /*
  *  Poll for next available mvapich_info object with read/write activity
  * 
@@ -457,7 +471,7 @@ again:
 
 		mvapich_debug3 ("mvapich_poll_next (nfds=%d, timeout=%d)\n", 
 				mp->nfds, startup_timeout (st));
-		if ((rc = poll (mp->fds, mp->nfds, startup_timeout (st))) < 0)
+		if ((rc = mvapich_poll_internal (mp)) < 0)
 			mvapich_terminate_job (st, "mvapich_poll_next: %m");
 		else if (rc == 0) {
 			/*
@@ -1284,8 +1298,9 @@ static int mvapich_abort_accept (mvapich_state_t *st)
 			mvapich_abort_timeout ());
 
 	while ((rc = poll (pfds, 1, mvapich_abort_timeout ())) < 0) {
-		if (errno != EINTR)
-			return (-1);
+		if (errno == EINTR || errno == EAGAIN)
+			continue;
+		return (-1);
 	}
 
 	/* 
@@ -1689,11 +1704,13 @@ mvapich_initialize_connections (mvapich_state_t *st,
 
 		mvapich_debug3 ("do_poll (nfds=%d)\n", nfds);
 
-		if ((rc = poll (fds, nfds, startup_timeout (st))) < 0) {
+		while ((rc = poll (fds, nfds, startup_timeout (st))) < 0) {
+			if (errno == EINTR || errno == EAGAIN)
+				continue;
 			error ("mvapich: poll: %m");
 			break;
 		}
-		else if (rc == 0) {
+		if (rc == 0) {
 			report_absent_tasks (st, 1);
 			mvapich_terminate_job (st, NULL);
 		}
