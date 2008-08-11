fd6d03a2ba2e1c879ba8da5f3d427984800dbaef
Moe Jette <jette1@llnl.gov>
2008-08-11 20:57:08 +0000

    Move slurm v1.3 work into a branch

---
+++ branches/slurm-1.3/contribs/perlapi/libslurm-perl/msg.h
@@ -0,0 +1,217 @@
+/*
+ * msg2hv.h - prototypes of msg-hv converting functions 
+ */
+
+#ifndef _MSG_H
+#define _MSG_H
+
+#include <perl.h>
+
+typedef char* charp;
+
+/*
+ * store an uint16_t into AV 
+ */
+inline static int av_store_uint16_t(AV* av, int index, uint16_t val)
+{
+	SV* sv = newSVuv(val);
+	
+	if (av_store(av, (I32)index, sv) == NULL) {
+		SvREFCNT_dec(sv);
+		return -1;
+	}
+	return 0;
+}
+
+/*
+ * store an uint32_t into AV 
+ */
+inline static int av_store_uint32_t(AV* av, int index, uint32_t val)
+{
+	SV* sv = newSVuv(val);
+	
+	if (av_store(av, (I32)index, sv) == NULL) {
+		SvREFCNT_dec(sv);
+		return -1;
+	}
+	return 0;
+}
+
+
+/*
+ * store an int into AV 
+ */
+inline static int av_store_int(AV* av, int index, int val)
+{
+	SV* sv = newSViv(val);
+	
+	if (av_store(av, (I32)index, sv) == NULL) {
+		SvREFCNT_dec(sv);
+		return -1;
+	}
+	return 0;
+}
+
+/*
+ * store a string into HV 
+ */
+inline static int hv_store_charp(HV* hv, const char *key, charp val)
+{
+	SV* sv = NULL;
+
+	if(val)
+		sv = newSVpv(val, 0);
+
+	if (!key || hv_store(hv, key, (I32)strlen(key), sv, 0) == NULL) {
+		SvREFCNT_dec(sv);
+		return -1;
+	}
+	return 0;
+}
+
+/*
+ * store an unsigned 32b int into HV 
+ */
+inline static int hv_store_uint32_t(HV* hv, const char *key, uint32_t val)
+{
+	SV* sv = newSVuv(val);
+	
+	if (!key || hv_store(hv, key, (I32)strlen(key), sv, 0) == NULL) {
+		SvREFCNT_dec(sv);
+		return -1;
+	}
+	return 0;
+}
+
+/*
+ * store an unsigned 16b int into HV 
+ */
+inline static int hv_store_uint16_t(HV* hv, const char *key, uint16_t val)
+{
+	SV* sv = newSVuv(val);
+	
+	if (!key || hv_store(hv, key, (I32)strlen(key), sv, 0) == NULL) {
+		SvREFCNT_dec(sv);
+		return -1;
+	}
+	return 0;
+}
+
+/*
+ * store an unsigned 8b int into HV 
+ */
+inline static int hv_store_uint8_t(HV* hv, const char *key, uint8_t val)
+{
+	SV* sv = newSVuv(val);
+	
+	if (!key || hv_store(hv, key, (I32)strlen(key), sv, 0) == NULL) {
+		SvREFCNT_dec(sv);
+		return -1;
+	}
+	return 0;
+}
+/*
+ * store a signed int into HV 
+ */
+inline static int hv_store_int(HV* hv, const char *key, int val)
+{
+	SV* sv = newSViv(val);
+	
+	if (!key || hv_store(hv, key, (I32)strlen(key), sv, 0) == NULL) {
+		SvREFCNT_dec(sv);
+		return -1;
+	}
+	return 0;
+}
+
+/*
+ * store a bool into HV 
+ */
+inline static int hv_store_bool(HV* hv, const char *key, bool val)
+{
+	if (!key || hv_store(hv, key, (I32)strlen(key), (val ? &PL_sv_yes : &PL_sv_no), 0) == NULL) {
+		return -1;
+	}
+	return 0;
+}
+
+/*
+ * store a time_t into HV 
+ */
+inline static int hv_store_time_t(HV* hv, const char *key, time_t val)
+{
+	SV* sv = newSVuv(val);
+	
+	if (!key || hv_store(hv, key, (I32)strlen(key), sv, 0) == NULL) {
+		SvREFCNT_dec(sv);
+		return -1;
+	}
+	return 0;
+}
+
+/*
+ * store a SV into HV 
+ */
+inline static int hv_store_sv(HV* hv, const char *key, SV* sv)
+{
+	if (!key || hv_store(hv, key, (I32)strlen(key), sv, 0) == NULL) {
+		return -1;
+	}
+	return 0;
+}
+
+#define SV2uint32_t(sv) SvUV(sv)
+#define SV2uint16_t(sv) SvUV(sv)
+#define SV2uint8_t(sv)  SvUV(sv)
+#define SV2time_t(sv)   SvUV(sv)
+#define SV2charp(sv)    SvPV_nolen(sv)
+#define SV2bool(sv)     SvTRUE(sv)
+
+
+#define FETCH_FIELD(hv, ptr, field, type, required) \
+	do { \
+		SV** svp; \
+		if ( (svp = hv_fetch (hv, #field, strlen(#field), FALSE)) ) { \
+			ptr->field = (type) (SV2##type (*svp)); \
+		} else if (required) { \
+			Perl_warn (aTHX_ "Required field \"" #field "\" missing in HV"); \
+			return -1; \
+		} \
+	} while (0)
+
+#define STORE_FIELD(hv, ptr, field, type) \
+	do { \
+		if (hv_store_##type(hv, #field, ptr->field)) { \
+			Perl_warn (aTHX_ "Failed to store field \"" #field "\""); \
+			return -1; \
+		} \
+	} while (0)
+
+
+extern int hv_to_job_desc_msg(HV* hv, job_desc_msg_t* job_desc_msg);
+extern void free_job_desc_msg_memory(job_desc_msg_t *msg);
+extern int resource_allocation_response_msg_to_hv(resource_allocation_response_msg_t* resp_msg, HV* hv);
+extern int job_alloc_info_response_msg_to_hv(job_alloc_info_response_msg_t *resp_msg, HV* hv);
+extern int submit_response_msg_to_hv(submit_response_msg_t *resp_msg, HV* hv);
+
+extern int job_info_msg_to_hv(job_info_msg_t* job_info_msg, HV* hv);
+extern int job_step_info_response_msg_to_hv(job_step_info_response_msg_t* job_step_info_msg, HV* hv);
+extern int slurm_step_layout_to_hv(slurm_step_layout_t* step_layout, HV* hv);
+
+extern int node_info_msg_to_hv(node_info_msg_t* node_info_msg, HV* hv);
+extern int hv_to_update_node_msg(HV* hv, update_node_msg_t *update_msg);
+
+extern int partition_info_msg_to_hv(partition_info_msg_t* part_info_msg, HV* hv);
+extern int hv_to_update_part_msg(HV* hv, update_part_msg_t* part_msg);
+
+extern int slurm_ctl_conf_to_hv(slurm_ctl_conf_t* conf, HV* hv);
+
+extern int trigger_info_to_hv(trigger_info_t *info, HV* hv);
+extern int trigger_info_msg_to_hv(trigger_info_msg_t *msg, HV* hv);
+extern int hv_to_trigger_info(HV* hv, trigger_info_t* info);
+
+extern int hv_to_slurm_step_ctx_params(HV* hv, slurm_step_ctx_params_t* params);
+extern int hv_to_slurm_step_launch_params(HV* hv, slurm_step_launch_params_t *params);
+extern void free_slurm_step_launch_params_memory(slurm_step_launch_params_t *params);
+
+#endif /* _MSG_H */
---
+++ branches/slurm-1.3/src/sstat/sstat.h
@@ -0,0 +1,119 @@
+/*****************************************************************************\
+ *  sstat.h - header file for sstat
+ *
+ *  $Id: sstat.h 7541 2006-03-18 01:44:58Z da $
+ *****************************************************************************
+ *  Copyright (C) 2002-2007 The Regents of the University of California.
+ *  Copyright (C) 2008 Lawrence Livermore National Security.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Morris Jette <jette1@llnl.gov>
+ *  LLNL-CODE-402394.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://www.llnl.gov/linux/slurm/>.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+#ifndef _SSTAT_H
+#define _SSTAT_H
+
+#include <ctype.h>
+#include <errno.h>
+#include <grp.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "src/common/getopt.h"
+#include "src/common/xmalloc.h"
+#include "src/common/xstring.h"
+#include "src/common/list.h"
+#include "src/common/hostlist.h"
+#include "src/common/slurm_jobacct_gather.h"
+#include "src/common/slurm_accounting_storage.h"
+#include "src/common/slurm_jobcomp.h"
+
+#define ERROR 2
+
+#define STAT_FIELDS "jobid,vsize,rss,pages,cputime,ntasks,state"
+
+#define BUFFER_SIZE 4096
+#define STATE_COUNT 10
+
+#define MAX_PRINTFIELDS 100
+
+#define SECONDS_IN_MINUTE 60
+#define SECONDS_IN_HOUR (60*SECONDS_IN_MINUTE)
+#define SECONDS_IN_DAY (24*SECONDS_IN_HOUR)
+
+/* On output, use fields 12-37 from JOB_STEP */
+
+typedef enum {	HEADLINE,
+		UNDERSCORE,
+		JOB,
+		JOBSTEP,
+		JOBCOMP
+} type_t;
+
+
+typedef struct fields {
+	char *name;		/* Specified in --fields= */
+	void (*print_routine) ();	/* Who gets to print it? */
+} fields_t;
+
+extern fields_t fields[];
+extern sacct_parameters_t params;
+
+extern List jobs;
+
+extern int printfields[MAX_PRINTFIELDS],	/* Indexed into fields[] */
+	nprintfields;
+
+/* process.c */
+void find_hostname(uint32_t pos, char *hosts, char *host);
+void aggregate_sacct(sacct_t *dest, sacct_t *from);
+
+/* print.c */
+void print_cputime(type_t type, void *object);
+void print_fields(type_t type, void *object);
+void print_jobid(type_t type, void *object);
+void print_ntasks(type_t type, void *object);
+void print_pages(type_t type, void *object);
+void print_rss(type_t type, void *object);
+void print_state(type_t type, void *object);
+void print_vsize(type_t type, void *object);
+
+
+/* options.c */
+void parse_command_line(int argc, char **argv);
+
+#endif /* !_SACCT_H */
