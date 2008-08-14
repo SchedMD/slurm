11f1b8f38c8e819475d1d1241e42bf9a568d7437
Danny Auble <da@llnl.gov>
2008-08-14 18:24:35 +0000

    added security for slurmdbd

--- branches/slurm-1.3/src/common/slurmdbd_defs.c
+++ branches/slurm-1.3/src/common/slurmdbd_defs.c
@@ -2204,6 +2204,13 @@ slurmdbd_unpack_init_msg(dbd_init_msg_t **msg, Buf buffer, char *auth_info)
 		goto unpack_error;
 	}
 	msg_ptr->uid = g_slurm_auth_get_uid(auth_cred, auth_info);
+	if(g_slurm_auth_errno(auth_cred) != SLURM_SUCCESS) {
+		
+		error("Bad authentication: %s",
+		      g_slurm_auth_errstr(g_slurm_auth_errno(auth_cred)));
+		goto unpack_error;
+	}
+	info("got %d for the uid", msg_ptr->uid);
 	g_slurm_auth_destroy(auth_cred);
 	return SLURM_SUCCESS;
 
