25e78ce49cab121b43511e93c7a4a0101525f2f0
Moe Jette <jette1@llnl.gov>
2008-08-22 22:45:40 +0000

    svn merge -r14810:14870 https://eris.llnl.gov/svn/slurm/branches/slurm-1.2

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -2998,19 +2998,6 @@ static void _job_timed_out(struct job_record *job_ptr)
 static int _validate_job_desc(job_desc_msg_t * job_desc_msg, int allocate, 
 			      uid_t submit_uid)
 {
-	/* Permit normal user to specify job id only for sched/wiki 
-	 * (Maui scheduler). This was also required with earlier
-	 * versions of the Moab scheduler (wiki2), but was fixed 
-	 * in early 2007 to submit jobs as user root */
-	if (!wiki_sched_test) {
-		char *sched_type = slurm_get_sched_type();
-		if ((strcmp(sched_type, "sched/wiki") == 0) ||
-		    (strcmp(sched_type, "sched/wiki2") == 0))
-			wiki_sched = true;
-		xfree(sched_type);
-		wiki_sched_test = true;
-	}
-
 	if ((job_desc_msg->num_procs == NO_VAL)
 	    &&  (job_desc_msg->min_nodes == NO_VAL)
 	    &&  (job_desc_msg->req_nodes == NULL)) {
@@ -3045,7 +3032,7 @@ static int _validate_job_desc(job_desc_msg_t * job_desc_msg, int allocate,
 
 	if (job_desc_msg->job_id != NO_VAL) {
 		struct job_record *dup_job_ptr;
-		if ((submit_uid != 0) && (!wiki_sched) &&
+		if ((submit_uid != 0) && 
 		    (submit_uid != slurmctld_conf.slurm_user_id)) {
 			info("attempt by uid %u to set job_id", submit_uid);
 			return ESLURM_INVALID_JOB_ID;
