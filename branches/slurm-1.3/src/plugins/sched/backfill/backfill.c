a984a4e615744b79d779e3186905fb4ea90180f3
Joseph P. Donaghy <donaghy1@llnl.gov>
2008-08-27 16:39:20 +0000

    fixed issue with backfill going crazy with no sleep

--- branches/slurm-1.3/src/plugins/sched/backfill/backfill.c
+++ branches/slurm-1.3/src/plugins/sched/backfill/backfill.c
@@ -177,6 +177,14 @@ extern void *backfill_agent(void *args)
 		READ_LOCK, WRITE_LOCK, WRITE_LOCK, READ_LOCK };
 
 	while (!stop_backfill) {
+		iter = (BACKFILL_CHECK_SEC * 1000000) /
+		       STOP_CHECK_USEC;
+		for (i=0; ((i<iter) && (!stop_backfill)); i++) {
+			/* test stop_backfill every 0.1 sec for
+			 * 2.0 secs to avoid running continuously */
+			usleep(STOP_CHECK_USEC);
+		}
+		
 		now = time(NULL);
 		/* Avoid resource fragmentation if important */
 		if (switch_no_frag() && job_is_completing())
@@ -195,13 +203,6 @@ extern void *backfill_agent(void *args)
 #if __DEBUG
 		info("backfill: completed, %s", tv_str);
 #endif
-		iter = (BACKFILL_CHECK_SEC * 1000000) /
-		       STOP_CHECK_USEC;
-		for (i=0; ((i<iter) && (!stop_backfill)); i++) {
-			/* test stop_backfill every 0.1 sec for
-			 * 2.0 secs to avoid running continuously */
-			usleep(STOP_CHECK_USEC);
-		}
 	}
 	return NULL;
 }
