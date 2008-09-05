eab85eeae73115718e41993c2bbe35ade96a7c65
Moe Jette <jette1@llnl.gov>
2008-09-05 22:45:54 +0000

    Make default CryptoType=crypto/munge

--- src/common/read_config.h
+++ src/common/read_config.h
@@ -55,7 +55,7 @@ extern char *default_plugstack;
 #define DEFAULT_AUTH_TYPE          "auth/none"
 #define DEFAULT_CACHE_GROUPS        0
 #define DEFAULT_COMPLETE_WAIT       0
-#define DEFAULT_CRYPTO_TYPE        "crypto/openssl"
+#define DEFAULT_CRYPTO_TYPE        "crypto/munge"
 #define DEFAULT_EPILOG_MSG_TIME     2000
 #define DEFAULT_FAST_SCHEDULE       1
 #define DEFAULT_FIRST_JOB_ID        1
