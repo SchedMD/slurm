6150436118640d896f409c8a48815db69ea3508f
Moe Jette <jette1@llnl.gov>
2008-09-16 21:27:17 +0000

    Set ENVIRONMENT=BATCH before launching /bin/su to get user env

--- src/common/env.c
+++ src/common/env.c
@@ -1405,6 +1405,7 @@ char **env_array_user_default(const char *username, int timeout, int mode)
 		return NULL;
 	}
 	if (child == 0) {
+		setenv("ENVIRONMENT", "BATCH", 1);
 		setpgid(0, 0);
 		close(0);
 		open("/dev/null", O_RDONLY);
