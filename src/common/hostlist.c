3e63f557324ca182ee4d06fca1767e716a1adfe7
Moe Jette <jette1@llnl.gov>
2008-10-14 21:01:07 +0000

    Rewrite of hostrange_hn_within(), avoids reporting "tst0" in the hostlist       "tst".

--- src/common/hostlist.c
+++ src/common/hostlist.c
@@ -1016,19 +1016,48 @@ static hostrange_t hostrange_intersect(hostrange_t h1, hostrange_t h2)
  */
 static int hostrange_hn_within(hostrange_t hr, hostname_t hn)
 {
-	int retval = 0;
+	if (hr->singlehost) {
+		/*  
+		 *  If the current hostrange [hr] is a `singlehost' (no valid 
+		 *   numeric suffix (lo and hi)), then the hostrange [hr]
+		 *   stores just one host with name == hr->prefix.
+		 *  
+		 *  Thus the full hostname in [hn] must match hr->prefix, in
+		 *   which case we return true. Otherwise, there is no 
+		 *   possibility that [hn] matches [hr].
+		 */
+		if (strcmp (hn->hostname, hr->prefix) == 0)
+			return 1;
+		else 
+			return 0;
+	}
 
-	if (strcmp(hr->prefix, hn->prefix) == 0) {
-		if (!hostname_suffix_is_valid(hn)) {
-			if (hr->singlehost)
-			retval = 1;
-		} else if (hn->num <= hr->hi && hn->num >= hr->lo) {
+	/*
+	 *  Now we know [hr] is not a "singlehost", so hostname
+	 *   better have a valid numeric suffix, or there is no 
+	 *   way we can match
+	 */
+	if (!hostname_suffix_is_valid (hn))
+		return 0;
+
+	/*
+	 *  If hostrange and hostname prefixes don't match, then
+	 *   there is way the hostname falls within the range [hr].
+	 */
+	if (strcmp(hr->prefix, hn->prefix) != 0) 
+		return 0;
+
+	/*
+	 *  Finally, check whether [hn], with a valid numeric suffix,
+	 *   falls within the range of [hr].
+	 */
+	if (hn->num <= hr->hi && hn->num >= hr->lo) {
 			int width = hostname_suffix_width(hn);
 			int num = hn->num;
-			retval = _width_equiv(hr->lo, &hr->width, num, &width);
-		}
+		return (_width_equiv(hr->lo, &hr->width, num, &width));
 	}
-	return retval;
+
+	return 0;
 }
 
 
