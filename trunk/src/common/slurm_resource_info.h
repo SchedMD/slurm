48fb8b52b79587034bf363a96e2caffa81a96a2b
Moe Jette <jette1@llnl.gov>
2008-10-31 23:11:23 +0000

    No change in logic, just moved the --cpu_bind and --mem_bind parsing logic from srun/opt.c to common/slurm_resource_info.c so that it can be used by sbatch and salloc.

--- trunk/src/common/slurm_resource_info.h
+++ trunk/src/common/slurm_resource_info.h
@@ -66,7 +66,15 @@ int slurm_get_avail_procs(const uint16_t mxsockets,
 			  const select_type_plugin_info_t cr_type,
 			  uint32_t job_id, char *name);
 
+void slurm_print_cpu_bind_help(void);
+void slurm_print_mem_bind_help(void);
+
 void slurm_sprint_cpu_bind_type(char *str, cpu_bind_type_t cpu_bind_type);
 void slurm_sprint_mem_bind_type(char *str, mem_bind_type_t mem_bind_type);
+
+int slurm_verify_cpu_bind(const char *arg, char **cpu_bind, 
+			  cpu_bind_type_t *flags);
+int slurm_verify_mem_bind(const char *arg, char **mem_bind, 
+			  mem_bind_type_t *flags);
 
 #endif /* !_RES_INFO_H */
