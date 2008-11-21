8de0a8599061dbd957ee0fde5f9f36c2df394489
Danny Auble <da@llnl.gov>
2008-11-21 21:19:01 +0000

    ok I think I have sacctmgr parsing += -= = correctly now

--- branches/slurm-1.3/src/sacctmgr/txn_functions.c
+++ branches/slurm-1.3/src/sacctmgr/txn_functions.c
@@ -47,13 +47,19 @@ static int _set_cond(int *start, int argc, char *argv[],
 	int i, end = 0;
 	int set = 0;
 	int command_len = 0;
+	int option = 0;
 
 	for (i=(*start); i<argc; i++) {
 		end = parse_option_end(argv[i]);
 		if(!end)
 			command_len=strlen(argv[i]);
-		else
+		else {
 			command_len=end-1;
+			if(argv[i][end] == '=') {
+				option = (int)argv[i][end-1];
+				end++;
+			}
+		}
 
 		if(!end && !strncasecmp(argv[i], "where",
 					MAX(command_len, 5))) {
