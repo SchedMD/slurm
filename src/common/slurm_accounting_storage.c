dd4f5dfb7c8b3c53a0a4caed0f4616e13adfe054
Danny Auble <da@llnl.gov>
2008-12-03 00:04:52 +0000

    svn merge -r15808:15810  https://eris.llnl.gov/svn/slurm/branches/slurm-1.3

--- src/common/slurm_accounting_storage.c
+++ src/common/slurm_accounting_storage.c
@@ -1854,6 +1854,7 @@ extern int unpack_acct_association_rec(void **object, uint16_t rpc_version,
 	*object = object_ptr;
 
 	init_acct_association_rec(object_ptr);
+
 	if (rpc_version >= 4) {
 		safe_unpack32(&count, buffer);
 		if(count != NO_VAL) {
