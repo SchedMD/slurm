441dc9c6754a6467734fa5ca28fd634ddaa745d5
Moe Jette <jette1@llnl.gov>
2008-12-12 17:56:30 +0000

    Improve logging for job step completion by providing a job/step_id on the failure.

--- src/slurmctld/proc_req.c
+++ src/slurmctld/proc_req.c
@@ -1870,10 +1870,7 @@ static void _slurm_rpc_step_complete(slurm_msg_t *msg)
 	rc = step_partial_comp(req, &rem, &step_rc);
 
 	if (rc || rem) {	/* some error or not totally done */
-		if (rc) {
-			info("step_partial_comp: %s",
-				slurm_strerror(rc));
-		}
+		/* Note: Error printed within step_partial_comp */
 		unlock_slurmctld(job_write_lock);
 		slurm_send_rc_msg(msg, rc);
 		if (!rc)	/* partition completion */
