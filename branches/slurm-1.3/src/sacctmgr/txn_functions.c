deeefecf37e2c35208e9494dff7e0cb8ebd46e70
Danny Auble <da@llnl.gov>
2008-12-12 21:04:09 +0000

    fixed length feature with sacctmgr and sreport and made it so the default key was used if none was set

--- branches/slurm-1.3/src/sacctmgr/txn_functions.c
+++ branches/slurm-1.3/src/sacctmgr/txn_functions.c
@@ -209,7 +209,15 @@ extern int sacctmgr_list_txn(int argc, char *argv[])
 	itr = list_iterator_create(format_list);
 	while((object = list_next(itr))) {
 		char *tmp_char = NULL;
-		int command_len = strlen(object);
+		int command_len = 0;
+		int newlen = 0;
+		
+		if((tmp_char = strstr(object, "\%"))) {
+			newlen = atoi(tmp_char+1);
+			tmp_char[0] = '\0';
+		} 
+
+		command_len = strlen(object);
 
 		field = xmalloc(sizeof(print_field_t));
 		if(!strncasecmp("Accounts", object, MAX(command_len, 3))) {
@@ -266,11 +274,10 @@ extern int sacctmgr_list_txn(int argc, char *argv[])
 			xfree(field);
 			continue;
 		}
-		if((tmp_char = strstr(object, "\%"))) {
-			int newlen = atoi(tmp_char+1);
-			if(newlen > 0) 
-				field->len = newlen;
-		}
+
+		if(newlen > 0) 
+			field->len = newlen;
+		
 		list_append(print_fields_list, field);		
 	}
 	list_iterator_destroy(itr);
