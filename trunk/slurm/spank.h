a36342c701de191d47c475558b67b658b6c771e8
Moe Jette <jette1@llnl.gov>
2009-02-27 19:20:01 +0000

    Add new allocator context to spank plugin framework PATCH 3/9 from Mark Grondona

--- trunk/slurm/spank.h
+++ trunk/slurm/spank.h
@@ -166,7 +166,8 @@ typedef enum spank_err spank_err_t;
 enum spank_context {
     S_CTX_ERROR,             /* Error obtaining current context              */
     S_CTX_LOCAL,             /* Local context (srun)                         */
-    S_CTX_REMOTE             /* Remote context (slurmd)                      */
+    S_CTX_REMOTE,            /* Remote context (slurmd)                      */
+    S_CTX_ALLOCATOR          /* Allocator context (sbatch/salloc)            */
 };
 
 typedef enum spank_context spank_context_t;
