1cab28fc13b78b68c0f9824c5404a3056dfe40db
Moe Jette <jette1@llnl.gov>
2009-04-02 21:22:36 +0000

    don't tread setuid/gid/initgroups calls as fatal error from trigger   program if the program is going to run as that user anyway.

--- branches/slurm-1.3/src/slurmctld/trigger_mgr.c
+++ branches/slurm-1.3/src/slurmctld/trigger_mgr.c
@@ -977,6 +977,8 @@ static void _trigger_run_program(trig_mgr_info_t *trig_in)
 		trig_in->group_id = child;
 	} else if (child == 0) {
 		int i;
+		bool run_as_self = (uid == getuid());
+
 		for (i=0; i<128; i++)
 			close(i);
 #ifdef SETPGRP_TWO_ARGS
@@ -985,15 +987,15 @@ static void _trigger_run_program(trig_mgr_info_t *trig_in)
 		setpgrp();
 #endif
 		setsid();
-		if (initgroups(user_name, gid) == -1) {
+		if ((initgroups(user_name, gid) == -1) && !run_as_self) {
 			error("trigger: initgroups: %m");
 			exit(1);
 		}
-		if (setgid(gid) == -1) {
+		if ((setgid(gid) == -1) && !run_as_self){
 			error("trigger: setgid: %m");
 			exit(1);
 		}
-		if (setuid(uid) == -1) {
+		if ((setuid(uid) == -1) && !run_as_self) {
 			error("trigger: setuid: %m");
 			exit(1);
 		}
