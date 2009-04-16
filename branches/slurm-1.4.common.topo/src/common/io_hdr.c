6e640231433ae537ebe17a5de88c6d178dec30ed
Moe Jette <jette1@llnl.gov>
2009-04-16 19:49:29 +0000

    create branch so the topology plugin can be moved to common and accessed from all commands  and daemons.

---
+++ branches/slurm-1.4.common.topo/src/common/io_hdr.c
@@ -0,0 +1,287 @@
+/*****************************************************************************\
+ * src/common/io_hdr.c - IO connection header functions
+ * $Id$
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Mark A. Grondona <mgrondona@llnl.gov>.
+ *  CODE-OCEC-09-009. All rights reserved.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://computing.llnl.gov/linux/slurm/>.
+ *  Please also read the included file: DISCLAIMER.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under 
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include "src/common/io_hdr.h"
+#include "src/common/slurm_protocol_defs.h"
+
+#define IO_PROTOCOL_VERSION 0xb001
+
+/*
+static void
+_print_data(char *data, int datalen)
+{
+	char buf[1024];
+	size_t len = 0;
+	int i;
+
+	for (i = 0; i < datalen; i += sizeof(char))
+		len += sprintf(buf+len, "%02x", data[i]);
+
+	info("data: %s", buf);
+}
+*/
+
+void
+io_hdr_pack(io_hdr_t *hdr, Buf buffer)
+{
+	pack16(hdr->type, buffer);
+	pack16(hdr->gtaskid, buffer);
+	pack16(hdr->ltaskid, buffer);
+	pack32(hdr->length, buffer);
+}
+
+int
+io_hdr_unpack(io_hdr_t *hdr, Buf buffer)
+{
+	safe_unpack16(&hdr->type, buffer);
+	safe_unpack16(&hdr->gtaskid, buffer);
+	safe_unpack16(&hdr->ltaskid, buffer);
+	safe_unpack32(&hdr->length, buffer);
+	return SLURM_SUCCESS;
+
+    unpack_error:
+	error("io_hdr_unpack error: %m");
+	return SLURM_ERROR;
+}
+
+int 
+io_hdr_packed_size()
+{
+	return sizeof(uint32_t) + 3*sizeof(uint16_t);
+}
+
+/*
+ * Only return when the all of the bytes have been read, or an unignorable
+ * error has occurred.
+ */
+static int _full_read(int fd, void *buf, size_t count)
+{
+	int n;
+	int left;
+	void *ptr;
+
+	left = count;
+	ptr = buf;
+	while (left > 0) {
+	again:
+		if ((n = read(fd, (void *) ptr, left)) < 0) {
+			if (errno == EINTR
+			    || errno == EAGAIN
+			    || errno == EWOULDBLOCK)
+				goto again;
+			debug3("Leaving  _full_read on error!");
+			return -1;
+		} else if (n == 0) { /* got eof */
+			debug3("  _full_read (_client_read) got eof");
+			return 0;
+		}
+		left -= n;
+		ptr += n;
+	}
+
+	return count;
+}
+
+/*
+ * Read and unpack an io_hdr_t from a file descriptor (socket).
+ */
+int io_hdr_read_fd(int fd, io_hdr_t *hdr)
+{
+	Buf buffer;
+	int n = 0;
+
+	debug3("Entering io_hdr_read_fd");
+	buffer = init_buf(io_hdr_packed_size());
+	n = _full_read(fd, buffer->head, io_hdr_packed_size());
+	if (n <= 0)
+		goto fail;
+	if (io_hdr_unpack(hdr, buffer) == SLURM_ERROR) {
+		n = -1;
+		goto fail;
+	}
+
+fail:
+	debug3("Leaving  io_hdr_read_fd");
+	free_buf(buffer);
+	return n;
+}
+
+
+
+
+int 
+io_init_msg_validate(struct slurm_io_init_msg *msg, const char *sig)
+{
+	debug2("Entering io_init_msg_validate");
+
+	debug3("  msg->version = %x", msg->version);
+	debug3("  msg->nodeid = %u", msg->nodeid);
+
+	if (msg->version != IO_PROTOCOL_VERSION) {
+		error("Invalid IO init header version");
+		return SLURM_ERROR;
+	}
+
+	if (memcmp((void *)sig, (void *)msg->cred_signature,
+		   SLURM_IO_KEY_SIZE)) {
+		error("Invalid IO init header signature");
+		return SLURM_ERROR;
+	}
+
+	debug2("Leaving  io_init_msg_validate");
+	return SLURM_SUCCESS;
+}
+
+
+static int
+io_init_msg_packed_size(void)
+{
+	int len;
+
+	len = sizeof(uint16_t)        /* version */
+		+ sizeof(uint32_t)    /* nodeid */
+		+ (SLURM_IO_KEY_SIZE + sizeof(uint32_t))  /* signature */
+		+ sizeof(uint32_t)    /* stdout_objs */
+		+ sizeof(uint32_t);   /* stderr_objs */
+	return len;
+}
+
+static void
+io_init_msg_pack(struct slurm_io_init_msg *hdr, Buf buffer)
+{
+	pack16(hdr->version, buffer);
+       	pack32(hdr->nodeid, buffer);
+	pack32(hdr->stdout_objs, buffer);
+	pack32(hdr->stderr_objs, buffer);
+	packmem((char *) hdr->cred_signature,
+		(uint32_t) SLURM_IO_KEY_SIZE, buffer);
+}
+
+
+static int
+io_init_msg_unpack(struct slurm_io_init_msg *hdr, Buf buffer)
+{
+	uint32_t val;
+	safe_unpack16(&hdr->version, buffer);
+	safe_unpack32(&hdr->nodeid, buffer);
+	safe_unpack32(&hdr->stdout_objs, buffer);
+	safe_unpack32(&hdr->stderr_objs, buffer);
+	safe_unpackmem((char *) hdr->cred_signature, &val, buffer);
+	if (val != SLURM_IO_KEY_SIZE)
+		goto unpack_error;
+
+	return SLURM_SUCCESS;
+
+    unpack_error:
+	error("unpack error in io_init_msg_unpack");
+	return SLURM_ERROR;
+}
+
+
+int
+io_init_msg_write_to_fd(int fd, struct slurm_io_init_msg *msg)
+{
+	Buf buf;
+	void *ptr;
+	int n;
+	
+	xassert(msg);
+
+	debug2("Entering io_init_msg_write_to_fd");
+	msg->version = IO_PROTOCOL_VERSION;
+	buf = init_buf(io_init_msg_packed_size());
+	debug2("  msg->nodeid = %d", msg->nodeid);
+	io_init_msg_pack(msg, buf);
+	
+	ptr = get_buf_data(buf);
+again:
+	if ((n = write(fd, ptr, io_init_msg_packed_size())) < 0) {
+		if (errno == EINTR)
+			goto again;
+		free_buf(buf);
+		return SLURM_ERROR;
+	}
+	if (n != io_init_msg_packed_size()) {
+		error("io init msg write too small");
+		free_buf(buf);
+		return SLURM_ERROR;
+	}
+
+	free_buf(buf);
+	debug2("Leaving  io_init_msg_write_to_fd");
+	return SLURM_SUCCESS;
+}
+
+
+int
+io_init_msg_read_from_fd(int fd, struct slurm_io_init_msg *msg)
+{
+	Buf buf;
+	void *ptr;
+	int n;
+	
+	xassert(msg);
+
+	debug2("Entering io_init_msg_read_from_fd");
+	buf = init_buf(io_init_msg_packed_size());
+	ptr = get_buf_data(buf);
+again:
+	if ((n = read(fd, ptr, io_init_msg_packed_size())) < 0) {
+		if (errno == EINTR)
+			goto again;
+		free_buf(buf);
+		return SLURM_ERROR;
+	}
+	if (n != io_init_msg_packed_size()) {
+		error("io init msg read too small");
+		free_buf(buf);
+		return SLURM_ERROR;
+	}
+	debug3("  read %d bytes", n);
+	io_init_msg_unpack(msg, buf);
+
+	free_buf(buf);
+
+	debug2("Leaving  io_init_msg_read_from_fd");
+	return SLURM_SUCCESS;
+}
---
+++ branches/slurm-1.4.common.topo/src/common/plugin.c
@@ -0,0 +1,311 @@
+/*****************************************************************************\
+ * plugin.h - plugin architecture implementation.
+ *****************************************************************************
+ *  Copyright (C) 2002-2007 The Regents of the University of California.
+ *  Copyright (C) 2008 Lawrence Livermore National Security.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Jay Windley <jwindley@lnxi.com>.
+ *  CODE-OCEC-09-009. All rights reserved.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://computing.llnl.gov/linux/slurm/>.
+ *  Please also read the included file: DISCLAIMER.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <errno.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <dlfcn.h>        /* don't know if there's an autoconf for this. */
+#include <string.h>
+
+#include "src/common/xmalloc.h"
+#include "src/common/log.h"
+#include "src/common/plugin.h"
+#include "src/common/xstring.h"
+#include "src/common/slurm_protocol_api.h"
+#include <slurm/slurm_errno.h>
+
+#  if HAVE_UNISTD_H
+#    include <unistd.h>
+#  endif /* HAVE_UNISTD_H */
+#  if HAVE_SYS_TYPES_H
+#    include <sys/types.h>
+#  endif
+#  if HAVE_SYS_STAT_H
+#    include <sys/stat.h>
+#  endif
+
+#  if HAVE_STDLIB_H
+#    include <stdlib.h>
+#  endif
+
+/* dlerror() on AIX sometimes fails, revert to strerror() as needed */
+static char *_dlerror(void)
+{
+	int error_code = errno;
+	char *rc = dlerror();
+
+	if ((rc == NULL) || (rc[0] == '\0'))
+		rc = strerror(error_code);
+
+	return rc;
+}
+
+int
+plugin_peek( const char *fq_path,
+			 char *plugin_type,
+			 const size_t type_len,
+			 uint32_t *plugin_version )
+{
+	plugin_handle_t plug;
+	char *type;
+	uint32_t *version;
+	
+	plug = dlopen( fq_path, RTLD_LAZY );
+	if ( plug == NULL ) {
+		debug3( "plugin_peek: dlopen(%s): %s", fq_path, _dlerror() );
+		return SLURM_ERROR;
+	}
+	if ( ( type = dlsym( plug, PLUGIN_TYPE ) ) != NULL ) {
+		if ( plugin_type != NULL ) {
+			strncpy( plugin_type, type, type_len );
+		}
+	} else {	
+		dlclose( plug );
+		/* could be vestigial library, don't treat as an error */
+		verbose( "%s: not a SLURM plugin", fq_path );
+		return SLURM_ERROR;
+	}
+	
+	if ( ( version = (uint32_t *) dlsym( plug, PLUGIN_VERSION ) ) != NULL ) {
+		if ( plugin_version != NULL ) {
+			*plugin_version = *version;
+		}
+	} else {
+		dlclose( plug );
+		/* could be vestigial library, don't treat as an error */
+		verbose( "%s: not a SLURM plugin", fq_path );
+		return SLURM_ERROR;
+	}
+
+	dlclose( plug );
+	return SLURM_SUCCESS;
+}
+
+plugin_handle_t
+plugin_load_from_file( const char *fq_path )
+{
+        plugin_handle_t plug;
+        int (*init)( void );
+        
+        /*
+         * Try to open the shared object.  
+	 *
+	 * Use RTLD_LAZY to allow plugins to use symbols that may be 
+	 * defined in only one slurm entity (e.g. srun and not slurmd),
+	 * when the use of that symbol is restricted to within the 
+	 * entity from which it is available. (i.e. srun symbols are only
+	 * used in the context of srun, not slurmd.)
+	 *
+         */
+        plug = dlopen( fq_path, RTLD_LAZY );
+        if ( plug == NULL ) {
+		error( "plugin_load_from_file: dlopen(%s): %s",
+			fq_path,
+			_dlerror() );
+                return PLUGIN_INVALID_HANDLE;
+        }
+	
+        /* Now see if our required symbols are defined. */
+        if ( ( dlsym( plug, PLUGIN_NAME ) == NULL ) ||
+             ( dlsym( plug, PLUGIN_TYPE ) == NULL ) ||
+             ( dlsym( plug, PLUGIN_VERSION ) == NULL ) ) {
+		debug( "plugin_load_from_file: invalid symbol");
+                /* slurm_seterrno( SLURM_PLUGIN_SYMBOLS ); */
+                return PLUGIN_INVALID_HANDLE;
+        }
+
+        /*
+         * Now call its init() function, if present.  If the function
+         * returns nonzero, unload the plugin and signal an error.
+         */
+        if ( ( init = dlsym( plug, "init" ) ) != NULL ) {
+                if ( (*init)() != 0 ) {
+			error( "plugin_load_from_file(%s): init() returned SLURM_ERROR", 
+				fq_path );
+                        (void) dlclose( plug );
+                        return PLUGIN_INVALID_HANDLE;
+                }
+        }
+        
+        return plug;
+}
+
+plugin_handle_t
+plugin_load_and_link(const char *type_name, int n_syms,
+		    const char *names[], void *ptrs[])
+{
+        plugin_handle_t plug = PLUGIN_INVALID_HANDLE;
+	struct stat st;
+	char *head=NULL, *dir_array=NULL, *so_name = NULL,
+		*file_name=NULL;
+	int i=0;
+	
+	if (!type_name)
+		return plug;
+
+	so_name = xstrdup_printf("%s.so", type_name);
+
+	while(so_name[i]) {
+		if(so_name[i] == '/')
+			so_name[i] = '_';
+		i++;
+	}
+	if(!(dir_array = slurm_get_plugin_dir())) {
+		error("plugin_load_and_link: No plugin dir given");
+		xfree(so_name);
+		return plug;
+	}
+	
+	head = dir_array;
+	for (i=0; ; i++) {
+		bool got_colon = 0;
+		if (dir_array[i] == ':') {
+			dir_array[i] = '\0';
+			got_colon = 1;
+		} else if(dir_array[i] != '\0') 
+			continue;
+		
+		file_name = xstrdup_printf("%s/%s", head, so_name);
+		debug3("Trying to load plugin %s", file_name);
+		if ((stat(file_name, &st) < 0) || (!S_ISREG(st.st_mode))) {
+			debug4("No Good.");
+			xfree(file_name);
+		} else {
+			plug = plugin_load_from_file(file_name);
+			xfree(file_name);
+			if (plugin_get_syms(plug, n_syms, names, ptrs) >= 
+			    n_syms) {
+				debug3("Success.");
+				break;
+			} else 
+				plug = PLUGIN_INVALID_HANDLE;
+		}
+
+		if (got_colon) {
+			head = dir_array + i + 1;
+		} else 
+			break;
+	}
+	
+	xfree(dir_array);
+	xfree(so_name);
+	return plug;
+}
+/*
+ * Must test plugin validity before doing dlopen() and dlsym()
+ * operations because some implementations of these functions
+ * crash if the library handle is not valid.
+ */
+
+void
+plugin_unload( plugin_handle_t plug )
+{
+        void (*fini)(void);
+        
+        if ( plug != PLUGIN_INVALID_HANDLE ) {
+                if ( ( fini = dlsym( plug, "fini" ) ) != NULL ) {
+                        (*fini)();
+                }
+                (void) dlclose( plug );
+        }
+}
+
+
+void *
+plugin_get_sym( plugin_handle_t plug, const char *name )
+{
+        if ( plug != PLUGIN_INVALID_HANDLE )
+                return dlsym( plug, name );
+        else
+                return NULL;
+}
+
+const char *
+plugin_get_name( plugin_handle_t plug )
+{
+        if ( plug != PLUGIN_INVALID_HANDLE )
+                return (const char *) dlsym( plug, PLUGIN_NAME );
+        else
+                return NULL;
+}
+
+const char *
+plugin_get_type( plugin_handle_t plug )
+{
+        if ( plug != PLUGIN_INVALID_HANDLE )
+                return (const char *) dlsym( plug, PLUGIN_TYPE );
+        else
+                return NULL;
+}
+
+uint32_t
+plugin_get_version( plugin_handle_t plug )
+{
+        uint32_t *ptr;
+
+        if ( plug == PLUGIN_INVALID_HANDLE ) return 0;        
+        ptr = (uint32_t *) dlsym( plug, PLUGIN_VERSION );
+        return ptr ? *ptr : 0;
+}
+
+int
+plugin_get_syms( plugin_handle_t plug,
+                 int n_syms,
+                 const char *names[],
+                 void *ptrs[] )
+{
+        int i, count;
+
+        count = 0;
+        for ( i = 0; i < n_syms; ++i ) {
+                ptrs[ i ] = dlsym( plug, names[ i ] );
+                if ( ptrs[ i ] ) 
+			++count;
+		else 
+			debug3("Couldn't find sym '%s' in the plugin",
+			       names[ i ]);
+	}
+
+        return count;
+}
+
