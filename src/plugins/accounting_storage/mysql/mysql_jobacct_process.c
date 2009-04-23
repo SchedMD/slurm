d67a68818cc70b2fa572d5149707e80fd12aa750
Danny Auble <da@llnl.gov>
2009-04-23 17:40:27 +0000

    fix for blank wckeys

--- src/plugins/accounting_storage/mysql/mysql_jobacct_process.c
+++ src/plugins/accounting_storage/mysql/mysql_jobacct_process.c
@@ -637,8 +637,11 @@ extern List mysql_jobacct_process_get_jobs(mysql_conn_t *mysql_conn, uid_t uid,
 		job->alloc_cpus = atoi(row[JOB_REQ_ALLOC_CPUS]);
 		job->associd = atoi(row[JOB_REQ_ASSOCID]);
 
-		if(row[JOB_REQ_WCKEY] && row[JOB_REQ_WCKEY][0])
+		/* we want a blank wckey if the name is null */
+		if(row[JOB_REQ_WCKEY])
 			job->wckey = xstrdup(row[JOB_REQ_WCKEY]);
+		else
+			job->wckey = xstrdup("");
 		job->wckeyid = atoi(row[JOB_REQ_WCKEYID]);
 
 		if(row[JOB_REQ_CLUSTER] && row[JOB_REQ_CLUSTER][0])
