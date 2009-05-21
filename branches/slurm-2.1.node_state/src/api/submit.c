0c1a4bbe962c0da61e79ef2fd71bdf07ed8d1b22
Moe Jette <jette1@llnl.gov>
2009-05-21 19:17:43 +0000

    create branch to use macros for node_state field

---
+++ branches/slurm-2.1.node_state/src/api/submit.c
@@ -0,0 +1,121 @@
+/*****************************************************************************\
+ *  submit.c - submit a job with supplied contraints
+ *  $Id$
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Morris Jette <jette1@llnl.gov>.
+ *  CODE-OCEC-09-009. All rights reserved.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://computing.llnl.gov/linux/slurm/>.
+ *  Please also read the included file: DISCLAIMER.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under 
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#ifndef __USE_XOPEN_EXTENDED
+extern pid_t getsid(pid_t pid);		/* missing from <unistd.h> */
+#endif
+
+#include <slurm/slurm.h>
+
+#include "src/common/read_config.h"
+#include "src/common/slurm_protocol_api.h"
+
+/*
+ * slurm_submit_batch_job - issue RPC to submit a job for later execution
+ * NOTE: free the response using slurm_free_submit_response_response_msg
+ * IN job_desc_msg - description of batch job request
+ * OUT slurm_alloc_msg - response to request
+ * RET 0 on success, otherwise return -1 and set errno to indicate the error
+ */
+int
+slurm_submit_batch_job (job_desc_msg_t *req, 
+		        submit_response_msg_t **resp )
+{
+        int rc;
+        slurm_msg_t req_msg;
+        slurm_msg_t resp_msg;
+	bool host_set = false;
+	char host[64];
+
+	slurm_msg_t_init(&req_msg);
+	slurm_msg_t_init(&resp_msg);
+
+
+	/* 
+	 * set Node and session id for this request
+	 */
+	if (req->alloc_sid == NO_VAL)
+		req->alloc_sid = getsid(0);
+
+	if ( (req->alloc_node == NULL)
+	    && (gethostname_short(host, sizeof(host)) == 0) ) {
+		req->alloc_node = host;
+		host_set  = true;
+	}
+
+	req_msg.msg_type = REQUEST_SUBMIT_BATCH_JOB ;
+	req_msg.data     = req; 
+
+	rc = slurm_send_recv_controller_msg(&req_msg, &resp_msg);
+
+	/*
+	 *  Clear this hostname if set internally to this function
+	 *    (memory is on the stack)
+	 */
+	if (host_set)
+		req->alloc_node = NULL;
+
+	if (rc == SLURM_SOCKET_ERROR)
+		return SLURM_ERROR;
+
+	switch (resp_msg.msg_type) {
+	case RESPONSE_SLURM_RC:
+		rc = ((return_code_msg_t *) resp_msg.data)->return_code;
+		if (rc) 
+			slurm_seterrno_ret(rc);
+		*resp = NULL;
+		break;
+	case RESPONSE_SUBMIT_BATCH_JOB:
+		*resp = (submit_response_msg_t *) resp_msg.data;
+		break;
+	default:
+		slurm_seterrno_ret(SLURM_UNEXPECTED_MSG_ERROR);
+	}
+
+	return SLURM_PROTOCOL_SUCCESS;
+}
