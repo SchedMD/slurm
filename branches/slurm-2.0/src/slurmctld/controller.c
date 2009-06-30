d99fc776dc4c19f048a2789a3d94ed05f9c88dc5
Danny Auble <da@llnl.gov>
2009-06-30 15:43:39 +0000

    Fixed bug if not enforcing associations, but running with them and the priority/multifactor, the slurmctld will not core dump on processing usage.

--- branches/slurm-2.0/src/slurmctld/controller.c
+++ branches/slurm-2.0/src/slurmctld/controller.c
@@ -1106,8 +1106,7 @@ static void _remove_assoc(acct_association_rec_t *rec)
 {
 	int cnt = 0;
 
-	if (accounting_enforce & ACCOUNTING_ENFORCE_ASSOCS)
-		cnt = job_cancel_by_assoc_id(rec->id);
+	cnt = job_cancel_by_assoc_id(rec->id);
 
 	if (cnt) {
 		info("Removed association id:%u user:%s, cancelled %u jobs",
