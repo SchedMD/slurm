5bb707f80a5a6d742c6927f56c3a15454d488b9d
Moe Jette <jette1@llnl.gov>
2009-08-06 17:07:37 +0000

    If the job state file is missing or too small, automatically try using the     previously saved state.

--- trunk/src/slurmctld/job_mgr.c
+++ trunk/src/slurmctld/job_mgr.c
@@ -140,6 +140,7 @@ static int  _load_job_details(struct job_record *job_ptr, Buf buffer);
 static int  _load_job_state(Buf buffer);
 static void _notify_srun_missing_step(struct job_record *job_ptr, int node_inx,
 				      time_t now, time_t node_boot_time);
+static int  _open_job_state_file(char **state_file);
 static void _pack_job_for_ckpt (struct job_record *job_ptr, Buf buffer);
 static void _pack_default_job_details(struct job_details *detail_ptr,
 				      Buf buffer);
@@ -407,6 +408,36 @@ int dump_all_job_state(void)
 	return error_code;
 }
 
+/* Open the job state save file, or backup if necessary.
+ * state_file IN - the name of the state save file used
+ * RET the file description to read from or error code
+ */
+static int _open_job_state_file(char **state_file)
+{
+	int state_fd;
+	struct stat stat_buf;
+
+
+	*state_file = xstrdup(slurmctld_conf.state_save_location);
+	xstrcat(*state_file, "/job_state");
+	state_fd = open(*state_file, O_RDONLY);
+	if (state_fd < 0) {
+		error("Could not open job state file %s: %m", *state_file);
+	} else if (fstat(state_fd, &stat_buf) < 0) {
+		error("Could not stat job state file %s: %m", *state_file);
+		(void) close(state_fd);
+	} else if (stat_buf.st_size < 10) {
+		error("Job state file %s too small", *state_file);
+		(void) close(state_fd);
+	} else 	/* Success */
+		return state_fd;
+
+	error("NOTE: Trying backup state save file. Jobs may be lost!");
+	xstrcat(*state_file, ".old");
+	state_fd = open(*state_file, O_RDONLY);
+	return state_fd;
+}
+
 /*
  * load_all_job_state - load the job state from file, recover from last 
  *	checkpoint. Execute this after loading the configuration file data.
@@ -426,10 +457,8 @@ extern int load_all_job_state(void)
 	uint32_t ver_str_len;
 
 	/* read the file */
-	state_file = xstrdup(slurmctld_conf.state_save_location);
-	xstrcat(state_file, "/job_state");
 	lock_state_files();
-	state_fd = open(state_file, O_RDONLY);
+	state_fd = _open_job_state_file(&state_file);
 	if (state_fd < 0) {
 		info("No job state file (%s) to recover", state_file);
 		error_code = ENOENT;
