d9b4bf372ff26cd7151a95183bfecb787142e92b
Danny Auble <da@llnl.gov>
2009-08-10 16:49:05 +0000

    fixed pam to build correctly, also added a version number to the libpmi

--- trunk/contribs/pam/pam_slurm.c
+++ trunk/contribs/pam/pam_slurm.c
@@ -46,7 +46,7 @@
 #include <dlfcn.h>
 
 #include "slurm/slurm.h"
-#include "common/hostlist.h"
+#include "hostlist.h"
 
 /*  Define the externally visible functions in this file.
  */
@@ -56,11 +56,18 @@
 
 
 struct _options {
-    int enable_debug;
-    int enable_silence;
-    const char *msg_prefix;
-    const char *msg_suffix;
+	int enable_debug;
+	int enable_silence;
+	const char *msg_prefix;
+	const char *msg_suffix;
 };
+
+/* Define the functions to be called before and after load since _init
+ * and _fini are obsolete, and their use can lead to unpredicatable
+ * results.
+ */
+void __attribute__ ((constructor)) libpam_slurm_init(void);
+void __attribute__ ((destructor)) libpam_slurm_fini(void);
 
 /*
  *  Handle for libslurm.so
@@ -80,13 +87,13 @@ static void _parse_args(struct _options *opts, int argc, const char **argv);
 static int  _hostrange_member(char *hostname, char *str);
 static int  _slurm_match_allocation(uid_t uid);
 static void _send_denial_msg(pam_handle_t *pamh, struct _options *opts,
-            const char *user, uid_t uid);
+			     const char *user, uid_t uid);
 
-#define DBG(msg,args...) \
-    do { \
-        if (debug) \
-           _log_msg(LOG_INFO, msg, ##args); \
-    } while (0);
+#define DBG(msg,args...)					\
+	do {							\
+		if (debug)					\
+			_log_msg(LOG_INFO, msg, ##args);	\
+	} while (0);
 
 /**********************************\
  *  Account Management Functions  *
@@ -95,43 +102,44 @@ static void _send_denial_msg(pam_handle_t *pamh, struct _options *opts,
 PAM_EXTERN int
 pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-    struct _options opts;
-    int retval;
-    char *user;
-    void *dummy;  /* needed to eliminate warning:
-                   * dereferencing type-punned pointer will break strict-aliasing rules */
-    struct passwd *pw;
-    uid_t uid;
-    int auth = PAM_PERM_DENIED;
+	struct _options opts;
+	int retval;
+	char *user;
+	void *dummy;  /* needed to eliminate warning:
+		       * dereferencing type-punned pointer will break
+		       * strict-aliasing rules */
+	struct passwd *pw;
+	uid_t uid;
+	int auth = PAM_PERM_DENIED;
 
-    _parse_args(&opts, argc, argv);
-    if (flags & PAM_SILENT)
-        opts.enable_silence = 1;
+	_parse_args(&opts, argc, argv);
+	if (flags & PAM_SILENT)
+		opts.enable_silence = 1;
 
-    retval = pam_get_item(pamh, PAM_USER, (const void **) &dummy);
-    user = (char *) dummy;
-    if ((retval != PAM_SUCCESS) || (user == NULL) || (*user == '\0')) {
-        _log_msg(LOG_ERR, "unable to identify user: %s",
-            pam_strerror(pamh, retval));
-        return(PAM_USER_UNKNOWN);
-    }
-    if (!(pw = getpwnam(user))) {
-        _log_msg(LOG_ERR, "user %s does not exist", user);
-        return(PAM_USER_UNKNOWN);
-    }
-    uid = pw->pw_uid;
+	retval = pam_get_item(pamh, PAM_USER, (const void **) &dummy);
+	user = (char *) dummy;
+	if ((retval != PAM_SUCCESS) || (user == NULL) || (*user == '\0')) {
+		_log_msg(LOG_ERR, "unable to identify user: %s",
+			 pam_strerror(pamh, retval));
+		return(PAM_USER_UNKNOWN);
+	}
+	if (!(pw = getpwnam(user))) {
+		_log_msg(LOG_ERR, "user %s does not exist", user);
+		return(PAM_USER_UNKNOWN);
+	}
+	uid = pw->pw_uid;
 
-    if (uid == 0)
-        auth = PAM_SUCCESS;
-    else if (_slurm_match_allocation(uid))
-        auth = PAM_SUCCESS;
+	if (uid == 0)
+		auth = PAM_SUCCESS;
+	else if (_slurm_match_allocation(uid))
+		auth = PAM_SUCCESS;
 
-    if ((auth != PAM_SUCCESS) && (!opts.enable_silence))
-        _send_denial_msg(pamh, &opts, user, uid);
-    _log_msg(LOG_INFO, "access %s for user %s (uid=%d)",
-        (auth == PAM_SUCCESS) ? "granted" : "denied", user, uid);
+	if ((auth != PAM_SUCCESS) && (!opts.enable_silence))
+		_send_denial_msg(pamh, &opts, user, uid);
+	_log_msg(LOG_INFO, "access %s for user %s (uid=%d)",
+		 (auth == PAM_SUCCESS) ? "granted" : "denied", user, uid);
 
-    return(auth);
+	return(auth);
 }
 
 
@@ -145,14 +153,14 @@ pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
 static void
 _log_msg(int level, const char *format, ...)
 {
-    va_list args;
+	va_list args;
 
-    openlog("pam_slurm", LOG_CONS | LOG_PID, LOG_AUTHPRIV);
-    va_start(args, format);
-    vsyslog(level, format, args);
-    va_end(args);
-    closelog();
-    return;
+	openlog("pam_slurm", LOG_CONS | LOG_PID, LOG_AUTHPRIV);
+	va_start(args, format);
+	vsyslog(level, format, args);
+	va_end(args);
+	closelog();
+	return;
 }
 
 /*
@@ -161,45 +169,47 @@ _log_msg(int level, const char *format, ...)
 static void
 _parse_args(struct _options *opts, int argc, const char **argv)
 {
-    int i;
+	int i;
 
-    opts->enable_debug = 0;
-    opts->enable_silence = 0;
-    opts->msg_prefix = "";
-    opts->msg_suffix = "";
+	opts->enable_debug = 0;
+	opts->enable_silence = 0;
+	opts->msg_prefix = "";
+	opts->msg_suffix = "";
 
-    /*  rsh_kludge:
-     *  The rsh service under RH71 (rsh-0.17-2.5) truncates the first char
-     *  of this msg.  The rsh client sends 3 NUL-terminated ASCII strings:
-     *  client-user-name, server-user-name, and command string.  The server
-     *  then validates the user.  If the user is valid, it responds with a
-     *  1-byte zero; o/w, it responds with a 1-byte one followed by an ASCII
-     *  error message and a newline.  RH's server is using the default PAM
-     *  conversation function which doesn't prepend the message with a
-     *  single-byte error code.  As a result, the client receives a string,
-     *  interprets the first byte as a non-zero status, and treats the
-     *  remaining string as an error message.  The rsh_kludge prepends a
-     *  newline which will be interpreted by the rsh client as an error status.
-     *
-     *  rlogin_kludge:
-     *  The rlogin service under RH71 (rsh-0.17-2.5) does not perform a
-     *  carriage-return after the PAM error message is displayed which results
-     *  in the "staircase-effect" of the next message. The rlogin_kludge
-     *  appends a carriage-return to prevent this.
-     */
-    for (i=0; i<argc; i++) {
-        if (!strcmp(argv[i], "debug"))
-            opts->enable_debug = debug = 1;
-        else if (!strcmp(argv[i], "no_warn"))
-            opts->enable_silence = 1;
-        else if (!strcmp(argv[i], "rsh_kludge"))
-            opts->msg_prefix = "\n";
-        else if (!strcmp(argv[i], "rlogin_kludge"))
-            opts->msg_suffix = "\r";
-        else
-            _log_msg(LOG_ERR, "unknown option [%s]", argv[i]);
-    }
-    return;
+	/*  rsh_kludge:
+	 *  The rsh service under RH71 (rsh-0.17-2.5) truncates the first char
+	 *  of this msg.  The rsh client sends 3 NUL-terminated ASCII strings:
+	 *  client-user-name, server-user-name, and command string.  The server
+	 *  then validates the user.  If the user is valid, it responds with a
+	 *  1-byte zero; o/w, it responds with a 1-byte one followed by an ASCII
+	 *  error message and a newline.  RH's server is using the default PAM
+	 *  conversation function which doesn't prepend the message with a
+	 *  single-byte error code.  As a result, the client receives a string,
+	 *  interprets the first byte as a non-zero status, and treats the
+	 *  remaining string as an error message.  The rsh_kludge prepends a
+	 *  newline which will be interpreted by the rsh client as an
+	 *  error status.
+	 *
+	 *  rlogin_kludge:
+	 *  The rlogin service under RH71 (rsh-0.17-2.5) does not perform a
+	 *  carriage-return after the PAM error message is displayed
+	 *  which results
+	 *  in the "staircase-effect" of the next message. The rlogin_kludge
+	 *  appends a carriage-return to prevent this.
+	 */
+	for (i=0; i<argc; i++) {
+		if (!strcmp(argv[i], "debug"))
+			opts->enable_debug = debug = 1;
+		else if (!strcmp(argv[i], "no_warn"))
+			opts->enable_silence = 1;
+		else if (!strcmp(argv[i], "rsh_kludge"))
+			opts->msg_prefix = "\n";
+		else if (!strcmp(argv[i], "rlogin_kludge"))
+			opts->msg_suffix = "\r";
+		else
+			_log_msg(LOG_ERR, "unknown option [%s]", argv[i]);
+	}
+	return;
 }
 
 /* 
@@ -210,21 +220,21 @@ _parse_args(struct _options *opts, int argc, const char **argv)
 static int
 _hostrange_member(char *hostname, char *str)
 {
-    hostlist_t hl;
-    int found_host;
+	hostlist_t hl;
+	int found_host;
 
-    if (!*hostname || !*str)
-        return 0;
+	if (!*hostname || !*str)
+		return 0;
 
-    if ((hl = hostlist_create(str)) == NULL)
-        return 0;
-    found_host = hostlist_find(hl, hostname);
-    hostlist_destroy(hl);
+	if ((hl = hostlist_create(str)) == NULL)
+		return 0;
+	found_host = hostlist_find(hl, hostname);
+	hostlist_destroy(hl);
 
-    if (found_host == -1)
-        return 0;
-    else
-        return 1;
+	if (found_host == -1)
+		return 0;
+	else
+		return 1;
 }
 
 /*
@@ -232,14 +242,14 @@ _hostrange_member(char *hostname, char *str)
  */
 int _slurm_load_jobs (job_info_msg_t **msgp)
 {
-    static int (*load_jobs) (time_t, job_info_msg_t **, uint16_t);
+	static int (*load_jobs) (time_t, job_info_msg_t **, uint16_t);
 
-    if (!(load_jobs = dlsym (slurm_h, "slurm_load_jobs"))) {
-        _log_msg (LOG_ERR, "Unable to resolve slurm_load_jobs\n");
-        return -1;
-    }
+	if (!(load_jobs = dlsym (slurm_h, "slurm_load_jobs"))) {
+		_log_msg (LOG_ERR, "Unable to resolve slurm_load_jobs\n");
+		return -1;
+	}
     
-    return load_jobs ((time_t) NULL, msgp, (uint16_t) SHOW_ALL);
+	return load_jobs ((time_t) NULL, msgp, (uint16_t) SHOW_ALL);
 }
 
 /*
@@ -247,14 +257,14 @@ int _slurm_load_jobs (job_info_msg_t **msgp)
  */
 char * _slurm_strerror (int errnum)
 {
-    static char * (*f) (int);
+	static char * (*f) (int);
 
-    if (!(f = dlsym (slurm_h, "slurm_strerror"))) {
-        _log_msg (LOG_ERR, "Unable to resolve slurm_strerror\n");
-        return "unknown error";
-    }
+	if (!(f = dlsym (slurm_h, "slurm_strerror"))) {
+		_log_msg (LOG_ERR, "Unable to resolve slurm_strerror\n");
+		return "unknown error";
+	}
 
-    return f (errnum);
+	return f (errnum);
 }
 
 /*
@@ -262,16 +272,16 @@ char * _slurm_strerror (int errnum)
  */
 void _free_msg (job_info_msg_t *msg)
 {
-    static void (*free_msg) (job_info_msg_t *);
+	static void (*free_msg) (job_info_msg_t *);
 
-    if (!(free_msg = dlsym (slurm_h, "slurm_free_job_info_msg"))) {
-        _log_msg (LOG_ERR, "Unable to resolve slurm_free_job...\n");
-        return;
-    }
+	if (!(free_msg = dlsym (slurm_h, "slurm_free_job_info_msg"))) {
+		_log_msg (LOG_ERR, "Unable to resolve slurm_free_job...\n");
+		return;
+	}
 
-    free_msg (msg);
+	free_msg (msg);
 
-    return;
+	return;
 }
 
 /*
@@ -282,45 +292,46 @@ void _free_msg (job_info_msg_t *msg)
 static int 
 _slurm_match_allocation(uid_t uid)
 {
-    int authorized = 0, i;
-    char hostname[MAXHOSTNAMELEN], *p;
-    job_info_msg_t * msg;
+	int authorized = 0, i;
+	char hostname[MAXHOSTNAMELEN], *p;
+	job_info_msg_t * msg;
 
-    if (gethostname(hostname, sizeof(hostname)) < 0) {
-        _log_msg(LOG_ERR, "gethostname: %m");
-        return 0;
-    }
-    if ((p = strchr(hostname, '.')))
-        *p = '\0';
+	if (gethostname(hostname, sizeof(hostname)) < 0) {
+		_log_msg(LOG_ERR, "gethostname: %m");
+		return 0;
+	}
+	if ((p = strchr(hostname, '.')))
+		*p = '\0';
 
-    DBG ("does uid %ld have \"%s\" allocated", uid, hostname);
+	DBG ("does uid %ld have \"%s\" allocated", uid, hostname);
 
-    if (_slurm_load_jobs(&msg) < 0) {
-        _log_msg(LOG_ERR, "slurm_load_jobs: %s", _slurm_strerror(errno));
-        return 0;
-    }
+	if (_slurm_load_jobs(&msg) < 0) {
+		_log_msg(LOG_ERR, "slurm_load_jobs: %s",
+			 _slurm_strerror(errno));
+		return 0;
+	}
 
-    DBG ("slurm_load_jobs returned %d records", msg->record_count);
+	DBG ("slurm_load_jobs returned %d records", msg->record_count);
 
-    for (i = 0; i < msg->record_count; i++) {
-        job_info_t *j = &msg->job_array[i];
+	for (i = 0; i < msg->record_count; i++) {
+		job_info_t *j = &msg->job_array[i];
 
-        if (  (j->user_id == uid) && (j->job_state == JOB_RUNNING)) {
+		if (  (j->user_id == uid) && (j->job_state == JOB_RUNNING)) {
 
-           DBG ("jobid %ld: nodes=\"%s\"", j->job_id, j->nodes);
+			DBG ("jobid %ld: nodes=\"%s\"", j->job_id, j->nodes);
 
-           if (_hostrange_member(hostname, j->nodes) ) {
-                 DBG ("user %ld allocated node %s in job %ld", 
-                      uid, hostname, j->job_id);
-                authorized = 1;
-                break;
-            }
-        }
-    }
+			if (_hostrange_member(hostname, j->nodes) ) {
+				DBG ("user %ld allocated node %s in job %ld", 
+				     uid, hostname, j->job_id);
+				authorized = 1;
+				break;
+			}
+		}
+	}
 
-    _free_msg (msg);
+	_free_msg (msg);
 
-    return authorized;
+	return authorized;
 }
 
 /*
@@ -331,70 +342,72 @@ static void
 _send_denial_msg(pam_handle_t *pamh, struct _options *opts,
                  const char *user, uid_t uid)
 {
-    int retval;
-    struct pam_conv *conv;
-    void *dummy;    /* needed to eliminate warning:
-                     * dereferencing type-punned pointer will break strict-aliasing rules */
-    int n;
-    char str[PAM_MAX_MSG_SIZE];
-    struct pam_message msg[1];
-    const struct pam_message *pmsg[1];
-    struct pam_response *prsp;
+	int retval;
+	struct pam_conv *conv;
+	void *dummy;    /* needed to eliminate warning:
+			 * dereferencing type-punned pointer will
+			 * break strict-aliasing rules */
+	int n;
+	char str[PAM_MAX_MSG_SIZE];
+	struct pam_message msg[1];
+	const struct pam_message *pmsg[1];
+	struct pam_response *prsp;
 
-    /*  Get conversation function to talk with app.
-     */
-    retval = pam_get_item(pamh, PAM_CONV, (const void **) &dummy);
-    conv = (struct pam_conv *) dummy;
-    if (retval != PAM_SUCCESS) {
-        _log_msg(LOG_ERR, "unable to get pam_conv: %s",
-            pam_strerror(pamh, retval));
-        return;
-    }
+	/*  Get conversation function to talk with app.
+	 */
+	retval = pam_get_item(pamh, PAM_CONV, (const void **) &dummy);
+	conv = (struct pam_conv *) dummy;
+	if (retval != PAM_SUCCESS) {
+		_log_msg(LOG_ERR, "unable to get pam_conv: %s",
+			 pam_strerror(pamh, retval));
+		return;
+	}
 
-    /*  Construct msg to send to app.
-     */
-    n = snprintf(str, sizeof(str),
-        "%sAccess denied: user %s (uid=%d) has no active jobs.%s",
-        opts->msg_prefix, user, uid, opts->msg_suffix);
-    if ((n < 0) || (n >= sizeof(str)))
-        _log_msg(LOG_ERR, "exceeded buffer for pam_conv message");
-    msg[0].msg_style = PAM_ERROR_MSG;
-    msg[0].msg = str;
-    pmsg[0] = &msg[0];
-    prsp = NULL;
+	/*  Construct msg to send to app.
+	 */
+	n = snprintf(str, sizeof(str),
+		     "%sAccess denied: user %s (uid=%d) has no active jobs.%s",
+		     opts->msg_prefix, user, uid, opts->msg_suffix);
+	if ((n < 0) || (n >= sizeof(str)))
+		_log_msg(LOG_ERR, "exceeded buffer for pam_conv message");
+	msg[0].msg_style = PAM_ERROR_MSG;
+	msg[0].msg = str;
+	pmsg[0] = &msg[0];
+	prsp = NULL;
 
-    /*  Send msg to app and free the (meaningless) rsp.
-     */
-    retval = conv->conv(1, pmsg, &prsp, conv->appdata_ptr);
-    if (retval != PAM_SUCCESS)
-        _log_msg(LOG_ERR, "unable to converse with app: %s",
-            pam_strerror(pamh, retval));
-    if (prsp != NULL)
-        _pam_drop_reply(prsp, 1);
+	/*  Send msg to app and free the (meaningless) rsp.
+	 */
+	retval = conv->conv(1, pmsg, &prsp, conv->appdata_ptr);
+	if (retval != PAM_SUCCESS)
+		_log_msg(LOG_ERR, "unable to converse with app: %s",
+			 pam_strerror(pamh, retval));
+	if (prsp != NULL)
+		_pam_drop_reply(prsp, 1);
 
-    return;
+	return;
 }
 
 /* 
  * Dynamically open system's libslurm.so with RTLD_GLOBAL flag.
  *  This allows subsequently loaded modules access to libslurm symbols.
  */
-extern void _init (void)
+extern void libpam_slurm_init (void)
 {
-    if (slurm_h)
-        return;
-
+	if (slurm_h)
+		return;
+   
 	if (!(slurm_h = dlopen("libslurm.so", RTLD_NOW|RTLD_GLOBAL))) 
-		_log_msg (LOG_ERR, "Unable to dlopen libslurm: %s\n", dlerror ());
+		_log_msg (LOG_ERR, "Unable to dlopen libslurm: %s\n",
+			  dlerror ());
 
-    return;
+	return;
 }
 
-extern void _fini (void)
+extern void libpam_slurm_fini (void)
 {
-    if (slurm_h)
-        dlclose (slurm_h);
-    return;
+ 	if (slurm_h)
+		dlclose (slurm_h);
+	return;
 }
 
 
@@ -404,13 +417,13 @@ extern void _fini (void)
 
 #ifdef PAM_STATIC
 struct pam_module _pam_rms_modstruct = {
-    "pam_slurm",
-    NULL,
-    NULL,
-    pam_sm_acct_mgmt,
-    NULL,
-    NULL,
-    NULL,
+	"pam_slurm",
+	NULL,
+	NULL,
+	pam_sm_acct_mgmt,
+	NULL,
+	NULL,
+	NULL,
 };
 #endif /* PAM_STATIC */
 
