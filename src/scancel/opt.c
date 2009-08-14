df46b8050c89d7a87b70c86c2e2d6b69f96730fe
Moe Jette <jette1@llnl.gov>
2009-08-14 23:18:55 +0000

    make cancel of job step go to slurmctld rather than directly to slurmd  so we can better log the event

--- src/scancel/opt.c
+++ src/scancel/opt.c
@@ -17,7 +17,7 @@
  *  any later version.
  *
  *  In addition, as a special exception, the copyright holders give permission 
- *  to link the code of portions of this program with the OpenSSL library under 
+ *  to link the code of portions of this program with the OpenSSL library unde
  *  certain conditions as described in each individual source file, and 
  *  distribute linked combinations including the two. You must obey the GNU 
  *  General Public License in all respects for all of the code used other than 
@@ -352,7 +352,8 @@ static void _opt_args(int argc, char **argv)
 		switch (opt_char) {
 			case (int)'?':
 				fprintf(stderr, 
-					"Try \"scancel --help\" for more information\n");
+					"Try \"scancel --help\" for more "
+					"information\n");
 				exit(1);
 				break;
 			case (int)'b':
