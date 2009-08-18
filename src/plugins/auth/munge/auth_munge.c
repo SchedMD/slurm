7dfcfc550b9c38bcca7593d3880a480967563e15
Moe Jette <jette1@llnl.gov>
2009-08-18 23:03:46 +0000

    revert auth_munge plugin version from 100 to 10

--- src/plugins/auth/munge/auth_munge.c
+++ src/plugins/auth/munge/auth_munge.c
@@ -103,7 +103,7 @@
 const char plugin_name[]       	= "auth plugin for Munge "
 				  "(http://home.gna.org/munge/)";
 const char plugin_type[]       	= "auth/munge";
-const uint32_t plugin_version   = 100;
+const uint32_t plugin_version   = 10;
 const uint32_t min_plug_version = 10; /* minimum version accepted */
 
 static int plugin_errno = SLURM_SUCCESS;
