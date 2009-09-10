1362fedc99f317d04e68488ac84c043bc4ab23fb
Moe Jette <jette1@llnl.gov>
2009-09-10 18:30:31 +0000

    tag v2.1.0-pre3

---
+++ tags/slurm-2-1-0-0-pre3/src/common/slurm_protocol_util.h
@@ -0,0 +1,93 @@
+/*****************************************************************************\
+ *  slurm_protocol_util.h - communication infrastructure functions
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Kevin Tew <tew1@llnl.gov> et. al.
+ *  CODE-OCEC-09-009. All rights reserved.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://computing.llnl.gov/linux/slurm/>.
+ *  Please also read the included file: DISCLAIMER.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under 
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifndef _SLURM_PROTOCOL_UTIL_H
+#define _SLURM_PROTOCOL_UTIL_H
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#  if HAVE_INTTYPES_H
+#    include <inttypes.h>
+#  else
+#    if HAVE_STDINT_H
+#      include <stdint.h>
+#    endif
+#  endif			/* HAVE_INTTYPES_H */
+#else				/* !HAVE_CONFIG_H */
+#  include <inttypes.h>
+#endif				/*  HAVE_CONFIG_H */
+
+#include <stdio.h>
+
+#include "src/common/slurm_protocol_defs.h"
+#include "src/common/slurm_protocol_pack.h"
+#include "src/common/slurm_protocol_common.h"
+
+/* 
+ * check_header_version checks to see that the specified header was sent 
+ * from a node running the same version of the protocol as the current node 
+ * IN header - the message header received
+ * RET - SLURM error code
+ */
+extern int check_header_version(header_t * header);
+
+/*
+ * init_header - simple function to create a header, always insuring that 
+ * an accurate version string is inserted
+ * OUT header - the message header to be send
+ * IN msg_type - type of message to be send
+ * IN flags - message flags to be send
+ */
+extern void 
+init_header(header_t * header, slurm_msg_t *msg, uint16_t flags);
+
+
+/*
+ * update_header - update a message header with the message len
+ * OUT header - the message header to update
+ * IN msg_length - length of message to be send 
+ */
+extern void 
+update_header(header_t * header, uint32_t msg_length);
+
+
+/* log the supplied slurm task launch message as debug3() level */
+extern void slurm_print_launch_task_msg(launch_tasks_request_msg_t * msg,
+					char *name);
+
+#endif /* !_SLURM_PROTOCOL_UTIL_H */
---
+++ tags/slurm-2-1-0-0-pre3/src/common/uid.h
@@ -0,0 +1,85 @@
+/*****************************************************************************\
+ * src/common/uid.h - uid/gid lookup utility functions
+ * $Id$
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Mark Grondona <mgrondona@llnl.gov>.
+ *  CODE-OCEC-09-009. All rights reserved.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://computing.llnl.gov/linux/slurm/>.
+ *  Please also read the included file: DISCLAIMER.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under 
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifndef __SLURM_UID_UTILITY_H__
+#define __SLURM_UID_UTILITY_H__
+
+#include <sys/types.h>
+#include <unistd.h>
+
+/*
+ * In an ideal world, we could use sysconf(_SC_GETPW_R_SIZE_MAX) to get the 
+ * maximum buffer size neede for getpwnam_r(), but if there is no maximum
+ * value configured, the value returned is 1024, which can too small.
+ * Diito for _SC_GETGR_R_SIZE_MAX. Use 64k byte buffer by default.
+ */
+#define PW_BUF_SIZE 65536
+
+/*
+ * Return validated uid_t for string in ``name'' which contains
+ *  either the UID number or user name
+ * 
+ * Returns uid int uidp after verifying presence in /etc/passwd, or
+ *  -1 on failure.
+ */
+int uid_from_string (char *name, uid_t *uidp);
+
+/*
+ * Return the primary group id for a given user id, or 
+ * (gid_t) -1 on failure.
+ */
+gid_t gid_from_uid (uid_t uid);
+
+/*
+ * Same as uid_from_name(), but for group name/id.
+ */
+int gid_from_string (char *name, gid_t *gidp);
+
+/* 
+ * Translate uid to user name, 
+ * NOTE: xfree the return value
+ */
+char *uid_to_string (uid_t uid);
+
+/*
+ * Same as uid_to_string, but for group name.
+ * NOTE: xfree the return value
+ */
+char *gid_to_string (gid_t gid);
+#endif /*__SLURM_UID_UTILITY_H__*/
