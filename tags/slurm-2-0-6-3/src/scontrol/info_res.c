39c686b0aee3ba47c3a079e0c2ba1260914d9ce1
Danny Auble <da@llnl.gov>
2009-10-08 16:46:42 +0000

    new tag

---
+++ tags/slurm-2-0-6-3/src/scontrol/info_res.c
@@ -0,0 +1,140 @@
+/*****************************************************************************\
+ *  info_res.c - reservation information functions for scontrol.
+ *****************************************************************************
+ *  Copyright (C) 2009 Lawrence Livermore National Security.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by David Bremer <dbremer@llnl.gov>
+ *  CODE-OCEC-09-009. All rights reserved.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://computing.llnl.gov/linux/slurm/>.
+ *  Please also read the included file: DISCLAIMER.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#include "scontrol.h"
+
+/* Load current reservation table information into *res_buffer_pptr */
+extern int 
+scontrol_load_reservations(reserve_info_msg_t **res_buffer_pptr)
+{
+	int error_code;
+	static reserve_info_msg_t *old_res_info_ptr = NULL;
+	reserve_info_msg_t *res_info_ptr = NULL;
+
+	if (old_res_info_ptr) {
+		error_code = slurm_load_reservations (
+						old_res_info_ptr->last_update,
+						&res_info_ptr);
+		if (error_code == SLURM_SUCCESS) {
+			slurm_free_reservation_info_msg (old_res_info_ptr);
+
+		} else if (slurm_get_errno () == SLURM_NO_CHANGE_IN_DATA) {
+			res_info_ptr = old_res_info_ptr;
+			error_code = SLURM_SUCCESS;
+			if (quiet_flag == -1)
+				printf ("slurm_load_reservations: no change in data\n");
+		}
+	}
+	else
+		error_code = slurm_load_reservations((time_t) NULL,
+						     &res_info_ptr);
+
+	if (error_code == SLURM_SUCCESS) {
+		old_res_info_ptr = res_info_ptr;
+		*res_buffer_pptr = res_info_ptr;
+	}
+
+	return error_code;
+}
+
+/*
+ * scontrol_print_res - print the specified reservation's information
+ * IN reservation_name - NULL to print information about all reservations
+ */
+extern void 
+scontrol_print_res (char *reservation_name) 
+{
+	int error_code, i, print_cnt = 0;
+	reserve_info_msg_t *res_info_ptr = NULL;
+	reserve_info_t *res_ptr = NULL;
+
+	error_code = scontrol_load_reservations(&res_info_ptr);
+	if (error_code) {
+		exit_code = 1;
+		if (quiet_flag != 1)
+			slurm_perror ("slurm_load_reservations error");
+		return;
+	}
+
+	if (quiet_flag == -1) {
+		char time_str[32];
+		slurm_make_time_str ((time_t *)&res_info_ptr->last_update, 
+			       time_str, sizeof(time_str));
+		printf ("last_update_time=%s, records=%d\n", 
+			time_str, res_info_ptr->record_count);
+	}
+
+	res_ptr = res_info_ptr->reservation_array;
+	for (i = 0; i < res_info_ptr->record_count; i++) {
+		if (reservation_name && 
+		    strcmp (reservation_name, res_ptr[i].name) != 0)
+			continue;
+		print_cnt++;
+		slurm_print_reservation_info (stdout, & res_ptr[i], 
+		                              one_liner ) ;
+		if (reservation_name)
+			break;
+	}
+
+	if (print_cnt == 0) {
+		if (reservation_name) {
+			exit_code = 1;
+			if (quiet_flag != 1)
+				printf ("Reservation %s not found\n", 
+				        reservation_name);
+		} else if (quiet_flag != 1)
+			printf ("No reservations in the system\n");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tags/slurm-2-0-6-3/src/srun/core-format.c
@@ -0,0 +1,164 @@
+/*****************************************************************************\
+ * src/srun/core-format.c - Change corefile characteristics for job
+ * $Id$
+ *****************************************************************************
+ *  Copyright (C) 2002 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Mark Grondona <mgrondona@llnl.gov>.
+ *  CODE-OCEC-09-009. All rights reserved.
+ *  
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://computing.llnl.gov/linux/slurm/>.
+ *  Please also read the included file: DISCLAIMER.
+ *  
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission 
+ *  to link the code of portions of this program with the OpenSSL library under 
+ *  certain conditions as described in each individual source file, and 
+ *  distribute linked combinations including the two. You must obey the GNU 
+ *  General Public License in all respects for all of the code used other than 
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this 
+ *  exception to your version of the file(s), but you are not obligated to do 
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in 
+ *  the program, then also delete it here.
+ *  
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+//#include "src/common/env.h" 
+#include "src/srun/core-format.h"
+#include "src/common/log.h"
+#include "src/common/env.h"
+
+#define CORE_NORMAL 0
+#define CORE_LIGHT  1 /* Default lightweight corefile from liblwcf */
+#define CORE_LCB    2 /* PTOOLS Lightweight Corefile Browser (LCB) compliant*/
+#define CORE_LIST   3 /* List core format types to stdout and exit */
+#define LIB_LIGHT   "liblwcf-preload.so"
+
+struct core_format_info {
+	core_format_t type;
+	const char *name;
+	const char *descr;
+};
+
+/*
+ * Supported types for core=%s
+ */
+struct core_format_info core_types[] = {
+	{ CORE_NORMAL,
+	  "normal", 
+	  "Default full corefile (do nothing)"
+        },
+	{ CORE_LIGHT,
+	  "light",
+	  "liblwcf default lightweight corefile format"
+        },
+	{ CORE_LCB,
+	  "lcb",
+	  "liblwcf Lightweight Corefile Browser compliant"
+	},
+	{ CORE_LIST, 
+	  "list",
+	  "list valid core format types"
+	},
+	{ CORE_INVALID,
+	  NULL,
+	  "Invalid format"
+	}
+};
+
+static struct core_format_info * _find_core_format_info (const char *name)
+{
+	struct core_format_info *ci;
+
+	for (ci = core_types; ci && ci->name != NULL; ci++) {
+		if ( strncasecmp (ci->name, name, strlen (ci->name)) == 0)
+			break;
+	}
+
+	return (ci);
+}
+
+static void _print_valid_core_types (void)
+{
+	struct core_format_info *ci;
+
+	info ("Valid corefile format types:");
+	for (ci = core_types; ci && ci->name != NULL; ci++) {
+		if ((ci->type == CORE_LIGHT) ||
+		    (ci->type == CORE_LCB)) {
+			struct stat buf;
+			if ((stat("/lib/"           LIB_LIGHT, &buf) < 0) &&
+			    (stat("/usr/lib/"       LIB_LIGHT, &buf) < 0) &&
+			    (stat("/usr/local/lib/" LIB_LIGHT, &buf) < 0))
+				continue;
+		}
+		if (ci->type != CORE_LIST) 
+			info (" %-8s -- %s", ci->name, ci->descr);
+	}
+	return;
+}
+
+core_format_t core_format_type (const char *str)
+{
+	struct core_format_info *ci = _find_core_format_info (str);
+
+	if (ci->type == CORE_LIST) {
+		_print_valid_core_types ();
+		exit (0);
+	}
+
+	return (ci->type);
+}
+
+const char * core_format_name (core_format_t type)
+{
+	struct core_format_info *ci;
+
+	for (ci = core_types; ci && ci->name != NULL; ci++) {
+		if (ci->type == type)
+			break;
+	}
+
+	return (ci->name);
+}
+
+int core_format_enable (core_format_t type)
+{
+	switch (type) {
+	case CORE_NORMAL: case CORE_INVALID:
+		break;
+	case CORE_LCB:
+		setenvfs ("LWCF_CORE_FORMAT=LCB");
+	case CORE_LIGHT:
+		setenvfs ("LD_PRELOAD=" LIB_LIGHT);
+		break;
+	}
+
+	return (0);
+}
+
