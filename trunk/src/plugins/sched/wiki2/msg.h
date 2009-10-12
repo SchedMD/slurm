033d2935ea425dccbd90939ec2a3a9514a8853b1
Moe Jette <jette1@llnl.gov>
2009-10-12 21:31:52 +0000

    In sched/wiki2, JOBWILLRUN command, add support for identification of     preemptable and preempted jobs (both new and old format of commands are     supported).

--- trunk/src/plugins/sched/wiki2/msg.h
+++ trunk/src/plugins/sched/wiki2/msg.h
@@ -16,7 +16,7 @@
  *  any later version.
  *
  *  In addition, as a special exception, the copyright holders give permission 
- *  to link the code of portions of this program with the OpenSSL library under 
+ *  to link the code of portions of this program with the OpenSSL library under
  *  certain conditions as described in each individual source file, and 
  *  distribute linked combinations including the two. You must obey the GNU 
  *  General Public License in all respects for all of the code used other than 
@@ -163,6 +163,7 @@ extern int	job_release_task(char *cmd_ptr, int *err_code, char **err_msg);
 extern int	job_requeue_wiki(char *cmd_ptr, int *err_code, char **err_msg);
 extern int	job_signal_wiki(char *cmd_ptr, int *err_code, char **err_msg);
 extern int	job_will_run(char *cmd_ptr, int *err_code, char **err_msg);
+extern int	job_will_run2(char *cmd_ptr, int *err_code, char **err_msg);
 extern char * 	moab2slurm_task_list(char *moab_tasklist, int *task_cnt);
 extern int	parse_wiki_config(void);
 extern char *	slurm_job2moab_task_list(struct job_record *job_ptr);
