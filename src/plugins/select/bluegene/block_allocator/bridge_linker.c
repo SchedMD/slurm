ccacd390233e075dd6953869ff25366b4198a2fe
Danny Auble <da@llnl.gov>
2009-10-15 17:40:33 +0000

    svn merge -r18877:18906 https://eris.llnl.gov/svn/slurm/branches/slurm-2.0

--- src/plugins/select/bluegene/block_allocator/bridge_linker.c
+++ src/plugins/select/bluegene/block_allocator/bridge_linker.c
@@ -251,8 +251,9 @@ extern int bridge_fini()
 {
 	if(handle)
 		dlclose(handle);
-
-	return SLURM_ERROR;
+	initialized = false;
+	
+	return SLURM_SUCCESS;
 }
 
 extern status_t bridge_get_bg(my_bluegene_t **bg)
@@ -300,8 +301,19 @@ extern status_t bridge_get_block_info(pm_partition_id_t pid,
 	int rc = CONNECTION_ERROR;
 	if(!bridge_init())
 		return rc;
+
+	/* this is here to make sure we don't lock up things with
+	   polling and the long running get_BG call */
+	rc = pthread_mutex_trylock(&api_file_mutex);
+	if (rc == EBUSY) 
+		return rc;
+	else if(rc) {
+		errno = rc;
+		error("%s:%d %s: pthread_mutex_trylock(): %m",
+		      __FILE__, __LINE__, __CURRENT_FUNC__);     
+	}
 	
-	slurm_mutex_lock(&api_file_mutex);
+	//slurm_mutex_lock(&api_file_mutex);
 	rc = (*(bridge_api.get_partition_info))(pid, partition);
 	slurm_mutex_unlock(&api_file_mutex);
 	return rc;
