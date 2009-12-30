02873777d79b84b494cfb1beb5498ab33e7179e0
Danny Auble <da@llnl.gov>
2009-12-30 20:56:14 +0000

    better code to determine if there is a selection or not

--- src/sview/block_info.c
+++ src/sview/block_info.c
@@ -883,8 +883,6 @@ extern void get_info_block(GtkTable *table, display_data_t *display_data)
 	int j=0;
 	ListIterator itr = NULL;
 	sview_block_info_t *sview_block_info_ptr = NULL;
-	GtkTreePath *path = NULL;
-	GtkTreeViewColumn *focus_column = NULL;
 
 	if(display_data)
 		local_display_data = display_data;
@@ -988,12 +986,18 @@ display_it:
 				 SORTID_CNT, SORTID_NODELIST, SORTID_COLOR);
 	}
 
-	/* highlight the correct nodes from the last selection */
-	gtk_tree_view_get_cursor(GTK_TREE_VIEW(display_widget),
-				 &path, &focus_column);
-	if(path)
-		highlight_grid(GTK_TREE_VIEW(display_widget), path,
-			       SORTID_NODE_INX, grid_button_list);
+	if(gtk_tree_selection_count_selected_rows(
+		   gtk_tree_view_get_selection(
+			   GTK_TREE_VIEW(display_widget)))) {
+		GtkTreePath *path = NULL;
+		GtkTreeViewColumn *focus_column = NULL;
+		/* highlight the correct nodes from the last selection */
+		gtk_tree_view_get_cursor(GTK_TREE_VIEW(display_widget),
+					 &path, &focus_column);
+		if(path)
+			highlight_grid(GTK_TREE_VIEW(display_widget), path,
+				       SORTID_NODE_INX, grid_button_list);
+	}
 
 	view = INFO_VIEW;
 	_update_info_block(block_list, GTK_TREE_VIEW(display_widget));
--- src/sview/part_info.c
+++ src/sview/part_info.c
@@ -1777,8 +1777,6 @@ extern void get_info_part(GtkTable *table, display_data_t *display_data)
 	sview_part_info_t *sview_part_info = NULL;
 	partition_info_t *part_ptr = NULL;
 	ListIterator itr = NULL;
-	GtkTreePath *path = NULL;
-	GtkTreeViewColumn *focus_column = NULL;
 
 	if(display_data)
 		local_display_data = display_data;
@@ -1879,12 +1877,18 @@ display_it:
 				 SORTID_CNT, SORTID_NAME, SORTID_COLOR);
 	}
 
-	/* highlight the correct nodes from the last selection */
-	gtk_tree_view_get_cursor(GTK_TREE_VIEW(display_widget),
-				 &path, &focus_column);
-	if(path)
-		highlight_grid(GTK_TREE_VIEW(display_widget), path,
-			       SORTID_NODE_INX, grid_button_list);
+	if(gtk_tree_selection_count_selected_rows(
+		   gtk_tree_view_get_selection(
+			   GTK_TREE_VIEW(display_widget)))) {
+		GtkTreePath *path = NULL;
+		GtkTreeViewColumn *focus_column = NULL;
+		/* highlight the correct nodes from the last selection */
+		gtk_tree_view_get_cursor(GTK_TREE_VIEW(display_widget),
+					 &path, &focus_column);
+		if(path)
+			highlight_grid(GTK_TREE_VIEW(display_widget), path,
+				       SORTID_NODE_INX, grid_button_list);
+	}
 
 	view = INFO_VIEW;
 	_update_info_part(info_list, GTK_TREE_VIEW(display_widget));
