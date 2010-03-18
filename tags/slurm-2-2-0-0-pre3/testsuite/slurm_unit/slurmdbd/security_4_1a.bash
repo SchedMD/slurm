3b3490a2ed8fd276530bcb4feecae68a5884e45a
Moe Jette <jette1@llnl.gov>
2010-03-18 20:49:19 +0000

    failed rpmbuild

--- tags/slurm-2-2-0-0-pre3/testsuite/slurm_unit/slurmdbd/security_4_1a.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Clean up previous files
-rm -f munge.key private.socket
-
-# Create a random munge key
-dd if=/dev/urandom bs=1 count=1024 >munge.key
-
-# Start munged in the foreground
-/usr/sbin/munged -F -f --socket=private.socket --key-file=munge.key
