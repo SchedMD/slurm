2ecce79ffac29710615f71b6759b8074e6a654c9
Danny Auble <da@llnl.gov>
2010-03-24 22:30:15 +0000

    fix whitespaces

--- src/slurmd/slurmstepd/slurmstepd_job.h
+++ src/slurmd/slurmstepd/slurmstepd_job.h
@@ -7,32 +7,32 @@
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
  *  Written by Mark Grondona <mgrondona@llnl.gov>.
  *  CODE-OCEC-09-009. All rights reserved.
- *  
+ *
  *  This file is part of SLURM, a resource management program.
  *  For details, see <https://computing.llnl.gov/linux/slurm/>.
  *  Please also read the included file: DISCLAIMER.
- *  
+ *
  *  SLURM is free software; you can redistribute it and/or modify it under
  *  the terms of the GNU General Public License as published by the Free
  *  Software Foundation; either version 2 of the License, or (at your option)
  *  any later version.
  *
- *  In addition, as a special exception, the copyright holders give permission 
+ *  In addition, as a special exception, the copyright holders give permission
  *  to link the code of portions of this program with the OpenSSL library under
- *  certain conditions as described in each individual source file, and 
- *  distribute linked combinations including the two. You must obey the GNU 
- *  General Public License in all respects for all of the code used other than 
- *  OpenSSL. If you modify file(s) with this exception, you may extend this 
- *  exception to your version of the file(s), but you are not obligated to do 
+ *  certain conditions as described in each individual source file, and
+ *  distribute linked combinations including the two. You must obey the GNU
+ *  General Public License in all respects for all of the code used other than
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this
+ *  exception to your version of the file(s), but you are not obligated to do
  *  so. If you do not wish to do so, delete this exception statement from your
- *  version.  If you delete this exception statement from all source files in 
+ *  version.  If you delete this exception statement from all source files in
  *  the program, then also delete it here.
- *  
+ *
  *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
  *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  *  details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License along
  *  with SLURM; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
@@ -84,7 +84,7 @@ typedef enum task_state {
 typedef struct task_info {
 	pthread_mutex_t mutex;	    /* mutex to protect task state          */
 	slurmd_task_state_t state;  /* task state                           */
- 
+
 	int             id;	    /* local task id                        */
 	uint32_t        gtid;	    /* global task id                       */
 	pid_t           pid;	    /* task pid                             */
@@ -186,7 +186,7 @@ typedef struct slurmd_job {
 	pid_t          jmgr_pid;     /* job manager pid                     */
 	pid_t          pgid;         /* process group id for tasks          */
 
-	uint16_t       task_flags; 
+	uint16_t       task_flags;
 	uint16_t       multi_prog;
 	uint16_t       overcommit;
 	env_t          *envtp;
@@ -214,8 +214,8 @@ void job_kill(slurmd_job_t *job, int signal);
 
 void job_destroy(slurmd_job_t *job);
 
-struct srun_info * srun_info_create(slurm_cred_t *cred, slurm_addr *respaddr, 
-		                    slurm_addr *ioaddr);
+struct srun_info * srun_info_create(slurm_cred_t *cred, slurm_addr *respaddr,
+				    slurm_addr *ioaddr);
 
 void  srun_info_destroy(struct srun_info *srun);
 
