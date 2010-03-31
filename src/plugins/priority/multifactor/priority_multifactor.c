e2fc5cbb0be663f8671c5bb91a715c237e461bb3
Moe Jette <jette1@llnl.gov>
2010-03-31 15:30:19 +0000

    correct format used in debug4 logging. Previously printed double as int.

--- src/plugins/priority/multifactor/priority_multifactor.c
+++ src/plugins/priority/multifactor/priority_multifactor.c
@@ -794,7 +794,7 @@ static void *_decay_thread(void *no_data)
 					debug4("adding %f new usage to "
 					       "assoc %u (user='%s' acct='%s') "
 					       "raw usage is now %Lf.  Group "
-					       "wall added %d making it %d.",
+					       "wall added %d making it %f.",
 					       real_decay, assoc->id,
 					       assoc->user, assoc->acct,
 					       assoc->usage_raw, run_delta,
