ce370ca371a81c7bcdcebab0dbeae10474c30a91
Moe Jette <jette1@llnl.gov>
2010-05-14 21:07:28 +0000

    Added limited gres support to select/linear. It prevents overallocation of gres resources including a map of specific allocated resources.
    Still need to add save/restore of that information,
    propagate detains to slurmd, consider topology, etc.

--- src/common/gres.h
+++ src/common/gres.h
@@ -208,20 +208,28 @@ extern uint32_t gres_plugin_job_test(List job_gres_list, List node_gres_list,
 /*
  * Allocate resource to a job and update node and job gres information
  * IN job_gres_list - job's gres_list built by gres_plugin_job_gres_validate()
- * IN node_gres_list - node's gres_list built by gres_plugin_node_config_validate()
+ * IN node_gres_list - node's gres_list built by
+ *		gres_plugin_node_config_validate()
+ * IN node_cnt - total number of nodes originally allocated to the job
+ * IN node_offset - zero-origin index to the node of interest
  * IN cpu_cnt - number of CPUs allocated to this job on this node
+ * RET SLURM_SUCCESS or error code
  */
-extern void gres_plugin_job_alloc(List job_gres_list, List node_gres_list, 
-				  uint32_t cpu_cnt);
+extern int gres_plugin_job_alloc(List job_gres_list, List node_gres_list, 
+				 int node_cnt, int node_offset,
+				 uint32_t cpu_cnt);
 
 /*
  * Deallocate resource from a job and update node and job gres information
  * IN job_gres_list - job's gres_list built by gres_plugin_job_gres_validate()
- * IN node_gres_list - node's gres_list built by gres_plugin_node_config_validate()
+ * IN node_gres_list - node's gres_list built by
+ *		gres_plugin_node_config_validate()
+ * IN node_offset - zero-origin index to the node of interest
  * IN cpu_cnt - number of CPUs allocated to this job on this node
+ * RET SLURM_SUCCESS or error code
  */
-extern void gres_plugin_job_dealloc(List job_gres_list, List node_gres_list, 
-				    uint32_t cpu_cnt);
+extern int gres_plugin_job_dealloc(List job_gres_list, List node_gres_list, 
+				   int node_offset, uint32_t cpu_cnt);
 
 /*
  * Log a job's current gres state
