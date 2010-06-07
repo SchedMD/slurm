f17edeaccd83b392ac93e161ac7adb814fa0541a
Danny Auble <da@llnl.gov>
2010-06-07 22:47:55 +0000

    svn merge -r20416:20433 https://eris.llnl.gov/svn/slurm/branches/slurm-2.1

--- trunk/src/slurmctld/controller.c
+++ trunk/src/slurmctld/controller.c
@@ -1051,7 +1051,10 @@ static int _accounting_cluster_ready()
 	int cpus = 0;
 	bitstr_t *total_node_bitmap = NULL;
 	char *cluster_nodes = NULL;
+	slurmctld_lock_t node_read_lock = {
+		NO_LOCK, NO_LOCK, READ_LOCK, NO_LOCK };
 
+	lock_slurmctld(node_read_lock);
 	node_ptr = node_record_table_ptr;
 	for (i = 0; i < node_record_count; i++, node_ptr++) {
 		if (node_ptr->name == '\0')
@@ -1079,6 +1082,7 @@ static int _accounting_cluster_ready()
 	bit_nset(total_node_bitmap, 0, node_record_count-1);
 	cluster_nodes = bitmap2node_name(total_node_bitmap);
 	FREE_NULL_BITMAP(total_node_bitmap);
+	unlock_slurmctld(node_read_lock);
 
 	rc = clusteracct_storage_g_cluster_cpus(acct_db_conn,
 						cluster_nodes,
@@ -1191,9 +1195,6 @@ static void *_slurmctld_background(void *no_data)
 	 * (Might kill jobs on nodes set DOWN) */
 	slurmctld_lock_t node_write_lock = {
 		READ_LOCK, WRITE_LOCK, WRITE_LOCK, NO_LOCK };
-	/* Locks: Read node */
-	slurmctld_lock_t node_read_lock = {
-		NO_LOCK, NO_LOCK, READ_LOCK, NO_LOCK };
 	/* Locks: Write node */
 	slurmctld_lock_t node_write_lock2 = {
 		NO_LOCK, NO_LOCK, WRITE_LOCK, NO_LOCK };
@@ -1380,9 +1381,7 @@ static void *_slurmctld_background(void *no_data)
 			/* Report current node state to account for added
 			 * or reconfigured nodes */
 			last_node_acct = now;
-			lock_slurmctld(node_read_lock);
 			_accounting_cluster_ready();
-			unlock_slurmctld(node_read_lock);
 		}
 
 		/* Reassert this machine as the primary controller.
