704e633989dd1724c814523b8e575c4ea83b4832
Moe Jette <jette1@llnl.gov>
2010-06-08 22:30:39 +0000

    svn merge -r20443:20445 https://eris.llnl.gov/svn/slurm/branches/slurm-2.1

--- src/plugins/sched/backfill/backfill.c
+++ src/plugins/sched/backfill/backfill.c
@@ -598,7 +598,7 @@ static void _attempt_backfill(void)
 			continue;
 		}
 
-		if (node_space_recs == max_backfill_job_cnt) {
+		if (node_space_recs >= max_backfill_job_cnt) {
 			/* Already have too many jobs to deal with */
 			break;
 		}
