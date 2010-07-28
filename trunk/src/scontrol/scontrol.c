83780ee60f4d901b4853d146b16f401d10eb9e2c
Danny Auble <da@llnl.gov>
2010-07-28 23:29:45 +0000

    fixes to make it better

--- trunk/src/scontrol/scontrol.c
+++ trunk/src/scontrol/scontrol.c
@@ -1367,7 +1367,7 @@ _update_bluegene_block (int argc, char *argv[])
 			else if (!strncasecmp(val, "REMOVE", MAX(vallen, 3)))
 				block_msg.state = RM_PARTITION_NAV;
 			else if (!strncasecmp(val, "RESUME", MAX(vallen, 3)))
-				block_msg.state = RM_PARTITION_READY;
+				block_msg.state = RM_PARTITION_DEALLOCATING;
 			else {
 				exit_code = 1;
 				fprintf (stderr, "Invalid input: %s\n",
