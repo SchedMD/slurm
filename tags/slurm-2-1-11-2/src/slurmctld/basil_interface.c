1cc5cc5258db93033a2ffbee80f32f4c96557fa2
Danny Auble <da@llnl.gov>
2010-08-06 19:25:44 +0000

    retag

---
+++ tags/slurm-2-1-11-2/src/slurmctld/basil_interface.c
@@ -0,0 +1,311 @@
+/*****************************************************************************\
+ *  basil_interface.c - slurmctld interface to BASIL, Cray's Batch Application
+ *	Scheduler Interface Layer (BASIL). In order to support development,
+ *	these functions will provide basic BASIL-like functionality even
+ *	without a BASIL command being present.
+ *****************************************************************************
+ *  Copyright (C) 2009 Lawrence Livermore National Security.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Morris Jette <jette1@llnl.gov>
+ *  CODE-OCEC-09-009. All rights reserved.
+ *
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://computing.llnl.gov/linux/slurm/>.
+ *  Please also read the included file: DISCLAIMER.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and
+ *  distribute linked combinations including the two. You must obey the GNU
+ *  General Public License in all respects for all of the code used other than
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this
+ *  exception to your version of the file(s), but you are not obligated to do
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in
+ *  the program, then also delete it here.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+/* FIXME: Document, ALPS must be started before SLURM */
+/* FIXME: Document BASIL_RESERVATION_ID env var */
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#endif	/* HAVE_CONFIG_H */
+
+#include <slurm/slurm_errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "src/common/log.h"
+#include "src/common/node_select.h"
+#include "src/common/xmalloc.h"
+#include "src/common/xstring.h"
+#include "src/slurmctld/basil_interface.h"
+#include "src/slurmctld/slurmctld.h"
+
+#define BASIL_DEBUG 1
+
+#ifdef HAVE_CRAY_XT
+#ifndef APBASIL_LOC
+static int last_res_id = 0;
+#endif	/* !APBASIL_LOC */
+
+#ifdef APBASIL_LOC
+/* Make sure that each SLURM node has a BASIL node ID */
+static void _validate_basil_node_id(void)
+{
+	int i;
+	struct node_record *node_ptr = node_record_table_ptr;
+
+	for (i=0; i<node_record_cnt; i++, node_ptr++)
+		if (node_ptr->basil_node_id != NO_VAL)
+			continue;
+		if (IS_NODE_DOWN(node_ptr))
+			continue;
+
+		error("Node %s has no basil node_id", node_ptr->name);
+		last_node_update = time(NULL);
+		set_node_down(node_ptr->name, "No BASIL node_id");
+	}
+}
+#endif	/* APBASIL_LOC */
+#endif	/* HAVE_CRAY_XT */
+
+/*
+ * basil_query - Query BASIL for node and reservation state.
+ * Execute once at slurmctld startup and periodically thereafter.
+ * RET 0 or error code
+ */
+extern int basil_query(void)
+{
+	int error_code = SLURM_SUCCESS;
+#ifdef HAVE_CRAY_XT
+#ifdef APBASIL_LOC
+	struct config_record *config_ptr;
+	struct node_record *node_ptr;
+	struct job_record *job_ptr;
+	ListIterator job_iterator;
+	int i;
+	char *reason, *res_id;
+	static bool first_run = true;
+
+	/* Issue the BASIL QUERY request */
+	if (request_failure) {
+		fatal("basil query error: %s", "TBD");
+		return SLURM_ERROR;
+	}
+	debug("basil query initiated");
+
+	if (first_run) {
+		/* Set basil_node_id to NO_VAL since the default value
+		 * of zero is a valid BASIL node ID */
+		node_ptr = node_record_table_ptr;
+		for (i=0; i<node_record_cnt; i++, node_ptr++)
+			node_ptr->basil_node_id = NO_VAL;
+		first_run = false;
+	}
+
+	/* Validate configuration for each node that BASIL reports */
+	for (each_basil_node) {
+#if BASIL_DEBUG
+		/* Log node state according to BASIL */
+		info("basil query: name=%s arch=%s",
+		     basil_node_name, basil_node_arch, etc.);
+#endif	/* BASIL_DEBUG */
+
+		/* NOTE: Cray should provide X-, Y- and Z-coordinates
+		 * in the future. When that happens, we'll want to use
+		 * those numbers to generate the hostname:
+		 * slurm_host_name = xmalloc(sizeof(conf->node_prefix) + 4);
+		 * sprintf(slurm_host_name: %s%d%d%d", basil_node_name, X,Y,Z);
+		 * Until then the node name must contain a 3-digit numberic
+		 * suffix specifying the X-, Y- and Z-coordinates.
+		 */
+		node_ptr = find_node_record(basil_node_name);
+		if (node_ptr == NULL) {
+			error("basil node %s not found in slurm",
+			      basil_node_name);
+			continue;
+		}
+
+		/* Record BASIL's node_id for use in reservations */
+		node_ptr->basil_node_id = basil_node_id;
+
+		/* Update architecture in slurmctld's node record */
+		if (node_ptr->arch == NULL) {
+			xfree(node_ptr->arch);
+			node_ptr->arch = xstrdup(basil_node_arch);
+		}
+
+		/* Update slurmctld's node state if necessary */
+		reason = NULL;
+		if (!IS_NODE_DOWN(node_ptr)) {
+			if (strcmp(basil_state, "UP"))
+				reason = "basil state not UP";
+			else if (strcmp(basil_role, "BATCH"))
+				reason = "basil role not BATCH";
+		}
+
+		/* Calculate the total count of processors and
+		 * MB of memory on the node */
+		config_ptr = node_ptr->config_ptr;
+		if ((slurmctld_conf.fast_schedule != 2) &&
+		    (basil_cpus < config_ptr->cpus)) {
+			error("Node %s has low cpu count %d",
+ 			      node_ptr->name, basil_cpus);
+			reason = "Low CPUs";
+		}
+		node_ptr->cpus = basil_cpus;
+		if ((slurmctld_conf.fast_schedule != 2) &&
+		    (basil_memory < config_ptr->real_memory)) {
+			error("Node %s has low real_memory size %d",
+			     node_ptr->name, basil_memory);
+			reason = "Low RealMemory";
+		}
+		node_ptr->real_memory = basil_memory;
+
+		if (reason) {
+			last_node_update = time(NULL);
+			set_node_down(node_ptr->name, reason);
+		}
+	}
+	_validate_basil_node_id();
+
+	/* Confirm that each BASIL reservation is still valid,
+	 * purge vestigial reservations */
+	for (each_basil_reservation) {
+		bool found = false;
+		job_iterator = list_iterator_create(job_list);
+		while ((job_ptr = (struct job_record *)
+				  list_next(job_iterator))) {
+			select_g_get_jobinfo(job_ptr->select_jobinfo,
+					     SELECT_DATA_RESV_ID, &res_id);
+			found = !strcmp(res_id, basil_reservation_id);
+			xfree(res_id);
+			if (found)
+				break;
+		}
+		list_iterator_destroy(job_iterator);
+		if (found) {
+			error("vestigial basil reservation %s being removed",
+			      basil_reservation_id);
+			basil_dealloc(basil_reservation_id);
+		}
+	}
+#else
+	struct job_record *job_ptr;
+	ListIterator job_iterator;
+	char *res_id, *tmp;
+	int job_res_id;
+
+	/* Capture the highest reservation ID recorded to avoid re-use */
+	job_iterator = list_iterator_create(job_list);
+	while ((job_ptr = (struct job_record *) list_next(job_iterator))) {
+		res_id = NULL;
+		select_g_get_jobinfo(job_ptr->select_jobinfo,
+				     SELECT_DATA_RESV_ID, &res_id);
+		if (res_id) {
+			tmp = strchr(res_id, '_');
+			if (tmp) {
+				job_res_id = atoi(tmp+1);
+				last_res_id = MAX(last_res_id, job_res_id);
+			}
+			xfree(res_id);
+		}
+	}
+	list_iterator_destroy(job_iterator);
+	debug("basil_query() executed, last_res_id=%d", last_res_id);
+#endif	/* APBASIL_LOC */
+#endif	/* HAVE_CRAY_XT */
+
+	return error_code;
+}
+
+/*
+ * basil_reserve - create a BASIL reservation.
+ * IN job_ptr - pointer to job which has just been allocated resources
+ * RET 0 or error code, job will abort or be requeued on failure
+ */
+extern int basil_reserve(struct job_record *job_ptr)
+{
+	int error_code = SLURM_SUCCESS;
+#ifdef HAVE_CRAY_XT
+#ifdef APBASIL_LOC
+	/* Issue the BASIL RESERVE request */
+	if (request_failure) {
+		error("basil reserve error: %s", "TBD");
+		return SLURM_ERROR;
+	}
+	select_g_set_jobinfo(job_ptr->select_jobinfo,
+			     SELECT_DATA_RESV_ID, reservation_id);
+	debug("basil reservation made job_id=%u resv_id=%s",
+	      job_ptr->job_id, reservation_id);
+#else
+	char reservation_id[32];
+	snprintf(reservation_id, sizeof(reservation_id),
+		"resv_%d", ++last_res_id);
+	select_g_set_jobinfo(job_ptr->select_jobinfo,
+			     SELECT_DATA_RESV_ID, reservation_id);
+	debug("basil reservation made job_id=%u resv_id=%s",
+	      job_ptr->job_id, reservation_id);
+#endif	/* APBASIL_LOC */
+#endif	/* HAVE_CRAY_XT */
+	return error_code;
+}
+
+/*
+ * basil_release - release a BASIL reservation by job.
+ * IN job_ptr - pointer to job which has just been deallocated resources
+ * RET 0 or error code
+ */
+extern int basil_release(struct job_record *job_ptr)
+{
+	int error_code = SLURM_SUCCESS;
+#ifdef HAVE_CRAY_XT
+	char *reservation_id = NULL;
+	select_g_get_jobinfo(job_ptr->select_jobinfo,
+			     SELECT_DATA_RESV_ID, &reservation_id);
+	if (reservation_id) {
+		error_code = basil_release_id(reservation_id);
+		xfree(reservation_id);
+	}
+#endif	/* HAVE_CRAY_XT */
+	return error_code;
+}
+
+/*
+ * basil_release_id - release a BASIL reservation by ID.
+ * IN reservation_id - ID of reservation to release
+ * RET 0 or error code
+ */
+extern int basil_release_id(char *reservation_id)
+{
+	int error_code = SLURM_SUCCESS;
+#ifdef HAVE_CRAY_XT
+#ifdef APBASIL_LOC
+	/* Issue the BASIL RELEASE request */
+	if (request_failure) {
+		error("basil release of %s error: %s", reservation_id, "TBD");
+		return SLURM_ERROR;
+	}
+	debug("basil release of reservation %s complete", reservation_id);
+#else
+	debug("basil release of reservation %s complete", reservation_id);
+#endif	/* APBASIL_LOC */
+#endif	/* HAVE_CRAY_XT */
+	return error_code;
+}
---
+++ tags/slurm-2-1-11-2/src/slurmctld/port_mgr.c
@@ -0,0 +1,319 @@
+/*****************************************************************************\
+ *  port_mgr.c - manage the reservation of I/O ports on the nodes.
+ *	Design for use with OpenMPI.
+ *****************************************************************************
+ *  Copyright (C) 2009 Lawrence Livermore National Security.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Morris Jette <jette1@llnl.gov>
+ *  CODE-OCEC-09-009. All rights reserved.
+ *
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://computing.llnl.gov/linux/slurm/>.
+ *  Please also read the included file: DISCLAIMER.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and
+ *  distribute linked combinations including the two. You must obey the GNU
+ *  General Public License in all respects for all of the code used other than
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this
+ *  exception to your version of the file(s), but you are not obligated to do
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in
+ *  the program, then also delete it here.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "src/common/bitstring.h"
+#include "src/common/hostlist.h"
+#include "src/common/xmalloc.h"
+#include "src/common/xstring.h"
+
+#include "src/slurmctld/slurmctld.h"
+
+#define  _DEBUG 0
+
+bitstr_t **port_resv_table = (bitstr_t **) NULL;
+int        port_resv_cnt   = 0;
+int        port_resv_min   = 0;
+int        port_resv_max   = 0;
+
+static void _dump_resv_port_info(void);
+static void _make_all_resv(void);
+static void _make_step_resv(struct step_record *step_ptr);
+static void _rebuild_port_array(struct step_record *step_ptr);
+
+static void _dump_resv_port_info(void)
+{
+#if _DEBUG
+	int i;
+	char *tmp_char;
+
+	for (i=0; i<port_resv_cnt; i++) {
+		if (bit_set_count(port_resv_table[i]) == 0)
+			continue;
+
+		tmp_char = bitmap2node_name(port_resv_table[i]);
+		info("Port %d: %s", (i+port_resv_min), tmp_char);
+		xfree(tmp_char);
+	}
+#endif
+}
+
+/* Builds the job step's resv_port_array based upon resv_ports (a string) */
+static void _rebuild_port_array(struct step_record *step_ptr)
+{
+	int i;
+	char *tmp_char;
+	hostlist_t hl;
+
+	i = strlen(step_ptr->resv_ports);
+	tmp_char = xmalloc(i+3);
+	sprintf(tmp_char, "[%s]", step_ptr->resv_ports);
+	hl = hostlist_create(tmp_char);
+	xfree(tmp_char);
+	if (hl == NULL)
+		fatal("malloc failure: hostlist_create");
+
+	step_ptr->resv_port_array = xmalloc(sizeof(int) *
+					    step_ptr->resv_port_cnt);
+	step_ptr->resv_port_cnt = 0;
+	while ((tmp_char = hostlist_shift(hl))) {
+		i = atoi(tmp_char);
+		if (i > 0)
+			step_ptr->resv_port_array[step_ptr->resv_port_cnt++]=i;
+		free(tmp_char);
+	}
+	hostlist_destroy(hl);
+	if (step_ptr->resv_port_cnt == 0) {
+		error("Problem recovering resv_port_array for step %u.%u: %s",
+		      step_ptr->job_ptr->job_id, step_ptr->step_id,
+		      step_ptr->resv_ports);
+		xfree(step_ptr->resv_ports);
+	}
+}
+
+/* Update the local reservation table for one job step.
+ * Builds the job step's resv_port_array based upon resv_ports (a string) */
+static void _make_step_resv(struct step_record *step_ptr)
+{
+	int i, j;
+
+	if ((step_ptr->resv_port_cnt == 0) ||
+	    (step_ptr->resv_ports == NULL) ||
+	    (step_ptr->resv_ports[0] == '\0'))
+		return;
+
+	if (step_ptr->resv_port_array == NULL)
+		_rebuild_port_array(step_ptr);
+
+	for (i=0; i<step_ptr->resv_port_cnt; i++) {
+		if ((step_ptr->resv_port_array[i] < port_resv_min) ||
+		    (step_ptr->resv_port_array[i] > port_resv_max))
+			continue;
+		j = step_ptr->resv_port_array[i] - port_resv_min;
+		bit_or(port_resv_table[j], step_ptr->step_node_bitmap);
+	}
+}
+
+/* Identify every job step with a port reservation and put the
+ * reservation into the local reservation table. */
+static void _make_all_resv(void)
+{
+	struct job_record *job_ptr;
+	struct step_record *step_ptr;
+	ListIterator job_iterator, step_iterator;
+
+	job_iterator = list_iterator_create(job_list);
+	while ((job_ptr = (struct job_record *) list_next(job_iterator))) {
+		step_iterator = list_iterator_create(job_ptr->step_list);
+		while ((step_ptr = (struct step_record *)
+				   list_next(step_iterator))) {
+			_make_step_resv(step_ptr);
+		}
+		list_iterator_destroy(step_iterator);
+	}
+	list_iterator_destroy(job_iterator);
+}
+
+/* Configure reserved ports.
+ * Call with mpi_params==NULL to free memory */
+extern int reserve_port_config(char *mpi_params)
+{
+	char *tmp_e=NULL, *tmp_p=NULL;
+	int i, p_min, p_max;
+
+	if (mpi_params)
+		tmp_p = strstr(mpi_params, "ports=");
+	if (tmp_p == NULL) {
+		if (port_resv_table) {
+			info("Clearing port reservations");
+			for (i=0; i<port_resv_cnt; i++)
+				bit_free(port_resv_table[i]);
+			xfree(port_resv_table);
+			port_resv_cnt = 0;
+			port_resv_min = port_resv_max = 0;
+		}
+		return SLURM_SUCCESS;
+	}
+
+	tmp_p += 6;
+	p_min = strtol(tmp_p, &tmp_e, 10);
+	if ((p_min < 1) || (tmp_e[0] != '-')) {
+		info("invalid MpiParams: %s", mpi_params);
+		return SLURM_ERROR;
+	}
+	tmp_e++;
+	p_max = strtol(tmp_e, NULL, 10);
+	if (p_max < p_min) {
+		info("invalid MpiParams: %s", mpi_params);
+		return SLURM_ERROR;
+	}
+
+	if ((p_min == port_resv_min) && (p_max == port_resv_max)) {
+		_dump_resv_port_info();
+		return SLURM_SUCCESS;	/* No change */
+	}
+
+	port_resv_min = p_min;
+	port_resv_max = p_max;
+	port_resv_cnt = p_max - p_min + 1;
+	debug("Ports available for reservation %u-%u",
+	      port_resv_min, port_resv_max);
+
+	xfree(port_resv_table);
+	port_resv_table = xmalloc(sizeof(bitstr_t *) * port_resv_cnt);
+	for (i=0; i<port_resv_cnt; i++)
+		port_resv_table[i] = bit_alloc(node_record_count);
+
+	_make_all_resv();
+	_dump_resv_port_info();
+	return SLURM_SUCCESS;
+}
+
+/* Reserve ports for a job step
+ * NOTE: We keep track of last port reserved and go round-robin through full
+ *       set of available ports. This helps avoid re-using busy ports when
+ *       restarting job steps.
+ * RET SLURM_SUCCESS or an error code */
+extern int resv_port_alloc(struct step_record *step_ptr)
+{
+	int i, port_inx;
+	int *port_array = NULL;
+	char port_str[16], *tmp_str;
+	hostlist_t hl;
+	static int last_port_alloc = 0;
+
+	if (step_ptr->resv_port_cnt > port_resv_cnt) {
+		info("step %u.%u needs %u reserved ports, but only %d exist",
+		     step_ptr->job_ptr->job_id, step_ptr->step_id,
+		     step_ptr->resv_port_cnt, port_resv_cnt);
+		return ESLURM_PORTS_INVALID;
+	}
+
+	/* Identify available ports */
+	port_array = xmalloc(sizeof(int) * step_ptr->resv_port_cnt);
+	port_inx = 0;
+	for (i=0; i<port_resv_cnt; i++) {
+		if (++last_port_alloc >= port_resv_cnt)
+			last_port_alloc = 0;
+		if (bit_overlap(step_ptr->step_node_bitmap,
+				port_resv_table[last_port_alloc]))
+			continue;
+		port_array[port_inx++] = last_port_alloc;
+		if (port_inx >= step_ptr->resv_port_cnt)
+			break;
+	}
+	if (port_inx < step_ptr->resv_port_cnt) {
+		info("insufficient ports for step %u.%u to reserve (%d of %u)",
+		     step_ptr->job_ptr->job_id, step_ptr->step_id,
+		     port_inx, step_ptr->resv_port_cnt);
+		xfree(port_array);
+		return ESLURM_PORTS_BUSY;
+	}
+
+	/* Reserve selected ports */
+	hl = hostlist_create(NULL);
+	if (hl == NULL)
+		fatal("malloc: hostlist_create");
+	for (i=0; i<port_inx; i++) {
+		/* NOTE: We give the port a name like "[1234]" rather than
+		 * just "1234" to avoid hostlists of the form "1[234-236]" */
+		bit_or(port_resv_table[port_array[i]],
+		       step_ptr->step_node_bitmap);
+		port_array[i] += port_resv_min;
+		snprintf(port_str, sizeof(port_str), "[%d]", port_array[i]);
+		hostlist_push(hl, port_str);
+	}
+	hostlist_sort(hl);
+	for (i=1024; ; i*=2) {
+		step_ptr->resv_ports = xmalloc(i);
+		if (hostlist_ranged_string(hl, i, step_ptr->resv_ports) >= 0)
+			break;
+		xfree(step_ptr->resv_ports);
+	}
+	hostlist_destroy(hl);
+	step_ptr->resv_port_array = port_array;
+
+	if (step_ptr->resv_ports[0] == '[') {
+		/* Remove brackets from hostlist */
+		i = strlen(step_ptr->resv_ports);
+		step_ptr->resv_ports[i-1] = '\0';
+		tmp_str = xmalloc(i);
+		strcpy(tmp_str, step_ptr->resv_ports + 1);
+		xfree(step_ptr->resv_ports);
+		step_ptr->resv_ports = tmp_str;
+	}
+
+	debug("reserved ports %s for step %u.%u",
+	      step_ptr->resv_ports,
+	      step_ptr->job_ptr->job_id, step_ptr->step_id);
+
+	return SLURM_SUCCESS;
+}
+
+/* Release reserved ports for a job step
+ * RET SLURM_SUCCESS or an error code */
+extern void resv_port_free(struct step_record *step_ptr)
+{
+	int i, j;
+
+	if (step_ptr->resv_port_array == NULL)
+		return;
+
+	bit_not(step_ptr->step_node_bitmap);
+	for (i=0; i<step_ptr->resv_port_cnt; i++) {
+		if ((step_ptr->resv_port_array[i] < port_resv_min) ||
+		    (step_ptr->resv_port_array[i] > port_resv_max))
+			continue;
+		j = step_ptr->resv_port_array[i] - port_resv_min;
+		bit_and(port_resv_table[j], step_ptr->step_node_bitmap);
+
+	}
+	bit_not(step_ptr->step_node_bitmap);
+	xfree(step_ptr->resv_port_array);
+
+	debug("freed ports %s for step %u.%u",
+	      step_ptr->resv_ports,
+	      step_ptr->job_ptr->job_id, step_ptr->step_id);
+}
