16d9262cccbc5f7f8dac8d647a2f092dd97fd6df
Danny Auble <da@llnl.gov>
2010-08-25 20:51:44 +0000

    BLUEGENE - Removed incorrect unlocking on error cases when starting jobs.

--- src/plugins/select/bluegene/plugin/bg_job_run.c
+++ src/plugins/select/bluegene/plugin/bg_job_run.c
@@ -316,7 +316,6 @@ static int _make_sure_block_still_exists(bg_update_t *bg_update_ptr,
 			job_fail(bg_update_ptr->job_ptr->job_id);
 		}
 		unlock_slurmctld(job_write_lock);
-		slurm_mutex_unlock(&job_start_mutex);
 		return 0;
 	}
 	return 1;
@@ -529,7 +528,6 @@ static void _sync_agent(bg_update_t *bg_update_ptr)
 			job_fail(bg_update_ptr->job_ptr->job_id);
 		}
 		unlock_slurmctld(job_write_lock);
-		slurm_mutex_unlock(&job_start_mutex);
 		return;
 	}
 
