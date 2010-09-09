139791545c296c910052a2be5eb9700fe7b336f8
Moe Jette <jette1@llnl.gov>
2010-09-09 00:25:49 +0000

    get lightuserdata functions working to pass the job descriptor and job record pointers

--- contribs/lua/job_submit.lua
+++ contribs/lua/job_submit.lua
@@ -2,6 +2,8 @@
 
  Example lua script demonstrating the SLURM job_submit/lua interface.
  This is only an example, not meant for use in its current form.
+ Leave the function names, arguments, local varialbes and setmetatable
+ logic in each function unchanged.
 
 --]]
 
@@ -14,41 +16,30 @@ require "posix"
 --########################################################################--
 
 
-function slurm_job_submit ( )
-	local account = "none"
-	local comment = "Set by job_submit/lua"
-	local user_id = 0
-	local job_desc = {}
+function slurm_job_submit ( job_desc_addr )
+	local job_desc = { job_desc_ptr=job_desc_addr }
 	setmetatable (job_desc, job_req_meta)
 
-	user_id = job_desc.user_id
 	if job_desc.account == nil then
+		local account = "***TEST_ACCOUNT***"
 		log_info("slurm_job_submit: job from uid %d, setting default account value: %s",
-			 user_id, account)
+			 job_desc.user_id, account)
 		job_desc.account = account
-	end
-
-	if job_desc.comment == nil then
-		log_info("slurm_job_submit: job from uid %d, setting default comment value: %s",
-			 user_id, comment)
-		job_desc.comment = comment
 	end
 
 	return
 end
 
-function slurm_job_modify ( )
-	local comment = "Set by job_submit/lua, modify"
-	local job_id = 0
-	local job_desc = {}
-	local job_rec = {}
+function slurm_job_modify ( job_desc_addr, job_rec_addr )
+	local job_desc = { job_desc_ptr=job_desc_addr }
+	local job_rec  = { job_rec_ptr=job_rec_addr }
 	setmetatable (job_desc, job_req_meta)
 	setmetatable (job_rec,  job_rec_meta)
 
-	job_id = job_rec.job_id
 	if job_desc.comment == nil then
+		local comment = "***TEST_COMMENT***"
 		log_info("slurm_job_modify: for job %u, setting default comment value: %s",
-			 job_id, comment)
+			 job_rec.job_id, comment)
 		job_desc.comment = comment
 	end
 
@@ -69,15 +60,15 @@ log_err = slurm.error
 
 job_rec_meta = {
 	__index = function (table, key)
-		return _get_job_rec_field(key)
+		return _get_job_rec_field(table.job_rec_ptr, key)
 	end
 }
 job_req_meta = {
 	__index = function (table, key)
-		return _get_job_req_field(key)
+		return _get_job_req_field(table.job_desc_ptr, key)
 	end,
 	__newindex = function (table, key, value)
-		return _set_job_req_field(key, value)
+		return _set_job_req_field(table.job_desc_ptr, key, value)
 	end
 }
 
