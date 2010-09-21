e99c48c7d58af15d10ca799c98b98952a75b86fc
Moe Jette <jette1@llnl.gov>
2010-09-21 22:18:59 +0000

    Remove unused function set_grid_name() from smap

--- trunk/src/smap/smap.h
+++ trunk/src/smap/smap.h
@@ -144,7 +144,6 @@ extern int quiet_flag;
 
 extern void init_grid(node_info_msg_t *node_info_ptr);
 extern int set_grid_inx(int start, int end, int count);
-extern int set_grid_name(char *nodes, int count);
 extern int set_grid_bg(int *start, int *end, int count, int set);
 extern void print_grid(int dir);
 bitstr_t *get_requested_node_bitmap();
