ce3d2f1ba2673a37b695c21ed5c29766c407f251
Moe Jette <jette1@llnl.gov>
2010-09-28 19:17:06 +0000

    Fix bug in how job step memory limits are calculated when the --relative     option is used.

--- branches/slurm-2.1/src/slurmd/slurmd/req.c
+++ branches/slurm-2.1/src/slurmd/slurmd/req.c
@@ -727,6 +727,16 @@ _check_job_credential(launch_tasks_request_msg_t *req, uid_t uid,
 
 	if ((arg.job_nhosts > 0) && (tasks_to_launch > 0)) {
 		uint32_t i, i_first_bit=0, i_last_bit=0;
+
+		/* Determine the CPU count based upon this node's index into
+		 * the _job's_ allocation (job's hostlist and core_bitmap) */
+		hostset_destroy(hset);
+		if (!(hset = hostset_create(arg.job_hostlist))) {
+			error("Unable to parse credential hostlist: `%s'",
+			      arg.job_hostlist);
+			goto fail;
+		}
+
 		host_index = hostset_find(hset, conf->node_name);
 		if ((host_index < 0) || (host_index >= arg.job_nhosts)) {
 			error("job cr credential invalid host_index %d for "
