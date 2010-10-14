e9da7da5b5a12d4cabd0c3f6b1fd2d8e7e80847a
Moe Jette <jette1@llnl.gov>
2010-10-14 16:47:23 +0000

    fix rounding error in mem-per-cpu calculation

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -4086,7 +4086,8 @@ static bool _valid_pn_min_mem(job_desc_msg_t * job_desc_msg)
 		ratio = (job_mem_limit + sys_mem_limit - 1) / sys_mem_limit;
 		if (job_desc_msg->cpus_per_task == (uint16_t) NO_VAL) {
 			job_desc_msg->cpus_per_task = ratio;
-			job_desc_msg->pn_min_memory  = job_mem_limit / ratio;
+			job_desc_msg->pn_min_memory  = job_mem_limit + ratio-1;
+			job_desc_msg->pn_min_memory /= ratio;
 			job_desc_msg->pn_min_memory |= MEM_PER_CPU;
 			return true;
 		}
