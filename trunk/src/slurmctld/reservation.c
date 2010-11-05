0e2e801147df8b9e75e5e84af27bd5b89f392b95
Moe Jette <jette1@llnl.gov>
2010-11-05 21:18:11 +0000

    Modify a log message to add more information for greater clarity.

--- trunk/src/slurmctld/reservation.c
+++ trunk/src/slurmctld/reservation.c
@@ -2805,7 +2805,8 @@ extern int job_test_resv(struct job_record *job_ptr, time_t *when,
 
 		if (slurm_get_debug_flags() & DEBUG_FLAG_RESERVATION) {
 			char *nodes=bitmap2node_name(*node_bitmap);
-			info("nodes:%s", nodes);
+			info("job_test_resv: job:%u reservation:%s nodes:%s",
+			     job_ptr->job_id, nodes, job_ptr->resv_name);
 			xfree(nodes);
 		}
 		return SLURM_SUCCESS;
