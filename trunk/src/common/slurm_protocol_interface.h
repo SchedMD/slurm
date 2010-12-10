a2bf760a4bbc131ff5369270ea30199ac8748ebd
Moe Jette <jette1@llnl.gov>
2010-12-10 15:59:52 +0000

    change author of a file.

--- trunk/src/common/slurm_protocol_interface.h
+++ trunk/src/common/slurm_protocol_interface.h
@@ -3,7 +3,7 @@
  *****************************************************************************
  *  Copyright (C) 2002-2006 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
- *  Written by Chris Dunlap <cdunlap@llnl.gov>, et. al.
+ *  Written by Kevin Tew <tew1@llnl.gov>, et. al.
  *  CODE-OCEC-09-009. All rights reserved.
  *
  *  This file is part of SLURM, a resource management program.
