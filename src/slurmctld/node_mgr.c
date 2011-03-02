8facdfa8d782ac584c484bf4379bcee41c444c5c
Moe Jette <jette1@llnl.gov>
2011-03-02 03:13:32 +0000

    minor code clean-up, patch 40_make_node_idle_revised.diff from Gerrit.

--- src/slurmctld/node_mgr.c
+++ src/slurmctld/node_mgr.c
@@ -2639,13 +2639,20 @@ void make_node_idle(struct node_record *node_ptr,
 	if (IS_NODE_DOWN(node_ptr)) {
 		debug3("make_node_idle: Node %s being left DOWN",
 			node_ptr->name);
-	} else if ((IS_NODE_DRAIN(node_ptr) || IS_NODE_FAIL(node_ptr)) &&
-		   (node_ptr->run_job_cnt == 0) &&
-		   (node_ptr->comp_job_cnt == 0)) {
-		node_ptr->node_state = NODE_STATE_IDLE | node_flags;
+		return;
+	}
+	bit_set(up_node_bitmap, inx);
+
+	if (IS_NODE_DRAIN(node_ptr) || IS_NODE_FAIL(node_ptr) ||
+	    IS_NODE_NO_RESPOND(node_ptr))
 		bit_clear(avail_node_bitmap, inx);
+	else
+		bit_set(avail_node_bitmap, inx);
+	
+	if ((IS_NODE_DRAIN(node_ptr) || IS_NODE_FAIL(node_ptr)) &&
+	    (node_ptr->run_job_cnt == 0) && (node_ptr->comp_job_cnt == 0)) {
+		node_ptr->node_state = NODE_STATE_IDLE | node_flags;
 		bit_set(idle_node_bitmap, inx);
-		bit_set(up_node_bitmap, inx);
 		debug3("make_node_idle: Node %s is DRAINED",
 		       node_ptr->name);
 		node_ptr->last_idle = now;
@@ -2655,17 +2662,11 @@ void make_node_idle(struct node_record *node_ptr,
 						slurm_get_slurm_user_id());
 	} else if (node_ptr->run_job_cnt) {
 		node_ptr->node_state = NODE_STATE_ALLOCATED | node_flags;
-		if (!IS_NODE_NO_RESPOND(node_ptr))
-			bit_set(avail_node_bitmap, inx);
-		bit_set(up_node_bitmap, inx);
 	} else {
 		node_ptr->node_state = NODE_STATE_IDLE | node_flags;
-		if (!IS_NODE_NO_RESPOND(node_ptr))
-			bit_set(avail_node_bitmap, inx);
 		if (!IS_NODE_NO_RESPOND(node_ptr) &&
 		    !IS_NODE_COMPLETING(node_ptr))
 			bit_set(idle_node_bitmap, inx);
-		bit_set(up_node_bitmap, inx);
 		node_ptr->last_idle = now;
 	}
 }
