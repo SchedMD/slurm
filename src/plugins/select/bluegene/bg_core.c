75bb892c5a3ec7d7ec5ba2cf1d11e878b241456c
Danny Auble <da@llnl.gov>
2011-03-28 21:27:14 +0000

    minor line edit

--- src/plugins/select/bluegene/bg_core.c
+++ src/plugins/select/bluegene/bg_core.c
@@ -264,8 +264,7 @@ extern bool blocks_overlap(bg_record_t *rec_a, bg_record_t *rec_b)
 		/* Test for conflicting passthroughs */
 		reset_ba_system(false);
 		check_and_set_mp_list(rec_a->ba_mp_list);
-		if (check_and_set_mp_list(rec_b->ba_mp_list)
-		    == SLURM_ERROR)
+		if (check_and_set_mp_list(rec_b->ba_mp_list) == SLURM_ERROR)
 			return true;
 		return false;
 	}
