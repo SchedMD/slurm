50449e17a386922bb3d90ec7fe22ce26c527089c
Moe Jette <jette1@llnl.gov>
2011-04-27 07:58:08 -0700

    Change scontrol show config form HashVal to HASH_VAL and note in man page that derived parameter names are all upper case. Values from the configuration files are mixed case (e.g. SLURM_VERSION vs. SlurmdPort).

--- src/api/config_info.c
+++ src/api/config_info.c
@@ -349,8 +349,8 @@ extern void *slurm_ctl_conf_2_key_pairs (slurm_ctl_conf_t* slurm_ctl_conf_ptr)
 	key_pair->value = xstrdup(tmp_str);
 	list_append(ret_list, key_pair);
 
-	if(slurm_ctl_conf_ptr->hash_val != NO_VAL) {
-		if(slurm_ctl_conf_ptr->hash_val == slurm_get_hash_val())
+	if (slurm_ctl_conf_ptr->hash_val != NO_VAL) {
+		if (slurm_ctl_conf_ptr->hash_val == slurm_get_hash_val())
 			snprintf(tmp_str, sizeof(tmp_str), "Match");
 		else {
 			snprintf(tmp_str, sizeof(tmp_str),
@@ -359,7 +359,7 @@ extern void *slurm_ctl_conf_2_key_pairs (slurm_ctl_conf_t* slurm_ctl_conf_ptr)
 				 slurm_ctl_conf_ptr->hash_val);
 		}
 		key_pair = xmalloc(sizeof(config_key_pair_t));
-		key_pair->name = xstrdup("HashVal");
+		key_pair->name = xstrdup("HASH_VAL");
 		key_pair->value = xstrdup(tmp_str);
 		list_append(ret_list, key_pair);
 	}
