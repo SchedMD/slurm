4e846ffb873bfe65eeef31c561835c161187fc00
Morris Jette <jette@schedmd.com>
2011-05-04 18:02:06 -0700

    slurmstepd: replace numeric constant with sizeof
    Found while reading mgr.c, often MAXPATHLEN is 1024, but not sure it is always.
    Patch from Gerrit Renker, CSCS

--- src/slurmd/slurmstepd/mgr.c
+++ src/slurmd/slurmstepd/mgr.c
@@ -1602,9 +1602,10 @@ _make_batch_dir(slurmd_job_t *job)
 	char path[MAXPATHLEN];
 
 	if (job->stepid == NO_VAL)
-		snprintf(path, 1024, "%s/job%05u", conf->spooldir, job->jobid);
+		snprintf(path, sizeof(path), "%s/job%05u",
+			 conf->spooldir, job->jobid);
 	else {
-		snprintf(path, 1024, "%s/job%05u.%05u",
+		snprintf(path, sizeof(path), "%s/job%05u.%05u",
 			 conf->spooldir, job->jobid, job->stepid);
 	}
 
@@ -1642,7 +1643,7 @@ _make_batch_script(batch_job_launch_msg_t *msg, char *path)
 		return NULL;
 	}
 
-	snprintf(script, 1024, "%s/%s", path, "slurm_script");
+	snprintf(script, sizeof(script), "%s/%s", path, "slurm_script");
 
   again:
 	if ((fp = safeopen(script, "w", SAFEOPEN_CREATE_ONLY)) == NULL) {
