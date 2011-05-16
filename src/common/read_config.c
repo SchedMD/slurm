SchedMD/slurm

commit 635abaeae680d129e27c7076026f1dcf8363b2cb

Get slurmd -f option working.
Get slurmd -f option working. Patch from Andriy Grytsenko.

src/slurmd/slurmd/slurmd.c
@@ -201,6 +201,20 @@ main (int argc, char *argv[])
 	_init_conf();
 	conf->argv = &argv;
 	conf->argc = &argc;
+
+	/*
+	 * Process commandline arguments first, since one option may be
+	 * an alternate location for the slurm config file.
+	 */
+	_process_cmdline(*conf->argc, *conf->argv);
+
+	/*
+	 * Read global slurm config file, ovverride necessary values from
+	 * defaults and command line.
+	 */
+	_read_config();
+	/* we should load config file _before_ analyzing SlurmUser below */
+
 	slurmd_uid = slurm_get_slurmd_user_id();
 	curr_uid = getuid();
 	if(curr_uid != slurmd_uid) {
@@ -230,7 +244,6 @@ main (int argc, char *argv[])
 	if (slurm_select_init(1) != SLURM_SUCCESS )
 		fatal( "failed to initialize node selection plugin" );
 
-	/* NOTE: conf->logfile always NULL at this point */
 	log_init(argv[0], conf->log_opts, LOG_DAEMON, conf->logfile);
 
 	xsignal(SIGTERM, &_term_handler);
@@ -1180,18 +1193,6 @@ _slurmd_init(void)
 	char slurm_stepd_path[MAXPATHLEN];
 	uint32_t cpu_cnt;
 
-	/*
-	 * Process commandline arguments first, since one option may be
-	 * an alternate location for the slurm config file.
-	 */
-	_process_cmdline(*conf->argc, *conf->argv);
-
-	/*
-	 * Read global slurm config file, ovverride necessary values from
-	 * defaults and command line.
-	 *
-	 */
-	_read_config();
 	cpu_cnt = MAX(conf->conf_cpus, conf->block_map_size);
 	if ((gres_plugin_init() != SLURM_SUCCESS) ||
 	    (gres_plugin_node_config_load(cpu_cnt) != SLURM_SUCCESS))
@@ -1490,7 +1491,7 @@ _hup_handler(int signum)
 
 
 static void
-_usage()
+_usage(void)
 {
 	fprintf(stderr, "\
 Usage: %s [OPTIONS]\n\

src/slurmd/slurmd/req.c
@@ -631,6 +631,8 @@ _forkexec_slurmstepd(slurmd_step_type_t type, void *req,
 			snprintf(slurm_stepd_path, sizeof(slurm_stepd_path),
 				 "%s/sbin/slurmstepd", SLURM_PREFIX);
 		}
+		/* inform slurmstepd about our config */
+		setenv("SLURM_CONF", conf->conffile, 1);
 
 		/*
 		 * Child forks and exits

commit 95453f4ac9d29f44a1063eefeb5c79efe8d46662

Restore slurmd hostname resolution
This restores proper slurmd hostname resolution on Frontend systems
(BlueGene and Cray) which was broken by commit 635abaeae680d129e27c
adding support for the slurmd -f option.

src/common/read_config.c
@@ -725,6 +725,11 @@ int slurm_conf_frontend_array(slurm_conf_frontend_t **ptr_array[])
 		return count;
 	} else {
 #ifdef HAVE_FRONT_END
+		/* No FrontendName in slurm.conf. Take the NodeAddr and
+		 * NodeHostName from the first node's record and use that to
+		 * build an equivalent structure to that constructed when
+		 * FrontendName is configured. This is intended for backward
+		 * compatability with SLURM version 2.2. */
 		static slurm_conf_frontend_t local_front_end;
 		static slurm_conf_frontend_t *local_front_end_array[2] =
 			{NULL, NULL};

src/slurmd/slurmd/slurmd.c
@@ -705,7 +705,6 @@ _read_config(void)
 	slurm_ctl_conf_t *cf = NULL;
 	bool cr_flag = false, gang_flag = false;
 
-	slurm_conf_reinit(conf->conffile);
 	cf = slurm_conf_lock();
 
 	slurm_mutex_lock(&conf->config_mutex);
@@ -862,7 +861,7 @@ _reconfigure(void)
 	bool did_change;
 
 	_reconfig = 0;
-	_update_logging();
+	slurm_conf_reinit(conf->conffile);
 	_read_config();
 
 	/*
@@ -1198,19 +1197,21 @@ _slurmd_init(void)
 	_process_cmdline(*conf->argc, *conf->argv);
 
 	/*
+	 * Build nodes table like in slurmctld
+	 * This is required by the topology stack
+	 * Node tables setup must preceed _read_config() so that the 
+	 * proper hostname is set.
+	 */
+	slurm_conf_init(conf->conffile);
+	init_node_conf();
+	build_all_nodeline_info(true);
+	build_all_frontend_info();
+
+	/*
 	 * Read global slurm config file, override necessary values from
 	 * defaults and command line.
 	 */
 	_read_config();
-	_update_logging();
-
-	/*
-	 * Build nodes table like in slurmctld
-	 * This is required by the topology stack
-	 */
-	init_node_conf();
-	build_all_nodeline_info(true);
-	build_all_frontend_info();
 
 	cpu_cnt = MAX(conf->conf_cpus, conf->block_map_size);
 	if ((gres_plugin_init() != SLURM_SUCCESS) ||