831b643334ad5786dcba0a0d62dba11144dd3719
Moe Jette <jette@computer-System-Product-Name.(none)>
2011-06-20 16:32:07 -0700

    Add accelerator support to alps emulation

--- src/plugins/select/cray/libemulate/alps_emulate.c
+++ src/plugins/select/cray/libemulate/alps_emulate.c
@@ -503,7 +503,8 @@ extern void   free_inv(struct basil_inventory *inv)
 
 extern long basil_reserve(const char *user, const char *batch_id,
 			  uint32_t width, uint32_t depth, uint32_t nppn,
-			  uint32_t mem_mb, struct nodespec *ns_head)
+			  uint32_t mem_mb, struct nodespec *ns_head,
+			  struct basil_accel_param *accel_head)
 {
 	int i;
 	uint32_t job_id;
