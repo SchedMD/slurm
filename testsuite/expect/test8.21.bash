d49fd8f9b0c396986cd9f15a547da200d9bbfce5
Morris Jette <jette@schedmd.com>
2011-08-12 15:48:54 -0700

    Bluegene/Q test enhancements
    These changes more thouroughly test Bluegene/Q job step placement
    algorithms and validate several recent bug fixes in the SLURM code.

--- testsuite/expect/test8.21.bash
+++ testsuite/expect/test8.21.bash
@@ -1,23 +1,43 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
-	echo "test8.21.bash <srun_path> <squeue_path> <job_id> <job_size>"
+if [ $# -ne 5 ]; then
+	echo "test8.21.bash <srun_path> <squeue_path> <job_id> <job_size> <mode:1|2?"
 	exit 1
 fi
 srun=$1
 squeue=$2
 job_id=$3
 job_size=$4
+test_mode=$5
 
-$srun -N1  --test-only /bin/true
-sleep 5
+delay_time=1
+while [ $delay_time -le 60 ]
+do
+	$srun -N1  --test-only --immediate /bin/true
+	rc=$?
+	if [ $rc -eq 0 ]
+	then
+		break
+	fi
+	sleep $delay_time
+	delay_time=`expr $delay_time + 1`
+done
+
+if [ $test_mode -gt 1 ]
+then
+	job_size=`expr $job_size + $job_size`
+	sleep_time=0
+else
+	sleep_time=1
+fi
 
 while [ $job_size -ge 2 ]
 do
 	job_size=`expr $job_size / 2`
 	$srun -N$job_size --test-only sleep 50 &
-	sleep 1
+	sleep $sleep_time
 done
+
 $srun -N1  --test-only sleep 50 &
 sleep 5
 $squeue --jobs=$job_id --steps --noheader --format='Step_ID=%i BP_List=%N'
