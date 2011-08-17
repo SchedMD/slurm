f7e43ab5e3464b2e624ecaae8cc51bf911130fea
Danny Auble <da@schedmd.com>
2011-08-17 15:26:51 -0700

    update copyright

--- src/smap/smap.c
+++ src/smap/smap.c
@@ -3,8 +3,9 @@
  *****************************************************************************
  *  Copyright (C) 2004-2007 The Regents of the University of California.
  *  Copyright (C) 2008-2011 Lawrence Livermore National Security.
+ *  Copyright (C) 2011 SchedMD LLC.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
- *  Written by Danny Auble <da@llnl.gov>
+ *  Written by Danny Auble <da@schedmd.com>
  *
  *  CODE-OCEC-09-009. All rights reserved.
  *
