109479cbe4c5cb86724de6c4f179efc981a4a166
Danny Auble <da@schedmd.com>
2011-09-07 12:48:54 -0700

    In sview when switching from a bluegene machine to a regular linux cluster and vice versa the node->base partition lists will be displayed if setup in your .slurm/sviewrc file.

--- src/sview/common.c
+++ src/sview/common.c
@@ -952,6 +952,13 @@ extern void set_page_opts(int page, display_data_t *display_data,
 	itr = list_iterator_create(page_opts->col_list);
 	while ((col_name = list_next(itr))) {
 		replus(col_name);
+		if (strstr(col_name, "list")) {
+			if (cluster_flags & CLUSTER_FLAG_BG)
+				xstrsubstitute(col_name, "node", "bp");
+			else
+				xstrsubstitute(col_name, "bp", "node");
+		}
+
 		while (display_data++) {
 			if (display_data->id == -1)
 				break;
