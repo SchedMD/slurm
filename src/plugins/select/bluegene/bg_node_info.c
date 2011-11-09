f67bc1319a3fc60c9191aa23d020690f72a4774e
Danny Auble <da@schedmd.com>
2011-11-09 15:44:50 -0800

    fixed typo

--- src/plugins/select/bluegene/bg_node_info.c
+++ src/plugins/select/bluegene/bg_node_info.c
@@ -411,7 +411,7 @@ extern int select_nodeinfo_get(select_nodeinfo_t *nodeinfo,
 	}
 
 	if (nodeinfo->magic != NODEINFO_MAGIC) {
-		error("get_nodeinfo: jobinfo magic bad");
+		error("get_nodeinfo: nodeinfo magic bad");
 		return SLURM_ERROR;
 	}
 
