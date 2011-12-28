f07038e9de749765d2a74e831e1d298900036147
Danny Auble <da@schedmd.com>
2011-12-28 13:29:42 -0800

    SVIEW - better formatting for reasons on a midplane.

--- src/sview/node_info.c
+++ src/sview/node_info.c
@@ -638,6 +638,7 @@ extern List create_node_info_list(node_info_msg_t *node_info_ptr,
 	}
 
 	for (i=0; i<node_info_ptr->record_count; i++) {
+		char *select_reason_str = NULL;
 		node_ptr = &(node_info_ptr->node_array[i]);
 
 		if (!node_ptr->name || (node_ptr->name[0] == '\0'))
@@ -656,10 +657,6 @@ extern List create_node_info_list(node_info_msg_t *node_info_ptr,
 		sview_node_info_ptr->node_ptr = node_ptr;
 		sview_node_info_ptr->pos = i;
 
-		slurm_get_select_nodeinfo(node_ptr->select_nodeinfo,
-					  SELECT_NODEDATA_EXTRA_INFO,
-					  0, &sview_node_info_ptr->reason);
-
 		if (node_ptr->reason &&
 		    (node_ptr->reason_uid != NO_VAL) && node_ptr->reason_time) {
 			struct passwd *pw = NULL;
@@ -671,11 +668,24 @@ extern List create_node_info_list(node_info_msg_t *node_info_ptr,
 					 node_ptr->reason_uid);
 			slurm_make_time_str(&node_ptr->reason_time,
 					    time_str, sizeof(time_str));
-			xstrfmtcat(sview_node_info_ptr->reason,
-				   "%s [%s@%s]",
-				   node_ptr->reason, user, time_str);
+			sview_node_info_ptr->reason = xstrdup_printf(
+				"%s [%s@%s]", node_ptr->reason, user, time_str);
 		} else if (node_ptr->reason)
-			xstrcat(sview_node_info_ptr->reason, node_ptr->reason);
+			sview_node_info_ptr->reason = xstrdup(node_ptr->reason);
+
+		slurm_get_select_nodeinfo(node_ptr->select_nodeinfo,
+					  SELECT_NODEDATA_EXTRA_INFO,
+					  0, &select_reason_str);
+		if (select_reason_str && select_reason_str[0]) {
+			if (sview_node_info_ptr->reason)
+				xstrfmtcat(sview_node_info_ptr->reason, "\n%s",
+					   select_reason_str);
+			else {
+				sview_node_info_ptr->reason = select_reason_str;
+				select_reason_str = NULL;
+			}
+		}
+		xfree(select_reason_str);
 
 
 		if (node_ptr->boot_time) {
