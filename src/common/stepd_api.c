ed31e6c7fdb5bcc1b0f0a8e3cbf5327604e64887
Morris Jette <jette@schedmd.com>
2012-03-21 11:30:50 -0700

    Modify the step completion RPC between slurmd and slurmstepd
    in the tightly coupled functions slurmd:stepd_completion and
    slurmstepd:_handle_completion, a jobacct structure is
    send from the main daemon to the step daemon to provide
    the statistics of the children slurmstepd and do the aggregation.
    
    The methodology used to send the structure is the use of
    jobacct_gather_g_{setinfo,getinfo} over a pipe (JOBACCT_DATA_PIPE).
    As {setinfo,getinfo} use a common internal lock and reading
    or writing to a pipe is equivalent to holding a lock, slurmd and
    slurmstepd have to avoid using both setinfo and getinfo over a
    pipe or deadlock situations can occured. For example :
    slurmd(lockforread,write)/slurmstepd(write,lockforread).
    
    This patch remove the call to jobacct_gather_g_setinfo in slurmd
    and the call to jobacct_gather_g_getinfo in slurmstepd ensuring
    that slurmd only do getinfo operations over a pipe and slurmstepd
    only do setinfo over a pipe. Instead jobacct_gather_g_{pack,unpack}
    are used to marshall/unmarshall the data for transmission over the
    pipe.
    Patch by Matthieu Hautreux, CEA.
    
    The patch committed here is a variation on the work by Matthieu.
    Specifically, the logic is added to slurmstepd to read a new format
    of RPC including an RPC version number and buffer with the data
    structure. The slurmd however will not send the RPC in the new format
    until SLURM version 2.5.

--- src/common/stepd_api.c
+++ src/common/stepd_api.c
@@ -872,6 +872,9 @@ rwfail:
 int
 stepd_completion(int fd, step_complete_msg_t *sent)
 {
+#if (SLURM_PROTOCOL_VERSION <= SLURM_2_4_PROTOCOL_VERSION)
+/* FIXME: Remove this code plus the read code from src/slurmd/slurmstepd/req.c
+ * in SLURM version 2.5 */
 	int req = REQUEST_STEP_COMPLETION;
 	int rc;
 	int errnum = 0;
@@ -891,6 +894,47 @@ stepd_completion(int fd, step_complete_msg_t *sent)
 	return rc;
 rwfail:
 	return -1;
+#else
+	int req = REQUEST_STEP_COMPLETION_V2;
+	int rc;
+	int errnum = 0;
+	Buf buffer;
+	int len = 0;
+	int version = SLURM_PROTOCOL_VERSION;
+
+	buffer = init_buf(0);
+
+	debug("Entering stepd_completion, range_first = %d, range_last = %d",
+	      sent->range_first, sent->range_last);
+	safe_write(fd, &req, sizeof(int));
+	safe_write(fd, &version, sizeof(int));
+	safe_write(fd, &sent->range_first, sizeof(int));
+	safe_write(fd, &sent->range_last, sizeof(int));
+	safe_write(fd, &sent->step_rc, sizeof(int));
+	/*
+	 * We must not use setinfo over a pipe with slurmstepd here 
+	 * Indeed, slurmd does a large use of getinfo over a pipe
+	 * with slurmstepd and doing the reverse can result in a deadlock
+	 * scenario with slurmstepd : 
+	 * slurmd(lockforread,write)/slurmstepd(write,lockforread)
+	 * Do pack/unpack instead to be sure of independances of 
+	 * slurmd and slurmstepd
+	 */
+	jobacct_gather_g_pack(sent->jobacct, SLURM_PROTOCOL_VERSION, buffer);
+	len = get_buf_offset(buffer);
+	safe_write(fd, &len, sizeof(int));
+	safe_write(fd, get_buf_data(buffer), len);
+	free_buf(buffer);
+
+	/* Receive the return code and errno */
+	safe_read(fd, &rc, sizeof(int));
+	safe_read(fd, &errnum, sizeof(int));
+
+	errno = errnum;
+	return rc;
+rwfail:
+	return -1;
+#endif
 }
 
 /* Wait for a file descriptor to be readable (up to 300 seconds).
--- src/slurmd/slurmstepd/req.c
+++ src/slurmd/slurmstepd/req.c
@@ -83,7 +83,8 @@ static int _handle_notify_job(int fd, slurmd_job_t *job, uid_t uid);
 static int _handle_suspend(int fd, slurmd_job_t *job, uid_t uid);
 static int _handle_resume(int fd, slurmd_job_t *job, uid_t uid);
 static int _handle_terminate(int fd, slurmd_job_t *job, uid_t uid);
-static int _handle_completion(int fd, slurmd_job_t *job, uid_t uid);
+static int _handle_completion(int fd, slurmd_job_t *job, uid_t uid,
+			      int protocol);
 static int _handle_stat_jobacct(int fd, slurmd_job_t *job, uid_t uid);
 static int _handle_task_info(int fd, slurmd_job_t *job);
 static int _handle_list_pids(int fd, slurmd_job_t *job);
@@ -517,7 +518,11 @@ _handle_request(int fd, slurmd_job_t *job, uid_t uid, gid_t gid)
 		break;
 	case REQUEST_STEP_COMPLETION:
 		debug("Handling REQUEST_STEP_COMPLETION");
-		rc = _handle_completion(fd, job, uid);
+		rc = _handle_completion(fd, job, uid, 1);
+		break;
+	case REQUEST_STEP_COMPLETION_V2:
+		debug("Handling REQUEST_STEP_COMPLETION");
+		rc = _handle_completion(fd, job, uid, 2);
 		break;
 	case REQUEST_STEP_TASK_INFO:
 		debug("Handling REQUEST_STEP_TASK_INFO");
@@ -1273,7 +1278,7 @@ rwfail:
 }
 
 static int
-_handle_completion(int fd, slurmd_job_t *job, uid_t uid)
+_handle_completion(int fd, slurmd_job_t *job, uid_t uid, int protocol)
 {
 	int rc = SLURM_SUCCESS;
 	int errnum = 0;
@@ -1281,6 +1286,10 @@ _handle_completion(int fd, slurmd_job_t *job, uid_t uid)
 	int last;
 	jobacctinfo_t *jobacct = NULL;
 	int step_rc;
+	char* buf;
+	int len;
+	Buf buffer;
+	int version;	/* For future use */
 
 	debug("_handle_completion for job %u.%u",
 	      job->jobid, job->stepid);
@@ -1297,11 +1306,32 @@ _handle_completion(int fd, slurmd_job_t *job, uid_t uid)
 		return SLURM_SUCCESS;
 	}
 
+	if (protocol >= 2)
+		safe_read(fd, &version, sizeof(int));
 	safe_read(fd, &first, sizeof(int));
 	safe_read(fd, &last, sizeof(int));
 	safe_read(fd, &step_rc, sizeof(int));
-	jobacct = jobacct_gather_g_create(NULL);
-	jobacct_gather_g_getinfo(jobacct, JOBACCT_DATA_PIPE, &fd);
+	if (protocol >= 2) {
+		/*
+		 * We must not use getinfo over a pipe with slurmd here 
+		 * Indeed, slurmstepd does a large use of setinfo over a pipe
+		 * with slurmd and doing the reverse can result in a deadlock
+		 * scenario with slurmd : 
+		 * slurmd(lockforread,write)/slurmstepd(write,lockforread)
+		 * Do pack/unpack instead to be sure of independances of 
+		 * slurmd and slurmstepd
+		 */
+		safe_read(fd, &len, sizeof(int));
+		buf = xmalloc(len);
+		safe_read(fd, buf, len);
+		buffer = create_buf(buf, len);
+		jobacct_gather_g_unpack(&jobacct, SLURM_PROTOCOL_VERSION,
+					buffer);
+		free_buf(buffer);
+	} else {
+		jobacct = jobacct_gather_g_create(NULL);
+		jobacct_gather_g_setinfo(jobacct, JOBACCT_DATA_PIPE, &fd);
+	}
 
 	/*
 	 * Record the completed nodes
