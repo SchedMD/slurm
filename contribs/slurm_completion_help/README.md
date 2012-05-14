cebe118db656b68f29d05eb8a6c24acf5abd7cae
Danny Auble <da@schedmd.com>
2012-05-14 10:41:28 -0700

    Code added to contribs dealing with auto completion of slurm commands. Original code by Damien François. <damien.francois@uclouvain.Be>

---
+++ contribs/slurm_completion_help/README.md
@@ -0,0 +1,45 @@
+slurm-helper
+============
+
+Bunch of helper files for the Slurm resource manager
+
+Vim syntax file
+---------------
+
+The Vim syntax file renders the Slurm batch submission scripts easier to read and to spot errors in the submission options.
+
+As submission scripts are indeed shell scripts, and all Slurm options are actually Shell comments, it can be difficult to spot errors in the options.
+
+This syntax file allows vim to understand the Slurm option and highlight them accordingly. Whenever possible, the syntax rules check the validity of the options and put in a special color what is not recognized as a valid option, or valid parameters values.
+
+__Installation__
+
+Under Linux or MacOS, simply copy the file in the directory
+
+    .vim/after/syntax/sh/
+
+or whatever shell other than ``sh`` you prefer.
+
+The syntax file is then read and applied on a Shell script after the usual syntax file has been processed.
+
+__Known issues__
+
+* Some regex needed to validate options or parameter values are not exactly correct, but should work in most cases.
+* Any new option unknown to the syntax file will be spotted as an error.
+
+Bash completion
+---------------
+
+The Bash completion script offers <TAB> completion for Slurm commands.
+
+At present the following Slurm commands are considered
+* scontrol
+* sreport
+
+__Instalation__
+
+Simply source the script in your .bashrc or .profile
+
+__Knwon issues__
+
+Keyword arguments are not auto-compelted beyond the first one.
---
+++ contribs/slurm_completion_help/slurm_completion.sh
@@ -0,0 +1,154 @@
+# Bash completion for Slurm
+# Maintainer: Dr. Damien François. <damien.francois@uclouvain.Be>
+# Last Change: April 11, 2012
+# Version: 091
+# URL: http://perso.uclouvain.be/damien.francois
+
+
+
+_scontrol()
+{
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	local prev=${COMP_WORDS[COMP_CWORD-1]}
+
+	local subopts=""
+
+	case "$prev" in
+	node)
+		local pprev=${COMP_WORDS[COMP_CWORD-2]}
+		if [[ "$pprev" == "show" ]]; then
+			subopts=$( scontrol show nodes | grep NodeName | cut -c 10- | cut -f 1 -d' ')
+		elif [[ "$pprev" == "update" ]]; then
+			subopts="NodeName= Features= Gres= Reason= State= Weight="
+		else
+			subopts=""
+		fi
+		;;
+
+	job)
+		local pprev=${COMP_WORDS[COMP_CWORD-2]}
+		if [[ "$pprev" == "show" ]]; then
+			subopts=$( scontrol -o show jobs | cut -d' ' -f 1 | cut -d'=' -f 2 )
+		elif [[ "$pprev" == "update" ]]; then
+			subopts="Account= Conn-Type= Contiguous= Dependency= EligibleTime="
+			subopts="$subopts ExcNodeList= Features= Geometry= Gres= JobId="
+			subopts="$subopts JobId= MinCpusNode= MinMemoryNode= MinTmpDiskNodea"
+			subopts="$subopts Name= Nice= NodeList= NumCPUs= NumNodes= NumTasks="
+			subopts="$subopts Partition= Priority= QOS= ReqCores= ReqNodelist="
+			subopts="$subopts ReqSockets= ReqThreads= Requeue= ReservationName="
+			subopts="$subopts Rotate= Shared= StartTime= TimeLimit= WCKey="
+		else
+			subopts=""
+		fi
+		;;
+	show)
+		subopts="config daemons job node partition reservation slurmd step topology"
+		subopts="$subopts hostlist hostnames"
+		;;
+	shutdown)
+		subopts="slurmctld controller"
+		;;
+	setdebug)
+		subopts="quiet fatal error info verbose debug debug2 debug3 debug4 debug5"
+		;;
+	notify | uhold | suspend |  release | requeue | resume | hold )
+		subopts=$( scontrol -o show jobs | cut -d' ' -f 1 | cut -d'=' -f 2 )
+		;;
+	step)
+		subopts="StepId= TimeLimit="
+		;;
+	delete)
+		subopts="PartitionName= Reservation="
+		;;
+	reservation)
+		subopts="Reservation= Accounts= Licences= NodeCnt= Nodes= StartTime="
+		subopts="$subopts EndTime= Duration= PartitionName=Flags= Features= Users="
+		;;
+	partition)
+		subopts="AllowGroups= AllocNodes= Alternate= Default= DefaultTime="
+		subopts="$subopts DisableRootJobs= Hidden= MaxNodes= MatxTime= MinNodes= Nodes="
+		subopts="$subopts PartitionName= PreemtpMode= Piority= RootOnly= Shared= State="
+		;;
+	create)
+		subopts="partition reservation"
+		;;
+	update)
+		subopts="job step node partition reservation"
+		;;
+	disable | enable | able | create | vacate | error | restart)
+		subopts=$( scontrol -o show jobs | cut -d' ' -f 1 | cut -d'=' -f 2 )
+		;;
+	checkpoint)
+		subopts="disable enable able create vacate error restart"
+		;;
+	scontrol)
+		if [[ "$cur" == - ]]; then
+			subopts="-a -d -h -M -o -Q -v -V"
+		elif [[ "$cur" == -- ]]; then
+			subopts="--all --details --help --hide --cluster"
+			subopts="$subopts --oneliner --quiet --verbose --version"
+		else
+			subopts="abort checkpoint create completing delete hold notify"
+			subopts="$subopts pidinfo listpids ping reconfigure release requeue"
+			subopts="$subopts resume setdebug show shutdown suspend takeover"
+			subopts="$subopts uhold update version"
+		fi
+		;;
+	*)
+		return 0
+		;;
+	esac
+	COMPREPLY=( $(compgen -W "${subopts}" -- $cur) )
+}
+complete -F _scontrol scontrol
+
+_sreport()
+{
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	local prev=${COMP_WORDS[COMP_CWORD-1]}
+
+	local subopts=""
+	local opts4all="All_Clusters Clusters= End= Format= Start="
+
+	case "$prev" in
+	user)
+		subopts="TopUsage"
+		;;
+	TopUsage)
+		subopts="$opts4all Accounts= Group TopCount= Users="
+		;;
+	reservation)
+		subopts="Utilization"
+		;;
+	Utilization)
+		subopts="$opts4all Names= Nodes="
+		;;
+	job)
+		subopts="SizesByAccount SizesByAccountAndWckey SizesByWckey"
+		;;
+	SizesByAccount|SizesByAccountAndWckey|SizesByWckey)
+		subopts="$opts4all Accounts= FlatView GID= Grouping= Jobs= Nodes= OPartitions= PrintJobCount Users= Wckeys="
+		;;
+	cluster)
+		subopts="AccountUtilizationByUser UserUtilizationByAccount UserUtilizationByWCKey Utilization WCKeyUtilizationByUser"
+		;;
+	AccountUtilizationByUser|UserUtilizationByAccount|UserUtilizationByWCKey|Utilization|WCKeyUtilizationByUser)
+		subopts="$opts4all Accounts= Tree Users= Wckeys="
+		;;
+	sreport)
+		if [[ "$cur" == - ]]; then
+			subopts="-a -n -h -p -P -t -v -V"
+		elif [[ "$cur" == -- ]]; then
+			subopts="--all_clusters --help --noheader --parsable"
+			subopts="$subopts --parsable2--quiet --verbose --version"
+		else
+			subopts="cluster job user reservation"
+		fi
+		;;
+	*)
+		return 0
+		;;
+	esac
+	COMPREPLY=( $(compgen -W "${subopts}" -- $cur) )
+}
+complete -F _sreport sreport
