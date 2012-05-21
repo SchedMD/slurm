f4e5da77a437da86581642f0011d90240f7400b2
Danny Auble <da@schedmd.com>
2012-05-21 10:34:51 -0700

    convert to generic context for mpi plugins

--- src/common/mpi.c
+++ src/common/mpi.c
@@ -69,147 +69,32 @@ typedef struct slurm_mpi_ops {
 	int          (*client_fini)       (mpi_plugin_client_state_t *);
 } slurm_mpi_ops_t;
 
-struct slurm_mpi_context {
-	char *			mpi_type;
-	plugrack_t              plugin_list;
-	plugin_handle_t         cur_plugin;
-	int                     mpi_errno;
-	slurm_mpi_ops_t	        ops;
+/*
+ * These strings must be kept in the same order as the fields
+ * declared for slurm_mpi_ops_t.
+ */
+static const char *syms[] = {
+	"p_mpi_hook_slurmstepd_prefork",
+	"p_mpi_hook_slurmstepd_task",
+	"p_mpi_hook_client_prelaunch",
+	"p_mpi_hook_client_single_task_per_node",
+	"p_mpi_hook_client_fini"
 };
 
-static slurm_mpi_context_t g_context = NULL;
+static slurm_mpi_ops_t ops;
+static plugin_context_t *g_context = NULL;
 static pthread_mutex_t      context_lock = PTHREAD_MUTEX_INITIALIZER;
 
-static slurm_mpi_context_t
-_slurm_mpi_context_create(const char *mpi_type)
-{
-	slurm_mpi_context_t c;
-
-	if ( mpi_type == NULL ) {
-		debug3( "_slurm_mpi_context_create: no mpi type" );
-		return NULL;
-	}
-
-	c = xmalloc(sizeof(struct slurm_mpi_context));
-
-	c->mpi_errno = SLURM_SUCCESS;
-
-	/* Copy the job completion authentication type. */
-	c->mpi_type = xstrdup(mpi_type);
-	if (c->mpi_type == NULL ) {
-		debug3( "can't make local copy of mpi type" );
-		xfree(c);
-		return NULL;
-	}
-
-	/* Plugin rack is demand-loaded on first reference. */
-	c->plugin_list = NULL;
-	c->cur_plugin = PLUGIN_INVALID_HANDLE;
-
-	return c;
-}
-
-static int
-_slurm_mpi_context_destroy( slurm_mpi_context_t c )
-{
-	int rc = SLURM_SUCCESS;
-
-	/*
-	 * Must check return code here because plugins might still
-	 * be loaded and active.
-	 */
-	if ( c->plugin_list ) {
-		if ( plugrack_destroy( c->plugin_list ) != SLURM_SUCCESS ) {
-			rc = SLURM_ERROR;
-		}
-	} else {
-		plugin_unload(c->cur_plugin);
-	}
-
-	xfree(c->mpi_type);
-	xfree(c);
-
-	return rc;
-}
-
-/*
- * Resolve the operations from the plugin.
- */
-static slurm_mpi_ops_t *
-_slurm_mpi_get_ops( slurm_mpi_context_t c )
-{
-	/*
-	 * These strings must be kept in the same order as the fields
-	 * declared for slurm_mpi_ops_t.
-	 */
-	static const char *syms[] = {
-		"p_mpi_hook_slurmstepd_prefork",
-		"p_mpi_hook_slurmstepd_task",
-		"p_mpi_hook_client_prelaunch",
-		"p_mpi_hook_client_single_task_per_node",
-		"p_mpi_hook_client_fini"
-	};
-	int n_syms = sizeof( syms ) / sizeof( char * );
-	char *plugin_dir = NULL;
-
-	/* Find the correct plugin. */
-        c->cur_plugin = plugin_load_and_link(c->mpi_type, n_syms, syms,
-					     (void **) &c->ops);
-        if ( c->cur_plugin != PLUGIN_INVALID_HANDLE )
-        	return &c->ops;
-
-	error("Couldn't find the specified plugin name for %s "
-	      "looking at all files",
-	      c->mpi_type);
-
-	/* Get the plugin list, if needed. */
-	if ( c->plugin_list == NULL ) {
-		c->plugin_list = plugrack_create();
-		if ( c->plugin_list == NULL ) {
-			error("Unable to create a plugin manager");
-			return NULL;
-		}
-
-		plugrack_set_major_type(c->plugin_list, "mpi");
-		plugrack_set_paranoia(c->plugin_list,
-				      PLUGRACK_PARANOIA_NONE,
-				      0);
-		plugin_dir = slurm_get_plugin_dir();
-		plugrack_read_dir(c->plugin_list, plugin_dir);
-		xfree(plugin_dir);
-	}
-
-	if (strcasecmp (c->mpi_type, "mpi/list") == 0) {
-		plugrack_print_all_plugin(c->plugin_list);
-		exit(0);
-	} else {
-		/* Find the correct plugin. */
-		c->cur_plugin = plugrack_use_by_type(c->plugin_list,
-						     c->mpi_type);
-		if ( c->cur_plugin == PLUGIN_INVALID_HANDLE ) {
-			error("can't find a valid plugin for type %s",
-				c->mpi_type);
-			return NULL;
-		}
-	}
-
-	/* Dereference the API. */
-	if ( plugin_get_syms( c->cur_plugin,
-				n_syms,
-				syms,
-				(void **) &c->ops ) < n_syms ) {
-		error( "incomplete mpi plugin detected" );
-		return NULL;
-	}
-
-	return &c->ops;
-}
 
 int _mpi_init (char *mpi_type)
 {
 	int retval = SLURM_SUCCESS;
-	char *full_type = NULL;
+	char *plugin_type = "mpi";
+	char *type = NULL;
 	int got_default = 0;
+
+	if ( g_context )
+		return retval;
 
 	slurm_mutex_lock( &context_lock );
 
@@ -225,28 +110,21 @@ int _mpi_init (char *mpi_type)
 		retval = SLURM_ERROR;
 		goto done;
 	}
-	setenvf (NULL, "SLURM_MPI_TYPE", "%s", mpi_type);
+	setenvf(NULL, "SLURM_MPI_TYPE", "%s", mpi_type);
 
-	full_type = xmalloc(sizeof(char) * (strlen(mpi_type)+5));
-	sprintf(full_type,"mpi/%s",mpi_type);
+	type = xstrdup_printf("mpi/%s", mpi_type);
 
-	g_context = _slurm_mpi_context_create(full_type);
-	xfree(full_type);
-	if ( g_context == NULL ) {
-		error( "cannot create a context for %s", mpi_type);
+	g_context = plugin_context_create(
+		plugin_type, type, (void **)&ops, syms, sizeof(syms));
+
+	if (!g_context) {
+		error("cannot create %s context for %s", plugin_type, type);
 		retval = SLURM_ERROR;
 		goto done;
 	}
 
-	if ( _slurm_mpi_get_ops( g_context ) == NULL ) {
-		error( "cannot resolve plugin operations for %s", mpi_type);
-		_slurm_mpi_context_destroy( g_context );
-		g_context = NULL;
-		retval = SLURM_ERROR;
-	}
-
-
 done:
+	xfree(type);
 	if(got_default)
 		xfree(mpi_type);
 	slurm_mutex_unlock( &context_lock );
@@ -272,7 +150,7 @@ int mpi_hook_slurmstepd_prefork (const slurmd_job_t *job, char ***env)
 	if (mpi_hook_slurmstepd_init(env) == SLURM_ERROR)
 		return SLURM_ERROR;
 
-	return (*(g_context->ops.slurmstepd_prefork))(job, env);
+	return (*(ops.slurmstepd_prefork))(job, env);
 }
 
 int mpi_hook_slurmstepd_task (const mpi_plugin_task_info_t *job, char ***env)
@@ -280,7 +158,7 @@ int mpi_hook_slurmstepd_task (const mpi_plugin_task_info_t *job, char ***env)
 	if (mpi_hook_slurmstepd_init(env) == SLURM_ERROR)
 		return SLURM_ERROR;
 
-	return (*(g_context->ops.slurmstepd_init))(job, env);
+	return (*(ops.slurmstepd_init))(job, env);
 }
 
 int mpi_hook_client_init (char *mpi_type)
@@ -299,7 +177,7 @@ mpi_hook_client_prelaunch(const mpi_plugin_client_info_t *job, char ***env)
 	if (_mpi_init(NULL) < 0)
 		return NULL;
 
-	return (*(g_context->ops.client_prelaunch))(job, env);
+	return (*(ops.client_prelaunch))(job, env);
 }
 
 bool mpi_hook_client_single_task_per_node (void)
@@ -313,7 +191,7 @@ bool mpi_hook_client_single_task_per_node (void)
 	*/
 	return true;
 #else
-	return (*(g_context->ops.client_single_task))();
+	return (*(ops.client_single_task))();
 #endif
 }
 
@@ -322,7 +200,7 @@ int mpi_hook_client_fini (mpi_plugin_client_state_t *state)
 	if (_mpi_init(NULL) < 0)
 		return SLURM_ERROR;
 
-	return (*(g_context->ops.client_fini))(state);
+	return (*(ops.client_fini))(state);
 }
 
 int mpi_fini (void)
@@ -332,7 +210,7 @@ int mpi_fini (void)
 	if (!g_context)
 		return SLURM_SUCCESS;
 
-	rc = _slurm_mpi_context_destroy(g_context);
+	rc = plugin_context_destroy(g_context);
 	return rc;
 }
 
