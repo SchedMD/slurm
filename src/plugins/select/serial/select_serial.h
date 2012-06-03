d633771e7856837edee08199bd73481884643e73
jette <jette@schedmd.com>
2012-06-03 10:25:08 -0700

    Modify select/serial to start up to 10000 jobs per node

--- src/plugins/select/serial/select_serial.h
+++ src/plugins/select/serial/select_serial.h
@@ -78,9 +78,9 @@
  * preemption, which can override these protections.
  */
 enum node_cr_state {
-	NODE_CR_AVAILABLE = 0,  /* The node may be IDLE or IN USE (shared) */
-	NODE_CR_ONE_ROW = 1,    /* node is in use by Shared=NO part */
-	NODE_CR_RESERVED = 100, /* node is in use by Shared=EXCLUSIVE part */
+	NODE_CR_AVAILABLE = 0,    /* The node may be IDLE or IN USE (shared) */
+	NODE_CR_ONE_ROW = 1,      /* node is in use by Shared=NO part */
+	NODE_CR_RESERVED = 10000, /* node is in use by Shared=EXCLUSIVE part */
 };
 
 /* a partition's per-row CPU allocation data */
