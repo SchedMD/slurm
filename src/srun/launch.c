afe6aeb025cdc94a4428c3bac74d6450bbad28a5
Danny Auble <da@schedmd.com>
2012-06-04 15:46:40 -0700

    format lines

--- src/srun/launch.c
+++ src/srun/launch.c
@@ -1,36 +1,36 @@
- /*  launch.c - Define job launch plugin functions.
- *****************************************************************************
- *  Copyright (C) 2012 SchedMD LLC
- *  Written by Danny Auble <da@schedmd.com>
- *
- *  This file is part of SLURM, a resource management program.
- *  For details, see <http://www.schedmd.com/slurmdocs/>.
- *  Please also read the included file: DISCLAIMER.
- *
- *  SLURM is free software; you can redistribute it and/or modify it under
- *  the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the License, or (at your option)
- *  any later version.
- *
- *  In addition, as a special exception, the copyright holders give permission
- *  to link the code of portions of this program with the OpenSSL library under
- *  certain conditions as described in each individual source file, and
- *  distribute linked combinations including the two. You must obey the GNU
- *  General Public License in all respects for all of the code used other than
- *  OpenSSL. If you modify file(s) with this exception, you may extend this
- *  exception to your version of the file(s), but you are not obligated to do
- *  so. If you do not wish to do so, delete this exception statement from your
- *  version.  If you delete this exception statement from all source files in
- *  the program, then also delete it here.
- *
- *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- *  details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with SLURM; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+/*  launch.c - Define job launch plugin functions.
+*****************************************************************************
+*  Copyright (C) 2012 SchedMD LLC
+*  Written by Danny Auble <da@schedmd.com>
+*
+*  This file is part of SLURM, a resource management program.
+*  For details, see <http://www.schedmd.com/slurmdocs/>.
+*  Please also read the included file: DISCLAIMER.
+*
+*  SLURM is free software; you can redistribute it and/or modify it under
+*  the terms of the GNU General Public License as published by the Free
+*  Software Foundation; either version 2 of the License, or (at your option)
+*  any later version.
+*
+*  In addition, as a special exception, the copyright holders give permission
+*  to link the code of portions of this program with the OpenSSL library under
+*  certain conditions as described in each individual source file, and
+*  distribute linked combinations including the two. You must obey the GNU
+*  General Public License in all respects for all of the code used other than
+*  OpenSSL. If you modify file(s) with this exception, you may extend this
+*  exception to your version of the file(s), but you are not obligated to do
+*  so. If you do not wish to do so, delete this exception statement from your
+*  version.  If you delete this exception statement from all source files in
+*  the program, then also delete it here.
+*
+*  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+*  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+*  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+*  details.
+*
+*  You should have received a copy of the GNU General Public License along
+*  with SLURM; if not, write to the Free Software Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
 #include <stdlib.h>
@@ -51,8 +51,8 @@ typedef struct {
 				    sig_atomic_t *destroy_job);
 	int (*step_launch)         (srun_job_t *job,
 				    slurm_step_io_fds_t *cio_fds,
-				    uint32_t *global_rc, bool got_alloc);
-	int (*step_wait)           (srun_job_t *job);
+				    uint32_t *global_rc);
+	int (*step_wait)           (srun_job_t *job, bool got_alloc);
 	int (*step_terminate)      (void);
 	void (*print_status)       (void);
 	void (*fwd_signal)         (int signal);
@@ -131,7 +131,7 @@ extern slurm_step_layout_t *launch_common_get_slurm_step_layout(srun_job_t *job)
 
 	slurm_step_ctx_get(job->step_ctx, SLURM_STEP_CTX_RESP, &resp);
 	if (!resp)
-	    return (NULL);
+		return (NULL);
 	return (resp->step_layout);
 }
 
@@ -338,27 +338,26 @@ extern int launch_g_create_job_step(srun_job_t *job, bool use_all_cpus,
 	if (launch_init() < 0)
 		return SLURM_ERROR;
 
-	return (*(ops.create_job_step))(job, use_all_cpus,
-							signal_function,
-							destroy_job);
+	return (*(ops.create_job_step))(
+		job, use_all_cpus, signal_function, destroy_job);
 }
 
 extern int launch_g_step_launch(
 	srun_job_t *job, slurm_step_io_fds_t *cio_fds,
-	uint32_t *global_rc, bool got_alloc)
+	uint32_t *global_rc)
 {
 	if (launch_init() < 0)
 		return SLURM_ERROR;
 
-	return (*(ops.step_launch))(job, cio_fds, global_rc,
-						    got_alloc);
+	return (*(ops.step_launch))(job, cio_fds, global_rc);
+}
 
-extern int launch_g_step_wait(srun_job_t *job)
+extern int launch_g_step_wait(srun_job_t *job, bool got_alloc)
 {
 	if (launch_init() < 0)
 		return SLURM_ERROR;
 
-	return (*(ops.step_wait))(job);
+	return (*(ops.step_wait))(job, got_alloc);
 }
 
 extern int launch_g_step_terminate(void)
