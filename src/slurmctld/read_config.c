7fc48554b86d91720b1210b9d270cf0f57862829
Morris Jette <jette@schedmd.com>
2012-06-20 14:29:58 -0700

    Fix bug in gang scheduling table initialization
    Without this fix, gang scheduling mode could start without creating
    a list resulting in an assert when jobs are submitted.

--- src/slurmctld/read_config.c
+++ src/slurmctld/read_config.c
@@ -115,7 +115,7 @@ static int  _sync_nodes_to_jobs(void);
 static int  _sync_nodes_to_active_job(struct job_record *job_ptr);
 static void _sync_nodes_to_suspended_job(struct job_record *job_ptr);
 static void _sync_part_prio(void);
-static int  _update_preempt(uint16_t old_enable_preempt);
+static int  _update_preempt(uint16_t old_enable_preempt, bool reconfig);
 #ifdef 	HAVE_ELAN
 static void _validate_node_proc_count(void);
 #endif
@@ -877,7 +877,7 @@ int read_slurm_conf(int recover, bool reconfig)
 			fatal( "failed to initialize preempt plugin" );
 	}
 	xfree(old_preempt_type);
-	rc = _update_preempt(old_preempt_mode);
+	rc = _update_preempt(old_preempt_mode, reconfig);
 	error_code = MAX(error_code, rc);	/* not fatal */
 
 	/* Update plugin parameters as possible */
@@ -1318,12 +1318,13 @@ static int  _preserve_select_type_param(slurm_ctl_conf_t *ctl_conf_ptr,
 
 /* Start or stop the gang scheduler module as needed based upon changes in
  *	configuration */
-static int _update_preempt(uint16_t old_preempt_mode)
+static int _update_preempt(uint16_t old_preempt_mode, bool reconfig)
 {
 	uint16_t new_preempt_mode = slurm_get_preempt_mode();
 
-	if ((old_preempt_mode & PREEMPT_MODE_GANG) ==
-	    (new_preempt_mode & PREEMPT_MODE_GANG))
+	if (reconfig &&
+	    ((old_preempt_mode & PREEMPT_MODE_GANG) ==
+	     (new_preempt_mode & PREEMPT_MODE_GANG)))
 		return SLURM_SUCCESS;
 
 	if (new_preempt_mode & PREEMPT_MODE_GANG) {
