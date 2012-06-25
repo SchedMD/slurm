9955b7df245f1160f204cf707c6deaa8c9f58e22
Danny Auble <da@schedmd.com>
2012-06-25 10:35:10 -0700

    same as last patch for alloc_cpus -> cpus_alloc

--- src/plugins/accounting_storage/mysql/as_mysql_jobacct_process.c
+++ src/plugins/accounting_storage/mysql/as_mysql_jobacct_process.c
@@ -1202,11 +1202,11 @@ extern int setup_job_cond_limits(mysql_conn_t *mysql_conn,
 			xstrcat(*extra, " where (");
 
 		if (job_cond->cpus_max) {
-			xstrfmtcat(*extra, "(t1.alloc_cpus between %u and %u))",
+			xstrfmtcat(*extra, "(t1.cpus_alloc between %u and %u))",
 				   job_cond->cpus_min, job_cond->cpus_max);
 
 		} else {
-			xstrfmtcat(*extra, "(t1.alloc_cpus='%u'))",
+			xstrfmtcat(*extra, "(t1.cpus_alloc='%u'))",
 				   job_cond->cpus_min);
 
 		}
