SchedMD/slurm

commit e7c17c70a899fb98c9054272ee078f67b2f7e4fc

Backport some performance enhancement work from serial branch
This is a backport of plugin initialization logic made in the
select/serial branch that we want to have in SLURM v2.4 and not
only in v2.5. Note the plugin logic in v2.5 is different and
this changes to not apply

src/common/gres.c
@@ -338,6 +338,9 @@ extern int gres_plugin_init(void)
 	int i, j, rc = SLURM_SUCCESS;
 	char *last = NULL, *names, *one_name, *full_name;
 
+	if (gres_context_cnt >= 0)
+		return rc;
+
 	slurm_mutex_lock(&gres_context_lock);
 	if (slurm_get_debug_flags() & DEBUG_FLAG_GRES)
 		gres_debug = true;

src/common/mpi.c
@@ -211,6 +211,9 @@ int _mpi_init (char *mpi_type)
 	char *full_type = NULL;
 	int got_default = 0;
 
+	if ( g_context )
+		return retval;
+
 	slurm_mutex_lock( &context_lock );
 
 	if ( g_context )

src/common/node_select.c
@@ -281,6 +281,9 @@ extern int slurm_select_init(bool only_default)
 	struct dirent *e;
 	char *dir_array = NULL, *head = NULL;
 
+	if ( select_context )
+		return retval;
+
 	slurm_mutex_lock( &select_context_lock );
 
 	if ( select_context )

src/common/slurm_accounting_storage.c
@@ -388,6 +388,9 @@ extern int slurm_acct_storage_init(char *loc)
 	int retval = SLURM_SUCCESS;
 	char *acct_storage_type = NULL;
 
+	if ( g_acct_storage_context )
+		return retval;
+
 	slurm_mutex_lock( &g_acct_storage_context_lock );
 
 	if ( g_acct_storage_context )

src/common/slurm_auth.c
@@ -315,6 +315,9 @@ extern int slurm_auth_init( char *auth_type )
         int retval = SLURM_SUCCESS;
 	char *auth_type_local = NULL;
 
+        if ( g_context )
+		return retval;
+
         slurm_mutex_lock( &context_lock );
 
         if ( g_context )

src/common/slurm_jobacct_gather.c
@@ -258,6 +258,9 @@ static int _slurm_jobacct_gather_init(void)
 	char	*jobacct_gather_type = NULL;
 	int	retval=SLURM_SUCCESS;
 
+	if ( g_jobacct_gather_context )
+		return(retval);
+
 	slurm_mutex_lock( &g_jobacct_gather_context_lock );
 	if ( g_jobacct_gather_context )
 		goto done;

src/common/slurm_priority.c
@@ -198,6 +198,9 @@ extern int slurm_priority_init(void)
 	int retval = SLURM_SUCCESS;
 	char *priority_type = NULL;
 
+	if ( g_priority_context )
+		return retval;
+
 	slurm_mutex_lock( &g_priority_context_lock );
 
 	if ( g_priority_context )

src/common/slurm_topology.c
@@ -207,6 +207,9 @@ slurm_topo_init( void )
 	int retval = SLURM_SUCCESS;
 	char *topo_type = NULL;
 
+	if ( g_topo_context )
+		return retval;
+
 	slurm_mutex_lock( &g_topo_context_lock );
 
 	if ( g_topo_context )

commit 7bed576b42d68b95182aa6f48788e78daf643d8c

Correct plugin race condition introduced in
https://github.com/SchedMD/slurm/commit/e7c17c70a899fb98c9054272ee078f67b2f7e4fc

src/common/gres.c
@@ -124,6 +124,7 @@ static slurm_gres_context_t *gres_context = NULL;
 static char *gres_plugin_list = NULL;
 static pthread_mutex_t gres_context_lock = PTHREAD_MUTEX_INITIALIZER;
 static List gres_conf_list = NULL;
+static bool init_run = false;
 
 /* Local functions */
 static gres_node_state_t *
@@ -338,7 +339,7 @@ extern int gres_plugin_init(void)
 	int i, j, rc = SLURM_SUCCESS;
 	char *last = NULL, *names, *one_name, *full_name;
 
-	if (gres_context_cnt >= 0)
+	if (init_run && (gres_context_cnt >= 0))
 		return rc;
 
 	slurm_mutex_lock(&gres_context_lock);
@@ -406,6 +407,7 @@ extern int gres_plugin_init(void)
 		gres_context[i].gres_name_colon_len =
 			strlen(gres_context[i].gres_name_colon);
 	}
+	init_run = true;
 
 fini:	slurm_mutex_unlock(&gres_context_lock);
 	return rc;
@@ -424,6 +426,7 @@ extern int gres_plugin_fini(void)
 	if (gres_context_cnt < 0)
 		goto fini;
 
+	init_run = false;
 	for (i=0; i<gres_context_cnt; i++) {
 		j = _unload_gres_plugin(gres_context + i);
 		if (j != SLURM_SUCCESS)

src/common/mpi.c
@@ -79,6 +79,7 @@ struct slurm_mpi_context {
 
 static slurm_mpi_context_t g_context = NULL;
 static pthread_mutex_t      context_lock = PTHREAD_MUTEX_INITIALIZER;
+static bool init_run = false;
 
 static slurm_mpi_context_t
 _slurm_mpi_context_create(const char *mpi_type)
@@ -211,7 +212,7 @@ int _mpi_init (char *mpi_type)
 	char *full_type = NULL;
 	int got_default = 0;
 
-	if ( g_context )
+	if ( init_run && g_context )
 		return retval;
 
 	slurm_mutex_lock( &context_lock );
@@ -247,7 +248,7 @@ int _mpi_init (char *mpi_type)
 		g_context = NULL;
 		retval = SLURM_ERROR;
 	}
-
+	init_run = true;
 
 done:
 	if(got_default)
@@ -335,6 +336,7 @@ int mpi_fini (void)
 	if (!g_context)
 		return SLURM_SUCCESS;
 
+	init_run = false;
 	rc = _slurm_mpi_context_destroy(g_context);
 	return rc;
 }

src/common/node_select.c
@@ -66,6 +66,7 @@ static int select_context_default = -1;
 static slurm_select_context_t * select_context = NULL;
 static pthread_mutex_t		select_context_lock =
 	PTHREAD_MUTEX_INITIALIZER;
+static bool init_run = false;
 
 /*
  * Locate and load the appropriate plugin
@@ -282,7 +283,7 @@ extern int slurm_select_init(bool only_default)
 	struct dirent *e;
 	char *dir_array = NULL, *head = NULL;
 
-	if ( select_context )
+	if ( init_run && select_context )
 		return retval;
 
 	slurm_mutex_lock( &select_context_lock );
@@ -453,6 +454,7 @@ skip_load_all:
 		}
 
 	}
+	init_run = true;
 
 done:
 	slurm_mutex_unlock( &select_context_lock );
@@ -469,6 +471,7 @@ extern int slurm_select_fini(void)
 	if (!select_context)
 		goto fini;
 
+	init_run = false;
 	for (i=0; i<select_context_cnt; i++) {
 		j = _select_context_destroy(select_context + i);
 		if (j != SLURM_SUCCESS)

src/common/slurm_accounting_storage.c
@@ -201,6 +201,7 @@ typedef struct slurm_acct_storage_context {
 static slurm_acct_storage_context_t * g_acct_storage_context = NULL;
 static pthread_mutex_t		g_acct_storage_context_lock =
 	PTHREAD_MUTEX_INITIALIZER;
+static bool init_run = false;
 
 /*
  * Local functions
@@ -388,7 +389,7 @@ extern int slurm_acct_storage_init(char *loc)
 	int retval = SLURM_SUCCESS;
 	char *acct_storage_type = NULL;
 
-	if ( g_acct_storage_context )
+	if ( init_run && g_acct_storage_context )
 		return retval;
 
 	slurm_mutex_lock( &g_acct_storage_context_lock );
@@ -414,7 +415,8 @@ extern int slurm_acct_storage_init(char *loc)
 		_acct_storage_context_destroy( g_acct_storage_context );
 		g_acct_storage_context = NULL;
 		retval = SLURM_ERROR;
-	}
+	} else
+		init_run = true;
 
 done:
 	slurm_mutex_unlock( &g_acct_storage_context_lock );
@@ -429,6 +431,7 @@ extern int slurm_acct_storage_fini(void)
 	if (!g_acct_storage_context)
 		return SLURM_SUCCESS;
 
+	init_run = false;
 //	(*(g_acct_storage_context->ops.acct_storage_fini))();
 	rc = _acct_storage_context_destroy( g_acct_storage_context );
 	g_acct_storage_context = NULL;

src/common/slurm_auth.c
@@ -106,6 +106,7 @@ struct slurm_auth_context {
  */
 static slurm_auth_context_t g_context    = NULL;
 static pthread_mutex_t      context_lock = PTHREAD_MUTEX_INITIALIZER;
+static bool init_run = false;
 
 /*
  * Order of advisory arguments passed to some of the plugins.
@@ -315,7 +316,7 @@ extern int slurm_auth_init( char *auth_type )
         int retval = SLURM_SUCCESS;
 	char *auth_type_local = NULL;
 
-        if ( g_context )
+        if ( init_run && g_context )
 		return retval;
 
         slurm_mutex_lock( &context_lock );
@@ -346,7 +347,8 @@ extern int slurm_auth_init( char *auth_type )
                 _slurm_auth_context_destroy( g_context );
                 g_context = NULL;
                 retval = SLURM_ERROR;
-        }
+        } else
+		init_run = true;
 
 done:
 	xfree(auth_type_local);
@@ -363,6 +365,7 @@ slurm_auth_fini( void )
 	if ( !g_context )
 		return SLURM_SUCCESS;
 
+	init_run = false;
 	rc = _slurm_auth_context_destroy( g_context );
 	g_context = NULL;
 	return rc;

src/common/slurm_jobacct_gather.c
@@ -107,6 +107,7 @@ typedef struct slurm_jobacct_gather_context {
 
 static slurm_jobacct_gather_context_t *g_jobacct_gather_context = NULL;
 static pthread_mutex_t g_jobacct_gather_context_lock = PTHREAD_MUTEX_INITIALIZER;
+static bool init_run = false;
 
 static int _slurm_jobacct_gather_init(void);
 
@@ -258,7 +259,7 @@ static int _slurm_jobacct_gather_init(void)
 	char	*jobacct_gather_type = NULL;
 	int	retval=SLURM_SUCCESS;
 
-	if ( g_jobacct_gather_context )
+	if (init_run &&  g_jobacct_gather_context )
 		return(retval);
 
 	slurm_mutex_lock( &g_jobacct_gather_context_lock );
@@ -281,7 +282,8 @@ static int _slurm_jobacct_gather_init(void)
 			g_jobacct_gather_context);
 		g_jobacct_gather_context = NULL;
 		retval = SLURM_ERROR;
-	}
+	} else
+		init_run = true;
 
 done:
 	slurm_mutex_unlock( &g_jobacct_gather_context_lock );
@@ -304,6 +306,7 @@ extern int slurm_jobacct_gather_fini(void)
 
 	slurm_mutex_lock( &g_jobacct_gather_context_lock );
 	if (g_jobacct_gather_context) {
+		init_run = false;
 		rc = _slurm_jobacct_gather_context_destroy(
 				g_jobacct_gather_context);
 		g_jobacct_gather_context = NULL;

src/common/slurm_priority.c
@@ -63,6 +63,7 @@ typedef struct slurm_priority_context {
 static slurm_priority_context_t * g_priority_context = NULL;
 static pthread_mutex_t		g_priority_context_lock =
 	PTHREAD_MUTEX_INITIALIZER;
+static bool init_run = false;
 
 /*
  * Local functions
@@ -198,7 +199,7 @@ extern int slurm_priority_init(void)
 	int retval = SLURM_SUCCESS;
 	char *priority_type = NULL;
 
-	if ( g_priority_context )
+	if ( init_run && g_priority_context )
 		return retval;
 
 	slurm_mutex_lock( &g_priority_context_lock );
@@ -221,7 +222,8 @@ extern int slurm_priority_init(void)
 		_priority_context_destroy( g_priority_context );
 		g_priority_context = NULL;
 		retval = SLURM_ERROR;
-	}
+	} else
+		init_run = true;
 
 done:
 	slurm_mutex_unlock( &g_priority_context_lock );
@@ -236,6 +238,7 @@ extern int slurm_priority_fini(void)
 	if (!g_priority_context)
 		return SLURM_SUCCESS;
 
+	init_run = false;
 	rc = _priority_context_destroy( g_priority_context );
 	g_priority_context = NULL;
 	return rc;

src/common/slurm_topology.c
@@ -75,7 +75,7 @@ typedef struct slurm_topo_context {
 
 static slurm_topo_context_t	*g_topo_context = NULL;
 static pthread_mutex_t		g_topo_context_lock = PTHREAD_MUTEX_INITIALIZER;
-
+static bool init_run = false;
 
 /* ************************************************************************ */
 /*  TAG(                       slurm_topo_get_ops                        )  */
@@ -207,7 +207,7 @@ slurm_topo_init( void )
 	int retval = SLURM_SUCCESS;
 	char *topo_type = NULL;
 
-	if ( g_topo_context )
+	if ( init_run && g_topo_context )
 		return retval;
 
 	slurm_mutex_lock( &g_topo_context_lock );
@@ -229,7 +229,8 @@ slurm_topo_init( void )
 		slurm_topo_context_destroy( g_topo_context );
 		g_topo_context = NULL;
 		retval = SLURM_ERROR;
-	}
+	} else
+		init_run = true;
 
 done:
 	slurm_mutex_unlock( &g_topo_context_lock );
@@ -248,6 +249,7 @@ slurm_topo_fini( void )
 	if (!g_topo_context)
 		return SLURM_SUCCESS;
 
+	init_run = false;
 	rc = slurm_topo_context_destroy(g_topo_context);
 	g_topo_context = NULL;
 	return rc;

src/common/switch.c
@@ -127,7 +127,7 @@ struct slurm_switch_context {
 
 static slurm_switch_context_t *g_context = NULL;
 static pthread_mutex_t      context_lock = PTHREAD_MUTEX_INITIALIZER;
-
+static bool init_run = false;
 
 static slurm_switch_context_t *
 _slurm_switch_context_create(const char *switch_type)
@@ -288,7 +288,7 @@ extern int switch_init( void )
 	int retval = SLURM_SUCCESS;
 	char *switch_type = NULL;
 
-	if ( g_context )
+	if ( init_run && g_context )
 		return retval;
 
 	slurm_mutex_lock( &context_lock );
@@ -309,7 +309,8 @@ extern int switch_init( void )
 		_slurm_switch_context_destroy( g_context );
 		g_context = NULL;
 		retval = SLURM_ERROR;
-	}
+	} else
+		init_run = true;
 
       done:
 	slurm_mutex_unlock( &context_lock );
@@ -324,6 +325,7 @@ extern int switch_fini(void)
 	if (!g_context)
 		return SLURM_SUCCESS;
 
+	init_run = false;
 	rc = _slurm_switch_context_destroy(g_context);
 	return rc;
 }

src/slurmctld/job_submit.c
@@ -93,6 +93,7 @@ static int submit_context_cnt = -1;
 static slurm_submit_context_t *submit_context = NULL;
 static char *submit_plugin_list = NULL;
 static pthread_mutex_t submit_context_lock = PTHREAD_MUTEX_INITIALIZER;
+static bool init_run = false;
 
 static int _load_submit_plugin(char *plugin_name,
 			       slurm_submit_context_t *plugin_context)
@@ -196,7 +197,7 @@ extern int job_submit_plugin_init(void)
 	int rc = SLURM_SUCCESS;
 	char *last = NULL, *names, *one_name;
 
-	if (submit_context_cnt >= 0)
+	if (init_run && (submit_context_cnt >= 0))
 		return rc;
 
 	slurm_mutex_lock(&submit_context_lock);
@@ -222,6 +223,7 @@ extern int job_submit_plugin_init(void)
 		one_name = strtok_r(NULL, ",", &last);
 	}
 	xfree(names);
+	init_run = true;
 
 fini:	slurm_mutex_unlock(&submit_context_lock);
 	return rc;
@@ -240,6 +242,7 @@ extern int job_submit_plugin_fini(void)
 	if (submit_context_cnt < 0)
 		goto fini;
 
+	init_run = false;
 	for (i=0; i<submit_context_cnt; i++) {
 		j = _unload_submit_plugin(submit_context + i);
 		if (j != SLURM_SUCCESS)

src/slurmctld/sched_plugin.c
@@ -83,7 +83,7 @@ typedef struct slurm_sched_context {
 
 static slurm_sched_context_t	*g_sched_context = NULL;
 static pthread_mutex_t		g_sched_context_lock = PTHREAD_MUTEX_INITIALIZER;
-
+static bool init_run = false;
 
 /* ************************************************************************ */
 /*  TAG(                       slurm_sched_get_ops                       )  */
@@ -225,7 +225,7 @@ slurm_sched_init( void )
 	int retval = SLURM_SUCCESS;
 	char *sched_type = NULL;
 
-	if ( g_sched_context )
+	if ( init_run && g_sched_context )
 		return retval;
 
 	slurm_mutex_lock( &g_sched_context_lock );
@@ -253,6 +253,7 @@ slurm_sched_init( void )
 	if ( (slurm_get_preempt_mode() & PREEMPT_MODE_GANG) &&
 	     (gs_init() != SLURM_SUCCESS))
 		error( "cannot start gang scheduler ");
+	init_run = true;
 
  done:
 	slurm_mutex_unlock( &g_sched_context_lock );
@@ -271,6 +272,7 @@ slurm_sched_fini( void )
 	if (!g_sched_context)
 		return SLURM_SUCCESS;
 
+	init_run = false;
 	rc = slurm_sched_context_destroy(g_sched_context);
 	g_sched_context = NULL;
 