SchedMD/slurm

commit 952401ec2a56da2b005e6870a0121ccb391736bc

Permit more than 100 jobs to be scheduled per node.

src/plugins/select/cons_res/select_cons_res.h
@@ -79,9 +79,9 @@
  * preemption, which can override these protections.
  */
 enum node_cr_state {
-	NODE_CR_AVAILABLE = 0,  /* The node may be IDLE or IN USE (shared) */
-	NODE_CR_ONE_ROW = 1,    /* node is in use by Shared=NO part */
-	NODE_CR_RESERVED = 100, /* node is in use by Shared=EXCLUSIVE part */
+	NODE_CR_AVAILABLE = 0,    /* The node may be IDLE or IN USE (shared) */
+	NODE_CR_ONE_ROW = 1,      /* node is in use by Shared=NO part */
+	NODE_CR_RESERVED = 10000, /* node is in use by Shared=EXCLUSIVE part */
 };
 
 /* a partition's per-row CPU allocation data */

commit de745d44a825c024bed486aa1db49b34b5ba1861

select/cons_res: fix bug in exclusive node allocation
This corrects a bug introduced in commit 952401ec2a56da2b005e6870a0121ccb391736bc
The NODE_CR_RESV value is being stored in a job as an 8-bit variable, so we
can not use a value of 10000 for an exclusive node allocation. It's value was
reduced to 250, which will be the maximum number of jobs that can be run on any
node. Problem demonstrated by "salloc -N1 --exclusive" allocating the job only
one CPU on the node rather than all CPUs on the node.

src/plugins/select/cons_res/select_cons_res.h
@@ -75,13 +75,15 @@
  * - Caution with NODE_CR_AVAILABLE: a Sharing partition could be full.
  *
  * - these values are staggered so that they can be incremented as multiple
- * jobs are allocated to each node. This is needed to be able to support
- * preemption, which can override these protections.
+ *   jobs are allocated to each node. This is needed to be able to support
+ *   preemption, which can override these protections.
+ * - An 8-bit field is used to store these values, so we can't run more than
+ *   254 jobs per node (NO_VAL)
  */
 enum node_cr_state {
 	NODE_CR_AVAILABLE = 0,    /* The node may be IDLE or IN USE (shared) */
 	NODE_CR_ONE_ROW = 1,      /* node is in use by Shared=NO part */
-	NODE_CR_RESERVED = 10000, /* node is in use by Shared=EXCLUSIVE part */
+	NODE_CR_RESERVED = 250,   /* node is in use by Shared=EXCLUSIVE part */
 };
 
 /* a partition's per-row CPU allocation data */