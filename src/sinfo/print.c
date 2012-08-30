54c569bf7213e0a9b4000b05c5d7c05cd4d9b75a
Don Albert <Don.Albert@Bull.com>
2012-08-30 15:45:19 -0700

    sinfo print's "*" after default partition name if field width not specified

--- src/sinfo/print.c
+++ src/sinfo/print.c
@@ -788,7 +788,7 @@ int _print_partition(sinfo_data_t * sinfo_data, int width,
 			char *tmp;
 			tmp = xstrdup(sinfo_data->part_info->name);
 			if (sinfo_data->part_info->flags & PART_FLAG_DEFAULT) {
-				if (strlen(tmp) < width)
+				if ( (strlen(tmp) < width) || (width == 0) )
 					xstrcat(tmp, "*");
 				else if (width > 0)
 					tmp[width-1] = '*';
