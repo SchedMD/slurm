c414f0f8c47a5b7360932f0a8638140e3bbb0d29
jcamps <jcamps@cnag-jcamps.adm.pcb.ub.es>
2012-10-18 16:08:00 +0200

    Fix: Number of fields adjusted in do_dump.

--- src/sacct/options.c
+++ src/sacct/options.c
@@ -1263,7 +1263,7 @@ void do_dump(void)
 			       job->blockid,	/* block id */
 			       "-");	/* reserved 1 */
 
-			printf("JOB_START 1 16 %d %d %s %d %d %d %s %s\n",
+			printf("JOB_START 1 17 %d %d %s %d %d %d %s %s\n",
 			       job->uid,
 			       job->gid,
 			       job->jobname,
@@ -1293,7 +1293,7 @@ void do_dump(void)
 				step->end = job->end;
 
 			gmtime_r(&step->end, &ts);
-			printf("JOB_STEP 1 50 %u %04d%02d%02d%02d%02d%02d ",
+			printf("JOB_STEP 1 41 %u %04d%02d%02d%02d%02d%02d ",
 			       step->stepid,
 			       1900+(ts.tm_year), 1+(ts.tm_mon), ts.tm_mday,
 			            ts.tm_hour, ts.tm_min, ts.tm_sec);
@@ -1350,7 +1350,7 @@ void do_dump(void)
 			       job->blockid,	/* block id */
 			       "-");	/* reserved 1 */
 			gmtime_r(&job->end, &ts);
-			printf("JOB_TERMINATED 1 50 %d ",
+			printf("JOB_TERMINATED 1 42 %d ",
 			       job->elapsed);
 			printf("%04d%02d%02d%02d%02d%02d ",
 			1900+(ts.tm_year), 1+(ts.tm_mon), ts.tm_mday,
