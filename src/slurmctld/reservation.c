4180ba2b904aea6e7c67399cd3540584dda27ef0
Morris Jette <jette@schedmd.com>
2012-10-30 10:19:05 -0700

    Cosmetic changes, improved logging in reservation logic

--- src/slurmctld/reservation.c
+++ src/slurmctld/reservation.c
@@ -3170,8 +3170,9 @@ fini:	FREE_NULL_BITMAP(save_bitmap);
 		info("pick_idle_nodes getting %s from select cons_res", str);
 		if (*core_bitmap) {
 			bit_fmt(str, (sizeof(str) - 1), *core_bitmap);
-			info("pick_idle_nodes getting coremap %s "
-			     "from select/cons_res", str);
+			info("pick_idle_nodes getting core_cnt:%d coremap:%s "
+			     "from select/cons_res",
+			     resv_desc_ptr->core_cnt, str);
 		}
 	}
 #endif
@@ -3546,7 +3547,7 @@ extern int job_test_resv(struct job_record *job_ptr, time_t *when,
 		list_iterator_destroy(iter);
 
 		if (slurm_get_debug_flags() & DEBUG_FLAG_RESERVATION) {
-			char *nodes=bitmap2node_name(*node_bitmap);
+			char *nodes = bitmap2node_name(*node_bitmap);
 			info("job_test_resv: job:%u reservation:%s nodes:%s",
 			     job_ptr->job_id, nodes, job_ptr->resv_name);
 			xfree(nodes);
