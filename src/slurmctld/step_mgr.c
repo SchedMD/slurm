b621f8fa7b4dda9478049c3f2397649c3c95d950
Morris Jette <jette@schedmd.com>
2012-11-07 13:53:24 -0800

    Energy Accounting bug, prevent divide by zero for step with no allocated CPUs

--- src/slurmctld/step_mgr.c
+++ src/slurmctld/step_mgr.c
@@ -2708,7 +2708,8 @@ extern int step_partial_comp(step_complete_msg_t *req, uid_t uid,
 	/* we have been adding task average frequencies for
 	 * jobacct->act_cpufreq so we need to divide with the
 	 * total number of tasks/cpus for the step average frequency */
-	step_ptr->jobacct->act_cpufreq /= step_ptr->cpu_count;
+	if (step_ptr->cpu_count)
+		step_ptr->jobacct->act_cpufreq /= step_ptr->cpu_count;
 
 	if (!step_ptr->exit_node_bitmap) {
 		/* initialize the node bitmap for exited nodes */
