db6115760fe8e69b44cbe863995b1c2cadd21788
Morris Jette <jette@schedmd.com>
2012-11-26 08:45:55 -0800

    Add missing header to avoid warning

--- src/plugins/switch/nrt/libpermapi/shr_64.c
+++ src/plugins/switch/nrt/libpermapi/shr_64.c
@@ -56,6 +56,7 @@
 #include "src/common/hostlist.h"
 #include "src/common/list.h"
 #include "src/common/log.h"
+#include "src/common/parse_time.h"
 #include "src/common/plugstack.h"
 #include "src/common/slurm_protocol_pack.h"
 #include "src/common/xmalloc.h"
