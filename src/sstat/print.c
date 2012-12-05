137dffa846ddb7888c94a32b6750b25e206131c4
Yiannis Georgiou <yiannis.georgiou@bull.net>
2012-12-05 12:19:48 -0800

    Energy RAPL updates
    1)updated scontrol show node to report "n/s" (not supported) for energy/power values in case  acct_gather_energy/rapl is configured and nodes do not support RAPL Previously, these values were reported as zero. The scontrol man page has been also updated to explain this.
    
    2)updated sstat / sacct to report no value for ConsumedEnergy in case acct_gather_energy/rapl is configured, a job/step is allocated and at least one node does not support RAPL. Previously for this case, sstat and sacct reported a partial value for ConsumedEnergy.
    
    3)updated the logging mechanism so that If acct_gather_energy/rapl is configured and a job/step is allocated any nodes that do not support RAPL, a single debug entry is made in the log file.  Previously, multiple repeating error messages were sent to the console and the log file.

--- src/sstat/print.c
+++ src/sstat/print.c
@@ -90,6 +90,7 @@ void print_fields(slurmdb_step_rec_t *step)
 	while ((field = list_next(print_fields_itr))) {
 		char *tmp_char = NULL;
 
+		memset(&outbuf, 0, sizeof(outbuf));
 		switch(field->type) {
 		case PRINT_AVECPU:
 
@@ -111,8 +112,9 @@ void print_fields(slurmdb_step_rec_t *step)
 					     (curr_inx == field_count));
 			break;
 		case PRINT_CONSUMED_ENERGY:
-
-			convert_num_unit((float)step->stats.consumed_energy,
+			if (!fuzzy_equal(step->stats.consumed_energy, NO_VAL))
+				convert_num_unit((float)
+						step->stats.consumed_energy,
 					 outbuf, sizeof(outbuf),
 					 UNIT_NONE);
 
