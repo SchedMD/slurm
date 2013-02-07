af29d9a7a759958a9c5653df374593577cd430d3
Danny Auble <da@schedmd.com>
2013-02-07 15:59:13 -0800

    When asking for job states with sacct default to 'now' instead of midnight of the current day.

--- src/sacct/options.c
+++ src/sacct/options.c
@@ -926,17 +926,22 @@ void parse_command_line(int argc, char **argv)
 	if(!job_cond->usage_start && !job_cond->step_list) {
 		struct tm start_tm;
 		job_cond->usage_start = time(NULL);
-
-		if (!localtime_r(&job_cond->usage_start, &start_tm)) {
-			error("Couldn't get localtime from %ld",
-			      (long)job_cond->usage_start);
-			return;
+		/* If we are looking for job states default to now.
+		   If not default to midnight of the current day.
+		*/
+		if (!job_cond->state_list
+		    || !list_count(job_cond->state_list)) {
+			if (!localtime_r(&job_cond->usage_start, &start_tm)) {
+				error("Couldn't get localtime from %ld",
+				      (long)job_cond->usage_start);
+				return;
+			}
+			start_tm.tm_sec = 0;
+			start_tm.tm_min = 0;
+			start_tm.tm_hour = 0;
+			start_tm.tm_isdst = -1;
+			job_cond->usage_start = mktime(&start_tm);
 		}
-		start_tm.tm_sec = 0;
-		start_tm.tm_min = 0;
-		start_tm.tm_hour = 0;
-		start_tm.tm_isdst = -1;
-		job_cond->usage_start = mktime(&start_tm);
 	}
 
 	if(verbosity > 0) {
