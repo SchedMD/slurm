fcef06b4f88cf5e7fc86a76176c00f11b42c92d1
Magnus Jonsson <magnus@hpc2n.umu.se>
2013-03-12 10:13:42 -0700

    Fix scheduling if node in more than one partition
    I found a bug in cons_res/select_p_select_nodeinfo_set_all.
    
    If a node is part of two (or more) partitions the code will only count the number of cores/cpus in the partition that has the most running jobs on that node.
    
    Patch attached to fix the problem.
    
    I also added an new function to bitstring to count the number of bits in an range (bit_set_count_range) and made a minor improvement of (bit_set_count) while reviewing the range version.
    
    Best regards,
    Magnus

--- src/plugins/select/cons_res/select_cons_res.c
+++ src/plugins/select/cons_res/select_cons_res.c
@@ -2200,7 +2200,7 @@ extern int select_p_select_nodeinfo_set_all(void)
 	struct part_res_record *p_ptr;
 	struct node_record *node_ptr = NULL;
 	int i=0, n=0, c, start, end;
-	uint16_t tmp, tmp_16 = 0;
+	uint16_t tmp, tmp_16 = 0, tmp_part;
 	static time_t last_set_all = 0;
 	uint32_t node_threads, node_cpus;
 	select_nodeinfo_t *nodeinfo = NULL;
@@ -2245,20 +2245,17 @@ extern int select_p_select_nodeinfo_set_all(void)
 		for (p_ptr = select_part_record; p_ptr; p_ptr = p_ptr->next) {
 			if (!p_ptr->row)
 				continue;
+			tmp_part = 0;
 			for (i = 0; i < p_ptr->num_rows; i++) {
 				if (!p_ptr->row[i].row_bitmap)
 					continue;
-				tmp = 0;
-				for (c = start; c < end; c++) {
-					if (bit_test(p_ptr->row[i].row_bitmap,
-						     c))
-						tmp++;
-				}
+				tmp = bit_set_count_range(p_ptr->row[i].row_bitmap,
+													start,end);
 				/* get the row with the largest cpu
 				   count on it. */
-				if (tmp > tmp_16)
-					tmp_16 = tmp;
+				tmp_part = MAX(tmp,tmp_part);
 			}
+			tmp_16 += tmp_part;
 		}
 
 		/* The minimum allocatable unit may a core, so scale
