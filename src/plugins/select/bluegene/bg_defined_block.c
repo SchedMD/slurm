54ddef91d54c673e46b5e8d5ad683050079101c5
Danny Auble <da@schedmd.com>
2013-04-17 02:01:22 -0700

    BGQ - redo - fix for handling half rack system in STATIC of OVERLAP mode to implicitly create full system block.

--- src/plugins/select/bluegene/bg_defined_block.c
+++ src/plugins/select/bluegene/bg_defined_block.c
@@ -340,6 +340,12 @@ extern int create_full_system_block(List bg_found_block_list)
 	if (bg_found_block_list) {
 		itr = list_iterator_create(bg_found_block_list);
 		while ((bg_record = (bg_record_t *) list_next(itr)) != NULL) {
+			/* Skip all small blocks since they are not
+			   the full system block.
+			*/
+			if (bg_record->cnode_cnt < bg_conf->mp_cnode_cnt)
+				continue;
+
 			if (!strcmp(name, bg_record->mp_str)) {
 				xfree(name);
 				list_iterator_destroy(itr);
@@ -363,15 +369,13 @@ extern int create_full_system_block(List bg_found_block_list)
 			if (bg_record->free_cnt)
 				continue;
 
+			/* Skip all small blocks since they are not
+			   the full system block.
+			*/
+			if (bg_record->cnode_cnt < bg_conf->mp_cnode_cnt)
+				continue;
+
 			if (!strcmp(name, bg_record->mp_str)) {
-				/* if the whole system block is just one MP
-				 * we need to match on more than just the
-				 * name - if this bg_record doesn't have a
-				 * MP worth of c-nodes, skip it.
-				 */
-				if (!larger && bg_conf->mp_cnode_cnt
-						> bg_record->cnode_cnt)
-					continue;
 				debug2("create_full_system_block: not "
 				       "implicitly adding full system block -"
 				       " block already defined");
