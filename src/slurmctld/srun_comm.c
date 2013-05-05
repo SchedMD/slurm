b0ed0501e7bbf8d537a41b949f6a119cdebaaa6b
Hongjia Cao <hjcao@nudt.edu.cn>
2013-05-05 10:35:43 -0700

    fix of slurmctld abort on node failure if affected job has pending steps

--- src/slurmctld/srun_comm.c
+++ src/slurmctld/srun_comm.c
@@ -172,6 +172,8 @@ extern void srun_node_fail (uint32_t job_id, char *node_name)
 
 	step_iterator = list_iterator_create(job_ptr->step_list);
 	while ((step_ptr = (struct step_record *) list_next(step_iterator))) {
+		if (step_ptr->step_node_bitmap == NULL)   /* pending step */
+			continue;
 		if ((bit_position >= 0) &&
 		    (!bit_test(step_ptr->step_node_bitmap, bit_position)))
 			continue;	/* job step not on this node */
