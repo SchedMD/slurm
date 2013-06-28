c6a65d05528a38e2d811b660eb5645c912448660
Morris Jette <jette@schedmd.com>
2013-06-28 09:28:14 -0700

    Change a comment for greater clarity

--- src/common/slurm_errno.c
+++ src/common/slurm_errno.c
@@ -395,7 +395,7 @@ static slurm_errtab_t slurm_errtab[] = {
 	{ ESLURM_BAD_NAME,
 	  "Unacceptable name given. (No '.' in name allowed)"   },
 
-	/* require_timelimit custom errors */
+	/* plugin and custom errors */
 	{ ESLURM_MISSING_TIME_LIMIT,
 	  "Missing time limit"                                  }
 };
