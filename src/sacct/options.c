912dd85035c9ab3b4be773641af05373fb029539
Danny Auble <da@schedmd.com>
2013-07-10 06:46:17 -0700

    sacct - Minor formatting change

--- src/sacct/options.c
+++ src/sacct/options.c
@@ -451,8 +451,8 @@ sacct [<OPTION>]                                                            \n\
                              mincputask,avecpu,ntasks,alloccpus,elapsed,    \n\
                              state,exitcode,avecpufreq,consumedenergy,      \n\
                              maxdiskread,maxdiskreadnode,maxdiskreadtask,   \n\
-                             avediskread, maxdiskwrite,maxdiskwritenode,    \n\
-                             maxdiskwritetask, avediskread'                 \n\
+                             avediskread,maxdiskwrite,maxdiskwritenode,     \n\
+                             maxdiskwritetask,avediskread'                  \n\
      -L, --allclusters:                                                     \n\
 	           Display jobs ran on all clusters. By default, only jobs  \n\
                    ran on the cluster from where sacct is called are        \n\
