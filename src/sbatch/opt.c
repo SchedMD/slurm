SchedMD/slurm

commit 1b7f5b91c18e747dd4a27fc1f64c000f04e78669

Fix sbatch use of ntasks-per-node with min+max node count specification
was setting task count to ntasks-per-node times min node count.
Now leaves ntasks unset

src/sbatch/opt.c
@@ -1515,7 +1515,7 @@ static void _set_options(int argc, char **argv)
 			break;
 		case LONG_OPT_NTASKSPERNODE:
 			opt.ntasks_per_node = _get_int(optarg,
-				"ntasks-per-node");
+						       "ntasks-per-node");
 			setenvf(NULL, "SLURM_NTASKS_PER_NODE", "%d",
 				opt.ntasks_per_node);
 			break;
@@ -2147,7 +2147,8 @@ static bool _opt_verify(void)
 	_fullpath(&opt.ifname, opt.cwd);
 	_fullpath(&opt.ofname, opt.cwd);
 
-	if ((opt.ntasks_per_node > 0) && (!opt.ntasks_set)) {
+	if ((opt.ntasks_per_node > 0) && (!opt.ntasks_set) &&
+	    (opt.max_nodes == 0)) {
 		opt.ntasks = opt.min_nodes * opt.ntasks_per_node;
 		opt.ntasks_set = 1;
 	}

commit 8ddec8b82f90652182cb7eddb7aa841e4fa4e639

Fix for batch job CPU count if --ntasks-per-node specified
and max_nodes == min_nodes or no max_nodes specified
bug introduced in commit 1b7f5b91c18e747dd4a27fc1f64c000f04e78669
fix for bug 343

src/sbatch/opt.c
@@ -2145,7 +2145,7 @@ static bool _opt_verify(void)
 				      opt.overcommit, 0);
 
 	if ((opt.ntasks_per_node > 0) && (!opt.ntasks_set) &&
-	    (opt.max_nodes == 0)) {
+	    ((opt.max_nodes == 0) || (opt.min_nodes == opt.max_nodes))) {
 		opt.ntasks = opt.min_nodes * opt.ntasks_per_node;
 		opt.ntasks_set = 1;
 	}