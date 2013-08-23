SchedMD/slurm

commit ac44db862c8d1f460e55ad09017d058942ff6499

Minor refactoring of squeue for single user/job_id optimizations

src/squeue/opts.c
@@ -394,7 +394,28 @@ parse_command_line( int argc, char* argv[] )
 		}
 	}
 
-	params.max_cpus = _max_cpus_per_node();
+	if (params.job_list && (list_count(params.job_list) == 1)) {
+		ListIterator iterator;
+		uint32_t *job_id_ptr;
+		iterator = list_iterator_create(params.job_list);
+		job_id_ptr = list_next(iterator);
+		params.job_id = *job_id_ptr;
+		list_iterator_destroy(iterator);
+	}
+	if (params.user_list && (list_count(params.user_list) == 1)) {
+		ListIterator iterator;
+		uint32_t *uid_ptr;
+		iterator = list_iterator_create(params.user_list);
+		while ((uid_ptr = list_next(iterator))) {
+			params.user_id = *uid_ptr;
+			break;
+		}
+		list_iterator_destroy(iterator);
+	}
+	if (params.job_id || params.user_id)
+		params.max_cpus = 1;	/* To minimize overhead */
+	else
+		params.max_cpus = _max_cpus_per_node();
 
 	if ( params.verbose )
 		_print_options();

src/squeue/squeue.h
@@ -102,6 +102,9 @@ struct squeue_parameters {
 	char* steps;
 	char* users;
 
+	uint32_t job_id;	/* set if request for a single job ID */
+	uint32_t user_id;	/* set if request for a single user ID */
+
 	List  account_list;
 	List  format_list;
 	List  job_list;

src/squeue/squeue.c
@@ -172,7 +172,6 @@ _print_job ( bool clear_old )
 	static job_info_msg_t * old_job_ptr = NULL, * new_job_ptr;
 	int error_code;
 	uint16_t show_flags = 0;
-	uint32_t job_id = 0;
 
 	if (params.all_flag || (params.job_list && list_count(params.job_list)))
 		show_flags |= SHOW_ALL;
@@ -181,22 +180,17 @@ _print_job ( bool clear_old )
 	if (params.format && strstr(params.format, "C"))
 		show_flags |= SHOW_DETAIL;
 
-	if (params.job_list && (list_count(params.job_list) == 1)) {
-		ListIterator iterator;
-		uint32_t *job_id_ptr;
-		iterator = list_iterator_create(params.job_list);
-		job_id_ptr = list_next(iterator);
-		job_id = *job_id_ptr;
-		list_iterator_destroy(iterator);
-	}
-
 	if (old_job_ptr) {
 		if (clear_old)
 			old_job_ptr->last_update = 0;
-		if (job_id) {
+		if (params.job_id) {
 			error_code = slurm_load_job(
-				&new_job_ptr, job_id,
+				&new_job_ptr, params.job_id,
 				show_flags);
+		} else if (params.user_id) {
+			error_code = slurm_load_job_user(&new_job_ptr,
+							 params.user_id,
+							 show_flags);
 		} else {
 			error_code = slurm_load_jobs(
 				old_job_ptr->last_update,
@@ -208,19 +202,12 @@ _print_job ( bool clear_old )
 			error_code = SLURM_SUCCESS;
 			new_job_ptr = old_job_ptr;
 		}
-	} else if (job_id) {
-		error_code = slurm_load_job(&new_job_ptr, job_id, show_flags);
-	} else if (params.user_list && (list_count(params.user_list) == 1)) {
-		ListIterator iterator;
-		uint32_t user_id = 0, *uid_ptr;
-		iterator = list_iterator_create(params.user_list);
-		while ((uid_ptr = list_next(iterator))) {
-			user_id = *uid_ptr;
-			break;
-		}
-		list_iterator_destroy(iterator);
-		error_code = slurm_load_job_user(&new_job_ptr, user_id,
-					     show_flags);
+	} else if (params.job_id) {
+		error_code = slurm_load_job(&new_job_ptr, params.job_id,
+					    show_flags);
+	} else if (params.user_id) {
+		error_code = slurm_load_job_user(&new_job_ptr, params.user_id,
+						 show_flags);
 	} else {
 		error_code = slurm_load_jobs((time_t) NULL, &new_job_ptr,
 					     show_flags);
@@ -231,7 +218,7 @@ _print_job ( bool clear_old )
 		return SLURM_ERROR;
 	}
 	old_job_ptr = new_job_ptr;
-	if (job_id)
+	if (params.job_id || params.job_id)
 		old_job_ptr->last_update = (time_t) 0;
 
 	if (params.verbose) {

commit 98e24b0dedc7af8b00c06ceaf99e67dbc6a1b5f2

Correct value of min_nodes returned by loading job info
This is a correction of a bug introduced in commit
https://github.com/SchedMD/slurm/commit/ac44db862c8d1f460e55ad09017d058942ff6499
That commit eliminated the need of reading the node state information
from squeue for performance reasons (mostly for large parallel systems
in which the Prolog ran squeue, which generates a lot of simultaneous
RPCs, slowing down the job launch process). It also assumed 1 CPU per
node. If a pending job specified a node count of 1 and a task count
larger than one, squeue was reporting the node count of the job as
the same as the task count. This patch moves that same calculation
of a pending job's minimum node count into slurmctld, so the squeue
still does not need to read the node information, but can report the
correct node count for pending jobs with minimal overhead.

src/slurmctld/job_mgr.c
@@ -6150,15 +6150,41 @@ void pack_job(struct job_record *dump_job_ptr, uint16_t show_flags, Buf buffer,
 	}
 }
 
+static int _find_node_max_cpu_cnt(void)
+{
+	int i, max_cpu_cnt = 1;
+	struct node_record *node_ptr = node_record_table_ptr;
+
+	for (i = 0; i < node_record_count; i++, node_ptr++) {
+#ifndef HAVE_BG
+		if (slurmctld_conf.fast_schedule) {
+			/* Only data from config_record used for scheduling */
+			max_cpu_cnt = MAX(max_cpu_cnt,
+					  node_ptr->config_ptr->cpus);
+		} else {
+#endif
+			/* Individual node data used for scheduling */
+			max_cpu_cnt = MAX(max_cpu_cnt, node_ptr->cpus);
+#ifndef HAVE_BG
+		}
+#endif
+	}
+	return max_cpu_cnt;
+}
+
 /* pack default job details for "get_job_info" RPC */
 static void _pack_default_job_details(struct job_record *job_ptr,
 				      Buf buffer, uint16_t protocol_version)
 {
+	static int max_cpu_cnt = -1;
 	int i;
 	struct job_details *detail_ptr = job_ptr->details;
 	char *cmd_line = NULL;
 	char *tmp = NULL;
 	uint32_t len = 0;
+
+	if (max_cpu_cnt == -1)
+		max_cpu_cnt = _find_node_max_cpu_cnt();
 
 	if (protocol_version >= SLURM_2_6_PROTOCOL_VERSION) {
 		if (detail_ptr) {
@@ -6210,7 +6236,14 @@ static void _pack_default_job_details(struct job_record *job_ptr,
 				pack32(job_ptr->total_nodes, buffer);
 				pack32((uint32_t) 0, buffer);
 			} else {
-				pack32(detail_ptr->min_nodes, buffer);
+				/* Use task count to help estimate min_nodes */
+				uint32_t min_nodes;
+				min_nodes = detail_ptr->num_tasks +
+					    max_cpu_cnt - 1;
+				min_nodes /= max_cpu_cnt;
+				min_nodes = MAX(min_nodes,
+						detail_ptr->min_nodes);
+				pack32(min_nodes, buffer);
 				pack32(detail_ptr->max_nodes, buffer);
 			}
 			pack16(detail_ptr->requeue,   buffer);

src/squeue/opts.c
@@ -79,7 +79,6 @@ static List  _build_step_list( char* str );
 static List  _build_user_list( char* str );
 static char *_get_prefix(char *token);
 static void  _help( void );
-static int   _max_cpus_per_node(void);
 static int   _parse_state( char* str, uint16_t* states );
 static void  _parse_token( char *token, char *field, int *field_size,
 			   bool *right_justify, char **suffix);
@@ -408,40 +407,9 @@ parse_command_line( int argc, char* argv[] )
 		}
 		list_iterator_destroy(iterator);
 	}
-	if (params.job_id || params.user_id)
-		params.max_cpus = 1;	/* To minimize overhead */
-	else
-		params.max_cpus = _max_cpus_per_node();
 
 	if ( params.verbose )
 		_print_options();
-}
-
-/* Return the maximum number of processors for any node in the cluster. */
-static int   _max_cpus_per_node(void)
-{
-	int error_code, max_cpus = 1;
-	node_info_msg_t *node_info_ptr = NULL;
-
-	/* Since slurm_load_node() is a high-overhead function call, use
-	 * slurm_load_node_single() instead and assume a homogeneous cluster */
-#if 0
-	error_code = slurm_load_node ((time_t) NULL, &node_info_ptr,
-				      params.all_flag);
-#else
-	error_code = slurm_load_node_single(&node_info_ptr, NULL,
-					    params.all_flag);
-#endif
-	if (error_code == SLURM_SUCCESS) {
-		int i;
-		node_info_t *node_ptr = node_info_ptr->node_array;
-		for (i=0; i<node_info_ptr->record_count; i++) {
-			max_cpus = MAX(max_cpus, node_ptr[i].cpus);
-		}
-		slurm_free_node_info_msg (node_info_ptr);
-	}
-
-	return max_cpus;
 }
 
 /*
@@ -916,7 +884,6 @@ _print_options(void)
 	printf( "iterate     = %d\n", params.iterate );
 	printf( "job_flag    = %d\n", params.job_flag );
 	printf( "jobs        = %s\n", params.jobs );
-	printf( "max_cpus    = %d\n", params.max_cpus ) ;
 	printf( "names       = %s\n", params.names );
 	printf( "nodes       = %s\n", hostlist ) ;
 	printf( "partitions  = %s\n", params.partitions ) ;

src/squeue/print.c
@@ -882,10 +882,6 @@ static int _get_node_cnt(job_info_t * job)
 				node_cnt = num_tasks;
 			else if (!node_cnt)
 				node_cnt = 1;
-		} else {
-			int round = job->num_cpus + params.max_cpus - 1;
-			round /= params.max_cpus;	/* round up */
-			node_cnt = MAX(node_cnt, round);
 		}
 	} else
 		node_cnt = _nodes_in_list(job->nodes);

src/squeue/squeue.h
@@ -85,7 +85,6 @@ struct squeue_parameters {
 	bool long_list;
 	bool no_header;
 	int  iterate;
-	int  max_cpus;
 	int  verbose;
 
 	char* accounts;