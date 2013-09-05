641a5f927465905521814381107334b6d7d20da8
Morris Jette <jette@schedmd.com>
2013-09-05 13:35:38 -0700

    Remove vestigial logic
    Seems to be left over from sview refactoring. Cleaned up
    bug 403

--- src/sview/common.c
+++ src/sview/common.c
@@ -74,40 +74,17 @@ static int _find_node_inx (char *name)
 
 static void _display_topology(void)
 {
-	int i, match = 0, match_cnt = 0;
-	hostset_t hs;
-	int one_liner = 1;
+	int i, one_liner = 1;
 
-	if (TOPO_DEBUG)
+	if (TOPO_DEBUG) {
 		g_print("_display_topology,  record_count = %d\n",
 			g_topo_info_msg_ptr->record_count);
-	for (i=0; i<g_topo_info_msg_ptr->record_count; i++) {
+	}
 
-		if ((g_topo_info_msg_ptr->topo_array[i].nodes == NULL) ||
-		    (g_topo_info_msg_ptr->topo_array[i].nodes[0] == '\0'))
-			continue;
-
-		if (g_topo_info_msg_ptr->topo_array[i].level == 0) {
-			hs = hostset_create(g_topo_info_msg_ptr->
-					    topo_array[i].nodes);
-			if (hs == NULL)
-				fatal("hostset_create: memory "
-				      "allocation failure");
-
-		}
-
-		hostset_destroy(hs);
-		if (!match)
-			continue;
-		match_cnt++;
+	for (i = 0; i < g_topo_info_msg_ptr->record_count; i++) {
 		slurm_print_topo_record(stdout,
 					&g_topo_info_msg_ptr->topo_array[i],
 					one_liner);
-		if (match_cnt == 0) {
-			g_print("Topology information contains no switch or "
-				"node named %s",
-				g_topo_info_msg_ptr->topo_array[i].nodes);
-		}
 	}
 }
 
