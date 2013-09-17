SchedMD/slurm

commit 7912c05bb410567899b857367450d7dbb7905e71

scontrol parsing fix
for setdebugflags command, avoid parsing "-flagname" as
an scontrol command line option.

src/scontrol/scontrol.c
@@ -127,9 +127,14 @@ main (int argc, char *argv[])
 		working_cluster_rec = list_peek(clusters);
 	}
 
-	while((opt_char = getopt_long(argc, argv, "adhM:oQvV",
-			long_options, &option_index)) != -1) {
+	while (1) {
+		if (!strncasecmp(argv[optind], "setdebugflags", 8))
+			break;	/* avoid parsing "-<flagname>" as option */
+		opt_char = getopt_long(argc, argv, "adhM:oQvV",
+				       long_options, &option_index);
 		switch (opt_char) {
+		case -1:
+			break;
 		case (int)'?':
 			fprintf(stderr, "Try \"scontrol --help\" for "
 				"more information\n");

commit 87ee63cbac781182eae612c8f02a305de188d6eb

scontrol parsing problem
Bug introduced earlier today in commit 7912c05bb410567899b857367450d7dbb7905e71

src/scontrol/scontrol.c
@@ -130,11 +130,10 @@ main (int argc, char *argv[])
 	while (1) {
 		if (!strncasecmp(argv[optind], "setdebugflags", 8))
 			break;	/* avoid parsing "-<flagname>" as option */
-		opt_char = getopt_long(argc, argv, "adhM:oQvV",
-				       long_options, &option_index);
-		switch (opt_char) {
-		case -1:
+		if (opt_char = getopt_long(argc, argv, "adhM:oQvV",
+					   long_options, &option_index) == -1)
 			break;
+		switch (opt_char) {
 		case (int)'?':
 			fprintf(stderr, "Try \"scontrol --help\" for "
 				"more information\n");