799a753be0c116a6117b0afa84f971fc2bdb9d87
Morris Jette <jette@schedmd.com>
2013-10-11 14:01:35 -0700

    Expand hostlist range count
    Increase maximum number of hostlist ranges from 12k to 64k and
    use malloc to allocate memory rather than using the stack
    bug 458

--- src/common/hostlist.c
+++ src/common/hostlist.c
@@ -193,7 +193,7 @@ extern void * lsd_nomem_error(char *file, int line, char *mesg);
 #define MAX_RANGE    (64*1024)    /* 64K Hosts */
 
 /* max number of ranges that will be processed between brackets */
-#define MAX_RANGES   (12*1024)    /* 12K Ranges */
+#define MAX_RANGES   (64*1024)    /* 64K Hosts */
 
 /* size of internal hostname buffer (+ some slop), hostnames will probably
  * be truncated if longer than MAXHOSTNAMELEN */
@@ -1828,21 +1828,25 @@ _push_range_list(hostlist_t hl, char *prefix, struct _range *range,
 	strncpy(tmp_prefix, prefix, sizeof(tmp_prefix));
 	if (((p = strrchr(tmp_prefix, '[')) != NULL) &&
 	    ((q = strrchr(p, ']')) != NULL)) {
-		struct _range prefix_range[MAX_RANGES];
+		struct _range *prefix_range;
 		struct _range *saved_range = range, *pre_range = prefix_range;
 		unsigned long j, prefix_cnt = 0;
 		*p++ = '\0';
 		*q++ = '\0';
 		if (strrchr(tmp_prefix, '[') != NULL)
 			return -1;	/* third range is illegal */
+		prefix_range = xmalloc(sizeof(struct _range) * MAX_RANGES);
 		nr = _parse_range_list(p, prefix_range, MAX_RANGES, dims);
-		if (nr < 0)
+		if (nr < 0) {
+			xfree(prefix_range);
 			return -1;	/* bad numeric expression */
+		}
 		for (i = 0; i < nr; i++) {
 			prefix_cnt += pre_range->hi - pre_range->lo + 1;
 			if (prefix_cnt > MAX_PREFIX_CNT) {
 				/* Prevent overflow of memory with user input
 				 * of something like "a[0-999999999].b[0-9]" */
+				xfree(prefix_range);
 				return -1;
 			}
 			for (j = pre_range->lo; j <= pre_range->hi; j++) {
@@ -1859,6 +1863,7 @@ _push_range_list(hostlist_t hl, char *prefix, struct _range *range,
 			}
 			pre_range++;
 		}
+		xfree(prefix_range);
 		return 0;
 	}
 
@@ -1879,7 +1884,7 @@ _hostlist_create_bracketed(const char *hostlist, char *sep,
 			   char *r_op, int dims)
 {
 	hostlist_t new = hostlist_new();
-	struct _range ranges[MAX_RANGES];
+	struct _range *ranges;
 	int nr, err;
 	char *p, *tok, *str, *orig;
 	char cur_tok[1024];
@@ -1892,6 +1897,7 @@ _hostlist_create_bracketed(const char *hostlist, char *sep,
 		return NULL;
 	}
 
+	ranges = xmalloc(sizeof(struct _range) * MAX_RANGES);
 	while ((tok = _next_tok(sep, &str)) != NULL) {
 		strncpy(cur_tok, tok, 1024);
 		if ((p = strrchr(tok, '[')) != NULL) {
@@ -1927,6 +1933,7 @@ _hostlist_create_bracketed(const char *hostlist, char *sep,
 		} else
 			hostlist_push_host_dims(new, cur_tok, dims);
 	}
+	xfree(ranges);
 
 	free(orig);
 	return new;
