SchedMD/slurm

commit b7e60b985602623add36023ebcf4867caffce5e2

Pending job steps will have step_id of INFINITE
Formerly was  NO_VAL and will be reported as "TBD" by scontrol
and squeue commands.

src/squeue/print.c
@@ -1339,13 +1339,23 @@ int _print_step_id(job_step_info_t * step, int width, bool right, char* suffix)
 	if (step == NULL) {	/* Print the Header instead */
 		_print_str("STEPID", width, right, true);
 	} else if (step->array_job_id) {
-		snprintf(id, FORMAT_STRING_SIZE, "%u_%u.%u",
-			 step->array_job_id, step->array_task_id,
-			 step->step_id);
+		if (step->step_id == INFINITE) {	/* Pending */
+			snprintf(id, FORMAT_STRING_SIZE, "%u_%u.TBD",
+				 step->array_job_id, step->array_task_id);
+		} else {
+			snprintf(id, FORMAT_STRING_SIZE, "%u_%u.%u",
+				 step->array_job_id, step->array_task_id,
+				 step->step_id);
+		}
 		_print_str(id, width, right, true);
 	} else {
-		snprintf(id, FORMAT_STRING_SIZE, "%u.%u", step->job_id,
-			 step->step_id);
+		if (step->step_id == INFINITE) {	/* Pending */
+			snprintf(id, FORMAT_STRING_SIZE, "%u.TBD", step->job_id);
+		} else {
+			snprintf(id, FORMAT_STRING_SIZE, "%u_%u.%u",
+				 step->array_job_id, step->array_task_id,
+				 step->step_id);
+		}
 		_print_str(id, width, right, true);
 	}
 	if (suffix)

RELEASE_NOTES
@@ -84,6 +84,8 @@ COMMAND CHANGES (see man pages for details)
     job can be put in a new special state called SPECIAL_EXIT indicating
     the job has exited with a special value.
     "scontrol requeuehold state=SpecialExit 123".
+ -- Pending job steps will have step_id of INFINITE rather than NO_VAL and
+    will be reported as "TBD" by scontrol and squeue commands.
 
 OTHER CHANGES
 =============

src/api/job_step_info.c
@@ -163,13 +163,25 @@ slurm_sprint_job_step_info ( job_step_info_t * job_step_ptr,
 		secs2time_str ((time_t)job_step_ptr->time_limit * 60,
 				limit_str, sizeof(limit_str));
 	if (job_step_ptr->array_job_id) {
-		snprintf(tmp_line, sizeof(tmp_line), "StepId=%u_%u.%u ",
-			 job_step_ptr->array_job_id,
-			 job_step_ptr->array_task_id, job_step_ptr->step_id);
+		if (job_step_ptr->step_id == INFINITE) {	/* Pending */
+			snprintf(tmp_line, sizeof(tmp_line), "StepId=%u_%u.TBD ",
+				 job_step_ptr->array_job_id,
+				 job_step_ptr->array_task_id);
+		} else {
+			snprintf(tmp_line, sizeof(tmp_line), "StepId=%u_%u.%u ",
+				 job_step_ptr->array_job_id,
+				 job_step_ptr->array_task_id,
+				 job_step_ptr->step_id);
+		}
 		out = xstrdup(tmp_line);
 	} else {
-		snprintf(tmp_line, sizeof(tmp_line), "StepId=%u.%u ",
-			 job_step_ptr->job_id, job_step_ptr->step_id);
+		if (job_step_ptr->step_id == INFINITE) {	/* Pending */
+			snprintf(tmp_line, sizeof(tmp_line), "StepId=%u.TBD ",
+				 job_step_ptr->job_id);
+		} else {
+			snprintf(tmp_line, sizeof(tmp_line), "StepId=%u.%u ",
+				 job_step_ptr->job_id, job_step_ptr->step_id);
+		}
 		out = xstrdup(tmp_line);
 	}
 	snprintf(tmp_line, sizeof(tmp_line),

src/slurmctld/step_mgr.c
@@ -205,7 +205,7 @@ static void _build_pending_step(struct job_record *job_ptr,
 	step_ptr->state     = JOB_PENDING;
 	step_ptr->cpu_count = step_specs->num_tasks;
 	step_ptr->time_last_active = time(NULL);
-	step_ptr->step_id = NO_VAL;
+	step_ptr->step_id   = INFINITE;
 }
 
 static void _internal_step_complete(

commit 858a1b2c155b190c0e5568a6e84ff6a937bfb411

squeue step output fix
Bug was introduced in commit b7e60b985602623add36023ebcf4867caffce5e2
which resulting in job step ID being preinted by squeue incorrectly

src/squeue/print.c
@@ -1352,9 +1352,8 @@ int _print_step_id(job_step_info_t * step, int width, bool right, char* suffix)
 		if (step->step_id == INFINITE) {	/* Pending */
 			snprintf(id, FORMAT_STRING_SIZE, "%u.TBD", step->job_id);
 		} else {
-			snprintf(id, FORMAT_STRING_SIZE, "%u_%u.%u",
-				 step->array_job_id, step->array_task_id,
-				 step->step_id);
+			snprintf(id, FORMAT_STRING_SIZE, "%u.%u",
+				 step->job_id, step->step_id);
 		}
 		_print_str(id, width, right, true);
 	}