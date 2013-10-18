SchedMD/slurm

commit dbbd78e236363a4784c4f05a5f4145b5602789dd

Permit PMI_Abort/PMI2_Abort to propagate an exit code

src/slurmd/slurmstepd/mgr.c
@@ -4,6 +4,7 @@
  *****************************************************************************
  *  Copyright (C) 2002-2007 The Regents of the University of California.
  *  Copyright (C) 2008-2009 Lawrence Livermore National Security.
+ *  Copyright (C) 2013      Intel, Inc.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
  *  Written by Mark Grondona <mgrondona@llnl.gov>.
  *  CODE-OCEC-09-009. All rights reserved.
@@ -255,17 +256,40 @@ mgr_launch_tasks_setup(launch_tasks_request_msg_t *msg, slurm_addr_t *cli,
  */
 static uint32_t _get_exit_code(stepd_step_rec_t *job)
 {
-	int i;
+	uint32_t i;
 	uint32_t step_rc = NO_VAL;
 
-	for (i = 0; i < job->node_tasks; i++) {
-		/* If signalled we only need to check one and then
-		 * break out of the loop */
-		if (WIFSIGNALED(job->task[i]->estatus)) {
-			step_rc = job->task[i]->estatus;
+	for (i = 0; i < job->ntasks; i++) {
+		if (NULL == job->task[i]) {
+			debug("get_exit_code task %u is NULL", i);
+			continue;
+		}
+		/* if this task was killed by cmd, ignore its
+		 * return status as it only reflects the fact
+		 * that we killed it
+		 */
+		if (job->task[i]->killed_by_cmd) {
+			debug("get_exit_code task %u killed by cmd", i);
+			continue;
+		}
+		/* if this task called PMI_Abort or PMI2_Abort,
+		 * then we let it define the exit status
+		 */
+		if (job->task[i]->aborted) {
+			step_rc = WEXITSTATUS(job->task[i]->estatus);
+			debug("get_exit_code task %u called abort", i);
 			break;
 		}
-		step_rc = MAX(step_complete.step_rc, job->task[i]->estatus);
+		/* If signalled we need to cycle thru all the
+		 * tasks in case one of them called abort
+		 */
+		if (WIFSIGNALED(job->task[i]->estatus)) {
+			step_rc = WTERMSIG(job->task[i]->estatus) + 128;
+			debug("get_exit_code task %u died by signal", i);
+		} else {
+			step_rc = WEXITSTATUS(job->task[i]->estatus);
+		}
+		step_rc = MAX(step_complete.step_rc, step_rc);
 	}
 	return step_rc;
 }
@@ -1642,10 +1666,10 @@ _log_task_exit(unsigned long taskid, unsigned long pid, int status)
 	 *   that code, but it is better than dropping a potentially useful
 	 *   exit status.
 	 */
-	if (WIFEXITED(status))
+	if (WIFEXITED(status)) {
 		verbose("task %lu (%lu) exited with exit code %d.",
 			taskid, pid, WEXITSTATUS(status));
-	else if (WIFSIGNALED(status))
+	} else if (WIFSIGNALED(status)) {
 		/* WCOREDUMP isn't available on AIX */
 		verbose("task %lu (%lu) exited. Killed by signal %d%s.",
 			taskid, pid, WTERMSIG(status),
@@ -1655,9 +1679,10 @@ _log_task_exit(unsigned long taskid, unsigned long pid, int status)
 			""
 #endif
 			);
-	else
+	} else {
 		verbose("task %lu (%lu) exited with status 0x%04x.",
 			taskid, pid, status);
+	}
 }
 
 /*
@@ -1725,7 +1750,6 @@ _wait_for_any_task(stepd_step_rec_t *job, bool waitflag)
 		if ((t = job_task_info_by_pid(job, pid))) {
 			completed++;
 			_log_task_exit(t->gtid, pid, status);
-
 			t->exited  = true;
 			t->estatus = status;
 			job->envtp->env = job->env;

src/slurmd/slurmstepd/req.c
@@ -667,6 +667,8 @@ _handle_signal_container(int fd, stepd_step_rec_t *job, uid_t uid)
 	static int msg_sent = 0;
 	char *ptr = NULL;
 	int target_node_id = 0;
+	stepd_step_task_info_t *task;
+	uint32_t i;
 
 	safe_read(fd, &sig, sizeof(int));
 	debug("_handle_signal_container for step=%u.%u uid=%d signal=%d",
@@ -689,6 +691,26 @@ _handle_signal_container(int fd, stepd_step_rec_t *job, uid_t uid)
 		rc = -1;
 		errnum = ESLURMD_JOB_NOTRUNNING;
 		goto done;
+	}
+
+	if ((sig == SIGTERM) || (sig == SIGKILL)) {
+		/* cycle thru the tasks and mark those that have not
+		 * called abort and/or terminated as killed_by_cmd
+		 */
+		for (i = 0; i < job->ntasks; i++) {
+			if (NULL == (task = job->task[i])) {
+				continue;
+			}
+			if (task->aborted || task->exited) {
+				continue;
+			}
+			/* mark that this task is going to be killed by
+			 * cmd so we ignore its exit status - otherwise,
+			 * we will probably report the final exit status
+			 * as SIGKILL
+			 */
+			task->killed_by_cmd = true;
+		}
 	}
 
 	ptr = getenvp(job->env, "SLURM_STEP_KILLED_MSG_NODE_ID");
@@ -901,6 +923,8 @@ _handle_terminate(int fd, stepd_step_rec_t *job, uid_t uid)
 {
 	int rc = SLURM_SUCCESS;
 	int errnum = 0;
+	stepd_step_task_info_t *task;
+	uint32_t i;
 
 	debug("_handle_terminate for step=%u.%u uid=%d",
 	      job->jobid, job->stepid, uid);
@@ -924,6 +948,24 @@ _handle_terminate(int fd, stepd_step_rec_t *job, uid_t uid)
 		rc = -1;
 		errnum = ESLURMD_JOB_NOTRUNNING;
 		goto done;
+	}
+
+	/* cycle thru the tasks and mark those that have not
+	 * called abort and/or terminated as killed_by_cmd
+	 */
+	for (i = 0; i < job->ntasks; i++) {
+		if (NULL == (task = job->task[i])) {
+			continue;
+		}
+		if (task->aborted || task->exited) {
+			continue;
+		}
+		/* mark that this task is going to be killed by
+		 * cmd so we ignore its exit status - otherwise,
+		 * we will probably report the final exit status
+		 * as SIGKILL
+		 */
+		task->killed_by_cmd = true;
 	}
 
 	/*

src/slurmd/slurmstepd/slurmstepd_job.c
@@ -4,6 +4,7 @@
  *****************************************************************************
  *  Copyright (C) 2002-2007 The Regents of the University of California.
  *  Copyright (C) 2008-2010 Lawrence Livermore National Security.
+ *  Copyright (C) 2013      Intel, Inc.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
  *  Written by Mark Grondona <mgrondona@llnl.gov>.
  *  CODE-OCEC-09-009. All rights reserved.
@@ -709,10 +710,14 @@ task_info_create(int taskid, int gtaskid,
 	t->from_stdout = -1;
 	t->stderr_fd   = -1;
 	t->from_stderr = -1;
-	t->estatus     = -1;
 	t->in          = NULL;
 	t->out         = NULL;
 	t->err         = NULL;
+	t->killed_by_cmd = false;
+	t->aborted     = false;
+	t->esent       = false;
+	t->exited      = false;
+	t->estatus     = -1;
 	t->argc	       = 0;
 	t->argv	       = NULL;
 	slurm_mutex_unlock(&t->mutex);

src/slurmd/slurmstepd/slurmstepd_job.h
@@ -4,6 +4,7 @@
  *****************************************************************************
  *  Copyright (C) 2002-2007 The Regents of the University of California.
  *  Copyright (C) 2008-2010 Lawrence Livermore National Security.
+ *  Copyright (C) 2013      Intel, Inc.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
  *  Written by Mark Grondona <mgrondona@llnl.gov>.
  *  CODE-OCEC-09-009. All rights reserved.
@@ -101,6 +102,8 @@ typedef struct {
 	eio_obj_t      *out;        /* standard output event IO object      */
 	eio_obj_t      *err;        /* standard error event IO object       */
 
+	bool            killed_by_cmd; /* true if task killed by our signal */
+	bool            aborted;    /* true if task called abort            */
 	bool            esent;      /* true if exit status has been sent    */
 	bool            exited;     /* true if task has exited              */
 	int             estatus;    /* this task's exit status              */

commit c55ac4fb23d2d8b4d1eb7f77dc8e750c53fab2ce

Correction to step exit code logic
This corrects a bug introduced in
commit dbbd78e236363a4784c4f05a5f4145b5602789dd
which went off the end of an array

src/slurmd/slurmstepd/mgr.c
@@ -259,11 +259,7 @@ static uint32_t _get_exit_code(stepd_step_rec_t *job)
 	uint32_t i;
 	uint32_t step_rc = NO_VAL;
 
-	for (i = 0; i < job->ntasks; i++) {
-		if (NULL == job->task[i]) {
-			debug("get_exit_code task %u is NULL", i);
-			continue;
-		}
+	for (i = 0; i < job->node_tasks; i++) {
 		/* if this task was killed by cmd, ignore its
 		 * return status as it only reflects the fact
 		 * that we killed it
@@ -283,8 +279,11 @@ static uint32_t _get_exit_code(stepd_step_rec_t *job)
 		/* If signalled we need to cycle thru all the
 		 * tasks in case one of them called abort
 		 */
-		if (WIFSIGNALED(job->task[i]->estatus))
-			debug("get_exit_code task %u died by signal", i);
+		if (WIFSIGNALED(job->task[i]->estatus)) {
+			error("get_exit_code task %u died by signal", i);
+			step_rc = job->task[i]->estatus;
+			break;
+		}
 		step_rc = MAX(step_complete.step_rc, job->task[i]->estatus);
 	}
 	return step_rc;