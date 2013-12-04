d9960545565e4c746793576b2f9dcab8f02aa13c
Danny Auble <da@schedmd.com>
2013-12-04 11:29:17 -0800

    The Native Cray stuff does not have multiple dimensions

--- src/plugins/select/cray/select_cray.c
+++ src/plugins/select/cray/select_cray.c
@@ -152,12 +152,6 @@ static void _update_app(struct job_record *job_ptr,
 			alpsc_ev_app_state_e state);
 #endif
 
-/* All current (2011) XT/XE installations have a maximum dimension of 3,
- * smaller systems deploy a 2D Torus which has no connectivity in
- * X-dimension.  We know the highest system dimensions possible here
- * are 3 so we set it to that.  Do not use SYSTEM_DIMENSIONS since
- * that could easily be wrong if built on a non Cray system. */
-static int select_cray_dim_size[3] = {-1};
 static uint32_t debug_flags = 0;
 
 /*
@@ -1516,55 +1510,11 @@ extern bitstr_t * select_p_resv_test(bitstr_t *avail_bitmap, uint32_t node_cnt,
 
 extern void select_p_ba_init(node_info_msg_t *node_info_ptr, bool sanity_check)
 {
-	int i, j, offset;
-	int dims = slurmdb_setup_cluster_dims();
-
-	if (select_cray_dim_size[0] == -1) {
-		node_info_t *node_ptr;
-
-		/* init the rest of the dim sizes. All current (2011)
-		 * XT/XE installations have a maximum dimension of 3,
-		 * smaller systems deploy a 2D Torus which has no
-		 * connectivity in X-dimension.  Just incase they
-		 * decide to change it where we only get 2 instead of
-		 * 3 we will initialize it later. */
-		for (i = 1; i < dims; i++)
-			select_cray_dim_size[i] = -1;
-		for (i = 0; i < node_info_ptr->record_count; i++) {
-			node_ptr = &(node_info_ptr->node_array[i]);
-			if (!node_ptr->node_addr ||
-			    (strlen(node_ptr->node_addr) != dims))
-				continue;
-			for (j = 0; j < dims; j++) {
-				offset = select_char2coord(
-					node_ptr->node_addr[j]);
-				select_cray_dim_size[j] =
-					MAX((offset+1),
-					    select_cray_dim_size[j]);
-			}
-		}
-	}
-
-	/*
-	 * Override the generic setup of dim_size made in _setup_cluster_rec()
-	 * FIXME: use a better way, e.g. encoding the 3-dim triplet as a
-	 *        string which gets stored in a database (event_table?) entry.
-	 */
-	if (working_cluster_rec) {
-		xfree(working_cluster_rec->dim_size);
-		working_cluster_rec->dim_size = xmalloc(sizeof(int) * dims);
-		for (j = 0; j < dims; j++)
-			working_cluster_rec->dim_size[j] =
-				select_cray_dim_size[j];
-	}
-
 	other_ba_init(node_info_ptr, sanity_check);
 }
 
 extern int *select_p_ba_get_dims(void)
 {
-	if (select_cray_dim_size[0] != -1)
-		return select_cray_dim_size;
 	return NULL;
 }
 
