SchedMD/slurm

commit ec4df3bfb6430b14a0b43db74f204280c7c2e726

Fix for hwloc returning zero core count

src/common/gres.c
@@ -2913,7 +2913,7 @@ extern int _job_alloc(void *job_gres_data, void *node_gres_data,
 		i = bit_size(node_gres_ptr->gres_bit_alloc);
 		if (i < node_gres_ptr->gres_cnt_avail) {
 			error("gres/%s: node %s gres bitmap size bad (%d < %u)",
-			      i, node_gres_ptr->gres_cnt_avail);
+			      gres_name, i, node_gres_ptr->gres_cnt_avail);
 			node_gres_ptr->gres_bit_alloc =
 				bit_realloc(node_gres_ptr->gres_bit_alloc,
 					    node_gres_ptr->gres_cnt_avail);

src/common/xcpuinfo.c
@@ -237,6 +237,12 @@ get_cpuinfo(uint16_t *p_cpus, uint16_t *p_boards,
 	}
 	nobj[SOCKET] = hwloc_get_nbobjs_by_type(topology, objtype[SOCKET]);
 	nobj[CORE]   = hwloc_get_nbobjs_by_type(topology, objtype[CORE]);
+	/*
+	 * Workaround for hwloc
+	 * hwloc_get_nbobjs_by_type() returns 0 on some architectures.
+	 */
+	if ( nobj[CORE] == 0 )
+	    nobj[CORE] = 1;
 	actual_cpus  = hwloc_get_nbobjs_by_type(topology, objtype[PU]);
 	nobj[PU]     = actual_cpus/nobj[CORE];  /* threads per core */
 	nobj[CORE]  /= nobj[SOCKET];            /* cores per socket */

commit 1712d6192098b4bbd1fc6d76605d93789afb7b52

Fix bad print
error introduced in commit ec4df3bfb6430b14a0b43db74f204280c7c2e726

src/common/gres.c
@@ -2913,7 +2913,8 @@ extern int _job_alloc(void *job_gres_data, void *node_gres_data,
 		i = bit_size(node_gres_ptr->gres_bit_alloc);
 		if (i < node_gres_ptr->gres_cnt_avail) {
 			error("gres/%s: node %s gres bitmap size bad (%d < %u)",
-			      gres_name, i, node_gres_ptr->gres_cnt_avail);
+			      gres_name, node_name,
+			      i, node_gres_ptr->gres_cnt_avail);
 			node_gres_ptr->gres_bit_alloc =
 				bit_realloc(node_gres_ptr->gres_bit_alloc,
 					    node_gres_ptr->gres_cnt_avail);