SchedMD/slurm

commit 8df024c19ed220810395ba5c4f72748188e1002d

Correct malloc buffer size
Previously was allocating sizeof(char *) rather than sizeof(char),
so we save some space here.

src/common/env.c
@@ -280,7 +280,7 @@ int setenvf(char ***envp, const char *name, const char *fmt, ...)
 		else
 			rc = 1;
 	} else {
-		int size = sizeof(char *) * (strlen(name)+strlen(value)+1);
+		int size = strlen(name) + strlen(value) + 1;
 		/* XXX Space is allocated on the heap and will never
 		 * be reclaimed.
 		 * Also you can not use xmalloc here since some of the

commit 3b5a90b1286c7439e282d84a8664fe03e9e73e02

Correction to buffer size
Bug introduced earlier today in
commit 8df024c19ed220810395ba5c4f72748188e1002d

src/common/env.c
@@ -280,14 +280,14 @@ int setenvf(char ***envp, const char *name, const char *fmt, ...)
 		else
 			rc = 1;
 	} else {
-		int size = strlen(name) + strlen(value) + 1;
+		int size = strlen(name) + strlen(value) + 2;
 		/* XXX Space is allocated on the heap and will never
 		 * be reclaimed.
 		 * Also you can not use xmalloc here since some of the
 		 * external api's like perl will crap out when they
 		 * try to free it.
 		 */
-		str = malloc(size + 1);
+		str = malloc(size);
 		snprintf(str, size, "%s=%s", name, value);
 		rc = putenv(str);
 	}