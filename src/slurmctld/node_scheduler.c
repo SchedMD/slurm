106051ad97521e8ac37f33bc0161ca09f19e8da4
Morris Jette <jette@schedmd.com>
2014-02-03 13:03:17 -0800

    Better error checking for core_spec value
    If a batch job is submitted that can not be started due to its
    core specialization count being equal to or larger than the
    node's core count, then reject it at submit time rather than
    queuing the job.

--- src/slurmctld/node_scheduler.c
+++ src/slurmctld/node_scheduler.c
@@ -1013,6 +1013,7 @@ _pick_best_nodes(struct node_set *node_set_ptr, int node_set_size,
 		 List *preemptee_job_list, bool has_xand,
 		 bitstr_t *exc_core_bitmap)
 {
+	struct node_record *node_ptr;
 	int error_code = SLURM_SUCCESS, i, j, pick_code;
 	int total_nodes = 0, avail_nodes = 0;
 	bitstr_t *avail_bitmap = NULL, *total_bitmap = NULL;
@@ -1087,6 +1088,23 @@ _pick_best_nodes(struct node_set *node_set_ptr, int node_set_size,
 		}
 		if (total_nodes > max_nodes) {	/* exceeds node limit */
 			return ESLURM_REQUESTED_PART_CONFIG_UNAVAILABLE;
+		}
+		if (job_ptr->details->core_spec) {
+			i = bit_ffs(job_ptr->details->req_node_bitmap);
+			if (i >= 0) {
+				node_ptr = node_record_table_ptr + i;
+				if (slurmctld_conf.fast_schedule) {
+					j = node_ptr->config_ptr->sockets *
+					    node_ptr->config_ptr->cores;
+				} else {
+					j = node_ptr->sockets * node_ptr->cores;
+				}
+			}
+			if ((i >= 0) && (job_ptr->details->core_spec >= j)) {
+				info("_pick_best_nodes: job %u never runnable",
+				     job_ptr->job_id);
+				return ESLURM_REQUESTED_NODE_CONFIG_UNAVAILABLE;
+			}
 		}
 
 		/* check the availability of these nodes */
