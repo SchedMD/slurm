702fb424bdfb343c262890896e07389c611d0f8e
Morris Jette <jette@schedmd.com>
2014-02-10 12:29:00 -0800

    Correction to shared node logic
    This fixes a bug introduced recently for core specialization logic

--- src/slurmctld/node_scheduler.c
+++ src/slurmctld/node_scheduler.c
@@ -695,7 +695,7 @@ _resolve_shared_status(struct job_record *job_ptr, uint16_t part_max_share,
 		return 1;
 
 	if (cons_res_flag) {
-		if (job_ptr->details->share_res != 1)
+		if (job_ptr->details->share_res == 0)
 			return 0;
 		return 1;
 	} else {
