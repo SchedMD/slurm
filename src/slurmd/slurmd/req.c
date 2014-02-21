9dfae3fa5f98b46709ee3fa30a2b29ffe6c4ca41
Morris Jette <jette@schedmd.com>
2014-02-21 11:39:22 -0800

    Move prolog complete RPC
    The slurm_complete_prolog() API originally in slurm.h and src/api
    is not a user API and the logic has been moved directly into
    src/slurmd/slurmd/req.c

--- src/slurmd/slurmd/req.c
+++ src/slurmd/slurmd/req.c
@@ -1400,9 +1400,28 @@ static void _note_batch_job_finished(uint32_t job_id)
 	slurm_mutex_unlock(&fini_mutex);
 }
 
+static void
+_notify_slurmctld_prolog_fini(uint32_t job_id, uint32_t prolog_return_code)
+{
+	int rc;
+	slurm_msg_t req_msg;
+	complete_prolog_msg_t req;
+
+	slurm_msg_t_init(&req_msg);
+	req.job_id	= job_id;
+	req.prolog_rc	= prolog_return_code;
+
+	req_msg.msg_type= REQUEST_COMPLETE_PROLOG;
+	req_msg.data	= &req;
+
+	if ((slurm_send_recv_controller_rc_msg(&req_msg, &rc) < 0) ||
+	    (rc != SLURM_SUCCESS))
+		error("Error sending prolog completion notification: %m");
+}
+
 static void _rpc_prolog(slurm_msg_t *msg)
 {
-	int  rc = SLURM_SUCCESS;
+	int rc = SLURM_SUCCESS;
 	prolog_launch_msg_t *req = (prolog_launch_msg_t *)msg->data;
 	job_env_t job_env;
 
@@ -1461,9 +1480,7 @@ static void _rpc_prolog(slurm_msg_t *msg)
 		rc = ESLURMD_PROLOG_FAILED;
 	}
 
-	if (slurm_complete_prolog(req->job_id, rc) < 0) {
-		error("Error finishing prolog: %m");
-	}
+	_notify_slurmctld_prolog_fini(req->job_id, rc);
 }
 
 static void
