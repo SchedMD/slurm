9b4f363456fe99629603386f57331f1a5011d6f3
Morris Jette <jette@schedmd.com>
2014-03-16 15:55:44 -0700

    Export "SLURM*" env vars if --export=NONE
    Previously if the sbatch --export=NONE option was used then several
    Slurm environment variables were not propagated from the sbatch
    command (SLURM_SUBMIT_DIR, SLURM_SUBMIT_HOST, SLURM_JOB_NAME, etc.)

--- src/sbatch/sbatch.c
+++ src/sbatch/sbatch.c
@@ -485,6 +485,8 @@ static int _fill_job_desc_from_opts(job_desc_msg_t *desc)
 		env_array_merge(&desc->environment, (const char **)environ);
 	} else if (!strcasecmp(opt.export_env, "NONE")) {
 		desc->environment = env_array_create();
+		env_array_merge_slurm(&desc->environment,
+				      (const char **)environ);
 		opt.get_user_env_time = 0;
 	} else {
 		_env_merge_filter(desc);
