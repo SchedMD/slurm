a45170c2a2fff6c57ce36f08a7bfe5457cb08572
Danny Auble <da@schedmd.com>
2014-03-17 11:31:46 -0700

    CRAY/ALPS - Add support for CLE52

--- src/plugins/select/cray/libalps/do_query.c
+++ src/plugins/select/cray/libalps/do_query.c
@@ -81,6 +81,7 @@ extern enum basil_version get_basil_version(void)
 	if (_get_alps_engine(engine_version, sizeof(engine_version)) == NULL)
 		fatal("can not determine ALPS Engine version");
 	else if ((strncmp(engine_version, "latest", 6) == 0) ||
+		 (strncmp(engine_version, "5.2", 3) == 0) ||
 		 (strncmp(engine_version, "5.1", 3) == 0))
 		bv = BV_5_1;
 	else if (strncmp(engine_version, "5.0", 3) == 0)
