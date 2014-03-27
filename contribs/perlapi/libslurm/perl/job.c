3026c4e1bc723c2906176c8fcd4676b7c91e25c2
Morris Jette <jette@schedmd.com>
2014-03-27 07:18:25 -0700

    Correct name and alphabetic order in last commit

--- contribs/perlapi/libslurm/perl/job.c
+++ contribs/perlapi/libslurm/perl/job.c
@@ -123,15 +123,15 @@ job_info_to_hv(job_info_t *job_info, HV *hv)
 	STORE_FIELD(hv, job_info, shared, uint16_t);
 	STORE_FIELD(hv, job_info, show_flags, uint16_t);
 	STORE_FIELD(hv, job_info, start_time, time_t);
+	if(job_info->state_desc)
+		STORE_FIELD(hv, job_info, state_desc, charp);
+	STORE_FIELD(hv, job_info, state_reason, uint16_t);
 	if(job_info->std_in)
 		STORE_FIELD(hv, job_info, std_in, charp);
 	if(job_info->std_out)
 		STORE_FIELD(hv, job_info, std_out, charp);
 	if(job_info->std_err)
 		STORE_FIELD(hv, job_info, std_err, charp);
-	if(job_info->state_desc)
-		STORE_FIELD(hv, job_info, state_desc, charp);
-	STORE_FIELD(hv, job_info, state_reason, uint16_t);
 	STORE_FIELD(hv, job_info, submit_time, time_t);
 	STORE_FIELD(hv, job_info, suspend_time, time_t);
 	STORE_FIELD(hv, job_info, time_limit, uint32_t);
