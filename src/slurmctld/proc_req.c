dbf299dd8e90cf971425bf00d2218e54177132e4
Morris Jette <jette@schedmd.com>
2014-04-02 16:11:10 -0700

    clear IDs when resetting RPC counters
    Prevously reset only the counters, now reset the IDs to better
    clean things up and avoid reporting a bunch of zero values.

--- src/slurmctld/proc_req.c
+++ src/slurmctld/proc_req.c
@@ -4803,10 +4803,12 @@ static void _clear_rpc_stats(void)
 	slurm_mutex_lock(&rpc_mutex);
 	for (i = 0; i < rpc_type_size; i++) {
 		rpc_type_cnt[i] = 0;
+		rpc_type_id[i] = 0;
 		rpc_type_time[i] = 0;
 	}
 	for (i = 0; i < rpc_user_size; i++) {
 		rpc_user_cnt[i] = 0;
+		rpc_user_id[i] = 0;
 		rpc_user_time[i] = 0;
 	}
 	slurm_mutex_unlock(&rpc_mutex);
