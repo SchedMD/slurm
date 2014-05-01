SchedMD/slurm

commit 2a674aee50936cc80e5a96e61b883dc87b785199

Handle the case in which errno is not reset to 0 after calling getgrent_r(), which causes the controller to core dump.

src/slurmctld/groups.c
@@ -115,13 +115,14 @@ extern uid_t *get_group_members(char *group_name)
 	while (1) {
 		res = getgrnam_r(group_name, &grp, grp_buffer, buflen,
 				 &grp_result);
-		if ((res != 0) && (errno == ERANGE)) {
-			buflen *= 2;
-			xrealloc(grp_buffer, buflen);
-			continue;
-		}
-		if ((res != 0) || (grp_result == NULL)) {
-			error("Could not find configured group %s", group_name);
+		if (res != 0) {
+			if (errno == ERANGE) {
+				buflen *= 2;
+				xrealloc(grp_buffer, buflen);
+				continue;
+			}
+			error("%s: Could not find configured group %s",
+			      __func__, group_name);
 			xfree(grp_buffer);
 			return NULL;
 		}
@@ -135,13 +136,14 @@ extern uid_t *get_group_members(char *group_name)
 	setgrent_r(&fp);
 	while (1) {
 		res = getgrent_r(&grp, grp_buffer, buflen, &fp);
-		if ((res != 0) && (errno == ERANGE)) {
-			buflen *= 2;
-			xrealloc(grp_buffer, buflen);
-			continue;
-		}
-		if (res != 0)
+		if (res != 0) {
+			if (errno == ERANGE) {
+				buflen *= 2;
+				xrealloc(grp_buffer, buflen);
+				continue;
+			}
 			break;
+		}
 		grp_result = &grp;
 #elif defined (__APPLE__) || defined (__CYGWIN__)
 	setgrent();
@@ -152,13 +154,14 @@ extern uid_t *get_group_members(char *group_name)
 	setgrent();
 	while (1) {
 		res = getgrent_r(&grp, grp_buffer, buflen, &grp_result);
-		if ((res != 0) && (errno == ERANGE)) {
-			buflen *= 2;
-			xrealloc(grp_buffer, buflen);
-			continue;
-		}
-		if ((res != 0) || (grp_result == NULL))
+		if (res != 0) {
+			if (errno == ERANGE) {
+				buflen *= 2;
+				xrealloc(grp_buffer, buflen);
+				continue;
+			}
 			break;
+		}
 #endif
 	        if (grp_result->gr_gid == my_gid) {
 			if (strcmp(grp_result->gr_name, group_name)) {
@@ -177,7 +180,7 @@ extern uid_t *get_group_members(char *group_name)
 					continue;
 				if (j+1 >= uid_cnt) {
 					uid_cnt += 100;
-					xrealloc(group_uids, 
+					xrealloc(group_uids,
 						 (sizeof(uid_t) * uid_cnt));
 				}
 				group_uids[j++] = my_uid;
@@ -230,7 +233,7 @@ extern void clear_group_cache(void)
 	pthread_mutex_unlock(&group_cache_mutex);
 }
 
-/* Get a record from our group/uid cache. 
+/* Get a record from our group/uid cache.
  * Return NULL if not found. */
 static uid_t *_get_group_cache(char *group_name)
 {

commit 768461345d80f2d872ba7ed51c12124605c40240

Fix allowgroup on bad group seg fault with the controller.
regression from 2a674aee50936cc80e5a96e61b883dc87b785199

src/slurmctld/groups.c
@@ -112,13 +112,16 @@ extern uid_t *get_group_members(char *group_name)
 	buflen = MAX(buflen, i);
 #endif
 	grp_buffer = xmalloc(buflen);
-	/* We need to check for !grp_result, since it appears some
-	 * versions of this function do not return an error on failure. */
 	while (1) {
 		slurm_seterrno(0);
 		res = getgrnam_r(group_name, &grp, grp_buffer, buflen,
 				 &grp_result);
-		if (res != 0) {
+
+		/* We need to check for !grp_result, since it appears some
+		 * versions of this function do not return an error on
+		 * failure.
+		 */
+		if (res != 0 || !grp_result) {
 			if (errno == ERANGE) {
 				buflen *= 2;
 				xrealloc(grp_buffer, buflen);