f528358f9726eefdf53772f7bb07d1a60ee1acd8
David Gloe <dgloe@cray.com>
2014-05-13 10:17:39 -0700

    when compiling with -Werror and -Warray-bounds:
    req.c: In function ‘_launch_complete_rm’:
    req.c:5372: error: array subscript is above array bounds
    req.c: In function ‘_launch_complete_add’:
    req.c:5328: error: array subscript is above array bounds
    
    The lines are
    if (job_id != active_job_id[j]) {
    
    after the for loops in those functions.
    
    If no match is found in the loop, j will be JOB_STATE_CNT and overflow the
    array by one.

--- src/slurmd/slurmd/req.c
+++ src/slurmd/slurmd/req.c
@@ -5333,7 +5333,7 @@ static void _launch_complete_add(uint32_t job_id)
 		if ((active_job_id[j] == 0) && (empty == -1))
 			empty = j;
 	}
-	if (job_id != active_job_id[j]) {
+	if (j >= JOB_STATE_CNT || job_id != active_job_id[j]) {
 		if (empty == -1)	/* Discard oldest job */
 			empty = 0;
 		for (j = empty + 1; j < JOB_STATE_CNT; j++) {
@@ -5377,7 +5377,7 @@ static void _launch_complete_rm(uint32_t job_id)
 		if (job_id == active_job_id[j])
 			break;
 	}
-	if (job_id == active_job_id[j]) {
+	if (j < JOB_STATE_CNT && job_id == active_job_id[j]) {
 		for (j = j + 1; j < JOB_STATE_CNT; j++) {
 			active_job_id[j - 1] = active_job_id[j];
 		}
