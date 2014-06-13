c289ed8bafb7d93ae052c3ba4e7070ef01f56231
Andrew Elwell <andrew.elwell@gmail.com>
2014-06-13 12:03:56 +0800

    Update FSF address
    No functional changes, just updating the last couple of files
    with the old FSF address in the comment.

--- src/common/slurm_step_layout.c
+++ src/common/slurm_step_layout.c
@@ -24,7 +24,7 @@
  *
  *  You should have received a copy of the GNU General Public License along
  *  with SLURM; if not, write to the Free Software Foundation, Inc.,
- *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
  *
  *  This file is patterned after hostlist.c, written by Mark Grondona and
  *  Copyright (C) 2002 The Regents of the University of California.
