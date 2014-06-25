354e43110a2311129e2244aa5322f34654801d02
Bill Brophy <bill.brophy@bull.com>
2014-06-25 12:32:54 -0700

    Make sure we lock on the conf when sending slurmd's conf to the slurmstepd.
    If we don't it could get messed up on a reconfig.

--- src/slurmd/slurmd/req.c
+++ src/slurmd/slurmd/req.c
@@ -448,7 +448,9 @@ static int _send_slurmd_conf_lite (int fd, slurmd_conf_t *cf)
 {
 	int len;
 	Buf buffer = init_buf(0);
+	slurm_mutex_lock(&cf->config_mutex);
 	pack_slurmd_conf_lite(cf, buffer);
+	slurm_mutex_unlock(&cf->config_mutex);
 	len = get_buf_offset(buffer);
 	safe_write(fd, &len, sizeof(int));
 	safe_write(fd, get_buf_data(buffer), len);
