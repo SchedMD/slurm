32e4bfabe72b4cc44fceb6a3e0db174939c68af4
Morris Jette <jette@schedmd.com>
2014-06-26 09:44:52 -0700

    Expand sched/backfill log
    For each job the backfill scheduling is testing, print not only
    the job ID, but also its priority and partition.

--- src/plugins/sched/backfill/backfill.c
+++ src/plugins/sched/backfill/backfill.c
@@ -783,8 +783,11 @@ static int _attempt_backfill(void)
 		}
 		job_ptr->part_ptr = part_ptr;
 
-		if (debug_flags & DEBUG_FLAG_BACKFILL)
-			info("backfill test for job %u", job_ptr->job_id);
+		if (debug_flags & DEBUG_FLAG_BACKFILL) {
+			info("backfill test for JobID=%u Prio=%u Partition=%s",
+			     job_ptr->job_id, job_ptr->priority,
+			     job_ptr->part_ptr->name);
+		}
 
 		if (max_backfill_job_per_part) {
 			bool skip_job = false;
