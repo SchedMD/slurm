SchedMD/slurm

commit d87d1b50ceedcca5f0c584fba6b8ff884a621e5e

Created new API for scancel. The API sends the jobid to slurmctld which then kill the job or the job array.

src/slurmctld/agent.c
@@ -269,18 +269,23 @@ void *agent(void *args)
 	thread_ptr = agent_info_ptr->thread_struct;
 
 	/* start the watchdog thread */
-	slurm_attr_init(&attr_wdog);
-	if (pthread_attr_setdetachstate
-	    (&attr_wdog, PTHREAD_CREATE_JOINABLE))
-		error("pthread_attr_setdetachstate error %m");
-	while (pthread_create(&thread_wdog, &attr_wdog, _wdog,
-				(void *) agent_info_ptr)) {
-		error("pthread_create error %m");
-		if (++retries > MAX_RETRIES)
-			fatal("Can't create pthread");
-		usleep(10000);	/* sleep and retry */
+	if (getenv("SLURM_NO_WDOG")) {
+		/* Development don't want threads be interrupted
+		 * by sigusr1. Remove later.
+		 */
+		slurm_attr_init(&attr_wdog);
+		if (pthread_attr_setdetachstate
+		    (&attr_wdog, PTHREAD_CREATE_JOINABLE))
+			error("pthread_attr_setdetachstate error %m");
+		while (pthread_create(&thread_wdog, &attr_wdog, _wdog,
+				      (void *) agent_info_ptr)) {
+			error("pthread_create error %m");
+			if (++retries > MAX_RETRIES)
+				fatal("Can't create pthread");
+			usleep(10000);	/* sleep and retry */
+		}
+		slurm_attr_destroy(&attr_wdog);
 	}
-	slurm_attr_destroy(&attr_wdog);
 #if 	AGENT_THREAD_COUNT < 1
 	fatal("AGENT_THREAD_COUNT value is invalid");
 #endif
@@ -328,7 +333,14 @@ void *agent(void *args)
 	}
 
 	/* wait for termination of remaining threads */
-	pthread_join(thread_wdog, NULL);
+
+	if (getenv("SLURM_NO_WDOG")) {
+		/* Development don't want threads be interrupted
+		 * by sigusr1. Remove later.
+		 */
+		pthread_join(thread_wdog, NULL);
+	}
+
 	delay = (int) difftime(time(NULL), begin_time);
 	if (delay > (slurm_get_msg_timeout() * 2)) {
 		info("agent msg_type=%u ran for %d seconds",

slurm/slurm.h.in
@@ -2683,6 +2683,15 @@ extern int slurm_kill_job PARAMS((uint32_t job_id, uint16_t signal,
  */
 extern int slurm_kill_job_step PARAMS((uint32_t job_id, uint32_t step_id,
 				       uint16_t signal));
+/*
+ * slurm_kill_job2()
+ */
+extern int slurm_kill_job2 PARAMS((const char *, uint16_t, uint16_t));
+
+/*
+ * slurm_kill_job_step2()
+ */
+extern int slurm_kill_job_step2 PARAMS((const char *, uint16_t, uint16_t));
 
 /*
  * slurm_signal_job - send the specified signal to all steps of an existing job

src/api/cancel.c
@@ -48,6 +48,7 @@
 
 #include "src/common/macros.h"
 #include "src/common/slurm_protocol_api.h"
+#include "src/common/xstring.h"
 
 /*
  * slurm_kill_job - send the specified signal to all steps of an existing job
@@ -114,6 +115,37 @@ slurm_kill_job_step (uint32_t job_id, uint32_t step_id, uint16_t signal)
 
 	if (rc)
 		slurm_seterrno_ret(rc);
+
+	return SLURM_SUCCESS;
+}
+
+/* slurm_kill_job2()
+ */
+int
+slurm_kill_job2(const char *job_id, uint16_t sig, uint16_t batch_flag)
+{
+	int cc;
+	slurm_msg_t msg;
+	struct job_kill_msg kill;
+
+	if (job_id == NULL) {
+		errno = EINVAL;
+		return SLURM_FAILURE;
+	}
+
+	slurm_msg_t_init(&msg);
+
+	kill.job_id = xstrdup(job_id);
+	kill.signal = sig;
+	kill.flags  = batch_flag;
+	msg.msg_type = REQUEST_KILL_JOB;
+	msg.data = &kill;
+
+	if (slurm_send_recv_controller_rc_msg(&msg, &cc) < 0)
+		return SLURM_FAILURE;
+
+	if (cc)
+		slurm_seterrno_ret(cc);
 
 	return SLURM_SUCCESS;
 }

src/common/slurm_protocol_defs.c
@@ -1113,6 +1113,16 @@ extern void slurm_free_ping_slurmd_resp(ping_slurmd_resp_msg_t *msg)
 	xfree(msg);
 }
 
+inline void slurm_free_kill_job_msg2(job_kill_msg_t *msg)
+{
+	if (msg != NULL)
+		return;
+
+	xfree(msg->job_id);
+	xfree(msg);
+
+}
+
 extern char *preempt_mode_string(uint16_t preempt_mode)
 {
 	char *gang_str;

src/common/slurm_protocol_defs.h
@@ -310,6 +310,8 @@ typedef enum {
 	REQUEST_FORWARD_DATA,
 	REQUEST_COMPLETE_BATCH_JOB,
 	REQUEST_SUSPEND_INT,
+	REQUEST_KILL_JOB,       /* 5032 */
+	REQUEST_KILL_JOBSTEP,
 
 	REQUEST_LAUNCH_TASKS = 6001,
 	RESPONSE_LAUNCH_TASKS,
@@ -506,6 +508,12 @@ typedef struct job_step_kill_msg {
 	uint16_t signal;
 	uint16_t flags;
 } job_step_kill_msg_t;
+
+typedef struct job_kill_msg {
+	char *job_id;
+	uint16_t signal;
+	uint16_t flags;
+} job_kill_msg_t;
 
 typedef struct job_notify_msg {
 	uint32_t job_id;
@@ -1244,7 +1252,7 @@ extern void slurm_free_spank_env_responce_msg(spank_env_responce_msg_t *msg);
 extern void slurm_free_requeue_msg(requeue_msg_t *);
 extern int slurm_free_msg_data(slurm_msg_type_t type, void *data);
 extern void slurm_free_license_info_request_msg(license_info_request_msg_t *msg);
-
+extern void slurm_free_kill_job_msg2(job_kill_msg_t *);
 extern uint32_t slurm_get_return_code(slurm_msg_type_t type, void *data);
 
 extern char *preempt_mode_string(uint16_t preempt_mode);

src/common/slurm_protocol_pack.c
@@ -209,6 +209,13 @@ static void _pack_kill_job_msg(kill_job_msg_t * msg, Buf buffer,
 static int _unpack_kill_job_msg(kill_job_msg_t ** msg, Buf buffer,
 				uint16_t protocol_version);
 
+static void _pack_kill_job_msg2(job_kill_msg_t *msg,
+				Buf buffer,
+				uint16_t protocol_version);
+static int _unpack_kill_job_msg2(job_kill_msg_t **msg,
+				 Buf buffer,
+				 uint16_t protocol_version);
+
 static void _pack_signal_job_msg(signal_job_msg_t * msg, Buf buffer,
 				 uint16_t protocol_version);
 static int _unpack_signal_job_msg(signal_job_msg_t ** msg, Buf buffer,
@@ -1318,6 +1325,11 @@ pack_msg(slurm_msg_t const *msg, Buf buffer)
 	case RESPONSE_LICENSE_INFO:
 		_pack_license_info_msg((slurm_msg_t *) msg, buffer);
 		break;
+	case REQUEST_KILL_JOB:
+		_pack_kill_job_msg2((struct job_kill_msg *)msg->data,
+				    buffer,
+				    msg->protocol_version);
+		break;
 	default:
 		debug("No pack method for msg type %u", msg->msg_type);
 		return EINVAL;
@@ -1950,6 +1962,11 @@ unpack_msg(slurm_msg_t * msg, Buf buffer)
 		                                      &(msg->data),
 		                                      buffer,
 		                                      msg->protocol_version);
+		break;
+	case REQUEST_KILL_JOB:
+		rc = _unpack_kill_job_msg2((job_kill_msg_t **)&(msg->data),
+					   buffer,
+					   msg->protocol_version);
 		break;
 	default:
 		debug("No unpack method for msg type %u", msg->msg_type);
@@ -4642,7 +4659,7 @@ static void _xlate_task_str(job_info_t *job_ptr)
 		if (bitstr_len < 0)
 			bitstr_len = 64;
 	}
-			
+
 	if (bitstr_len > 0) {
 		/* Print the first bitstr_len bytes of the bitmap string */
 		buf_size = bitstr_len;
@@ -11784,6 +11801,50 @@ unpack_error:
 	return SLURM_ERROR;
 }
 
+
+/* _pack_kill_job_msg2()
+ */
+static void
+_pack_kill_job_msg2(job_kill_msg_t * msg, Buf buffer, uint16_t protocol_version)
+{
+	xassert(msg != NULL);
+
+	if (protocol_version >= SLURM_14_11_PROTOCOL_VERSION) {
+		packstr(msg->job_id, buffer);
+		pack16(msg->signal, buffer);
+		pack16(msg->signal, buffer);
+	} else {
+		error("%s: protocol_version %hu not supported",
+		      __func__, protocol_version);
+	}
+}
+
+static int
+_unpack_kill_job_msg2(job_kill_msg_t **msg,
+		      Buf buffer,
+		      uint16_t protocol_version)
+{
+	uint32_t cc;
+
+	xassert(msg);
+	*msg = xmalloc(sizeof(kill_job_msg_t));
+
+	if (protocol_version >= SLURM_14_11_PROTOCOL_VERSION) {
+		safe_unpackstr_xmalloc(&(*msg)->job_id, &cc, buffer);
+		safe_unpack16(&(*msg)->signal, buffer);
+		safe_unpack16(&(*msg)->signal, buffer);
+	} else {
+		error("%s: protocol_version %hu not supported",
+		      __func__, protocol_version);
+	}
+
+	return SLURM_SUCCESS;
+
+unpack_error:
+	slurm_free_kill_job_msg2(*msg);
+	*msg = NULL;
+	return SLURM_ERROR;
+}
 
 /* template
    void pack_ ( * msg , Buf buffer )

src/scancel/opt.c
@@ -429,6 +429,7 @@ static void _opt_args(int argc, char **argv)
 
 	if (optind < argc) {
 		char **rest = argv + optind;
+		opt.job_list = xstrdup(*rest);
 		_xlate_job_step_ids(rest);
 	}
 

src/scancel/scancel.c
@@ -83,6 +83,7 @@ static void _load_job_records (void);
 static int  _multi_cluster(List clusters);
 static int  _proc_cluster(void);
 static int  _verify_job_ids (void);
+static int  _kill_job(void);
 
 static job_info_msg_t * job_buffer_ptr = NULL;
 
@@ -120,9 +121,9 @@ main (int argc, char *argv[])
 	if (opt.clusters)
 		rc = _multi_cluster(opt.clusters);
 	else
-		rc = _proc_cluster();
+		rc = _kill_job();
 
-	exit (rc);
+	exit(rc);
 }
 
 /* _multi_cluster - process job cancellation across a list of clusters */
@@ -722,4 +723,22 @@ _confirmation (int i, uint32_t step_id)
 			return 0;
 	}
 
+}
+
+/* _kill_job()
+ */
+static int
+_kill_job(void)
+{
+	int cc;
+
+	if (opt.signal == NO_VAL)
+		opt.signal = SIGKILL;
+
+	cc = slurm_kill_job2(opt.job_list, opt.signal, 0);
+	if (cc != SLURM_SUCCESS) {
+		error("slurm_kill_job2() failed %s", slurm_strerror(errno));
+		return -1;
+	}
+	return 0;
 }

commit 8079e28d85b4a1c1dcea3941bf24c1f3b3036ea7

Fix for slurmctld agent watchdog logic
Was failing to run watchdog thread to make note of hung threads
and not completion of RPC, which is needed for node ping.
This bug was introduced in commit d87d1b50ceedcca5f0c584fba6b8ff884a621e5e

src/slurmctld/agent.c
@@ -270,9 +270,10 @@ void *agent(void *args)
 
 	/* start the watchdog thread */
 	if (getenv("SLURM_NO_WDOG")) {
-		/* Development don't want threads be interrupted
-		 * by sigusr1. Remove later.
-		 */
+		/* Test purposes only. Do not want threads to be interrupted
+		 * by SIGUSR1. Breaks normal operation */
+		error("%s: Watchdog thread disabled", __func__);
+	} else {
 		slurm_attr_init(&attr_wdog);
 		if (pthread_attr_setdetachstate
 		    (&attr_wdog, PTHREAD_CREATE_JOINABLE))
@@ -332,12 +333,10 @@ void *agent(void *args)
 		slurm_mutex_unlock(&agent_info_ptr->thread_mutex);
 	}
 
-	/* wait for termination of remaining threads */
-
-	if (getenv("SLURM_NO_WDOG")) {
-		/* Development don't want threads be interrupted
-		 * by sigusr1. Remove later.
-		 */
+	/* Wait for termination of remaining threads */
+	if (!getenv("SLURM_NO_WDOG")) {
+		/* Test purposes only. Do not want threads to be interrupted
+		 * by SIGUSR1 */
 		pthread_join(thread_wdog, NULL);
 	}
 