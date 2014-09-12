61d2ccea25aaa0977c4ae3626933ce60fc283d38
Danny Auble <da@schedmd.com>
2014-09-12 13:56:00 -0700

    CRAY - When a job is modified don't modify the gres in the cray job_submit plugin

--- src/plugins/job_submit/cray/job_submit_cray.c
+++ src/plugins/job_submit/cray/job_submit_cray.c
@@ -138,6 +138,9 @@ extern int job_submit(struct job_descriptor *job_desc, uint32_t submit_uid)
 extern int job_modify(struct job_descriptor *job_desc,
 		      struct job_record *job_ptr, uint32_t submit_uid)
 {
-	_append_gres(job_desc);
+	/* Don't call this on modify it shouldn't be needed and will
+	 * mess things up if modifying a running job
+	 */
+	//_append_gres(job_desc);
 	return SLURM_SUCCESS;
 }
