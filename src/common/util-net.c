cff3a1d66221c83f72cddcc77e0063d84661268e
Nicolas Joly <njoly@pasteur.fr>
2014-09-25 15:33:59 -0700

    NetBSD does not have get_current_dir_name() function unlike Linux and FreeBSD

--- src/common/util-net.c
+++ src/common/util-net.c
@@ -47,6 +47,7 @@
 #include <arpa/inet.h>
 #include <assert.h>
 #include <errno.h>
+#include <limits.h>	/* for PATH_MAX */
 #include <netdb.h>
 #include <netinet/in.h>
 #include <pthread.h>
@@ -395,7 +396,12 @@ extern char *make_full_path(char *rpath)
 	char *cwd2;
 	int len;
 
-	cwd =  get_current_dir_name();
+#ifdef HAVE_GET_CURRENT_DIR_NAME
+	cwd = get_current_dir_name();
+#else
+	cwd = xmalloc(PATH_MAX);
+	cwd = getcwd(cwd, PATH_MAX);
+#endif
 	/* 2 = / + 0
 	 */
 	len = strlen(cwd) + strlen(rpath) + 2;
