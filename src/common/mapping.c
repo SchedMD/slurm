ba2f4fe98db3f5869138e0f48e9d9379edcebe06
Artem Polyakov <artpol84@gmail.com>
2014-11-14 18:31:20 +0600

    1. Fix the tupo in mapping function 2. Add unmapping functions that can be used to reconstruct the mapping on stepd's 3. Add the test suite right in mapping.c (by default is preprocessed out).

--- src/common/mapping.c
+++ src/common/mapping.c
@@ -35,6 +35,10 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
+#define STANDALONE 0
+
+#if (STANDALONE == 0)
+
 #include <stdint.h>
 #include <string.h>
 #include "xmalloc.h"
@@ -43,6 +47,34 @@
 #include "slurm/slurm_errno.h"
 
 BEGIN_C_DECLS
+
+#else
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define error printf
+#define xassert assert
+#define xmalloc malloc
+#define xfree free
+#define SLURM_ERROR (-1)
+#define xstrdup strdup
+#define xstrfmtcat(ptr, fmt, args ... )     \
+{											\
+	char *nptr = NULL;                      \
+	asprintf(&nptr, "%s"fmt, ptr, ##args);  \
+	free(ptr);                              \
+	ptr = nptr;							    \
+}
+#define xstrcat(a, b) xstrfmtcat(a, "%s", b)
+
+#endif
+
+
 
 char *pack_process_mapping(uint32_t node_cnt, uint32_t task_cnt, uint16_t *tasks, uint32_t **tids)
 {
@@ -87,7 +119,7 @@ char *pack_process_mapping(uint32_t node_cnt, uint32_t task_cnt, uint16_t *tasks
 			} else {
 				// If this is not the first node in the bar check that:
 				// 1. First tid on this node is sequentially next after last tid on the previous node
-				if( t2n_map[i-1][next_task[i-1]-1] + 1 != t2n_map[i][next_task[i]] ){
+				if( tids[i-1][next_task[i-1]-1] + 1 != tids[i][next_task[i]] ){
 					end_node = i;
 					continue;
 				}
@@ -108,4 +140,228 @@ char *pack_process_mapping(uint32_t node_cnt, uint32_t task_cnt, uint16_t *tasks
 	return packing;
 }
 
+uint32_t *unpack_process_mapping_flat(char *map, uint32_t node_cnt, uint32_t task_cnt, uint16_t *tasks)
+{
+	// Start from the flat array. For i'th task is located on the task_map[i]'th node
+	uint32_t *task_map = xmalloc(sizeof(int) * task_cnt);
+	char *prefix = "(vector,", *p = NULL;
+	uint32_t taskid, i;
+
+	if( tasks ){
+		for(i=0; i<node_cnt; i++){
+			tasks[i] = 0;
+		}
+	}
+
+	if( (p = strstr(map, prefix)) == NULL ){
+		error("unpack_process_mapping: The mapping string should start from %s", prefix);
+		goto err_exit;
+	}
+
+	// Skip prefix
+	p += strlen(prefix);
+	taskid = 0;
+	while( ( p = strchr(p,'(') ) ){
+		int depth, node, end_node;
+		p++;
+		if( 3!= sscanf(p,"%d,%d,%d", &node, &end_node, &depth) ){
+			goto err_exit;
+		}
+		end_node += node;
+		xassert(node < node_cnt && end_node <= node_cnt );
+		for(; node < end_node; node++){
+			for(i=0; i<depth; i++){
+				task_map[taskid++] = node;
+				if( tasks != NULL ){
+					// Cont tasks on each node if was requested
+					tasks[node]++;
+				}
+			}
+		}
+	}
+	return task_map;
+err_exit:
+	xfree(task_map);
+	return NULL;
+}
+
+int unpack_process_mapping(char *map, uint32_t node_cnt, uint32_t task_cnt, uint16_t *tasks, uint32_t **tids)
+{
+	// Start from the flat array. For i'th task is located on the task_map[i]'th node
+	uint32_t *task_map = NULL;
+	uint16_t *node_task_cnt = NULL;
+	uint32_t taskid, i;
+	int rc = 0;
+
+	if( ( task_map = unpack_process_mapping_flat(map, node_cnt, task_cnt, tasks) ) == NULL ){
+		rc = SLURM_ERROR;
+		goto err_exit;
+	}
+
+	node_task_cnt = xmalloc(sizeof(uint16_t) * node_cnt);
+	for(i=0; i<node_cnt; i++){
+		tids[i] = xmalloc(sizeof(uint32_t) * tasks[i]);
+		node_task_cnt[i] = 0;
+	}
+
+	for(i=0;i<task_cnt;i++){
+		uint32_t node = task_map[i];
+		tids[node][ node_task_cnt[node]++ ] = i;
+		xassert( node_task_cnt[node] <= tasks[node] );
+	}
+
+	goto exit;
+err_exit:
+	error("unpack_process_mapping: bad mapping format");
+exit:
+	if( task_map != NULL ){
+		xfree( task_map );
+	}
+	if( node_task_cnt != NULL ){
+		xfree( node_task_cnt );
+	}
+	return rc;
+}
+
+
+
+#if (STANDALONE == 0 )
+
 END_C_DECLS
+
+#else
+
+/*
+ * Mutual check for both routines
+ */
+
+// Emulate 16-core nodes
+#define NCPUS 16
+#define NODES 200
+
+static void block_distr(uint32_t task_cnt, uint16_t *tasks, uint32_t **tids)
+{
+	int i, j, tnum = 0;
+	for(i=0; i < NODES;i++){
+		tasks[i] = 0;
+	}
+	// BLOCK distribution
+	for(i=0; (i < NODES) && (tnum < task_cnt); i++ ){
+		for(j=0; j < NCPUS && (tnum < task_cnt); j++){
+			tids[i][j] = tnum++;
+		}
+		tasks[i] = j;
+	}
+}
+
+static void cyclic_distr(uint32_t task_cnt, uint16_t *tasks, uint32_t **tids)
+{
+	int i, j, tnum = 0;
+	// CYCLIC distribution
+	tnum = 0;
+	for(i=0; i < NODES;i++){
+		tasks[i] = 0;
+	}
+	for(j=0; j < NCPUS && (tnum < task_cnt); j++){
+		for(i=0; (i < NODES) && (tnum < task_cnt); i++ ){
+			tids[i][j] = tnum++;
+			tasks[i]++;
+		}
+	}
+}
+
+
+static void plane_distr(uint32_t task_cnt, int plane_factor, uint16_t *tasks, uint32_t **tids)
+{
+	int i, j, tnum = 0;
+	// PLANE distribution
+	tnum = 0;
+	for(i=0; i < NODES;i++){
+		tasks[i] = 0;
+	}
+	while( tnum < task_cnt ){
+		for(i=0; (i < NODES) && (tnum < task_cnt); i++ ){
+			for(j=0; (j < plane_factor) && (tasks[i] < NCPUS) && (tnum < task_cnt); j++){
+				tids[i][tasks[i]++] = tnum++;
+			}
+		}
+	}
+}
+
+static void check(uint32_t node_cnt, uint32_t task_cnt, uint16_t *tasks, uint32_t **tids)
+{
+	uint16_t *new_tasks;
+	uint32_t **new_tids;
+	char *map = pack_process_mapping(node_cnt, task_cnt, tasks, tids);
+	int i,j;
+
+	printf("mapping: %s\n", map);
+
+	new_tasks = xmalloc(sizeof(uint16_t) * node_cnt);
+	new_tids = xmalloc(sizeof(uint32_t *) * node_cnt);
+	unpack_process_mapping(map,node_cnt,task_cnt,new_tasks,new_tids);
+
+	for(i=0; i<node_cnt; i++){
+		if( new_tasks[i] != tasks[i] ){
+			printf("Task count mismatch on node %d\n", i);
+			exit(0);
+		}
+		for(j=0;j<tasks[i];j++){
+			if( new_tids[i][j] != tids[i][j] ){
+				printf("Task id mismatch on node %d, idx = %d\n", i, j);
+				exit(0);
+			}
+		}
+	}
+	for(i=0;i<node_cnt;i++){
+		xfree(new_tids[i]);
+	}
+	xfree(new_tasks);
+	xfree(new_tids);
+
+	xfree(map);
+
+}
+
+int main()
+{
+	uint16_t  tasks[NODES] = { 0 };
+	uint32_t **tids = NULL;
+	int tnum = 0, i;
+
+	tids = xmalloc(sizeof(uint32_t*) * NODES);
+	for(i=0;i<NODES;i++){
+		tids[i] = xmalloc(sizeof(uint32_t) * NCPUS);
+	}
+
+	for(tnum=1; tnum< NCPUS*NODES; tnum++){
+		printf("Map %d tasks into cluster %dx%d\n",tnum,NODES, NCPUS);
+		block_distr(tnum, tasks, tids);
+		check(NODES,tnum, tasks, tids);
+
+		cyclic_distr(tnum, tasks, tids);
+		check(NODES,tnum, tasks, tids);
+
+		plane_distr(tnum,2,tasks, tids);
+		check(NODES,tnum, tasks, tids);
+
+		plane_distr(tnum,4,tasks, tids);
+		check(NODES,tnum, tasks, tids);
+
+		plane_distr(tnum,6,tasks, tids);
+		check(NODES,tnum, tasks, tids);
+
+		plane_distr(tnum,8,tasks, tids);
+		check(NODES,tnum, tasks, tids);
+	}
+
+
+	for(i=0;i<NODES;i++){
+		xfree(tids[i]);
+	}
+	xfree(tids);
+
+	return 0;
+}
+
+#endif
