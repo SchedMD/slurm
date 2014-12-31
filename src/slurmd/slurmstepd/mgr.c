SchedMD/slurm

commit 2a5271c1af4599f5bd17dbe68dcc12fc024fe717

Make it so a newer version of a slurmstepd can talk to an older srun.

src/slurmd/slurmstepd/mgr.c
@@ -177,7 +177,7 @@ typedef struct kill_thread {
  */
 static int  _access(const char *path, int modes, uid_t uid, gid_t gid);
 static void _send_launch_failure(launch_tasks_request_msg_t *,
-				 slurm_addr_t *, int);
+				 slurm_addr_t *, int, uint16_t);
 static int  _drain_node(char *reason);
 static int  _fork_all_tasks(stepd_step_rec_t *job, bool *io_initialized);
 static int  _become_user(stepd_step_rec_t *job, struct priv_state *ps);
@@ -228,17 +228,17 @@ static stepd_step_rec_t *reattach_job;
  */
 extern stepd_step_rec_t *
 mgr_launch_tasks_setup(launch_tasks_request_msg_t *msg, slurm_addr_t *cli,
-		       slurm_addr_t *self)
+		       slurm_addr_t *self, uint16_t protocol_version)
 {
 	stepd_step_rec_t *job = NULL;
 
-	if (!(job = stepd_step_rec_create(msg))) {
+	if (!(job = stepd_step_rec_create(msg, protocol_version))) {
 		/* We want to send back to the slurmd the reason we
 		   failed so keep track of it since errno could be
 		   reset in _send_launch_failure.
 		*/
 		int fail = errno;
-		_send_launch_failure (msg, cli, errno);
+		_send_launch_failure(msg, cli, errno, protocol_version);
 		errno = fail;
 		return NULL;
 	}
@@ -707,6 +707,7 @@ _send_exit_msg(stepd_step_rec_t *job, uint32_t *tid, int n, int status)
 	i = list_iterator_create(job->sruns);
 	while ((srun = list_next(i))) {
 		resp.address = srun->resp_addr;
+		resp.protocol_version = srun->protocol_version;
 		if ((resp.address.sin_family == 0) &&
 		    (resp.address.sin_port == 0)   &&
 		    (resp.address.sin_addr.s_addr == 0))
@@ -2081,7 +2082,8 @@ static int _drain_node(char *reason)
 }
 
 static void
-_send_launch_failure (launch_tasks_request_msg_t *msg, slurm_addr_t *cli, int rc)
+_send_launch_failure(launch_tasks_request_msg_t *msg, slurm_addr_t *cli, int rc,
+		     uint16_t protocol_version)
 {
 	slurm_msg_t resp_msg;
 	launch_tasks_response_msg_t resp;
@@ -2104,6 +2106,7 @@ _send_launch_failure (launch_tasks_request_msg_t *msg, slurm_addr_t *cli, int rc
 		       NULL);
 	resp_msg.data = &resp;
 	resp_msg.msg_type = RESPONSE_LAUNCH_TASKS;
+	resp_msg.protocol_version = protocol_version;
 
 	resp.node_name     = name;
 	resp.return_code   = rc ? rc : -1;
@@ -2130,6 +2133,7 @@ _send_launch_resp(stepd_step_rec_t *job, int rc)
 
 	slurm_msg_t_init(&resp_msg);
 	resp_msg.address	= srun->resp_addr;
+	resp_msg.protocol_version = srun->protocol_version;
 	resp_msg.data		= &resp;
 	resp_msg.msg_type	= RESPONSE_LAUNCH_TASKS;
 

src/slurmd/slurmd/req.c
@@ -472,7 +472,7 @@ static int _send_slurmd_conf_lite (int fd, slurmd_conf_t *cf)
 static int
 _send_slurmstepd_init(int fd, slurmd_step_type_t type, void *req,
 		      slurm_addr_t *cli, slurm_addr_t *self,
-		      hostset_t step_hset)
+		      hostset_t step_hset, uint16_t protocol_version)
 {
 	int len = 0;
 	Buf buffer = NULL;
@@ -481,7 +481,7 @@ _send_slurmstepd_init(int fd, slurmd_step_type_t type, void *req,
 	gid_t gid = (uid_t)-1;
 	gids_t *gids = NULL;
 
-	int rank;
+	int rank, proto;
 	int parent_rank, children, depth, max_depth;
 	char *parent_alias = NULL;
 	char *user_name = NULL;
@@ -626,9 +626,18 @@ _send_slurmstepd_init(int fd, slurmd_step_type_t type, void *req,
 	}
 	buffer = init_buf(0);
 	msg.data = req;
-	msg.protocol_version = SLURM_PROTOCOL_VERSION;
+
+	if (protocol_version == (uint16_t)NO_VAL)
+		proto = SLURM_PROTOCOL_VERSION;
+	else
+		proto = protocol_version;
+
+	msg.protocol_version = (uint16_t)proto;
 	pack_msg(&msg, buffer);
 	len = get_buf_offset(buffer);
+
+	safe_write(fd, &proto, sizeof(int));
+
 	safe_write(fd, &len, sizeof(int));
 	safe_write(fd, get_buf_data(buffer), len);
 	free_buf(buffer);
@@ -708,7 +717,7 @@ rwfail:
 static int
 _forkexec_slurmstepd(slurmd_step_type_t type, void *req,
 		     slurm_addr_t *cli, slurm_addr_t *self,
-		     const hostset_t step_hset)
+		     const hostset_t step_hset, uint16_t protocol_version)
 {
 	pid_t pid;
 	int to_stepd[2] = {-1, -1};
@@ -750,7 +759,8 @@ _forkexec_slurmstepd(slurmd_step_type_t type, void *req,
 
 		if ((rc = _send_slurmstepd_init(to_stepd[1], type,
 						req, cli, self,
-						step_hset)) != 0) {
+						step_hset,
+						protocol_version)) != 0) {
 			error("Unable to init slurmstepd");
 			goto done;
 		}
@@ -1227,7 +1237,7 @@ _rpc_launch_tasks(slurm_msg_t *msg)
 
 	debug3("_rpc_launch_tasks: call to _forkexec_slurmstepd");
 	errnum = _forkexec_slurmstepd(LAUNCH_TASKS, (void *)req, cli, &self,
-				      step_hset);
+				      step_hset, msg->protocol_version);
 	debug3("_rpc_launch_tasks: return from _forkexec_slurmstepd");
 	_launch_complete_add(req->job_id);
 
@@ -1671,7 +1681,7 @@ _rpc_batch_job(slurm_msg_t *msg, bool new_msg)
 
 	debug3("_rpc_batch_job: call to _forkexec_slurmstepd");
 	rc = _forkexec_slurmstepd(LAUNCH_BATCH_JOB, (void *)req, cli, NULL,
-				  (hostset_t)NULL);
+				  (hostset_t)NULL, SLURM_PROTOCOL_VERSION);
 	debug3("_rpc_batch_job: return from _forkexec_slurmstepd: %d", rc);
 
 	slurm_mutex_unlock(&launch_mutex);

src/slurmd/slurmstepd/io.c
@@ -1932,6 +1932,7 @@ _user_managed_io_connect(srun_info_t *srun, uint32_t gtid)
 	slurm_msg_t msg;
 
 	slurm_msg_t_init(&msg);
+	msg.protocol_version = srun->protocol_version;
 	msg.msg_type = TASK_USER_MANAGED_IO_STREAM;
 	msg.data = &user_io_msg;
 	user_io_msg.task_id = gtid;

src/slurmd/slurmstepd/mgr.h
@@ -56,13 +56,15 @@ void batch_finish(stepd_step_rec_t *job, int rc);
  * Initialize a stepd_step_rec_t structure for a launch tasks
  */
 stepd_step_rec_t *mgr_launch_tasks_setup(launch_tasks_request_msg_t *msg,
-				     slurm_addr_t *client, slurm_addr_t *self);
+					 slurm_addr_t *client,
+					 slurm_addr_t *self,
+					 uint16_t protocol_version);
 
 /*
  * Initialize a stepd_step_rec_t structure for a batch job
  */
 stepd_step_rec_t *mgr_launch_batch_job_setup(batch_job_launch_msg_t *msg,
-					 slurm_addr_t *client);
+					     slurm_addr_t *client);
 
 /*
  * Finalize a batch job.

src/slurmd/slurmstepd/slurmstepd.c
@@ -77,7 +77,7 @@ static void _dump_user_env(void);
 static void _send_ok_to_slurmd(int sock);
 static void _send_fail_to_slurmd(int sock);
 static stepd_step_rec_t *_step_setup(slurm_addr_t *cli, slurm_addr_t *self,
-				 slurm_msg_t *msg);
+				     slurm_msg_t *msg);
 #ifdef MEMORY_LEAK_DEBUG
 static void _step_cleanup(stepd_step_rec_t *job, slurm_msg_t *msg, int rc);
 #endif
@@ -378,7 +378,7 @@ _init_from_slurmd(int sock, char **argv,
 	char *incoming_buffer = NULL;
 	Buf buffer;
 	int step_type;
-	int len;
+	int len, proto;
 	slurm_addr_t *cli = NULL;
 	slurm_addr_t *self = NULL;
 	slurm_msg_t *msg = NULL;
@@ -453,6 +453,9 @@ _init_from_slurmd(int sock, char **argv,
 	/* Receive cpu_frequency info from slurmd */
 	cpu_freq_recv_info(sock);
 
+	/* get the protocol version of the srun */
+	safe_read(sock, &proto, sizeof(int));
+
 	/* receive req from slurmd */
 	safe_read(sock, &len, sizeof(int));
 	incoming_buffer = xmalloc(sizeof(char) * len);
@@ -461,7 +464,7 @@ _init_from_slurmd(int sock, char **argv,
 
 	msg = xmalloc(sizeof(slurm_msg_t));
 	slurm_msg_t_init(msg);
-	msg->protocol_version = SLURM_PROTOCOL_VERSION;
+	msg->protocol_version = (uint16_t)proto;
 
 	switch(step_type) {
 	case LAUNCH_BATCH_JOB:
@@ -517,7 +520,8 @@ _step_setup(slurm_addr_t *cli, slurm_addr_t *self, slurm_msg_t *msg)
 		break;
 	case REQUEST_LAUNCH_TASKS:
 		debug2("setup for a launch_task");
-		job = mgr_launch_tasks_setup(msg->data, cli, self);
+		job = mgr_launch_tasks_setup(msg->data, cli, self,
+					     msg->protocol_version);
 		break;
 	default:
 		fatal("handle_launch_message: Unrecognized launch RPC");

src/slurmd/slurmstepd/slurmstepd_job.c
@@ -279,7 +279,7 @@ _task_info_destroy(stepd_step_task_info_t *t, uint16_t multi_prog)
 
 /* create a slurmd job structure from a launch tasks message */
 extern stepd_step_rec_t *
-stepd_step_rec_create(launch_tasks_request_msg_t *msg)
+stepd_step_rec_create(launch_tasks_request_msg_t *msg, uint16_t protocol_version)
 {
 	stepd_step_rec_t  *job = NULL;
 	srun_info_t   *srun = NULL;
@@ -387,7 +387,8 @@ stepd_step_rec_create(launch_tasks_request_msg_t *msg)
 			       NULL);
 	}
 
-	srun = srun_info_create(msg->cred, &resp_addr, &io_addr);
+	srun = srun_info_create(msg->cred, &resp_addr, &io_addr,
+				protocol_version);
 
 	job->buffered_stdio = msg->buffered_stdio;
 	job->labelio = msg->labelio;
@@ -545,7 +546,7 @@ batch_stepd_step_rec_create(batch_job_launch_msg_t *msg)
 	get_cred_gres(msg->cred, conf->node_name,
 		      &job->job_gres_list, &job->step_gres_list);
 
-	srun = srun_info_create(NULL, NULL, NULL);
+	srun = srun_info_create(NULL, NULL, NULL, (uint16_t)NO_VAL);
 
 	list_append(job->sruns, (void *) srun);
 
@@ -608,7 +609,8 @@ stepd_step_rec_destroy(stepd_step_rec_t *job)
 }
 
 extern srun_info_t *
-srun_info_create(slurm_cred_t *cred, slurm_addr_t *resp_addr, slurm_addr_t *ioaddr)
+srun_info_create(slurm_cred_t *cred, slurm_addr_t *resp_addr,
+		 slurm_addr_t *ioaddr, uint16_t protocol_version)
 {
 	char             *data = NULL;
 	uint32_t          len  = 0;
@@ -616,7 +618,9 @@ srun_info_create(slurm_cred_t *cred, slurm_addr_t *resp_addr, slurm_addr_t *ioad
 	srun_key_t       *key  = xmalloc(sizeof(srun_key_t));
 
 	srun->key    = key;
-
+	if (protocol_version == (uint16_t)NO_VAL)
+		protocol_version = SLURM_PROTOCOL_VERSION;
+	srun->protocol_version = protocol_version;
 	/*
 	 * If no credential was provided, return the empty
 	 * srun info object. (This is used, for example, when

src/slurmd/slurmstepd/slurmstepd_job.h
@@ -72,6 +72,7 @@ typedef struct {
 	slurm_addr_t ioaddr;       /* Address to connect on for normal I/O.
 				      Spawn IO uses messages to the normal
 				      resp_addr. */
+	uint16_t protocol_version; /* protocol_version of the srun */
 } srun_info_t;
 
 typedef enum {
@@ -235,13 +236,14 @@ typedef struct {
 } stepd_step_rec_t;
 
 
-stepd_step_rec_t * stepd_step_rec_create(launch_tasks_request_msg_t *msg);
+stepd_step_rec_t * stepd_step_rec_create(launch_tasks_request_msg_t *msg,
+					 uint16_t protocol_version);
 stepd_step_rec_t * batch_stepd_step_rec_create(batch_job_launch_msg_t *msg);
 
 void stepd_step_rec_destroy(stepd_step_rec_t *job);
 
 srun_info_t * srun_info_create(slurm_cred_t *cred, slurm_addr_t *respaddr,
-				    slurm_addr_t *ioaddr);
+			       slurm_addr_t *ioaddr, uint16_t protocol_version);
 
 void  srun_info_destroy(srun_info_t *srun);
 

commit 501b8c1af6f7ffd1753d0c6aeeae2d1e95cb612f

Fix for sattach protocol version
This was broken by commit 2a5271c1af4599f5bd17dbe68dcc12fc024fe717
test16.4 was failing since sattach was not setting a protocol version.

src/slurmd/slurmstepd/mgr.c
@@ -707,11 +707,13 @@ _send_exit_msg(stepd_step_rec_t *job, uint32_t *tid, int n, int status)
 	i = list_iterator_create(job->sruns);
 	while ((srun = list_next(i))) {
 		resp.address = srun->resp_addr;
-		resp.protocol_version = srun->protocol_version;
 		if ((resp.address.sin_family == 0) &&
 		    (resp.address.sin_port == 0)   &&
 		    (resp.address.sin_addr.s_addr == 0))
 			continue;	/* no srun or sattach here */
+
+		if (srun->protocol_version)	/* Not set by sattach */
+			resp.protocol_version = srun->protocol_version;
 
 		if (_send_srun_resp_msg(&resp, job->nnodes) != SLURM_SUCCESS)
 			error("Failed to send MESSAGE_TASK_EXIT: %m");