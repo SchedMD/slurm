SchedMD/slurm

commit 6a389a7aa045f8ca40326dfccc0bc3cbfbe69ac2

In srun honor ntasks_per_node before looking at cpu count when the user doesn't request a number of tasks.

src/srun/libsrun/srun_job.c
@@ -106,7 +106,6 @@ static int pty_sigarray[] = { SIGWINCH, 0 };
 /*
  * Prototypes:
  */
-static int        _compute_task_count(allocation_info_t *info);
 static void       _set_ntasks(allocation_info_t *info);
 static srun_job_t *_job_create_structure(allocation_info_t *info);
 static char *     _normalize_hostlist(const char *hostlist);
@@ -747,33 +746,31 @@ job_force_termination(srun_job_t *job)
 	kill_sent++;
 }
 
-static int
-_compute_task_count(allocation_info_t *ainfo)
-{
-	int i, cnt = 0;
-#if defined HAVE_BGQ
-//#if defined HAVE_BGQ && HAVE_BG_FILES
-	/* always return the ntasks here for Q */
-	return opt.ntasks;
-#endif
-	if (opt.cpus_set) {
-		for (i = 0; i < ainfo->num_cpu_groups; i++)
-			cnt += ( ainfo->cpu_count_reps[i] *
-				 (ainfo->cpus_per_node[i]/opt.cpus_per_task));
-	} else if (opt.ntasks_per_node != NO_VAL)
-		cnt = ainfo->nnodes * opt.ntasks_per_node;
-
-	return (cnt < ainfo->nnodes) ? ainfo->nnodes : cnt;
-}
-
 static void
 _set_ntasks(allocation_info_t *ai)
 {
-	if (!opt.ntasks_set) {
-		opt.ntasks = _compute_task_count(ai);
-		if (opt.cpus_set)
-			opt.ntasks_set = true;	/* implicit */
+	int cnt = 0;
+
+	if (opt.ntasks_set)
+		return;
+
+#if defined HAVE_BGQ
+	/* always return the ntasks here for Q */
+	return opt.ntasks;
+#endif
+	if (opt.ntasks_per_node != NO_VAL) {
+		cnt = ai->nnodes * opt.ntasks_per_node;
+		opt.ntasks_set = true;	/* implicit */
+	} else if (opt.cpus_set) {
+		int i;
+
+		for (i = 0; i < ai->num_cpu_groups; i++)
+			cnt += (ai->cpu_count_reps[i] *
+				(ai->cpus_per_node[i] / opt.cpus_per_task));
+		opt.ntasks_set = true;	/* implicit */
 	}
+
+	opt.ntasks = (cnt < ai->nnodes) ? ai->nnodes : cnt;
 }
 
 /*

src/srun/libsrun/opt.c
@@ -2054,8 +2054,10 @@ static bool _opt_verify(void)
 			opt.ntasks *= opt.cores_per_socket;
 			opt.ntasks *= opt.threads_per_core;
 			opt.ntasks_set = true;
-		} else if (opt.ntasks_per_node != NO_VAL)
+		} else if (opt.ntasks_per_node != NO_VAL) {
 			opt.ntasks *= opt.ntasks_per_node;
+			opt.ntasks_set = true;
+		}
 
 		/* massage the numbers */
 		if (opt.nodelist) {

commit 5afaf3929e47ba68c636cb8b0721c188e9ddafad

BGQ - Fix regression 6a389a7a caused.  This check is no longer needed because of the referenced commit.  ntasks_set is always true on a BGQ at this point.

src/srun/libsrun/srun_job.c
@@ -754,10 +754,6 @@ _set_ntasks(allocation_info_t *ai)
 	if (opt.ntasks_set)
 		return;
 
-#if defined HAVE_BGQ
-	/* always return the ntasks here for Q */
-	return opt.ntasks;
-#endif
 	if (opt.ntasks_per_node != NO_VAL) {
 		cnt = ai->nnodes * opt.ntasks_per_node;
 		opt.ntasks_set = true;	/* implicit */