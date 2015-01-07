096d534cc3147887ea351697ac13791ac847e5c6
Brian Christiansen <brian@schedmd.com>
2015-01-07 14:05:21 -0800

    Increase maximum node list size (for incoming RPC) from 64k to num_tasks * MAXHOSTNAMELEN.
    Bug 1352

--- src/slurmctld/step_mgr.c
+++ src/slurmctld/step_mgr.c
@@ -2154,7 +2154,8 @@ step_create(job_step_create_request_msg_t *step_specs,
 	    _test_strlen(step_specs->host, "host", 1024)		||
 	    _test_strlen(step_specs->name, "name", 1024)		||
 	    _test_strlen(step_specs->network, "network", 1024)		||
-	    _test_strlen(step_specs->node_list, "node_list", 1024*64))
+	    _test_strlen(step_specs->node_list, "node_list",
+		    step_specs->num_tasks * MAXHOSTNAMELEN))
 		return ESLURM_PATHNAME_TOO_LONG;
 
 	if (job_ptr->next_step_id >= slurmctld_conf.max_step_cnt)
