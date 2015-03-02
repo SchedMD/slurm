49770e20b6c18e4aedd3fe2567505bbcc8247451
Morris Jette <jette@schedmd.com>
2015-03-02 15:28:53 -0800

    Abort I/O for debugged app launch fail
    For job running under a debugger, if the exec of the task fails, then
    cancel its I/O and abort immediately rather than waiting 60 seconds for
    I/O timeout.

--- src/slurmd/slurmstepd/mgr.c
+++ src/slurmd/slurmstepd/mgr.c
@@ -1665,8 +1665,10 @@ _fork_all_tasks(stepd_step_rec_t *job, bool *io_initialized)
 		 * (if specified and able)
 		 */
 		if (pdebug_trace_process(job, job->task[i]->pid)
-		    == SLURM_ERROR)
+		    == SLURM_ERROR) {
 			rc = SLURM_ERROR;
+			goto fail2;
+		}
 	}
 
 	return rc;
