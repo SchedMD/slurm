f495ac06aec5ef8c8539260f21e3c0276fbf1308
Morris Jette <jette@schedmd.com>
2015-05-11 15:20:09 -0700

    Fix to enable gang scheduling with preemption
    test34.1 was failing with a configuration of
    PreemptMode=suspend,gang
    PreemptType=preempt/partition_prio
    due to changes in commit f8fb79d5cd185f2babb20ad39de6419496f6c696
    That change appears to no longer be necessary and breaks a
    valid configuration.

--- src/plugins/select/cons_res/job_test.c
+++ src/plugins/select/cons_res/job_test.c
@@ -2816,6 +2816,7 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *node_bitmap,
 			bitstr_t *exc_core_bitmap, bool prefer_alloc_nodes,
 			bool qos_preemptor)
 {
+	static int gang_mode = -1;
 	int error_code = SLURM_SUCCESS, ll; /* ll = layout array index */
 	uint16_t *layout_ptr = NULL;
 	bitstr_t *orig_map, *avail_cores, *free_cores, *part_core_map = NULL;
@@ -2828,6 +2829,13 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *node_bitmap,
 	struct part_res_record *p_ptr, *jp_ptr;
 	uint16_t *cpu_count;
 	int first, last;
+
+	if (gang_mode == -1) {
+		if (slurm_get_preempt_mode() & PREEMPT_MODE_GANG)
+			gang_mode = 1;
+		else
+			gang_mode = 0;
+	}
 
 	details_ptr = job_ptr->details;
 	layout_ptr  = details_ptr->req_node_layout;
@@ -3027,15 +3035,14 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *node_bitmap,
 		goto alloc_job;
 	}
 
-	if (job_node_req == NODE_CR_ONE_ROW) {
+	if ((gang_mode == 0) && (job_node_req == NODE_CR_ONE_ROW)) {
 		/* This job CANNOT share CPUs regardless of priority,
 		 * so we fail here. Note that Shared=EXCLUSIVE was already
 		 * addressed in _verify_node_state() and job preemption
 		 * removes jobs from simulated resource allocation map
 		 * before this point. */
 		if (select_debug_flags & DEBUG_FLAG_SELECT_TYPE) {
-			info("cons_res: cr_job_test: test 1 fail - "
-			     "no idle resources available");
+			info("cons_res: cr_job_test: test 1 fail - no idle resources available");
 		}
 		goto alloc_job;
 	}
