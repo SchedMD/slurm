1225474b7cd7017665b3e310f50719553a0df11b
Morris Jette <jette@schedmd.com>
2015-05-14 12:48:36 -0700

    Remove error for container step on reconfig
    Previously was reporting:
    Missing node_list for step_id 12727.4294967295

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -8974,6 +8974,7 @@ static void _reset_step_bitmaps(struct job_record *job_ptr)
 			delete_step_record (job_ptr, step_ptr->step_id);
 		}
 		if ((step_ptr->step_node_bitmap == NULL) &&
+		    (step_ptr->step_id == INFINITE) &&
 		    (step_ptr->batch_step == 0)) {
 			error("Missing node_list for step_id %u.%u",
 			      job_ptr->job_id, step_ptr->step_id);
