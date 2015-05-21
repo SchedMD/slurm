SchedMD/slurm

commit 6cb722dcdff6d7bff09dad2e7f31cc744d3ff74f

Fix job container start race condition
If the prolog container creation call (PrologFlags=contain) was
in progress when a job termination RPC arrived at the slurmd,
the slurmstepd and sleep processes could be orphaned until the
slurmd restarted.

src/slurmd/slurmd/req.c
@@ -207,8 +207,8 @@ static uid_t _get_job_uid(uint32_t jobid);
 
 static gids_t *_gids_cache_lookup(char *user, gid_t gid);
 
-static int  _add_starting_step(slurmd_step_type_t type, void *req);
-static int  _remove_starting_step(slurmd_step_type_t type, void *req);
+static int  _add_starting_step(uint16_t type, void *req);
+static int  _remove_starting_step(uint16_t type, void *req);
 static int  _compare_starting_steps(void *s0, void *s1);
 static int  _wait_for_starting_step(uint32_t job_id, uint32_t step_id);
 static bool _step_is_starting(uint32_t job_id, uint32_t step_id);
@@ -476,7 +476,7 @@ static int _send_slurmd_conf_lite (int fd, slurmd_conf_t *cf)
 }
 
 static int
-_send_slurmstepd_init(int fd, slurmd_step_type_t type, void *req,
+_send_slurmstepd_init(int fd, uint16_t type, void *req,
 		      slurm_addr_t *cli, slurm_addr_t *self,
 		      hostset_t step_hset, uint16_t protocol_version)
 {
@@ -726,7 +726,7 @@ rwfail:
  * will be init, not slurmd.
  */
 static int
-_forkexec_slurmstepd(slurmd_step_type_t type, void *req,
+_forkexec_slurmstepd(uint16_t type, void *req,
 		     slurm_addr_t *cli, slurm_addr_t *self,
 		     const hostset_t step_hset, uint16_t protocol_version)
 {
@@ -5467,7 +5467,7 @@ init_gids_cache(int cache)
 
 
 static int
-_add_starting_step(slurmd_step_type_t type, void *req)
+_add_starting_step(uint16_t type, void *req)
 {
 	starting_step_t *starting_step;
 	int rc = SLURM_SUCCESS;
@@ -5477,7 +5477,7 @@ _add_starting_step(slurmd_step_type_t type, void *req)
 	slurm_mutex_lock(&conf->starting_steps_lock);
 	starting_step = xmalloc(sizeof(starting_step_t));
 	if (!starting_step) {
-		error("_add_starting_step failed to allocate memory");
+		error("%s failed to allocate memory", __func__);
 		rc = SLURM_FAILURE;
 		goto fail;
 	}
@@ -5494,14 +5494,18 @@ _add_starting_step(slurmd_step_type_t type, void *req)
 		starting_step->step_id =
 			((launch_tasks_request_msg_t *)req)->job_step_id;
 		break;
+	case REQUEST_LAUNCH_PROLOG:
+		starting_step->job_id  = ((prolog_launch_msg_t *)req)->job_id;
+		starting_step->step_id = SLURM_EXTERN_CONT;
+		break;
 	default:
-		error("_add_starting_step called with an invalid type");
+		error("%s called with an invalid type: %u", __func__, type);
 		rc = SLURM_FAILURE;
 		xfree(starting_step);
 		goto fail;
 	}
 	if (!list_append(conf->starting_steps, starting_step)) {
-		error("_add_starting_step failed to allocate memory for list");
+		error("%s failed to allocate memory for list", __func__);
 		rc = SLURM_FAILURE;
 		xfree(starting_step);
 		goto fail;
@@ -5514,7 +5518,7 @@ fail:
 
 
 static int
-_remove_starting_step(slurmd_step_type_t type, void *req)
+_remove_starting_step(uint16_t type, void *req)
 {
 	uint32_t job_id, step_id;
 	ListIterator iter;
@@ -5533,8 +5537,12 @@ _remove_starting_step(slurmd_step_type_t type, void *req)
 		job_id =  ((launch_tasks_request_msg_t *)req)->job_id;
 		step_id = ((launch_tasks_request_msg_t *)req)->job_step_id;
 		break;
+	case REQUEST_LAUNCH_PROLOG:
+		job_id  = ((prolog_launch_msg_t *)req)->job_id;
+		step_id = SLURM_EXTERN_CONT;
+		break;
 	default:
-		error("_remove_starting_step called with an invalid type");
+		error("%s called with an invalid type: %u", __func__, type);
 		rc = SLURM_FAILURE;
 		goto fail;
 	}
@@ -5552,7 +5560,7 @@ _remove_starting_step(slurmd_step_type_t type, void *req)
 		}
 	}
 	if (!found) {
-		error("_remove_starting_step: step not found");
+		error("%s: step %u.%u not found", __func__, job_id, step_id);
 		rc = SLURM_FAILURE;
 	}
 fail:

commit 847bd326e6c6dd36cf2e16f1381dc08a39c2dc27

Prolog startup bug fix
This bug was introduced yesterday in commit
6cb722dcdff6d7bff09dad2e7f31cc744d3ff74f

src/slurmd/slurmd/req.c
@@ -476,7 +476,7 @@ static int _send_slurmd_conf_lite (int fd, slurmd_conf_t *cf)
 }
 
 static int
-_send_slurmstepd_init(int fd, uint16_t type, void *req,
+_send_slurmstepd_init(int fd, int type, void *req,
 		      slurm_addr_t *cli, slurm_addr_t *self,
 		      hostset_t step_hset, uint16_t protocol_version)
 {
@@ -5481,7 +5481,7 @@ _add_starting_step(uint16_t type, void *req)
 		rc = SLURM_FAILURE;
 		goto fail;
 	}
-	switch(type) {
+	switch (type) {
 	case LAUNCH_BATCH_JOB:
 		starting_step->job_id =
 			((batch_job_launch_msg_t *)req)->job_id;
@@ -5504,6 +5504,7 @@ _add_starting_step(uint16_t type, void *req)
 		xfree(starting_step);
 		goto fail;
 	}
+
 	if (!list_append(conf->starting_steps, starting_step)) {
 		error("%s failed to allocate memory for list", __func__);
 		rc = SLURM_FAILURE;
@@ -5536,10 +5537,6 @@ _remove_starting_step(uint16_t type, void *req)
 	case LAUNCH_TASKS:
 		job_id =  ((launch_tasks_request_msg_t *)req)->job_id;
 		step_id = ((launch_tasks_request_msg_t *)req)->job_step_id;
-		break;
-	case REQUEST_LAUNCH_PROLOG:
-		job_id  = ((prolog_launch_msg_t *)req)->job_id;
-		step_id = SLURM_EXTERN_CONT;
 		break;
 	default:
 		error("%s called with an invalid type: %u", __func__, type);

src/slurmd/slurmstepd/slurmstepd.c
@@ -466,7 +466,7 @@ _init_from_slurmd(int sock, char **argv,
 	slurm_msg_t_init(msg);
 	msg->protocol_version = (uint16_t)proto;
 
-	switch(step_type) {
+	switch (step_type) {
 	case LAUNCH_BATCH_JOB:
 		msg->msg_type = REQUEST_BATCH_JOB_LAUNCH;
 		break;
@@ -474,7 +474,7 @@ _init_from_slurmd(int sock, char **argv,
 		msg->msg_type = REQUEST_LAUNCH_TASKS;
 		break;
 	default:
-		fatal("Unrecognized launch RPC");
+		fatal("%s: Unrecognized launch RPC (%d)", __func__, step_type);
 		break;
 	}
 	if (unpack_msg(msg, buffer) == SLURM_ERROR)