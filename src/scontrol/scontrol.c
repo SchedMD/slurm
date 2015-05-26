SchedMD/slurm

commit 935b0cbea55d38573d9cd5d75b20dfadf7cba93d

Add the ability to view and update layout info

src/scontrol/scontrol.c
@@ -1466,20 +1466,25 @@ _show_it (int argc, char *argv[])
 {
 	char *tag = NULL, *val = NULL;
 	int tag_len = 0;
+	bool allow_opt=false;
 
-	if (argc > 3) {
+	if (argc < 2) {
+		exit_code = 1;
+		if (quiet_flag != 1)
+			fprintf(stderr,
+				"too few arguments for keyword:%s\n", argv[0]);
+		return;
+	}
+
+	if (strncasecmp (argv[1], "layouts", MAX(tag_len, 2)) != 0)
+		allow_opt=true;
+
+	if (argc > 3 && allow_opt) {
 		exit_code = 1;
 		if (quiet_flag != 1)
 			fprintf(stderr,
 				"too many arguments for keyword:%s\n",
 				argv[0]);
-		return;
-	}
-	else if (argc < 2) {
-		exit_code = 1;
-		if (quiet_flag != 1)
-			fprintf(stderr,
-				"too few arguments for keyword:%s\n", argv[0]);
 		return;
 	}
 
@@ -1489,7 +1494,7 @@ _show_it (int argc, char *argv[])
 	if (val) {
 		tag_len = val - argv[1];
 		val++;
-	} else if (argc == 3) {
+	} else if (argc == 3 && !allow_opt) {
 		val = argv[2];
 	} else {
 		val = NULL;
@@ -1543,6 +1548,8 @@ _show_it (int argc, char *argv[])
 		scontrol_print_job (val);
 	} else if (strncasecmp(tag, "licenses", MAX(tag_len, 2)) == 0) {
 		scontrol_print_licenses(val);
+	} else if (strncasecmp (tag, "layouts", MAX(tag_len, 2)) == 0) {
+		scontrol_print_layout(argc-1, &argv[1]);
 	} else if (strncasecmp (tag, "nodes", MAX(tag_len, 1)) == 0) {
 		scontrol_print_node_list (val);
 	} else if (strncasecmp (tag, "partitions", MAX(tag_len, 1)) == 0 ||
@@ -1584,6 +1591,7 @@ _update_it (int argc, char *argv[])
 	int node_tag = 0, part_tag = 0, job_tag = 0;
 	int block_tag = 0, sub_tag = 0, res_tag = 0;
 	int debug_tag = 0, step_tag = 0, front_end_tag = 0;
+	int layout_tag=0;
 	int jerror_code = SLURM_SUCCESS;
 
 	/* First identify the entity to update */
@@ -1623,9 +1631,10 @@ _update_it (int argc, char *argv[])
 		} else if (!strncasecmp(tag, "SlurmctldDebug",
 					MAX(tag_len, 2))) {
 			debug_tag = 1;
+		} else if (!strncasecmp(tag, "Layouts",	MAX(tag_len, 5))) {
+			layout_tag = 1;
 		}
 	}
-
 	/* The order of tests matters here.  An update job request can include
 	 * partition and reservation tags, possibly before the jobid tag, but
 	 * none of the other updates have a jobid tag, so check jobtag first.
@@ -1651,6 +1660,8 @@ _update_it (int argc, char *argv[])
 		error_code = _update_bluegene_submp (argc, argv);
 	else if (debug_tag)
 		error_code = _update_slurmctld_debug(val);
+	else if (layout_tag)
+		error_code = scontrol_update_layout(argc, argv);
 	else {
 		exit_code = 1;
 		fprintf(stderr, "No valid entity in update command\n");
@@ -1660,7 +1671,7 @@ _update_it (int argc, char *argv[])
 				"(i.e. bgl000[0-3]),");
 		}
 		fprintf(stderr, "\"PartitionName\", \"Reservation\", "
-			"\"JobId\", or \"SlurmctldDebug\" \n");
+			"\"JobId\", \"SlurmctldDebug\" or \"Layouts\"\n");
 	}
 
 	if (error_code) {

doc/man/man1/scontrol.1
@@ -1472,6 +1472,42 @@ NOTE: Even on BGQ where node names are given in bg0000[00000] format
 this option takes an ionode name bg0000[0].
 
 .TP
+\fBSPECIFICATIONS FOR UPDATE COMMAND, LAYOUTS \fR
+.TP
+\fILayout\fP=<name>
+Identify the layout to be updated. This specification is required.
+.TP
+\fIEntity\fP=<entity list>
+Identify the entities to be updated. This specification is required.
+.TP
+\fIKey\fP=<value>
+Keys/Values to update for the entities. The format must respect the layout.d
+configuration files. Key=Type cannot be updated. One Key/Value is required,
+several can be set.
+
+.TP
+\fBSPECIFICATIONS FOR SHOW COMMAND, LAYOUTS\fR
+.PP
+Without options, lists all configured layouts. With a layout specified,
+shows entities with following options:
+
+.TP
+\fIKey\fP=<value>
+Keys/Values to update for the entities. The format must respect the layout.d
+configuration files. Key=Type cannot be updated. One Key/Value is required,
+several can be set.
+.TP
+\fIEntity\fP=<value>
+Entities to show, default is not used. Can be set to "*".
+.TP
+\fIType\fP=<value>
+Type of entities to show, default is not used.
+.TP
+\fInolayout\fP
+If not used, only entities with defining the tree are shown.
+With the option, only leaves are shown.
+
+.TP
 \fBDESCRIPTION FOR SHOW COMMAND, NODES\fR
 .TP
 The meaning of the energy information is as follows:

slurm/slurm.h.in
@@ -1852,6 +1852,15 @@ typedef struct job_alloc_info_msg {
 	uint32_t job_id;	/* job ID */
 } job_alloc_info_msg_t;
 
+typedef struct layout_info_msg {
+        uint32_t record_count;  /* number of char* */
+	char** records;		/*layout or list of layouts*/
+} layout_info_msg_t;
+
+typedef struct update_layout_msg {
+	char* layout;
+	char* arg;
+} update_layout_msg_t;
 
 typedef struct step_alloc_info_msg {
 	uint32_t job_id;	/* job ID */
@@ -3986,6 +3995,17 @@ extern int slurm_update_partition PARAMS((update_part_msg_t * part_msg));
  * RET 0 on success, otherwise return -1 and set errno to indicate the error
  */
 extern int slurm_delete_partition PARAMS((delete_part_msg_t * part_msg));
+
+/*****************************************************************************\
+ *      SLURM LAYOUT PRINT/UPDATE FUNCTIONS
+\*****************************************************************************/
+extern void slurm_print_layout_info PARAMS(
+	(FILE* out, layout_info_msg_t *layout_info_ptr, int one_liner ));
+extern int slurm_load_layout PARAMS(
+	(char *layout_type, char *entities, char *type,
+	 uint32_t norelation, layout_info_msg_t **resp));
+extern int slurm_update_layout PARAMS((update_layout_msg_t * layout_info_msg));
+
 
 /*****************************************************************************\
  *	SLURM RESERVATION CONFIGURATION READ/PRINT/UPDATE FUNCTIONS

src/api/Makefile.am
@@ -94,6 +94,7 @@ slurmapi_src =           \
 	job_step_info.c  \
 	license_info.c   \
 	node_info.c      \
+	layout_info.c     \
 	partition_info.c \
 	pmi_server.c     \
 	pmi_server.h     \

src/api/Makefile.in
@@ -189,10 +189,11 @@ am__objects_1 = allocate.lo allocate_msg.lo block_info.lo \
 	burst_buffer_info.lo cache_info.lo cancel.lo checkpoint.lo \
 	complete.lo config_info.lo front_end_info.lo init_msg.lo \
 	job_info.lo job_step_info.lo license_info.lo node_info.lo \
-	partition_info.lo pmi_server.lo reservation_info.lo signal.lo \
-	slurm_get_statistics.lo slurm_hostlist.lo slurm_pmi.lo \
-	step_ctx.lo step_io.lo step_launch.lo submit.lo suspend.lo \
-	topo_info.lo triggers.lo reconfigure.lo update_config.lo
+	layout_info.lo partition_info.lo pmi_server.lo \
+	reservation_info.lo signal.lo slurm_get_statistics.lo \
+	slurm_hostlist.lo slurm_pmi.lo step_ctx.lo step_io.lo \
+	step_launch.lo submit.lo suspend.lo topo_info.lo triggers.lo \
+	reconfigure.lo update_config.lo
 am_libslurmhelper_la_OBJECTS = $(am__objects_1)
 libslurmhelper_la_OBJECTS = $(am_libslurmhelper_la_OBJECTS)
 PROGRAMS = $(noinst_PROGRAMS)
@@ -599,6 +600,7 @@ slurmapi_src = \
 	job_step_info.c  \
 	license_info.c   \
 	node_info.c      \
+	layout_info.c     \
 	partition_info.c \
 	pmi_server.c     \
 	pmi_server.h     \
@@ -788,6 +790,7 @@ distclean-compile:
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/init_msg.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/job_info.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/job_step_info.Plo@am__quote@
+@AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/layout_info.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/license_info.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/node_info.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/partition_info.Plo@am__quote@

src/api/layout_info.c
@@ -1,0 +1,123 @@
+/*****************************************************************************\
+ *  layout_info.c - get/print the layout information of slurm
+ *****************************************************************************
+ *  Copyright (C) 2015
+ *  Written by Bull - Thomas Cadeau
+ *
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://slurm.schedmd.com/>.
+ *  Please also read the included file: DISCLAIMER.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and
+ *  distribute linked combinations including the two. You must obey the GNU
+ *  General Public License in all respects for all of the code used other than
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this
+ *  exception to your version of the file(s), but you are not obligated to do
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in
+ *  the program, then also delete it here.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#ifdef HAVE_SYS_SYSLOG_H
+#  include <sys/syslog.h>
+#endif
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "slurm/slurm.h"
+
+#include "src/common/slurm_protocol_api.h"
+#include "src/common/xmalloc.h"
+#include "src/common/xstring.h"
+
+/*
+ * slurm_load_layout - issue RPC to get slurm specific layout information
+ *	if changed since update_time
+ * OUT resp - place to store a node configuration pointer
+ * IN l_type - type of layout
+ * RET 0 or a slurm error code
+ * NOTE: free the response using slurm_free_layout_info_msg
+ */
+/*extern int slurm_load_layout (node_info_msg_t **resp, char* l_type)
+{
+	return SLURM_PROTOCOL_SUCCESS;
+}*/
+
+extern int slurm_load_layout (char *layout_type, char *entities, char *type,
+			      uint32_t norelation, layout_info_msg_t **resp)
+{
+        int rc;
+        slurm_msg_t req_msg;
+        slurm_msg_t resp_msg;
+        layout_info_request_msg_t req;
+
+	slurm_msg_t_init(&req_msg);
+	slurm_msg_t_init(&resp_msg);
+
+	req.layout_type  = layout_type;
+	req.entities     = entities;
+	req.type         = type;
+	req.norelation   = norelation;
+        req_msg.msg_type = REQUEST_LAYOUT_INFO;
+        req_msg.data     = &req;
+
+	if (slurm_send_recv_controller_msg(&req_msg, &resp_msg) < 0)
+		return SLURM_ERROR;
+
+	switch (resp_msg.msg_type) {
+	case RESPONSE_LAYOUT_INFO:
+		*resp = (layout_info_msg_t *) resp_msg.data;
+		break;
+	case RESPONSE_SLURM_RC:
+		rc = ((return_code_msg_t *) resp_msg.data)->return_code;
+		slurm_free_return_code_msg(resp_msg.data);
+		if (rc)
+			slurm_seterrno_ret(rc);
+		*resp = NULL;
+		break;
+	default:
+		slurm_seterrno_ret(SLURM_UNEXPECTED_MSG_ERROR);
+		break;
+	}
+
+	return SLURM_PROTOCOL_SUCCESS;
+}
+
+void slurm_print_layout_info ( FILE* out, layout_info_msg_t *layout_info_ptr,
+			       int one_liner )
+{
+	int i;
+	for (i=0; i<layout_info_ptr->record_count; i++) {
+		//FIXME change "\n" if one_liner
+		fprintf ( out, "%s", layout_info_ptr->records[i]);
+	}
+	if ( !one_liner)
+		fprintf(out, "\n");
+}

src/api/update_config.c
@@ -129,6 +129,17 @@ slurm_update_node ( update_node_msg_t * node_msg)
 {
 	return _slurm_update ((void *) node_msg, REQUEST_UPDATE_NODE);
 }
+/*
+ * slurm_update_layout - issue RPC to a layout's configuration per request,
+ *	only usable by user root
+ * IN layout_msg - command line (same format as conf)
+ * RET 0 on success, otherwise return -1 and set errno to indicate the error
+ */
+int
+slurm_update_layout ( update_layout_msg_t * layout_msg)
+{
+	return _slurm_update ((void *) layout_msg, REQUEST_UPDATE_LAYOUT);
+}
 
 /*
  * slurm_create_partition - create a new partition, only usable by user root

src/common/layouts_mgr.c
@@ -1580,9 +1580,13 @@ static int _layouts_build_relations(layout_plugin_t* plugin)
  * functions used when packing layouts into a buffer as a set of strings.
  */
 typedef struct _pack_args {
-	Buf       buffer;
-	char      *current_line;
-	layout_t* layout;
+	Buf        buffer;
+	char       *current_line;
+	layout_t   *layout;
+	hostlist_t list_entities;
+	char       *type;
+	uint32_t   all;
+	uint32_t   norelation;
 } _pack_args_t;
 
 /*
@@ -1738,10 +1742,14 @@ static uint8_t _pack_layout_tree(xtree_node_t* node, uint8_t which,
 	}
 
 	/* print this entity as root if necessary */
-	if (level == 0) {
-		str = xstrdup_printf("Root=%s\n", e_name);
-		packstr(str, buffer);
-		xfree(str);
+	if (level == 0 && pargs->norelation != 1 && pargs->type == NULL) {
+		if( pargs->all != 0 ||
+		    pargs->list_entities == NULL ||
+		    hostlist_find(pargs->list_entities, e_name) != -1) {
+			str = xstrdup_printf("Root=%s\n", e_name);
+			packstr(str, buffer);
+			xfree(str);
+		}
 	}
 
 	/* print entity name and type when possible */
@@ -1762,6 +1770,38 @@ static uint8_t _pack_layout_tree(xtree_node_t* node, uint8_t which,
 	str = pargs->current_line;
 	pargs->current_line = NULL;
 
+	/*don't print enclosed if norelation option*/
+	if( pargs->norelation == 1
+	    && enclosed_str != NULL
+	    && pargs->list_entities == NULL) {
+		xfree(enclosed_str);
+		xfree(str);
+		return 1;
+	}
+
+	/*don't print non enclosed if no "entities char*" option*/
+	if( pargs->all == 0
+	    && pargs->list_entities == NULL
+	    && enclosed_str == NULL ) {
+		xfree(str);
+		return 1;
+	}
+
+	/*don't print entities if not named in "entities char*"*/
+	if( pargs->all == 0
+	    && pargs->list_entities != NULL
+	    && hostlist_find(pargs->list_entities, e_name) == -1) {
+		xfree(str);
+		return 1;
+	}
+
+	/*don't print entities if not type of "type char*"*/
+	if( pargs->type != NULL
+	    && (e_type == NULL || strcasecmp(e_type,pargs->type)!=0)) {
+		xfree(str);
+		return 1;
+	}
+
 	/* print enclosed entities if any */
 	if (!enclosed_str) {
 		xstrcat(str, "\n");
@@ -1771,9 +1811,10 @@ static uint8_t _pack_layout_tree(xtree_node_t* node, uint8_t which,
 		xfree(str);
 		str = strdump;
 	}
-	packstr(str, buffer);
-	xfree(str);
 
+	packstr(str, buffer);
+
+	xfree(str);
 	return 1;
 }
 
@@ -2481,7 +2522,9 @@ entity_t* layouts_get_entity(const char* name)
 	return e;
 }
 
-int layouts_pack_layout(char *l_type, Buf buffer)
+
+int layouts_pack_layout(char *l_type, char *char_entities, char *type,
+			uint32_t norelation, Buf buffer)
 {
 	_pack_args_t pargs;
 	layout_t* layout;
@@ -2494,16 +2537,30 @@ int layouts_pack_layout(char *l_type, Buf buffer)
 		error("unable to get layout of type '%s'", l_type);
 		return SLURM_ERROR;
 	}
-
 	/* initialize args for recursive packing */
 	pargs.buffer = buffer;
 	pargs.layout = layout;
 	pargs.current_line = NULL;
+	pargs.all = 0;
+	pargs.list_entities = NULL;
+	if (char_entities != NULL) {
+		if (strcmp(char_entities, "*") == 0)
+			pargs.all = 1;
+		else
+			pargs.list_entities = hostlist_create(char_entities);
+	}
+	pargs.type = type;
+	pargs.norelation = norelation;
 
-	/* start by packing the layout priority */
-	str = xstrdup_printf("Priority=%u\n", layout->priority);
-	packstr(str, buffer);
-	xfree(str);
+
+	if ( pargs.norelation == 0
+	     && pargs.list_entities == NULL
+	     && pargs.type == NULL ) {
+		/* start by packing the layout priority */
+		str = xstrdup_printf("Priority=%u\n", layout->priority);
+		packstr(str, buffer);
+		xfree(str);
+	}
 
 	/* pack according to the layout struct type */
 	switch(layout->struct_type) {
@@ -2512,6 +2569,8 @@ int layouts_pack_layout(char *l_type, Buf buffer)
 			   _pack_layout_tree, &pargs);
 		break;
 	}
+	if (pargs.list_entities != NULL)
+		slurm_hostlist_destroy(pargs.list_entities);
 
 	/* EOF */
 	packstr("", buffer);
@@ -2571,7 +2630,8 @@ int layouts_state_save_layout(char* l_type)
 	START_TIMER;
 
 	/* pack the targeted layout into a tmp buffer */
-	error_code = layouts_pack_layout(l_type, buffer);
+	error_code = layouts_pack_layout(l_type, NULL, NULL, 0, buffer);
+
 	if (error_code != SLURM_SUCCESS) {
 		error("unable to save layout[%s] state", l_type);
 		return error_code;

commit 3cfb6f3feead3ad7fda4b6a0d447bfdc1a195efb

Layout bug in scontrol
Fix for bug introduced in commit 935b0cbea55d38573d9cd5d75b20dfadf7cba93d

src/scontrol/scontrol.c
@@ -1494,7 +1494,7 @@ _show_it (int argc, char *argv[])
 	if (val) {
 		tag_len = val - argv[1];
 		val++;
-	} else if (argc == 3 && !allow_opt) {
+	} else if (argc == 3) {
 		val = argv[2];
 	} else {
 		val = NULL;