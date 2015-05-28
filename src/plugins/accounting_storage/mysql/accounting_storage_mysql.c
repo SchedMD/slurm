f0b7ee381fa752d2790686dcd85fad9adffd3f6e
Danny Auble <da@schedmd.com>
2015-05-28 14:22:24 -0700

    Add primary keys for mariadb-galera which apparently needs all tables to have one.

--- src/plugins/accounting_storage/mysql/accounting_storage_mysql.c
+++ src/plugins/accounting_storage/mysql/accounting_storage_mysql.c
@@ -1280,7 +1280,8 @@ extern int create_cluster_tables(mysql_conn_t *mysql_conn, char *cluster_name)
 		 cluster_name, last_ran_table);
 	if (mysql_db_create_table(mysql_conn, table_name,
 				  last_ran_table_fields,
-				  ")")
+				  ", primary key (hourly_rollup, "
+				  "daily_rollup, monthly_rollup))")
 	    == SLURM_ERROR)
 		return SLURM_ERROR;
 
@@ -1304,7 +1305,8 @@ extern int create_cluster_tables(mysql_conn_t *mysql_conn, char *cluster_name)
 		 cluster_name, suspend_table);
 	if (mysql_db_create_table(mysql_conn, table_name,
 				  suspend_table_fields,
-				  ", key job_db_inx_times (job_db_inx, "
+				  ", primary key (job_db_inx, time_start), "
+				  "key job_db_inx_times (job_db_inx, "
 				  "time_start, time_end))") == SLURM_ERROR)
 		return SLURM_ERROR;
 
