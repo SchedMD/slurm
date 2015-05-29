SchedMD/slurm

commit a6d3074d24027f7771cb5b4dca68a032ad9b8acc

Cray - NPPCU support patch
    I am sending the latest update of my NPPCU-support patch for Slurm 2.5.0.  As before, this patch is applied over my basic BASIL 1.3 support patch.  The reason for this latest version is that it came to my attention, that certain jobs that should have been rejected by Slurm were allowed through.  I then further noticed that this would cause the backfill algorithm to slow down dramatically (often not being able to process any other jobs).
     The cause of the problem was that when I introduced the functionality into Slurm to properly set the "nppcu" (number of processors per compute unit) attribute in the XML reservation request to ALPS, I didn't also adjust the tests earlier in the code that eliminate nodes from consideration that do not have sufficient resources.  In other words, jobs that would exceed the absolute total number of processors on the node would be rejected as always (this is good).  Jobs that required the reduced number of "visible" processors on the node or less were allocated and worked fine (this is good).  Unfortunately, jobs that needed a number of processors somewhere in between these limits (let's call them the soft and hard limits) were allowed through by Slurm.  Making matters worse, when Slurm would subsequently try to request the ALPS reservation, ALPS would correctly reject it but Slurm would keep trying--this would then kill the backfilling.  In my opinion, these jobs should have been rejected from the onset by Slurm as they are asking for more processors per node than can be supplied.  If the user wants this number of processors they should specify the "--ntasks-per-core=..." (in our case "2" as that is the full number of hardware threads per core).  Obviously, this problem only appeared when I used CR_ONE_TASK_PER_CORE in the slurm.conf as I had modified the code to set nppcu to 1 when Slurm was configured with that option and the user didn't explicitly specify a different value.

src/plugins/select/linear/select_linear.c
@@ -503,12 +503,11 @@ static uint16_t _get_avail_cpus(struct job_record *job_ptr, int index)
 	debug2("host %s HW_ cpus %u boards %u sockets %u cores %u threads %u ",
 	       node_ptr->name, cpus, boards, sockets, cores, threads);
 #endif
-
 	avail_cpus = slurm_get_avail_procs(
 		min_sockets, min_cores, min_threads, cpus_per_task,
 		ntasks_per_node, ntasks_per_socket, ntasks_per_core,
 		&cpus, &sockets, &cores, &threads, NULL,
-		CR_CPU, job_ptr->job_id, node_ptr->name);
+		CR_CORE, job_ptr->job_id, node_ptr->name);
 
 #if SELECT_DEBUG
 	debug("avail_cpus index %d = %d (out of %d %d %d %d)",
@@ -532,6 +531,32 @@ static uint16_t _get_total_cpus(int index)
 		return node_ptr->cpus;
 }
 
+static uint16_t _get_total_threads(int index)
+{
+	struct node_record *node_ptr = &(select_node_ptr[index]);
+	if (select_fast_schedule)
+		return node_ptr->config_ptr->threads;
+	else
+		return node_ptr->threads;
+}
+
+/*
+ * _get_ntasks_per_core - Retrieve the value of ntasks_per_core from
+ *	the given job_details record.  If it wasn't set, return 0xffff.
+ *	Intended for use with the _adjust_cpus_nppcu function.
+ */
+
+static uint16_t _get_ntasks_per_core(struct job_details *details) {
+	uint16_t ntasks_per_core;
+
+	if ((details->mc_ptr))
+		ntasks_per_core = details->mc_ptr->ntasks_per_core;
+	else
+		ntasks_per_core = 0xffff;
+
+	return ntasks_per_core;
+}
+
 static job_resources_t *_create_job_resources(int node_cnt)
 {
 	job_resources_t *job_resrcs_ptr;
@@ -553,7 +578,7 @@ static void _build_select_struct(struct job_record *job_ptr, bitstr_t *bitmap)
 {
 	int i, j, k;
 	int first_bit, last_bit;
-	uint32_t node_cpus, total_cpus = 0, node_cnt;
+	uint32_t node_cpus, total_cpus = 0, node_cnt, node_threads;
 	struct node_record *node_ptr;
 	uint32_t job_memory_cpu = 0, job_memory_node = 0;
 	job_resources_t *job_resrcs_ptr;
@@ -586,10 +611,15 @@ static void _build_select_struct(struct job_record *job_ptr, bitstr_t *bitmap)
 		if (!bit_test(bitmap, i))
 			continue;
 		node_ptr = &(select_node_ptr[i]);
-		if (select_fast_schedule)
-			node_cpus = node_ptr->config_ptr->cpus;
-		else
-			node_cpus = node_ptr->cpus;
+		if (select_fast_schedule) {
+			node_cpus    = node_ptr->config_ptr->cpus;
+			node_threads = node_ptr->config_ptr->threads;
+		} else {
+			node_cpus    = node_ptr->cpus;
+			node_threads = node_ptr->threads;
+		}
+
+		node_cpus = _adjust_cpus_nppcu(_get_ntasks_per_core(job_ptr->details), node_threads, node_cpus);
 		job_resrcs_ptr->cpus[j] = node_cpus;
 		if ((k == -1) ||
 		    (job_resrcs_ptr->cpu_array_value[k] != node_cpus)) {
@@ -861,7 +891,7 @@ static int _job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 				rem_nodes--;
 				max_nodes--;
 				rem_cpus   -= avail_cpus;
-				total_cpus += _get_total_cpus(index);
+				total_cpus += _adjust_cpus_nppcu(_get_ntasks_per_core(job_ptr->details), _get_total_threads(index), _get_total_cpus(index));
 			} else {	 /* node not required (yet) */
 				bit_clear(bitmap, index);
 				consec_cpus[consec_index] += avail_cpus;
@@ -968,7 +998,7 @@ static int _job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 		if (best_fit_nodes == 0)
 			break;
 		if (job_ptr->details->contiguous &&
-		    ((best_fit_cpus < rem_cpus) ||
+		     ((best_fit_cpus < rem_cpus) ||
 		     (!_enough_nodes(best_fit_nodes, rem_nodes,
 				     min_nodes, req_nodes))))
 			break;	/* no hole large enough */
@@ -988,7 +1018,7 @@ static int _job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 				max_nodes--;
 				avail_cpus = _get_avail_cpus(job_ptr, i);
 				rem_cpus   -= avail_cpus;
-				total_cpus += _get_total_cpus(i);
+				total_cpus += _adjust_cpus_nppcu(_get_ntasks_per_core(job_ptr->details), _get_total_threads(i), _get_total_cpus(i));
 			}
 			for (i = (best_fit_req - 1);
 			     i >= consec_start[best_fit_location]; i--) {
@@ -1002,7 +1032,7 @@ static int _job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 				max_nodes--;
 				avail_cpus = _get_avail_cpus(job_ptr, i);
 				rem_cpus   -= avail_cpus;
-				total_cpus += _get_total_cpus(i);
+				total_cpus += _adjust_cpus_nppcu(_get_ntasks_per_core(job_ptr->details), _get_total_threads(i), _get_total_cpus(i));
 			}
 		} else {
 			for (i = consec_start[best_fit_location];
@@ -1017,7 +1047,7 @@ static int _job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 				max_nodes--;
 				avail_cpus = _get_avail_cpus(job_ptr, i);
 				rem_cpus   -= avail_cpus;
-				total_cpus += _get_total_cpus(i);
+				total_cpus += _adjust_cpus_nppcu(_get_ntasks_per_core(job_ptr->details), _get_total_threads(i), _get_total_cpus(i));
 			}
 		}
 		if (job_ptr->details->contiguous ||
@@ -1243,7 +1273,7 @@ static int _job_test_topo(struct job_record *job_ptr, bitstr_t *bitmap,
 				bit_set(bitmap, i);
 				alloc_nodes++;
 				rem_cpus -= avail_cpus;
-				total_cpus += _get_total_cpus(i);
+				total_cpus += _adjust_cpus_nppcu(_get_ntasks_per_core(job_ptr->details), _get_total_threads(i), _get_total_cpus(i));
 			}
 		}
 		/* Accumulate additional resources from leafs that
@@ -1278,7 +1308,7 @@ static int _job_test_topo(struct job_record *job_ptr, bitstr_t *bitmap,
 				bit_set(bitmap, i);
 				alloc_nodes++;
 				rem_cpus -= _get_avail_cpus(job_ptr, i);
-				total_cpus += _get_total_cpus(i);
+				total_cpus += _adjust_cpus_nppcu(_get_ntasks_per_core(job_ptr->details), _get_total_threads(i), _get_total_cpus(i));
 				if ((alloc_nodes > max_nodes) ||
 				    ((alloc_nodes >= want_nodes) &&
 				     (rem_cpus <= 0)))
@@ -1348,7 +1378,7 @@ static int _job_test_topo(struct job_record *job_ptr, bitstr_t *bitmap,
 			bit_set(bitmap, i);
 			alloc_nodes++;
 			rem_cpus -= _get_avail_cpus(job_ptr, i);
-			total_cpus += _get_total_cpus(i);
+			total_cpus += _adjust_cpus_nppcu(_get_ntasks_per_core(job_ptr->details), _get_total_threads(i), _get_total_cpus(i));
 			if ((alloc_nodes > max_nodes) ||
 			    ((alloc_nodes >= want_nodes) && (rem_cpus <= 0)))
 				break;

src/common/node_conf.c
@@ -1094,3 +1094,18 @@ extern uint32_t cr_get_coremap_offset(uint32_t node_index)
 	xassert(cr_node_cores_offset);
 	return cr_node_cores_offset[node_index];
 }
+
+/* The value of threads should be from config_ptr->threads if slurm.conf has fast schedule turned on (config_record);
+ * 		otherwise, it should be from node_ptr->threads (node_record).
+ */
+extern int _adjust_cpus_nppcu(uint16_t ntasks_per_core, uint16_t threads, int cpus ) {
+
+	if (ntasks_per_core == 0xffff)
+		ntasks_per_core = threads;
+
+	/* Adjust the number of CPUs according to the percentage of the hwthreads/core being used. */
+	cpus *= ntasks_per_core;
+	cpus /= threads;
+
+	return cpus;
+}

src/common/node_conf.h
@@ -281,4 +281,9 @@ extern void cr_fini_global_core_data(void);
 /*return the coremap index to the first core of the given node */
 extern uint32_t cr_get_coremap_offset(uint32_t node_index);
 
+/* Given the number of tasks per core and the actual number of hw threads, compute
+ * how many CPUs are "visible" and, hence, usable on the node.
+ */
+extern int _adjust_cpus_nppcu(uint16_t ntasks_per_core, uint16_t threads, int cpus);
+
 #endif /* !_HAVE_NODE_CONF_H */

src/common/slurm_resource_info.c
@@ -137,21 +137,9 @@ int slurm_get_avail_procs(const uint16_t socket_cnt,
 			  uint32_t job_id,
 			  char *name)
 {
-	uint16_t avail_cpus = 0, max_cpus = 0;
-	uint16_t allocated_cpus = 0, allocated_cores = 0, allocated_sockets = 0;
-	uint16_t max_avail_cpus = 0xffff;	/* for alloc_* accounting */
-	uint16_t min_sockets = 1, max_sockets = 0xffff;
-	uint16_t min_cores   = 1, max_cores   = 0xffff;
-	uint16_t                  max_threads = 0xffff;
-	int i;
+	uint16_t avail_cpus = 0;
 
         /* pick defaults for any unspecified items */
-	if (socket_cnt != (uint16_t) NO_VAL)
-		min_sockets = max_sockets = socket_cnt;
-	if (core_cnt != (uint16_t) NO_VAL)
-		min_cores = max_cores = core_cnt;	
-	if (thread_cnt != (uint16_t) NO_VAL)
-		max_threads = thread_cnt;
 	if (cpus_per_task <= 0)
 		cpus_per_task = 1;
 	if (*threads <= 0)
@@ -160,11 +148,7 @@ int slurm_get_avail_procs(const uint16_t socket_cnt,
 	    	*cores = 1;
 	if (*sockets <= 0)
 	    	*sockets = *cpus / *cores / *threads;
-	for (i = 0 ; alloc_cores && i < *sockets; i++) {
-		allocated_cores += alloc_cores[i];
-		if (alloc_cores[i])
-			allocated_sockets++;
-	}
+
 #if (DEBUG)
 	info("get_avail_procs %u %s User_ sockets %u cores %u threads %u",
 			job_id, name, socket_cnt, core_cnt, thread_cnt);
@@ -173,139 +157,31 @@ int slurm_get_avail_procs(const uint16_t socket_cnt,
 	info("get_avail_procs %u %s Ntask node   %u sockets %u core   %u",
 			job_id, name, ntaskspernode, ntaskspersocket,
 			ntaskspercore);
-	info("get_avail_procs %u %s cr_type %d cpus %u  alloc_ c %u s %u",
-			job_id, name, cr_type, *cpus, allocated_cores,
-			allocated_sockets);
+	info("get_avail_procs %u %s cr_type %d cpus %u",
+			job_id, name, cr_type, *cpus);
 	for (i = 0; alloc_cores && i < *sockets; i++)
 		info("get_avail_procs %u %s alloc_cores[%d] = %u",
 		     job_id, name, i, alloc_cores[i]);
 #endif
-	allocated_cpus = allocated_cores * (*threads);
+	uint32_t nppcu = ntaskspercore;
 
-	/* For the following CR types, nodes have no notion of socket, core,
-	   and thread.  Only one level of logical processors */
-	if (cr_type & CR_CORE) {
-		if (*cpus >= allocated_cpus)
-			*cpus -= allocated_cpus;
-		else {
-			*cpus = 0;
-			error("cons_res: *cpus underflow");
-		}
-		if (allocated_cores > 0) {
-			max_avail_cpus = 0;
-			int tmp_diff = 0;
-			for (i=0; i<*sockets; i++) {
-				tmp_diff = *cores - alloc_cores[i];
-				if (min_cores <= tmp_diff) {
-					tmp_diff *= (*threads);
-					max_avail_cpus += tmp_diff;
-				}
-			}
-		}
-
-		/*** honor socket/core/thread maximums ***/
-		*sockets = MIN(*sockets, max_sockets);
-		*cores   = MIN(*cores,   max_cores);
-		*threads = MIN(*threads, max_threads);
-
-		if (min_sockets > *sockets) {
-			*cpus = 0;
-		} else {
-			int max_cpus_socket = 0;
-			max_cpus = 0;
-			for (i=0; i<*sockets; i++) {
-				max_cpus_socket = 0;
-				if (min_cores <= *cores) {
-				        int num_threads = *threads;
-					if (ntaskspercore > 0) {
-						num_threads = MIN(num_threads,
-							       ntaskspercore);
-					}
-					max_cpus_socket = *cores * num_threads;
-				}
-				if (ntaskspersocket > 0) {
-					max_cpus_socket = MIN(max_cpus_socket,
-							      ntaskspersocket);
-				}
-				max_cpus += max_cpus_socket;
-			}
-			max_cpus = MIN(max_cpus, max_avail_cpus);
-		}
-
-		/*** honor any availability maximum ***/
-		max_cpus = MIN(max_cpus, max_avail_cpus);
-
-		if (ntaskspernode > 0) {
-			max_cpus = MIN(max_cpus, ntaskspernode);
-		}
-	} else if (cr_type & CR_SOCKET) {
-		if (*sockets >= allocated_sockets)
-			*sockets -= allocated_sockets; /* sockets count */
-		else {
-			*sockets = 0;
-			error("cons_res: *sockets underflow");
-		}
-		if (*cpus >= allocated_cpus)
-			*cpus -= allocated_cpus;
-		else {
-			*cpus = 0;
-			error("cons_res: *cpus underflow");
-		}
-
-		if (min_sockets > *sockets)
-			*cpus = 0;
-
-		/*** honor socket/core/thread maximums ***/
-		*sockets = MIN(*sockets, max_sockets);
-		*cores   = MIN(*cores,   max_cores);
-		*threads = MIN(*threads, max_threads);
-
-		/*** compute an overall maximum cpu count honoring ntasks* ***/
-		max_cpus  = *threads;
-		if (ntaskspercore > 0) {
-			max_cpus = MIN(max_cpus, ntaskspercore);
-		}
-		max_cpus *= *cores;
-		if (ntaskspersocket > 0) {
-			max_cpus = MIN(max_cpus, ntaskspersocket);
-		}
-		max_cpus *= *sockets;
-		if (ntaskspernode > 0) {
-			max_cpus = MIN(max_cpus, ntaskspernode);
-		}
-
-		/*** honor any availability maximum ***/
-		max_cpus = MIN(max_cpus, max_avail_cpus);
-	} else {	/* CR_CPU (default) */
-		if ((cr_type & CR_CPU) ||
-		    (!(cr_type & CR_MEMORY))) {
-			if (*cpus >= allocated_cpus)
-				*cpus -= allocated_cpus;
-			else {
-				*cpus = 0;
-				error("cons_res: *cpus underflow");
-			}
-		}
-
-		/*** compute an overall maximum cpu count honoring ntasks* ***/
-		max_cpus  = *cpus;
-		if (ntaskspernode > 0) {
-			max_cpus = MIN(max_cpus, ntaskspernode);
-		}
+	if (nppcu == 0xffff) { /* nppcu was not explicitly set, use all threads */
+		info( "***** nppcu was not explicitly set, use all threads *****");
+		nppcu = *threads;
 	}
 
-	/*** factor cpus_per_task into max_cpus ***/
-	max_cpus *= cpus_per_task;
-	/*** round down available based on cpus_per_task ***/
-	avail_cpus = (*cpus / cpus_per_task) * cpus_per_task;
-	avail_cpus = MIN(avail_cpus, max_cpus);
+	avail_cpus = *sockets * *cores * *threads;
+	avail_cpus = avail_cpus * nppcu / *threads;
+
+	if(ntaskspernode>0)
+		avail_cpus = MIN(avail_cpus, ntaskspernode * cpus_per_task);
 
 #if (DEBUG)
 	info("get_avail_procs %u %s return cpus %u sockets %u cores %u threads %u",
 			job_id, name, *cpus, *sockets, *cores, *threads);
 	info("get_avail_procs %d %s avail_cpus %u",  job_id, name, avail_cpus);
 #endif
-	return(avail_cpus);
+	return avail_cpus;
 }
 
 /*

src/plugins/select/cray/basil_alps.h
@@ -448,7 +448,8 @@ struct basil_rsvn_param {
 				depth,		/* depth > 0,         -d  */
 				nppn,		/* nppn > 0,          -N  */
 				npps,		/* PEs per segment,   -S  */
-				nspn;		/* segments per node, -sn */
+				nspn,		/* segments per node, -sn */
+				nppcu;		/* Processors Per Compute Unit. BASIL 1.3 */
 
 	char				*nodes;		/* NodeParamArray   */
 	struct basil_label		*labels;	/* LabelParamArray  */
@@ -622,7 +623,7 @@ extern void   free_inv(struct basil_inventory *inv);
 
 extern long basil_reserve(const char *user, const char *batch_id,
 			  uint32_t width, uint32_t depth, uint32_t nppn,
-			  uint32_t mem_mb, struct nodespec *ns_head,
+			  uint32_t mem_mb, uint32_t nppcu, struct nodespec *ns_head,
 			  struct basil_accel_param *accel_head);
 extern int basil_confirm(uint32_t rsvn_id, int job_id, uint64_t pagg_id);
 extern const struct basil_rsvn *basil_rsvn_by_id(const struct basil_inventory *inv,

src/plugins/select/cray/basil_interface.c
@@ -741,6 +741,8 @@ extern int do_basil_reserve(struct job_record *job_ptr)
 	long rc;
 	char *user, batch_id[16];
 	struct basil_accel_param* bap;
+	uint16_t nppcu = 0;
+	uint16_t hwthreads_per_core = 1;
 
 	if (!job_ptr->job_resrcs || job_ptr->job_resrcs->nhosts == 0)
 		return SLURM_SUCCESS;
@@ -771,6 +773,17 @@ extern int do_basil_reserve(struct job_record *job_ptr)
 			mppmem = job_ptr->details->pn_min_memory & ~MEM_PER_CPU;
 	} else if (job_ptr->details->pn_min_memory) {
 		node_min_mem = job_ptr->details->pn_min_memory;
+	}
+
+	if (slurmctld_conf.select_type_param & CR_ONE_TASK_PER_CORE) {
+		if ( job_ptr->details && job_ptr->details->mc_ptr && job_ptr->details->mc_ptr->ntasks_per_core == 0xffff ) {
+			nppcu = 1;
+			debug("No explicit ntasks-per-core has been set, using nppcu=1.");
+		}
+	}
+
+	if ( job_ptr->details && job_ptr->details->mc_ptr && job_ptr->details->mc_ptr->ntasks_per_core != 0xffff ) {
+		nppcu = job_ptr->details->mc_ptr->ntasks_per_core;
 	}
 
 	for (i = first_bit; i <= last_bit; i++) {
@@ -836,6 +849,12 @@ extern int do_basil_reserve(struct job_record *job_ptr)
 	for (i = 0; i < job_ptr->job_resrcs->nhosts; i++) {
 		uint32_t node_tasks = job_ptr->job_resrcs->cpus[i] / mppdepth;
 
+		if ( job_ptr->job_resrcs->sockets_per_node[i] > 0 && job_ptr->job_resrcs->cores_per_socket[i] > 0 )
+			hwthreads_per_core = job_ptr->job_resrcs->cpus[i] / job_ptr->job_resrcs->sockets_per_node[i] / job_ptr->job_resrcs->cores_per_socket[i];
+
+		if (nppcu)
+			node_tasks = node_tasks * nppcu / hwthreads_per_core;
+
 		if (mppnppn && mppnppn < node_tasks)
 			node_tasks = mppnppn;
 		mppwidth += node_tasks;
@@ -850,7 +869,7 @@ extern int do_basil_reserve(struct job_record *job_ptr)
 		bap = NULL;
 
 	rc   = basil_reserve(user, batch_id, mppwidth, mppdepth, mppnppn,
-			     mppmem, ns_head, bap);
+			     mppmem, (uint32_t)nppcu, ns_head, bap);
 	xfree(user);
 	if (rc <= 0) {
 		/* errno value will be resolved by select_g_job_begin() */

src/plugins/select/cray/libalps/basil_request.c
@@ -88,9 +88,9 @@ static void _rsvn_write_reserve_xml(FILE *fp, struct basil_reservation *r,
 		if (version >= BV_5_1)
 			_write_xml(fp, "  <ReserveParam architecture=\"%s\" "
 				   "width=\"%ld\" depth=\"%ld\" nppn=\"%ld\""
-				   " nppcu=\"0\"",
+				   " nppcu=\"%d\"",
 				   nam_arch[param->arch],
-				   param->width, param->depth, param->nppn);
+				   param->width, param->depth, param->nppn, param->nppcu);
 		else
 			_write_xml(fp, "  <ReserveParam architecture=\"%s\" "
 				   "width=\"%ld\" depth=\"%ld\" nppn=\"%ld\"",

src/plugins/select/cray/libalps/do_reserve.c
@@ -44,7 +44,7 @@ static int _rsvn_add_mem_param(struct basil_rsvn_param *rp, uint32_t mem_mb)
  */
 static int _rsvn_add_params(struct basil_reservation *resv,
 			    uint32_t width, uint32_t depth, uint32_t nppn,
-			    uint32_t mem_mb, char *mppnodes,
+			    uint32_t mem_mb, char *mppnodes, uint32_t nppcu,
 			    struct basil_accel_param *accel)
 {
 	struct basil_rsvn_param *rp = xmalloc(sizeof(*rp));
@@ -57,6 +57,7 @@ static int _rsvn_add_params(struct basil_reservation *resv,
 	rp->depth = depth;
 	rp->nppn  = nppn;
 	rp->nodes = mppnodes;
+	rp->nppcu = nppcu;
 	rp->accel = accel;
 
 	if (mem_mb && _rsvn_add_mem_param(rp, mem_mb) < 0) {
@@ -89,7 +90,7 @@ static struct basil_reservation *_rsvn_new(const char *user,
 					   const char *batch_id,
 					   uint32_t width, uint32_t depth,
 					   uint32_t nppn, uint32_t mem_mb,
-					   char *mppnodes,
+					   uint32_t nppcu, char *mppnodes,
 					   struct basil_accel_param *accel)
 {
 	struct basil_reservation *res;
@@ -105,7 +106,7 @@ static struct basil_reservation *_rsvn_new(const char *user,
 		strncpy(res->batch_id, batch_id, sizeof(res->batch_id));
 
 	if (_rsvn_add_params(res, width, depth, nppn,
-			     mem_mb, mppnodes, accel) < 0) {
+			     mem_mb, mppnodes, nppcu, accel) < 0) {
 		free_rsvn(res);
 		return NULL;
 	}
@@ -127,7 +128,7 @@ static struct basil_reservation *_rsvn_new(const char *user,
  */
 long basil_reserve(const char *user, const char *batch_id,
 		   uint32_t width, uint32_t depth, uint32_t nppn,
-		   uint32_t mem_mb, struct nodespec *ns_head,
+		   uint32_t mem_mb, uint32_t nppcu, struct nodespec *ns_head,
 		   struct basil_accel_param *accel_head)
 {
 	struct basil_reservation *rsvn;
@@ -138,7 +139,7 @@ long basil_reserve(const char *user, const char *batch_id,
 
 	free_nodespec(ns_head);
 	rsvn = _rsvn_new(user, batch_id, width, depth, nppn, mem_mb,
-			 mppnodes, accel_head);
+			 nppcu, mppnodes, accel_head);
 	if (rsvn == NULL)
 		return -BE_INTERNAL;
 

commit 58623ec75900640573ffcda398c5160d9d44f339

select/linear: Correct CPU count
Correct count of CPUs allocated to job on system with hyperthreads.
The bug was introduced in commit a6d3074d24027f7
On a system with hyperthreads:
srun -n1 --ntasks-per-core=1 hostname
you would get:
slurmctld: error: job_update_cpu_cnt: cpu_cnt underflow on job_id 67072

src/plugins/select/linear/select_linear.c
@@ -537,28 +537,6 @@ static uint16_t _get_total_cpus(int index)
 		return node_ptr->cpus;
 }
 
-static uint16_t _get_total_threads(int index)
-{
-	struct node_record *node_ptr = &(select_node_ptr[index]);
-	if (select_fast_schedule)
-		return node_ptr->config_ptr->threads;
-	else
-		return node_ptr->threads;
-}
-
-/*
- * _get_ntasks_per_core - Retrieve the value of ntasks_per_core from
- *	the given job_details record.  If it wasn't set, return 0xffff.
- *	Intended for use with the adjust_cpus_nppcu function.
- */
-
-static uint16_t _get_ntasks_per_core(struct job_details *details) {
-	if (details->mc_ptr)
-		return details->mc_ptr->ntasks_per_core;
-	else
-		return 0xffff;
-}
-
 static job_resources_t *_create_job_resources(int node_cnt)
 {
 	job_resources_t *job_resrcs_ptr;
@@ -580,8 +558,7 @@ static void _build_select_struct(struct job_record *job_ptr, bitstr_t *bitmap)
 {
 	int i, j, k;
 	int first_bit, last_bit;
-	uint32_t node_cpus, total_cpus = 0, node_cnt, node_threads;
-	struct node_record *node_ptr;
+	uint32_t node_cpus, total_cpus = 0, node_cnt;
 	uint32_t job_memory_cpu = 0, job_memory_node = 0;
 	job_resources_t *job_resrcs_ptr;
 
@@ -609,21 +586,10 @@ static void _build_select_struct(struct job_record *job_ptr, bitstr_t *bitmap)
 	last_bit  = bit_fls(bitmap);
 	if (last_bit == -1)
 		last_bit = -2;	/* no bits set */
-	for (i=first_bit, j=0, k=-1; i<=last_bit; i++) {
+	for (i = first_bit, j = 0, k = -1; i <= last_bit; i++) {
 		if (!bit_test(bitmap, i))
 			continue;
-		node_ptr = &(select_node_ptr[i]);
-		if (select_fast_schedule) {
-			node_cpus    = node_ptr->config_ptr->cpus;
-			node_threads = node_ptr->config_ptr->threads;
-		} else {
-			node_cpus    = node_ptr->cpus;
-			node_threads = node_ptr->threads;
-		}
-
-		node_cpus = adjust_cpus_nppcu(
-					_get_ntasks_per_core(job_ptr->details),
-					node_threads, node_cpus);
+		node_cpus = _get_total_cpus(i);
 		job_resrcs_ptr->cpus[j] = node_cpus;
 		if ((k == -1) ||
 		    (job_resrcs_ptr->cpu_array_value[k] != node_cpus)) {
@@ -895,10 +861,7 @@ static int _job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 				rem_nodes--;
 				max_nodes--;
 				rem_cpus   -= avail_cpus;
-				total_cpus += adjust_cpus_nppcu(
-						_get_ntasks_per_core(job_ptr->details),
-						_get_total_threads(index),
-						_get_total_cpus(index));
+				total_cpus += _get_total_cpus(index);
 			} else {	 /* node not required (yet) */
 				bit_clear(bitmap, index);
 				consec_cpus[consec_index] += avail_cpus;
@@ -1025,10 +988,7 @@ static int _job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 				max_nodes--;
 				avail_cpus = _get_avail_cpus(job_ptr, i);
 				rem_cpus   -= avail_cpus;
-				total_cpus += adjust_cpus_nppcu(
-						_get_ntasks_per_core(job_ptr->details),
-						_get_total_threads(i),
-						_get_total_cpus(i));
+				total_cpus += _get_total_cpus(i);
 			}
 			for (i = (best_fit_req - 1);
 			     i >= consec_start[best_fit_location]; i--) {
@@ -1042,10 +1002,7 @@ static int _job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 				max_nodes--;
 				avail_cpus = _get_avail_cpus(job_ptr, i);
 				rem_cpus   -= avail_cpus;
-				total_cpus += adjust_cpus_nppcu(
-						_get_ntasks_per_core(job_ptr->details),
-						_get_total_threads(i),
-						_get_total_cpus(i));
+				total_cpus += _get_total_cpus(i);
 			}
 		} else {
 			for (i = consec_start[best_fit_location];
@@ -1060,10 +1017,7 @@ static int _job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 				max_nodes--;
 				avail_cpus = _get_avail_cpus(job_ptr, i);
 				rem_cpus   -= avail_cpus;
-				total_cpus += adjust_cpus_nppcu(
-						_get_ntasks_per_core(job_ptr->details),
-						_get_total_threads(i),
-						_get_total_cpus(i));
+				total_cpus += _get_total_cpus(i);
 			}
 		}
 		if (job_ptr->details->contiguous ||
@@ -1289,10 +1243,7 @@ static int _job_test_topo(struct job_record *job_ptr, bitstr_t *bitmap,
 				bit_set(bitmap, i);
 				alloc_nodes++;
 				rem_cpus -= avail_cpus;
-				total_cpus += adjust_cpus_nppcu(
-						_get_ntasks_per_core(job_ptr->details),
-						_get_total_threads(i),
-						_get_total_cpus(i));
+				total_cpus += _get_total_cpus(i);
 			}
 		}
 		/* Accumulate additional resources from leafs that
@@ -1327,10 +1278,7 @@ static int _job_test_topo(struct job_record *job_ptr, bitstr_t *bitmap,
 				bit_set(bitmap, i);
 				alloc_nodes++;
 				rem_cpus -= _get_avail_cpus(job_ptr, i);
-				total_cpus += adjust_cpus_nppcu(
-						_get_ntasks_per_core(job_ptr->details),
-						_get_total_threads(i),
-						_get_total_cpus(i));
+				total_cpus += _get_total_cpus(i);
 				if ((alloc_nodes > max_nodes) ||
 				    ((alloc_nodes >= want_nodes) &&
 				     (rem_cpus <= 0)))
@@ -1400,10 +1348,7 @@ static int _job_test_topo(struct job_record *job_ptr, bitstr_t *bitmap,
 			bit_set(bitmap, i);
 			alloc_nodes++;
 			rem_cpus -= _get_avail_cpus(job_ptr, i);
-			total_cpus += adjust_cpus_nppcu(
-					_get_ntasks_per_core(job_ptr->details),
-					_get_total_threads(i),
-					_get_total_cpus(i));
+			total_cpus += _get_total_cpus(i);
 			if ((alloc_nodes > max_nodes) ||
 			    ((alloc_nodes >= want_nodes) && (rem_cpus <= 0)))
 				break;