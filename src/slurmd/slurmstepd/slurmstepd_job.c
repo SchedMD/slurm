SchedMD/slurm

commit ef66b2eb7e9d62f2f6e9d82a6f110b8c1af8a6f7

switch/cray: Refine PMI_CRAY_NO_SMP_ENV set
switch/cray: Refine logic to set PMI_CRAY_NO_SMP_ENV environment variable.
Rather than testing for the task distribution option, test the actual
task IDs to see fi they are monotonically increasing across all nodes.
Based upon idea from Brian Gilmer (Cray).

src/slurmd/slurmstepd/slurmstepd_job.c
@@ -197,7 +197,7 @@ static void
 _job_init_task_info(stepd_step_rec_t *job, uint32_t **gtid,
 		    char *ifname, char *ofname, char *efname)
 {
-	int          i, node_id = job->nodeid;
+	int          i, j, node_id = job->nodeid;
 	char        *in, *out, *err;
 
 	if (job->node_tasks == 0) {
@@ -206,10 +206,21 @@ _job_init_task_info(stepd_step_rec_t *job, uint32_t **gtid,
 		return;
 	}
 
+#if defined(HAVE_NATIVE_CRAY)
+	for (i = 0; i < job->nnodes; i++) {
+		for (j = 1; j < job->task_cnts[i]; j++) {
+			if (gtid[i][j] != gtid[i][j-1] + 1)) {
+				job->non_smp = 1;
+				break;
+			}
+		}
+	}
+#endif
+
 	job->task = (stepd_step_task_info_t **)
 		xmalloc(job->node_tasks * sizeof(stepd_step_task_info_t *));
 
-	for (i = 0; i < job->node_tasks; i++){
+	for (i = 0; i < job->node_tasks; i++) {
 		in = _expand_stdio_filename(ifname, gtid[node_id][i], job);
 		out = _expand_stdio_filename(ofname, gtid[node_id][i], job);
 		err = _expand_stdio_filename(efname, gtid[node_id][i], job);

src/plugins/switch/cray/util.c
@@ -86,7 +86,7 @@ int create_apid_dir(uint64_t apid, uid_t uid, gid_t gid)
  */
 int set_job_env(stepd_step_rec_t *job, slurm_cray_jobinfo_t *sw_job)
 {
-	int rc, i, non_smp;
+	int rc, i;
 	char *buff = NULL, *resv_ports = NULL, *tmp = NULL;
 
 	/*
@@ -141,42 +141,9 @@ int set_job_env(stepd_step_rec_t *job, slurm_cray_jobinfo_t *sw_job)
 
 	}
 
-	/*
-	 * Determine whether non-SMP ordering is used.
-	 * That is, if distribution is cyclic and there are more
-	 * tasks than nodes, or the distribution is arbitrary.
-	 */
-	switch (job->task_dist) {
-	case SLURM_DIST_BLOCK:
-	case SLURM_DIST_BLOCK_CYCLIC:
-	case SLURM_DIST_BLOCK_BLOCK:
-		non_smp = 0;
-		break;
-	case SLURM_DIST_CYCLIC:
-	case SLURM_DIST_CYCLIC_CYCLIC:
-	case SLURM_DIST_CYCLIC_BLOCK:
-		if (job->ntasks > job->nnodes) {
-			CRAY_INFO("Non-SMP ordering identified; distribution"
-				  " %s tasks %"PRIu32" nodes %"PRIu32,
-				  slurm_step_layout_type_name(job->task_dist),
-				  job->ntasks, job->nnodes);
-			non_smp = 1;
-		} else {
-		    non_smp = 0;
-		}
-		break;
-	case SLURM_DIST_ARBITRARY:
-	case SLURM_DIST_PLANE:
-	case SLURM_NO_LLLP_DIST:
-	case SLURM_DIST_UNKNOWN:
-	default:
-		CRAY_INFO("Non-SMP ordering identified; distribution %s",
-			  slurm_step_layout_type_name(job->task_dist));
-		non_smp = 1;
-		break;
-	}
+	/* Set if task IDs are not monotonically increasing across all nodes */
 	rc = env_array_overwrite_fmt(&job->env, PMI_CRAY_NO_SMP_ENV,
-				     "%d", non_smp);
+				     "%d", job->non_smp);
 	if (rc == 0) {
 		CRAY_ERR("Failed to set env var "PMI_CRAY_NO_SMP_ENV);
 		return SLURM_ERROR;

src/slurmd/slurmstepd/slurmstepd_job.h
@@ -232,6 +232,9 @@ typedef struct {
 					  * POINTER. */
 	mpmd_set_t     *mpmd_set;	/* MPMD specifications for Cray */
 	uint16_t	job_core_spec;	/* count of specialized cores */
+	int		non_smp;	/* Set if task IDs are not monotonically
+					 * increasing across all nodes, set only
+					 * native Cray systems */
 } stepd_step_rec_t;
 
 

commit 50e0d6e444226745cb552f120e8dfbf3b02529e2

Fix broken build caused by commit ef66b2eb

src/slurmd/slurmstepd/slurmstepd_job.c
@@ -197,7 +197,7 @@ static void
 _job_init_task_info(stepd_step_rec_t *job, uint32_t **gtid,
 		    char *ifname, char *ofname, char *efname)
 {
-	int          i, j, node_id = job->nodeid;
+	int          i, node_id = job->nodeid;
 	char        *in, *out, *err;
 
 	if (job->node_tasks == 0) {