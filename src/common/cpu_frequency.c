214c5372ae2ca11cb69d39a406abca898b4bd0e7
Brian Christiansen <brian@schedmd.com>
2015-06-11 14:37:35 -0700

    One more addition to 9d20cf02ee
    Prevent double free.

--- src/common/cpu_frequency.c
+++ src/common/cpu_frequency.c
@@ -184,7 +184,7 @@ cpu_freq_init(slurmd_conf_t *conf)
 	unsigned int i, j;
 
 	xfree(slurmd_spooldir);
-	slurmd_spooldir = conf->spooldir;
+	slurmd_spooldir = xstrdup(conf->spooldir);
 
 	if (run_in_daemon("slurmstepd"))
 		return;
