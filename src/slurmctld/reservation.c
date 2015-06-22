SchedMD/slurm

commit 5f258072133b2cc6fae8c576c351b48ed6e30ef4

Make _create_cluster_core_bitmap a global function and make it work for BLUEGENE systems as well.

src/slurmctld/reservation.c
@@ -3061,11 +3061,6 @@ static int  _resize_resv(slurmctld_resv_t *resv_ptr, uint32_t node_cnt)
 	return i;
 }
 
-static void _create_cluster_core_bitmap(bitstr_t **core_bitmap)
-{
-	*core_bitmap = bit_alloc(cr_get_coremap_offset(node_record_count));
-}
-
 /* Given a reservation create request, select appropriate nodes for use */
 static int  _select_nodes(resv_desc_msg_t *resv_desc_ptr,
 			  struct part_record **part_ptr,
@@ -3133,8 +3128,19 @@ static int  _select_nodes(resv_desc_msg_t *resv_desc_ptr,
 				bit_and(node_bitmap, resv_ptr->node_bitmap);
 				bit_not(resv_ptr->node_bitmap);
 			} else {
-				if (*core_bitmap == NULL)
-					_create_cluster_core_bitmap(core_bitmap);
+				int core_mult = 1;
+#ifdef HAVE_BG
+				if (!cnodes_per_mp)
+					select_g_alter_node_cnt(
+						SELECT_GET_NODE_SCALING,
+						&cnodes_per_mp);
+				core_mult = cnodes_per_mp;
+#endif
+
+				if (!*core_bitmap)
+					*core_bitmap =
+						cr_create_cluster_core_bitmap(
+							core_mult);
 				bit_or(*core_bitmap, resv_ptr->core_bitmap);
 			}
 		}
@@ -3318,7 +3324,7 @@ static void _check_job_compatibility(struct job_record *job_ptr,
 {
 	uint32_t total_nodes;
 	bitstr_t *full_node_bitmap;
-	int i_core, i_node;
+	int i_core, i_node, core_mult = 1;
 	int start = 0;
 	int rep_count = 0;
 	job_resources_t *job_res = job_ptr->job_resrcs;
@@ -3339,10 +3345,17 @@ static void _check_job_compatibility(struct job_record *job_ptr,
 }
 #endif
 
+#ifdef HAVE_BG
+	if (!cnodes_per_mp)
+		select_g_alter_node_cnt(SELECT_GET_NODE_SCALING,
+					&cnodes_per_mp);
+	core_mult = cnodes_per_mp;
+#endif
+
 	full_node_bitmap = bit_copy(job_res->node_bitmap);
 
-	if (*core_bitmap == NULL)
-		_create_cluster_core_bitmap(core_bitmap);
+	if (!*core_bitmap)
+		*core_bitmap = cr_create_cluster_core_bitmap(core_mult);
 
 	i_node = 0;
 	while (i_node < total_nodes) {

src/common/node_conf.c
@@ -1147,6 +1147,26 @@ extern uint32_t cr_get_coremap_offset(uint32_t node_index)
 	return cr_node_cores_offset[node_index];
 }
 
+/* Return a bitmap the size of the machine in cores. On a Bluegene
+ * system it will return a bitmap in cnodes. */
+extern bitstr_t *cr_create_cluster_core_bitmap(int core_mult)
+{
+	/* DEF_TIMERS; */
+	/* START_TIMER; */
+	bitstr_t *core_bitmap;
+	static int cnt = 0;
+
+	if (!cnt) {
+		cnt = cr_get_coremap_offset(node_record_count);
+		if (core_mult)
+			cnt *= core_mult;
+	}
+	core_bitmap = bit_alloc(cnt);
+	/* END_TIMER; */
+	/* info("creating of core bitmap of %d took %s", cnt, TIME_STR); */
+	return core_bitmap;
+}
+
 /* Given the number of tasks per core and the actual number of hw threads,
  * compute how many CPUs are "visible" and, hence, usable on the node.
  */

src/common/node_conf.h
@@ -312,6 +312,10 @@ extern void cr_fini_global_core_data(void);
 /*return the coremap index to the first core of the given node */
 extern uint32_t cr_get_coremap_offset(uint32_t node_index);
 
+/* Return a bitmap the size of the machine in cores. On a Bluegene
+ * system it will return a bitmap in cnodes. */
+extern bitstr_t *cr_create_cluster_core_bitmap(int core_mult);
+
 /* Given the number of tasks per core and the actual number of hw threads,
  * compute how many CPUs are "visible" and, hence, usable on the node.
  */

commit a6454176987cb48d156d411de292556dc55dc8ea

Advanced reservation fixes
Updates of existing bluegene advanced reservations did not work at all.
Some multi-core configurations resulting in an abort due to creating
  core_bitmaps for the reservation that only had one bit per node rather
  than one bit per core.
These bugs were introduced in commit 5f258072133b2cc6fae8c576c351b48ed6e30ef4

src/slurmctld/reservation.c
@@ -185,6 +185,22 @@ static void _advance_time(time_t *res_time, int day_cnt)
 	}
 }
 
+static void _create_cluster_core_bitmap(bitstr_t **core_bitmap)
+{
+	if (*core_bitmap)
+		return;
+
+#ifdef HAVE_BG
+	if (!cnodes_per_mp) {
+		select_g_alter_node_cnt(SELECT_GET_NODE_SCALING,
+					&cnodes_per_mp);
+	}
+	*core_bitmap = cr_create_cluster_core_bitmap(cnodes_per_mp);
+#else
+	*core_bitmap = bit_alloc(cr_get_coremap_offset(node_record_count));
+#endif
+}
+
 static List _list_dup(List license_list)
 {
 	ListIterator iter;
@@ -1923,9 +1939,7 @@ extern int create_resv(resv_desc_msg_t *resv_desc_ptr)
 				select_g_alter_node_cnt(SELECT_GET_NODE_SCALING,
 							&cnodes_per_mp);
 
-			if (!core_bitmap)
-				core_bitmap = cr_create_cluster_core_bitmap(
-					cnodes_per_mp);
+			_create_cluster_core_bitmap(&core_bitmap);
 			if (!resv_desc_ptr->core_cnt) {
 				resv_desc_ptr->core_cnt =
 					xmalloc(sizeof(uint32_t) * 2);
@@ -2148,9 +2162,14 @@ extern int update_resv(resv_desc_msg_t *resv_desc_ptr)
 		return ESLURM_RESERVATION_INVALID;
 
 	/* FIXME: Support more core based reservation updates */
+#ifndef HAVE_BG
 	if ((resv_ptr->full_nodes == 0) &&
-	    (resv_desc_ptr->node_cnt || resv_desc_ptr->node_list))
+	    (resv_desc_ptr->node_cnt || resv_desc_ptr->node_list)) {
+		info("Core-based reservation %s can not be updated",
+		     resv_desc_ptr->name);
 		return ESLURM_RESERVATION_NOT_USABLE;
+	}
+#endif
 
 	/* Make backup to restore state in case of failure */
 	resv_backup = _copy_resv(resv_ptr);
@@ -2206,7 +2225,7 @@ extern int update_resv(resv_desc_msg_t *resv_desc_ptr)
 			goto update_failure;
 		}
 	}
-	if (resv_desc_ptr->partition && (resv_desc_ptr->partition[0] == '\0')){
+	if (resv_desc_ptr->partition && (resv_desc_ptr->partition[0] == '\0')) {
 		/* Clear the partition */
 		xfree(resv_desc_ptr->partition);
 		xfree(resv_ptr->partition);
@@ -2331,6 +2350,8 @@ extern int update_resv(resv_desc_msg_t *resv_desc_ptr)
 	}
 
 	if (resv_ptr->start_time >= resv_ptr->end_time) {
+		info("Reservation %s request has invalid times (start > end)",
+		     resv_desc_ptr->name);
 		error_code = ESLURM_INVALID_TIME_VALUE;
 		goto update_failure;
 	}
@@ -2374,6 +2395,9 @@ extern int update_resv(resv_desc_msg_t *resv_desc_ptr)
 			resv_ptr->flags &= (~RESERVE_FLAG_PART_NODES);
 			if (node_name2bitmap(resv_desc_ptr->node_list,
 					    false, &node_bitmap)) {
+				info("Reservation %s request has invalid node name (%s)",
+				     resv_desc_ptr->name,
+				     resv_desc_ptr->node_list);
 				error_code = ESLURM_INVALID_NODE_NAME;
 				goto update_failure;
 			}
@@ -3335,19 +3359,7 @@ static int  _select_nodes(resv_desc_msg_t *resv_desc_ptr,
 				bit_and(node_bitmap, resv_ptr->node_bitmap);
 				bit_not(resv_ptr->node_bitmap);
 			} else {
-				int core_mult = 1;
-#ifdef HAVE_BG
-				if (!cnodes_per_mp)
-					select_g_alter_node_cnt(
-						SELECT_GET_NODE_SCALING,
-						&cnodes_per_mp);
-				core_mult = cnodes_per_mp;
-#endif
-
-				if (!*core_bitmap)
-					*core_bitmap =
-						cr_create_cluster_core_bitmap(
-							core_mult);
+				_create_cluster_core_bitmap(core_bitmap);
 				bit_or(*core_bitmap, resv_ptr->core_bitmap);
 			}
 		}
@@ -3534,7 +3546,7 @@ static void _check_job_compatibility(struct job_record *job_ptr,
 {
 	uint32_t total_nodes;
 	bitstr_t *full_node_bitmap;
-	int i_core, i_node, core_mult = 1;
+	int i_core, i_node;
 	int start = 0;
 	int rep_count = 0;
 	job_resources_t *job_res = job_ptr->job_resrcs;
@@ -3555,17 +3567,8 @@ static void _check_job_compatibility(struct job_record *job_ptr,
 }
 #endif
 
-#ifdef HAVE_BG
-	if (!cnodes_per_mp)
-		select_g_alter_node_cnt(SELECT_GET_NODE_SCALING,
-					&cnodes_per_mp);
-	core_mult = cnodes_per_mp;
-#endif
-
 	full_node_bitmap = bit_copy(job_res->node_bitmap);
-
-	if (!*core_bitmap)
-		*core_bitmap = cr_create_cluster_core_bitmap(core_mult);
+	_create_cluster_core_bitmap(core_bitmap);
 
 	i_node = 0;
 	while (i_node < total_nodes) {