2f879270a2fdf80ecd642d7d4a08b4733f5cdc8d
Danny Auble <da@schedmd.com>
2015-07-08 15:04:20 -0700

    Fix NULL pointer issue

--- src/common/slurm_protocol_socket_implementation.c
+++ src/common/slurm_protocol_socket_implementation.c
@@ -776,6 +776,12 @@ extern void slurm_print_slurm_addr ( slurm_addr_t * address, char *buf,
 				     size_t n )
 {
 	char addrbuf[INET_ADDRSTRLEN];
+
+	if (!address) {
+		snprintf(buf, n, "NULL");
+		return;
+	}
+
 	inet_ntop(AF_INET, &address->sin_addr, addrbuf, INET_ADDRSTRLEN);
 	/* warning: silently truncates */
 	snprintf(buf, n, "%s:%d", addrbuf, ntohs(address->sin_port));
