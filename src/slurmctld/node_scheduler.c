8e8d80b3c75085c80a4ebce1c9bab1f452767eb8
Morris Jette <jette@schedmd.com>
2015-07-14 15:56:32 -0700

    Prevent changing job HOLD reason set by select plugin
    Under some conditions the select/cons_res plugin will hold a job,
      setting it's priority to zero and reason to HELD. The logic in
      slurmctld's main scheduling loop previously kept its priority
      at zero, but changed the reason from HELD to RESOURCES. This
      change leaves the proper job state as set by the select plugin.
    This may be related to bug 1750

--- src/slurmctld/node_scheduler.c
+++ src/slurmctld/node_scheduler.c
@@ -1794,6 +1794,9 @@ extern int select_nodes(struct job_record *job_ptr, bool test_only,
 		} else if ((job_ptr->state_reason == WAIT_BLOCK_MAX_ERR) ||
 			   (job_ptr->state_reason == WAIT_BLOCK_D_ACTION)) {
 			/* state_reason was already setup */
+		} else if ((job_ptr->state_reason == WAIT_HELD) &&
+			   (job_ptr->priority == 0)) {
+			/* Held by select plugin due to some failure */
 		} else {
 			job_ptr->state_reason = WAIT_RESOURCES;
 			xfree(job_ptr->state_desc);
