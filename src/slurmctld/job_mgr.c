SchedMD/slurm

commit aa81c5503b63b581913af6ddf52aae2270588fec

Fix bug in slurmctld which was displaying job in CF as RUNNING.

src/slurmctld/job_mgr.c
@@ -6734,6 +6734,7 @@ void job_time_limit(void)
 			    slurmctld_conf.msg_timeout + 1);
 	time_t over_run;
 	int resv_status = 0;
+        uint8_t prolog;
 
 	if (slurmctld_conf.over_time_limit == (uint16_t) INFINITE)
 		over_run = now - (365 * 24 * 60 * 60);	/* one year */
@@ -6753,7 +6754,12 @@ void job_time_limit(void)
 		 * power_node_bitmap so bit_overlap always returns 0
 		 * and erroneously removes the flag.
 		 */
-		if (IS_JOB_CONFIGURING(job_ptr)) {
+                prolog = 0;
+                if (job_ptr->details)
+                        prolog = job_ptr->details->prolog_running;
+
+		if (prolog == 0
+                    && IS_JOB_CONFIGURING(job_ptr)) {
 			if (!IS_JOB_RUNNING(job_ptr) ||
 			    (bit_overlap(job_ptr->node_bitmap,
 					 power_node_bitmap) == 0)) {

src/slurmctld/job_scheduler.c
@@ -3151,6 +3151,8 @@ extern int prolog_slurmctld(struct job_record *job_ptr)
 	if (job_ptr->details)
 		job_ptr->details->prolog_running++;
 
+        job_ptr->job_state |= JOB_CONFIGURING;
+
 	slurm_attr_init(&thread_attr_prolog);
 	pthread_attr_setdetachstate(&thread_attr_prolog,
 				    PTHREAD_CREATE_DETACHED);
@@ -3230,6 +3232,7 @@ static void *_run_prolog(void *arg)
 			break;
 		}
 	}
+
 	if (status != 0) {
 		bool kill_job = false;
 		slurmctld_lock_t job_write_lock = {
@@ -3264,6 +3267,7 @@ static void *_run_prolog(void *arg)
 			error("prolog_slurmctld job %u now defunct", job_id);
 	}
 	if (job_ptr) {
+                job_ptr->job_state &= ~JOB_CONFIGURING;
 		if (job_ptr->details)
 			job_ptr->details->prolog_running--;
 		if (job_ptr->batch_flag &&

src/slurmctld/step_mgr.c
@@ -957,9 +957,12 @@ _pick_step_nodes (struct job_record  *job_ptr,
 				return NULL;
 			}
 		}
-		job_ptr->job_state &= (~JOB_CONFIGURING);
-		debug("Configuration for job %u complete", job_ptr->job_id);
-	}
+                if (job_ptr->details
+                    && job_ptr->details->prolog_running == 0) {
+                        job_ptr->job_state &= (~JOB_CONFIGURING);
+                        debug("Configuration for job %u complete", job_ptr->job_id);
+                }
+        }
 
 	/* In exclusive mode, just satisfy the processor count.
 	 * Do not use nodes that have no unused CPUs or insufficient

commit 55743ca0349fed0f254394ae2eb8674917e40f7f

Fix build problem on BG caused by aa81c5503b6.

src/slurmctld/job_mgr.c
@@ -6734,8 +6734,9 @@ void job_time_limit(void)
 			    slurmctld_conf.msg_timeout + 1);
 	time_t over_run;
 	int resv_status = 0;
+#ifndef HAVE_BG
         uint8_t prolog;
-
+#endif
 	if (slurmctld_conf.over_time_limit == (uint16_t) INFINITE)
 		over_run = now - (365 * 24 * 60 * 60);	/* one year */
 	else