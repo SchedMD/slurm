SchedMD/slurm

commit 1647714fecdbac81a7df11116598d521494502b4

Update man pages.

src/scontrol/update_job.c
@@ -731,7 +731,12 @@ scontrol_update_job (int argc, char *argv[])
 	for (i = 0; i < argc; i++) {
 		tag = argv[i];
 		val = strchr(argv[i], '=');
-		if (val) {
+		if (! val) {
+			tag = argv[i];
+			val = argv[i + 1];
+			++i;
+			vallen = strlen(val);
+		} else if (val) {
 			taglen = val - argv[i];
 			val++;
 			vallen = strlen(val);

doc/man/man3/slurm_job_cpus_allocated_str_on_node.3
@@ -1,0 +1,1 @@
+.so man3/slurm_free_job_info_msg.3

doc/man/man3/slurm_job_cpus_allocated_str_on_node_id.3
@@ -1,0 +1,1 @@
+.so man3/slurm_free_job_info_msg.3

src/scontrol/scontrol.c
@@ -1575,11 +1575,14 @@ _update_it (int argc, char *argv[])
 		char *tag = argv[i];
 		int tag_len = 0;
 		val = strchr(argv[i], '=');
-		if (!val)
-			continue;
-		tag_len = val - argv[i];
-		val++;
-
+		if (!val){
+			tag = argv[i];
+			tag_len = strlen(tag);
+			++i;
+		} else {
+			tag_len = val - argv[i];
+			val++;
+		}
 		if (!strncasecmp(tag, "NodeName", MAX(tag_len, 3))) {
 			node_tag = 1;
 		} else if (!strncasecmp(tag, "PartitionName",

commit 72a4f7ca57de926da34e72e918efea63464c0496

Fix issue with commit 1647714fecdbac8 which broke scontrol update job nice.

src/scontrol/update_job.c
@@ -726,12 +726,7 @@ scontrol_update_job (int argc, char *argv[])
 	for (i = 0; i < argc; i++) {
 		tag = argv[i];
 		val = strchr(argv[i], '=');
-		if (! val) {
-			tag = argv[i];
-			val = argv[i + 1];
-			++i;
-			vallen = strlen(val);
-		} else if (val) {
+		if (val) {
 			taglen = val - argv[i];
 			val++;
 			vallen = strlen(val);
@@ -741,6 +736,12 @@ scontrol_update_job (int argc, char *argv[])
 			job_msg.nice = NICE_OFFSET + 100;
 			update_cnt++;
 			continue;
+		} else if (! val) {
+			tag = argv[i];
+			val = argv[i + 1];
+			++i;
+			if (val)
+				vallen = strlen(val);
 		} else {
 			exit_code = 1;
 			fprintf (stderr, "Invalid input: %s\n", argv[i]);