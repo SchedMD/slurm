e95d63bc3d1f420eed2a180b6142f22e161f0804
Sergey Meirovich <rathamahata@gmail.com>
2015-08-28 13:01:01 -0700

    Ignore partition preempt mode of GANG

--- src/plugins/preempt/partition_prio/preempt_partition_prio.c
+++ src/plugins/preempt/partition_prio/preempt_partition_prio.c
@@ -179,9 +179,13 @@ static int _sort_by_prio (void *x, void *y)
 /**************************************************************************/
 extern uint16_t job_preempt_mode(struct job_record *job_ptr)
 {
-	if (job_ptr->part_ptr &&
-	    (job_ptr->part_ptr->preempt_mode != (uint16_t) NO_VAL))
-		return job_ptr->part_ptr->preempt_mode;
+	struct part_record *part_ptr = job_ptr->part_ptr;
+	if (part_ptr && (part_ptr->preempt_mode != (uint16_t) NO_VAL)) {
+		if (part_ptr->preempt_mode & PREEMPT_MODE_GANG)
+			verbose("Partition '%s' preempt mode 'gang' has no "
+				"sense. Filtered out.\n", part_ptr->name);
+		return (part_ptr->preempt_mode & (~PREEMPT_MODE_GANG));
+	}
 
 	return (slurm_get_preempt_mode() & (~PREEMPT_MODE_GANG));
 }
