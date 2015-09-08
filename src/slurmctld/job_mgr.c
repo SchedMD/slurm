19360d5b21ec4216d7b85e5e3bdcda2b60ec0ae8
jette <jette@schedmd.com>
2015-09-08 09:24:36 -0700

    Remove vestigial comment from last commit

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -3946,7 +3946,7 @@ static int _select_nodes_parts(struct job_record *job_ptr, bool test_only,
 			if (job_limits_check(&job_ptr, false) != WAIT_NO_REASON)
 				continue;
 			rc = select_nodes(job_ptr, test_only,
-					  select_node_bitmap, NULL, err_msg);	//
+					  select_node_bitmap, NULL, err_msg);
 			if ((rc != ESLURM_REQUESTED_NODE_CONFIG_UNAVAILABLE) &&
 			    (rc != ESLURM_REQUESTED_PART_CONFIG_UNAVAILABLE) &&
 			    (rc != ESLURM_RESERVATION_BUSY) &&
@@ -3960,7 +3960,7 @@ static int _select_nodes_parts(struct job_record *job_ptr, bool test_only,
 	} else {
 		if (job_limits_check(&job_ptr, false) != WAIT_NO_REASON)
 			test_only = true;
-		rc = select_nodes(job_ptr, test_only, select_node_bitmap,	//
+		rc = select_nodes(job_ptr, test_only, select_node_bitmap,
 				  NULL, err_msg);
 	}
 
