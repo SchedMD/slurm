af1163a20e1f82db6e177b13584de398c48fa9fe
Morris Jette <jette@schedmd.com>
2015-09-10 09:12:54 -0700

    Fix gres tracking for multiple steps
    GRES were not being properly tracks for multiple simultaneous steps.
    A step which could have run later could be rejected as never being
    able to run.
    Replacement for commit dd842d7997, which was reverted in commit 6f73812875c
    bug 1925

--- src/common/gres.c
+++ src/common/gres.c
@@ -4628,8 +4628,11 @@ static uint32_t _step_test(void *step_gres_data, void *job_gres_data,
 			gres_cnt = NO_VAL;
 	} else if (job_gres_ptr->gres_cnt_step_alloc &&
 		   job_gres_ptr->gres_cnt_step_alloc[node_offset]) {
-		gres_cnt = job_gres_ptr->gres_cnt_alloc -
-			   job_gres_ptr->gres_cnt_step_alloc[node_offset];
+		gres_cnt = job_gres_ptr->gres_cnt_alloc;
+		if (!ignore_alloc) {
+			gres_cnt -= job_gres_ptr->
+				    gres_cnt_step_alloc[node_offset];
+		}
 		if (step_gres_ptr->gres_cnt_alloc > gres_cnt)
 			gres_cnt = 0;
 		else
