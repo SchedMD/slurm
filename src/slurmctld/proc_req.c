SchedMD/slurm

commit fd12a0910adf75040d19d7bbdb87ca30c542be26

Send a tres_alloc_str for the batch step

src/slurmctld/proc_req.c
@@ -2123,6 +2123,8 @@ static void _slurm_rpc_complete_batch_script(slurm_msg_t * msg, bool locked)
 		batch_step.name = "batch";
 		batch_step.select_jobinfo = job_ptr->select_jobinfo;
 
+		step_set_alloc_tres(&batch_step, 1, false, false);
+
 		jobacct_storage_g_step_start(acct_db_conn, &batch_step);
 		jobacct_storage_g_step_complete(acct_db_conn, &batch_step);
 		FREE_NULL_BITMAP(batch_step.step_node_bitmap);

src/plugins/accounting_storage/mysql/as_mysql_job.c
@@ -977,6 +977,12 @@ extern int as_mysql_step_start(mysql_conn_t *mysql_conn,
 		*/
 		snprintf(node_list, BUFFER_SIZE, "%s", step_ptr->gres);
 		nodes = tasks = 1;
+		if (!step_ptr->tres_alloc_str)
+			xstrfmtcat(step_ptr->tres_alloc_str,
+				   "%s%u=%u,%u=%u",
+				   step_ptr->tres_alloc_str ? "," : "",
+				   TRES_CPU, 1,
+				   TRES_NODE, 1);
 	} else {
 		char *ionodes = NULL, *temp_nodes = NULL;
 		char temp_bit[BUF_SIZE];

src/slurmctld/step_mgr.c
@@ -3368,36 +3368,49 @@ extern void step_set_alloc_tres(
 	struct step_record *step_ptr, uint32_t node_count,
 	bool assoc_mgr_locked, bool make_formatted)
 {
-	uint64_t cpu_count, tres_count;
+	uint64_t cpu_count = 1, mem_count = 1;
 	char *tmp_tres_str = NULL;
 
-	xfree(step_ptr->tres_alloc_str);
-#ifdef HAVE_BG_L_P
-	/* Only L and P use this code */
-	if (step_ptr->job_ptr->details)
-		cpu_count = (uint64_t)step_ptr->job_ptr->details->min_cpus;
-	else
-		cpu_count = (uint64_t)step_ptr->job_ptr->cpu_cnt;
-#else
-	if (!step_ptr->step_layout || !step_ptr->step_layout->task_cnt)
-		cpu_count = (uint64_t)step_ptr->job_ptr->total_cpus;
-	else
-		cpu_count = (uint64_t)step_ptr->cpu_count;
-#endif
-	xfree(step_ptr->tres_alloc_str);
+	xassert(step_ptr);
 
-	tres_count = (uint64_t)step_ptr->pn_min_memory;
-	if (tres_count & MEM_PER_CPU) {
-		tres_count &= (~MEM_PER_CPU);
-		tres_count *= cpu_count;
-	} else
-		tres_count *= node_count;
+	xfree(step_ptr->tres_alloc_str);
+	xfree(step_ptr->tres_fmt_alloc_str);
+
+	if ((step_ptr->step_id == SLURM_BATCH_SCRIPT) &&
+	    step_ptr->job_ptr->job_resrcs) {
+		/* get the cpus and memory on the first node */
+		if (step_ptr->job_ptr->job_resrcs->cpus)
+			cpu_count = step_ptr->job_ptr->job_resrcs->cpus[0];
+		if (step_ptr->job_ptr->job_resrcs->memory_allocated)
+			mem_count = step_ptr->job_ptr->job_resrcs->
+				memory_allocated[0];
+	} else {
+#ifdef HAVE_BG_L_P
+		/* Only L and P use this code */
+		if (step_ptr->job_ptr->details)
+			cpu_count =
+				(uint64_t)step_ptr->job_ptr->details->min_cpus;
+		else
+			cpu_count = (uint64_t)step_ptr->job_ptr->cpu_cnt;
+#else
+		if (!step_ptr->step_layout || !step_ptr->step_layout->task_cnt)
+			cpu_count = (uint64_t)step_ptr->job_ptr->total_cpus;
+		else
+			cpu_count = (uint64_t)step_ptr->cpu_count;
+#endif
+		mem_count = (uint64_t)step_ptr->pn_min_memory;
+		if (mem_count & MEM_PER_CPU) {
+			mem_count &= (~MEM_PER_CPU);
+			mem_count *= cpu_count;
+		} else
+			mem_count *= node_count;
+	}
 
 	xstrfmtcat(step_ptr->tres_alloc_str,
 		   "%s%u=%"PRIu64",%u=%"PRIu64",%u=%u",
 		   step_ptr->tres_alloc_str ? "," : "",
 		   TRES_CPU, cpu_count,
-		   TRES_MEM, tres_count,
+		   TRES_MEM, mem_count,
 		   TRES_NODE, node_count);
 
 	if ((tmp_tres_str = gres_2_tres_str(step_ptr->gres_list, 0, true))) {
@@ -3410,8 +3423,6 @@ extern void step_set_alloc_tres(
 	if (make_formatted) {
 		assoc_mgr_lock_t locks = { NO_LOCK, NO_LOCK, NO_LOCK, NO_LOCK,
 					   READ_LOCK, NO_LOCK, NO_LOCK };
-		xfree(step_ptr->tres_fmt_alloc_str);
-
 		if (!assoc_mgr_locked)
 			assoc_mgr_lock(&locks);
 

commit 14a39998c48f6f3d5cd7449e3dadc0c73fa3f416

Fix memory leak caused by fd12a0910adf75.

src/slurmctld/proc_req.c
@@ -2128,6 +2128,7 @@ static void _slurm_rpc_complete_batch_script(slurm_msg_t * msg, bool locked)
 		jobacct_storage_g_step_start(acct_db_conn, &batch_step);
 		jobacct_storage_g_step_complete(acct_db_conn, &batch_step);
 		FREE_NULL_BITMAP(batch_step.step_node_bitmap);
+		xfree(batch_step.tres_alloc_str);
 	}
 
 #ifdef HAVE_FRONT_END