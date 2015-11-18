SchedMD/slurm

commit 06b54823b193c724d05045bc032fdad79b4b6a65

remove pow() function calls to avoid dependency on -lm for clang

src/common/gres.c
@@ -816,9 +816,9 @@ static int _parse_gres_config(void **dest, slurm_parser_enum_t type,
 		else if ((last[0] == 'g') || (last[0] == 'G'))
 			tmp_uint64 *= (1024 * 1024 * 1024);
 		else if ((last[0] == 't') || (last[0] == 'T'))
-			tmp_uint64 *= (uint64_t)pow(1024, 4);
+			tmp_uint64 *= (1024 * 1024 * 1024 * 1024);
 		else if ((last[0] == 'p') || (last[0] == 'P'))
-			tmp_uint64 *= (uint64_t)pow(1024, 5);
+			tmp_uint64 *= (1024 * 1024 * 1024 * 1024 * 1024);
 		else if (last[0] != '\0') {
 			fatal("Invalid gres data for %s, Count=%s", p->name,
 			      tmp_str);
@@ -1507,9 +1507,9 @@ static void _get_gres_cnt(gres_node_state_t *gres_data, char *orig_config,
 			else if ((last_num[0] == 'g') || (last_num[0] == 'G'))
 				tmp_gres_cnt *= (1024 * 1024 * 1024);
 			else if ((last_num[0] == 't') || (last_num[0] == 'T'))
-				tmp_gres_cnt *= (uint64_t)pow(1024, 4);
+				tmp_gres_cnt *= (1024 * 1024 * 1024 * 1024);
 			else if ((last_num[0] == 'p') || (last_num[0] == 'P'))
-				tmp_gres_cnt *= (uint64_t)pow(1024, 5);
+				tmp_gres_cnt *= (1024 * 1024 * 1024 * 1024 * 1024);
 			else {
 				error("Bad GRES configuration: %s", tok);
 				break;

commit 7188282dbe85fa28fdf14b6204c53c8c21d3b5ef

Fix for possible overflow
bug introduced in commit 06b54823b193c724d05045bc032fdad79b4b6a65

src/common/gres.c
@@ -814,11 +814,12 @@ static int _parse_gres_config(void **dest, slurm_parser_enum_t type,
 		else if ((last[0] == 'm') || (last[0] == 'M'))
 			tmp_uint64 *= (1024 * 1024);
 		else if ((last[0] == 'g') || (last[0] == 'G'))
-			tmp_uint64 *= (1024 * 1024 * 1024);
+			tmp_uint64 *= ((uint64_t)1024 * 1024 * 1024);
 		else if ((last[0] == 't') || (last[0] == 'T'))
-			tmp_uint64 *= (1024 * 1024 * 1024 * 1024);
+			tmp_uint64 *= ((uint64_t)1024 * 1024 * 1024 * 1024);
 		else if ((last[0] == 'p') || (last[0] == 'P'))
-			tmp_uint64 *= (1024 * 1024 * 1024 * 1024 * 1024);
+			tmp_uint64 *= ((uint64_t)1024 * 1024 * 1024 * 1024 *
+				       1024);
 		else if (last[0] != '\0') {
 			fatal("Invalid gres data for %s, Count=%s", p->name,
 			      tmp_str);
@@ -1505,11 +1506,13 @@ static void _get_gres_cnt(gres_node_state_t *gres_data, char *orig_config,
 			else if ((last_num[0] == 'm') || (last_num[0] == 'M'))
 				tmp_gres_cnt *= (1024 * 1024);
 			else if ((last_num[0] == 'g') || (last_num[0] == 'G'))
-				tmp_gres_cnt *= (1024 * 1024 * 1024);
+				tmp_gres_cnt *= ((uint64_t)1024 * 1024 * 1024);
 			else if ((last_num[0] == 't') || (last_num[0] == 'T'))
-				tmp_gres_cnt *= (1024 * 1024 * 1024 * 1024);
+				tmp_gres_cnt *= ((uint64_t)1024 * 1024 * 1024 *
+						 1024);
 			else if ((last_num[0] == 'p') || (last_num[0] == 'P'))
-				tmp_gres_cnt *= (1024 * 1024 * 1024 * 1024 * 1024);
+				tmp_gres_cnt *= ((uint64_t)1024 * 1024 * 1024 *
+						 1024 * 1024);
 			else {
 				error("Bad GRES configuration: %s", tok);
 				break;