70c5ef3f8b5ea036e6cc1a37b07b5dff759399b2
Josko Plazonic <plazonic@princeton.edu>
2015-11-25 10:17:09 +0100

    Install the pmi2 socket in the slurmd spool directory instead of /tmp. #2169

--- src/plugins/mpi/pmi2/setup.c
+++ src/plugins/mpi/pmi2/setup.c
@@ -71,7 +71,7 @@
 #include "kvs.h"
 #include "ring.h"
 
-#define PMI2_SOCK_ADDR_FMT "/tmp/sock.pmi2.%u.%u"
+#define PMI2_SOCK_ADDR_FMT "%s/sock.pmi2.%u.%u"
 
 
 extern char **environ;
@@ -266,6 +266,7 @@ _setup_stepd_sockets(const stepd_step_rec_t *job, char ***env)
 {
 	struct sockaddr_un sa;
 	int i;
+	char *spool;
 
 	debug("mpi/pmi2: setup sockets");
 
@@ -275,9 +276,12 @@ _setup_stepd_sockets(const stepd_step_rec_t *job, char ***env)
 		return SLURM_ERROR;
 	}
 	sa.sun_family = PF_UNIX;
+
+	spool = slurm_get_slurmd_spooldir();
 	snprintf(sa.sun_path, sizeof(sa.sun_path), PMI2_SOCK_ADDR_FMT,
-		 job->jobid, job->stepid);
+		 spool, job->jobid, job->stepid);
 	unlink(sa.sun_path);    /* remove possible old socket */
+	xfree(spool);
 
 	if (bind(tree_sock, (struct sockaddr *)&sa, SUN_LEN(&sa)) < 0) {
 		error("mpi/pmi2: failed to bind tree socket: %m");
@@ -609,6 +613,7 @@ _setup_srun_tree_info(const mpi_plugin_client_info_t *job)
 {
 	char *p;
 	uint16_t p_port;
+	char *spool;
 
 	memset(&tree_info, 0, sizeof(tree_info));
 
@@ -628,8 +633,10 @@ _setup_srun_tree_info(const mpi_plugin_client_info_t *job)
 	} else
 		tree_info.srun_addr = NULL;
 
+	spool = slurm_get_slurmd_spooldir();
 	snprintf(tree_sock_addr, 128, PMI2_SOCK_ADDR_FMT,
-		 job->jobid, job->stepid);
+		 spool, job->jobid, job->stepid);
+	xfree(spool);
 
 	/* init kvs seq to 0. TODO: reduce array size */
 	tree_info.children_kvs_seq = xmalloc(sizeof(uint32_t) *
