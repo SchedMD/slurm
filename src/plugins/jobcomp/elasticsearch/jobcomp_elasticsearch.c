27aedc3e3b1b0afbd916fc1f004a7b679e356e55
Alejandro Sanchez <alex@schedmd.com>
2015-12-01 10:51:18 -0800

    Elasticsearch code changes for performance improvements

--- src/plugins/jobcomp/elasticsearch/jobcomp_elasticsearch.c
+++ src/plugins/jobcomp/elasticsearch/jobcomp_elasticsearch.c
@@ -58,6 +58,7 @@
 #include <unistd.h>
 #include "src/common/assoc_mgr.h"
 #include "src/common/fd.h"
+#include "src/common/list.h"
 #include "src/common/parse_time.h"
 #include "src/common/slurm_jobcomp.h"
 #include "src/common/slurm_protocol_defs.h"
@@ -153,6 +154,8 @@ char *log_url = NULL;
 
 static pthread_mutex_t save_lock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t pend_jobs_lock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_t job_handler_thread;
+List jobslist = NULL;
 
 /* A plugin-global errno. */
 static int plugin_errno = SLURM_SUCCESS;
@@ -422,7 +425,8 @@ static char *_json_escape(const char *str)
 {
 	char *ret = NULL;
 	int i;
-	for (i = 0; i < strlen(str); ++i) {
+	int len = strlen(str);
+	for (i = 0; i < len; ++i) {
 		switch (str[i]) {
 		case '\\':
 			xstrcat(ret, "\\\\");
@@ -447,6 +451,9 @@ static char *_json_escape(const char *str)
 			break;
 		case '<':
 			xstrcat(ret, "\\u003C");
+			break;
+		case '/':
+			xstrcat(ret, "\\/");
 			break;
 		default:
 			xstrcatchar(ret, str[i]);
@@ -538,11 +545,11 @@ static int _save_state(void)
 }
 
 /* Add jobcomp data to the pending jobs structure */
-static void _push_pending_job(char *j)
+static void _push_pending_job(char **j)
 {
 	pend_jobs.jobs = xrealloc(pend_jobs.jobs,
 				  sizeof(char *) * (pend_jobs.nelems + 1));
-	pend_jobs.jobs[pend_jobs.nelems] = xstrdup(j);
+	pend_jobs.jobs[pend_jobs.nelems] = xstrdup(*j);
 	pend_jobs.nelems++;
 }
 
@@ -632,6 +639,292 @@ static void _make_time_str(time_t * time, char *string, int size)
 	}
 }
 
+extern int _job_serialize(struct job_record *job_ptr, char **buffer)
+{
+	int nwritten, B_SIZE = 1024;
+	char usr_str[32], grp_str[32], start_str[32], end_str[32];
+	char submit_str[32], *cluster = NULL, *qos, *state_string;
+	time_t elapsed_time, submit_time, eligible_time;
+	enum job_states job_state;
+	uint32_t time_limit;
+	uint16_t ntasks_per_node;
+	int i, tmp_size;
+	char *tmp, *script_str, *script;
+
+	_get_user_name(job_ptr->user_id, usr_str, sizeof(usr_str));
+	_get_group_name(job_ptr->group_id, grp_str, sizeof(grp_str));
+
+	if ((job_ptr->time_limit == NO_VAL) && job_ptr->part_ptr)
+		time_limit = job_ptr->part_ptr->max_time;
+	else
+		time_limit = job_ptr->time_limit;
+
+	if (job_ptr->job_state & JOB_RESIZING) {
+		time_t now = time(NULL);
+		state_string = job_state_string(job_ptr->job_state);
+		if (job_ptr->resize_time) {
+			_make_time_str(&job_ptr->resize_time, start_str,
+				       sizeof(start_str));
+		} else {
+			_make_time_str(&job_ptr->start_time, start_str,
+				       sizeof(start_str));
+		}
+		_make_time_str(&now, end_str, sizeof(end_str));
+	} else {
+		/* Job state will typically have JOB_COMPLETING or JOB_RESIZING
+		 * flag set when called. We remove the flags to get the eventual
+		 * completion state: JOB_FAILED, JOB_TIMEOUT, etc. */
+		job_state = job_ptr->job_state & JOB_STATE_BASE;
+		state_string = job_state_string(job_state);
+		if (job_ptr->resize_time) {
+			_make_time_str(&job_ptr->resize_time, start_str,
+				       sizeof(start_str));
+		} else if (job_ptr->start_time > job_ptr->end_time) {
+			/* Job cancelled while pending and
+			 * expected start time is in the future. */
+			snprintf(start_str, sizeof(start_str), "Unknown");
+		} else {
+			_make_time_str(&job_ptr->start_time, start_str,
+				       sizeof(start_str));
+		}
+		_make_time_str(&job_ptr->end_time, end_str, sizeof(end_str));
+	}
+
+	elapsed_time = job_ptr->end_time - job_ptr->start_time;
+
+	*buffer = xmalloc(B_SIZE);
+
+	nwritten = snprintf(*buffer, B_SIZE, JOBCOMP_DATA_FORMAT,
+			    (unsigned long) job_ptr->job_id, usr_str,
+			    (unsigned long) job_ptr->user_id, grp_str,
+			    (unsigned long) job_ptr->group_id, start_str,
+			    end_str, (long) elapsed_time,
+			    job_ptr->partition, job_ptr->alloc_node,
+			    job_ptr->nodes, (unsigned long) job_ptr->total_cpus,
+			    (unsigned long) job_ptr->total_nodes,
+			    (unsigned long) job_ptr->derived_ec,
+			    (unsigned long) job_ptr->exit_code, state_string);
+
+	if (nwritten >= B_SIZE) {
+		B_SIZE += nwritten + 1;
+		*buffer = xrealloc(*buffer, B_SIZE);
+
+		nwritten = snprintf(*buffer, B_SIZE, JOBCOMP_DATA_FORMAT,
+				    (unsigned long) job_ptr->job_id, usr_str,
+				    (unsigned long) job_ptr->user_id, grp_str,
+				    (unsigned long) job_ptr->group_id,
+				    start_str, end_str, (long) elapsed_time,
+				    job_ptr->partition, job_ptr->alloc_node,
+				    job_ptr->nodes,
+				    (unsigned long) job_ptr->total_cpus,
+				    (unsigned long) job_ptr->total_nodes,
+				    (unsigned long) job_ptr->derived_ec,
+				    (unsigned long) job_ptr->exit_code,
+				    state_string);
+
+		if (nwritten >= B_SIZE) {
+			debug("Job completion data truncated and lost");
+			return SLURM_ERROR;
+		}
+	}
+
+	tmp_size = 256;
+	tmp = xmalloc(tmp_size * sizeof(char));
+
+	sprintf(tmp, ",\"cpu_hours\":%.6f",
+		((float) elapsed_time * (float) job_ptr->total_cpus) /
+		(float) 3600);
+	xstrcat(*buffer, tmp);
+
+	if (job_ptr->array_task_id != NO_VAL) {
+		xstrfmtcat(*buffer, ",\"array_job_id\":%lu",
+			   (unsigned long) job_ptr->array_job_id);
+		xstrfmtcat(*buffer, ",\"array_task_id\":%lu",
+			   (unsigned long) job_ptr->array_task_id);
+	}
+
+	if (job_ptr->details && (job_ptr->details->submit_time != NO_VAL)) {
+		submit_time = job_ptr->details->submit_time;
+		_make_time_str(&submit_time, submit_str, sizeof(submit_str));
+		xstrfmtcat(*buffer, ",\"@submit\":\"%s\"", submit_str);
+	}
+
+	if (job_ptr->details && (job_ptr->details->begin_time != NO_VAL)) {
+		eligible_time =
+		    job_ptr->start_time - job_ptr->details->begin_time;
+		xstrfmtcat(*buffer, ",\"eligible_time\":%lu", eligible_time);
+	}
+
+	if (job_ptr->details
+	    && (job_ptr->details->work_dir && job_ptr->details->work_dir[0])) {
+		xstrfmtcat(*buffer, ",\"work_dir\":\"%s\"",
+			   job_ptr->details->work_dir);
+	}
+
+	if (job_ptr->details
+	    && (job_ptr->details->std_err && job_ptr->details->std_err[0])) {
+		xstrfmtcat(*buffer, ",\"std_err\":\"%s\"",
+			   job_ptr->details->std_err);
+	}
+
+	if (job_ptr->details
+	    && (job_ptr->details->std_in && job_ptr->details->std_in[0])) {
+		xstrfmtcat(*buffer, ",\"std_in\":\"%s\"",
+			   job_ptr->details->std_in);
+	}
+
+	if (job_ptr->details
+	    && (job_ptr->details->std_out && job_ptr->details->std_out[0])) {
+		xstrfmtcat(*buffer, ",\"std_out\":\"%s\"",
+			   job_ptr->details->std_out);
+	}
+
+	if (job_ptr->assoc_ptr != NULL) {
+		cluster = ((slurmdb_assoc_rec_t *) job_ptr->assoc_ptr)->cluster;
+		xstrfmtcat(*buffer, ",\"cluster\":\"%s\"", cluster);
+	}
+
+	if (job_ptr->qos_ptr != NULL) {
+		slurmdb_qos_rec_t *assoc =
+		    (slurmdb_qos_rec_t *) job_ptr->qos_ptr;
+		qos = assoc->name;
+		xstrfmtcat(*buffer, ",\"qos\":\"%s\"", qos);
+	}
+
+	if (job_ptr->details && (job_ptr->details->num_tasks != NO_VAL)) {
+		xstrfmtcat(*buffer, ",\"ntasks\":%hu",
+			   job_ptr->details->num_tasks);
+	}
+
+	if (job_ptr->details && (job_ptr->details->ntasks_per_node != NO_VAL)) {
+		ntasks_per_node = job_ptr->details->ntasks_per_node;
+		xstrfmtcat(*buffer, ",\"ntasks_per_node\":%hu", ntasks_per_node);
+	}
+
+	if (job_ptr->details && (job_ptr->details->cpus_per_task != NO_VAL)) {
+		xstrfmtcat(*buffer, ",\"cpus_per_task\":%hu",
+			   job_ptr->details->cpus_per_task);
+	}
+
+	if (job_ptr->details
+	    && (job_ptr->details->orig_dependency
+		&& job_ptr->details->orig_dependency[0])) {
+		xstrfmtcat(*buffer, ",\"orig_dependency\":\"%s\"",
+			   job_ptr->details->orig_dependency);
+	}
+
+	if (job_ptr->details
+	    && (job_ptr->details->exc_nodes
+		&& job_ptr->details->exc_nodes[0])) {
+		xstrfmtcat(*buffer, ",\"excluded_nodes\":\"%s\"",
+			   job_ptr->details->exc_nodes);
+	}
+
+	if (time_limit != INFINITE) {
+		xstrfmtcat(*buffer, ",\"time_limit\":%lu",
+			(unsigned long) time_limit * 60);
+	}
+
+	if (job_ptr->resv_name && job_ptr->resv_name[0]) {
+		xstrfmtcat(*buffer, ",\"reservation_name\":\"%s\"",
+			   job_ptr->resv_name);
+	}
+
+	if (job_ptr->gres_req && job_ptr->gres_req[0]) {
+		xstrfmtcat(*buffer, ",\"gres_req\":\"%s\"", job_ptr->gres_req);
+	}
+
+
+	if (job_ptr->gres_alloc && job_ptr->gres_alloc[0]) {
+		xstrfmtcat(*buffer, ",\"gres_alloc\":\"%s\"",
+			   job_ptr->gres_alloc);
+	}
+
+	if (job_ptr->account && job_ptr->account[0]) {
+		xstrfmtcat(*buffer, ",\"account\":\"%s\"", job_ptr->account);
+	}
+
+	script = get_job_script(job_ptr);
+	if (script && script[0]) {
+		script_str = _json_escape(script);
+		xstrfmtcat(*buffer, ",\"script\":\"%s\"", script_str);
+		xfree(script_str);
+		xfree(script);
+	}
+
+	if (job_ptr->assoc_ptr) {
+		assoc_mgr_lock_t locks = { READ_LOCK, NO_LOCK, NO_LOCK, NO_LOCK,
+					   NO_LOCK, NO_LOCK, NO_LOCK };
+		slurmdb_assoc_rec_t *assoc_ptr = job_ptr->assoc_ptr;
+		char *parent_accounts = NULL;
+		char **acc_aux = NULL;
+		int nparents = 0;
+
+		assoc_mgr_lock(&locks);
+
+		/* Start at the first parent and go up.  When studying
+		 * this code it was slightly faster to do 2 loops on
+		 * the association linked list and only 1 xmalloc but
+		 * we opted for cleaner looking code and going with a
+		 * realloc. */
+		while (assoc_ptr) {
+			if (assoc_ptr->acct) {
+				acc_aux = xrealloc(acc_aux,
+						   sizeof(char *) *
+						   (nparents + 1));
+				acc_aux[nparents++] = assoc_ptr->acct;
+			}
+			assoc_ptr = assoc_ptr->usage->parent_assoc_ptr;
+		}
+
+		for (i = nparents-1; i >= 0; i--)
+			xstrfmtcat(parent_accounts, "/%s", acc_aux[i]);
+		xfree(acc_aux);
+
+		xstrfmtcat(*buffer, ",\"parent_accounts\":\"%s\"",
+			   parent_accounts);
+
+		xfree(parent_accounts);
+
+		assoc_mgr_unlock(&locks);
+	}
+
+	xstrcat(*buffer, "}");
+
+	xfree(tmp);
+	//xfree(buffer);
+
+	return SLURM_SUCCESS;
+}
+
+
+
+extern void* _process_jobs()
+{
+	char *json_job;
+
+	while(1)
+	{
+		if (list_is_empty(jobslist)) {
+			debug5("%s: No finished jobs to log into Elasticsearch, waiting", plugin_type);
+			sleep(1);
+			continue;
+		}
+		json_job = list_dequeue(jobslist);
+		if (_index_job(json_job) == SLURM_ERROR) {
+			slurm_mutex_lock(&pend_jobs_lock);
+			_push_pending_job(&json_job);
+			slurm_mutex_unlock(&pend_jobs_lock);
+			// Moved _save_state to fini()
+			//_save_state();
+		} else {
+			xfree(json_job);
+			// we just retry pending jobs at set_location()
+			//_index_retry();
+		}
+	}
+}
+
 /*
  * init() is called when the plugin is loaded, before any other functions
  * are called. Put global initialization here.
@@ -640,6 +933,12 @@ extern int init(void)
 {
 	int rc;
 
+	jobslist = list_create(NULL);
+	pthread_attr_t thread_attr;
+        slurm_attr_init(&thread_attr);
+        if (pthread_create(&job_handler_thread, &thread_attr,
+        		   _process_jobs, NULL))
+        	fatal("pthread_create error %m");
 	slurm_mutex_lock(&pend_jobs_lock);
 	rc = _load_pending_jobs();
 	slurm_mutex_unlock(&pend_jobs_lock);
@@ -649,9 +948,12 @@ extern int init(void)
 
 extern int fini(void)
 {
+	_save_state();
+	list_destroy(jobslist);
 	xfree(log_url);
 	xfree(save_state_file);
 	xfree(index_type);
+	pthread_join(job_handler_thread, NULL);
 	return SLURM_SUCCESS;
 }
 
@@ -697,269 +999,13 @@ extern int slurm_jobcomp_set_location(char *location)
 
 extern int slurm_jobcomp_log_record(struct job_record *job_ptr)
 {
-	int nwritten, B_SIZE = 1024, rc = SLURM_SUCCESS;
-	char usr_str[32], grp_str[32], start_str[32], end_str[32];
-	char submit_str[32], *script, *cluster = NULL, *qos, *state_string;
-	char *script_str;
-	time_t elapsed_time, submit_time, eligible_time;
-	enum job_states job_state;
-	uint32_t time_limit;
-	uint16_t ntasks_per_node;
-	int i, tmp_size;
-	char *buffer, *tmp;
+	char *json_job;
+	int rc;
 
-	_get_user_name(job_ptr->user_id, usr_str, sizeof(usr_str));
-	_get_group_name(job_ptr->group_id, grp_str, sizeof(grp_str));
+	rc = _job_serialize(job_ptr, &json_job);
 
-	if ((job_ptr->time_limit == NO_VAL) && job_ptr->part_ptr)
-		time_limit = job_ptr->part_ptr->max_time;
-	else
-		time_limit = job_ptr->time_limit;
-
-	if (job_ptr->job_state & JOB_RESIZING) {
-		time_t now = time(NULL);
-		state_string = job_state_string(job_ptr->job_state);
-		if (job_ptr->resize_time) {
-			_make_time_str(&job_ptr->resize_time, start_str,
-				       sizeof(start_str));
-		} else {
-			_make_time_str(&job_ptr->start_time, start_str,
-				       sizeof(start_str));
-		}
-		_make_time_str(&now, end_str, sizeof(end_str));
-	} else {
-		/* Job state will typically have JOB_COMPLETING or JOB_RESIZING
-		 * flag set when called. We remove the flags to get the eventual
-		 * completion state: JOB_FAILED, JOB_TIMEOUT, etc. */
-		job_state = job_ptr->job_state & JOB_STATE_BASE;
-		state_string = job_state_string(job_state);
-		if (job_ptr->resize_time) {
-			_make_time_str(&job_ptr->resize_time, start_str,
-				       sizeof(start_str));
-		} else if (job_ptr->start_time > job_ptr->end_time) {
-			/* Job cancelled while pending and
-			 * expected start time is in the future. */
-			snprintf(start_str, sizeof(start_str), "Unknown");
-		} else {
-			_make_time_str(&job_ptr->start_time, start_str,
-				       sizeof(start_str));
-		}
-		_make_time_str(&job_ptr->end_time, end_str, sizeof(end_str));
-	}
-
-	elapsed_time = job_ptr->end_time - job_ptr->start_time;
-
-	buffer = xmalloc(B_SIZE);
-
-	nwritten = snprintf(buffer, B_SIZE, JOBCOMP_DATA_FORMAT,
-			    (unsigned long) job_ptr->job_id, usr_str,
-			    (unsigned long) job_ptr->user_id, grp_str,
-			    (unsigned long) job_ptr->group_id, start_str,
-			    end_str, (long) elapsed_time,
-			    job_ptr->partition, job_ptr->alloc_node,
-			    job_ptr->nodes, (unsigned long) job_ptr->total_cpus,
-			    (unsigned long) job_ptr->total_nodes,
-			    (unsigned long) job_ptr->derived_ec,
-			    (unsigned long) job_ptr->exit_code, state_string);
-
-	if (nwritten >= B_SIZE) {
-		B_SIZE += nwritten + 1;
-		buffer = xrealloc(buffer, B_SIZE);
-
-		nwritten = snprintf(buffer, B_SIZE, JOBCOMP_DATA_FORMAT,
-				    (unsigned long) job_ptr->job_id, usr_str,
-				    (unsigned long) job_ptr->user_id, grp_str,
-				    (unsigned long) job_ptr->group_id,
-				    start_str, end_str, (long) elapsed_time,
-				    job_ptr->partition, job_ptr->alloc_node,
-				    job_ptr->nodes,
-				    (unsigned long) job_ptr->total_cpus,
-				    (unsigned long) job_ptr->total_nodes,
-				    (unsigned long) job_ptr->derived_ec,
-				    (unsigned long) job_ptr->exit_code,
-				    state_string);
-
-		if (nwritten >= B_SIZE) {
-			debug("Job completion data truncated and lost");
-			rc = SLURM_ERROR;
-		}
-	}
-
-	tmp_size = 256;
-	tmp = xmalloc(tmp_size * sizeof(char));
-
-	sprintf(tmp, ",\"cpu_hours\":%.6f",
-		((float) elapsed_time * (float) job_ptr->total_cpus) /
-		(float) 3600);
-	xstrcat(buffer, tmp);
-
-	if (job_ptr->array_task_id != NO_VAL) {
-		xstrfmtcat(buffer, ",\"array_job_id\":%lu",
-			   (unsigned long) job_ptr->array_job_id);
-		xstrfmtcat(buffer, ",\"array_task_id\":%lu",
-			   (unsigned long) job_ptr->array_task_id);
-	}
-
-	if (job_ptr->details && (job_ptr->details->submit_time != NO_VAL)) {
-		submit_time = job_ptr->details->submit_time;
-		_make_time_str(&submit_time, submit_str, sizeof(submit_str));
-		xstrfmtcat(buffer, ",\"@submit\":\"%s\"", submit_str);
-	}
-
-	if (job_ptr->details && (job_ptr->details->begin_time != NO_VAL)) {
-		eligible_time =
-		    job_ptr->start_time - job_ptr->details->begin_time;
-		xstrfmtcat(buffer, ",\"eligible_time\":%lu", eligible_time);
-	}
-
-	if (job_ptr->details
-	    && (job_ptr->details->work_dir && job_ptr->details->work_dir[0])) {
-		xstrfmtcat(buffer, ",\"work_dir\":\"%s\"",
-			   job_ptr->details->work_dir);
-	}
-
-	if (job_ptr->details
-	    && (job_ptr->details->std_err && job_ptr->details->std_err[0])) {
-		xstrfmtcat(buffer, ",\"std_err\":\"%s\"",
-			   job_ptr->details->std_err);
-	}
-
-	if (job_ptr->details
-	    && (job_ptr->details->std_in && job_ptr->details->std_in[0])) {
-		xstrfmtcat(buffer, ",\"std_in\":\"%s\"",
-			   job_ptr->details->std_in);
-	}
-
-	if (job_ptr->details
-	    && (job_ptr->details->std_out && job_ptr->details->std_out[0])) {
-		xstrfmtcat(buffer, ",\"std_out\":\"%s\"",
-			   job_ptr->details->std_out);
-	}
-
-	if (job_ptr->assoc_ptr != NULL) {
-		cluster = ((slurmdb_assoc_rec_t *) job_ptr->assoc_ptr)->cluster;
-		xstrfmtcat(buffer, ",\"cluster\":\"%s\"", cluster);
-	}
-
-	if (job_ptr->qos_ptr != NULL) {
-		slurmdb_qos_rec_t *assoc =
-		    (slurmdb_qos_rec_t *) job_ptr->qos_ptr;
-		qos = assoc->name;
-		xstrfmtcat(buffer, ",\"qos\":\"%s\"", qos);
-	}
-
-	if (job_ptr->details && (job_ptr->details->num_tasks != NO_VAL)) {
-		xstrfmtcat(buffer, ",\"ntasks\":%hu",
-			   job_ptr->details->num_tasks);
-	}
-
-	if (job_ptr->details && (job_ptr->details->ntasks_per_node != NO_VAL)) {
-		ntasks_per_node = job_ptr->details->ntasks_per_node;
-		xstrfmtcat(buffer, ",\"ntasks_per_node\":%hu", ntasks_per_node);
-	}
-
-	if (job_ptr->details && (job_ptr->details->cpus_per_task != NO_VAL)) {
-		xstrfmtcat(buffer, ",\"cpus_per_task\":%hu",
-			   job_ptr->details->cpus_per_task);
-	}
-
-	if (job_ptr->details
-	    && (job_ptr->details->orig_dependency
-		&& job_ptr->details->orig_dependency[0])) {
-		xstrfmtcat(buffer, ",\"orig_dependency\":\"%s\"",
-			   job_ptr->details->orig_dependency);
-	}
-
-	if (job_ptr->details
-	    && (job_ptr->details->exc_nodes
-		&& job_ptr->details->exc_nodes[0])) {
-		xstrfmtcat(buffer, ",\"excluded_nodes\":\"%s\"",
-			   job_ptr->details->exc_nodes);
-	}
-
-	if (time_limit != INFINITE) {
-		xstrfmtcat(buffer, ",\"time_limit\":%lu",
-			(unsigned long) time_limit * 60);
-	}
-
-	if (job_ptr->resv_name && job_ptr->resv_name[0]) {		
-		xstrfmtcat(buffer, ",\"reservation_name\":\"%s\"",
-			   job_ptr->resv_name);
-	}
-
-	if (job_ptr->gres_req && job_ptr->gres_req[0]) {		
-		xstrfmtcat(buffer, ",\"gres_req\":\"%s\"", job_ptr->gres_req);
-	}
-
-	if (job_ptr->gres_alloc && job_ptr->gres_alloc[0]) {		
-		xstrfmtcat(buffer, ",\"gres_alloc\":\"%s\"",
-			   job_ptr->gres_alloc);
-	}
-
-	if (job_ptr->account && job_ptr->account[0]) {
-		xstrfmtcat(buffer, ",\"account\":\"%s\"", job_ptr->account);
-	}
-
-	script = get_job_script(job_ptr);
-	if (script && script[0]) {
-		script_str = _json_escape(script);
-		xstrfmtcat(buffer, ",\"script\":\"%s\"", script_str);
-		xfree(script_str);
-		xfree(script);
-	}
-
-	if (job_ptr->assoc_ptr) {
-		assoc_mgr_lock_t locks = { READ_LOCK, NO_LOCK, NO_LOCK, NO_LOCK,
-					   NO_LOCK, NO_LOCK, NO_LOCK };
-		slurmdb_assoc_rec_t *assoc_ptr = job_ptr->assoc_ptr;
-		char *parent_accounts = NULL;
-		char **acc_aux = NULL;
-		int nparents = 0;
-
-		assoc_mgr_lock(&locks);
-
-		/* Start at the first parent and go up.  When studying
-		 * this code it was slightly faster to do 2 loops on
-		 * the association linked list and only 1 xmalloc but
-		 * we opted for cleaner looking code and going with a
-		 * realloc. */
-		while (assoc_ptr) {
-			if (assoc_ptr->acct) {
-				acc_aux = xrealloc(acc_aux,
-						   sizeof(char *) *
-						   (nparents + 1));
-				acc_aux[nparents++] = assoc_ptr->acct;
-			}
-			assoc_ptr = assoc_ptr->usage->parent_assoc_ptr;
-		}
-
-		for (i = nparents-1; i >= 0; i--)
-			xstrfmtcat(parent_accounts, "/%s", acc_aux[i]);
-		xfree(acc_aux);
-
-		xstrfmtcat(buffer, ",\"parent_accounts\":\"%s\"",
-			   parent_accounts);
-
-		xfree(parent_accounts);
-
-		assoc_mgr_unlock(&locks);
-	}
-
-	xstrcat(buffer, "}");
-
-	if (rc == SLURM_SUCCESS) {
-		if (_index_job(buffer) == SLURM_ERROR) {
-			slurm_mutex_lock(&pend_jobs_lock);
-			_push_pending_job(buffer);
-			slurm_mutex_unlock(&pend_jobs_lock);
-			rc = _save_state();
-		} else {
-			rc = _index_retry();
-		}
-	}
-
-	xfree(tmp);
-	xfree(buffer);
+	if (rc == SLURM_SUCCESS)
+		list_enqueue(jobslist, json_job);
 
 	return rc;
 }
