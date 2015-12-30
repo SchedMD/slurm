3300184a5c5b77e58f36ed74bae401b18a0bda5c
David Bigagli <david@schedmd.com>
2015-12-30 12:54:38 +0100

    Add missing \n

--- contribs/openlava/bjobs.pl
+++ contribs/openlava/bjobs.pl
@@ -92,7 +92,7 @@ sub _print_job_brief
 	_shrink_char(\$job->{'nodes'}, 11);
 	_shrink_char(\$job->{'name'}, 10);
 
-	printf("\n%-7.7s %-7.7s %-5.5s %-10.10s %-11.11s %-11.11s %-10s %-12.12s",
+	printf("\n%-7.7s %-7.7s %-5.5s %-10.10s %-11.11s %-11.11s %-10s %-12.12s\n",
 	       $job->{'job_id'}, $job->{'user_name'},
 	       $job->{'job_state_str'},
 	       $job->{'partition'}, $job->{'alloc_node'}, $job->{'nodes'},
