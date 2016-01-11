2323188e70fcc66e6465c84cf4b7a6e92a942c32
Morris Jette <jette@schedmd.com>
2016-01-11 09:14:40 -0800

    burst_cray/cray: Eliminate need for dedicated nodes
    The restriction from Cray has been lifted.
    bug 2317

--- src/plugins/burst_buffer/cray/burst_buffer_cray.c
+++ src/plugins/burst_buffer/cray/burst_buffer_cray.c
@@ -2811,7 +2811,6 @@ extern int bb_p_job_validate(struct job_descriptor *job_desc,
 		}
 	}
 
-	job_desc->shared = 0;	/* Compute nodes can not be shared */
 	job_desc->tres_req_cnt[bb_state.tres_pos] = bb_size / (1024 * 1024);
 
 fini:	pthread_mutex_unlock(&bb_state.bb_mutex);
