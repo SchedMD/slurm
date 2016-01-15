9fd86e7758fedd59108202c762232c477832b635
Brian Christiansen <brian@schedmd.com>
2016-01-14 16:23:29 -0800

    Fix nodes from being overallocated when allocation straddles multiple nodes.
    Bug 2343

--- src/slurmctld/step_mgr.c
+++ src/slurmctld/step_mgr.c
@@ -2631,8 +2631,10 @@ extern slurm_step_layout_t *step_layout_create(struct step_record *step_ptr,
 			if ((job_resrcs_ptr->whole_node != 1)
 			    && (slurmctld_conf.select_type_param
 				& (CR_CORE | CR_SOCKET))
-			    && (job_ptr->details->cpu_bind_type
-				& CPU_BIND_ONE_THREAD_PER_CORE)) {
+			    && ((job_ptr->details->cpu_bind_type !=
+				 (uint16_t)NO_VAL)
+				&& (job_ptr->details->cpu_bind_type
+				    & CPU_BIND_ONE_THREAD_PER_CORE))) {
 				uint16_t threads;
 				if (slurmctld_conf.fast_schedule)
 					threads = node_ptr->config_ptr->threads;
