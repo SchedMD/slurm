SchedMD/slurm

commit a801d2642882ee24a501394b93720d198ec9cd21

Don't relocated multi-node core reservations
Do not automatically relocate an advanced reservation for individual cores
    that spans multiple nodes when nodes in that reservation go down (e.g.
    a 1 core reservation on node "tux1" will be moved if node "tux1" goes
    down, but a reservation containing 2 cores on node "tux1" and 3 cores on
    "tux2" will not be moved node "tux1" goes down). Advanced reservations for
    whole nodes will be moved by default for down nodes.
bug 2326

src/slurmctld/reservation.c
@@ -3269,10 +3269,13 @@ static void _resv_node_replace(slurmctld_resv_t *resv_ptr)
 		resv_desc.start_time  = resv_ptr->start_time;
 		resv_desc.end_time    = resv_ptr->end_time;
 		resv_desc.features    = resv_ptr->features;
+		resv_desc.core_cnt    = xmalloc(sizeof(uint32_t) * 2);
+		resv_desc.core_cnt[0] = resv_ptr->core_cnt;
 		resv_desc.node_cnt    = xmalloc(sizeof(uint32_t) * 2);
 		resv_desc.node_cnt[0] = add_nodes;
 		i = _select_nodes(&resv_desc, &resv_ptr->part_ptr, &new_bitmap,
 				  &core_bitmap);
+		xfree(resv_desc.core_cnt);
 		xfree(resv_desc.node_cnt);
 		xfree(resv_desc.node_list);
 		xfree(resv_desc.partition);
@@ -3328,6 +3331,7 @@ static void _validate_node_choice(slurmctld_resv_t *resv_ptr)
 	resv_desc_msg_t resv_desc;
 
 	if ((resv_ptr->node_bitmap == NULL) ||
+	    (!resv_ptr->full_nodes && (resv_ptr->node_cnt > 1)) ||
 	    (resv_ptr->flags & RESERVE_FLAG_SPEC_NODES) ||
 	    (resv_ptr->flags & RESERVE_FLAG_STATIC))
 		return;
@@ -3349,10 +3353,13 @@ static void _validate_node_choice(slurmctld_resv_t *resv_ptr)
 	resv_desc.start_time = resv_ptr->start_time;
 	resv_desc.end_time   = resv_ptr->end_time;
 	resv_desc.features   = resv_ptr->features;
+	resv_desc.core_cnt   = xmalloc(sizeof(uint32_t) * 2);
+	resv_desc.core_cnt[0]= resv_ptr->core_cnt;
 	resv_desc.node_cnt   = xmalloc(sizeof(uint32_t) * 2);
 	resv_desc.node_cnt[0]= resv_ptr->node_cnt - i;
 	i = _select_nodes(&resv_desc, &resv_ptr->part_ptr, &tmp_bitmap,
 			  &core_bitmap);
+	xfree(resv_desc.core_cnt);
 	xfree(resv_desc.node_cnt);
 	xfree(resv_desc.node_list);
 	xfree(resv_desc.partition);
@@ -3685,8 +3692,8 @@ static int  _select_nodes(resv_desc_msg_t *resv_desc_ptr,
 			    (end_relative   <= resv_desc_ptr->start_time))
 				continue;
 			if (!resv_ptr->core_bitmap && !resv_ptr->full_nodes) {
-				error("Reservation has no core_bitmap and "
-				      "full_nodes is zero");
+				error("Reservation %s has no core_bitmap and "
+				      "full_nodes is zero", resv_ptr->name);
 				resv_ptr->full_nodes = 1;
 			}
 			if (resv_ptr->full_nodes || !resv_desc_ptr->core_cnt) {
@@ -4246,6 +4253,7 @@ extern void job_claim_resv(struct job_record *job_ptr)
 	resv_ptr = (slurmctld_resv_t *) list_find_first (resv_list,
 			_find_resv_name, job_ptr->resv_name);
 	if (!resv_ptr ||
+	    (!resv_ptr->full_nodes && (resv_ptr->node_cnt > 1)) ||
 	    !(resv_ptr->flags & RESERVE_FLAG_REPLACE) ||
 	    (resv_ptr->flags & RESERVE_FLAG_SPEC_NODES) ||
 	    (resv_ptr->flags & RESERVE_FLAG_STATIC))

doc/html/reservations.shtml
@@ -358,7 +358,11 @@ $ scontrol create reservation user=operator nodes=tux8 \
 to jobs are automatically replaced with new idle nodes.
 The effect of this is to always maintain a constant size pool of resources.
 This is accomplished by using a "replace" flag as shown in the example below.
-This option is not supported on IBM Bluegene systems.</p>
+This option is not supported on IBM Bluegene systems or for reservations
+of individual cores which span more than one node rather than full nodes 
+(e.g. a 1 core reservation on node "tux1" will be moved if node "tux1" goes
+down, but a reservation containing 2 cores on node "tux1" and 3 cores on "tux2"
+will not be moved node "tux1" goes down).</p>
 <pre>
 $ scontrol create reservation starttime=now duration=60 \
   users=foo nodecnt=2 flags=replace
@@ -422,7 +426,7 @@ considering the initiation of jobs.
 This will prevent the initiation of some jobs which would complete execution
 before a reservation given fewer jobs to time-slice with.</p>
 
-<p style="text-align: center;">Last modified 20 October 2015</p>
+<p style="text-align: center;">Last modified 28 January 2016</p>
 
 <!--#include virtual="footer.txt"-->
 

commit 31ac01ce9fe25156bda8177f7dd7875ff8c35f9d

reservation relocation fix
This fixes a bug introduced in commit a801d2642882ee24a501394b93720d198ec9cd21
Whole node resource allocations with REPLACE option were not working.
Detected by test3.14 failure.

src/slurmctld/reservation.c
@@ -3269,8 +3269,10 @@ static void _resv_node_replace(slurmctld_resv_t *resv_ptr)
 		resv_desc.start_time  = resv_ptr->start_time;
 		resv_desc.end_time    = resv_ptr->end_time;
 		resv_desc.features    = resv_ptr->features;
-		resv_desc.core_cnt    = xmalloc(sizeof(uint32_t) * 2);
-		resv_desc.core_cnt[0] = resv_ptr->core_cnt;
+		if (!resv_ptr->full_nodes) {
+			resv_desc.core_cnt    = xmalloc(sizeof(uint32_t) * 2);
+			resv_desc.core_cnt[0] = resv_ptr->core_cnt;
+		}
 		resv_desc.node_cnt    = xmalloc(sizeof(uint32_t) * 2);
 		resv_desc.node_cnt[0] = add_nodes;
 		i = _select_nodes(&resv_desc, &resv_ptr->part_ptr, &new_bitmap,
@@ -3353,8 +3355,10 @@ static void _validate_node_choice(slurmctld_resv_t *resv_ptr)
 	resv_desc.start_time = resv_ptr->start_time;
 	resv_desc.end_time   = resv_ptr->end_time;
 	resv_desc.features   = resv_ptr->features;
-	resv_desc.core_cnt   = xmalloc(sizeof(uint32_t) * 2);
-	resv_desc.core_cnt[0]= resv_ptr->core_cnt;
+	if (!resv_ptr->full_nodes) {
+		resv_desc.core_cnt    = xmalloc(sizeof(uint32_t) * 2);
+		resv_desc.core_cnt[0] = resv_ptr->core_cnt;
+	}
 	resv_desc.node_cnt   = xmalloc(sizeof(uint32_t) * 2);
 	resv_desc.node_cnt[0]= resv_ptr->node_cnt - i;
 	i = _select_nodes(&resv_desc, &resv_ptr->part_ptr, &tmp_bitmap,