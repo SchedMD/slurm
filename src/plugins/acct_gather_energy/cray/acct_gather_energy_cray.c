SchedMD/slurm

commit c6771b1e630a3aa65ffc931419a37567d0b44a8b

Prevent segfault in acct_gather_energy_cray if data requested before set.
Match acct_gather_energy/rapl plugin. Bug 2397.

src/plugins/acct_gather_energy/cray/acct_gather_energy_cray.c
@@ -274,6 +274,12 @@ extern int acct_gather_energy_p_get_data(enum acct_energy_type data_type,
 
 	xassert(_run_in_daemon());
 
+	if (!local_energy) {
+		debug("%s: trying to get data %d, but no local_energy yet.",
+		      __func__, data_type);
+		acct_gather_energy_p_conf_set(NULL);
+	}
+
 	switch (data_type) {
 	case ENERGY_DATA_JOULES_TASK:
 	case ENERGY_DATA_NODE_ENERGY_UP:
@@ -336,6 +342,10 @@ extern void acct_gather_energy_p_conf_set(s_p_hashtbl_t *tbl)
 	static bool flag_init = 0;
 
 	if (!_run_in_daemon())
+		return;
+
+	/* Already been here, we shouldn't need to visit again */
+	if (local_energy)
 		return;
 
 	if (!flag_init) {

commit 1a2e481966ae92a066038a08246018c98ecd5c2f

Fix minor regression from commit c6771b1e6.

src/plugins/acct_gather_energy/cray/acct_gather_energy_cray.c
@@ -83,6 +83,8 @@ enum {
 	GET_POWER
 };
 
+extern void acct_gather_energy_p_conf_set(s_p_hashtbl_t *tbl);
+
 static uint32_t _get_latest_stats(int type)
 {
 	uint32_t data = 0;