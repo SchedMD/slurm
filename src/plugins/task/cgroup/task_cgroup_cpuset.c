efa83a021ad0021992edf8a3c9f72c3b73db805b
Morris Jette <jette@schedmd.com>
2016-03-23 12:38:48 -0700

    task/cgroup: Fix for task binding anomaly
    Here's how to reproduce on smd-server with 2 sockets, 6 cores per
    socket and 2 threads per core, just run the following command line
    3 times in quick succession (all active at the same time):
    srun --cpus-per-task=4 -m block sleep 30
    What was happening is the first job would be allocated cores 0+1
    The second job would be allocated cores 2+3
    The thrid job would test use of cores 0-3 then exit because the
     job only needs 4 CPUs. The resulting core binding would include
     NO CPUs. The new logic tests that the core being considered for
     use actually has some resources available to the job before
     updating the counter which is being tested against the needed
     CPU counter.

--- src/plugins/task/cgroup/task_cgroup_cpuset.c
+++ src/plugins/task/cgroup/task_cgroup_cpuset.c
@@ -636,6 +636,24 @@ static void _add_hwloc_cpuset(
 	}
 }
 
+static int _hwloc_bit_count(hwloc_bitmap_t cpuset)
+{
+	int i_first, i_last, i, cnt = 0;
+
+	if (!cpuset)
+		return cnt;
+	i_first = hwloc_bitmap_first(cpuset);
+	if (i_first < 0)
+		return cnt;
+	cnt = 1;	/* For bit set at i_first */
+	i_last = hwloc_bitmap_last(cpuset);
+	for (i = i_first + 1; i <= i_last; i++) {
+		if (hwloc_bitmap_isset(cpuset, i))
+			cnt++;
+	}
+	return cnt;
+}
+
 static int _task_cgroup_cpuset_dist_cyclic(
 	hwloc_topology_t topology, hwloc_obj_type_t hwtype,
 	hwloc_obj_type_t req_hwtype, stepd_step_rec_t *job, int bind_verbose,
@@ -648,7 +666,7 @@ static int _task_cgroup_cpuset_dist_cyclic(
 	uint32_t *t_ix;		/* thread index by core by socket */
 	uint32_t npus, ncores, nsockets;
 	uint32_t taskid = job->envtp->localid;
-	int spec_thread_cnt = 0;
+	int spec_thread_cnt = 0, pu_cnt;
 	bitstr_t *spec_threads = NULL;
 
 	uint32_t obj_idxs[3], nthreads, cps,
@@ -736,6 +754,7 @@ static int _task_cgroup_cpuset_dist_cyclic(
 				topology, HWLOC_OBJ_SOCKET, s_ix,
 				hwtype, c_ixc[s_ix]);
 			if (obj != NULL) {
+				pu_cnt = _hwloc_bit_count(obj->allowed_cpuset);
 				if (hwloc_compare_types(hwtype, HWLOC_OBJ_PU)
 									>= 0) {
 					/* granularity is thread */
@@ -768,10 +787,15 @@ static int _task_cgroup_cpuset_dist_cyclic(
 						if (c_ixc[s_ix] == cps)
 							s_ix++;
 					}
+				} else if (pu_cnt < 1) {
+					/* No CPUs available on this core */
+					c_ixc[s_ix]++;
+					if (c_ixc[s_ix] == cps)
+						s_ix++;
 				} else {
 					/* granularity is core or larger */
 					c_ixc[s_ix]++;
-					j++;
+					j += pu_cnt;
 					if (i == ntskip)
 						_add_hwloc_cpuset(hwtype,
 							req_hwtype, obj, taskid,
@@ -785,7 +809,7 @@ static int _task_cgroup_cpuset_dist_cyclic(
 		/* if it succeeds, switch to the next task, starting
 		 * with the next available socket, otherwise, loop back
 		 * from the first socket trying to find available slots. */
-		if (j == npdist) {
+		if (j >= npdist) {
 			i++;
 			j = 0;
 			s_ix++; // no validity check, handled by the while
@@ -829,7 +853,7 @@ static int _task_cgroup_cpuset_dist_block(
 	uint32_t taskid = job->envtp->localid;
 	int hwdepth;
 	uint32_t npus, ncores, nsockets;
-	int spec_thread_cnt = 0;
+	int spec_thread_cnt = 0, pu_cnt;
 	bitstr_t *spec_threads = NULL;
 
 	uint32_t core_idx;
@@ -867,6 +891,10 @@ static int _task_cgroup_cpuset_dist_block(
 					hwtype, thread_idx[core_idx]);
 				if (obj != NULL) {
 					thread_idx[core_idx]++;
+					pu_cnt = _hwloc_bit_count(
+							obj->allowed_cpuset);
+					if (pu_cnt < 1)     /* No avail CPUs */
+						continue;
 					j++;
 					if (i == ntskip)
 						_add_hwloc_cpuset(hwtype,
