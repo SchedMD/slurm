d8b18ff8dc6f2237db3729f4ab085b003e54c89e
Morris Jette <jette@schedmd.com>
2016-04-05 12:55:50 -0700

    Fix backfill scheduler race condition
    Fix backfill scheduler race condition that could cause invalid pointer in
        select/cons_res plugin. Bug introduced in 15.08.9, commit:
        efd9d35eb3ab2c75665beff05a74294b27d94f75
    
    The scenario is as follows
    1. Backfill scheduler is running, then releases locks
    2. Main scheduling loop starts a job "A"
    3. Backfill scheduler resumes, finds job "A" in its queue and
       resets it's partition pointer.
    4. Job "A" completes and tries to remove resource allocation record
       from select/cons_res data structure, but fails to find it because
       it is looking in the table for the wrong partition.
    5. Job "A" record gets purged from slurmctld
    6. Select/cons_res plugin attempts to operate on resource allocation
       data structure, finds pointer into the now purged data structure
       of job "A" and aborts or gets SEGV
    Bug 2603

--- src/plugins/sched/backfill/backfill.c
+++ src/plugins/sched/backfill/backfill.c
@@ -962,8 +962,6 @@ static int _attempt_backfill(void)
 		}
 
 		job_ptr  = job_queue_rec->job_ptr;
-		part_ptr = job_queue_rec->part_ptr;
-		job_ptr->part_ptr = part_ptr;
 
 		/* With bf_continue configured, the original job could have
 		 * been cancelled and purged. Validate pointer here. */
@@ -980,7 +978,12 @@ static int _attempt_backfill(void)
 			if (!job_ptr)	/* All task array elements started */
 				continue;
 		}
+		if (!IS_JOB_PENDING(job_ptr) ||	/* Started in other partition */
+		    (job_ptr->priority == 0))	/* Job has been held */
+			continue;
 
+		part_ptr = job_queue_rec->part_ptr;
+		job_ptr->part_ptr = part_ptr;
 		if (job_ptr->state_reason == FAIL_ACCOUNT) {
 			slurmdb_assoc_rec_t assoc_rec;
 			memset(&assoc_rec, 0, sizeof(slurmdb_assoc_rec_t));
@@ -1255,6 +1258,7 @@ next_task:
 				continue;	/* No available frontend */
 
 			job_ptr->time_limit = save_time_limit;
+			job_ptr->part_ptr = part_ptr;
 			/* Reset backfill scheduling timers, resume testing */
 			sched_start = time(NULL);
 			gettimeofday(&start_tv, NULL);
