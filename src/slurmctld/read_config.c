575912661569a92f70dab047b71ae9a338d84e94
Morris Jette <jette@schedmd.com>
2016-04-13 12:33:25 -0700

    Preserve node CPU Load on reconfig
    A node's CPULoad was getting reset to NoVal on reconfiguration.
    This change preserves the old value, which can fix a possible
    race condition with respect to test4.13

--- src/slurmctld/read_config.c
+++ src/slurmctld/read_config.c
@@ -1466,6 +1466,7 @@ static int _restore_node_state(int recover,
 
 		node_ptr->last_response = old_node_ptr->last_response;
 		node_ptr->protocol_version = old_node_ptr->protocol_version;
+		node_ptr->cpu_load = old_node_ptr->cpu_load;
 
 		/* make sure we get the old state from the select
 		 * plugin, just swap it out to avoid possible memory leak */
