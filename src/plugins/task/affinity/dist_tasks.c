SchedMD/slurm

commit 31aa3244b55bcf6fafe8d76a2c3b8047afeac6e3

Add support to ntasks_per_socket in task/affinity
Prior logic only supported ntasks_per_core
bug 2655

src/plugins/task/affinity/dist_tasks.c
@@ -1103,6 +1103,7 @@ static int _task_layout_lllp_block(launch_tasks_request_msg_t *req,
 	int max_cpus = max_tasks * req->cpus_per_task;
 	bitstr_t *avail_map;
 	bitstr_t **masks = NULL;
+	int sock_inx, pu_per_socket, *socket_tasks = NULL;
 
 	info("_task_layout_lllp_block ");
 
@@ -1137,6 +1138,9 @@ static int _task_layout_lllp_block(launch_tasks_request_msg_t *req,
 	*masks_p = xmalloc(max_tasks * sizeof(bitstr_t*));
 	masks = *masks_p;
 
+	pu_per_socket = hw_cores * hw_threads;
+	socket_tasks = xmalloc(hw_sockets);
+
 	/* block distribution with oversubsciption */
 	c = 0;
 	while (taskcount < max_tasks) {
@@ -1151,6 +1155,11 @@ static int _task_layout_lllp_block(launch_tasks_request_msg_t *req,
 			/* skip unavailable resources */
 			if (bit_test(avail_map, i) == 0)
 				continue;
+
+			sock_inx = i / pu_per_socket;
+			if (socket_tasks[sock_inx] >= req->ntasks_per_socket)
+				continue;  /* Default ntasks_per_socket = INFINITE */
+			socket_tasks[sock_inx]++;
 
 			if (!masks[taskcount])
 				masks[taskcount] = bit_alloc(
@@ -1188,6 +1197,7 @@ static int _task_layout_lllp_block(launch_tasks_request_msg_t *req,
 				break;
 		}
 	}
+	xfree(socket_tasks);
 
 	/* last step: expand the masks to bind each task
 	 * to the requested resource */

commit c31ee795f7059edcdc42355ed84ed4f6c91a8e1d

Correction to task binding
This fixes a problem introduced in commit
31aa3244b55bcf6fafe8d76a2c3b8047afeac6e3
which was causing test1.91 to generate a fatal slurmd error.

src/plugins/task/affinity/dist_tasks.c
@@ -1157,8 +1157,9 @@ static int _task_layout_lllp_block(launch_tasks_request_msg_t *req,
 				continue;
 
 			sock_inx = i / pu_per_socket;
-			if (socket_tasks[sock_inx] >= req->ntasks_per_socket)
-				continue;  /* Default ntasks_per_socket = INFINITE */
+			if ((req->ntasks_per_socket != 0) &&
+			    (socket_tasks[sock_inx] >= req->ntasks_per_socket))
+				continue;
 			socket_tasks[sock_inx]++;
 
 			if (!masks[taskcount])
@@ -1196,6 +1197,8 @@ static int _task_layout_lllp_block(launch_tasks_request_msg_t *req,
 			if (++taskcount >= max_tasks)
 				break;
 		}
+		for (i = 0; i < hw_sockets; i++)
+			socket_tasks[i] = 0;
 	}
 	xfree(socket_tasks);
 