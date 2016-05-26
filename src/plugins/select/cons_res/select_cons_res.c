SchedMD/slurm

commit 21c52d2f61e8086209d0c4d18f4700c07588ead9

Properly track resources for suspended jobs on reconfig
Properly account for memory, CPUs and GRES when slurmctld is reconfigured
    while there is a suspended job. Previous logic would add the CPUs, but not
    memory or GPUs. This would result in underflow/overflow errors in select
    cons_res plugin.
bug 2353

src/plugins/select/cons_res/select_cons_res.c
@@ -778,8 +778,8 @@ static void _build_row_bitmaps(struct part_res_record *p_ptr,
  * - add 'struct job_resources' resources to 'struct part_res_record'
  * - add job's memory requirements to 'struct node_res_record'
  *
- * if action = 0 then add cores and memory (starting new job)
- * if action = 1 then only add memory (adding suspended job)
+ * if action = 0 then add cores, memory + GRES (starting new job)
+ * if action = 1 then add memory + GRES (adding suspended job)
  * if action = 2 then only add cores (suspended job is resumed)
  */
 static int _add_job_to_res(struct job_record *job_ptr, int action)
@@ -1125,8 +1125,8 @@ static int _job_expand(struct job_record *from_job_ptr,
  * - subtract 'struct job_resources' resources from 'struct part_res_record'
  * - subtract job's memory requirements from 'struct node_res_record'
  *
- * if action = 0 then subtract cores and memory (running job was terminated)
- * if action = 1 then only subtract memory (suspended job was terminated)
+ * if action = 0 then subtract cores, memory + GRES (running job was terminated)
+ * if action = 1 then subtract memory + GRES (suspended job was terminated)
  * if action = 2 then only subtract cores (job is suspended)
  *
  */
@@ -2481,10 +2481,12 @@ extern int select_p_select_nodeinfo_set(struct job_record *job_ptr)
 	xassert(job_ptr);
 	xassert(job_ptr->magic == JOB_MAGIC);
 
-	if (!IS_JOB_RUNNING(job_ptr) && !IS_JOB_SUSPENDED(job_ptr))
+	if (IS_JOB_RUNNING(job_ptr))
+		rc = _add_job_to_res(job_ptr, 0);
+	else if (IS_JOB_SUSPENDED(job_ptr))
+		rc = _add_job_to_res(job_ptr, 1);
+	else
 		return SLURM_SUCCESS;
-
-	rc = _add_job_to_res(job_ptr, 0);
 	gres_plugin_job_state_log(job_ptr->gres_list, job_ptr->job_id);
 
 	return rc;
@@ -2688,7 +2690,7 @@ extern int select_p_reconfigure(void)
 			_add_job_to_res(job_ptr, 0);
 		} else if (IS_JOB_SUSPENDED(job_ptr)) {
 			/* add the job in a suspended state */
-			_add_job_to_res(job_ptr, 2);
+			_add_job_to_res(job_ptr, 1);
 		}
 	}
 	list_iterator_destroy(job_iterator);

commit 4ce626789dbfff156254345362abb54ebda92784

Fix for tracking a node's allocated CPUs with gang scheduling.
This bug was introduced by commit 21c52d2f61e8086209d0c4d18f4700c07588ead9
which fixed a different problem tracking resources associated with suspended
jobs. There are subtle differences between jobs that are suspended by a
user/administrator and jobs suspended by gang scheduling which resulted in
undercounting allocated CPUs when a job suspended by gang scheduling
was active at the same time of a slurmctld reconfiguration request.
See bugs 2353 (original bug related to commit 21c52d2f61e8086209d0c4d18f4700c07588ead9
and bug 2765

src/plugins/select/cons_res/select_cons_res.c
@@ -2535,9 +2535,12 @@ extern int select_p_select_nodeinfo_set(struct job_record *job_ptr)
 
 	if (IS_JOB_RUNNING(job_ptr))
 		rc = _add_job_to_res(job_ptr, 0);
-	else if (IS_JOB_SUSPENDED(job_ptr))
-		rc = _add_job_to_res(job_ptr, 1);
-	else
+	else if (IS_JOB_SUSPENDED(job_ptr)) {
+		if (job_ptr->priority == 0)
+			rc = _add_job_to_res(job_ptr, 1);
+		else	/* Gang schedule suspend */
+			rc = _add_job_to_res(job_ptr, 0);
+	} else
 		return SLURM_SUCCESS;
 	gres_plugin_job_state_log(job_ptr->gres_list, job_ptr->job_id);
 
@@ -2743,7 +2746,10 @@ extern int select_p_reconfigure(void)
 			_add_job_to_res(job_ptr, 0);
 		} else if (IS_JOB_SUSPENDED(job_ptr)) {
 			/* add the job in a suspended state */
-			_add_job_to_res(job_ptr, 1);
+			if (job_ptr->priority == 0)
+				rc = _add_job_to_res(job_ptr, 1);
+			else	/* Gang schedule suspend */
+				rc = _add_job_to_res(job_ptr, 0);
 		} else if (_job_cleaning(job_ptr)) {
 			cleaning_job_cnt++;
 			run_time = (int) difftime(now, job_ptr->end_time);