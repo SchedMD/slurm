SchedMD/slurm

commit 2a8177345ceebba615245172d39df3d5ca79302c

Make it so --mail-type=NONE didn't throw an invalid error.

src/common/proc_args.c
@@ -981,7 +981,7 @@ bool verify_hint(const char *arg, int *min_sockets, int *min_cores,
 uint16_t parse_mail_type(const char *arg)
 {
 	char *buf, *tok, *save_ptr = NULL;
-	uint16_t rc = 0;
+	uint16_t rc = (uint16_t)INFINITE;
 
 	if (!arg)
 		return rc;

src/salloc/opt.c
@@ -1035,7 +1035,7 @@ void set_options(const int argc, char **argv)
 			break;
 		case LONG_OPT_MAIL_TYPE:
 			opt.mail_type |= parse_mail_type(optarg);
-			if (opt.mail_type == 0) {
+			if (opt.mail_type == (uint16_t)INFINITE) {
 				error("--mail-type=%s invalid", optarg);
 				exit(error_exit);
 			}

src/sbatch/opt.c
@@ -1502,7 +1502,7 @@ static void _set_options(int argc, char **argv)
 			break;
 		case LONG_OPT_MAIL_TYPE:
 			opt.mail_type |= parse_mail_type(optarg);
-			if (opt.mail_type == 0) {
+			if (opt.mail_type == (uint16_t)INFINITE) {
 				error("--mail-type=%s invalid", optarg);
 				exit(error_exit);
 			}
@@ -1889,7 +1889,7 @@ static void _set_pbs_options(int argc, char **argv)
 			break;
 		case 'm':
 			opt.mail_type |= _parse_pbs_mail_type(optarg);
-			if ((opt.mail_type == 0) && strcasecmp(optarg, "n")) {
+			if (opt.mail_type == (uint16_t)INFINITE) {
 				error("-m=%s invalid", optarg);
 				exit(error_exit);
 			}
@@ -2686,7 +2686,7 @@ static bool _opt_verify(void)
 
 static uint16_t _parse_pbs_mail_type(const char *arg)
 {
-	uint16_t rc =  0;
+	uint16_t rc = (uint16_t)INFINITE;
 
 	if (strchr(arg, 'b') || strchr(arg, 'B'))
 		rc |= MAIL_JOB_BEGIN;
@@ -2694,6 +2694,8 @@ static uint16_t _parse_pbs_mail_type(const char *arg)
 		rc |= MAIL_JOB_END;
 	if (strchr(arg, 'a') || strchr(arg, 'A'))
 		rc |= MAIL_JOB_FAIL;
+	if (strchr(arg, 'n') || strchr(arg, 'N'))
+		rc = 0;
 
 	return rc;
 }

src/srun/libsrun/opt.c
@@ -1474,7 +1474,7 @@ static void _set_options(const int argc, char **argv)
 			break;
 		case LONG_OPT_MAIL_TYPE:
 			opt.mail_type |= parse_mail_type(optarg);
-			if (opt.mail_type == 0) {
+			if (opt.mail_type == (uint16_t)INFINITE) {
 				error("--mail-type=%s invalid", optarg);
 				exit(error_exit);
 			}

commit 7d90f534ce1e1c1e92508b492cf9b0a060c59cc2

Continuation of commit 2a8177345ce which broke functionality.

src/common/proc_args.c
@@ -981,16 +981,18 @@ bool verify_hint(const char *arg, int *min_sockets, int *min_cores,
 uint16_t parse_mail_type(const char *arg)
 {
 	char *buf, *tok, *save_ptr = NULL;
-	uint16_t rc = (uint16_t)INFINITE;
+	uint16_t rc = 0;
+	bool none_set = false;
 
 	if (!arg)
-		return rc;
+		return (uint16_t)INFINITE;
 
 	buf = xstrdup(arg);
 	tok = strtok_r(buf, ",", &save_ptr);
 	while (tok) {
 		if (strcasecmp(tok, "NONE") == 0) {
 			rc = 0;
+			none_set = true;
 			break;
 		} else if (strcasecmp(tok, "BEGIN") == 0)
 			rc |= MAIL_JOB_BEGIN;
@@ -1016,6 +1018,8 @@ uint16_t parse_mail_type(const char *arg)
 		tok = strtok_r(NULL, ",", &save_ptr);
 	}
 	xfree(buf);
+	if (!rc && !none_set)
+		rc = (uint16_t)INFINITE;
 
 	return rc;
 }