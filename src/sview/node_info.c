dbfbcfe96d094bfda6fcbbf228905bc05367d6b9
Tim Wickberg <tim@schedmd.com>
2016-06-02 16:59:39 -0400

    Print correct return code on failure to update node features through sview
    Wrong order of operations results in the return code being 0/1.

--- src/sview/node_info.c
+++ src/sview/node_info.c
@@ -1173,7 +1173,7 @@ extern int update_features_node(GtkDialog *dialog, const char *nodelist,
 			g_free(edit);
 			goto end_it;
 		}
-		if ((rc = slurm_update_node(node_msg) == SLURM_SUCCESS)) {
+		if ((rc = slurm_update_node(node_msg)) == SLURM_SUCCESS) {
 			edit = g_strdup_printf(
 				"Node(s) %s updated successfully.",
 				nodelist);
