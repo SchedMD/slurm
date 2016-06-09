SchedMD/slurm

commit addb3574984806eae82fed29beb155bfc6137117

`qstat -u` should return zero even with no records printed
Match behavior of other PBS-like resource managers. Bug 2330.

contribs/torque/qstat.pl
@@ -227,8 +227,8 @@ if(defined($queueList)) {
 			print_job_brief($job, $line);
 			$line++;
 		}
-		$rc = 0;
 	}
+	$rc = 0;
 }
 
 # Exit with status code

commit 7a042d3308f81b6d8cbdea5d5f1aec85c06458df

Fix qstat wrapper script to return 153 when no results found.
Except return 0 if using the -u argument. Bug introduced by addb35749,
fix bug 2330 differently while solving bug 2807.

configure
@@ -20214,9 +20214,10 @@ char *malloc ();
 int
 main ()
 {
-return ! malloc (0);
-  ;
-  return 0;
+char *a = malloc(0);
+int b = !a;
+free(a);
+return b;
 }
 _ACEOF
 if ac_fn_c_try_run "$LINENO"; then :

contribs/torque/qstat.pl
@@ -227,8 +227,13 @@ if(defined($queueList)) {
 			print_job_brief($job, $line);
 			$line++;
 		}
+		$rc = 0;
 	}
-	$rc = 0;
+
+	# return 0 even if no records printed when using -u flag
+	if (@userIds) {
+		$rc = 0;
+	}
 }
 
 # Exit with status code