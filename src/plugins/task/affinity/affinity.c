01b658a386e80c66396a1a32e59aec5cb6ea6c83
Jason Bacon <bacon4000@gmail.com>
2016-06-10 10:56:18 -0700

    Fix task/affinity plugin for FreeBSD

--- src/plugins/task/affinity/affinity.c
+++ src/plugins/task/affinity/affinity.c
@@ -290,12 +290,34 @@ int get_cpuset(cpu_set_t *mask, stepd_step_rec_t *job)
 	return false;
 }
 
+/* For sysctl() functions */
+#if defined(__FreeBSD__) || defined(__NetBSD__)
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#endif
+
+#define	BUFFLEN	127
+
 /* Return true if Power7 processor */
 static bool _is_power_cpu(void)
 {
 	if (is_power == -1) {
+#if defined(__FreeBSD__) || defined(__NetBSD__)
+
+		char    buffer[BUFFLEN+1];
+		size_t  len = BUFFLEN;
+    
+		if ( sysctlbyname("hw.model", buffer, &len, NULL, 0) == 0 )
+		    is_power = ( strstr(buffer, "POWER7") != NULL );
+		else {
+		    error("_get_is_power: sysctl could not retrieve hw.model");
+		    return false;
+		}
+
+#elif defined(__linux__)
+
 		FILE *cpu_info_file;
-		char buffer[128];
+		char buffer[BUFFLEN+1];
 		char* _cpuinfo_path = "/proc/cpuinfo";
 		cpu_info_file = fopen(_cpuinfo_path, "r");
 		if (cpu_info_file == NULL) {
@@ -312,6 +334,14 @@ static bool _is_power_cpu(void)
 			}
 		}
 		fclose(cpu_info_file);
+
+#else
+
+/* Assuming other platforms don't support sysctlbyname() or /proc/cpuinfo */
+#warning	"Power7 check not implemented for this platform."
+	is_power = 0;
+
+#endif
 	}
 
 	if (is_power == 1)
@@ -335,7 +365,11 @@ void reset_cpuset(cpu_set_t *new_mask, cpu_set_t *cur_mask)
 	if (slurm_getaffinity(1, sizeof(full_mask), &full_mask)) {
 		/* Try to get full CPU mask from process init */
 		CPU_ZERO(&full_mask);
+#ifdef __FreeBSD__
+		CPU_OR(&full_mask, cur_mask);
+#else
 		CPU_OR(&full_mask, &full_mask, cur_mask);
+#endif
 	}
 	CPU_ZERO(&newer_mask);
 	for (cur_offset = 0; cur_offset < CPU_SETSIZE; cur_offset++) {
@@ -360,7 +394,10 @@ int slurm_setaffinity(pid_t pid, size_t size, const cpu_set_t *mask)
 	int rval;
 	char mstr[1 + CPU_SETSIZE / 4];
 
-#ifdef SCHED_GETAFFINITY_THREE_ARGS
+#ifdef __FreeBSD__
+        rval = cpuset_setaffinity(CPU_LEVEL_WHICH, CPU_WHICH_PID,
+				pid, size, mask);
+#elif SCHED_GETAFFINITY_THREE_ARGS
 	rval = sched_setaffinity(pid, size, mask);
 #else
 	rval = sched_setaffinity(pid, mask);
@@ -378,7 +415,19 @@ int slurm_getaffinity(pid_t pid, size_t size, cpu_set_t *mask)
 	char mstr[1 + CPU_SETSIZE / 4];
 
 	CPU_ZERO(mask);
-#ifdef SCHED_GETAFFINITY_THREE_ARGS
+
+	/*
+	 * The FreeBSD cpuset API is a superset of the Linux API.
+	 * In addition to PIDs, it supports threads, interrupts,
+	 * jails, and potentially other objects.  The first two arguments
+	 * to cpuset_*etaffinity() below indicate that the third argument
+	 * is a PID.  -1 indicates the PID of the calling process.
+	 * Linux sched_*etaffinity() uses 0 for this.
+	 */
+#ifdef __FreeBSD__
+        rval = cpuset_getaffinity(CPU_LEVEL_WHICH, CPU_WHICH_PID,
+				pid, size, mask);
+#elif SCHED_GETAFFINITY_THREE_ARGS
 	rval = sched_getaffinity(pid, size, mask);
 #else
 	rval = sched_getaffinity(pid, mask);
--- src/plugins/task/affinity/affinity.h
+++ src/plugins/task/affinity/affinity.h
@@ -32,6 +32,18 @@
  *  with SLURM; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
+
+/*
+ * FreeBSD and Linux affinity functions have a slightly different interface
+ * and are defined in different headers.  See platform-dependencies in
+ * affinity.c.
+ */
+#ifdef __FreeBSD__
+#include <sys/param.h>
+#include <sys/cpuset.h>
+typedef cpuset_t cpu_set_t;
+#endif
+
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
 #endif
