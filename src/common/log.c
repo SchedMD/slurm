e4079afbc7e230cb93b789707e943b31c77530e4
Brian Christiansen <brian@schedmd.com>
2016-06-30 11:35:59 -0600

    Use more compatible function to get thread name
    prctld(PR_GET_NAME) >= 2.6.11

--- src/common/log.c
+++ src/common/log.c
@@ -54,6 +54,10 @@
 #  include "config.h"
 #endif
 
+#if HAVE_SYS_PRCTL_H
+#  include <sys/prctl.h>
+#endif
+
 #include <stdio.h>
 
 #if HAVE_STRING_H
@@ -686,14 +690,14 @@ set_idbuf(char *idbuf)
 	int max_len = 12; /* handles current longest thread name */
 
 	gettimeofday(&now, NULL);
-#ifdef GLIB_PTHREAD_GETNAME
-	if (pthread_getname_np(pthread_self(), thread_name, NAMELEN)) {
+#if HAVE_SYS_PRCTL_H
+	if (prctl(PR_GET_NAME, thread_name, NULL, NULL, NULL) < 0) {
 		error("failed to get thread name: %m");
-		return;
+		max_len = 0;
+		thread_name[0] = '\0';
 	}
 #else
-	/* pthread_getname_np is a non-standard extension
-	 * skip printing thread name if not available */
+	/* skip printing thread name if not available */
 	max_len = 0;
 	thread_name[0] = '\0';
 #endif
