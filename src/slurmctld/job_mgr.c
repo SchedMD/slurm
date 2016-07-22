SchedMD/slurm

commit 05eac196147b84b83e297065147adc09e3afde16

Don't allow QOS to be changed on running jobs.
Bug 2386

src/slurmctld/job_mgr.c
@@ -10294,7 +10294,7 @@ static int _update_job(struct job_record *job_ptr, job_desc_msg_t * job_specs,
 	/* NOTE: Update QOS before updating partition in order to enforce
 	 * AllowQOS and DenyQOS partition configuration options */
 	if (job_specs->qos) {
-		if (!authorized && !IS_JOB_PENDING(job_ptr))
+		if (!authorized || !IS_JOB_PENDING(job_ptr))
 			error_code = ESLURM_JOB_NOT_PENDING;
 		else {
 			slurmdb_qos_rec_t qos_rec;

commit 22171a3a6c1887d9898792f015e87d5956c1720e

Fix to allow users to update QOS on pending jobs.
Inadvertently broken in commit 05eac196147b84.

Bug 2912.

src/slurmctld/job_mgr.c
@@ -10315,7 +10315,7 @@ static int _update_job(struct job_record *job_ptr, job_desc_msg_t * job_specs,
 	/* NOTE: Update QOS before updating partition in order to enforce
 	 * AllowQOS and DenyQOS partition configuration options */
 	if (job_specs->qos) {
-		if (!authorized || !IS_JOB_PENDING(job_ptr))
+		if (!IS_JOB_PENDING(job_ptr))
 			error_code = ESLURM_JOB_NOT_PENDING;
 		else {
 			slurmdb_qos_rec_t qos_rec;