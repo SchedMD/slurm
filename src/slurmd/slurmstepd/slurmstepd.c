SchedMD/slurm

commit 03cf4a5d09f18ef3c3abf9767c6cb58b0f635e96

Lock slurmstepd in memory
If slurmstepd had been swapped out before upgrade happened it could
  easily lead to SIGBUS at any time after upgrade. Prevent that by
  mlocking it.
bug 2334

src/slurmd/slurmstepd/slurmstepd.c
@@ -157,6 +157,17 @@ main (int argc, char *argv[])
 	 * on STDERR_FILENO for us. */
 	dup2(STDERR_FILENO, STDOUT_FILENO);
 
+	/* slurmstepd is the only daemon that should survive upgrade. If it
+	 * had been swapped out before upgrade happened it could easily lead
+	 * to SIGBUS at any time after upgrade. Avoid that by locking it
+	 * in-memory. */
+#ifdef _POSIX_MEMLOCK
+	if (mlockall(MCL_FUTURE | MCL_CURRENT) < 0)
+		info("failed to mlock() slurmstepd pages: %m");
+#else
+	info("mlockall() system call does not appear to be available");
+#endif
+
 	/* This does most of the stdio setup, then launches all the tasks,
 	 * and blocks until the step is complete */
 	rc = job_manager(job);

commit 2edc7012d98402d76f7b63f32e454a6214739b5d

Add header include
Problem introduced in commit 03cf4a5d09f18ef3c3abf9767c6cb58b0f635e96

src/slurmd/slurmstepd/slurmstepd.c
@@ -42,9 +42,10 @@
 #  include "config.h"
 #endif
 
-#include <unistd.h>
-#include <stdlib.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #include "src/common/cpu_frequency.h"
 #include "src/common/gres.h"