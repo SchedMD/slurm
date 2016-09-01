c75da93a734acb5b0ed00569dbde40a42b692d8d
Morris Jette <jette@schedmd.com>
2016-09-01 14:28:52 -0600

    backfill scheduling partition/qos check
    sched/backfill - Check that a user's QOS is allowed to use a partition
        before trying to schedule resources on that partition for the job.
    bug 3039

--- src/plugins/sched/backfill/backfill.c
+++ src/plugins/sched/backfill/backfill.c
@@ -66,6 +66,7 @@
 #include <unistd.h>
 
 #include "slurm/slurm.h"
+#include "slurm/slurmdb.h"
 #include "slurm/slurm_errno.h"
 
 #include "src/common/assoc_mgr.h"
@@ -1094,6 +1095,15 @@ static int _attempt_backfill(void)
 				job_ptr->state_reason = WAIT_NO_REASON;
 				last_job_update = now;
 			}
+		}
+
+		qos_ptr = (slurmdb_qos_rec_t *)job_ptr->qos_ptr;
+		if (part_policy_valid_qos(job_ptr->part_ptr, qos_ptr) !=
+		    SLURM_SUCCESS) {
+			xfree(job_ptr->state_desc);
+			job_ptr->state_reason = WAIT_QOS;
+			last_job_update = now;
+			continue;
 		}
 
 		if (!acct_policy_job_runnable_state(job_ptr) &&
