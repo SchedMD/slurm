SchedMD/slurm

commit 844830d42eb2cfb07557752438432a506422d944

Fix FreeBSD build.

src/common/slurm_persist_conn.c
@@ -37,7 +37,10 @@
 
 #include <poll.h>
 #include <pthread.h>
+
+#if HAVE_SYS_PRCTL_H
 #include <sys/prctl.h>
+#endif
 
 #include "slurm/slurm_errno.h"
 #include "src/common/fd.h"

commit 0059928c971ca289bdc6a923497034ab123d52fd

Add config.h include to slurm_persist_conn.c for HAVE_SYS_PRCTL_H
Fixes build issue caused by 844830d42eb2.

src/common/slurm_persist_conn.c
@@ -35,6 +35,8 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
+#include "config.h"
+
 #include <poll.h>
 #include <pthread.h>
 
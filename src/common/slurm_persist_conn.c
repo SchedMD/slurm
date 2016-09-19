0059928c971ca289bdc6a923497034ab123d52fd
Tim Wickberg <tim@schedmd.com>
2016-09-19 19:36:12 -0400

    Add config.h include to slurm_persist_conn.c for HAVE_SYS_PRCTL_H
    Fixes build issue caused by 844830d42eb2.

--- src/common/slurm_persist_conn.c
+++ src/common/slurm_persist_conn.c
@@ -35,6 +35,8 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
+#include "config.h"
+
 #include <poll.h>
 #include <pthread.h>
 
