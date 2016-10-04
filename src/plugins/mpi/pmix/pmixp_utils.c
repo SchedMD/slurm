SchedMD/slurm

commit ee4a9776a3235f64e550f3a40bf58cab30c8bd86

mpi/pmix - improve the performance of point to point communication
Avoid using slurm_forward_data because it causes thread spawn that
introduces unwanted delays.

Bug 3102.

src/plugins/mpi/pmix/pmixp_utils.c
@@ -2,7 +2,7 @@
  **	pmix_utils.c - Various PMIx utility functions
  *****************************************************************************
  *  Copyright (C) 2014-2015 Artem Polyakov. All rights reserved.
- *  Copyright (C) 2015      Mellanox Technologies. All rights reserved.
+ *  Copyright (C) 2015-2016 Mellanox Technologies. All rights reserved.
  *  Written by Artem Polyakov <artpol84@gmail.com, artemp@mellanox.com>.
  *
  *  This file is part of SLURM, a resource management program.
@@ -300,6 +300,104 @@ int pmixp_stepd_send(char *nodelist, const char *address, char *data,
 		delay *= 2;
 	}
 	xfree(copy_of_nodelist);
+
+	return rc;
+}
+
+static int _pmix_p2p_send_core(char *nodename, const char *address, char *data,
+				uint32_t len)
+{
+	int rc, timeout;
+	slurm_msg_t msg;
+	forward_data_msg_t req;
+	List ret_list;
+	ret_data_info_t *ret_data_info = NULL;
+
+	slurm_msg_t_init(&msg);
+
+	PMIXP_DEBUG("nodelist=%s, address=%s, len=%u",
+			nodename, address, len);
+	req.address = address;
+	req.len = len;
+	req.data = (char *)data;
+
+	msg.msg_type = REQUEST_FORWARD_DATA;
+	msg.data = &req;
+
+	rc = slurm_conf_get_addr(nodename, &msg.address);
+	if (SLURM_ERROR == rc) {
+		PMIXP_ERROR("Can't find address for host "
+			    "%s, check slurm.conf", nodename);
+	}
+
+	timeout = slurm_get_msg_timeout() * 1000;
+	msg.forward.timeout = timeout;
+	msg.forward.cnt = 0;
+	msg.forward.nodelist = NULL;
+	ret_list = slurm_send_addr_recv_msgs(&msg, nodename,
+						timeout);
+	if (ret_list) {
+		int ret_cnt = list_count(ret_list);
+		if (0 == ret_cnt &&
+		    (errno != SLURM_COMMUNICATIONS_CONNECTION_ERROR)) {
+			PMIXP_ERROR("failed to send to %s, errno=%d",
+				    nodename, errno);
+			return SLURM_ERROR;
+		}
+	} else {
+		/* This should never happen (when this was
+		 * written slurm_send_addr_recv_msgs always
+		 * returned a list */
+		PMIXP_ERROR("No return list given from "
+			    "slurm_send_addr_recv_msgs spawned for %s",
+			    nodename);
+		return SLURM_ERROR;
+	}
+
+	rc = SLURM_SUCCESS;
+	while ((ret_data_info = list_pop(ret_list))) {
+		int temp_rc =0;
+		temp_rc = slurm_get_return_code(ret_data_info->type,
+					ret_data_info->data);
+		if (temp_rc != SLURM_SUCCESS) {
+			rc = temp_rc;
+		}
+		destroy_data_info(ret_data_info);
+	}
+
+	FREE_NULL_LIST(ret_list);
+
+	return rc;
+}
+
+int pmixp_p2p_send(char *nodename, const char *address, char *data,
+		     uint32_t len, unsigned int start_delay,
+		     unsigned int retry_cnt, int silent)
+{
+
+	int retry = 0, rc;
+	unsigned int delay = start_delay; /* in milliseconds */
+
+	while (1) {
+		if (!silent && retry >= 1) {
+			PMIXP_ERROR("send failed, rc=%d, try #%d", rc, retry);
+		}
+
+		rc = _pmix_p2p_send_core(nodename, address, data, len);
+
+		if (rc == SLURM_SUCCESS)
+			break;
+
+		retry++;
+		if (retry >= retry_cnt)
+			break;
+
+		/* wait with constantly increasing delay */
+		struct timespec ts =
+			{(delay / 1000), ((delay % 1000) * 1000000)};
+		nanosleep(&ts, NULL);
+		delay *= 2;
+	}
 
 	return rc;
 }

src/plugins/mpi/pmix/pmixp_coll.c
@@ -2,7 +2,7 @@
  **  pmix_coll.c - PMIx collective primitives
  *****************************************************************************
  *  Copyright (C) 2014-2015 Artem Polyakov. All rights reserved.
- *  Copyright (C) 2015      Mellanox Technologies. All rights reserved.
+ *  Copyright (C) 2015-2016 Mellanox Technologies. All rights reserved.
  *  Written by Artem Polyakov <artpol84@gmail.com, artemp@mellanox.com>.
  *
  *  This file is part of SLURM, a resource management program.
@@ -518,7 +518,7 @@ static void _progress_fan_in(pmixp_coll_t *coll)
 	pmixp_srv_cmd_t type;
 	const char *addr = pmixp_info_srv_addr();
 	char *hostlist = NULL;
-	int rc;
+	int rc, is_p2p = 0;
 	Buf root_buf;
 
 	PMIXP_DEBUG("%s:%d: start, local=%d, child_cntr=%d",
@@ -548,6 +548,7 @@ static void _progress_fan_in(pmixp_coll_t *coll)
 		type = PMIXP_MSG_FAN_IN;
 		PMIXP_DEBUG("%s:%d: switch to PMIXP_COLL_FAN_OUT state",
 			    pmixp_info_namespace(), pmixp_info_nodeid());
+		is_p2p = 1;
 	} else {
 		if (0 < hostlist_count(coll->all_children)) {
 			hostlist = hostlist_ranged_string_xmalloc(
@@ -577,7 +578,7 @@ static void _progress_fan_in(pmixp_coll_t *coll)
 		}
 		rc = pmixp_server_send(hostlist, type, coll->seq, addr,
 				get_buf_data(coll->buf),
-				get_buf_offset(coll->buf));
+				get_buf_offset(coll->buf), is_p2p);
 
 		if (SLURM_SUCCESS != rc) {
 			PMIXP_ERROR(

src/plugins/mpi/pmix/pmixp_dmdx.c
@@ -2,7 +2,7 @@
  **  pmix_debug.h - PMIx debug primitives
  *****************************************************************************
  *  Copyright (C) 2014-2015 Artem Polyakov. All rights reserved.
- *  Copyright (C) 2015      Mellanox Technologies. All rights reserved.
+ *  Copyright (C) 2015-2016 Mellanox Technologies. All rights reserved.
  *  Written by Artem Polyakov <artpol84@gmail.com, artemp@mellanox.com>.
  *
  *  This file is part of SLURM, a resource management program.
@@ -193,7 +193,7 @@ static void _respond_with_error(int seq_num, char *sender_host,
 	addr = pmixp_info_nspace_usock(sender_ns);
 	/* send response */
 	rc = pmixp_server_send(sender_host, PMIXP_MSG_DMDX, seq_num, addr,
-			get_buf_data(buf), get_buf_offset(buf));
+			get_buf_data(buf), get_buf_offset(buf), 1);
 	if (SLURM_SUCCESS != rc) {
 		PMIXP_ERROR("Cannot send direct modex error" " response to %s",
 				sender_host);
@@ -223,7 +223,7 @@ static void _dmdx_pmix_cb(pmix_status_t status, char *data, size_t sz,
 	/* send the request */
 	rc = pmixp_server_send(caddy->sender_host, PMIXP_MSG_DMDX,
 			caddy->seq_num, addr, get_buf_data(buf),
-			get_buf_offset(buf));
+			get_buf_offset(buf), 1);
 	if (SLURM_SUCCESS != rc) {
 		/* not much we can do here. Caller will react by timeout */
 		PMIXP_ERROR("Cannot send direct modex response to %s",
@@ -273,7 +273,7 @@ int pmixp_dmdx_get(const char *nspace, int rank,
 
 	/* send the request */
 	rc = pmixp_server_send(host, PMIXP_MSG_DMDX, seq, addr,
-			get_buf_data(buf), get_buf_offset(buf));
+			get_buf_data(buf), get_buf_offset(buf), 1);
 
 	/* cleanup the resources */
 	xfree(addr);

src/plugins/mpi/pmix/pmixp_server.c
@@ -2,7 +2,7 @@
  **  pmix_server.c - PMIx server side functionality
  *****************************************************************************
  *  Copyright (C) 2014-2015 Artem Polyakov. All rights reserved.
- *  Copyright (C) 2015      Mellanox Technologies. All rights reserved.
+ *  Copyright (C) 2015-2016 Mellanox Technologies. All rights reserved.
  *  Written by Artem Polyakov <artpol84@gmail.com, artemp@mellanox.com>.
  *
  *  This file is part of SLURM, a resource management program.
@@ -322,7 +322,7 @@ static int _recv_unpack_hdr(void *net, void *host)
 }
 
 int pmixp_server_send(char *hostlist, pmixp_srv_cmd_t type, uint32_t seq,
-		const char *addr, void *data, size_t size)
+		const char *addr, void *data, size_t size, int p2p)
 {
 	send_header_t hdr;
 	char nhdr[sizeof(send_header_t)];
@@ -339,7 +339,11 @@ int pmixp_server_send(char *hostlist, pmixp_srv_cmd_t type, uint32_t seq,
 	hsize = _send_pack_hdr(&hdr, nhdr);
 	memcpy(data, nhdr, hsize);
 
-	rc = pmixp_stepd_send(hostlist, addr, data, size, 500, 7, 0);
+	if( !p2p ){
+		rc = pmixp_stepd_send(hostlist, addr, data, size, 500, 7, 0);
+	} else {
+		rc = pmixp_p2p_send(hostlist, addr, data, size, 500, 7, 0);
+	}
 	if (SLURM_SUCCESS != rc) {
 		PMIXP_ERROR("Cannot send message to %s, size = %u, hostlist:\n%s",
 			    addr, (uint32_t) size, hostlist);

src/plugins/mpi/pmix/pmixp_server.h
@@ -2,7 +2,7 @@
  **  pmix_server.h - PMIx server side functionality
  *****************************************************************************
  *  Copyright (C) 2014-2015 Artem Polyakov. All rights reserved.
- *  Copyright (C) 2015      Mellanox Technologies. All rights reserved.
+ *  Copyright (C) 2015-2016 Mellanox Technologies. All rights reserved.
  *  Written by Artem Polyakov <artpol84@gmail.com, artemp@mellanox.com>.
  *
  *  This file is part of SLURM, a resource management program.
@@ -52,7 +52,7 @@ int pmixp_stepd_finalize(void);
 int pmix_srun_init(const mpi_plugin_client_info_t *job, char ***env);
 void pmix_server_new_conn(int fd);
 int pmixp_server_send(char *hostlist, pmixp_srv_cmd_t type, uint32_t seq,
-		      const char *addr, void *data, size_t size);
+		      const char *addr, void *data, size_t size, int p2p);
 int pmixp_server_health_chk(char *hostlist,  const char *addr);
 
 

src/plugins/mpi/pmix/pmixp_utils.h
@@ -2,7 +2,7 @@
  **	pmix_utils.c - Various PMIx utility functions
  *****************************************************************************
  *  Copyright (C) 2014-2015 Artem Polyakov. All rights reserved.
- *  Copyright (C) 2015      Mellanox Technologies. All rights reserved.
+ *  Copyright (C) 2015-2016 Mellanox Technologies. All rights reserved.
  *  Written by Artem Polyakov <artpol84@gmail.com, artemp@mellanox.com>.
  *
  *  This file is part of SLURM, a resource management program.
@@ -53,6 +53,9 @@ int pmixp_srun_send(slurm_addr_t *addr, uint32_t len, char *data);
 int pmixp_stepd_send(char *nodelist, const char *address, char *data,
 		     uint32_t len, unsigned int start_delay, unsigned int retry_cnt,
 		     int silent);
+int pmixp_p2p_send(char *nodename, const char *address, char *data,
+		     uint32_t len, unsigned int start_delay,
+		     unsigned int retry_cnt, int silent);
 int pmixp_rmdir_recursively(char *path);
 int pmixp_fixrights(char *path, uid_t uid, mode_t mode);
 int pmixp_mkdir(char *path, mode_t rights);

commit 4b6a88ea02b5e0f92d7d4ef02253d79c49d959d3

Fix compiling issues introduced in commit ee4a9776a3235f

src/plugins/mpi/pmix/pmixp_utils.c
@@ -51,6 +51,9 @@
 #include "pmixp_utils.h"
 #include "pmixp_debug.h"
 
+/* must come after the above pmixp includes */
+#include "src/common/forward.h"
+
 void pmixp_xfree_xmalloced(void *x)
 {
 	xfree(x);
@@ -317,9 +320,9 @@ static int _pmix_p2p_send_core(char *nodename, const char *address, char *data,
 
 	PMIXP_DEBUG("nodelist=%s, address=%s, len=%u",
 			nodename, address, len);
-	req.address = address;
+	req.address = (char *)address;
 	req.len = len;
-	req.data = (char *)data;
+	req.data = data;
 
 	msg.msg_type = REQUEST_FORWARD_DATA;
 	msg.data = &req;