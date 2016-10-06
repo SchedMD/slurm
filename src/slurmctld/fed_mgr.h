cd9143c1e05125f287e7d1c4c60194f98befea02
Brian Christiansen <brian@schedmd.com>
2016-10-06 16:10:05 -0600

    Sort and prettify fed_mgr.h

--- src/slurmctld/fed_mgr.h
+++ src/slurmctld/fed_mgr.h
@@ -42,23 +42,26 @@
 
 extern slurmdb_federation_rec_t *fed_mgr_fed_rec;
 
-extern int fed_mgr_init(void *db_conn);
-extern int fed_mgr_fini();
-extern int fed_mgr_update_feds(slurmdb_update_object_t *update);
-extern int fed_mgr_state_save(char *state_save_location);
+extern int       fed_mgr_add_sibling_conn(slurm_persist_conn_t *persist_conn,
+					  char **out_buffer);
+extern char     *fed_mgr_cluster_ids_to_names(uint64_t cluster_ids);
+extern int       fed_mgr_fini();
+extern uint32_t  fed_mgr_get_cluster_id(uint32_t id);
+extern char     *fed_mgr_get_cluster_name(uint32_t id);
+extern uint32_t  fed_mgr_get_job_id(uint32_t orig);
+extern uint32_t  fed_mgr_get_local_id(uint32_t id);
+extern int       fed_mgr_init(void *db_conn);
+extern bool      fed_mgr_is_active();
+extern bool      fed_mgr_is_tracker_only_job(struct job_record *job_ptr);
+extern int       fed_mgr_job_allocate(slurm_msg_t *msg,
+				      job_desc_msg_t *job_desc, uid_t uid,
+				      uint16_t protocol_version,
+				      struct job_record **job_pptr,
+				      char **err_msg);
+extern int       fed_mgr_sib_will_run(slurm_msg_t *msg,
+				      job_desc_msg_t *job_desc, uid_t uid,
+				      will_run_response_msg_t **resp);
 extern slurmdb_federation_rec_t *fed_mgr_state_load(char *state_save_location);
-extern bool fed_mgr_is_active();
-extern uint32_t fed_mgr_get_job_id(uint32_t orig);
-extern uint32_t fed_mgr_get_local_id(uint32_t id);
-extern uint32_t fed_mgr_get_cluster_id(uint32_t id);
-extern char *fed_mgr_get_cluster_name(uint32_t id);
-extern char *fed_mgr_cluster_ids_to_names(uint64_t cluster_ids);
-extern int fed_mgr_add_sibling_conn(slurm_persist_conn_t *persist_conn,
-				    char **out_buffer);
-extern bool fed_mgr_is_tracker_only_job(struct job_record *job_ptr);
-extern int fed_mgr_job_allocate(slurm_msg_t *msg, job_desc_msg_t *job_desc,
-				uid_t uid, uint16_t protocol_version,
-				struct job_record **job_pptr, char **err_msg);
-extern int fed_mgr_sib_will_run(slurm_msg_t *msg, job_desc_msg_t *job_desc,
-				uid_t uid, will_run_response_msg_t **resp);
+extern int       fed_mgr_state_save(char *state_save_location);
+extern int       fed_mgr_update_feds(slurmdb_update_object_t *update);
 #endif /* _SLURM_FED_MGR_H */
