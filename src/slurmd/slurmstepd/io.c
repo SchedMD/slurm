SchedMD/slurm

commit 3251406cebea7739aea990a3431d274992be3283

Add task launch flag field
Add "flag" field to launch_tasks_request_msg. Remove the following fields
    (moved into flags): multi_prog, task_flags, user_managed_io, pty,
    buffered_stdio, and labelio. More flags to be added later.

src/slurmd/slurmstepd/io.c
@@ -933,7 +933,7 @@ _init_task_stdio_fds(stepd_step_task_info_t *task, stepd_step_rec_t *job)
 	 *  Initialize stdin
 	 */
 #ifdef HAVE_PTY_H
-	if (job->pty) {
+	if (job->flags & LAUNCH_PTY) {
 		/* All of the stdin fails unless EVERY
 		 * task gets an eio object for stdin.
 		 * Its not clear why that is. */
@@ -1019,7 +1019,7 @@ _init_task_stdio_fds(stepd_step_task_info_t *task, stepd_step_rec_t *job)
 	 *  Initialize stdout
 	 */
 #ifdef HAVE_PTY_H
-	if (job->pty) {
+	if (job->flags & LAUNCH_PTY) {
 		if (task->gtid == 0) {
 			task->stdout_fd = dup(task->stdin_fd);
 			fd_set_close_on_exec(task->stdout_fd);
@@ -1037,8 +1037,9 @@ _init_task_stdio_fds(stepd_step_task_info_t *task, stepd_step_rec_t *job)
 			fd_set_close_on_exec(task->stdout_fd);
 			task->from_stdout = -1;  /* not used */
 		}
-	} else if (task->ofname != NULL &&
-		   (!job->labelio || xstrcmp(task->ofname, "/dev/null")==0)) {
+	} else if ((task->ofname != NULL) &&
+		   (((job->flags & LAUNCH_LABEL_IO) == 0) ||
+		    (xstrcmp(task->ofname, "/dev/null") == 0))) {
 #else
 	if (task->ofname != NULL &&
 	    (!job->labelio || xstrcmp(task->ofname, "/dev/null")==0) ) {
@@ -1061,7 +1062,7 @@ _init_task_stdio_fds(stepd_step_task_info_t *task, stepd_step_rec_t *job)
 		/* create pipe and eio object */
 		int pout[2];
 #if HAVE_PTY_H
-		if (job->buffered_stdio) {
+		if (job->flags & LAUNCH_BUFFERED_IO) {
 #if HAVE_SETRESUID
 			if (setresuid(geteuid(), geteuid(), 0) < 0)
 				error("%s: %d setresuid() %m",
@@ -1104,7 +1105,7 @@ _init_task_stdio_fds(stepd_step_task_info_t *task, stepd_step_rec_t *job)
 	 *  Initialize stderr
 	 */
 #ifdef HAVE_PTY_H
-	if (job->pty) {
+	if (job->flags & LAUNCH_PTY) {
 		if (task->gtid == 0) {
 			/* Make a file descriptor for the task to write to, but
 			   don't make a separate one read from, because in pty
@@ -1123,11 +1124,14 @@ _init_task_stdio_fds(stepd_step_task_info_t *task, stepd_step_rec_t *job)
 			fd_set_close_on_exec(task->stderr_fd);
 			task->from_stderr = -1;  /* not used */
 		}
-	} else if (task->efname != NULL &&
-		   (!job->labelio || xstrcmp(task->efname, "/dev/null")==0)) {
+
+	} else if ((task->efname != NULL) &&
+		   (((job->flags & LAUNCH_LABEL_IO) == 0) ||
+		    (xstrcmp(task->efname, "/dev/null") == 0))) {
 #else
-	if (task->efname != NULL &&
-	    (!job->labelio || xstrcmp(task->efname, "/dev/null")==0) ) {
+	if ((task->efname != NULL) &&
+	    (((job->flags & LAUNCH_LABEL_IO) == 0) ||
+	     (xstrcmp(task->efname, "/dev/null") == 0))) {
 #endif
 		int count = 0;
 		/* open file on task's stdout */

src/api/step_launch.c
@@ -280,7 +280,9 @@ int slurm_step_launch (slurm_step_ctx_t *ctx,
 	launch.mem_bind_type	= params->mem_bind_type;
 	launch.mem_bind		= params->mem_bind;
 	launch.accel_bind_type	= params->accel_bind_type;
-	launch.multi_prog	= params->multi_prog ? 1 : 0;
+	launch.flags		= 0;
+	if (params->multi_prog)
+		launch.flags	|= LAUNCH_MULTI_PROG;
 	launch.cpus_per_task	= params->cpus_per_task;
 	launch.ntasks_per_board = params->ntasks_per_board;
 	launch.ntasks_per_core  = params->ntasks_per_core;
@@ -288,7 +290,8 @@ int slurm_step_launch (slurm_step_ctx_t *ctx,
 
 	launch.task_dist	= params->task_dist;
 	launch.partition	= params->partition;
-	launch.pty              = params->pty;
+	if (params->pty)
+		launch.flags |= LAUNCH_PTY;
 	launch.ckpt_dir         = params->ckpt_dir;
 	launch.restart_dir      = params->restart_dir;
 	launch.acctg_freq	= params->acctg_freq;
@@ -297,24 +300,26 @@ int slurm_step_launch (slurm_step_ctx_t *ctx,
 	launch.complete_nodelist =
 		xstrdup(ctx->step_resp->step_layout->node_list);
 	spank_set_remote_options (launch.options);
-	launch.task_flags = 0;
 	if (params->parallel_debug)
-		launch.task_flags |= TASK_PARALLEL_DEBUG;
+		launch.flags |= LAUNCH_PARALLEL_DEBUG;
 
 	launch.tasks_to_launch = ctx->step_resp->step_layout->tasks;
 	launch.global_task_ids = ctx->step_resp->step_layout->tids;
 
 	launch.select_jobinfo  = ctx->step_resp->select_jobinfo;
 
-	launch.user_managed_io = params->user_managed_io ? 1 : 0;
+	if (params->user_managed_io)
+		launch.flags	|= LAUNCH_USER_MANAGED_IO;
 	ctx->launch_state->user_managed_io = params->user_managed_io;
 
 	if (!ctx->launch_state->user_managed_io) {
 		launch.ofname = params->remote_output_filename;
 		launch.efname = params->remote_error_filename;
 		launch.ifname = params->remote_input_filename;
-		launch.buffered_stdio = params->buffered_stdio ? 1 : 0;
-		launch.labelio = params->labelio ? 1 : 0;
+		if (params->buffered_stdio)
+			launch.flags	|= LAUNCH_BUFFERED_IO;
+		if (params->labelio)
+			launch.flags	|= LAUNCH_LABEL_IO;
 		ctx->launch_state->io.normal =
 			client_io_handler_create(params->local_fds,
 						 ctx->step_req->num_tasks,
@@ -466,11 +471,14 @@ int slurm_step_launch_add (slurm_step_ctx_t *ctx,
 	launch.mem_bind_type	= params->mem_bind_type;
 	launch.mem_bind		= params->mem_bind;
 	launch.accel_bind_type	= params->accel_bind_type;
-	launch.multi_prog	= params->multi_prog ? 1 : 0;
+	launch.flags = 0;
+	if (params->multi_prog)
+		launch.flags |= LAUNCH_MULTI_PROG;
 	launch.cpus_per_task	= params->cpus_per_task;
 	launch.task_dist	= params->task_dist;
 	launch.partition	= params->partition;
-	launch.pty              = params->pty;
+	if (params->pty)
+		launch.flags |= LAUNCH_PTY;
 	launch.ckpt_dir         = params->ckpt_dir;
 	launch.restart_dir      = params->restart_dir;
 	launch.acctg_freq	= params->acctg_freq;
@@ -480,16 +488,16 @@ int slurm_step_launch_add (slurm_step_ctx_t *ctx,
 		xstrdup(ctx->step_resp->step_layout->node_list);
 
 	spank_set_remote_options (launch.options);
-	launch.task_flags = 0;
 	if (params->parallel_debug)
-		launch.task_flags |= TASK_PARALLEL_DEBUG;
+		launch.flags |= LAUNCH_PARALLEL_DEBUG;
 
 	launch.tasks_to_launch = ctx->step_resp->step_layout->tasks;
 	launch.global_task_ids = ctx->step_resp->step_layout->tids;
 
 	launch.select_jobinfo  = ctx->step_resp->select_jobinfo;
 
-	launch.user_managed_io = params->user_managed_io ? 1 : 0;
+	if (params->user_managed_io)
+		launch.flags |= LAUNCH_USER_MANAGED_IO;
 
 	/* user_managed_io is true */
 	if (!ctx->launch_state->io.user) {

src/common/slurm_protocol_defs.h
@@ -156,15 +156,6 @@
    typedef struct switch_node_info switch_node_info_t;
 #endif
 
-/* used to define flags of the launch_tasks_request_msg_t and
- * spawn task_request_msg_t task_flags
- */
-enum task_flag_vals {
-	TASK_PARALLEL_DEBUG = 0x1,
-	TASK_UNUSED1 = 0x2,
-	TASK_UNUSED2 = 0x4
-};
-
 /*
  * SLURM Message types
  *
@@ -765,6 +756,13 @@ typedef struct job_step_create_response_msg {
 				      */
 } job_step_create_response_msg_t;
 
+#define LAUNCH_PARALLEL_DEBUG	0x00000001
+#define LAUNCH_MULTI_PROG	0x00000002
+#define LAUNCH_PTY		0x00000004
+#define LAUNCH_BUFFERED_IO	0x00000008
+#define LAUNCH_LABEL_IO		0x00000010
+#define LAUNCH_USER_MANAGED_IO	0x00000020
+
 typedef struct launch_tasks_request_msg {
 	uint32_t  job_id;
 	uint32_t  job_step_id;
@@ -784,7 +782,6 @@ typedef struct launch_tasks_request_msg {
 	uint16_t  *tasks_to_launch;
 	uint32_t  envc;
 	uint32_t  argc;
-	uint16_t  multi_prog;
 	uint16_t  node_cpus;
 	uint16_t  cpus_per_task;
 	char    **env;
@@ -800,14 +797,10 @@ typedef struct launch_tasks_request_msg {
 
         /* Distribution at the lowest level of logical processor (lllp) */
 	uint32_t task_dist;  /* --distribution=, -m dist	*/
-	uint16_t  task_flags;
+	uint32_t flags;		/* See LAUNCH_* flags defined above */
 	uint32_t **global_task_ids;
 	slurm_addr_t orig_addr;	  /* where message really came from for io */
-
-	uint16_t user_managed_io; /* 0 for "normal" IO,
-				     1 for "user manged" IO */
 	uint8_t open_mode;	/* stdout/err append or truncate */
-	uint8_t pty;		/* use pseudo tty */
 	char *acctg_freq;	/* accounting polling intervals */
 	uint32_t cpu_freq_min;  /* Minimum cpu frequency  */
 	uint32_t cpu_freq_max;  /* Maximum cpu frequency  */
@@ -819,8 +812,6 @@ typedef struct launch_tasks_request_msg {
 	char     *ofname; /* stdout filename pattern */
 	char     *efname; /* stderr filename pattern */
 	char     *ifname; /* stdin filename pattern */
-	uint8_t   buffered_stdio; /* 1 for line-buffered, 0 for unbuffered */
-	uint8_t   labelio;  /* prefix output lines with the task number */
 	uint16_t  num_io_port;
 	uint16_t  *io_port;  /* array of available client IO listen ports */
 	/**********  END  "normal" IO only options **********/

src/common/slurm_protocol_pack.c
@@ -9993,15 +9993,11 @@ _pack_launch_tasks_request_msg(launch_tasks_request_msg_t * msg, Buf buffer,
 		pack16(msg->mem_bind_type, buffer);
 		packstr(msg->mem_bind, buffer);
 		packstr_array(msg->argv, msg->argc, buffer);
-		pack16(msg->task_flags, buffer);
-		pack16(msg->multi_prog, buffer);
-		pack16(msg->user_managed_io, buffer);
-		if (msg->user_managed_io == 0) {
+		pack32(msg->flags, buffer);
+		if ((msg->flags & LAUNCH_USER_MANAGED_IO) == 0) {
 			packstr(msg->ofname, buffer);
 			packstr(msg->efname, buffer);
 			packstr(msg->ifname, buffer);
-			pack8(msg->buffered_stdio, buffer);
-			pack8(msg->labelio, buffer);
 			pack16(msg->num_io_port, buffer);
 			for (i = 0; i < msg->num_io_port; i++)
 				pack16(msg->io_port[i], buffer);
@@ -10017,7 +10013,6 @@ _pack_launch_tasks_request_msg(launch_tasks_request_msg_t * msg, Buf buffer,
 		packstr(msg->complete_nodelist, buffer);
 
 		pack8(msg->open_mode, buffer);
-		pack8(msg->pty, buffer);
 		packstr(msg->acctg_freq, buffer);
 		pack32(msg->cpu_freq_min, buffer);
 		pack32(msg->cpu_freq_max, buffer);
@@ -10036,6 +10031,20 @@ _pack_launch_tasks_request_msg(launch_tasks_request_msg_t * msg, Buf buffer,
 						     protocol_version);
 		}
 	} else if (protocol_version >= SLURM_16_05_PROTOCOL_VERSION) {
+		uint16_t task_flags = 0, multi_prog = 0, user_managed_io = 0;
+		uint8_t pty = 0, buffered_stdio = 0, labelio = 0;
+		if (msg->flags & LAUNCH_PARALLEL_DEBUG)
+			task_flags = 1;
+		if (msg->flags & LAUNCH_MULTI_PROG)
+			multi_prog = 1;
+		if (msg->flags & LAUNCH_USER_MANAGED_IO)
+			user_managed_io = 1;
+		if (msg->flags & LAUNCH_PTY)
+			pty = 1;
+		if (msg->flags & LAUNCH_BUFFERED_IO)
+			buffered_stdio = 1;
+		if (msg->flags & LAUNCH_LABEL_IO)
+			labelio = 1;
 		pack32(msg->job_id, buffer);
 		pack32(msg->job_step_id, buffer);
 		pack32(msg->ntasks, buffer);
@@ -10076,15 +10085,15 @@ _pack_launch_tasks_request_msg(launch_tasks_request_msg_t * msg, Buf buffer,
 		pack16(msg->mem_bind_type, buffer);
 		packstr(msg->mem_bind, buffer);
 		packstr_array(msg->argv, msg->argc, buffer);
-		pack16(msg->task_flags, buffer);
-		pack16(msg->multi_prog, buffer);
-		pack16(msg->user_managed_io, buffer);
-		if (msg->user_managed_io == 0) {
+		pack16(task_flags, buffer);
+		pack16(multi_prog, buffer);
+		pack16(user_managed_io, buffer);
+		if (user_managed_io == 0) {
 			packstr(msg->ofname, buffer);
 			packstr(msg->efname, buffer);
 			packstr(msg->ifname, buffer);
-			pack8(msg->buffered_stdio, buffer);
-			pack8(msg->labelio, buffer);
+			pack8(buffered_stdio, buffer);
+			pack8(labelio, buffer);
 			pack16(msg->num_io_port, buffer);
 			for (i = 0; i < msg->num_io_port; i++)
 				pack16(msg->io_port[i], buffer);
@@ -10100,7 +10109,7 @@ _pack_launch_tasks_request_msg(launch_tasks_request_msg_t * msg, Buf buffer,
 		packstr(msg->complete_nodelist, buffer);
 
 		pack8(msg->open_mode, buffer);
-		pack8(msg->pty, buffer);
+		pack8(pty, buffer);
 		packstr(msg->acctg_freq, buffer);
 		pack32(msg->cpu_freq_min, buffer);
 		pack32(msg->cpu_freq_max, buffer);
@@ -10119,6 +10128,20 @@ _pack_launch_tasks_request_msg(launch_tasks_request_msg_t * msg, Buf buffer,
 						     protocol_version);
 		}
 	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
+		uint16_t task_flags = 0, multi_prog = 0, user_managed_io = 0;
+		uint8_t pty = 0, buffered_stdio = 0, labelio = 0;
+		if (msg->flags & LAUNCH_PARALLEL_DEBUG)
+			task_flags = 1;
+		if (msg->flags & LAUNCH_MULTI_PROG)
+			multi_prog = 1;
+		if (msg->flags & LAUNCH_USER_MANAGED_IO)
+			user_managed_io = 1;
+		if (msg->flags & LAUNCH_PTY)
+			pty = 1;
+		if (msg->flags & LAUNCH_BUFFERED_IO)
+			buffered_stdio = 1;
+		if (msg->flags & LAUNCH_LABEL_IO)
+			labelio = 1;
 		pack32(msg->job_id, buffer);
 		pack32(msg->job_step_id, buffer);
 		pack32(msg->ntasks, buffer);
@@ -10156,15 +10179,15 @@ _pack_launch_tasks_request_msg(launch_tasks_request_msg_t * msg, Buf buffer,
 		pack16(msg->mem_bind_type, buffer);
 		packstr(msg->mem_bind, buffer);
 		packstr_array(msg->argv, msg->argc, buffer);
-		pack16(msg->task_flags, buffer);
-		pack16(msg->multi_prog, buffer);
-		pack16(msg->user_managed_io, buffer);
-		if (msg->user_managed_io == 0) {
+		pack16(task_flags, buffer);
+		pack16(multi_prog, buffer);
+		pack16(user_managed_io, buffer);
+		if (user_managed_io == 0) {
 			packstr(msg->ofname, buffer);
 			packstr(msg->efname, buffer);
 			packstr(msg->ifname, buffer);
-			pack8(msg->buffered_stdio, buffer);
-			pack8(msg->labelio, buffer);
+			pack8(buffered_stdio, buffer);
+			pack8(labelio, buffer);
 			pack16(msg->num_io_port, buffer);
 			for (i = 0; i < msg->num_io_port; i++)
 				pack16(msg->io_port[i], buffer);
@@ -10180,7 +10203,7 @@ _pack_launch_tasks_request_msg(launch_tasks_request_msg_t * msg, Buf buffer,
 		packstr(msg->complete_nodelist, buffer);
 
 		pack8(msg->open_mode, buffer);
-		pack8(msg->pty, buffer);
+		pack8(pty, buffer);
 		packstr(msg->acctg_freq, buffer);
 		pack32(msg->cpu_freq_min, buffer);
 		pack32(msg->cpu_freq_max, buffer);
@@ -10269,18 +10292,14 @@ _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **
 		safe_unpack16(&msg->mem_bind_type, buffer);
 		safe_unpackstr_xmalloc(&msg->mem_bind, &uint32_tmp, buffer);
 		safe_unpackstr_array(&msg->argv, &msg->argc, buffer);
-		safe_unpack16(&msg->task_flags, buffer);
-		safe_unpack16(&msg->multi_prog, buffer);
-		safe_unpack16(&msg->user_managed_io, buffer);
-		if (msg->user_managed_io == 0) {
+		safe_unpack32(&msg->flags, buffer);
+		if ((msg->flags & LAUNCH_USER_MANAGED_IO) == 0) {
 			safe_unpackstr_xmalloc(&msg->ofname, &uint32_tmp,
 					       buffer);
 			safe_unpackstr_xmalloc(&msg->efname, &uint32_tmp,
 					       buffer);
 			safe_unpackstr_xmalloc(&msg->ifname, &uint32_tmp,
 					       buffer);
-			safe_unpack8(&msg->buffered_stdio, buffer);
-			safe_unpack8(&msg->labelio, buffer);
 			safe_unpack16(&msg->num_io_port, buffer);
 			if (msg->num_io_port > 0) {
 				msg->io_port = xmalloc(sizeof(uint16_t) *
@@ -10311,7 +10330,6 @@ _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **
 				       buffer);
 
 		safe_unpack8(&msg->open_mode, buffer);
-		safe_unpack8(&msg->pty, buffer);
 		safe_unpackstr_xmalloc(&msg->acctg_freq, &uint32_tmp, buffer);
 		safe_unpack32(&msg->cpu_freq_min, buffer);
 		safe_unpack32(&msg->cpu_freq_max, buffer);
@@ -10324,7 +10342,9 @@ _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **
 						       protocol_version);
 		}
 	} else if (protocol_version >= SLURM_16_05_PROTOCOL_VERSION) {
-		uint32_t tmp_mem;
+		uint32_t tmp_mem = 0;
+		uint16_t task_flags = 0, multi_prog = 0, user_managed_io = 0;
+		uint8_t pty = 0, buffered_stdio = 0, labelio = 0;
 		safe_unpack32(&msg->job_id, buffer);
 		safe_unpack32(&msg->job_step_id, buffer);
 		safe_unpack32(&msg->ntasks, buffer);
@@ -10377,18 +10397,18 @@ _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **
 		safe_unpack16(&msg->mem_bind_type, buffer);
 		safe_unpackstr_xmalloc(&msg->mem_bind, &uint32_tmp, buffer);
 		safe_unpackstr_array(&msg->argv, &msg->argc, buffer);
-		safe_unpack16(&msg->task_flags, buffer);
-		safe_unpack16(&msg->multi_prog, buffer);
-		safe_unpack16(&msg->user_managed_io, buffer);
-		if (msg->user_managed_io == 0) {
+		safe_unpack16(&task_flags, buffer);
+		safe_unpack16(&multi_prog, buffer);
+		safe_unpack16(&user_managed_io, buffer);
+		if (user_managed_io == 0) {
 			safe_unpackstr_xmalloc(&msg->ofname, &uint32_tmp,
 					       buffer);
 			safe_unpackstr_xmalloc(&msg->efname, &uint32_tmp,
 					       buffer);
 			safe_unpackstr_xmalloc(&msg->ifname, &uint32_tmp,
 					       buffer);
-			safe_unpack8(&msg->buffered_stdio, buffer);
-			safe_unpack8(&msg->labelio, buffer);
+			safe_unpack8(&buffered_stdio, buffer);
+			safe_unpack8(&labelio, buffer);
 			safe_unpack16(&msg->num_io_port, buffer);
 			if (msg->num_io_port > 0) {
 				msg->io_port = xmalloc(sizeof(uint16_t) *
@@ -10419,7 +10439,7 @@ _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **
 				       buffer);
 
 		safe_unpack8(&msg->open_mode, buffer);
-		safe_unpack8(&msg->pty, buffer);
+		safe_unpack8(&pty, buffer);
 		safe_unpackstr_xmalloc(&msg->acctg_freq, &uint32_tmp, buffer);
 		safe_unpack32(&msg->cpu_freq_min, buffer);
 		safe_unpack32(&msg->cpu_freq_max, buffer);
@@ -10431,8 +10451,22 @@ _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **
 						       buffer,
 						       protocol_version);
 		}
+		if (task_flags)
+			msg->flags |= LAUNCH_PARALLEL_DEBUG;
+		if (multi_prog)
+			msg->flags |= LAUNCH_MULTI_PROG;
+		if (user_managed_io)
+			msg->flags |= LAUNCH_USER_MANAGED_IO;
+		if (pty)
+			msg->flags |= LAUNCH_PTY;
+		if (buffered_stdio)
+			msg->flags |= LAUNCH_BUFFERED_IO;
+		if (labelio)
+			msg->flags |= LAUNCH_LABEL_IO;
 	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
-		uint32_t tmp_mem;
+		uint32_t tmp_mem = 0;
+		uint16_t task_flags = 0, multi_prog = 0, user_managed_io = 0;
+		uint8_t pty = 0, buffered_stdio = 0, labelio = 0;
 		msg->ntasks_per_board = (uint16_t)NO_VAL;
 		msg->ntasks_per_core = (uint16_t)NO_VAL;
 		msg->ntasks_per_socket = (uint16_t)NO_VAL;
@@ -10486,18 +10520,18 @@ _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **
 		safe_unpack16(&msg->mem_bind_type, buffer);
 		safe_unpackstr_xmalloc(&msg->mem_bind, &uint32_tmp, buffer);
 		safe_unpackstr_array(&msg->argv, &msg->argc, buffer);
-		safe_unpack16(&msg->task_flags, buffer);
-		safe_unpack16(&msg->multi_prog, buffer);
-		safe_unpack16(&msg->user_managed_io, buffer);
-		if (msg->user_managed_io == 0) {
+		safe_unpack16(&task_flags, buffer);
+		safe_unpack16(&multi_prog, buffer);
+		safe_unpack16(&user_managed_io, buffer);
+		if (user_managed_io == 0) {
 			safe_unpackstr_xmalloc(&msg->ofname, &uint32_tmp,
 					       buffer);
 			safe_unpackstr_xmalloc(&msg->efname, &uint32_tmp,
 					       buffer);
 			safe_unpackstr_xmalloc(&msg->ifname, &uint32_tmp,
 					       buffer);
-			safe_unpack8(&msg->buffered_stdio, buffer);
-			safe_unpack8(&msg->labelio, buffer);
+			safe_unpack8(&buffered_stdio, buffer);
+			safe_unpack8(&labelio, buffer);
 			safe_unpack16(&msg->num_io_port, buffer);
 			if (msg->num_io_port > 0) {
 				msg->io_port = xmalloc(sizeof(uint16_t) *
@@ -10528,7 +10562,7 @@ _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **
 				       buffer);
 
 		safe_unpack8(&msg->open_mode, buffer);
-		safe_unpack8(&msg->pty, buffer);
+		safe_unpack8(&pty, buffer);
 		safe_unpackstr_xmalloc(&msg->acctg_freq, &uint32_tmp, buffer);
 		safe_unpack32(&msg->cpu_freq_min, buffer);
 		safe_unpack32(&msg->cpu_freq_max, buffer);
@@ -10540,6 +10574,18 @@ _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **
 						       buffer,
 						       protocol_version);
 		}
+		if (task_flags)
+			msg->flags |= LAUNCH_PARALLEL_DEBUG;
+		if (multi_prog)
+			msg->flags |= LAUNCH_MULTI_PROG;
+		if (user_managed_io)
+			msg->flags |= LAUNCH_USER_MANAGED_IO;
+		if (pty)
+			msg->flags |= LAUNCH_PTY;
+		if (buffered_stdio)
+			msg->flags |= LAUNCH_BUFFERED_IO;
+		if (labelio)
+			msg->flags |= LAUNCH_LABEL_IO;
 	} else {
 		error("_unpack_launch_tasks_request_msg: protocol_version "
 		      "%hu not supported", protocol_version);

src/common/slurm_protocol_util.c
@@ -181,7 +181,7 @@ void slurm_print_launch_task_msg(launch_tasks_request_msg_t *msg, char *name)
 	}
 	debug3("msg -> resp_port  = %u", *(msg->resp_port));
 	debug3("msg -> io_port    = %u", *(msg->io_port));
-	debug3("msg -> task_flags = %x", msg->task_flags);
+	debug3("msg -> flags      = %x", msg->flags);
 
 	for (i = 0; i < msg->tasks_to_launch[node_id]; i++) {
 		debug3("global_task_id[%d]: %u ", i,

src/slurmd/slurmstepd/mgr.c
@@ -542,8 +542,7 @@ _setup_normal_io(stepd_step_rec_t *job)
 		   the stepd rather than sent back to srun or written directly
 		   from the node.  When a task has ofname or efname == NULL, it
 		   means data gets sent back to the client. */
-
-		if (job->labelio) {
+		if (job->flags & LAUNCH_LABEL_IO) {
 			slurmd_filename_pattern_t outpattern, errpattern;
 			bool same = false;
 			int file_flags;
@@ -558,7 +557,7 @@ _setup_normal_io(stepd_step_rec_t *job)
 				for (ii = 0; ii < job->node_tasks; ii++) {
 					rc = io_create_local_client(
 						job->task[ii]->ofname,
-						file_flags, job, job->labelio,
+						file_flags, job, 1,
 						job->task[ii]->id,
 						same ? job->task[ii]->id : -2);
 					if (rc != SLURM_SUCCESS) {
@@ -574,9 +573,8 @@ _setup_normal_io(stepd_step_rec_t *job)
 			} else if (outpattern == SLURMD_ALL_SAME) {
 				/* Open a file for all tasks */
 				rc = io_create_local_client(
-					job->task[0]->ofname,
-					file_flags, job, job->labelio,
-					-1, same ? -1 : -2);
+					job->task[0]->ofname, file_flags,
+					job, 1, -1, same ? -1 : -2);
 				if (rc != SLURM_SUCCESS) {
 					error("Could not open output file %s: %m",
 					      job->task[0]->ofname);
@@ -595,8 +593,7 @@ _setup_normal_io(stepd_step_rec_t *job)
 					     ii < job->node_tasks; ii++) {
 						rc = io_create_local_client(
 							job->task[ii]->efname,
-							file_flags, job,
-							job->labelio,
+							file_flags, job, 1,
 							-2, job->task[ii]->id);
 						if (rc != SLURM_SUCCESS) {
 							error("Could not open error file %s: %m",
@@ -611,8 +608,7 @@ _setup_normal_io(stepd_step_rec_t *job)
 					/* Open a file for all tasks */
 					rc = io_create_local_client(
 						job->task[0]->efname,
-						file_flags, job, job->labelio,
-						-2, -1);
+						file_flags, job, 1, -2, -1);
 					if (rc != SLURM_SUCCESS) {
 						error("Could not open error file %s: %m",
 						      job->task[0]->efname);
@@ -1341,7 +1337,8 @@ fail2:
 	/*
 	 * Wait for io thread to complete (if there is one)
 	 */
-	if (!job->batch && !job->user_managed_io && io_initialized)
+	if (!job->batch && io_initialized &&
+	    ((job->flags & LAUNCH_USER_MANAGED_IO) == 0))
 		_wait_for_io(job);
 
 	/*
@@ -1566,7 +1563,7 @@ static int exec_wait_kill_children (List exec_wait_list)
 static void prepare_stdio (stepd_step_rec_t *job, stepd_step_task_info_t *task)
 {
 #ifdef HAVE_PTY_H
-	if (job->pty && (task->gtid == 0)) {
+	if ((job->flags & LAUNCH_PTY) && (task->gtid == 0)) {
 		if (login_tty(task->stdin_fd))
 			error("login_tty: %m");
 		else
@@ -1633,7 +1630,7 @@ _fork_all_tasks(stepd_step_rec_t *job, bool *io_initialized)
 		goto fail1; /* pam_setup error */
 
 	set_umask(job);		/* set umask for stdout/err files */
-	if (job->user_managed_io)
+	if (job->flags & LAUNCH_USER_MANAGED_IO)
 		rc = _setup_user_managed_io(job);
 	else
 		rc = _setup_normal_io(job);
@@ -1813,13 +1810,10 @@ _fork_all_tasks(stepd_step_rec_t *job, bool *io_initialized)
 		 * session, causing setpgid() to fail, setsid()
 		 * has already set its process group as desired
 		 */
-		if ((job->pty == 0)
-		    &&  (setpgid (job->task[i]->pid, job->pgid) < 0)) {
-			error("Unable to put task %d (pid %d) into "
-			      "pgrp %d: %m",
-			      i,
-			      job->task[i]->pid,
-			      job->pgid);
+		if (((job->flags & LAUNCH_PTY) == 0) &&
+		    (setpgid (job->task[i]->pid, job->pgid) < 0)) {
+			error("Unable to put task %d (pid %d) into pgrp %d: %m",
+			      i, job->task[i]->pid, job->pgid);
 		}
 
 		if (proctrack_g_add(job, job->task[i]->pid)
@@ -2554,7 +2548,9 @@ _slurmd_job_log_init(stepd_step_rec_t *job)
 	 *   STDERR_FILENO to the task's pts on stderr to avoid hangs in
 	 *   the slurmstepd.
 	 */
-	if (!job->user_managed_io && !job->pty && job->task != NULL) {
+	if (((job->flags & LAUNCH_USER_MANAGED_IO) == 0) &&
+	    ((job->flags & LAUNCH_PTY) == 0) &&
+	    (job->task != NULL)) {
 		if (dup2(job->task[0]->stderr_fd, STDERR_FILENO) < 0) {
 			error("job_log_init: dup2(stderr): %m");
 			return ESLURMD_IO_ERROR;

src/slurmd/slurmstepd/pdebug.c
@@ -62,7 +62,7 @@ pdebug_trace_process(stepd_step_rec_t *job, pid_t pid)
 	 *  ptrace(PTRACE_DETACH).
 	 */
 
-	if (job->task_flags & TASK_PARALLEL_DEBUG) {
+	if (job->flags & LAUNCH_PARALLEL_DEBUG) {
 		int status;
 		waitpid(pid, &status, WUNTRACED);
 		if (!WIFSTOPPED(status)) {
@@ -126,7 +126,7 @@ pdebug_stop_current(stepd_step_rec_t *job)
 	/*
 	 * Stop the task on exec for TotalView to connect
 	 */
-	if ( (job->task_flags & TASK_PARALLEL_DEBUG)
+	if ( (job->flags & LAUNCH_PARALLEL_DEBUG)
 #ifdef BSD
 	     && (_PTRACE(PT_TRACE_ME, 0, (caddr_t)0, 0) < 0) )
 #elif defined(PT_TRACE_ME)
@@ -189,7 +189,7 @@ static bool _pid_to_wake(pid_t pid)
  */
 void pdebug_wake_process(stepd_step_rec_t *job, pid_t pid)
 {
-	if ((job->task_flags & TASK_PARALLEL_DEBUG) && (pid > (pid_t) 0)) {
+	if ((job->flags & LAUNCH_PARALLEL_DEBUG) && (pid > (pid_t) 0)) {
 		if (_pid_to_wake(pid)) {
 			if (kill(pid, SIGCONT) < 0)
 				error("kill(%lu): %m", (unsigned long) pid);

src/slurmd/slurmstepd/slurmstepd_job.c
@@ -157,7 +157,7 @@ _job_init_task_info(stepd_step_rec_t *job, uint32_t **gtid,
 	job->task = (stepd_step_task_info_t **)
 		xmalloc(job->node_tasks * sizeof(stepd_step_task_info_t *));
 
-	if (!job->multi_prog && job->argv) {
+	if (((job->flags & LAUNCH_MULTI_PROG) == 0) && job->argv) {
 		char *new_path = build_path(job->argv[0], job->env, job->cwd);
 		xfree(job->argv[0]);
 		job->argv[0] = new_path;
@@ -169,13 +169,13 @@ _job_init_task_info(stepd_step_rec_t *job, uint32_t **gtid,
 		err = _expand_stdio_filename(efname, gtid[node_id][i], job);
 		job->task[i] = task_info_create(i, gtid[node_id][i], in, out,
 						err);
-		if (!job->multi_prog) {
+		if ((job->flags & LAUNCH_MULTI_PROG) == 0) {
 			job->task[i]->argc = job->argc;
 			job->task[i]->argv = job->argv;
 		}
 	}
 
-	if (job->multi_prog) {
+	if (job->flags & LAUNCH_MULTI_PROG) {
 		char *switch_type = slurm_get_switch_type();
 		if (!xstrcmp(switch_type, "switch/cray"))
 			multi_prog_parse(job, gtid);
@@ -352,10 +352,9 @@ stepd_step_rec_create(launch_tasks_request_msg_t *msg, uint16_t protocol_version
 	} else {
 		memset(&resp_addr, 0, sizeof(slurm_addr_t));
 	}
-	job->user_managed_io = msg->user_managed_io;
 	if (!msg->io_port)
-		msg->user_managed_io = 1;
-	if (!msg->user_managed_io) {
+		msg->flags |= LAUNCH_USER_MANAGED_IO;
+	if ((msg->flags & LAUNCH_USER_MANAGED_IO) == 0) {
 		memcpy(&io_addr,   &msg->orig_addr, sizeof(slurm_addr_t));
 		slurm_set_addr(&io_addr,
 			       msg->io_port[nodeid % msg->num_io_port],
@@ -366,9 +365,6 @@ stepd_step_rec_create(launch_tasks_request_msg_t *msg, uint16_t protocol_version
 
 	srun = srun_info_create(msg->cred, &resp_addr, &io_addr,
 				protocol_version);
-
-	job->buffered_stdio = msg->buffered_stdio;
-	job->labelio = msg->labelio;
 
 	job->profile     = msg->profile;
 	job->task_prolog = xstrdup(msg->task_prolog);
@@ -391,11 +387,9 @@ stepd_step_rec_create(launch_tasks_request_msg_t *msg, uint16_t protocol_version
 	acct_gather_profile_startpoll(msg->acctg_freq,
 				      conf->job_acct_gather_freq);
 
-	job->multi_prog  = msg->multi_prog;
 	job->timelimit   = (time_t) -1;
-	job->task_flags  = msg->task_flags;
+	job->flags       = msg->flags;
 	job->switch_job  = msg->switch_job;
-	job->pty         = msg->pty;
 	job->open_mode   = msg->open_mode;
 	job->options     = msg->options;
 	format_core_allocs(msg->cred, conf->node_name, conf->cpus,
@@ -489,7 +483,6 @@ batch_stepd_step_rec_create(batch_job_launch_msg_t *msg)
 	acct_gather_profile_startpoll(msg->acctg_freq,
 				      conf->job_acct_gather_freq);
 
-	job->multi_prog = 0;
 	job->open_mode  = msg->open_mode;
 	job->overcommit = (bool) msg->overcommit;
 
@@ -574,13 +567,16 @@ batch_stepd_step_rec_create(batch_job_launch_msg_t *msg)
 extern void
 stepd_step_rec_destroy(stepd_step_rec_t *job)
 {
+	uint16_t multi_prog = 0;
 	int i;
 
 	_array_free(&job->env);
 	_array_free(&job->argv);
 
+	if (job->flags & LAUNCH_MULTI_PROG)
+		multi_prog = 1;
 	for (i = 0; i < job->node_tasks; i++)
-		_task_info_destroy(job->task[i], job->multi_prog);
+		_task_info_destroy(job->task[i], multi_prog);
 	eio_handle_destroy(job->eio);
 	FREE_NULL_LIST(job->sruns);
 	FREE_NULL_LIST(job->clients);

commit 88d8f73794b5ee5a9cca9087eb31d91cf20bfba5

Fix FreeBSD build failure from labelio removal.
Inadvertently broken by commit 3251406cebea77.

src/slurmd/slurmstepd/io.c
@@ -1042,7 +1042,8 @@ _init_task_stdio_fds(stepd_step_task_info_t *task, stepd_step_rec_t *job)
 		    (xstrcmp(task->ofname, "/dev/null") == 0))) {
 #else
 	if (task->ofname != NULL &&
-	    (!job->labelio || xstrcmp(task->ofname, "/dev/null")==0) ) {
+	    (((job->flags & LAUNCH_LABEL_IO) == 0) ||
+	     xstrcmp(task->ofname, "/dev/null") == 0)) {
 #endif
 		int count = 0;
 		/* open file on task's stdout */