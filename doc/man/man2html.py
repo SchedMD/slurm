b26f2731cb1290c344fc50690a897d5095584714
Joseph Mingrone <jrm@ftfl.ca>
2016-10-18 07:54:05 -0600

    Improve man2html invocation
    Old command line would not work with FreeBSD. New command line
      should work with all systems
    bug 3021

--- doc/man/man2html.py
+++ doc/man/man2html.py
@@ -180,7 +180,7 @@ version = sys.argv[1]
 for f in sys.argv[4:]:
     posLastDot = f.rfind(".")
     mhtmlname = f[:posLastDot] + ".mhtml"
-    cmd = "man2html " + f + "> " + mhtmlname
+    cmd = "man2html < " + f + "> " + mhtmlname
     os.system(cmd)
     print(">>>>>>> " + mhtmlname)
     files.append(mhtmlname)
