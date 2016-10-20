SchedMD/slurm

commit fb46c84b7990132f8d743b0dba10a8a3df86d7d8

Fix problem updating job state_reason
This patch has two parts:
1. When a job is intially submitted, the Slurm was failing to set
   an initial reason for the job not starting.
2. After a job was submitted, it was sometimes failing to reset
   the job's reason. It was also failing to reset the "last_job_update"
   time, so something like "squeue -i1" would not get the new reason.
bug 3025

src/slurmctld/job_mgr.c
@@ -4121,6 +4121,29 @@ static int _select_nodes_parts(struct job_record *job_ptr, bool test_only,
 		}
 	}
 
+	if (rc == ESLURM_NODES_BUSY)
+		job_ptr->state_reason = WAIT_RESOURCES;
+	else if ((rc == ESLURM_RESERVATION_BUSY) ||
+		 (rc == ESLURM_RESERVATION_NOT_USABLE))
+		job_ptr->state_reason = WAIT_RESERVATION;
+	else if (rc == ESLURM_JOB_HELD)
+		job_ptr->state_reason = WAIT_HELD;
+	else if (rc == ESLURM_NODE_NOT_AVAIL)
+		job_ptr->state_reason = WAIT_NODE_NOT_AVAIL;
+	else if (rc == ESLURM_QOS_THRES)
+		job_ptr->state_reason = WAIT_QOS_THRES;
+	else if (rc == ESLURM_ACCOUNTING_POLICY)
+		job_ptr->state_reason = WAIT_ACCOUNT_POLICY;
+	else if (rc == ESLURM_REQUESTED_PART_CONFIG_UNAVAILABLE)
+		job_ptr->state_reason = WAIT_PART_CONFIG;
+	else if (rc == ESLURM_POWER_NOT_AVAIL)
+		job_ptr->state_reason = WAIT_POWER_NOT_AVAIL;
+	else if (rc == ESLURM_BURST_BUFFER_WAIT)
+		job_ptr->state_reason = WAIT_BURST_BUFFER_RESOURCE;
+	else if (rc == ESLURM_POWER_RESERVED)
+		job_ptr->state_reason = WAIT_POWER_RESERVED;
+	else if (rc == ESLURM_PARTITION_DOWN)
+		job_ptr->state_reason = WAIT_PART_DOWN;
 	return rc;
 }
 
@@ -5453,12 +5476,12 @@ static int _valid_job_part(job_desc_msg_t * job_desc,
 				     "partitions with partition based "
 				     "associations", __func__);
 				rc = SLURM_ERROR;
-			} else
+			} else {
 				rc = _part_access_check(part_ptr_tmp, job_desc,
 							req_bitmap, submit_uid,
 							qos_ptr, assoc_ptr ?
 							assoc_ptr->acct : NULL);
-
+			}
 			if ((rc != SLURM_SUCCESS) &&
 			    ((rc == ESLURM_ACCESS_DENIED) ||
 			     (rc == ESLURM_USER_ID_MISSING) ||
@@ -5923,7 +5946,6 @@ static int _job_create(job_desc_msg_t *job_desc, int allocate, int will_run,
 	error_code = _valid_job_part(job_desc, submit_uid, req_bitmap,
 				     &part_ptr, part_ptr_list,
 				     assoc_ptr, qos_ptr);
-
 	if (error_code != SLURM_SUCCESS)
 		goto cleanup_fail;
 
@@ -10521,7 +10543,6 @@ static int _update_job(struct job_record *job_ptr, job_desc_msg_t * job_specs,
 				job_ptr->details->req_node_bitmap,
 				&tmp_part_ptr, part_ptr_list,
 				job_ptr->assoc_ptr, job_ptr->qos_ptr);
-
 			if (!error_code) {
 				xfree(job_ptr->partition);
 				job_ptr->partition =

slurm/slurm.h.in
@@ -504,6 +504,8 @@ enum job_state_reason {
 	/********************/
 	WAIT_QOS_MAX_JOB_PER_ACCT,    /* QOS MaxJobPerAccount exceeded */
 	WAIT_QOS_MAX_SUB_JOB_PER_ACCT,/* QOS MaxJobSubmitSPerAccount exceeded */
+	WAIT_PART_CONFIG,	      /* Generic partition configuration reason */
+	WAIT_ACCOUNT_POLICY,          /* Generic accounting policy reason */
 };
 
 enum job_acct_types {

src/common/slurm_protocol_defs.c
@@ -1627,6 +1627,10 @@ extern char *job_reason_string(enum job_state_reason inx)
 		return "MaxJobsPerAccount";
 	case WAIT_QOS_MAX_SUB_JOB_PER_ACCT:
 		return "MaxSubmitJobsPerAccount";
+	case WAIT_PART_CONFIG:
+		return "PartitionConfig";
+	case WAIT_ACCOUNT_POLICY:
+		return "AccountingPolicy";
 	default:
 		snprintf(val, sizeof(val), "%d", inx);
 		return val;

src/slurmctld/job_scheduler.c
@@ -329,6 +329,7 @@ extern List build_job_queue(bool clear_start, bool backfill)
 	int tested_jobs = 0;
 	char jobid_buf[32];
 	int job_part_pairs = 0;
+	time_t now = time(NULL);
 
 	(void) _delta_tv(&start_tv);
 	job_queue = list_create(_job_queue_rec_del);
@@ -427,7 +428,6 @@ extern List build_job_queue(bool clear_start, bool backfill)
 	while ((job_ptr = (struct job_record *) list_next(job_iterator))) {
 		if (((tested_jobs % 100) == 0) &&
 		    (_delta_tv(&start_tv) >= build_queue_timeout)) {
-			time_t now = time(NULL);
 			if (difftime(now, last_log_time) > 600) {
 				/* Log at most once every 10 minutes */
 				info("%s has run for %d usec, exiting with %d "
@@ -455,10 +455,10 @@ extern List build_job_queue(bool clear_start, bool backfill)
 				job_ptr->part_ptr = part_ptr;
 				reason = job_limits_check(&job_ptr, backfill);
 				if ((reason != WAIT_NO_REASON) &&
-				    (reason != job_ptr->state_reason) &&
-				    (!part_policy_job_runnable_state(job_ptr))){
+				    (reason != job_ptr->state_reason)) {
 					job_ptr->state_reason = reason;
 					xfree(job_ptr->state_desc);
+					last_job_update = now;
 				}
 				/* priority_array index matches part_ptr_list
 				 * position: increment inx */

commit 8712a254d115787f46b286cd614f3a0cda8da26c

Fix "sbatch --hold" to set JobHeldUser not JobHeldAdmin.
_select_nodes_parts() was resetting state_reason to an admin hold
without regard to admin vs user hold state. state_reason is the only
place that user vs. admin is distinguished, so this prevented users
from releasing these jobs.

Bug introduced by commit fb46c84b7990 in 16.05.5.

Bug 3197.

src/slurmctld/job_mgr.c
@@ -4127,7 +4127,10 @@ static int _select_nodes_parts(struct job_record *job_ptr, bool test_only,
 		 (rc == ESLURM_RESERVATION_NOT_USABLE))
 		job_ptr->state_reason = WAIT_RESERVATION;
 	else if (rc == ESLURM_JOB_HELD)
-		job_ptr->state_reason = WAIT_HELD;
+		/* Do not reset the state_reason field here. select_nodes()
+		 * already set the state_reason field, and this error code
+		 * does not distinguish between user and admin holds. */
+		;
 	else if (rc == ESLURM_NODE_NOT_AVAIL)
 		job_ptr->state_reason = WAIT_NODE_NOT_AVAIL;
 	else if (rc == ESLURM_QOS_THRES)