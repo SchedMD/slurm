SchedMD/slurm

commit 1fbd95f740ca7d41d5709764fe390713ce299c34

Fix issue where number of nodes is not properly allocated when sbatch and salloc are requested with -n tasks < hosts from -w hostlist or from -N.

src/salloc/opt.c
@@ -1456,6 +1456,8 @@ static bool _opt_verify(void)
 {
 	bool verified = true;
 	uint32_t cluster_flags = slurmdb_setup_cluster_flags();
+	hostlist_t hl = NULL;
+	int hl_cnt = 0;
 
 	if (opt.quiet && opt.verbose) {
 		error ("don't specify both --verbose (-v) and --quiet (-Q)");
@@ -1503,8 +1505,7 @@ static bool _opt_verify(void)
 	}
 
 	if (opt.nodelist) {
-		int hl_cnt;
-		hostlist_t hl = hostlist_create(opt.nodelist);
+		hl = hostlist_create(opt.nodelist);
 		if (!hl) {
 			error("memory allocation failure");
 			exit(error_exit);
@@ -1515,6 +1516,7 @@ static bool _opt_verify(void)
 			opt.min_nodes = MAX(hl_cnt, opt.min_nodes);
 		else
 			opt.min_nodes = hl_cnt;
+		opt.nodes_set = true;
 	}
 
 
@@ -1681,19 +1683,34 @@ static bool _opt_verify(void)
 			      "nodes, setting nnodes to %d",
 			      opt.ntasks, opt.min_nodes, opt.ntasks);
 
-			opt.min_nodes = opt.ntasks;
-			if (   opt.max_nodes
-			       && (opt.min_nodes > opt.max_nodes) )
-				opt.max_nodes = opt.min_nodes;
-		}
+			opt.min_nodes = opt.max_nodes = opt.ntasks;
+
+			if (hl_cnt > opt.min_nodes) {
+				int del_cnt, i;
+				char *host;
+				del_cnt = hl_cnt - opt.min_nodes;
+				for (i=0; i<del_cnt; i++) {
+					host = hostlist_pop(hl);
+					free(host);
+				}
+				xfree(opt.nodelist);
+				opt.nodelist =
+					hostlist_ranged_string_xmalloc(hl);
+			}
+
+		} else if (opt.max_nodes == 0)
+			opt.max_nodes = opt.ntasks;
 
 	} /* else if (opt.ntasks_set && !opt.nodes_set) */
+
+	if (hl)
+		hostlist_destroy(hl);
 
 	/* set up the proc and node counts based on the arbitrary list
 	   of nodes */
 	if (((opt.distribution & SLURM_DIST_STATE_BASE) == SLURM_DIST_ARBITRARY)
 	    && (!opt.nodes_set || !opt.ntasks_set)) {
-		hostlist_t hl = hostlist_create(opt.nodelist);
+		hl = hostlist_create(opt.nodelist);
 		if (!opt.ntasks_set) {
 			opt.ntasks_set = 1;
 			opt.ntasks = hostlist_count(hl);

src/sbatch/opt.c
@@ -2650,6 +2650,8 @@ static bool _opt_verify(void)
 	bool verified = true;
 	char *dist = NULL, *lllp_dist = NULL;
 	uint32_t cluster_flags = slurmdb_setup_cluster_flags();
+	hostlist_t hl = NULL;
+	int hl_cnt = 0;
 
 	if (opt.quiet && opt.verbose) {
 		error ("don't specify both --verbose (-v) and --quiet (-Q)");
@@ -2706,8 +2708,7 @@ static bool _opt_verify(void)
 	}
 
 	if (opt.nodelist) {
-		int hl_cnt;
-		hostlist_t hl = hostlist_create(opt.nodelist);
+		hl = hostlist_create(opt.nodelist);
 
 		if (!hl) {
 			error("memory allocation failure");
@@ -2719,6 +2720,7 @@ static bool _opt_verify(void)
 			opt.min_nodes = MAX(hl_cnt, opt.min_nodes);
 		else
 			opt.min_nodes = hl_cnt;
+		opt.nodes_set = true;
 	}
 
 	if (cluster_flags & CLUSTER_FLAG_BGQ)
@@ -2862,19 +2864,34 @@ static bool _opt_verify(void)
 			      "nodes, setting nnodes to %d",
 			      opt.ntasks, opt.min_nodes, opt.ntasks);
 
-			opt.min_nodes = opt.ntasks;
-			if (   opt.max_nodes
-			       && (opt.min_nodes > opt.max_nodes) )
-				opt.max_nodes = opt.min_nodes;
-		}
+			opt.min_nodes = opt.max_nodes = opt.ntasks;
+
+			if (hl_cnt > opt.min_nodes) {
+				int del_cnt, i;
+				char *host;
+				del_cnt = hl_cnt - opt.min_nodes;
+				for (i=0; i<del_cnt; i++) {
+					host = hostlist_pop(hl);
+					free(host);
+				}
+				xfree(opt.nodelist);
+				opt.nodelist =
+					hostlist_ranged_string_xmalloc(hl);
+			}
+
+		} else if (opt.max_nodes == 0)
+			opt.max_nodes = opt.ntasks;
 
 	} /* else if (opt.ntasks_set && !opt.nodes_set) */
+
+	if (hl)
+		hostlist_destroy(hl);
 
 	/* set up the proc and node counts based on the arbitrary list
 	   of nodes */
 	if (((opt.distribution & SLURM_DIST_STATE_BASE) == SLURM_DIST_ARBITRARY)
 	   && (!opt.nodes_set || !opt.ntasks_set)) {
-		hostlist_t hl = hostlist_create(opt.nodelist);
+		hl = hostlist_create(opt.nodelist);
 		if (!opt.ntasks_set) {
 			opt.ntasks_set = 1;
 			opt.ntasks = hostlist_count(hl);

commit 5799984c8c812fcdcba1882cf654ab564776b15f

Fix for regression in commit 1fbd95f740ca7 which would cause
salloc/sbatch -w node0 -n2 to allocate 2 nodes instead of only 1 if it
could.

This makes the code look like it does in srun which does do the correct
thing.

src/salloc/opt.c
@@ -1682,6 +1682,12 @@ static bool _opt_verify(void)
 		}
 
 	} else if (opt.nodes_set && opt.ntasks_set) {
+		/*
+		 * Make sure that the number of
+		 * max_nodes is <= number of tasks
+		 */
+		if (opt.ntasks < opt.max_nodes)
+			opt.max_nodes = opt.ntasks;
 
 		/*
 		 *  make sure # of procs >= min_nodes
@@ -1707,8 +1713,7 @@ static bool _opt_verify(void)
 					hostlist_ranged_string_xmalloc(hl);
 			}
 
-		} else if (opt.max_nodes == 0)
-			opt.max_nodes = opt.ntasks;
+		}
 
 	} /* else if (opt.ntasks_set && !opt.nodes_set) */
 

src/salloc/salloc.c
@@ -676,8 +676,12 @@ static int _fill_job_desc_from_opts(job_desc_msg_t *desc)
 	desc->exc_nodes = opt.exc_nodes;
 	desc->partition = opt.partition;
 	desc->min_nodes = opt.min_nodes;
-	if (opt.nodes_set)
+
+	if (opt.max_nodes)
 		desc->max_nodes = opt.max_nodes;
+	else if (opt.nodes_set)
+		desc->max_nodes = opt.min_nodes;
+
 	desc->user_id = opt.uid;
 	desc->group_id = opt.gid;
 	if (opt.dependency)

src/sbatch/opt.c
@@ -2854,6 +2854,12 @@ static bool _opt_verify(void)
 		}
 
 	} else if (opt.nodes_set && opt.ntasks_set) {
+		/*
+		 * Make sure that the number of
+		 * max_nodes is <= number of tasks
+		 */
+		if (opt.ntasks < opt.max_nodes)
+			opt.max_nodes = opt.ntasks;
 
 		/*
 		 *  make sure # of procs >= min_nodes
@@ -2879,8 +2885,7 @@ static bool _opt_verify(void)
 					hostlist_ranged_string_xmalloc(hl);
 			}
 
-		} else if (opt.max_nodes == 0)
-			opt.max_nodes = opt.ntasks;
+		}
 
 	} /* else if (opt.ntasks_set && !opt.nodes_set) */
 