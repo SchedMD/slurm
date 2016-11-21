71e457f69714c23c7ab8d12820c53f03ac1bd34e
Tim Wickberg <tim@schedmd.com>
2016-11-21 13:28:26 -0700

    Remove unused unpack_bit_str macro.

--- src/common/pack.h
+++ src/common/pack.h
@@ -294,21 +294,6 @@ int	unpackmem_array(char *valp, uint32_t size_valp, Buf buffer);
 		pack32(NO_VAL, buf);                 	\
 } while (0)
 
-#define unpack_bit_str(bitmap,buf) do {					\
-	char *tmp_str = NULL;						\
-	uint32_t _size = NO_VAL;					\
-	assert(*bitmap == NULL);					\
-	assert(buf->magic == BUF_MAGIC);				\
-	safe_unpack32(&_size, buf);					\
-	if (_size != NO_VAL) {						\
-		*bitmap = bit_alloc(_size);				\
-		safe_unpackstr_xmalloc(&tmp_str, &_size, buf);		\
-		bit_unfmt(*bitmap, tmp_str);				\
-		xfree(tmp_str);						\
-	} else								\
-		*bitmap = NULL;						\
-} while (0)
-
 #define pack_bit_str_hex(bitmap,buf) do {		\
 	assert(buf->magic == BUF_MAGIC);		\
 	if (bitmap) {					\
