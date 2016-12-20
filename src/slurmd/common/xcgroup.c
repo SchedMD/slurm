SchedMD/slurm

commit 0d4773a95129e45e8e3d9892e23432fe26fea60b

Fix some memory problems
Fix 2 memory leaks
Fix 1 file descriptor leak
Fix 2 buffer overflow problems
All problems reported by Coverity

src/slurmd/common/xcgroup.c
@@ -707,8 +707,8 @@ int xcgroup_get_uint32_param(xcgroup_t* cg, char* param, uint32_t* value)
 {
 	int fstatus = XCGROUP_ERROR;
 	char file_path[PATH_MAX];
-	char* cpath = cg->path;
-	uint32_t* values;
+	char *cpath = cg->path;
+	uint32_t *values = NULL;
 	int vnb;
 
 	if (snprintf(file_path, PATH_MAX, "%s/%s", cpath, param) >= PATH_MAX) {
@@ -724,9 +724,9 @@ int xcgroup_get_uint32_param(xcgroup_t* cg, char* param, uint32_t* value)
 				__func__, param, cpath);
 		} else {
 			*value = values[0];
-			xfree(values);
 			fstatus = XCGROUP_SUCCESS;
 		}
+		xfree(values);
 	}
 	return fstatus;
 }
@@ -758,8 +758,8 @@ int xcgroup_get_uint64_param(xcgroup_t* cg, char* param, uint64_t* value)
 {
 	int fstatus = XCGROUP_ERROR;
 	char file_path[PATH_MAX];
-	char* cpath = cg->path;
-	uint64_t* values;
+	char *cpath = cg->path;
+	uint64_t *values;
 	int vnb;
 
 	if (snprintf(file_path, PATH_MAX, "%s/%s", cpath, param) >= PATH_MAX) {
@@ -776,9 +776,9 @@ int xcgroup_get_uint64_param(xcgroup_t* cg, char* param, uint64_t* value)
 				__func__, param, cpath);
 		} else {
 			*value = values[0];
-			xfree(values);
 			fstatus = XCGROUP_SUCCESS;
 		}
+		xfree(values);
 	}
 	return fstatus;
 }
@@ -1056,7 +1056,7 @@ int _file_read_uint32s(char* file_path, uint32_t** pvalues, int* pnb)
 	}
 
 	/* get file size */
-	fsize=_file_getsize(fd);
+	fsize =_file_getsize(fd);
 	if (fsize == -1) {
 		close(fd);
 		return XCGROUP_ERROR;
@@ -1140,7 +1140,6 @@ int _file_read_content(char* file_path, char** content, size_t *csize)
 	int fstatus;
 	int rc;
 	int fd;
-
 	size_t fsize;
 	char* buf;
 
@@ -1166,7 +1165,7 @@ int _file_read_content(char* file_path, char** content, size_t *csize)
 	}
 
 	/* read file contents */
-	buf = (char*) xmalloc((fsize+1)*sizeof(char));
+	buf = xmalloc((fsize+1)*sizeof(char));
 	buf[fsize]='\0';
 	do {
 		rc = read(fd, buf, fsize);
@@ -1177,6 +1176,8 @@ int _file_read_content(char* file_path, char** content, size_t *csize)
 		*content = buf;
 		*csize = rc;
 		fstatus = XCGROUP_SUCCESS;
+	} else {
+		xfree(buf);
 	}
 
 	/* close file */

src/slurmd/common/proctrack.c
@@ -219,15 +219,15 @@ static bool _test_core_dumping(char* stat_fname)
 	int proc_fd, proc_stat_size = BUF_SIZE;
 	bool dumping_results = false;
 
-	proc_stat = (char *) xmalloc(proc_stat_size);
 	proc_fd = open(stat_fname, O_RDONLY, 0);
 	if (proc_fd == -1)
 		return false;  /* process is now gone */
+	proc_stat = xmalloc(proc_stat_size + 1);
 	while ((num = read(proc_fd, proc_stat, proc_stat_size)) > 0) {
 		if (num < (proc_stat_size-1))
 			break;
 		proc_stat_size += BUF_SIZE;
-		xrealloc(proc_stat, proc_stat_size);
+		xrealloc(proc_stat, proc_stat_size + 1);
 		if (lseek(proc_fd, (off_t) 0, SEEK_SET) != 0)
 			break;
 	}
@@ -236,9 +236,8 @@ static bool _test_core_dumping(char* stat_fname)
 	/* split into "PID (cmd" and "<rest>" */
 	str_ptr = (char *)strrchr(proc_stat, ')');
 	if (str_ptr == NULL) {
-		error("\
-%s: unexpected format of %s (%s) bracket missing?", __func__,
-		      stat_fname, proc_stat);
+		error("%s: unexpected format of %s (%s) bracket missing?",
+		      __func__, stat_fname, proc_stat);
 		xfree(proc_stat);
 		return false;
 	}

src/slurmd/slurmd/read_proc.c
@@ -249,7 +249,7 @@ read_proc(void)
 		} 
 	} 
 	proc_stat_size = BUF_SIZE;
-	proc_stat = (char *) xmalloc(proc_stat_size);
+	proc_stat = (char *) xmalloc(proc_stat_size + 1);
 	proc_fs = opendir("/proc");
 	if (proc_fs == NULL) {
 		error ("read_proc: opendir unable to open /proc %m");
@@ -276,7 +276,7 @@ read_proc(void)
 			if (n < (proc_stat_size-1))
 				break;
 			proc_stat_size += BUF_SIZE;
-			xrealloc(proc_stat, proc_stat_size);
+			xrealloc(proc_stat, proc_stat_size + 1);
 			if (lseek(proc_fd, (off_t) 0, SEEK_SET) != 0) 
 				break;
 		}

src/slurmd/slurmd/slurmd.c
@@ -1788,7 +1788,7 @@ cleanup:
 	xfree(new_file);
 	if (buffer)
 		free_buf(buffer);
-	if (cred_fd > 0)
+	if (cred_fd >= 0)
 		close(cred_fd);
 	return error_code;
 }

commit 7eafe641c292659518faa4651018e7cd682f048e

Fix for uninitialize variable
Bug introduced in commit 0d4773a95129e45e8e3d9892e23432fe26fea60b

src/slurmd/common/xcgroup.c
@@ -759,7 +759,7 @@ int xcgroup_get_uint64_param(xcgroup_t* cg, char* param, uint64_t* value)
 	int fstatus = XCGROUP_ERROR;
 	char file_path[PATH_MAX];
 	char *cpath = cg->path;
-	uint64_t *values;
+	uint64_t *values = NULL;
 	int vnb;
 
 	if (snprintf(file_path, PATH_MAX, "%s/%s", cpath, param) >= PATH_MAX) {