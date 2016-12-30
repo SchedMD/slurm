SchedMD/slurm

commit 72d8d9b7596fb664c710634b280c947e5245f48f

Fix memory leaks
Fix several memory leaks and possible out-of-bound memory references
  reported by Coverity.

src/slurmd/slurmstepd/multi_prog.c
@@ -211,7 +211,7 @@ extern int multi_prog_get_argv(char *config_data, char **prog_env,
 		args_spec = p;
 		while (*args_spec != '\0') {
 			/* Only simple quote and escape supported */
-			prog_argv[prog_argc ++] = args_spec;
+			prog_argv[prog_argc++] = xstrdup(args_spec);
 			if ((prog_argc + 1) >= MAX_ARGC) {
 				info("Exceeded multi-prog argc limit");
 				break;
@@ -283,7 +283,7 @@ extern int multi_prog_get_argv(char *config_data, char **prog_env,
 
 		*argc = prog_argc;
 		*argv = prog_argv;
-		/* FIXME - local_data is leaked */
+		xfree(local_data);
 		return 0;
 	}
 

src/slurmctld/job_mgr.c
@@ -1048,6 +1048,7 @@ extern int load_last_job_id( void )
 
 unpack_error:
 	debug("Invalid job data checkpoint file");
+	xfree(ver_str);
 	free_buf(buffer);
 	return SLURM_FAILURE;
 }
@@ -6944,6 +6945,7 @@ _read_data_array_from_file(int fd, char *file_name, char ***data,
 			xfree(buffer);
 			return -1;
 		}
+		buffer[pos + amount] = '\0';
 		pos += amount;
 		if (amount < BUF_SIZE)	/* end of file */
 			break;

src/slurmctld/step_mgr.c
@@ -4380,7 +4380,7 @@ extern int update_step(step_update_request_msg_t *req, uid_t uid)
 	struct step_record *step2_ptr = NULL;
 	ListIterator step_iterator;
 	int mod_cnt = 0;
-	bool new_step = 0;
+	bool new_step = false;
 
 	job_ptr = find_job_record(req->job_id);
 	if (job_ptr == NULL) {
@@ -4476,12 +4476,6 @@ extern int update_step(step_update_request_msg_t *req, uid_t uid)
 
 			jobacct_storage_g_step_complete(acct_db_conn, step_ptr);
 
-			if (new_step) {
-				/* This was a temporary step record, never
-				 * linked to the job, so there is no need to
-				 * check SELECT_JOBDATA_CLEANING. */
-				_free_step_rec(step_ptr);
-			}
 			mod_cnt++;
 			info("Updating step %u.%u jobacct info",
 			     req->job_id, req->step_id);
@@ -4494,6 +4488,11 @@ extern int update_step(step_update_request_msg_t *req, uid_t uid)
 	}
 	if (mod_cnt)
 		last_job_update = time(NULL);
+	if (new_step) {
+		/* This was a temporary step record, never linked to the job,
+		 * so there is no need to check SELECT_JOBDATA_CLEANING. */
+		_free_step_rec(step_ptr);
+	}
 
 	return SLURM_SUCCESS;
 }

src/slurmd/common/proctrack.c
@@ -224,6 +224,7 @@ static bool _test_core_dumping(char* stat_fname)
 		return false;  /* process is now gone */
 	proc_stat = xmalloc(proc_stat_size + 1);
 	while ((num = read(proc_fd, proc_stat, proc_stat_size)) > 0) {
+		proc_stat[num] = '\0';
 		if (num < (proc_stat_size-1))
 			break;
 		proc_stat_size += BUF_SIZE;

src/slurmd/slurmd/read_proc.c
@@ -273,7 +273,8 @@ read_proc(void)
 		if (proc_fd == -1) 
 			continue;  /* process is now gone */
 		while ((n = read(proc_fd, proc_stat, proc_stat_size)) > 0) {
-			if (n < (proc_stat_size-1))
+			proc_stat[n] = '\0';
+			if (n < (proc_stat_size - 1))
 				break;
 			proc_stat_size += BUF_SIZE;
 			xrealloc(proc_stat, proc_stat_size + 1);

src/slurmd/slurmstepd/pdebug.c
@@ -154,6 +154,8 @@ static bool _pid_to_wake(pid_t pid)
 		return false;  /* process is now gone */
 	proc_stat = xmalloc(4097);
 	len = read(proc_fd, proc_stat, 4096);
+	if (len >= 0)
+		proc_stat[len] = '\0';
 	close(proc_fd);
 	if (len < 14) {
 		xfree(proc_stat);

src/slurmd/slurmstepd/req.c
@@ -1208,8 +1208,12 @@ done:
 			}
 		}
 	}
-
+	if (srun) {
+		xfree(srun->key);
+		xfree(srun);
+	}
 	return SLURM_SUCCESS;
+
 rwfail:
 	if (srun) {
 		xfree(srun->key);

src/slurmd/slurmstepd/slurmstepd.c
@@ -215,11 +215,11 @@ ending:
 }
 
 
-static slurmd_conf_t * read_slurmd_conf_lite (int fd)
+static slurmd_conf_t *read_slurmd_conf_lite(int fd)
 {
 	int rc;
 	int len;
-	Buf buffer;
+	Buf buffer = NULL;
 	slurmd_conf_t *confl, *local_conf = NULL;
 	int tmp_int = 0;
 
@@ -272,6 +272,7 @@ static slurmd_conf_t * read_slurmd_conf_lite (int fd)
 	return (confl);
 
 rwfail:
+	FREE_NULL_BUFFER(buffer);
 	xfree(local_conf);
 	return (NULL);
 }

commit 6a548985229d7bcadaf2d07e1950eee26fa97b10

Fix memory leak reported by Coverity
This is a revision of commit 72d8d9b7596fb664c710634b280c947e5245f48f,
  which introduced a parsing bug

src/slurmd/slurmstepd/multi_prog.c
@@ -165,7 +165,6 @@ extern int multi_prog_get_argv(char *config_data, char **prog_env,
 	}
 
 	local_data = xstrdup(config_data);
-
 	line = strtok_r(local_data, "\n", &ptrptr);
 	while (line) {
 		if (line_num > 0)
@@ -174,16 +173,16 @@ extern int multi_prog_get_argv(char *config_data, char **prog_env,
 			error("No executable program specified for this task");
 			goto fail;
 		}
-		line_num ++;
-		if (strlen (line) >= (BUF_SIZE - 1)) {
+		line_num++;
+		if (strlen(line) >= (BUF_SIZE - 1)) {
 			error ("Line %d of configuration file too long",
 				line_num);
 			goto fail;
 		}
 
 		p = line;
-		while (*p != '\0' && isspace (*p)) /* remove leading spaces */
-			p ++;
+		while ((*p != '\0') && isspace (*p)) /* remove leading spaces */
+			p++;
 
 		if (*p == '#') /* only whole-line comments handled */
 			continue;
@@ -193,33 +192,34 @@ extern int multi_prog_get_argv(char *config_data, char **prog_env,
 
 		rank_spec = p;
 
-		while (*p != '\0' && !isspace (*p))
-			p ++;
+		while ((*p != '\0') && !isspace (*p))
+			p++;
 		if (*p == '\0') {
 			error("Invalid MPMD configuration line %d", line_num);
 			goto fail;
 		}
-		*p ++ = '\0';
+		*p++ = '\0';
 
-		if (!_in_range (task_rank, rank_spec, &task_offset))
+		if (!_in_range(task_rank, rank_spec, &task_offset))
 			continue;
 
 		/* skip all whitspace after the range spec */
-		while (*p != '\0' && isspace (*p))
+		while ((*p != '\0') && isspace (*p))
 			p++;
 
 		args_spec = p;
 		while (*args_spec != '\0') {
 			/* Only simple quote and escape supported */
-			prog_argv[prog_argc++] = xstrdup(args_spec);
+			args_spec = xstrdup(args_spec);
+			prog_argv[prog_argc++] = args_spec;
 			if ((prog_argc + 1) >= MAX_ARGC) {
 				info("Exceeded multi-prog argc limit");
 				break;
 			}
-		CONT:	while (*args_spec != '\0' && *args_spec != '\\'
-			&&     *args_spec != '%'
-			&&     *args_spec != '\'' && !isspace (*args_spec)) {
-				args_spec ++;
+		CONT:	while ((*args_spec != '\0') && (*args_spec != '\\') &&
+			       (*args_spec != '%')  && (*args_spec != '\'') &&
+			       !isspace(*args_spec)) {
+				args_spec++;
 			}
 			if (*args_spec == '\0') {
 				/* the last argument */
@@ -227,47 +227,48 @@ extern int multi_prog_get_argv(char *config_data, char **prog_env,
 
 			} else if (*args_spec == '%') {
 				_sub_expression(args_spec, task_rank,
-					task_offset);
-				args_spec ++;
+						task_offset);
+				args_spec++;
 				goto CONT;
 
 			} else if (*args_spec == '\\') {
 				/* escape, just remove the backslash */
-				s = args_spec ++;
+				s = args_spec++;
 				p = args_spec;
 				do {
-					*s ++ = *p;
-				} while (*p ++ != '\0');
+					*s++ = *p;
+				} while (*p++ != '\0');
 				goto CONT;
 
 			} else if (*args_spec == '\'') {
 				/* single quote,
 				 * preserve all characters quoted. */
 				p = args_spec + 1;
-				while (*p != '\0' && *p != '\'') {
+				while ((*p != '\0') && (*p != '\'')) {
 					/* remove quote */
-					*args_spec ++ = *p ++;
+					*args_spec++ = *p++;
 				}
 				if (*p == '\0') {
 					/* closing quote not found */
 					error("Program arguments specification"
-						" format invalid: %s.",
-						prog_argv[prog_argc -1]);
+					      " format invalid: %s.",
+					      prog_argv[prog_argc -1]);
 					goto fail;
 				}
-				p ++; /* skip closing quote */
+				p++; /* skip closing quote */
 				s = args_spec;
 				do {
-					*s ++ = *p;
-				} while (*p ++ != '\0');
+					*s++ = *p;
+				} while (*p++ != '\0');
 				goto CONT;
 
 			} else {
 				/* space */
-				*args_spec ++ = '\0';
-				while (*args_spec != '\0'
-				&& isspace (*args_spec))
-					args_spec ++;
+				*args_spec++ = '\0';
+				while ((*args_spec != '\0') &&
+				       isspace(*args_spec)) {
+					args_spec++;
+				}
 			}
 
 		}