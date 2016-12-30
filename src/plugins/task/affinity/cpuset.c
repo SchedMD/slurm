SchedMD/slurm

commit 239cfa969375fd8fc434cab15222682e49f55cd4

Fix memory reference problems reported by Coverity
Several possibly unterminated string operations
Several memory leak fixes

src/plugins/task/affinity/cpuset.c
@@ -192,7 +192,7 @@ int	slurm_set_cpuset(char *base, char *path, pid_t pid, size_t size,
 	int fd, rc;
 	int cpu_cnt = 0, mem_cnt = 0;
 	char file_path[PATH_MAX];
-	char mstr[1 + CPU_SETSIZE * 4];
+	char mstr[2 + CPU_SETSIZE * 4];
 
 	if (mkdir(path, 0700) && (errno != EEXIST)) {
 		error("%s: mkdir(%s): %m", __func__, path);
@@ -204,10 +204,12 @@ int	slurm_set_cpuset(char *base, char *path, pid_t pid, size_t size,
 		 base, cpuset_prefix);
 	fd = open(file_path, O_RDONLY);
 	if (fd >= 0) {
-		rc = read(fd, mstr, sizeof(mstr));
+		rc = read(fd, mstr, sizeof(mstr) - 1);
 		close(fd);
-		if (rc > 0)
+		if (rc > 0) {
+			mstr[rc] = '\0';
 			cpu_cnt = str_to_cnt(mstr);
+		}
 	}
 
 	/* Set "cpus" per user request */
@@ -235,12 +237,14 @@ int	slurm_set_cpuset(char *base, char *path, pid_t pid, size_t size,
 		error("open(%s): %m", file_path);
 	} else {
 		memset(mstr, 0, sizeof(mstr));
-		rc = read(fd, mstr, sizeof(mstr)-1);	/* Insure NULL at end */
+		rc = read(fd, mstr, sizeof(mstr) - 1);
 		close(fd);
 		if (rc < 1) {
 			error("read(%s): %m", file_path);
 			return SLURM_ERROR;
 		}
+		if (rc >= 0)
+			mstr[rc] = '\0';
 		if (rc > 0)
 			mem_cnt = str_to_cnt(mstr);
 		if ((cpu_cnt > 1) && (mem_cnt > 1))
@@ -252,7 +256,7 @@ int	slurm_set_cpuset(char *base, char *path, pid_t pid, size_t size,
 			error("open(%s): %m", file_path);
 			return SLURM_ERROR;
 		}
-		rc = write(fd, mstr, strlen(mstr)+1);
+		rc = write(fd, mstr, strlen(mstr) + 1);
 		close(fd);
 		if (rc < 1) {
 			error("write(%s): %m", file_path);
@@ -283,7 +287,7 @@ int	slurm_set_cpuset(char *base, char *path, pid_t pid, size_t size,
 		return SLURM_ERROR;
 	}
 	snprintf(mstr, sizeof(mstr), "%d", pid);
-	rc = write(fd, mstr, strlen(mstr)+1);
+	rc = write(fd, mstr, strlen(mstr) + 1);
 	close(fd);
 	if (rc < 1) {
 		error("write(%s, %s): %m", file_path, mstr);
@@ -297,7 +301,7 @@ int	slurm_get_cpuset(char *path, pid_t pid, size_t size, cpu_set_t *mask)
 {
 	int fd, rc;
 	char file_path[PATH_MAX];
-	char mstr[1 + CPU_SETSIZE * 4];
+	char mstr[2 + CPU_SETSIZE * 4];
 
 	snprintf(file_path, sizeof(file_path), "%s/%scpus",
 		 path, cpuset_prefix);
@@ -306,13 +310,13 @@ int	slurm_get_cpuset(char *path, pid_t pid, size_t size, cpu_set_t *mask)
 		error("open(%s): %m", file_path);
 		return SLURM_ERROR;
 	}
-	memset(mstr, 0, sizeof(mstr));
-	rc = read(fd, mstr, sizeof(mstr)-1);	/* Insure NULL at end */
+	rc = read(fd, mstr, sizeof(mstr) - 1);
 	close(fd);
 	if (rc < 1) {
 		error("read(%s): %m", file_path);
 		return SLURM_ERROR;
 	}
+	mstr[rc] = '\0';
 	str_to_cpuset(mask, mstr);
 
 	snprintf(file_path, sizeof(file_path), "%s/tasks", path);
@@ -327,6 +331,7 @@ int	slurm_get_cpuset(char *path, pid_t pid, size_t size, cpu_set_t *mask)
 		error("read(%s): %m", file_path);
 		return SLURM_ERROR;
 	}
+	mstr[rc] = '\0';
 
 	/* FIXME: verify that pid is in mstr */
 

src/plugins/task/cgroup/task_cgroup_cpuset.c
@@ -176,30 +176,32 @@ static int _xcgroup_cpuset_init(xcgroup_t* cg)
 	char cpuset_meta[PATH_MAX];
 	char* cpuset_conf;
 	size_t csize;
-
 	xcgroup_t acg;
-	char* acg_name;
-	char* p;
+	char *acg_name, *p;
 
 	fstatus = XCGROUP_ERROR;
 
 	/* load ancestor cg */
-	acg_name = (char*) xstrdup(cg->name);
-	p = rindex(acg_name,'/');
+	acg_name = (char *)xstrdup(cg->name);
+	p = rindex(acg_name, '/');
 	if (p == NULL) {
 		debug2("task/cgroup: unable to get ancestor path for "
-		       "cpuset cg '%s' : %m",cg->path);
+		       "cpuset cg '%s' : %m", cg->path);
+		xfree(acg_name);
 		return fstatus;
-	} else
+	} else {
 		*p = '\0';
-	if (xcgroup_load(cg->ns,&acg, acg_name) != XCGROUP_SUCCESS) {
+	}
+	if (xcgroup_load(cg->ns, &acg, acg_name) != XCGROUP_SUCCESS) {
 		debug2("task/cgroup: unable to load ancestor for "
-		       "cpuset cg '%s' : %m",cg->path);
+		       "cpuset cg '%s' : %m", cg->path);
+		xfree(acg_name);
 		return fstatus;
 	}
+	xfree(acg_name);
 
 	/* inherits ancestor params */
-	for (i = 0 ; i < 2 ; i++) {
+	for (i = 0; i < 2; i++) {
 	again:
 		snprintf(cpuset_meta, sizeof(cpuset_meta), "%s%s",
 			 cpuset_prefix, cpuset_metafiles[i]);
@@ -1124,9 +1126,7 @@ extern int task_cgroup_cpuset_create(stepd_step_rec_t *job)
 {
 	int rc;
 	int fstatus = SLURM_ERROR;
-
 	xcgroup_t cpuset_cg;
-
 	uint32_t jobid = job->jobid;
 	uint32_t stepid = job->stepid;
 	uid_t uid = job->uid;
@@ -1135,8 +1135,7 @@ extern int task_cgroup_cpuset_create(stepd_step_rec_t *job)
 	char* job_alloc_cores = NULL;
 	char* step_alloc_cores = NULL;
 	char cpuset_meta[PATH_MAX];
-
-	char* cpus = NULL;
+	char *cpus = NULL;
 	size_t cpus_size;
 
 	char* slurm_cgpath;
@@ -1148,30 +1147,29 @@ extern int task_cgroup_cpuset_create(stepd_step_rec_t *job)
 
 	/* create slurm root cg in this cg namespace */
 	slurm_cgpath = task_cgroup_create_slurm_cg(&cpuset_ns);
-	if ( slurm_cgpath == NULL ) {
+	if (slurm_cgpath == NULL)
 		return SLURM_ERROR;
-	}
 
 	/* check that this cgroup has cpus allowed or initialize them */
-	if (xcgroup_load(&cpuset_ns,&slurm_cg,slurm_cgpath)
-	    != XCGROUP_SUCCESS) {
+	if (xcgroup_load(&cpuset_ns,&slurm_cg,slurm_cgpath) != XCGROUP_SUCCESS){
 		error("task/cgroup: unable to load slurm cpuset xcgroup");
 		xfree(slurm_cgpath);
 		return SLURM_ERROR;
 	}
 again:
 	snprintf(cpuset_meta, sizeof(cpuset_meta), "%scpus", cpuset_prefix);
-	rc = xcgroup_get_param(&slurm_cg, cpuset_meta, &cpus,&cpus_size);
-	if (rc != XCGROUP_SUCCESS || cpus_size == 1) {
+	rc = xcgroup_get_param(&slurm_cg, cpuset_meta, &cpus, &cpus_size);
+	if ((rc != XCGROUP_SUCCESS) || (cpus_size == 1)) {
 		if (!cpuset_prefix_set && (rc != XCGROUP_SUCCESS)) {
 			cpuset_prefix_set = 1;
 			cpuset_prefix = "cpuset.";
+			xfree(cpus);
 			goto again;
 		}
 
 		/* initialize the cpusets as it was non-existent */
-		if (_xcgroup_cpuset_init(&slurm_cg) !=
-		    XCGROUP_SUCCESS) {
+		if (_xcgroup_cpuset_init(&slurm_cg) != XCGROUP_SUCCESS) {
+			xfree(cpus);
 			xfree(slurm_cgpath);
 			xcgroup_destroy(&slurm_cg);
 			return SLURM_ERROR;
@@ -1234,7 +1232,7 @@ again:
 	 * a task. The release_agent will have to lock the root cpuset cgroup
 	 * to avoid this scenario.
 	 */
-	if (xcgroup_create(&cpuset_ns,&cpuset_cg,"",0,0) != XCGROUP_SUCCESS) {
+	if (xcgroup_create(&cpuset_ns, &cpuset_cg, "", 0,0) != XCGROUP_SUCCESS){
 		error("task/cgroup: unable to create root cpuset xcgroup");
 		return SLURM_ERROR;
 	}
@@ -1269,9 +1267,8 @@ again:
 	/*
 	 * create user cgroup in the cpuset ns (it could already exist)
 	 */
-	if (xcgroup_create(&cpuset_ns,&user_cpuset_cg,
-			   user_cgroup_path,
-			   getuid(),getgid()) != XCGROUP_SUCCESS) {
+	if (xcgroup_create(&cpuset_ns,&user_cpuset_cg, user_cgroup_path,
+			   getuid(), getgid()) != XCGROUP_SUCCESS) {
 		goto error;
 	}
 	if (xcgroup_instantiate(&user_cpuset_cg) != XCGROUP_SUCCESS) {
@@ -1282,21 +1279,21 @@ again:
 	/*
 	 * check that user's cpuset cgroup is consistant and add the job cores
 	 */
-	rc = xcgroup_get_param(&user_cpuset_cg, cpuset_meta, &cpus,&cpus_size);
+	rc = xcgroup_get_param(&user_cpuset_cg, cpuset_meta, &cpus, &cpus_size);
 	if (rc != XCGROUP_SUCCESS || cpus_size == 1) {
 		/* initialize the cpusets as it was non-existent */
-		if (_xcgroup_cpuset_init(&user_cpuset_cg) !=
-		    XCGROUP_SUCCESS) {
+		if (_xcgroup_cpuset_init(&user_cpuset_cg) != XCGROUP_SUCCESS) {
 			(void)xcgroup_delete(&user_cpuset_cg);
 			xcgroup_destroy(&user_cpuset_cg);
+			xfree(cpus);
 			goto error;
 		}
 	}
 	user_alloc_cores = xstrdup(job_alloc_cores);
-	if (cpus != NULL && cpus_size > 1) {
+	if ((cpus != NULL) && (cpus_size > 1)) {
 		cpus[cpus_size-1]='\0';
-		xstrcat(user_alloc_cores,",");
-		xstrcat(user_alloc_cores,cpus);
+		xstrcat(user_alloc_cores, ",");
+		xstrcat(user_alloc_cores, cpus);
 	}
 	xcgroup_set_param(&user_cpuset_cg, cpuset_meta, user_alloc_cores);
 	xfree(cpus);

src/plugins/task/cgroup/task_cgroup_devices.c
@@ -435,6 +435,7 @@ error:
 	xcgroup_destroy(&devices_cg);
 	xfree(gres_step_bit_alloc);
 	xfree(gres_job_bit_alloc);
+	xfree(gres_count);
 	xfree(gres_name);
 	xfree(dev_path);
 	xfree(gres_cgroup);

src/sattach/sattach.c
@@ -270,6 +270,7 @@ static void print_layout_info(slurm_step_layout_t *layout)
 		printf("\n");
 		free(name);
 	}
+	hostlist_destroy(nl);
 }
 
 

src/scontrol/info_job.c
@@ -545,8 +545,8 @@ static void
 _list_pids_one_step(const char *node_name, uint32_t jobid, uint32_t stepid)
 {
 	int fd;
-	slurmstepd_task_info_t *task_info;
-	uint32_t *pids;
+	slurmstepd_task_info_t *task_info = NULL;
+	uint32_t *pids = NULL;
 	uint32_t count = 0;
 	uint32_t tcount = 0;
 	int i;
@@ -598,10 +598,8 @@ _list_pids_one_step(const char *node_name, uint32_t jobid, uint32_t stepid)
 		}
 	}
 
-	if (count > 0)
-		xfree(pids);
-	if (tcount > 0)
-		xfree(task_info);
+	xfree(pids);
+	xfree(task_info);
 	close(fd);
 }
 
@@ -785,6 +783,7 @@ scontrol_encode_hostlist(char *hostlist, bool sorted)
 	hl = hostlist_create(tmp_list);
 	if (hl == NULL) {
 		fprintf(stderr, "Invalid hostlist: %s\n", tmp_list);
+		xfree(io_buf);
 		return SLURM_ERROR;
 	}
 	if (sorted)

commit 103d59ebdebf8ba57fc0f749ca7a4194fd74072f

Fix for possible out-of-bounds memory reference
Bug introduced yesterday in commit 239cfa969375fd8fc434cab15222682e49f55cd4

src/plugins/task/affinity/cpuset.c
@@ -325,7 +325,7 @@ int	slurm_get_cpuset(char *path, pid_t pid, size_t size, cpu_set_t *mask)
 		error("open(%s): %m", file_path);
 		return SLURM_ERROR;
 	}
-	rc = read(fd, mstr, sizeof(mstr));
+	rc = read(fd, mstr, sizeof(mstr) - 1);
 	close(fd);
 	if (rc < 1) {
 		error("read(%s): %m", file_path);