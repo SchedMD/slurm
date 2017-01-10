SchedMD/slurm

commit 07ae3e72c28b50cfa81e4d03f40c26d49453c864

Fix some memory problems reported by Coverity
4 possible memory leaks
2 failures to check for errors returned by functions and
4 possibly unterminated strings

src/sacctmgr/file_functions.c
@@ -307,7 +307,7 @@ static sacctmgr_file_opts_t *_parse_options(char *options)
 	xfree(option);
 
 	if (!file_opts->name) {
-		exit_code=1;
+		exit_code = 1;
 		fprintf(stderr, " No name given\n");
 	}
 
@@ -1906,7 +1906,7 @@ extern void load_sacctmgr_cfg_file (int argc, char **argv)
 			xfree(parent);
 
 			if (!file_opts) {
-				exit_code=1;
+				exit_code = 1;
 				fprintf(stderr, " Problem with line(%d)\n", lc);
 				rc = SLURM_ERROR;
 				break;
@@ -2302,4 +2302,5 @@ extern void load_sacctmgr_cfg_file (int argc, char **argv)
 	FREE_NULL_LIST(curr_assoc_list);
 	FREE_NULL_LIST(curr_cluster_list);
 	FREE_NULL_LIST(curr_user_list);
+	_destroy_sacctmgr_file_opts(file_opts);
 }

src/plugins/acct_gather_energy/rapl/acct_gather_energy_rapl.c
@@ -248,13 +248,13 @@ static void _hardware(void)
 		if (!xstrncmp(buf, "physical id", sizeof("physical id") - 1)) {
 			sscanf(buf, "physical id\t: %d", &pkg);
 
-			if (pkg >= MAX_PKGS)
+			if (pkg >= MAX_PKGS) {
 				fatal("Slurm can only handle %d sockets for "
 				      "rapl, you seem to have more than that.  "
 				      "Update src/plugins/acct_gather_energy/"
 				      "rapl/acct_gather_energy_rapl.h "
 				      "(MAX_PKGS) and recompile.", MAX_PKGS);
-			if (pkg2cpu[pkg] == -1) {
+			} else if (pkg2cpu[pkg] == -1) {
 				nb_pkg++;
 				pkg2cpu[pkg] = cpu;
 			}

src/plugins/mpi/mvapich/mvapich.c
@@ -1220,7 +1220,7 @@ static void mvapich_barrier (mvapich_state_t *st)
 	mvapich_poll_reset (mp);
 	while ((m = mvapich_poll_next (mp, 0)))
 		mvapich_write (m, &m->rank, sizeof (m->rank));
-
+	mvapich_poll_destroy (mp);
 	return;
 }
 

src/plugins/proctrack/cgroup/proctrack_cgroup.c
@@ -143,6 +143,7 @@ int _slurm_cgroup_create(stepd_step_rec_t *job, uint64_t id, uid_t uid, gid_t gi
 
 	if (xcgroup_create(&freezer_ns, &slurm_freezer_cg, pre,
 			   getuid(), getgid()) != XCGROUP_SUCCESS) {
+		xfree(pre);
 		return SLURM_ERROR;
 	}
 
@@ -154,7 +155,10 @@ int _slurm_cgroup_create(stepd_step_rec_t *job, uint64_t id, uid_t uid, gid_t gi
 	 * shared directories that could result in the failure of the
 	 * hierarchy setup
 	 */
-	xcgroup_lock(&freezer_cg);
+	if (xcgroup_lock(&freezer_cg) != XCGROUP_SUCCESS) {
+		error("%s: xcgroup_lock error", __func__);
+		goto bail;
+	}
 
 	/* create slurm cgroup in the freezer ns (it could already exist) */
 	if (xcgroup_instantiate(&slurm_freezer_cg) != XCGROUP_SUCCESS)
@@ -272,7 +276,10 @@ static int _move_current_to_root_cgroup(xcgroup_ns_t *ns)
 
 int _slurm_cgroup_destroy(void)
 {
-	xcgroup_lock(&freezer_cg);
+	if (xcgroup_lock(&freezer_cg) != XCGROUP_SUCCESS) {
+		error("%s: xcgroup_lock error", __func__);
+		return SLURM_ERROR;
+	}
 
 	/*
 	 *  First move slurmstepd process to the root cgroup, otherwise

src/plugins/proctrack/linuxproc/kill_tree.c
@@ -341,12 +341,15 @@ extern pid_t find_ancestor(pid_t process, char *process_name)
 			break;
 		}
 		buf_used = read(fd, rbuf, 4096);
+		if (buf_used >= 0)
+			rbuf[buf_used] = '\0';
+		else
+			rbuf[0] = '\0';	
 		if ((buf_used <= 0) || (buf_used >= 4096)) {
 			close(fd);
 			pid = 0;
 			break;
 		}
-		rbuf[buf_used] = '\0';
 		close(fd);
 		if (sscanf(rbuf, "%ld %*s %*s %ld", &pid, &ppid) != 2) {
 			pid = 0;
@@ -358,11 +361,14 @@ extern pid_t find_ancestor(pid_t process, char *process_name)
 			continue;
 		}
 		buf_used = read(fd, rbuf, 4096);
+		if (buf_used >= 0)
+			rbuf[buf_used] = '\0';
+		else
+			rbuf[0] = '\0';	
 		if ((buf_used <= 0) || (buf_used >= 4096)) {
 			close(fd);
 			continue;
 		}
-		rbuf[buf_used] = '\0';
 		close(fd);
 	} while (!strstr(rbuf, process_name));
 	xfree(rbuf);

src/slurmd/common/proctrack.c
@@ -223,7 +223,7 @@ static bool _test_core_dumping(char* stat_fname)
 	if (proc_fd == -1)
 		return false;  /* process is now gone */
 	proc_stat = xmalloc(proc_stat_size + 1);
-	while ((num = read(proc_fd, proc_stat, proc_stat_size)) > 0) {
+	while ((num = read(proc_fd, proc_stat, proc_stat_size)) >= 0) {
 		proc_stat[num] = '\0';
 		if (num < proc_stat_size)
 			break;

src/squeue/opts.c
@@ -1776,6 +1776,7 @@ _build_job_list( char* str )
 		list_append( my_list, job_step_id );
 		job = strtok_r (NULL, ",", &tmp_char);
 	}
+	xfree(my_job_list);
 	return my_list;
 }
 

commit dad0970fb9bed78c91f2f7d0356e9e2e328dea33

Fix for memory bug
This sets a memory pointer to NULL after releasing its memory to
  avoid a duplicate xfree call. Bug introduced in commit
  07ae3e72c28b50cfa81e4d03f40c26d49453c864

src/sacctmgr/file_functions.c
@@ -1867,6 +1867,7 @@ extern void load_sacctmgr_cfg_file (int argc, char **argv)
 						slurm_strerror(rc));
 					rc = SLURM_ERROR;
 					_destroy_sacctmgr_file_opts(file_opts);
+					file_opts = NULL;
 					break;
 				}
 				/* This needs to be commited or
@@ -1879,6 +1880,7 @@ extern void load_sacctmgr_cfg_file (int argc, char **argv)
 			}
 
 			_destroy_sacctmgr_file_opts(file_opts);
+			file_opts = NULL;
 
 			/* assoc_cond if set up above */
 			curr_assoc_list = acct_storage_g_get_assocs(
@@ -1886,7 +1888,7 @@ extern void load_sacctmgr_cfg_file (int argc, char **argv)
 			FREE_NULL_LIST(assoc_cond.cluster_list);
 
 			if (!curr_assoc_list) {
-				exit_code=1;
+				exit_code = 1;
 				fprintf(stderr, " Problem getting assocs "
 					"for this cluster\n");
 				rc = SLURM_ERROR;
@@ -1926,6 +1928,7 @@ extern void load_sacctmgr_cfg_file (int argc, char **argv)
 				break;
 			}
 			_destroy_sacctmgr_file_opts(file_opts);
+			file_opts = NULL;
 			continue;
 		} else if (!parent) {
 			parent = xstrdup("root");
@@ -2026,6 +2029,7 @@ extern void load_sacctmgr_cfg_file (int argc, char **argv)
 				}
 			}
 			_destroy_sacctmgr_file_opts(file_opts);
+			file_opts = NULL;
 			continue;
 		} else if (!xstrcasecmp("User", object)) {
 			file_opts = _parse_options(line+start);
@@ -2136,9 +2140,10 @@ extern void load_sacctmgr_cfg_file (int argc, char **argv)
 			}
 			//info("got a user %s", file_opts->name);
 			_destroy_sacctmgr_file_opts(file_opts);
+			file_opts = NULL;
 			continue;
 		} else {
-			exit_code=1;
+			exit_code = 1;
 			fprintf(stderr,
 				" Misformatted line(%d): %s\n", lc, line);
 			rc = SLURM_ERROR;