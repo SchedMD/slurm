SchedMD/slurm

commit 93adc329eff33fb3aa3f61ea66bd1558ad3164dd

Fix bad logix in proctrack/linuxproc reported by Coverity
This isn't a new bug, but logic in this area changed and Coverity
  reported it as a new problem (duplicate logic). The logic also
  incorrectly used a strstr() rather than strcmp() to find maching
  process names.

src/plugins/proctrack/linuxproc/kill_tree.c
@@ -329,7 +329,7 @@ extern pid_t find_ancestor(pid_t process, char *process_name)
 
 	rbuf = xmalloc_nz(4097);
 	pid = ppid = (long)process;
-	do {
+	while (1) {
 		if (ppid <= 1) {
 			pid = 0;
 			break;
@@ -370,7 +370,9 @@ extern pid_t find_ancestor(pid_t process, char *process_name)
 			continue;
 		}
 		close(fd);
-	} while (!strstr(rbuf, process_name));
+		if (strcmp(rbuf, process_name) == 0)
+			break;
+	}
 	xfree(rbuf);
 
 	return pid;

commit 9ad6275f752292956988da829b309316a758864c

proctrack/linuxproc bug fix
This bug was introduced in commit 93adc329eff33fb3aa3f61ea66bd1558ad3164dd
Symptom is failure of test 2.7

src/plugins/proctrack/linuxproc/kill_tree.c
@@ -324,9 +324,10 @@ extern pid_t find_ancestor(pid_t process, char *process_name)
 {
 	char path[PATH_MAX], *rbuf;
 	ssize_t buf_used;
-	int fd;
+	int fd, len;
 	long pid, ppid;
 
+	len = strlen(process_name);
 	rbuf = xmalloc_nz(4097);
 	pid = ppid = (long)process;
 	while (1) {
@@ -370,7 +371,7 @@ extern pid_t find_ancestor(pid_t process, char *process_name)
 			continue;
 		}
 		close(fd);
-		if (strcmp(rbuf, process_name) == 0)
+		if (strncmp(rbuf, process_name, len) == 0)
 			break;
 	}
 	xfree(rbuf);