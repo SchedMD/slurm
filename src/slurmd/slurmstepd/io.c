SchedMD/slurm

commit c08f892252304962a802ad06cf2039e49f6859ab

ensure stepd -> srun client socket fully shutdown
ensure eio objects get explicitly shutdown when
eio_handle_mainloop exits. currently depending on
whether the order the eio_handle_mainloop and
eio_signal_shutdown get called relative to each other

when stepd is instructed to shut down the socket use
SHUT_RDWR instead of SHUT_RD. just using SHUT_RD can
cause srun to receive ECONNRESET if there's outstanding
data that's been sent to stepd that the task has not
read.

bug 3166

src/slurmd/slurmstepd/io.c
@@ -225,7 +225,7 @@ _client_readable(eio_obj_t *obj)
 	xassert(client->magic == CLIENT_IO_MAGIC);
 
 	if (client->in_eof) {
-		debug5("  false");
+		debug5("  false, in_eof");
 		/* We no longer want the _client_read() function to handle
 		   errors on write now that the read side of the connection
 		   is closed.  Setting handle_read to NULL will result in
@@ -237,7 +237,7 @@ _client_readable(eio_obj_t *obj)
 
 	if (obj->shutdown) {
 		debug5("  false, shutdown");
-		shutdown(obj->fd, SHUT_RD);
+		shutdown(obj->fd, SHUT_RDWR);
 		client->in_eof = true;
 		return false;
 	}

src/common/eio.c
@@ -301,13 +301,16 @@ int eio_handle_mainloop(eio_handle_t *eio)
 			 * Note: xrealloc() also handles initial malloc
 			 */
 		}
+		if (!pollfds)  /* Fix for CLANG false positive */
+			goto done;
 
 		debug4("eio: handling events for %d objects",
 		       list_count(eio->obj_list));
 		nfds = _poll_setup_pollfds(pollfds, map, eio->obj_list);
-		if ((nfds <= 0) ||
-		    (pollfds == NULL))	/* Fix for CLANG false positive */
+		if (nfds <= 0) {
+			_mark_shutdown_true(eio->obj_list);
 			goto done;
+		}
 
 		/*
 		 *  Setup eio handle signalling fd
@@ -318,12 +321,14 @@ int eio_handle_mainloop(eio_handle_t *eio)
 
 		xassert(nfds <= maxnfds + 1);
 
+		/* Get shutdown_time to pass to _poll_internal */
 		slurm_mutex_lock(&eio->shutdown_mutex);
 		shutdown_time = eio->shutdown_time;
 		slurm_mutex_unlock(&eio->shutdown_mutex);
 		if (_poll_internal(pollfds, nfds, shutdown_time) < 0)
 			goto error;
 
+		/* See if we've been told to shut down by eio_signal_shutdown */
 		if (pollfds[nfds-1].revents & POLLIN)
 			_eio_wakeup_handler(eio);
 

commit 435f96a685c059226cf62d32918a5c61b39ae128

I/O fix at job end
Commit c08f892252304962a802ad06cf2039e49f6859ab introduced a bug
  that results in truncated job stdout/stderr.
bug 3166

src/slurmd/slurmstepd/io.c
@@ -237,7 +237,7 @@ _client_readable(eio_obj_t *obj)
 
 	if (obj->shutdown) {
 		debug5("  false, shutdown");
-		shutdown(obj->fd, SHUT_RDWR);
+		shutdown(obj->fd, SHUT_RD);
 		client->in_eof = true;
 		return false;
 	}