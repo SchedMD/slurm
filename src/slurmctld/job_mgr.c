4b4104112fca02657b7ffc905ba23948fed09d9c
Brian Christiansen <brian@schedmd.com>
2017-02-13 16:08:17 -0700

    Revert part of 9a206b9470ab658aedad4
    This part wasn't supposed to go into this commit.

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -8015,8 +8015,7 @@ static int _validate_job_desc(job_desc_msg_t * job_desc_msg, int allocate,
 
 	if (job_desc_msg->job_id != NO_VAL) {
 		struct job_record *dup_job_ptr;
-		if (!fed_mgr_is_active() &&
-		    (submit_uid != 0) &&
+		if ((submit_uid != 0) &&
 		    (submit_uid != slurmctld_conf.slurm_user_id)) {
 			info("attempt by uid %u to set job_id to %u",
 			     submit_uid, job_desc_msg->job_id);
