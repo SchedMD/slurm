b988531d62a78626cbd89917a92cab0859063a38
Morris Jette <jette@schedmd.com>
2017-02-14 11:26:55 -0700

    Defer interactive job allocation until nodes ready
    Defer interactive job allocation until ALL allocated nodes are ready
      rather than after PrologSlurmctld (if any) completes.

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -7488,12 +7488,13 @@ extern bool test_job_nodes_ready(struct job_record *job_ptr)
 	if (bit_overlap(job_ptr->node_bitmap, power_node_bitmap))
 		return false;
 
-	if (job_ptr->wait_all_nodes || job_ptr->burst_buffer) {
+	if (!job_ptr->batch_flag ||
+	    job_ptr->wait_all_nodes || job_ptr->burst_buffer) {
 		/* Make sure all nodes ready to start job */
 		if ((select_g_job_ready(job_ptr) & READY_NODE_STATE) == 0)
 			return false;
 	} else if (job_ptr->batch_flag) {
-		/* Make first node is ready to start batch job */
+		/* Make sure first node is ready to start batch job */
 		int i_first = bit_ffs(job_ptr->node_bitmap);
 		struct node_record *node_ptr = node_record_table_ptr + i_first;
 		if ((i_first != -1) &&
