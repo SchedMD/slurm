c8bc8cfcda6674021cc463e2bf7ff0063155ab71
Brian Christiansen <brian@schedmd.com>
2017-02-24 09:04:21 -0700

    Fix indentations

--- testsuite/slurm_unit/common/slurm_protocol_pack/pack_job_alloc_info_msg-test.c
+++ testsuite/slurm_unit/common/slurm_protocol_pack/pack_job_alloc_info_msg-test.c
@@ -193,16 +193,16 @@ Suite* suite(SRunner *sr)
 
 int main(void)
 {
-    int number_failed;
-    SRunner* sr = srunner_create(NULL);
-    //srunner_set_fork_status(sr, CK_NOFORK);
-    srunner_add_suite(sr, suite(sr));
+	int number_failed;
+	SRunner* sr = srunner_create(NULL);
+	//srunner_set_fork_status(sr, CK_NOFORK);
+	srunner_add_suite(sr, suite(sr));
 
-    srunner_run_all(sr, CK_VERBOSE);
-    //srunner_run_all(sr, CK_NORMAL);
-    number_failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
+	srunner_run_all(sr, CK_VERBOSE);
+	//srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
 
-    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
--- testsuite/slurm_unit/common/slurmdb_pack/pack_used_limits-test.c
+++ testsuite/slurm_unit/common/slurmdb_pack/pack_used_limits-test.c
@@ -140,16 +140,16 @@ Suite* suite(void)
 
 int main(void)
 {
-    int number_failed;
-    SRunner* sr = srunner_create(suite());
+	int number_failed;
+	SRunner* sr = srunner_create(suite());
 
-    srunner_set_fork_status(sr, CK_NOFORK);
+	srunner_set_fork_status(sr, CK_NOFORK);
 
-    srunner_run_all(sr, CK_VERBOSE);
-    //srunner_run_all(sr, CK_NORMAL);
-    number_failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
+	srunner_run_all(sr, CK_VERBOSE);
+	//srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
 
-    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
