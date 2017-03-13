80c282f1aba77c0b117048419dbf62df06df4ffd
Danny Auble <da@schedmd.com>
2017-03-13 16:27:08 -0600

    OK, this one is what we are all waiting for.
    What this does is removes the join on the step table during rollups.  It turns
    out this was a very costly join if the tables were fairly large.
    
    We can now just grab the data directly from the job_table which already contains
    the data in the tres_alloc column.

--- src/plugins/accounting_storage/mysql/as_mysql_rollup.c
+++ src/plugins/accounting_storage/mysql/as_mysql_rollup.c
@@ -363,10 +363,16 @@ static void _add_tres_time_2_list(List tres_list, char *tres_str,
 				loc_seconds = 0;
 		}
 
-		count = slurm_atoull(++tmp_str);
-		time = count * loc_seconds;
+		time = count = slurm_atoull(++tmp_str);
+		/* ENERGY is already totalled for the entire job so don't
+		 * multiple with time.
+		 */
+		if (id != TRES_ENERGY)
+			time *= loc_seconds;
+
 		loc_tres = _add_time_tres(tres_list, type, id,
 					  time, times_count);
+
 		if (loc_tres && !loc_tres->count)
 			loc_tres->count = count;
 
@@ -927,8 +933,7 @@ extern int as_mysql_hourly_rollup(mysql_conn_t *mysql_conn,
 		"job.time_suspended",
 		"job.cpus_req",
 		"job.id_resv",
-		"job.tres_alloc",
-		"SUM(step.consumed_energy)"
+		"job.tres_alloc"
 	};
 	char *job_str = NULL;
 	enum {
@@ -944,7 +949,6 @@ extern int as_mysql_hourly_rollup(mysql_conn_t *mysql_conn,
 		JOB_REQ_RCPU,
 		JOB_REQ_RESVID,
 		JOB_REQ_TRES,
-		JOB_REQ_ENERGY,
 		JOB_REQ_COUNT
 	};
 
@@ -1131,9 +1135,6 @@ extern int as_mysql_hourly_rollup(mysql_conn_t *mysql_conn,
 
 		/* now get the jobs during this time only  */
 		query = xstrdup_printf("select %s from \"%s_%s\" as job "
-				       "left outer join \"%s_%s\" as step on "
-				       "job.job_db_inx=step.job_db_inx "
-				       "and (step.id_step>=0) "
 				       "where (job.time_eligible && "
 				       "job.time_eligible < %ld && "
 				       "(job.time_end >= %ld || "
@@ -1142,7 +1143,6 @@ extern int as_mysql_hourly_rollup(mysql_conn_t *mysql_conn,
 				       "order by job.id_assoc, "
 				       "job.time_eligible",
 				       job_str, cluster_name, job_table,
-				       cluster_name, step_table,
 				       curr_end, curr_start);
 
 		if (debug_flags & DEBUG_FLAG_DB_USAGE)
@@ -1166,12 +1166,9 @@ extern int as_mysql_hourly_rollup(mysql_conn_t *mysql_conn,
 			time_t row_end = slurm_atoul(row[JOB_REQ_END]);
 			uint32_t row_rcpu = slurm_atoul(row[JOB_REQ_RCPU]);
 			List loc_tres = NULL;
-			uint64_t row_energy = 0;
 			int loc_seconds = 0;
 			int seconds = 0, suspend_seconds = 0;
 
-			if (row[JOB_REQ_ENERGY])
-				row_energy = slurm_atoull(row[JOB_REQ_ENERGY]);
 			if (row_start && (row_start < curr_start))
 				row_start = curr_start;
 
@@ -1283,15 +1280,6 @@ extern int as_mysql_hourly_rollup(mysql_conn_t *mysql_conn,
 						      row[JOB_REQ_TRES],
 						      TIME_ALLOC, seconds,
 						      suspend_seconds, 0);
-
-			_add_time_tres(loc_tres,
-				       TIME_ALLOC, TRES_ENERGY,
-				       row_energy, 0);
-			if (w_usage)
-				_add_time_tres(
-					w_usage->loc_tres,
-					TIME_ALLOC, TRES_ENERGY,
-					row_energy, 0);
 
 			/* Now figure out there was a disconnected
 			   slurmctld durning this job.
