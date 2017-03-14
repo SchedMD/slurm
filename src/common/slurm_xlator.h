SchedMD/slurm

commit 5a5347c75f1371bb20806f43a23798e083fdad43

Export missing slurmdbd_defs_[init|fini] needed for libslurmdb.so to work.

src/common/slurm_xlator.h
@@ -350,6 +350,8 @@
 #define reservation_flags_string slurm_reservation_flags_string
 
 /* slurmdbd_defs.[ch] functions */
+#define slurmdbd_defs_init      slurmdb_slurmdbd_defs_init
+#define slurmdbd_defs_fini      slurmdb_slurmdbd_defs_fini
 #define slurmdbd_free_list_msg	slurmdb_slurmdbd_free_list_msg
 #define slurmdbd_free_usage_msg slurmdb_slurmdbd_free_usage_msg
 #define slurmdbd_free_id_rc_msg slurmdb_slurmdbd_free_id_rc_msg

src/common/slurmdbd_defs.c
@@ -69,10 +69,11 @@
 ** Define slurm-specific aliases for use by plugins, see slurm_xlator.h
 ** for details.
  */
+strong_alias(slurmdbd_defs_init,        slurmdb_slurmdbd_defs_init);
+strong_alias(slurmdbd_defs_fini,        slurmdb_slurmdbd_defs_fini);
 strong_alias(slurmdbd_free_list_msg,	slurmdb_slurmdbd_free_list_msg);
 strong_alias(slurmdbd_free_usage_msg,	slurmdb_slurmdbd_free_usage_msg);
 strong_alias(slurmdbd_free_id_rc_msg,	slurmdb_slurmdbd_free_id_rc_msg);
-
 
 #define DBD_MAGIC		0xDEAD3219
 #define MAX_AGENT_QUEUE		10000

commit 7eef69e378baa85988c94e7889ef80c7fd75c3bc

Fix slurmdbd_defs.c to not have half symbols go to libslurm.so and the other half go to libslurmdb.so.
Turns out in 17.11 if you are using --with-shared-libslurm sacct will link to
both libslurmfull.so and libslurmdb.so.  In the case of linking to the
accounting_storage/slurmdbd plugin it will callback to slurmdbd_defs_init
which would be in libslurmdb.so but the call to acct_storage_p_get_connection
would call slurm_open_slurmdbd_conn which is in libslurmfull.so and it would
xassert on slurmdbd_defs_inited which was set in libslurmdb.so but not in
libslurmfull.so.

So the moral of the story is don't export half a file in one lib and the other
half in a different lib.

Perhaps we should only have 1 lib all together, but that isn't the way it is
done today.  This fixes the issue and has the entire file exported to
libslurmfull.so so we should be good.  But just a note for the future.

This was an unexpected regression caused by commit 5a5347c75f1371.

src/common/slurm_xlator.h
@@ -350,11 +350,11 @@
 #define reservation_flags_string slurm_reservation_flags_string
 
 /* slurmdbd_defs.[ch] functions */
-#define slurmdbd_defs_init      slurmdb_slurmdbd_defs_init
-#define slurmdbd_defs_fini      slurmdb_slurmdbd_defs_fini
-#define slurmdbd_free_list_msg	slurmdb_slurmdbd_free_list_msg
-#define slurmdbd_free_usage_msg slurmdb_slurmdbd_free_usage_msg
-#define slurmdbd_free_id_rc_msg slurmdb_slurmdbd_free_id_rc_msg
+#define slurmdbd_defs_init      slurm_slurmdbd_defs_init
+#define slurmdbd_defs_fini      slurm_slurmdbd_defs_fini
+#define slurmdbd_free_list_msg	slurm_slurmdbd_free_list_msg
+#define slurmdbd_free_usage_msg slurm_slurmdbd_free_usage_msg
+#define slurmdbd_free_id_rc_msg slurm_slurmdbd_free_id_rc_msg
 
 /* plugin.[ch] functions */
 #define plugin_get_syms         slurm_plugin_get_syms

src/common/slurmdbd_defs.c
@@ -69,11 +69,11 @@
 ** Define slurm-specific aliases for use by plugins, see slurm_xlator.h
 ** for details.
  */
-strong_alias(slurmdbd_defs_init,        slurmdb_slurmdbd_defs_init);
-strong_alias(slurmdbd_defs_fini,        slurmdb_slurmdbd_defs_fini);
-strong_alias(slurmdbd_free_list_msg,	slurmdb_slurmdbd_free_list_msg);
-strong_alias(slurmdbd_free_usage_msg,	slurmdb_slurmdbd_free_usage_msg);
-strong_alias(slurmdbd_free_id_rc_msg,	slurmdb_slurmdbd_free_id_rc_msg);
+strong_alias(slurmdbd_defs_init,        slurm_slurmdbd_defs_init);
+strong_alias(slurmdbd_defs_fini,        slurm_slurmdbd_defs_fini);
+strong_alias(slurmdbd_free_list_msg,	slurm_slurmdbd_free_list_msg);
+strong_alias(slurmdbd_free_usage_msg,	slurm_slurmdbd_free_usage_msg);
+strong_alias(slurmdbd_free_id_rc_msg,	slurm_slurmdbd_free_id_rc_msg);
 
 #define DBD_MAGIC		0xDEAD3219
 #define MAX_AGENT_QUEUE		10000