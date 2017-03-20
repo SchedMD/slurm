SchedMD/slurm

commit 1b0d98513768669d9a7a396ace18d8223a5a121e

Removed slurmd_allocation_lookup()
Since it's not used and slurm_allocation_lookup_lite() is, we're going
to rename slurm_allocation_allocation_lite() to
slurm_allocation_allocation(). The rename is in the next commit.

src/common/slurm_protocol_defs.h
@@ -285,8 +285,6 @@ typedef enum {
 	RESPONSE_JOB_ATTACH,
 	REQUEST_JOB_WILL_RUN,
 	RESPONSE_JOB_WILL_RUN,
-	REQUEST_JOB_ALLOCATION_INFO,
-	RESPONSE_JOB_ALLOCATION_INFO,
 	REQUEST_JOB_ALLOCATION_INFO_LITE,
 	RESPONSE_JOB_ALLOCATION_INFO_LITE,
 	REQUEST_UPDATE_JOB_TIME,
@@ -1411,8 +1409,6 @@ extern void slurm_free_resource_allocation_response_msg_members (
 	resource_allocation_response_msg_t * msg);
 extern void slurm_free_resource_allocation_response_msg (
 		resource_allocation_response_msg_t * msg);
-extern void slurm_free_job_alloc_info_response_msg (
-		job_alloc_info_response_msg_t * msg);
 extern void slurm_free_job_step_create_response_msg(
 		job_step_create_response_msg_t * msg);
 extern void slurm_free_submit_response_response_msg(

contribs/perlapi/libslurm/perl/Slurm.xs
@@ -458,32 +458,6 @@ slurm_allocate_resources_blocking(slurm_t self, HV *user_req, time_t timeout=0, 
 
 
 HV *
-slurm_allocation_lookup(slurm_t self, uint32_t job_id)
-	PREINIT:
-		job_alloc_info_response_msg_t *resp_msg = NULL;
-		int rc;
-	CODE:
-		if (self); /* this is needed to avoid a warning about
-			      unused variables.  But if we take slurm_t self
-			      out of the mix Slurm-> doesn't work,
-			      only Slurm::
-			    */
-		rc = slurm_allocation_lookup(job_id, &resp_msg);
-		if(rc != SLURM_SUCCESS) {
-			slurm_free_job_alloc_info_response_msg(resp_msg);
-			XSRETURN_UNDEF;
-		}
-		RETVAL = newHV();
-		sv_2mortal((SV*)RETVAL);
-		rc = job_alloc_info_response_msg_to_hv(resp_msg, RETVAL);
-		slurm_free_job_alloc_info_response_msg(resp_msg);
-		if (rc < 0) {
-			XSRETURN_UNDEF;
-		}
-	OUTPUT:
-		RETVAL
-
-HV *
 slurm_allocation_lookup_lite(slurm_t self, uint32_t job_id)
 	PREINIT:
 		resource_allocation_response_msg_t *resp_msg = NULL;

contribs/perlapi/libslurm/perl/alloc.c
@@ -279,46 +279,6 @@ resource_allocation_response_msg_to_hv(resource_allocation_response_msg_t *resp_
 }
 
 /*
- * convert job_alloc_info_response_msg_t to perl HV
- */
-int
-job_alloc_info_response_msg_to_hv(job_alloc_info_response_msg_t *resp_msg, HV* hv)
-{
-	AV* av;
-	int i;
-
-	STORE_FIELD(hv, resp_msg, job_id, uint32_t);
-	if(resp_msg->node_list)
-		STORE_FIELD(hv, resp_msg, node_list, charp);
-	STORE_FIELD(hv, resp_msg, num_cpu_groups, uint16_t);
-	if(resp_msg->num_cpu_groups) {
-		av = newAV();
-		for(i = 0; i < resp_msg->num_cpu_groups; i ++) {
-			av_store_uint16_t(av, i, resp_msg->cpus_per_node[i]);
-		}
-		hv_store_sv(hv, "cpus_per_node", newRV_noinc((SV*)av));
-
-		av = newAV();
-		for(i = 0; i < resp_msg->num_cpu_groups; i ++) {
-			av_store_uint32_t(av, i, resp_msg->cpu_count_reps[i]);
-		}
-		hv_store_sv(hv, "cpu_count_reps", newRV_noinc((SV*)av));
-	}
-	STORE_FIELD(hv, resp_msg, node_cnt, uint32_t);
-	if(resp_msg->node_cnt) {
-		av = newAV();
-		for(i = 0; i < resp_msg->node_cnt; i ++) {
-			/* XXX: This is a packed inet address */
-			av_store(av, i, newSVpvn((char*)(resp_msg->node_addr + i), sizeof(slurm_addr_t)));
-		}
-		hv_store_sv(hv, "node_addr", newRV_noinc((SV*)av));
-	}
-	STORE_FIELD(hv, resp_msg, error_code, uint32_t);
-	STORE_PTR_FIELD(hv, resp_msg, select_jobinfo, "Slurm::dynamic_plugin_data_t");
-	return 0;
-}
-
-/*
  * convert submit_response_msg_t to perl HV
  */
 int

contribs/perlapi/libslurm/perl/lib/Slurm.pm
@@ -394,18 +394,6 @@ pending callback is given, it will be called with the job id of the pending job 
     
 =back
 
-=head3 $resp = $slurm->allocation_lookup($job_id);
-
-Retrieve info for an existing resource allocation.
-
-=over 2
-
-=item * IN $job_id: job allocation identifier.
-
-=item * RET: job allocation info, with structure of C<job_alloc_info_response_msg_t>. On failure C<undef> is returned with errno set.    
-
-=back
-
 =head3 $resp = $slurm->allocatiion_lookup_lite($job_id);
 
 Retrieve minor info for an existing resource allocation.

contribs/perlapi/libslurm/perl/slurm-perl.h
@@ -47,8 +47,6 @@ extern int hv_to_job_desc_msg(HV *hv, job_desc_msg_t *job_desc);
 extern void free_job_desc_msg_memory(job_desc_msg_t *msg);
 extern int resource_allocation_response_msg_to_hv(
     resource_allocation_response_msg_t *resp_msg, HV *hv);
-extern int job_alloc_info_response_msg_to_hv(job_alloc_info_response_msg_t
-					     *resp_msg, HV *hv);
 extern int submit_response_msg_to_hv(submit_response_msg_t *resp_msg, HV *hv);
 extern int job_sbcast_cred_msg_to_hv(job_sbcast_cred_msg_t *msg, HV *hv);
 extern int srun_job_complete_msg_to_hv(srun_job_complete_msg_t *msg, HV *hv);

contribs/perlapi/libslurm/perl/t/04-alloc.c
@@ -32,20 +32,12 @@ ok($jobid, "allocate resources blocking") or diag("allocate_resources_blocking: 
 # 4
 SKIP: {
     skip "resource allocation fail", 1 unless $jobid;
-    $resp = $slurm->allocation_lookup($jobid);
-    ok(defined $resp, "allocation lookup") or diag("allocation_lookup: " . $slurm->strerror());
-}
-
-
-# 5
-SKIP: {
-    skip "resource allocation fail", 1 unless $jobid;
     $resp = $slurm->allocation_lookup_lite($jobid);
     ok(defined $resp, "allocation lookup lite") or diag("allocation_lookup_lite: " . $slurm->strerror());
 }
 
 
-# 6
+# 5
 $callbacks = {
     ping => sub { print STDERR "ping from slurmctld, $_->{job_id}.$_->{step_id}\n"; },
     job_complete => sub { print STDERR "job complete, $_->{job_id}.$_->{step_id}\n"; },
@@ -59,7 +51,7 @@ ok(ref($thr) eq "Slurm::allocation_msg_thread_t" && defined $port, "allocation m
 $slurm->allocation_msg_thr_destroy($thr) if $thr;
 
 
-# 7
+# 6
 SKIP: {
     skip "resource allocation fail", 1 unless $jobid;
     $resp = $slurm->sbcast_lookup($jobid);
@@ -68,19 +60,19 @@ SKIP: {
 $slurm->kill_job($jobid, SIGKILL) if $jobid;
 
 
-# 8
+# 7
 $job_desc->{script} = "#!/bin/sh\nsleep 1000\n";
 $resp = $slurm->submit_batch_job($job_desc);
 ok($resp, "submit batch job") or diag("submit_batch_job: " . $slurm->strerror());
 $slurm->kill_job($resp->{job_id}, SIGKILL) if $resp;
 
 
-# 9 
+# 8
 $rc = $slurm->job_will_run($job_desc);
 ok(defined $rc, "job will run") or diag("job_will_run: " . $slurm->strerror());
 
 
-# 10
+# 9
 SKIP: {
     skip "do not know how to test", 1 if 1;
     $hl = $slurm->read_hostfile($file, 8);

doc/man/man3/Makefile.am
@@ -3,7 +3,6 @@ man3_MANS = slurm_hostlist_create.3 \
 	slurm_hostlist_shift.3 \
 	slurm_allocate_resources.3 \
 	slurm_allocate_resources_blocking.3 \
-	slurm_allocation_lookup.3 \
 	slurm_allocation_lookup_lite.3 \
 	slurm_allocation_msg_thr_create.3 \
 	slurm_allocation_msg_thr_destroy.3 \

doc/man/man3/slurm_allocate_resources.3
@@ -3,8 +3,7 @@
 .SH "NAME"
 slurm_allocate_resources, slurm_allocate_resources_blocking,
 slurm_allocation_msg_thr_create, slurm_allocation_msg_thr_destroy,
-slurm_allocation_lookup, slurm_allocation_lookup_lite,
-slurm_confirm_allocation,
+slurm_allocation_lookup_lite, slurm_confirm_allocation,
 slurm_free_submit_response_response_msg, slurm_init_job_desc_msg,
 slurm_job_will_run, slurm_job_will_run2,
 slurm_read_hostfile, slurm_submit_batch_job
@@ -40,14 +39,6 @@ allocation_msg_thread_t *\fBslurm_allocation_msg_thr_create\fR (
 void *\fBslurm_allocation_msg_thr_destroy\fR (
 .br
 	allocation_msg_thread_t *\fIslurm_alloc_msg_thr_ptr\fP
-.br
-);
-.LP
-int \fBslurm_allocation_lookup\fR {
-.br
-	uint32_t \fIjobid\fP,
-.br
-	resource_allocation_response_msg_t **\fIslurm_alloc_msg_pptr\fP
 .br
 );
 .LP
@@ -137,7 +128,7 @@ structure's contents.
 Specifies the pointer to the structure to be created and filled in by the function
 \fIslurm_allocate_resources\fP,
 \fIslurm_allocate_resources_blocking\fP,
-\fIslurm_allocation_lookup\fP, \fIslurm_allocation_lookup_lite\fP,
+\fIslurm_allocation_lookup_lite\fP,
 \fIslurm_confirm_allocation\fP, \fIslurm_job_will_run\fP or
 \fIslurm_job_will_run\fP.
 .TP
@@ -199,8 +190,8 @@ instead.\fR This function may only be successfully executed by the job's
 owner or user root.
 .LP
 \fBslurm_free_resource_allocation_response_msg\fR Release the storage generated in response
-to a call of the function \fBslurm_allocate_resources\fR,
-\fBslurm_allocation_lookup\fR, or \fBslurm_allocation_lookup_lite\fR.
+to a call of the function \fBslurm_allocate_resources\fR
+or \fBslurm_allocation_lookup_lite\fR.
 .LP
 \fBslurm_free_submit_response_msg\fR Release the storage generated in response
 to a call of the function \fBslurm_submit_batch_job\fR.

commit 0809af4f6db8963e51d14e37aae4660a49eba6ab

Restore removed enum
Removing values in the middle of an enum will shift all of the values
and break backward compatability for old RPCs..
Broken in commit 1b0d98513768669d9a7a396ace18d8223a5a121e

src/common/slurm_protocol_defs.h
@@ -251,7 +251,7 @@ typedef enum {
 	RESPONSE_POWERCAP_INFO,
 	REQUEST_ASSOC_MGR_INFO,
 	RESPONSE_ASSOC_MGR_INFO,
-	REQUEST_EVENT_LOG,		/* DEFUNCT */
+	REQUEST_EVENT_LOG,
 	RESPONSE_SICP_INFO_DEFUNCT,	/* DEFUNCT */
 	REQUEST_LAYOUT_INFO,
 	RESPONSE_LAYOUT_INFO,
@@ -287,6 +287,8 @@ typedef enum {
 	RESPONSE_JOB_WILL_RUN,
 	REQUEST_JOB_ALLOCATION_INFO,
 	RESPONSE_JOB_ALLOCATION_INFO,
+	REQUEST_JOB_ALLOCATION_INFO_LITE,	/* DEFUNCT */
+	RESPONSE_JOB_ALLOCATION_INFO_LITE,	/* DEFUNCT */
 	REQUEST_UPDATE_JOB_TIME,
 	REQUEST_JOB_READY,
 	RESPONSE_JOB_READY,		/* 4020 */
@@ -299,7 +301,7 @@ typedef enum {
 	REQUEST_SIB_JOB_REQUEUE,
 	REQUEST_SIB_JOB_COMPLETE,
 	REQUEST_SIB_JOB_LOCK,
-	REQUEST_SIB_JOB_UNLOCK,
+	REQUEST_SIB_JOB_UNLOCK,		/* 4030 */
 	REQUEST_SIB_JOB_WILL_RUN,
 	REQUEST_SIB_SUBMIT_BATCH_JOB,
 	REQUEST_SIB_RESOURCE_ALLOCATION,