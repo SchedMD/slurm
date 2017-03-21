2b36adecdb01f5a667db618175e13db8e11b1bd6
Danny Auble <da@schedmd.com>
2017-03-21 16:19:39 -0600

    Fix --ntasks-per-node issue with srun so DenyOnLimit would work correctly.
    Bug 3097

--- src/srun/libsrun/opt.c
+++ src/srun/libsrun/opt.c
@@ -2319,6 +2319,11 @@ static bool _opt_verify(void)
 
 	} /* else if (opt.ntasks_set && !opt.nodes_set) */
 
+	if ((opt.ntasks_per_node != NO_VAL) && (!opt.ntasks_set)) {
+		opt.ntasks = opt.min_nodes * opt.ntasks_per_node;
+		opt.ntasks_set = 1;
+	}
+
 	if (hl)
 		hostlist_destroy(hl);
 
