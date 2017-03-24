SchedMD/slurm

commit c8bf6b5dce359aa972db2af712356acbf56d80ad

Be able to request job info for specific jobs.

src/common/slurm_protocol_defs.c
@@ -734,7 +734,10 @@ extern void slurm_free_job_step_kill_msg(job_step_kill_msg_t * msg)
 
 extern void slurm_free_job_info_request_msg(job_info_request_msg_t *msg)
 {
-	xfree(msg);
+	if (msg) {
+		xfree(msg->job_ids);
+		xfree(msg);
+	}
 }
 
 extern void slurm_free_job_step_info_request_msg(job_step_info_request_msg_t *msg)
@@ -4261,6 +4264,9 @@ extern int slurm_free_msg_data(slurm_msg_type_t type, void *data)
 	case REQUEST_CTLD_MULT_MSG:
 	case RESPONSE_CTLD_MULT_MSG:
 		slurm_free_ctld_multi_msg(data);
+		break;
+	case RESPONSE_JOB_INFO:
+		slurm_free_job_info(data);
 		break;
 	default:
 		error("invalid type trying to be freed %u", type);

RELEASE_NOTES
@@ -91,6 +91,8 @@ In struct job_details: Added cluster_features to hold requestsed cluster
 	features.
 In job_fed_details_t: Rename siblings to siblings_active.
 		      Added siblings_viable.
+In job_info_request_msg: Added job_ids to be able to request job info for
+		       specific jobs.
 
 Added the following struct definitions
 ======================================

src/api/job_info.c
@@ -1025,7 +1025,7 @@ slurm_load_jobs (time_t update_time, job_info_msg_t **job_info_msg_pptr,
 	int rc;
 	slurm_msg_t resp_msg;
 	slurm_msg_t req_msg;
-	job_info_request_msg_t req;
+	job_info_request_msg_t req = {0};
 
 	slurm_msg_t_init(&req_msg);
 	slurm_msg_t_init(&resp_msg);

src/common/slurm_protocol_defs.h
@@ -612,6 +612,8 @@ typedef struct job_step_id_msg {
 typedef struct job_info_request_msg {
 	time_t last_update;
 	uint16_t show_flags;
+	List   job_ids;		/* Optional list of job_ids, otherwise show all
+				 * jobs. */
 } job_info_request_msg_t;
 
 typedef struct job_step_info_request_msg {

src/common/slurm_protocol_pack.c
@@ -10104,12 +10104,28 @@ static void
 _pack_job_info_request_msg(job_info_request_msg_t * msg, Buf buffer,
 			   uint16_t protocol_version)
 {
+	uint32_t count = NO_VAL;
+	ListIterator itr;
+
 	xassert(msg);
 	xassert(buffer);
 
 	if (protocol_version >= SLURM_17_11_PROTOCOL_VERSION) {
 		pack_time(msg->last_update, buffer);
 		pack16((uint16_t)msg->show_flags, buffer);
+
+		if (msg->job_ids)
+			count = list_count(msg->job_ids);
+
+		pack32(count, buffer);
+		if (count && count != NO_VAL) {
+			itr = list_iterator_create(msg->job_ids);
+			uint32_t *uint32_ptr;
+			while ((uint32_ptr = list_next(itr)))
+				pack32(*uint32_ptr, buffer);
+			list_iterator_destroy(itr);
+		}
+
 	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		pack_time(msg->last_update, buffer);
 		pack16((uint16_t)msg->show_flags, buffer);
@@ -10124,6 +10140,9 @@ _unpack_job_info_request_msg(job_info_request_msg_t** msg,
 			     Buf buffer,
 			     uint16_t protocol_version)
 {
+	int       i;
+	uint32_t  count;
+	uint32_t *uint32_ptr;
 	job_info_request_msg_t *job_info;
 
 	job_info = xmalloc(sizeof(job_step_info_request_msg_t));
@@ -10132,6 +10151,18 @@ _unpack_job_info_request_msg(job_info_request_msg_t** msg,
 	if (protocol_version >= SLURM_17_11_PROTOCOL_VERSION) {
 		safe_unpack_time(&job_info->last_update, buffer);
 		safe_unpack16(&job_info->show_flags, buffer);
+
+		safe_unpack32(&count, buffer);
+		if (count != NO_VAL) {
+			job_info->job_ids =
+				list_create(slurm_destroy_uint32_ptr);
+			for (i = 0; i < count; i++) {
+				uint32_ptr = xmalloc(sizeof(uint32_t));
+				safe_unpack32(uint32_ptr, buffer);
+				list_append(job_info->job_ids, uint32_ptr);
+				uint32_ptr = NULL;
+			}
+		}
 	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		safe_unpack_time(&job_info->last_update, buffer);
 		safe_unpack16(&job_info->show_flags, buffer);

src/slurmctld/job_mgr.c
@@ -8615,6 +8615,77 @@ extern void pack_all_jobs(char **buffer_ptr, int *buffer_size,
 }
 
 /*
+ * pack_spec_jobs - dump job information for specified jobs in
+ *	machine independent form (for network transmission)
+ * OUT buffer_ptr - the pointer is set to the allocated buffer.
+ * OUT buffer_size - set to size of the buffer in bytes
+ * IN show_flags - job filtering options
+ * IN job_ids - list of job_ids to pack
+ * IN uid - uid of user making request (for partition filtering)
+ * IN filter_uid - pack only jobs belonging to this user if not NO_VAL
+ * global: job_list - global list of job records
+ * NOTE: the buffer at *buffer_ptr must be xfreed by the caller
+ * NOTE: change _unpack_job_desc_msg() in common/slurm_protocol_pack.c
+ *	whenever the data format changes
+ */
+extern void pack_spec_jobs(char **buffer_ptr, int *buffer_size, List job_ids,
+			   uint16_t show_flags, uid_t uid, uint32_t filter_uid,
+			   uint16_t protocol_version)
+{
+	ListIterator itr;
+	struct job_record *job_ptr;
+	uint32_t jobs_packed = 0, tmp_offset, *jobid_ptr;
+	Buf buffer;
+
+	xassert(job_ids);
+
+	buffer_ptr[0] = NULL;
+	*buffer_size = 0;
+
+	buffer = init_buf(BUF_SIZE);
+
+	/* write message body header : size and time */
+	/* put in a place holder job record count of 0 for now */
+	pack32(jobs_packed, buffer);
+	pack_time(time(NULL), buffer);
+
+	/* write individual job records */
+	part_filter_set(uid);
+	itr = list_iterator_create(job_ids);
+	while ((jobid_ptr = (uint32_t *)list_next(itr))) {
+
+		if (!(job_ptr = find_job_record(*jobid_ptr)))
+			continue;
+
+		xassert (job_ptr->magic == JOB_MAGIC);
+
+		if (((show_flags & SHOW_ALL) == 0) && (uid != 0) &&
+		    _all_parts_hidden(job_ptr))
+			continue;
+
+		if (_hide_job(job_ptr, uid, show_flags))
+			continue;
+
+		if ((filter_uid != NO_VAL) && (filter_uid != job_ptr->user_id))
+			continue;
+
+		pack_job(job_ptr, show_flags, buffer, protocol_version, uid);
+		jobs_packed++;
+	}
+	list_iterator_destroy(itr);
+	part_filter_clear();
+
+	/* put the real record count in the message body header */
+	tmp_offset = get_buf_offset(buffer);
+	set_buf_offset(buffer, 0);
+	pack32(jobs_packed, buffer);
+	set_buf_offset(buffer, tmp_offset);
+
+	*buffer_size = get_buf_offset(buffer);
+	buffer_ptr[0] = xfer_buf_data(buffer);
+}
+
+/*
  * pack_one_job - dump information for one jobs in
  *	machine independent form (for network transmission)
  * OUT buffer_ptr - the pointer is set to the allocated buffer.

src/slurmctld/proc_req.c
@@ -1343,9 +1343,16 @@ static void _slurm_rpc_dump_jobs(slurm_msg_t * msg)
 		debug3("_slurm_rpc_dump_jobs, no change");
 		slurm_send_rc_msg(msg, SLURM_NO_CHANGE_IN_DATA);
 	} else {
-		pack_all_jobs(&dump, &dump_size,
-			      job_info_request_msg->show_flags, uid, NO_VAL,
-			      msg->protocol_version);
+		if (job_info_request_msg->job_ids) {
+			pack_spec_jobs(&dump, &dump_size,
+				       job_info_request_msg->job_ids,
+				       job_info_request_msg->show_flags, uid,
+				       NO_VAL, msg->protocol_version);
+		} else {
+			pack_all_jobs(&dump, &dump_size,
+				      job_info_request_msg->show_flags, uid,
+				      NO_VAL, msg->protocol_version);
+		}
 		unlock_slurmctld(job_read_lock);
 		END_TIMER2("_slurm_rpc_dump_jobs");
 #if 0

src/slurmctld/slurmctld.h
@@ -1789,6 +1789,24 @@ extern void pack_all_jobs(char **buffer_ptr, int *buffer_size,
 			  uint16_t protocol_version);
 
 /*
+ * pack_spec_jobs - dump job information for specified jobs in
+ *	machine independent form (for network transmission)
+ * OUT buffer_ptr - the pointer is set to the allocated buffer.
+ * OUT buffer_size - set to size of the buffer in bytes
+ * IN show_flags - job filtering options
+ * IN job_ids - list of job_ids to pack
+ * IN uid - uid of user making request (for partition filtering)
+ * IN filter_uid - pack only jobs belonging to this user if not NO_VAL
+ * global: job_list - global list of job records
+ * NOTE: the buffer at *buffer_ptr must be xfreed by the caller
+ * NOTE: change _unpack_job_desc_msg() in common/slurm_protocol_pack.c
+ *	whenever the data format changes
+ */
+extern void pack_spec_jobs(char **buffer_ptr, int *buffer_size, List job_ids,
+			   uint16_t show_flags, uid_t uid, uint32_t filter_uid,
+			   uint16_t protocol_version);
+
+/*
  * pack_all_node - dump all configuration and node information for all nodes
  *	in machine independent form (for network transmission)
  * OUT buffer_ptr - pointer to the stored data

commit a3ba9d747d102f86fec570c7f1436ef7709bcf63

Change xfree to FREE_NULL_LIST
The wrong function was being called to release memory.
Bug introduced yesterday in commit c8bf6b5dce359aa972

src/common/slurm_protocol_defs.c
@@ -743,7 +743,7 @@ extern void slurm_free_job_step_kill_msg(job_step_kill_msg_t * msg)
 extern void slurm_free_job_info_request_msg(job_info_request_msg_t *msg)
 {
 	if (msg) {
-		xfree(msg->job_ids);
+		FREE_NULL_LIST(msg->job_ids);						//
 		xfree(msg);
 	}
 }