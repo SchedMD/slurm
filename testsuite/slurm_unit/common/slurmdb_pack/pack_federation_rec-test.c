9fd35764fb6655c96109f01e6124e6acf823b04f
Brian Christiansen <brian@schedmd.com>
2017-04-03 15:29:52 -0600

    Update federation_rec unit tests for 17.11 proto

--- testsuite/slurm_unit/common/slurmdb_pack/pack_federation_rec-test.c
+++ testsuite/slurm_unit/common/slurmdb_pack/pack_federation_rec-test.c
@@ -142,6 +142,91 @@ START_TEST(pack_1702_federation_rec_fail)
 END_TEST
 
 
+/* 17.11 */
+START_TEST(pack_1711_null_federation_rec)
+{
+	int rc;
+	Buf buf = init_buf(1024);
+
+	slurmdb_pack_federation_rec(NULL, SLURM_17_11_PROTOCOL_VERSION, buf);
+
+	set_buf_offset(buf, 0);
+
+	slurmdb_federation_rec_t *unpack_fr = NULL;
+	rc = slurmdb_unpack_federation_rec((void **)&unpack_fr, SLURM_17_11_PROTOCOL_VERSION, buf);
+	ck_assert(rc        == SLURM_SUCCESS);
+	ck_assert(unpack_fr == NULL);
+
+	free_buf(buf);
+	slurmdb_destroy_federation_rec(unpack_fr);
+}
+END_TEST
+
+START_TEST(pack_1711_federation_rec)
+{
+	int rc;
+
+	slurmdb_federation_rec_t *pack_fr = xmalloc(sizeof(slurmdb_federation_rec_t));
+	pack_fr->flags        = 7;
+	pack_fr->name         = xstrdup("Saint Augustine");
+	pack_fr->cluster_list = list_create(slurmdb_destroy_cluster_rec);
+
+	slurmdb_cluster_rec_t *x = xmalloc(sizeof(slurmdb_cluster_rec_t));
+	x->name = xstrdup("Thomas Aquinas");
+
+	list_append(pack_fr->cluster_list, x);
+
+	Buf buf = init_buf(1024);
+	slurmdb_pack_federation_rec(pack_fr, SLURM_17_11_PROTOCOL_VERSION, buf);
+
+	set_buf_offset(buf, 0);
+
+	slurmdb_federation_rec_t *unpack_fr;
+	rc = slurmdb_unpack_federation_rec((void **)&unpack_fr, SLURM_17_11_PROTOCOL_VERSION, buf);
+	ck_assert(rc                    == SLURM_SUCCESS);
+	ck_assert(pack_fr->flags        == unpack_fr->flags);
+	ck_assert_str_eq(pack_fr->name, unpack_fr->name);
+
+	slurmdb_cluster_rec_t *before = (slurmdb_cluster_rec_t *)list_peek(pack_fr->cluster_list);
+ 	slurmdb_cluster_rec_t *after  = (slurmdb_cluster_rec_t *)list_peek(unpack_fr->cluster_list);
+	ck_assert_str_eq(before->name, after->name);
+
+	free_buf(buf);
+	slurmdb_destroy_federation_rec(pack_fr);
+	slurmdb_destroy_federation_rec(unpack_fr);
+}
+END_TEST
+
+
+START_TEST(pack_1711_federation_rec_empty_list)
+{
+	int rc;
+
+	slurmdb_federation_rec_t *pack_fr = xmalloc(sizeof(slurmdb_federation_rec_t));
+	pack_fr->flags        = 7;
+	pack_fr->name         = xstrdup("Saint Augustine");
+	pack_fr->cluster_list = NULL;
+
+	Buf buf = init_buf(1024);
+	slurmdb_pack_federation_rec(pack_fr, SLURM_17_11_PROTOCOL_VERSION, buf);
+
+	set_buf_offset(buf, 0);
+
+	slurmdb_federation_rec_t *unpack_fr;
+	rc = slurmdb_unpack_federation_rec((void **)&unpack_fr, SLURM_17_11_PROTOCOL_VERSION, buf);
+	ck_assert(rc                    == SLURM_SUCCESS);
+	ck_assert_str_eq(pack_fr->name,    unpack_fr->name);
+	ck_assert(pack_fr->flags        == unpack_fr->flags);
+	ck_assert(pack_fr->cluster_list == unpack_fr->cluster_list);
+
+	free_buf(buf);
+	slurmdb_destroy_federation_rec(pack_fr);
+	slurmdb_destroy_federation_rec(unpack_fr);
+}
+END_TEST
+
+
+
 /*****************************************************************************
  * TEST SUITE                                                                *
  ****************************************************************************/
@@ -153,9 +238,13 @@ Suite* suite(void)
 	tcase_add_test(tc_core, invalid_protocol);
 	tcase_add_test(tc_core, pack_1702_federation_rec);
 	tcase_add_test(tc_core, pack_1702_null_federation_rec);
-	//tcase_add_test(tc_core, pack_1702_federation_rec_fail);
 	tcase_add_test(tc_core, pack_1702_federation_rec_empty_list);
 	tcase_add_test(tc_core, pack_1702_federation_rec_fail);
+
+	tcase_add_test(tc_core, pack_1711_federation_rec);
+	tcase_add_test(tc_core, pack_1711_null_federation_rec);
+	tcase_add_test(tc_core, pack_1711_federation_rec_empty_list);
+
 	suite_add_tcase(s, tc_core);
 	return s;
 }
