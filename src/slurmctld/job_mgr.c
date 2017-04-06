SchedMD/slurm

commit 1967d5126b9a13fe90c6a6f1ce85114774efe057

Fix sched/backfill logic for min/max time limit and part limit
Fix sched/backfill logic to initiate jobs with maximum time limit over the
partition limit, but the minimum time limit permits it to start.
Related to bug 251

src/slurmctld/job_mgr.c
@@ -2906,7 +2906,7 @@ static int _select_nodes_parts(struct job_record *job_ptr, bool test_only,
 			job_ptr->part_ptr = part_ptr;
 			debug2("Try job %u on next partition %s",
 			       job_ptr->job_id, part_ptr->name);
-			if (job_limits_check(&job_ptr) != WAIT_NO_REASON)
+			if (job_limits_check(&job_ptr, false) != WAIT_NO_REASON)
 				continue;
 			rc = select_nodes(job_ptr, test_only,
 					  select_node_bitmap);
@@ -2916,7 +2916,7 @@ static int _select_nodes_parts(struct job_record *job_ptr, bool test_only,
 		}
 		list_iterator_destroy(iter);
 	} else {
-		if (job_limits_check(&job_ptr) != WAIT_NO_REASON)
+		if (job_limits_check(&job_ptr, false) != WAIT_NO_REASON)
 			test_only = true;
 		rc = select_nodes(job_ptr, test_only, select_node_bitmap);
 	}
@@ -3808,9 +3808,11 @@ fini:	FREE_NULL_LIST(part_ptr_list);
 /*
  * job_limits_check - check the limits specified for the job.
  * IN job_ptr - pointer to job table entry.
+ * IN check_min_time - if true test job's minimum time limit,
+ *		otherwise test maximum time limit
  * RET WAIT_NO_REASON on success, fail status otherwise.
  */
-extern int job_limits_check(struct job_record **job_pptr)
+extern int job_limits_check(struct job_record **job_pptr, bool check_min_time)
 {
 	struct job_details *detail_ptr;
 	enum job_state_reason fail_reason;
@@ -3820,6 +3822,7 @@ extern int job_limits_check(struct job_record **job_pptr)
 	slurmdb_association_rec_t *assoc_ptr;
 	uint32_t job_min_nodes, job_max_nodes;
 	uint32_t part_min_nodes, part_max_nodes;
+	uint32_t time_check;
 #ifdef HAVE_BG
 	static uint16_t cpus_per_node = 0;
 	if (!cpus_per_node)
@@ -3846,6 +3849,10 @@ extern int job_limits_check(struct job_record **job_pptr)
 
 	fail_reason = WAIT_NO_REASON;
 
+	if (check_min_time && job_ptr->time_min)
+		time_check = job_ptr->time_min;
+	else
+		time_check = job_ptr->time_limit;
 	if ((job_min_nodes > part_max_nodes) &&
 	    (!qos_ptr || (qos_ptr && !(qos_ptr->flags
 				       & QOS_FLAG_PART_MAX_NODE)))) {
@@ -3871,13 +3878,12 @@ extern int job_limits_check(struct job_record **job_pptr)
 		debug2("Job %u requested inactive partition %s",
 		       job_ptr->job_id, part_ptr->name);
 		fail_reason = WAIT_PART_INACTIVE;
-	} else if ((((job_ptr->time_limit != NO_VAL) &&
-		     (job_ptr->time_limit > part_ptr->max_time)) ||
-		    ((job_ptr->time_min   != NO_VAL) &&
-		     (job_ptr->time_min   > part_ptr->max_time))) &&
-		     (!qos_ptr || (qos_ptr && !(qos_ptr->flags &
-		 			       QOS_FLAG_PART_TIME_LIMIT)))) {
-		debug2("Job %u exceeds partition time limit", job_ptr->job_id);
+	} else if ((time_check != NO_VAL) &&
+		   (time_check > part_ptr->max_time) &&
+		   (!qos_ptr || (qos_ptr && !(qos_ptr->flags &
+		 			     QOS_FLAG_PART_TIME_LIMIT)))) {
+		info("Job %u exceeds partition time limit (%u > %u)",
+		       job_ptr->job_id, time_check, part_ptr->max_time);
 		fail_reason = WAIT_PART_TIME_LIMIT;
 	} else if (qos_ptr && assoc_ptr &&
 		   (qos_ptr->flags & QOS_FLAG_ENFORCE_USAGE_THRES) &&
@@ -8016,7 +8022,7 @@ int update_job(job_desc_msg_t * job_specs, uid_t uid)
 	if (error_code != SLURM_SUCCESS)
 		goto fini;
 
-	fail_reason = job_limits_check(&job_ptr);
+	fail_reason = job_limits_check(&job_ptr, false);
 	if (fail_reason != WAIT_NO_REASON) {
 		if (fail_reason == WAIT_QOS_THRES)
 			error_code = ESLURM_QOS_THRES;

src/plugins/sched/backfill/backfill.c
@@ -582,7 +582,7 @@ static int _attempt_backfill(void)
 	if (slurm_get_root_filter())
 		filter_root = true;
 
-	job_queue = build_job_queue(true);
+	job_queue = build_job_queue(true, true);
 	if (list_count(job_queue) == 0) {
 		debug("backfill: no jobs to backfill");
 		list_destroy(job_queue);

src/plugins/sched/builtin/builtin.c
@@ -154,7 +154,7 @@ static void _compute_start_times(void)
 	sched_start = now;
 	last_job_alloc = now - 1;
 	alloc_bitmap = bit_alloc(node_record_count);
-	job_queue = build_job_queue(true);
+	job_queue = build_job_queue(true, false);
 	while ((job_queue_rec = (job_queue_rec_t *)
 				list_pop_bottom(job_queue, sort_job_queue2))) {
 		job_ptr  = job_queue_rec->job_ptr;

src/slurmctld/job_scheduler.c
@@ -91,7 +91,8 @@ static void	_job_queue_append(List job_queue, struct job_record *job_ptr,
 static void	_job_queue_rec_del(void *x);
 static bool	_job_runnable_test1(struct job_record *job_ptr,
 				    bool clear_start);
-static bool	_job_runnable_test2(struct job_record *job_ptr);
+static bool	_job_runnable_test2(struct job_record *job_ptr,
+				    bool check_min_time);
 static void *	_run_epilog(void *arg);
 static void *	_run_prolog(void *arg);
 static bool	_scan_depend(List dependency_list, uint32_t job_id);
@@ -193,12 +194,17 @@ static bool _job_runnable_test1(struct job_record *job_ptr, bool clear_start)
 	return true;
 }
 
-/* Job and partition tests for ability to run now */
-static bool _job_runnable_test2(struct job_record *job_ptr)
+/*
+ * Job and partition tests for ability to run now
+ * IN job_ptr - job to test
+ * IN check_min_time - If set, test job's minimum time limit
+ *		otherwise test maximum time limit
+ */
+static bool _job_runnable_test2(struct job_record *job_ptr, bool check_min_time)
 {
 	int reason;
 
-	reason = job_limits_check(&job_ptr);
+	reason = job_limits_check(&job_ptr, check_min_time);
 	if ((reason != job_ptr->state_reason) &&
 	    (part_policy_job_runnable_state(job_ptr))) {
 		job_ptr->state_reason = reason;
@@ -212,10 +218,11 @@ static bool _job_runnable_test2(struct job_record *job_ptr)
 /*
  * build_job_queue - build (non-priority ordered) list of pending jobs
  * IN clear_start - if set then clear the start_time for pending jobs
+ * IN backfill - true if running backfill scheduler, enforce min time limit
  * RET the job queue
  * NOTE: the caller must call list_destroy() on RET value to free memory
  */
-extern List build_job_queue(bool clear_start)
+extern List build_job_queue(bool clear_start, bool backfill)
 {
 	List job_queue;
 	ListIterator job_iterator, part_iterator;
@@ -235,7 +242,7 @@ extern List build_job_queue(bool clear_start)
 	      			while ((part_ptr = (struct part_record *)
 					list_next(part_iterator))) {
 				job_ptr->part_ptr = part_ptr;
-				reason = job_limits_check(&job_ptr);
+				reason = job_limits_check(&job_ptr, backfill);
 				if ((reason != job_ptr->state_reason) &&
 				    (!part_policy_job_runnable_state(job_ptr))){
 					job_ptr->state_reason = reason;
@@ -260,7 +267,7 @@ extern List build_job_queue(bool clear_start)
 				      "part %s", job_ptr->job_id,
 				      job_ptr->partition);
 			}
-			if (!_job_runnable_test2(job_ptr))
+			if (!_job_runnable_test2(job_ptr, backfill))
 				continue;
 			_job_queue_append(job_queue, job_ptr,
 					  job_ptr->part_ptr);
@@ -482,7 +489,7 @@ next_part:		part_ptr = (struct part_record *)
 				continue;
 			}
 		}
-		if (job_limits_check(&job_ptr) != WAIT_NO_REASON)
+		if (job_limits_check(&job_ptr, false) != WAIT_NO_REASON)
 			continue;
 
 		/* Test for valid account, QOS and required nodes on each pass */
@@ -761,7 +768,7 @@ extern int schedule(uint32_t job_limit)
 		slurmctld_diag_stats.schedule_queue_len = list_count(job_list);
 		job_iterator = list_iterator_create(job_list);
 	} else {
-		job_queue = build_job_queue(false);
+		job_queue = build_job_queue(false, false);
 		slurmctld_diag_stats.schedule_queue_len = list_count(job_queue);
 	}
 	while (1) {
@@ -785,7 +792,7 @@ next_part:			part_ptr = (struct part_record *)
 					   list_next(part_iterator);
 				if (part_ptr) {
 					job_ptr->part_ptr = part_ptr;
-					if (job_limits_check(&job_ptr) !=
+					if (job_limits_check(&job_ptr, false) !=
 					    WAIT_NO_REASON)
 						continue;
 				} else {
@@ -794,7 +801,7 @@ next_part:			part_ptr = (struct part_record *)
 					continue;
 				}
 			} else {
-				if (!_job_runnable_test2(job_ptr))
+				if (!_job_runnable_test2(job_ptr, false))
 					continue;
 			}
 		} else {

src/slurmctld/job_scheduler.h
@@ -60,10 +60,11 @@ extern int build_feature_list(struct job_record *job_ptr);
 /*
  * build_job_queue - build (non-priority ordered) list of pending jobs
  * IN clear_start - if set then clear the start_time for pending jobs
+ * IN backfill - true if running backfill scheduler, enforce min time limit
  * RET the job queue
  * NOTE: the caller must call list_destroy() on RET value to free memory
  */
-extern List build_job_queue(bool clear_start);
+extern List build_job_queue(bool clear_start, bool backfill);
 
 /* Given a scheduled job, return a pointer to it batch_job_launch_msg_t data */
 extern batch_job_launch_msg_t *build_launch_job_msg(

src/slurmctld/slurmctld.h
@@ -775,9 +775,11 @@ extern struct part_record *create_part_record (void);
 /*
  * job_limits_check - check the limits specified for the job.
  * IN job_ptr - pointer to job table entry.
+ * IN check_min_time - if true test job's minimum time limit,
+ *		otherwise test maximum time limit
  * RET WAIT_NO_REASON on success, fail status otherwise.
  */
-extern int job_limits_check(struct job_record **job_pptr);
+extern int job_limits_check(struct job_record **job_pptr, bool check_min_time);
 
 /*
  * delete_job_details - delete a job's detail record and clear it's pointer

commit 5c14c21d87c90b43f7eb313cd80a932ee5742312

Downgrade info message about exceeding partition time limit to a debug2.
This was a regression in commit 1967d5126.

src/slurmctld/job_mgr.c
@@ -5856,7 +5856,7 @@ extern int job_limits_check(struct job_record **job_pptr, bool check_min_time)
 		   (time_check > part_ptr->max_time) &&
 		   (!qos_ptr || (qos_ptr && !(qos_ptr->flags &
 					     QOS_FLAG_PART_TIME_LIMIT)))) {
-		info("Job %u exceeds partition %s time limit (%u > %u)",
+		debug2("Job %u exceeds partition %s time limit (%u > %u)",
 		       job_ptr->job_id, part_ptr->name, time_check,
 		       part_ptr->max_time);
 		fail_reason = WAIT_PART_TIME_LIMIT;