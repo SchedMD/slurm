572ff34de20e4f621c3c68dfe3b0a58d3921c061
Gary B Skouson <Gary.Skouson@pnnl.gov>
2017-04-21 10:21:12 -0600

    Correction to backfill limits work
    bug 3689

--- src/plugins/sched/backfill/backfill.c
+++ src/plugins/sched/backfill/backfill.c
@@ -1135,7 +1135,6 @@ static int _attempt_backfill(void)
 		}
 
 		if (!assoc_limit_stop &&
-		    !acct_policy_job_runnable_state(job_ptr) &&
 		    !acct_policy_job_runnable_pre_select(job_ptr)) {
 			continue;
 		}
