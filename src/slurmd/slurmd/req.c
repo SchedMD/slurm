SchedMD/slurm

commit ad07aebc0caf282db6a3d5abfeaa84a58c81bbda

Add job "features" to node reboot RPC

src/slurmd/slurmd/req.c
@@ -2145,7 +2145,8 @@ _rpc_shutdown(slurm_msg_t *msg)
 static void
 _rpc_reboot(slurm_msg_t *msg)
 {
-	char *reboot_program, *sp;
+	char *reboot_program, *cmd = NULL, *sp;
+	reboot_msg_t *reboot_msg;
 	slurm_ctl_conf_t *cfg;
 	uid_t req_uid = g_slurm_auth_get_uid(msg->auth_cred,
 					     slurm_get_auth_info());
@@ -2163,13 +2164,20 @@ _rpc_reboot(slurm_msg_t *msg)
 				sp = xstrndup(reboot_program,
 					      (sp - reboot_program));
 			else
-			    sp = xstrdup(reboot_program);
+				sp = xstrdup(reboot_program);
+			reboot_msg = (reboot_msg_t *) msg->data;
+			if (reboot_msg && reboot_msg->features) {
+				xstrfmtcat(cmd, "%s %s",
+					   sp, reboot_msg->features);
+			} else
+				cmd = xstrdup(sp);
 			if (access(sp, R_OK | X_OK) < 0)
 				error("Cannot run RebootProgram [%s]: %m", sp);
-			else if ((exit_code = system(reboot_program)))
+			else if ((exit_code = system(cmd)))
 				error("system(%s) returned %d", reboot_program,
 				      exit_code);
 			xfree(sp);
+			xfree(cmd);
 		} else
 			error("RebootProgram isn't defined in config");
 		slurm_conf_unlock();

src/common/slurm_protocol_defs.c
@@ -481,6 +481,7 @@ extern void slurm_free_last_update_msg(last_update_msg_t * msg)
 extern void slurm_free_reboot_msg(reboot_msg_t * msg)
 {
 	if (msg) {
+		xfree(msg->features);
 		xfree(msg->node_list);
 		xfree(msg);
 	}

src/common/slurm_protocol_defs.h
@@ -680,6 +680,7 @@ typedef struct epilog_complete_msg {
 } epilog_complete_msg_t;
 
 typedef struct reboot_msg {
+	char *features;
 	char *node_list;
 } reboot_msg_t;
 

src/common/slurm_protocol_pack.c
@@ -10328,10 +10328,20 @@ static void
 _pack_reboot_msg(reboot_msg_t * msg, Buf buffer,
 		 uint16_t protocol_version)
 {
-	if (msg && msg->node_list)
-		packstr(msg->node_list, buffer);
-	else
-		packnull(buffer);
+	if (protocol_version >= SLURM_16_05_PROTOCOL_VERSION) {
+		if (msg) {
+			packstr(msg->features, buffer);
+			packstr(msg->node_list, buffer);
+		} else {
+			packnull(buffer);
+			packnull(buffer);
+		}
+	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
+		if (msg)
+			packstr(msg->node_list, buffer);
+		else
+			packnull(buffer);
+	}
 }
 
 static int
@@ -10344,7 +10354,12 @@ _unpack_reboot_msg(reboot_msg_t ** msg_ptr, Buf buffer,
 	msg = xmalloc(sizeof(reboot_msg_t));
 	*msg_ptr = msg;
 
-	safe_unpackstr_xmalloc(&msg->node_list, &uint32_tmp, buffer);
+	if (protocol_version >= SLURM_16_05_PROTOCOL_VERSION) {
+		safe_unpackstr_xmalloc(&msg->features, &uint32_tmp, buffer);
+		safe_unpackstr_xmalloc(&msg->node_list, &uint32_tmp, buffer);
+	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
+		safe_unpackstr_xmalloc(&msg->node_list, &uint32_tmp, buffer);
+	}
 	return SLURM_SUCCESS;
 
 unpack_error:

src/scontrol/scontrol.c
@@ -651,6 +651,7 @@ static int _reboot_nodes(char *node_list)
 
 	slurm_msg_t_init(&msg);
 
+	bzero(&req, sizeof(reboot_msg_t));
 	req.node_list = node_list;
 	msg.msg_type = REQUEST_REBOOT_NODES;
 	msg.data = &req;

src/slurmctld/job_scheduler.c
@@ -3306,11 +3306,13 @@ extern int reboot_job_nodes(struct job_record *job_ptr)
 	pthread_t thread_id_prolog;
 	pthread_attr_t thread_attr_prolog;
 	agent_arg_t *reboot_agent_args = NULL;
+	reboot_msg_t *reboot_msg;
 	struct node_record *node_ptr;
 	time_t now = time(NULL);
 	uint16_t resume_timeout = slurm_get_resume_timeout();
 
 	if ((job_ptr->reboot == 0) || (job_ptr->node_bitmap == NULL) ||
+	    (job_ptr->details == NULL) ||
 	    (slurmctld_conf.reboot_program == NULL) ||
 	    (slurmctld_conf.reboot_program[0] == '\0'))
 		return SLURM_SUCCESS;
@@ -3320,6 +3322,9 @@ extern int reboot_job_nodes(struct job_record *job_ptr)
 	reboot_agent_args->retry = 0;
 	reboot_agent_args->protocol_version = SLURM_PROTOCOL_VERSION;
 	reboot_agent_args->hostlist = hostlist_create(NULL);
+	reboot_msg = xmalloc(sizeof(reboot_msg_t));
+	reboot_msg->features = xstrdup(job_ptr->details->features);
+	reboot_agent_args->msg_args = reboot_msg;
 	for (i = 0, node_ptr = node_record_table_ptr; i < node_record_count;
 	     i++, node_ptr++) {
 		if (!bit_test(job_ptr->node_bitmap, i))

commit 64c3fc37673ba63fbf0401a3853ffd1ec72f7351

Allow reboot program to use arguments.
Bug 3612

Looks like a regression in commit ad07aebc0caf

src/slurmd/slurmd/req.c
@@ -2599,6 +2599,10 @@ _rpc_reboot(slurm_msg_t *msg)
 				sp = xstrdup(reboot_program);
 			reboot_msg = (reboot_msg_t *) msg->data;
 			if (reboot_msg && reboot_msg->features) {
+				/*
+				 * Run reboot_program with only arguments given
+				 * in reboot_msg->features.
+				 */
 				info("Node reboot request with features %s being processed",
 				     reboot_msg->features);
 				(void) node_features_g_node_set(
@@ -2610,7 +2614,8 @@ _rpc_reboot(slurm_msg_t *msg)
 					cmd = xstrdup(sp);
 				}
 			} else {
-				cmd = xstrdup(sp);
+				/* Run reboot_program verbatim */
+				cmd = xstrdup(reboot_program);
 				info("Node reboot request being processed");
 			}
 			if (access(sp, R_OK | X_OK) < 0)