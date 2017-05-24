ba46ffbe88d993df756f24373115c6dc103d98e5
Brian Christiansen <brian@schedmd.com>
2017-05-24 17:32:22 -0600

    Move rerouting of msg to deeper level
    Move from slurm_send_recv_controller_rc_msg to
    slurm_send_recv_controller_msg. This allows scontrol requeue to be
    rerouted to the origin.

--- src/common/slurm_protocol_api.c
+++ src/common/slurm_protocol_api.c
@@ -4349,6 +4349,7 @@ extern int slurm_send_recv_controller_msg(slurm_msg_t * request_msg,
 	uint16_t slurmctld_timeout;
 	slurm_addr_t ctrl_addr;
 	static bool use_backup = false;
+	slurmdb_cluster_rec_t *save_comm_cluster_rec = comm_cluster_rec;
 
 	/* Just in case the caller didn't initialize his slurm_msg_t, and
 	 * since we KNOW that we are only sending to one node (the controller),
@@ -4358,6 +4359,8 @@ extern int slurm_send_recv_controller_msg(slurm_msg_t * request_msg,
 	request_msg->ret_list = NULL;
 	request_msg->forward_struct = NULL;
 
+tryagain:
+	retry = 1;
 	if (comm_cluster_rec)
 		request_msg->flags |= SLURM_GLOBAL_AUTH_KEY;
 
@@ -4408,6 +4411,24 @@ extern int slurm_send_recv_controller_msg(slurm_msg_t * request_msg,
 		if (rc == -1)
 			break;
 	}
+
+	if (!rc && (response_msg->msg_type == RESPONSE_SLURM_REROUTE_MSG)) {
+		reroute_msg_t *rr_msg = (reroute_msg_t *)response_msg->data;
+
+		/* Don't expect mutliple hops but in the case it does
+		 * happen, free the previous rr cluster_rec. */
+		if (comm_cluster_rec &&
+		    (comm_cluster_rec != save_comm_cluster_rec))
+			slurmdb_destroy_cluster_rec(comm_cluster_rec);
+
+		comm_cluster_rec = rr_msg->working_cluster_rec;
+		slurmdb_setup_cluster_rec(comm_cluster_rec);
+		rr_msg->working_cluster_rec = NULL;
+		goto tryagain;
+	}
+
+	if (comm_cluster_rec != save_comm_cluster_rec)
+		slurmdb_destroy_cluster_rec(comm_cluster_rec);
 
 cleanup:
 	if (rc != 0)
@@ -4663,33 +4684,14 @@ extern int slurm_send_recv_controller_rc_msg(slurm_msg_t *req, int *rc,
 {
 	int ret_c;
 	slurm_msg_t resp;
-	slurmdb_cluster_rec_t *save_comm_cluster_rec = comm_cluster_rec;
 
-tryagain:
 	if (!slurm_send_recv_controller_msg(req, &resp, comm_cluster_rec)) {
-		if (resp.msg_type == RESPONSE_SLURM_REROUTE_MSG) {
-			reroute_msg_t *rr_msg = (reroute_msg_t *)resp.data;
-
-			/* Don't expect mutliple hops but in the case it does
-			 * happen, free the previous rr cluster_rec. */
-			if (comm_cluster_rec &&
-			    (comm_cluster_rec != save_comm_cluster_rec))
-				slurmdb_destroy_cluster_rec(comm_cluster_rec);
-
-			comm_cluster_rec = rr_msg->working_cluster_rec;
-			slurmdb_setup_cluster_rec(comm_cluster_rec);
-			rr_msg->working_cluster_rec = NULL;
-			goto tryagain;
-		}
 		*rc = slurm_get_return_code(resp.msg_type, resp.data);
 		slurm_free_msg_data(resp.msg_type, resp.data);
 		ret_c = 0;
 	} else {
 		ret_c = -1;
 	}
-
-	if (comm_cluster_rec != save_comm_cluster_rec)
-		slurmdb_destroy_cluster_rec(comm_cluster_rec);
 
 	return ret_c;
 }
