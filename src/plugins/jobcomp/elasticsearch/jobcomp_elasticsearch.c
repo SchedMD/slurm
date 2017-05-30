SchedMD/slurm

commit 8d10ebf596dfb39a8d024aef0c2be02a2949664e

Replace void * definitions with appropriate types, thusly removing many unneeded typecasts.

src/plugins/jobcomp/elasticsearch/jobcomp_elasticsearch.c
@@ -728,15 +728,12 @@ extern int slurm_jobcomp_log_record(struct job_record *job_ptr)
 	}
 
 	if (job_ptr->assoc_ptr != NULL) {
-		cluster = ((slurmdb_assoc_rec_t *) job_ptr->assoc_ptr)->cluster;
-		xstrfmtcat(buffer, ",\"cluster\":\"%s\"", cluster);
+		xstrfmtcat(buffer, ",\"cluster\":\"%s\"",
+			   job_ptr->assoc_ptr->cluster);
 	}
 
 	if (job_ptr->qos_ptr != NULL) {
-		slurmdb_qos_rec_t *assoc =
-			(slurmdb_qos_rec_t *) job_ptr->qos_ptr;
-		qos = assoc->name;
-		xstrfmtcat(buffer, ",\"qos\":\"%s\"", qos);
+		xstrfmtcat(buffer, ",\"qos\":\"%s\"", job_ptr->qos_ptr->name);
 	}
 
 	if (job_ptr->details && (job_ptr->details->num_tasks != NO_VAL)) {

src/plugins/burst_buffer/cray/burst_buffer_cray.c
@@ -491,11 +491,8 @@ static bb_job_t *_get_bb_job(struct job_record *job_ptr)
 	bb_job->account = xstrdup(job_ptr->account);
 	if (job_ptr->part_ptr)
 		bb_job->partition = xstrdup(job_ptr->part_ptr->name);
-	if (job_ptr->qos_ptr) {
-		slurmdb_qos_rec_t *qos_ptr =
-			(slurmdb_qos_rec_t *)job_ptr->qos_ptr;
-		bb_job->qos = xstrdup(qos_ptr->name);
-	}
+	if (job_ptr->qos_ptr)
+		bb_job->qos = xstrdup(job_ptr->qos_ptr->name);
 	bb_job->state = BB_STATE_PENDING;
 	bb_job->user_id = job_ptr->user_id;
 	bb_specs = xstrdup(job_ptr->burst_buffer);
@@ -4492,8 +4489,7 @@ static void *_create_persistent(void *x)
 					",%u,", assoc->id);
 			}
 			if (job_ptr->qos_ptr) {
-				slurmdb_qos_rec_t *qos_ptr =
-					(slurmdb_qos_rec_t *)job_ptr->qos_ptr;
+				slurmdb_qos_rec_t *qos_ptr = job_ptr->qos_ptr;
 				bb_alloc->qos_ptr = qos_ptr;
 				bb_alloc->qos = xstrdup(qos_ptr->name);
 			}

src/plugins/job_submit/lua/job_submit_lua.c
@@ -366,9 +366,7 @@ static int _job_rec_field(const struct job_record *job_ptr,
 		lua_pushnumber (L, job_ptr->priority);
 	} else if (!xstrcmp(name, "qos")) {
 		if (job_ptr->qos_ptr) {
-			slurmdb_qos_rec_t *qos_ptr =
-				(slurmdb_qos_rec_t *)job_ptr->qos_ptr;
-			lua_pushstring (L, qos_ptr->name);
+			lua_pushstring (L, job_ptr->qos_ptr->name);
 		} else {
 			lua_pushnil (L);
 		}

src/plugins/jobcomp/filetxt/jobcomp_filetxt.c
@@ -318,9 +318,7 @@ extern int slurm_jobcomp_log_record ( struct job_record *job_ptr )
 		account = "";
 
 	if (job_ptr->qos_ptr != NULL) {
-		slurmdb_qos_rec_t *assoc =
-			(slurmdb_qos_rec_t *) job_ptr->qos_ptr;
-		qos = assoc->name;
+		qos = job_ptr->qos_ptr->name;
 	} else
 		qos = "";
 
@@ -330,7 +328,7 @@ extern int slurm_jobcomp_log_record ( struct job_record *job_ptr )
 		wckey = "";
 
 	if (job_ptr->assoc_ptr != NULL)
-		cluster = ((slurmdb_assoc_rec_t *) job_ptr->assoc_ptr)->cluster;
+		cluster = job_ptr->assoc_ptr->cluster;
 	else
 		cluster = "unknown";
 

src/plugins/preempt/job_prio/preempt_job_prio.c
@@ -107,10 +107,8 @@ static bool _account_preemptable(struct job_record *preemptor_job_ptr,
 	bool is_from_same_account = false;
 	int i;
 
-	preemptor_assoc =
-		(slurmdb_assoc_rec_t *)preemptor_job_ptr->assoc_ptr;
-	preemptee_assoc =
-		(slurmdb_assoc_rec_t *)preemptee_job_ptr->assoc_ptr;
+	preemptor_assoc = preemptor_job_ptr->assoc_ptr;
+	preemptee_assoc = preemptee_job_ptr->assoc_ptr;
 	if (!preemptor_assoc || !preemptee_assoc)
 		return false;
 
@@ -347,8 +345,7 @@ static int _get_nb_cpus(struct job_record *job_ptr)
 static slurmdb_assoc_rec_t *_get_job_fs_ass(char *job_type,
 					    struct job_record *job_ptr)
 {
-	slurmdb_assoc_rec_t *temp_fs_ass =
-		(slurmdb_assoc_rec_t *)job_ptr->assoc_ptr;
+	slurmdb_assoc_rec_t *temp_fs_ass = job_ptr->assoc_ptr;
 
 	if (slurm_get_debug_flags() & DEBUG_FLAG_PRIO) {
 		info("%s: Pre %s JobID:%u ParentAcct:%s MyAcct:%s "
@@ -407,8 +404,7 @@ static void _account_under_alloc(struct job_record *preemptor_job_ptr,
 	acct_usage_element_t *found_acct_usage_ptr = NULL;
 	char *share_type;
 
-	preemptor_assoc = (slurmdb_assoc_rec_t *)
-			  preemptor_job_ptr->assoc_ptr;
+	preemptor_assoc = preemptor_job_ptr->assoc_ptr;
 	preemptor_temp_fs_ass = _get_job_fs_ass("preemptor", preemptor_job_ptr);
 	preemptor_grp_used_cpu = preemptor_temp_fs_ass->usage->
 		grp_used_tres[TRES_ARRAY_CPU];
@@ -420,8 +416,7 @@ static void _account_under_alloc(struct job_record *preemptor_job_ptr,
 	}
 
 	while ((preemptee_job_ptr = (struct job_record *) list_next(it))) {
-		preemptee_assoc = ((slurmdb_assoc_rec_t *)
-				   preemptee_job_ptr->assoc_ptr);
+		preemptee_assoc = preemptee_job_ptr->assoc_ptr;
 		preemptee_assoc_id = preemptee_assoc->id;
 		preemptee_temp_fs_ass = _get_job_fs_ass("preemptee",
 							preemptee_job_ptr);
@@ -520,8 +515,7 @@ static void _account_under_alloc(struct job_record *preemptor_job_ptr,
 		preemptee_current_usage =
 			(long)(preemptee_acct_usage_ptr->current_usage*EPSILON);
 		if (((xstrcmp(preemptor_assoc->acct,
-			      ((slurmdb_assoc_rec_t *)
-			       preemptee_job_ptr->assoc_ptr)->acct) != 0) &&
+			      preemptee_job_ptr->assoc_ptr->acct) != 0) &&
 		    (preemptor_new_usage >= preemptee_acct_usage_ptr->current_usage ||
 		     preemptee_acct_usage_ptr->current_cpu_count <= 0)) &&
 		    (preemptee_acct_usage_ptr->current_usage > 0)) {
@@ -539,11 +533,9 @@ static void _account_under_alloc(struct job_record *preemptor_job_ptr,
 				      preemptor_new_usage,
 				      preemptee_current_usage,
 				      preemptor_new_usage_long,
-				      ((slurmdb_assoc_rec_t*)
-				       preemptee_job_ptr->assoc_ptr)->acct,
-				      ((slurmdb_assoc_rec_t*)
-				       preemptee_job_ptr->assoc_ptr)->
-				       parent_acct);
+				      preemptee_job_ptr->assoc_ptr->acct,
+				      preemptee_job_ptr->assoc_ptr->
+				      parent_acct);
 			}
 			preemptee_acct_usage_ptr->current_usage +=
 				((double)preemptee_cpu_cnt) /
@@ -582,8 +574,8 @@ static int _overalloc_test(struct job_record *preemptor,
 	cpu_cnt_preemptee = _get_nb_cpus(preemptee);
 	cpu_cnt_preemptor = _get_nb_cpus(preemptor);
 
-	assoc_preemptee = (slurmdb_assoc_rec_t *)preemptee->assoc_ptr;
-	assoc_preemptor = (slurmdb_assoc_rec_t *)preemptor->assoc_ptr;
+	assoc_preemptee = preemptee->assoc_ptr;
+	assoc_preemptor = preemptor->assoc_ptr;
 
 	if (!assoc_preemptee || !assoc_preemptee->usage ||
 	    !assoc_preemptor || !assoc_preemptor->usage) {
@@ -844,9 +836,8 @@ extern uint16_t job_preempt_mode(struct job_record *job_ptr)
 {
 	uint16_t mode;
 
-	if (job_ptr->qos_ptr &&
-	   ((slurmdb_qos_rec_t *)job_ptr->qos_ptr)->preempt_mode) {
-		mode = ((slurmdb_qos_rec_t *)job_ptr->qos_ptr)->preempt_mode;
+	if (job_ptr->qos_ptr && job_ptr->qos_ptr->preempt_mode) {
+		mode = job_ptr->qos_ptr->preempt_mode;
 		if (slurm_get_debug_flags() & DEBUG_FLAG_PRIO) {
 			info("%s: in job_preempt_mode return = %s",
 			     plugin_type, preempt_mode_string(mode));

src/plugins/preempt/qos/preempt_qos.c
@@ -215,9 +215,8 @@ static int _sort_by_youngest(void *x, void *y)
 
 extern uint16_t job_preempt_mode(struct job_record *job_ptr)
 {
-	if (job_ptr->qos_ptr &&
-	    ((slurmdb_qos_rec_t *)job_ptr->qos_ptr)->preempt_mode)
-		return ((slurmdb_qos_rec_t *)job_ptr->qos_ptr)->preempt_mode;
+	if (job_ptr->qos_ptr && job_ptr->qos_ptr->preempt_mode)
+		return job_ptr->qos_ptr->preempt_mode;
 
 	return (slurm_get_preempt_mode() & (~PREEMPT_MODE_GANG));
 }

src/plugins/priority/basic/priority_basic.c
@@ -180,8 +180,7 @@ extern void priority_p_job_end(struct job_record *job_ptr)
 
 	assoc_mgr_lock(&locks);
 	if (job_ptr->qos_ptr) {
-		slurmdb_qos_rec_t *qos_ptr =
-			(slurmdb_qos_rec_t *)job_ptr->qos_ptr;
+		slurmdb_qos_rec_t *qos_ptr = job_ptr->qos_ptr;
 		for (i=0; i<slurmctld_tres_cnt; i++) {
 			if (unused_tres_run_secs[i] >
 			    qos_ptr->usage->grp_used_tres_run_secs[i]) {
@@ -196,7 +195,7 @@ extern void priority_p_job_end(struct job_record *job_ptr)
 					unused_tres_run_secs[i];
 		}
 	}
-	assoc_ptr = (slurmdb_assoc_rec_t *)job_ptr->assoc_ptr;
+	assoc_ptr = job_ptr->assoc_ptr;
 	while (assoc_ptr) {
 		/* If the job finished early remove the extra time now. */
 		for (i=0; i<slurmctld_tres_cnt; i++) {

src/plugins/priority/multifactor/priority_multifactor.c
@@ -443,7 +443,7 @@ static double _get_fairshare_priority(struct job_record *job_ptr)
 
 	assoc_mgr_lock(&locks);
 
-	job_assoc = (slurmdb_assoc_rec_t *)job_ptr->assoc_ptr;
+	job_assoc = job_ptr->assoc_ptr;
 
 	if (!job_assoc) {
 		assoc_mgr_unlock(&locks);
@@ -887,7 +887,7 @@ static void _handle_tres_run_secs(uint64_t *tres_run_delta,
 				  struct job_record *job_ptr)
 {
 
-	slurmdb_assoc_rec_t *assoc = (slurmdb_assoc_rec_t *)job_ptr->assoc_ptr;
+	slurmdb_assoc_rec_t *assoc = job_ptr->assoc_ptr;
 
 	_handle_qos_tres_run_secs(NULL, tres_run_delta,
 				  job_ptr->job_id, job_ptr->qos_ptr);
@@ -1094,8 +1094,8 @@ static int _apply_new_usage(struct job_record *job_ptr,
 	   changed make sure we get it again
 	   here.
 	*/
-	qos = (slurmdb_qos_rec_t *)job_ptr->qos_ptr;
-	assoc = (slurmdb_assoc_rec_t *)job_ptr->assoc_ptr;
+	qos = job_ptr->qos_ptr;
+	assoc = job_ptr->assoc_ptr;
 
 	/* now apply the usage factor for this qos */
 	if (qos) {
@@ -1123,7 +1123,7 @@ static int _apply_new_usage(struct job_record *job_ptr,
 	/* sanity check, there should always be a part_ptr here, but only do
 	 * the qos if it isn't the same qos as the job is using */
 	if (job_ptr->part_ptr && (job_ptr->part_ptr->qos_ptr != qos))
-		qos = (slurmdb_qos_rec_t *)job_ptr->part_ptr->qos_ptr;
+		qos = job_ptr->part_ptr->qos_ptr;
 	else
 		qos = NULL;
 
@@ -2064,7 +2064,7 @@ extern void set_priority_factors(time_t start_time, struct job_record *job_ptr)
 		       sizeof(priority_factors_object_t));
 	}
 
-	qos_ptr = (slurmdb_qos_rec_t *)job_ptr->qos_ptr;
+	qos_ptr = job_ptr->qos_ptr;
 
 	if (weight_age) {
 		uint32_t diff = 0;

commit 5940a5f87649b3e7917267f1b66c7679e88e5b9c

Fix another build issue with elasticsearch.
Another issue inadvertently caused by 8d10ebf596.

src/plugins/jobcomp/elasticsearch/jobcomp_elasticsearch.c
@@ -606,7 +606,7 @@ static void _make_time_str(time_t * time, char *string, int size)
 extern int slurm_jobcomp_log_record(struct job_record *job_ptr)
 {
 	char usr_str[32], grp_str[32], start_str[32], end_str[32];
-	char time_str[32], *qos, *state_string;
+	char time_str[32], *state_string;
 	time_t elapsed_time;
 	enum job_states job_state;
 	uint32_t time_limit;