cff4e66120905eafad3a616a5556ac2283a3b2cc
Danny Auble <da@schedmd.com>
2017-06-01 11:55:32 -0600

    Better commit from last

--- testsuite/expect/test7.15.prog.c
+++ testsuite/expect/test7.15.prog.c
@@ -17,15 +17,15 @@ int main (int ac, char **av)
 
 	for (i = 1; i < SIGRTMAX; i++) {
 		sigaction (i, NULL, &act);
-		/* NOTE: If the slurmd is started from a terminal such as rxvt
-		 * or anything like it (aterm) it will IGN SIGFPE (8) thus
-		 * failing this test.
+		/* NOTE: If the slurmd is started from a terminal such as
+		 * rxvt-unicode or anything like it (aterm) it will ignore
+		 * SIGFPE (8) thus failing this test.
 		 */
 		if (act.sa_handler == SIG_IGN) {
 			fprintf (stderr, "%s: Signal %d is ignored!\n",
 				 hostname, i);
 			if (i == SIGFPE)
-				fprintf (stderr, "%s: Terminals like rxvt/aterm will ignore SIGFPE.  Rerun this test where the slurmd isn't started from that terminal if you get this message.\n",
+				fprintf (stderr, "%s: Terminals like rxvt-unicode/aterm will ignore SIGFPE.  Rerun this test where the slurmd isn't started from that terminal if you get this message.\n",
 					 hostname);
 			rc = 1;
 		} else if (act.sa_handler != SIG_DFL) {
