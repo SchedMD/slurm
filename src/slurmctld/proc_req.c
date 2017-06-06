2ab627f0d8e83839ec907761ce8ea00b212ae4fc
Isaac Hartung <ihartung@schedmd.com>
2017-06-06 11:00:28 -0600

    Enable scontrol suspend in a federation
    Routes request to origin and then to the cluster running the job.

--- src/slurmctld/proc_req.c
+++ src/slurmctld/proc_req.c
@@ -4317,6 +4317,7 @@ inline static void _slurm_rpc_suspend(slurm_msg_t * msg)
 		NO_LOCK, WRITE_LOCK, WRITE_LOCK, NO_LOCK, NO_LOCK };
 	uid_t uid = g_slurm_auth_get_uid(msg->auth_cred,
 					 slurmctld_config.auth_info);
+	struct job_record *job_ptr;
 	char *op;
 
 	START_TIMER;
@@ -4333,8 +4334,54 @@ inline static void _slurm_rpc_suspend(slurm_msg_t * msg)
 	info("Processing RPC: REQUEST_SUSPEND(%s) from uid=%u",
 	     op, (unsigned int) uid);
 
+	/* Get the job id part of the jobid. It could be an array id. Currently
+	 * in a federation, job arrays only run on the origin cluster so we just
+	 * want to find if the array, not a specific task, is on the origin
+	 * cluster. */
+	if ((sus_ptr->job_id == NO_VAL) && sus_ptr->job_id_str)
+		sus_ptr->job_id = strtol(sus_ptr->job_id_str, NULL, 10);
+
 	lock_slurmctld(job_write_lock);
-	if (sus_ptr->job_id_str) {
+	job_ptr = find_job_record(sus_ptr->job_id);
+
+	/* If job is found on the cluster, it could be pending, the origin
+	 * cluster, or running on the sibling cluster. If it's not there then
+	 * route it to the origin, otherwise try to suspend the job. If it's
+	 * pending an error should be returned. If it's running then it should
+	 * suspend the job. */
+	if (!job_ptr &&
+	    !_route_msg_to_origin(msg, NULL, sus_ptr->job_id, uid)) {
+		unlock_slurmctld(job_write_lock);
+		return;
+	}
+	if (!job_ptr)
+		error_code = ESLURM_INVALID_JOB_ID;
+	else if (fed_mgr_cluster_rec && job_ptr->fed_details &&
+		 fed_mgr_is_origin_job(job_ptr) &&
+		 IS_JOB_REVOKED(job_ptr) &&
+		 job_ptr->fed_details->cluster_lock &&
+		 (job_ptr->fed_details->cluster_lock !=
+		  fed_mgr_cluster_rec->fed.id)) {
+
+		/* Route to the cluster that is running the job. */
+		slurmdb_cluster_rec_t *dst =
+			fed_mgr_get_cluster_by_id(
+					job_ptr->fed_details->cluster_lock);
+		if (dst) {
+			slurm_send_reroute_msg(msg, dst);
+			info("%s: %s job %d uid %d routed to %s",
+			     __func__, rpc_num2string(msg->msg_type),
+			     job_ptr->job_id, uid, dst->name);
+
+			unlock_slurmctld(job_write_lock);
+			END_TIMER2("_slurm_rpc_suspend");
+			return;
+		}
+
+		error("couldn't find cluster by cluster id %d",
+		      job_ptr->fed_details->cluster_lock);
+		error_code = ESLURM_INVALID_CLUSTER_NAME;
+	} else if (sus_ptr->job_id_str) {
 		error_code = job_suspend2(sus_ptr, uid, msg->conn_fd, true,
 					  msg->protocol_version);
 	} else {
