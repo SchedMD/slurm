SchedMD/slurm

commit 250378c26a151875e040bf9a70a12a04816d254b

Actually use ignore_state_errors
Bug 3502.

src/common/assoc_mgr.c
@@ -5366,7 +5366,9 @@ extern int load_assoc_usage(char *state_save_location)
 	state_fd = open(state_file, O_RDONLY);
 	if (state_fd < 0) {
 		debug2("No Assoc usage file (%s) to recover", state_file);
-		goto unpack_error;
+		xfree(state_file);
+		assoc_mgr_unlock(&locks);
+		return ENOENT;
 	} else {
 		data_allocated = BUF_SIZE;
 		data = xmalloc(data_allocated);
@@ -5396,9 +5398,13 @@ extern int load_assoc_usage(char *state_save_location)
 	safe_unpack16(&ver, buffer);
 	debug3("Version in assoc_usage header is %u", ver);
 	if (ver > SLURM_PROTOCOL_VERSION || ver < SLURM_MIN_PROTOCOL_VERSION) {
+		if (!ignore_state_errors)
+			fatal("Can not recover assoc_usage state, incompatible version, got %u need >= %u <= %u, start with '-i' to ignore this",
+			      ver, SLURM_MIN_PROTOCOL_VERSION,
+			      SLURM_PROTOCOL_VERSION);
 		error("***********************************************");
 		error("Can not recover assoc_usage state, "
-		      "incompatible version, got %u need > %u <= %u", ver,
+		      "incompatible version, got %u need >= %u <= %u", ver,
 		      SLURM_MIN_PROTOCOL_VERSION, SLURM_PROTOCOL_VERSION);
 		error("***********************************************");
 		free_buf(buffer);
@@ -5453,6 +5459,9 @@ extern int load_assoc_usage(char *state_save_location)
 	return SLURM_SUCCESS;
 
 unpack_error:
+	if (!ignore_state_errors)
+		fatal("Incomplete assoc usage state file, start with '-i' to ignore this");
+	error("Incomplete assoc usage state file");
 	if (buffer)
 		free_buf(buffer);
 	xfree(tmp_str);
@@ -5484,7 +5493,9 @@ extern int load_qos_usage(char *state_save_location)
 	state_fd = open(state_file, O_RDONLY);
 	if (state_fd < 0) {
 		debug2("No Qos usage file (%s) to recover", state_file);
-		goto unpack_error;
+		xfree(state_file);
+		assoc_mgr_unlock(&locks);
+		return ENOENT;
 	} else {
 		data_allocated = BUF_SIZE;
 		data = xmalloc(data_allocated);
@@ -5514,6 +5525,10 @@ extern int load_qos_usage(char *state_save_location)
 	safe_unpack16(&ver, buffer);
 	debug3("Version in qos_usage header is %u", ver);
 	if (ver > SLURM_PROTOCOL_VERSION || ver < SLURM_MIN_PROTOCOL_VERSION) {
+		if (!ignore_state_errors)
+			fatal("Can not recover qos_usage state, incompatible version, "
+			      "got %u need >= %u <= %u, start with '-i' to ignore this",
+			      ver, SLURM_MIN_PROTOCOL_VERSION, SLURM_PROTOCOL_VERSION);
 		error("***********************************************");
 		error("Can not recover qos_usage state, "
 		      "incompatible version, got %u need > %u <= %u", ver,
@@ -5559,6 +5574,9 @@ extern int load_qos_usage(char *state_save_location)
 	return SLURM_SUCCESS;
 
 unpack_error:
+	if (!ignore_state_errors)
+		fatal("Incomplete QOS usage state file, start with '-i' to ignore this");
+	error("Incomplete QOS usage state file");
 	if (buffer)
 		free_buf(buffer);
 	if (itr)
@@ -5591,7 +5609,9 @@ extern int load_assoc_mgr_state(char *state_save_location)
 	state_fd = open(state_file, O_RDONLY);
 	if (state_fd < 0) {
 		debug2("No association state file (%s) to recover", state_file);
-		goto unpack_error;
+		xfree(state_file);
+		assoc_mgr_unlock(&locks);
+		return ENOENT;
 	} else {
 		data_allocated = BUF_SIZE;
 		data = xmalloc(data_allocated);
@@ -5621,6 +5641,10 @@ extern int load_assoc_mgr_state(char *state_save_location)
 	safe_unpack16(&ver, buffer);
 	debug3("Version in assoc_mgr_state header is %u", ver);
 	if (ver > SLURM_PROTOCOL_VERSION || ver < SLURM_MIN_PROTOCOL_VERSION) {
+		if (!ignore_state_errors)
+			fatal("Can not recover assoc_mgr state, incompatible version, "
+			      "got %u need >= %u <= %u, start with '-i' to ignore this",
+			      ver, SLURM_MIN_PROTOCOL_VERSION, SLURM_PROTOCOL_VERSION);
 		error("***********************************************");
 		error("Can not recover assoc_mgr state, incompatible version, "
 		      "got %u need > %u <= %u", ver,
@@ -5759,6 +5783,9 @@ extern int load_assoc_mgr_state(char *state_save_location)
 	return SLURM_SUCCESS;
 
 unpack_error:
+	if (!ignore_state_errors)
+		fatal("Incomplete assoc mgr state file, start with '-i' to ignore this");
+	error("Incomplete assoc mgr state file");
 	if (buffer)
 		free_buf(buffer);
 	assoc_mgr_unlock(&locks);

RELEASE_NOTES
@@ -29,6 +29,9 @@ of Slurm will result in loss of state information.
 If using SPANK plugins that use the Slurm APIs, they should be recompiled when
 upgrading Slurm to a new major release.
 
+NOTE: The slurmctld is now set to fatal if there are any problems with
+      any state files.  To avoid this use the new '-i' flag.
+
 NOTE: systemd services files are installed automatically, but not enabled.
       You will need to manually enable them on the appropriate systems:
       - Controller: systemctl enable slurmctld
@@ -47,6 +50,8 @@ NOTE: If you are upgrading with any jobs from 14.03 or earlier
 NOTE: If you interact with any memory values in a job_submit plugin, you will
       need to test against NO_VAL64 instead of NO_VAL, and change your printf
       format as well.
+
+
 
 HIGHLIGHTS
 ==========

src/plugins/burst_buffer/cray/burst_buffer_cray.c
@@ -931,6 +931,8 @@ static void _recover_bb_state(void)
 	buffer = create_buf(data, data_size);
 	safe_unpack16(&protocol_version, buffer);
 	if (protocol_version == (uint16_t)NO_VAL) {
+		if (!ignore_state_errors)
+			fatal("Can not recover burst_buffer/cray state, data version incompatible, start with '-i' to ignore this");
 		error("******************************************************************");
 		error("Can not recover burst_buffer/cray state, data version incompatible");
 		error("******************************************************************");
@@ -1002,6 +1004,8 @@ static void _recover_bb_state(void)
 	return;
 
 unpack_error:
+	if (!ignore_state_errors)
+		fatal("Incomplete burst buffer data checkpoint file, start with '-i' to ignore this");
 	error("Incomplete burst buffer data checkpoint file");
 	xfree(account);
 	xfree(name);

src/plugins/priority/multifactor/priority_multifactor.c
@@ -314,6 +314,8 @@ static void _read_last_decay_ran(time_t *last_ran, time_t *last_reset)
 	return;
 
 unpack_error:
+	if (!ignore_state_errors)
+		fatal("Incomplete priority last decay file exiting, start with '-i' to ignore this");
 	error("Incomplete priority last decay file returning");
 	free_buf(buffer);
 	return;

src/plugins/select/bluegene/select_bluegene.c
@@ -654,6 +654,8 @@ static int _load_state_file(List curr_block_list, char *dir_name)
 		safe_unpack16(&protocol_version, buffer);
 
 	if (protocol_version == (uint16_t)NO_VAL) {
+		if (!ignore_state_errors)
+			fatal("Can not recover block state, data version incompatible, start with '-i' to ignore this");
 		error("***********************************************");
 		error("Can not recover block state, "
 		      "data version incompatible");
@@ -809,9 +811,11 @@ static int _load_state_file(List curr_block_list, char *dir_name)
 	return SLURM_SUCCESS;
 
 unpack_error:
+	if (!ignore_state_errors)
+		fatal("Incomplete block data checkpoint file, start with '-i' to ignore this");
+	error("Incomplete block data checkpoint file");
 	FREE_NULL_BITMAP(usable_mp_bitmap);
 	slurm_mutex_unlock(&block_state_mutex);
-	error("Incomplete block data checkpoint file");
 	free_buf(buffer);
 
 	return SLURM_FAILURE;

src/plugins/select/cray/select_cray.c
@@ -1469,6 +1469,8 @@ extern int select_p_state_restore(char *dir_name)
 	debug3("Version in blade_state header is %u", protocol_version);
 
 	if (protocol_version == (uint16_t)NO_VAL) {
+		if (!ignore_state_errors)
+			fatal("Can not recover blade state, data version incompatible, start with '-i' to ignore this");
 		error("***********************************************");
 		error("Can not recover blade state, "
 		      "data version incompatible");
@@ -1562,6 +1564,8 @@ extern int select_p_state_restore(char *dir_name)
 unpack_error:
 	slurm_mutex_unlock(&blade_mutex);
 
+	if (!ignore_state_errors)
+		fatal("Incomplete blade data checkpoint file, you may get unexpected issues if jobs were running. Start with '-i' to ignore this");
 	error("Incomplete blade data checkpoint file, you may get "
 	      "unexpected issues if jobs were running.");
 	free_buf(buffer);

src/plugins/slurmctld/nonstop/do_work.c
@@ -419,6 +419,8 @@ extern int restore_nonstop_state(void)
 	debug3("Version in slurmctld/nonstop header is %u", protocol_version);
 
 	if (protocol_version == (uint16_t) NO_VAL) {
+		if (!ignore_state_errors)
+			fatal("Can not recover slurmctld/nonstop state, incompatible version, start with '-i' to ignore this");
 		error("*************************************************************");
 		error("Can not recover slurmctld/nonstop state, incompatible version");
 		error("*************************************************************");
@@ -445,6 +447,8 @@ extern int restore_nonstop_state(void)
 	return error_code;
 
 unpack_error:
+	if (!ignore_state_errors)
+		fatal("Incomplete nonstop state file, start with '-i' to ignore this");
 	error("Incomplete nonstop state file");
 	free_buf(buffer);
 	return SLURM_FAILURE;

src/slurmctld/controller.c
@@ -2086,9 +2086,7 @@ extern void ctld_assoc_mgr_init(slurm_trigger_callbacks_t *callbacks)
 	}
 
 	/* Now load the usage from a flat file since it isn't kept in
-	   the database No need to check for an error since if this
-	   fails we will get an error message and we will go on our
-	   way.  If we get an error we can't do anything about it.
+	   the database
 	*/
 	load_assoc_usage(slurmctld_conf.state_save_location);
 	load_qos_usage(slurmctld_conf.state_save_location);

commit 281f5b39ce59b3e33ed69f53bc6898982e224a54

Fix Coverity issues caused by commit 250378c26a15.
Since you can no longer get into the code without there being a buffer there is
no reason to check it.

src/common/assoc_mgr.c
@@ -5462,8 +5462,9 @@ unpack_error:
 	if (!ignore_state_errors)
 		fatal("Incomplete assoc usage state file, start with '-i' to ignore this");
 	error("Incomplete assoc usage state file");
-	if (buffer)
-		free_buf(buffer);
+
+	free_buf(buffer);
+
 	xfree(tmp_str);
 	assoc_mgr_unlock(&locks);
 	return SLURM_ERROR;
@@ -5577,8 +5578,9 @@ unpack_error:
 	if (!ignore_state_errors)
 		fatal("Incomplete QOS usage state file, start with '-i' to ignore this");
 	error("Incomplete QOS usage state file");
-	if (buffer)
-		free_buf(buffer);
+
+	free_buf(buffer);
+
 	if (itr)
 		list_iterator_destroy(itr);
 	xfree(tmp_str);
@@ -5786,8 +5788,9 @@ unpack_error:
 	if (!ignore_state_errors)
 		fatal("Incomplete assoc mgr state file, start with '-i' to ignore this");
 	error("Incomplete assoc mgr state file");
-	if (buffer)
-		free_buf(buffer);
+
+	free_buf(buffer);
+
 	assoc_mgr_unlock(&locks);
 	return SLURM_ERROR;
 }