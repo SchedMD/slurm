SchedMD/slurm

commit fec995e0cd0870a77b277c7608876abc541c28d6

Fix gres output of a job if it is updated while pending to be displayed correctly with Slurm tools.
What this does is replace the incoming string with the gres actually chosen.

Continuation of last commit.

Bug 3521

src/common/gres.c
@@ -3003,7 +3003,7 @@ static bool _is_gres_cnt_zero(char *config)
  */
 extern int gres_plugin_job_state_validate(char **req_config, List *gres_list)
 {
-	char *tmp_str, *tok, *last = NULL;
+	char *tmp_str, *tok, *last = NULL, *new_req_config = NULL;
 	int i, rc;
 	gres_state_t *gres_ptr;
 	gres_job_state_t *job_gres_data;
@@ -3041,6 +3041,9 @@ extern int gres_plugin_job_state_validate(char **req_config, List *gres_list)
 				xfree(job_gres_data);
 				break;
 			}
+			if (new_req_config != NULL)
+			    xstrcat(new_req_config, ",");
+			xstrcat(new_req_config, tok);
 			gres_ptr = xmalloc(sizeof(gres_state_t));
 			gres_ptr->plugin_id = gres_context[i].plugin_id;
 			gres_ptr->gres_data = job_gres_data;
@@ -3060,6 +3063,9 @@ extern int gres_plugin_job_state_validate(char **req_config, List *gres_list)
 		tok = strtok_r(NULL, ",", &last);
 	}
 	slurm_mutex_unlock(&gres_context_lock);
+
+	xfree(*req_config);
+	*req_config = new_req_config;
 
 	xfree(tmp_str);
 	return rc;

commit 028bf3e13dedaf1387cbf49c3bd811607f7d12e1

Fix issue with whole gres not being printed out with Slurm tools.
This is a regression from commit fec995e0cd087.

It turns out using tok here was erroneous for situations where the gres had
a type and name and potentially a count (i.e. network:gigabit:1)

_get_gres_req_cnt() would alter the incoming char *config which is what tok
was.  So when we print it back to the requested string it would only have
what was there to the first ':'.  As we didn't need to \0 out the first char
as we skip over it anyway I just kept track of what the replaced \0 was for
the number portion and put it back when we are done copying it.

Related to bug 3521

src/common/gres.c
@@ -2922,11 +2922,20 @@ static int _get_gres_req_cnt(
 		}
 
 		if (type && ((cnt == -1) || (type != num))) {
-			type[0] = '\0';
-			if (num && type != num)
+			char tmp_char = '\0';
+
+			if (num && type != num) {
+				tmp_char = num[0];
 				num[0] = '\0';
+			}
 			type++;
 			*type_out = xstrdup(type);
+			/*
+			 * Since we don't want to change the original char sent
+			 * in we want to set this back to the way it was.
+			 */
+			if (tmp_char)
+				num[0] = tmp_char;
 		}
 	} else {
 		/* Did not find this GRES name, check for zero value */