SchedMD/slurm

commit f1110568a937efb4875a98b40be6149def9df71c

Properly set up srun debugging for pack jobs
This allocates an array to the proper size (based upon all tasks
  to be launched). Work still needed to populate the data structure
  properly for all tasks.

src/srun/srun.c
@@ -255,18 +255,34 @@ static void _launch_app(srun_job_t *job, List srun_job_list, bool got_alloc)
 	_launch_app_data_t *opts;
 	pthread_attr_t attr_steps;
 	pthread_t thread_steps = (pthread_t) 0;
-	int step_cnt = 0;
+	int total_ntasks = 0, step_cnt = 0;
 	pthread_mutex_t step_mutex = PTHREAD_MUTEX_INITIALIZER;
 	pthread_cond_t step_cond   = PTHREAD_COND_INITIALIZER;
 	srun_job_t *first_job = NULL;
+	char *launch_type;
+	bool need_mpir = false;
+
+	launch_type = slurm_get_launch_type();
+	if (launch_type && strstr(launch_type, "slurm"))
+		need_mpir = true;
+	xfree(launch_type);
 
 	if (srun_job_list) {
 		if (!opt_list) {
 			fatal("%s: have srun_job_list, but no opt_list",
 			      __func__);
 		}
-		slurm_attr_init(&attr_steps);
+
 		job_iter = list_iterator_create(srun_job_list);
+		if (need_mpir) {
+			while ((job = (srun_job_t *) list_next(job_iter))) {
+				total_ntasks = job->ntasks;
+			}
+			list_iterator_reset(job_iter);
+			mpir_init(total_ntasks);
+		}
+
+		slurm_attr_init(&attr_steps);
 		opt_iter = list_iterator_create(opt_list);
 		step_cnt = list_count(opt_list);
 		while ((opt_local = (opt_t *) list_next(opt_iter))) {
@@ -309,6 +325,8 @@ static void _launch_app(srun_job_t *job, List srun_job_list, bool got_alloc)
 		if (first_job)
 			fini_srun(first_job, got_alloc, &global_rc, 0);
 	} else {
+		if (need_mpir)
+			mpir_init(job->ntasks);
 		opts = xmalloc(sizeof(_launch_app_data_t));
 		opts->got_alloc   = got_alloc;
 		opts->job         = job;

src/plugins/launch/slurm/launch_slurm.c
@@ -703,9 +703,6 @@ extern int launch_p_step_launch(srun_job_t *job, slurm_step_io_fds_t *cio_fds,
 		slurm_mutex_unlock(&launch_lock);
 	}
 
-//FIXME-PACK: Need to configure all debugger environment for pack-jobs
-	mpir_init(job->ntasks);
-
 	update_job_state(job, SRUN_JOB_LAUNCHING);
 	launch_start_time = time(NULL);
 	if (first_launch) {

src/srun/libsrun/multi_prog.c
@@ -242,19 +242,12 @@ mpir_init(int num_tasks)
 	/* Use symbols from the runjob.so library provided by IBM.
 	 * Do NOT use debugger symbols local to the srun command */
 #else
-//FIXME-PACK: Need to call once per srun
-static pthread_mutex_t mpir_mutex = PTHREAD_MUTEX_INITIALIZER;
-num_tasks += 16;
-slurm_mutex_lock(&mpir_mutex);
-if (!MPIR_proctable) {
 	MPIR_proctable_size = num_tasks;
 	MPIR_proctable = xmalloc(sizeof(MPIR_PROCDESC) * num_tasks);
 	if (MPIR_proctable == NULL) {
 		error("Unable to initialize MPIR_proctable: %m");
 		exit(error_exit);
 	}
-}
-slurm_mutex_unlock(&mpir_mutex);
 #endif
 }
 

commit dfae8eb6d90f1c41cd4c976643ca21e13e4153e5

srun debug fix
Fix for debugger setup bug introduced in commit
f1110568a937efb4875a98b40be6149def9df71c

src/srun/srun.c
@@ -276,7 +276,7 @@ static void _launch_app(srun_job_t *job, List srun_job_list, bool got_alloc)
 		job_iter = list_iterator_create(srun_job_list);
 		if (need_mpir) {
 			while ((job = (srun_job_t *) list_next(job_iter))) {
-				total_ntasks = job->ntasks;
+				total_ntasks += job->ntasks;
 			}
 			list_iterator_reset(job_iter);
 			mpir_init(total_ntasks);