SchedMD/slurm

commit afeca4e22ccf105006dbaf11ec542577f7b5f818

Change daemon() call to xdaemon() to avoid confusion with libc versions.
The glibc version does not double-fork like Slurm's version.
Rename to xdaemon to avoid confusion and ensure no one tries to
remove it again without further investigation
(see commits 6f45a2bf85/954288ad).

While here, simplify it as the arguements are unchanged in the three
calling locations.

Remove incorrect documentation as well - our xdaemon() call does
not close everything.

src/common/daemonize.c
@@ -50,15 +50,14 @@
 #include "src/common/macros.h"
 #include "src/common/xassert.h"
 
-/* detach and go into background.
- * caller is responsible for umasks
- *
- * if nochdir == 0, will do a chdir to /
- * if noclose == 0, will close all FDs
+/*
+ * Double-fork and go into background.
+ * Caller is responsible for umasks
  */
-int
-daemon(int nochdir, int noclose)
+int xdaemon(void)
 {
+	int devnull;
+
 	switch (fork()) {
 		case  0 : break;        /* child */
 		case -1 : return -1;
@@ -74,38 +73,22 @@ daemon(int nochdir, int noclose)
 		default: _exit(0);      /* exit parent */
 	}
 
-	if (!nochdir && chdir("/") < 0) {
-		error("chdir(/): %m");
-		return -1;
-	}
-
-	/* Close all file descriptors if requested
+	/*
+	 * dup stdin, stdout, and stderr onto /dev/null
 	 */
-	if (!noclose) {
-		closeall(0);
-		if (open("/dev/null", O_RDWR) != 0)
-			error("Unable to open /dev/null on stdin: %m");
-		dup2(0, STDOUT_FILENO);
-		dup2(0, STDERR_FILENO);
-	} else {
-		/*
-		 * Otherwise, dup stdin, stdout, and stderr onto /dev/null
-		 */
-		int devnull = open("/dev/null", O_RDWR);
-		if (devnull < 0)
-			error("Unable to open /dev/null: %m");
-		if (dup2(devnull, STDIN_FILENO) < 0)
-			error("Unable to dup /dev/null onto stdin: %m");
-		if (dup2(devnull, STDOUT_FILENO) < 0)
-			error("Unable to dup /dev/null onto stdout: %m");
-		if (dup2(devnull, STDERR_FILENO) < 0)
-			error("Unable to dup /dev/null onto stderr: %m");
-		if (close(devnull) < 0)
-			error("Unable to close /dev/null: %m");
-	}
+	devnull = open("/dev/null", O_RDWR);
+	if (devnull < 0)
+		error("Unable to open /dev/null: %m");
+	if (dup2(devnull, STDIN_FILENO) < 0)
+		error("Unable to dup /dev/null onto stdin: %m");
+	if (dup2(devnull, STDOUT_FILENO) < 0)
+		error("Unable to dup /dev/null onto stdout: %m");
+	if (dup2(devnull, STDERR_FILENO) < 0)
+		error("Unable to dup /dev/null onto stderr: %m");
+	if (close(devnull) < 0)
+		error("Unable to close /dev/null: %m");
 
-	return 0;
-
+	return -1;
 }
 
 /*

src/common/daemonize.h
@@ -40,13 +40,12 @@
 #ifndef _HAVE_DAEMONIZE_H
 #define _HAVE_DAEMONIZE_H
 
-/* fork process into background and inherit new session
- * if nochdir is 0, performs a chdir("/")
- * if noclose is 0, closes all fds and dups stdout/err of daemon onto /dev/null
+/*
+ * Fork process into background and inherit new session.
  *
- * returns -1 on error.
+ * Returns -1 on error.
  */
-extern int daemon(int nochdir, int noclose);
+extern int xdaemon(void);
 
 /* Write pid into file pidfile if uid is not 0 change the owner of the
  * pidfile to that user.

src/slurmctld/controller.c
@@ -287,7 +287,7 @@ int main(int argc, char **argv)
 
 	if (daemonize) {
 		slurmctld_config.daemonize = 1;
-		if (daemon(1, 1))
+		if (xdaemon())
 			error("daemon(): %m");
 		log_set_timefmt(slurmctld_conf.log_fmt);
 		log_alter(log_opts, LOG_DAEMON,
@@ -304,8 +304,6 @@ int main(int argc, char **argv)
 	 * (init_pidfile() exits if it can't initialize pid file).
 	 * On Linux we also need to make this setuid job explicitly
 	 * able to write a core dump.
-	 * This also has to happen after daemon(), which closes all fd's,
-	 * so we keep the write lock of the pidfile.
 	 */
 	_init_pidfile();
 	_become_slurm_user();

src/slurmd/slurmd/slurmd.c
@@ -290,10 +290,9 @@ main (int argc, char **argv)
 
 	/*
 	 * Become a daemon if desired.
-	 * Do not chdir("/") or close all fd's
 	 */
 	if (conf->daemonize) {
-		if (daemon(1,1) == -1)
+		if (xdaemon())
 			error("Couldn't daemonize slurmd: %m");
 	}
 	test_core_limit();
@@ -348,8 +347,6 @@ main (int argc, char **argv)
 	_create_msg_socket();
 
 	conf->pid = getpid();
-	/* This has to happen after daemon(), which closes all fd's,
-	 * so we keep the write lock of the pidfile. */
 	pidfd = create_pidfile(conf->pidfile, 0);
 
 	rfc2822_timestamp(time_stamp, sizeof(time_stamp));

src/slurmdbd/slurmdbd.c
@@ -159,8 +159,6 @@ int main(int argc, char **argv)
 	 * (init_pidfile() exits if it can't initialize pid file).
 	 * On Linux we also need to make this setuid job explicitly
 	 * able to write a core dump.
-	 * This also has to happen after daemon(), which closes all fd's,
-	 * so we keep the write lock of the pidfile.
 	 */
 	_init_pidfile();
 	_become_slurm_user();
@@ -607,7 +605,7 @@ static void _init_pidfile(void)
  * "cd" to the LogFile directory (if one is configured) */
 static void _daemonize(void)
 {
-	if (daemon(1, 1))
+	if (xdaemon())
 		error("daemon(): %m");
 	log_alter(log_opts, LOG_DAEMON, slurmdbd_conf->log_file);
 }

commit 941cac12d3818620124d50750b745790569a2987

Fix incorrect return code from xdaemon().
Regression in commit afeca4e22cc. Bug 4026.

src/common/daemonize.c
@@ -88,7 +88,7 @@ int xdaemon(void)
 	if (close(devnull) < 0)
 		error("Unable to close /dev/null: %m");
 
-	return -1;
+	return 0;
 }
 
 /*