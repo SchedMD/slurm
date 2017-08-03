ae932d2bdfcd37dd1f42cfe456fcc9eb8bda0783
Morris Jette <jette@schedmd.com>
2017-08-03 10:19:40 -0600

    Add error handling logic for Coverity
    CID 44936

--- src/api/step_launch.c
+++ src/api/step_launch.c
@@ -1867,6 +1867,10 @@ _exec_prog(slurm_msg_t *msg)
 	child = fork();
 	if (child == 0) {
 		int fd = open("/dev/null", O_RDONLY);
+		if (fd < 0) {
+			error("%s: can not open /dev/null", __func__);
+			exit(1);
+		}
 		dup2(fd, 0);		/* stdin from /dev/null */
 		dup2(pfd[1], 1);	/* stdout to pipe */
 		dup2(pfd[1], 2);	/* stderr to pipe */
