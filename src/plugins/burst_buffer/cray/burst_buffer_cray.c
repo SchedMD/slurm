SchedMD/slurm

commit 241f31d7470e5ed3e0a15ac8b2c4df7f0fb7f685

Add burst buffer support for heterogeneous jobs

src/plugins/burst_buffer/cray/burst_buffer_cray.c
@@ -2625,7 +2625,10 @@ static int _xlate_batch(struct job_descriptor *job_desc)
 		} else {
 			if (is_cont) {
 				if (line_type != prev_type) {
-					/* Mixing "#DW" with "#BB", error */
+					/*
+					 * Mixing "#DW" with "#BB" on same
+					 * (continued) line, error
+					 */
 					rc =ESLURM_INVALID_BURST_BUFFER_REQUEST;
 					break;
 				}
@@ -3293,7 +3296,6 @@ extern int bb_p_job_validate2(struct job_record *job_ptr, char **err_msg)
 		xstrfmtcat(script_file, "%s/script", job_dir);
 		if (job_ptr->batch_flag == 0)
 			rc = _build_bb_script(job_ptr, script_file);
-	}
 
 	/* Run "job_process" function, validates user script */
 	script_argv = xmalloc(sizeof(char *) * 10);	/* NULL terminated */
@@ -5285,4 +5287,17 @@ extern char *bb_p_xlate_bb_2_tres_str(char *burst_buffer)
 		xstrfmtcat(result, "%d=%"PRIu64, bb_state.tres_id, total);
 
 	return result;
+}
+
+/*
+ * Convert a pack job batch script into a script containing only the portions
+ * relevant to a specific pack job component.
+ *
+ * script IN - Whole job batch script
+ * pack_job_offset IN - Zero origin pack job component ID
+ * RET script for that job component, call xfree() to release memory
+ */
+extern char *bb_p_build_pack_script(char *script, uint32_t pack_job_offset)
+{
+	return bb_build_pack_script(script, pack_job_offset);
 }

doc/html/heterogeneous_jobs.shtml
@@ -5,6 +5,7 @@
 <ul>
 <li><a href="#overview">Overview</a></li>
 <li><a href="#submitting">Submitting Jobs</a></li>
+<li><a href="#burst_buffer">Burst Buffers</a></li>
 <li><a href="#managing">Managing Jobs</a></li>
 <li><a href="#job_steps">Launching Applications (Job Steps)</a></li>
 <li><a href="#env_var">Environment Variables</a></li>
@@ -104,6 +105,11 @@ A list of propagated options follows.</p>
 <li>--workdir</li>
 </ul>
 
+<p>Some options should be specified only in the first job component.
+For example, specifyihg a batch job output file in the second job component's
+options will result in the first job component (where the batch script executes)
+using the default output file name.</p>
+
 <p>Environment variables used to specify default options for the job submit
 command will be applied to every component of the heterogeneous job
 (e.g. <i>SBATCH_ACCOUNT</i>).</p>
@@ -152,6 +158,58 @@ The heterogeneous job as a whole is also tested, but in a more limited
 fashion with respect to quality of service (QOS) limits.
 Each component of a heterogeneous job counts as a "job" with respect to
 resource limits.</p>
+
+<h2><a name="burst_buffer">Burst Buffers</a></h2>
+
+<p>A burst buffer can either be persistent or linked to a specific job ID
+(at least the Cray implementation).
+Since a heterogeneous job consists of multiple job IDs, a job-specific burst
+buffer will be associated with only one heterogeneous job component.
+Only a persistent burst buffer can be accessed by all components of a
+heterogeneous job.
+A sample batch script demonstrating this for a Cray system is appended.</p>
+
+<pre>
+#!/bin/bash
+#SBATCH --nodes=1 --constraint=haswell
+#BB create_persistent name=alpha capacity=10 access=striped type=scratch
+#DW persistentdw name=alpha
+#SBATCH packjob
+#SBATCH --nodes=16 --constraint=knl
+#DW persistentdw name=alpha
+...
+</pre>
+
+<p><b>NOTE:</b> Cray's DataWarp interface directly reads the job script, but
+has no knowledge of "Slurm's "packjob" directive, so Slurm internally rebuilds
+the script for each job component so that only that job components burst buffer
+directives are included in that script. The batch script first component of the
+job will be modified in order to replace the "#DW" directives of other
+job components with "#EXCLUDED DW" to prevent their interpretation by Cray
+infrastructure. Since the batch script will only be executed by the first job
+component, the subsequent job components will not include commands from the
+original script. An example is shown below:</p>
+
+<pre>
+<b>Rebuilt script for first job component</b>
+
+#!/bin/bash
+#SBATCH --nodes=1 --constraint=haswell
+#BB create_persistent name=alpha capacity=10 access=striped type=scratch
+#DW persistentdw name=alpha
+#SBATCH packjob
+#SBATCH --nodes=16 --constraint=knl
+#EXCLUDED DW persistentdw name=alpha
+...
+
+
+<b>Rebuilt script for second job component</b>
+
+#!/bin/bash
+#SBATCH --nodes=16 --constraint=knl
+#DW persistentdw name=alpha
+exit 0
+</pre>
 
 <h2><a name="managing">Managing Jobs</a></h2>
 
@@ -579,6 +637,6 @@ especially other heterogeneous jobs.</p>
 
 <p class="footer"><a href="#top">top</a></p>
 
-<p style="text-align:center;">Last modified 4 September 2017</p>
+<p style="text-align:center;">Last modified 8 September 2017</p>
 
 <!--#include virtual="footer.txt"-->

src/plugins/burst_buffer/common/burst_buffer_common.c
@@ -1670,3 +1670,68 @@ extern bool bb_valid_pool_test(bb_state_t *state_ptr, char *pool_name)
 
 	return false;
 }
+
+/* Return true if pack job separator in the script */
+static bool _pack_check(char *tok)
+{
+	if (strncmp(tok+1, "SLURM",  5) &&
+	    strncmp(tok+1, "SBATCH", 6))
+		return false;
+	if (!strstr(tok+6, "packjob"))
+		return false;
+	return true;
+}
+
+/*
+ * Convert a pack job batch script into a script containing only the portions
+ * relevant to a specific pack job component.
+ *
+ * script IN - Whole job batch script
+ * pack_job_offset IN - Zero origin pack job component ID
+ * RET script for that job component, call xfree() to release memory
+ */
+extern char *bb_build_pack_script(char *script, uint32_t pack_job_offset)
+{
+	char *result = NULL, *tmp = NULL;
+	char *tok, *save_ptr = NULL;
+	bool fini = false;
+	int cur_offset = 0;
+
+	tmp = xstrdup(script);
+	tok = strtok_r(tmp, "\n", &save_ptr);
+	while (tok) {
+		if (!result) {
+			xstrfmtcat(result, "%s\n", tok);
+		} else if (tok[0] != '#') {
+			fini = true;
+		} else if (_pack_check(tok)) {
+			cur_offset++;
+			if (cur_offset > pack_job_offset)
+				fini = true;
+		} else if (cur_offset == pack_job_offset) {
+			xstrfmtcat(result, "%s\n", tok);
+		}
+		if (fini)
+			break;
+		tok = strtok_r(NULL, "\n", &save_ptr);
+	}
+
+	if (pack_job_offset == 0) {
+		while (tok) {
+			char *sep = "";
+			if ((tok[0] == '#') &&
+			    (((tok[1] == 'B') && (tok[2] == 'B')) ||
+			     ((tok[1] == 'D') && (tok[2] == 'W')))) {
+				sep = "#EXCLUDED ";
+				tok++;
+			}
+			xstrfmtcat(result, "%s%s\n", sep, tok);
+			tok = strtok_r(NULL, "\n", &save_ptr);
+		}
+	} else if (result) {
+		xstrcat(result, "exit 0\n");
+	}
+	xfree(tmp);
+
+	return result;
+}

src/plugins/burst_buffer/common/burst_buffer_common.h
@@ -365,4 +365,14 @@ extern int bb_post_persist_delete(bb_alloc_t *bb_alloc, bb_state_t *state_ptr);
 /* Determine if the specified pool name is valid on this system */
 extern bool bb_valid_pool_test(bb_state_t *state_ptr, char *pool_name);
 
+/*
+ * Convert a pack job batch script into a script containing only the portions
+ * relevant to a specific pack job component.
+ *
+ * script IN - Whole job batch script
+ * pack_job_offset IN - Zero origin pack job component ID
+ * RET script for that job component, call xfree() to release memory
+ */
+extern char *bb_build_pack_script(char *script, uint32_t pack_job_offset);
+
 #endif	/* __BURST_BUFFER_COMMON_H__ */

src/plugins/burst_buffer/generic/burst_buffer_generic.c
@@ -285,3 +285,16 @@ extern char *bb_p_xlate_bb_2_tres_str(char *burst_buffer)
 {
 	return NULL;
 }
+
+/*
+ * Convert a pack job batch script into a script containing only the portions
+ * relevant to a specific pack job component.
+ *
+ * script IN - Whole job batch script
+ * pack_job_offset IN - Zero origin pack job component ID
+ * RET script for that job component, call xfree() to release memory
+ */
+extern char *bb_p_build_pack_script(char *script, uint32_t pack_job_offset)
+{
+	return bb_build_pack_script(script, pack_job_offset);
+}

src/slurmctld/burst_buffer.c
@@ -66,6 +66,8 @@ typedef struct slurm_bb_ops {
 					 uid_t submit_uid);
 	int		(*job_validate2)(struct job_record *job_ptr,
 					 char **err_msg);
+	char *		(*build_pack_script)(char *script,
+					     uint32_t pack_job_offset);
 	void		(*job_set_tres_cnt) (struct job_record *job_ptr,
 					     uint64_t *tres_cnt, bool locked);
 	time_t		(*job_get_est_start) (struct job_record *job_ptr);
@@ -91,6 +93,7 @@ static const char *syms[] = {
 	"bb_p_reconfig",
 	"bb_p_job_validate",
 	"bb_p_job_validate2",
+	"bb_p_build_pack_script",
 	"bb_p_job_set_tres_cnt",
 	"bb_p_job_get_est_start",
 	"bb_p_job_try_stage_in",
@@ -369,6 +372,41 @@ extern int bb_g_job_validate2(struct job_record *job_ptr, char **err_msg)
 	END_TIMER2(__func__);
 
 	return rc;
+}
+
+/*
+ * Convert a pack job batch script into a script containing only the portions
+ * relevant to a specific pack job component.
+ *
+ * script IN - Whole job batch script
+ * pack_job_offset IN - Zero origin pack job component ID
+ * RET script for that job component, call xfree() to release memory
+ */
+extern char *bb_g_build_pack_script(char *script, uint32_t pack_job_offset)
+{
+	DEF_TIMERS;
+	char *result = NULL;
+	int i;
+
+	START_TIMER;
+	(void) bb_g_init();
+	slurm_mutex_lock(&g_context_lock);
+	for (i = 0; i < g_context_cnt; i++) {
+		if (i > 0) {
+			/*
+			 * This should not be a problem in practice.
+			 * All plugins call a single function in common.
+			 */
+			debug("%s: Only one burst buffer plugin currently supported",
+			      __func__);
+			break;
+		}
+		result = (*(ops[i].build_pack_script))(script, pack_job_offset);
+	}
+	slurm_mutex_unlock(&g_context_lock);
+	END_TIMER2(__func__);
+
+	return result;
 }
 
 /*

src/slurmctld/burst_buffer.h
@@ -114,6 +114,16 @@ extern int bb_g_job_validate(struct job_descriptor *job_desc,
 extern int bb_g_job_validate2(struct job_record *job_ptr, char **err_msg);
 
 /*
+ * Convert a pack job batch script into a script containing only the portions
+ * relevant to a specific pack job component.
+ *
+ * script IN - Whole job batch script
+ * pack_job_offset IN - Zero origin pack job component ID
+ * RET script for that job component, call xfree() to release memory
+ */
+extern char *bb_g_build_pack_script(char *script, uint32_t pack_job_offset);
+
+/*
  * Fill in the tres_cnt (in MB) based off the job record
  * NOTE: Based upon job-specific burst buffers, excludes persistent buffers
  * IN job_ptr - job record

src/slurmctld/proc_req.c
@@ -3867,11 +3867,12 @@ static void _slurm_rpc_submit_batch_pack_job(slurm_msg_t *msg)
 	ListIterator iter;
 	int error_code = SLURM_SUCCESS, alloc_only = 0;
 	DEF_TIMERS;
-	uint32_t pack_job_id = 0, pack_job_offset = 0;;
+	uint32_t pack_job_id = 0, pack_job_offset = 0;
 	struct job_record *job_ptr = NULL, *first_job_ptr = NULL;
 	slurm_msg_t response_msg;
 	submit_response_msg_t submit_msg;
 	job_desc_msg_t *job_desc_msg;
+	char *script = NULL;
 	/* Locks: Read config, read job, read node, read partition */
 	slurmctld_lock_t job_read_lock = {
 		READ_LOCK, READ_LOCK, READ_LOCK, READ_LOCK, NO_LOCK };
@@ -3962,6 +3963,8 @@ static void _slurm_rpc_submit_batch_pack_job(slurm_msg_t *msg)
 	START_TIMER;	/* Restart after we have locks */
 	iter = list_iterator_create(job_req_list);
 	while ((job_desc_msg = (job_desc_msg_t *) list_next(iter))) {
+		if (!script)
+			script = xstrdup(job_desc_msg->script);
 		if (pack_job_offset && job_desc_msg->script) {
 			info("%s: Pack job %u offset %u has script, being ignord",
 			     __func__, pack_job_id, pack_job_offset);
@@ -3972,6 +3975,10 @@ static void _slurm_rpc_submit_batch_pack_job(slurm_msg_t *msg)
 			/* Email notifications disable except for pack leader */
 			job_desc_msg->mail_type = 0;
 			xfree(job_desc_msg->mail_user);
+		}
+		if (!job_desc_msg->burst_buffer) {
+			job_desc_msg->script =
+				bb_g_build_pack_script(script, pack_job_offset);
 		}
 		error_code = job_allocate(job_desc_msg,
 					  job_desc_msg->immediate, false,
@@ -4007,6 +4014,7 @@ static void _slurm_rpc_submit_batch_pack_job(slurm_msg_t *msg)
 			break;
 	}
 	list_iterator_destroy(iter);
+	xfree(script);
 
 	if ((pack_job_id == 0) && !reject_job) {
 		info("%s: No error, but no pack_job_id", __func__);

commit b7e6c42e0aa5fcc6cd1eff7daf40d92d39cd050e

Correction to commit 241f31d7470e5e
Accidentally removed bracked in checked-in code

src/plugins/burst_buffer/cray/burst_buffer_cray.c
@@ -3296,6 +3296,7 @@ extern int bb_p_job_validate2(struct job_record *job_ptr, char **err_msg)
 		xstrfmtcat(script_file, "%s/script", job_dir);
 		if (job_ptr->batch_flag == 0)
 			rc = _build_bb_script(job_ptr, script_file);
+	}
 
 	/* Run "job_process" function, validates user script */
 	script_argv = xmalloc(sizeof(char *) * 10);	/* NULL terminated */