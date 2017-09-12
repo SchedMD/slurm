86121c93842f6272920f74c3780cf39d39387795
Morris Jette <jette@schedmd.com>
2017-09-12 14:21:50 -0600

    Add check for bad node names
    Coverity CID 44695

--- src/slurmctld/proc_req.c
+++ src/slurmctld/proc_req.c
@@ -2374,7 +2374,8 @@ static void _slurm_rpc_complete_batch_script(slurm_msg_t *msg,
 		return;
 	}
 
-	/* Send batch step info to accounting, only if the job is
+	/*
+	 * Send batch step info to accounting, only if the job is
 	 * still completing.
 	 *
 	 * When a job is requeued because of node failure, and there is no
@@ -2390,8 +2391,8 @@ static void _slurm_rpc_complete_batch_script(slurm_msg_t *msg,
 	 * into account the COMPLETING FLAG which is valid, but not
 	 * always set yet when the step exits normally.
 	 */
-	if (association_based_accounting && job_ptr
-	    && (job_ptr->job_state != JOB_PENDING)) {
+	if (association_based_accounting && job_ptr &&
+	    (job_ptr->job_state != JOB_PENDING)) {
 		struct step_record batch_step;
 		memset(&batch_step, 0, sizeof(struct step_record));
 		batch_step.job_ptr = job_ptr;
@@ -2402,8 +2403,11 @@ static void _slurm_rpc_complete_batch_script(slurm_msg_t *msg,
 		nodes = job_ptr->nodes;
 #endif
 		batch_step.gres = nodes;
-		node_name2bitmap(batch_step.gres, false,
-				 &batch_step.step_node_bitmap);
+		if (node_name2bitmap(batch_step.gres, false,
+				     &batch_step.step_node_bitmap) != 0) {
+			error("%s: job %u has invalide node list (%s)",
+			      __func__, job_ptr->job_id, batch_step.gres);
+		}
 		batch_step.requid = -1;
 		batch_step.start_time = job_ptr->start_time;
 		batch_step.name = "batch";
