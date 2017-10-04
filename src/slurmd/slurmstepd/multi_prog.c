bb16277315ac22ac5f5956cf144ab2882e9a88c8
Dominik Bartkiewicz <bart@schedmd.com>
2017-10-04 16:09:28 -0600

    [PATCH 1/2] fix memory handling in multi_prog_get_argv

--- src/slurmd/slurmstepd/multi_prog.c
+++ src/slurmd/slurmstepd/multi_prog.c
@@ -119,25 +119,6 @@ _in_range(int rank, char* spec, int *offset)
 	return 0;
 }
 
-/* substitute "%t" or "%o" in argument with task number or range offset */
-static void
-_sub_expression(char *args_spec, int task_rank, int task_offset)
-{
-	char tmp[BUF_SIZE];
-
-	if (args_spec[0] == '%') {
-		if (args_spec[1] == 't') {
-			/* task rank */
-			strlcpy(tmp, &args_spec[2], sizeof(tmp));
-			sprintf(args_spec, "%d%s", task_rank, tmp);
-		} else if (args_spec[1] == 'o') {
-			/* task offset */
-			strlcpy(tmp, &args_spec[2], sizeof(tmp));
-			sprintf(args_spec, "%d%s", task_offset, tmp);
-		}
-	}
-}
-
 /*
  * FIXME - It would be nice to parse the multi-prog array just once
  *	to retrieve the argv arrays for each task on this node, rather
@@ -150,11 +131,15 @@ extern int multi_prog_get_argv(char *config_data, char **prog_env,
 	char *line = NULL;
 	int i, line_num = 0;
 	int task_offset;
-	char *p = NULL, *s = NULL, *ptrptr = NULL;
+	char *p = NULL, *ptrptr = NULL;
 	char *rank_spec = NULL, *args_spec = NULL;
 	int prog_argc = 0;
 	char **prog_argv = NULL;
 	char *local_data = NULL;
+	size_t tmp_buf_len = 256;
+	char tmp_buf[tmp_buf_len];
+	char *arg_buf = NULL;
+
 
 	prog_argv = (char **)xmalloc(sizeof(char *) * MAX_ARGC);
 
@@ -175,11 +160,6 @@ extern int multi_prog_get_argv(char *config_data, char **prog_env,
 			goto fail;
 		}
 		line_num++;
-		if (strlen(line) >= (BUF_SIZE - 1)) {
-			error ("Line %d of configuration file too long",
-				line_num);
-			goto fail;
-		}
 
 		p = line;
 		while ((*p != '\0') && isspace (*p)) /* remove leading spaces */
@@ -211,67 +191,80 @@ extern int multi_prog_get_argv(char *config_data, char **prog_env,
 		args_spec = p;
 		while (*args_spec != '\0') {
 			/* Only simple quote and escape supported */
-			args_spec = xstrdup(args_spec);
-			prog_argv[prog_argc++] = args_spec;
+			if (arg_buf) {
+				prog_argv[prog_argc++] = arg_buf;
+				arg_buf=NULL;
+			}
 			if ((prog_argc + 1) >= MAX_ARGC) {
 				info("Exceeded multi-prog argc limit");
 				break;
 			}
-		CONT:	while ((*args_spec != '\0') && (*args_spec != '\\') &&
+		CONT:	p = args_spec;
+			while ((*args_spec != '\0') && (*args_spec != '\\') &&
 			       (*args_spec != '%')  && (*args_spec != '\'') &&
 			       !isspace(*args_spec)) {
 				args_spec++;
 			}
+			xstrncat(arg_buf, p, (args_spec - p));
 			if (*args_spec == '\0') {
 				/* the last argument */
 				break;
 
 			} else if (*args_spec == '%') {
-				_sub_expression(args_spec, task_rank,
-						task_offset);
+				args_spec++;
+				if (*args_spec == 't') {
+					/* task rank */
+					snprintf(tmp_buf, tmp_buf_len, "%d",
+						 task_rank);
+					xstrcat(arg_buf, tmp_buf);
+				} else if (*args_spec == 'o') {
+					/* task offset */
+					snprintf(tmp_buf, tmp_buf_len, "%d",
+						 task_offset);
+					xstrcat(arg_buf, tmp_buf);
+				}
 				args_spec++;
 				goto CONT;
 
 			} else if (*args_spec == '\\') {
 				/* escape, just remove the backslash */
-				s = args_spec++;
-				p = args_spec;
-				do {
-					*s++ = *p;
-				} while (*p++ != '\0');
+				args_spec++;
+				if (*args_spec != '\0') {
+					xstrcatchar(arg_buf, *args_spec);
+					args_spec++;
+				}
 				goto CONT;
 
 			} else if (*args_spec == '\'') {
 				/* single quote,
 				 * preserve all characters quoted. */
-				p = args_spec + 1;
-				while ((*p != '\0') && (*p != '\'')) {
-					/* remove quote */
-					*args_spec++ = *p++;
+				p = ++args_spec;
+				while ((*args_spec != '\0') &&
+				       (*args_spec != '\'')) {
+					args_spec++;
 				}
-				if (*p == '\0') {
+				if (*args_spec == '\0') {
 					/* closing quote not found */
-					error("Program arguments specification"
-					      " format invalid: %s.",
-					      prog_argv[prog_argc -1]);
+					error("Program arguments specification format invalid: %s.",
+					      prog_argv[prog_argc - 1]);
 					goto fail;
 				}
-				p++; /* skip closing quote */
-				s = args_spec;
-				do {
-					*s++ = *p;
-				} while (*p++ != '\0');
+				xstrncat(arg_buf, p, (args_spec - p));
+				args_spec++;
 				goto CONT;
 
 			} else {
 				/* space */
-				*args_spec++ = '\0';
 				while ((*args_spec != '\0') &&
 				       isspace(*args_spec)) {
 					args_spec++;
 				}
 			}
 
+		}
+		if (arg_buf) {
+			prog_argv[prog_argc++] = arg_buf;
+			arg_buf = NULL;
 		}
 
 		for (i = 2; i < global_argc; i++) {
