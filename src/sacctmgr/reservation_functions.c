76fe4a7b2ae3321761b5d8a202944c6e84994403
Marshall Garey <marshall@schedmd.com>
2017-10-25 10:58:04 -0600

    Reorder conditions for reservations

--- src/sacctmgr/reservation_functions.c
+++ src/sacctmgr/reservation_functions.c
@@ -80,14 +80,13 @@ static int _set_cond(int *start, int argc, char **argv,
 			if (slurm_addto_char_list(reservation_cond->cluster_list,
 						  argv[i]+end))
 				set = 1;
-		} else if (!xstrncasecmp(argv[i], "Names",
-					 MAX(command_len, 2))) {
-			if (!reservation_cond->name_list) {
-				reservation_cond->name_list =
-					list_create(slurm_destroy_char);
-			}
-			if (slurm_addto_char_list(reservation_cond->name_list,
-						  argv[i]+end))
+		} else if (!xstrncasecmp(argv[i], "End",
+					 MAX(command_len, 1))) {
+			reservation_cond->time_end =
+				parse_time(argv[i]+end, 1);
+			if (errno == ESLURM_INVALID_TIME_VALUE)
+				exit_code = 1;
+			else
 				set = 1;
 		} else if (!xstrncasecmp(argv[i], "Format",
 					 MAX(command_len, 1))) {
@@ -102,6 +101,15 @@ static int _set_cond(int *start, int argc, char **argv,
 			if (slurm_addto_char_list(reservation_cond->id_list,
 						 argv[i]+end))
 				set = 1;
+		} else if (!xstrncasecmp(argv[i], "Names",
+					 MAX(command_len, 2))) {
+			if (!reservation_cond->name_list) {
+				reservation_cond->name_list =
+					list_create(slurm_destroy_char);
+			}
+			if (slurm_addto_char_list(reservation_cond->name_list,
+						  argv[i]+end))
+				set = 1;
 		} else if (!xstrncasecmp(argv[i], "Nodes",
 					 MAX(command_len, 2))) {
 			xfree(reservation_cond->nodes);
@@ -111,14 +119,6 @@ static int _set_cond(int *start, int argc, char **argv,
 		} else if (!xstrncasecmp(argv[i], "Start",
 					 MAX(command_len, 5))) {
 			reservation_cond->time_start =
-				parse_time(argv[i]+end, 1);
-			if (errno == ESLURM_INVALID_TIME_VALUE)
-				exit_code = 1;
-			else
-				set = 1;
-		} else if (!xstrncasecmp(argv[i], "End",
-					 MAX(command_len, 1))) {
-			reservation_cond->time_end =
 				parse_time(argv[i]+end, 1);
 			if (errno == ESLURM_INVALID_TIME_VALUE)
 				exit_code = 1;
