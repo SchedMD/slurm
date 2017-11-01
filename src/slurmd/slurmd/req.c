b30e9e9ee2ade6951bfaf28e15ef77325a206971
Ryan Day <day36@llnl.gov>
2017-10-31 18:28:16 -0600

    Fix security issue in Prolog and Epilog by always prepending SPANK_ to env vars.
    CVE-2017-15566.
    
    Bug 4228.

--- src/slurmd/slurmd/req.c
+++ src/slurmd/slurmd/req.c
@@ -5580,10 +5580,8 @@ _build_env(job_env_t *job_env)
 		job_env->spank_job_env_size = 0;
 		job_env->spank_job_env = (char **) NULL;
 	}
-	if (job_env->spank_job_env_size) {
-		env_array_merge_spank(&env,
-				      (const char **) job_env->spank_job_env);
-	}
+	if (job_env->spank_job_env_size)
+		env_array_merge(&env, (const char **) job_env->spank_job_env);
 
 	slurm_mutex_lock(&conf->config_mutex);
 	setenvf(&env, "SLURMD_NODENAME", "%s", conf->node_name);
