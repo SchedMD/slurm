SchedMD/slurm

commit 9e0b976a291c88bedeb619f4709427a5b653ca4e

Fix for heterogeneous job starvation bug
bug 4370

src/slurmctld/job_scheduler.c
@@ -1631,7 +1631,7 @@ next_part:			part_ptr = (struct part_record *)
 			continue;	/* scheduled in another partition */
 
 		if (job_ptr->pack_job_id) {
-			fail_by_part = false;
+			fail_by_part = true;
 			goto fail_this_part;
 		}
 

commit e101faa539b65b79f50c23e25a5a12c45ba1d577

Fix for illegal memory reference
Fix for bug introduced in commit 9e0b976a291c88bedeb61
bug 4400

src/slurmctld/job_scheduler.c
@@ -2097,8 +2097,19 @@ skip_start:
 			fail_by_part = false;
 
 fail_this_part:	if (fail_by_part) {
-		 	/* do not schedule more jobs in this partition or on
-			 * nodes in this partition */
+			/* Search for duplicates */
+			for (i = 0; i < failed_part_cnt; i++) {
+				if (failed_parts[i] == job_ptr->part_ptr) {
+					fail_by_part = false;
+					break;
+				}
+			}
+		}
+		if (fail_by_part) {
+			/*
+			 * Do not schedule more jobs in this partition or on
+			 * nodes in this partition
+			 */
 			failed_parts[failed_part_cnt++] = job_ptr->part_ptr;
 			bit_and_not(avail_node_bitmap,
 				job_ptr->part_ptr->node_bitmap);