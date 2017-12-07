SchedMD/slurm

commit a268b64477af87c5407d27e1a19fdb54e95e4e9a

X11 fowarding - handle systems with X11UseLocalhost=no in sshd_config.
This setting causes /unix to be omitted from the xauth string.
Simplify the regex to handle this by adding / to the earlier match and
dropping the /unix pattern.

Bug 4417.

src/common/x11_util.c
@@ -124,7 +124,16 @@ extern char *x11_get_xauth(void)
 	regex_t reg;
 	regmatch_t regmatch[2];
 	char *result, *cookie;
-	static char *cookie_pattern = "^[[:alnum:].-]+/unix:[[:digit:]]+"
+	/*
+	 * Two real-world examples:
+	 * "zoidberg/unix:10  MIT-MAGIC-COOKIE-1  abcdef0123456789"
+	 * "zoidberg:10  MIT-MAGIC-COOKIE-1  abcdef0123456789"
+	 *
+	 * The "/unix" bit is optional, and captured in "[[:alnum:].-/]+:".
+	 * '.' and '-' are also allowed in the hostname portion, so match them
+	 * in addition to '/'.
+	 */
+	static char *cookie_pattern = "^[[:alnum:].-/]+:[[:digit:]]+"
 				      "[[:space:]]+MIT-MAGIC-COOKIE-1"
 				      "[[:space:]]+([[:xdigit:]]+)\n$";
 

commit 312b73324b1c5a0d43bb87c05fdb239b65505e12

X11 forwarding - fix xauth regex to allow for - in hostnames.
The - character is treated as a range if not first or last in the
[] brackets. Moving it in between . and / broke the regex subtly.

Inadvertently broken by a268b64477a.

Bug 4417.

src/common/x11_util.c
@@ -132,8 +132,12 @@ extern char *x11_get_xauth(void)
 	 * The "/unix" bit is optional, and captured in "[[:alnum:].-/]+:".
 	 * '.' and '-' are also allowed in the hostname portion, so match them
 	 * in addition to '/'.
+	 *
+	 * Warning: the '-' must be either first or last in the [] brackets,
+	 * otherwise it will be interpreted as a range instead of the literal
+	 * character.
 	 */
-	static char *cookie_pattern = "^[[:alnum:].-/]+:[[:digit:]]+"
+	static char *cookie_pattern = "^[[:alnum:]./-]+:[[:digit:]]+"
 				      "[[:space:]]+MIT-MAGIC-COOKIE-1"
 				      "[[:space:]]+([[:xdigit:]]+)\n$";
 