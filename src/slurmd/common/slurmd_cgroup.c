SchedMD/slurm

commit 8325e9f88e91791a50e6f69dc7e9436648bd6eb4

Make slurmd oom-unkillable by default, using oom.h OOM_SCORE_ADJ_MIN macro.
Bug 4478.

src/slurmd/common/slurmd_cgroup.c
@@ -41,6 +41,7 @@
 #define _GNU_SOURCE
 #include <ctype.h>
 #include <limits.h>
+#include <linux/oom.h>
 #include <sched.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -187,6 +188,7 @@ extern int init_system_memory_cgroup(void)
 {
 	int fstatus = SLURM_ERROR;
 	char* slurm_cgpath;
+	const char *oom_score_min = (const char *)OOM_SCORE_ADJ_MIN;
 
 	/* read cgroup configuration */
 	if (read_slurm_cgroup_conf(&slurm_cgroup_conf))
@@ -256,12 +258,12 @@ extern int init_system_memory_cgroup(void)
          *
          *  If an env value is already set for slurmstepd
          *  OOM killer behavior, keep it, otherwise set the
-         *  -1000 value, wich means do not let OOM killer kill it
+         *  OOM_SCORE_ADJ_MIN value, wich means do not let OOM killer kill it
          *
          *  FYI, setting "export SLURMSTEPD_OOM_ADJ=-1000"
          *  in /etc/sysconfig/slurm would be the same
          */
-        setenv("SLURMSTEPD_OOM_ADJ", "-1000", 0);
+        setenv("SLURMSTEPD_OOM_ADJ", oom_score_min, 0);
 
 	/* create slurm root cg in this cg namespace */
 	slurm_cgpath = _system_cgroup_create_slurm_cg(&memory_ns);

src/slurmd/slurmd/slurmd.c
@@ -53,6 +53,7 @@
 #include <dlfcn.h>
 #include <fcntl.h>
 #include <grp.h>
+#include <linux/oom.h>
 #include <pthread.h>
 #include <sched.h>
 #include <stdlib.h>
@@ -306,11 +307,10 @@ main (int argc, char **argv)
 	info("slurmd version %s started", SLURM_VERSION_STRING);
 	debug3("finished daemonize");
 
-	if ((oom_value = getenv("SLURMD_OOM_ADJ"))) {
-		i = atoi(oom_value);
-		debug("Setting slurmd oom_adj to %d", i);
-		set_oom_adj(i);
-	}
+	oom_value = getenv("SLURMD_OOM_ADJ");
+	i = oom_value ? atoi(oom_value) : OOM_SCORE_ADJ_MIN;
+	debug("Setting slurmd oom_adj to %d", i);
+	set_oom_adj(i);
 
 	_kill_old_slurmd();
 

commit 2d0df2ed76b18eb30771265732ce14d8d5d22602

Fix problem setting SLURMSTEPD_OOM_ADJ introduced in 8325e9f88e9.
Bug 4478 comment 25.

src/slurmd/common/slurmd_cgroup.c
@@ -188,7 +188,6 @@ extern int init_system_memory_cgroup(void)
 {
 	int fstatus = SLURM_ERROR;
 	char* slurm_cgpath;
-	const char *oom_score_min = (const char *)OOM_SCORE_ADJ_MIN;
 
 	/* read cgroup configuration */
 	if (read_slurm_cgroup_conf(&slurm_cgroup_conf))
@@ -260,10 +259,10 @@ extern int init_system_memory_cgroup(void)
          *  OOM killer behavior, keep it, otherwise set the
          *  OOM_SCORE_ADJ_MIN value, wich means do not let OOM killer kill it
          *
-         *  FYI, setting "export SLURMSTEPD_OOM_ADJ=-1000"
+         *  FYI, setting "export SLURMSTEPD_OOM_ADJ=OOM_SCORE_ADJ_MIN (-1000)"
          *  in /etc/sysconfig/slurm would be the same
          */
-        setenv("SLURMSTEPD_OOM_ADJ", oom_score_min, 0);
+        setenvfs("SLURMSTEPD_OOM_ADJ=%d", OOM_SCORE_ADJ_MIN);
 
 	/* create slurm root cg in this cg namespace */
 	slurm_cgpath = _system_cgroup_create_slurm_cg(&memory_ns);