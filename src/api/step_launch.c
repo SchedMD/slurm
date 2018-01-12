SchedMD/slurm

commit 777a45f9d28e4d0c631c5ad17b1bb9c2e51865ce

Support srun --export with MPI plugins
Bug 4132

src/common/env.c
@@ -278,7 +278,7 @@ int setenvf(char ***envp, const char *name, const char *fmt, ...)
 		return ENOMEM;
 	}
 
-	if (envp && *envp) {
+	if (envp) {
 		if (env_array_overwrite(envp, name, value) == 1)
 			rc = 0;
 		else

commit bc7838d9c5cf468b4fbb736d2874d4e3c240ac7e

srun env var logic fix
This fixes the problem introduced in by commit
777a45f9d28e4d0c631c5ad17b1bb9c2e51865ce and maintains proper PMIx
operation.
bugs 4132 and 4615

src/api/step_launch.c
@@ -243,6 +243,7 @@ extern int slurm_step_launch(slurm_step_ctx_t *ctx,
 	if (params->pack_jobid && (params->pack_jobid != NO_VAL))
 		_rebuild_mpi_layout(ctx, params);
 
+	mpi_env = xmalloc(sizeof(char *));  /* Needed for setenvf used by MPI */
 	if ((ctx->launch_state->mpi_state =
 	     mpi_hook_client_prelaunch(ctx->launch_state->mpi_info, &mpi_env))
 	    == NULL) {

src/common/env.c
@@ -282,7 +282,7 @@ int setenvf(char ***envp, const char *name, const char *fmt, ...)
 		return ENOMEM;
 	}
 
-	if (envp) {
+	if (envp && *envp) {
 		if (env_array_overwrite(envp, name, value) == 1)
 			rc = 0;
 		else