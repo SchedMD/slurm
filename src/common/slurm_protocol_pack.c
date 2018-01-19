a907830edbb4b0b54eada7723042400f0a96bc8e
Brian Christiansen <brian@schedmd.com>
2018-01-19 09:38:50 -0700

    Fix dead assignment reported by CLANG
    From 5eff149865e

--- src/common/slurm_protocol_pack.c
+++ src/common/slurm_protocol_pack.c
@@ -6856,7 +6856,6 @@ _pack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t * build_ptr, Buf buffer,
 			}
 			list_iterator_destroy(itr);
 		}
-		count = NO_VAL;
 
 		pack16(build_ptr->select_type_param, buffer);
 
