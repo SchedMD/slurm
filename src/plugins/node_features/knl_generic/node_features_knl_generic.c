SchedMD/slurm

commit f2efbb606a8ecbb1fbff79549d439297c65188fb

Report NodeFeatures plugin config
Report NodeFeatures plugin configuration with scontrol and sview commands.
bug 4036

src/plugins/node_features/knl_generic/node_features_knl_generic.c
@@ -2037,3 +2037,76 @@ extern uint32_t node_features_p_boot_time(void)
 {
 	return boot_time;
 }
+
+/* Get node features plugin configuration */
+extern void node_features_p_get_config(List *data)
+{
+	config_key_pair_t *key_pair;
+
+	xassert(*data);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("AllowMCDRAM");
+	key_pair->value = xstrdup_printf("%s", _knl_mcdram_str(allow_mcdram));
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("AllowNUMA");
+	key_pair->value = xstrdup_printf("%s", _knl_numa_str(allow_numa));
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("AllowUserBoot");
+	key_pair->value = xstrdup_printf("%s",
+				_make_uid_str(allowed_uid, allowed_uid_cnt));
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("BootTime");
+	key_pair->value = xstrdup_printf("%u", boot_time);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("DefaultMCDRAM");
+	key_pair->value = xstrdup_printf("%s", _knl_mcdram_str(default_mcdram));
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("DefaultNUMA");
+	key_pair->value = xstrdup_printf("%s", _knl_numa_str(default_numa));
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("Force");
+	key_pair->value = xstrdup_printf("%u", force_load);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("McPath");
+	key_pair->value = xstrdup_printf("%s", mc_path);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("SyscfgPath");
+	key_pair->value = xstrdup_printf("%s", syscfg_path);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("SyscfgTimeout");
+	key_pair->value = xstrdup_printf("%u", syscfg_timeout);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("SystemType");
+	key_pair->value = xstrdup_printf("%s", _knl_system_type_str(knl_system_type));
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("UmeCheckInterval");
+	key_pair->value = xstrdup_printf("%u", ume_check_interval);
+	list_append(*data, key_pair);
+
+	list_sort(*data, (ListCmpF) sort_key_pairs);
+
+	return;
+}

slurm/slurm.h.in
@@ -2854,6 +2854,7 @@ typedef struct slurm_ctl_conf {
 	uint16_t msg_timeout;	/* message timeout */
 	uint16_t tcp_timeout;	/* tcp timeout */
 	uint32_t next_job_id;	/* next slurm generated job_id to assign */
+	void *node_features_conf; /* Node Features Plugin config file */
 	char *node_features_plugins; /* List of node_features plugins to use */
 	char *node_prefix;      /* prefix of nodes in partition, only set in
 				   bluegene clusters NULL otherwise */

src/api/config_info.c
@@ -450,6 +450,7 @@ void slurm_print_ctl_conf ( FILE* out,
 	void *ret_list = NULL;
 	char *select_title = "Select Plugin Configuration";
 	uint32_t cluster_flags = slurmdb_setup_cluster_flags();
+	char *features_str = NULL;
 
 	if (cluster_flags & CLUSTER_FLAG_BGQ)
 		select_title = "\nBluegene/Q configuration\n";
@@ -475,6 +476,12 @@ void slurm_print_ctl_conf ( FILE* out,
 
 	slurm_print_key_pairs(out, slurm_ctl_conf_ptr->ext_sensors_conf,
 			      "\nExternal Sensors\n");
+
+	xstrfmtcat(features_str, "\nNode Features: %s\n",
+		   slurm_ctl_conf_ptr->node_features_plugins);
+	slurm_print_key_pairs(out, slurm_ctl_conf_ptr->node_features_conf,
+			      features_str);
+	xfree(features_str);
 
 	slurm_print_key_pairs(out, slurm_ctl_conf_ptr->select_conf_key_pairs,
 			      select_title);

src/common/node_features.c
@@ -74,6 +74,7 @@ typedef struct node_features_ops {
 	void	(*step_config)	(bool mem_sort, bitstr_t *numa_bitmap);
 	int	(*reconfig)	(void);
 	bool	(*user_update)	(uid_t uid);
+	void	(*get_config)	(List *data);
 } node_features_ops_t;
 
 /*
@@ -96,7 +97,8 @@ static const char *syms[] = {
 	"node_features_p_node_xlate2",
 	"node_features_p_step_config",
 	"node_features_p_reconfig",
-	"node_features_p_user_update"
+	"node_features_p_user_update",
+	"node_features_p_get_config"
 };
 
 static int g_context_cnt = -1;
@@ -564,4 +566,25 @@ extern uint32_t node_features_g_boot_time(void)
 	END_TIMER2("node_features_g_user_update");
 
 	return boot_time;
+}
+
+/* Get node features plugin configuration */
+extern List node_features_g_get_config(void)
+{
+	DEF_TIMERS;
+	int i, rc;
+	List node_features_conf_l = list_create(destroy_config_key_pair);
+
+	START_TIMER;
+	rc = node_features_g_init();
+
+	slurm_mutex_lock(&g_context_lock);
+	for (i = 0; ((i < g_context_cnt) && (rc == SLURM_SUCCESS)); i++) {
+		(*(ops[i].get_config))(&node_features_conf_l);
+	}
+	slurm_mutex_unlock(&g_context_lock);
+
+	END_TIMER2("node_features_g_get_config");
+
+	return node_features_conf_l;
 }

src/common/node_features.h
@@ -135,4 +135,7 @@ extern bool node_features_g_user_update(uid_t uid);
 /* Return estimated reboot time, in seconds */
 extern uint32_t node_features_g_boot_time(void);
 
+/* Get node features plugin configuration */
+extern List node_features_g_get_config(void);
+
 #endif /* !_NODE_FEATURES_H */

src/common/read_config.c
@@ -2416,6 +2416,7 @@ free_slurm_conf (slurm_ctl_conf_t *ctl_conf_ptr, bool purge_node_hash)
 	xfree (ctl_conf_ptr->mpi_default);
 	xfree (ctl_conf_ptr->mpi_params);
 	xfree (ctl_conf_ptr->msg_aggr_params);
+	FREE_NULL_LIST(ctl_conf_ptr->node_features_conf);
 	xfree (ctl_conf_ptr->node_features_plugins);
 	xfree (ctl_conf_ptr->node_prefix);
 	xfree (ctl_conf_ptr->plugindir);

src/common/slurm_protocol_pack.c
@@ -6459,6 +6459,21 @@ _pack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t * build_ptr, Buf buffer,
 		pack16(build_ptr->msg_timeout, buffer);
 
 		pack32(build_ptr->next_job_id, buffer);
+		if (build_ptr->node_features_conf)
+			count = list_count(build_ptr->node_features_conf);
+		else
+			count = NO_VAL;
+		pack32(count, buffer);
+		if (count && (count != NO_VAL)) {
+			ListIterator itr = list_iterator_create(
+				(List)build_ptr->node_features_conf);
+			config_key_pair_t *key_pair = NULL;
+			while ((key_pair = list_next(itr))) {
+				pack_config_key_pair(key_pair,
+						     protocol_version, buffer);
+			}
+			list_iterator_destroy(itr);
+		}
 		packstr(build_ptr->node_features_plugins, buffer);
 		packstr(build_ptr->node_prefix, buffer);
 
@@ -7383,6 +7398,23 @@ _unpack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t **build_buffer_ptr,
 		safe_unpack16(&build_ptr->msg_timeout, buffer);
 
 		safe_unpack32(&build_ptr->next_job_id, buffer);
+		safe_unpack32(&count, buffer);
+		if (count > NO_VAL)
+			goto unpack_error;
+		if (count != NO_VAL) {
+			List tmp_list = list_create(destroy_config_key_pair);
+			config_key_pair_t *object = NULL;
+			int i;
+			for (i = 0; i < count; i++) {
+				if (unpack_config_key_pair(
+					    (void *)&object, protocol_version,
+					    buffer)
+				    == SLURM_ERROR)
+					goto unpack_error;
+				list_append(tmp_list, object);
+			}
+			build_ptr->node_features_conf = (void *)tmp_list;
+		}
 		safe_unpackstr_xmalloc(&build_ptr->node_features_plugins,
 				       &uint32_tmp, buffer);
 		safe_unpackstr_xmalloc(&build_ptr->node_prefix,

src/plugins/node_features/knl_cray/node_features_knl_cray.c
@@ -3039,3 +3039,85 @@ extern uint32_t node_features_p_boot_time(void)
 {
 	return boot_time;
 }
+
+/* Get node features plugin configuration */
+extern void node_features_p_get_config(List *data)
+{
+	config_key_pair_t *key_pair;
+
+	xassert(*data);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("AllowMCDRAM");
+	key_pair->value = xstrdup_printf("%s", _knl_mcdram_str(allow_mcdram));
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("AllowNUMA");
+	key_pair->value = xstrdup_printf("%s", _knl_numa_str(allow_numa));
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("AllowUserBoot");
+	key_pair->value = xstrdup_printf("%s",
+				_make_uid_str(allowed_uid, allowed_uid_cnt));
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("BootTime");
+	key_pair->value = xstrdup_printf("%u", boot_time);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("CapmcPath");
+	key_pair->value = xstrdup_printf("%s", capmc_path);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("CapmcPollFreq");
+	key_pair->value = xstrdup_printf("%u", capmc_poll_freq);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("CapmcRetries");
+	key_pair->value = xstrdup_printf("%u", capmc_retries);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("CapmcTimeout");
+	key_pair->value = xstrdup_printf("%u", capmc_timeout);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("CnselectPath");
+	key_pair->value = xstrdup_printf("%s", cnselect_path);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("DefaultMCDRAM");
+	key_pair->value = xstrdup_printf("%s", _knl_mcdram_str(default_mcdram));
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("DefaultNUMA");
+	key_pair->value = xstrdup_printf("%s", _knl_numa_str(default_numa));
+	list_append(*data, key_pair);
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("McPath");
+	key_pair->value = xstrdup_printf("%s", mc_path);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("SyscfgPath");
+	key_pair->value = xstrdup_printf("%s", syscfg_path);
+	list_append(*data, key_pair);
+
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("UmeCheckInterval");
+	key_pair->value = xstrdup_printf("%u", ume_check_interval);
+	list_append(*data, key_pair);
+
+	list_sort(*data, (ListCmpF) sort_key_pairs);
+
+	return;
+}

commit 94cfea9c47ff34ced8ba94f32b6ffe342e9ca8ea

Fix memory leaks
Introduced in commit f2efbb606a8ecbb1fbff79549d439297c65188fb
Coverity CID 182262, 182263 and 182264

src/plugins/node_features/knl_generic/node_features_knl_generic.c
@@ -2057,8 +2057,7 @@ extern void node_features_p_get_config(List *data)
 
 	key_pair = xmalloc(sizeof(config_key_pair_t));
 	key_pair->name = xstrdup("AllowUserBoot");
-	key_pair->value = xstrdup_printf("%s",
-				_make_uid_str(allowed_uid, allowed_uid_cnt));
+	key_pair->value = _make_uid_str(allowed_uid, allowed_uid_cnt);
 	list_append(*data, key_pair);
 
 	key_pair = xmalloc(sizeof(config_key_pair_t));
@@ -2068,12 +2067,12 @@ extern void node_features_p_get_config(List *data)
 
 	key_pair = xmalloc(sizeof(config_key_pair_t));
 	key_pair->name = xstrdup("DefaultMCDRAM");
-	key_pair->value = xstrdup_printf("%s", _knl_mcdram_str(default_mcdram));
+	key_pair->value = _knl_mcdram_str(default_mcdram);
 	list_append(*data, key_pair);
 
 	key_pair = xmalloc(sizeof(config_key_pair_t));
 	key_pair->name = xstrdup("DefaultNUMA");
-	key_pair->value = xstrdup_printf("%s", _knl_numa_str(default_numa));
+	key_pair->value = _knl_numa_str(default_numa);
 	list_append(*data, key_pair);
 
 	key_pair = xmalloc(sizeof(config_key_pair_t));
@@ -2098,7 +2097,7 @@ extern void node_features_p_get_config(List *data)
 
 	key_pair = xmalloc(sizeof(config_key_pair_t));
 	key_pair->name = xstrdup("SystemType");
-	key_pair->value = xstrdup_printf("%s", _knl_system_type_str(knl_system_type));
+	key_pair->value = xstrdup(_knl_system_type_str(knl_system_type));
 	list_append(*data, key_pair);
 
 	key_pair = xmalloc(sizeof(config_key_pair_t));