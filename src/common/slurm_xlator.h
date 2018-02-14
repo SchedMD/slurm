b5f722774dc95b2e9b1984a63ae9096cebd3d951
Tim Wickberg <tim@schedmd.com>
2018-02-13 22:11:16 -0700

    Remove unused xstrstrip() function.

--- src/common/slurm_xlator.h
+++ src/common/slurm_xlator.h
@@ -327,7 +327,6 @@
 #define	xstrdup_printf		slurm_xstrdup_printf
 #define	xbasename		slurm_xbasename
 #define	_xstrsubstitute		slurm_xstrsubstitute
-#define	xstrstrip		slurm_xstrstrip
 #define	xshort_hostname		slurm_xshort_hostname
 #define xstring_is_whitespace   slurm_xstring_is_whitespace
 #define	xstrtolower		slurm_xstrtolower
