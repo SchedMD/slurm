3afe33b27a967fcc8e1b74ba09495f495e6d7131
Dominik Bartkiewicz <bart@schedmd.com>
2018-03-27 09:13:26 -0600

    Fix missing unlock on prolog_mutex.
    Bug 4990.

--- src/slurmd/slurmd/req.c
+++ src/slurmd/slurmd/req.c
@@ -2314,6 +2314,7 @@ _rpc_batch_job(slurm_msg_t *msg, bool new_msg)
 			 */
 			if (retry_cnt > 50) {
 				rc = ESLURMD_PROLOG_FAILED;
+				slurm_mutex_unlock(&prolog_mutex);
 				goto done;
 			}
 
