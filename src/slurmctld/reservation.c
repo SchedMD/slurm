SchedMD/slurm

commit ef1f3e7366d2135fe05c21c49ce1d36bbce8e452

Backfill scheduling improvements
Improve backfill scheduling algorithm with respect to starting jobs as soon
    as possible while avoiding advanced reservations.
bug 3757

src/slurmctld/reservation.c
@@ -5086,6 +5086,7 @@ extern int job_test_resv(struct job_record *job_ptr, time_t *when,
 				     "will not share nodes",
 				     resv_ptr->name, job_ptr->job_id);
 #endif
+				*resv_overlap = true;
 				bit_and_not(*node_bitmap, resv_ptr->node_bitmap);
 			} else {
 #if _DEBUG
@@ -5094,12 +5095,17 @@ extern int job_test_resv(struct job_record *job_ptr, time_t *when,
 #endif
 				if (resv_ptr->core_bitmap == NULL) {
 					;
+				} else if (exc_core_bitmap == NULL) {
+					error("job_test_resv: exc_core_bitmap is NULL");
+					*resv_overlap = true;
 				} else if (*exc_core_bitmap == NULL) {
 					*exc_core_bitmap =
 						bit_copy(resv_ptr->core_bitmap);
+					*resv_overlap = true;
 				} else {
 					bit_or(*exc_core_bitmap,
 					       resv_ptr->core_bitmap);
+					*resv_overlap = true;
 				}
 			}
 		}
@@ -5150,8 +5156,7 @@ extern time_t find_resv_end(time_t start_time)
 
 	iter = list_iterator_create(resv_list);
 	while ((resv_ptr = (slurmctld_resv_t *) list_next(iter))) {
-		if ((start_time < resv_ptr->start_time) ||
-		    (start_time > resv_ptr->end_time))
+		if (start_time > resv_ptr->end_time)
 			continue;
 		if ((end_time == 0) || (resv_ptr->end_time < end_time))
 			end_time = resv_ptr->end_time;

src/plugins/sched/backfill/backfill.c
@@ -1665,6 +1665,7 @@ next_task:
 		FREE_NULL_BITMAP(avail_bitmap);
 		FREE_NULL_BITMAP(exc_core_bitmap);
 		start_res   = later_start;
+		resv_end = 0;
 		later_start = 0;
 		/* Determine impact of any advance reservations */
 		j = job_test_resv(job_ptr, &start_res, true, &avail_bitmap,
@@ -1682,7 +1683,8 @@ next_task:
 			end_time = (time_limit * 60) + now;
 		if (end_time < now)	/* Overflow 32-bits */
 			end_time = INFINITE;
-		resv_end = find_resv_end(start_res);
+		if (resv_overlap)
+			resv_end = find_resv_end(start_res);
 		/* Identify usable nodes for this job */
 		bit_and(avail_bitmap, part_ptr->node_bitmap);
 		bit_and(avail_bitmap, up_node_bitmap);
@@ -1786,9 +1788,16 @@ next_task:
 			       "Need to use features which can be made "
 			       "available after node reboot", job_ptr->job_id);
 			/* Determine impact of any advance reservations */
-			j = job_test_resv(job_ptr, &start_res, true,
+			resv_end = 0;
+			j = job_test_resv(job_ptr, &start_res, false,
 					  &tmp_node_bitmap, &tmp_core_bitmap,
 					  &resv_overlap, true);
+			if (resv_overlap)
+				resv_end = find_resv_end(start_res);
+			if (resv_end && (++resv_end < window_end) &&
+			    ((later_start == 0) || (resv_end < later_start))) {
+				later_start = resv_end;
+			}
 			if (j == SLURM_SUCCESS) {
 				FREE_NULL_BITMAP(exc_core_bitmap);
 				exc_core_bitmap = tmp_core_bitmap;

commit 217a06984368bcdb382c1d88185e07b5c4dec576

Fix problem with wrongly set as Reservation job state_reason.
regression from ef1f3e736.
Bug 4885.

src/slurmctld/reservation.c
@@ -5313,7 +5313,6 @@ extern int job_test_resv(struct job_record *job_ptr, time_t *when,
 				     "will not share nodes",
 				     resv_ptr->name, job_ptr->job_id);
 #endif
-				*resv_overlap = true;
 				bit_and_not(*node_bitmap, resv_ptr->node_bitmap);
 			} else {
 #if _DEBUG
@@ -5324,16 +5323,20 @@ extern int job_test_resv(struct job_record *job_ptr, time_t *when,
 					;
 				} else if (exc_core_bitmap == NULL) {
 					error("job_test_resv: exc_core_bitmap is NULL");
-					*resv_overlap = true;
 				} else if (*exc_core_bitmap == NULL) {
 					*exc_core_bitmap =
 						bit_copy(resv_ptr->core_bitmap);
-					*resv_overlap = true;
 				} else {
 					bit_or(*exc_core_bitmap,
 					       resv_ptr->core_bitmap);
-					*resv_overlap = true;
 				}
+			}
+
+			if(!job_ptr->part_ptr ||
+			    bit_overlap(job_ptr->part_ptr->node_bitmap,
+					resv_ptr->node_bitmap)) {
+				*resv_overlap = true;
+				continue;
 			}
 		}
 		list_iterator_destroy(iter);