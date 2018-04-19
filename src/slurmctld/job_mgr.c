SchedMD/slurm

commit 892ffa891affa10435cdd2dc701919058b530795

Replace sequence of (bit_not, bit_and, bit_not) with bit_and_not().
Improve performance by looping once over the bitstring rather than
three times. This also allows us to remove some temporary bitstring
variables that were holding an inverted version.

src/slurmctld/job_mgr.c
@@ -4475,7 +4475,7 @@ extern int job_str_signal(char *job_id_str, uint16_t signal, uint16_t flags,
 	time_t now = time(NULL);
 	char *end_ptr = NULL, *tok, *tmp;
 	long int long_id;
-	bitstr_t *array_bitmap = NULL, *tmp_bitmap;
+	bitstr_t *array_bitmap = NULL;
 	bool valid = true;
 	int32_t i, i_first, i_last;
 	int rc = SLURM_SUCCESS, rc2, len;
@@ -4624,14 +4624,11 @@ extern int job_str_signal(char *job_id_str, uint16_t signal, uint16_t flags,
 			job_ptr->array_recs->task_id_bitmap = bit_realloc(
 				job_ptr->array_recs->task_id_bitmap, i_last);
 		}
-		tmp_bitmap = bit_copy(job_ptr->array_recs->task_id_bitmap);
 		if (signal == SIGKILL) {
 			uint32_t orig_task_cnt, new_task_count;
-			bit_not(array_bitmap);
-			bit_and(job_ptr->array_recs->task_id_bitmap,
+			bit_and_not(job_ptr->array_recs->task_id_bitmap,
 				array_bitmap);
 			xfree(job_ptr->array_recs->task_id_str);
-			bit_not(array_bitmap);
 			orig_task_cnt = job_ptr->array_recs->task_cnt;
 			new_task_count = bit_set_count(job_ptr->array_recs->
 						       task_id_bitmap);
@@ -4657,13 +4654,11 @@ extern int job_str_signal(char *job_id_str, uint16_t signal, uint16_t flags,
 			 * limit for submitted jobs correctly.
 			 */
 			job_ptr->array_recs->task_cnt = new_task_count;
-			bit_not(tmp_bitmap);
-			bit_and(array_bitmap, tmp_bitmap);
-			FREE_NULL_BITMAP(tmp_bitmap);
+			bit_and_not(array_bitmap,
+				    job_ptr->array_recs->task_id_bitmap);
 		} else {
-			bit_not(tmp_bitmap);
-			bit_and(array_bitmap, tmp_bitmap);
-			FREE_NULL_BITMAP(tmp_bitmap);
+			bit_and_not(array_bitmap,
+				    job_ptr->array_recs->task_id_bitmap);
 			rc = ESLURM_TRANSITION_STATE_NO_UPDATE;
 		}
 	}
@@ -11677,10 +11672,8 @@ extern int update_job_str(slurm_msg_t *msg, uid_t uid)
 			/* Update the record with all pending tasks */
 			rc2 = _update_job(job_ptr, job_specs, uid);
 			_resp_array_add(&resp_array, job_ptr, rc2);
-			bit_not(job_ptr->array_recs->task_id_bitmap);
-			bit_and(array_bitmap,
-				job_ptr->array_recs->task_id_bitmap);
-			bit_not(job_ptr->array_recs->task_id_bitmap);
+			bit_and_not(array_bitmap,
+				    job_ptr->array_recs->task_id_bitmap);
 		} else {
 			/* Need to split out tasks to separate job records */
 			tmp_bitmap = bit_copy(job_ptr->array_recs->

src/common/gres.c
@@ -5831,10 +5831,8 @@ static int _step_alloc(void *step_gres_data, void *job_gres_data,
 	gres_bit_alloc = bit_copy(job_gres_ptr->gres_bit_alloc[node_offset]);
 	if (job_gres_ptr->gres_bit_step_alloc &&
 	    job_gres_ptr->gres_bit_step_alloc[node_offset]) {
-		bit_not(job_gres_ptr->gres_bit_step_alloc[node_offset]);
-		bit_and(gres_bit_alloc,
+		bit_and_not(gres_bit_alloc,
 			job_gres_ptr->gres_bit_step_alloc[node_offset]);
-		bit_not(job_gres_ptr->gres_bit_step_alloc[node_offset]);
 	}
 
 	gres_needed = step_gres_ptr->gres_cnt_alloc;

src/plugins/route/topology/route_topology.c
@@ -230,8 +230,7 @@ extern int route_p_split_hostlist(hostlist_t hl,
 		}
 		(*sp_hl)[hl_ndx] = bitmap2hostlist(fwd_bitmap);
 		/* Now remove nodes from this switch from message list */
-		bit_not(fwd_bitmap);
-		bit_and(nodes_bitmap, fwd_bitmap);
+		bit_and_not(nodes_bitmap, fwd_bitmap);
 		FREE_NULL_BITMAP(fwd_bitmap);
 		if (debug_flags & DEBUG_FLAG_ROUTE) {
 			buf = hostlist_ranged_string_xmalloc((*sp_hl)[hl_ndx]);

src/plugins/sched/backfill/backfill.c
@@ -1454,10 +1454,8 @@ next_task:
 		}
 
 		if (job_ptr->details->exc_node_bitmap) {
-			bit_not(job_ptr->details->exc_node_bitmap);
-			bit_and(avail_bitmap,
+			bit_and_not(avail_bitmap,
 				job_ptr->details->exc_node_bitmap);
-			bit_not(job_ptr->details->exc_node_bitmap);
 		}
 
 		/* Test if insufficient nodes remain OR

src/plugins/select/cons_res/job_test.c
@@ -3117,7 +3117,6 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *node_bitmap,
 	static int gang_mode = -1;
 	int error_code = SLURM_SUCCESS;
 	bitstr_t *orig_map, *avail_cores, *free_cores, *part_core_map = NULL;
-	bitstr_t *tmpcore = NULL;
 	bool test_only;
 	uint32_t c, j, k, n, csize, total_cpus;
 	uint64_t save_mem = 0;
@@ -3292,22 +3291,17 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *node_bitmap,
 		bit_fmt(str, (sizeof(str) - 1), exc_core_bitmap);
 		debug2("excluding cores reserved: %s", str);
 #endif
-		bit_not(exc_core_bitmap);
-		bit_and(free_cores, exc_core_bitmap);
-		bit_not(exc_core_bitmap);
+		bit_and_not(free_cores, exc_core_bitmap);
 	}
 
 	/* remove all existing allocations from free_cores */
-	tmpcore = bit_copy(free_cores);
 	for (p_ptr = cr_part_ptr; p_ptr; p_ptr = p_ptr->next) {
 		if (!p_ptr->row)
 			continue;
 		for (i = 0; i < p_ptr->num_rows; i++) {
 			if (!p_ptr->row[i].row_bitmap)
 				continue;
-			bit_copybits(tmpcore, p_ptr->row[i].row_bitmap);
-			bit_not(tmpcore); /* set bits now "free" resources */
-			bit_and(free_cores, tmpcore);
+			bit_and_not(free_cores, p_ptr->row[i].row_bitmap);
 			if (p_ptr->part_ptr != job_ptr->part_ptr)
 				continue;
 			if (part_core_map) {
@@ -3356,9 +3350,7 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *node_bitmap,
 	bit_copybits(free_cores, avail_cores);
 
 	if (exc_core_bitmap) {
-		bit_not(exc_core_bitmap);
-		bit_and(free_cores, exc_core_bitmap);
-		bit_not(exc_core_bitmap);
+		bit_and_not(free_cores, exc_core_bitmap);
 	}
 
 	for (jp_ptr = cr_part_ptr; jp_ptr; jp_ptr = jp_ptr->next) {
@@ -3398,9 +3390,7 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *node_bitmap,
 			for (i = 0; i < p_ptr->num_rows; i++) {
 				if (!p_ptr->row[i].row_bitmap)
 					continue;
-				bit_copybits(tmpcore, p_ptr->row[i].row_bitmap);
-				bit_not(tmpcore); /* add to "free" resources */
-				bit_and(free_cores, tmpcore);
+				bit_and_not(free_cores, p_ptr->row[i].row_bitmap);
 			}
 		}
 	}
@@ -3443,9 +3433,7 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *node_bitmap,
 		for (i = 0; i < p_ptr->num_rows; i++) {
 			if (!p_ptr->row[i].row_bitmap)
 				continue;
-			bit_copybits(tmpcore, p_ptr->row[i].row_bitmap);
-			bit_not(tmpcore); /* set bits now "free" resources */
-			bit_and(free_cores, tmpcore);
+			bit_and_not(free_cores, p_ptr->row[i].row_bitmap);
 		}
 	}
 
@@ -3477,7 +3465,6 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *node_bitmap,
 	/*** Step 4 ***/
 	/* try to fit the job into an existing row
 	 *
-	 * tmpcore = worker core_bitmap
 	 * free_cores = core_bitmap to be built
 	 * avail_cores = static core_bitmap of all available cores
 	 */
@@ -3515,9 +3502,7 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *node_bitmap,
 			break;
 		bit_copybits(node_bitmap, orig_map);
 		bit_copybits(free_cores, avail_cores);
-		bit_copybits(tmpcore, jp_ptr->row[i].row_bitmap);
-		bit_not(tmpcore);
-		bit_and(free_cores, tmpcore);
+		bit_and_not(free_cores, jp_ptr->row[i].row_bitmap);
 
 		if (job_ptr->details->whole_node == 1)
 			_block_whole_nodes(node_bitmap, avail_cores,
@@ -3583,7 +3568,6 @@ alloc_job:
 	 * distribute the job on the bits, and exit
 	 */
 	FREE_NULL_BITMAP(orig_map);
-	FREE_NULL_BITMAP(tmpcore);
 	FREE_NULL_BITMAP(part_core_map);
 	if ((!cpu_count) || (!job_ptr->best_switch)) {
 		/* we were sent here to cleanup and exit */

src/plugins/select/cons_res/select_cons_res.c
@@ -3342,9 +3342,7 @@ extern bitstr_t * select_p_resv_test(resv_desc_msg_t *resv_desc_ptr,
 			_make_core_bitmap_filtered(switches_bitmap[i], 1);
 
 		if (*core_bitmap) {
-			bit_not(*core_bitmap);
-			bit_and(switches_core_bitmap[i], *core_bitmap);
-			bit_not(*core_bitmap);
+			bit_and_not(switches_core_bitmap[i], *core_bitmap);
 		}
 		bit_fmt(str, sizeof(str), switches_core_bitmap[i]);
 		switches_cpu_cnt[i] = bit_set_count(switches_core_bitmap[i]);

src/plugins/select/cray/select_cray.c
@@ -1880,9 +1880,7 @@ extern int select_p_job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 					bit_nclear(bitmap, 0,
 						   bit_size(bitmap) - 1);
 			} else {
-				bit_not(blade_nodes_running_npc);
-				bit_and(bitmap, blade_nodes_running_npc);
-				bit_not(blade_nodes_running_npc);
+				bit_and_not(bitmap, blade_nodes_running_npc);
 			}
 		}
 	}

src/plugins/select/serial/job_test.c
@@ -503,7 +503,6 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *bitmap, int mode,
 	static int gang_mode = -1;
 	int error_code = SLURM_SUCCESS;
 	bitstr_t *orig_map, *avail_cores, *free_cores;
-	bitstr_t *tmpcore = NULL;
 	bool test_only;
 	uint32_t c, i, j, k, csize;
 	uint64_t save_mem = 0;
@@ -623,16 +622,13 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *bitmap, int mode,
 	bit_copybits(free_cores, avail_cores);
 
 	/* remove all existing allocations from free_cores */
-	tmpcore = bit_copy(free_cores);
 	for (p_ptr = cr_part_ptr; p_ptr; p_ptr = p_ptr->next) {
 		if (!p_ptr->row)
 			continue;
 		for (i = 0; i < p_ptr->num_rows; i++) {
 			if (!p_ptr->row[i].row_bitmap)
 				continue;
-			bit_copybits(tmpcore, p_ptr->row[i].row_bitmap);
-			bit_not(tmpcore); /* set bits now "free" resources */
-			bit_and(free_cores, tmpcore);
+			bit_and_not(free_cores, p_ptr->row[i].row_bitmap);
 		}
 	}
 	cpu_count = _select_nodes(job_ptr, bitmap, cr_node_cnt, free_cores,
@@ -689,9 +685,7 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *bitmap, int mode,
 		for (i = 0; i < p_ptr->num_rows; i++) {
 			if (!p_ptr->row[i].row_bitmap)
 				continue;
-			bit_copybits(tmpcore, p_ptr->row[i].row_bitmap);
-			bit_not(tmpcore); /* set bits now "free" resources */
-			bit_and(free_cores, tmpcore);
+			bit_and_not(free_cores, p_ptr->row[i].row_bitmap);
 		}
 	}
 	/* make these changes permanent */
@@ -728,9 +722,7 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *bitmap, int mode,
 		for (i = 0; i < p_ptr->num_rows; i++) {
 			if (!p_ptr->row[i].row_bitmap)
 				continue;
-			bit_copybits(tmpcore, p_ptr->row[i].row_bitmap);
-			bit_not(tmpcore); /* set bits now "free" resources */
-			bit_and(free_cores, tmpcore);
+			bit_and_not(free_cores,  p_ptr->row[i].row_bitmap);
 		}
 	}
 	cpu_count = _select_nodes(job_ptr, bitmap, cr_node_cnt, free_cores,
@@ -756,7 +748,6 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *bitmap, int mode,
 	/*** Step 4 ***/
 	/* try to fit the job into an existing row
 	 *
-	 * tmpcore = worker core_bitmap
 	 * free_cores = core_bitmap to be built
 	 * avail_cores = static core_bitmap of all available cores
 	 */
@@ -789,9 +780,7 @@ extern int cr_job_test(struct job_record *job_ptr, bitstr_t *bitmap, int mode,
 			break;
 		bit_copybits(bitmap, orig_map);
 		bit_copybits(free_cores, avail_cores);
-		bit_copybits(tmpcore, jp_ptr->row[i].row_bitmap);
-		bit_not(tmpcore);
-		bit_and(free_cores, tmpcore);
+		bit_and_not(free_cores, jp_ptr->row[i].row_bitmap);
 		cpu_count = _select_nodes(job_ptr, bitmap, cr_node_cnt,
 					  free_cores, node_usage, cr_type,
 					  test_only);
@@ -851,7 +840,6 @@ alloc_job:
 	 */
 	FREE_NULL_BITMAP(orig_map);
 	FREE_NULL_BITMAP(avail_cores);
-	FREE_NULL_BITMAP(tmpcore);
 	if (!cpu_count) {
 		/* we were sent here to cleanup and exit */
 		FREE_NULL_BITMAP(free_cores);

commit 8432f9f64cb40d9291bdb43bb3e12864641778b3

Fix incorrect error thrown when cancelling part of a job array.
Fix an issue in the bit manipulation log introduced in commit 892ffa891af.

Bug 4997.

src/slurmctld/job_mgr.c
@@ -5516,6 +5516,9 @@ extern int job_str_signal(char *job_id_str, uint16_t signal, uint16_t flags,
 		}
 		if (signal == SIGKILL) {
 			uint32_t orig_task_cnt, new_task_count;
+			/* task_id_bitmap changes, so we need a copy of it */
+			bitstr_t *task_id_bitmap_orig =
+				bit_copy(job_ptr->array_recs->task_id_bitmap);
 			bit_and_not(job_ptr->array_recs->task_id_bitmap,
 				array_bitmap);
 			xfree(job_ptr->array_recs->task_id_str);
@@ -5547,8 +5550,8 @@ extern int job_str_signal(char *job_id_str, uint16_t signal, uint16_t flags,
 			 * limit for submitted jobs correctly.
 			 */
 			job_ptr->array_recs->task_cnt = new_task_count;
-			bit_and_not(array_bitmap,
-				    job_ptr->array_recs->task_id_bitmap);
+			bit_and_not(array_bitmap, task_id_bitmap_orig);
+			FREE_NULL_BITMAP(task_id_bitmap_orig);
 		} else {
 			bit_and_not(array_bitmap,
 				    job_ptr->array_recs->task_id_bitmap);