SchedMD/slurm

commit 11a75ff4c977241422338cbf688a2b5da1ae6ca0

Make RESPONSE_NODE_REGISTRATION actually do something.
What it does here currently is sends the assoc_mgr_tres_list to the
slurmd who then creates the assoc_mgr end there.

src/slurmd/slurmd/slurmd.c
@@ -66,6 +66,7 @@
 #include <sys/utsname.h>
 #include <unistd.h>
 
+#include "src/common/assoc_mgr.h"
 #include "src/common/bitstring.h"
 #include "src/common/cpu_frequency.h"
 #include "src/common/daemonize.h"
@@ -126,6 +127,7 @@
 
 /* global, copied to STDERR_FILENO in tasks before the exec */
 int devnull = -1;
+bool get_reg_resp = 1;
 slurmd_conf_t * conf = NULL;
 int fini_job_cnt = 0;
 uint32_t *fini_job_id = NULL;
@@ -554,14 +556,60 @@ cleanup:
 	return NULL;
 }
 
+static void _handle_node_reg_resp(slurm_msg_t *resp_msg)
+{
+	int rc;
+	slurm_node_reg_resp_msg_t *resp = NULL;
+
+	switch (resp_msg->msg_type) {
+	case RESPONSE_NODE_REGISTRATION:
+		resp = (slurm_node_reg_resp_msg_t *) resp_msg->data;
+		break;
+	case RESPONSE_SLURM_RC:
+		rc = ((return_code_msg_t *) resp_msg->data)->return_code;
+		slurm_free_return_code_msg(resp_msg->data);
+		if (rc)
+			slurm_seterrno(rc);
+		resp = NULL;
+		break;
+	default:
+		slurm_seterrno(SLURM_UNEXPECTED_MSG_ERROR);
+		break;
+	}
+
+
+	if (resp) {
+		assoc_mgr_lock_t locks = {
+			NO_LOCK, NO_LOCK, NO_LOCK, NO_LOCK,
+			WRITE_LOCK, NO_LOCK, NO_LOCK };
+		/*
+		 * We don't care about the assoc/qos locks
+		 * assoc_mgr_post_tres_list is requesting as those lists
+		 * don't exist here.
+		 */
+		assoc_mgr_lock(&locks);
+		assoc_mgr_post_tres_list(resp->tres_list);
+		debug("%s: slurmctld sent back %u TRES.",
+		      __func__, g_tres_count);
+		assoc_mgr_unlock(&locks);
+		/* assoc_mgr_post_tres_list will destroy the list */
+		resp->tres_list = NULL;
+	}
+}
+
 extern int
 send_registration_msg(uint32_t status, bool startup)
 {
-	int rc, ret_val = SLURM_SUCCESS;
+	int ret_val = SLURM_SUCCESS;
 	slurm_node_registration_status_msg_t *msg =
 		xmalloc (sizeof (slurm_node_registration_status_msg_t));
+
 	if (startup)
 		msg->flags |= SLURMD_REG_FLAG_STARTUP;
+	if (get_reg_resp) {
+		msg->flags |= SLURMD_REG_FLAG_RESP;
+		get_reg_resp = false;
+	}
 
 	_fill_registration_msg(msg);
 	msg->status  = status;
@@ -573,24 +621,36 @@ send_registration_msg(uint32_t status, bool startup)
 		req->msg_type = MESSAGE_NODE_REGISTRATION_STATUS;
 		req->data     = msg;
 
-		msg_aggr_add_msg(req, 1, NULL);
+		msg_aggr_add_msg(req, 1, _handle_node_reg_resp);
 	} else {
 		slurm_msg_t req;
+		slurm_msg_t resp_msg;
+
 		slurm_msg_t_init(&req);
+		slurm_msg_t_init(&resp_msg);
+
 		req.msg_type = MESSAGE_NODE_REGISTRATION_STATUS;
 		req.data     = msg;
 
-		if (slurm_send_recv_controller_rc_msg(&req, &rc,
-						working_cluster_rec) < 0) {
+		if (slurm_send_recv_controller_msg(&req, &resp_msg,
+						   working_cluster_rec) < 0) {
 			error("Unable to register: %m");
 			ret_val = SLURM_FAILURE;
+			goto fail;
 		}
 		slurm_free_node_registration_status_msg(msg);
+
+		_handle_node_reg_resp(&resp_msg);
+		if (errno) {
+			ret_val = errno;
+			errno = 0;
+		}
+
 	}
 
 	if (ret_val == SLURM_SUCCESS)
 		sent_reg_time = time(NULL);
-
+fail:
 	return ret_val;
 }
 

src/common/msg_aggr.c
@@ -423,6 +423,7 @@ extern void msg_aggr_resp(slurm_msg_t *msg)
 		info("msg_aggr_resp: processing composite msg_list...");
 	while ((next_msg = list_next(itr))) {
 		switch (next_msg->msg_type) {
+		case RESPONSE_NODE_REGISTRATION:
 		case REQUEST_BATCH_JOB_LAUNCH:
 		case RESPONSE_SLURM_RC:
 			/* signal sending thread that slurmctld received this

src/common/slurm_protocol_defs.h
@@ -160,6 +160,8 @@
 #define YEAR_SECONDS (365 * 24 * 60 * 60)
 
 #define SLURMD_REG_FLAG_STARTUP  0x0001
+#define SLURMD_REG_FLAG_RESP     0x0002
+
 /* These defines have to be here to avoid circular dependancy with
  * switch.h
  */

src/slurmctld/proc_req.c
@@ -3119,7 +3119,33 @@ static void _slurm_rpc_node_registration(slurm_msg_t * msg,
 	} else {
 		debug2("_slurm_rpc_node_registration complete for %s %s",
 		       node_reg_stat_msg->node_name, TIME_STR);
-		slurm_send_rc_msg(msg, SLURM_SUCCESS);
+		/* If the slurmd is requesting a response send it */
+		if (node_reg_stat_msg->flags & SLURMD_REG_FLAG_RESP) {
+			slurm_node_reg_resp_msg_t *resp = NULL, tmp_resp;
+			if (msg->msg_index && msg->ret_list) {
+				/*
+				 * If this is the case then the resp must be
+				 * xmalloced and will be freed when dealt with
+				 * later.
+				 */
+				resp = xmalloc(
+					sizeof(slurm_node_reg_resp_msg_t));
+			} else {
+				memset(&tmp_resp, 0,
+				       sizeof(slurm_node_reg_resp_msg_t));
+				resp = &tmp_resp;
+			}
+
+			/*
+			 * Don't add the assoc_mgr_tres_list here as it could
+			 * get freed later if you do.  The pack functions grab
+			 * it for us if it isn't here.
+			 */
+			//resp->tres_list = assoc_mgr_tres_list;
+
+			slurm_send_msg(msg, RESPONSE_NODE_REGISTRATION, resp);
+		} else
+			slurm_send_rc_msg(msg, SLURM_SUCCESS);
 	}
 }
 

src/slurmd/slurmd/req.c
@@ -378,6 +378,7 @@ slurmd_req(slurm_msg_t *msg)
 		break;
 	case REQUEST_NODE_REGISTRATION_STATUS:
 		debug2("Processing RPC: REQUEST_NODE_REGISTRATION_STATUS");
+		get_reg_resp = 1;
 		/* Treat as ping (for slurmctld agent, just return SUCCESS) */
 		rc = _rpc_ping(msg);
 		last_slurmctld_msg = time(NULL);

src/slurmd/slurmd/slurmd.h
@@ -54,6 +54,7 @@ extern pid_t getpgid(pid_t pid);
 #endif
 
 extern int devnull;
+extern bool get_reg_resp;
 
 /*
  * Message aggregation types

commit 650de913d06c4f8cac0af0f3e80ca0b1d74cb1d5

Fix memory leak
Bug introduced in commit 11a75ff4c9772414

src/slurmd/slurmd/slurmd.c
@@ -641,6 +641,10 @@ send_registration_msg(uint32_t status, bool startup)
 		slurm_free_node_registration_status_msg(msg);
 
 		_handle_node_reg_resp(&resp_msg);
+		if (resp_msg.msg_type != RESPONSE_SLURM_RC) {
+			/* RESPONSE_SLURM_RC freed by _handle_node_reg_resp() */
+			slurm_free_msg_data(resp_msg.msg_type, resp_msg.data);
+		}
 		if (errno) {
 			ret_val = errno;
 			errno = 0;