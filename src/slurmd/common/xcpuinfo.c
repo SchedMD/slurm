720418cb12bb63bd47530e0253554807e31f81a2
Danny Auble <da@schedmd.com>
2018-04-25 14:04:58 -0600

    Move function header to be over the correct function no code change.

--- src/slurmd/common/xcpuinfo.c
+++ src/slurmd/common/xcpuinfo.c
@@ -122,18 +122,6 @@ get_procs(uint16_t *procs)
 	return 0;
 }
 
-/*
- * get_cpuinfo - Return detailed cpuinfo on this system
- * Output: p_cpus - number of processors on the system
- *         p_boards - number of baseboards (containing sockets)
- *         p_sockets - number of physical processor sockets
- *         p_cores - total number of physical CPU cores
- *         p_threads - total number of hardware execution threads
- *         block_map - asbtract->physical block distribution map
- *         block_map_inv - physical->abstract block distribution map (inverse)
- *         return code - 0 if no error, otherwise errno
- * NOTE: User must xfree block_map and block_map_inv
- */
 #ifdef HAVE_HWLOC
 #if _DEBUG
 static void _hwloc_children(hwloc_topology_t topology, hwloc_obj_t obj,
@@ -165,6 +153,18 @@ static int _core_child_count(hwloc_topology_t topology, hwloc_obj_t obj)
 	return count;
 }
 
+/*
+ * get_cpuinfo - Return detailed cpuinfo on this system
+ * Output: p_cpus - number of processors on the system
+ *         p_boards - number of baseboards (containing sockets)
+ *         p_sockets - number of physical processor sockets
+ *         p_cores - total number of physical CPU cores
+ *         p_threads - total number of hardware execution threads
+ *         block_map - asbtract->physical block distribution map
+ *         block_map_inv - physical->abstract block distribution map (inverse)
+ *         return code - 0 if no error, otherwise errno
+ * NOTE: User must xfree block_map and block_map_inv
+ */
 extern int
 get_cpuinfo(uint16_t *p_cpus, uint16_t *p_boards,
 	    uint16_t *p_sockets, uint16_t *p_cores, uint16_t *p_threads,
