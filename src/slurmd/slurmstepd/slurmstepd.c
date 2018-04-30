207898d6a57cedc70f91e3265fe18960a96d66c4
Danny Auble <da@schedmd.com>
2018-04-30 17:12:29 -0600

    Sanity checks to make sure we have TRES when starting the slurmstepd.
    This is a followup to the last commit.

--- src/slurmd/slurmstepd/slurmstepd.c
+++ src/slurmd/slurmstepd/slurmstepd.c
@@ -474,6 +474,10 @@ _init_from_slurmd(int sock, char **argv,
 	char buf[16];
 	log_options_t lopts = LOG_OPTS_INITIALIZER;
 	uint32_t jobid = 0, stepid = 0;
+	List tmp_list = NULL;
+	assoc_mgr_lock_t locks = {
+		NO_LOCK, NO_LOCK, NO_LOCK, NO_LOCK,
+		WRITE_LOCK, NO_LOCK, NO_LOCK };
 
 	log_init(argv[0], lopts, LOG_DAEMON, NULL);
 
@@ -493,27 +497,24 @@ _init_from_slurmd(int sock, char **argv,
 	/* Receive TRES information for slurmd */
 	safe_read(sock, &len, sizeof(int));
 	if (len > 0) {
-		List tmp_list = NULL;
-		assoc_mgr_lock_t locks = {
-			NO_LOCK, NO_LOCK, NO_LOCK, NO_LOCK,
-			WRITE_LOCK, NO_LOCK, NO_LOCK };
-
 		incoming_buffer = xmalloc(sizeof(char) * len);
 		safe_read(sock, incoming_buffer, len);
-		buffer = create_buf(incoming_buffer,len);
+		buffer = create_buf(incoming_buffer, len);
 		slurm_unpack_list(&tmp_list,
 				  slurmdb_unpack_tres_rec,
 				  slurmdb_destroy_tres_rec,
 				  buffer, SLURM_PROTOCOL_VERSION);
-		assoc_mgr_lock(&locks);
-		assoc_mgr_post_tres_list(tmp_list);
-		debug("%s: slurmd sent %u TRES.", __func__, g_tres_count);
-		/* assoc_mgr_post_tres_list destroys tmp_list */
-		tmp_list = NULL;
-		assoc_mgr_unlock(&locks);
-
 		free_buf(buffer);
 	}
+
+	xassert(tmp_list);
+
+	assoc_mgr_lock(&locks);
+	assoc_mgr_post_tres_list(tmp_list);
+	debug2("%s: slurmd sent %u TRES.", __func__, g_tres_count);
+	/* assoc_mgr_post_tres_list destroys tmp_list */
+	tmp_list = NULL;
+	assoc_mgr_unlock(&locks);
 
 	/* receive job type from slurmd */
 	safe_read(sock, &step_type, sizeof(int));
