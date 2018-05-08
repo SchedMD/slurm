SchedMD/slurm

commit 90b116c24914174e2c3de4a4900179704c54fb7e

Change the slurmd -> slurmstepd init to send the gids/ngids as part of the msg.
Stop sending them separately.

Force the RPC format used to communicate with slurmstepd to the latest.
(This is separate from the field used to indicate what version the srun
command is expecting to see, so stop conflating the two of them.)

src/slurmd/slurmd/req.c
@@ -471,15 +471,11 @@ _send_slurmstepd_init(int fd, int type, void *req,
 		      slurm_addr_t *cli, slurm_addr_t *self,
 		      hostset_t step_hset, uint16_t protocol_version)
 {
-	int len = 0, ngids, i;
+	int len = 0;
 	Buf buffer = NULL;
 	slurm_msg_t msg;
-	uid_t uid = (uid_t)-1;
-	gid_t gid = (gid_t)-1;
-	gid_t *gids;
-	uint32_t jobid = 0;
 
-	int rank, proto;
+	int rank;
 	int parent_rank, children, depth, max_depth;
 	char *parent_alias = NULL;
 	slurm_addr_t parent_addr = {0};
@@ -606,20 +602,9 @@ _send_slurmstepd_init(int fd, int type, void *req,
 	/* send req over to slurmstepd */
 	switch (type) {
 	case LAUNCH_BATCH_JOB:
-		jobid = ((batch_job_launch_msg_t *)req)->job_id;
-		gid = (uid_t)((batch_job_launch_msg_t *)req)->gid;
-		uid = (uid_t)((batch_job_launch_msg_t *)req)->uid;
 		msg.msg_type = REQUEST_BATCH_JOB_LAUNCH;
 		break;
 	case LAUNCH_TASKS:
-		/*
-		 * The validity of req->uid was verified against the
-		 * auth credential in _rpc_launch_tasks().  req->gid
-		 * has NOT yet been checked!
-		 */
-		jobid = ((launch_tasks_request_msg_t *)req)->job_id;
-		gid = (uid_t)((launch_tasks_request_msg_t *)req)->gid;
-		uid = (uid_t)((launch_tasks_request_msg_t *)req)->uid;
 		msg.msg_type = REQUEST_LAUNCH_TASKS;
 		break;
 	default:
@@ -629,29 +614,18 @@ _send_slurmstepd_init(int fd, int type, void *req,
 	buffer = init_buf(0);
 	msg.data = req;
 
-	if (protocol_version == NO_VAL16)
-		proto = SLURM_PROTOCOL_VERSION;
-	else
-		proto = protocol_version;
-
-	msg.protocol_version = (uint16_t)proto;
+	/* always force the RPC format to the latest */
+	msg.protocol_version = SLURM_PROTOCOL_VERSION;
 	pack_msg(&msg, buffer);
 	len = get_buf_offset(buffer);
 
-	safe_write(fd, &proto, sizeof(int));
+	/* this is the protocol version for the client srun only */
+	safe_write(fd, &protocol_version, sizeof(int));
 
 	safe_write(fd, &len, sizeof(int));
 	safe_write(fd, get_buf_data(buffer), len);
 	free_buf(buffer);
 	buffer = NULL;
-
-	ngids = group_cache_lookup_job(jobid, uid, gid, &gids);
-	safe_write(fd, &ngids, sizeof(int));
-	for (i = 0; i < ngids; i++) {
-		/* we assume sizeof(gid_t) == sizeof(uint32_t) */
-		safe_write(fd, &gids[i], sizeof(uint32_t));
-	}
-	xfree(gids);
 
 	return 0;
 

src/slurmd/slurmstepd/slurmstepd.c
@@ -72,8 +72,7 @@
 #include "src/slurmd/slurmstepd/slurmstepd_job.h"
 
 static int _init_from_slurmd(int sock, char **argv, slurm_addr_t **_cli,
-			     slurm_addr_t **_self, slurm_msg_t **_msg,
-			     int *_ngids, gid_t **_gids);
+			     slurm_addr_t **_self, slurm_msg_t **_msg);
 
 static void _dump_user_env(void);
 static void _send_ok_to_slurmd(int sock);
@@ -101,8 +100,6 @@ main (int argc, char **argv)
 	slurm_addr_t *self;
 	slurm_msg_t *msg;
 	stepd_step_rec_t *job;
-	int ngids;
-	gid_t *gids;
 	int rc = 0;
 	char *launch_params;
 
@@ -120,8 +117,7 @@ main (int argc, char **argv)
 		fatal( "failed to initialize authentication plugin" );
 
 	/* Receive job parameters from the slurmd */
-	_init_from_slurmd(STDIN_FILENO, argv, &cli, &self, &msg,
-			  &ngids, &gids);
+	_init_from_slurmd(STDIN_FILENO, argv, &cli, &self, &msg);
 
 	/* Create the stepd_step_rec_t, mostly from info in a
 	 * launch_tasks_request_msg_t or a batch_job_launch_msg_t */
@@ -130,8 +126,6 @@ main (int argc, char **argv)
 		rc = SLURM_FAILURE;
 		goto ending;
 	}
-	job->ngids = ngids;
-	job->gids = gids;
 
 	/* fork handlers cause mutexes on some global data structures
 	 * to be re-initialized after the fork. */
@@ -438,8 +432,7 @@ rwfail:
  */
 static int
 _init_from_slurmd(int sock, char **argv,
-		  slurm_addr_t **_cli, slurm_addr_t **_self, slurm_msg_t **_msg,
-		  int *_ngids, gid_t **_gids)
+		  slurm_addr_t **_cli, slurm_addr_t **_self, slurm_msg_t **_msg)
 {
 	char *incoming_buffer = NULL;
 	Buf buffer;
@@ -448,8 +441,6 @@ _init_from_slurmd(int sock, char **argv,
 	slurm_addr_t *cli = NULL;
 	slurm_addr_t *self = NULL;
 	slurm_msg_t *msg = NULL;
-	int ngids = 0;
-	gid_t *gids = NULL;
 	uint16_t port;
 	char buf[16];
 	log_options_t lopts = LOG_OPTS_INITIALIZER;
@@ -533,7 +524,8 @@ _init_from_slurmd(int sock, char **argv,
 
 	msg = xmalloc(sizeof(slurm_msg_t));
 	slurm_msg_t_init(msg);
-	msg->protocol_version = (uint16_t)proto;
+	/* Always unpack as the current version. */
+	msg->protocol_version = SLURM_PROTOCOL_VERSION;
 
 	switch (step_type) {
 	case LAUNCH_BATCH_JOB:
@@ -550,25 +542,17 @@ _init_from_slurmd(int sock, char **argv,
 		fatal("slurmstepd: we didn't unpack the request correctly");
 	free_buf(buffer);
 
-	/* receive cached group ids array for the relevant uid */
-	safe_read(sock, &ngids, sizeof(int));
-	if (ngids > 0) {
-		int i;
-		uint32_t tmp32;
-
-		gids = (gid_t *)xmalloc(sizeof(gid_t) * ngids);
-		for (i = 0; i < ngids; i++) {
-			safe_read(sock, &tmp32, sizeof(uint32_t));
-			gids[i] = (gid_t)tmp32;
-			debug2("got gid %d", gids[i]);
-		}
-	}
+	/*
+	 * Swap the field to the srun client version, which will eventually
+	 * end up stored as protocol_version in srun_info_t. It's a hack to
+	 * pass it in-band, while still using the correct version to unpack
+	 * the launch request message above.
+	 */
+	msg->protocol_version = proto;
 
 	*_cli = cli;
 	*_self = self;
 	*_msg = msg;
-	*_ngids = ngids;
-	*_gids = gids;
 
 	return 1;
 

src/slurmd/slurmstepd/slurmstepd_job.c
@@ -66,12 +66,6 @@
 #include "src/slurmd/slurmstepd/multi_prog.h"
 #include "src/slurmd/slurmstepd/slurmstepd_job.h"
 
-#ifdef HAVE_NATIVE_CRAY
-static bool already_validated_uid = true;
-#else
-static bool already_validated_uid = false;
-#endif
-
 static char **_array_copy(int n, char **src);
 static void _array_free(char ***array);
 static void _job_init_task_info(stepd_step_rec_t *job, uint32_t **gtid,
@@ -254,10 +248,6 @@ extern stepd_step_rec_t *stepd_step_rec_create(launch_tasks_request_msg_t *msg,
 	xassert(msg->complete_nodelist != NULL);
 	debug3("entering stepd_step_rec_create");
 
-	if (!slurm_valid_uid_gid((uid_t)msg->uid, &(msg->gid),
-				 &(msg->user_name), already_validated_uid, 1))
-		return NULL;
-
 	if (acct_gather_check_acct_freq_task(msg->job_mem_lim, msg->acctg_freq))
 		return NULL;
 
@@ -289,8 +279,12 @@ extern stepd_step_rec_t *stepd_step_rec_create(launch_tasks_request_msg_t *msg,
 	job->stepid	= msg->job_step_id;
 
 	job->uid	= (uid_t) msg->uid;
-	job->user_name  = xstrdup(msg->user_name);
 	job->gid	= (gid_t) msg->gid;
+	job->user_name	= xstrdup(msg->user_name);
+	job->ngids = (int) msg->ngids;
+	job->gids = xmalloc(job->ngids * sizeof(gid_t));
+	memcpy(job->gids, msg->gids, job->ngids * sizeof(gid_t));
+
 	job->cwd	= xstrdup(msg->cwd);
 	job->task_dist	= msg->task_dist;
 
@@ -469,10 +463,6 @@ batch_stepd_step_rec_create(batch_job_launch_msg_t *msg)
 
 	debug3("entering batch_stepd_step_rec_create");
 
-	if (!slurm_valid_uid_gid((uid_t)msg->uid, &(msg->gid),
-				 &(msg->user_name), already_validated_uid, 1))
-		return NULL;
-
 	if (acct_gather_check_acct_freq_task(msg->job_mem, msg->acctg_freq))
 		return NULL;
 
@@ -497,9 +487,13 @@ batch_stepd_step_rec_create(batch_job_launch_msg_t *msg)
 
 	job->batch   = true;
 	job->node_name  = xstrdup(conf->node_name);
-	job->user_name  = xstrdup(msg->user_name);
-	job->uid        = (uid_t) msg->uid;
-	job->gid        = (gid_t) msg->gid;
+
+	job->uid	= (uid_t) msg->uid;
+	job->gid	= (gid_t) msg->gid;
+	job->user_name	= xstrdup(msg->user_name);
+	job->ngids = (int) msg->ngids;
+	job->gids = xmalloc(job->ngids * sizeof(gid_t));
+	memcpy(job->gids, msg->gids, job->ngids * sizeof(gid_t));
 
 	job->profile    = msg->profile;
 

commit 77d65f4fd334581206015fffdab6243278c7b8f4

Fix issue with invalid protocol_version when using srun on ppc64.
Caused by a corrupted protocol_version field value being received
by the slurmstepd, as we cannot safely write/read a uint16_t across
the pipe as if it was an int.

Regression caused by commit 90b116c2491.

Bug 5133.

src/slurmd/slurmd/req.c
@@ -479,7 +479,7 @@ _send_slurmstepd_init(int fd, int type, void *req,
 	Buf buffer = NULL;
 	slurm_msg_t msg;
 
-	int rank;
+	int rank, proto;
 	int parent_rank, children, depth, max_depth;
 	char *parent_alias = NULL;
 	slurm_addr_t parent_addr = {0};
@@ -623,8 +623,14 @@ _send_slurmstepd_init(int fd, int type, void *req,
 	pack_msg(&msg, buffer);
 	len = get_buf_offset(buffer);
 
-	/* this is the protocol version for the client srun only */
-	safe_write(fd, &protocol_version, sizeof(int));
+	/*
+	 * This is the protocol version for the client srun only.
+	 * Must store into an int before writing, as the argument
+	 * is a uint16_t not an int, and will result in a corrupted
+	 * version being received by the slurmstepd.
+	 */
+	proto = protocol_version;
+	safe_write(fd, &proto, sizeof(int));
 
 	safe_write(fd, &len, sizeof(int));
 	safe_write(fd, get_buf_data(buffer), len);