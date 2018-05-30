SchedMD/slurm

commit e5f03971b97ed2d0d0bd441da31e16647e6ac579

Remove unsafe use of pthread_cancel in pmix plugin.
Can lead to deadlock within malloc depending on the exact timing.

Rework thread startup and shutdown code so pthread_cancel is not
needed.

Bug 5119, 5103.

src/plugins/mpi/pmix/pmixp_agent.c
@@ -50,34 +50,15 @@
 #include "pmixp_utils.h"
 #include "pmixp_dconn.h"
 
-static volatile bool _agent_is_running = false;
-static volatile bool _timer_is_running = false;
-static pthread_mutex_t _flag_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-static void _run_flag_set(volatile bool *flag, bool val)
-{
-	slurm_mutex_lock(&_flag_mutex);
-	*flag = val;
-	slurm_mutex_unlock(&_flag_mutex);
-}
-
-static bool _run_flag_get(volatile bool *flag)
-{
-	bool rc;
-	slurm_mutex_lock(&_flag_mutex);
-	rc = *flag;
-	slurm_mutex_unlock(&_flag_mutex);
-	return rc;
-}
+static pthread_mutex_t agent_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t agent_running_cond = PTHREAD_COND_INITIALIZER;
 
 static eio_handle_t *_io_handle = NULL;
 
-static int _agent_spawned = 0, _timer_spawned = 0;
 static pthread_t _agent_tid = 0;
 static pthread_t _timer_tid = 0;
 
 struct timer_data_t {
-	int initialized;
 	int work_in, work_out;
 	int stop_in, stop_out;
 };
@@ -219,8 +200,6 @@ static int _setup_timeout_fds(void)
 	timer_data.stop_in = fds[0];
 	timer_data.stop_out = fds[1];
 
-	timer_data.initialized = 1;
-
 	return SLURM_SUCCESS;
 }
 
@@ -251,10 +230,6 @@ static void *_agent_thread(void *unused)
 {
 	PMIXP_DEBUG("Start agent thread");
 	eio_obj_t *obj;
-	int preval;
-
-	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, &preval);
-	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &preval);
 
 	_io_handle = eio_handle_create(0);
 
@@ -275,14 +250,12 @@ static void *_agent_thread(void *unused)
 		pmixp_dconn_regio(_io_handle);
 	}
 
-	_run_flag_set(&_agent_is_running, true);
+	pthread_cond_signal(&agent_running_cond);
 
 	eio_handle_mainloop(_io_handle);
 
 	PMIXP_DEBUG("agent thread exit");
 	eio_handle_destroy(_io_handle);
-
-	_run_flag_set(&_agent_is_running, false);
 
 	return NULL;
 }
@@ -290,17 +263,11 @@ static void *_agent_thread(void *unused)
 static void *_pmix_timer_thread(void *unused)
 {
 	struct pollfd pfds[1];
-	int preval;
-
-	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, &preval);
-	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &preval);
 
 	PMIXP_DEBUG("Start timer thread");
 
 	pfds[0].fd = timer_data.stop_in;
 	pfds[0].events = POLLIN;
-
-	_run_flag_set(&_timer_is_running, true);
 
 	/* our job is to sleep 1 sec and then trigger
 	 * the timer event in the main loop */
@@ -321,8 +288,6 @@ static void *_pmix_timer_thread(void *unused)
 	}
 
 rwfail:
-	_run_flag_set(&_timer_is_running, false);
-
 	return NULL;
 }
 
@@ -335,16 +300,15 @@ void _direct_init_sent_buf_cb(int rc, pmixp_p2p_ctx_t ctx, void *data)
 
 int pmixp_agent_start(void)
 {
+	slurm_mutex_lock(&agent_mutex);
+
 	_setup_timeout_fds();
 
 	/* start agent thread */
-	slurm_thread_create_detached(&_agent_tid, _agent_thread, NULL);
-	_agent_spawned = 1;
+	slurm_thread_create(&_agent_tid, _agent_thread, NULL);
 
 	/* wait for the agent thread to initialize */
-	while (!_run_flag_get(&_agent_is_running)) {
-		sched_yield();
-	}
+	slurm_cond_wait(&agent_running_cond, &agent_mutex);
 
 	/* Check if a ping-pong run was requested by user
 	 * NOTE: enabled only if `--enable-debug` configuration
@@ -392,59 +356,46 @@ int pmixp_agent_start(void)
 
 			if (SLURM_SUCCESS != rc) {
 				PMIXP_ERROR_STD("send init msg error");
+				slurm_mutex_unlock(&agent_mutex);
 				return SLURM_ERROR;
 			}
 		}
 	}
 
-
-	slurm_thread_create_detached(&_timer_tid, _pmix_timer_thread, NULL);
-	_timer_spawned = 1;
-
-	/* wait for the agent thread to initialize */
-	while (!_run_flag_get(&_timer_is_running)) {
-		sched_yield();
-	}
+	slurm_thread_create(&_timer_tid, _pmix_timer_thread, NULL);
 
 	PMIXP_DEBUG("timer thread started: tid = %lu",
 		    (unsigned long) _timer_tid);
 
+	slurm_mutex_unlock(&agent_mutex);
 	return SLURM_SUCCESS;
 }
 
 int pmixp_agent_stop(void)
 {
+	int rc = SLURM_SUCCESS;
 	char c = 1;
-	if (_run_flag_get(&_agent_is_running)) {
+
+	slurm_mutex_lock(&agent_mutex);
+
+	if (_agent_tid) {
 		eio_signal_shutdown(_io_handle);
 		/* wait for the agent thread to stop */
-		while (_run_flag_get(&_agent_is_running)) {
-			sched_yield();
-		}
-	}
-	if (_agent_spawned) {
-		pthread_cancel(_agent_tid);
+		pthread_join(_agent_tid, NULL);
+		_agent_tid = 0;
 	}
 
-	if (timer_data.initialized) {
+	if (_timer_tid) {
 		/* cancel timer */
-		safe_write(timer_data.stop_out, &c, 1);
-		while (_run_flag_get(&_timer_is_running) ) {
-			sched_yield();
-		}
+		if (write(timer_data.stop_out, &c, 1) == -1)
+			rc = SLURM_ERROR;
+		pthread_join(_timer_tid, NULL);
+		_timer_tid = 0;
+
 		/* close timer fds */
 		_shutdown_timeout_fds();
 	}
 
-	if (_timer_spawned) {
-		pthread_cancel(_timer_tid);
-	}
-	return SLURM_SUCCESS;
-
-rwfail:
-	if (_timer_spawned)
-		pthread_cancel(_timer_tid);
-
-	error("%s: failed", __func__);
-	return SLURM_ERROR;
+	slurm_mutex_unlock(&agent_mutex);
+	return rc;
 }

commit 7c1dad6e4fd70418d8c3ce2754d02247867d9a52

Fix race in pmixp_agent_start().
Caused by pthread_cancel cleanup by commit e5f03971b97  in 17.11.6.

Bug 5181.

src/plugins/mpi/pmix/pmixp_agent.c
@@ -250,7 +250,9 @@ static void *_agent_thread(void *unused)
 		pmixp_dconn_regio(_io_handle);
 	}
 
+	slurm_mutex_lock(&agent_mutex);
 	pthread_cond_signal(&agent_running_cond);
+	slurm_mutex_unlock(&agent_mutex);
 
 	eio_handle_mainloop(_io_handle);
 