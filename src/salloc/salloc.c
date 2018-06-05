782d7b424bc20b3ec669182d358b43096656c759
Morris Jette <jette@schedmd.com>
2018-06-05 16:31:14 -0600

    Eliminate redundant NULL pointer check
    Coverity CID 186424

--- src/salloc/salloc.c
+++ src/salloc/salloc.c
@@ -476,9 +476,7 @@ int main(int argc, char **argv)
 		/* Allocation granted to regular job */
 		my_job_id = alloc->job_id;
 
-		if (alloc)
-			print_multi_line_string(
-				alloc->job_submit_user_msg, -1);
+		print_multi_line_string(alloc->job_submit_user_msg, -1);
 		info("Granted job allocation %u", my_job_id);
 
 		if (_proc_alloc(alloc) != SLURM_SUCCESS)
