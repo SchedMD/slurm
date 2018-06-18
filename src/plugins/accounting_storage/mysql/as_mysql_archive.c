4c448fd72c4f36ae309a46af8de2c9799f363822
Danny Auble <da@schedmd.com>
2018-06-18 15:43:31 +0200

    MySQL - Prevent deadlock caused by archive logic locking reads.
    Specifically due to SELECT ... FOR UPDATE ones.
    
    Bug 5086.

--- src/plugins/accounting_storage/mysql/as_mysql_archive.c
+++ src/plugins/accounting_storage/mysql/as_mysql_archive.c
@@ -2887,7 +2887,7 @@ static uint32_t _archive_table(purge_type_t type, mysql_conn_t *mysql_conn,
 	case PURGE_TXN:
 		query = xstrdup_printf("select %s from \"%s\" where "
 				       "timestamp <= %ld && cluster='%s' "
-				       "order by timestamp asc for update",
+				       "order by timestamp asc",
 				       cols, sql_table,
 				       period_end, cluster_name);
 		break;
@@ -2895,21 +2895,21 @@ static uint32_t _archive_table(purge_type_t type, mysql_conn_t *mysql_conn,
 	case PURGE_CLUSTER_USAGE:
 		query = xstrdup_printf("select %s from \"%s_%s\" where "
 				       "time_start <= %ld "
-				       "order by time_start asc for update",
+				       "order by time_start asc",
 				       cols, cluster_name, sql_table,
 				       period_end);
 		break;
 	case PURGE_JOB:
 		query = xstrdup_printf("select %s from \"%s_%s\" where "
 				       "time_submit < %ld && time_end != 0 "
-				       "order by time_submit asc for update",
+				       "order by time_submit asc",
 				       cols, cluster_name, job_table,
 				       period_end);
 		break;
 	default:
 		query = xstrdup_printf("select %s from \"%s_%s\" where "
 				       "time_start <= %ld && time_end != 0 "
-				       "order by time_start asc for update",
+				       "order by time_start asc",
 				       cols, cluster_name, sql_table,
 				       period_end);
 		break;
