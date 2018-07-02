c901b5e30bb38fe772f3a4765c156b9313b4e814
Broderick Gardner <broderick@schedmd.com>
2018-07-02 15:42:46 -0600

    Add infrastructure for modified slurmctld stats RPC
    bug 5337

--- src/slurmctld/proc_req.c
+++ src/slurmctld/proc_req.c
@@ -6269,23 +6269,44 @@ static void _pack_rpc_stats(int resp, char **buffer_ptr, int *buffer_size,
 	slurm_mutex_lock(&rpc_mutex);
 	buffer = create_buf(*buffer_ptr, *buffer_size);
 	set_buf_offset(buffer, *buffer_size);
-	for (i = 0; i < rpc_type_size; i++) {
-		if (rpc_type_id[i] == 0)
-			break;
-	}
-	pack32(i, buffer);
-	pack16_array(rpc_type_id,   i, buffer);
-	pack32_array(rpc_type_cnt,  i, buffer);
-	pack64_array(rpc_type_time, i, buffer);
 
-	for (i = 1; i < rpc_user_size; i++) {
-		if (rpc_user_id[i] == 0)
-			break;
+	if (protocol_version >= SLURM_18_08_PROTOCOL_VERSION) {
+		for (i = 0; i < rpc_type_size; i++) {
+			if (rpc_type_id[i] == 0)
+				break;
+		}
+		pack32(i, buffer);
+		pack16_array(rpc_type_id,   i, buffer);
+		pack32_array(rpc_type_cnt,  i, buffer);
+		pack64_array(rpc_type_time, i, buffer);
+
+		for (i = 1; i < rpc_user_size; i++) {
+			if (rpc_user_id[i] == 0)
+				break;
+		}
+		pack32(i, buffer);
+		pack32_array(rpc_user_id,   i, buffer);
+		pack32_array(rpc_user_cnt,  i, buffer);
+		pack64_array(rpc_user_time, i, buffer);
+	} else if (protocol_version >= SLURM_17_11_PROTOCOL_VERSION) {
+		for (i = 0; i < rpc_type_size; i++) {
+			if (rpc_type_id[i] == 0)
+				break;
+		}
+		pack32(i, buffer);
+		pack16_array(rpc_type_id,   i, buffer);
+		pack32_array(rpc_type_cnt,  i, buffer);
+		pack64_array(rpc_type_time, i, buffer);
+
+		for (i = 1; i < rpc_user_size; i++) {
+			if (rpc_user_id[i] == 0)
+				break;
+		}
+		pack32(i, buffer);
+		pack32_array(rpc_user_id,   i, buffer);
+		pack32_array(rpc_user_cnt,  i, buffer);
+		pack64_array(rpc_user_time, i, buffer);
 	}
-	pack32(i, buffer);
-	pack32_array(rpc_user_id,   i, buffer);
-	pack32_array(rpc_user_cnt,  i, buffer);
-	pack64_array(rpc_user_time, i, buffer);
 	slurm_mutex_unlock(&rpc_mutex);
 
 	*buffer_size = get_buf_offset(buffer);
