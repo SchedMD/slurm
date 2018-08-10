SchedMD/slurm

commit d2a1a96c54a6d556f2d91c1f24845a8f4089b41f

Fix multi partition jobs priority_array assignment order.
Bug 3844.

src/plugins/priority/multifactor/priority_multifactor.c
@@ -489,6 +489,18 @@ static double _get_fairshare_priority(struct job_record *job_ptr)
 	return priority_fs;
 }
 
+static int _sort_part_tier(void *x, void *y)
+{
+	struct part_record *parta = (struct part_record *) x;
+	struct part_record *partb = (struct part_record *) y;
+
+	if (parta->priority_tier > partb->priority_tier)
+		return -1;
+	if (parta->priority_tier < partb->priority_tier)
+		return 1;
+
+	return 0;
+}
 
 /* Returns the priority after applying the weight factors */
 static uint32_t _get_priority_internal(time_t start_time,
@@ -586,6 +598,7 @@ static uint32_t _get_priority_internal(time_t start_time,
 		}
 
 		i = 0;
+		list_sort(job_ptr->part_ptr_list, _sort_part_tier);
 		part_iterator = list_iterator_create(job_ptr->part_ptr_list);
 		while ((part_ptr = (struct part_record *)
 			list_next(part_iterator))) {

commit 21d2ab6ed1694bf7b12e824ce41b66bb143e22b3

Fix invalid read caused by d2a1a96c54a6d.
Fixes bad casting caused by Tim on review.

Make one definitive copy of sort_part_tier() in partition_msg.c and use it.

Bug 3844, 5552.

src/plugins/priority/multifactor/priority_multifactor.c
@@ -489,19 +489,6 @@ static double _get_fairshare_priority(struct job_record *job_ptr)
 	return priority_fs;
 }
 
-static int _sort_part_tier(void *x, void *y)
-{
-	struct part_record *parta = (struct part_record *) x;
-	struct part_record *partb = (struct part_record *) y;
-
-	if (parta->priority_tier > partb->priority_tier)
-		return -1;
-	if (parta->priority_tier < partb->priority_tier)
-		return 1;
-
-	return 0;
-}
-
 /* Returns the priority after applying the weight factors */
 static uint32_t _get_priority_internal(time_t start_time,
 				       struct job_record *job_ptr)
@@ -598,7 +585,7 @@ static uint32_t _get_priority_internal(time_t start_time,
 		}
 
 		i = 0;
-		list_sort(job_ptr->part_ptr_list, _sort_part_tier);
+		list_sort(job_ptr->part_ptr_list, sort_part_tier);
 		part_iterator = list_iterator_create(job_ptr->part_ptr_list);
 		while ((part_ptr = (struct part_record *)
 			list_next(part_iterator))) {

src/slurmctld/job_mgr.c
@@ -4573,19 +4573,6 @@ static void _create_job_array(struct job_record *job_ptr,
 	}
 }
 
-static int _sort_part_tier(void *x, void *y)
-{
-	struct part_record *parta = *(struct part_record **) x;
-	struct part_record *partb = *(struct part_record **) y;
-
-	if (parta->priority_tier > partb->priority_tier)
-		return -1;
-	if (parta->priority_tier < partb->priority_tier)
-		return 1;
-
-	return 0;
-}
-
 /*
  * Wrapper for select_nodes() function that will test all valid partitions
  * for a new job
@@ -4606,7 +4593,7 @@ static int _select_nodes_parts(struct job_record *job_ptr, bool test_only,
 	int best_rc = -1, part_limits_rc = WAIT_NO_REASON;
 
 	if (job_ptr->part_ptr_list) {
-		list_sort(job_ptr->part_ptr_list, _sort_part_tier);
+		list_sort(job_ptr->part_ptr_list, sort_part_tier);
 		iter = list_iterator_create(job_ptr->part_ptr_list);
 		while ((part_ptr = list_next(iter))) {
 			job_ptr->part_ptr = part_ptr;

src/slurmctld/partition_mgr.c
@@ -156,6 +156,19 @@ extern void set_partition_tres()
 	list_for_each(part_list, _calc_part_tres, NULL);
 }
 
+extern int sort_part_tier(void *x, void *y)
+{
+	struct part_record *parta = *(struct part_record **) x;
+	struct part_record *partb = *(struct part_record **) y;
+
+	if (parta->priority_tier > partb->priority_tier)
+		return -1;
+	if (parta->priority_tier < partb->priority_tier)
+		return 1;
+
+	return 0;
+}
+
 /*
  * build_part_bitmap - update the total_cpus, total_nodes, and node_bitmap
  *	for the specified partition, also reset the partition pointers in

src/slurmctld/slurmctld.h
@@ -2531,6 +2531,11 @@ waitpid_timeout(const char *, pid_t, int *, int);
 extern void set_partition_tres();
 
 /*
+ * Sort partitions on Priority Tier.
+ */
+extern int sort_part_tier(void *x, void *y);
+
+/*
  * Update job's federated siblings strings.
  *
  * IN job_ptr - job_ptr to update