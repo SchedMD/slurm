SchedMD/slurm

commit 011b2f23e3080e4a33bbd22f08da1f55ff1af7ea

Added info string on sh5util when deleting an empty file.
Bug 4620

src/plugins/acct_gather_profile/hdf5/sh5util/sh5util.c
@@ -303,6 +303,7 @@ static void _remove_empty_output(void)
 	 * the program failed somewhere along the
 	 * way and the file is just left hanging...
 	 */
+	info("Output file generated is empty, removing it: %s", params.output);
 	if ((sb.st_size == 0) &&
 	    (remove(params.output) == -1))
 		error("%s: remove(%s): %m", __func__, params.output);

commit e8355c3d8909362d5543071bf1791e6e4e6e09b4

Print correctly when sh5util finds and empty file.
Fixs regression in 011b2f23e3080e4a33b.

src/plugins/acct_gather_profile/hdf5/sh5util/sh5util.c
@@ -303,10 +303,12 @@ static void _remove_empty_output(void)
 	 * the program failed somewhere along the
 	 * way and the file is just left hanging...
 	 */
-	info("Output file generated is empty, removing it: %s", params.output);
-	if ((sb.st_size == 0) &&
-	    (remove(params.output) == -1))
-		error("%s: remove(%s): %m", __func__, params.output);
+	if ((sb.st_size == 0)) {
+		info("Output file generated is empty, removing it: %s",
+		     params.output);
+		if (remove(params.output) == -1)
+			error("%s: remove(%s): %m", __func__, params.output);
+	}
 }
 
 static void _init_opts(void)