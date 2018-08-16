00ff3081f7634e926e450c9e777f77d8797d6166
Danny Auble <da@schedmd.com>
2018-08-16 15:57:52 -0600

    Fix coverity 187316
    Brian approved

--- src/slurmd/slurmd/slurmd.c
+++ src/slurmd/slurmd/slurmd.c
@@ -1507,7 +1507,8 @@ _slurmd_init(void)
 	}
 
 	/* Set up the hwloc whole system xml file */
-	xcpuinfo_init();
+	if (xcpuinfo_init() != XCPUINFO_SUCCESS)
+		return SLURM_FAILURE;
 
 	fini_job_cnt = cpu_cnt = MAX(conf->conf_cpus, conf->block_map_size);
 	fini_job_id = xmalloc(sizeof(uint32_t) * fini_job_cnt);
