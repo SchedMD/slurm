3f602c4d9fa87bcd11e722c9e19b2c34b1ba277c
Marshall Garey <marshall@schedmd.com>
2018-09-06 15:01:04 -0600

    Fix sacctmgr show runawayjobs.
    Bug 5679
    
    regression from combination of 037bb3cbf52 and 5a6a26f6438

--- src/sacctmgr/runaway_job_functions.c
+++ src/sacctmgr/runaway_job_functions.c
@@ -197,7 +197,7 @@ static List _get_runaway_jobs(slurmdb_job_cond_t *job_cond)
 	List runaway_jobs = NULL;
 	List cluster_list;
 
-	job_cond->flags |= JOBCOND_FLAG_RUNAWAY;
+	job_cond->flags |= JOBCOND_FLAG_RUNAWAY | JOBCOND_FLAG_NO_TRUNC;
 	job_cond->state_list = list_create(slurm_destroy_char);
 	slurm_addto_char_list(job_cond->state_list, "0");
 	slurm_addto_char_list(job_cond->state_list, "1");
