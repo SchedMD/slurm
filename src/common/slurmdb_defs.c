SchedMD/slurm

commit a044d01b3931a838f695fa150e2130d38b76f74d

Add SLURMDB_JOB_FLAG_* enums and translation functions.
Bug 5699

src/common/slurmdb_defs.c
@@ -385,6 +385,23 @@ static uint32_t _str_2_res_flags(char *flags)
 	return 0;
 }
 
+static uint32_t _str_2_job_flags(char *flags)
+{
+	if (xstrcasestr(flags, "None"))
+		return SLURMDB_JOB_FLAG_NONE;
+
+	if (xstrcasestr(flags, "SchedSubmit"))
+		return SLURMDB_JOB_FLAG_SUBMIT;
+
+	if (xstrcasestr(flags, "SchedMain"))
+		return SLURMDB_JOB_FLAG_SCHED;
+
+	if (xstrcasestr(flags, "SchedBackfill"))
+		return SLURMDB_JOB_FLAG_BACKFILL;
+
+	return SLURMDB_JOB_FLAG_NOTSET;
+}
+
 static void _destroy_local_cluster_rec(void *object)
 {
 	xfree(object);
@@ -1756,6 +1773,60 @@ extern uint32_t str_2_cluster_fed_states(char *state)
 	}
 
 	return fed_state;
+}
+
+extern char *slurmdb_job_flags_str(uint32_t flags)
+{
+	char *job_flags = NULL;
+
+	if (flags & SLURMDB_JOB_FLAG_NONE)
+		return xstrdup("None");
+
+	if (flags & SLURMDB_JOB_FLAG_NOTSET)
+		return xstrdup("NotSet");
+
+	if (flags & SLURMDB_JOB_FLAG_SUBMIT)
+		xstrcat(job_flags, "SchedSubmit");
+	else if (flags & SLURMDB_JOB_FLAG_SCHED)
+		xstrcat(job_flags, "SchedMain");
+	else if (flags & SLURMDB_JOB_FLAG_BACKFILL)
+		xstrcat(job_flags, "SchedBackfill");
+
+	/*
+	 * In the future if there are more flags we will need to add comma's to
+	 * the end of Backfilled and NormalSched above and uncomment this code
+	 * below.
+	 */
+	/* if (job_flags) */
+	/* 	job_flags[strlen(job_flags)-1] = '\0'; */
+
+	return job_flags;
+}
+
+extern uint32_t str_2_job_flags(char *flags)
+{
+	uint32_t job_flags = 0;
+	char *token, *my_flags, *last = NULL;
+
+	if (!flags) {
+		error("We need a server job flags string to translate");
+		return SLURMDB_JOB_FLAG_NONE;
+	}
+
+	my_flags = xstrdup(flags);
+	token = strtok_r(my_flags, ",", &last);
+	while (token) {
+		job_flags |= _str_2_job_flags(token);
+		if (job_flags & SLURMDB_JOB_FLAG_NOTSET) {
+			error("%s: Invalid job flag %s", __func__, token);
+			xfree(my_flags);
+			return SLURMDB_JOB_FLAG_NOTSET;
+		}
+		token = strtok_r(NULL, ",", &last);
+	}
+	xfree(my_flags);
+
+	return job_flags;
 }
 
 extern char *slurmdb_qos_flags_str(uint32_t flags)

slurm/slurmdb.h
@@ -168,6 +168,18 @@ enum cluster_fed_states {
 /* flags and types of resources */
 /* when we come up with some */
 
+/*
+ * Translation of db_flags in the struct job_record and flag
+ * slurmdb_job_[rec|cond]_t
+ */
+#define SLURMDB_JOB_FLAG_NONE     0x00000000 /* No flags */
+#define SLURMDB_JOB_CLEAR_SCHED   0x0000000f /* clear scheduling bits */
+#define SLURMDB_JOB_FLAG_NOTSET   0x00000001 /* Not set */
+#define SLURMDB_JOB_FLAG_SUBMIT   0x00000002 /* Job was started on submit */
+#define SLURMDB_JOB_FLAG_SCHED    0x00000004 /* Job was started from main
+					      * scheduler */
+#define SLURMDB_JOB_FLAG_BACKFILL 0x00000008 /* Job was started from backfill */
+
 /* Slurm job condition flags */
 #define JOBCOND_FLAG_DUP      0x00000001 /* Report duplicate job entries */
 #define JOBCOND_FLAG_NO_STEP  0x00000002 /* Don't report job step info */

src/common/slurmdb_defs.h
@@ -122,6 +122,8 @@ extern char *slurmdb_cluster_fed_states_str(uint32_t states);
 extern uint32_t str_2_cluster_fed_states(char *states);
 extern char *slurmdb_federation_flags_str(uint32_t flags);
 extern uint32_t str_2_federation_flags(char *flags, int option);
+extern char *slurmdb_job_flags_str(uint32_t flags);
+extern uint32_t str_2_job_flags(char *flags);
 extern char *slurmdb_qos_str(List qos_list, uint32_t level);
 extern uint32_t str_2_slurmdb_qos(List qos_list, char *level);
 extern char *slurmdb_qos_flags_str(uint32_t flags);

commit bf1255cf5d49b6c5e36574f9aaefd159e2889daa

Fix bad logic
Problem introduced in commit a044d01b39
Coverity CID 188212

src/common/slurmdb_defs.c
@@ -1782,7 +1782,7 @@ extern char *slurmdb_job_flags_str(uint32_t flags)
 {
 	char *job_flags = NULL;
 
-	if (flags & SLURMDB_JOB_FLAG_NONE)
+	if (flags == SLURMDB_JOB_FLAG_NONE)
 		return xstrdup("None");
 
 	if (flags & SLURMDB_JOB_FLAG_NOTSET)