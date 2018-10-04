2db3817b8f3980faadcd35d500fe5e98f3e9e379
Tim Wickberg <tim@schedmd.com>
2018-10-03 18:41:52 -0600

    Change to SLURM_ERROR to fix build; overlooked in prior merge from 18.08.
    SLURM_FAILURE removed in a76a9486078.

--- src/slurmd/common/xcpuinfo.c
+++ src/slurmd/common/xcpuinfo.c
@@ -241,7 +241,7 @@ handle_write:
 	if (hwloc_topology_load(*topology)) {
 		/* error in load hardware topology */
 		debug("hwloc_topology_load() failed.");
-		ret = SLURM_FAILURE;
+		ret = SLURM_ERROR;
 	} else if (!conf->def_config) {
 		debug2("hwloc_topology_export_xml");
 		if (_internal_hwloc_topology_export_xml(*topology, topo_file)) {
