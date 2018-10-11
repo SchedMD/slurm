SchedMD/slurm

commit 0d9b7d5d83c67a418ca1b509c80b3d24e2063eb4

Add constraints/features to the database and query it.
Bug 5700

src/plugins/accounting_storage/mysql/as_mysql_job.c
@@ -495,6 +495,8 @@ no_rollup_change:
 			xstrcat(query, ", tres_req");
 		if (job_ptr->details->work_dir)
 			xstrcat(query, ", work_dir");
+		if (job_ptr->details->features)
+			xstrcat(query, ", constraints");
 
 		xstrfmtcat(query,
 			   ") values (%u, UNIX_TIMESTAMP(), "
@@ -549,6 +551,9 @@ no_rollup_change:
 		if (job_ptr->details->work_dir)
 			xstrfmtcat(query, ", '%s'",
 				   job_ptr->details->work_dir);
+		if (job_ptr->details->features)
+			xstrfmtcat(query, ", '%s'",
+				   job_ptr->details->features);
 
 		xstrfmtcat(query,
 			   ") on duplicate key update "
@@ -615,6 +620,9 @@ no_rollup_change:
 		if (job_ptr->details->work_dir)
 			xstrfmtcat(query, ", work_dir='%s'",
 				   job_ptr->details->work_dir);
+		if (job_ptr->details->features)
+			xstrfmtcat(query, ", constraints='%s'",
+				   job_ptr->details->features);
 
 		if (debug_flags & DEBUG_FLAG_DB_JOB)
 			DB_DEBUG(mysql_conn->conn, "query\n%s", query);
@@ -679,6 +687,9 @@ no_rollup_change:
 		if (job_ptr->details->work_dir)
 			xstrfmtcat(query, "work_dir='%s', ",
 				   job_ptr->details->work_dir);
+		if (job_ptr->details->features)
+			xstrfmtcat(query, ", constraints='%s'",
+				   job_ptr->details->features);
 
 		xstrfmtcat(query, "time_start=%ld, job_name='%s', "
 			   "state=greatest(state, %u), "

slurm/slurmdb.h
@@ -297,6 +297,7 @@ typedef struct {
 	List acct_list;		/* list of char * */
 	List associd_list;	/* list of char */
 	List cluster_list;	/* list of char * */
+	List constraint_list; 	/* list of char * */
 	uint32_t cpus_max;      /* number of cpus high range */
 	uint32_t cpus_min;      /* number of cpus low range */
 	uint32_t db_flags;      /* flags sent from the slurmctld on the job */
@@ -767,6 +768,7 @@ typedef struct {
 	uint32_t associd;
 	char	*blockid;
 	char    *cluster;
+	char    *constraints;
 	uint32_t derived_ec;
 	char	*derived_es; /* aka "comment" */
 	uint32_t elapsed;

src/common/slurmdb_defs.c
@@ -855,6 +855,7 @@ extern void slurmdb_destroy_job_rec(void *object)
 		xfree(job->array_task_str);
 		xfree(job->blockid);
 		xfree(job->cluster);
+		xfree(job->constraints);
 		xfree(job->derived_es);
 		xfree(job->jobname);
 		xfree(job->mcs_label);
@@ -1152,6 +1153,7 @@ extern void slurmdb_destroy_job_cond(void *object)
 		FREE_NULL_LIST(job_cond->acct_list);
 		FREE_NULL_LIST(job_cond->associd_list);
 		FREE_NULL_LIST(job_cond->cluster_list);
+		FREE_NULL_LIST(job_cond->constraint_list);
 		FREE_NULL_LIST(job_cond->groupid_list);
 		FREE_NULL_LIST(job_cond->jobname_list);
 		FREE_NULL_LIST(job_cond->partition_list);

src/common/slurmdb_pack.c
@@ -4553,6 +4553,7 @@ extern void slurmdb_pack_job_cond(void *in, uint16_t protocol_version,
 			pack32(NO_VAL, buffer);	/* count(acct_list) */
 			pack32(NO_VAL, buffer);	/* count(associd_list) */
 			pack32(NO_VAL, buffer);	/* count(cluster_list) */
+			pack32(NO_VAL, buffer);	/* count(constraint_list) */
 			pack32(0, buffer);	/* cpus_max */
 			pack32(0, buffer);	/* cpus_min */
 			pack32(SLURMDB_JOB_FLAG_NOTSET, buffer); /* db_flags */
@@ -4611,6 +4612,16 @@ extern void slurmdb_pack_job_cond(void *in, uint16_t protocol_version,
 		pack32(count, buffer);
 		if (count && (count != NO_VAL)) {
 			itr = list_iterator_create(object->cluster_list);
+			while ((tmp_info = list_next(itr))) {
+				packstr(tmp_info, buffer);
+			}
+			list_iterator_destroy(itr);
+		}
+
+		count = _list_count_null(object->constraint_list);
+		pack32(count, buffer);
+		if (count && (count != NO_VAL)) {
+			itr = list_iterator_create(object->constraint_list);
 			while ((tmp_info = list_next(itr))) {
 				packstr(tmp_info, buffer);
 			}
@@ -5291,6 +5302,20 @@ extern int slurmdb_unpack_job_cond(void **object, uint16_t protocol_version,
 				safe_unpackstr_xmalloc(&tmp_info, &uint32_tmp,
 						       buffer);
 				list_append(object_ptr->cluster_list, tmp_info);
+			}
+		}
+
+		safe_unpack32(&count, buffer);
+		if (count > NO_VAL)
+			goto unpack_error;
+		if (count && (count != NO_VAL)) {
+			object_ptr->constraint_list =
+				list_create(slurm_destroy_char);
+			for (i = 0; i < count; i++) {
+				safe_unpackstr_xmalloc(&tmp_info, &uint32_tmp,
+						       buffer);
+				list_append(object_ptr->constraint_list,
+					    tmp_info);
 			}
 		}
 
@@ -5993,6 +6018,7 @@ extern void slurmdb_pack_job_rec(void *object, uint16_t protocol_version,
 		pack32(job->associd, buffer);
 		packstr(job->blockid, buffer);
 		packstr(job->cluster, buffer);
+		packstr(job->constraints, buffer);
 		pack32((uint32_t)job->derived_ec, buffer);
 		packstr(job->derived_es, buffer);
 		pack32(job->elapsed, buffer);
@@ -6246,6 +6272,8 @@ extern int slurmdb_unpack_job_rec(void **job, uint16_t protocol_version,
 		safe_unpack32(&job_ptr->associd, buffer);
 		safe_unpackstr_xmalloc(&job_ptr->blockid, &uint32_tmp, buffer);
 		safe_unpackstr_xmalloc(&job_ptr->cluster, &uint32_tmp, buffer);
+		safe_unpackstr_xmalloc(&job_ptr->constraints,
+				       &uint32_tmp, buffer);
 		safe_unpack32(&uint32_tmp, buffer);
 		job_ptr->derived_ec = (int32_t)uint32_tmp;
 		safe_unpackstr_xmalloc(&job_ptr->derived_es, &uint32_tmp,
@@ -6256,6 +6284,7 @@ extern int slurmdb_unpack_job_rec(void **job, uint16_t protocol_version,
 		safe_unpack32(&uint32_tmp, buffer);
 		job_ptr->exitcode = (int32_t)uint32_tmp;
 		safe_unpack32(&job_ptr->flags, buffer);
+
 		safe_unpack32(&job_ptr->gid, buffer);
 		safe_unpack32(&job_ptr->jobid, buffer);
 		safe_unpackstr_xmalloc(&job_ptr->jobname, &uint32_tmp, buffer);

src/common/slurmdbd_defs.h
@@ -260,6 +260,7 @@ typedef struct dbd_job_start_msg {
 	uint32_t array_task_pending;/* number of tasks still pending */
 	uint32_t assoc_id;	/* accounting association id */
 	char *   block_id;      /* Bluegene block id */
+	char *   constraints;   /* features/constraints requested by job */
 	uint32_t db_flags;      /* flags about job */
 	uint64_t db_index;	/* index into the db for this job */
 	time_t   eligible_time;	/* time job becomes eligible to run */

src/common/slurmdbd_pack.c
@@ -499,6 +499,7 @@ static void _pack_job_start_msg(void *in,
 		pack32(msg->array_task_pending, buffer);
 		pack32(msg->assoc_id, buffer);
 		packstr(msg->block_id, buffer);
+		packstr(msg->constraints, buffer);
 		pack32(msg->db_flags, buffer);
 		pack64(msg->db_index, buffer);
 		pack_time(msg->eligible_time, buffer);
@@ -590,6 +591,8 @@ static int _unpack_job_start_msg(void **msg,
 		safe_unpack32(&msg_ptr->array_task_pending, buffer);
 		safe_unpack32(&msg_ptr->assoc_id, buffer);
 		safe_unpackstr_xmalloc(&msg_ptr->block_id, &uint32_tmp, buffer);
+		safe_unpackstr_xmalloc(&msg_ptr->constraints,
+				       &uint32_tmp, buffer);
 		safe_unpack32(&msg_ptr->db_flags, buffer);
 		safe_unpack64(&msg_ptr->db_index, buffer);
 		safe_unpack_time(&msg_ptr->eligible_time, buffer);

src/plugins/accounting_storage/mysql/accounting_storage_mysql.c
@@ -1266,6 +1266,7 @@ extern int create_cluster_tables(mysql_conn_t *mysql_conn, char *cluster_name)
 		{ "array_task_str", "text" },
 		{ "array_max_tasks", "int unsigned default 0 not null" },
 		{ "array_task_pending", "int unsigned default 0 not null" },
+		{ "constraints", "text default ''" },
 		{ "cpus_req", "int unsigned not null" },
 		{ "derived_ec", "int unsigned default 0 not null" },
 		{ "derived_es", "text" },

src/plugins/accounting_storage/mysql/as_mysql_archive.c
@@ -87,6 +87,7 @@ typedef struct {
 	char *array_max_tasks;
 	char *array_taskid;
 	char *blockid;
+	char *constraints;
 	char *derived_ec;
 	char *derived_es;
 	char *exit_code;
@@ -249,6 +250,7 @@ static char *job_req_inx[] = {
 	"id_array_job",
 	"id_array_task",
 	"id_block",
+	"constraints",
 	"derived_ec",
 	"derived_es",
 	"exit_code",
@@ -293,6 +295,7 @@ enum {
 	JOB_REQ_ARRAYJOBID,
 	JOB_REQ_ARRAYTASKID,
 	JOB_REQ_BLOCKID,
+	JOB_REQ_CONSTRAINTS,
 	JOB_REQ_DERIVED_EC,
 	JOB_REQ_DERIVED_ES,
 	JOB_REQ_EXIT_CODE,
@@ -601,6 +604,7 @@ static void _pack_local_job(local_job_t *object,
 	packstr(object->array_max_tasks, buffer);
 	packstr(object->array_taskid, buffer);
 	packstr(object->blockid, buffer);
+	packstr(object->constraints, buffer);
 	packstr(object->derived_ec, buffer);
 	packstr(object->derived_es, buffer);
 	packstr(object->exit_code, buffer);
@@ -676,6 +680,7 @@ static int _unpack_local_job(local_job_t *object,
 		unpackstr_ptr(&object->array_max_tasks, &tmp32, buffer);
 		unpackstr_ptr(&object->array_taskid, &tmp32, buffer);
 		unpackstr_ptr(&object->blockid, &tmp32, buffer);
+		unpackstr_ptr(&object->constraints, &tmp32, buffer);
 		unpackstr_ptr(&object->derived_ec, &tmp32, buffer);
 		unpackstr_ptr(&object->derived_es, &tmp32, buffer);
 		unpackstr_ptr(&object->exit_code, &tmp32, buffer);
@@ -2342,6 +2347,7 @@ static Buf _pack_archive_jobs(MYSQL_RES *result, char *cluster_name,
 		job.array_max_tasks = row[JOB_REQ_ARRAY_MAX];
 		job.array_taskid = row[JOB_REQ_ARRAYTASKID];
 		job.blockid = row[JOB_REQ_BLOCKID];
+		job.constraints = row[JOB_REQ_CONSTRAINTS];
 		job.derived_ec = row[JOB_REQ_DERIVED_EC];
 		job.derived_es = row[JOB_REQ_DERIVED_ES];
 		job.exit_code = row[JOB_REQ_EXIT_CODE];
@@ -2419,6 +2425,7 @@ static char *_load_jobs(uint16_t rpc_version, Buf buffer,
 			   object.array_jobid,
 			   object.array_taskid,
 			   object.blockid,
+			   object.constraints,
 			   object.derived_ec,
 			   object.derived_es,
 			   object.exit_code,

commit 3765528957afc3a3f9334955758b4ec23e504306

Fix misplaced comma in SQL
bug introduced in commit 0d9b7d5d83c67a

src/plugins/accounting_storage/mysql/as_mysql_job.c
@@ -683,7 +683,7 @@ no_rollup_change:
 			xstrfmtcat(query, "work_dir='%s', ",
 				   job_ptr->details->work_dir);
 		if (job_ptr->details->features)
-			xstrfmtcat(query, ", constraints='%s'",
+			xstrfmtcat(query, "constraints='%s', ",
 				   job_ptr->details->features);
 
 		xstrfmtcat(query, "time_start=%ld, job_name='%s', "