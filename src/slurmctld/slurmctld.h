8f2c2b04b254a6de5e86723ad2ced897b025cb97
Brian Christiansen <brian@schedmd.com>
2018-10-24 16:01:28 -0600

    Refactor send_job_warn_signal() function
    to send job warn signal. In the gracetime logic, it wasn't checking for
    the B option.
    
    Bug 5867

--- src/slurmctld/slurmctld.h
+++ src/slurmctld/slurmctld.h
@@ -2776,4 +2776,11 @@ extern bool valid_tres_name(char *name);
  */
 extern void check_reboot_nodes();
 
+/*
+ * Send warning signal to job before end time.
+ *
+ * IN job_ptr - job to send warn signal to.
+ */
+extern void send_job_warn_signal(struct job_record *job_ptr);
+
 #endif /* !_HAVE_SLURMCTLD_H */
