SchedMD/slurm

commit b1ff43429f6426c3c976134974dfa616d1f330a4

Split up src/common/slurmdbd_defs into slurmdbd_[defs|pack] and move the agent into the accounting_storage/slurmdbd plugin.
This should be cleaner going forward and will be easier to maintain.

src/plugins/accounting_storage/none/accounting_storage_none.c
@@ -553,6 +553,12 @@ extern int acct_storage_p_clear_stats(void *db_conn, bool dbd)
 	return SLURM_SUCCESS;
 }
 
+extern int acct_storage_p_get_data(void *db_conn, acct_storage_info_t dinfo,
+				   void *data)
+{
+	return SLURM_SUCCESS;
+}
+
 extern int acct_storage_p_shutdown(void *db_conn, bool dbd)
 {
 	return SLURM_SUCCESS;

src/plugins/accounting_storage/slurmdbd/accounting_storage_slurmdbd.c
@@ -58,6 +58,8 @@
 #include "src/slurmctld/slurmctld.h"
 #include "src/slurmctld/locks.h"
 
+#include "slurmdbd_agent.h"
+
 #define BUFFER_SIZE 4096
 
 /* These are defined here so when we link with something other than
@@ -67,9 +69,15 @@
 #if defined(__APPLE__)
 slurm_ctl_conf_t slurmctld_conf __attribute__((weak_import));
 List job_list __attribute__((weak_import)) = NULL;
+uint16_t running_cache __attribute__((weak_import)) = 0;
+pthread_mutex_t assoc_cache_mutex __attribute__((weak_import));
+pthread_cond_t assoc_cache_cond __attribute__((weak_import));
 #else
 slurm_ctl_conf_t slurmctld_conf;
 List job_list = NULL;
+uint16_t running_cache = 0;
+pthread_mutex_t assoc_cache_mutex;
+pthread_cond_t assoc_cache_cond;
 #endif
 
 /*
@@ -458,7 +466,7 @@ static void *_set_db_inx_thread(void *no_data)
 extern int init ( void )
 {
 	if (first) {
-		char *cluster_name = NULL, *auth_info;
+		char *cluster_name = NULL;
 		/* since this can be loaded from many different places
 		   only tell us once. */
 		if (!(cluster_name = slurm_get_cluster_name()))
@@ -466,12 +474,8 @@ extern int init ( void )
 			      plugin_name);
 		xfree(cluster_name);
 
-		auth_info = slurm_get_accounting_storage_pass();
-		verbose("%s loaded with AuthInfo=%s",
-			plugin_name, auth_info);
+		verbose("%s loaded", plugin_name);
 
-		slurmdbd_defs_init(auth_info);
-		xfree(auth_info);
 		if (job_list && !(slurm_get_accounting_storage_enforce() &
 				  ACCOUNTING_ENFORCE_NO_JOBS)) {
 			/* only do this when job_list is defined
@@ -507,7 +511,6 @@ extern int fini ( void )
 		pthread_join(db_inx_handler_thread, NULL);
 
 	first = 1;
-	slurmdbd_defs_fini();
 
 	return SLURM_SUCCESS;
 }
@@ -3099,6 +3102,27 @@ extern int acct_storage_p_clear_stats(void *db_conn)
 	msg.msg_type = DBD_CLEAR_STATS;
 	slurm_send_slurmdbd_recv_rc_msg(SLURM_PROTOCOL_VERSION, &msg, &rc);
 
+	return rc;
+}
+
+extern int acct_storage_p_get_data(void *db_conn, acct_storage_info_t dinfo,
+				   void *data)
+{
+	int *int_data = (int *) data;
+	int rc = SLURM_SUCCESS;
+
+	switch (dinfo) {
+	case ACCT_STORAGE_INFO_CONN_ACTIVE:
+		*int_data = slurmdbd_conn_active();
+		break;
+	case ACCT_STORAGE_INFO_AGENT_COUNT:
+		*int_data = slurmdbd_agent_queue_count();
+		break;
+	default:
+		error("%s: data request %d invalid", __func__, dinfo);
+		rc = SLURM_ERROR;
+		break;
+	}
 	return rc;
 }
 

src/common/Makefile.am
@@ -79,6 +79,7 @@ libcommon_la_SOURCES = 			\
 	slurmdb_defs.c slurmdb_defs.h   \
 	slurmdb_pack.c slurmdb_pack.h   \
 	slurmdbd_defs.c slurmdbd_defs.h	\
+	slurmdbd_pack.c slurmdbd_pack.h	\
 	working_cluster.c working_cluster.h   \
 	uid.c uid.h			\
 	util-net.c util-net.h		\

src/common/Makefile.in
@@ -173,11 +173,11 @@ am_libcommon_la_OBJECTS = assoc_mgr.lo cpu_frequency.lo \
 	slurm_protocol_api.lo slurm_protocol_pack.lo \
 	slurm_protocol_util.lo slurm_protocol_socket_implementation.lo \
 	slurm_protocol_defs.lo slurm_rlimits_info.lo slurmdb_defs.lo \
-	slurmdb_pack.lo slurmdbd_defs.lo working_cluster.lo uid.lo \
-	util-net.lo slurm_auth.lo slurm_acct_gather.lo \
-	slurm_accounting_storage.lo slurm_jobacct_gather.lo \
-	slurm_acct_gather_energy.lo slurm_acct_gather_profile.lo \
-	slurm_acct_gather_interconnect.lo \
+	slurmdb_pack.lo slurmdbd_defs.lo slurmdbd_pack.lo \
+	working_cluster.lo uid.lo util-net.lo slurm_auth.lo \
+	slurm_acct_gather.lo slurm_accounting_storage.lo \
+	slurm_jobacct_gather.lo slurm_acct_gather_energy.lo \
+	slurm_acct_gather_profile.lo slurm_acct_gather_interconnect.lo \
 	slurm_acct_gather_filesystem.lo slurm_jobcomp.lo \
 	slurm_route.lo slurm_time.lo slurm_topology.lo switch.lo \
 	slurm_selecttype_info.lo slurm_resource_info.lo hostlist.lo \
@@ -609,6 +609,7 @@ libcommon_la_SOURCES = \
 	slurmdb_defs.c slurmdb_defs.h   \
 	slurmdb_pack.c slurmdb_pack.h   \
 	slurmdbd_defs.c slurmdbd_defs.h	\
+	slurmdbd_pack.c slurmdbd_pack.h	\
 	working_cluster.c working_cluster.h   \
 	uid.c uid.h			\
 	util-net.c util-net.h		\
@@ -836,6 +837,7 @@ distclean-compile:
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/slurmdb_defs.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/slurmdb_pack.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/slurmdbd_defs.Plo@am__quote@
+@AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/slurmdbd_pack.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/state_control.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/stepd_api.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/strlcpy.Plo@am__quote@

src/common/assoc_mgr.c
@@ -47,6 +47,7 @@
 #include "src/common/uid.h"
 #include "src/common/xstring.h"
 #include "src/common/slurm_priority.h"
+#include "src/common/slurmdbd_pack.h"
 #include "src/slurmdbd/read_config.h"
 
 #define ASSOC_HASH_SIZE 1000
@@ -77,6 +78,14 @@ static int *assoc_mgr_tres_old_pos = NULL;
 
 static pthread_mutex_t locks_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t locks_cond = PTHREAD_COND_INITIALIZER;
+
+static bool _running_cache(void)
+{
+	if (init_setup.running_cache && *init_setup.running_cache)
+		return true;
+
+	return false;
+}
 
 static int _get_str_inx(char *name)
 {
@@ -1425,7 +1434,7 @@ static int _get_assoc_mgr_tres_list(void *db_conn, int enforce)
 
 	assoc_mgr_unlock(&locks);
 
-	if (!running_cache && changed && init_setup.update_cluster_tres) {
+	if (changed && !_running_cache() && init_setup.update_cluster_tres) {
 		/* update jobs here, this needs to be outside of the
 		 * assoc_mgr locks */
 		init_setup.update_cluster_tres();
@@ -1979,7 +1988,7 @@ extern int assoc_mgr_init(void *db_conn, assoc_init_args_t *args,
 	if (args)
 		memcpy(&init_setup, args, sizeof(assoc_init_args_t));
 
-	if (running_cache) {
+	if (_running_cache()) {
 		debug4("No need to run assoc_mgr_init, "
 		       "we probably don't have a connection.  "
 		       "If we do use assoc_mgr_refresh_lists instead.");
@@ -2095,7 +2104,9 @@ extern int assoc_mgr_fini(bool save_state)
 	assoc_mgr_wckey_list = NULL;
 
 	assoc_mgr_root_assoc = NULL;
-	running_cache = 0;
+
+	if (_running_cache())
+		*init_setup.running_cache = 0;
 
 	xfree(assoc_hash_id);
 	xfree(assoc_hash);
@@ -6018,8 +6029,10 @@ extern int load_assoc_mgr_state(bool only_tres)
 		if (only_tres)
 			break;
 	}
-	if (!only_tres)
-		running_cache = 1;
+
+	if (!only_tres && init_setup.running_cache)
+		*init_setup.running_cache = 1;
+
 	free_buf(buffer);
 	assoc_mgr_unlock(&locks);
 	return SLURM_SUCCESS;
@@ -6077,8 +6090,8 @@ extern int assoc_mgr_refresh_lists(void *db_conn, uint16_t cache_level)
 			    db_conn, init_setup.enforce) == SLURM_ERROR)
 			return SLURM_ERROR;
 
-	if (!partial_list)
-		running_cache = 0;
+	if (!partial_list && _running_cache())
+		*init_setup.running_cache = 0;
 
 	return SLURM_SUCCESS;
 }

commit 3d85c8f9240542d9e6dfb727244e75e449430aac

Handle symbol resolution errors in the 18.08 slurmdbd.
Caused by b1ff43429f6426c when moving the slurmdbd agent internals.

Bug 5882.

src/common/slurm_xlator.h
@@ -365,9 +365,14 @@
 /* slurmdbd_defs.[ch] functions */
 #define slurmdbd_defs_init      slurm_slurmdbd_defs_init
 #define slurmdbd_defs_fini      slurm_slurmdbd_defs_fini
+#define slurmdbd_free_buffer	slurm_slurmdbd_free_buffer
 #define slurmdbd_free_list_msg	slurm_slurmdbd_free_list_msg
 #define slurmdbd_free_usage_msg slurm_slurmdbd_free_usage_msg
 #define slurmdbd_free_id_rc_msg slurm_slurmdbd_free_id_rc_msg
+
+/* slurmdbd_pack.[ch] functions */
+#define pack_slurmdbd_msg	slurm_pack_slurmdbd_msg
+#define unpack_slurmdbd_msg	slurm_unpack_slurmdbd_msg
 
 /* plugin.[ch] functions */
 #define plugin_get_syms         slurm_plugin_get_syms

src/common/slurmdbd_defs.c
@@ -44,6 +44,7 @@
  * Define slurm-specific aliases for use by plugins, see slurm_xlator.h
  * for details.
  */
+strong_alias(slurmdbd_free_buffer,	slurm_slurmdbd_free_buffer);
 strong_alias(slurmdbd_free_list_msg,	slurm_slurmdbd_free_list_msg);
 strong_alias(slurmdbd_free_usage_msg,	slurm_slurmdbd_free_usage_msg);
 strong_alias(slurmdbd_free_id_rc_msg,	slurm_slurmdbd_free_id_rc_msg);

src/common/slurmdbd_pack.c
@@ -44,6 +44,13 @@
 #include "src/common/xmalloc.h"
 #include "src/common/xstring.h"
 
+/*
+ * Define slurm-specific aliases for use by plugins, see slurm_xlator.h
+ * for details.
+ */
+strong_alias(pack_slurmdbd_msg, slurm_pack_slurmdbd_msg);
+strong_alias(unpack_slurmdbd_msg, slurm_unpack_slurmdbd_msg);
+
 static int _unpack_config_name(char **object, uint16_t rpc_version, Buf buffer)
 {
 	char *config_name;

src/plugins/accounting_storage/none/accounting_storage_none.c
@@ -36,6 +36,7 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
+#include "src/common/slurm_xlator.h"
 #include "src/common/slurm_accounting_storage.h"
 
 /*

src/plugins/accounting_storage/slurmdbd/accounting_storage_slurmdbd.c
@@ -72,12 +72,14 @@ List job_list __attribute__((weak_import)) = NULL;
 uint16_t running_cache __attribute__((weak_import)) = 0;
 pthread_mutex_t assoc_cache_mutex __attribute__((weak_import));
 pthread_cond_t assoc_cache_cond __attribute__((weak_import));
+int node_record_count __attribute__((weak_import);
 #else
 slurm_ctl_conf_t slurmctld_conf;
 List job_list = NULL;
 uint16_t running_cache = 0;
 pthread_mutex_t assoc_cache_mutex;
 pthread_cond_t assoc_cache_cond;
+int node_record_count;
 #endif
 
 /*

src/plugins/accounting_storage/slurmdbd/slurmdbd_agent.c
@@ -37,6 +37,7 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
+#include "src/common/slurm_xlator.h"
 #include "src/common/slurmdbd_pack.h"
 #include "src/common/xsignal.h"
 #include "src/common/xstring.h"