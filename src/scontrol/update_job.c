87f036b3693490fe667082e2e6aa28355c3cf800
Danny Auble <da@schedmd.com>
2018-10-31 10:58:30 -0600

    Add scontrol update job ResetAccrueTime to clear a job's time previously accrued for priority.
    Bug 5881
    
    Brian approved.

--- src/scontrol/update_job.c
+++ src/scontrol/update_job.c
@@ -752,10 +752,15 @@ extern int scontrol_update_job(int argc, char **argv)
 			}
 			val++;
 			vallen = strlen(val);
-		} else if (xstrncasecmp(tag, "Nice", MAX(strlen(tag), 2)) == 0){
-			/* "Nice" is the only tag that might not have an
-			   equal sign, so it is handled specially. */
+		}
+		/* Handle any tags that might not have an equal sign here */
+		else if (xstrncasecmp(tag, "Nice", MAX(strlen(tag), 2)) == 0) {
 			job_msg.nice = NICE_OFFSET + 100;
+			update_cnt++;
+			continue;
+		} else if (!xstrncasecmp(tag, "ResetAccrueTime",
+					 MAX(strlen(tag), 3))) {
+			job_msg.bitflags |= RESET_ACCRUE_TIME;
 			update_cnt++;
 			continue;
 		} else if (!val && argv[i + 1]) {
