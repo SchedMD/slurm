303b76bf3a24902b1656ed8e776688b7d74fba2e
Nathan Rini <nate@schedmd.com>
2019-02-22 10:21:26 -0700

    Promote info message to error when --mem and --mem-per-cpu are both set.
    Bug 6187.

--- src/salloc/opt.c
+++ src/salloc/opt.c
@@ -1570,7 +1570,7 @@ static bool _opt_verify(void)
 			     "to mem-per-cpu");
 			opt.pn_min_memory = opt.mem_per_cpu;
 		}
-		info("WARNING: --mem and --mem-per-cpu are mutually exclusive.");
+		error("--mem and --mem-per-cpu are mutually exclusive.");
 	}
 
         /* Check to see if user has specified enough resources to
--- src/srun/libsrun/opt.c
+++ src/srun/libsrun/opt.c
@@ -2337,7 +2337,7 @@ static void _opt_args(int argc, char **argv, int pack_offset)
 			     "to mem-per-cpu");
 			opt.pn_min_memory = opt.mem_per_cpu;
 		}
-		info("WARNING: --mem and --mem-per-cpu are mutually exclusive.");
+		error("--mem and --mem-per-cpu are mutually exclusive.");
 	}
 
 	if (sropt.pty) {
