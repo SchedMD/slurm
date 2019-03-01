fe8e98d7c06225610776e7631ea170e67f7dbca4
Michael Hinton <hinton@schedmd.com>
2019-03-01 15:51:55 -0700

    enable running of specific sub-tests from 39.18

--- testsuite/expect/test39.18.ulong.c
+++ testsuite/expect/test39.18.ulong.c
@@ -38,7 +38,7 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-    printf("%lu", sizeof(unsigned long)*8);
+    printf("%lu\n", sizeof(unsigned long)*8);
     return 0;
 }
 // See https://unix.stackexchange.com/a/115244/303114
