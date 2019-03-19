2e298f0546f228b9f4cfb4f16c211c8b680f2ac7
Tim Wickberg <tim@schedmd.com>
2019-03-19 14:21:36 -0600

    Add COMMON_SBATCH_STRING_OPTION macro.

--- src/common/slurm_opt.c
+++ src/common/slurm_opt.c
@@ -80,6 +80,41 @@ static void arg_reset_##field(slurm_opt_t *opt)			\
 	xfree(opt->field);					\
 }
 
+#define COMMON_SBATCH_STRING_OPTION(field)	\
+COMMON_SBATCH_STRING_OPTION_SET(field)		\
+COMMON_SBATCH_STRING_OPTION_GET(field)		\
+COMMON_SBATCH_STRING_OPTION_RESET(field)
+#define COMMON_SBATCH_STRING_OPTION_SET(field)			\
+static int arg_set_##field(slurm_opt_t *opt, const char *arg)	\
+__attribute__((nonnull (1)));					\
+static int arg_set_##field(slurm_opt_t *opt, const char *arg)	\
+{								\
+	if (!opt->sbatch_opt)					\
+		return SLURM_ERROR;				\
+								\
+	xfree(opt->sbatch_opt->field);				\
+	opt->sbatch_opt->field = xstrdup(arg);			\
+								\
+	return SLURM_SUCCESS;					\
+}
+#define COMMON_SBATCH_STRING_OPTION_GET(field)			\
+static char *arg_get_##field(slurm_opt_t *opt)			\
+__attribute__((nonnull));					\
+static char *arg_get_##field(slurm_opt_t *opt)			\
+{								\
+	if (!opt->sbatch_opt)					\
+		return xstrdup("invalid-context");		\
+	return xstrdup(opt->sbatch_opt->field);			\
+}
+#define COMMON_SBATCH_STRING_OPTION_RESET(field)		\
+static void arg_reset_##field(slurm_opt_t *opt)			\
+__attribute__((nonnull));					\
+static void arg_reset_##field(slurm_opt_t *opt)			\
+{								\
+	if (opt->sbatch_opt)					\
+		xfree(opt->sbatch_opt->field);			\
+}
+
 #define COMMON_OPTION_RESET(field, value)			\
 static void arg_reset_##field(slurm_opt_t *opt)			\
 __attribute__((nonnull));					\
