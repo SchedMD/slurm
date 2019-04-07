5e0419c70394ea4d4d781b2f99397604fde8c440
Morris Jette <jette@schedmd.com>
2019-04-07 08:41:16 -0600

    Cosmetic changes to regression test

--- testsuite/expect/test39.21.prog.cu
+++ testsuite/expect/test39.21.prog.cu
@@ -1,45 +1,47 @@
+// Slurm regression test39.21.prog.cu
 #include <iostream>
 #include <math.h>
 // Kernel function to add the elements of two arrays
 __global__
 void add(int n, float *x, float *y)
 {
-  int index = threadIdx.x;
-  int stride = blockDim.x;
-  for (int i = index; i < n; i += stride)
-    y[i] = x[i] + y[i];
+	int index = threadIdx.x;
+	int stride = blockDim.x;
+	for (int i = index; i < n; i += stride)
+		y[i] = x[i] + y[i];
 }
 
 int main(void)
 {
-  int N = 1024;
-  float *x, *y;
+	int N = 1024;
+	int i;
+	float *x, *y;
+	float maxError = 0.0f;
 
-  // Allocate Unified Memory – accessible from CPU or GPU
-  cudaMallocManaged(&x, N*sizeof(float));
-  cudaMallocManaged(&y, N*sizeof(float));
+	// Allocate Unified Memory – accessible from CPU or GPU
+	cudaMallocManaged(&x, N * sizeof(float));
+	cudaMallocManaged(&y, N * sizeof(float));
 
-  // initialize x and y arrays on the host
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
-  }
+	// initialize x and y arrays on the host
+	for (i = 0; i < N; i++) {
+		x[i] = 1.0f;
+		y[i] = 2.0f;
+	}
 
-  // Run kernel on 1000 elements on the GPU
-  add<<<1, 256>>>(N, x, y);
+	// Run kernel on 256 elements at a time on the GPU
+	add<<<1, 256>>>(N, x, y);
 
-  // Wait for GPU to finish before accessing on host
-  cudaDeviceSynchronize();
+	// Wait for GPU to finish before accessing on host
+	cudaDeviceSynchronize();
 
-  // Check for errors (all values should be 3.0f)
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(y[i]-3.0f));
-  std::cout << "Max error: " << maxError << std::endl;
+	// Check for errors (all values should be 3.0f)
+	for (i = 0; i < N; i++)
+		maxError = fmax(maxError, fabs(y[i] - 3.0f));
+	std::cout << "Max error: " << maxError << std::endl;
 
-  // Free memory
-  cudaFree(x);
-  cudaFree(y);
+	// Free memory
+	cudaFree(x);
+	cudaFree(y);
 
-  return 0;
+	return 0;
 }
