5b3ab52408d476be33d8d8cda2a310e84221dfae
Nate Rini <nate@schedmd.com>
2019-04-22 16:52:47 -0600

    Log when Lua script is loaded or unloaded.
    Bug 6513

--- src/plugins/job_submit/lua/job_submit_lua.c
+++ src/plugins/job_submit/lua/job_submit_lua.c
@@ -1675,8 +1675,11 @@ static int _load_script(void)
 	}
 	
 	if (st.st_mtime <= lua_script_last_loaded) {
+		debug3("%s: skipping loading Lua script: %s", __func__,
+		       lua_script_path);
 		return SLURM_SUCCESS;
 	}
+	debug3("%s: loading Lua script: %s", __func__, lua_script_path);
 
 	/*
 	 *  Initilize lua
@@ -1790,6 +1793,7 @@ int init(void)
 int fini(void)
 {
 	if (L) {
+		debug3("%s: Unloading Lua script", __func__);
 		lua_close(L);
 		L = NULL;
 		lua_script_last_loaded = 0;
