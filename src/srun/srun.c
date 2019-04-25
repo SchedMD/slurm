SchedMD/slurm

commit 2f162b7a7addf0440474defe65c7e31bc65a73bb

Add mapping from task to originating pack index to allow the stepd to identify which tasks are part of which packs

src/srun/srun.c
@@ -357,6 +357,7 @@ static void _launch_app(srun_job_t *job, List srun_job_list, bool got_alloc)
 	bool need_mpir = false;
 	uint16_t *tmp_task_cnt = NULL, *pack_task_cnts = NULL;
 	uint32_t **tmp_tids = NULL, **pack_tids = NULL;
+	uint32_t *pack_tid_offsets = NULL;
 
 	launch_type = slurm_get_launch_type();
 	if (launch_type && strstr(launch_type, "slurm"))
@@ -384,6 +385,14 @@ static void _launch_app(srun_job_t *job, List srun_job_list, bool got_alloc)
 			(void) slurm_step_ctx_get(job->step_ctx,
 						  SLURM_STEP_CTX_TASKS,
 						  &tmp_task_cnt);
+			xrealloc(pack_tid_offsets,
+				 sizeof(uint32_t) * total_ntasks);
+
+			for (i = total_ntasks - job->ntasks;
+			     i < total_ntasks;
+			     i++)
+				pack_tid_offsets[i] = job->pack_offset;
+
 			if (!tmp_task_cnt) {
 				fatal("%s: job %u has NULL task array",
 				      __func__, job->jobid);
@@ -435,6 +444,8 @@ static void _launch_app(srun_job_t *job, List srun_job_list, bool got_alloc)
 			mpir_init(total_ntasks);
 
 		opt_iter = list_iterator_create(opt_list);
+
+		/* copy aggregated pack data back into each sub-job */
 		while ((opt_local = list_next(opt_iter))) {
 			srun_opt_t *srun_opt = opt_local->srun_opt;
 			xassert(srun_opt);
@@ -460,15 +471,21 @@ static void _launch_app(srun_job_t *job, List srun_job_list, bool got_alloc)
 			job->pack_node_list = xstrdup(pack_node_list);
 			if ((pack_step_cnt > 1) && pack_task_cnts) {
 				xassert(node_offset == job->pack_nnodes);
-				job->pack_task_cnts = xmalloc(sizeof(uint16_t) *
-							      job->pack_nnodes);
+				job->pack_task_cnts = xcalloc(job->pack_nnodes,
+							      sizeof(uint16_t));
 				memcpy(job->pack_task_cnts, pack_task_cnts,
 				       sizeof(uint16_t) * job->pack_nnodes);
-				job->pack_tids = xmalloc(sizeof(uint32_t *) *
-							 job->pack_nnodes);
+				job->pack_tids = xcalloc(job->pack_nnodes,
+							 sizeof(uint32_t *));
 				memcpy(job->pack_tids, pack_tids,
 				       sizeof(uint32_t *) * job->pack_nnodes);
+
+				job->pack_tid_offsets = xcalloc(
+					total_ntasks, sizeof(uint32_t));
+				memcpy(job->pack_tid_offsets, pack_tid_offsets,
+				       sizeof(uint32_t) * total_ntasks);
 			}
+
 			opts = xmalloc(sizeof(_launch_app_data_t));
 			opts->got_alloc   = got_alloc;
 			opts->job         = job;
@@ -483,6 +500,7 @@ static void _launch_app(srun_job_t *job, List srun_job_list, bool got_alloc)
 		}
 		xfree(pack_node_list);
 		xfree(pack_task_cnts);
+		xfree(pack_tid_offsets);
 		list_iterator_destroy(job_iter);
 		list_iterator_destroy(opt_iter);
 		slurm_mutex_lock(&step_mutex);
@@ -493,24 +511,30 @@ static void _launch_app(srun_job_t *job, List srun_job_list, bool got_alloc)
 		if (first_job)
 			fini_srun(first_job, got_alloc, &global_rc, 0);
 	} else {
+		int i;
 		if (need_mpir)
 			mpir_init(job->ntasks);
 		if (job->pack_jobid && (job->pack_jobid != NO_VAL)) {
 			(void) slurm_step_ctx_get(job->step_ctx,
 						  SLURM_STEP_CTX_TASKS,
 						  &tmp_task_cnt);
-			job->pack_task_cnts = xmalloc(sizeof(uint16_t) *
-						      job->pack_nnodes);
+			job->pack_task_cnts = xcalloc(job->pack_nnodes,
+						      sizeof(uint16_t));
 			memcpy(job->pack_task_cnts, tmp_task_cnt,
 			       sizeof(uint16_t) * job->pack_nnodes);
 			(void) slurm_step_ctx_get(job->step_ctx,
 						  SLURM_STEP_CTX_TIDS,
 						  &tmp_tids);
-			job->pack_tids = xmalloc(sizeof(uint32_t *) *
-						 job->pack_nnodes);
+			job->pack_tids = xcalloc(job->pack_nnodes,
+						 sizeof(uint32_t *));
 			memcpy(job->pack_tids, tmp_tids,
 			       sizeof(uint32_t *) * job->pack_nnodes);
 			job->pack_node_list = xstrdup(job->nodelist);
+
+			job->pack_tid_offsets = xcalloc(
+				total_ntasks, sizeof(uint32_t));
+			for (i = 0; i < job->ntasks; i++)
+				job->pack_tid_offsets[i] = job->pack_offset;
 		}
 		opts = xmalloc(sizeof(_launch_app_data_t));
 		opts->got_alloc   = got_alloc;

slurm/slurm.h.in
@@ -1963,6 +1963,7 @@ typedef struct {
 	uint32_t pack_step_cnt;	/* total step count for entire pack job */
 	uint16_t *pack_task_cnts; /* Number of tasks on each node in pack job */
 	uint32_t **pack_tids;	/* Task IDs on each node in pack job */
+	uint32_t *pack_tid_offsets;/* map of tasks (by id) to originating pack*/
 	uint32_t pack_offset;	/* Pack job offset or NO_VAL */
 	uint32_t pack_task_offset; /* Pack job task offset or NO_VAL */
 	char *pack_node_list;	/* Pack step node list */

src/api/step_launch.c
@@ -269,6 +269,7 @@ extern int slurm_step_launch(slurm_step_ctx_t *ctx,
 	launch.pack_task_offset = params->pack_task_offset;
 	launch.pack_task_cnts = params->pack_task_cnts;
 	launch.pack_tids = params->pack_tids;
+	launch.pack_tid_offsets = params->pack_tid_offsets;
 	launch.pack_node_list = params->pack_node_list;
 	if (params->env == NULL) {
 		/*
@@ -467,6 +468,7 @@ extern int slurm_step_launch_add(slurm_step_ctx_t *ctx,
 	launch.pack_task_offset = params->pack_task_offset;
 	launch.pack_task_cnts = params->pack_task_cnts;
 	launch.pack_tids = params->pack_tids;
+	launch.pack_tid_offsets = params->pack_tid_offsets;
 	launch.pack_node_list = params->pack_node_list;
 	if (params->env == NULL) {
 		/*

src/common/slurm_protocol_defs.c
@@ -1338,6 +1338,7 @@ extern void slurm_free_launch_tasks_request_msg(launch_tasks_request_msg_t * msg
 			xfree(msg->pack_tids[i]);
 		xfree(msg->pack_tids);
 	}
+	xfree(msg->pack_tid_offsets);
 	xfree(msg->tasks_to_launch);
 	xfree(msg->resp_port);
 	xfree(msg->io_port);

src/common/slurm_protocol_defs.h
@@ -808,6 +808,7 @@ typedef struct launch_tasks_request_msg {
 	uint32_t  pack_ntasks;	/* total task count for entire pack job */
 	uint16_t *pack_task_cnts; /* Number of tasks on each node in pack job */
 	uint32_t **pack_tids;   /* Task IDs on each node of pack job */
+	uint32_t *pack_tid_offsets;/* map of tasks (by id) to originating pack*/
 	uint32_t  pack_offset;	/* pack job offset of NO_VAL */
 	uint32_t  pack_step_cnt; /* number of steps for entire pack job */
 	uint32_t  pack_task_offset;/* pack job task ID offset of NO_VAL */

src/common/slurm_protocol_pack.c
@@ -10101,6 +10101,9 @@ static void _pack_launch_tasks_request_msg(launch_tasks_request_msg_t *msg,
 				     buffer);
 		}
 		pack32(msg->pack_ntasks, buffer);
+		if (msg->pack_ntasks != NO_VAL)
+			for (i = 0; i < msg->pack_ntasks; i++)
+				pack32(msg->pack_tid_offsets[i], buffer);
 		pack32(msg->pack_offset, buffer);
 		pack32(msg->pack_step_cnt, buffer);
 		pack32(msg->pack_task_offset, buffer);
@@ -10410,6 +10413,13 @@ static int _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **msg_ptr
 				goto unpack_error;
 		}
 		safe_unpack32(&msg->pack_ntasks, buffer);
+		if (msg->pack_ntasks != NO_VAL) {
+			safe_xcalloc(msg->pack_tid_offsets, msg->pack_ntasks,
+				     sizeof(uint32_t));
+			for (i = 0; i < msg->pack_ntasks; i++)
+				safe_unpack32(&msg->pack_tid_offsets[i],
+					      buffer);
+		}
 		safe_unpack32(&msg->pack_offset, buffer);
 		safe_unpack32(&msg->pack_step_cnt, buffer);
 		safe_unpack32(&msg->pack_task_offset, buffer);

src/plugins/launch/slurm/launch_slurm.c
@@ -736,6 +736,7 @@ extern int launch_p_step_launch(srun_job_t *job, slurm_step_io_fds_t *cio_fds,
 	launch_params.pack_task_offset = job->pack_task_offset;
 	launch_params.pack_task_cnts = job->pack_task_cnts;
 	launch_params.pack_tids = job->pack_tids;
+	launch_params.pack_tid_offsets = job->pack_tid_offsets;
 	launch_params.pack_node_list = job->pack_node_list;
 	launch_params.partition = job->partition;
 	launch_params.profile = opt_local->profile;

src/slurmd/slurmstepd/slurmstepd_job.c
@@ -273,9 +273,9 @@ extern stepd_step_rec_t *stepd_step_rec_create(launch_tasks_request_msg_t *msg,
 	slurm_cond_init(&job->state_cond, NULL);
 	slurm_mutex_init(&job->state_mutex);
 	job->node_tasks	= msg->tasks_to_launch[nodeid];
-	i = sizeof(uint16_t) * msg->nnodes;
-	job->task_cnts  = xmalloc(i);
-	memcpy(job->task_cnts, msg->tasks_to_launch, i);
+	job->task_cnts  = xcalloc(msg->nnodes, sizeof(uint16_t));
+	memcpy(job->task_cnts, msg->tasks_to_launch,
+	       sizeof(uint16_t) * msg->nnodes);
 	job->ntasks	= msg->ntasks;
 	job->jobid	= msg->job_id;
 	job->stepid	= msg->job_step_id;
@@ -311,24 +311,33 @@ extern stepd_step_rec_t *stepd_step_rec_create(launch_tasks_request_msg_t *msg,
 	job->pack_nnodes = msg->pack_nnodes;	/* Used for env vars */
 	if (msg->pack_nnodes && msg->pack_ntasks && msg->pack_task_cnts) {
 		job->pack_ntasks = msg->pack_ntasks;	/* Used for env vars */
-		i = sizeof(uint16_t) * msg->pack_nnodes;
-		job->pack_task_cnts = xmalloc(i);
-		memcpy(job->pack_task_cnts, msg->pack_task_cnts, i);
+		job->pack_task_cnts = xcalloc(msg->pack_nnodes,
+					      sizeof(uint16_t));
+		memcpy(job->pack_task_cnts, msg->pack_task_cnts,
+		       sizeof(uint16_t) * msg->pack_nnodes);
 		if (msg->pack_tids) {
 			/* pack_tids == NULL if request from pre-v19.05 srun */
 			job->pack_tids = xcalloc(msg->pack_nnodes,
 						 sizeof(uint32_t *));
 			for (i = 0; i < msg->pack_nnodes; i++) {
-				size_t j;
-				j = sizeof(uint32_t) * job->pack_task_cnts[i];
-				job->pack_tids[i] = xcalloc(job->pack_task_cnts[i],
-							    sizeof(uint32_t));
-				memcpy(job->pack_tids[i], msg->pack_tids[i], j);
+				job->pack_tids[i] =
+					xcalloc(job->pack_task_cnts[i],
+						sizeof(uint32_t));
+				memcpy(job->pack_tids[i], msg->pack_tids[i],
+				       sizeof(uint32_t) *
+				       job->pack_task_cnts[i]);
 			}
+		}
+		if (msg->pack_tid_offsets) {
+			job->pack_tid_offsets = xcalloc(job->pack_ntasks,
+							sizeof(uint32_t));
+			memcpy(job->pack_tid_offsets, msg->pack_tid_offsets,
+			       job->pack_ntasks * sizeof(uint32_t));
 		}
 	}
 	job->pack_offset = msg->pack_offset;	/* Used for env vars & labels */
-	job->pack_task_offset = msg->pack_task_offset;	/* Used for env vars & labels */
+	job->pack_task_offset = msg->pack_task_offset;	/* Used for env vars &
+							 * labels */
 	job->pack_node_list = xstrdup(msg->pack_node_list);
 	for (i = 0; i < msg->envc; i++) {
 		/*                         1234567890123456789 */
@@ -621,6 +630,7 @@ stepd_step_rec_destroy(stepd_step_rec_t *job)
 			xfree(job->pack_tids[i]);
 		xfree(job->pack_tids);
 	}
+	xfree(job->pack_tid_offsets);
 	xfree(job->task_prolog);
 	xfree(job->task_epilog);
 	xfree(job->job_alloc_cores);

commit 275f9f9b2da62cbc9802878e5599cf0b43ba6e8b

Fix for het jobs ability to run steps
Without this change a heterogeneous job allocation could not run
a step on any single component of a hetjob, but only across multiple
hetjob components. Note that by default a step runs on only the
first hetjob component, so the default mode of operation running
steps on a hetjob was broken. This bug caused tests 38.7 and 38.11
to fail consistently. The bug was introduced in commit 2f162b7a7addf

src/srun/srun.c
@@ -526,8 +526,8 @@ static void _launch_app(srun_job_t *job, List srun_job_list, bool got_alloc)
 			       sizeof(uint32_t *) * job->pack_nnodes);
 			job->pack_node_list = xstrdup(job->nodelist);
 
-			job->pack_tid_offsets = xcalloc(
-				total_ntasks, sizeof(uint32_t));
+			job->pack_tid_offsets = xcalloc(job->ntasks,
+							sizeof(uint32_t));
 			for (i = 0; i < job->ntasks; i++)
 				job->pack_tid_offsets[i] = job->pack_offset;
 		}