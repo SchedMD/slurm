SchedMD/slurm

commit 8d643e793df46569552173b2f85ed87b5fa219ae

Fix the MySQL archive load to handle the changes in 989a92827bc17.

src/plugins/accounting_storage/mysql/as_mysql_archive.c
@@ -741,6 +741,24 @@ static int _unpack_local_job(local_job_t *object,
 		unpackstr_ptr(&object->qos, &tmp32, buffer);
 		unpackstr_ptr(&object->req_cpus, &tmp32, buffer);
 		unpackstr_ptr(&object->req_mem, &tmp32, buffer);
+		if (object->req_mem) {
+			uint64_t tmp_uint64 = slurm_atoull(object->req_mem);
+			if ((tmp_uint64 & 0x80000000) &&
+			    (tmp_uint64 < 0x100000000)) {
+				/*
+				 * Handle old conversion of memory
+				 * stored incorrectly in the database.
+				 * This will be fixed in 17.11 and we
+				 * can remove this check.  0x80000000
+				 * was the old value of MEM_PER_CPU
+				 */
+				tmp_uint64 &= (~0x80000000);
+				tmp_uint64 |= MEM_PER_CPU;
+				xfree(object->req_mem);
+				object->req_mem = xstrdup_printf("%"PRIu64,
+								 tmp_uint64);
+			}
+		}
 		unpackstr_ptr(&object->resvid, &tmp32, buffer);
 		unpackstr_ptr(&object->start, &tmp32, buffer);
 		unpackstr_ptr(&object->state, &tmp32, buffer);
@@ -780,6 +798,24 @@ static int _unpack_local_job(local_job_t *object,
 		unpackstr_ptr(&object->qos, &tmp32, buffer);
 		unpackstr_ptr(&object->req_cpus, &tmp32, buffer);
 		unpackstr_ptr(&object->req_mem, &tmp32, buffer);
+		if (object->req_mem) {
+			uint64_t tmp_uint64 = slurm_atoull(object->req_mem);
+			if ((tmp_uint64 & 0x80000000) &&
+			    (tmp_uint64 < 0x100000000)) {
+				/*
+				 * Handle old conversion of memory
+				 * stored incorrectly in the database.
+				 * This will be fixed in 17.11 and we
+				 * can remove this check.  0x80000000
+				 * was the old value of MEM_PER_CPU
+				 */
+				tmp_uint64 &= (~0x80000000);
+				tmp_uint64 |= MEM_PER_CPU;
+				xfree(object->req_mem);
+				object->req_mem = xstrdup_printf("%"PRIu64,
+								 tmp_uint64);
+			}
+		}
 		unpackstr_ptr(&object->resvid, &tmp32, buffer);
 		unpackstr_ptr(&object->start, &tmp32, buffer);
 		unpackstr_ptr(&object->state, &tmp32, buffer);
@@ -819,6 +855,24 @@ static int _unpack_local_job(local_job_t *object,
 		unpackstr_ptr(&object->qos, &tmp32, buffer);
 		unpackstr_ptr(&object->req_cpus, &tmp32, buffer);
 		unpackstr_ptr(&object->req_mem, &tmp32, buffer);
+		if (object->req_mem) {
+			uint64_t tmp_uint64 = slurm_atoull(object->req_mem);
+			if ((tmp_uint64 & 0x80000000) &&
+			    (tmp_uint64 < 0x100000000)) {
+				/*
+				 * Handle old conversion of memory
+				 * stored incorrectly in the database.
+				 * This will be fixed in 17.11 and we
+				 * can remove this check.  0x80000000
+				 * was the old value of MEM_PER_CPU
+				 */
+				tmp_uint64 &= (~0x80000000);
+				tmp_uint64 |= MEM_PER_CPU;
+				xfree(object->req_mem);
+				object->req_mem = xstrdup_printf("%"PRIu64,
+								 tmp_uint64);
+			}
+		}
 		unpackstr_ptr(&object->resvid, &tmp32, buffer);
 		object->pack_job_id = "0";
 		object->pack_job_offset = "4294967294";
@@ -857,6 +911,24 @@ static int _unpack_local_job(local_job_t *object,
 		unpackstr_ptr(&object->qos, &tmp32, buffer);
 		unpackstr_ptr(&object->req_cpus, &tmp32, buffer);
 		unpackstr_ptr(&object->req_mem, &tmp32, buffer);
+		if (object->req_mem) {
+			uint64_t tmp_uint64 = slurm_atoull(object->req_mem);
+			if ((tmp_uint64 & 0x80000000) &&
+			    (tmp_uint64 < 0x100000000)) {
+				/*
+				 * Handle old conversion of memory
+				 * stored incorrectly in the database.
+				 * This will be fixed in 17.11 and we
+				 * can remove this check.  0x80000000
+				 * was the old value of MEM_PER_CPU
+				 */
+				tmp_uint64 &= (~0x80000000);
+				tmp_uint64 |= MEM_PER_CPU;
+				xfree(object->req_mem);
+				object->req_mem = xstrdup_printf("%"PRIu64,
+								 tmp_uint64);
+			}
+		}
 		unpackstr_ptr(&object->resvid, &tmp32, buffer);
 		object->pack_job_id = "0";
 		object->pack_job_offset = "4294967294";

commit e174e1353f35dceccc8588969302e7aed374bfe8

accounting_storage/mysql - fix SIGABRT in the archive load logic.
The problem was freeing an interior pointer to buffer contents before
the call to FREE_NULL_BUFFER. The issue was only triggered when loading
an archived data with protocol version < 17.11.

Regression introduced in 8d643e793df4.

Bug 6832.

src/plugins/accounting_storage/mysql/as_mysql_archive.c
@@ -789,7 +789,6 @@ static int _unpack_local_job(local_job_t *object,
 				 */
 				tmp_uint64 &= (~0x80000000);
 				tmp_uint64 |= MEM_PER_CPU;
-				xfree(object->req_mem);
 				object->req_mem = xstrdup_printf("%"PRIu64,
 								 tmp_uint64);
 			}
@@ -846,7 +845,6 @@ static int _unpack_local_job(local_job_t *object,
 				 */
 				tmp_uint64 &= (~0x80000000);
 				tmp_uint64 |= MEM_PER_CPU;
-				xfree(object->req_mem);
 				object->req_mem = xstrdup_printf("%"PRIu64,
 								 tmp_uint64);
 			}
@@ -903,7 +901,6 @@ static int _unpack_local_job(local_job_t *object,
 				 */
 				tmp_uint64 &= (~0x80000000);
 				tmp_uint64 |= MEM_PER_CPU;
-				xfree(object->req_mem);
 				object->req_mem = xstrdup_printf("%"PRIu64,
 								 tmp_uint64);
 			}
@@ -959,7 +956,6 @@ static int _unpack_local_job(local_job_t *object,
 				 */
 				tmp_uint64 &= (~0x80000000);
 				tmp_uint64 |= MEM_PER_CPU;
-				xfree(object->req_mem);
 				object->req_mem = xstrdup_printf("%"PRIu64,
 								 tmp_uint64);
 			}