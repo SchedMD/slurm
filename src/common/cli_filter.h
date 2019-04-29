7f2b0a9cd313d1875260aff9577b9db6bb37fda0
Doug Jacobsen <dmjacobsen@lbl.gov>
2019-04-28 20:28:09 -0600

    Add cli_filter plugin driver code, and cli_filter/none example.

---
+++ src/common/cli_filter.h
@@ -0,0 +1,109 @@
+/*****************************************************************************\
+ *  cli_filter.h - driver for the cli_filter plugin
+ *****************************************************************************
+ *  Copyright (C) 2017-2019 Regents of the University of California
+ *  Produced at Lawrence Berkeley National Laboratory (cf, DISCLAIMER).
+ *  Written by Douglas Jacobsen <dmjacobsen@lbl.gov>
+ *  All rights reserved.
+ *
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://slurm.schedmd.com/>.
+ *  Please also read the included file: DISCLAIMER.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and
+ *  distribute linked combinations including the two. You must obey the GNU
+ *  General Public License in all respects for all of the code used other than
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this
+ *  exception to your version of the file(s), but you are not obligated to do
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in
+ *  the program, then also delete it here.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifndef _CLI_FILTER_H
+#define _CLI_FILTER_H
+
+#include "slurm/slurm.h"
+#include "src/common/slurm_opt.h"
+
+/*
+ * Initialize the cli filter plugin.
+ *
+ * Returns a SLURM errno.
+ */
+extern int cli_filter_plugin_init(void);
+
+/*
+ * Terminate the cli filter plugin. Free memory.
+ *
+ * Returns a SLURM errno.
+ */
+extern int cli_filter_plugin_fini(void);
+
+/*
+ **************************************************************************
+ *                          P L U G I N   C A L L S                       *
+ **************************************************************************
+ */
+
+
+/*
+ * Execute the setup_defaults() function in each cli filter plugin.
+ * setup_defaults() is executed before the CLI performs option processing
+ * and thus can be used to change default values in the CLI.
+ * IN/OUT opt	- pointer to slurm_opt_t data structure. Value of pointer
+ *		  cannot change, but OK to mutate some of the values within
+ *		  the dereferenced memory using the appropriate argset
+ *		  functions.
+ * IN early	- is the setup_defaults() running in the early pass or not
+ * RETURNs	- SLURM_SUCCESS if cli_filter processing should allow the
+ *				CLI to continue execution
+ *		- SLURM_ERROR	if any condition is determined that should
+ *				cease processing of the CLI
+ */
+extern int cli_filter_plugin_setup_defaults(slurm_opt_t *opt, bool early);
+
+/*
+ * Execute the pre_submit() function in each cli filter plugin.
+ * pre_submit() is executed after the CLI performs option processing
+ * but before final option validation is performed.
+ * IN/OUT opt	- pointer to slurm_opt_t data structure. Value of pointer
+ *		  cannot change, but OK to mutate some of the values within
+ *		  the dereferenced memory using the appropriate argset
+ *		  functions.
+ * RETURNs	- SLURM_SUCCESS if cli_filter processing should allow the
+ *				CLI to continue execution
+ *		- SLURM_ERROR   if any condition is determined that should
+ *				cease processing of the CLI
+ */
+extern int cli_filter_plugin_pre_submit(slurm_opt_t *opt);
+
+/*
+ * Execute the post_submit() function in each cli filter plugin.
+ * post_submit() is executed after the CLI receives a response from the
+ * controller with the jobid. This is primarily for logging. The plugin
+ * should not attempt to read or modify any arguments at this stage as the
+ * strings interface may not be stable. Instead this is meant to be used
+ * primarily for logging purposes using data collected from the pre_submit()
+ * hook.
+ * IN jobid	- job identifier back from the controller.
+ */
+extern void cli_filter_plugin_post_submit(uint32_t jobid);
+
+#endif /* !_CLI_FILTER_H */
---
+++ src/plugins/cli_filter/none/cli_filter_none.c
@@ -0,0 +1,96 @@
+/*****************************************************************************\
+ *  cli_filter_none.c - blank referance cli_filter plugin
+ *****************************************************************************
+ *  Copyright (C) 2017-2019 Regents of the University of California
+ *  Produced at Lawrence Berkeley National Laboratory (cf, DISCLAIMER).
+ *  Written by Douglas Jacobsen <dmjacobsen@lbl.gov>
+ *  All rights reserved.
+ *
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <https://slurm.schedmd.com/>.
+ *  Please also read the included file: DISCLAIMER.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and
+ *  distribute linked combinations including the two. You must obey the GNU
+ *  General Public License in all respects for all of the code used other than
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this
+ *  exception to your version of the file(s), but you are not obligated to do
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in
+ *  the program, then also delete it here.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#define _GNU_SOURCE
+
+#include <inttypes.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "src/common/slurm_xlator.h"
+
+#include "slurm/slurm_errno.h"
+#include "src/common/cli_filter.h"
+#include "src/common/slurm_opt.h"
+
+/*
+ * These variables are required by the generic plugin interface.  If they
+ * are not found in the plugin, the plugin loader will ignore it.
+ *
+ * plugin_name - a string giving a human-readable description of the
+ * plugin.  There is no maximum length, but the symbol must refer to
+ * a valid string.
+ *
+ * plugin_type - a string suggesting the type of the plugin or its
+ * applicability to a particular form of data or method of data handling.
+ * If the low-level plugin API is used, the contents of this string are
+ * unimportant and may be anything.  SLURM uses the higher-level plugin
+ * interface which requires this string to be of the form
+ *
+ *	<application>/<method>
+ *
+ * where <application> is a description of the intended application of
+ * the plugin (e.g., "auth" for SLURM authentication) and <method> is a
+ * description of how this plugin satisfies that application.  SLURM will
+ * only load authentication plugins if the plugin_type string has a prefix
+ * of "auth/".
+ *
+ * plugin_version - an unsigned 32-bit integer containing the Slurm version
+ * (major.minor.micro combined into a single number).
+ */
+const char plugin_name[]	= "cli filter none plugin";
+const char plugin_type[]	= "cli_filter/none";
+const uint32_t plugin_version	= SLURM_VERSION_NUMBER;
+
+extern int setup_defaults(slurm_opt_t *opt, bool early)
+{
+	return SLURM_SUCCESS;
+}
+
+extern int pre_submit(slurm_opt_t *opt)
+{
+	return SLURM_SUCCESS;
+}
+
+extern void post_submit(uint32_t jobid)
+{
+	return;
+}
