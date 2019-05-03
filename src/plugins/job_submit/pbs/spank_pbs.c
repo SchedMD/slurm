6fe93df7e4fb47525310c2c334a2af1632540ff2
Andy Georges <itkovian@gmail.com>
2019-05-03 12:07:16 +0200

    Required include file

--- src/plugins/job_submit/pbs/spank_pbs.c
+++ src/plugins/job_submit/pbs/spank_pbs.c
@@ -38,6 +38,7 @@
 \*****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "slurm/spank.h"
 
