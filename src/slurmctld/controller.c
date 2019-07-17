957663435594217ea9bb78c4779d7b9e6930b382
Brian Christiansen <brian@schedmd.com>
2019-07-17 16:52:58 -0600

    Fini fed_mgr after backup relinquishes control
    This makes it so that the backup will reinint the fed_mgr and load fed
    state when a backup takes over on subsequent takeovers.
    
    Bug 7320

--- src/slurmctld/controller.c
+++ src/slurmctld/controller.c
@@ -784,6 +784,7 @@ int main(int argc, char **argv)
 		bb_g_fini();
 		power_g_fini();
 		slurm_mcs_fini();
+		fed_mgr_fini();
 
 		if (running_cache) {
 			/* break out and end the association cache
@@ -878,7 +879,6 @@ int main(int argc, char **argv)
 	purge_front_end_state();
 	resv_fini();
 	trigger_fini();
-	fed_mgr_fini();
 	assoc_mgr_fini(1);
 	reserve_port_config(NULL);
 	free_rpc_stats();
