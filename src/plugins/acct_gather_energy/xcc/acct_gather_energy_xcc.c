25e6f4d84ca339ac579715bfe71cf642ad5c8ebb
Brian Christiansen <brian@schedmd.com>
2019-09-06 08:26:31 -0600

    Correct "extern" definition of variable if compiling with __APPLE__
    Bug 7699

--- src/plugins/acct_gather_energy/xcc/acct_gather_energy_xcc.c
+++ src/plugins/acct_gather_energy/xcc/acct_gather_energy_xcc.c
@@ -64,7 +64,7 @@
  * overwritten when linking with the slurmctld.
  */
 #if defined (__APPLE__)
-extenr slurmd_conf_t *conf __attribute__((weak_import));
+extern slurmd_conf_t *conf __attribute__((weak_import));
 #else
 slurmd_conf_t *conf = NULL;
 #endif
