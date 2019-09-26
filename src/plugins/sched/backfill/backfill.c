SchedMD/slurm

commit fb26b7063e50cb083e23cfcdd9725af5526ca41b

Small enhancement to use variable already set.

src/plugins/sched/backfill/backfill.c
@@ -1343,7 +1343,7 @@ static int _attempt_backfill(void)
 
 	gettimeofday(&bf_time1, NULL);
 
-	slurmctld_diag_stats.bf_queue_len = list_count(job_queue);
+	slurmctld_diag_stats.bf_queue_len = job_test_count;
 	slurmctld_diag_stats.bf_queue_len_sum += slurmctld_diag_stats.
 						 bf_queue_len;
 	slurmctld_diag_stats.bf_last_depth = 0;

commit 4c8a10dfc70490cf235e99802da189d0734c5cd8

Fix sdiag backfill last and mean queue length stats.
Regression introduced in fb26b7063e5.

Bug 7675

src/plugins/sched/backfill/backfill.c
@@ -1553,10 +1553,8 @@ static int _attempt_backfill(void)
 			debug("backfill: no jobs to backfill");
 		FREE_NULL_LIST(job_queue);
 		return 0;
-	} else {
+	} else
 		debug("backfill: %u jobs to backfill", job_test_count);
-		job_test_count = 0;
-	}
 
 	if (backfill_continue)
 		list_for_each(job_list, _clear_job_start_times, NULL);
@@ -1569,6 +1567,8 @@ static int _attempt_backfill(void)
 	slurmctld_diag_stats.bf_queue_len = job_test_count;
 	slurmctld_diag_stats.bf_queue_len_sum += slurmctld_diag_stats.
 						 bf_queue_len;
+	job_test_count = 0;
+
 	slurmctld_diag_stats.bf_last_depth = 0;
 	slurmctld_diag_stats.bf_last_depth_try = 0;
 	slurmctld_diag_stats.bf_when_last_cycle = now;