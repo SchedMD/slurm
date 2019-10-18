6ca833fa36c9062e9e18ea40609217945b3e80c8
Felip Moll <felip.moll@schedmd.com>
2019-10-18 14:48:41 -0600

    Remove stray 'Fixme' comment.

--- src/common/gres.c
+++ src/common/gres.c
@@ -12582,7 +12582,7 @@ extern void gres_plugin_step_set_env(char ***job_env_ptr, List step_gres_list,
 		if (!found) { /* No data fond */
 			if (accel_bind_type || tres_bind) {
 				(*(gres_context[i].ops.step_reset_env))
-					(job_env_ptr, NULL, NULL); /* Fixme */
+					(job_env_ptr, NULL, NULL);
 			} else {
 				(*(gres_context[i].ops.step_set_env))
 					(job_env_ptr, NULL);
