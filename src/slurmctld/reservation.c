SchedMD/slurm

commit c55f6d657cd0b16b35f5a10671f816c60b70c5a7

Fix create_resv()
use correct start_time for TIME_FLOAT resevation
in _job_overlap()

Bug 7458

src/slurmctld/reservation.c
@@ -1797,7 +1797,6 @@ static bool _job_overlap(time_t start_time, uint32_t flags,
 		return overlap;
 	if (flags & RESERVE_FLAG_TIME_FLOAT)
 		start_time += time(NULL);
-
 	job_iterator = list_iterator_create(job_list);
 	while ((job_ptr = (struct job_record *) list_next(job_iterator))) {
 		if (IS_JOB_RUNNING(job_ptr)		&&
@@ -2245,7 +2244,7 @@ extern int create_resv(resv_desc_msg_t *resv_desc_ptr)
 		total_node_cnt = bit_set_count(node_bitmap);
 		if (!(resv_desc_ptr->flags & RESERVE_FLAG_IGN_JOBS) &&
 		    !resv_desc_ptr->core_cnt &&
-		    _job_overlap(resv_desc_ptr->start_time,
+		    _job_overlap(resv_desc_ptr->start_time - now,
 				 resv_desc_ptr->flags, node_bitmap, NULL)) {
 			info("Reservation request overlaps jobs");
 			rc = ESLURM_NODES_BUSY;

commit 4c1ed636c8a0a587feba33a47f127ed750004da9

Fix regression caused by c55f6d657.
Remove the TIME_FLOAT flag from the reservation to ensure _job_overlap()
does not add the current time on top of the start_time. The prior
approach was incorrect for non-TIME_FLOAT reservations and would
lead to valid reservations being rejected.

Bug 7458, 7908.

src/slurmctld/reservation.c
@@ -2245,12 +2245,23 @@ extern int create_resv(resv_desc_msg_t *resv_desc_ptr)
 		}
 		total_node_cnt = bit_set_count(node_bitmap);
 		if (!(resv_desc_ptr->flags & RESERVE_FLAG_IGN_JOBS) &&
-		    !resv_desc_ptr->core_cnt &&
-		    _job_overlap(resv_desc_ptr->start_time,
-				 resv_desc_ptr->flags, node_bitmap, NULL)) {
-			info("Reservation request overlaps jobs");
-			rc = ESLURM_NODES_BUSY;
-			goto bad_parse;
+		    !resv_desc_ptr->core_cnt) {
+			uint32_t flags = resv_desc_ptr->flags;
+
+			/*
+			 * Need to clear this flag before _job_overlap()
+			 * which would otherwise add the current time
+			 * on to the start_time. start_time for floating
+			 * reservations has already been set to now.
+			 */
+			flags &= ~RESERVE_FLAG_TIME_FLOAT;
+
+			if (_job_overlap(resv_desc_ptr->start_time, flags,
+					 node_bitmap, NULL)) {
+				info("Reservation request overlaps jobs");
+				rc = ESLURM_NODES_BUSY;
+				goto bad_parse;
+			}
 		}
 		/* We do allow to request cores with nodelist */
 		if (resv_desc_ptr->core_cnt) {