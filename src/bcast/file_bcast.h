a371b4480f97d3c3e9f62c7929a9c0fdc1fcf710
Alejandro Sanchez <alex@schedmd.com>
2020-01-17 13:44:15 +0100

    Convert bcast_parameters continuation.
    Bug 4546.

--- src/bcast/file_bcast.h
+++ src/bcast/file_bcast.h
@@ -47,7 +47,7 @@ struct bcast_parameters {
 	int fanout;
 	bool force;
 	uint32_t het_job_offset;	/* HetJob offset or NO_VAL */
-	uint32_t job_id;		/* Job ID or Pack Job ID */
+	uint32_t job_id;		/* Job ID or hetjob ID */
 	bool preserve;
 	char *src_fname;
 	uint32_t step_id;
