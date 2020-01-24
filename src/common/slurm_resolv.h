f2d4ba513342f83312329d119600fb3bde8067ab
Tim Wickberg <tim@schedmd.com>
2020-01-24 15:29:31 -0700

    Add slurm_resolv.[ch] to the build, and resolve_ctls_from_dns_srv().
    Lookup DNS SRV entries for the slurmctld of the form _slurmctld._tcp
    in the local search path.

---
+++ src/common/slurm_resolv.h
@@ -0,0 +1,54 @@
+/*****************************************************************************\
+ *  slurm_resolv.h - functions for DNS SRV resolution
+ *****************************************************************************
+ *  Copyright (C) 2020 SchedMD LLC.
+ *  Written by Tim Wickberg <tim@schedmd.com>
+ *
+ *  This file is part of Slurm, a resource management program.
+ *  For details, see <https://slurm.schedmd.com/>.
+ *  Please also read the included file: DISCLAIMER.
+ *
+ *  Slurm is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and
+ *  distribute linked combinations including the two. You must obey the GNU
+ *  General Public License in all respects for all of the code used other than
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this
+ *  exception to your version of the file(s), but you are not obligated to do
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in
+ *  the program, then also delete it here.
+ *
+ *  Slurm is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Slurm; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifndef _SLURM_RESOLV_H_
+#define _SLURM_RESOLV_H_
+
+typedef struct {
+	uint16_t priority;
+	uint16_t port;
+	char hostname[1024];
+} ctl_entry_t;
+
+/*
+ * Returns a sorted List of SlurmctldHosts with port numbers assembled
+ * from the _slurmctld._tcp DNS records for the current search domain,
+ * or NULL on error.
+ * Must list_destroy() the result to avoid a memory leak.
+ */
+extern List resolve_ctls_from_dns_srv(void);
+
+#endif
