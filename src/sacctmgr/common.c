35e203d0da77b447290a3906058db1be231064e3
Brian Christiansen <brian@schedmd.com>
2020-01-27 23:08:01 -0700

    cluster_list is never NULL
    Bug 5691

--- src/sacctmgr/common.c
+++ src/sacctmgr/common.c
@@ -2242,15 +2242,13 @@ extern int sacctmgr_validate_cluster_list(List cluster_list)
 	int rc = SLURM_SUCCESS;
 	ListIterator itr = NULL, itr_c = NULL;
 
-	if (cluster_list) {
-		slurmdb_cluster_cond_t cluster_cond;
-		slurmdb_init_cluster_cond(&cluster_cond, 0);
-		cluster_cond.cluster_list = cluster_list;
+	xassert(cluster_list);
 
-		temp_list = slurmdb_clusters_get(db_conn, &cluster_cond);
-	} else
-		temp_list = slurmdb_clusters_get(db_conn, NULL);
+	slurmdb_cluster_cond_t cluster_cond;
+	slurmdb_init_cluster_cond(&cluster_cond, 0);
+	cluster_cond.cluster_list = cluster_list;
 
+	temp_list = slurmdb_clusters_get(db_conn, &cluster_cond);
 
 	itr_c = list_iterator_create(cluster_list);
 	itr = list_iterator_create(temp_list);
