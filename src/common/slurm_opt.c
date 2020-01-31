e159c44ff9f80f6f9b05192e0eb686fb39233561
Nate Rini <nate@schedmd.com>
2020-01-30 20:27:13 -0700

    Add arg_set_data_time_limit()

--- src/common/slurm_opt.c
+++ src/common/slurm_opt.c
@@ -4125,12 +4125,38 @@ static int arg_set_time_limit(slurm_opt_t *opt, const char *arg)
 	opt->time_limit = time_limit;
 	return SLURM_SUCCESS;
 }
+static int arg_set_data_time_limit(slurm_opt_t *opt, const data_t *arg,
+			       data_t *errors)
+{
+	int rc;
+	char *str = NULL;
+
+	if (!opt->sbatch_opt && !opt->srun_opt)
+		return SLURM_ERROR;
+
+	if ((rc = data_get_string_converted(arg, &str)))
+		ADD_DATA_ERROR("Unable to read string", rc);
+	else {
+		int time_limit = time_str2mins(str);
+		if (time_limit == NO_VAL) {
+			rc = SLURM_ERROR;
+			ADD_DATA_ERROR("Invalid time specification", rc);
+		} else if (time_limit == 0) {
+			opt->time_limit = INFINITE;
+		} else
+			opt->time_limit = time_limit;
+	}
+
+	xfree(str);
+	return rc;
+}
 COMMON_TIME_DURATION_OPTION_GET_AND_RESET(time_limit);
 static slurm_cli_opt_t slurm_opt_time_limit = {
 	.name = "time",
 	.has_arg = required_argument,
 	.val = 't',
 	.set_func = arg_set_time_limit,
+	.set_func_data = arg_set_data_time_limit,
 	.get_func = arg_get_time_limit,
 	.reset_func = arg_reset_time_limit,
 };
