22f4362d3476b8ad64bb61bbe8cb93d2bd1363ed
Nate Rini <nate@schedmd.com>
2020-02-06 16:04:31 -0700

    Add DEBUG_FLAG_NET

--- src/common/read_config.c
+++ src/common/read_config.c
@@ -5315,6 +5315,11 @@ extern char * debug_flags2str(uint64_t debug_flags)
 			xstrcat(rc, ",");
 		xstrcat(rc, "License");
 	}
+	if (debug_flags & DEBUG_FLAG_NET) {
+		if (rc)
+			xstrcat(rc, ",");
+		xstrcat(rc, "Network");
+	}
 	if (debug_flags & DEBUG_FLAG_NO_CONF_HASH) {
 		if (rc)
 			xstrcat(rc, ",");
@@ -5490,6 +5495,9 @@ extern int debug_str2flags(char *debug_flags, uint64_t *flags_out)
 			(*flags_out) |= DEBUG_FLAG_JOB_CONT;
 		else if (xstrcasecmp(tok, "License") == 0)
 			(*flags_out) |= DEBUG_FLAG_LICENSE;
+		else if (xstrcasecmp(tok, "Network") == 0 ||
+			 xstrcasecmp(tok, "Net") == 0)
+			(*flags_out) |= DEBUG_FLAG_NET;
 		else if (xstrcasecmp(tok, "NO_CONF_HASH") == 0)
 			(*flags_out) |= DEBUG_FLAG_NO_CONF_HASH;
 		else if (xstrcasecmp(tok, "NodeFeatures") == 0)
