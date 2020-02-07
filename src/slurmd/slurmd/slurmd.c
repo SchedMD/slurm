2f0b14c2307a51b34793fec14aa66f3a34e874ec
Michael Hinton <hinton@schedmd.com>
2020-02-06 18:07:20 -0700

    Remove no-op in slurmd -C
    slurmd -C also should not even try to read from slurm.conf, since it
    is meant to help admins build the slurm.conf. See commits f8b2d1fa and
    6aecc72d.
    
    Bug 8464

--- src/slurmd/slurmd/slurmd.c
+++ src/slurmd/slurmd/slurmd.c
@@ -1393,9 +1393,6 @@ _print_config(void)
 	int days, hours, mins, secs;
 	char name[128];
 
-	if (conf->cluster_name)
-		printf("ClusterName=%s ", conf->cluster_name);
-
 	gethostname_short(name, sizeof(name));
 	printf("NodeName=%s ", name);
 
