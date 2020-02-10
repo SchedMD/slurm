SchedMD/slurm

commit 821a261b51b6ac0e732ea50940e42fe998da043b

Fix missing check from conversion of cray -> cray_aries.
The old proctrack/cray plugin was changed to proctrack/cray_aries.

Continuation of c6e6089f0c8

Bug 6824.

src/common/read_config.c
@@ -4529,13 +4529,14 @@ _validate_and_set_defaults(slurm_ctl_conf_t *conf, s_p_hashtbl_t *hashtbl)
 				     "proctrack/linuxproc"))
 				fatal("Invalid combination: PrologFlags=X11 cannot be combined with proctrack/linuxproc");
 			/*
-			 * proctrack/cray or proctrack/cgroup are required for
-			 * pam_slurm_adopt, but don't fatal if using a different
-			 * proctrack plugin.
+			 * proctrack/cray_aries or proctrack/cgroup are
+			 * required for pam_slurm_adopt, but don't fatal if
+			 * using a different proctrack plugin.
 			 */
 			if (xstrcmp(conf->proctrack_type, "proctrack/cgroup") &&
-			    xstrcmp(conf->proctrack_type, "proctrack/cray"))
-				error("WARNING: If using PrologFlags=Contain for pam_slurm_adopt, either proctrack/cgroup or proctrack/cray is required.");
+			    xstrcmp(conf->proctrack_type,
+				    "proctrack/cray_aries"))
+				error("If using PrologFlags=Contain for pam_slurm_adopt, either proctrack/cgroup or proctrack/cray_aries is required.  If not using pam_slurm_adopt, please ignore error.");
 		}
 		if (conf->prolog_flags & PROLOG_FLAG_NOHOLD) {
 			conf->prolog_flags |= PROLOG_FLAG_ALLOC;

src/plugins/proctrack/cray_aries/Makefile.am
@@ -1,4 +1,4 @@
-# Makefile for proctrack/cray plugin
+# Makefile for proctrack/cray_aries plugin
 
 AUTOMAKE_OPTIONS = foreign
 

src/plugins/proctrack/cray_aries/Makefile.in
@@ -14,7 +14,7 @@
 
 @SET_MAKE@
 
-# Makefile for proctrack/cray plugin
+# Makefile for proctrack/cray_aries plugin
 
 VPATH = @srcdir@
 am__is_gnu_make = { \

commit 1d33536d8ed3196a6d877d1b8412fe6b3933c366

Limit PrologFlags=Contain error message to slurmctld.
The error message introduced in 821a261b51 was printing to the stdout
of all client commands, which was quite annoying.

Bug 6824

Co-authored-by: Marshall Garey <marshall@schedmd.com>

src/common/read_config.c
@@ -4495,7 +4495,8 @@ _validate_and_set_defaults(slurm_ctl_conf_t *conf, s_p_hashtbl_t *hashtbl)
 			 * required for pam_slurm_adopt, but don't fatal if
 			 * using a different proctrack plugin.
 			 */
-			if (xstrcmp(conf->proctrack_type, "proctrack/cgroup") &&
+			if (running_in_slurmctld() &&
+			    xstrcmp(conf->proctrack_type, "proctrack/cgroup") &&
 			    xstrcmp(conf->proctrack_type,
 				    "proctrack/cray_aries"))
 				error("If using PrologFlags=Contain for pam_slurm_adopt, either proctrack/cgroup or proctrack/cray_aries is required.  If not using pam_slurm_adopt, please ignore error.");