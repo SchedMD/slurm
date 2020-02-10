SchedMD/slurm

commit 32122eee7e355628919790705b192fa268c174c6

Restore older prolog_slurmctld synchronization behavior.
Without this, warnings of "het_job->nodes == NULL" would occur in the logs
and the associated HetJob environment variables would be skipped.

This was a regression introduced when converting to the PrEp interface as
the hidden synchronization between threads was ensuring all calling paths
into select_nodes() had resolved before the environment was constructed.

Handle this up-front so that all prep plugins will benefit from this
behavior, rather than burying it in the prep/script plugin.

src/slurmctld/job_scheduler.c
@@ -4444,16 +4444,47 @@ static void *_wait_boot(void *arg)
 #endif
 
 /*
+ * Deferring this setup ensures that all calling paths into select_nodes()
+ * have had a chance to update all appropriate job records.
+ * This works since select_nodes() will always be holding the job_write lock,
+ * and thus this new thread will be blocked waiting to acquire job_write
+ * until that has completed.
+ * For HetJobs in particular, this is critical to ensure that all components
+ * have been setup properly before prolog_slurmctld actually runs.
+ *
+ * FIXME: explore other ways to refactor slurmctld to avoid this extraneous
+ * thread
+ */
+static void *_start_prolog_slurmctld_thread(void *x)
+{
+	slurmctld_lock_t node_write_lock = {
+		.conf = READ_LOCK, .job = READ_LOCK,
+		.node = WRITE_LOCK, .fed = READ_LOCK };
+	uint32_t *job_id = (uint32_t *) x;
+	job_record_t *job_ptr;
+
+	lock_slurmctld(node_write_lock);
+	job_ptr = find_job_record(*job_id);
+	prep_prolog_slurmctld(job_ptr);
+	unlock_slurmctld(node_write_lock);
+	xfree(job_id);
+
+	return NULL;
+}
+
+/*
  * prolog_slurmctld - execute the prolog_slurmctld for a job that has just
  *	been allocated resources.
  * IN job_ptr - pointer to job that will be initiated
  */
 extern void prolog_slurmctld(job_record_t *job_ptr)
 {
+	uint32_t *job_id = xmalloc(sizeof(*job_id));
 	xassert(verify_lock(JOB_LOCK, WRITE_LOCK));
 	xassert(verify_lock(NODE_LOCK, WRITE_LOCK));
 
-	prep_prolog_slurmctld(job_ptr);
+	*job_id = job_ptr->job_id;
+	slurm_thread_create_detached(NULL, _start_prolog_slurmctld_thread, job_id);
 }
 
 /* Decrement a job's prolog_running counter and launch the job if zero */

commit 0981a692713d592ecce264706ed068ec21c8d829

Fix race causing batch job launch failures caused by 32122eee7e.
Need to ensure JOB_CONFIGURING is added on whenever prolog_slurmctld()
is called. Since the prep_prolog_slurmctld() was moved to a separate
thread this wasn't happening early enough, and the batch job would
launch earlier than expected and die due to a prolog launch failure.

src/common/prep.c
@@ -250,12 +250,6 @@ extern void prep_prolog_slurmctld(job_record_t *job_ptr)
 		if (async)
 			job_ptr->prep_prolog_cnt++;
 	}
-
-	if (job_ptr->prep_prolog_cnt && job_ptr->details) {
-		job_ptr->details->prolog_running++;
-		job_ptr->job_state |= JOB_CONFIGURING;
-	}
-
 	slurm_mutex_unlock(&g_context_lock);
 	END_TIMER2(__func__);
 }

src/slurmctld/job_scheduler.c
@@ -4458,7 +4458,7 @@ static void *_wait_boot(void *arg)
 static void *_start_prolog_slurmctld_thread(void *x)
 {
 	slurmctld_lock_t node_write_lock = {
-		.conf = READ_LOCK, .job = READ_LOCK,
+		.conf = READ_LOCK, .job = WRITE_LOCK,
 		.node = WRITE_LOCK, .fed = READ_LOCK };
 	uint32_t *job_id = (uint32_t *) x;
 	job_record_t *job_ptr;
@@ -4466,6 +4466,14 @@ static void *_start_prolog_slurmctld_thread(void *x)
 	lock_slurmctld(node_write_lock);
 	job_ptr = find_job_record(*job_id);
 	prep_prolog_slurmctld(job_ptr);
+
+	/*
+	 * No async prolog_slurmctld threads running, so decrement now to move
+	 * on with the job launch.
+	 */
+	if (!job_ptr->prep_prolog_cnt)
+		prolog_running_decr(job_ptr);
+
 	unlock_slurmctld(node_write_lock);
 	xfree(job_id);
 
@@ -4482,6 +4490,9 @@ extern void prolog_slurmctld(job_record_t *job_ptr)
 	uint32_t *job_id = xmalloc(sizeof(*job_id));
 	xassert(verify_lock(JOB_LOCK, WRITE_LOCK));
 	xassert(verify_lock(NODE_LOCK, WRITE_LOCK));
+
+	job_ptr->details->prolog_running++;
+	job_ptr->job_state |= JOB_CONFIGURING;
 
 	*job_id = job_ptr->job_id;
 	slurm_thread_create_detached(NULL, _start_prolog_slurmctld_thread, job_id);