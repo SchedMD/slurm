SchedMD/slurm

commit e9bf8faf7948b54f2ebe0b904f67cf1201be5183

Remove acctng_store_job_comment variable from slurm_ctl_conf_t
This is now part of the conf_flags.

Bug 7496

src/common/read_config.c
@@ -4047,10 +4047,8 @@ _validate_and_set_defaults(slurm_ctl_conf_t *conf, s_p_hashtbl_t *hashtbl)
 				xstrdup(default_storage_pass);
 	}
 	if (s_p_get_boolean(&truth, "AccountingStoreJobComment", hashtbl)
-	    && !truth)
-		conf->acctng_store_job_comment = 0;
-	else
-		conf->acctng_store_job_comment = 1;
+	    && truth)
+		conf->conf_flags |= CTL_CONF_SJC;
 
 	if (!s_p_get_uint32(&conf->accounting_storage_port,
 			    "AccountingStoragePort", hashtbl)) {

contribs/perlapi/libslurm/perl/conf.c
@@ -32,7 +32,6 @@ slurm_ctl_conf_to_hv(slurm_ctl_conf_t *conf, HV *hv)
 	STORE_FIELD(hv, conf, acct_gather_node_freq, uint16_t);
 	if (conf->acct_gather_profile_type)
 		STORE_FIELD(hv, conf, acct_gather_profile_type, charp);
-	STORE_FIELD(hv, conf, acctng_store_job_comment, uint16_t);
 
 	if (conf->accounting_storage_backup_host)
 		STORE_FIELD(hv, conf, accounting_storage_backup_host, charp);
@@ -422,7 +421,6 @@ hv_to_slurm_ctl_conf(HV *hv, slurm_ctl_conf_t *conf)
 	FETCH_FIELD(hv, conf, acct_gather_interconnect_type, charp, FALSE);
 	FETCH_FIELD(hv, conf, acct_gather_node_freq, uint16_t, FALSE);
 	FETCH_FIELD(hv, conf, acct_gather_profile_type, charp, FALSE);
-	FETCH_FIELD(hv, conf, acctng_store_job_comment, uint16_t, FALSE);
 	FETCH_FIELD(hv, conf, accounting_storage_enforce, uint16_t, TRUE);
 	FETCH_FIELD(hv, conf, accounting_storage_backup_host, charp, FALSE);
 	FETCH_FIELD(hv, conf, accounting_storage_host, charp, FALSE);

slurm/slurm.h.in
@@ -2613,6 +2613,7 @@ typedef struct reservation_name_msg {
 #define PROLOG_FLAG_X11		0x0010 /* enable slurm x11 forwarding support */
 
 #define CTL_CONF_OR             0x00000001 /*SlurmdParameters=config_overrides*/
+#define CTL_CONF_SJC            0x00000002 /* AccountingStoreJobComment */
 
 #define LOG_FMT_ISO8601_MS      0
 #define LOG_FMT_ISO8601         1
@@ -2637,7 +2638,6 @@ typedef struct slurm_ctl_conf {
 	uint32_t accounting_storage_port;/* node accountinging storage port */
 	char *accounting_storage_type; /* accounting storage type */
 	char *accounting_storage_user; /* accounting storage user */
-	uint16_t acctng_store_job_comment; /* send job comment to accounting */
 	void *acct_gather_conf; /* account gather config */
 	char *acct_gather_energy_type; /* energy accounting type */
 	char *acct_gather_profile_type; /* profile accounting type */

src/api/config_info.c
@@ -551,7 +551,7 @@ extern void *slurm_ctl_conf_2_key_pairs (slurm_ctl_conf_t* slurm_ctl_conf_ptr)
 	key_pair = xmalloc(sizeof(config_key_pair_t));
 	key_pair->name = xstrdup("AccountingStoreJobComment");
 	key_pair->value = xstrdup(
-		slurm_ctl_conf_ptr->acctng_store_job_comment ? "Yes" : "No");
+		(slurm_ctl_conf_ptr->conf_flags & CTL_CONF_SJC) ? "Yes" : "No");
 	list_append(ret_list, key_pair);
 
 	key_pair = xmalloc(sizeof(config_key_pair_t));

src/common/slurm_protocol_pack.c
@@ -5682,7 +5682,6 @@ _pack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t * build_ptr, Buf buffer,
 		packstr(build_ptr->accounting_storage_tres, buffer);
 		packstr(build_ptr->accounting_storage_type, buffer);
 		packstr(build_ptr->accounting_storage_user, buffer);
-		pack16(build_ptr->acctng_store_job_comment, buffer);
 
 		if (build_ptr->acct_gather_conf)
 			count = list_count(build_ptr->acct_gather_conf);
@@ -5980,7 +5979,7 @@ _pack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t * build_ptr, Buf buffer,
 		packstr(build_ptr->accounting_storage_tres, buffer);
 		packstr(build_ptr->accounting_storage_type, buffer);
 		packstr(build_ptr->accounting_storage_user, buffer);
-		pack16(build_ptr->acctng_store_job_comment, buffer);
+		pack16((build_ptr->conf_flags & CTL_CONF_SJC) ? 1 : 0, buffer);
 
 		if (build_ptr->acct_gather_conf)
 			count = list_count(build_ptr->acct_gather_conf);
@@ -6276,7 +6275,7 @@ _pack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t * build_ptr, Buf buffer,
 		packstr(build_ptr->accounting_storage_tres, buffer);
 		packstr(build_ptr->accounting_storage_type, buffer);
 		packstr(build_ptr->accounting_storage_user, buffer);
-		pack16(build_ptr->acctng_store_job_comment, buffer);
+		pack16((build_ptr->conf_flags & CTL_CONF_SJC) ? 1 : 0, buffer);
 
 		if (build_ptr->acct_gather_conf)
 			count = list_count(build_ptr->acct_gather_conf);
@@ -6599,7 +6598,6 @@ _unpack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t **build_buffer_ptr,
 				       &uint32_tmp, buffer);
 		safe_unpackstr_xmalloc(&build_ptr->accounting_storage_user,
 				       &uint32_tmp, buffer);
-		safe_unpack16(&build_ptr->acctng_store_job_comment, buffer);
 
 		if (unpack_key_pair_list(&build_ptr->acct_gather_conf,
 					 protocol_version, buffer)
@@ -7000,7 +6998,9 @@ _unpack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t **build_buffer_ptr,
 				       &uint32_tmp, buffer);
 		safe_unpackstr_xmalloc(&build_ptr->accounting_storage_user,
 				       &uint32_tmp, buffer);
-		safe_unpack16(&build_ptr->acctng_store_job_comment, buffer);
+		safe_unpack16(&uint16_tmp, buffer);
+		if (uint16_tmp == 1)
+			build_ptr->conf_flags |= CTL_CONF_SJC;
 
 		if (unpack_key_pair_list(&build_ptr->acct_gather_conf,
 					 protocol_version, buffer)
@@ -7400,7 +7400,9 @@ _unpack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t **build_buffer_ptr,
 				       &uint32_tmp, buffer);
 		safe_unpackstr_xmalloc(&build_ptr->accounting_storage_user,
 				       &uint32_tmp, buffer);
-		safe_unpack16(&build_ptr->acctng_store_job_comment, buffer);
+		safe_unpack16(&uint16_tmp, buffer);
+		if (uint16_tmp == 1)
+			build_ptr->conf_flags |= CTL_CONF_SJC;
 
 		if (unpack_key_pair_list(&build_ptr->acct_gather_conf,
 					 protocol_version, buffer)

src/plugins/accounting_storage/slurmdbd/accounting_storage_slurmdbd.c
@@ -2633,7 +2633,7 @@ extern int jobacct_storage_p_job_complete(void *db_conn,
 
 	req.admin_comment = job_ptr->admin_comment;
 
-	if (slurmctld_conf.acctng_store_job_comment)
+	if (slurmctld_conf.conf_flags & CTL_CONF_SJC)
 		req.comment = job_ptr->comment;
 
 	req.db_index    = job_ptr->db_index;

src/slurmctld/proc_req.c
@@ -708,7 +708,6 @@ static void _fill_ctld_conf(slurm_ctl_conf_t * conf_ptr)
 		xstrdup(conf->accounting_storage_type);
 	conf_ptr->accounting_storage_user =
 		xstrdup(conf->accounting_storage_user);
-	conf_ptr->acctng_store_job_comment = conf->acctng_store_job_comment;
 
 	conf_ptr->acct_gather_conf = acct_gather_conf_values();
 	conf_ptr->acct_gather_energy_type =

commit f2a5905b72095915107bd33f4a0a44f885da456c

Fix regression where AccountingStoreJobComment was not defaulting to yes
Regression was introduced in commit e9bf8faf7948.

Bug 8694

src/common/read_config.c
@@ -4174,8 +4174,8 @@ _validate_and_set_defaults(slurm_ctl_conf_t *conf, s_p_hashtbl_t *hashtbl)
 			conf->accounting_storage_pass =
 				xstrdup(default_storage_pass);
 	}
-	if (s_p_get_boolean(&truth, "AccountingStoreJobComment", hashtbl)
-	    && truth)
+	if (!s_p_get_boolean(&truth, "AccountingStoreJobComment", hashtbl)
+	    || truth)
 		conf->conf_flags |= CTL_CONF_SJC;
 
 	if (!s_p_get_uint32(&conf->accounting_storage_port,