d6c11f59b56cdd723ce8f44f65e2b5a0a516f422
Dominik Bartkiewicz <bart@schedmd.com>
2020-04-06 18:15:22 +0200

    Print note when reservation can't be created
    Bug 8508

--- src/scontrol/create_res.c
+++ src/scontrol/create_res.c
@@ -493,6 +493,10 @@ scontrol_create_res(int argc, char **argv)
 	if (!new_res_name) {
 		exit_code = 1;
 		slurm_perror("Error creating the reservation");
+		if ((errno == ESLURM_REQUESTED_NODE_CONFIG_UNAVAILABLE) ||
+		    (errno == ESLURM_NODES_BUSY))
+			printf("Note, unless nodes are directly requested a reservation must exist in a single partition.\n"
+			       "If no partition is requested the default partition is assumed.\n");
 		ret = slurm_get_errno();
 	} else {
 		printf("Reservation created: %s\n", new_res_name);
