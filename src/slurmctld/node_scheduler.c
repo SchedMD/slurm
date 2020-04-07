SchedMD/slurm

commit 9d02ed357d85e3dd56f74198407ae1005804ad4f

Mark valid_feature_counts() as void since there are no error paths.
No functional change.

Bug 8508

src/slurmctld/node_scheduler.c
@@ -3425,9 +3425,8 @@ extern int list_find_feature(void *feature_entry, void *key)
  *	active, otherwise use available features
  * IN/OUT node_bitmap - nodes available for use, clear if unusable
  * OUT has_xor - set if XOR/XAND found in feature expression
- * RET true if valid, false otherwise
  */
-extern bool valid_feature_counts(job_record_t *job_ptr, bool use_active,
+extern void valid_feature_counts(job_record_t *job_ptr, bool use_active,
 				 bitstr_t *node_bitmap, bool *has_xor)
 {
 	struct job_details *detail_ptr = job_ptr->details;
@@ -3437,7 +3436,7 @@ extern bool valid_feature_counts(job_record_t *job_ptr, bool use_active,
 	int last_paren_cnt = 0;
 	bitstr_t *feature_bitmap, *paren_bitmap = NULL;
 	bitstr_t *tmp_bitmap, *work_bitmap;
-	bool have_count = false, rc = true, user_update;
+	bool have_count = false, user_update;
 
 	xassert(detail_ptr);
 	xassert(node_bitmap);
@@ -3445,7 +3444,7 @@ extern bool valid_feature_counts(job_record_t *job_ptr, bool use_active,
 
 	*has_xor = false;
 	if (detail_ptr->feature_list == NULL)	/* no constraints */
-		return rc;
+		return;
 
 	user_update = node_features_g_user_update(job_ptr->user_id);
 	find_feature_nodes(detail_ptr->feature_list, user_update);
@@ -3543,7 +3542,7 @@ extern bool valid_feature_counts(job_record_t *job_ptr, bool use_active,
 	xfree(tmp);
 }
 #endif
-	return rc;
+	return;
 }
 
 /*
@@ -3602,8 +3601,7 @@ extern int job_req_node_filter(job_record_t *job_ptr,
 		}
 	}
 
-	if (!valid_feature_counts(job_ptr, false, avail_bitmap, &has_xor))
-		return EINVAL;
+	valid_feature_counts(job_ptr, false, avail_bitmap, &has_xor);
 
 	return SLURM_SUCCESS;
 }
@@ -3744,17 +3742,7 @@ static int _build_node_list(job_record_t *job_ptr,
 		bit_nset(usable_node_mask, 0, (node_record_count - 1));
 	}
 
-	if (!valid_feature_counts(job_ptr, false, usable_node_mask, &has_xor)) {
-		info("%pJ feature requirements can not be satisfied",
-		     job_ptr);
-		FREE_NULL_BITMAP(usable_node_mask);
-		if (err_msg) {
-			xfree(*err_msg);
-			*err_msg = xstrdup("Node feature requirements can not "
-					   "be satisfied");
-		}
-		return ESLURM_REQUESTED_NODE_CONFIG_UNAVAILABLE;
-	}
+	valid_feature_counts(job_ptr, false, usable_node_mask, &has_xor);
 
 	if (can_reboot)
 		reboot_bitmap = bit_alloc(node_record_count);

src/slurmctld/node_scheduler.h
@@ -172,9 +172,8 @@ extern void launch_prolog(job_record_t *job_ptr);
  *	active, otherwise use available features
  * IN/OUT node_bitmap - nodes available for use, clear if unusable
  * OUT has_xor - set if XOR/XAND found in feature expression
- * RET true if valid, false otherwise
  */
-extern bool valid_feature_counts(job_record_t *job_ptr, bool use_active,
+extern void valid_feature_counts(job_record_t *job_ptr, bool use_active,
 				 bitstr_t *node_bitmap, bool *has_xor);
 
 #endif /* !_HAVE_NODE_SCHEDULER_H */

src/slurmctld/reservation.c
@@ -3964,15 +3964,12 @@ static int _select_nodes(resv_desc_msg_t *resv_desc_ptr,
 			 */
 			bitstr_t *tmp_bitmap;
 			tmp_bitmap = bit_copy(node_bitmap);
-			if (!valid_feature_counts(job_ptr, true, node_bitmap,
-						  &dummy)) {
-				rc = ESLURM_INVALID_FEATURE;
-			} else if (bit_set_count(node_bitmap) < total_node_cnt){
+			valid_feature_counts(job_ptr, true,
+					     node_bitmap, &dummy);
+			if (bit_set_count(node_bitmap) < total_node_cnt) {
 				bit_or(node_bitmap, tmp_bitmap);
-				if (!valid_feature_counts(job_ptr, false,
-							  node_bitmap, &dummy)){
-					rc = ESLURM_INVALID_FEATURE;
-				}
+				valid_feature_counts(job_ptr, false,
+						     node_bitmap, &dummy);
 			}
 			FREE_NULL_BITMAP(tmp_bitmap);
 

commit 331a8e7967bddfe69630cf0e9ee27280e1fca72c

Fix _DEBUG output in valid_feature_counts()
Regression introduced in 9d02ed357d85e3

Bug 8508

src/slurmctld/node_scheduler.c
@@ -3538,7 +3538,7 @@ extern void valid_feature_counts(job_record_t *job_ptr, bool use_active,
 {
 	char * tmp;
 	tmp = bitmap2node_name(node_bitmap);
-	info("%s: RC:%d NODES:%s HAS_XOR:%d", __func__, rc, tmp, *has_xor);
+	info("%s: NODES:%s HAS_XOR:%d", __func__, tmp, *has_xor);
 	xfree(tmp);
 }
 #endif