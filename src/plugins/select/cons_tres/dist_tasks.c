2db5ded74734ab304dd9b6dfec36a96babd99f6d
Marcin Stolarek <cinek@schedmd.com>
2020-04-16 09:56:03 +0000

    Break infinite loop in cons_tres - fix of 6dcb598c6b9
    Revert the changes from 6dcb598c6b9 and break appropriate loop.
    
    Bug 8818.
    
    Co-authored-by: Danny Auble <da@schedmd.com>

--- src/plugins/select/cons_tres/dist_tasks.c
+++ src/plugins/select/cons_tres/dist_tasks.c
@@ -757,16 +757,18 @@ static int _compute_c_b_task_dist(struct job_record *job_ptr,
 			rem_tasks = vpus[n] / job_ptr->details->cpus_per_task;
 			rem_tasks = MAX(rem_tasks, 1);
 			for (t = 0; ((t < rem_tasks) && (tid < maxtasks)); t++){
-				if (!over_subscribe &&
-				    ((avail_cpus[n] - job_res->cpus[n]) <
-				     job_ptr->details->cpus_per_task))
-					break;
-				if (!_tres_tasks_avail(gres_task_limit,
-						       job_res, n))
-					break;
-				if (_at_tpn_limit(n, job_ptr, "fill additional",
-						  false))
-					break;
+				if (!over_subscribe) {
+					if ((avail_cpus[n] - job_res->cpus[n]) <
+					    job_ptr->details->cpus_per_task)
+						break;
+					if (!_tres_tasks_avail(gres_task_limit,
+							       job_res, n))
+						break;
+					if (_at_tpn_limit(n, job_ptr,
+							  "fill additional",
+							  false))
+						break;
+				}
 				tid++;
 				job_res->tasks_per_node[n]++;
 				for (l = 0; l < job_ptr->details->cpus_per_task;
@@ -794,12 +796,15 @@ static int _compute_c_b_task_dist(struct job_record *job_ptr,
 	while (tid < maxtasks) {
 		bool more_tres_tasks = false;
 		for (n = 0; ((n < job_res->nhosts) && (tid < maxtasks)); n++) {
-			if (test_tres_tasks &&
-			    !_tres_tasks_avail(gres_task_limit, job_res, n))
-				continue;
-			if (_at_tpn_limit(n, job_ptr, "fill non-dedicated CPUs",
-					  true))
-				continue;
+			if (test_tres_tasks) {
+				if (!_tres_tasks_avail(gres_task_limit,
+						       job_res, n))
+					continue;
+				if (_at_tpn_limit(n, job_ptr,
+						  "fill non-dedicated CPUs",
+						  true))
+					continue;
+			}
 			more_tres_tasks = true;
 			tid++;
 			job_res->tasks_per_node[n]++;
