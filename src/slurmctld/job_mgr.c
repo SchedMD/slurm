11933176720768579f52a5a53d5a7be2704597b2
Marshall Garey <marshall@schedmd.com>
2020-04-16 15:00:16 -0600

    Always pack state_reason, not state_reason_prev.
    This is a partial revert of commit 74184e4db3b. From that commit message:
    
    "At the start of a scheduling cycle, the job's "reason" field can
    be cleared. If the scheduler fails to reach that job and set its
    value to a new reason, the original reason was lost and the state
    reports would report NoReason."
    
    This isn't true anymore so we revert that change (packing the previous
    state_reason) and just pack the job's state_reason.
    
    Bug 8848

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -9957,12 +9957,7 @@ void pack_job(job_record_t *dump_job_ptr, uint16_t show_flags, Buf buffer,
 
 		pack32(dump_job_ptr->job_state,    buffer);
 		pack16(dump_job_ptr->batch_flag,   buffer);
-		if ((dump_job_ptr->state_reason == WAIT_NO_REASON) &&
-		    IS_JOB_PENDING(dump_job_ptr)) {
-			/* Scheduling cycle in progress, send latest reason */
-			pack16(dump_job_ptr->state_reason_prev, buffer);
-		} else
-			pack16(dump_job_ptr->state_reason, buffer);
+		pack16(dump_job_ptr->state_reason, buffer);
 		pack8(dump_job_ptr->power_flags,   buffer);
 		pack8(dump_job_ptr->reboot,        buffer);
 		pack16(dump_job_ptr->restart_cnt,  buffer);
@@ -10191,12 +10186,7 @@ void pack_job(job_record_t *dump_job_ptr, uint16_t show_flags, Buf buffer,
 
 		pack32(dump_job_ptr->job_state,    buffer);
 		pack16(dump_job_ptr->batch_flag,   buffer);
-		if ((dump_job_ptr->state_reason == WAIT_NO_REASON) &&
-		    IS_JOB_PENDING(dump_job_ptr)) {
-			/* Scheduling cycle in progress, send latest reason */
-			pack16(dump_job_ptr->state_reason_prev, buffer);
-		} else
-			pack16(dump_job_ptr->state_reason, buffer);
+		pack16(dump_job_ptr->state_reason, buffer);
 		pack8(dump_job_ptr->power_flags,   buffer);
 		pack8(dump_job_ptr->reboot,        buffer);
 		pack16(dump_job_ptr->restart_cnt,  buffer);
@@ -10411,12 +10401,7 @@ void pack_job(job_record_t *dump_job_ptr, uint16_t show_flags, Buf buffer,
 
 		pack32(dump_job_ptr->job_state,    buffer);
 		pack16(dump_job_ptr->batch_flag,   buffer);
-		if ((dump_job_ptr->state_reason == WAIT_NO_REASON) &&
-		    IS_JOB_PENDING(dump_job_ptr)) {
-			/* Scheduling cycle in progress, send latest reason */
-			pack16(dump_job_ptr->state_reason_prev, buffer);
-		} else
-			pack16(dump_job_ptr->state_reason, buffer);
+		pack16(dump_job_ptr->state_reason, buffer);
 		pack8(dump_job_ptr->power_flags,   buffer);
 		pack8(dump_job_ptr->reboot,        buffer);
 		pack16(dump_job_ptr->restart_cnt,  buffer);
