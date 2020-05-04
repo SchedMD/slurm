20937db10e157739f888fa86b7fdb28cdee2cada
Danny Auble <da@schedmd.com>
2020-05-04 12:23:29 -0600

    Fix calling stat on a NULL variable.
    Continuation of abd4db776c7
    
    Signed-off-by: Tim Wickberg <tim@schedmd.com>

--- src/slurmctld/read_config.c
+++ src/slurmctld/read_config.c
@@ -182,7 +182,8 @@ static void _stat_slurm_dirs(void)
 	 * _is_valid_path() instead
 	 */
 
-	if ((stat(slurmctld_conf.plugstack, &stat_buf) == 0) &&
+	if (slurmctld_conf.plugstack &&
+	    (stat(slurmctld_conf.plugstack, &stat_buf) == 0) &&
 	    (stat_buf.st_mode & S_IWOTH)) {
 		problem_dir = "PlugStack";
 	}
