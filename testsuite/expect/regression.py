98f98986bcb13380e00fbc3eee5e2547b60a4604
Scott Jackson <scottmo@schedmd.com>
2020-05-07 13:49:11 -0600

    Testsuite - New skipped test status and assert routines
    Introduction of new SKIPPED test status with associated print_skipped
    routine. regression.py modified to tally skipped tests. Introduction of
    testsuite_log_level logging threshold and suppression of messages in log_*
    lower than the configured threshold. Addition of log_{fatal,debug,trace}
    logging statements. exit_with_failure renamed to fail. exit_with_warning
    renamed to skip. exit_with_success renamed to pass. Addition of assert
    and assert_or_* helper routines.
    
    Bug 6597

--- testsuite/expect/regression.py
+++ testsuite/expect/regression.py
@@ -45,6 +45,7 @@ def main(argv=None):
     tests = []
     failed_tests = []
     passed_tests = []
+    skipped_tests = []
     begin = (1,1)
 
     # Handle command line parameters
@@ -145,6 +146,9 @@ def main(argv=None):
                 except IOError as e:
                     print('ERROR failed to close %s %s' % (testlog_name, e),
                             file=sys.stederr);
+        elif retcode > 127:
+            skipped_tests.append(test)
+            sys.stdout.write('SKIPPED\n')
         else:
             failed_tests.append(test)
             os.rename(testlog_name, testlog_name+'.failed')
@@ -159,6 +163,7 @@ def main(argv=None):
           %((end_time-start_time)/60,(end_time-start_time)%60), file=sys.stdout)
     print('Completions  :', len(passed_tests), file=sys.stdout)
     print('Failures     :', len(failed_tests), file=sys.stdout)
+    print('Skipped      :', len(skipped_tests), file=sys.stdout)
     if len(failed_tests) > 0:
         print('Failed tests : ', file=sys.stdout)
         first = True
