51051d54df4a231bc4aa1e53f44f02e1507b2f14
Danny Auble <da@schedmd.com>
2020-06-04 14:27:58 -0600

    Make a _unpack_step_id_members function
    Bug 7593

--- src/common/slurm_protocol_pack.c
+++ src/common/slurm_protocol_pack.c
@@ -115,6 +115,12 @@ static int _unpack_ret_list(List *ret_list, uint16_t size_val, Buf buffer,
 
 static void _priority_factors_resp_list_del(void *x);
 
+static void _pack_step_id(slurm_step_id_t *msg, Buf buffer,
+			  uint16_t protocol_version);
+static int _unpack_step_id_members(slurm_step_id_t *msg, Buf buffer,
+				   uint16_t protocol_version);
+static int _unpack_step_id(slurm_step_id_t **msg_ptr, Buf buffer,
+			   uint16_t protocol_version);
 
 /* pack_header
  * packs a slurm protocol header that precedes every slurm message
@@ -8625,13 +8631,10 @@ static void _pack_step_id(slurm_step_id_t *msg, Buf buffer,
 		      __func__, protocol_version);
 }
 
-static int _unpack_step_id(slurm_step_id_t **msg_ptr, Buf buffer,
-			   uint16_t protocol_version)
+static int _unpack_step_id_members(slurm_step_id_t *msg, Buf buffer,
+				   uint16_t protocol_version)
 {
-	slurm_step_id_t *msg;
-
-	msg = xmalloc(sizeof(*msg));
-	*msg_ptr = msg;
+	xassert(msg);
 
 	if (protocol_version >= SLURM_20_11_PROTOCOL_VERSION) {
 		safe_unpack32(&msg->job_id, buffer);
@@ -8648,6 +8651,22 @@ static int _unpack_step_id(slurm_step_id_t **msg_ptr, Buf buffer,
 	return SLURM_SUCCESS;
 
 unpack_error:
+	return SLURM_ERROR;
+}
+
+
+static int _unpack_step_id(slurm_step_id_t **msg_ptr, Buf buffer,
+			   uint16_t protocol_version)
+{
+	slurm_step_id_t *msg;
+
+	msg = xmalloc(sizeof(*msg));
+	*msg_ptr = msg;
+
+	if (_unpack_step_id_members(msg, buffer, protocol_version) ==
+	    SLURM_SUCCESS)
+		return SLURM_SUCCESS;
+
 	slurm_free_step_id(msg);
 	*msg_ptr = NULL;
 	return SLURM_ERROR;
