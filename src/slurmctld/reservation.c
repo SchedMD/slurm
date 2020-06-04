52b01a015b3a2fa97b1594e296f353eecb76dfd0
Nate Rini <nate@schedmd.com>
2020-06-04 14:47:40 -0600

    Log when reservation asks for all nodes
    Bug 7366.

--- src/slurmctld/reservation.c
+++ src/slurmctld/reservation.c
@@ -4291,12 +4291,16 @@ static bitstr_t *_pick_node_cnt(bitstr_t *avail_bitmap,
 		return NULL;
 	} else if ((total_node_cnt == node_cnt) &&
 		   (resv_desc_ptr->flags & RESERVE_FLAG_IGN_JOBS)) {
+		log_flag(RESERVATION, "%s: reservation %s requests all %d nodes",
+			__func__, resv_desc_ptr->name, total_node_cnt);
 		return select_g_resv_test(resv_desc_ptr, node_cnt,
 					  avail_bitmap, core_bitmap);
 	} else if ((node_cnt == 0) &&
 		   ((resv_desc_ptr->core_cnt == NULL) ||
 		    (resv_desc_ptr->core_cnt[0] == 0)) &&
 		   (resv_desc_ptr->flags & RESERVE_FLAG_ANY_NODES)) {
+		log_flag(RESERVATION, "%s: reservation %s requests any of all %d nodes",
+			__func__, resv_desc_ptr->name, total_node_cnt);
 		return bit_alloc(bit_size(avail_bitmap));
 	}
 
