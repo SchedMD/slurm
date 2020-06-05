SchedMD/slurm

commit ed638974bfb41fa9ac9e2780bf411b20229abafe

Use reliable communication with srun in _send_srun_resp_msg().
Do not use slurm_send_only_node_msg().

There is no way to tell if the srun has received the message before
the socket is shutdown if we do not wait to receive data. Use
slurm_send_recv_rc_msg_only_one() instead, and send back a response
from the other side.

We still need the older (and unreliable) behavior when talking to older
srun client commands, so make this change dependent on the protocol_version
field in the message.

Bug 5164.

src/slurmd/slurmstepd/mgr.c
@@ -233,9 +233,22 @@ _send_srun_resp_msg(slurm_msg_t *resp_msg, uint32_t nnodes)
 	 * it is resumed */
 	wait_for_resumed(resp_msg->msg_type);
 	while (1) {
-		rc = slurm_send_only_node_msg(resp_msg);
-		if (rc == SLURM_SUCCESS)
-			break;
+		if (resp_msg->protocol_version >= SLURM_18_08_PROTOCOL_VERSION) {
+			int msg_rc = 0;
+			msg_rc = slurm_send_recv_rc_msg_only_one(resp_msg,
+								 &rc, 0);
+			/* Both must be zero for a successful transmission. */
+			if (!msg_rc && !rc)
+				break;
+		} else {
+			/*
+			 * Old unreliable method. See slurm_send_only_node_msg()
+			 * for further details.
+			 */
+			rc = slurm_send_only_node_msg(resp_msg);
+			if (rc == SLURM_SUCCESS)
+				break;
+		}
 
 		if (!max_retry)
 			max_retry = (nnodes / 1024) + 5;

src/api/step_launch.c
@@ -1577,10 +1577,12 @@ _handle_msg(void *arg, slurm_msg_t *msg)
 	case RESPONSE_LAUNCH_TASKS:
 		debug2("received task launch");
 		_launch_handler(sls, msg);
+		slurm_send_rc_msg(msg, SLURM_SUCCESS);
 		break;
 	case MESSAGE_TASK_EXIT:
 		debug2("received task exit");
 		_exit_handler(sls, msg);
+		slurm_send_rc_msg(msg, SLURM_SUCCESS);
 		break;
 	case SRUN_PING:
 		debug3("slurmctld ping received");

commit 18ab118ea0d7a5a25c110f472cb0d9d7375809e3

Ensure _send_srun_resp_msg() returns an error on timeout.
rc is only set if msg_rc != 0, so it'd be returned uninitialized here.
(Which is likely zero, which is SLURM_SUCCESS, which is problematic.)

Regression from ed638974bf.

Bug 9156.

src/slurmd/slurmstepd/mgr.c
@@ -228,7 +228,7 @@ mgr_launch_tasks_setup(launch_tasks_request_msg_t *msg, slurm_addr_t *cli,
 inline static int
 _send_srun_resp_msg(slurm_msg_t *resp_msg, uint32_t nnodes)
 {
-	int rc, retry = 0, max_retry = 0;
+	int rc = SLURM_ERROR, retry = 0, max_retry = 0;
 	unsigned long delay = 100000;
 
 	/* NOTE: Wait until suspended job step is resumed or the RPC