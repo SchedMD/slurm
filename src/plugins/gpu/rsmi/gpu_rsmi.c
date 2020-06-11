c2dd3937d8559b327ee91132723bb88349534fe8
Danny Auble <da@schedmd.com>
2020-06-11 11:16:16 -0600

    Follow on to 270c0f3d59b4409c04d
    Signed-off-by: Alejandro Sanchez <alex@schedmd.com>

--- src/plugins/gpu/rsmi/gpu_rsmi.c
+++ src/plugins/gpu/rsmi/gpu_rsmi.c
@@ -1107,7 +1107,7 @@ static List _get_system_gpu_list_rsmi(node_config_load_t *node_config)
 		_rsmi_print_freqs(i, LOG_LEVEL_DEBUG2);
 
 		add_gres_to_list(gres_list_system, "gpu", 1,
-				 node_config->cpu_cnt, NULL,
+				 node_config->cpu_cnt, NULL, NULL,
 				 device_file, device_brand, NULL);
 
 		xfree(device_file);
