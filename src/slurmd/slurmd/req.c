3dddfdd913ab85799df8b50165249705bca57a29
Tim Wickberg <tim@schedmd.com>
2020-06-17 19:33:12 -0600

    Remove message aggregation code from _epilog_complete().

--- src/slurmd/slurmd/req.c
+++ src/slurmd/slurmd/req.c
@@ -4708,41 +4708,25 @@ static void _epilog_complete_msg_setup(
  *   Returns SLURM_SUCCESS if message sent successfully,
  *           SLURM_ERROR if epilog complete message fails to be sent.
  */
-static int
-_epilog_complete(uint32_t jobid, int rc)
+static int _epilog_complete(uint32_t jobid, int rc)
 {
-	int ret = SLURM_SUCCESS;
+	slurm_msg_t msg;
+	epilog_complete_msg_t req;
 
-	if (conf->msg_aggr_window_msgs > 1) {
-		/* message aggregation is enabled */
-		slurm_msg_t *msg = xmalloc(sizeof(slurm_msg_t));
-		epilog_complete_msg_t *req =
-			xmalloc(sizeof(epilog_complete_msg_t));
+	_epilog_complete_msg_setup(&msg, &req, jobid, rc);
 
-		_epilog_complete_msg_setup(msg, req, jobid, rc);
-
-		/* we need to copy this symbol */
-		req->node_name   = xstrdup(conf->node_name);
-
-		msg_aggr_add_msg(msg, 0, NULL);
-	} else {
-		slurm_msg_t msg;
-		epilog_complete_msg_t req;
-
-		_epilog_complete_msg_setup(&msg, &req, jobid, rc);
-
-		/* Note: No return code to message, slurmctld will resend
-		 * TERMINATE_JOB request if message send fails */
-		if (slurm_send_only_controller_msg(&msg, working_cluster_rec)
-		    < 0) {
-			error("Unable to send epilog complete message: %m");
-			ret = SLURM_ERROR;
-		} else {
-			debug("Job %u: sent epilog complete msg: rc = %d",
-			      jobid, rc);
-		}
+	/*
+	 * Note: No return code from message, slurmctld will resend
+	 * TERMINATE_JOB request if message send fails.
+	 */
+	if (slurm_send_only_controller_msg(&msg, working_cluster_rec) < 0) {
+		error("Unable to send epilog complete message: %m");
+		return SLURM_ERROR;
 	}
-	return ret;
+	
+	debug("JobId=%u: sent epilog complete msg: rc = %d", jobid, rc);
+
+	return SLURM_SUCCESS;
 }
 
 /* if a lock is granted to the job then return 1; else return 0 if
