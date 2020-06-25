SchedMD/slurm

commit 21d851d2f52403f47d90bf4085b7aaa6d2507630

Add in missing length checks before creating any Unix domain sockets.
If a Unix socket path exceeds the limit, emit errors and exit.
Fix memory leak on socket() failure.

Bug 5386

src/plugins/mpi/pmi2/setup.c
@@ -301,16 +301,27 @@ _setup_stepd_sockets(const stepd_step_rec_t *job, char ***env)
 	/*
 	 * Make sure we adjust for the spool dir coming in on the address to
 	 * point to the right spot.
-	 */
-	xstrsubstitute(spool, "%n", job->node_name);
-	xstrsubstitute(spool, "%h", job->node_name);
-	snprintf(sa.sun_path, sizeof(sa.sun_path), PMI2_SOCK_ADDR_FMT,
-		 spool, job_info.jobid, job_info.stepid);
-	/*
 	 * We need to unlink this later so we need a formatted version of the
 	 * string to unlink.
 	 */
-	fmt_tree_sock_addr = xstrdup(sa.sun_path);
+	xstrsubstitute(spool, "%n", job->node_name);
+	xstrsubstitute(spool, "%h", job->node_name);
+	xstrfmtcat(fmt_tree_sock_addr, PMI2_SOCK_ADDR_FMT, spool,
+		   job_info.jobid, job_info.stepid);
+	/*
+	 * If socket name would be truncated, emit error and exit
+	 */
+	if (strlen(fmt_tree_sock_addr) >= sizeof(sa.sun_path)) {
+		error("%s: Unix socket path '%s' is too long. (%ld > %ld)",
+		      __func__, fmt_tree_sock_addr,
+		      strlen(fmt_tree_sock_addr) + 1, sizeof(sa.sun_path));
+		xfree(spool);
+		xfree(fmt_tree_sock_addr);
+		return SLURM_ERROR;
+	}
+
+	strlcpy(sa.sun_path, fmt_tree_sock_addr, sizeof(sa.sun_path));
+	xfree(fmt_tree_sock_addr);
 
 	unlink(sa.sun_path);    /* remove possible old socket */
 	xfree(spool);

contribs/perlapi/libslurm/perl/lib/Slurm/Constant.pm
@@ -1170,6 +1170,8 @@ head2 SLURM ERRNO
 
 =item * ESLURMD_STEP_NOTSUSPENDED               4029
 
+=item * ESLURMD_INVALID_SOCKET_NAME_LEN         4030
+
 =back
 
 =head3 slurmd errors in user batch job

slurm/slurm_errno.h
@@ -246,6 +246,7 @@ enum {
 	ESLURMD_JOB_NOTRUNNING,
 	ESLURMD_STEP_SUSPENDED,
 	ESLURMD_STEP_NOTSUSPENDED,
+	ESLURMD_INVALID_SOCKET_NAME_LEN =		4030,
 
 	/* slurmd errors in user batch job */
 	ESCRIPT_CHDIR_FAILED =			4100,

src/common/slurm_errno.c
@@ -429,6 +429,8 @@ static slurm_errtab_t slurm_errtab[] = {
 	  "Job step is suspended"                               },
  	{ ESLURMD_STEP_NOTSUSPENDED,
 	  "Job step is not currently suspended"                 },
+	{ ESLURMD_INVALID_SOCKET_NAME_LEN,
+	  "Unix socket name exceeded maximum length"		},
 
 	/* slurmd errors in user batch job */
 	{ ESCRIPT_CHDIR_FAILED,

src/common/stepd_api.c
@@ -154,19 +154,28 @@ _step_connect(const char *directory, const char *nodename,
 	int fd;
 	int len;
 	struct sockaddr_un addr;
-	char *name = NULL;
+	char *name = xstrdup_printf("%s/%s_%u.%u",
+				    directory, nodename, jobid, stepid);
+
+	/*
+	 * If socket name would be truncated, emit error and exit
+	 */
+	if (strlen(name) >= sizeof(addr.sun_path)) {
+		error("%s: Unix socket path '%s' is too long. (%ld > %ld)",
+		      __func__, name, strlen(name) + 1, sizeof(addr.sun_path));
+		xfree(name);
+		return -1;
+	}
 
 	if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
 		error("%s: socket() failed dir %s node %s job %u step %u %m",
 		      __func__, directory, nodename, jobid, stepid);
+		xfree(name);
 		return -1;
 	}
 
 	memset(&addr, 0, sizeof(addr));
 	addr.sun_family = AF_UNIX;
-
-	xstrfmtcat(name, "%s/%s_%u.%u", directory, nodename, jobid, stepid);
-
 	strlcpy(addr.sun_path, name, sizeof(addr.sun_path));
 	len = strlen(addr.sun_path) + 1 + sizeof(addr.sun_family);
 

src/plugins/mpi/pmix/node_leader/ln_leader.c
@@ -93,6 +93,14 @@ int prepare_srv_socket(char *path)
     static struct sockaddr_un sa;
     int ret = 0;
 
+    if (strlen(path) >= sizeof(sa.sun_path)) {
+        /*PMIXP_ERROR_STD*/
+        printf("UNIX socket path is too long: %lu, max %lu",
+               (unsigned long)strlen(path),
+               (unsigned long)sizeof(sa.sun_path)-1);
+        return SLURM_ERROR;
+    }
+
     /* Make sure that socket file doesn't exists */
     if (0 == access(path, F_OK)) {
         /* remove old file */
@@ -102,14 +110,6 @@ int prepare_srv_socket(char *path)
                     path);
             return SLURM_ERROR;
         }
-    }
-
-    if (strlen(path) >= sizeof(sa.sun_path)) {
-        /*PMIXP_ERROR_STD*/
-        printf("UNIX socket path is too long: %lu, max %lu",
-                (unsigned long)strlen(path),
-                (unsigned long)sizeof(sa.sun_path)-1);
-        return SLURM_ERROR;
     }
 
     int fd = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -145,6 +145,15 @@ err_fd:
 int connect_to_server(char *path)
 {
     static struct sockaddr_un sa;
+
+    if (strlen(path) >= sizeof(sa.sun_path)) {
+        /*PMIXP_ERROR_STD*/
+        printf("UNIX socket path is too long: %lu, max %lu",
+               (unsigned long)strlen(path),
+               (unsigned long)sizeof(sa.sun_path)-1);
+        return -1;
+    }
+
     memset(&sa, 0, sizeof(sa));
     sa.sun_family = AF_UNIX;
     strcpy(sa.sun_path, path);

src/slurmd/slurmd/req.c
@@ -6283,8 +6283,13 @@ _rpc_forward_data(slurm_msg_t *msg)
 	debug3("Entering _rpc_forward_data, address: %s, len: %u",
 	       req->address, req->len);
 
-	/* sanity check */
-	if (strlen(req->address) > sizeof(sa.sun_path) - 1) {
+	/*
+	 * If socket name would be truncated, emit error and exit
+	 */
+	if (strlen(req->address) >= sizeof(sa.sun_path)) {
+		error("%s: Unix socket path '%s' is too long. (%ld > %ld)",
+		      __func__, req->address, strlen(req->address) + 1,
+		      sizeof(sa.sun_path));
 		slurm_seterrno(EINVAL);
 		rc = errno;
 		goto done;

src/slurmd/slurmstepd/req.c
@@ -152,6 +152,16 @@ _create_socket(const char *name)
 	int len;
 	struct sockaddr_un addr;
 
+	/*
+	 * If socket name would be truncated, emit error and exit
+	 */
+	if (strlen(name) > sizeof(addr.sun_path) - 1) {
+		error("%s: Unix socket path '%s' is too long. (%ld > %ld)",
+		      __func__, name, strlen(name) + 1, sizeof(addr.sun_path));
+		errno = ESLURMD_INVALID_SOCKET_NAME_LEN;
+		return -1;
+	}
+
 	/* create a unix domain stream socket */
 	if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
 		return -1;
@@ -234,7 +244,7 @@ _domain_socket_destroy(int fd)
 		error("Unable to close domain socket: %m");
 
 	if (unlink(socket_name) == -1)
-		error("Unable to unlink domain socket: %m");
+		error("Unable to unlink domain socket `%s`: %m", socket_name);
 }
 
 /* Wait for the job to be running (pids added) before continuing. */

commit e5a2a2b53592fdd97ee6f22474427a2c4e8c414e

pmix2 - clean up pmi2 sockets at step completion.
The formatted socket path needs to be kept until plugin fini() so the
socket can be unlinked.

Regression from 21d851d2f52.

Bug 9089.

src/plugins/mpi/pmi2/setup.c
@@ -322,7 +322,6 @@ _setup_stepd_sockets(const stepd_step_rec_t *job, char ***env)
 	}
 
 	strlcpy(sa.sun_path, fmt_tree_sock_addr, sizeof(sa.sun_path));
-	xfree(fmt_tree_sock_addr);
 
 	unlink(sa.sun_path);    /* remove possible old socket */
 	xfree(spool);