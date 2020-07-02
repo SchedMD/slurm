4c70c9fa62e6a6c6690be489e8cecf9f86780f85
Nate Rini <nate@schedmd.com>
2020-07-02 13:53:29 -0600

    slurmrestd/v0.0.36 - return array of nodes instead of dictionary
    Bug 9131.
    
    Co-authored-by: "Andrew E. Bruno" <aebruno2@buffalo.edu>

--- src/slurmrestd/plugins/openapi/v0.0.36/nodes.c
+++ src/slurmrestd/plugins/openapi/v0.0.36/nodes.c
@@ -91,7 +91,7 @@ static int _dump_node(data_t *p, node_info_t *node)
 		return SLURM_SUCCESS;
 	}
 
-	d = data_set_dict(data_key_set(p, node->name));
+	d = data_set_dict(data_list_append(p));
 
 	data_set_string(data_key_set(d, "architecture"), node->arch);
 	data_set_string(data_key_set(d, "burstbuffer_network_address"),
@@ -157,7 +157,7 @@ static int _op_handler_nodes(const char *context_id,
 	int rc = SLURM_SUCCESS;
 	data_t *d = data_set_dict(resp);
 	data_t *errors = data_set_list(data_key_set(d, "errors"));
-	data_t *nodes = data_set_dict(data_key_set(d, "nodes"));
+	data_t *nodes = data_set_list(data_key_set(d, "nodes"));
 	node_info_msg_t *node_info_ptr = NULL;
 
 	if (tag == URL_TAG_NODES)
