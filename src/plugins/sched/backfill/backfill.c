SchedMD/slurm

commit 5efab599b6edd2ae0db4a8ae173bbc42fcff53aa

Fix job array preemption in backfill scheduling.
Previously only a single task of a job array could preempt during
backfill scheduling. This allows multiple tasks to preempt and have
resources reserved in backfill.

Bug 5405.

src/plugins/sched/backfill/backfill.c
@@ -1052,12 +1052,38 @@ static bool _job_runnable_now(struct job_record *job_ptr)
 		return false;
 	if (IS_JOB_COMPLETING(job_ptr))	/* Started, requeue and completing */
 		return false;
+	/*
+	 * Already reserved resources for either bf_max_job_array_resv or
+	 * max_run_tasks number of jobs in the array. If max_run_tasks is 0, it
+	 * wasn't set, so ignore it.
+	 */
+	if (job_ptr->array_recs &&
+	    ((job_ptr->array_recs->pend_run_tasks >= bf_max_job_array_resv) ||
+	     (job_ptr->array_recs->max_run_tasks &&
+	      (job_ptr->array_recs->pend_run_tasks >=
+	     job_ptr->array_recs->max_run_tasks))))
+		return false;
+
 	select_g_select_jobinfo_get(job_ptr->select_jobinfo,
 				    SELECT_JOBDATA_CLEANING, &cleaning);
 	if (cleaning)			/* Started, requeue and completing */
 		return false;
 
 	return true;
+}
+
+static void _restore_preempt_state(struct job_record *job_ptr,
+				   time_t *tmp_preempt_start_time,
+				   bool *tmp_preempt_in_progress)
+{
+	if ((*tmp_preempt_start_time != 0)
+	    && (job_ptr->details->preempt_start_time == 0)) {
+		job_ptr->details->preempt_start_time =
+			*tmp_preempt_start_time;
+		job_ptr->preempt_in_progress = *tmp_preempt_in_progress;
+		*tmp_preempt_start_time = 0;
+		*tmp_preempt_in_progress = false;
+	}
 }
 
 static int _attempt_backfill(void)
@@ -1102,6 +1128,8 @@ static int _attempt_backfill(void)
 	int part_inx = -1, user_inx = -1;
 	uint32_t qos_flags = 0;
 	time_t qos_blocked_until = 0, qos_part_blocked_until = 0;
+	time_t tmp_preempt_start_time = 0;
+	bool tmp_preempt_in_progress = false;
 	/* QOS Read lock */
 	assoc_mgr_lock_t qos_read_lock =
 		{ NO_LOCK, NO_LOCK, READ_LOCK, NO_LOCK,
@@ -1247,6 +1275,10 @@ static int _attempt_backfill(void)
 		bf_job_priority  = job_queue_rec->priority;
 		bf_array_task_id = job_queue_rec->array_task_id;
 		xfree(job_queue_rec);
+
+		/* Restore preemption state if needed. */
+		_restore_preempt_state(job_ptr, &tmp_preempt_start_time,
+				       &tmp_preempt_in_progress);
 
 		if (slurmctld_config.shutdown_time ||
 		    (difftime(time(NULL),orig_sched_start) >= bf_max_time)){
@@ -1428,10 +1460,22 @@ static int _attempt_backfill(void)
 			}
 		}
 
+		if (tmp_preempt_in_progress)
+			continue; 	/* scheduled in another partition */
+
 		orig_start_time = job_ptr->start_time;
 		orig_time_limit = job_ptr->time_limit;
 
 next_task:
+		/*
+		 * Save the current preemption state. Reset preemption state
+		 * in the job_ptr so a job array can preempt multiple jobs.
+		 */
+		tmp_preempt_in_progress = job_ptr->preempt_in_progress;
+		tmp_preempt_start_time = job_ptr->details->preempt_start_time;
+		job_ptr->details->preempt_start_time = 0;
+		job_ptr->preempt_in_progress = false;
+
 		job_test_count++;
 		slurmctld_diag_stats.bf_last_depth++;
 		already_counted = false;
@@ -1439,8 +1483,6 @@ next_task:
 		if (!IS_JOB_PENDING(job_ptr) ||	/* Started in other partition */
 		    (job_ptr->priority == 0))	/* Job has been held */
 			continue;
-		if (job_ptr->preempt_in_progress)
-			continue; 	/* scheduled in another partition */
 		if (!avail_front_end(job_ptr))
 			continue;	/* No available frontend for this job */
 		if (!_job_part_valid(job_ptr, part_ptr))
@@ -2382,11 +2424,25 @@ skip_start:
 			} else {
 				test_array_count++;
 			}
+
+			/*
+			 * Don't consider the next task if it would exceed the
+			 * maximum number of runnable tasks. If max_run_tasks is
+			 * 0, then it wasn't set, so ignore it.
+			 */
 			if ((test_array_count < bf_max_job_array_resv) &&
-			    (test_array_count < job_ptr->array_recs->task_cnt))
+			    (test_array_count <
+			     job_ptr->array_recs->task_cnt) &&
+			    (!job_ptr->array_recs->max_run_tasks ||
+			     (job_ptr->array_recs->pend_run_tasks <
+			     job_ptr->array_recs->max_run_tasks)))
 				goto next_task;
 		}
 	}
+
+	/* Restore preemption state if needed. */
+	_restore_preempt_state(job_ptr, &tmp_preempt_start_time,
+			       &tmp_preempt_in_progress);
 
 	_job_pack_deadlock_fini();
 	_pack_start_test(node_space);

doc/html/preempt.shtml
@@ -269,6 +269,18 @@ A good way to observe the operation of the gang scheduler is by running
 <I>squeue -i&lt;time&gt;</I> in a terminal window.
 </P>
 
+<H2>Limitations of Preemption During Backfill Scheduling</H2>
+
+<P>
+For performance reasons, the backfill scheduler reserves whole nodes for jobs,
+not partial nodes. If during backfill scheduling a job preempts one or more
+other jbos, the whole nodes for those preempted jobs are reserved for the
+preemptor job, even if the preemptor job requested fewer resources than that.
+These reserved nodes aren't available to other jobs during that backfill
+cycle, even if the other jobs could fit on the nodes. Therefore, jobs may
+preempt more resources during a single backfill iteration than they requested.
+</P>
+
 <H2>A Simple Example</H2>
 
 <P>

doc/man/man5/slurm.conf.5
@@ -1705,6 +1705,14 @@ enable gang scheduling independent of whether preemption is enabled
 specified in addition to a \fBPreemptMode\fR setting with the two
 options comma separated.  The \fBSUSPEND\fR option requires that gang
 scheduling be enabled (i.e, "PreemptMode=SUSPEND,GANG").
+\fBNOTE:
+For performance reasons, the backfill scheduler reserves whole nodes for jobs,
+not partial nodes. If during backfill scheduling a job preempts one or more
+other jbos, the whole nodes for those preempted jobs are reserved for the
+preemptor job, even if the preemptor job requested fewer resources than that.
+These reserved nodes aren't available to other jobs during that backfill
+cycle, even if the other jobs could fit on the nodes. Therefore, jobs may
+preempt more resources during a single backfill iteration than they requested.
 .RS
 .TP 12
 \fBOFF\fR
@@ -2552,6 +2560,12 @@ expected times.
 This has no impact upon the number of tasks from a job array that can be
 started immediately, only those tasks expected to start at some future time.
 The default value is 20 tasks.
+NOTE:
+Jobs submitted to multiple partitions appear in the job queue once per
+partition. If different copies of a single job array record aren't consecutive
+in the job queue and another job array record is in between, then
+bf_max_job_array_resv tasks are considered per partition that the job is
+submitted to.
 .TP
 \fBbf_max_job_assoc=#\fR
 The maximum number of jobs per user association to attempt starting with the

src/slurmctld/node_scheduler.c
@@ -2752,6 +2752,8 @@ extern int select_nodes(struct job_record *job_ptr, bool test_only,
 		    (detail_ptr->preempt_start_time == 0)) {
   			detail_ptr->preempt_start_time = now;
 			job_ptr->preempt_in_progress = true;
+			if (job_ptr->array_recs)
+				job_ptr->array_recs->pend_run_tasks++;
 		}
 	}
 	if (error_code) {

src/slurmctld/slurmctld.h
@@ -585,6 +585,8 @@ typedef struct job_array_struct {
 	uint32_t tot_run_tasks;		/* Current running task count */
 	uint32_t min_exit_code;		/* Minimum exit code from any task */
 	uint32_t max_exit_code;		/* Maximum exit code from any task */
+	uint32_t pend_run_tasks;	/* Number of tasks ready to run due to
+					 * preempting other jobs */
 	uint32_t tot_comp_tasks;	/* Completed task count */
 } job_array_struct_t;
 

commit 32ef15950be5d099b90c9bb5864e655ac0659a0d

Fix bad preemption logic
Problem introduced in 5efab599b6e

Wasn't restoring preempt_start_time for job that actually preempted. Was
happening for next job in queue.

Bug 9094

src/plugins/sched/backfill/backfill.c
@@ -1203,9 +1203,10 @@ static void _restore_preempt_state(job_record_t *job_ptr,
 		job_ptr->details->preempt_start_time =
 			*tmp_preempt_start_time;
 		job_ptr->preempt_in_progress = *tmp_preempt_in_progress;
-		*tmp_preempt_start_time = 0;
-		*tmp_preempt_in_progress = false;
 	}
+
+	*tmp_preempt_start_time = 0;
+	*tmp_preempt_in_progress = false;
 }
 
 /*
@@ -1706,6 +1707,10 @@ static int _attempt_backfill(void)
 		if (job_ptr) {
 			job_resv_clear_promiscous_flag(job_ptr);
 			fill_array_reasons(job_ptr, reject_array_job);
+
+			/* Restore preemption state if needed. */
+			_restore_preempt_state(job_ptr, &tmp_preempt_start_time,
+			                       &tmp_preempt_in_progress);
 		}
 		job_queue_rec = (job_queue_rec_t *) list_pop(job_queue);
 		if (!job_queue_rec) {
@@ -1772,10 +1777,6 @@ static int _attempt_backfill(void)
 			if (!job_ptr)	/* All task array elements started */
 				continue;
 		}
-
-		/* Restore preemption state if needed. */
-		_restore_preempt_state(job_ptr, &tmp_preempt_start_time,
-				       &tmp_preempt_in_progress);
 
 		/*
 		 * Establish baseline (worst case) start time for hetjob
@@ -1932,7 +1933,7 @@ static int _attempt_backfill(void)
 				job_no_reserve = TEST_NOW_ONLY;
 		}
 
-		if (tmp_preempt_in_progress)
+		if (job_ptr->preempt_in_progress)
 			continue; 	/* scheduled in another partition */
 
 		orig_start_time = job_ptr->start_time;
@@ -1948,10 +1949,12 @@ next_task:
 		 * Save the current preemption state. Reset preemption state
 		 * in the job_ptr so a job array can preempt multiple jobs.
 		 */
-		tmp_preempt_in_progress = job_ptr->preempt_in_progress;
-		tmp_preempt_start_time = job_ptr->details->preempt_start_time;
-		job_ptr->details->preempt_start_time = 0;
-		job_ptr->preempt_in_progress = false;
+		if (job_ptr->preempt_in_progress) {
+			tmp_preempt_in_progress = job_ptr->preempt_in_progress;
+			tmp_preempt_start_time = job_ptr->details->preempt_start_time;
+			job_ptr->details->preempt_start_time = 0;
+			job_ptr->preempt_in_progress = false;
+		}
 
 		job_test_count++;
 		slurmctld_diag_stats.bf_last_depth++;