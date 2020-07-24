dd64da0a2b1c9d7187ba612ea67fd45e3431634e
Tim Wickberg <tim@schedmd.com>
2020-07-24 10:53:29 -0600

    Rename HAPHAZARD to MAGNETIC.

--- src/common/slurm_protocol_defs.c
+++ src/common/slurm_protocol_defs.c
@@ -3266,12 +3266,12 @@ extern char *reservation_flags_string(reserve_info_t * resv_ptr)
 	if (flags & RESERVE_FLAG_PROM) {
 		if (flag_str[0])
 			xstrcat(flag_str, ",");
-		xstrcat(flag_str, "HAPHAZARD");
+		xstrcat(flag_str, "MAGNETIC");
 	}
 	if (flags & RESERVE_FLAG_NO_PROM) {
 		if (flag_str[0])
 			xstrcat(flag_str, ",");
-		xstrcat(flag_str, "NO_HAPHAZARD");
+		xstrcat(flag_str, "NO_MAGNETIC");
 	}
 
 
