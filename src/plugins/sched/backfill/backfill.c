6d7a7d51ec3d7a2b9478e600721e4c2ec0961def
Dominik Bartkiewicz <bart@schedmd.com>
2020-08-20 18:04:53 +0200

    Avoid NULL pointer dereference in _restore_preempt_state()
    Bug 9593

--- src/plugins/sched/backfill/backfill.c
+++ src/plugins/sched/backfill/backfill.c
@@ -2808,13 +2808,13 @@ skip_start:
 		}
 	}
 
-	/* Restore preemption state if needed. */
-	_restore_preempt_state(job_ptr, &tmp_preempt_start_time,
-			       &tmp_preempt_in_progress);
-
 	if (job_ptr) {
+		/* Restore preemption state if needed. */
+		_restore_preempt_state(job_ptr, &tmp_preempt_start_time,
+				       &tmp_preempt_in_progress);
 		job_resv_clear_promiscous_flag(job_ptr);
 	}
+
 	_het_job_deadlock_fini();
 	if (!bf_hetjob_immediate &&
 	    (!max_backfill_jobs_start ||
