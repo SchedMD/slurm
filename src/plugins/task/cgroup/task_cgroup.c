cb50811f12883745bff3d28715e8ba23eed61c37
Nate Rini <nate@schedmd.com>
2020-08-31 11:58:39 -0600

    task/cgroup - Catch errors in task_p_pre_launch_priv()
    Bug 9714

--- src/plugins/task/cgroup/task_cgroup.c
+++ src/plugins/task/cgroup/task_cgroup.c
@@ -234,23 +234,24 @@ extern int task_p_pre_setuid (stepd_step_rec_t *job)
  */
 extern int task_p_pre_launch_priv(stepd_step_rec_t *job, pid_t pid)
 {
+	int rc[3] = {0};
 
 	if (use_cpuset) {
 		/* attach the task to the cpuset cgroup */
-		task_cgroup_cpuset_attach_task(job);
+		rc[0] = task_cgroup_cpuset_attach_task(job);
 	}
 
 	if (use_memory) {
 		/* attach the task to the memory cgroup */
-		task_cgroup_memory_attach_task(job, pid);
+		rc[1] = task_cgroup_memory_attach_task(job, pid);
 	}
 
 	if (use_devices) {
 		/* attach the task to the devices cgroup */
-		task_cgroup_devices_attach_task(job);
+		rc[2] = task_cgroup_devices_attach_task(job);
 	}
 
-	return SLURM_SUCCESS;
+	return MAX(rc[0], MAX(rc[1], rc[2]));
 }
 
 /*
