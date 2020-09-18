SchedMD/slurm

commit d56cd7443dac16ef36016f44c863224e61569a6b

Move _addto_id_char_list to common location to be used in sacct as well as sacctmgr.

src/sacct/options.c
@@ -42,7 +42,6 @@
 #include "src/common/proc_args.h"
 #include "src/common/read_config.h"
 #include "src/common/slurm_time.h"
-#include "src/common/uid.h"
 #include "src/common/xstring.h"
 #include "sacct.h"
 #include <time.h>
@@ -101,109 +100,6 @@ static void _help_fields_msg(void)
 	}
 	printf("\n");
 	return;
-}
-
-static char *_convert_to_id(char *name, bool gid)
-{
-	if (gid) {
-		gid_t gid;
-		if ( gid_from_string( name, &gid ) != 0 ) {
-			fprintf(stderr, "Invalid group id: %s\n", name);
-			exit(1);
-		}
-		xfree(name);
-		name = xstrdup_printf( "%d", (int) gid );
-	} else {
-		uid_t uid;
-		if ( uid_from_string( name, &uid ) != 0 ) {
-			fprintf(stderr, "Invalid user id: %s\n", name);
-			exit(1);
-		}
-		xfree(name);
-		name = xstrdup_printf( "%d", (int) uid );
-	}
-	return name;
-}
-
-/* returns number of objects added to list */
-static int _addto_id_char_list(List char_list, char *names, bool gid)
-{
-	int i=0, start=0;
-	char *name = NULL, *tmp_char = NULL;
-	ListIterator itr = NULL;
-	char quote_c = '\0';
-	int quote = 0;
-	int count = 0;
-
-	if (!char_list) {
-		error("No list was given to fill in");
-		return 0;
-	}
-
-	itr = list_iterator_create(char_list);
-	if (names) {
-		if (names[i] == '\"' || names[i] == '\'') {
-			quote_c = names[i];
-			quote = 1;
-			i++;
-		}
-		start = i;
-		while (names[i]) {
-			//info("got %d - %d = %d", i, start, i-start);
-			if (quote && names[i] == quote_c)
-				break;
-			else if (names[i] == '\"' || names[i] == '\'')
-				names[i] = '`';
-			else if (names[i] == ',') {
-				if ((i-start) > 0) {
-					name = xmalloc((i-start+1));
-					memcpy(name, names+start, (i-start));
-					//info("got %s %d", name, i-start);
-					name = _convert_to_id( name, gid );
-
-					while ((tmp_char = list_next(itr))) {
-						if (!xstrcasecmp(tmp_char,
-								 name))
-							break;
-					}
-
-					if (!tmp_char) {
-						list_append(char_list, name);
-						count++;
-					} else
-						xfree(name);
-					list_iterator_reset(itr);
-				}
-				i++;
-				start = i;
-				if (!names[i]) {
-					info("There is a problem with "
-					     "your request.  It appears you "
-					     "have spaces inside your list.");
-					break;
-				}
-			}
-			i++;
-		}
-		if ((i-start) > 0) {
-			name = xmalloc((i-start)+1);
-			memcpy(name, names+start, (i-start));
-			name = _convert_to_id(name, gid);
-
-			while ((tmp_char = list_next(itr))) {
-				if (!xstrcasecmp(tmp_char, name))
-					break;
-			}
-
-			if (!tmp_char) {
-				list_append(char_list, name);
-				count++;
-			} else
-				xfree(name);
-		}
-	}
-	list_iterator_destroy(itr);
-	return count;
 }
 
 /* returns number of objects added to list */
@@ -931,7 +827,8 @@ extern void parse_command_line(int argc, char **argv)
 			if (!job_cond->groupid_list)
 				job_cond->groupid_list =
 					list_create(slurm_destroy_char);
-			_addto_id_char_list(job_cond->groupid_list, optarg, 1);
+			slurm_addto_id_char_list(job_cond->groupid_list,
+						 optarg, 1);
 			break;
 		case 'h':
 			params.opt_help = 1;
@@ -1097,7 +994,8 @@ extern void parse_command_line(int argc, char **argv)
 			if (!job_cond->userid_list)
 				job_cond->userid_list =
 					list_create(slurm_destroy_char);
-			_addto_id_char_list(job_cond->userid_list, optarg, 0);
+			slurm_addto_id_char_list(job_cond->userid_list,
+						 optarg, 0);
 			break;
 		case 'v':
 			/* Handle -vvv thusly...

src/common/slurm_protocol_defs.c
@@ -58,6 +58,7 @@
 #include "src/common/slurm_protocol_defs.h"
 #include "src/common/slurm_time.h"
 #include "src/common/switch.h"
+#include "src/common/uid.h"
 #include "src/common/xmalloc.h"
 #include "src/common/xstring.h"
 
@@ -92,6 +93,28 @@ static void _free_all_partitions (partition_info_msg_t *msg);
 static void  _free_all_reservations(reserve_info_msg_t *msg);
 
 static void _free_all_step_info (job_step_info_response_msg_t *msg);
+
+static char *_convert_to_id(char *name, bool gid)
+{
+	if (gid) {
+		gid_t gid;
+		if (gid_from_string( name, &gid )) {
+			error("Invalid group id: %s\n", name);
+			return NULL;
+		}
+		xfree(name);
+		name = xstrdup_printf( "%d", (int) gid );
+	} else {
+		uid_t uid;
+		if (uid_from_string( name, &uid )) {
+			error("Invalid user id: %s\n", name);
+			return NULL;
+		}
+		xfree(name);
+		name = xstrdup_printf( "%d", (int) uid );
+	}
+	return name;
+}
 
 /*
  * slurm_msg_t_init - initialize a slurm message
@@ -402,6 +425,88 @@ endit:
 	return count;
 }
 
+/* Parses string and converts names to either uid or gid list */
+extern int slurm_addto_id_char_list(List char_list, char *names, bool gid)
+{
+	int i=0, start=0;
+	char *name = NULL, *tmp_char = NULL;
+	ListIterator itr = NULL;
+	char quote_c = '\0';
+	int quote = 0;
+	int count = 0;
+
+	if (!char_list) {
+		error("No list was given to fill in");
+		return 0;
+	}
+
+	itr = list_iterator_create(char_list);
+	if (names) {
+		if (names[i] == '\"' || names[i] == '\'') {
+			quote_c = names[i];
+			quote = 1;
+			i++;
+		}
+		start = i;
+		while (names[i]) {
+			//info("got %d - %d = %d", i, start, i-start);
+			if (quote && names[i] == quote_c)
+				break;
+			else if (names[i] == '\"' || names[i] == '\'')
+				names[i] = '`';
+			else if (names[i] == ',') {
+				if ((i-start) > 0) {
+					name = xmalloc((i-start+1));
+					memcpy(name, names+start, (i-start));
+					//info("got %s %d", name, i-start);
+					name = _convert_to_id(name, gid);
+					if (!name)
+						return 0;
+					while ((tmp_char = list_next(itr))) {
+						if (!xstrcasecmp(tmp_char,
+								 name))
+							break;
+					}
+
+					if (!tmp_char) {
+						list_append(char_list, name);
+						count++;
+					} else
+						xfree(name);
+					list_iterator_reset(itr);
+				}
+				i++;
+				start = i;
+				if (!names[i]) {
+					info("There is a problem with your request.  It appears you have spaces inside your list.");
+					break;
+				}
+			}
+			i++;
+		}
+		if ((i-start) > 0) {
+			name = xmalloc((i-start)+1);
+			memcpy(name, names+start, (i-start));
+			name = _convert_to_id(name, gid);
+			if (!name)
+				return 0;
+
+			while ((tmp_char = list_next(itr))) {
+				if (!xstrcasecmp(tmp_char, name))
+					break;
+			}
+
+			if (!tmp_char) {
+				list_append(char_list, name);
+				count++;
+			} else
+				xfree(name);
+		}
+	}
+	list_iterator_destroy(itr);
+	return count;
+}
+
 /* Parses strings such as stra,+strb,-strc and appends the default mode to each
  * string in the list if no specific mode is listed.
  * RET: returns the number of items added to the list. -1 on error. */
@@ -536,7 +641,6 @@ end_it:
 	list_iterator_destroy(itr);
 	return count;
 }
-
 
 static int _addto_step_list_internal(List step_list, char *names,
 				     int start, int end)

src/common/slurm_protocol_defs.h
@@ -1325,6 +1325,7 @@ extern List slurm_copy_char_list(List char_list);
 extern int slurm_addto_char_list(List char_list, char *names);
 extern int slurm_addto_char_list_with_case(List char_list, char *names,
 					   bool lower_case_normalization);
+extern int slurm_addto_id_char_list(List char_list, char *names, bool gid);
 extern int slurm_addto_mode_char_list(List char_list, char *names, int mode);
 extern int slurm_addto_step_list(List step_list, char *names);
 extern int slurm_char_list_copy(List dst, List src);

commit ed3c373651f793327da32c904e78cda0a82eb231

Have sacct exit correctly when a user/group id isn't valid.
Regression from d56cd7443da

Bug 9022

src/sacct/options.c
@@ -830,8 +830,9 @@ extern void parse_command_line(int argc, char **argv)
 		case 'g':
 			if (!job_cond->groupid_list)
 				job_cond->groupid_list = list_create(xfree_ptr);
-			slurm_addto_id_char_list(job_cond->groupid_list,
-						 optarg, 1);
+			if (!slurm_addto_id_char_list(job_cond->groupid_list,
+			                              optarg, 1))
+				exit(1);
 			break;
 		case 'h':
 			params.opt_help = 1;
@@ -992,8 +993,9 @@ extern void parse_command_line(int argc, char **argv)
 			all_users = false;
 			if (!job_cond->userid_list)
 				job_cond->userid_list = list_create(xfree_ptr);
-			slurm_addto_id_char_list(job_cond->userid_list,
-						 optarg, 0);
+			if (!slurm_addto_id_char_list(job_cond->userid_list,
+			                              optarg, 0))
+				exit(1);
 			break;
 		case 'v':
 			/* Handle -vvv thusly...

src/sacctmgr/job_functions.c
@@ -119,8 +119,9 @@ static int _set_cond(int *start, int argc, char **argv,
 					 MAX(command_len, 1))) {
 			if (!job_cond->userid_list)
 				job_cond->userid_list = list_create(xfree_ptr);
-			slurm_addto_id_char_list(job_cond->userid_list,
-						 argv[i]+end, 0);
+			if (!slurm_addto_id_char_list(job_cond->userid_list,
+			                              argv[i]+end, 0))
+				exit(1);
 			set = 1;
 		} else {
 			exit_code = 1;