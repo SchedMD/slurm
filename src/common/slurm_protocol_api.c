1948a0589df3bdfac4c13d10f9e68ca938d6b756
Danny Auble <da@schedmd.com>
2020-09-24 17:47:45 -0600

    Fix issue in 14e39ef8daebc9, we were reading from the wrong a NULL auth_cred.

--- src/common/slurm_protocol_api.c
+++ src/common/slurm_protocol_api.c
@@ -973,7 +973,7 @@ extern int slurm_unpack_received_msg(slurm_msg_t *msg, int fd, Buf buffer)
 		goto total_return;
 	}
 
-	msg->auth_uid = g_slurm_auth_get_uid(msg->auth_cred);
+	msg->auth_uid = g_slurm_auth_get_uid(auth_cred);
 	msg->auth_uid_set = true;
 
 	/*
@@ -1237,7 +1237,7 @@ List slurm_receive_msgs(int fd, int steps, int timeout)
 		goto total_return;
 	}
 
-	msg.auth_uid = g_slurm_auth_get_uid(msg.auth_cred);
+	msg.auth_uid = g_slurm_auth_get_uid(auth_cred);
 	msg.auth_uid_set = true;
 
 	/*
@@ -1455,7 +1455,7 @@ int slurm_receive_msg_and_forward(int fd, slurm_addr_t *orig_addr,
 		goto total_return;
 	}
 
-	msg->auth_uid = g_slurm_auth_get_uid(msg->auth_cred);
+	msg->auth_uid = g_slurm_auth_get_uid(auth_cred);
 	msg->auth_uid_set = true;
 
 	/*
