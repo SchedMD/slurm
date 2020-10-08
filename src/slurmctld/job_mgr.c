SchedMD/slurm

commit ce32018a28d6b7ad851ab3a2817bd300d0f2a655

Display job's estimated NodeCount based off of partition's configured resources rather than the whole system's.
Bug 1478

src/slurmctld/job_mgr.c
@@ -8052,8 +8052,15 @@ void pack_job(struct job_record *dump_job_ptr, uint16_t show_flags, Buf buffer,
 
 static void _find_node_config(int *cpu_cnt_ptr, int *core_cnt_ptr)
 {
-	int i, max_cpu_cnt = 1, max_core_cnt = 1;
+	static int max_cpu_cnt = -1, max_core_cnt = -1;
+	int i;
 	struct node_record *node_ptr = node_record_table_ptr;
+
+	*cpu_cnt_ptr  = max_cpu_cnt;
+	*core_cnt_ptr = max_core_cnt;
+
+	if (max_cpu_cnt != -1)
+		return;
 
 	for (i = 0; i < node_record_count; i++, node_ptr++) {
 #ifndef HAVE_BG
@@ -8072,15 +8079,13 @@ static void _find_node_config(int *cpu_cnt_ptr, int *core_cnt_ptr)
 		}
 #endif
 	}
-	*cpu_cnt_ptr  = max_cpu_cnt;
-	*core_cnt_ptr = max_core_cnt;
 }
 
 /* pack default job details for "get_job_info" RPC */
 static void _pack_default_job_details(struct job_record *job_ptr,
 				      Buf buffer, uint16_t protocol_version)
 {
-	static int max_cpu_cnt = -1, max_core_cnt = -1;
+	int max_cpu_cnt = -1, max_core_cnt = -1;
 	int i;
 	struct job_details *detail_ptr = job_ptr->details;
 	char *cmd_line = NULL;
@@ -8107,7 +8112,10 @@ static void _pack_default_job_details(struct job_record *job_ptr,
 	} else
 		shared = (uint16_t) NO_VAL;	/* No user or partition info */
 
-	if (max_cpu_cnt == -1)
+	if (job_ptr->part_ptr && job_ptr->part_ptr->max_cpu_cnt) {
+		max_cpu_cnt  = job_ptr->part_ptr->max_cpu_cnt;
+		max_core_cnt = job_ptr->part_ptr->max_core_cnt;
+	} else
 		_find_node_config(&max_cpu_cnt, &max_core_cnt);
 
 	if (protocol_version >= SLURM_15_08_PROTOCOL_VERSION) {

src/slurmctld/partition_mgr.c
@@ -83,7 +83,6 @@ struct part_record *default_part_loc = NULL; /* default partition location */
 time_t last_part_update;	/* time of last update to partition records */
 uint16_t part_max_priority = 0;         /* max priority in all partitions */
 
-static int    _build_part_bitmap(struct part_record *part_ptr);
 static int    _delete_part_record(char *name);
 static void   _dump_part_state(struct part_record *part_ptr,
 			       Buf buffer);
@@ -98,7 +97,7 @@ static uid_t *_remove_duplicate_uids(uid_t *);
 static int _uid_cmp(const void *, const void *);
 
 /*
- * _build_part_bitmap - update the total_cpus, total_nodes, and node_bitmap
+ * build_part_bitmap - update the total_cpus, total_nodes, and node_bitmap
  *	for the specified partition, also reset the partition pointers in
  *	the node back to this partition.
  * IN part_ptr - pointer to the partition
@@ -107,7 +106,7 @@ static int _uid_cmp(const void *, const void *);
  * NOTE: this does not report nodes defined in more than one partition. this
  *	is checked only upon reading the configuration file, not on an update
  */
-static int _build_part_bitmap(struct part_record *part_ptr)
+extern int build_part_bitmap(struct part_record *part_ptr)
 {
 	char *this_node_name;
 	bitstr_t *old_bitmap;
@@ -116,6 +115,8 @@ static int _build_part_bitmap(struct part_record *part_ptr)
 
 	part_ptr->total_cpus = 0;
 	part_ptr->total_nodes = 0;
+	part_ptr->max_cpu_cnt = 0;
+	part_ptr->max_core_cnt = 0;
 
 	if (part_ptr->node_bitmap == NULL) {
 		part_ptr->node_bitmap = bit_alloc(node_record_count);
@@ -132,6 +133,12 @@ static int _build_part_bitmap(struct part_record *part_ptr)
 		return 0;
 	}
 
+	if (!strcmp(part_ptr->nodes, "ALL")) {
+		bit_nset(part_ptr->node_bitmap, 0, node_record_count - 1);
+		xfree(part_ptr->nodes);
+		part_ptr->nodes = bitmap2node_name(part_ptr->node_bitmap);
+		bit_nclear(part_ptr->node_bitmap, 0, node_record_count - 1);
+	}
 	if ((host_list = hostlist_create(part_ptr->nodes)) == NULL) {
 		FREE_NULL_BITMAP(old_bitmap);
 		error("hostlist_create error on %s, %m",
@@ -142,7 +149,7 @@ static int _build_part_bitmap(struct part_record *part_ptr)
 	while ((this_node_name = hostlist_shift(host_list))) {
 		node_ptr = find_node_record(this_node_name);
 		if (node_ptr == NULL) {
-			error("_build_part_bitmap: invalid node name %s",
+			error("build_part_bitmap: invalid node name %s",
 				this_node_name);
 			free(this_node_name);
 			FREE_NULL_BITMAP(old_bitmap);
@@ -150,10 +157,19 @@ static int _build_part_bitmap(struct part_record *part_ptr)
 			return ESLURM_INVALID_NODE_NAME;
 		}
 		part_ptr->total_nodes++;
-		if (slurmctld_conf.fast_schedule)
+		if (slurmctld_conf.fast_schedule) {
 			part_ptr->total_cpus += node_ptr->config_ptr->cpus;
-		else
+			part_ptr->max_cpu_cnt = MAX(part_ptr->max_cpu_cnt,
+					node_ptr->config_ptr->cpus);
+			part_ptr->max_core_cnt = MAX(part_ptr->max_core_cnt,
+					node_ptr->config_ptr->cores);
+		} else {
 			part_ptr->total_cpus += node_ptr->cpus;
+			part_ptr->max_cpu_cnt  = MAX(part_ptr->max_cpu_cnt,
+					node_ptr->cpus);
+			part_ptr->max_core_cnt = MAX(part_ptr->max_core_cnt,
+					node_ptr->cores);
+		}
 		node_ptr->part_cnt++;
 		xrealloc(node_ptr->part_pptr, (node_ptr->part_cnt *
 			sizeof(struct part_record *)));
@@ -1573,7 +1589,7 @@ extern int update_part (update_part_msg_t * part_desc, bool create_flag)
 			}
 		}
 
-		error_code = _build_part_bitmap(part_ptr);
+		error_code = build_part_bitmap(part_ptr);
 		if (error_code) {
 			xfree(part_ptr->nodes);
 			part_ptr->nodes = backup_node_list;

src/slurmctld/read_config.c
@@ -257,50 +257,14 @@ static void _reorder_nodes_by_rank(void)
 static void _build_bitmaps_pre_select(void)
 {
 	struct part_record   *part_ptr;
-	struct node_record   *node_ptr;
 	ListIterator part_iterator;
-	int i;
 
 	/* scan partition table and identify nodes in each */
 	part_iterator = list_iterator_create(part_list);
 	while ((part_ptr = (struct part_record *) list_next(part_iterator))) {
-		FREE_NULL_BITMAP(part_ptr->node_bitmap);
-
-		if ((part_ptr->nodes == NULL) || (part_ptr->nodes[0] == '\0')) {
-			/* Partitions need a bitmap, even if empty */
-			part_ptr->node_bitmap = bit_alloc(node_record_count);
-			continue;
-		}
-
-		if (!strcmp(part_ptr->nodes, "ALL")) {
-			part_ptr->node_bitmap = (bitstr_t *)
-						bit_alloc(node_record_count);
-			bit_nset(part_ptr->node_bitmap, 0, node_record_count-1);
-			xfree(part_ptr->nodes);
-			part_ptr->nodes=bitmap2node_name(part_ptr->node_bitmap);
-		} else {
-			if (node_name2bitmap(part_ptr->nodes, false,
-					     &part_ptr->node_bitmap)) {
-				fatal("Invalid node names in partition %s",
-				      part_ptr->name);
-			}
-		}
-
-		for (i = 0; i < node_record_count; i++) {
-			if (bit_test(part_ptr->node_bitmap, i) == 0)
-				continue;
-			node_ptr = &node_record_table_ptr[i];
-			part_ptr->total_nodes++;
-			if (slurmctld_conf.fast_schedule)
-				part_ptr->total_cpus +=
-					node_ptr->config_ptr->cpus;
-			else
-				part_ptr->total_cpus += node_ptr->cpus;
-			node_ptr->part_cnt++;
-			xrealloc(node_ptr->part_pptr, (node_ptr->part_cnt *
-				sizeof(struct part_record *)));
-			node_ptr->part_pptr[node_ptr->part_cnt-1] = part_ptr;
-		}
+		if (build_part_bitmap(part_ptr) == ESLURM_INVALID_NODE_NAME)
+			fatal("Invalid node names in partition %s",
+					part_ptr->name);
 	}
 	list_iterator_destroy(part_iterator);
 	return;

src/slurmctld/slurmctld.h
@@ -357,6 +357,8 @@ struct part_record {
 	uint16_t state_up;	/* See PARTITION_* states in slurm.h */
 	uint32_t total_nodes;	/* total number of nodes in the partition */
 	uint32_t total_cpus;	/* total number of cpus in the partition */
+	uint32_t max_cpu_cnt;	/* max # of cpus on a node in the partition */
+	uint32_t max_core_cnt;	/* max # of cores on a node in the partition */
 	uint16_t cr_type;	/* Custom CR values for partition (if supported by select plugin) */
 };
 
@@ -875,6 +877,18 @@ extern void  build_config_feature_list(struct config_record *config_ptr);
  * NOTE: allocates memory that should be xfreed with delete_part_record
  */
 extern struct part_record *create_part_record (void);
+
+/*
+ * build_part_bitmap - update the total_cpus, total_nodes, and node_bitmap
+ *	for the specified partition, also reset the partition pointers in
+ *	the node back to this partition.
+ * IN part_ptr - pointer to the partition
+ * RET 0 if no error, errno otherwise
+ * global: node_record_table_ptr - pointer to global node table
+ * NOTE: this does not report nodes defined in more than one partition. this
+ *	is checked only upon reading the configuration file, not on an update
+ */
+extern int build_part_bitmap(struct part_record *part_ptr);
 
 /*
  * job_limits_check - check the limits specified for the job.

commit 0f92f6ecc15fc08014174a8674b97ca0db644240

When a partition has no nodes fix estimate of max cpus possible on a job trying to run there.
Fix _find_node_config()

return correct values at first call - regression introduced in ce32018a28d
make _find_node_config() thread-safe

Bug 9540

src/slurmctld/job_mgr.c
@@ -10622,20 +10622,27 @@ void pack_job(job_record_t *dump_job_ptr, uint16_t show_flags, Buf buffer,
 static void _find_node_config(int *cpu_cnt_ptr, int *core_cnt_ptr)
 {
 	static int max_cpu_cnt = -1, max_core_cnt = -1;
+	static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 	int i;
 	node_record_t *node_ptr = node_record_table_ptr;
+
+	slurm_mutex_lock(&lock);
+	if (max_cpu_cnt == -1) {
+		for (i = 0; i < node_record_count; i++, node_ptr++) {
+			/* Only data from config_record used for scheduling */
+			max_cpu_cnt = MAX(max_cpu_cnt,
+					  node_ptr->config_ptr->cpus);
+			max_core_cnt = MAX(max_core_cnt,
+					   node_ptr->config_ptr->cores);
+		}
+	}
+	slurm_mutex_unlock(&lock);
 
 	*cpu_cnt_ptr  = max_cpu_cnt;
 	*core_cnt_ptr = max_core_cnt;
 
-	if (max_cpu_cnt != -1)
-		return;
+	return;
 
-	for (i = 0; i < node_record_count; i++, node_ptr++) {
-		/* Only data from config_record used for scheduling */
-		max_cpu_cnt = MAX(max_cpu_cnt, node_ptr->config_ptr->cpus);
-		max_core_cnt = MAX(max_core_cnt, node_ptr->config_ptr->cores);
-	}
 }
 
 /* pack default job details for "get_job_info" RPC */