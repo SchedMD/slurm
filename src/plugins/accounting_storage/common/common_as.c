4a4f015d3763c74a7b340053e8c3541c46f227b7
Tim McMullan <mcmullan@schedmd.com>
2020-10-19 18:17:53 -0400

    Fix unintialized slurm_addr_t in cluster_first_reg().
    Coverity 214959

--- src/plugins/accounting_storage/common/common_as.c
+++ src/plugins/accounting_storage/common/common_as.c
@@ -370,6 +370,7 @@ extern int cluster_first_reg(char *host, uint16_t port, uint16_t rpc_version)
 	info("First time to register cluster requesting "
 	     "running jobs and system information.");
 
+	memset(&ctld_address, 0, sizeof(ctld_address));
 	slurm_set_addr(&ctld_address, port, host);
 	fd = slurm_open_msg_conn(&ctld_address);
 	if (fd < 0) {
