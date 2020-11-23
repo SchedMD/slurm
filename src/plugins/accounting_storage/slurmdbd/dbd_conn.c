d55fa2571449bd346686bc065ebcec05e7ea9e8e
Marcin Stolarek <cinek@schedmd.com>
2020-11-23 11:53:47 +0000

    Fix regresion from 44c9fb88b0b
    In 44c9fb88b0b some functions were moved to separate file that didn't
    include slurm_xlator.h. This resulted in functions like xstrdup(),
    running_in_slurmctld() being unknown when called from libslurm (API).
    
    Bug 10245

--- src/plugins/accounting_storage/slurmdbd/dbd_conn.c
+++ src/plugins/accounting_storage/slurmdbd/dbd_conn.c
@@ -38,6 +38,7 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
+#include "src/common/slurm_xlator.h"
 #include "src/common/slurmdbd_pack.h"
 #include "src/slurmctld/trigger_mgr.h"
 #include "slurmdbd_agent.h"
