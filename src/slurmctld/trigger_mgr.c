5cf426e85e242a91f1d0b69b4f0d9539bbf5815e
Dominik Bartkiewicz <bart@schedmd.com>
2020-12-11 15:49:15 +0100

    remove job_ptr from trig_mgr_info_t
    See also aa4b5b746e6
    
    Bug 10400

--- src/slurmctld/trigger_mgr.c
+++ src/slurmctld/trigger_mgr.c
@@ -106,7 +106,6 @@ typedef struct trig_mgr_info {
 	char *   res_id;	/* node name or job_id (string) */
 	bitstr_t *nodes_bitmap;	/* bitmap of requested nodes (if applicable) */
 	uint32_t job_id;	/* job ID (if applicable) */
-	job_record_t *job_ptr;	/* pointer to job record (if applicable) */
 	uint32_t trig_type;	/* TRIGGER_TYPE_* */
 	time_t   trig_time;	/* offset (pending) or time stamp (complete) */
 	uint32_t user_id;	/* user requesting trigger */
@@ -471,7 +470,6 @@ extern int trigger_set(uid_t uid, gid_t gid, trigger_info_msg_t *msg)
 			bitmap = NULL;
 		}
 		trig_add->job_id = job_id;
-		trig_add->job_ptr = job_ptr;
 		if (msg->trigger_array[i].res_id) {
 			trig_add->res_id = msg->trigger_array[i].res_id;
 			trig_add->orig_res_id = xstrdup(trig_add->res_id);
@@ -724,7 +722,6 @@ static void _dump_trigger_state(trig_mgr_info_t *trig_ptr, buf_t *buffer)
 	packstr  (trig_ptr->orig_res_id, buffer);  /* restores res_id too */
 	/* rebuild nodes_bitmap as needed from res_id */
 	/* rebuild job_id as needed from res_id */
-	/* rebuild job_ptr as needed from res_id */
 	pack32   (trig_ptr->trig_type, buffer);
 	pack_time(trig_ptr->orig_time, buffer);    /* restores trig_time too */
 	pack32   (trig_ptr->user_id,   buffer);
@@ -755,7 +752,6 @@ static int _load_trigger_state(buf_t *buffer, uint16_t protocol_version)
 		safe_unpackstr_xmalloc(&trig_ptr->res_id, &str_len, buffer);
 		/* rebuild nodes_bitmap as needed from res_id */
 		/* rebuild job_id as needed from res_id */
-		/* rebuild job_ptr as needed from res_id */
 		safe_unpack32   (&trig_ptr->trig_type, buffer);
 		safe_unpack_time(&trig_ptr->trig_time, buffer);
 		safe_unpack32   (&trig_ptr->user_id,   buffer);
@@ -773,15 +769,14 @@ static int _load_trigger_state(buf_t *buffer, uint16_t protocol_version)
 	    (trig_ptr->state > 2))
 		goto unpack_error;
 	if (trig_ptr->res_type == TRIGGER_RES_TYPE_JOB) {
+		job_record_t *job_ptr;
 		trig_ptr->job_id = (uint32_t) atol(trig_ptr->res_id);
-		trig_ptr->job_ptr = find_job_record(trig_ptr->job_id);
-		if (((trig_ptr->job_id == 0)     ||
-		     (trig_ptr->job_ptr == NULL)) &&
+		job_ptr = find_job_record(trig_ptr->job_id);
+		if (((trig_ptr->job_id == 0) || (job_ptr == NULL)) &&
 		    (trig_ptr->state != 2))
 			goto unpack_error;
 	} else if (trig_ptr->res_type == TRIGGER_RES_TYPE_NODE) {
 		trig_ptr->job_id = 0;
-		trig_ptr->job_ptr = NULL;
 		if ((trig_ptr->res_id != NULL)   &&
 		    (trig_ptr->res_id[0] != '*') &&
 		    (node_name2bitmap(trig_ptr->res_id, false,
@@ -1002,13 +997,13 @@ static bool _front_end_job_test(bitstr_t *front_end_bitmap,
 /* Test if the event has been triggered, change trigger state as needed */
 static void _trigger_job_event(trig_mgr_info_t *trig_in, time_t now)
 {
+	job_record_t *job_ptr;
 	xassert(verify_lock(JOB_LOCK, READ_LOCK));
 
-	trig_in->job_ptr = find_job_record(trig_in->job_id);
+	job_ptr = find_job_record(trig_in->job_id);
 
 	if ((trig_in->trig_type & TRIGGER_TYPE_FINI) &&
-	    ((trig_in->job_ptr == NULL) ||
-	     (IS_JOB_COMPLETED(trig_in->job_ptr)))) {
+	    ((job_ptr == NULL) || (IS_JOB_COMPLETED(job_ptr)))) {
 		trig_in->state = 1;
 		trig_in->trig_time = now + (trig_in->trig_time - 0x8000);
 		log_flag(TRIGGERS, "trigger[%u] event for job %u fini",
@@ -1016,7 +1011,7 @@ static void _trigger_job_event(trig_mgr_info_t *trig_in, time_t now)
 		return;
 	}
 
-	if (trig_in->job_ptr == NULL) {
+	if (job_ptr == NULL) {
 		log_flag(TRIGGERS, "trigger[%u] for defunct job %u",
 			 trig_in->trig_id, trig_in->job_id);
 		trig_in->state = 2;
@@ -1024,9 +1019,9 @@ static void _trigger_job_event(trig_mgr_info_t *trig_in, time_t now)
 		return;
 	}
 
-	if (!IS_JOB_PENDING(trig_in->job_ptr) &&
+	if (!IS_JOB_PENDING(job_ptr) &&
 	    (trig_in->trig_type & TRIGGER_TYPE_TIME)) {
-		long rem_time = (trig_in->job_ptr->end_time - now);
+		long rem_time = (job_ptr->end_time - now);
 		if (rem_time <= (0x8000 - trig_in->trig_time)) {
 			trig_in->state = 1;
 			trig_in->trig_time = now;
@@ -1038,7 +1033,7 @@ static void _trigger_job_event(trig_mgr_info_t *trig_in, time_t now)
 
 	if (trig_in->trig_type & TRIGGER_TYPE_DOWN) {
 		if (_front_end_job_test(trigger_down_front_end_bitmap,
-					trig_in->job_ptr)) {
+					job_ptr)) {
 			log_flag(TRIGGERS, "trigger[%u] for job %u down",
 				 trig_in->trig_id, trig_in->job_id);
 			trig_in->state = 1;
@@ -1050,7 +1045,7 @@ static void _trigger_job_event(trig_mgr_info_t *trig_in, time_t now)
 
 	if (trig_in->trig_type & TRIGGER_TYPE_DOWN) {
 		if (trigger_down_nodes_bitmap &&
-		    bit_overlap_any(trig_in->job_ptr->node_bitmap,
+		    bit_overlap_any(job_ptr->node_bitmap,
 				    trigger_down_nodes_bitmap)) {
 			log_flag(TRIGGERS, "trigger[%u] for job %u down",
 				 trig_in->trig_id, trig_in->job_id);
@@ -1063,7 +1058,7 @@ static void _trigger_job_event(trig_mgr_info_t *trig_in, time_t now)
 
 	if (trig_in->trig_type & TRIGGER_TYPE_FAIL) {
 		if (trigger_fail_nodes_bitmap &&
-		    bit_overlap_any(trig_in->job_ptr->node_bitmap,
+		    bit_overlap_any(job_ptr->node_bitmap,
 				    trigger_fail_nodes_bitmap)) {
 			log_flag(TRIGGERS, "trigger[%u] for job %u node fail",
 				 trig_in->trig_id, trig_in->job_id);
@@ -1076,7 +1071,7 @@ static void _trigger_job_event(trig_mgr_info_t *trig_in, time_t now)
 
 	if (trig_in->trig_type & TRIGGER_TYPE_UP) {
 		if (trigger_up_nodes_bitmap &&
-		    bit_overlap_any(trig_in->job_ptr->node_bitmap,
+		    bit_overlap_any(job_ptr->node_bitmap,
 				    trigger_up_nodes_bitmap)) {
 			trig_in->state = 1;
 			trig_in->trig_time = now +
@@ -1558,7 +1553,6 @@ static void _trigger_clone(trig_mgr_info_t *trig_in)
 		trig_add->orig_bitmap  = bit_copy(trig_in->orig_bitmap);
 	}
 	trig_add->job_id    = trig_in->job_id;
-	trig_add->job_ptr   = trig_in->job_ptr;
 	trig_add->trig_type = trig_in->trig_type;
 	trig_add->trig_time = trig_in->orig_time;
 	trig_add->orig_time = trig_in->orig_time;
