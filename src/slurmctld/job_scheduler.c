SchedMD/slurm

commit 2427f291f62621629e51f6ba2fdb4ff6cd2b9e3b

Change schedule() to use a boolean argument for full_queue.
Arguments were always 0 or INFINITE (indicate a full queue run should be
performed). As such, make the argument easier to understand, and get rid
of some overly-complex intermediate calculations to track this.

Note that as soon as any request has been made for a full queue run, that
request is "sticky" and persists until _schedule() is actually called.

No functional change.

src/slurmctld/job_scheduler.c
@@ -799,9 +799,9 @@ static bool _all_partition_priorities_same(void)
  *	order (by submit time), so the sorting should be pretty fast.
  * Note: job_write_lock must be unlocked before calling this.
  */
-extern int schedule(uint32_t job_limit)
+extern int schedule(bool full_queue)
 {
-	static uint32_t sched_job_limit = NO_VAL;
+	static bool sched_full_queue = false;
 	int job_count = 0;
 	struct timeval now;
 	long delta_t;
@@ -820,12 +820,12 @@ extern int schedule(uint32_t job_limit)
 	}
 
 	slurm_mutex_lock(&sched_mutex);
-	if (sched_job_limit == INFINITE)
-		;				/* leave unlimited */
-	else if (job_limit == INFINITE)
-		sched_job_limit = INFINITE;	/* set unlimited */
-	else if (sched_job_limit == NO_VAL)
-		sched_job_limit = job_limit;	/* set initial value */
+	/*
+	 * A thread may not get its scheduling request immediately satisfied.
+	 * If any of those requests were for full_queue, the next thread that
+	 * launches _schedule() will ensure that is honored eventually.
+	 */
+	sched_full_queue |= full_queue;
 
 	if (delta_t >= sched_min_interval) {
 		/* Temporarily set time in the future until we get the real
@@ -833,11 +833,10 @@ extern int schedule(uint32_t job_limit)
 		sched_last.tv_sec  = now.tv_sec;
 		sched_last.tv_usec = now.tv_usec;
 		sched_running = true;
-		job_limit = sched_job_limit;
-		sched_job_limit = NO_VAL;
+		sched_full_queue = false;
 		slurm_mutex_unlock(&sched_mutex);
 
-		job_count = _schedule((job_limit == INFINITE));
+		job_count = _schedule(sched_full_queue);
 
 		slurm_mutex_lock(&sched_mutex);
 		gettimeofday(&now, NULL);

src/slurmctld/controller.c
@@ -1874,7 +1874,6 @@ static void *_slurmctld_background(void *no_data)
 	time_t now;
 	int no_resp_msg_interval, ping_interval, purge_job_interval;
 	int i;
-	uint32_t job_limit;
 	DEF_TIMERS;
 
 	/* Locks: Read config */
@@ -1929,6 +1928,8 @@ static void *_slurmctld_background(void *no_data)
 	debug3("_slurmctld_background pid = %u", getpid());
 
 	while (1) {
+		bool call_schedule = false, full_queue = false;
+
 		for (i = 0; ((i < 10) && (slurmctld_config.shutdown_time == 0));
 		     i++) {
 			usleep(100000);
@@ -2138,10 +2139,10 @@ static void *_slurmctld_background(void *no_data)
 			slurm_mutex_unlock(&check_bf_running_lock);
 		}
 
-		job_limit = NO_VAL;
 		if (difftime(now, last_full_sched_time) >= sched_interval) {
 			slurm_mutex_lock(&sched_cnt_mutex);
-			job_limit = INFINITE;
+			call_schedule = true;
+			full_queue = true;
 			job_sched_cnt = 0;
 			slurm_mutex_unlock(&sched_cnt_mutex);
 			last_full_sched_time = now;
@@ -2150,18 +2151,18 @@ static void *_slurmctld_background(void *no_data)
 			if (job_sched_cnt &&
 			    (difftime(now, last_sched_time) >=
 			     batch_sched_delay)) {
-				job_limit = 0;	/* Default depth */
+				call_schedule = true;
 				job_sched_cnt = 0;
 			}
 			slurm_mutex_unlock(&sched_cnt_mutex);
 		}
-		if (job_limit != NO_VAL) {
+		if (call_schedule) {
 			lock_slurmctld(job_write_lock2);
 			now = time(NULL);
 			last_sched_time = now;
 			bb_g_load_state(false);	/* May alter job nice/prio */
 			unlock_slurmctld(job_write_lock2);
-			if (schedule(job_limit))
+			if (schedule(full_queue))
 				last_checkpoint_time = 0; /* force state save */
 			set_job_elig_time();
 		}

src/slurmctld/job_scheduler.h
@@ -216,9 +216,7 @@ extern void rebuild_job_part_list(job_record_t *job_ptr);
  * schedule - attempt to schedule all pending jobs
  *	pending jobs for each partition will be scheduled in priority
  *	order until a request fails
- * IN job_limit - maximum number of jobs to test now, avoid testing the full
- *		  queue on every job submit (0 means to use the system default,
- *		  SchedulerParameters for default_queue_depth)
+ * IN full_queue - test the full queue, or only default_queue_depth jobs
  * RET count of jobs scheduled
  * Note: If the scheduler has executed recently, rather than executing again
  *	right away, a thread will be spawned to execute later in an effort
@@ -228,7 +226,7 @@ extern void rebuild_job_part_list(job_record_t *job_ptr);
  *	changed with the update_job RPC. In general nodes will be in priority
  *	order (by submit time), so the sorting should be pretty fast.
  */
-extern int schedule(uint32_t job_limit);
+extern int schedule(bool full_queue);
 
 /*
  * set_job_elig_time - set the eligible time for pending jobs once their

commit 5b7d05119b6b491e7670a95d15380611b87558ee

Fix regression preventing a full pass of the main scheduler from ever running.
full_queue needs to be reset after the call to _schedule().
Regression from commit 2427f291f62.

Bug 10818.

src/slurmctld/job_scheduler.c
@@ -834,7 +834,6 @@ extern int schedule(bool full_queue)
 		sched_last.tv_sec  = now.tv_sec;
 		sched_last.tv_usec = now.tv_usec;
 		sched_running = true;
-		sched_full_queue = false;
 		slurm_mutex_unlock(&sched_mutex);
 
 		job_count = _schedule(sched_full_queue);
@@ -843,6 +842,7 @@ extern int schedule(bool full_queue)
 		gettimeofday(&now, NULL);
 		sched_last.tv_sec  = now.tv_sec;
 		sched_last.tv_usec = now.tv_usec;
+		sched_full_queue = false;
 		sched_running = false;
 		slurm_mutex_unlock(&sched_mutex);
 	} else if (sched_pend_thread == 0) {