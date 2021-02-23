9a865dff39bfa2d785f85b23196d7ca595297d43
Danny Auble <da@schedmd.com>
2021-02-23 12:43:10 -0700

    Combine 2 similar functions into one internal function.
    This also changes slurm_kill_job to use REQUEST_KILL_JOB instead of
    REQUEST_CANCEL_JOB_STEP.
    
    Looking at the history of slurm_kill_job, I think the rpc is wrong
    to start with. 44ae8707ec536a switches slurm_kill_job to
    slurm_kill_job2.  Then in 506d5e4548f720 Brian added the function
    we wanted to remove just to send in sibling to the mix.  So yeah,
    based on 44ae8707ec536a I think slurm_kill_job should also be sending
    in REQUEST_KILL_JOB instead of REQUEST_CANCEL_JOB_STEP.  If people
    really wanted to send that then use slurm_kill_job_step.
    
    Bug 10353
    
    # Conflicts:
    #	src/api/cancel.c

--- src/api/cancel.c
+++ src/api/cancel.c
@@ -47,6 +47,46 @@
 #include "src/common/xmalloc.h"
 #include "src/common/xstring.h"
 
+static int _slurm_kill_job_internal(uint32_t job_id,
+				    const char *sjob_id_in, const char *sibling,
+				    uint16_t signal, uint16_t flags)
+{
+	int cc = 0, rc = SLURM_SUCCESS;
+	slurm_msg_t msg;
+	job_step_kill_msg_t req;
+	char *sjob_id =
+		job_id ? xstrdup_printf("%u", job_id) : xstrdup(sjob_id_in);
+
+	if (!sjob_id) {
+		errno = EINVAL;
+		return SLURM_ERROR;
+	}
+
+	slurm_msg_t_init(&msg);
+
+	memset(&req, 0, sizeof(req));
+	req.step_id.job_id = NO_VAL;
+	req.sjob_id = sjob_id;
+	req.step_id.step_id = NO_VAL;
+	req.step_id.step_het_comp = NO_VAL;
+	req.signal = signal;
+	req.flags = flags;
+	req.sibling = xstrdup(sibling);
+	msg.msg_type = REQUEST_KILL_JOB;
+        msg.data = &req;
+
+	if (slurm_send_recv_controller_rc_msg(&msg, &cc, working_cluster_rec))
+		rc = SLURM_ERROR;
+
+	xfree(sjob_id);
+	xfree(req.sibling);
+
+	if (cc)
+		slurm_seterrno_ret(cc);
+
+	return rc;
+}
+
 /*
  * slurm_kill_job - send the specified signal to all steps of an existing job
  * IN job_id     - the job's id
@@ -57,30 +97,7 @@
 extern int
 slurm_kill_job (uint32_t job_id, uint16_t signal, uint16_t flags)
 {
-	int rc;
-	slurm_msg_t msg;
-	job_step_kill_msg_t req;
-
-	slurm_msg_t_init(&msg);
-	/*
-	 * Request message:
-	 */
-	memset(&req, 0, sizeof(job_step_kill_msg_t));
-	req.step_id.job_id = job_id;
-	req.sjob_id     = NULL;
-	req.step_id.step_id = NO_VAL;
-	req.step_id.step_het_comp = NO_VAL;
-	req.signal      = signal;
-	req.flags       = flags;
-	msg.msg_type    = REQUEST_CANCEL_JOB_STEP;
-	msg.data        = &req;
-	if (slurm_send_recv_controller_rc_msg(&msg, &rc, working_cluster_rec)<0)
-		return SLURM_ERROR;
-
-	if (rc)
-		slurm_seterrno_ret(rc);
-
-	return SLURM_SUCCESS;
+	return _slurm_kill_job_internal(job_id, NULL, NULL, signal, flags);
 }
 
 /*
@@ -124,38 +141,5 @@ slurm_kill_job_step (uint32_t job_id, uint32_t step_id, uint16_t signal)
 extern int slurm_kill_job2(const char *job_id, uint16_t signal, uint16_t flags,
 			   const char *sibling)
 {
-	int cc, rc = SLURM_SUCCESS;
-	slurm_msg_t msg;
-	job_step_kill_msg_t req;
-
-	if (job_id == NULL) {
-		errno = EINVAL;
-		return SLURM_ERROR;
-	}
-
-	slurm_msg_t_init(&msg);
-
-	memset(&req, 0, sizeof(job_step_kill_msg_t));
-	req.step_id.job_id = NO_VAL;
-	req.sjob_id     = xstrdup(job_id);
-	req.step_id.step_id = NO_VAL;
-	req.step_id.step_het_comp = NO_VAL;
-	req.signal      = signal;
-	req.flags	= flags;
-	req.sibling = xstrdup(sibling);
-	msg.msg_type    = REQUEST_KILL_JOB;
-        msg.data        = &req;
-
-	if (slurm_send_recv_controller_rc_msg(&msg, &cc, working_cluster_rec)) {
-		rc = SLURM_ERROR;
-		goto fini;
-	}
-
-	if (cc)
-		slurm_seterrno_ret(cc);
-
-fini:
-	xfree(req.sjob_id);
-	xfree(req.sibling);
-	return rc;
+	return _slurm_kill_job_internal(0, job_id, sibling, signal, flags);
 }
