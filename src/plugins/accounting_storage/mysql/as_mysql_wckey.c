SchedMD/slurm

commit 0dc23eea44daa6daae833d4b06b5a5da4dd2a9dc

Limit data when checking for a default wckey.
By sending in the local cluster list and changing the query to only
give the default wckey first and then the newest one will always be the
one we just added making it not needed to have FOR UPDATE specified.

This also makes it so we only query non-deleted wckeys.

Bug 10561

src/plugins/accounting_storage/mysql/as_mysql_wckey.c
@@ -130,12 +130,13 @@ static int _make_sure_users_have_default(
 		while ((cluster = list_next(clus_itr))) {
 			MYSQL_RES *result = NULL;
 			MYSQL_ROW row;
-			char *wckey = NULL;
+
 			/* only look at non * and non deleted ones */
 			query = xstrdup_printf(
 				"select distinct is_def, wckey_name from "
 				"\"%s_%s\" where user='%s' and wckey_name "
-				"not like '*%%' and deleted=0 FOR UPDATE;",
+				"not like '*%%' and deleted=0 ORDER BY "
+				"is_dev desc, creation_time desc LIMIT 1;",
 				cluster, wckey_table, user);
 			debug4("%d(%s:%d) query\n%s",
 			       mysql_conn->conn, THIS_FILE, __LINE__, query);
@@ -154,24 +155,22 @@ static int _make_sure_users_have_default(
 				mysql_free_result(result);
 				continue;
 			}
-			while ((row = mysql_fetch_row(result))) {
-				if (row[0][0] == '1')
-					break;
-				if (!wckey)
-					wckey = xstrdup(row[1]);
-			}
-			mysql_free_result(result);
 
-			/* we found one so just continue */
-			if (row || !wckey) {
-				xfree(wckey);
+			/* check if row is default */
+			row = mysql_fetch_row(result);
+			if (row[0][0] == '1') {
+				/* default found, continue */
+				mysql_free_result(result);
 				continue;
 			}
+
+			/* if we made it here, there is no default */
 			query = xstrdup_printf(
 				"update \"%s_%s\" set is_def=1 where "
 				"user='%s' and wckey_name='%s';",
-				cluster, wckey_table, user, wckey);
-			xfree(wckey);
+				cluster, wckey_table, user, row[1]);
+			mysql_free_result(result);
+
 			DB_DEBUG(DB_WCKEY, mysql_conn->conn, "query\n%s",
 			         query);
 			rc = mysql_db_query(mysql_conn, query);

commit 2f9dea99d57b2916e570bb3e6cc4f920065b03ed

accounting_storage/mysql - fix unknown column in as_mysql_wckey code.
Regression introduced in 0dc23eea44.

Bug 10935.

src/plugins/accounting_storage/mysql/as_mysql_wckey.c
@@ -136,7 +136,7 @@ static int _make_sure_users_have_default(
 				"select distinct is_def, wckey_name from "
 				"\"%s_%s\" where user='%s' and wckey_name "
 				"not like '*%%' and deleted=0 ORDER BY "
-				"is_dev desc, creation_time desc LIMIT 1;",
+				"is_def desc, creation_time desc LIMIT 1;",
 				cluster, wckey_table, user);
 			debug4("%d(%s:%d) query\n%s",
 			       mysql_conn->conn, THIS_FILE, __LINE__, query);