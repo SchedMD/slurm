dce02f37b70619c41d0b327615704faa5aadfeca
Dominik Bartkiewicz <bart@schedmd.com>
2021-03-12 15:53:16 +0100

    Optimization of _bit_fmt_hexmask()
    Use lookup table rather than interat bit by bit
    
    Bug 11013
    
    Signed-off-by: Marcin Stolarek <cinek@schedmd.com>

--- src/common/bitstring.c
+++ src/common/bitstring.c
@@ -147,6 +147,78 @@ strong_alias(bit_copybits,	slurm_bit_copybits);
 strong_alias(bit_get_bit_num,	slurm_bit_get_bit_num);
 strong_alias(bit_get_pos_num,	slurm_bit_get_pos_num);
 
+#ifdef SLURM_BIGENDIAN
+static const char* hexmask_lookup[256] = {
+	"00",	"80",	"40",	"C0",	"20",	"A0",	"60",	"E0",
+	"10",	"90",	"50",	"D0",	"30",	"B0",	"70",	"F0",
+	"08",	"88",	"48",	"C8",	"28",	"A8",	"68",	"E8",
+	"18",	"98",	"58",	"D8",	"38",	"B8",	"78",	"F8",
+	"04",	"84",	"44",	"C4",	"24",	"A4",	"64",	"E4",
+	"14",	"94",	"54",	"D4",	"34",	"B4",	"74",	"F4",
+	"0C",	"8C",	"4C",	"CC",	"2C",	"AC",	"6C",	"EC",
+	"1C",	"9C",	"5C",	"DC",	"3C",	"BC",	"7C",	"FC",
+	"02",	"82",	"42",	"C2",	"22",	"A2",	"62",	"E2",
+	"12",	"92",	"52",	"D2",	"32",	"B2",	"72",	"F2",
+	"0A",	"8A",	"4A",	"CA",	"2A",	"AA",	"6A",	"EA",
+	"1A",	"9A",	"5A",	"DA",	"3A",	"BA",	"7A",	"FA",
+	"06",	"86",	"46",	"C6",	"26",	"A6",	"66",	"E6",
+	"16",	"96",	"56",	"D6",	"36",	"B6",	"76",	"F6",
+	"0E",	"8E",	"4E",	"CE",	"2E",	"AE",	"6E",	"EE",
+	"1E",	"9E",	"5E",	"DE",	"3E",	"BE",	"7E",	"FE",
+	"01",	"81",	"41",	"C1",	"21",	"A1",	"61",	"E1",
+	"11",	"91",	"51",	"D1",	"31",	"B1",	"71",	"F1",
+	"09",	"89",	"49",	"C9",	"29",	"A9",	"69",	"E9",
+	"19",	"99",	"59",	"D9",	"39",	"B9",	"79",	"F9",
+	"05",	"85",	"45",	"C5",	"25",	"A5",	"65",	"E5",
+	"15",	"95",	"55",	"D5",	"35",	"B5",	"75",	"F5",
+	"0D",	"8D",	"4D",	"CD",	"2D",	"AD",	"6D",	"ED",
+	"1D",	"9D",	"5D",	"DD",	"3D",	"BD",	"7D",	"FD",
+	"03",	"83",	"43",	"C3",	"23",	"A3",	"63",	"E3",
+	"13",	"93",	"53",	"D3",	"33",	"B3",	"73",	"F3",
+	"0B",	"8B",	"4B",	"CB",	"2B",	"AB",	"6B",	"EB",
+	"1B",	"9B",	"5B",	"DB",	"3B",	"BB",	"7B",	"FB",
+	"07",	"87",	"47",	"C7",	"27",	"A7",	"67",	"E7",
+	"17",	"97",	"57",	"D7",	"37",	"B7",	"77",	"F7",
+	"0F",	"8F",	"4F",	"CF",	"2F",	"AF",	"6F",	"EF",
+	"1F",	"9F",	"5F",	"DF",	"3F",	"BF",	"7F",	"FF",
+};
+#else
+static const char* hexmask_lookup[256] = {
+	"00",	"01",	"02",	"03",	"04",	"05",	"06",	"07",
+	"08",	"09",	"0A",	"0B",	"0C",	"0D",	"0E",	"0F",
+	"10",	"11",	"12",	"13",	"14",	"15",	"16",	"17",
+	"18",	"19",	"1A",	"1B",	"1C",	"1D",	"1E",	"1F",
+	"20",	"21",	"22",	"23",	"24",	"25",	"26",	"27",
+	"28",	"29",	"2A",	"2B",	"2C",	"2D",	"2E",	"2F",
+	"30",	"31",	"32",	"33",	"34",	"35",	"36",	"37",
+	"38",	"39",	"3A",	"3B",	"3C",	"3D",	"3E",	"3F",
+	"40",	"41",	"42",	"43",	"44",	"45",	"46",	"47",
+	"48",	"49",	"4A",	"4B",	"4C",	"4D",	"4E",	"4F",
+	"50",	"51",	"52",	"53",	"54",	"55",	"56",	"57",
+	"58",	"59",	"5A",	"5B",	"5C",	"5D",	"5E",	"5F",
+	"60",	"61",	"62",	"63",	"64",	"65",	"66",	"67",
+	"68",	"69",	"6A",	"6B",	"6C",	"6D",	"6E",	"6F",
+	"70",	"71",	"72",	"73",	"74",	"75",	"76",	"77",
+	"78",	"79",	"7A",	"7B",	"7C",	"7D",	"7E",	"7F",
+	"80",	"81",	"82",	"83",	"84",	"85",	"86",	"87",
+	"88",	"89",	"8A",	"8B",	"8C",	"8D",	"8E",	"8F",
+	"90",	"91",	"92",	"93",	"94",	"95",	"96",	"97",
+	"98",	"99",	"9A",	"9B",	"9C",	"9D",	"9E",	"9F",
+	"A0",	"A1",	"A2",	"A3",	"A4",	"A5",	"A6",	"A7",
+	"A8",	"A9",	"AA",	"AB",	"AC",	"AD",	"AE",	"AF",
+	"B0",	"B1",	"B2",	"B3",	"B4",	"B5",	"B6",	"B7",
+	"B8",	"B9",	"BA",	"BB",	"BC",	"BD",	"BE",	"BF",
+	"C0",	"C1",	"C2",	"C3",	"C4",	"C5",	"C6",	"C7",
+	"C8",	"C9",	"CA",	"CB",	"CC",	"CD",	"CE",	"CF",
+	"D0",	"D1",	"D2",	"D3",	"D4",	"D5",	"D6",	"D7",
+	"D8",	"D9",	"DA",	"DB",	"DC",	"DD",	"DE",	"DF",
+	"E0",	"E1",	"E2",	"E3",	"E4",	"E5",	"E6",	"E7",
+	"E8",	"E9",	"EA",	"EB",	"EC",	"ED",	"EE",	"EF",
+	"F0",	"F1",	"F2",	"F3",	"F4",	"F5",	"F6",	"F7",
+	"F8",	"F9",	"FA",	"FB",	"FC",	"FD",	"FE",	"FF",
+};
+#endif
+
 /*
  * Allocate a bitstring.
  *   nbits (IN)		valid bits in new bitstring, initialized to all clear
@@ -1358,8 +1430,10 @@ static char *_bit_fmt_hexmask(bitstr_t *bitmap, bool trim_output)
 {
 	char *retstr, *ptr;
 	char current;
-	bitoff_t i;
+	const int32_t word_size = sizeof(bitstr_t) * 8;
+	bitoff_t i, j, word;
 	bitoff_t bitsize;
+
 	if (trim_output)
 		bitsize = bit_fls(bitmap) + 1;
 	else
@@ -1377,18 +1451,36 @@ static char *_bit_fmt_hexmask(bitstr_t *bitmap, bool trim_output)
 	retstr[1] = 'x';
 	retstr[charsize + 2] = '\0';
 	ptr = &retstr[charsize + 1];
-	for (i=0; i < bitsize;) {
-		current = 0;
-		if (                 bit_test(bitmap,i++)) current |= 0x1;
-		if ((i < bitsize) && bit_test(bitmap,i++)) current |= 0x2;
-		if ((i < bitsize) && bit_test(bitmap,i++)) current |= 0x4;
-		if ((i < bitsize) && bit_test(bitmap,i++)) current |= 0x8;
-		if (current <= 9) {
-			current += '0';
+
+	for (i = 0; i < bitsize;) {
+		if (i + word_size <= bitsize) {
+			word = _bit_word(i);
+			for (j = 0; j < sizeof(bitstr_t); j++) {
+				uint8_t idx = ((uint8_t *)(&(bitmap[word])))[j];
+				*ptr = hexmask_lookup[idx][1];
+				ptr--;
+				*ptr = hexmask_lookup[idx][0];
+				ptr--;
+			}
+			i += word_size;
 		} else {
-			current += 'A' - 10;
+			current = 0;
+			if (                 bit_test(bitmap,i++))
+				current |= 0x1;
+			if ((i < bitsize) && bit_test(bitmap,i++))
+				current |= 0x2;
+			if ((i < bitsize) && bit_test(bitmap,i++))
+				current |= 0x4;
+			if ((i < bitsize) && bit_test(bitmap,i++))
+				current |= 0x8;
+
+			if (current <= 9) {
+				current += '0';
+			} else {
+				current += 'A' - 10;
+			}
+			*ptr-- = current;
 		}
-		*ptr-- = current;
 	}
 
 	return retstr;
