SchedMD/slurm

commit 7ad3636630f7981a708291ec97f4cb14c49cc43b

Cleanup, no functional change.
# Conflicts:
#	src/plugins/select/cons_res/select_cons_res.c

src/plugins/select/cons_res/select_cons_res.c
@@ -139,170 +139,26 @@ const uint32_t plugin_version = SLURM_VERSION_NUMBER;
 const uint32_t pstate_version = 7;	/* version control on saved state */
 const uint16_t nodeinfo_magic = 0x82aa;
 
-extern select_nodeinfo_t *select_p_select_nodeinfo_alloc(void);
-extern int select_p_select_nodeinfo_free(select_nodeinfo_t *nodeinfo);
-
-/* Procedure Declarations */
-static void _spec_core_filter(bitstr_t *node_bitmap, bitstr_t **core_bitmap);
-bitstr_t *_pick_first_cores(bitstr_t *avail_bitmap, uint32_t node_cnt,
-			    uint32_t *core_cnt, bitstr_t ***core_bitmap);
-bitstr_t *_sequential_pick(bitstr_t *avail_bitmap, uint32_t node_cnt,
-			   uint32_t *core_cnt, bitstr_t ***core_bitmap);
-
-/*
- * init() is called when the plugin is loaded, before any other functions
- * are called.  Put global initialization here.
- */
-extern int init(void)
+/* Given available node and core bitmaps, remove all specialized cores
+ * node_bitmap IN - Nodes available for use
+ * core_bitmap IN/OUT - Cores currently NOT available for use */
+static void _spec_core_filter(bitstr_t *node_bitmap, bitstr_t **core_bitmap)
 {
-	common_init();
+	bitstr_t **p_spec_core_map =
+		common_mark_avail_cores(node_bitmap, NO_VAL16);
 
-	cons_common_callbacks.can_job_run_on_node = can_job_run_on_node;
-	cons_common_callbacks.choose_nodes = choose_nodes;
-	cons_common_callbacks.dist_tasks_compute_c_b = dist_tasks_compute_c_b;
-	cons_common_callbacks.pick_first_cores = _pick_first_cores;
-	cons_common_callbacks.sequential_pick = _sequential_pick;
-	cons_common_callbacks.spec_core_filter = _spec_core_filter;
+	xassert(core_bitmap);
 
-	return SLURM_SUCCESS;
+	if (*core_bitmap) {
+		core_array_and_not(core_bitmap, p_spec_core_map);
+	} else {
+		bit_not(*p_spec_core_map);
+		*core_bitmap = *p_spec_core_map;
+		*p_spec_core_map = NULL;
+	}
+
+	free_core_array(&p_spec_core_map);
 }
-
-extern int fini(void)
-{
-	common_fini();
-
-	return SLURM_SUCCESS;
-}
-
-/*
- * The remainder of this file implements the standard Slurm
- * node selection API.
- */
-
-/* select_p_state_save() in cons_common */
-
-/* select_p_state_restore() in cons_common */
-
-/* select_p_job_init() in cons_common */
-
-/* select_p_node_ranking() in cons_common */
-
-/* select_p_node_init() in cons_common */
-
-/* select_p_block_init() in cons_common */
-
-/*
- * select_p_job_test - Given a specification of scheduling requirements,
- *	identify the nodes which "best" satisfy the request.
- * 	"best" is defined as either a minimal number of consecutive nodes
- *	or if sharing resources then sharing them with a job of similar size.
- * IN/OUT job_ptr - pointer to job being considered for initiation,
- *                  set's start_time when job expected to start
- * IN/OUT bitmap - usable nodes are set on input, nodes not required to
- *	satisfy the request are cleared, other left set
- * IN min_nodes - minimum count of nodes
- * IN req_nodes - requested (or desired) count of nodes
- * IN max_nodes - maximum count of nodes (0==don't care)
- * IN mode - SELECT_MODE_RUN_NOW   (0): try to schedule job now
- *           SELECT_MODE_TEST_ONLY (1): test if job can ever run
- *           SELECT_MODE_WILL_RUN  (2): determine when and where job can run
- * IN preemptee_candidates - List of pointers to jobs which can be preempted.
- * IN/OUT preemptee_job_list - Pointer to list of job pointers. These are the
- *		jobs to be preempted to initiate the pending job. Not set
- *		if mode=SELECT_MODE_TEST_ONLY or input pointer is NULL.
- * RET zero on success, EINVAL otherwise
- * globals (passed via select_p_node_init):
- *	node_record_count - count of nodes configured
- *	node_record_table_ptr - pointer to global node table
- * NOTE: the job information that is considered for scheduling includes:
- *	req_node_bitmap: bitmap of specific nodes required by the job
- *	contiguous: allocated nodes must be sequentially located
- *	num_cpus: minimum number of processors required by the job
- * NOTE: bitmap must be a superset of req_nodes at the time that
- *	select_p_job_test is called
- */
-extern int select_p_job_test(struct job_record *job_ptr, bitstr_t * bitmap,
-			     uint32_t min_nodes, uint32_t max_nodes,
-			     uint32_t req_nodes, uint16_t mode,
-			     List preemptee_candidates,
-			     List *preemptee_job_list,
-			     bitstr_t *exc_core_bitmap)
-{
-	xassert(bitmap);
-
-	debug2("%s for %pJ", __func__, job_ptr);
-
-	if (!job_ptr->details)
-		return EINVAL;
-
-	return common_job_test(job_ptr, bitmap, min_nodes, max_nodes,
-			       req_nodes, mode, preemptee_candidates,
-			       preemptee_job_list, &exc_core_bitmap);
-}
-
-/* select_p_job_begin() in cons_common */
-
-/* select_p_job_ready() in cons_common */
-
-/* select_p_job_resized() in cons_common */
-
-/* select_p_job_expand() in cons_common */
-
-/* select_p_job_signal() in cons_common */
-
-/* select_p_job_mem_confirm() in cons_common */
-
-/* select_p_job_fini() in cons_common */
-
-/* select_p_job_suspend() in cons_common */
-
-/* select_p_job_resume() in cons_common */
-
-/* select_p_step_pick_nodes() in cons_common */
-
-/* select_p_step_start() in cons_common */
-
-/* select_p_step_finish() in cons_common */
-
-/* select_p_select_nodeinfo_pack() in cons_common */
-
-/* select_p_select_nodeinfo_unpack() in cons_common */
-
-/* select_p_select_nodeinfo_alloc() in cons_common */
-
-/* select_p_select_nodeinfo_free() in cons_common */
-
-/* select_p_select_nodeinfo_set_all() in cons_common */
-
-/* select_p_select_nodeinfo_set() in cons_common */
-
-/* select_p_select_nodeinfo_get() in cons_common */
-
-/* select_p_select_jobinfo_alloc() in cons_common */
-
-/* select_p_select_jobinfo_free() in cons_common */
-
-/* select_p_select_jobinfo_set() in cons_common */
-
-/* select_p_select_jobinfo_get() in cons_common */
-
-/* select_p_select_jobinfo_copy() in cons_common */
-
-/* select_p_select_jobinfo_pack() in cons_common */
-
-/* select_p_select_jobinfo_unpack() in cons_common */
-
-/* select_p_select_jobinfo_sprint() in cons_common */
-
-/* select_p_select_jobinfo_xstrdup() in cons_common */
-
-/* select_p_get_info_from_plugin() in cons_common */
-
-/* select_p_update_node_config() in cons_common */
-
-/* select_p_update_node_state() in cons_common */
-
-/* select_p_reconfigure() in cons_common */
 
 /* Once here, if core_cnt is NULL, avail_bitmap has nodes not used by any job or
  * reservation */
@@ -537,25 +393,159 @@ bitstr_t *_pick_first_cores(bitstr_t *avail_bitmap, uint32_t node_cnt,
 	return sp_avail_bitmap;
 }
 
-/* Given available node and core bitmaps, remove all specialized cores
- * node_bitmap IN - Nodes available for use
- * core_bitmap IN/OUT - Cores currently NOT available for use */
-static void _spec_core_filter(bitstr_t *node_bitmap, bitstr_t **core_bitmap)
+/*
+ * init() is called when the plugin is loaded, before any other functions
+ * are called.  Put global initialization here.
+ */
+extern int init(void)
 {
-	bitstr_t **p_spec_core_map =
-		common_mark_avail_cores(node_bitmap, NO_VAL16);
+	common_init();
 
-	xassert(core_bitmap);
+	cons_common_callbacks.can_job_run_on_node = can_job_run_on_node;
+	cons_common_callbacks.choose_nodes = choose_nodes;
+	cons_common_callbacks.dist_tasks_compute_c_b = dist_tasks_compute_c_b;
+	cons_common_callbacks.pick_first_cores = _pick_first_cores;
+	cons_common_callbacks.sequential_pick = _sequential_pick;
+	cons_common_callbacks.spec_core_filter = _spec_core_filter;
 
-	if (*core_bitmap) {
-		core_array_and_not(core_bitmap, p_spec_core_map);
-	} else {
-		bit_not(*p_spec_core_map);
-		*core_bitmap = *p_spec_core_map;
-		*p_spec_core_map = NULL;
-	}
-
-	free_core_array(&p_spec_core_map);
+	return SLURM_SUCCESS;
 }
+
+extern int fini(void)
+{
+	common_fini();
+
+	return SLURM_SUCCESS;
+}
+
+/*
+ * The remainder of this file implements the standard Slurm
+ * node selection API.
+ */
+
+/* select_p_state_save() in cons_common */
+
+/* select_p_state_restore() in cons_common */
+
+/* select_p_job_init() in cons_common */
+
+/* select_p_node_ranking() in cons_common */
+
+/* select_p_node_init() in cons_common */
+
+/* select_p_block_init() in cons_common */
+
+/*
+ * select_p_job_test - Given a specification of scheduling requirements,
+ *	identify the nodes which "best" satisfy the request.
+ * 	"best" is defined as either a minimal number of consecutive nodes
+ *	or if sharing resources then sharing them with a job of similar size.
+ * IN/OUT job_ptr - pointer to job being considered for initiation,
+ *                  set's start_time when job expected to start
+ * IN/OUT bitmap - usable nodes are set on input, nodes not required to
+ *	satisfy the request are cleared, other left set
+ * IN min_nodes - minimum count of nodes
+ * IN req_nodes - requested (or desired) count of nodes
+ * IN max_nodes - maximum count of nodes (0==don't care)
+ * IN mode - SELECT_MODE_RUN_NOW   (0): try to schedule job now
+ *           SELECT_MODE_TEST_ONLY (1): test if job can ever run
+ *           SELECT_MODE_WILL_RUN  (2): determine when and where job can run
+ * IN preemptee_candidates - List of pointers to jobs which can be preempted.
+ * IN/OUT preemptee_job_list - Pointer to list of job pointers. These are the
+ *		jobs to be preempted to initiate the pending job. Not set
+ *		if mode=SELECT_MODE_TEST_ONLY or input pointer is NULL.
+ * RET zero on success, EINVAL otherwise
+ * globals (passed via select_p_node_init):
+ *	node_record_count - count of nodes configured
+ *	node_record_table_ptr - pointer to global node table
+ * NOTE: the job information that is considered for scheduling includes:
+ *	req_node_bitmap: bitmap of specific nodes required by the job
+ *	contiguous: allocated nodes must be sequentially located
+ *	num_cpus: minimum number of processors required by the job
+ * NOTE: bitmap must be a superset of req_nodes at the time that
+ *	select_p_job_test is called
+ */
+extern int select_p_job_test(struct job_record *job_ptr, bitstr_t * bitmap,
+			     uint32_t min_nodes, uint32_t max_nodes,
+			     uint32_t req_nodes, uint16_t mode,
+			     List preemptee_candidates,
+			     List *preemptee_job_list,
+			     bitstr_t *exc_core_bitmap)
+{
+	xassert(bitmap);
+
+	debug2("%s for %pJ", __func__, job_ptr);
+
+	if (!job_ptr->details)
+		return EINVAL;
+
+	return common_job_test(job_ptr, bitmap, min_nodes, max_nodes,
+			       req_nodes, mode, preemptee_candidates,
+			       preemptee_job_list, &exc_core_bitmap);
+}
+
+/* select_p_job_begin() in cons_common */
+
+/* select_p_job_ready() in cons_common */
+
+/* select_p_job_resized() in cons_common */
+
+/* select_p_job_expand() in cons_common */
+
+/* select_p_job_signal() in cons_common */
+
+/* select_p_job_mem_confirm() in cons_common */
+
+/* select_p_job_fini() in cons_common */
+
+/* select_p_job_suspend() in cons_common */
+
+/* select_p_job_resume() in cons_common */
+
+/* select_p_step_pick_nodes() in cons_common */
+
+/* select_p_step_start() in cons_common */
+
+/* select_p_step_finish() in cons_common */
+
+/* select_p_select_nodeinfo_pack() in cons_common */
+
+/* select_p_select_nodeinfo_unpack() in cons_common */
+
+/* select_p_select_nodeinfo_alloc() in cons_common */
+
+/* select_p_select_nodeinfo_free() in cons_common */
+
+/* select_p_select_nodeinfo_set_all() in cons_common */
+
+/* select_p_select_nodeinfo_set() in cons_common */
+
+/* select_p_select_nodeinfo_get() in cons_common */
+
+/* select_p_select_jobinfo_alloc() in cons_common */
+
+/* select_p_select_jobinfo_free() in cons_common */
+
+/* select_p_select_jobinfo_set() in cons_common */
+
+/* select_p_select_jobinfo_get() in cons_common */
+
+/* select_p_select_jobinfo_copy() in cons_common */
+
+/* select_p_select_jobinfo_pack() in cons_common */
+
+/* select_p_select_jobinfo_unpack() in cons_common */
+
+/* select_p_select_jobinfo_sprint() in cons_common */
+
+/* select_p_select_jobinfo_xstrdup() in cons_common */
+
+/* select_p_get_info_from_plugin() in cons_common */
+
+/* select_p_update_node_config() in cons_common */
+
+/* select_p_update_node_state() in cons_common */
+
+/* select_p_reconfigure() in cons_common */
 
 /* select_p_resv_test() in cons_common */

src/plugins/select/cons_tres/select_cons_tres.c
@@ -88,18 +88,15 @@ const uint16_t nodeinfo_magic = 0x8a5d;
 /* Global variables */
 bitstr_t **spec_core_res	= NULL;
 
-/* Global functions */
-extern select_nodeinfo_t *select_p_select_nodeinfo_alloc(void);
-extern int select_p_select_nodeinfo_free(select_nodeinfo_t *nodeinfo);
+/* Clear from avail_cores all specialized cores */
+static void _spec_core_filter(bitstr_t *node_bitmap, bitstr_t **avail_cores)
+{
+	if (!spec_core_res)
+		return;	/* No specialized cores */
 
-/* Local functions */
-static bitstr_t *_pick_first_cores(bitstr_t *avail_node_bitmap,
-				   uint32_t node_cnt, uint32_t *core_cnt,
-				   bitstr_t ***exc_cores);
-static bitstr_t *_sequential_pick(bitstr_t *avail_node_bitmap,
-				  uint32_t node_cnt, uint32_t *core_cnt,
-				  bitstr_t ***exc_cores);
-static void _spec_core_filter(bitstr_t *node_bitmap, bitstr_t **avail_cores);
+	xassert(avail_cores);
+	core_array_and_not(avail_cores, spec_core_res);
+}
 
 /*
  * Select resources for advanced reservation
@@ -408,16 +405,6 @@ static bitstr_t *_sequential_pick(bitstr_t *avail_node_bitmap,
 	}
 
 	return picked_node_bitmap;
-}
-
-/* Clear from avail_cores all specialized cores */
-static void _spec_core_filter(bitstr_t *node_bitmap, bitstr_t **avail_cores)
-{
-	if (!spec_core_res)
-		return;	/* No specialized cores */
-
-	xassert(avail_cores);
-	core_array_and_not(avail_cores, spec_core_res);
 }
 
 /*

commit 968e9ef417968ed868d4a04f82dce418ca5f75df

cons_res - Fix issue where running jobs were not taken into consideration when creating a reservation.
This fixes a regression from 7ad3636630f
(affecting CoreCnt cons_res reservations)

_spec_core_filter for cons_tres does core_array_and_not since we have a
spec cores array there. In cons_res we have available_cores array here
so we need only core_array_and.

This made it so running jobs were not taken into consideration when
creating a reservation.

Bug 9749

src/plugins/select/cons_res/select_cons_res.c
@@ -150,7 +150,7 @@ static void _spec_core_filter(bitstr_t *node_bitmap, bitstr_t **core_bitmap)
 	xassert(core_bitmap);
 
 	if (*core_bitmap) {
-		core_array_and_not(core_bitmap, p_spec_core_map);
+		core_array_and(core_bitmap, p_spec_core_map);
 	} else {
 		bit_not(*p_spec_core_map);
 		*core_bitmap = *p_spec_core_map;