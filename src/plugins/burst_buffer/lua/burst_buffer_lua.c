fbb1d1f0b28e1171287666105b5f282c8d89cef9
Marshall Garey <marshall@schedmd.com>
2021-04-19 09:55:45 -0600

    Add a burst_buffer/lua plugin.

---
+++ src/plugins/burst_buffer/lua/burst_buffer_lua.c
@@ -0,0 +1,287 @@
+/*****************************************************************************\
+ *  burst_buffer_lua.c - Plugin for managing burst buffers with lua
+ *****************************************************************************
+ *  Copyright (C) 2021 SchedMD LLC.
+ *  Written by Marshall Garey <marshall@schedmd.com>
+ *
+ *  This file is part of Slurm, a resource management program.
+ *  For details, see <https://slurm.schedmd.com/>.
+ *  Please also read the included file: DISCLAIMER.
+ *
+ *  Slurm is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and
+ *  distribute linked combinations including the two. You must obey the GNU
+ *  General Public License in all respects for all of the code used other than
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this
+ *  exception to your version of the file(s), but you are not obligated to do
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in
+ *  the program, then also delete it here.
+ *
+ *  Slurm is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Slurm; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#include "config.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "slurm/slurm.h"
+
+#include "src/slurmctld/slurmctld.h"
+#include "src/plugins/burst_buffer/common/burst_buffer_common.h"
+
+/*
+ * These variables are required by the burst buffer plugin interface.  If they
+ * are not found in the plugin, the plugin loader will ignore it.
+ *
+ * plugin_name - a string giving a human-readable description of the
+ * plugin.  There is no maximum length, but the symbol must refer to
+ * a valid string.
+ *
+ * plugin_type - a string suggesting the type of the plugin or its
+ * applicability to a particular form of data or method of data handling.
+ * If the low-level plugin API is used, the contents of this string are
+ * unimportant and may be anything.  Slurm uses the higher-level plugin
+ * interface which requires this string to be of the form
+ *
+ *      <application>/<method>
+ *
+ * where <application> is a description of the intended application of
+ * the plugin (e.g., "burst_buffer" for Slurm burst_buffer) and <method> is a
+ * description of how this plugin satisfies that application.  Slurm will only
+ * load a burst_buffer plugin if the plugin_type string has a prefix of
+ * "burst_buffer/".
+ *
+ * plugin_version - an unsigned 32-bit integer containing the Slurm version
+ * (major.minor.micro combined into a single number).
+ */
+const char plugin_name[]        = "burst_buffer lua plugin";
+const char plugin_type[]        = "burst_buffer/lua";
+const uint32_t plugin_version   = SLURM_VERSION_NUMBER;
+
+/*
+ * init() is called when the plugin is loaded, before any other functions
+ * are called.  Put global initialization here.
+ */
+extern int init(void)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * fini() is called when the plugin is unloaded. Free all memory.
+ */
+extern int fini(void)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * Return the total burst buffer size in MB
+ */
+extern uint64_t bb_p_get_system_size(void)
+{
+	uint64_t size = 0;
+	return size;
+}
+
+/*
+ * Load the current burst buffer state (e.g. how much space is available now).
+ * Run at the beginning of each scheduling cycle in order to recognize external
+ * changes to the burst buffer state (e.g. capacity is added, removed, fails,
+ * etc.)
+ *
+ * init_config IN - true if called as part of slurmctld initialization
+ * Returns a Slurm errno.
+ */
+extern int bb_p_load_state(bool init_config)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * Return string containing current burst buffer status
+ * argc IN - count of status command arguments
+ * argv IN - status command arguments
+ * RET status string, release memory using xfree()
+ */
+extern char *bb_p_get_status(uint32_t argc, char **argv)
+{
+	return NULL;
+}
+
+/*
+ * Note configuration may have changed. Handle changes in BurstBufferParameters.
+ *
+ * Returns a Slurm errno.
+ */
+extern int bb_p_reconfig(void)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * Pack current burst buffer state information for network transmission to
+ * user (e.g. "scontrol show burst")
+ *
+ * Returns a Slurm errno.
+ */
+extern int bb_p_state_pack(uid_t uid, buf_t *buffer, uint16_t protocol_version)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * Preliminary validation of a job submit request with respect to burst buffer
+ * options. Performed after setting default account + qos, but prior to
+ * establishing job ID or creating script file.
+ *
+ * Returns a Slurm errno.
+ */
+extern int bb_p_job_validate(job_desc_msg_t *job_desc, uid_t submit_uid)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * Secondary validation of a job submit request with respect to burst buffer
+ * options. Performed after establishing job ID and creating script file.
+ *
+ * Returns a Slurm errno.
+ */
+extern int bb_p_job_validate2(job_record_t *job_ptr, char **err_msg)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * Fill in the tres_cnt (in MB) based off the job record
+ * NOTE: Based upon job-specific burst buffers, excludes persistent buffers
+ * IN job_ptr - job record
+ * IN/OUT tres_cnt - fill in this already allocated array with tres_cnts
+ * IN locked - if the assoc_mgr tres read locked is locked or not
+ */
+extern void bb_p_job_set_tres_cnt(job_record_t *job_ptr, uint64_t *tres_cnt,
+				  bool locked)
+{
+}
+
+/*
+ * For a given job, return our best guess if when it might be able to start
+ */
+extern time_t bb_p_job_get_est_start(job_record_t *job_ptr)
+{
+	time_t est_start = time(NULL);
+	return est_start;
+}
+
+/*
+ * Attempt to allocate resources and begin file staging for pending jobs.
+ */
+extern int bb_p_job_try_stage_in(List job_queue)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * Determine if a job's burst buffer stage-in is complete
+ * job_ptr IN - Job to test
+ * test_only IN - If false, then attempt to allocate burst buffer if possible
+ *
+ * RET: 0 - stage-in is underway
+ *      1 - stage-in complete
+ *     -1 - stage-in not started or burst buffer in some unexpected state
+ */
+extern int bb_p_job_test_stage_in(job_record_t *job_ptr, bool test_only)
+{
+	return 1;
+}
+
+/* Attempt to claim burst buffer resources.
+ * At this time, bb_g_job_test_stage_in() should have been run successfully AND
+ * the compute nodes selected for the job.
+ *
+ * Returns a Slurm errno.
+ */
+extern int bb_p_job_begin(job_record_t *job_ptr)
+{
+	return SLURM_SUCCESS;
+}
+
+/* Revoke allocation, but do not release resources.
+ * Executed after bb_p_job_begin() if there was an allocation failure.
+ * Does not release previously allocated resources.
+ *
+ * Returns a Slurm errno.
+ */
+extern int bb_p_job_revoke_alloc(job_record_t *job_ptr)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * Trigger a job's burst buffer stage-out to begin
+ *
+ * Returns a Slurm errno.
+ */
+extern int bb_p_job_start_stage_out(job_record_t *job_ptr)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * Determine if a job's burst buffer post_run operation is complete
+ *
+ * RET: 0 - post_run is underway
+ *      1 - post_run complete
+ *     -1 - fatal error
+ */
+extern int bb_p_job_test_post_run(job_record_t *job_ptr)
+{
+	return 1;
+}
+
+/*
+ * Determine if a job's burst buffer stage-out is complete
+ *
+ * RET: 0 - stage-out is underway
+ *      1 - stage-out complete
+ *     -1 - fatal error
+ */
+extern int bb_p_job_test_stage_out(job_record_t *job_ptr)
+{
+	return 1;
+}
+
+/*
+ * Terminate any file staging and completely release burst buffer resources
+ *
+ * Returns a Slurm errno.
+ */
+extern int bb_p_job_cancel(job_record_t *job_ptr)
+{
+	return SLURM_SUCCESS;
+}
+
+/*
+ * Translate a burst buffer string to it's equivalent TRES string
+ * Caller must xfree the return value
+ */
+extern char *bb_p_xlate_bb_2_tres_str(char *burst_buffer)
+{
+	return NULL;
+}
