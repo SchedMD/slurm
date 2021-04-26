cb8ec6ecd2a6092e89969a49ff8b44f6a5ee31ab
Scott Hilton <scott@schedmd.com>
2021-04-26 14:32:28 -0600

    Add --gpu-bind=per_task:<gpus_per_task> option
    Using this option, each task will be bound to the number of specified gpus.
    Using --gpus-per-task will implictly set this option by default.
    
    Bug 10375

--- src/srun/libsrun/launch.c
+++ src/srun/libsrun/launch.c
@@ -362,6 +362,11 @@ static job_step_create_request_msg_t *_create_job_step_create_request(
 				   opt_local->ntasks_per_gpu);
 	}
 
+	if (!opt_local->tres_bind && opt_local->gpus_per_task) {
+		/* Implicit GPU binding with gpus_per_task */
+		xstrfmtcat(opt_local->tres_bind, "gpu:per_task:%s",
+			   opt_local->gpus_per_task);
+	}
 	step_req->tres_bind = xstrdup(opt_local->tres_bind);
 	step_req->tres_freq = xstrdup(opt_local->tres_freq);
 
