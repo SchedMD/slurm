SchedMD/slurm

commit 4e3cca0161b72733de957c9b3a4e23d78d495a86

Add DEBUG_FLAG_JAG
Bug 10585

src/common/read_config.c
@@ -5224,6 +5224,11 @@ extern char * debug_flags2str(uint64_t debug_flags)
 			xstrcat(rc, ",");
 		xstrcat(rc, "Accrue");
 	}
+	if (debug_flags & DEBUG_FLAG_JAG) {
+		if (rc)
+			xstrcat(rc, ",");
+		xstrcat(rc, "JobAccountGather");
+	}
 	if (debug_flags & DEBUG_FLAG_AGENT) {
 		if (rc)
 			xstrcat(rc, ",");
@@ -5515,6 +5520,9 @@ extern int debug_str2flags(char *debug_flags, uint64_t *flags_out)
 	while (tok) {
 		if (xstrcasecmp(tok, "Accrue") == 0)
 			(*flags_out) |= DEBUG_FLAG_ACCRUE;
+		if (!xstrcasecmp(tok, "JobAccountGather") ||
+		    !xstrcasecmp(tok, "JAG"))
+			(*flags_out) |= DEBUG_FLAG_JAG;
 		else if (xstrcasecmp(tok, "Agent") == 0)
 			(*flags_out) |= DEBUG_FLAG_AGENT;
 		else if (xstrcasecmp(tok, "Backfill") == 0)

doc/man/man5/slurm.conf.5
@@ -675,6 +675,9 @@ Heterogeneous job details
 \fBGang\fR
 Gang scheduling details
 .TP
+\fBJobAccountGather\fR
+Common job account gathering details (not plugin specific).
+.TP
 \fBJobContainer\fR
 Job container plugin details
 .TP

slurm/slurm.h.in
@@ -2644,6 +2644,7 @@ typedef struct reservation_name_msg {
 #define DEBUG_FLAG_POWER_SAVE   0x0008000000000000 /* Power Save debug */
 #define DEBUG_FLAG_AGENT	0x0010000000000000 /* RPC Agent debug */
 #define DEBUG_FLAG_DEPENDENCY	0x0020000000000000 /* Dependency debug */
+#define DEBUG_FLAG_JAG		0x0040000000000000 /* Job Account Gather debug */
 
 #define PREEMPT_MODE_OFF	0x0000	/* disable job preemption */
 #define PREEMPT_MODE_SUSPEND	0x0001	/* suspend jobs to preempt */

commit 61accccef5b5ed57ff3f9035fee3dc322ebd4c96

Fix DebugFlags=Accrue - regression from commit 4e3cca0161b
Commit 4e3cca0161b incorrectly added a DebugFlag such that the accrue
debug flag will always error:

slurmctld: error: Invalid DebugFlag: accrue
slurmctld: error: DebugFlags invalid: accrue
slurmctld: fatal: Unable to process configuration file

This commit makes accrue not error anymore and alphabetizes the option.

Bug 11757

src/common/read_config.c
@@ -5434,9 +5434,6 @@ extern int debug_str2flags(char *debug_flags, uint64_t *flags_out)
 	while (tok) {
 		if (xstrcasecmp(tok, "Accrue") == 0)
 			(*flags_out) |= DEBUG_FLAG_ACCRUE;
-		if (!xstrcasecmp(tok, "JobAccountGather") ||
-		    !xstrcasecmp(tok, "JAG"))
-			(*flags_out) |= DEBUG_FLAG_JAG;
 		else if (xstrcasecmp(tok, "Agent") == 0)
 			(*flags_out) |= DEBUG_FLAG_AGENT;
 		else if (xstrcasecmp(tok, "Backfill") == 0)
@@ -5499,6 +5496,9 @@ extern int debug_str2flags(char *debug_flags, uint64_t *flags_out)
 			(*flags_out) |= DEBUG_FLAG_INTERCONNECT;
 		else if (xstrcasecmp(tok, "Filesystem") == 0)
 			(*flags_out) |= DEBUG_FLAG_FILESYSTEM;
+		else if (!xstrcasecmp(tok, "JobAccountGather") ||
+			 !xstrcasecmp(tok, "JAG"))
+			(*flags_out) |= DEBUG_FLAG_JAG;
 		else if (xstrcasecmp(tok, "JobContainer") == 0)
 			(*flags_out) |= DEBUG_FLAG_JOB_CONT;
 		else if (xstrcasecmp(tok, "License") == 0)