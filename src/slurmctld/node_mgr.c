d45152bbf0e03516b2a78459ce8c3388b0f09f79
Marcin Stolarek <cinek@schedmd.com>
2021-06-10 11:20:47 +0000

    Fix formatting
    Bug 11802

--- src/slurmctld/node_mgr.c
+++ src/slurmctld/node_mgr.c
@@ -773,10 +773,10 @@ static bool _node_is_hidden(node_record_t *node_ptr, uid_t uid)
 {
 	int i;
 
-	if ((slurm_conf.private_data & PRIVATE_DATA_NODES)
-	    && (slurm_mcs_get_privatedata() == 1)
-	    && !validate_operator(uid)
-	    && (mcs_g_check_mcs_label(uid, node_ptr->mcs_label) != 0))
+	if ((slurm_conf.private_data & PRIVATE_DATA_NODES) &&
+	    (slurm_mcs_get_privatedata() == 1) &&
+	    !validate_operator(uid) &&
+	    (mcs_g_check_mcs_label(uid, node_ptr->mcs_label) != 0))
 		return true;
 
 	if (!node_ptr->part_cnt)
