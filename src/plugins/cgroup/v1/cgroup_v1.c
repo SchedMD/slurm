8315e291cd6337f668f82672d7ceebbd2c0efa70
Danny Auble <da@schedmd.com>
2021-06-11 15:11:15 -0600

    Only force_empty if running jobacct_gather/cgroup as done before.
    It turns out setting memory.force_empty adds a large delay ~2 sec.
    Before this version of the code we only would set this in the
    jobacct_gather/cgroup plugin.  This puts things back to that.

--- src/plugins/cgroup/v1/cgroup_v1.c
+++ src/plugins/cgroup/v1/cgroup_v1.c
@@ -634,9 +634,14 @@ extern int cgroup_p_step_destroy(cgroup_ctl_type_t sub)
 		 * writting to 'force_empty'. Note that when
 		 * memory.kmem.limit_in_bytes is set the charges due to kernel
 		 * pages will still be seen.
+		 *
+		 * Since this adds a large delay (~2 sec) only do this if
+		 * running jobacct_gather/cgroup.
 		 */
-		common_cgroup_set_param(&g_step_cg[CG_MEMORY],
-					"memory.force_empty", "1");
+		if (!xstrcmp(slurm_conf.job_acct_gather_type,
+		             "jobacct_gather/cgroup"))
+			common_cgroup_set_param(&g_step_cg[CG_MEMORY],
+						"memory.force_empty", "1");
 		break;
 	case CG_DEVICES:
 		break;
