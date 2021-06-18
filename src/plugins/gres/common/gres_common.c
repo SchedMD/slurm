7477a1ab357af13c2ab964e3ecdeb387e7358508
Michael Hinton <hinton@schedmd.com>
2021-06-18 12:50:21 -0600

    GRES: Print out UniqueID
    Useful for debugging, and needed for test 39.18 to test MIGs/unique_id.
    
    Bug 10970
    
    Co-authored-by: Danny Auble <da@schedmd.com>

--- src/plugins/gres/common/gres_common.c
+++ src/plugins/gres/common/gres_common.c
@@ -428,12 +428,12 @@ extern void print_gres_conf(gres_slurmd_conf_t *gres_slurmd_conf,
 			    log_level_t log_lvl)
 {
 	log_var(log_lvl, "    GRES[%s] Type:%s Count:%"PRIu64" Cores(%d):%s  "
-		"Links:%s Flags:%s File:%s", gres_slurmd_conf->name,
+		"Links:%s Flags:%s File:%s UniqueId:%s", gres_slurmd_conf->name,
 		gres_slurmd_conf->type_name, gres_slurmd_conf->count,
 		gres_slurmd_conf->cpu_cnt, gres_slurmd_conf->cpus,
 		gres_slurmd_conf->links,
 		gres_flags2str(gres_slurmd_conf->config_flags),
-		gres_slurmd_conf->file);
+		gres_slurmd_conf->file, gres_slurmd_conf->unique_id);
 }
 
 
@@ -444,11 +444,14 @@ extern void print_gres_conf(gres_slurmd_conf_t *gres_slurmd_conf,
 static void _print_gres_conf_parsable(gres_slurmd_conf_t *gres_slurmd_conf,
 				      log_level_t log_lvl)
 {
-	log_var(log_lvl, "GRES_PARSABLE[%s](%"PRIu64"):%s|%d|%s|%s|%s|",
+	/* Only print out unique_id if set */
+	log_var(log_lvl, "GRES_PARSABLE[%s](%"PRIu64"):%s|%d|%s|%s|%s|%s%s",
 		gres_slurmd_conf->name, gres_slurmd_conf->count,
 		gres_slurmd_conf->type_name, gres_slurmd_conf->cpu_cnt,
 		gres_slurmd_conf->cpus, gres_slurmd_conf->links,
-		gres_slurmd_conf->file);
+		gres_slurmd_conf->file,
+		gres_slurmd_conf->unique_id ? gres_slurmd_conf->unique_id : "",
+		gres_slurmd_conf->unique_id ? "|" : "");
 }
 
 /*
