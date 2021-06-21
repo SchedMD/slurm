acc3012a70c736a2997306993f0b52e89e4fb539
Nate Rini <nate@schedmd.com>
2021-06-21 12:22:09 -0600

    Swap %h to %n in DEFAULT_OCI_CONTAINER_ID_PATTERN

--- src/common/read_config.h
+++ src/common/read_config.h
@@ -127,7 +127,7 @@ extern uint16_t drop_priv_flag;
 #define DEFAULT_MSG_AGGR_WINDOW_MSGS 1
 #define DEFAULT_MSG_AGGR_WINDOW_TIME 100
 #define DEFAULT_MSG_TIMEOUT         10
-#define DEFAULT_OCI_CONTAINER_ID_PATTERN "%h.%u.%j.%s.%t"
+#define DEFAULT_OCI_CONTAINER_ID_PATTERN "%n.%u.%j.%s.%t"
 #define DEFAULT_OCI_RUNTIME         "/usr/sbin/runc"
 #define DEFAULT_POWER_PLUGIN        ""
 #if defined WITH_CGROUP
