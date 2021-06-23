5fe924b977f2677c49a1344333385920a7f35b9d
Danny Auble <da@schedmd.com>
2021-06-23 12:18:01 -0600

    Remove unneeded error.
    This is possible and slurm_opt_create_job_desc() should print out
    the real error.

--- src/srun/libsrun/allocate.c
+++ src/srun/libsrun/allocate.c
@@ -729,7 +729,6 @@ static job_desc_msg_t *_job_desc_msg_create_from_opts(slurm_opt_t *opt_local)
 	job_desc_msg_t *j = slurm_opt_create_job_desc(opt_local);
 
 	if (!j) {
-		error("No job_desc_msg_t created. This should never happen");
 		return NULL;
 	}
 
