SchedMD/slurm

commit 59f9f78dd2721118e1bebdd67155f27427f4fd15

Fix segfault in sacct after 15ddc70b482.
log_build_step_id_str doesn't handle NULL's very well.

Bug 11270

src/sacct/print.c
@@ -987,6 +987,7 @@ extern void print_fields(type_t type, void *object)
 				tmp_char = xstrdup_printf("%u", job->jobid);
 				break;
 			case JOBSTEP:
+				id = xmalloc(FORMAT_STRING_SIZE);
 				log_build_step_id_str(&step->step_id, id,
 						      sizeof(id),
 						      (STEP_ID_FLAG_NO_PREFIX |
@@ -994,6 +995,7 @@ extern void print_fields(type_t type, void *object)
 				tmp_char = xstrdup_printf("%u.%s",
 							  step->job_ptr->jobid,
 							  id);
+				xfree(id);
 				break;
 			case JOBCOMP:
 				tmp_char = xstrdup_printf("%u",

commit cf669058bf71c923b058744a29f793d4a7deacf3

sacct - fix truncation when printing jobidraw field.
Wrong expression - sizeof(id) is 8, instead of FORMAT_STRING_SIZE.

Regression from 59f9f78dd2.

CID 220383.

src/sacct/print.c
@@ -995,7 +995,7 @@ extern void print_fields(type_t type, void *object)
 			case JOBSTEP:
 				id = xmalloc(FORMAT_STRING_SIZE);
 				log_build_step_id_str(&step->step_id, id,
-						      sizeof(id),
+						      FORMAT_STRING_SIZE,
 						      (STEP_ID_FLAG_NO_PREFIX |
 						       STEP_ID_FLAG_NO_JOB));
 				tmp_char = xstrdup_printf("%u.%s",