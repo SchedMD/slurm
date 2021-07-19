SchedMD/slurm

commit 9412a883f15b64377541987899208a4106d06b1e

Avoid memory leak by switching to data_set_string_own().
CID 220291.

src/plugins/openapi/dbv0.0.36/parse.c
@@ -2241,7 +2241,7 @@ static int _foreach_step(void *x, void *arg)
 		hostlist_iterator_t itr = hostlist_iterator_create(host_list);
 
 		while ((host = hostlist_next(itr)))
-			data_set_string(data_list_append(d), host);
+			data_set_string_own(data_list_append(d), host);
 
 		hostlist_iterator_destroy(itr);
 	}

commit 8cc03d03fa731d234052d2fed9bc903bebe35dad

openapi/dbv0.0.36 - need host as xstring in _foreach_step()
hostlist_next() does not use xstrdup(), which makes it incompatible with
data_set_string_own(). Regression from 9412a883f1.

Bug 12062

src/plugins/openapi/dbv0.0.36/parse.c
@@ -2240,8 +2240,10 @@ static int _foreach_step(void *x, void *arg)
 			data_define_dict_path(dstep, "nodes/list"));
 		hostlist_iterator_t itr = hostlist_iterator_create(host_list);
 
-		while ((host = hostlist_next(itr)))
-			data_set_string_own(data_list_append(d), host);
+		while ((host = hostlist_next(itr))) {
+			data_set_string(data_list_append(d), host);
+			free(host);
+		}
 
 		hostlist_iterator_destroy(itr);
 	}