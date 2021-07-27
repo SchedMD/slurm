SchedMD/slurm

commit 8bdc304bd27ee9776ab0aefa2c0ede8994a1ef34

Jobs that request the whole node check to see if any gres are allocated.
Bug 9887

src/common/gres.c
@@ -4285,6 +4285,23 @@ extern void gres_node_state_log(List gres_list, char *node_name)
 	slurm_mutex_unlock(&gres_context_lock);
 }
 
+/* Find node_state_t gres record with any allocated gres (key is unused) */
+static int _find_node_state_with_alloc_gres(void *x, void *key)
+{
+	gres_state_t *gres_ptr = (gres_state_t *) x;
+
+	if (((gres_node_state_t *) gres_ptr->gres_data)->gres_cnt_alloc)
+		return 1;
+	else
+		return 0;
+}
+
+extern bool gres_node_state_list_has_alloc_gres(List gres_list)
+{
+	return list_find_first(gres_list,
+			       _find_node_state_with_alloc_gres, NULL);
+}
+
 /*
  * Build a string indicating a node's drained GRES
  * IN gres_list - generated by gres_node_config_validate()

doc/man/man5/slurm.conf.5
@@ -1963,6 +1963,10 @@ higher PriorityTier partitions will suspend jobs in lower PriorityTier
 partitions to run on the released resources. Only when the preemptor job ends
 will the suspended jobs will be resumed by the Gang scheduler.
 .br
+
+\fBNOTE:\fR Suspended jobs will not release GRES. Higher priority jobs will not
+be able to preempt to gain access to GRES.
+.br
 If \fBPreemptType=preempt/qos\fR is configured and if the preempted job(s) and
 the preemptor job are on the same partition, then they will share resources with
 the Gang scheduler (time-slicing). If not (i.e. if the preemptees and preemptor
@@ -5347,8 +5351,8 @@ Jobs that run in partitions with \fBOverSubscribe=EXCLUSIVE\fR will have
 exclusive access to all allocated nodes.
 .TP
 \fBFORCE\fR
-Makes all resources in the partition available for oversubscription without
-any means for users to disable it.
+Makes all resources (except GRES) in the partition available for
+oversubscription without any means for users to disable it.
 May be followed with a colon and maximum number of jobs in
 running or suspended state.
 For example \fBOverSubscribe=FORCE:4\fR enables each node, socket or
@@ -5374,8 +5378,8 @@ in the slurmdbd log stating: "We have more allocated time than is possible."
 
 .TP
 \fBYES\fR
-Makes all resources in the partition available for sharing upon request by
-the job.
+Makes all resources (except GRES) in the partition available for sharing upon
+request by the job.
 Resources will only be over\-subscribed when explicitly requested
 by the user using the "\-\-oversubscribe" option on job submission.
 May be followed with a colon and maximum number of jobs in

src/common/gres.h
@@ -558,6 +558,9 @@ extern void gres_node_state_dealloc_all(List gres_list);
  */
 extern void gres_node_state_log(List gres_list, char *node_name);
 
+/* Return true if any node_state_t has gres_cnt_alloc greater than 0 */
+extern bool gres_node_state_list_has_alloc_gres(List gres_list);
+
 /*
  * Build a string indicating a node's drained GRES
  * IN gres_list - generated by gres_node_config_validate()

src/plugins/select/cons_common/job_test.c
@@ -714,6 +714,14 @@ static int _verify_node_state(part_res_record_t *cr_part_ptr,
 			gres_list = node_usage[i].gres_list;
 		else
 			gres_list = node_ptr->gres_list;
+
+		if ((job_ptr->details->whole_node == WHOLE_NODE_REQUIRED) &&
+		    gres_node_state_list_has_alloc_gres(gres_list)) {
+			debug3("node %s has GRES in use (whole node requested)",
+			       node_ptr->name);
+			goto clear_bit;
+		}
+
 		gres_cores = gres_job_test(job_ptr->gres_list_req,
 					   gres_list, true,
 					   NULL, 0, 0, job_ptr->job_id,

commit 8e4c09884de15b0f9118317b552c77104eeb3405

Fix NULL pointer dereference
Regression introduced in 8bdc304bd27

Bug 12112

src/common/gres.c
@@ -4426,6 +4426,9 @@ static int _find_node_state_with_alloc_gres(void *x, void *key)
 
 extern bool gres_node_state_list_has_alloc_gres(List gres_list)
 {
+	if (!gres_list)
+		return false;
+
 	return list_find_first(gres_list,
 			       _find_node_state_with_alloc_gres, NULL);
 }