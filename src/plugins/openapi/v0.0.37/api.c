892f9b98f6667db99a559c9a6ba1754f0f3609d4
Nate Rini <nate@schedmd.com>
2021-08-09 09:58:36 -0600

    openapi/v0.0.37 - replace "REST" with "Slurm OpenAPI" for plugin_name
    Bug 12232

--- src/plugins/openapi/v0.0.37/api.c
+++ src/plugins/openapi/v0.0.37/api.c
@@ -78,7 +78,7 @@
  * plugin_version - an unsigned 32-bit integer containing the Slurm version
  * (major.minor.micro combined into a single number).
  */
-const char plugin_name[] = "REST v0.0.37";
+const char plugin_name[] = "Slurm OpenAPI v0.0.37";
 const char plugin_type[] = "openapi/v0.0.37";
 const uint32_t plugin_id = 100;
 const uint32_t plugin_version = SLURM_VERSION_NUMBER;
