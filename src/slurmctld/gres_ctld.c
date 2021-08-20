SchedMD/slurm

commit 7e9a19a873574157184daa66f4f73839a084cbc8

Avoid putting gres with count of 0 on a TRES req/alloc.
Fix issue where list entries with 0 gres were made on steps

i.e. gres/gpu:k80=1,gres/gpu:tesla=0,gres/gpu=1

salloc --exclusive (where there are multiple gpu types on the node)
srun --gpus=1 hostname

That previously would give you a tres line like above.  This fixes it
so the 0 isn't given.

src/slurmctld/gres_ctld.c
@@ -2005,7 +2005,8 @@ static int _step_alloc_type(gres_state_t *job_gres_ptr,
 	gres_step_state_t *step_alloc_data_ptr;
 
 	/* This isn't the gres we are looking for */
-	if (!gres_find_job_by_key_with_cnt(job_gres_ptr, args->job_search_key))
+	if (!args->gres_needed ||
+	    !gres_find_job_by_key_with_cnt(job_gres_ptr, args->job_search_key))
 		return 0;
 
 	step_alloc_data_ptr = _step_get_alloc_gres_ptr(

commit 73885d946e72f705faef78061f30751e28dc18bc

Check max_gres when doing step allocation, fix for regression in rc2.
Fix step gres allocation issue introduced in 7e9a19a873

Bug 12152

src/slurmctld/gres_ctld.c
@@ -2005,7 +2005,7 @@ static int _step_alloc_type(gres_state_t *job_gres_ptr,
 	gres_step_state_t *step_alloc_data_ptr;
 
 	/* This isn't the gres we are looking for */
-	if (!args->gres_needed ||
+	if ((!args->gres_needed && !args->max_gres) ||
 	    !gres_find_job_by_key_with_cnt(job_gres_ptr, args->job_search_key))
 		return 0;
 