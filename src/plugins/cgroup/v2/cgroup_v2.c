0ffc39056ab8569214457693b9f48856c8bbc302
Felip Moll <felip.moll@schedmd.com>
2021-08-31 14:32:33 +0200

    Implement initialization functions in cgroup/v2
    In v2, initializing the plugin from slurmd will create the main directories
    in the cgroup tree. The tree is pictured in the cgroup_v2.c file. We need
    to respect cgroup unified rules:
    
    - No internal process constraint
    - Top-down constraint
    
    And also, we need to enable the subtree_control to non-leaf directories in
    order to enable the controllers. By default we will enable all available
    controllers.
    
    Note that in order to coexist with systemd and be able to restart slurmd
    while keeping steps alive, we need to completely separate the stepds into
    a different cgroup, so we separate slrumd from slurmstepds and inform
    systemd about the change. We do so by a call to Dbus to talk to systemd.
    
    Bug 12255

--- src/plugins/cgroup/v2/cgroup_v2.c
+++ src/plugins/cgroup/v2/cgroup_v2.c
@@ -38,9 +38,247 @@
 
 #include "cgroup_v2.h"
 
+#define SYSTEM_CGSLICE "system.slice"
+#define SYSTEM_CGSCOPE "slurmstepd_home.scope"
+#define SYSTEM_CGDIR "system"
+
 const char plugin_name[] = "Cgroup v2 plugin";
 const char plugin_type[] = "cgroup/v2";
 const uint32_t plugin_version = SLURM_VERSION_NUMBER;
+
+/* Internal cgroup structs */
+static List task_list;
+static uint16_t step_active_cnt;
+static xcgroup_ns_t int_cg_ns;
+static xcgroup_t int_cg[CG_LEVEL_CNT];
+static bitstr_t *avail_controllers = NULL;
+static bitstr_t *enabled_controllers = NULL;
+static char *ctl_names[] = {
+	[CG_TRACK] = "freezer",
+	[CG_CPUS] = "cpuset",
+	[CG_MEMORY] = "memory",
+	[CG_CPUACCT] = "cpu",
+};
+
+typedef struct {
+	xcgroup_t task_cg;
+	uint32_t taskid;
+} task_cg_info_t;
+
+/* Hierarchy will take this form:
+ *        [int_cg_ns]             [int_cg_ns]
+ *      "slurmd service"       "slurmtepds scope"
+ *      root(delegated)         root(delegated) [CG_LEVEL_ROOT]
+ *		|	       /	      \
+ *		|	      /		      |
+ *           slurmd          |         job_x ... job_n [CG_LEVEL_JOB]
+ *                         system             |
+ *                      (waiting area         |
+ *                      for new stepds)       |
+ *		                          step_0 ... step_n [CG_LEVEL_STEP]
+ *                                         /   \
+ *      [CG_LEVEL_STEP_USER] user_processes     slurm_processes [CG_LEVEL_STEP_SLURM]
+ *			         /               (slurmstepds)
+ *			        /
+ *                             |
+ *                    task_special...task_0...task_n [CG_LEVEL_TASK] (user pids)
+ *                 (task_id = NO_VAL)
+ */
+
+/*
+ * Fill up the internal cgroup namespace object. This mainly contains the path
+ * to the root.
+ *
+ * The cgroup v2 documented way to know which is the process root in the cgroup
+ * hierarchy is just to read /proc/self/cgroup. In Unified hierarchies this
+ * must contain only one line. If there are more lines this would mean we are
+ * in Hybrid or in Legacy cgroup.
+ */
+static void _set_int_cg_ns()
+{
+	char *buf, *start = NULL, *p;
+	size_t sz;
+	struct stat st;
+
+	/* We already know where we will live if we're stepd. */
+	if (running_in_slurmstepd()) {
+		xstrfmtcat(int_cg_ns.mnt_point, "%s/%s/%s_%s",
+			   slurm_cgroup_conf.cgroup_mountpoint, SYSTEM_CGSLICE,
+			   conf->node_name, SYSTEM_CGSCOPE);
+		if (stat(int_cg_ns.mnt_point, &st) < 0) {
+			error("cannot read cgroup path %s: %m",
+			      int_cg_ns.mnt_point);
+			xfree(int_cg_ns.mnt_point);
+		}
+		return;
+	}
+
+	if (common_file_read_content("/proc/self/cgroup", &buf, &sz)
+	    != SLURM_SUCCESS)
+		fatal("cannot read /proc/self/cgroup contents: %m");
+
+	/*
+	 * In Unified mode there will be just one line containing the path
+	 * of the cgroup, so get it as our root and replace the \n:
+	 * "0::/system.slice/slurmd<nodename>.service\n"
+	 *
+	 * The final path will look like this:
+	 * /sys/fs/cgroup/system.slice/slurmd.service/
+	 *
+	 * If we have multiple slurmd, we will likely have one unit file per
+	 * node, and the path takes the name of the service file, e.g:
+	 * /sys/fs/cgroup/system.slice/slurmd-<nodename>.service/
+	 */
+	if ((p = xstrchr(buf, ':')) != NULL) {
+		if ((p + 2) < (buf + sz - 1))
+			start = p + 2;
+	}
+
+	if (start && *start != '\0') {
+		if ((p = xstrchr(start, '\n')))
+			*p = '\0';
+		xstrfmtcat(int_cg_ns.mnt_point, "%s%s",
+			   slurm_cgroup_conf.cgroup_mountpoint, start);
+	}
+
+	xfree(buf);
+}
+
+/*
+ * For each available controller, enable it in this path. This operation is
+ * only intended to be done in the Domain controllers, never in a leaf where
+ * processes reside. If it is done in a leaf it *won't be possible* to add any
+ * pid to it. Enabling the controllers will make their interfaces available
+ * (e.g. the memory.*, cpu.*, cpuset.* ... files) to control the cgroup.
+ */
+static int _enable_subtree_control(xcgroup_t *cg)
+{
+	int i, rc = SLURM_SUCCESS;
+	char *param = NULL;
+
+	for (i = 0; i < CG_CTL_CNT; i++) {
+		if (bit_test(avail_controllers, i)) {
+			xstrfmtcat(param, "+%s", ctl_names[i]);
+			rc = common_cgroup_set_param(cg,
+						     "cgroup.subtree_control",
+						     param);
+			xfree(param);
+			if (rc != SLURM_SUCCESS) {
+				error("Cannot enable %s in %s/cgroup.subtree_control",
+				      ctl_names[i], cg->path);
+				bit_clear(avail_controllers, i);
+				rc = SLURM_ERROR;
+			} else {
+				log_flag(CGROUP, "Enabled %s controller in %s",
+					 ctl_names[i], cg->path);
+				bit_set(enabled_controllers, i);
+			}
+		}
+	}
+
+	return rc;
+}
+
+/*
+ * Read the cgroup.controllers file of the root to detect which are the
+ * available controllers in this system.
+ */
+static int _check_avail_controllers()
+{
+	char *buf, *ptr, *save_ptr, *ctl_filepath = NULL;
+	size_t sz;
+
+	xstrfmtcat(ctl_filepath, "%s/cgroup.controllers", int_cg_ns.mnt_point);
+	if (common_file_read_content(ctl_filepath, &buf, &sz) !=
+	    SLURM_SUCCESS || !buf) {
+		error("cannot read %s: %m", ctl_filepath);
+		xfree(ctl_filepath);
+		return SLURM_ERROR;
+	}
+	xfree(ctl_filepath);
+
+	ptr = strtok_r(buf, " ", &save_ptr);
+	while (ptr) {
+		for (int i = 0; i < CG_CTL_CNT; i++) {
+			if (!xstrcmp(ctl_names[i], ""))
+				continue;
+			if (!xstrcasecmp(ctl_names[i], ptr))
+				bit_set(avail_controllers, i);
+		}
+		ptr = strtok_r(NULL, " ", &save_ptr);
+	}
+	xfree(buf);
+
+	/* Field not used in v2 */
+	int_cg_ns.subsystems = NULL;
+
+	return SLURM_SUCCESS;
+}
+
+static void _free_task_cg_info(void *x)
+{
+	task_cg_info_t *task_cg = (task_cg_info_t *)x;
+
+	if (task_cg) {
+		common_cgroup_destroy(&task_cg->task_cg);
+		xfree(task_cg);
+	}
+}
+
+/*
+ * Talk with systemd through dbus to create a new scope where we will put all
+ * the slurmstepds and user processes. This way we can safely restart slurmd
+ * and not affect jobs at all.
+ * Technically it must do:
+ *	- Start a new transient scope with Delegate=yes and all controllers.
+ *      - Create a new system/ directory under it.
+ */
+static void _create_new_scope(const char *slice, const char *scope,
+			      const char *dir)
+{
+	char *scope_path = NULL;
+	char *full_path = NULL;
+
+	xstrfmtcat(scope_path, "/sys/fs/cgroup/%s/%s_%s",
+		   slice, conf->node_name, scope);
+	xstrfmtcat(full_path, "%s/%s", scope_path, dir);
+
+	// Don't fail if it already exists.
+	mkdir(scope_path, O_CREAT);
+	mkdir(full_path, 0755);
+
+	xfree(scope_path);
+	xfree(full_path);
+}
+
+/*
+ * Talk with systemd through dbus to move the slurmstepd pid into the reserved
+ * scope for stepds and user processes.
+ */
+static int _move_pid_to_scope(const char *slice, const char *scope,
+			      const char *dir, pid_t pid)
+{
+	char *scope_path = NULL;
+	char *dir_path = NULL;
+
+	xstrfmtcat(scope_path, "/sys/fs/cgroup/%s/%s_%s",
+		   slice, conf->node_name, scope);
+	xstrfmtcat(dir_path, "/%s", dir);
+
+	common_cgroup_create(&int_cg_ns, &int_cg[CG_LEVEL_SYSTEM],
+			     dir_path, (uid_t) 0, (gid_t) 0);
+	common_cgroup_move_process(&int_cg[CG_LEVEL_SYSTEM], pid);
+
+	if (_enable_subtree_control(&int_cg[CG_LEVEL_ROOT]) != SLURM_SUCCESS) {
+		error("Cannot enable subtree_control at the top level %s",
+		      int_cg_ns.mnt_point);
+		return SLURM_ERROR;
+	}
+
+	xfree(scope_path);
+	xfree(dir_path);
+	return SLURM_SUCCESS;
+}
 
 /*
  * Initialize the cgroup plugin. Slurmd MUST be started by systemd and the
@@ -68,12 +306,80 @@ const uint32_t plugin_version = SLURM_VERSION_NUMBER;
  */
 extern int init(void)
 {
+	avail_controllers = bit_alloc(CG_CTL_CNT);
+	enabled_controllers = bit_alloc(CG_CTL_CNT);
+	step_active_cnt = 0;
+	FREE_NULL_LIST(task_list);
+	task_list = list_create(_free_task_cg_info);
+
+	/*
+	 * If we are slurmd we need to create a new place for forked stepds to
+	 * give them its independence. If we don't do that, a slurmd restart
+	 * through systemd would not succeed because the cgroup would be busy
+	 * and systemd would fail to place the new slurmd in the cgroup.
+	 */
+	if (running_in_slurmd())
+		_create_new_scope(SYSTEM_CGSLICE, SYSTEM_CGSCOPE, SYSTEM_CGDIR);
+
+	/*
+	 * Check our current root dir. Systemd MUST have Delegated it to us,
+	 * so we want slurmd to be started by systemd
+	 */
+	_set_int_cg_ns();
+	if (!int_cg_ns.mnt_point) {
+		error("Cannot setup the cgroup namespace.");
+		return SLURM_ERROR;
+	}
+
+	/*
+	 * Check available controllers in cgroup.controller and record them in
+	 * our bitmap.
+	 */
+	if (_check_avail_controllers() != SLURM_SUCCESS)
+		return SLURM_ERROR;
+
+	/* Setup the root cgroup object. */
+	common_cgroup_create(&int_cg_ns, &int_cg[CG_LEVEL_ROOT], "",
+			     (uid_t) 0, (gid_t) 0);
+
+	/*
+	 * If we are slurmstepd we are living in slurmd's place. We need first
+	 * to emancipate to our new place and tell systemd about it or we will
+	 * mess its accounting.
+	 */
+	if (running_in_slurmstepd())
+		_move_pid_to_scope(SYSTEM_CGSLICE, SYSTEM_CGSCOPE, SYSTEM_CGDIR,
+				   getpid());
+
+	/*
+	 * If we're slurmd we're all set and able to constrain things, i.e.
+	 * CoreSpec* and MemSpec*.
+	 *
+	 * If we are a new slurmstepd we are ready now to create job steps. In
+	 * that case, since we're still living in slurmd's place, we will need
+	 * to emancipate to the slurmd_family cgroup, and then create
+	 * int_cg[CG_LEVEL_ROOT].path/job_x/step_x. Per each new step we'll need
+	 * to first move the stepd process out of slurmd directory where we
+	 * still live.
+	 */
 	debug("%s loaded", plugin_name);
 	return SLURM_SUCCESS;
 }
 
 extern int fini(void)
 {
+	/*
+	 * Clear up the namespace and cgroups memory. Don't rmdir anything since
+	 * we may not be stopping yet. When the process terminates systemd will
+	 * remove the remaining directories.
+	 */
+	FREE_NULL_BITMAP(avail_controllers);
+	FREE_NULL_BITMAP(enabled_controllers);
+	common_cgroup_destroy(&int_cg[CG_LEVEL_SYSTEM]);
+	common_cgroup_destroy(&int_cg[CG_LEVEL_ROOT]);
+	common_cgroup_ns_destroy(&int_cg_ns);
+	FREE_NULL_LIST(task_list);
+
 	debug("unloading %s", plugin_name);
 	return SLURM_SUCCESS;
 }
@@ -88,6 +394,14 @@ extern int fini(void)
  */
 extern int cgroup_p_initialize(cgroup_ctl_type_t ctl)
 {
+	switch (ctl) {
+	case CG_DEVICES:
+		/* initialize_and_set_ebpf_program() */
+		break;
+	default:
+		break;
+	}
+
 	return SLURM_SUCCESS;
 }
 
