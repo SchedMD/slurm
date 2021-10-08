SchedMD/slurm

commit 3bd0778f9ad610cb37cd0717d556d0d006f1fa1a

gres/gpu - Avoid stripping unmatched GRES type
If others GRES have type in _normalize_sys_gres_types(), then avoid
stripping the unmatched GRES types to ensure the logs include the full
name to allow easier configuration.

Bug 12126

src/plugins/gres/gpu/gres_gpu.c
@@ -228,6 +228,19 @@ static int _find_type_in_gres_list(void *x, void *key)
 		return 0;
 }
 
+static int _find_nonnull_type_in_gres_list(void *x, void *key)
+{
+	gres_slurmd_conf_t *conf_gres = (gres_slurmd_conf_t *) x;
+
+	if (!conf_gres)
+		return 0;
+
+	if (conf_gres->type_name && conf_gres->type_name[0])
+		return 1;
+
+	return 0;
+}
+
 /*
  * Sync the GRES type of each device detected on the system (gres_list_system)
  * to its corresponding GRES type specified in [gres|slurm].conf. In effect, the
@@ -241,10 +254,20 @@ static void _normalize_sys_gres_types(List gres_list_system,
 {
 	gres_slurmd_conf_t *sys_gres, *conf_gres;
 	ListIterator itr;
+	bool strip_type = true;
 
 	/* No need to sync anything if configured GRES list is empty */
 	if (!gres_list_conf_single || list_count(gres_list_conf_single) == 0)
 		return;
+
+	/*
+	 * Determine if any of the existing GRES have their types defined. If
+	 * have a type, then all GRES must have a type defined and stripping the
+	 * type is not helpful
+	 */
+	if (list_find_first(gres_list_conf_single,
+			    _find_nonnull_type_in_gres_list, NULL))
+		strip_type = false;
 
 	/*
 	 * Sort conf and sys gres lists by longest GRES type to shortest, so we
@@ -260,10 +283,12 @@ static void _normalize_sys_gres_types(List gres_list_system,
 					    _find_type_in_gres_list,
 					    sys_gres->type_name);
 		if (!conf_gres) {
-			info("Could not find an unused configuration record with a GRES type that is a substring of system device `%s`. Setting system GRES type to NULL",
-			     sys_gres->type_name);
-			xfree(sys_gres->type_name);
-			sys_gres->config_flags &= ~GRES_CONF_HAS_TYPE;
+			if (strip_type) {
+				info("Could not find an unused configuration record with a GRES type that is a substring of system device `%s`. Setting system GRES type to NULL",
+				     sys_gres->type_name);
+				xfree(sys_gres->type_name);
+				sys_gres->config_flags &= ~GRES_CONF_HAS_TYPE;
+			}
 			continue;
 		}
 

commit 5db3bbca15e1a14a04f3d7e65e2e0c187ac49a0c

gres/gpu - fix configured/system-detected GRES match for some cases.
Before, we were sorting GPUs by File in descending order right before doing
conf-to-system matching. This was so that config GPUs with a NULL File would
have the lowest matching priority.

However, after commit 3bd0778f9a, this caused issues because
_normalize_sys_gres_types() was no longer clearing out the type, causing all
GPUs to be available during matching and making the intermediate descending
order relevant to the selection. This was a problem when there were more
system GPUs than config GPUs. So when we sorted GPUs in descending GPU File
order before matching, we would end up selecting GPUs from the opposite end
of the list than expected, and leave the first GPUs as unused instead of the
last GPUs.

We want to sort GPUs according to File in ascending order, but we also want
NULL File GPUs to be last. So that is what this commit does.

This fixes test 39.18 subtests 0.[38,39], which were failing ever since
commit 3bd0778f9a.

Bug 12126

src/plugins/gres/gpu/gres_gpu.c
@@ -393,32 +393,25 @@ static int _file_inx(char *fname)
 	return val;
 }
 
-/* Sort gres/gpu records by "File" value */
-static int _sort_gpu_by_file(void *x, void *y, bool asc)
+/* Sort gres/gpu records by "File" value in ascending order, with nulls last */
+static int _sort_gpu_by_file(void *x, void *y)
 {
 	gres_slurmd_conf_t *gres_record1 = *(gres_slurmd_conf_t **) x;
 	gres_slurmd_conf_t *gres_record2 = *(gres_slurmd_conf_t **) y;
 	int val1, val2;
 
+	/* Make NULLs greater than non-NULLs, so NULLs are sorted last */
+	if (!gres_record1->file && gres_record2->file)
+		return 1;
+	else if (gres_record1->file && !gres_record2->file)
+		return -1;
+	else if (!gres_record1->file && !gres_record2->file)
+		return 0;
+
 	val1 = _file_inx(gres_record1->file);
 	val2 = _file_inx(gres_record2->file);
 
-	if (asc)
-		return (val1 - val2);
-	else
-		return -(val1 - val2);
-}
-
-/* Sort gres/gpu records by "File" value in descending order (nulls first) */
-static int _sort_gpu_by_file_desc(void *x, void *y)
-{
-	return _sort_gpu_by_file(x, y, false);
-}
-
-/* Sort gres/gpu records by "File" value in ascending order */
-static int _sort_gpu_by_file_asc(void *x, void *y)
-{
-	return _sort_gpu_by_file(x, y, true);
+	return (val1 - val2);
 }
 
 /*
@@ -573,10 +566,14 @@ static void _merge_system_gres_conf(List gres_list_conf, List gres_list_system)
 	 */
 	_normalize_sys_gres_types(gres_list_system, gres_list_conf_single);
 
-	/* Sort so null files are last for _match_gres() */
-	list_sort(gres_list_conf_single, _sort_gpu_by_file_desc);
+	/*
+	 *  Sort null files last, so that conf records with a specified File
+	 *  are matched first in _match_gres(). Then, conf records without a
+	 *  File can fill in any remaining holes.
+	 */
+	list_sort(gres_list_conf_single, _sort_gpu_by_file);
 	/* Sort system devices in the same way for convenience */
-	list_sort(gres_list_system, _sort_gpu_by_file_desc);
+	list_sort(gres_list_system, _sort_gpu_by_file);
 
 	itr = list_iterator_create(gres_list_conf_single);
 	itr2 = list_iterator_create(gres_list_system);
@@ -688,7 +685,7 @@ static void _merge_system_gres_conf(List gres_list_conf, List gres_list_system)
 	list_flush(gres_list_conf);
 	if (gres_list_gpu && list_count(gres_list_gpu)) {
 		/* Sort by device file first, in case no links */
-		list_sort(gres_list_gpu, _sort_gpu_by_file_asc);
+		list_sort(gres_list_gpu, _sort_gpu_by_file);
 		/* Sort by links, which is a stand-in for PCI bus ID order */
 		list_sort(gres_list_gpu, _sort_gpu_by_links_order);
 		debug2("gres_list_gpu");