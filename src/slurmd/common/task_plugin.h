f3938983e166d54edace5d947c20ded15c43f1f0
Michael Hinton <hinton@schedmd.com>
2021-10-12 12:45:27 -0600

    Add function descriptions
    No change in behavior.
    
    Bug 6701

--- src/slurmd/common/task_plugin.h
+++ src/slurmd/common/task_plugin.h
@@ -161,8 +161,27 @@ extern int task_g_add_pid(pid_t pid);
 
 extern void task_slurm_chkaffinity(cpu_set_t *mask, stepd_step_rec_t *job,
 				   int statval, uint32_t taskid);
+
+/*
+ * Convert a CPU bitmask to a hex string.
+ *
+ * IN mask - A CPU bitmask pointer.
+ * IN/OUT str - A char pointer used to return a string of size
+ *		CPU_SET_HEX_STR_SIZE.
+ * RET - Returns a pointer to a string slice in str that starts at the first
+ *	 non-zero hex char or last zero hex char if all bits are not set.
+ */
 extern char *task_cpuset_to_str(const cpu_set_t *mask, char *str);
 
+/*
+ * Convert a hex string to a CPU bitmask.
+ *
+ * IN/OUT mask - An empty CPU bitmask pointer that will be set according to CPUs
+ *		 specified by the hex values in str.
+ * IN str - A null-terminated hex string that specifies CPUs to set.
+ * RET - Returns -1 if str could not be interpreted into valid hex or if str is
+ *	 too large, else returns 0 on success.
+ */
 extern int task_str_to_cpuset(cpu_set_t *mask, const char* str);
 
 #endif /* _SLURMD_TASK_PLUGIN_H_ */
