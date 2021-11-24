6e13352fc261ebecfbc2748e2c2779f6809f7b72
Marshall Garey <marshall@schedmd.com>
2021-11-24 11:13:51 -0700

    Fix srun -c and --threads-per-core imply --exact
    In 21.08, srun --cpus-per-task and threads-per-core wer supposed to
    imply --exact. This worked when that new behavior was introduced, but
    was regressed by commit 5154ed21e2c before 21.08 was released. That
    commit did not handle setting --exact in step_req if -c was also set.
    
    Bug 12909

--- src/srun/libsrun/launch.c
+++ src/srun/libsrun/launch.c
@@ -182,8 +182,6 @@ static job_step_create_request_msg_t *_create_job_step_create_request(
 		step_req->flags |= SSF_EXCLUSIVE;
 	if (opt_local->overcommit)
 		step_req->flags |= SSF_OVERCOMMIT;
-	if (!srun_opt->exact)
-		step_req->flags |= SSF_WHOLE;
 	if (opt_local->no_kill)
 		step_req->flags |= SSF_NO_KILL;
 	if (srun_opt->interactive) {
@@ -404,6 +402,13 @@ static job_step_create_request_msg_t *_create_job_step_create_request(
 	step_req->user_id = opt_local->uid;
 
 	step_req->container = xstrdup(opt_local->container);
+
+	/*
+	 * This must be handled *after* we potentially set srun_opt->exact
+	 * above.
+	 */
+	if (!srun_opt->exact)
+		step_req->flags |= SSF_WHOLE;
 
 	return step_req;
 }
