SchedMD/slurm

commit 6300d47c2d2485683dedac290b157a8bfe77f918

Make it so TRES have the TRES type always added to them and process them correctly afterwards.
It turns out before this all 'TRES' variables really were 'GRES' variables.
This is fixed in this patch to have them correctly noted.  This makes
it possible to add other TRES to the lists like will be done in
following commits.

Bug 11089

src/common/slurm_opt.c
@@ -40,6 +40,7 @@
 #include <sys/param.h>
 
 #include "src/common/cpu_frequency.h"
+#include "src/common/gres.h"
 #include "src/common/log.h"
 #include "src/common/optz.h"
 #include "src/common/parse_time.h"
@@ -1941,7 +1942,7 @@ static int arg_set_gres(slurm_opt_t *opt, const char *arg)
 	}
 
 	xfree(opt->gres);
-	opt->gres = xstrdup(arg);
+	opt->gres = gres_prepend_tres_type(arg);
 
 	return SLURM_SUCCESS;
 }
@@ -1958,8 +1959,7 @@ static int arg_set_data_gres(slurm_opt_t *opt, const data_t *arg,
 		ADD_DATA_ERROR("GRES \"help\" not supported", rc);
 	} else {
 		xfree(opt->gres);
-		opt->gres = str;
-		str = NULL;
+		opt->gres = gres_prepend_tres_type(str);
 	}
 
 	xfree(str);

src/common/gres.c
@@ -4796,6 +4796,16 @@ next:	if (*save_ptr[0] == '\0') {	/* Empty input token */
 		goto fini;
 	}
 
+	if (!(sep = xstrstr(*save_ptr, "gres:"))) {
+		debug2("%s is not a gres", *save_ptr);
+		xfree(name);
+		*save_ptr = NULL;
+		goto fini;
+	} else {
+		sep += 5; /* strlen "gres:" */
+		*save_ptr = sep;
+	}
+
 	name = xstrdup(*save_ptr);
 	comma = strchr(name, ',');
 	if (comma) {
@@ -5209,7 +5219,7 @@ extern int gres_job_state_validate(char *cpus_per_tres,
 		 */
 		uint32_t gpus = *num_tasks / *ntasks_per_tres;
 		char *save_ptr = NULL, *gres = NULL, *in_val;
-		xstrfmtcat(gres, "gpu:%u", gpus);
+		xstrfmtcat(gres, "gres:gpu:%u", gpus);
 		in_val = gres;
 		while ((job_gres_data = _get_next_job_gres(in_val, &cnt,
 							   *gres_list,
@@ -8149,7 +8159,7 @@ static void _handle_ntasks_per_tres_step(List new_step_list,
 		uint32_t gpus = *num_tasks / ntasks_per_tres;
 		/* For now, do type-less GPUs */
 		char *save_ptr = NULL, *gres = NULL, *in_val;
-		xstrfmtcat(gres, "gpu:%u", gpus);
+		xstrfmtcat(gres, "gres:gpu:%u", gpus);
 		in_val = gres;
 		while ((step_gres_data =
 			_get_next_step_gres(in_val, &cnt,
@@ -9779,4 +9789,15 @@ extern void add_gres_to_list(List gres_list, char *name, uint64_t device_cnt,
 	if (!use_empty_first_record)
 		list_append(gres_list, gpu_record);
 	list_iterator_destroy(itr);
+}
+
+extern char *gres_prepend_tres_type(const char *gres_str)
+{
+	char *output = NULL;
+
+	if (gres_str) {
+		output = xstrdup_printf("gres:%s", gres_str);
+		xstrsubstituteall(output, ",", ",gres:");
+	}
+	return output;
 }

src/common/gres.h
@@ -1065,4 +1065,10 @@ extern void gres_add_type(char *type, gres_node_state_t *gres_data,
 extern void gres_validate_node_cores(gres_node_state_t *node_gres_ptr,
 				     int cores_ctld, char *node_name);
 
+/*
+ * Prepend "gres:" to each gres for proper parsing with TRES functions
+ * caller must xfree result.
+ */
+extern char *gres_prepend_tres_type(const char *gres_str);
+
 #endif /* !_GRES_H */

src/common/proc_args.c
@@ -1726,7 +1726,7 @@ extern int validate_acctg_freq(char *acctg_freq)
 /*
  * Format a tres_per_* argument
  * dest OUT - resulting string
- * prefix IN - TRES type (e.g. "gpu")
+ * prefix IN - TRES type (e.g. "gres:gpu")
  * src IN - user input, can include multiple comma-separated specifications
  */
 extern void xfmt_tres(char **dest, char *prefix, char *src)
@@ -1753,7 +1753,7 @@ extern void xfmt_tres(char **dest, char *prefix, char *src)
 /*
  * Format a tres_freq argument
  * dest OUT - resulting string
- * prefix IN - TRES type (e.g. "gpu")
+ * prefix IN - TRES type (e.g. "gres:gpu")
  * src IN - user input
  */
 extern void xfmt_tres_freq(char **dest, char *prefix, char *src)

src/common/slurm_protocol_pack.c
@@ -2532,6 +2532,7 @@ extern int _unpack_job_step_create_request_msg(
 		safe_unpackstr_xmalloc(&tmp_ptr->tres_per_task, &uint32_tmp,
 				       buffer);
 	} else if (protocol_version >= SLURM_20_11_PROTOCOL_VERSION) {
+		char *temp_str;
 		if (unpack_step_id_members(&tmp_ptr->step_id, buffer,
 					   protocol_version) != SLURM_SUCCESS)
 			goto unpack_error;
@@ -2579,14 +2580,22 @@ extern int _unpack_job_step_create_request_msg(
 				       buffer);
 		safe_unpackstr_xmalloc(&tmp_ptr->tres_freq, &uint32_tmp,
 				       buffer);
-		safe_unpackstr_xmalloc(&tmp_ptr->tres_per_step, &uint32_tmp,
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp,
 				       buffer);
-		safe_unpackstr_xmalloc(&tmp_ptr->tres_per_node, &uint32_tmp,
+		tmp_ptr->tres_per_step = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp,
 				       buffer);
-		safe_unpackstr_xmalloc(&tmp_ptr->tres_per_socket, &uint32_tmp,
+		tmp_ptr->tres_per_node = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp,
 				       buffer);
-		safe_unpackstr_xmalloc(&tmp_ptr->tres_per_task, &uint32_tmp,
+		tmp_ptr->tres_per_socket = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp,
 				       buffer);
+		tmp_ptr->tres_per_task = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
 	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		char *temp_str;
 		uint16_t uint16_tmp;
@@ -2655,14 +2664,22 @@ extern int _unpack_job_step_create_request_msg(
 				       buffer);
 		safe_unpackstr_xmalloc(&tmp_ptr->tres_freq, &uint32_tmp,
 				       buffer);
-		safe_unpackstr_xmalloc(&tmp_ptr->tres_per_step, &uint32_tmp,
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp,
 				       buffer);
-		safe_unpackstr_xmalloc(&tmp_ptr->tres_per_node, &uint32_tmp,
+		tmp_ptr->tres_per_step = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp,
 				       buffer);
-		safe_unpackstr_xmalloc(&tmp_ptr->tres_per_socket, &uint32_tmp,
+		tmp_ptr->tres_per_node = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp,
 				       buffer);
-		safe_unpackstr_xmalloc(&tmp_ptr->tres_per_task, &uint32_tmp,
+		tmp_ptr->tres_per_socket = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp,
 				       buffer);
+		tmp_ptr->tres_per_task = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
 	} else {
 		error("%s: protocol_version %hu not supported",
 		      __func__,  protocol_version);
@@ -3204,6 +3221,7 @@ _unpack_job_step_info_members(job_step_info_t * step, buf_t *buffer,
 			      uint16_t protocol_version)
 {
 	uint32_t uint32_tmp = 0;
+	char *temp_str;
 
 	if (protocol_version >= SLURM_21_08_PROTOCOL_VERSION) {
 		safe_unpack32(&step->array_job_id, buffer);
@@ -3303,14 +3321,19 @@ _unpack_job_step_info_members(job_step_info_t * step, buf_t *buffer,
 				       &uint32_tmp, buffer);
 		safe_unpackstr_xmalloc(&step->tres_freq,
 				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&step->tres_per_step,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&step->tres_per_node,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&step->tres_per_socket,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&step->tres_per_task,
-				       &uint32_tmp, buffer);
+
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		step->tres_per_step = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		step->tres_per_node = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		step->tres_per_socket = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		step->tres_per_task = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
 	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		safe_unpack32(&step->array_job_id, buffer);
 		safe_unpack32(&step->array_task_id, buffer);
@@ -3355,14 +3378,19 @@ _unpack_job_step_info_members(job_step_info_t * step, buf_t *buffer,
 				       &uint32_tmp, buffer);
 		safe_unpackstr_xmalloc(&step->tres_freq,
 				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&step->tres_per_step,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&step->tres_per_node,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&step->tres_per_socket,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&step->tres_per_task,
-				       &uint32_tmp, buffer);
+
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		step->tres_per_step = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		step->tres_per_node = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		step->tres_per_socket = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		step->tres_per_task = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
 	} else {
 		error("_unpack_job_step_info_members: protocol_version "
 		      "%hu not supported", protocol_version);
@@ -3495,6 +3523,7 @@ _unpack_job_info_members(job_info_t * job, buf_t *buffer,
 {
 	uint32_t uint32_tmp = 0;
 	multi_core_data_t *mc_ptr;
+	char *temp_str;
 
 	if (protocol_version >= SLURM_21_08_PROTOCOL_VERSION) {
 		safe_unpack32(&job->array_job_id, buffer);
@@ -3861,14 +3890,19 @@ _unpack_job_info_members(job_info_t * job, buf_t *buffer,
 				       buffer);
 		safe_unpackstr_xmalloc(&job->tres_freq, &uint32_tmp,
 				       buffer);
-		safe_unpackstr_xmalloc(&job->tres_per_job, &uint32_tmp,
-				       buffer);
-		safe_unpackstr_xmalloc(&job->tres_per_node, &uint32_tmp,
-				       buffer);
-		safe_unpackstr_xmalloc(&job->tres_per_socket, &uint32_tmp,
-				       buffer);
-		safe_unpackstr_xmalloc(&job->tres_per_task, &uint32_tmp,
-				       buffer);
+
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job->tres_per_job = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job->tres_per_node = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job->tres_per_socket = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job->tres_per_task = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
 
 		safe_unpack16(&job->mail_type, buffer);
 		safe_unpackstr_xmalloc(&job->mail_user, &uint32_tmp, buffer);
@@ -4048,14 +4082,19 @@ _unpack_job_info_members(job_info_t * job, buf_t *buffer,
 				       buffer);
 		safe_unpackstr_xmalloc(&job->tres_freq, &uint32_tmp,
 				       buffer);
-		safe_unpackstr_xmalloc(&job->tres_per_job, &uint32_tmp,
-				       buffer);
-		safe_unpackstr_xmalloc(&job->tres_per_node, &uint32_tmp,
-				       buffer);
-		safe_unpackstr_xmalloc(&job->tres_per_socket, &uint32_tmp,
-				       buffer);
-		safe_unpackstr_xmalloc(&job->tres_per_task, &uint32_tmp,
-				       buffer);
+
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job->tres_per_job = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job->tres_per_node = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job->tres_per_socket = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job->tres_per_task = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
 
 		safe_unpack16(&job->mail_type, buffer);
 		safe_unpackstr_xmalloc(&job->mail_user, &uint32_tmp, buffer);
@@ -6225,6 +6264,7 @@ _unpack_job_desc_msg(job_desc_msg_t ** job_desc_buffer_ptr, buf_t *buffer,
 {
 	uint32_t uint32_tmp;
 	job_desc_msg_t *job_desc_ptr = NULL;
+	char *temp_str;
 
 	if (protocol_version >= SLURM_21_08_PROTOCOL_VERSION) {
 		job_desc_ptr = xmalloc(sizeof(job_desc_msg_t));
@@ -6591,14 +6631,21 @@ _unpack_job_desc_msg(job_desc_msg_t ** job_desc_buffer_ptr, buf_t *buffer,
 				       &uint32_tmp, buffer);
 		safe_unpackstr_xmalloc(&job_desc_ptr->tres_freq,
 				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&job_desc_ptr->tres_per_job,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&job_desc_ptr->tres_per_node,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&job_desc_ptr->tres_per_socket,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&job_desc_ptr->tres_per_task,
-				       &uint32_tmp, buffer);
+
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job_desc_ptr->tres_per_job = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job_desc_ptr->tres_per_node = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job_desc_ptr->tres_per_socket =
+			gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job_desc_ptr->tres_per_task = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+
 		if (unpack_cron_entry(&job_desc_ptr->crontab_entry,
 				      protocol_version, buffer))
 			goto unpack_error;
@@ -6783,14 +6830,20 @@ _unpack_job_desc_msg(job_desc_msg_t ** job_desc_buffer_ptr, buf_t *buffer,
 				       &uint32_tmp, buffer);
 		safe_unpackstr_xmalloc(&job_desc_ptr->tres_freq,
 				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&job_desc_ptr->tres_per_job,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&job_desc_ptr->tres_per_node,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&job_desc_ptr->tres_per_socket,
-				       &uint32_tmp, buffer);
-		safe_unpackstr_xmalloc(&job_desc_ptr->tres_per_task,
-				       &uint32_tmp, buffer);
+
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job_desc_ptr->tres_per_job = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job_desc_ptr->tres_per_node = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job_desc_ptr->tres_per_socket =
+			gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
+		safe_unpackstr_xmalloc(&temp_str, &uint32_tmp, buffer);
+		job_desc_ptr->tres_per_task = gres_prepend_tres_type(temp_str);
+		xfree(temp_str);
 	} else {
 		error("_unpack_job_desc_msg: protocol_version "
 		      "%hu not supported", protocol_version);

src/slurmctld/job_mgr.c
@@ -8418,14 +8418,14 @@ static bool _valid_pn_min_mem(job_desc_msg_t *job_desc_msg,
 
 /*
  * Validate TRES specification of the form:
- * "name=[type:]#[,[type:]#][;name=[type:]#]"
- * For example: "gpu:kepler:2,craynetwork=1"
+ * "name=tres_type:name:[#|type:#]"
+ * For example: "cpu:2,gres:gpu:kepler:2,gres:craynetwork:1"
  */
 extern bool valid_tres_cnt(char *tres)
 {
-	char *end_ptr = NULL, *colon, *save_ptr = NULL, *sep, *tok, *tmp;
+	char *end_ptr = NULL, *value = NULL, *save_ptr = NULL, *sep, *tok, *tmp;
 	bool rc = true;
-	long long int val;
+	uint64_t count = 1;
 
 	if (!tres || (tres[0] == '\0'))
 		return true;
@@ -8433,45 +8433,79 @@ extern bool valid_tres_cnt(char *tres)
 	tmp = xstrdup(tres);
 	tok = strtok_r(tmp, ",", &save_ptr);
 	while (tok) {
-		bool valid_name = false;
-		sep = strchr(tok, ':');
-		if (sep) {
+		int pos;
+
+		/* First check to see if the last part is a count or not */
+		if ((value = strrchr(tok, ':'))) {
+			if (!value[1]) {
+				rc = false;
+				break;
+			}
+			if (isdigit(value[1])) {
+				count = strtoull(value + 1, &end_ptr, 10);
+
+				if (((count < 0) || (count == LLONG_MAX))) {
+					rc = false;
+					break;
+				}
+
+				/*
+				 * Now check that any count suffic is valid.
+				 */
+				if (suffix_mult(end_ptr) == NO_VAL64) {
+					rc = false;
+					break;
+				}
+			} else /* This isn't a value so set it to NULL */
+				value = NULL;
+		}
+
+		/* Now check to see what the first part is */
+		if (!(sep = strchr(tok, ':'))) {
+			rc = false;
+			break;
+		}
+
+		/* This means there was a sep */
+		if (value != sep) {
 			sep[0] = '\0';
 			sep++;
-		}
-		if (valid_gres_name(tok))
-			valid_name = true;
-		if (!sep) {	/* No model or count. Implicit count of 1 */
-			if (!valid_name) {
-				rc = false;
-				break;
-			}
-		} else if ((colon = strchr(sep, ':'))) {
-			/* Includes explicit "name:type:count" */
-			sep = colon + 1;	/* Points to count */
-			val = strtoll(sep, &end_ptr, 10);
-			/* First only check numeric component for validity */
-			if (((val < 0) ||
-			    (val == LLONG_MAX)) ||
-			    (!valid_name && (val != 0))) {
-				rc = false;
-				break;
-			}
+		} else
+			sep = NULL;
 
+		/* Now we zero out value since we checked against sep */
+		if (value) {
+			value[0] = '\0';
 			/*
-			 * Now check that any count suffic is valid.
+			 * This is here to handle the old craynetwork:0
+			 * Any gres that is formatted correctly and has a count
+			 * of 0 is valid to be thrown away but allow job to
+			 * allocate.
 			 */
-			if (suffix_mult(end_ptr) == NO_VAL64) {
-				rc = false;
-				break;
-			}
-		} else {
-			/*
-			 * Includes "name:type" or "name:count"
-			 * Since we don't know if there is a count,
-			 * we can not do more now.
-			 */
+			if (count == 0)
+				goto next;
 		}
+
+		/* gres doesn't have to be a TRES to be valid */
+		if (!xstrcmp(tok, "gres")) {
+			/* We only want the first part of a gres for this */
+			if ((tok = strchr(sep, ':')))
+				tok[0] = '\0';
+			pos = valid_gres_name(sep) ? 1 : -1;
+		} else {
+			slurmdb_tres_rec_t tres_rec = {
+				.type = tok,
+				.name = sep,
+			};
+
+			pos = assoc_mgr_find_tres_pos(&tres_rec, false);
+		}
+
+		if (pos == -1) {
+			rc = false;
+			break;
+		}
+	next:
 		tok = strtok_r(NULL, ",", &save_ptr);
 	}
 	xfree(tmp);

src/srun/libsrun/allocate.c
@@ -933,21 +933,22 @@ static job_desc_msg_t *_job_desc_msg_create_from_opts(slurm_opt_t *opt_local)
 	j->clusters = xstrdup(opt_local->clusters);
 
 	if (opt_local->cpus_per_gpu)
-		xstrfmtcat(j->cpus_per_tres, "gpu:%d", opt_local->cpus_per_gpu);
+		xstrfmtcat(j->cpus_per_tres, "gres:gpu:%d",
+			   opt_local->cpus_per_gpu);
 	j->tres_bind = xstrdup(opt_local->tres_bind);
 	j->tres_freq = xstrdup(opt_local->tres_freq);
-	xfmt_tres(&j->tres_per_job,    "gpu", opt_local->gpus);
-	xfmt_tres(&j->tres_per_node,   "gpu", opt_local->gpus_per_node);
+	xfmt_tres(&j->tres_per_job,    "gres:gpu", opt_local->gpus);
+	xfmt_tres(&j->tres_per_node,   "gres:gpu", opt_local->gpus_per_node);
 	if (opt_local->gres && xstrcasecmp(opt_local->gres, "NONE")) {
 		if (j->tres_per_node)
 			xstrfmtcat(j->tres_per_node, ",%s", opt_local->gres);
 		else
 			j->tres_per_node = xstrdup(opt_local->gres);
 	}
-	xfmt_tres(&j->tres_per_socket, "gpu", opt_local->gpus_per_socket);
-	xfmt_tres(&j->tres_per_task,   "gpu", opt_local->gpus_per_task);
+	xfmt_tres(&j->tres_per_socket, "gres:gpu", opt_local->gpus_per_socket);
+	xfmt_tres(&j->tres_per_task,   "gres:gpu", opt_local->gpus_per_task);
 	if (opt_local->mem_per_gpu != NO_VAL64)
-		xstrfmtcat(j->mem_per_tres, "gpu:%"PRIu64,
+		xstrfmtcat(j->mem_per_tres, "gres:gpu:%"PRIu64,
 			   opt_local->mem_per_gpu);
 
 	rc = gres_job_state_validate(j->cpus_per_tres,

src/srun/libsrun/launch.c
@@ -171,7 +171,7 @@ static job_step_create_request_msg_t *_create_job_step_create_request(
 	step_req->cpu_freq_gov = opt_local->cpu_freq_gov;
 
 	if (opt_local->cpus_per_gpu) {
-		xstrfmtcat(step_req->cpus_per_tres, "gpu:%d",
+		xstrfmtcat(step_req->cpus_per_tres, "gres:gpu:%d",
 			   opt_local->cpus_per_gpu);
 	}
 
@@ -200,7 +200,7 @@ static job_step_create_request_msg_t *_create_job_step_create_request(
 		step_req->max_nodes = opt_local->max_nodes;
 
 	if (opt_local->mem_per_gpu != NO_VAL64)
-		xstrfmtcat(step_req->mem_per_tres, "gpu:%"PRIu64,
+		xstrfmtcat(step_req->mem_per_tres, "gres:gpu:%"PRIu64,
 			   opt.mem_per_gpu);
 
 	step_req->min_nodes = job->nhosts;
@@ -339,9 +339,10 @@ static job_step_create_request_msg_t *_create_job_step_create_request(
 	step_req->tres_bind = xstrdup(opt_local->tres_bind);
 	step_req->tres_freq = xstrdup(opt_local->tres_freq);
 
-	xfmt_tres(&step_req->tres_per_step, "gpu", opt_local->gpus);
+	xfmt_tres(&step_req->tres_per_step, "gres:gpu", opt_local->gpus);
 
-	xfmt_tres(&step_req->tres_per_node, "gpu", opt_local->gpus_per_node);
+	xfmt_tres(&step_req->tres_per_node, "gres:gpu",
+		  opt_local->gpus_per_node);
 	if (opt_local->gres)
 		add_tres = opt_local->gres;
 	else
@@ -353,10 +354,11 @@ static job_step_create_request_msg_t *_create_job_step_create_request(
 			step_req->tres_per_node = xstrdup(add_tres);
 	}
 
-	xfmt_tres(&step_req->tres_per_socket, "gpu",
+	xfmt_tres(&step_req->tres_per_socket, "gres:gpu",
 		  opt_local->gpus_per_socket);
 
-	xfmt_tres(&step_req->tres_per_task, "gpu", opt_local->gpus_per_task);
+	xfmt_tres(&step_req->tres_per_task, "gres:gpu",
+		  opt_local->gpus_per_task);
 
 	if (opt_local->time_limit != NO_VAL)
 		step_req->time_limit = opt_local->time_limit;

commit 8b7b1e7128fcf1ac013ab308cf0c09fbc20485f2

Fix regression that broke --gres=none
This was broken by commit 6300d47c2d (and related) in 21.08.0pre1 which
prepended "gres:" to all GRES, including "none". This caused jobs and steps
that used --gres=none to fail.

Jobs would fail with this error message:

	Invalid generic resource (gres) specification

Steps would fail with this similar error message:

	Invalid Trackable RESource (TRES) specification

Bug 12880.

src/common/slurm_opt.c
@@ -2072,7 +2072,15 @@ static int arg_set_gres(slurm_opt_t *opt, const char *arg)
 	}
 
 	xfree(opt->gres);
-	opt->gres = gres_prepend_tres_type(arg);
+	/*
+	 * Do not prepend "gres:" to none; none is handled specially by
+	 * slurmctld to mean "do not copy the job's GRES to the step" -
+	 * see _copy_job_tres_to_step()
+	 */
+	if (!xstrcasecmp(arg, "none"))
+		opt->gres = xstrdup(arg);
+	else
+		opt->gres = gres_prepend_tres_type(arg);
 
 	return SLURM_SUCCESS;
 }
@@ -2089,7 +2097,16 @@ static int arg_set_data_gres(slurm_opt_t *opt, const data_t *arg,
 		ADD_DATA_ERROR("GRES \"help\" not supported", rc);
 	} else {
 		xfree(opt->gres);
-		opt->gres = gres_prepend_tres_type(str);
+		/*
+		 * Do not prepend "gres:" to none; none is handled specially by
+		 * slurmctld to mean "do not copy the job's GRES to the step" -
+		 * see _copy_job_tres_to_step()
+		 */
+		if (!xstrcasecmp(str, "none")) {
+			opt->gres = str;
+			str = NULL;
+		} else
+			opt->gres = gres_prepend_tres_type(str);
 	}
 
 	xfree(str);
@@ -6122,6 +6139,7 @@ extern job_desc_msg_t *slurm_opt_create_job_desc(slurm_opt_t *opt_local,
 	xfmt_tres(&job_desc->tres_per_job, "gres:gpu", opt_local->gpus);
 	xfmt_tres(&job_desc->tres_per_node, "gres:gpu",
 		  opt_local->gpus_per_node);
+	/* --gres=none for jobs means no GRES, so don't send it to slurmctld */
 	if (opt_local->gres && xstrcasecmp(opt_local->gres, "NONE")) {
 		if (job_desc->tres_per_node)
 			xstrfmtcat(job_desc->tres_per_node, ",%s",