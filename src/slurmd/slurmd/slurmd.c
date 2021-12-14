SchedMD/slurm

commit 4cca854e3103ad2d9186c64101c0572cae9736e6

slurmd - fix assert failure on initialization due to bad node name.
Move the already existing find_node_record2() call up in the code and
bail with error if no record found.

The justification for the call movement is to avoid unneeded spool dir
creation attempt as well as further operations for non-configless setups
if no record is found.

Change the used call to find_node_record() too while here so that the
function itself logs the error if no record found for us.

Bug 11621

src/slurmd/slurmd/slurmd.c
@@ -1724,6 +1724,9 @@ _slurmd_init(void)
 	 */
 	_read_config();
 
+	if (!(node_rec = find_node_record(conf->node_name)))
+		return SLURM_ERROR;
+
 	/*
 	 * slurmd -G, calling it here rather than from _process_cmdline
 	 * since it relies on gres_init and _read_config.
@@ -1753,8 +1756,7 @@ _slurmd_init(void)
 
 	fini_job_cnt = cpu_cnt = MAX(conf->conf_cpus, conf->block_map_size);
 	fini_job_id = xmalloc(sizeof(uint32_t) * fini_job_cnt);
-	node_rec = find_node_record2(conf->node_name);
-	if (node_rec && node_rec->config_ptr) {
+	if (node_rec->config_ptr) {
 		(void) gres_init_node_config(conf->node_name,
 					     node_rec->config_ptr->gres,
 					     &gres_list);

commit 0a5fb55d107b1044d626b69cee99540f5a93439c

Fix regression in 21.08.0 that broke dynamic future nodes
Broken by commit 4cca854e31 (bug 11621).

When a FUTURE node slurmd initializes, the following information appears
in the slurmd log:

error: _find_node_record: lookup failure for node "node01"
error: slurmd initialization failed

With FUTURE nodes, the hostname for that node is not known by the
controller until the node registers.

Bug 12959

Signed-off-by: Marshall Garey <marshall@schedmd.com>

src/slurmd/slurmd/slurmd.c
@@ -1674,7 +1674,7 @@ _slurmd_init(void)
 	struct rlimit rlim;
 	struct stat stat_buf;
 	uint32_t cpu_cnt;
-	node_record_t *node_rec;
+	node_record_t *node_rec = NULL;
 	List gres_list = NULL;
 	int rc;
 
@@ -1724,7 +1724,9 @@ _slurmd_init(void)
 	 */
 	_read_config();
 
-	if (!(node_rec = find_node_record(conf->node_name)))
+	/* Dynamic nodes won't be found at this point */
+	if (!conf->dynamic &&
+	    !(node_rec = find_node_record(conf->node_name)))
 		return SLURM_ERROR;
 
 	/*
@@ -1756,7 +1758,8 @@ _slurmd_init(void)
 
 	fini_job_cnt = cpu_cnt = MAX(conf->conf_cpus, conf->block_map_size);
 	fini_job_id = xmalloc(sizeof(uint32_t) * fini_job_cnt);
-	if (node_rec->config_ptr) {
+	/* node_rec==NULL is expected for dynamic nodes */
+	if (node_rec && node_rec->config_ptr) {
 		(void) gres_init_node_config(conf->node_name,
 					     node_rec->config_ptr->gres,
 					     &gres_list);