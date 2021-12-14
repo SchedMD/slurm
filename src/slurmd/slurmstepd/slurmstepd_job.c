SchedMD/slurm

commit 3d6036824463645241b912e4b22d1566758a65f6

Remove LAUNCH_USER_MANAGED_IO flag.

src/slurmd/slurmstepd/slurmstepd_job.c
@@ -438,15 +438,9 @@ extern stepd_step_rec_t *stepd_step_rec_create(launch_tasks_request_msg_t *msg,
 	} else {
 		memset(&resp_addr, 0, sizeof(slurm_addr_t));
 	}
-	if (!msg->io_port)
-		msg->flags |= LAUNCH_USER_MANAGED_IO;
-	if ((msg->flags & LAUNCH_USER_MANAGED_IO) == 0) {
-		memcpy(&io_addr,   &msg->orig_addr, sizeof(slurm_addr_t));
-		slurm_set_port(&io_addr,
-			       msg->io_port[nodeid % msg->num_io_port]);
-	} else {
-		memset(&io_addr, 0, sizeof(slurm_addr_t));
-	}
+
+	memcpy(&io_addr, &msg->orig_addr, sizeof(slurm_addr_t));
+	slurm_set_port(&io_addr, msg->io_port[nodeid % msg->num_io_port]);
 
 	srun = srun_info_create(msg->cred, &resp_addr, &io_addr,
 				protocol_version);

src/api/step_launch.c
@@ -338,8 +338,6 @@ extern int slurm_step_launch(slurm_step_ctx_t *ctx,
 
 	launch.select_jobinfo  = ctx->step_resp->select_jobinfo;
 
-	if (params->user_managed_io)
-		launch.flags	|= LAUNCH_USER_MANAGED_IO;
 	ctx->launch_state->user_managed_io = params->user_managed_io;
 
 	if (!ctx->launch_state->user_managed_io) {
@@ -527,9 +525,6 @@ extern int slurm_step_launch_add(slurm_step_ctx_t *ctx,
 	launch.global_task_ids = ctx->step_resp->step_layout->tids;
 
 	launch.select_jobinfo  = ctx->step_resp->select_jobinfo;
-
-	if (params->user_managed_io)
-		launch.flags |= LAUNCH_USER_MANAGED_IO;
 
 	/* user_managed_io is true */
 	if (!ctx->launch_state->io.user) {

src/common/slurm_protocol_defs.h
@@ -823,7 +823,7 @@ typedef struct job_step_create_response_msg {
 #define LAUNCH_PTY		0x00000004
 #define LAUNCH_BUFFERED_IO	0x00000008
 #define LAUNCH_LABEL_IO		0x00000010
-#define LAUNCH_USER_MANAGED_IO	0x00000020
+/* free for reuse two versions after 22.05: 0x00000020 */
 #define LAUNCH_NO_ALLOC 	0x00000040
 
 typedef struct launch_tasks_request_msg {

src/common/slurm_protocol_pack.c
@@ -7163,14 +7163,12 @@ static void _pack_launch_tasks_request_msg(launch_tasks_request_msg_t *msg,
 		packstr(msg->mem_bind, buffer);
 		packstr_array(msg->argv, msg->argc, buffer);
 		pack32(msg->flags, buffer);
-		if ((msg->flags & LAUNCH_USER_MANAGED_IO) == 0) {
-			packstr(msg->ofname, buffer);
-			packstr(msg->efname, buffer);
-			packstr(msg->ifname, buffer);
-			pack16(msg->num_io_port, buffer);
-			for (i = 0; i < msg->num_io_port; i++)
-				pack16(msg->io_port[i], buffer);
-		}
+		packstr(msg->ofname, buffer);
+		packstr(msg->efname, buffer);
+		packstr(msg->ifname, buffer);
+		pack16(msg->num_io_port, buffer);
+		for (i = 0; i < msg->num_io_port; i++)
+			pack16(msg->io_port[i], buffer);
 		pack32(msg->profile, buffer);
 		packstr(msg->task_prolog, buffer);
 		packstr(msg->task_epilog, buffer);
@@ -7262,14 +7260,12 @@ static void _pack_launch_tasks_request_msg(launch_tasks_request_msg_t *msg,
 		packstr(msg->mem_bind, buffer);
 		packstr_array(msg->argv, msg->argc, buffer);
 		pack32(msg->flags, buffer);
-		if ((msg->flags & LAUNCH_USER_MANAGED_IO) == 0) {
-			packstr(msg->ofname, buffer);
-			packstr(msg->efname, buffer);
-			packstr(msg->ifname, buffer);
-			pack16(msg->num_io_port, buffer);
-			for (i = 0; i < msg->num_io_port; i++)
-				pack16(msg->io_port[i], buffer);
-		}
+		packstr(msg->ofname, buffer);
+		packstr(msg->efname, buffer);
+		packstr(msg->ifname, buffer);
+		pack16(msg->num_io_port, buffer);
+		for (i = 0; i < msg->num_io_port; i++)
+			pack16(msg->io_port[i], buffer);
 		pack32(msg->profile, buffer);
 		packstr(msg->task_prolog, buffer);
 		packstr(msg->task_epilog, buffer);
@@ -7409,23 +7405,17 @@ static int _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **msg_ptr
 		safe_unpackstr_xmalloc(&msg->mem_bind, &uint32_tmp, buffer);
 		safe_unpackstr_array(&msg->argv, &msg->argc, buffer);
 		safe_unpack32(&msg->flags, buffer);
-		if ((msg->flags & LAUNCH_USER_MANAGED_IO) == 0) {
-			safe_unpackstr_xmalloc(&msg->ofname, &uint32_tmp,
-					       buffer);
-			safe_unpackstr_xmalloc(&msg->efname, &uint32_tmp,
-					       buffer);
-			safe_unpackstr_xmalloc(&msg->ifname, &uint32_tmp,
-					       buffer);
-			safe_unpack16(&msg->num_io_port, buffer);
-			if (msg->num_io_port >= NO_VAL16)
-				goto unpack_error;
-			if (msg->num_io_port > 0) {
-				safe_xcalloc(msg->io_port, msg->num_io_port,
-				             sizeof(uint16_t));
-				for (i = 0; i < msg->num_io_port; i++)
-					safe_unpack16(&msg->io_port[i],
-						      buffer);
-			}
+		safe_unpackstr_xmalloc(&msg->ofname, &uint32_tmp, buffer);
+		safe_unpackstr_xmalloc(&msg->efname, &uint32_tmp, buffer);
+		safe_unpackstr_xmalloc(&msg->ifname, &uint32_tmp, buffer);
+		safe_unpack16(&msg->num_io_port, buffer);
+		if (msg->num_io_port >= NO_VAL16)
+			goto unpack_error;
+		if (msg->num_io_port > 0) {
+			safe_xcalloc(msg->io_port, msg->num_io_port,
+			             sizeof(uint16_t));
+			for (i = 0; i < msg->num_io_port; i++)
+				safe_unpack16(&msg->io_port[i], buffer);
 		}
 		safe_unpack32(&msg->profile, buffer);
 		safe_unpackstr_xmalloc(&msg->task_prolog, &uint32_tmp, buffer);
@@ -7562,23 +7552,17 @@ static int _unpack_launch_tasks_request_msg(launch_tasks_request_msg_t **msg_ptr
 		safe_unpackstr_xmalloc(&msg->mem_bind, &uint32_tmp, buffer);
 		safe_unpackstr_array(&msg->argv, &msg->argc, buffer);
 		safe_unpack32(&msg->flags, buffer);
-		if ((msg->flags & LAUNCH_USER_MANAGED_IO) == 0) {
-			safe_unpackstr_xmalloc(&msg->ofname, &uint32_tmp,
-					       buffer);
-			safe_unpackstr_xmalloc(&msg->efname, &uint32_tmp,
-					       buffer);
-			safe_unpackstr_xmalloc(&msg->ifname, &uint32_tmp,
-					       buffer);
-			safe_unpack16(&msg->num_io_port, buffer);
-			if (msg->num_io_port >= NO_VAL16)
-				goto unpack_error;
-			if (msg->num_io_port > 0) {
-				safe_xcalloc(msg->io_port, msg->num_io_port,
-				             sizeof(uint16_t));
-				for (i = 0; i < msg->num_io_port; i++)
-					safe_unpack16(&msg->io_port[i],
-						      buffer);
-			}
+		safe_unpackstr_xmalloc(&msg->ofname, &uint32_tmp, buffer);
+		safe_unpackstr_xmalloc(&msg->efname, &uint32_tmp, buffer);
+		safe_unpackstr_xmalloc(&msg->ifname, &uint32_tmp, buffer);
+		safe_unpack16(&msg->num_io_port, buffer);
+		if (msg->num_io_port >= NO_VAL16)
+			goto unpack_error;
+		if (msg->num_io_port > 0) {
+			safe_xcalloc(msg->io_port, msg->num_io_port,
+			             sizeof(uint16_t));
+			for (i = 0; i < msg->num_io_port; i++)
+				safe_unpack16(&msg->io_port[i], buffer);
 		}
 		safe_unpack32(&msg->profile, buffer);
 		safe_unpackstr_xmalloc(&msg->task_prolog, &uint32_tmp, buffer);

src/slurmd/slurmstepd/mgr.c
@@ -1330,8 +1330,7 @@ fail2:
 	/*
 	 * Wait for io thread to complete (if there is one)
 	 */
-	if (!job->batch && io_initialized &&
-	    ((job->flags & LAUNCH_USER_MANAGED_IO) == 0))
+	if (!job->batch && io_initialized)
 		_wait_for_io(job);
 
 	/*
@@ -2650,7 +2649,7 @@ _slurmd_job_log_init(stepd_step_rec_t *job)
 	/*
 	 *  Connect slurmd stderr to stderr of job
 	 */
-	if ((job->flags & LAUNCH_USER_MANAGED_IO) || (job->flags & LAUNCH_PTY))
+	if (job->flags & LAUNCH_PTY)
 		fd_set_nonblocking(STDERR_FILENO);
 	if (job->task != NULL) {
 		if (dup2(job->task[0]->stderr_fd, STDERR_FILENO) < 0) {

commit 87000408ea0df0ed58100f25f6e4014eaaa7e9bf

Fix regression in 3d60368244 which caused extern step launches to fail.

src/slurmd/slurmstepd/slurmstepd_job.c
@@ -439,8 +439,13 @@ extern stepd_step_rec_t *stepd_step_rec_create(launch_tasks_request_msg_t *msg,
 		memset(&resp_addr, 0, sizeof(slurm_addr_t));
 	}
 
-	memcpy(&io_addr, &msg->orig_addr, sizeof(slurm_addr_t));
-	slurm_set_port(&io_addr, msg->io_port[nodeid % msg->num_io_port]);
+	if (msg->num_io_port) {
+		memcpy(&io_addr, &msg->orig_addr, sizeof(slurm_addr_t));
+		slurm_set_port(&io_addr,
+			       msg->io_port[nodeid % msg->num_io_port]);
+	} else {
+		memset(&io_addr, 0, sizeof(slurm_addr_t));
+	}
 
 	srun = srun_info_create(msg->cred, &resp_addr, &io_addr,
 				protocol_version);