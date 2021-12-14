SchedMD/slurm

commit 74235ac9b42397fd4251bd63a53276a608fd8ddd

Put cloud and dynamic check on its own
Prepping for a bigger "cloud" only check.

Bug 7654

src/slurmctld/node_mgr.c
@@ -488,8 +488,17 @@ extern int load_all_node_state ( bool state_only )
 			       node_name);
 		} else if (state_only) {
 			uint32_t orig_flags;
-			if (IS_NODE_CLOUD(node_ptr) ||
-			    (node_state & NODE_STATE_DYNAMIC)) {
+			if ((IS_NODE_CLOUD(node_ptr) ||
+			    (node_state & NODE_STATE_DYNAMIC)) &&
+			    comm_name && node_hostname) {
+				/* Recover NodeAddr and NodeHostName */
+				set_node_comm_name(node_ptr,
+						   comm_name,
+						   node_hostname);
+				comm_name = NULL;
+				node_hostname = NULL;
+			}
+			if (IS_NODE_CLOUD(node_ptr)) {
 				if ((!power_save_mode) &&
 				    ((node_state & NODE_STATE_POWERED_DOWN) ||
 				     (node_state & NODE_STATE_POWERING_DOWN) ||
@@ -501,14 +510,6 @@ extern int load_all_node_state ( bool state_only )
 						hostset_insert(hs, node_name);
 					else
 						hs = hostset_create(node_name);
-				}
-				if (comm_name && node_hostname) {
-					/* Recover NodeAddr and NodeHostName */
-					set_node_comm_name(node_ptr,
-							   comm_name,
-							   node_hostname);
-					comm_name = NULL;
-					node_hostname = NULL;
 				}
 				node_ptr->node_state    = node_state;
 			} else if (IS_NODE_UNKNOWN(node_ptr)) {

commit d2739c296336efcd17e297e049579f34a4e6d434

Fix dynamic future nodes remembering active state on restart
Regression caused by 74235ac9b42.

Bug 12959

Signed-off-by: Marshall Garey <marshall@schedmd.com>

src/slurmctld/node_mgr.c
@@ -498,7 +498,12 @@ extern int load_all_node_state ( bool state_only )
 				comm_name = NULL;
 				node_hostname = NULL;
 			}
-			if (IS_NODE_CLOUD(node_ptr)) {
+			if (IS_NODE_FUTURE(node_ptr) &&
+			    (node_state & NODE_STATE_DYNAMIC)) {
+				/* Preserve active dynamic future node state */
+				node_ptr->node_state    = node_state;
+
+			} else if (IS_NODE_CLOUD(node_ptr)) {
 				if ((!power_save_mode) &&
 				    ((node_state & NODE_STATE_POWERED_DOWN) ||
 				     (node_state & NODE_STATE_POWERING_DOWN) ||