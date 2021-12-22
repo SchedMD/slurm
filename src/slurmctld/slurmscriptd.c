SchedMD/slurm

commit f576ab779d99a165915b4b15488ab00254f0cbc5

Run MailProg with slurmscriptd
Bug 12513

src/slurmctld/slurmscriptd.c
@@ -622,6 +622,7 @@ static int _handle_run_script(slurmscriptd_msg_t *recv_msg)
 					&signalled);
 		break;
 	case SLURMSCRIPTD_EPILOG: /* fall-through */
+	case SLURMSCRIPTD_MAIL:
 	case SLURMSCRIPTD_PROLOG:
 		if (script_msg->job_id)
 			log_flag(SCRIPT, "Handling %s (%s) for JobId=%u",
@@ -694,6 +695,7 @@ static int _handle_script_complete(slurmscriptd_msg_t *msg)
 
 	switch (script_complete->script_type) {
 	case SLURMSCRIPTD_BB_LUA:
+	case SLURMSCRIPTD_MAIL:
 		if (script_complete->job_id)
 			log_flag(SCRIPT, "Handling %s (%s) for JobId=%u",
 				 rpc_num2string(msg->msg_type),
@@ -918,6 +920,31 @@ extern void slurmscriptd_flush_job(uint32_t job_id)
 
 	_send_to_slurmscriptd(SLURMSCRIPTD_REQUEST_FLUSH_JOB, &msg, false,
 			      NULL, NULL);
+}
+
+extern int slurmscriptd_run_mail(char *script_path, uint32_t argc, char **argv,
+				 char **env, uint32_t timeout, char **resp)
+{
+	int status;
+	run_script_msg_t run_script_msg;
+
+	memset(&run_script_msg, 0, sizeof(run_script_msg));
+
+	/* Init run_script_msg */
+	run_script_msg.argc = argc;
+	run_script_msg.argv = argv;
+	run_script_msg.env = NULL;
+	run_script_msg.script_name = "MailProg";
+	run_script_msg.script_path = script_path;
+	run_script_msg.script_type = SLURMSCRIPTD_MAIL;
+	run_script_msg.timeout = timeout;
+
+	/* Send message; wait for response */
+	status = _send_to_slurmscriptd(SLURMSCRIPTD_REQUEST_RUN_SCRIPT,
+				       &run_script_msg, true, resp, NULL);
+
+	/* Cleanup */
+	return status;
 }
 
 extern int slurmscriptd_run_bb_lua(uint32_t job_id, char *function,

RELEASE_NOTES
@@ -36,6 +36,8 @@ HIGHLIGHTS
  -- Storing batch scripts and env vars are now in indexed tables using
     substantially less disk space.  Those storing scripts in 21.08 will all
     be moved and indexed automatically.
+ -- Run MailProg through slurmscriptd instead of directly fork+exec()'ing
+    from slurmctld.
 
 CONFIGURATION FILE CHANGES (see man appropriate man page for details)
 =====================================================================

src/slurmctld/agent.c
@@ -100,6 +100,7 @@
 #include "src/slurmctld/locks.h"
 #include "src/slurmctld/ping_nodes.h"
 #include "src/slurmctld/slurmctld.h"
+#include "src/slurmctld/slurmscriptd.h"
 #include "src/slurmctld/state_save.h"
 #include "src/slurmctld/srun_comm.h"
 
@@ -108,7 +109,7 @@
 #define RPC_PACK_MAX_AGE	30	/* Rebuild data over 30 seconds old */
 #define DUMP_RPC_COUNT 		25
 #define HOSTLIST_MAX_SIZE 	80
-#define MAIL_PROG_TIMEOUT 120*1000
+#define MAIL_PROG_TIMEOUT 120 /* Timeout in seconds */
 
 typedef enum {
 	DSH_NEW,        /* Request not yet started */
@@ -1888,8 +1889,9 @@ static void *_mail_proc(void *arg)
 	char *argv[5] = {
 		slurm_conf.mail_prog, "-s", mi->message, mi->user_name, NULL};
 
-	result = run_command("MailProg", slurm_conf.mail_prog, argv,
-			     mi->environment, MAIL_PROG_TIMEOUT, 0, &status);
+	status = slurmscriptd_run_mail(slurm_conf.mail_prog, 5, argv,
+				       mi->environment, MAIL_PROG_TIMEOUT,
+				       &result);
 	if (status)
 		error("MailProg returned error, it's output was '%s'", result);
 	else if (result && (strlen(result) > 0))

src/slurmctld/slurmscriptd.h
@@ -71,6 +71,9 @@ extern int slurmscriptd_run_bb_lua(uint32_t job_id, char *function,
 				   uint32_t argc, char **argv, uint32_t timeout,
 				   char **resp, bool *track_script_signalled);
 
+extern int slurmscriptd_run_mail(char *script_path, uint32_t argc, char **argv,
+				 char **env, uint32_t timeout, char **resp);
+
 /*
  * slurmscriptd_run_prepilog
  * Tell slurmscriptd to run PrologSlurmctld or EpilogSlurmctld for the job

src/slurmctld/slurmscriptd_protocol_defs.h
@@ -49,6 +49,7 @@ typedef enum {
 				* from RPC's that weren't running a script. */
 	SLURMSCRIPTD_BB_LUA,
 	SLURMSCRIPTD_EPILOG,
+	SLURMSCRIPTD_MAIL,
 	SLURMSCRIPTD_PROLOG,
 } script_type_t;
 

commit 762178a2840e650c8ee65e804f162533760dc1e2

slurmscriptd - fix underflow for script_count
Regression from commit f576ab779d where script_count was not incremented
for MailProg. With this commit, script_count will automatically get
incremented for all SLURMSCRIPTD_REQUEST_RUN_SCRIPT RPC's.

Bug 12513

src/slurmctld/slurmscriptd.c
@@ -103,6 +103,8 @@ typedef struct {
 	bool track_script_signalled;
 } script_response_t;
 
+static void _incr_script_cnt(void);
+
 static int slurmctld_readfd = -1;
 static int slurmctld_writefd = -1;
 static pid_t slurmscriptd_pid;
@@ -288,6 +290,8 @@ static int _send_to_slurmscriptd(uint32_t msg_type, void *msg_data, bool wait,
 		rc = SLURM_ERROR;
 		goto cleanup;
 	}
+	if (msg_type == SLURMSCRIPTD_REQUEST_RUN_SCRIPT)
+		_incr_script_cnt();
 	_write_msg(slurmctld_writefd, msg.msg_type, buffer);
 
 	if (wait) {
@@ -967,7 +971,6 @@ extern int slurmscriptd_run_bb_lua(uint32_t job_id, char *function,
 	run_script_msg.timeout = timeout;
 
 	/* Send message; wait for response */
-	_incr_script_cnt();
 	status = _send_to_slurmscriptd(SLURMSCRIPTD_REQUEST_RUN_SCRIPT,
 				       &run_script_msg, true, resp,
 				       track_script_signalled);
@@ -1004,7 +1007,6 @@ extern void slurmscriptd_run_prepilog(uint32_t job_id, bool is_epilog,
 	run_script_msg.script_path = script;
 	run_script_msg.timeout = (uint32_t) slurm_conf.prolog_epilog_timeout;
 
-	_incr_script_cnt();
 	_send_to_slurmscriptd(SLURMSCRIPTD_REQUEST_RUN_SCRIPT, &run_script_msg,
 			      false, NULL, NULL);
 