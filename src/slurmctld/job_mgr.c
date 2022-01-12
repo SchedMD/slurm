8b547dba52a2e9ac8bcae755a7eaabcc72f63d93
Dominik Bartkiewicz <bart@schedmd.com>
2022-01-12 13:57:22 +0000

    Fix slurmctld segfault due to job array resv_list double free.
    Bug 13166

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -4718,6 +4718,8 @@ extern job_record_t *job_array_split(job_record_t *job_ptr)
 		       job_ptr->priority_array, i);
 	}
 	job_ptr_pend->resv_name = xstrdup(job_ptr->resv_name);
+	if (job_ptr->resv_list)
+		job_ptr_pend->resv_list = list_shallow_copy(job_ptr->resv_list);
 	job_ptr_pend->resp_host = xstrdup(job_ptr->resp_host);
 	if (job_ptr->select_jobinfo) {
 		job_ptr_pend->select_jobinfo =
