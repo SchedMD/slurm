SchedMD/slurm

commit c101bf57c9d29c3486a651486b16ea20fa456cb5

common/slurm_mpi - fix mpi_fini() to be thread-safe.
Bug 12969

src/common/slurm_mpi.c
@@ -317,10 +317,13 @@ int mpi_fini (void)
 {
 	int rc;
 
+	slurm_mutex_lock(&context_lock);
 	if (!g_context)
 		return SLURM_SUCCESS;
 
 	init_run = false;
 	rc = plugin_context_destroy(g_context);
+	g_context = NULL;
+	slurm_mutex_unlock(&context_lock);
 	return rc;
 }

commit 0a0e118d04f3b25f53f14ceac8eb3ba1e9878ec0

Avoid deadlock from successive mpi_fini() calls.
Regression from c101bf57c9.

CID 248253.

src/common/slurm_mpi.c
@@ -318,8 +318,10 @@ int mpi_fini (void)
 	int rc;
 
 	slurm_mutex_lock(&context_lock);
-	if (!g_context)
+	if (!g_context) {
+		slurm_mutex_unlock(&context_lock);
 		return SLURM_SUCCESS;
+	}
 
 	init_run = false;
 	rc = plugin_context_destroy(g_context);