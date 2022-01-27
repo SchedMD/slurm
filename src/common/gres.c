SchedMD/slurm

commit 4fb3d42333aae7bfbbc4b45c8aac65de23b7fdcb

Reduce indentation in gres_g_step_set_env()
Bug 11525

src/common/gres.c
@@ -8999,23 +8999,21 @@ extern void gres_g_step_set_env(char ***job_env_ptr, List step_gres_list)
 	for (i = 0; i < gres_context_cnt; i++) {
 		if (!gres_context[i].ops.step_set_env)
 			continue;	/* No plugin to call */
+		if (!step_gres_list)
+			continue;
 		found = false;
-		if (step_gres_list) {
-			gres_iter = list_iterator_create(step_gres_list);
-			while ((gres_ptr = (gres_state_t *)
-				list_next(gres_iter))) {
-				if (gres_ptr->plugin_id !=
-				    gres_context[i].plugin_id)
-					continue;
-				/* Set env for all tasks in step */
-				(*(gres_context[i].ops.step_set_env))
-					(job_env_ptr,
-					 gres_ptr->gres_data,
-					 GRES_INTERNAL_FLAG_NONE);
-				found = true;
-			}
-			list_iterator_destroy(gres_iter);
+		gres_iter = list_iterator_create(step_gres_list);
+		while ((gres_ptr = (gres_state_t *)list_next(gres_iter))) {
+			if (gres_ptr->plugin_id != gres_context[i].plugin_id)
+				continue;
+			/* Set env for all tasks in step */
+			(*(gres_context[i].ops.step_set_env))
+				(job_env_ptr,
+				 gres_ptr->gres_data,
+				 GRES_INTERNAL_FLAG_NONE);
+			found = true;
 		}
+		list_iterator_destroy(gres_iter);
 		if (!found) { /* No data fond */
 			(*(gres_context[i].ops.step_set_env))
 				(job_env_ptr, NULL,

commit 53af0dede07ece1448faaaabd8ceba6ab4b92d7b

Call step_set_env/task_set_env with NULL values if no step_gres_list
Regression from commit 4fb3d42333a.

Bug 13042

Co-authored by: Michael Hinton <hinton@schedmd.com>

src/common/gres.c
@@ -9161,8 +9161,12 @@ extern void gres_g_step_set_env(char ***job_env_ptr, List step_gres_list)
 		slurm_gres_context_t *gres_ctx = &gres_context[i];
 		if (!gres_ctx->ops.step_set_env)
 			continue;	/* No plugin to call */
-		if (!step_gres_list)
+		if (!step_gres_list) {
+			/* Clear GRES environment variables */
+			(*(gres_ctx->ops.step_set_env))(
+				job_env_ptr, NULL, 0, GRES_INTERNAL_FLAG_NONE);
 			continue;
+		}
 		gres_iter = list_iterator_create(step_gres_list);
 		while ((gres_state_step = list_next(gres_iter))) {
 			if (gres_state_step->plugin_id != gres_ctx->plugin_id)
@@ -9210,8 +9214,13 @@ extern void gres_g_task_set_env(char ***job_env_ptr, List step_gres_list,
 		slurm_gres_context_t *gres_ctx = &gres_context[i];
 		if (!gres_ctx->ops.task_set_env)
 			continue;	/* No plugin to call */
-		if (!step_gres_list)
+		if (!step_gres_list) {
+			/* Clear GRES environment variables */
+			(*(gres_ctx->ops.task_set_env))(
+				job_env_ptr, NULL, 0, NULL,
+				GRES_INTERNAL_FLAG_NONE);
 			continue;
+		}
 
 
 		gres_iter = list_iterator_create(step_gres_list);