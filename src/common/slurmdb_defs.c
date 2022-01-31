c61f1ae5abd746003ca9dbb90c7b70b12bb7b40c
Scott Hilton <scott@schedmd.com>
2022-01-31 13:45:04 -0700

    Fix sacctmgr to set MaxJobsAccruePer{User|Account} and MinPrioThres in sacctmgr add qos, modify already worked correctly.
    Bug 13287

--- src/common/slurmdb_defs.c
+++ src/common/slurmdb_defs.c
@@ -3473,6 +3473,8 @@ extern void slurmdb_copy_qos_rec_limits(slurmdb_qos_rec_t *out,
 
 	out->max_jobs_pa = in->max_jobs_pa;
 	out->max_jobs_pu = in->max_jobs_pu;
+	out->max_jobs_accrue_pa = in->max_jobs_accrue_pa;
+	out->max_jobs_accrue_pu = in->max_jobs_accrue_pu;
 	out->max_submit_jobs_pa = in->max_submit_jobs_pa;
 	out->max_submit_jobs_pu = in->max_submit_jobs_pu;
 	xfree(out->max_tres_mins_pj);
@@ -3490,6 +3492,7 @@ extern void slurmdb_copy_qos_rec_limits(slurmdb_qos_rec_t *out,
 	xfree(out->max_tres_run_mins_pu);
 	out->max_tres_run_mins_pu = xstrdup(in->max_tres_run_mins_pu);
 	out->max_wall_pj = in->max_wall_pj;
+	out->min_prio_thresh = in->min_prio_thresh;
 	xfree(out->min_tres_pj);
 	out->min_tres_pj = xstrdup(in->min_tres_pj);
 
