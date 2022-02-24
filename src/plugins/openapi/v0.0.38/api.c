c85a20d819b3f6b90fdf5e934c8d745f3d6776c6
Nathan Rini <nate@schedmd.com>
2022-02-24 16:51:17 -0700

    openapi/v0.0.38 - use new operationId generation to ensure uniqueness.
    Simplify existing operationIds to remove redundancy as they no longer
    need to be unique in the openapi.json file.
    Activate OAS_FLAG_MANGLE_OPID to mangle the operationIds with the path.
    
    Bug 12897

--- src/plugins/openapi/v0.0.38/api.c
+++ src/plugins/openapi/v0.0.38/api.c
@@ -164,6 +164,8 @@ extern data_t *slurm_openapi_p_get_specification(openapi_spec_flags_t *flags)
 {
 	data_t *spec = NULL;
 
+	*flags |= OAS_FLAG_MANGLE_OPID;
+
 	static_ref_json_to_data_t(spec, openapi_json);
 
 	return spec;
