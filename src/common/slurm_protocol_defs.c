SchedMD/slurm

commit b92429559b989abf8d877aa8d7597c665ea23cd0

Add PreemptMode=WITHIN flag and PREEMPT_MODE_WITHIN macro.
Bug 10062.

src/common/slurm_protocol_defs.c
@@ -2725,6 +2725,8 @@ extern const char *preempt_mode_string(uint16_t preempt_mode)
 		return "OFF";
 	if (preempt_mode == PREEMPT_MODE_GANG)
 		return "GANG";
+	if (preempt_mode == PREEMPT_MODE_WITHIN)
+		return "WITHIN";
 
 	if (preempt_mode & PREEMPT_MODE_GANG) {
 		preempt_mode &= (~PREEMPT_MODE_GANG);
@@ -2735,6 +2737,15 @@ extern const char *preempt_mode_string(uint16_t preempt_mode)
 		else if (preempt_mode == PREEMPT_MODE_SUSPEND)
 			return "GANG,SUSPEND";
 		return "GANG,UNKNOWN";
+	} else if (preempt_mode & PREEMPT_MODE_WITHIN) {
+		preempt_mode &= (~PREEMPT_MODE_WITHIN);
+		if (preempt_mode == PREEMPT_MODE_CANCEL)
+			return "WITHIN,CANCEL";
+		else if (preempt_mode == PREEMPT_MODE_REQUEUE)
+			return "WITHIN,REQUEUE";
+		else if (preempt_mode == PREEMPT_MODE_SUSPEND)
+			return "WITHIN,SUSPEND";
+		return "WITHIN,UNKNOWN";
 	} else {
 		if (preempt_mode == PREEMPT_MODE_CANCEL)
 			return "CANCEL";
@@ -2761,6 +2772,8 @@ extern uint16_t preempt_mode_num(const char *preempt_mode)
 	while (tok) {
 		if (xstrcasecmp(tok, "gang") == 0) {
 			mode_num |= PREEMPT_MODE_GANG;
+		} else if (!xstrcasecmp(tok, "within")) {
+			mode_num |= PREEMPT_MODE_WITHIN;
 		} else if ((xstrcasecmp(tok, "off") == 0)
 			   || (xstrcasecmp(tok, "cluster") == 0)) {
 			mode_num += PREEMPT_MODE_OFF;
@@ -2783,7 +2796,15 @@ extern uint16_t preempt_mode_num(const char *preempt_mode)
 		tok = strtok_r(NULL, ",", &last);
 	}
 	xfree(tmp_str);
+
 	if (preempt_modes > 1) {
+		/*
+		 * Only one mode value may be set, optionally combined with
+		 * GANG or WITHIN.
+		 */
+		mode_num = NO_VAL16;
+	} else if (mode_num & (PREEMPT_MODE_GANG | PREEMPT_MODE_WITHIN)) {
+		/* "GANG,WITHIN" is an invalid combination */
 		mode_num = NO_VAL16;
 	}
 

slurm/slurm.h
@@ -2714,6 +2714,7 @@ typedef struct reservation_name_msg {
 
 #define PREEMPT_MODE_CANCEL	0x0008	/* always cancel the job */
 #define PREEMPT_MODE_COND_OFF	0x0010	/* represents PREEMPT_MODE_OFF in list*/
+#define PREEMPT_MODE_WITHIN	0x4000	/* enable preemption within qos */
 #define PREEMPT_MODE_GANG	0x8000	/* enable gang scheduling */
 
 #define RECONFIG_KEEP_PART_INFO 0x0001 /* keep dynamic partition info on scontrol reconfig */

src/common/read_config.c
@@ -4345,6 +4345,7 @@ static int _validate_and_set_defaults(slurm_conf_t *conf,
 		conf->preempt_type = xstrdup(DEFAULT_PREEMPT_TYPE);
 	if (xstrcmp(conf->preempt_type, "preempt/qos") == 0) {
 		int preempt_mode = conf->preempt_mode & (~PREEMPT_MODE_GANG);
+		preempt_mode &= ~PREEMPT_MODE_WITHIN;
 		if (preempt_mode == PREEMPT_MODE_OFF) {
 			error("PreemptType and PreemptMode values "
 			      "incompatible");
@@ -4352,6 +4353,7 @@ static int _validate_and_set_defaults(slurm_conf_t *conf,
 		}
 	} else if (xstrcmp(conf->preempt_type, "preempt/partition_prio") == 0) {
 		int preempt_mode = conf->preempt_mode & (~PREEMPT_MODE_GANG);
+		preempt_mode &= ~PREEMPT_MODE_WITHIN;
 		if (preempt_mode == PREEMPT_MODE_OFF) {
 			error("PreemptType and PreemptMode values "
 			      "incompatible");

commit 7be460032f1d033b877802d7745cb8baa5c317db

Fix incorrect logic breaking gang scheduling.
Regression in b92429559b.

src/common/slurm_protocol_defs.c
@@ -2803,7 +2803,8 @@ extern uint16_t preempt_mode_num(const char *preempt_mode)
 		 * GANG or WITHIN.
 		 */
 		mode_num = NO_VAL16;
-	} else if (mode_num & (PREEMPT_MODE_GANG | PREEMPT_MODE_WITHIN)) {
+	} else if ((mode_num & PREEMPT_MODE_GANG) &&
+		   (mode_num & PREEMPT_MODE_WITHIN)) {
 		/* "GANG,WITHIN" is an invalid combination */
 		mode_num = NO_VAL16;
 	}