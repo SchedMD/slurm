a4b36fa84f6aa31c4c3a6f2a026ad38c754bb862
Michael Hinton <hinton@schedmd.com>
2022-03-10 10:44:01 -0700

    Fix GANG PreemptMode checks for gs_fini() and gs_wake_jobs() operations.
    A reconfigure needs to be able to disable Gang scheduling with
    gs_wake_jobs() and gs_fini() in _update_preempt() after the gang flag gets
    unset. And gs_fini() should only be called from sched_g_fini() when Gang
    is enabled.
    
    Bug 13594
    
    Co-authored-by: Alejandro Sanchez <alex@schedmd.com>

--- src/slurmctld/gang.c
+++ src/slurmctld/gang.c
@@ -1130,9 +1130,6 @@ extern void gs_init(void)
 /* Terminate the gang scheduling thread and free its data structures */
 extern void gs_fini(void)
 {
-	if (!(slurm_conf.preempt_mode & PREEMPT_MODE_GANG))
-		return;
-
 	/* terminate the timeslicer thread */
 	log_flag(GANG, "gang: entering gs_fini");
 	slurm_mutex_lock(&thread_flag_mutex);
@@ -1209,9 +1206,6 @@ extern void gs_wake_jobs(void)
 {
 	job_record_t *job_ptr;
 	ListIterator job_iterator;
-
-	if (!(slurm_conf.preempt_mode & PREEMPT_MODE_GANG))
-		return;
 
 	if (!job_list)	/* no jobs */
 		return;
