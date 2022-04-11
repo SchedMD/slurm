988f6e0c502f31ea27e8e60b6c2924c550f7a48f
Marcin Stolarek <cinek@schedmd.com>
2022-04-11 11:52:02 +0000

    common/data - return NULL for empty list in data_resolve_dict_path()
    This makes it easier to interpret the result.
    
    Bug 13751

--- src/common/data.c
+++ src/common/data.c
@@ -1920,6 +1920,13 @@ extern data_t *data_resolve_dict_path(data_t *data, const char *path)
 		log_flag(DATA, "%s: data (0x%"PRIXPTR") failed to resolve dictionary path \"%s\"",
 			 __func__, (uintptr_t) data, path);
 
+	if ((data_get_type(found) == DATA_TYPE_LIST) &&
+	    (!found->data.list_u->count)) {
+		log_flag(DATA, "%s: Returning NULL for a 0 count list",
+			 __func__);
+		return NULL;
+	}
+
 	return found;
 }
 
