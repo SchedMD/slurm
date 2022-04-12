SchedMD/slurm

commit 1563a1a689e99632a5d783445933857bc5d755a2

Preserve dynamic nodes on reconfig
Bug 13679

src/slurmctld/read_config.c
@@ -1304,6 +1304,36 @@ void _sync_jobs_to_conf(void)
 	last_job_update = now;
 }
 
+static int _find_config_ptr(void *x, void *arg)
+{
+	return (x == arg);
+}
+
+static void _preserve_dynamic_nodes(node_record_t **old_node_table_ptr,
+				    int old_node_record_count,
+				    List old_config_list)
+{
+	for (int i = 0; i < old_node_record_count; i++) {
+		node_record_t *node_ptr = old_node_table_ptr[i];
+
+		if (!node_ptr ||
+		    !IS_NODE_DYNAMIC(node_ptr) ||
+		    find_node_record2(node_ptr->name))
+			continue;
+
+		insert_node_record(node_ptr);
+		old_node_table_ptr[i] = NULL;
+
+		/*
+		 * insert_node_record() appends node_ptr->config_ptr to the
+		 * global config_list. remove from old config_list so it
+		 * doesn't get free'd.
+		 */
+		list_remove_first(old_config_list, _find_config_ptr,
+				  node_ptr->config_ptr);
+	}
+}
+
 /*
  * read_slurm_conf - load the slurm configuration from the configured file.
  * read_slurm_conf can be called more than once if so desired.
@@ -1326,7 +1356,7 @@ int read_slurm_conf(int recover, bool reconfig)
 	int i, rc = 0, load_job_ret = SLURM_SUCCESS;
 	int old_node_record_count = 0;
 	node_record_t **old_node_table_ptr = NULL, *node_ptr;
-	List old_part_list = NULL;
+	List old_part_list = NULL, old_config_list = NULL;
 	char *old_def_part_name = NULL;
 	char *old_auth_type = xstrdup(slurm_conf.authtype);
 	char *old_bb_type = xstrdup(slurm_conf.bb_type);
@@ -1365,6 +1395,8 @@ int read_slurm_conf(int recover, bool reconfig)
 			node_ptr->port   = node_ptr->config_ptr->cpus;
 			node_ptr->weight = node_ptr->config_ptr->weight;
 		}
+		old_config_list = config_list;
+		config_list = NULL;
 		node_record_table_ptr = NULL;
 		node_record_count = 0;
 		xhash_free(node_hash_table);
@@ -1505,6 +1537,10 @@ int read_slurm_conf(int recover, bool reconfig)
 			rc = _restore_node_state(recover, old_node_table_ptr,
 						 old_node_record_count);
 			error_code = MAX(error_code, rc);  /* not fatal */
+
+			_preserve_dynamic_nodes(old_node_table_ptr,
+						old_node_record_count,
+						old_config_list);
 		}
 		if (old_part_list && ((recover > 1) ||
 		    (slurm_conf.reconfig_flags & RECONFIG_KEEP_PART_INFO))) {
@@ -1579,6 +1615,7 @@ int read_slurm_conf(int recover, bool reconfig)
 	(void) sync_job_files();
 	_purge_old_node_state(old_node_table_ptr, old_node_record_count);
 	_purge_old_part_state(old_part_list, old_def_part_name);
+	FREE_NULL_LIST(old_config_list);
 
 	reserve_port_config(slurm_conf.mpi_params);
 

src/common/node_conf.c
@@ -835,6 +835,41 @@ extern node_record_t *add_node_record(char *alias, config_record_t *config_ptr)
 	return node_ptr;
 }
 
+static int _find_config_ptr(void *x, void *arg)
+{
+	return (x == arg);
+}
+
+extern void insert_node_record(node_record_t *node_ptr)
+{
+	for (int i = 0; i < node_record_count; i++) {
+		if (node_record_table_ptr[i])
+			continue;
+
+		if (i > last_node_index)
+			last_node_index = i;
+
+		if (!node_ptr->config_ptr)
+			error("node should have config_ptr from previous tables");
+
+		if (!list_find_first(config_list, _find_config_ptr,
+				     node_ptr->config_ptr))
+			list_append(config_list, node_ptr->config_ptr);
+
+		node_record_table_ptr[i] = node_ptr;
+		xhash_add(node_hash_table, node_ptr);
+
+		/* re-add node to conf node hash tables */
+		slurm_reset_alias(node_ptr->name,
+				  node_ptr->comm_name,
+				  node_ptr->node_hostname);
+		return;
+	}
+
+	error("Not able to add node '%s' to node_record_table_ptr",
+	      node_ptr->name);
+}
+
 extern void delete_node_record(char *name)
 {
 	int node_index;
@@ -965,6 +1000,8 @@ extern void init_node_conf(void)
 		purge_node_rec(node_ptr);
 
 	node_record_count = 0;
+	node_record_table_size = 0;
+	last_node_index = -1;
 	xfree(node_record_table_ptr);
 	xhash_free(node_hash_table);
 

src/common/node_conf.h
@@ -316,6 +316,15 @@ extern node_record_t *create_node_record_at(int index, char *node_name,
 extern node_record_t *add_node_record(char *alias, config_record_t *config_ptr);
 
 /*
+ * Add existing record to node_record_table_ptr
+ *
+ * e.g. Preserving dynamic nodes after a reconfig.
+ * Node must fit in currently allocated node_record_count/MaxNodeCount.
+ * node_ptr->config_ptr is added to the the global config_list.
+ */
+extern void insert_node_record(node_record_t *node_ptr);
+
+/*
  * Delete node from node_record_table_ptr.
  *
  * IN name - name of node_record_t to delete

commit a7c09abfc0320df73761d3db658351f8839774c7

slurmctld/read_config - fix front_end_list memory leak.
Regression from 1563a1a689.

Bug 13784

src/slurmctld/read_config.c
@@ -1396,6 +1396,7 @@ int read_slurm_conf(int recover, bool reconfig)
 		}
 		old_config_list = config_list;
 		config_list = NULL;
+		FREE_NULL_LIST(front_end_list);
 		node_record_table_ptr = NULL;
 		node_record_count = 0;
 		xhash_free(node_hash_table);