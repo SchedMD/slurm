e31c90932fb705c1263be7913cddb02e23a33d00
Tim Wickberg <tim@schedmd.com>
2022-04-22 18:32:29 -0600

    Add SLURM_JOB_RESERVATION to SlurmctldProlog/SlurmctldEpilog.

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -19182,6 +19182,11 @@ extern char **job_common_env_vars(job_record_t *job_ptr, bool is_complete)
 		setenvf(&my_env, "SLURM_JOB_PARTITION", "%s",
 			job_ptr->partition);
 	}
+
+	if (job_ptr->resv_ptr)
+		setenvf(&my_env, "SLURM_JOB_RESERVATION", "%s",
+			job_ptr->resv_ptr->name);
+
 	setenvf(&my_env, "SLURM_JOB_UID", "%u", job_ptr->user_id);
 	name = uid_to_string((uid_t) job_ptr->user_id);
 	setenvf(&my_env, "SLURM_JOB_USER", "%s", name);
