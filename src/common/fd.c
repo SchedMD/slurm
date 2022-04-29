8275b1e3be7c278c6da07a9bb325860d71564805
Marshall Garey <marshall@schedmd.com>
2022-04-29 15:23:19 -0600

    Speed up closeall().
    Blindly closing all possible file descriptors is slow.
    
    This commit drastically reduces the time to complete closeall() by only
    closing the currently open file descriptors.
    
    If /proc/self/fd is unavailable for some reason, fall back to blindly
    iterating over the full range of file descriptors.
    
    Bug 13967

--- src/common/fd.c
+++ src/common/fd.c
@@ -35,6 +35,7 @@
  *  Refer to "fd.h" for documentation on public functions.
 \*****************************************************************************/
 
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
@@ -70,7 +71,7 @@ strong_alias(receive_fd_over_pipe, slurm_receive_fd_over_pipe);
 static int fd_get_lock(int fd, int cmd, int type);
 static pid_t fd_test_lock(int fd, int type);
 
-extern void closeall(int fd)
+static void _slow_closeall(int fd)
 {
 	struct rlimit rlim;
 
@@ -81,6 +82,36 @@ extern void closeall(int fd)
 
 	while (fd < rlim.rlim_cur)
 		close(fd++);
+}
+
+extern void closeall(int fd)
+{
+	char *name = "/proc/self/fd";
+	DIR *d;
+	struct dirent *dir;
+
+	/*
+	 * Blindly closing all file descriptors is slow.
+	 *
+	 * Instead get all open file descriptors from /proc/self/fd, then
+	 * close each one of those that are greater than or equal to fd.
+	 */
+	if (!(d = opendir(name))) {
+		debug("Could not read open files from %s: %m, closing all potential file descriptors",
+		      name);
+		_slow_closeall(fd);
+		return;
+	}
+
+	while ((dir = readdir(d))) {
+		/* Ignore "." and ".." entries */
+		if (dir->d_type != DT_DIR) {
+			int open_fd = atoi(dir->d_name);
+			if (open_fd >= fd)
+				close(fd);
+		}
+	}
+	closedir(d);
 }
 
 void fd_set_close_on_exec(int fd)
