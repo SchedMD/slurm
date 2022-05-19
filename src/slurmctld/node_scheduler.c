SchedMD/slurm

commit 459cfa6fa916f21bdd0020e4dceda4b3053bd98e

If requesting --ntasks-per-node with no tasks set tasks correctly.
Bug 5977

src/slurmctld/node_scheduler.c
@@ -1343,11 +1343,6 @@ _get_req_features(struct node_set *node_set_ptr, int node_set_size,
 			saved_job_num_tasks = job_ptr->details->num_tasks;
 			job_ptr->details->min_nodes = feat_ptr->count;
 			job_ptr->details->min_cpus = feat_ptr->count;
-			if (job_ptr->details->ntasks_per_node &&
-			    job_ptr->details->num_tasks) {
-				job_ptr->details->num_tasks = min_nodes *
-					job_ptr->details->ntasks_per_node;
-			}
 			FREE_NULL_LIST(*preemptee_job_list);
 			job_ptr->details->pn_min_memory = orig_req_mem;
 			if (sort_again) {
@@ -4219,6 +4214,12 @@ extern void build_node_details(struct job_record *job_ptr, bool new_alloc)
 	if ((host_list = hostlist_create(job_ptr->nodes)) == NULL)
 		fatal("hostlist_create error for %s: %m", job_ptr->nodes);
 	job_ptr->total_nodes = job_ptr->node_cnt = hostlist_count(host_list);
+
+	/* Update the job num_tasks to account for variable node count jobs */
+	if (job_ptr->details->ntasks_per_node && job_ptr->details->num_tasks)
+		job_ptr->details->num_tasks = job_ptr->node_cnt *
+			job_ptr->details->ntasks_per_node;
+
 	xrealloc(job_ptr->node_addr,
 		 (sizeof(slurm_addr_t) * job_ptr->node_cnt));
 

src/salloc/salloc.c
@@ -509,6 +509,11 @@ int main(int argc, char **argv)
 		while ((desc = (job_desc_msg_t *) list_next(iter_req))) {
 			alloc = (resource_allocation_response_msg_t *)
 				list_next(iter_resp);
+
+			if (alloc && desc && (desc->bitflags & JOB_NTASKS_SET))
+				desc->num_tasks =
+					alloc->node_cnt * desc->ntasks_per_node;
+
 			if (env_array_for_job(&env, alloc, desc, i++) !=
 			    SLURM_SUCCESS)
 				goto relinquish;
@@ -516,6 +521,9 @@ int main(int argc, char **argv)
 		list_iterator_destroy(iter_resp);
 		list_iterator_destroy(iter_req);
 	} else {
+		if (alloc && desc && (desc->bitflags & JOB_NTASKS_SET))
+			desc->num_tasks =
+				alloc->node_cnt * desc->ntasks_per_node;
 		if (env_array_for_job(&env, alloc, desc, -1) != SLURM_SUCCESS)
 			goto relinquish;
 	}

commit 4f40a5c179b189cf81e3ec4ca109b44870437549

Fix node allocation w/ --constraint and --ntasks-per-node
If ntasks_per_node is set, num_tasks is recalculated before
_pick_best_nodes is called. This ensures that the correct # of nodes
is allocated according to the # of tasks.

This fixes a regression caused by commit 459cfa6f, restoring the fixed
behavior originally in commit 28a8dec1.

Bug 13986

src/slurmctld/node_scheduler.c
@@ -1311,6 +1311,13 @@ static int _get_req_features(struct node_set *node_set_ptr, int node_set_size,
 			saved_job_num_tasks = job_ptr->details->num_tasks;
 			job_ptr->details->min_nodes = feat_ptr->count;
 			job_ptr->details->min_cpus = feat_ptr->count;
+			/*
+			 * Ensure that num_tasks is accurate if ntasks_per_node
+			 * is set
+			 */
+			if (job_ptr->details->ntasks_per_node)
+				job_ptr->details->num_tasks = min_nodes *
+					job_ptr->details->ntasks_per_node;
 			FREE_NULL_LIST(*preemptee_job_list);
 			job_ptr->details->pn_min_memory = orig_req_mem;
 			if (sort_again) {