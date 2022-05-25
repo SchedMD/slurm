016b8aa3ac3d5af0f93c3e21b88f1e2d3a7bd03f
Nathan Rini <nate@schedmd.com>
2022-05-25 16:10:24 -0600

    simplify if() logic in slurm_send_node_msg
    Bug 13228

--- src/common/slurm_protocol_api.c
+++ src/common/slurm_protocol_api.c
@@ -1995,15 +1995,17 @@ int slurm_send_node_msg(int fd, slurm_msg_t * msg)
 	 */
 	rc = slurm_bufs_sendto(fd, buffers);
 
-	if ((rc < 0) && (errno == ENOTCONN)) {
+	if (rc >= 0) {
+		/* sent successfully */
+	} else if (errno == ENOTCONN) {
 		log_flag(NET, "%s: peer has disappeared for msg_type=%u",
 			 __func__, msg->msg_type);
-	} else if ((rc < 0) && (errno == EBADF)) {
+	} else if (errno == EBADF) {
 		/* failure of sendto() and peer lookup will never work */
 		error("%s: slurm_msg_sendto(fd=%d) with msg_type=%s failed: %s",
 		      __func__, fd, rpc_num2string(msg->msg_type),
 		      slurm_strerror(rc));
-	} else if (rc < 0) {
+	} else {
 		char *peer = fd_resolve_peer(fd);
 		error("%s: [%s] slurm_msg_sendto(msg_type=%s) failed: %s",
 		      __func__, peer, rpc_num2string(msg->msg_type),
