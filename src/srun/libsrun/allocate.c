SchedMD/slurm

commit f18b8c2f9136db210cf93586084b3f4b4c601a4c

srun - avoid waiting for nodes to be ready when het comp job fails to launch
srun (with allocation) would wait until a timeout for nodes to be ready and
when a het comp job failed to launch (ex. after a node prolog failure) srun
would wait until timeout to end. This fix avoids the wait under this condition.

Bug 13747

src/srun/libsrun/allocate.c
@@ -87,6 +87,7 @@ static void _signal_while_allocating(int signo);
 static int _wait_nodes_ready(resource_allocation_response_msg_t *alloc);
 
 static sig_atomic_t destroy_job = 0;
+static bool is_het_job = false;
 
 static void _set_pending_job_id(uint32_t job_id)
 {
@@ -137,15 +138,16 @@ static void _signal_while_allocating(int signo)
 /* This typically signifies the job was cancelled by scancel */
 static void _job_complete_handler(srun_job_complete_msg_t *msg)
 {
-	if (pending_job_id && (pending_job_id != msg->job_id)) {
+	if (!is_het_job && pending_job_id && (pending_job_id != msg->job_id)) {
 		error("Ignoring job_complete for job %u because our job ID is %u",
 		      msg->job_id, pending_job_id);
 		return;
 	}
 
-	if (msg->step_id == NO_VAL)
+	if (msg->step_id == NO_VAL) {
 		info("Force Terminated job %u", msg->job_id);
-	else
+		destroy_job = 1;
+	} else
 		info("Force Terminated %ps", msg);
 }
 
@@ -260,6 +262,8 @@ static int _wait_nodes_ready(resource_allocation_response_msg_t *alloc)
 		rc = slurm_job_node_ready(alloc->job_id);
 		if (rc == READY_JOB_FATAL)
 			break;				/* fatal error */
+		if (destroy_job)
+			break;
 		if ((rc == READY_JOB_ERROR) || (rc == EAGAIN))
 			continue;			/* retry */
 		if ((rc & READY_JOB_STATE) == 0) {	/* job killed */
@@ -271,8 +275,6 @@ static int _wait_nodes_ready(resource_allocation_response_msg_t *alloc)
 			is_ready = 1;
 			break;
 		}
-		if (destroy_job)
-			break;
 	}
 	if (is_ready) {
 		resource_allocation_response_msg_t *resp;
@@ -555,6 +557,8 @@ List allocate_het_job_nodes(bool handle_signals)
 		for (i = 0; sig_array[i]; i++)
 			xsignal(sig_array[i], _signal_while_allocating);
 	}
+
+	is_het_job = true;
 
 	while (first_opt && !job_resp_list) {
 		job_resp_list = slurm_allocate_het_job_blocking(job_req_list,

commit f8aa58d50a8dd842971af7c6a9e43ea6d40d09dc

Fix race caused by f18b8c2f91 in test1.23.
This also simplifies the print here as %ps handles step_id = NO_VAL.

We only want to print the Force Terminated if we really were.  Before
we could race into this we were just denied an allocation for one reason
or the other.

Continuation of f18b8c2f91

Bug 14108

src/srun/libsrun/allocate.c
@@ -88,6 +88,7 @@ static int _wait_nodes_ready(resource_allocation_response_msg_t *alloc);
 
 static sig_atomic_t destroy_job = 0;
 static bool is_het_job = false;
+static bool revoke_job = false;
 
 static void _set_pending_job_id(uint32_t job_id)
 {
@@ -144,11 +145,10 @@ static void _job_complete_handler(srun_job_complete_msg_t *msg)
 		return;
 	}
 
-	if (msg->step_id == NO_VAL) {
-		info("Force Terminated job %u", msg->job_id);
-		destroy_job = 1;
-	} else
+	/* Only print if we know we were signaled */
+	if (destroy_job)
 		info("Force Terminated %ps", msg);
+	revoke_job = true;
 }
 
 /*
@@ -262,7 +262,7 @@ static int _wait_nodes_ready(resource_allocation_response_msg_t *alloc)
 		rc = slurm_job_node_ready(alloc->job_id);
 		if (rc == READY_JOB_FATAL)
 			break;				/* fatal error */
-		if (destroy_job)
+		if (destroy_job || revoke_job)
 			break;
 		if ((rc == READY_JOB_ERROR) || (rc == EAGAIN))
 			continue;			/* retry */
@@ -452,7 +452,7 @@ extern resource_allocation_response_msg_t *
 				error("Something is wrong with the boot of the nodes.");
 			goto relinquish;
 		}
-	} else if (destroy_job) {
+	} else if (destroy_job || revoke_job) {
 		goto relinquish;
 	}
 
@@ -465,7 +465,7 @@ extern resource_allocation_response_msg_t *
 
 relinquish:
 	if (resp) {
-		if (destroy_job)
+		if (destroy_job || revoke_job)
 			slurm_complete_job(resp->job_id, 1);
 		slurm_free_resource_allocation_response_msg(resp);
 	}

testsuite/expect/test1.38
@@ -83,7 +83,7 @@ expect {
 		cancel_job $job_id
 		exp_continue
 	}
-	-re "Force Terminated job" {
+	-re "Job step aborted" {
 		set match_term 1
 		exp_continue
 	}