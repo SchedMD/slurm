SchedMD/slurm

commit 84d16737529cfb3360f4c722f0cbe35f2a48f3e9

Make it so we don't have to xstrdup things.
We are able to just use the already existent pointers since we are
lock protected

Bug 14045

src/plugins/priority/multifactor/priority_multifactor.c
@@ -1479,8 +1479,12 @@ static void _filter_job(job_record_t *job_ptr,
 				slurm_copy_priority_factors_object(obj,
 							job_ptr->prio_factors);
 			}
+			/*
+			 * Don't xstrdup dup anything here, the list is freed
+			 * with xfree_ptr.
+			 */
 			obj->job_id = job_ptr->job_id;
-			obj->partition = xstrdup(job_part_ptr->name);
+			obj->partition = job_part_ptr->name;
 			obj->user_id = job_ptr->user_id;
 			list_append(ret_list, obj);
 		}
@@ -1513,8 +1517,12 @@ static void _filter_job(job_record_t *job_ptr,
 				 job_part_ptr->priority_job_factor :
 				 job_part_ptr->norm_priority) *
 				(double)weight_part;
+			/*
+			 * Don't xstrdup dup anything here, the list is freed
+			 * with xfree_ptr.
+			 */
 			obj->job_id = job_ptr->job_id;
-			obj->partition = xstrdup(job_part_ptr->name);
+			obj->partition = job_part_ptr->name;
 			obj->user_id = job_ptr->user_id;
 
 			if (obj->priority_tres) {
@@ -1935,7 +1943,7 @@ extern List priority_p_get_priority_factors_list(
 	if (job_list && list_count(job_list)) {
 		time_t use_time;
 
-		ret_list = list_create(slurm_destroy_priority_factors_object);
+		ret_list = list_create(xfree_ptr);
 		itr = list_iterator_create(job_list);
 		while ((job_ptr = list_next(itr))) {
 			if (!(flags & PRIORITY_FLAGS_CALCULATE_RUNNING) &&

commit 5a022fa13bb4fff463dc3c81389bede9cfc9f165

Use new slurm_destroy_priority_factors() when creating list.
Fixes memory leaks caused by 84d1673752

src/plugins/priority/multifactor/priority_multifactor.c
@@ -152,6 +152,17 @@ static double decay_factor = 1; /* The decay factor when decaying time. */
 static void _priority_p_set_assoc_usage_debug(slurmdb_assoc_rec_t *assoc);
 static void _set_assoc_usage_efctv(slurmdb_assoc_rec_t *assoc);
 
+static void _destroy_priority_factors_obj_light(void *object)
+{
+	priority_factors_object_t *obj_ptr = object;
+
+	if (!obj_ptr)
+		return;
+
+	slurm_destroy_priority_factors(obj_ptr->prio_factors);
+	xfree(obj_ptr);
+}
+
 /*
  * apply decay factor to all associations usage_raw
  * IN: real_decay - decay to be applied to each associations' used
@@ -1953,7 +1964,7 @@ extern List priority_p_get_priority_factors_list(
 	if (job_list && list_count(job_list)) {
 		time_t use_time;
 
-		ret_list = list_create(xfree_ptr);
+		ret_list = list_create(_destroy_priority_factors_obj_light);
 		itr = list_iterator_create(job_list);
 		while ((job_ptr = list_next(itr))) {
 			if (!(flags & PRIORITY_FLAGS_CALCULATE_RUNNING) &&