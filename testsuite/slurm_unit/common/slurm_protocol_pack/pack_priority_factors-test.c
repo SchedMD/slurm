036491aa621f1865d73fbcfdb57911c0d7833652
Danny Auble <da@schedmd.com>
2022-06-01 17:11:06 -0600

    Fix test to match structure change.
    Continuation of 748454bcb1

--- testsuite/slurm_unit/common/slurm_protocol_pack/pack_priority_factors-test.c
+++ testsuite/slurm_unit/common/slurm_protocol_pack/pack_priority_factors-test.c
@@ -9,6 +9,7 @@
 #include "src/common/assoc_mgr.h"
 
 priority_factors_object_t pack_req = {0};
+priority_factors_t pack_f_req = {0};
 
 #ifndef NDEBUG
 START_TEST(pack_null_resp)
@@ -27,40 +28,41 @@ END_TEST
 
 void setup()
 {
+	pack_req.prio_factors = &pack_f_req;
 	pack_req.cluster_name = xstrdup("blah");
 	pack_req.job_id = 12345;
 	pack_req.partition = xstrdup("part");
 	pack_req.user_id = 1111;
 
-	pack_req.priority_age = 20;
-	pack_req.priority_assoc = 21;
-	pack_req.priority_fs = 22;
-	pack_req.priority_js = 23;
-	pack_req.priority_part = 24;
-	pack_req.priority_qos = 25;
-	pack_req.direct_prio = 26;
-	pack_req.priority_site = 27;
+	pack_f_req.priority_age = 20;
+	pack_f_req.priority_assoc = 21;
+	pack_f_req.priority_fs = 22;
+	pack_f_req.priority_js = 23;
+	pack_f_req.priority_part = 24;
+	pack_f_req.priority_qos = 25;
+	pack_req.direct_prio = 0;
+	pack_f_req.priority_site = 27;
 
-	pack_req.tres_cnt = 4;
-	pack_req.tres_weights = xmalloc(sizeof(double) * pack_req.tres_cnt);
-	pack_req.tres_weights[0] = 30;
-	pack_req.tres_weights[1] = 31;
-	pack_req.tres_weights[2] = 32;
-	pack_req.tres_weights[3] = 33;
+	pack_f_req.tres_cnt = 4;
+	pack_f_req.tres_weights = xmalloc(sizeof(double) * pack_f_req.tres_cnt);
+	pack_f_req.tres_weights[0] = 30;
+	pack_f_req.tres_weights[1] = 31;
+	pack_f_req.tres_weights[2] = 32;
+	pack_f_req.tres_weights[3] = 33;
 
-	pack_req.priority_tres = xmalloc(sizeof(double) * pack_req.tres_cnt);
-	pack_req.priority_tres[0] = 40;
-	pack_req.priority_tres[1] = 41;
-	pack_req.priority_tres[2] = 42;
-	pack_req.priority_tres[3] = 43;
+	pack_f_req.priority_tres = xmalloc(sizeof(double) * pack_f_req.tres_cnt);
+	pack_f_req.priority_tres[0] = 40;
+	pack_f_req.priority_tres[1] = 41;
+	pack_f_req.priority_tres[2] = 42;
+	pack_f_req.priority_tres[3] = 43;
 
-	assoc_mgr_tres_name_array = xmalloc(sizeof(char *) * pack_req.tres_cnt);
+	assoc_mgr_tres_name_array = xmalloc(sizeof(char *) * pack_f_req.tres_cnt);
 	assoc_mgr_tres_name_array[0] = xstrdup("hello1");
 	assoc_mgr_tres_name_array[1] = xstrdup("hello2");
 	assoc_mgr_tres_name_array[2] = xstrdup("hello3");
 	assoc_mgr_tres_name_array[3] = xstrdup("hello4");
 
-	pack_req.nice = 50;
+	pack_f_req.nice = 50;
 }
 
 void teardown()
@@ -71,41 +73,43 @@ void compare_test(priority_factors_response_msg_t *unpack_resp,
 		  uint16_t protocol_version)
 {
 	priority_factors_object_t *unpack_req;
+	priority_factors_t *prio_factors;
 
 	ck_assert(unpack_resp->priority_factors_list);
 
 	unpack_req = list_peek(unpack_resp->priority_factors_list);
 	ck_assert(unpack_req);
+	prio_factors = unpack_req->prio_factors;
 	ck_assert(!unpack_req->cluster_name);
 	ck_assert_uint_eq(unpack_req->job_id, pack_req.job_id);
 
 	ck_assert(!xstrcmp(pack_req.partition, unpack_req->partition));
 	ck_assert(pack_req.user_id == unpack_req->user_id);
 
-	ck_assert(pack_req.priority_age == unpack_req->priority_age);
+	ck_assert(pack_f_req.priority_age == prio_factors->priority_age);
 
-	ck_assert(pack_req.priority_fs == unpack_req->priority_fs);
-	ck_assert(pack_req.priority_js == unpack_req->priority_js);
-	ck_assert(pack_req.priority_part == unpack_req->priority_part);
-	ck_assert(pack_req.priority_qos == unpack_req->priority_qos);
+	ck_assert(pack_f_req.priority_fs == prio_factors->priority_fs);
+	ck_assert(pack_f_req.priority_js == prio_factors->priority_js);
+	ck_assert(pack_f_req.priority_part == prio_factors->priority_part);
+	ck_assert(pack_f_req.priority_qos == prio_factors->priority_qos);
 
-	ck_assert(pack_req.tres_cnt == unpack_req->tres_cnt);
-	for (int i = 0; i < pack_req.tres_cnt; i++)
-		ck_assert(pack_req.tres_weights[i] == unpack_req->tres_weights[i]);
+	ck_assert(pack_f_req.tres_cnt == prio_factors->tres_cnt);
+	for (int i = 0; i < pack_f_req.tres_cnt; i++)
+		ck_assert(pack_f_req.tres_weights[i] == prio_factors->tres_weights[i]);
 
-	for (int i = 0; i < pack_req.tres_cnt; i++)
-		ck_assert(pack_req.priority_tres[i] == unpack_req->priority_tres[i]);
+	for (int i = 0; i < pack_f_req.tres_cnt; i++)
+		ck_assert(pack_f_req.priority_tres[i] == prio_factors->priority_tres[i]);
 
-	for (int i = 0; i < pack_req.tres_cnt; i++)
-		ck_assert(!xstrcmp(assoc_mgr_tres_name_array[i], unpack_req->tres_names[i]));
+	for (int i = 0; i < pack_f_req.tres_cnt; i++)
+		ck_assert(!xstrcmp(assoc_mgr_tres_name_array[i], prio_factors->tres_names[i]));
 
-	ck_assert(pack_req.nice == unpack_req->nice);
+	ck_assert(pack_f_req.nice == prio_factors->nice);
 
 	if (protocol_version >= SLURM_PROTOCOL_VERSION)
 		ck_assert(pack_req.direct_prio == unpack_req->direct_prio);
 	if (protocol_version >= SLURM_ONE_BACK_PROTOCOL_VERSION) {
-		ck_assert(pack_req.priority_assoc == unpack_req->priority_assoc);
-		ck_assert(pack_req.priority_site == unpack_req->priority_site);
+		ck_assert(pack_f_req.priority_assoc == prio_factors->priority_assoc);
+		ck_assert(pack_f_req.priority_site == prio_factors->priority_site);
 	}
 }
 
