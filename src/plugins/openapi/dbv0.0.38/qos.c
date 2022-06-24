4b7ead27ba3890f695c34f9af5e7213be4376245
Nathan Rini <nate@schedmd.com>
2022-06-23 19:50:23 -0600

    openapi/dbv0.0.38 - default preempt_mode to off
    slurmdb_init_qos_rec() will set preempt_mode to NO_VAL which results in
    no change in modify request. This default setting causes issues as the
    flags attempt to get ORed into it pointlessly. With slurmrestd, it is
    expected that the full definition will be provided for the QOS object
    and not just requested changes.
    
    Bug 14388

--- src/plugins/openapi/dbv0.0.38/qos.c
+++ src/plugins/openapi/dbv0.0.38/qos.c
@@ -203,6 +203,9 @@ static data_for_each_cmd_t _foreach_update_qos(data_t *data, void *arg)
 	qos = xmalloc(sizeof(slurmdb_qos_rec_t));
 	slurmdb_init_qos_rec(qos, false, NO_VAL);
 
+	/* force to off instead of NO_VAL */
+	qos->preempt_mode = PREEMPT_MODE_OFF;
+
 	if (parse(PARSE_QOS, qos, data, args->errors, &penv)) {
 		slurmdb_destroy_qos_rec(qos);
 		return DATA_FOR_EACH_FAIL;
