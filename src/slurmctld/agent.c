SchedMD/slurm

commit 98e90af3d81e97da311fc1ad161bba61d75677a4

Pass reboot features to RebootProgram if run from ctld
Bug 12366

src/slurmctld/agent.c
@@ -2281,9 +2281,10 @@ extern int retry_list_size(void)
 
 static void _reboot_from_ctld(agent_arg_t *agent_arg_ptr)
 {
-	char *argv[3], *pname;
+	char *argv[4], *pname;
 	pid_t child;
 	int rc, status = 0;
+	reboot_msg_t *reboot_msg = agent_arg_ptr->msg_args;
 
 	if (!agent_arg_ptr->hostlist) {
 		error("%s: hostlist is NULL", __func__);
@@ -2300,7 +2301,11 @@ static void _reboot_from_ctld(agent_arg_t *agent_arg_ptr)
 	else
 		argv[0] = slurm_conf.reboot_program;
 	argv[1] = hostlist_deranged_string_xmalloc(agent_arg_ptr->hostlist);
-	argv[2] = NULL;
+	if (reboot_msg->features) {
+		argv[2] = reboot_msg->features;
+		argv[3] = NULL;
+	} else
+		argv[2] = NULL;
 
 	child = fork();
 	if (child == 0) {

RELEASE_NOTES
@@ -30,6 +30,7 @@ HIGHLIGHTS
  -- Allow jobs to pack onto nodes already rebooting with the desired features.
  -- Reset job start time after nodes are rebooted, previously only done for
     cloud/power save boots.
+ -- Node features (if any) are passed to RebootProgram if run from slurmctld.
 
 CONFIGURATION FILE CHANGES (see man appropriate man page for details)
 =====================================================================

doc/man/man5/slurm.conf.5
@@ -3715,7 +3715,9 @@ otherwise a SIGTERM will be sent to the tasks.
 .TP
 \fBreboot_from_controller\fR
 Run the \fBRebootProgram\fR from the controller instead of on the slurmds. The
-RebootProgram will be passed a comma\-separated list of nodes to reboot.
+RebootProgram will be passed a comma\-separated list of nodes to reboot as the
+first argument and if applicable the required features needed for reboot as the
+second argument.
 .TP
 \fBuser_resv_delete\fR
 Allow any user able to run in a reservation to delete it.

commit f9910e8159722ff4c549a225ca063166d3030d97

Fix segfault with reboot_from_controller and scontrol reboot
With SlurmctldParameters=reboot_from_controller configured,
scontrol reboot causes the slurmctld to segfault.

Caused by commit 98e90af3d81.

Bug 14481

src/slurmctld/agent.c
@@ -2362,7 +2362,7 @@ static void _reboot_from_ctld(agent_arg_t *agent_arg_ptr)
 	else
 		argv[0] = slurm_conf.reboot_program;
 	argv[1] = hostlist_deranged_string_xmalloc(agent_arg_ptr->hostlist);
-	if (reboot_msg->features) {
+	if (reboot_msg && reboot_msg->features) {
 		argv[2] = reboot_msg->features;
 		argv[3] = NULL;
 	} else