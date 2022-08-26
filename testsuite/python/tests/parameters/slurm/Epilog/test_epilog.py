4c48d3dd36ce8d6f5af63d7da8152a9b2414b295
Scott Jackson <scottmo@schedmd.com>
2022-08-25 18:24:01 -0600

    Testsuite - Adapt test_epilog to new behavior
    Some commits changed behavior to:
    1) No longer kill child processes of the epilog when it exits
       This happened with 0acdcc87073c, and was concluded to be desirable.
    2) Wait for the epilog and all children to complete before shutting
       down the slurmd. This was 8f35e107a6a9.
    
    Due to these changes, this test will now verify that the child sleep
    continues after the job ends (and epilog completes) and the job
    remains in the completing state until the PrologEpilogTimeout, at
    which point the job should complete and the child sleep is killed.
    
    Bug 14704

--- testsuite/python/tests/parameters/slurm/Epilog/test_epilog.py
+++ testsuite/python/tests/parameters/slurm/Epilog/test_epilog.py
@@ -5,11 +5,15 @@ import atf
 import os
 import pytest
 import shutil
+import time
+
+epilog_timeout = 10
 
 # Setup
 @pytest.fixture(scope="module", autouse=True)
 def setup():
     atf.require_auto_config("wants to set the Epilog")
+    atf.require_config_parameter('PrologEpilogTimeout', epilog_timeout)
     atf.require_slurm_running()
 
 def test_epilog(tmp_path):
@@ -25,9 +29,23 @@ exit 0
 """)
     atf.set_config_parameter('Epilog', epilog)
 
-    # Verify epilog ran by checking for the file creation
-    atf.run_job(f"-t1 true", fatal=True)
+    # Verify that the epilog ran by checking for the file creation
+    job_id = atf.run_job_id(f"-t1 true", fatal=True)
     assert atf.wait_for_file(touched_file), f"File ({touched_file}) was not created"
 
-    # Verify that the child processes of the epilog have been killed off
-    assert atf.run_command_exit(f"pgrep -f {sleep_symlink}", xfail=True) != 0, "Process ({sleep_symlink}) should have been killed"
+    # The child (sleep) should continue running after the epilog exits until the epilog timeout
+    time.sleep(1)
+
+    # Verify that the job enters the completing state
+    assert atf.wait_for_job_state(job_id, "COMPLETING", fatal=True)
+
+    # Verify that the child processes of the epilog is still running
+    assert atf.run_command_exit(f"pgrep -f {sleep_symlink}") == 0, "The epilog child process should continue running after the epilog completes (until the the epilog timeout)"
+
+    # After the epilog timeout, the job should complete and the process should be killed
+
+    # Verify that the job enters the completing state
+    assert atf.wait_for_job_state(job_id, "DONE", fatal=True, timeout=epilog_timeout+5)
+
+    # Verify that the child processes of the epilog has been killed
+    assert atf.run_command_exit(f"pgrep -f {sleep_symlink}", xfail=True) != 0, "The epilog child process should have been killed after the epilog timeout"
