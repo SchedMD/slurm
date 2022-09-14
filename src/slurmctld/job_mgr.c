69406ed1ded4760422c878f806428c78616fd3be
Scott Hilton <scott@schedmd.com>
2022-09-14 16:12:25 -0600

    Fix the number of allocated cpus for an auto-adjustment case
    Job requests --ntasks-per-node and --mem (per-node) but the limit is
    MaxMemPerCPU.
    
    Bug 14395

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -8930,6 +8930,11 @@ static bool _valid_pn_min_mem(job_desc_msg_t *job_desc_msg,
 			      "limit", min_cpus);
 			job_desc_msg->pn_min_cpus = min_cpus;
 			cpus_per_node = MAX(cpus_per_node, min_cpus);
+			if (job_desc_msg->ntasks_per_node)
+				job_desc_msg->cpus_per_task =
+					(job_desc_msg->pn_min_cpus +
+					 job_desc_msg->ntasks_per_node - 1) /
+					job_desc_msg->ntasks_per_node;
 		}
 		sys_mem_limit *= cpus_per_node;
 	}
