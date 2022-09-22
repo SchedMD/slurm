e6f4978367728d88442e437a2a9bf8d5a8a92f0b
Scott Jackson <scottmo@schedmd.com>
2022-09-22 15:30:09 -0600

    Testsuite - Rewrite test_prolog avoiding child processes
    The behavior of the prolog changed in b9f7cfc0ce96 such that the
    job will not start until its child processes have completed.
    Although it was decided that this is the desirable behavior, it
    remains undocumented.
    
    Bug 14923

--- testsuite/python/tests/parameters/slurm/Prolog/test_prolog.py
+++ testsuite/python/tests/parameters/slurm/Prolog/test_prolog.py
@@ -2,9 +2,8 @@
 # Copyright (C) SchedMD LLC.
 ############################################################################
 import atf
-import os
 import pytest
-import shutil
+import re
 
 # Setup
 @pytest.fixture(scope="module", autouse=True)
@@ -12,23 +11,46 @@ def setup():
     atf.require_auto_config("wants to set the Prolog")
     atf.require_slurm_running()
 
-def test_prolog(tmp_path):
-    """Test Prolog"""
+def test_prolog_success(tmp_path):
+    """Test successful exit"""
 
-    sleep_symlink = str(tmp_path / f"sleep_symlink_{os.getpid()}")
-    os.symlink(shutil.which('sleep'), sleep_symlink)
-    prolog = str(tmp_path / 'prolog.sh')
-    touched_file = str(tmp_path / 'touched_file')
-    atf.make_bash_script(prolog, f"""touch {touched_file}
-{sleep_symlink} 60 &
-exit 0
+    prolog_script = str(tmp_path / 'prolog.sh')
+    prolog_touched_file = str(tmp_path / 'prolog_touched_file')
+    atf.make_bash_script(prolog_script, f"touch {prolog_touched_file}")
+    atf.set_config_parameter('Prolog', prolog_script)
+
+    job_id = atf.submit_job(fatal=True)
+
+    # Verify that the prolog ran by checking for the file creation
+    assert atf.wait_for_file(prolog_touched_file), f"File ({prolog_touched_file}) was not created"
+
+    # Verify that the job runs
+    assert atf.wait_for_job_state(job_id, "RUNNING")
+
+    atf.cancel_jobs([job_id])
+
+
+def test_prolog_failure(tmp_path):
+    """Test failed exit"""
+
+    prolog_script = str(tmp_path / 'prolog.sh')
+    prolog_output_file = str(tmp_path / 'prolog_output_file')
+    atf.make_bash_script(prolog_script, f"""printenv SLURMD_NODENAME > {prolog_output_file}
+exit 1
 """)
-    atf.set_config_parameter('Prolog', prolog)
-    atf.run_command("scontrol reconfigure", user=atf.properties['slurm-user'], fatal=True)
+    atf.set_config_parameter('Prolog', prolog_script)
 
-    # Verify prolog ran by checking for the file creation
-    atf.run_job(f"-t1 true", fatal=True)
-    assert atf.wait_for_file(touched_file), f"File ({touched_file}) was not created"
+    job_id = atf.submit_job(fatal=True)
 
-    # Verify that the child processes of the prolog have been killed off
-    assert atf.run_command_exit(f"pgrep {sleep_symlink}", xfail=True) != 0, "Process ({sleep_symlink}) should have been killed"
+    # Verify that the prolog ran by checking for the file creation
+    assert atf.wait_for_file(prolog_output_file), f"File ({prolog_output_file}) was not created"
+
+    # The job should be requeued in a held state
+    assert atf.wait_for_job_state(job_id, "PENDING")
+    assert atf.repeat_command_until(f"scontrol show job {job_id}", lambda results: re.search(r"Reason=launch_failed_requeued_held", results['stdout']))
+
+    # The node should be set to a DRAIN state
+    node_name = atf.run_command_output(f"cat {prolog_output_file}", fatal=True).strip()
+    assert atf.repeat_until(lambda : atf.get_node_parameter(node_name, 'State'), lambda state: re.search(r'DRAIN', state) != None, fatal=True), "The node is not in the drain state"
+
+    atf.cancel_jobs([job_id])
