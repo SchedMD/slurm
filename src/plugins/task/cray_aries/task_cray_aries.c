3fe7145ade9f4e0312a84cd6793b676df4728078
Felip Moll <felip.moll@schedmd.com>
2022-10-11 18:55:22 +0200

    Fix task/cray_aries interactive step termination
    It tried to read the cpuset for step_4294967290 instead of
    step_interactive.
    
    Bug 14870

--- src/plugins/task/cray_aries/task_cray_aries.c
+++ src/plugins/task/cray_aries/task_cray_aries.c
@@ -518,6 +518,15 @@ extern int task_p_post_step (stepd_step_rec_t *job)
 			CRAY_ERR("snprintf failed. Return code: %d", rc);
 			return SLURM_ERROR;
 		}
+	} else if (job->step_id.step_id == SLURM_INTERACTIVE_STEP) {
+		/* Interactive Job Step */
+		rc = snprintf(path, sizeof(path),
+			      "/dev/cpuset/slurm/uid_%u/job_%u/step_interactive",
+			      job->uid, jobid);
+		if (rc < 0) {
+			CRAY_ERR("snprintf failed. Return code: %d", rc);
+			return SLURM_ERROR;
+		}
 	} else {
 		// Normal Job Step
 
