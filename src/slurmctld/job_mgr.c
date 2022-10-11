SchedMD/slurm

commit 9b2c3d68cb3cbcf67f650dcf4de144525891a1d8

Fix pointers in job_array_split
Bug 14997

Signed-off-by: Dominik Bartkiewicz <bart@schedmd.com>

src/slurmctld/job_mgr.c
@@ -4915,6 +4915,12 @@ extern job_record_t *job_array_split(job_record_t *job_ptr)
 	details_new->prefer = xstrdup(job_details->prefer);
 	details_new->prefer_list =
 		feature_list_copy(job_details->prefer_list);
+	/*
+	 * features_use and feature_list_use are set in the schedulers before
+	 * attempting to schedule the job, so just set them to NULL here.
+	 */
+	details_new->features_use = NULL;
+	details_new->feature_list_use = NULL;
 	if (job_details->mc_ptr) {
 		i = sizeof(multi_core_data_t);
 		details_new->mc_ptr = xmalloc(i);

commit ff58271487c4a42e9e4eca59d1637a6e36972f2c

Show correct features in user command after job_array_split
Regression from commit 9b2c3d68cb. features_use is used in
packing/unpacking the job. Since it was set to NULL for the new job
record NULL after the job array was split, the user command displayed
NULL for the features until the next time the job was evaluated by the
scheduler.

Bug 15092

src/slurmctld/job_mgr.c
@@ -4915,12 +4915,7 @@ extern job_record_t *job_array_split(job_record_t *job_ptr)
 	details_new->prefer = xstrdup(job_details->prefer);
 	details_new->prefer_list =
 		feature_list_copy(job_details->prefer_list);
-	/*
-	 * features_use and feature_list_use are set in the schedulers before
-	 * attempting to schedule the job, so just set them to NULL here.
-	 */
-	details_new->features_use = NULL;
-	details_new->feature_list_use = NULL;
+	set_job_features_use(details_new);
 	if (job_details->mc_ptr) {
 		i = sizeof(multi_core_data_t);
 		details_new->mc_ptr = xmalloc(i);