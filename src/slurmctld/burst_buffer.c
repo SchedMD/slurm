SchedMD/slurm

commit d33a09924b03054d5310b55ea18ca08faea00e0e

Move datawarp logic to build het job script into the plugin
Bug 15108

src/slurmctld/burst_buffer.c
@@ -423,18 +423,6 @@ extern int bb_g_job_validate2(job_record_t *job_ptr, char **err_msg)
 }
 
 
-/* Return true if hetjob separator in the script */
-static bool _hetjob_check(char *tok)
-{
-	if (strncmp(tok + 1, "SLURM",  5) &&
-	    strncmp(tok + 1, "SBATCH", 6))
-		return false;
-	if (!strstr(tok+6, "packjob") &&
-	    !strstr(tok+6, "hetjob"))
-		return false;
-	return true;
-}
-
 /*
  * Convert a hetjob batch script into a script containing only the portions
  * relevant to a specific hetjob component.
@@ -445,10 +433,7 @@ static bool _hetjob_check(char *tok)
  */
 extern char *bb_g_build_het_job_script(char *script, uint32_t het_job_offset)
 {
-	char *result = NULL, *tmp = NULL;
-	char *tok, *save_ptr = NULL;
-	bool fini = false;
-	int cur_offset = 0;
+	char *result;
 	DEF_TIMERS;
 
 	if (!script) {
@@ -457,41 +442,16 @@ extern char *bb_g_build_het_job_script(char *script, uint32_t het_job_offset)
 	}
 
 	START_TIMER;
-	tmp = xstrdup(script);
-	tok = strtok_r(tmp, "\n", &save_ptr);
-	while (tok) {
-		if (!result) {
-			xstrfmtcat(result, "%s\n", tok);
-		} else if (tok[0] != '#') {
-			fini = true;
-		} else if (_hetjob_check(tok)) {
-			cur_offset++;
-			if (cur_offset > het_job_offset)
-				fini = true;
-		} else if (cur_offset == het_job_offset) {
-			xstrfmtcat(result, "%s\n", tok);
-		}
-		if (fini)
-			break;
-		tok = strtok_r(NULL, "\n", &save_ptr);
+	if (bb_g_init() != SLURM_SUCCESS) {
+		END_TIMER2(__func__);
+		return NULL;
 	}
 
-	if (het_job_offset == 0) {
-		while (tok) {
-			char *sep = "";
-			if ((tok[0] == '#') &&
-			    (((tok[1] == 'B') && (tok[2] == 'B')) ||
-			     ((tok[1] == 'D') && (tok[2] == 'W')))) {
-				sep = "#EXCLUDED ";
-				tok++;
-			}
-			xstrfmtcat(result, "%s%s\n", sep, tok);
-			tok = strtok_r(NULL, "\n", &save_ptr);
-		}
-	} else if (result) {
-		xstrcat(result, "exit 0\n");
-	}
-	xfree(tmp);
+	slurm_mutex_lock(&g_context_lock);
+	/* This currently only supports a single burst buffer plugin */
+	result = (*(ops[0].build_het_job_script))(script, het_job_offset);
+	slurm_mutex_unlock(&g_context_lock);
+
 	END_TIMER2(__func__);
 
 	return result;

src/plugins/burst_buffer/common/burst_buffer_common.c
@@ -765,6 +765,18 @@ static void _pack_alloc(struct bb_alloc *bb_alloc, buf_t *buffer,
 	}
 }
 
+/* Return true if hetjob separator in the script */
+static bool _hetjob_check(char *tok)
+{
+	if (strncmp(tok + 1, "SLURM",  5) &&
+	    strncmp(tok + 1, "SBATCH", 6))
+		return false;
+	if (!strstr(tok + 6, "packjob") &&
+	    !strstr(tok + 6, "hetjob"))
+		return false;
+	return true;
+}
+
 /* Pack individual burst buffer records into a buffer */
 extern int bb_pack_bufs(uid_t uid, bb_state_t *state_ptr, buf_t *buffer,
 			uint16_t protocol_version)
@@ -1179,6 +1191,52 @@ extern int bb_build_bb_script(job_record_t *job_ptr, char *script_file)
 	xfree(out_buf);
 
 	return rc;
+}
+
+extern char *bb_common_build_het_job_script(char *script,
+					    uint32_t het_job_offset,
+					    bool (*is_directive) (char *tok))
+{
+	char *result = NULL, *tmp = NULL;
+	char *tok, *save_ptr = NULL;
+	bool fini = false;
+	int cur_offset = 0;
+
+	tmp = xstrdup(script);
+	tok = strtok_r(tmp, "\n", &save_ptr);
+	while (tok) {
+		if (!result) {
+			xstrfmtcat(result, "%s\n", tok);
+		} else if (tok[0] != '#') {
+			fini = true;
+		} else if (_hetjob_check(tok)) {
+			cur_offset++;
+			if (cur_offset > het_job_offset)
+				fini = true;
+		} else if (cur_offset == het_job_offset) {
+			xstrfmtcat(result, "%s\n", tok);
+		}
+		if (fini)
+			break;
+		tok = strtok_r(NULL, "\n", &save_ptr);
+	}
+
+	if (het_job_offset == 0) {
+		while (tok) {
+			char *sep = "";
+			if (is_directive(tok)) {
+				sep = "#EXCLUDED ";
+				tok++;
+			}
+			xstrfmtcat(result, "%s%s\n", sep, tok);
+			tok = strtok_r(NULL, "\n", &save_ptr);
+		}
+	} else if (result) {
+		xstrcat(result, "exit 0\n");
+	}
+	xfree(tmp);
+
+	return result;
 }
 
 /* Free memory associated with allocated bb record, caller is responsible for

src/plugins/burst_buffer/common/burst_buffer_common.h
@@ -253,6 +253,16 @@ extern bb_alloc_t *bb_alloc_name_rec(bb_state_t *state_ptr, char *name,
  */
 extern int bb_build_bb_script(job_record_t *job_ptr, char *script_file);
 
+/*
+ * Create job script based on het job offsets
+ *
+ * Offset 0 - prepend burst buffer directives w/#EXCLUDED for offsets > 0
+ * Offset > 0 - remove all directives that are not current component
+ */
+extern char *bb_common_build_het_job_script(char *script,
+					    uint32_t het_job_offset,
+					    bool (*is_directive) (char *tok));
+
 /* Clear all cached burst buffer records, freeing all memory. */
 extern void bb_clear_cache(bb_state_t *state_ptr);
 

src/plugins/burst_buffer/datawarp/burst_buffer_datawarp.c
@@ -2739,9 +2739,20 @@ static void _purge_vestigial_bufs(void)
 	}
 }
 
+static bool _is_directive(char *tok)
+{
+	if ((tok[0] == '#') &&
+	    (((tok[1] == 'B') && (tok[2] == 'B')) ||
+	     ((tok[1] == 'D') && (tok[2] == 'W')))) {
+		return true;
+	}
+	return false;
+}
+
 extern char *bb_p_build_het_job_script(char *script, uint32_t het_job_offset)
 {
-	return NULL;
+	return bb_common_build_het_job_script(script, het_job_offset,
+					      _is_directive);
 }
 
 /*

commit 24f5f21c2b1ab82902d4e911c6b83346e410e032

Fix slurmctld segfault when submitting a het job
This only happens without a burst buffer plugin configured in slurm.conf.

Regression from commit d33a09924b.

Bug 15624, 15108

src/slurmctld/burst_buffer.c
@@ -441,6 +441,9 @@ extern char *bb_g_build_het_job_script(char *script, uint32_t het_job_offset)
 		return NULL;
 	}
 
+	if (!g_context_cnt)
+		return xstrdup(script);
+
 	START_TIMER;
 	if (bb_g_init() != SLURM_SUCCESS) {
 		END_TIMER2(__func__);