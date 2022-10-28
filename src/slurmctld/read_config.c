ed52636cbae8e2c1c2d82dbe17f398d6acb7c7ea
Tim McMullan <mcmullan@schedmd.com>
2022-10-28 09:05:08 -0400

    fatal() when job_submit plugin init fails on reconfig.
    Bug 15234

--- src/slurmctld/read_config.c
+++ src/slurmctld/read_config.c
@@ -1851,8 +1851,8 @@ int read_slurm_conf(int recover, bool reconfig)
 	_update_preempt(old_preempt_mode);
 
 	/* Update plugin parameters as possible */
-	rc = job_submit_g_reconfig();
-	error_code = MAX(error_code, rc);	/* not fatal */
+	if (reconfig && (job_submit_g_reconfig() != SLURM_SUCCESS))
+		fatal("Failed to reconfigure job_submit plugin");
 	rc = prep_g_reconfig();
 	error_code = MAX(error_code, rc);	/* not fatal */
 	rc = switch_g_reconfig();
