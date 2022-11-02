SchedMD/slurm

commit 82e826106715dfd67e5cc55ebea11492e7d76ac5

Force sbatch/srun/salloc error when ntasks-per-core > threads-per-core
Bug 13487

RELEASE_NOTES
@@ -82,6 +82,7 @@ COMMAND CHANGES (see man pages for details)
  -- srun --ntasks-per-core now applies to job and step allocations. Now, use of
     --ntasks-per-core=1 implies --cpu-bind=cores and --ntasks-per-core>1 implies
     --cpu-bind=threads.
+ -- salloc/sbatch/srun - Check and abort if ntasks-per-core > threads-per-core.
 
 API CHANGES
 ===========

src/salloc/opt.c
@@ -663,6 +663,12 @@ static bool _opt_verify(void)
 	    (getenv("SLURM_NTASKS_PER_CORE") == NULL)) {
 		setenvf(NULL, "SLURM_NTASKS_PER_CORE", "%d",
 			opt.ntasks_per_core);
+		if ((opt.threads_per_core !=  NO_VAL) &&
+		    (opt.threads_per_core < opt.ntasks_per_core)) {
+			error("--ntasks-per-core (%d) can not be bigger than --threads-per-core (%d)",
+			opt.ntasks_per_core, opt.threads_per_core);
+			verified = false;
+		}
 	}
 
 	if ((opt.ntasks_per_gpu != NO_VAL) &&

src/sbatch/opt.c
@@ -920,8 +920,15 @@ static bool _opt_verify(void)
 	if (opt.ntasks_set && (opt.ntasks > 0))
 		het_job_env.ntasks = opt.ntasks;
 
-	if (opt.ntasks_per_core != NO_VAL)
+	if (opt.ntasks_per_core != NO_VAL) {
 		het_job_env.ntasks_per_core = opt.ntasks_per_core;
+		if ((opt.threads_per_core != NO_VAL) &&
+		    (opt.threads_per_core < opt.ntasks_per_core)) {
+			error("--ntasks-per-core (%d) can not be bigger than --threads-per-core (%d)",
+			opt.ntasks_per_core, opt.threads_per_core);
+			verified = false;
+		}
+	}
 
 	if (opt.ntasks_per_tres != NO_VAL)
 		het_job_env.ntasks_per_tres = opt.ntasks_per_tres;

src/srun/libsrun/opt.c
@@ -1182,6 +1182,12 @@ static bool _opt_verify(void)
 			else
 				sropt.cpu_bind_type |= CPU_BIND_TO_THREADS;
 		}
+		if ((opt.threads_per_core !=  NO_VAL) &&
+		    (opt.threads_per_core < opt.ntasks_per_core)) {
+			error("--ntasks-per-core (%d) can not be bigger than --threads-per-core (%d)",
+			opt.ntasks_per_core, opt.threads_per_core);
+			verified = false;
+		}
 	}
 	if (opt.ntasks_per_socket > 0) {
 		/* if cpu_bind_type doesn't already have a auto pref,

commit 63dfe9c0115e1bafd31e6a4723c9f1514a139134

Testsuite - Adapt test1.118 to a new behavior
Test1.118 had to be adapted to avoid an error which is now emitted
by sbatch/srun/salloc when ntasks-per-core > threads-per-core. This
error was introduced by commit 82e8261067 (for bug 13487).

Bug 15267

testsuite/expect/test1.118
@@ -27,11 +27,13 @@ source ./globals
 
 set job_id 0
 
+
 proc cleanup {} {
 	global job_id
 
 	cancel_job $job_id
 }
+
 
 proc check_error {client option prog error} {
 	global sbatch salloc srun test_name number job_id
@@ -95,11 +97,21 @@ mutual_error_tests
 set env(SLURM_HINT) "nomultithread"
 mutual_error_tests
 
+# Options are given a higher precedence than the environment. Options that conflict
+# with the environment cause the conflicting environment variables to be overridden.
+# Thus, when --ntasks-per-core or --threads-per-core options are specified
+# in conjunction with SLURM_HINT, since these conflict, the SLURM_HINT is ignored.
+
+# We will use 2 threads per core and tasks per core if available in order to
+# demonstrate that the nomultithread hint is being ignored.
+set node_name [get_nodes_by_request "--ntasks-per-core=2 -t1"]
+set threads_per_core [expr [get_node_param $node_name "ThreadsPerCore"] > 1 ? 2 : 1]
+
 set client "$sbatch"
 set prog "--wrap=hostname"
-set options [list "--threads-per-core=2 --ntasks-per-core=3"]
+set options [list "--threads-per-core=$threads_per_core --ntasks-per-core=$threads_per_core"]
 foreach option $options {
-	set cmd "$client -v -H -J$test_name -t1 $option $prog"
+	set cmd "$client -w $node_name -v -H -J$test_name -t1 $option $prog"
 
 	set job_id 0
 	set matches 0
@@ -124,8 +136,8 @@ foreach option $options {
 
 	set threads_req [get_job_param $job_id "ReqB:S:C:T"]
 	set tasks_req [get_job_param $job_id "NtasksPerN:B:S:C"]
-	set threads_match "0:0:*:2"
-	set tasks_match "0:0:*:3"
+	set threads_match "0:0:*:$threads_per_core"
+	set tasks_match "0:0:*:$threads_per_core"
 	subtest {$threads_match eq $threads_req} "ReqB:S:C:T should be $threads_match" "$threads_req != $threads_match"
 	subtest {$tasks_match eq $tasks_req} "NtasksPerN:B:S:C should be $tasks_match" "$tasks_req != $tasks_match"
 	cancel_job $job_id