SchedMD/slurm

commit b0ec01496b5a563dab061b7cf76da1861a534d07

srun - track and use user/group from allocation response msg

src/srun/srun.c
@@ -192,6 +192,17 @@ int srun(int ac, char **av)
 	} else
 		create_srun_job((void **) &job, &got_alloc, 0, 1);
 
+	/*
+	 * Detect is process is in non-matching user namespace or UIDs
+	 * with controller are mismatching.
+	 */
+	if (job->uid != getuid())
+		debug3("%s: %ps UID %u and srun process UID %u mismatch",
+		       __func__, &job->step_id, job->uid, getuid());
+	if (job->gid != getgid())
+		debug3("%s: %ps GID %u and srun process GID %u mismatch",
+		       __func__, &job->step_id, job->gid, getgid());
+
 	_setup_job_env(job, srun_job_list, got_alloc);
 
 	/*
@@ -657,8 +668,10 @@ static void _setup_one_job_env(slurm_opt_t *opt_local, srun_job_t *job,
 	env->account = job->account;
 	env->qos = job->qos;
 	env->resv_name = job->resv_name;
-	env->uid = getuid();
-	env->user_name = uid_to_string(env->uid);
+	env->uid = job->uid;
+	env->user_name = xstrdup(job->user_name);
+	env->gid = job->gid;
+	env->group_name = xstrdup(job->group_name);
 
 	if (srun_opt->pty && (set_winsize(job) < 0)) {
 		error("Not using a pseudo-terminal, disregarding --pty option");

src/srun/libsrun/srun_job.c
@@ -101,6 +101,10 @@ typedef struct allocation_info {
 	char                   *partition;
 	dynamic_plugin_data_t  *select_jobinfo;
 	slurm_step_id_t         step_id;
+	uid_t uid; /* resolved user id of job */
+	char *user_name; /* resolved user name of job */
+	gid_t gid; /* resolved group id of job */
+	char *group_name; /* resolved group name of job */
 } allocation_info_t;
 
 typedef struct het_job_resp_struct {
@@ -505,6 +509,11 @@ extern srun_job_t *job_create_allocation(
 	i->ntasks_per_socket = resp->ntasks_per_socket;
 
 	i->select_jobinfo = select_g_select_jobinfo_copy(resp->select_jobinfo);
+
+	i->uid = resp->uid;
+	i->user_name = xstrdup(resp->user_name);
+	i->gid = resp->gid;
+	i->group_name = xstrdup(resp->group_name);
 
 	job = _job_create_structure(i, opt_local);
 	if (job) {
@@ -1715,6 +1724,11 @@ static srun_job_t *_job_create_structure(allocation_info_t *ainfo,
 	job->rc       = -1;
 
 	job_update_io_fnames(job, opt_local);
+
+	job->uid = ainfo->uid;
+	job->user_name = xstrdup(ainfo->user_name);
+	job->gid = ainfo->gid;
+	job->group_name = xstrdup(ainfo->group_name);
 
 	return (job);
 }

src/srun/libsrun/srun_job.h
@@ -128,6 +128,10 @@ typedef struct srun_job {
 	char *account;    /* account of this job */
 	char *qos;        /* job's qos */
 	char *resv_name;  /* reservation the job is using */
+	uid_t uid; /* resolved user id of job */
+	char *user_name; /* resolved user name of job */
+	gid_t gid; /* resolved group id of job */
+	char *group_name; /* resolved group name of job */
 } srun_job_t;
 
 void    update_job_state(srun_job_t *job, srun_job_state_t newstate);

commit f706ffcd97fe8cbcf676267b360038a6d0506169

srun - prevent segfault on NULL job pointer
Regression from b0ec01496b.

Bug 15359

src/srun/srun.c
@@ -196,10 +196,10 @@ int srun(int ac, char **av)
 	 * Detect is process is in non-matching user namespace or UIDs
 	 * with controller are mismatching.
 	 */
-	if (job->uid != getuid())
+	if (job && (job->uid != getuid()))
 		debug3("%s: %ps UID %u and srun process UID %u mismatch",
 		       __func__, &job->step_id, job->uid, getuid());
-	if (job->gid != getgid())
+	if (job && (job->gid != getgid()))
 		debug3("%s: %ps GID %u and srun process GID %u mismatch",
 		       __func__, &job->step_id, job->gid, getgid());
 