fa0e6e1472530182f626bff176b3b541ba5d6208
Danny Auble <da@schedmd.com>
2022-11-16 14:43:19 -0700

    Fix regression in e62bf0fe7a
    That commit erroneously took out SlurmctldParameters from being printed.
    
    Bug 15422

--- src/api/config_info.c
+++ src/api/config_info.c
@@ -1596,6 +1596,11 @@ extern void *slurm_ctl_conf_2_key_pairs(slurm_conf_t *slurm_ctl_conf_ptr)
 	key_pair->value = xstrdup(tmp_str);
 	list_append(ret_list, key_pair);
 
+	key_pair = xmalloc(sizeof(config_key_pair_t));
+	key_pair->name = xstrdup("SlurmctldParameters");
+	key_pair->value = xstrdup(slurm_ctl_conf_ptr->slurmctld_params);
+	list_append(ret_list, key_pair);
+
 	snprintf(tmp_str, sizeof(tmp_str), "%s",
 		 log_num2string(slurm_ctl_conf_ptr->slurmd_debug));
 	key_pair = xmalloc(sizeof(config_key_pair_t));
