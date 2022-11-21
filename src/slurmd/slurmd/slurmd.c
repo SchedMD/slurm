SchedMD/slurm

commit 4cca854e3103ad2d9186c64101c0572cae9736e6

slurmd - fix assert failure on initialization due to bad node name.
Move the already existing find_node_record2() call up in the code and
bail with error if no record found.

The justification for the call movement is to avoid unneeded spool dir
creation attempt as well as further operations for non-configless setups
if no record is found.

Change the used call to find_node_record() too while here so that the
function itself logs the error if no record found for us.

Bug 11621

src/slurmd/slurmd/slurmd.c
@@ -1724,6 +1724,9 @@ _slurmd_init(void)
 	 */
 	_read_config();
 
+	if (!(node_rec = find_node_record(conf->node_name)))
+		return SLURM_ERROR;
+
 	/*
 	 * slurmd -G, calling it here rather than from _process_cmdline
 	 * since it relies on gres_init and _read_config.
@@ -1753,8 +1756,7 @@ _slurmd_init(void)
 
 	fini_job_cnt = cpu_cnt = MAX(conf->conf_cpus, conf->block_map_size);
 	fini_job_id = xmalloc(sizeof(uint32_t) * fini_job_cnt);
-	node_rec = find_node_record2(conf->node_name);
-	if (node_rec && node_rec->config_ptr) {
+	if (node_rec->config_ptr) {
 		(void) gres_init_node_config(conf->node_name,
 					     node_rec->config_ptr->gres,
 					     &gres_list);

commit 97c8d6edab9b536e5cd3bc3dedd9b684cc43c1de

Fix _slurmd_init() on FrontEnd mode
Regression from 4cca854e31

Bug 15429

src/slurmd/slurmd/slurmd.c
@@ -1975,8 +1975,10 @@ _slurmd_init(void)
 		return SLURM_ERROR;
 	}
 
+#ifndef HAVE_FRONT_END
 	if (!find_node_record(conf->node_name))
 		return SLURM_ERROR;
+#endif
 
 	/*
 	 * slurmd -G, calling it here rather than from _process_cmdline