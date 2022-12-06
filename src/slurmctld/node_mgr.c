SchedMD/slurm

commit 82db921cdef5828ff1e9fa761af2f4106ee45bcd

Make master 22.05

src/slurmctld/node_mgr.c
@@ -391,7 +391,7 @@ extern int load_all_node_state ( bool state_only )
 			    SLURM_SUCCESS)
 				goto unpack_error;
 			base_state = node_state & NODE_STATE_BASE;
-		} else if (protocol_version >= SLURM_20_11_PROTOCOL_VERSION) {
+		} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 			uint32_t len;
 			safe_unpackstr_xmalloc(&comm_name, &len, buffer);
 			safe_unpackstr_xmalloc(&node_name, &len, buffer);
@@ -424,39 +424,6 @@ extern int load_all_node_state ( bool state_only )
 						   node_name,
 						   protocol_version) !=
 			    SLURM_SUCCESS)
-				goto unpack_error;
-			base_state = node_state & NODE_STATE_BASE;
-		} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
-			safe_unpackstr_xmalloc (&comm_name, &name_len, buffer);
-			safe_unpackstr_xmalloc (&node_name, &name_len, buffer);
-			safe_unpackstr_xmalloc (&node_hostname,
-							    &name_len, buffer);
-			safe_unpackstr_xmalloc (&reason,    &name_len, buffer);
-			safe_unpackstr_xmalloc (&features,  &name_len, buffer);
-			safe_unpackstr_xmalloc (&features_act,&name_len,buffer);
-			safe_unpackstr_xmalloc (&gres,      &name_len, buffer);
-			safe_unpackstr_xmalloc (&cpu_spec_list,
-							    &name_len, buffer);
-			safe_unpack32 (&next_state,  buffer);
-			safe_unpack32 (&node_state,  buffer);
-			safe_unpack32 (&cpu_bind,    buffer);
-			safe_unpack16 (&cpus,        buffer);
-			safe_unpack16 (&boards,     buffer);
-			safe_unpack16 (&sockets,     buffer);
-			safe_unpack16 (&cores,       buffer);
-			safe_unpack16 (&core_spec_cnt, buffer);
-			safe_unpack16 (&threads,     buffer);
-			safe_unpack64 (&real_memory, buffer);
-			safe_unpack32 (&tmp_disk,    buffer);
-			safe_unpack32 (&reason_uid,  buffer);
-			safe_unpack_time (&reason_time, buffer);
-			safe_unpack_time (&boot_req_time, buffer);
-			safe_unpack_time(&last_response, buffer);
-			safe_unpack16 (&obj_protocol_version, buffer);
-			safe_unpackstr_xmalloc (&mcs_label, &name_len, buffer);
-			if (gres_node_state_unpack(
-				    &gres_list, buffer, node_name,
-				    protocol_version) != SLURM_SUCCESS)
 				goto unpack_error;
 			base_state = node_state & NODE_STATE_BASE;
 		} else {
@@ -1054,75 +1021,6 @@ static void _pack_node(node_record_t *dump_node_ptr, buf_t *buffer,
 				     protocol_version);
 
 		packstr(dump_node_ptr->tres_fmt_str, buffer);
-	} else if (protocol_version >= SLURM_20_11_PROTOCOL_VERSION) {
-		packstr(dump_node_ptr->name, buffer);
-		packstr(dump_node_ptr->node_hostname, buffer);
-		packstr(dump_node_ptr->comm_name, buffer);
-		packstr(dump_node_ptr->bcast_address, buffer);
-		pack16(dump_node_ptr->port, buffer);
-		pack32(dump_node_ptr->next_state, buffer);
-		pack32(dump_node_ptr->node_state, buffer);
-		packstr(dump_node_ptr->version, buffer);
-
-		/* Only data from config_record used for scheduling */
-		pack16(dump_node_ptr->config_ptr->cpus, buffer);
-		pack16(dump_node_ptr->config_ptr->boards, buffer);
-		pack16(dump_node_ptr->config_ptr->tot_sockets, buffer);
-		pack16(dump_node_ptr->config_ptr->cores, buffer);
-		pack16(dump_node_ptr->config_ptr->threads, buffer);
-		pack64(dump_node_ptr->config_ptr->real_memory, buffer);
-		pack32(dump_node_ptr->config_ptr->tmp_disk, buffer);
-
-		packstr(dump_node_ptr->mcs_label, buffer);
-		pack32(dump_node_ptr->owner, buffer);
-		pack16(dump_node_ptr->core_spec_cnt, buffer);
-		pack32(dump_node_ptr->cpu_bind, buffer);
-		pack64(dump_node_ptr->mem_spec_limit, buffer);
-		packstr(dump_node_ptr->cpu_spec_list, buffer);
-
-		pack32(dump_node_ptr->cpu_load, buffer);
-		pack64(dump_node_ptr->free_mem, buffer);
-		pack32(dump_node_ptr->config_ptr->weight, buffer);
-		pack32(dump_node_ptr->reason_uid, buffer);
-
-		pack_time(dump_node_ptr->boot_time, buffer);
-		pack_time(dump_node_ptr->reason_time, buffer);
-		pack_time(dump_node_ptr->slurmd_start_time, buffer);
-
-		select_g_select_nodeinfo_pack(dump_node_ptr->select_nodeinfo,
-					      buffer, protocol_version);
-
-		packstr(dump_node_ptr->arch, buffer);
-		packstr(dump_node_ptr->features, buffer);
-		packstr(dump_node_ptr->features_act, buffer);
-		if (dump_node_ptr->gres)
-			packstr(dump_node_ptr->gres, buffer);
-		else
-			packstr(dump_node_ptr->config_ptr->gres, buffer);
-
-		/* Gathering GRES details is slow, so don't by default */
-		if (show_flags & SHOW_DETAIL) {
-			gres_drain =
-				gres_get_node_drain(dump_node_ptr->gres_list);
-			gres_used  =
-				gres_get_node_used(dump_node_ptr->gres_list);
-		}
-		packstr(gres_drain, buffer);
-		packstr(gres_used, buffer);
-		xfree(gres_drain);
-		xfree(gres_used);
-
-		packstr(dump_node_ptr->os, buffer);
-		packstr(dump_node_ptr->comment, buffer);
-		packstr(dump_node_ptr->reason, buffer);
-		acct_gather_energy_pack(dump_node_ptr->energy, buffer,
-					protocol_version);
-		ext_sensors_data_pack(dump_node_ptr->ext_sensors, buffer,
-				      protocol_version);
-		power_mgmt_data_pack(dump_node_ptr->power, buffer,
-				     protocol_version);
-
-		packstr(dump_node_ptr->tres_fmt_str, buffer);
 	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		packstr(dump_node_ptr->name, buffer);
 		packstr(dump_node_ptr->node_hostname, buffer);
@@ -1182,6 +1080,7 @@ static void _pack_node(node_record_t *dump_node_ptr, buf_t *buffer,
 		xfree(gres_used);
 
 		packstr(dump_node_ptr->os, buffer);
+		packstr(dump_node_ptr->comment, buffer);
 		packstr(dump_node_ptr->reason, buffer);
 		acct_gather_energy_pack(dump_node_ptr->energy, buffer,
 					protocol_version);
@@ -3862,7 +3761,7 @@ void push_reconfig_to_slurmd(char **slurmd_config_files)
 	curr_args->msg_type = REQUEST_RECONFIGURE_WITH_CONFIG;
 	curr_args->retry = 0;
 	curr_args->hostlist = hostlist_create(NULL);
-	curr_args->protocol_version = SLURM_21_08_PROTOCOL_VERSION;
+	curr_args->protocol_version = SLURM_PROTOCOL_VERSION;
 	curr_config = xmalloc(sizeof(*curr_config));
 	load_config_response_list(curr_config, slurmd_config_files);
 	curr_args->msg_args = curr_config;
@@ -3871,7 +3770,7 @@ void push_reconfig_to_slurmd(char **slurmd_config_files)
 	prev_args->msg_type = REQUEST_RECONFIGURE_WITH_CONFIG;
 	prev_args->retry = 0;
 	prev_args->hostlist = hostlist_create(NULL);
-	prev_args->protocol_version = SLURM_20_11_PROTOCOL_VERSION;
+	prev_args->protocol_version = SLURM_ONE_BACK_PROTOCOL_VERSION;
 	prev_config = xmalloc(sizeof(*prev_config));
 	load_config_response_msg(prev_config, CONFIG_REQUEST_SLURMD);
 	prev_args->msg_args = prev_config;
@@ -3880,7 +3779,7 @@ void push_reconfig_to_slurmd(char **slurmd_config_files)
 	old_args->msg_type = REQUEST_RECONFIGURE_WITH_CONFIG;
 	old_args->retry = 0;
 	old_args->hostlist = hostlist_create(NULL);
-	old_args->protocol_version = SLURM_20_02_PROTOCOL_VERSION;
+	old_args->protocol_version = SLURM_MIN_PROTOCOL_VERSION;
 	old_config = xmalloc(sizeof(*old_config));
 	load_config_response_msg(old_config, CONFIG_REQUEST_SLURMD);
 	old_args->msg_args = old_config;
@@ -3894,15 +3793,15 @@ void push_reconfig_to_slurmd(char **slurmd_config_files)
 		     IS_NODE_POWERING_DOWN(node_ptr)))
 			continue;
 
-		if (node_ptr->protocol_version >= SLURM_21_08_PROTOCOL_VERSION) {
+		if (node_ptr->protocol_version >= SLURM_PROTOCOL_VERSION) {
 			hostlist_push_host(curr_args->hostlist, node_ptr->name);
 			curr_args->node_count++;
 		} else if (node_ptr->protocol_version ==
-			   SLURM_20_11_PROTOCOL_VERSION) {
+			   SLURM_ONE_BACK_PROTOCOL_VERSION) {
 			hostlist_push_host(prev_args->hostlist, node_ptr->name);
 			prev_args->node_count++;
 		} else if (node_ptr->protocol_version ==
-			   SLURM_20_02_PROTOCOL_VERSION) {
+			   SLURM_MIN_PROTOCOL_VERSION) {
 			hostlist_push_host(old_args->hostlist, node_ptr->name);
 			old_args->node_count++;
 		}

META
@@ -5,11 +5,11 @@
 ##
   Meta:		1
   Name:		slurm
-  Major:	21
-  Minor:	08
+  Major:	22
+  Minor:	05
   Micro:	0
-  Version:	21.08.0
-  Release:	0rc2
+  Version:	22.05.0
+  Release:	0pre1
 
 ##
 #  When making a new Major/Minor version update
@@ -28,6 +28,6 @@
 #        the config.h file that may be used throughout Slurm, so don't remove
 #	 them.
 ##
-  API_CURRENT:	37
+  API_CURRENT:	38
   API_AGE:	0
   API_REVISION:	0

RELEASE_NOTES
@@ -1,4 +1,4 @@
-RELEASE NOTES FOR SLURM VERSION 21.08
+RELEASE NOTES FOR SLURM VERSION 22.05
 
 IMPORTANT NOTES:
 If using the slurmdbd (Slurm DataBase Daemon) you must update this first.
@@ -6,179 +6,26 @@ If using the slurmdbd (Slurm DataBase Daemon) you must update this first.
 NOTE: If using a backup DBD you must start the primary first to do any
 database conversion, the backup will not start until this has happened.
 
-The 21.08 slurmdbd will work with Slurm daemons of version 20.02 and above.
+The 22.05 slurmdbd will work with Slurm daemons of version 20.11 and above.
 You will not need to update all clusters at the same time, but it is very
 important to update slurmdbd first and having it running before updating
 any other clusters making use of it.
 
-Slurm can be upgraded from version 20.02 or 20.11 to version 21.08 without loss
+Slurm can be upgraded from version 20.11 or 21.08 to version 22.05 without loss
 of jobs or other state information. Upgrading directly from an earlier version
 of Slurm will result in loss of state information.
 
 If using SPANK plugins that use the Slurm APIs, they should be recompiled when
 upgrading Slurm to a new major release.
 
-NOTE: If upgrading from < 20.11.
-      In the database the AllocGres and ReqGres columns have been removed as
-      they are duplicates for those using AccountingStorageTRES.  If you use
-      these columns please make sure to backup this information since it will
-      be lost and set up AccountingStorageTRES with your GRES to be able to
-      get equivalent information out of AllocTRES and ReqTRES after upgrade.
-
 HIGHLIGHTS
 ==========
- -- Removed gres/mic plugin used to support Xeon Phi coprocessors.
- -- Add LimitFactor to the QOS. A float that is factored into an associations
-    GrpTRES limits.  For example, if the LimitFactor is 2, then an association
-    with a GrpTRES of 30 CPUs, would be allowed to allocate 60 CPUs when
-    running under this QOS.
- -- A job's next_step_id counter now resets to 0 after being requeued.
-    Previously, the step id's would continue from the job's last run.
- -- API change: Removed slurm_kill_job_msg and modified the function signature
-    for slurm_kill_job2. slurm_kill_job2 should be used instead of
-    slurm_kill_job_msg.
- -- AccountingStoreFlags=job_script allows you to store the job's batch script.
- -- AccountingStoreFlags=job_env allows you to store the job's env vars.
- -- configure: the --with option handling has been made consistent across the
-    various optional libraries. Specifying --with-foo=/path/to/foo will only
-    check that directory for the applicable library (rather than, in some cases,
-    falling back to the default directories), and will always error the build
-    if the library is not found (instead of a mix of error messages and non-
-    fatal warning messages).
- -- configure: replace --with-rmsi_dir option with proper handling for
-    --with-rsmi=dir.
- -- Removed sched/hold plugin.
- -- cli_filter/lua, jobcomp/lua, job_submit/lua now load their scripts from the
-    same directory as the slurm.conf file (and thus now will respect changes
-    to the SLURM_CONF environment variable).
- -- SPANK - call slurm_spank_init if defined without slurm_spank_slurmd_exit in
-    slurmd context.
- -- Add new 'PLANNED' state to a node to represent when the backfill scheduler
-    has it planned to be used in the future instead of showing as 'IDLE'.
-    sreport also has changed it's cluster utilization report column name from
-    'Reserved' to 'Planned' to match this nomenclature.
- -- Put node into "INVAL" state upon registering with an invalid node
-    configuration. Node must register with a valid configuration to continue.
- -- Remove SLURM_DIST_LLLP environment variable in favor of just
-    SLURM_DISTRIBUTION.
- -- Make --cpu-bind=threads default for --threads-per-core -- cli and env can
-    override.
- -- slurmd - allow multiple comma-separated controllers to be specified in
-    configless mode with --conf-server
- -- configure - add --with-systemdsystemunitdir option to automatically install
-    the Slurm unit files.
- -- Manually powering down of nodes with scontrol now ignores
-    SuspendExc<Nodes|Parts>.
- -- Distinguish queued reboot requests (REBOOT@) from issued reboots (REBOOT^).
- -- auth/jwt - add support for RS256 tokens. Also permit the username in the
-    'username' field in addition to the 'sun' (Slurm UserName) field.
- -- service files - change dependency to network-online rather than just
-    network to ensure DNS and other services are available.
- -- Add "Extra" field to node to store extra information other than a comment.
- -- Add ResumeTimeout, SuspendTimeout and SuspendTime to Partitions.
- -- The memory.force_empty parameters is no longer set by jobacct_gather/cgroup
-    when deleting the cgroup. This previously caused a significant delay (~2s)
-    when terminating a job, and is not believed to have provided any perceivable
-    benefit. However, this may lead to slightly higher reported kernel mem page
-    cache usage since the kernel cgroup memory is no longer freed immediately.
- -- TaskPluginParam=verbose is now treated as a default. Previously it would be
-    applied regardless of the job specifying a --cpu-bind.
- -- Add "node_reg_mem_percent" SlurmctldParameter to define percentage of
-    memory nodes are allowed to register with.
- -- Define and separate node power state transitions. Previously a powering
-    down node was in both states, POWERING_OFF and POWERED_OFF. These are now
-    separated.
-    e.g.
-       IDLE+POWERED_OFF (IDLE~)
-    -> IDLE+POWERING_UP (IDLE#)   - Manual power up or allocation
-    -> IDLE
-    -> IDLE+POWER_DOWN (IDLE!)    - Node waiting for power down
-    -> IDLE+POWERING_DOWN (IDLE%) - Node powering down
-    -> IDLE+POWERED_OFF (IDLE~)   - Powered off
- -- Some node state flag names have changed. These would be noticeable for
-    example if using a state flag to filter nodes with sinfo.
-    e.g.
-    POWER_UP -> POWERING_UP
-    POWER_DOWN -> POWERED_DOWN
-    POWER_DOWN now represents a node pending power down
- -- Create a new process called slurmscriptd which runs PrologSlurmctld and
-    EpilogSlurmctld. This avoids fork() calls from slurmctld, and can avoid
-    performance issues if the slurmctld has a large memory footprint.
- -- Pass JSON of job to node mappings to ResumeProgram.
- -- QOS accrue limits only apply to the job QOS, not partition QOS.
- -- Any return code from SPANK plugin or SPANK function that is not
-    SLURM_SUCCESS (zero) will be considered to be an error. Previously, only
-    negative return codes were considered an error.
- -- Add support for automatically detecting and broadcasting executable shared
-    object dependencies for sbcast and srun --bcast.
- -- All SPANK error codes now start at 3000. Where previously SPANK would give
-    a return code of 1, it will now return 3000. This change will break ABI
-    compatibility with SPANK plugins compiled against older version of Slurm.
- -- SPANK plugins are now required to match the current Slurm version, and must
-    be recompiled for each new Slurm release.
 
 CONFIGURATION FILE CHANGES (see man appropriate man page for details)
 =====================================================================
- -- Errors detected in the parser handlers due to invalid configurations are now
-    propagated and can lead to fatal (and thus exit) the calling process.
- -- Enforce a valid configuration for AccountingStorageEnforce in slurm.conf.
-    If the configuration is invalid, then an error message will be printed and
-    the command or daemon (including slurmctld) will not run.
- -- Removed AccountingStoreJobComment option.  Please update your config to use
-    AccountingStoreFlags=job_comment instead.
- -- Removed DefaultStorage{Host,Loc,Pass,Port,Type,User} options.
- -- Removed CacheGroups, CheckpointType, JobCheckpointDir, MemLimitEnforce,
-    SchedulerPort, SchedulerRootFilter options.
- -- Added Script DebugFlags for debugging slurmscriptd (the process that runs
-    slurmctld scripts such as PrologSlurmctld and EpilogSlurmctld).
- -- Rename SbcastParameters to BcastParameters.
- -- systemd service files - add new '-s' option to each daemon which will
-    change the working directory even with the -D option. (Ensures any core
-    files are placed in an accessible location, rather than /.)
- -- Added BcastParameters=send_libs and BcastExclude options.
- -- Remove the (incomplete) burst_buffer/generic plugin.
 
 COMMAND CHANGES (see man pages for details)
 ===========================================
- -- Changed the --format handling for negative field widths (left justified)
-    to apply to the column headers as well as the printed fields.
- -- Invalidate multiple partition requests when using partition based
-    associations.
- -- --cpus-per-task and --threads-per-core now imply --exact.
-    This fixes issues where steps would be allocated the wrong number of CPUs.
- -- scrontab - create the temporary file under the TMPDIR environment variable
-    (if set), otherwise continue to use TmpFS as configured in slurm.conf.
- -- sbcast / srun --bcast - removed support for zlib compression. lz4 is vastly
-    superior in performance, and (counter-intuitively) zlib could provide worse
-    performance than no compression at all on many systems.
- -- sacctmgr - changed column headings to "ParentID" and "ParentName" instead
-    of "Par ID" and "Par Name" respectively.
- -- SALLOC_THREADS_PER_CORE and SBATCH_THREADS_PER_CORE have been added as
-    input environment variables for salloc and sbatch, respectively. They do
-    the same thing as --threads-per-core.
- -- Don't display node's comment with "scontrol show nodes" unless set.
- -- Added SLURM_GPUS_ON_NODE environment variable within each job/step.
- -- sreport - change to sorting TopUsage by the --tres option.
- -- slurmrestd - do not run allow operation as SlurmUser/root by default.
- -- "scontrol show node" now shows State as base_state+flags instead of
-    shortened state with flags appended. eg. IDLE# -> IDLE+POWERING_UP.
-    Also "POWER" state flag string is "POWERED_DOWN".
- -- scrontab - add ability to update crontab from a file or standard input.
- -- scrontab - added ability to set and expand variables.
- -- Make srun sensitive to BcastParameters.
- -- Added sbcast/srun --send-libs, sbcast --exclude and srun --bcast-exclude.
- -- Changed ReqMem field in sacct to match memory from ReqTRES. It now shows
-    the requested memory of the whole job with a letter appended indicating
-    units (M for megabytes, G for gigabytes, etc.). ReqMem is only displayed for
-    the job, since the step does not have requested TRES. Previously ReqMem
-    was also displayed for the step but was just displaying ReqMem for the job.
 
 API CHANGES
 ===========
- -- jobcomp plugin: change plugin API to jobcomp_p_*().
- -- sched plugin: change plugin API to sched_p_*() and remove
-    slurm_sched_p_initial_priority() call.
- -- step_ctx code has been removed from the api.
- -- slurm_stepd_get_info()/stepd_get_info() has been removed from the api.
- -- The v0.0.35 OpenAPI plugin has now been marked as deprecated.
-    Please convert your requests to the v0.0.37 OpenAPI plugin.

commit 51f33fb9e6850e3bf44ea5f31356eab4854afdb7

Fix push_reconfig_to_slurmd() when talking to 21.08.
Need to load the List representation of the configs for 21.08.
Otherwise this won't send updated configs to the 21.08 slurmds.

Regression from 82db921cde.

Bug 15555.

src/slurmctld/node_mgr.c
@@ -3985,7 +3985,7 @@ void push_reconfig_to_slurmd(char **slurmd_config_files)
 	prev_args->hostlist = hostlist_create(NULL);
 	prev_args->protocol_version = SLURM_ONE_BACK_PROTOCOL_VERSION;
 	prev_config = xmalloc(sizeof(*prev_config));
-	load_config_response_msg(prev_config, CONFIG_REQUEST_SLURMD);
+	load_config_response_list(prev_config, slurmd_config_files);
 	prev_args->msg_args = prev_config;
 
 	old_args = xmalloc(sizeof(*old_args));