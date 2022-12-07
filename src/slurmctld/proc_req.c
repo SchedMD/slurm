SchedMD/slurm

commit 578692d667f64499b65cc5d65a563d46f2210e3f

Add dynamic node registrations
Nodes can be added to partitions using the dynamic feature and using
nodesets or the ALL keyword.

e.g.
slurmd -Zf1

NodeSet=ns1 Feature=f1
PartitionName=p1 Nodes=ALL
PartitionName=p2 Nodes=f1

Bug 13679

src/slurmctld/proc_req.c
@@ -2798,7 +2798,7 @@ static void _slurm_rpc_node_registration(slurm_msg_t *msg)
 		(slurm_node_registration_status_msg_t *) msg->data;
 	/* Locks: Read config, write job, write node */
 	slurmctld_lock_t job_write_lock = {
-		READ_LOCK, WRITE_LOCK, WRITE_LOCK, NO_LOCK, READ_LOCK };
+		READ_LOCK, WRITE_LOCK, WRITE_LOCK, WRITE_LOCK, READ_LOCK };
 
 	START_TIMER;
 	if (!validate_slurm_user(msg->auth_uid)) {
@@ -2844,6 +2844,8 @@ static void _slurm_rpc_node_registration(slurm_msg_t *msg)
 				 * mapped node_name.
 				 */
 				_find_avail_future_node(msg);
+			} else {
+				error_code = create_dynamic_reg_node(msg);
 			}
 
 			if (!(msg->flags & CTLD_QUEUE_PROCESSING))

src/common/node_conf.c
@@ -804,6 +804,35 @@ extern node_record_t *create_node_record_at(int index, char *node_name,
 	return node_ptr;
 }
 
+extern node_record_t *add_node_record(char *alias, config_record_t *config_ptr)
+{
+	node_record_t *node_ptr = NULL;
+
+	if ((node_ptr = find_node_record2(alias))) {
+		error("Node '%s' already exists in the node table", alias);
+		return NULL;
+	}
+
+	for (int i = 0; i < node_record_count; i++) {
+		if (node_record_table_ptr[i])
+			continue;
+
+		if (!(node_ptr = create_node_record_at(i, alias, config_ptr)))
+			return NULL;
+
+		bit_set(config_ptr->node_bitmap, i);
+
+		gres_init_node_config(node_ptr->config_ptr->gres,
+				      &node_ptr->gres_list);
+
+		break;
+	}
+	if (!node_ptr)
+		error("Unable to add node '%s', node table is full", alias);
+
+	return node_ptr;
+}
+
 extern void delete_node_record(char *name)
 {
 	int node_index;

src/common/node_conf.h
@@ -298,6 +298,16 @@ extern node_record_t *create_node_record_at(int index, char *node_name,
 					    config_record_t *config_ptr);
 
 /*
+ * Add a node to node_record_table_ptr without growing the table and increasing
+ * node_reocrd_count. The node in an empty slot in the node_record_table_ptr.
+ *
+ * IN alias - name of node.
+ * IN config_ptr - config_record_t* to initialize node with.
+ * RET node_record_t* on SUCESS, NULL otherwise.
+ */
+extern node_record_t *add_node_record(char *alias, config_record_t *config_ptr);
+
+/*
  * Delete node from node_record_table_ptr.
  *
  * IN name - name of node_record_t to delete

src/common/slurm_protocol_defs.h
@@ -1284,6 +1284,7 @@ typedef struct {
 typedef enum {
 	DYN_NODE_NONE = 0,
 	DYN_NODE_FUTURE,
+	DYN_NODE_NORM,
 } dynamic_node_type_t;
 
 /*****************************************************************************\

src/slurmctld/node_mgr.c
@@ -4450,6 +4450,85 @@ extern void set_node_comm_name(node_record_t *node_ptr, char *comm_name,
 			  node_ptr->node_hostname);
 }
 
+static int _foreach_build_part_bitmap(void *x, void *arg)
+{
+	build_part_bitmap(x);
+	return SLURM_SUCCESS;
+}
+
+static void _update_parts()
+{
+	/* scan partition table and identify nodes in each */
+	list_for_each(part_list, _foreach_build_part_bitmap, NULL);
+	set_partition_tres();
+}
+
+extern int create_dynamic_reg_node(slurm_msg_t *msg)
+{
+	config_record_t *config_ptr;
+	node_record_t *node_ptr;
+	slurm_addr_t addr;
+	char *comm_name = NULL;
+	slurm_node_registration_status_msg_t *reg_msg = msg->data;
+
+	xassert(verify_lock(JOB_LOCK, WRITE_LOCK));
+	xassert(verify_lock(NODE_LOCK, WRITE_LOCK));
+	xassert(verify_lock(PART_LOCK, WRITE_LOCK));
+
+	if (find_node_record2(reg_msg->node_name))
+		return SLURM_SUCCESS;
+
+	config_ptr = create_config_record();
+	config_ptr->boards = reg_msg->boards;
+	config_ptr->cores = reg_msg->cores;
+	config_ptr->cpus = reg_msg->cpus;
+	config_ptr->node_bitmap = bit_alloc(node_record_count);
+	config_ptr->nodes = xstrdup(reg_msg->node_name);
+	config_ptr->real_memory = reg_msg->real_memory;
+	config_ptr->threads = reg_msg->threads;
+	config_ptr->tmp_disk = reg_msg->tmp_disk;
+	config_ptr->tot_sockets = reg_msg->sockets;
+
+	if (!(node_ptr = add_node_record(reg_msg->node_name, config_ptr))) {
+		list_delete_ptr(config_list, config_ptr);
+		return SLURM_ERROR;
+	}
+
+	/* Get IP of slurmd */
+	if (msg->conn_fd >= 0 &&
+	    !slurm_get_peer_addr(msg->conn_fd, &addr)) {
+		comm_name = xmalloc(INET6_ADDRSTRLEN);
+		slurm_get_ip_str(&addr, comm_name,
+				 INET6_ADDRSTRLEN);
+	}
+
+	set_node_comm_name(node_ptr,
+			   comm_name ? comm_name :
+			   xstrdup(reg_msg->node_name),
+			   xstrdup(reg_msg->node_name));
+
+	slurm_set_addr(&node_ptr->slurm_addr, node_ptr->port,
+		       node_ptr->comm_name);
+
+	if (reg_msg->dynamic_feature) {
+		node_ptr->features = xstrdup(reg_msg->dynamic_feature);
+		update_feature_list(avail_feature_list, node_ptr->features,
+				    config_ptr->node_bitmap);
+		node_ptr->features_act = xstrdup(reg_msg->dynamic_feature);
+		update_feature_list(active_feature_list, node_ptr->features_act,
+				    config_ptr->node_bitmap);
+	}
+
+	make_node_idle(node_ptr, NULL);
+	node_ptr->node_state |= NODE_STATE_DYNAMIC;
+
+	set_cluster_tres(false);
+	_update_parts();
+	select_g_reconfigure();
+
+	return SLURM_SUCCESS;
+}
+
 static void _remove_node_from_features(node_record_t *node_ptr)
 {
 	bitstr_t *node_bitmap = bit_alloc(node_record_count);
@@ -4527,8 +4606,8 @@ extern int delete_nodes(char *names, char **err_msg)
 	}
 
 	if (one_success) {
-		set_partition_tres();
 		rehash_node();
+		_update_parts();
 		select_g_reconfigure();
 	} else {
 		char *nodes = hostlist_ranged_string_xmalloc(error_hostlist);

src/slurmctld/slurmctld.h
@@ -2527,6 +2527,14 @@ extern int update_node_record_acct_gather_data(
 	acct_gather_node_resp_msg_t *msg);
 
 /*
+ * Create and add dynamic node to system from registration.
+ *
+ * IN msg - slurm_msg_t containing slurm_node_registration_status_msg_t.
+ * RET SLURM_SUCCESS on success, SLURM_ERROR otherwise.
+ */
+extern int create_dynamic_reg_node(slurm_msg_t *msg);
+
+/*
  * Delete node names from system from a slurmctld perspective.
  *
  * e.g. remove node from partitions, reconfig cons_tres, etc.

src/slurmd/slurmd/slurmd.c
@@ -606,25 +606,21 @@ cleanup:
 static int _load_gres()
 {
 	int rc;
+	uint32_t cpu_cnt;
 	node_record_t *node_rec;
+	List gres_list = NULL;
 
 	node_rec = find_node_record(conf->node_name);
 	if (node_rec && node_rec->config_ptr) {
-		uint32_t cpu_cnt;
-		List gres_list = NULL;
 		(void) gres_init_node_config(node_rec->config_ptr->gres,
 					     &gres_list);
-
-		cpu_cnt = MAX(conf->conf_cpus, conf->block_map_size);
-		rc = gres_g_node_config_load(cpu_cnt, conf->node_name, gres_list,
-					     (void *)&xcpuinfo_abs_to_mac,
-					     (void *)&xcpuinfo_mac_to_abs);
-		FREE_NULL_LIST(gres_list);
-	} else {
-		rc = SLURM_ERROR;
-		error("Unable to find node record for node:%s",
-		      conf->node_name);
 	}
+
+	cpu_cnt = MAX(conf->conf_cpus, conf->block_map_size);
+	rc = gres_g_node_config_load(cpu_cnt, conf->node_name, gres_list,
+				     (void *)&xcpuinfo_abs_to_mac,
+				     (void *)&xcpuinfo_mac_to_abs);
+	FREE_NULL_LIST(gres_list);
 
 	return rc;
 }
@@ -925,7 +921,10 @@ _read_config(void)
 			conf->node_name,
 			conf->hostname);
 
-	conf->port = slurm_conf_get_port(conf->node_name);
+	if (conf->dynamic_type == DYN_NODE_NORM)
+		conf->port = cf->slurmd_port;
+	else
+		conf->port = slurm_conf_get_port(conf->node_name);
 	slurm_conf.slurmd_port = conf->port;
 	slurm_conf_get_cpus_bsct(conf->node_name,
 				 &conf->conf_cpus, &conf->conf_boards,
@@ -1382,7 +1381,7 @@ static void _print_gres(void)
 static void
 _process_cmdline(int ac, char **av)
 {
-	static char *opt_string = "bcCd:Df:F::GhL:Mn:N:svV";
+	static char *opt_string = "bcCd:Df:F::GhL:Mn:N:svVZ::";
 	int c;
 	char *tmp_char;
 
@@ -1461,6 +1460,10 @@ _process_cmdline(int ac, char **av)
 		case 'V':
 			print_slurm_version();
 			exit(0);
+			break;
+		case 'Z':
+			conf->dynamic_type = DYN_NODE_NORM;
+			conf->dynamic_feature = xstrdup(optarg);
 			break;
 		case LONG_OPT_CONF_SERVER:
 			conf->conf_server = xstrdup(optarg);
@@ -1734,6 +1737,7 @@ _slurmd_init(void)
 		conf->sockets = conf->actual_sockets;
 		conf->cores   = conf->actual_cores;
 		conf->threads = conf->actual_threads;
+		get_memory(&conf->real_memory_size);
 
 		send_registration_msg(SLURM_SUCCESS);
 
@@ -1747,7 +1751,9 @@ _slurmd_init(void)
 	 */
 	_read_config();
 
-	if (!find_node_record(conf->node_name))
+	/* All but normal dynamic nodes should find a node record */
+	if ((conf->dynamic_type != DYN_NODE_NORM) &&
+	    !find_node_record(conf->node_name))
 		return SLURM_ERROR;
 
 	/*
@@ -2071,7 +2077,8 @@ Usage: %s [OPTIONS]\n\
    -N node                    Run the daemon for specified nodename.\n\
    -s                         Change working directory to SlurmdLogFile/SlurmdSpoolDir.\n\
    -v                         Verbose mode. Multiple -v's increase verbosity.\n\
-   -V                         Print version information and exit.\n",
+   -V                         Print version information and exit.\n\
+   -Z[feature]                Start as Dynamic Normal node w/optional Feature.\n",
 		conf->prog);
 	return;
 }

commit 48732d396fcd682b4ff4a1e472f439e54a38f06e

Run _slurm_rpc_node_registration with proper locks when enable_rpc_queue
Regression from 578692d667

Bug 15471

src/slurmctld/proc_req.c
@@ -6440,6 +6440,7 @@ slurmctld_rpc_t slurmctld_rpcs[] =
 			.conf = READ_LOCK,
 			.job = WRITE_LOCK,
 			.node = WRITE_LOCK,
+			.part = WRITE_LOCK,
 			.fed = READ_LOCK,
 		},
 	},{