01ea23384e68250e7d688e26dd41dafc5d5bc1d1
Caden Ellis <caden@schedmd.com>
2022-12-12 12:07:36 -0700

    Check for passed deadline on jobs being held after a finished resv
    Previously, if a job was submitted to a reservation with a deadline and
    the reservation expired, the job would stay in the JOB_RESV_DEL_HOLD
    state forever, even after its deadline had passed. Now we will check for
    valid deadlines for jobs in that state, and clear them if the job
    deadline has passed.
    
    Bug 15275

--- src/slurmctld/job_scheduler.c
+++ src/slurmctld/job_scheduler.c
@@ -329,8 +329,17 @@ static bool _job_runnable_test1(job_record_t *job_ptr, bool sched_plugin)
 	if (sched_plugin)
 		job_ptr->start_time = (time_t) 0;
 	if (job_ptr->priority == 0)	{ /* held */
+		/*
+		 * If a job is held because of a deleted reservation,
+		 * check to see if it has a deadline that has passed.
+		 */
+		if ((job_ptr->job_state & JOB_RESV_DEL_HOLD) &&
+		    (job_ptr->deadline) && (job_ptr->deadline != NO_VAL))
+			deadline_ok(job_ptr, "sched");
+
 		if ((job_ptr->state_reason != FAIL_BAD_CONSTRAINTS) &&
 		    (job_ptr->state_reason != FAIL_BURST_BUFFER_OP) &&
+		    (job_ptr->state_reason != FAIL_DEADLINE) &&
 		    (job_ptr->state_reason != WAIT_HELD) &&
 		    (job_ptr->state_reason != WAIT_HELD_USER) &&
 		    (job_ptr->state_reason != WAIT_MAX_REQUEUE) &&
