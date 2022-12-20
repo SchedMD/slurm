eb24a9b087f3a7a1dd0b8491a5e7d0f1effdc10f
Danny Auble <da@schedmd.com>
2022-12-20 16:08:44 -0700

    Make it so we can update gres with a new task count.
    Bug 14867

--- src/interfaces/gres.c
+++ src/interfaces/gres.c
@@ -5319,10 +5319,9 @@ static int _test_gres_cnt(gres_state_t *gres_state_job,
 			if (*num_tasks == NO_VAL)
 				*num_tasks = req_tasks;
 			else if (*num_tasks != req_tasks) {
-				error("Failed to validate job spec. Based on --%ss and --%ss-per-task number of requested tasks differ from -n/--ntasks.",
-				      gres_state_job->gres_name,
-				      gres_state_job->gres_name);
-				return -1;
+				/* requesting new task count */
+				gres_js->total_gres = gres_js->gres_per_job =
+					*num_tasks * gres_js->gres_per_task;
 			}
 		} else if (*num_tasks != NO_VAL) {
 			gres_js->gres_per_job = *num_tasks *
