da23a6ca87eee746092f4b6e3e316d0f5cd9807f
Nathan Rini <nate@schedmd.com>
2023-01-02 16:52:28 -0700

    Testsuite - Add python test sinfo --json
    Bug 14536

---
+++ testsuite/python/tests/commands/sinfo/test_--json.py
@@ -0,0 +1,18 @@
+############################################################################
+# Copyright (C) SchedMD LLC.
+############################################################################
+import atf
+import pytest
+import re
+import json
+
+# Setup
+@pytest.fixture(scope="module", autouse=True)
+def setup():
+    atf.require_slurm_running()
+
+def test_json():
+    """Verify sinfo --json has the correct format"""
+
+    output = atf.run_command_output(f"sinfo --json", fatal=True)
+    assert json.loads(output) is not None
