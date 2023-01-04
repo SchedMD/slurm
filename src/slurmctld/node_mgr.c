SchedMD/slurm

commit 8a282e6ca43e6b2d2002d12ef0ba5402d4998264

Use new nodespec_to_hostlist() to update nodes.
Bug 13099

src/slurmctld/node_mgr.c
@@ -1374,13 +1374,16 @@ int update_node ( update_node_msg_t * update_node_msg )
 		return ESLURM_INVALID_NODE_NAME;
 	}
 
-	host_list = hostlist_create(update_node_msg->node_names);
-	if (host_list == NULL) {
-		info("update_node: hostlist_create error on %s: %m",
-		      update_node_msg->node_names);
+	if (!(host_list = nodespec_to_hostlist(update_node_msg->node_names,
+					       NULL)))
+		return ESLURM_INVALID_NODE_NAME;
+
+	if (!(node_cnt = hostlist_count(host_list))) {
+		info("%s: expansion of node specification '%s' resulted in zero nodes",
+		     __func__, update_node_msg->node_names);
+		FREE_NULL_HOSTLIST(host_list);
 		return ESLURM_INVALID_NODE_NAME;
 	}
-	node_cnt = hostlist_count(host_list);
 
 	if (update_node_msg->node_addr) {
 		hostaddr_list = hostlist_create(update_node_msg->node_addr);

doc/man/man1/scontrol.1
@@ -1425,7 +1425,11 @@ allowed.
 .TP
 \fBNodeName\fR=<\fIname\fR>
 Identify the node(s) to be updated. Multiple node names may be specified using
-simple node range expressions (e.g. "lx[10\-20]"). This specification is required.
+simple node range expressions (e.g. "lx[10\-20]"). Nodesets can also be
+specified by themselves or mixed with node range expressions, using a comma as
+a list separator. If the keyword "ALL" is specified alone, then the update
+will be attempted against all the nodes in the local cluster. This specification
+is required.
 .IP
 
 .TP

commit 2e334626d6cf5166978c980e8d475cc02fd430de

Fix regression when updating unsorted nodelists with addrs/hostnames
Introduced in 8a282e6ca4, which causes the NodeName=<nodelist> to be
both sorted and uniq, after taking care of ALL keyword, nodelists, and
nodesets. If NodeAddr and/or NodeHostname were specified to be updated
for the given nodelist, the intended comma-separated 1:1 matching
relation could not match depending on the sorting/uniq changing the
original <nodelist> specified in the node update request.

Bug 15731

src/slurmctld/node_mgr.c
@@ -1368,14 +1368,18 @@ int update_node(update_node_msg_t *update_node_msg, uid_t auth_uid)
 	hostlist_t host_list, hostaddr_list = NULL, hostname_list = NULL;
 	uint32_t base_state = 0, node_flags, state_val;
 	time_t now = time(NULL);
+	bool uniq = true;
 
 	if (update_node_msg->node_names == NULL ) {
 		info("%s: invalid node name", __func__);
 		return ESLURM_INVALID_NODE_NAME;
 	}
 
+	if (update_node_msg->node_addr || update_node_msg->node_hostname)
+		uniq = false;
+
 	if (!(host_list = nodespec_to_hostlist(update_node_msg->node_names,
-					       NULL)))
+					       uniq, NULL)))
 		return ESLURM_INVALID_NODE_NAME;
 
 	if (!(node_cnt = hostlist_count(host_list))) {
@@ -4867,7 +4871,7 @@ extern int delete_nodes(char *names, char **err_msg)
 
 	lock_slurmctld(write_lock);
 
-	if (!(to_delete = nodespec_to_hostlist(names, NULL))) {
+	if (!(to_delete = nodespec_to_hostlist(names, true, NULL))) {
 		ret_rc = ESLURM_INVALID_NODE_NAME;
 		goto cleanup;
 	}

src/slurmctld/partition_mgr.c
@@ -192,7 +192,7 @@ extern int build_part_bitmap(part_record_t *part_ptr)
 			   node_record_count - 1);
 	}
 
-	if (!(host_list = nodespec_to_hostlist(part_ptr->orig_nodes,
+	if (!(host_list = nodespec_to_hostlist(part_ptr->orig_nodes, true,
 					       &part_ptr->nodesets))) {
 		/* Error, restore original bitmap */
 		FREE_NULL_BITMAP(part_ptr->node_bitmap);

src/slurmctld/read_config.c
@@ -307,7 +307,9 @@ static void _add_nodes_with_feature(hostlist_t hl, char *feature)
 	}
 }
 
-extern hostlist_t nodespec_to_hostlist(const char *nodes, char **nodesets)
+extern hostlist_t nodespec_to_hostlist(const char *nodes,
+				       bool uniq,
+				       char **nodesets)
 {
 	int count;
 	slurm_conf_nodeset_t *ptr, **ptr_array;
@@ -354,7 +356,8 @@ extern hostlist_t nodespec_to_hostlist(const char *nodes, char **nodesets)
 		}
 	}
 
-	hostlist_uniq(hl);
+	if (uniq)
+		hostlist_uniq(hl);
 	return hl;
 }
 

src/slurmctld/slurmctld.h
@@ -2941,6 +2941,7 @@ extern int update_node_avail_features(char *node_names, char *avail_features,
  * Handles node range expressions, nodesets and ALL keyword.
  *
  * IN nodes - nodelist that can have nodesets or ALL in it.
+ * IN uniq - call hostlist_uniq() before returning the hostlist
  * OUT nodesets (optional) - list of nodesets found in nodes string
  *
  * RET NULL on error, hostlist_t otherwise.
@@ -2948,7 +2949,9 @@ extern int update_node_avail_features(char *node_names, char *avail_features,
  * NOTE: Caller must FREE_NULL_HOSTLIST() returned hostlist_t.
  * NOTE: Caller should interpret a non-NULL but empty hostlist conveniently.
  */
-extern hostlist_t nodespec_to_hostlist(const char *nodes, char **nodesets);
+extern hostlist_t nodespec_to_hostlist(const char *nodes,
+				       bool uniq,
+				       char **nodesets);
 
 /*
  * set_node_reboot_reason - appropriately set node reason with reboot message