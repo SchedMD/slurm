f42bb1b727a4889635ade0d06b8ab9204198eeee
Oscar Hernández <oscar.hernandez@schedmd.com>
2023-01-26 13:25:59 -0700

    Add jobid to job_step_create_response_msg_t.
    For job array tasks the underlying jobid will be needed for further srun
    communication with the slurmd as part of the launch.

--- src/common/slurm_protocol_pack.c
+++ src/common/slurm_protocol_pack.c
@@ -3003,6 +3003,7 @@ static void _pack_job_step_create_response_msg(
 	if (protocol_version >= SLURM_23_02_PROTOCOL_VERSION) {
 		pack32(msg->def_cpu_bind_type, buffer);
 		packstr(msg->resv_ports, buffer);
+		pack32(msg->job_id, buffer);
 		pack32(msg->job_step_id, buffer);
 		pack_slurm_step_layout(msg->step_layout, buffer,
 				       protocol_version);
@@ -3041,6 +3042,7 @@ static int _unpack_job_step_create_response_msg(
 	if (protocol_version >= SLURM_23_02_PROTOCOL_VERSION) {
 		safe_unpack32(&tmp_ptr->def_cpu_bind_type, buffer);
 		safe_unpackstr(&tmp_ptr->resv_ports, buffer);
+		safe_unpack32(&tmp_ptr->job_id, buffer);
 		safe_unpack32(&tmp_ptr->job_step_id, buffer);
 		if (unpack_slurm_step_layout(&tmp_ptr->step_layout, buffer,
 					     protocol_version))
