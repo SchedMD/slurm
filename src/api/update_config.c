SchedMD/slurm

commit 85e92d048f8b2127207308e340cbe9df77660673

Add err_msg to single job job_submit_plugin_modify/scontrol update job
This commit adds handling of sending a message to a user from
job_submit_plugin_modify to slurmctld when handling scontrol update of a
single job - not array task, not set of array task elements

Bug 14539

src/api/update_config.c
@@ -121,7 +121,7 @@ tryagain:
 	case RESPONSE_JOB_ARRAY_ERRORS:
 		*resp = (job_array_resp_msg_t *) resp_msg.data;
 		break;
-	case RESPONSE_SLURM_RC:
+	case RESPONSE_SLURM_RC_MSG:
 		rc = ((return_code_msg_t *) resp_msg.data)->return_code;
 		if (rc)
 			slurm_seterrno(rc);

src/plugins/job_submit/all_partitions/job_submit_all_partitions.c
@@ -105,7 +105,7 @@ extern int job_submit(job_desc_msg_t *job_desc, uint32_t submit_uid,
 }
 
 extern int job_modify(job_desc_msg_t *job_desc, job_record_t *job_ptr,
-		      uint32_t submit_uid)
+		      uint32_t submit_uid, char **err_msg)
 {
 	return SLURM_SUCCESS;
 }

src/plugins/job_submit/cray_aries/job_submit_cray_aries.c
@@ -115,7 +115,7 @@ extern int job_submit(job_desc_msg_t *job_desc, uint32_t submit_uid)
 }
 
 extern int job_modify(job_desc_msg_t *job_desc, job_record_t *job_ptr,
-		      uint32_t submit_uid)
+		      uint32_t submit_uid, char **err_msg)
 {
 	/* Don't call this on modify it shouldn't be needed and will
 	 * mess things up if modifying a running job

src/plugins/job_submit/defaults/job_submit_defaults.c
@@ -112,7 +112,7 @@ extern int job_submit(job_desc_msg_t *job_desc, uint32_t submit_uid,
 }
 
 extern int job_modify(job_desc_msg_t *job_desc, job_record_t *job_ptr,
-		      uint32_t submit_uid)
+		      uint32_t submit_uid, char **err_msg)
 {
 #if 0
 	uint16_t acctg_freq = 0;

src/plugins/job_submit/logging/job_submit_logging.c
@@ -101,7 +101,7 @@ extern int job_submit(job_desc_msg_t *job_desc, uint32_t submit_uid,
 }
 
 extern int job_modify(job_desc_msg_t *job_desc, job_record_t *job_ptr,
-		      uint32_t submit_uid)
+		      uint32_t submit_uid, char **err_msg)
 {
 	/* Log select fields from a job modify request. See slurm/slurm.h
 	 * for information about additional fields in job_desc_msg_t.

src/plugins/job_submit/lua/job_submit_lua.c
@@ -1380,7 +1380,7 @@ out:	slurm_mutex_unlock (&lua_lock);
 
 /* Lua script hook called for "modify job" event. */
 extern int job_modify(job_desc_msg_t *job_desc, job_record_t *job_ptr,
-		      uint32_t submit_uid)
+		      uint32_t submit_uid, char **err_msg)
 {
 	int rc;
 	slurm_mutex_lock (&lua_lock);
@@ -1425,9 +1425,8 @@ extern int job_modify(job_desc_msg_t *job_desc, job_record_t *job_ptr,
 	slurm_lua_stack_dump(
 		"job_submit/lua", "job_modify, after lua_pcall", L);
 	if (user_msg) {
-		error("Use of log.user() in job_modify is not supported. "
-		      "Message discarded: (\"%s\")", user_msg);
-		xfree(user_msg);
+		*err_msg = user_msg;
+		user_msg = NULL;
 	}
 
 out:	slurm_mutex_unlock (&lua_lock);

src/plugins/job_submit/partition/job_submit_partition.c
@@ -192,7 +192,7 @@ extern int job_submit(job_desc_msg_t *job_desc, uint32_t submit_uid,
 }
 
 extern int job_modify(job_desc_msg_t *job_desc, job_record_t *job_ptr,
-		      uint32_t submit_uid)
+		      uint32_t submit_uid, char **err_msg)
 {
 	return SLURM_SUCCESS;
 }

src/plugins/job_submit/pbs/job_submit_pbs.c
@@ -349,7 +349,7 @@ extern int job_submit(job_desc_msg_t *job_desc, uint32_t submit_uid)
 
 /* Lua script hook called for "modify job" event. */
 extern int job_modify(job_desc_msg_t *job_desc, job_record_t *job_ptr,
-		      uint32_t submit_uid)
+		      uint32_t submit_uid, char **err_msg)
 {
 	/* Locks: Read config, write job, read node, read partition
 	 * HAVE BEEN SET ON ENTRY TO THIS FUNCTION */

commit e18aad167458b873890744c1eec43dc7f2f7ea6f

This should still be RESPONSE_SLURM_RC.
The API will never see a RESPONSE_SLURM_RC_MSG message type, as those will
be translated back into RESPONSE_SLURM_RC within unpack_msg() after printing
the error message directly to the terminal.

This meant that callers of slurm_update_job2() would receive
SLURM_UNEXPECTED_MSG_ERROR instead of the return code from the job_submit
plugin, and there's be a (very small) memory leak of the return_code_msg_t.

Regression from 85e92d048f.

Bug 14539.

src/api/update_config.c
@@ -123,7 +123,7 @@ tryagain:
 	case RESPONSE_JOB_ARRAY_ERRORS:
 		*resp = (job_array_resp_msg_t *) resp_msg.data;
 		break;
-	case RESPONSE_SLURM_RC_MSG:
+	case RESPONSE_SLURM_RC:
 		rc = ((return_code_msg_t *) resp_msg.data)->return_code;
 		if (rc)
 			slurm_seterrno(rc);