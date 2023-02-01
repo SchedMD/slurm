336cdb8d408684a031d853af2f0d4340835e7ca8
Tim Wickberg <tim@schedmd.com>
2023-02-01 15:30:10 -0700

    Add 23.02 blocks to _pack_license() / _unpack_license_info_msg().

--- src/slurmctld/licenses.c
+++ src/slurmctld/licenses.c
@@ -993,7 +993,13 @@ extern void license_set_job_tres_cnt(list_t *license_list,
 static void _pack_license(licenses_t *lic, buf_t *buffer,
 			  uint16_t protocol_version)
 {
-	if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
+	if (protocol_version >= SLURM_23_02_PROTOCOL_VERSION) {
+		packstr(lic->name, buffer);
+		pack32(lic->total, buffer);
+		pack32(lic->used, buffer);
+		pack32(lic->reserved, buffer);
+		pack8(lic->remote, buffer);
+	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		packstr(lic->name, buffer);
 		pack32(lic->total, buffer);
 		pack32(lic->used, buffer);
