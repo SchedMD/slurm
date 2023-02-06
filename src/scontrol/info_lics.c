9541ca09a634cdf7bd675c250a732396e62390ad
Tim Wickberg <tim@schedmd.com>
2023-02-06 16:40:24 -0700

    scontrol - Split up new licenses info for remote licenses onto a new line.

--- src/scontrol/info_lics.c
+++ src/scontrol/info_lics.c
@@ -155,7 +155,8 @@ static void _print_license_info(const char *name, license_info_msg_t *msg)
 			char time_str[256];
 			slurm_make_time_str(&sorted_lic[cc]->last_update,
 					    time_str, sizeof(time_str));
-			printf(" LastConsumed=%u LastDeficit=%u LastUpdate=%s\n",
+			printf("%sLastConsumed=%u LastDeficit=%u LastUpdate=%s\n",
+			       one_liner ? " " : "\n    ",
 			       sorted_lic[cc]->last_consumed,
 			       sorted_lic[cc]->last_deficit, time_str);
 		} else {
