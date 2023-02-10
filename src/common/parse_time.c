9f41a5eb08642685340df592a0d859fd1b7d793c
Carlos Tripiana Montes <tripiana@schedmd.com>
2023-02-10 11:34:00 +0100

    common/parse_time - format unset time (NO_VAL) as empty string.
    Instead of generating a formatted string off of NO_VAL value which is
    confusing.
    
    Bug 15953

--- src/common/parse_time.c
+++ src/common/parse_time.c
@@ -779,7 +779,9 @@ extern int time_str2mins(const char *string)
 
 extern void secs2time_str(time_t time, char *string, int size)
 {
-	if (time == INFINITE) {
+	if (time == NO_VAL) {
+		snprintf(string, size, "%s", "");
+	} else if (time == INFINITE) {
 		snprintf(string, size, "UNLIMITED");
 	} else {
 		long days, hours, minutes, seconds;
@@ -805,7 +807,9 @@ extern void secs2time_str(time_t time, char *string, int size)
 
 extern void mins2time_str(uint32_t time, char *string, int size)
 {
-	if (time == INFINITE) {
+	if (time == NO_VAL) {
+		snprintf(string, size, "%s", "");
+	} else if (time == INFINITE) {
 		snprintf(string, size, "UNLIMITED");
 	} else {
 		long days, hours, minutes, seconds;
