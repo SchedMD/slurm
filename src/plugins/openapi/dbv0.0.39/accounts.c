d9ef60fcd0a1dbbd29155f08de99c394ae3d9fb9
Nathan Rini <nate@schedmd.com>
2023-02-14 16:16:48 -0700

    openapi/dbv0.0.39 - avoid incorrect return in _foreach_rm_acct_coord()
    CID 307457

--- src/plugins/openapi/dbv0.0.39/accounts.c
+++ src/plugins/openapi/dbv0.0.39/accounts.c
@@ -268,7 +268,7 @@ static int _foreach_rm_acct_coord(void *x, void *arg)
 	FREE_NULL_LIST(rm_list);
 	FREE_NULL_LIST(assoc_cond.user_list);
 
-	return rc ? SLURM_ERROR : SLURM_ERROR;
+	return rc ? SLURM_ERROR : SLURM_SUCCESS;
 }
 
 static int _foreach_update_acct(void *x, void *arg)
