SchedMD/slurm

commit f141cd9f60e81b3411d07b084d30cf97e5f8ddc9

sinfo - convert to data_parser interface

src/sinfo/sinfo.c
@@ -45,6 +45,7 @@
 #include "src/common/slurm_time.h"
 #include "src/sinfo/sinfo.h"
 #include "src/sinfo/print.h"
+#include "src/interfaces/data_parser.h"
 
 /********************
  * Global Variables *
@@ -75,8 +76,6 @@ static pthread_mutex_t sinfo_list_mutex = PTHREAD_MUTEX_INITIALIZER;
 /*************
  * Functions *
  *************/
-extern int dump_data(int argc, char **argv);
-
 static void _free_sinfo_format(void *object);
 static void _free_params(void);
 void *      _build_part_info(void *args);
@@ -87,8 +86,8 @@ static sinfo_data_t *_create_sinfo(partition_info_t* part_ptr,
 				   uint16_t part_inx, node_info_t *node_ptr);
 static int  _find_part_list(void *x, void *key);
 static bool _filter_out(node_info_t *node_ptr);
-static int  _get_info(bool clear_old, slurmdb_federation_rec_t *fed,
-		      char *cluster_name);
+static int _get_info(bool clear_old, slurmdb_federation_rec_t *fed,
+		     char *cluster_name, int argc, char **argv);
 static int  _insert_node_ptr(List sinfo_list, uint16_t part_num,
 			     partition_info_t *part_ptr,
 			     node_info_t *node_ptr);
@@ -96,7 +95,7 @@ static int  _load_resv(reserve_info_msg_t ** reserv_pptr, bool clear_old);
 static bool _match_node_data(sinfo_data_t *sinfo_ptr, node_info_t *node_ptr);
 static bool _match_part_data(sinfo_data_t *sinfo_ptr,
 			     partition_info_t* part_ptr);
-static int  _multi_cluster(List clusters);
+static int _multi_cluster(List clusters, int argc, char **argv);
 static void _node_list_delete(void *data);
 static void _part_list_delete(void *data);
 static List _query_fed_servers(slurmdb_federation_rec_t *fed,
@@ -124,18 +123,15 @@ int main(int argc, char **argv)
 		log_alter(opts, SYSLOG_FACILITY_USER, NULL);
 	}
 
-	if (params.mimetype)
-		exit(dump_data(argc, argv));
-
 	while (1) {
-		if ((!params.no_header) &&
+		if (!params.no_header && !params.mimetype &&
 		    (params.iterate || params.verbose || params.long_output))
 			print_date();
 
 		if (!params.clusters) {
-			if (_get_info(false, params.fed, NULL))
+			if (_get_info(false, params.fed, NULL, argc, argv))
 				rc = 1;
-		} else if (_multi_cluster(params.clusters) != 0)
+		} else if (!_multi_cluster(params.clusters, argc, argv))
 			rc = 1;
 		if (params.iterate) {
 			printf("\n");
@@ -189,7 +185,7 @@ static void prepend_cluster_name(void)
 	format_prepend_cluster_name(params.format_list, 8, false, "");
 }
 
-static int _multi_cluster(List clusters)
+static int _multi_cluster(List clusters, int argc, char **argv)
 {
 	ListIterator itr;
 	bool first = true;
@@ -207,7 +203,8 @@ static int _multi_cluster(List clusters)
 				printf("\n");
 			printf("CLUSTER: %s\n", working_cluster_rec->name);
 		}
-		rc2 = _get_info(true, NULL, working_cluster_rec->name);
+		rc2 = _get_info(true, NULL, working_cluster_rec->name, argc,
+				argv);
 		if (rc2)
 			rc = 1;
 	}
@@ -229,7 +226,7 @@ static int _set_cluster_name(void *x, void *arg)
  * fed IN - information about other clusters in this federation
  */
 static int _get_info(bool clear_old, slurmdb_federation_rec_t *fed,
-		     char *cluster_name)
+		     char *cluster_name, int argc, char **argv)
 {
 	List node_info_msg_list = NULL, part_info_msg_list = NULL;
 	reserve_info_msg_t *reserv_msg = NULL;
@@ -261,12 +258,16 @@ static int _get_info(bool clear_old, slurmdb_federation_rec_t *fed,
 	}
 
 	sort_sinfo_list(sinfo_list);
-	print_sinfo_list(sinfo_list);
+	if (params.mimetype)
+		rc = DATA_DUMP_CLI(SINFO_DATA_LIST, sinfo_list, "sinfo", argc,
+				   argv, NULL, params.mimetype);
+	else
+		rc = print_sinfo_list(sinfo_list);
 
 	FREE_NULL_LIST(node_info_msg_list);
 	FREE_NULL_LIST(part_info_msg_list);
 	FREE_NULL_LIST(sinfo_list);
-	return SLURM_SUCCESS;
+	return rc;
 }
 
 /*

src/sinfo/Makefile.am
@@ -10,7 +10,7 @@ sinfo_LDADD = $(LIB_SLURM) $(DL_LIBS)
 sinfo_DEPENDENCIES = $(LIB_SLURM_BUILD)
 
 noinst_HEADERS = sinfo.h print.h
-sinfo_SOURCES = data.c sinfo.c opts.c print.c sort.c
+sinfo_SOURCES = sinfo.c opts.c print.c sort.c
 
 force:
 $(sinfo_LDADD) : force

src/sinfo/Makefile.in
@@ -156,8 +156,8 @@ CONFIG_CLEAN_FILES =
 CONFIG_CLEAN_VPATH_FILES =
 am__installdirs = "$(DESTDIR)$(bindir)"
 PROGRAMS = $(bin_PROGRAMS)
-am_sinfo_OBJECTS = data.$(OBJEXT) sinfo.$(OBJEXT) opts.$(OBJEXT) \
-	print.$(OBJEXT) sort.$(OBJEXT)
+am_sinfo_OBJECTS = sinfo.$(OBJEXT) opts.$(OBJEXT) print.$(OBJEXT) \
+	sort.$(OBJEXT)
 sinfo_OBJECTS = $(am_sinfo_OBJECTS)
 am__DEPENDENCIES_1 =
 AM_V_lt = $(am__v_lt_@AM_V@)
@@ -182,8 +182,8 @@ am__v_at_1 =
 DEFAULT_INCLUDES = -I.@am__isrc@ -I$(top_builddir) -I$(top_builddir)/slurm
 depcomp = $(SHELL) $(top_srcdir)/auxdir/depcomp
 am__maybe_remake_depfiles = depfiles
-am__depfiles_remade = ./$(DEPDIR)/data.Po ./$(DEPDIR)/opts.Po \
-	./$(DEPDIR)/print.Po ./$(DEPDIR)/sinfo.Po ./$(DEPDIR)/sort.Po
+am__depfiles_remade = ./$(DEPDIR)/opts.Po ./$(DEPDIR)/print.Po \
+	./$(DEPDIR)/sinfo.Po ./$(DEPDIR)/sort.Po
 am__mv = mv -f
 COMPILE = $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) \
 	$(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS)
@@ -496,7 +496,7 @@ AM_CPPFLAGS = -I$(top_srcdir)
 sinfo_LDADD = $(LIB_SLURM) $(DL_LIBS)
 sinfo_DEPENDENCIES = $(LIB_SLURM_BUILD)
 noinst_HEADERS = sinfo.h print.h
-sinfo_SOURCES = data.c sinfo.c opts.c print.c sort.c
+sinfo_SOURCES = sinfo.c opts.c print.c sort.c
 sinfo_LDFLAGS = $(CMD_LDFLAGS)
 all: all-am
 
@@ -591,7 +591,6 @@ mostlyclean-compile:
 distclean-compile:
 	-rm -f *.tab.c
 
-@AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/data.Po@am__quote@ # am--include-marker
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/opts.Po@am__quote@ # am--include-marker
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/print.Po@am__quote@ # am--include-marker
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/sinfo.Po@am__quote@ # am--include-marker
@@ -723,8 +722,7 @@ clean: clean-am
 clean-am: clean-binPROGRAMS clean-generic clean-libtool mostlyclean-am
 
 distclean: distclean-am
-		-rm -f ./$(DEPDIR)/data.Po
-	-rm -f ./$(DEPDIR)/opts.Po
+		-rm -f ./$(DEPDIR)/opts.Po
 	-rm -f ./$(DEPDIR)/print.Po
 	-rm -f ./$(DEPDIR)/sinfo.Po
 	-rm -f ./$(DEPDIR)/sort.Po
@@ -773,8 +771,7 @@ install-ps-am:
 installcheck-am:
 
 maintainer-clean: maintainer-clean-am
-		-rm -f ./$(DEPDIR)/data.Po
-	-rm -f ./$(DEPDIR)/opts.Po
+		-rm -f ./$(DEPDIR)/opts.Po
 	-rm -f ./$(DEPDIR)/print.Po
 	-rm -f ./$(DEPDIR)/sinfo.Po
 	-rm -f ./$(DEPDIR)/sort.Po

src/sinfo/data.c
@@ -1,110 +1,0 @@
-/*****************************************************************************\
- *  data.c - data functions for sinfo
- *****************************************************************************
- *  Copyright (C) 2021 SchedMD LLC
- *  Written by Nathan Rini <nate@schedmd.com>.
- *
- *  This file is part of Slurm, a resource management program.
- *  For details, see <https://slurm.schedmd.com/>.
- *  Please also read the included file: DISCLAIMER.
- *
- *  Slurm is free software; you can redistribute it and/or modify it under
- *  the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the License, or (at your option)
- *  any later version.
- *
- *  In addition, as a special exception, the copyright holders give permission
- *  to link the code of portions of this program with the OpenSSL library under
- *  certain conditions as described in each individual source file, and
- *  distribute linked combinations including the two. You must obey the GNU
- *  General Public License in all respects for all of the code used other than
- *  OpenSSL. If you modify file(s) with this exception, you may extend this
- *  exception to your version of the file(s), but you are not obligated to do
- *  so. If you do not wish to do so, delete this exception statement from your
- *  version.  If you delete this exception statement from all source files in
- *  the program, then also delete it here.
- *
- *  Slurm is distributed in the hope that it will be useful, but WITHOUT ANY
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- *  details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with Slurm; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
-\*****************************************************************************/
-
-#include "config.h"
-
-#include "slurm/slurm.h"
-
-#include "src/common/fd.h"
-#include "src/common/log.h"
-#include "src/interfaces/openapi.h"
-#include "src/common/parse_time.h"
-#include "src/common/xassert.h"
-#include "src/common/xmalloc.h"
-#include "src/common/xstring.h"
-#include "src/interfaces/serializer.h"
-
-#include "src/sinfo/sinfo.h"
-
-#define TARGET "/slurm/v0.0.39/nodes/"
-#define PLUGIN "openapi/v0.0.39"
-
-openapi_handler_t dump_nodes = NULL;
-int dump_tag = -1;
-
-extern void *openapi_get_db_conn(void *ctxt)
-{
-	fatal("%s should never be called in sinfo", __func__);
-}
-
-extern int bind_operation_handler(const char *str_path,
-				  openapi_handler_t callback, int callback_tag)
-{
-	debug3("%s: binding %s to 0x%"PRIxPTR,
-	       __func__, str_path, (uintptr_t) callback);
-
-	if (!xstrcmp(str_path, TARGET)) {
-		dump_nodes = callback;
-		dump_tag = callback_tag;
-	}
-
-	return SLURM_SUCCESS;
-}
-
-extern int unbind_operation_handler(openapi_handler_t callback)
-{
-	/* no-op */
-	return SLURM_SUCCESS;
-}
-
-extern int dump_data(int argc, char **argv)
-{
-	openapi_t *oas = NULL;
-	data_t *resp = data_new();
-	char *out = NULL;
-	char *ctxt;
-
-	if (init_openapi(&oas, PLUGIN, NULL))
-		fatal("unable to load openapi plugins");
-
-	ctxt = fd_resolve_path(STDIN_FILENO);
-
-	dump_nodes(ctxt, HTTP_REQUEST_GET, NULL, NULL, dump_tag, resp, NULL);
-
-	serialize_g_data_to_string(&out, NULL, resp, params.mimetype,
-				   SER_FLAGS_PRETTY);
-
-	printf("%s", out);
-
-#ifdef MEMORY_LEAK_DEBUG
-	xfree(ctxt);
-	xfree(out);
-	FREE_NULL_DATA(resp);
-	destroy_openapi(oas);
-#endif /* MEMORY_LEAK_DEBUG */
-
-	return SLURM_SUCCESS;
-}

commit 6ae341b822eb87c63bd15a7c62acb5f6ac67cb13

Don't return non-zero when querying specific clusters with sinfo
Regression from f141cd9f60e:

-		} else if (_multi_cluster(params.clusters) != 0)
+		} else if (!_multi_cluster(params.clusters, argc, argv))
 			rc = 1;

Bug 16104

src/sinfo/sinfo.c
@@ -131,7 +131,7 @@ int main(int argc, char **argv)
 		if (!params.clusters) {
 			if (_get_info(false, params.fed, NULL, argc, argv))
 				rc = 1;
-		} else if (!_multi_cluster(params.clusters, argc, argv))
+		} else if (_multi_cluster(params.clusters, argc, argv))
 			rc = 1;
 		if (params.iterate) {
 			printf("\n");