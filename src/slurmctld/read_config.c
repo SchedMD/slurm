SchedMD/slurm

commit 9210bead73986da784421ef926a7cec0dfbae39f

Move "ALL" keyword handling for partition nodes to expand_nodesets().
The previous logic was setting the partition node_bitmap and nodes twice,
once in the previous ALL logic if statement and then again in the while
loop below.

But the main purpose is to standarize future callers of expand_nodesets()
anticipating all of them will also be sensitive to ALL.

Bug 13099

src/slurmctld/read_config.c
@@ -312,14 +312,20 @@ extern char *expand_nodesets(const char *nodes, char **nodesets)
 	char *ret_nodelist;
 	slurm_conf_nodeset_t *ptr, **ptr_array;
 	hostlist_t hl;
+	node_record_t *node_ptr;
 
 	if (nodesets)
 		xfree(*nodesets);
 
+	if (!xstrcmp(nodes, "ALL")) {
+		hl = hostlist_create(NULL);
+		for (int i = 0; (node_ptr = next_node(&i)); i++)
+			hostlist_push_host(hl, node_ptr->name);
+	} else {
+		hl = hostlist_create(nodes);
+	}
+
 	count = slurm_conf_nodeset_array(&ptr_array);
-
-	hl = hostlist_create(nodes);
-
 	for (int i = 0; i < count; i++) {
 		ptr = ptr_array[i];
 

src/slurmctld/partition_mgr.c
@@ -201,12 +201,6 @@ extern int build_part_bitmap(part_record_t *part_ptr)
 		return 0;
 	}
 
-	if (!xstrcmp(part_ptr->nodes, "ALL")) {
-		bit_nset(part_ptr->node_bitmap, 0, node_record_count - 1);
-		xfree(part_ptr->nodes);
-		part_ptr->nodes = bitmap2node_name(part_ptr->node_bitmap);
-		bit_nclear(part_ptr->node_bitmap, 0, node_record_count - 1);
-	}
 	if ((host_list = hostlist_create(part_ptr->nodes)) == NULL) {
 		FREE_NULL_BITMAP(old_bitmap);
 		error("hostlist_create error on %s, %m",

commit a08c37b7046e9628365a537cb293f0673638d7f4

Fix regression in 22.05.0rc1 that broke Nodes=ALL in a NodeSet
Regression from commit 9210bead73.

Bug 16302

src/slurmctld/read_config.c
@@ -351,8 +351,13 @@ extern hostlist_t nodespec_to_hostlist(const char *nodes,
 			if (ptr->feature)
 				_add_nodes_with_feature(hl, ptr->feature);
 
-			if (ptr->nodes)
+			/* Handle keywords for Nodes= in a NodeSet */
+			if (!xstrcasecmp(ptr->nodes, "ALL")) {
+				for (int i = 0; (node_ptr = next_node(&i)); i++)
+					hostlist_push_host(hl, node_ptr->name);
+			} else if (ptr->nodes) {
 				hostlist_push(hl, ptr->nodes);
+			}
 		}
 	}
 