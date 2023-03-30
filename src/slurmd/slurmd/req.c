SchedMD/slurm

commit c1590de35eb8073f95afe38f09a04cdee020950d

Move use of aliases to later
Bug 15207

Co-authored-by: Dominik Bartkiewicz <bart@schedmd.com>

src/slurmd/slurmd/req.c
@@ -1446,11 +1446,6 @@ _rpc_launch_tasks(slurm_msg_t *msg)
 		goto done;
 	}
 
-	if (_set_node_alias(req)) {
-		errnum = ESLURM_INVALID_NODE_NAME;
-		goto done;
-	}
-
 	slurm_get_ip_str(cli, host, sizeof(host));
 	port = slurm_get_port(cli);
 	if (req->het_job_id && (req->het_job_id != NO_VAL)) {
@@ -1488,6 +1483,11 @@ _rpc_launch_tasks(slurm_msg_t *msg)
 #ifndef HAVE_FRONT_END
 		slurm_mutex_unlock(&prolog_mutex);
 #endif
+		goto done;
+	}
+
+	if (_set_node_alias(req)) {
+		errnum = ESLURM_INVALID_NODE_NAME;
 		goto done;
 	}
 

commit 1b241d26be160df98e46a57d605730e9fcdab37e

Fix deadlock introduced in c1590de35eb
_set_node_alias() was moved to inside the scope of a mutex and didn't
unlock the mutex on error.

Bug 15207

src/slurmd/slurmd/req.c
@@ -1538,6 +1538,7 @@ _rpc_launch_tasks(slurm_msg_t *msg)
 
 	if (_set_node_alias(req)) {
 		errnum = ESLURM_INVALID_NODE_NAME;
+		slurm_mutex_unlock(&prolog_mutex);
 		goto done;
 	}
 