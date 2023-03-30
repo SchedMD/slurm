46493347fdce47ef129effb7f4610368e6d347d0
Brian Christiansen <brian@schedmd.com>
2023-03-30 14:06:42 -0600

    Handle select_jobinfo unpacks from <23.11
    See 74882c9363b and 4d3fbf5084
    
    23.02 doesn't pack select_jobinfo anymore AND in 23.11 the slurmd
    doesn't load in the select plugin in 23.11.
    
    Bug 15973
    
    Co-authored-by: Danny Auble <da@schedmd.com>

--- src/interfaces/select.c
+++ src/interfaces/select.c
@@ -967,6 +967,18 @@ extern int select_g_select_jobinfo_unpack(dynamic_plugin_data_t **jobinfo,
 {
 	dynamic_plugin_data_t *jobinfo_ptr = NULL;
 
+	/*
+	 * Remove 2 versions after 23.02 -- 23.02 doesn't pack jobinfo anymore
+	 * AND the select plugin isn't loaded in the slurmd in 23.11.
+	 */
+	if (running_in_slurmd() &&
+	    (protocol_version <= SLURM_23_02_PROTOCOL_VERSION)) {
+		uint32_t plugin_id;
+		safe_unpack32(&plugin_id, buffer);
+		*jobinfo = NULL;
+		return SLURM_SUCCESS;
+	}
+
 	xassert(select_context_cnt >= 0);
 
 	jobinfo_ptr = xmalloc(sizeof(dynamic_plugin_data_t));
