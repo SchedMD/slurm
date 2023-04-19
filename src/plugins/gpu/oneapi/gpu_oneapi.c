SchedMD/slurm

commit ceae922dc3f6878a7e77af38e93f84c8d6cd2cdf

Use strlcpy().

src/plugins/gpu/oneapi/gpu_oneapi.c
@@ -45,6 +45,7 @@
 #include <ze_api.h>
 #include <zes_api.h>
 
+#include "src/common/strlcpy.h"
 #include "src/plugins/gpu/common/gpu_common.h"
 
 #define MAX_GPU_NUM 256
@@ -805,7 +806,7 @@ static bool _oneapi_read_cpu_affinity_list(const char *file,
 			debug2("tok is :%s", tok);
 			pos = strcspn(tok, "-");
 			if (pos > 0 && pos < strlen(tok)) {
-				strncpy(buf, tok, pos);
+				strlcpy(buf, tok, pos);
 				min_cpu = atoi(buf);
 				strcpy(buf, tok + pos + 1);
 				max_cpu = atoi(buf);

commit 2d8014d976404e453bc127d362cffe5c2e73289d

gpu_oneapi - Fix CPU range parsing
A regression from ceae922dc3 caused the string to be truncated by one
character. Rather than correcting the strlcpy() math, just pivot to
using atoi() directly on the existing strings.

Bug 16547

src/plugins/gpu/oneapi/gpu_oneapi.c
@@ -786,7 +786,6 @@ static bool _oneapi_read_cpu_affinity_list(const char *file,
 					   unsigned int size)
 {
 	char line[CPU_LINE_SIZE] = {'\0'};
-	char buf[CPU_LINE_SIZE] = {'\0'};
 	char *save_ptr = line, *tok = NULL;
 	int min_cpu = -1, max_cpu = -1;
 	FILE *fp = NULL;
@@ -808,10 +807,8 @@ static bool _oneapi_read_cpu_affinity_list(const char *file,
 			debug2("tok is :%s", tok);
 			pos = strcspn(tok, "-");
 			if (pos > 0 && pos < strlen(tok)) {
-				strlcpy(buf, tok, pos);
-				min_cpu = atoi(buf);
-				strlcpy(buf, tok + pos + 1, sizeof(buf));
-				max_cpu = atoi(buf);
+				min_cpu = atoi(tok);
+				max_cpu = atoi(tok + pos + 1);
 			} else if (pos > 0 && pos == strlen(tok)) {
 				max_cpu = min_cpu = atoi(tok);
 			} else {