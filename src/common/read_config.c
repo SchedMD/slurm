SchedMD/slurm

commit dfa1186d990a5a4fe58154aebb1c0a31f1b5e73a

Use node_ptr to get node information instead of names_ll_t
slurmd is the only place that uses this information and we can get it
from the node.

Bug 15489

src/slurmd/slurmd/slurmd.c
@@ -977,20 +977,23 @@ _read_config(void)
 			conf->node_name,
 			conf->hostname);
 
+	node_ptr = find_node_record(conf->node_name);
+	xassert(node_ptr);
+
 	if (conf->dynamic_type == DYN_NODE_NORM)
 		conf->port = cf->slurmd_port;
 	else
-		conf->port = slurm_conf_get_port(conf->node_name);
+		conf->port = node_ptr->port;
 	slurm_conf.slurmd_port = conf->port;
-	slurm_conf_get_cpus_bsct(conf->node_name,
-				 &conf->conf_cpus, &conf->conf_boards,
-				 &conf->conf_sockets, &conf->conf_cores,
-				 &conf->conf_threads);
 
-	slurm_conf_get_res_spec_info(conf->node_name,
-				     &conf->cpu_spec_list,
-				     &conf->core_spec_cnt,
-				     &conf->mem_spec_limit);
+	conf->conf_boards = node_ptr->boards;
+	conf->conf_cores = node_ptr->cores;
+	conf->conf_cpus = node_ptr->cpus;
+	conf->conf_sockets = node_ptr->tot_sockets;
+	conf->conf_threads = node_ptr->threads;
+	conf->core_spec_cnt = node_ptr->core_spec_cnt;
+	conf->cpu_spec_list = xstrdup(node_ptr->cpu_spec_list);
+	conf->mem_spec_limit = node_ptr->mem_spec_limit;
 
 	/* store hardware properties in slurmd_config */
 	xfree(conf->block_map);
@@ -1128,8 +1131,6 @@ _read_config(void)
 	 * memory and the configured memory is needed to setup the slurmd's
 	 * memory cgroup.
 	 */
-	node_ptr = find_node_record(conf->node_name);
-	xassert(node_ptr);
 	conf->conf_memory_size = node_ptr->real_memory;
 
 	get_memory(&conf->physical_memory_size);

commit 6b650b64062c93529ee301cf3c4fee1bc00bea65

Get port for FrontEnd slurmd
Regression from dfa1186d99. A names_ll_t object is created for each
FrontEnd node and slurm_conf_get_port() would get the port.
slurm_conf_get_port() was removed in 657f056c54. We create a function to
get the port from the front_end_list.

Bug 16443

src/common/read_config.c
@@ -6438,3 +6438,28 @@ extern void slurm_conf_remove_node(char *node_name)
 	_internal_conf_remove_node(node_name);
 	slurm_conf_unlock();
 }
+
+#ifdef HAVE_FRONT_END
+extern uint16_t slurm_conf_get_frontend_port(char *node_hostname)
+{
+	uint16_t port = 0;
+	slurm_conf_frontend_t *front_end_ptr = NULL;
+
+	slurm_conf_lock();
+	_init_slurmd_nodehash();
+
+	if (!front_end_list) {
+		debug("front_end_list is NULL");
+	} else if ((front_end_ptr = list_find_first(front_end_list,
+						    _list_find_frontend,
+						    node_hostname))) {
+		if (front_end_ptr->port)
+			port = front_end_ptr->port;
+		else
+			port = conf_ptr->slurmd_port;
+	}
+	slurm_conf_unlock();
+
+	return port;
+}
+#endif

src/common/read_config.h
@@ -728,4 +728,11 @@ extern void slurm_conf_add_node(node_record_t *node_ptr);
  */
 extern void slurm_conf_remove_node(char *node_name);
 
+#ifdef HAVE_FRONT_END
+/*
+ * Return the frontend port for the given hostname.
+ */
+extern uint16_t slurm_conf_get_frontend_port(char *node_hostname);
+#endif
+
 #endif /* !_READ_CONFIG_H */

src/slurmd/slurmd/slurmd.c
@@ -1016,6 +1016,8 @@ _read_config(void)
 	conf->core_spec_cnt = node_ptr->core_spec_cnt;
 	conf->cpu_spec_list = xstrdup(node_ptr->cpu_spec_list);
 	conf->mem_spec_limit = node_ptr->mem_spec_limit;
+#else
+	conf->port = slurm_conf_get_frontend_port(conf->node_name);
 #endif
 
 	/* store hardware properties in slurmd_config */