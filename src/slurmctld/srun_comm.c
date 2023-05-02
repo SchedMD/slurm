SchedMD/slurm

commit 11712ce7099c4281ad9a705bfabda7ec2110ca59

Fix controller being able to contact non-addressable step hosts
Bug 15557

src/slurmctld/srun_comm.c
@@ -213,6 +213,7 @@ static int _srun_node_fail(void *x, void *arg)
 	step_record_t *step_ptr = (step_record_t *) x;
 	srun_node_fail_args_t *args = (srun_node_fail_args_t *) arg;
 	slurm_addr_t *addr;
+	char *nodeaddr;
 	srun_node_fail_msg_t *msg_arg;
 
 	if (!step_ptr->step_node_bitmap)   /* pending step */
@@ -225,8 +226,11 @@ static int _srun_node_fail(void *x, void *arg)
 	if (!step_ptr->port || !step_ptr->host || (step_ptr->host[0] == '\0'))
 		return 0;
 
-	addr = xmalloc(sizeof(*addr));
-	slurm_set_addr(addr, step_ptr->port, step_ptr->host);
+	addr = xmalloc(sizeof(slurm_addr_t));
+	nodeaddr = slurm_conf_get_nodeaddr(step_ptr->host);
+	slurm_set_addr(addr, step_ptr->port, nodeaddr);
+	xfree(nodeaddr);
+
 	msg_arg = xmalloc(sizeof(*msg_arg));
 	memcpy(&msg_arg->step_id, &step_ptr->step_id, sizeof(msg_arg->step_id));
 	msg_arg->nodelist = xstrdup(args->node_name);
@@ -329,6 +333,7 @@ static int _srun_step_timeout(void *x, void *arg)
 {
 	step_record_t *step_ptr = (step_record_t *) x;
 	slurm_addr_t *addr;
+	char *nodeaddr;
 	srun_timeout_msg_t *msg_arg;
 
 	xassert(step_ptr);
@@ -342,7 +347,11 @@ static int _srun_step_timeout(void *x, void *arg)
 	addr = xmalloc(sizeof(*addr));
 	msg_arg = xmalloc(sizeof(*msg_arg));
 
-	slurm_set_addr(addr, step_ptr->port, step_ptr->host);
+	addr = xmalloc(sizeof(slurm_addr_t));
+	nodeaddr = slurm_conf_get_nodeaddr(step_ptr->host);
+	slurm_set_addr(addr, step_ptr->port, nodeaddr);
+	xfree(nodeaddr);
+
 	memcpy(&msg_arg->step_id, &step_ptr->step_id, sizeof(msg_arg->step_id));
 	msg_arg->timeout = step_ptr->job_ptr->end_time;
 
@@ -528,8 +537,12 @@ extern void srun_step_complete(step_record_t *step_ptr)
 
 	xassert(step_ptr);
 	if (step_ptr->port && step_ptr->host && step_ptr->host[0]) {
+		char *nodeaddr;
 		addr = xmalloc(sizeof(slurm_addr_t));
-		slurm_set_addr(addr, step_ptr->port, step_ptr->host);
+		nodeaddr = slurm_conf_get_nodeaddr(step_ptr->host);
+		slurm_set_addr(addr, step_ptr->port, nodeaddr);
+		xfree(nodeaddr);
+
 		msg_arg = xmalloc(sizeof(srun_job_complete_msg_t));
 		memcpy(&msg_arg->step_id, &step_ptr->step_id,
 		       sizeof(msg_arg->step_id));
@@ -552,8 +565,12 @@ extern void srun_step_missing(step_record_t *step_ptr, char *node_list)
 
 	xassert(step_ptr);
 	if (step_ptr->port && step_ptr->host && step_ptr->host[0]) {
+		char *nodeaddr;
 		addr = xmalloc(sizeof(slurm_addr_t));
-		slurm_set_addr(addr, step_ptr->port, step_ptr->host);
+		nodeaddr = slurm_conf_get_nodeaddr(step_ptr->host);
+		slurm_set_addr(addr, step_ptr->port, nodeaddr);
+		xfree(nodeaddr);
+
 		msg_arg = xmalloc(sizeof(srun_step_missing_msg_t));
 		memcpy(&msg_arg->step_id, &step_ptr->step_id,
 		       sizeof(msg_arg->step_id));
@@ -577,8 +594,12 @@ extern void srun_step_signal(step_record_t *step_ptr, uint16_t signal)
 
 	xassert(step_ptr);
 	if (step_ptr->port && step_ptr->host && step_ptr->host[0]) {
+		char *nodeaddr;
 		addr = xmalloc(sizeof(slurm_addr_t));
-		slurm_set_addr(addr, step_ptr->port, step_ptr->host);
+		nodeaddr = slurm_conf_get_nodeaddr(step_ptr->host);
+		slurm_set_addr(addr, step_ptr->port, nodeaddr);
+		xfree(nodeaddr);
+
 		msg_arg = xmalloc(sizeof(job_step_kill_msg_t));
 		memcpy(&msg_arg->step_id, &step_ptr->step_id,
 		       sizeof(msg_arg->step_id));

commit a5fa2ad707b41490566a663215e75cb665bde831

Fix communication with srun running outside of a Slurm node.
Regression in 11712ce709.

If we fail to find the node in the configuration we should fallback to
using the raw host value we have in step_ptr.

This may happen if the configured NodeHostname differs from the
gethostname() result on the host, but matches one of the aliases.

Another common case is for srun running on a submit host that is not
configured as a Slurm node.

Bug 16401

src/slurmctld/srun_comm.c
@@ -213,9 +213,11 @@ slurm_addr_t *_srun_set_addr(step_record_t *step_ptr)
 	char *nodeaddr;
 	slurm_addr_t *addr = xmalloc(sizeof(*addr));
 
-	nodeaddr = slurm_conf_get_nodeaddr(step_ptr->host);
-	slurm_set_addr(addr, step_ptr->port, nodeaddr);
-	xfree(nodeaddr);
+	if ((nodeaddr = slurm_conf_get_nodeaddr(step_ptr->host))) {
+		slurm_set_addr(addr, step_ptr->port, nodeaddr);
+		xfree(nodeaddr);
+	} else
+		slurm_set_addr(addr, step_ptr->port, step_ptr->host);
 
 	return addr;
 }