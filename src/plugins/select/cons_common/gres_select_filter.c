SchedMD/slurm

commit 10322fa24670c6100ed39b3ba8305c23db83d459

Attempt equal placing of cpus on GRES in first_pass
If we're in first_pass and the request is for -n --gpus, we can attempt
using same number of cpus per GRES for optimal task placement.

This makes the following algorithm aware of number of cpus potentially
required for task on socket to reduce number of GPUs used per socket if
we know that we won't be able to get enough CPUs on the socket to use
higher number of GPUs on the socket.

Bug 15519

src/plugins/select/cons_common/gres_select_filter.c
@@ -555,6 +555,21 @@ extern void gres_select_filter_sock_core(gres_mc_data_t *mc_ptr,
 			 (gres_js->ntasks_per_gres != NO_VAL16)) {
 			cpus_per_gres = gres_js->ntasks_per_gres *
 				mc_ptr->cpus_per_task;
+		} else if (first_pass && mc_ptr->ntasks_per_job &&
+			   (mc_ptr->ntasks_per_job != NO_VAL) &&
+			   gres_js->gres_per_job) {
+			uint64_t tasks_per_gres = mc_ptr->ntasks_per_job;
+			tasks_per_gres +=  gres_js->gres_per_job - 1;
+			tasks_per_gres /= gres_js->gres_per_job;
+			cpus_per_gres = tasks_per_gres * mc_ptr->cpus_per_task;
+			/*
+			 * We use round-up division here in case of requests
+			 * like -n3 --gpus=2, where coming up with 3/2=1 CPU
+			 * per GRES results in underestimation and may lead to
+			 * too many GRES selected from the socket. If we won't
+			 * allocate because of overestimate this won't be an
+			 * issue since it's only in first_pass.
+			 */
 		} else if (gres_js->def_cpus_per_gres) {
 			cpus_per_gres = gres_js->def_cpus_per_gres;
 			has_cpus_per_gres = true;

commit f6e23f78462b19fd6a3366f9097dc00731e190ae

Fix ignoring DefCpuPerGPU on the first pass
This was a regression from commit 10322fa2467.
If DefCpuPerGPU is defined, make that take priority over calculating
cpus_per_gres on the first pass.

Bug 16662

src/plugins/select/cons_common/gres_select_filter.c
@@ -555,6 +555,9 @@ extern void gres_select_filter_sock_core(gres_mc_data_t *mc_ptr,
 			 (gres_js->ntasks_per_gres != NO_VAL16)) {
 			cpus_per_gres = gres_js->ntasks_per_gres *
 				mc_ptr->cpus_per_task;
+		} else if (gres_js->def_cpus_per_gres) {
+			cpus_per_gres = gres_js->def_cpus_per_gres;
+			has_cpus_per_gres = true;
 		} else if (first_pass && mc_ptr->ntasks_per_job &&
 			   (mc_ptr->ntasks_per_job != NO_VAL) &&
 			   gres_js->gres_per_job) {
@@ -570,9 +573,6 @@ extern void gres_select_filter_sock_core(gres_mc_data_t *mc_ptr,
 			 * allocate because of overestimate this won't be an
 			 * issue since it's only in first_pass.
 			 */
-		} else if (gres_js->def_cpus_per_gres) {
-			cpus_per_gres = gres_js->def_cpus_per_gres;
-			has_cpus_per_gres = true;
 		}
 
 		/* Filter out unusable GRES by socket */