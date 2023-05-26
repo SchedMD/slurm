SchedMD/slurm

commit 0cbce00e23e16bb98e2092905632e88e8dd1345f

Fix regression that set SLURM_NTASKS for sbatch when it should not be
Regression in 23.02.2 in commit 6570a35388.

Bug 16666

src/common/env.c
@@ -1221,6 +1221,7 @@ extern int
 env_array_for_batch_job(char ***dest, const batch_job_launch_msg_t *batch,
 			const char *node_name)
 {
+	bool ntasks_set = batch->ntasks ? true : false;
 	char *tmp = NULL;
 	int i;
 	slurm_step_layout_t *step_layout = NULL;
@@ -1313,7 +1314,7 @@ env_array_for_batch_job(char ***dest, const batch_job_launch_msg_t *batch,
 		env_array_overwrite_fmt(dest, "SLURM_CPUS_PER_TASK", "%u",
 					cpus_per_task);
 
-	if (step_layout_req.num_tasks) {
+	if (ntasks_set) {
 		env_array_overwrite_fmt(dest, "SLURM_NTASKS", "%u",
 					step_layout_req.num_tasks);
 		/* keep around for old scripts */

commit 94956a8119a9ae6f7a9a85df90a90150f66206e9

Fix SLURM_TASKS_PER_NODE being calculated wrong
This is a regression from commit 0cbce00e23e that broke commit
6570a35388.

Bug 16835

Signed-off-by: Felip Moll <felip.moll@schedmd.com>

src/common/env.c
@@ -1321,8 +1321,10 @@ env_array_for_batch_job(char ***dest, const batch_job_launch_msg_t *batch,
 		/* keep around for old scripts */
 		env_array_overwrite_fmt(dest, "SLURM_NPROCS", "%u",
 					step_layout_req.num_tasks);
-	} else {
+	} else if (!step_layout_req.num_tasks) {
 		/*
+		 * Figure out num_tasks if it was not set by either
+		 * batch->ntasks or SLURM_NTASKS_PER_NODE above
 		 * Iterate over all kind of cluster nodes, and accum. the number
 		 * of tasks all the group can hold.
 		 */