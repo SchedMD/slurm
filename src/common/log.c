SchedMD/slurm

commit cf49eb9ac125178d14f91478b04facb596726a10

Add new warning() logging command.
Will print at the info level, but with a prefix of "warning: ".
For syslog use, logs at the LOG_WARNING level.

src/common/log.c
@@ -93,7 +93,7 @@
 	    (sched && (level <= highest_sched_log_level))) {	\
 		va_list ap;					\
 		va_start(ap, fmt);				\
-		_log_msg(level, sched, false, fmt, ap);		\
+		_log_msg(level, sched, false, false, fmt, ap);	\
 		va_end(ap);					\
 	}							\
 }
@@ -1183,7 +1183,8 @@ static void _log_printf(log_t *log, cbuf_t *cb, FILE *stream,
  * log a message at the specified level to facilities that have been
  * configured to receive messages at that level
  */
-static void _log_msg(log_level_t level, bool sched, bool spank, const char *fmt, va_list args)
+static void _log_msg(log_level_t level, bool sched, bool spank, bool warn,
+		     const char *fmt, va_list args)
 {
 	char *pfx = "";
 	char *buf = NULL;
@@ -1225,15 +1226,15 @@ static void _log_msg(log_level_t level, bool sched, bool spank, const char *fmt,
 			break;
 
 		case LOG_LEVEL_ERROR:
-			priority = LOG_ERR;
 			pfx = sched? "error: sched: " : "error: ";
 			pfx = spank ? "" : pfx;
 			break;
 
 		case LOG_LEVEL_INFO:
 		case LOG_LEVEL_VERBOSE:
-			priority = LOG_INFO;
+			priority = warn ? LOG_WARNING : LOG_INFO;
 			pfx = sched ? "sched: " : "";
+			pfx = warn ? "warning: " : pfx;
 			break;
 
 		case LOG_LEVEL_DEBUG:
@@ -1447,7 +1448,15 @@ void spank_log(const char *fmt, ...)
 {
 	va_list ap;
 	va_start(ap, fmt);
-	_log_msg(LOG_LEVEL_ERROR, false, true, fmt, ap);
+	_log_msg(LOG_LEVEL_ERROR, false, true, false, fmt, ap);
+	va_end(ap);
+}
+
+void warning(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	_log_msg(LOG_LEVEL_INFO, false, false, true, fmt, ap);
 	va_end(ap);
 }
 

src/common/log.h
@@ -308,6 +308,7 @@ extern void fatal_abort(const char *, ...)
 extern void fatal(const char *, ...)
 	__attribute__((format (printf, 1, 2))) __attribute__((noreturn));
 int	error(const char *, ...) __attribute__ ((format (printf, 1, 2)));
+void	warning(const char *, ...) __attribute__ ((format (printf, 1, 2)));
 void	slurm_info(const char *, ...) __attribute__ ((format (printf, 1, 2)));
 void	slurm_verbose(const char *, ...) __attribute__ ((format (printf, 1, 2)));
 

commit d9aebfda4375317787a1f7485f7968b4c41643ca

Set syslog priority to LOG_ERR for error() messages.
Regression in commit cf49eb9ac1.

Bug 16699.

src/common/log.c
@@ -1226,6 +1226,7 @@ static void _log_msg(log_level_t level, bool sched, bool spank, bool warn,
 			break;
 
 		case LOG_LEVEL_ERROR:
+			priority = LOG_ERR;
 			pfx = sched? "error: sched: " : "error: ";
 			pfx = spank ? "" : pfx;
 			break;