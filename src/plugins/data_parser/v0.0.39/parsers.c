31fa0d65242033bd7c4c3c8a7bf0d7c0dd3a653f
Nathan Rini <nate@schedmd.com>
2023-06-15 21:09:27 -0600

    data_parser/v0.0.39 - handle UINT64_NO_VAL with JOB_MEM_PER_NODE parser
    JOB_MEM_PER_NODE becomes a UINT64_NO_VAL type which requires the parser
    to attempt that format.
    
    Bug 16981

--- src/plugins/data_parser/v0.0.39/parsers.c
+++ src/plugins/data_parser/v0.0.39/parsers.c
@@ -3291,7 +3291,8 @@ static int PARSE_FUNC(JOB_MEM_PER_NODE)(const parser_t *const parser, void *obj,
 		return SLURM_SUCCESS;
 	}
 
-	if (data_get_type(src) == DATA_TYPE_INT_64) {
+	if ((data_get_type(src) == DATA_TYPE_INT_64) ||
+	    (data_get_type(src) == DATA_TYPE_DICT)) {
 		if ((rc = PARSE(UINT64_NO_VAL, node_mem, src, parent_path,
 				args))) {
 			/* error already logged */
