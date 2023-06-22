2401066170dbbb5ddc43a51116be9cf24acd55e3
Tim Wickberg <tim@schedmd.com>
2023-06-22 15:16:49 -0600

    Revert "data_t - Improve data type string descriptions"
    This reverts commit af92b5bdc961eab718f6cfa1da41a70968f2692b.

--- src/common/data.c
+++ src/common/data.c
@@ -2160,11 +2160,11 @@ extern char *data_type_to_string(data_type_t type)
 		case DATA_TYPE_DICT:
 			return "dictionary";
 		case DATA_TYPE_INT_64:
-			return "64 bit integer";
+			return "int 64bits";
 		case DATA_TYPE_STRING:
 			return "string";
 		case DATA_TYPE_FLOAT:
-			return "floating point number";
+			return "floating point";
 		case DATA_TYPE_BOOL:
 			return "boolean";
 		default:
