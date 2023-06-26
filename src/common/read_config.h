2f301e79589e1d5470527a857652ecc75366c824
Benjamin Witham <benjamin.witham@schedmd.com>
2023-06-26 11:01:14 -0600

    Change default value of PriorityType to "priority/multifactor".
    Bug 16949

--- src/common/read_config.h
+++ src/common/read_config.h
@@ -129,7 +129,7 @@ typedef struct node_record node_record_t;
 #define DEFAULT_PREP_PLUGINS        "prep/script"
 #define DEFAULT_PRIORITY_DECAY      604800 /* 7 days */
 #define DEFAULT_PRIORITY_CALC_PERIOD 300 /* in seconds */
-#define DEFAULT_PRIORITY_TYPE       "priority/basic"
+#define DEFAULT_PRIORITY_TYPE       "priority/multifactor"
 #define DEFAULT_RECONF_KEEP_PART_STATE 0
 #define DEFAULT_RETURN_TO_SERVICE   0
 #define DEFAULT_RESUME_RATE         300
