SchedMD/slurm

commit 202aa181fdf938a4dd1870310ee586dbaf8dca0a

Move build_feature_list_[eq|ne]() to earlier
build_feature_list_[eq_ne] needs to be called before partition bitmaps
are set (_build_part_bitmaps()/build_part_bitmap()) because partitions
using nodesets with features might reference dynamic nodes that could
have been reordered by a reconfig. If dynamic nodes had features and
were reordered, then nodespec_to_hostlist()->_add_nodes_with_feature()
had an out of date avail_feature_list with out of date node_feature_t
bitmaps.

_build_node_config_bitmaps() needs to be called before
build_feature_list_[eq_ne]() because they rely on the
config_ptr->node_bitmaps.

Bug 16295

Signed-off-by: Marshall Garey <marshall@schedmd.com>

src/slurmctld/read_config.c
@@ -1805,9 +1805,34 @@ int read_slurm_conf(int recover, bool reconfig)
 		load_job_ret = load_all_job_state();
 	}
 
-	_sync_part_prio();
+	/* NOTE: Run restore_node_features before _restore_job_accounting */
+	restore_node_features(recover);
+
+	if ((node_features_g_count() > 0) &&
+	    (node_features_g_get_node(NULL) != SLURM_SUCCESS)) {
+		error("failed to initialize node features");
+		test_config_rc = 1;
+	}
+
+	/*
+	 * _build_bitmaps() must follow node_features_g_get_node() and
+	 * precede build_features_list_*()
+	 */
+	_build_bitmaps();
+	/*
+	 * _build_node_config_bitmaps() must be called before
+	 * build_features_list_*()
+	 */
 	_build_node_config_bitmaps();
-	_build_part_bitmaps();
+
+	/* Active and available features can be different on -R */
+	if ((node_features_g_count() == 0) && (recover != 2))
+		build_feature_list_eq();
+	else
+		build_feature_list_ne();
+
+	_sync_part_prio();
+	_build_part_bitmaps(); /* Must be called after build_feature_list_*() */
 
 	if ((select_g_node_init() != SLURM_SUCCESS) ||
 	    (select_g_state_restore(state_save_dir) != SLURM_SUCCESS) ||
@@ -1862,27 +1887,6 @@ int read_slurm_conf(int recover, bool reconfig)
 
 	init_requeue_policy();
 	init_depend_policy();
-
-	/* NOTE: Run restore_node_features before _restore_job_accounting */
-	restore_node_features(recover);
-
-	if ((node_features_g_count() > 0) &&
-	    (node_features_g_get_node(NULL) != SLURM_SUCCESS)) {
-		error("failed to initialize node features");
-		test_config_rc = 1;
-	}
-
-	/*
-	 * _build_bitmaps() must follow node_features_g_get_node() and
-	 * preceed build_features_list_*()
-	 */
-	_build_bitmaps();
-
-	/* Active and available features can be different on -R */
-	if ((node_features_g_count() == 0) && (recover != 2))
-		build_feature_list_eq();
-	else
-		build_feature_list_ne();
 
 	/*
 	 * Must be at after nodes and partitons (e.g.

commit 46eab0dfdc5c33ce70a1a6c0dc6afec5cda41aec

Load node gres before verifying gres state
This was a regression from commit 202aa181fd which reordered loading some
node and partition state in read_slurm_conf.

Gres should be loaded with _gres_reconfig before
verifying if the new gres state matches the original gres state in
restore_node_features -> gres_node_reconfig.

Bug 16894

src/slurmctld/read_config.c
@@ -1817,6 +1817,8 @@ int read_slurm_conf(int recover, bool reconfig)
 		load_job_ret = load_all_job_state();
 	}
 
+	/* _gres_reconfig needs to happen before restore_node_features */
+	_gres_reconfig(reconfig);
 	/* NOTE: Run restore_node_features before _restore_job_accounting */
 	restore_node_features(recover);
 
@@ -1864,8 +1866,6 @@ int read_slurm_conf(int recover, bool reconfig)
 	 */
 	config_power_mgr();
 
-
-	_gres_reconfig(reconfig);
 	_sync_jobs_to_conf();		/* must follow select_g_job_init() */
 
 	/*