d1a96bec19bc97dc37b2d7d706104b7ac4e3e81c
Chad Vizino <chad@schedmd.com>
2023-07-12 16:29:37 -0600

    slurmctld - clear job's total_nodes field when requeuing
    batch_requeue_fini() was not clearing a requeued job's total_nodes field. Fix
    that.
    
    sacct now correctly shows that such a job has 0 AllocNodes while it is
    pending or if it is canceled before restarting.
    
    Bug 17092

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -16481,6 +16481,7 @@ void batch_requeue_fini(job_record_t *job_ptr)
 	job_ptr->state_reason_prev_db = 0;
 
 	job_ptr->node_cnt = 0;
+	job_ptr->total_nodes = 0;
 	xfree(job_ptr->alias_list);
 	xfree(job_ptr->batch_host);
 	free_job_resources(&job_ptr->job_resrcs);
