SchedMD/slurm

commit fb77d97e8ad6c4e3c27a857919209a1b03f252c8

Allow changing ArrayTaskThrottle for completed jobs
This is useful for job requeue.
bug 5693

src/slurmctld/job_mgr.c
@@ -11486,6 +11486,26 @@ static int _update_job(struct job_record *job_ptr, job_desc_msg_t * job_specs,
 	if (error_code != SLURM_SUCCESS)
 		goto fini;
 
+	if (job_specs->array_inx && job_ptr->array_recs) {
+		int throttle;
+		throttle = strtoll(job_specs->array_inx, (char **) NULL, 10);
+		if (throttle >= 0) {
+			info("%s: set max_run_tasks to %d for job array %pJ",
+			     __func__, throttle, job_ptr);
+			job_ptr->array_recs->max_run_tasks = throttle;
+		} else {
+			info("%s: invalid max_run_tasks of %d for job array %pJ, ignored",
+			     __func__, throttle, job_ptr);
+			error_code = ESLURM_BAD_TASK_COUNT;
+		}
+		/*
+		 * Even if the job is complete, permit changing
+		 * ArrayTaskThrottle for other elements of the task array
+		 */
+		if (IS_JOB_FINISHED(job_ptr))
+			goto fini;
+	}
+
 	if (IS_JOB_FINISHED(job_ptr)) {
 		error_code = ESLURM_JOB_FINISHED;
 		goto fini;
@@ -13078,20 +13098,6 @@ static int _update_job(struct job_record *job_ptr, job_desc_msg_t * job_specs,
 		gres_build_job_details(job_ptr->gres_list,
 				       &job_ptr->gres_detail_cnt,
 				       &job_ptr->gres_detail_str);
-	}
-
-	if (job_specs->array_inx && job_ptr->array_recs) {
-		int throttle;
-		throttle = strtoll(job_specs->array_inx, (char **) NULL, 10);
-		if (throttle >= 0) {
-			info("update_job: set max_run_tasks to %d for job array %pJ",
-			     throttle, job_ptr);
-			job_ptr->array_recs->max_run_tasks = throttle;
-		} else {
-			info("update_job: invalid max_run_tasks of %d for job array %pJ, ignored",
-			     throttle, job_ptr);
-			error_code = ESLURM_BAD_TASK_COUNT;
-		}
 	}
 
 	if (job_specs->ntasks_per_node != NO_VAL16) {

commit 442c8442d817551c26e75f25e58bfc8d02552372

Reject job ArrayTaskThrottle update requests from unprivileged users.
Regression from fb77d97e8ad (Slurm 19.05pre1). 6981c318e40 blocked the case
of update for 'FINISHED' array job. It was reverted by fb77d97e8ad,
unfortunately allowing this parameter updates by other users too.

This commit will change the error returned - in case of attempt to update a
finished job the user doesn't have rights to we'll return
ESLURM_USER_ID_MISSING instead of ESLURM_JOB_FINISHED.

Bug 17156

src/slurmctld/job_mgr.c
@@ -12496,6 +12496,26 @@ static int _update_job(job_record_t *job_ptr, job_desc_msg_t *job_desc,
 		return ESLURM_JOB_SETTING_DB_INX;
 
 	operator = validate_operator(uid);
+
+	/* Check authorization for modifying this job */
+	is_coord_oldacc = assoc_mgr_is_user_acct_coord(acct_db_conn, uid,
+						       job_ptr->account);
+	is_coord_newacc = assoc_mgr_is_user_acct_coord(acct_db_conn, uid,
+						       job_desc->account);
+	if ((job_ptr->user_id != uid) && !operator) {
+		/*
+		 * Fail if we are not coordinators of the current account or
+		 * if we are changing an account and  we are not coordinators
+		 * of both src and dest accounts.
+		 */
+		if (!is_coord_oldacc ||
+		    (!is_coord_newacc && job_desc->account)) {
+			error("Security violation, JOB_UPDATE RPC from uid %u",
+			      uid);
+			return ESLURM_USER_ID_MISSING;
+		}
+	}
+
 	if (job_desc->burst_buffer) {
 		/*
 		 * burst_buffer contents are validated at job submit time and
@@ -12579,25 +12599,6 @@ static int _update_job(job_record_t *job_ptr, job_desc_msg_t *job_desc,
 		acct_policy_limit_set.qos = ADMIN_SET_LIMIT;
 	} else
 		memset(tres, 0, sizeof(tres));
-
-	/* Check authorization for modifying this job */
-	is_coord_oldacc = assoc_mgr_is_user_acct_coord(acct_db_conn, uid,
-						       job_ptr->account);
-	is_coord_newacc = assoc_mgr_is_user_acct_coord(acct_db_conn, uid,
-						       job_desc->account);
-	if ((job_ptr->user_id != uid) && !operator) {
-		/*
-		 * Fail if we are not coordinators of the current account or
-		 * if we are changing an account and  we are not coordinators
-		 * of both src and dest accounts.
-		 */
-		if (!is_coord_oldacc ||
-		    (!is_coord_newacc && job_desc->account)) {
-			error("Security violation, JOB_UPDATE RPC from uid %u",
-			      uid);
-			return ESLURM_USER_ID_MISSING;
-		}
-	}
 
 	detail_ptr = job_ptr->details;
 	if (detail_ptr)