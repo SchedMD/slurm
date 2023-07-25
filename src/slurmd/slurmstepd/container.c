SchedMD/slurm

commit 5bbd1007062d9f09181e9d763cbc6f6d9957ab51

Gracefully handle NULL task in _generate_spooldir()
Trim path at first taskid replacement in _generate_spooldir() when task is
NULL. This avoids having to swap in a placeholder value for taskid when the
task is not specified.

Bug 16316

src/slurmd/slurmstepd/container.c
@@ -571,6 +571,25 @@ static int _generate_container_paths(stepd_step_rec_t *step)
 	return rc;
 }
 
+static bool _pattern_has_taskid(const char *pattern)
+{
+	const char *p = pattern;
+
+	while (*p) {
+		if (!(p = xstrchr(p, '%')))
+			break;
+
+		if ((p[1] == '%') && (p[2] != '\0'))
+			p += 2;
+		else if (p[1] == 't')
+			return true;
+		else
+			p++;
+	}
+
+	return false;
+}
+
 static char *_generate_spooldir(stepd_step_rec_t *step,
 				stepd_step_task_info_t *task)
 {
@@ -590,9 +609,44 @@ static char *_generate_spooldir(stepd_step_rec_t *step,
 	if (task) {
 		id = task->id;
 		argv = task->argv;
+	} else {
+		char *start, *end, *next;
+
+		/* trim pattern at first taskid replacement */
+
+		if (pattern[0] == '/')
+			next = pattern + 1;
+		else
+			next = pattern;
+
+		while (next) {
+			char term;
+
+			start = next;
+
+			if (!(end = xstrchr(next, '/'))) {
+				end = start + strlen(start);
+				next = NULL;
+			} else {
+				next = end + 1;
+			}
+
+			term = end[1];
+			end[1] = '\0';
+
+			if (_pattern_has_taskid(start)) {
+				/* cut pattern at this directory */
+				*start = '\0';
+				break;
+			}
+
+			end[1] = term;
+		}
 	}
 
+	xassert((id != -1) || !xstrstr(pattern, "%t"));
 	path = _generate_pattern(pattern, step, id, argv);
+	debug3("%s: task:%d pattern:%s path:%s", __func__, id, pattern, path);
 
 	return path;
 }

commit 24955591218a8f479e8c4e266925eedaa1ccac2f

slurmstepd - Correct off by one in _generate_spooldir()
end pointer needs to be used instead of end[1].

Regression from 5bbd100706.

Bug 17272

src/slurmd/slurmstepd/container.c
@@ -631,8 +631,8 @@ static char *_generate_spooldir(stepd_step_rec_t *step,
 				next = end + 1;
 			}
 
-			term = end[1];
-			end[1] = '\0';
+			term = *end;
+			*end = '\0';
 
 			if (_pattern_has_taskid(start)) {
 				/* cut pattern at this directory */
@@ -640,7 +640,7 @@ static char *_generate_spooldir(stepd_step_rec_t *step,
 				break;
 			}
 
-			end[1] = term;
+			*end = term;
 		}
 	}
 