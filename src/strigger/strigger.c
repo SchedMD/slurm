e928af65fe84f22d86597e919b5916c4aabb10f2
Tim Wickberg <tim@schedmd.com>
2023-08-16 11:30:31 -0600

    Use uid_to_string_or_null().

--- src/strigger/strigger.c
+++ src/strigger/strigger.c
@@ -53,6 +53,7 @@
 #include "slurm/slurm.h"
 
 #include "src/common/read_config.h"
+#include "src/common/uid.h"
 #include "src/common/xmalloc.h"
 #include "src/common/xstring.h"
 #include "src/strigger/strigger.h"
@@ -229,6 +230,7 @@ static int _get_trigger(void)
 {
 	trigger_info_msg_t * trig_msg;
 	int line_no = 0, i;
+	char *user;
 
 	if (slurm_get_triggers(&trig_msg)) {
 		slurm_perror("slurm_get_triggers");
@@ -422,15 +424,17 @@ static int _get_trigger(void)
 		}
 		line_no++;
 
+		user = _trig_user(trig_msg->trigger_array[i].user_id);
 		printf("%7u %-9s %7s %-35s %6d %-8s %-5s %s\n",
 			trig_msg->trigger_array[i].trig_id,
 			trigger_res_type(trig_msg->trigger_array[i].res_type),
 			trig_msg->trigger_array[i].res_id,
 			trigger_type(trig_msg->trigger_array[i].trig_type),
 			_trig_offset(trig_msg->trigger_array[i].offset),
-			_trig_user(trig_msg->trigger_array[i].user_id),
+			user,
 			_trig_flags(trig_msg->trigger_array[i].flags),
 			trig_msg->trigger_array[i].program);
+		xfree(user);
 	}
 
 	slurm_free_trigger_msg(trig_msg);
@@ -454,11 +458,10 @@ static int _trig_offset(uint16_t offset)
 
 static char *_trig_user(uint32_t user_id)
 {
-	uid_t uid = user_id;
-	struct passwd *pw;
+	char *user = uid_to_string_or_null(user_id);
 
-	pw = getpwuid(uid);
-	if (pw == NULL)
-		return "unknown";
-	return pw->pw_name;
+	if (!user)
+		user = xstrdup("unknown");
+
+	return user;
 }
