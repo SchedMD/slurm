1110e8d726ee9de50d15820404056dd6be1097e4
Nathan Rini <nate@schedmd.com>
2023-08-28 17:22:18 -0600

    openapi/slurmdbd - Handle usage times in _op_handler_associations()
    Bug 16960

--- src/slurmrestd/plugins/openapi/slurmdbd/associations.c
+++ src/slurmrestd/plugins/openapi/slurmdbd/associations.c
@@ -377,6 +377,11 @@ static int _op_handler_associations(ctxt_t *ctxt)
 		       ctxt->query, ctxt->parent_path))
 		goto cleanup;
 
+	if (assoc_cond->usage_start && !assoc_cond->usage_end)
+		assoc_cond->usage_end = time(NULL);
+	else if (assoc_cond->usage_start > assoc_cond->usage_end)
+		SWAP(assoc_cond->usage_start, assoc_cond->usage_end);
+
 	if (ctxt->method == HTTP_REQUEST_GET)
 		_dump_assoc_cond(ctxt, assoc_cond, false);
 	else if (ctxt->method == HTTP_REQUEST_POST)
