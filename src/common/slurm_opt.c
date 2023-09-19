SchedMD/slurm

commit 40a3bf37127f85f00581bfc5ff8dcac9fef74118

Delay slurm_verify_cpu_bind after cli_filter_g_pre_submit
This allows access to the value set by --cpu-bind in cli_filter plugins,
if --cpu-bind is set in cli_filter_g_pre_submit then cpu_bind_type
should be set based on the modified value.

Bug 13554

src/common/slurm_opt.c
@@ -1090,43 +1090,7 @@ static slurm_cli_opt_t slurm_opt_cores_per_socket = {
 	.reset_each_pass = true,
 };
 
-static int arg_set_cpu_bind(slurm_opt_t *opt, const char *arg)
-{
-	if (!opt->srun_opt)
-		return SLURM_ERROR;
-
-	if (slurm_verify_cpu_bind(arg, &opt->srun_opt->cpu_bind,
-				  &opt->srun_opt->cpu_bind_type))
-		return SLURM_ERROR;
-
-	return SLURM_SUCCESS;
-}
-static char *arg_get_cpu_bind(slurm_opt_t *opt)
-{
-	char tmp[100];
-
-	if (!opt->srun_opt)
-		return xstrdup("invalid-context");
-
-	slurm_sprint_cpu_bind_type(tmp, opt->srun_opt->cpu_bind_type);
-
-	return xstrdup(tmp);
-}
-static void arg_reset_cpu_bind(slurm_opt_t *opt)
-{
-	if (opt->srun_opt) {
-		bool cpu_bind_verbose = false;
-		if (opt->srun_opt->cpu_bind_type & CPU_BIND_VERBOSE)
-			cpu_bind_verbose = true;
-
-		xfree(opt->srun_opt->cpu_bind);
-		opt->srun_opt->cpu_bind_type = 0;
-		if (cpu_bind_verbose)
-			slurm_verify_cpu_bind("verbose",
-					      &opt->srun_opt->cpu_bind,
-					      &opt->srun_opt->cpu_bind_type);
-	}
-}
+COMMON_SRUN_STRING_OPTION(cpu_bind);
 static slurm_cli_opt_t slurm_opt_cpu_bind = {
 	.name = "cpu-bind",
 	.has_arg = required_argument,
@@ -5811,7 +5775,7 @@ static void _validate_threads_per_core_option(slurm_opt_t *opt)
 					      &opt->srun_opt->cpu_bind,
 					      &opt->srun_opt->cpu_bind_type);
 	} else if (opt->srun_opt &&
-		   (opt->srun_opt->cpu_bind_type == CPU_BIND_VERBOSE)) {
+		   !xstrcmp(opt->srun_opt->cpu_bind, "verbose")) {
 		verbose("Setting --cpu-bind=threads,verbose as a default of --threads-per-core use");
 		if (opt->srun_opt)
 			slurm_verify_cpu_bind("threads,verbose",

src/srun/libsrun/opt.c
@@ -965,6 +965,14 @@ static bool _opt_verify(void)
 			opt.nodes_set = false;
 	}
 
+	/* slurm_verify_cpu_bind has to be called before validate_hint_option */
+	if (opt.srun_opt->cpu_bind) {
+		if (slurm_verify_cpu_bind(opt.srun_opt->cpu_bind,
+					  &opt.srun_opt->cpu_bind,
+					  &opt.srun_opt->cpu_bind_type))
+			verified = false;
+	}
+
 	if (opt.hint &&
 	    !validate_hint_option(&opt)) {
 		xassert(opt.ntasks_per_core == NO_VAL);

commit 76716bd80fe2c60f56ef3780738311c677cb2996

srun cpu-bind is consistent when used with threads-per-core.
Srun cpu-bind option would return different cpu-binds if the cpu-binds were
"=v" and "=verbose" even though these should be the same. The case for the
"=v" option is added to the if statement. The CPU_BIND_VERBOSE flag doesn't work
becasue the flags are not set until after the _validate_threads_per_core_option
function has run.

This fixes a regression from commit 40a3bf3712.

Bug 17571

src/common/slurm_opt.c
@@ -5910,7 +5910,8 @@ static void _validate_threads_per_core_option(slurm_opt_t *opt)
 					      &opt->srun_opt->cpu_bind,
 					      &opt->srun_opt->cpu_bind_type);
 	} else if (opt->srun_opt &&
-		   !xstrcmp(opt->srun_opt->cpu_bind, "verbose")) {
+		  (!xstrcasecmp(opt->srun_opt->cpu_bind, "verbose") ||
+		   !xstrcasecmp(opt->srun_opt->cpu_bind, "v"))) {
 		if (opt->verbose)
 			info("Setting --cpu-bind=threads,verbose as a default of --threads-per-core use");
 		if (opt->srun_opt)