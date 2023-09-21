2674a3a8aa67031e12b73903612a3c36b290f787
Nathan Rini <nate@schedmd.com>
2023-09-21 09:41:35 -0600

    auth/jwt - Ignore JWKS x5c field
    Revert commit 1572786459.
    
    The 'x5c' field is a base64 wrapped DER encoded x509 certificate which
    is not supported by libjwt currently, causing otherwise valid JWKS files
    to result in JWT decode failures.
    
    The original code expected a string instead of the array (per RFC7517),
    which resulted in it being ignored (and the key being reconstructed
    from 'e' and 'n' instead) in almost all cases when a valid JWKS was
    presented, which is how this issue wasn't caught previously.
    
    Bug 16586

--- src/plugins/auth/jwt/auth_jwt.c
+++ src/plugins/auth/jwt/auth_jwt.c
@@ -131,22 +131,12 @@ static const char *jwks_key_field = "jwks=";
 
 static data_for_each_cmd_t _build_jwks_keys(data_t *d, void *arg)
 {
-	char *alg, *kid, *n, *e, *key, *x5c, *kty;
+	char *alg, *kid, *n, *e, *key;
 
 	if (!(kid = data_get_string(data_key_get(d, "kid"))))
 		fatal("%s: failed to load kid field", __func__);
 
 	alg = data_get_string(data_key_get(d, "alg"));
-	kty = data_get_string(data_key_get(d, "kty"));
-	x5c = data_get_string(data_key_get(d, "x5c"));
-
-	if (kty && x5c && !xstrcasecmp(kty, "RSA") && x5c[0]) {
-		debug3("x5c for kid %s is %s", kid, x5c);
-
-		data_set_int(data_key_set(d, "slurm-pem-len"), strlen(x5c));
-		data_set_string_own(data_key_set(d, "slurm-pem"), x5c);
-		return DATA_FOR_EACH_CONT;
-	}
 
 	/* Ignore non-RS256 keys in the JWKS */
 	if (xstrcasecmp(alg, "RS256"))
