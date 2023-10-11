59304a849780b671d59b39a3d24e6e74242d17b9
Tim Wickberg <tim@schedmd.com>
2023-10-11 13:18:59 -0600

    Run setup_container() as the user.

--- src/slurmd/slurmstepd/slurmstepd.c
+++ src/slurmd/slurmstepd/slurmstepd.c
@@ -797,7 +797,18 @@ _step_setup(slurm_addr_t *cli, slurm_addr_t *self, slurm_msg_t *msg)
 	}
 
 	if (step->container) {
-		int rc = setup_container(step);
+	        struct priv_state sprivs;
+		int rc;
+
+		if (drop_privileges(step, false, &sprivs, true) < 0) {
+			error("%s: drop_priviledges failed", __func__);
+			return NULL;
+		}
+		rc = setup_container(step);
+		if (reclaim_privileges(&sprivs) < 0) {
+			error("%s: reclaim_priviledges failed", __func__);
+			return NULL;
+		}
 
 		if (rc == ESLURM_CONTAINER_NOT_CONFIGURED) {
 			debug2("%s: container %s requested but containers are not configured on this node",
