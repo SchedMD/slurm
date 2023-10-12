cd9796de46ed239050a7958919959b62c99d6cc6
Caden Ellis <caden@schedmd.com>
2023-10-12 17:07:23 +0000

    gpu/nrt - Implement gpu_p_get_system_gpu_list
    Bug 16748

--- src/plugins/gpu/nrt/gpu_nrt.c
+++ src/plugins/gpu/nrt/gpu_nrt.c
@@ -41,6 +41,13 @@
 #include "../common/gpu_common.h"
 
 #define NEURON_SYSFS_PREFIX "/sys/devices/virtual/neuron_device/"
+#define NEURON_SYSFS_DEVICE_NAME_PREFIX \
+	NEURON_SYSFS_PREFIX "neuron%d/info/architecture/device_name"
+#define NEURON_SYSFS_CONNECTED_DEV_PREFIX \
+	NEURON_SYSFS_PREFIX "neuron%d/connected_devices"
+
+#define CONNECTED_DEVICES_SZ 100
+#define DEVICE_NAME_SZ 50
 
 /*
  * These variables are required by the generic plugin interface.  If they
@@ -92,6 +99,145 @@ static int _count_devices(unsigned int *dev_count)
 	return SLURM_SUCCESS;
 }
 
+static char *_get_device_name(unsigned int dev_inx)
+{
+	FILE *fp = NULL;
+	char *sysfs_file = NULL;
+	char *device_name = NULL;
+
+	sysfs_file = xstrdup_printf(NEURON_SYSFS_DEVICE_NAME_PREFIX, dev_inx);
+	fp = fopen(sysfs_file, "r");
+	if (!fp) {
+		debug("Could not access device name in Neuron sysfs interface");
+		xfree(sysfs_file);
+		return NULL;
+	}
+
+	device_name = xmalloc(DEVICE_NAME_SZ);
+
+	if (!fscanf(fp, "%s", device_name))
+		debug("Could not read Neuron device name");
+
+	xstrtolower(device_name);
+	xfree(sysfs_file);
+	fclose(fp);
+	return device_name;
+}
+
+static bool _is_link(int *link_nums, unsigned int dev_cnt, int dev_inx)
+{
+	for (unsigned int i = 0; i < dev_cnt; i++) {
+		if (link_nums[i] == dev_inx)
+			return true;
+	}
+	return false;
+}
+
+static char *_get_connected_devices(int dev_inx, unsigned int dev_cnt)
+{
+	FILE *fp = NULL;
+	char *sysfs_file = NULL;
+	char *tok, *save_ptr;
+	char conn_dev[CONNECTED_DEVICES_SZ];
+	int link_nums[CONNECTED_DEVICES_SZ];
+	char *links = NULL;
+	int tmp_link;
+	int num_links = 0;
+
+	sysfs_file = xstrdup_printf(NEURON_SYSFS_CONNECTED_DEV_PREFIX, dev_inx);
+	fp = fopen(sysfs_file, "r");
+	if (!fp) {
+		debug("Could not access connected_devices in Neuron sysfs interface");
+		xfree(sysfs_file);
+		return NULL;
+	}
+
+	if (!fgets(conn_dev, CONNECTED_DEVICES_SZ, fp)) {
+		debug("Could not read Neuron connected devices. Setting empty links");
+		xfree(sysfs_file);
+		return links;
+	}
+
+	/*
+	 * Convert to array of ints for processing
+	 * The link numbers can be in any order
+	 */
+	tok = strtok_r(conn_dev, ", ", &save_ptr);
+	while (tok) {
+		tmp_link = atoi(tok);
+		link_nums[num_links++] = tmp_link;
+		tok = strtok_r(NULL, ", ", &save_ptr);
+	}
+
+	for (unsigned int i = 0; i < dev_cnt; i++) {
+		if (_is_link(link_nums, num_links, i))
+			xstrfmtcat(links, "%s%d", i ? "," : "", 1);
+		else if (i == dev_inx)
+			xstrfmtcat(links, "%s%d", i ? "," : "", -1);
+		else
+			xstrfmtcat(links, "%s%d", i ? "," : "", 0);
+	}
+
+	xfree(sysfs_file);
+	fclose(fp);
+	return links;
+}
+
+static list_t *_get_system_gpu_list_neuron(node_config_load_t *node_conf)
+{
+	struct dirent *de;
+	unsigned int dev_inx;
+	unsigned int dev_cnt = 0;
+	list_t *gres_list_system = NULL;
+	DIR *dr = opendir(NEURON_SYSFS_PREFIX);
+
+	if (!dr)
+		return NULL;
+
+	_count_devices(&dev_cnt);
+
+	while ((de = readdir(dr))) {
+		if ((sscanf(de->d_name, "neuron%d\n", &dev_inx) == 1)) {
+
+			char *device_file = NULL;
+			char *links = NULL;
+			char *device_name = NULL;
+
+			gres_slurmd_conf_t gres_slurmd_conf = {
+				.count = 1,
+				.cpu_cnt = node_conf->cpu_cnt,
+				.name = "gpu",
+			};
+
+			xstrfmtcat(device_file, "/dev/neuron%u", dev_inx);
+			device_name = _get_device_name(dev_inx);
+			links = _get_connected_devices(dev_inx, dev_cnt);
+
+			debug2("GPU index %u:", dev_inx);
+			debug2("    Name: %s", device_name);
+			debug2("    Links: %s", links);
+			debug2("    Device File: %s", device_file);
+
+			gres_slurmd_conf.type_name = device_name;
+			gres_slurmd_conf.links = links;
+			gres_slurmd_conf.file = device_file;
+
+			if (!gres_list_system)
+				gres_list_system =
+					list_create(destroy_gres_slurmd_conf);
+
+			/* Add the GPU to list */
+			add_gres_to_list(gres_list_system, &gres_slurmd_conf);
+
+			xfree(device_file);
+			xfree(links);
+			xfree(device_name);
+		}
+	}
+	closedir(dr);
+	return gres_list_system;
+}
+
 extern int init(void)
 {
 	debug("%s: %s loaded", __func__, plugin_name);
@@ -119,9 +265,16 @@ extern void gpu_p_reconfig(void)
 	return;
 }
 
-extern List gpu_p_get_system_gpu_list(node_config_load_t *node_conf)
+extern list_t *gpu_p_get_system_gpu_list(node_config_load_t *node_conf)
 {
-	return NULL;
+	list_t *gres_list_system = _get_system_gpu_list_neuron(node_conf);
+
+	xassert(node_conf);
+
+	if (!gres_list_system)
+		error("System GPU detection failed");
+
+	return gres_list_system;
 }
 
 extern void gpu_p_step_hardware_init(bitstr_t *usable_gpus, char *tres_freq)
