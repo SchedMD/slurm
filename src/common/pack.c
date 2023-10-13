SchedMD/slurm

commit 7ca8a771e443cd075ad7afffc0602d8bd4f362fd

Introduce create_shadow_buf().
Usable with the unpack routines, but does not directly manage the
data buffer. Instead points at some independently-managed data
buffer.

src/common/pack.c
@@ -69,6 +69,7 @@
  */
 strong_alias(create_buf,	slurm_create_buf);
 strong_alias(create_mmap_buf,	slurm_create_mmap_buf);
+strong_alias(create_shadow_buf,	slurm_create_shadow_buf);
 strong_alias(free_buf,		slurm_free_buf);
 strong_alias(grow_buf,		slurm_grow_buf);
 strong_alias(init_buf,		slurm_init_buf);
@@ -125,6 +126,7 @@ buf_t *create_buf(char *data, uint32_t size)
 	my_buf->processed = 0;
 	my_buf->head = data;
 	my_buf->mmaped = false;
+	my_buf->shadow = false;
 
 	return my_buf;
 }
@@ -170,6 +172,15 @@ buf_t *create_mmap_buf(const char *file)
 	return my_buf;
 }
 
+extern buf_t *create_shadow_buf(char *data, uint32_t size)
+{
+	buf_t *my_buf = create_buf(data, size);
+
+	if (my_buf)
+		my_buf->shadow = true;
+
+	return my_buf;
+}
 
 /* free_buf - release memory associated with a given buffer */
 void free_buf(buf_t *my_buf)
@@ -179,7 +190,7 @@ void free_buf(buf_t *my_buf)
 	xassert(my_buf->magic == BUF_MAGIC);
 	if (my_buf->mmaped)
 		munmap(my_buf->head, my_buf->size);
-	else
+	else if (!my_buf->shadow)
 		xfree(my_buf->head);
 
 	xfree(my_buf);
@@ -190,6 +201,8 @@ void grow_buf(buf_t *buffer, uint32_t size)
 {
 	if (buffer->mmaped)
 		fatal_abort("attempt to grow mmap()'d buffer not supported");
+	if (buffer->shadow)
+		fatal_abort("attempt to grow shadow buffer not supported");
 	if ((buffer->size + size) > MAX_BUF_SIZE) {
 		error("%s: Buffer size limit exceeded (%u > %u)",
 		      __func__, (buffer->size + size), MAX_BUF_SIZE);
@@ -231,6 +244,8 @@ void *xfer_buf_data(buf_t *my_buf)
 
 	if (my_buf->mmaped)
 		fatal_abort("attempt to xfer mmap()'d buffer not supported");
+	if (my_buf->shadow)
+		fatal_abort("attempt to xfer shadow buffer not supported");
 
 	data_ptr = (void *) my_buf->head;
 	xfree(my_buf);

src/common/pack.h
@@ -71,6 +71,7 @@ typedef struct {
 	uint32_t size;
 	uint32_t processed;
 	bool mmaped;
+	bool shadow;
 } buf_t;
 
 #define get_buf_data(__buf)		(__buf->head)
@@ -87,6 +88,7 @@ typedef struct {
 
 extern buf_t *create_buf(char *data, uint32_t size);
 extern buf_t *create_mmap_buf(const char *file);
+extern buf_t *create_shadow_buf(char *data, uint32_t size);
 extern void free_buf(buf_t *my_buf);
 extern buf_t *init_buf(uint32_t size);
 extern void grow_buf(buf_t *my_buf, uint32_t size);

commit 3e97ddb45b784fc230d7c09738369f149f1083e3

Initialize shadow element in init_buf().
Regression in 7ca8a771e4 can cause:
fatal: attempt to xfer shadow buffer not supported

src/common/pack.c
@@ -228,6 +228,7 @@ buf_t *init_buf(uint32_t size)
 	my_buf->processed = 0;
 	my_buf->head = xmalloc(size);
 	my_buf->mmaped = false;
+	my_buf->shadow = false;
 	return my_buf;
 }
 