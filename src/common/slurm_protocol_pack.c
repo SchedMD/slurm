SchedMD/slurm

commit be34104add921b012a27dc3c7d218aaf88159a78

Move verification into cred_p_unpack().
Add verified bool into sbcast_cred_t. This also avoids needing to
reconstruct the credential buffer just to validate it.

src/interfaces/cred.c
@@ -776,9 +776,7 @@ extern sbcast_cred_arg_t *extract_sbcast_cred(sbcast_cred_t *sbcast_cred,
 {
 	sbcast_cred_arg_t *arg;
 	struct sbcast_cache *next_cache_rec;
-	int rc;
 	time_t now = time(NULL);
-	buf_t *buffer;
 
 	xassert(g_context);
 
@@ -786,22 +784,9 @@ extern sbcast_cred_arg_t *extract_sbcast_cred(sbcast_cred_t *sbcast_cred,
 		return NULL;
 
 	if (block_no == 1 && !(flags & FILE_BCAST_SO)) {
-		buffer = init_buf(4096);
-		_pack_sbcast_cred(sbcast_cred, buffer, protocol_version);
-		/* NOTE: the verification checks that the credential was
-		 * created by SlurmUser or root */
-		rc = (*(ops.cred_verify_sign))(get_buf_data(buffer),
-					       get_buf_offset(buffer),
-					       sbcast_cred->signature);
-		FREE_NULL_BUFFER(buffer);
-
-		if (rc) {
-			error("sbcast_cred verify: %s",
-			      (*(ops.cred_str_error))(rc));
+		if (!sbcast_cred->verified)
 			return NULL;
-		}
 		_sbcast_cache_add(sbcast_cred);
-
 	} else {
 		bool cache_match_found = false;
 		list_itr_t *sbcast_iter;

src/interfaces/cred.h
@@ -162,6 +162,7 @@ typedef struct sbcast_cred {
 	uint32_t *gids;		/* extended group ids for user		*/
 	char *nodes;		/* nodes for which credential is valid	*/
 	char *signature;	/* credential signature			*/
+	bool verified;		/* credential has been verified successfully */
 } sbcast_cred_t;
 
 /* Used by slurm_cred_get() */

src/plugins/cred/munge/cred_munge.c
@@ -413,11 +413,21 @@ extern void *cred_p_extract_net_cred(char *net_cred, uint16_t protocol_version)
 extern sbcast_cred_t *sbcast_p_unpack(buf_t *buf, uint16_t protocol_version)
 {
 	sbcast_cred_t *cred;
+	uint32_t cred_start = get_buf_offset(buf), siglen = 0;
 
-	if (!(cred = sbcast_cred_unpack(buf, protocol_version))) {
+	if (!(cred = sbcast_cred_unpack(buf, &siglen, protocol_version))) {
 		error("%s: sbcast_cred_unpack() failed", __func__);
 		return NULL;
+	}
 
+	if (running_in_slurmd()) {
+		if (cred_p_verify_sign(get_buf_data(buf) + cred_start,
+				       siglen, cred->signature)) {
+			delete_sbcast_cred(cred);
+			return NULL;
+		}
+
+		cred->verified = true;
 	}
 
 	return cred;

src/plugins/cred/none/cred_none.c
@@ -151,12 +151,16 @@ extern void *cred_p_extract_net_cred(char *net_cred, uint16_t protocol_version)
 
 extern sbcast_cred_t *sbcast_p_unpack(buf_t *buf, uint16_t protocol_version)
 {
+	uint32_t siglen;
 	sbcast_cred_t *cred;
 
-	if (!(cred = sbcast_cred_unpack(buf, protocol_version))) {
+	if (!(cred = sbcast_cred_unpack(buf, &siglen, protocol_version))) {
 		error("%s: sbcast_cred_unpack() failed", __func__);
 		return NULL;
 	}
+
+	/* why bother checking? */
+	cred->verified = true;
 
 	return cred;
 }

src/plugins/cred/common/cred_common.c
@@ -651,10 +651,11 @@ unpack_error:
 	return NULL;
 }
 
-extern sbcast_cred_t *sbcast_cred_unpack(buf_t *buffer,
+extern sbcast_cred_t *sbcast_cred_unpack(buf_t *buffer, uint32_t *siglen,
 					 uint16_t protocol_version)
 {
 	sbcast_cred_t *sbcast_cred = xmalloc(sizeof(*sbcast_cred));
+	uint32_t cred_start = get_buf_offset(buffer);
 
 	if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		safe_unpack_time(&sbcast_cred->ctime, buffer);
@@ -668,6 +669,8 @@ extern sbcast_cred_t *sbcast_cred_unpack(buf_t *buffer,
 		safe_unpack32_array(&sbcast_cred->gids, &sbcast_cred->ngids,
 				    buffer);
 		safe_unpackstr(&sbcast_cred->nodes, buffer);
+
+		*siglen = get_buf_offset(buffer) - cred_start;
 
 		/* "signature" must be last */
 		safe_unpackstr(&sbcast_cred->signature, buffer);

src/plugins/cred/common/cred_common.h
@@ -44,7 +44,7 @@ extern int cred_unpack(void **out, buf_t *buffer, uint16_t protocol_version);
 extern slurm_cred_t *cred_unpack_with_signature(buf_t *buffer,
 						uint16_t protocol_version);
 
-extern sbcast_cred_t *sbcast_cred_unpack(buf_t *buffer,
+extern sbcast_cred_t *sbcast_cred_unpack(buf_t *buffer, uint32_t *siglen,
 					 uint16_t protocol_version);
 
 #endif

commit 5cd90ab3e3825f8a37be1d484034337c7c947697

Avoid revalidating credentials.
MUNGE will, on non-developer builds, complain about replayed credentials
when sending multiple blocks for the same file, or for the library files.

The 'void *' argument is to avoid an issue where the type is defined in
slurm_protocol_defs.h, but cred.h is included at the top of that header,
leading to an awkward cyclical include path that's best avoided.

Regression from be34104add, but much easier to fix now.

src/common/slurm_protocol_pack.c
@@ -1713,7 +1713,7 @@ static int _unpack_job_sbcast_cred_msg(slurm_msg_t *smsg, buf_t *buffer)
 	safe_unpack32(&uint32_tmp, buffer); /* was node_cnt */
 
 	tmp_ptr->sbcast_cred =
-		unpack_sbcast_cred(buffer, smsg->protocol_version);
+		unpack_sbcast_cred(buffer, NULL, smsg->protocol_version);
 	if (tmp_ptr->sbcast_cred == NULL)
 		goto unpack_error;
 
@@ -10373,7 +10373,8 @@ static int _unpack_file_bcast(file_bcast_msg_t ** msg_ptr , buf_t *buffer,
 		if (uint32_tmp != msg->block_len)
 			goto unpack_error;
 
-		msg->cred = unpack_sbcast_cred(buffer, protocol_version);
+		msg->cred = unpack_sbcast_cred(buffer, msg,
+					       protocol_version);
 		if (msg->cred == NULL)
 			goto unpack_error;
 	}

src/interfaces/cred.c
@@ -79,7 +79,7 @@ typedef struct {
 					 uint16_t protocol_version);
 	sbcast_cred_t *(*sbcast_create)	(sbcast_cred_arg_t *cred,
 					 uint16_t protocol_version);
-	sbcast_cred_t *(*sbcast_unpack)	(buf_t *buffer,
+	sbcast_cred_t *(*sbcast_unpack)	(buf_t *buffer, bool verify,
 					 uint16_t protocol_version);
 } slurm_cred_ops_t;
 
@@ -751,10 +751,16 @@ extern void pack_sbcast_cred(sbcast_cred_t *sbcast_cred, buf_t *buffer,
 	packbuf(sbcast_cred->buffer, buffer);
 }
 
-extern sbcast_cred_t *unpack_sbcast_cred(buf_t *buffer,
+extern sbcast_cred_t *unpack_sbcast_cred(buf_t *buffer, void *msg,
 					 uint16_t protocol_version)
 {
-	return (*(ops.sbcast_unpack))(buffer, protocol_version);
+	file_bcast_msg_t *bmsg = msg;
+	bool verify = false;
+
+	if (bmsg && (bmsg->block_no == 1) && !(bmsg->flags & FILE_BCAST_SO))
+		verify = true;
+
+	return (*(ops.sbcast_unpack))(buffer, verify, protocol_version);
 }
 
 extern void print_sbcast_cred(sbcast_cred_t *sbcast_cred)

src/interfaces/cred.h
@@ -339,7 +339,8 @@ extern sbcast_cred_arg_t *extract_sbcast_cred(sbcast_cred_t *sbcast_cred,
 					      uint16_t protocol_version);
 extern void pack_sbcast_cred(sbcast_cred_t *sbcast_cred, buf_t *buffer,
 		      uint16_t protocol_Version);
-extern sbcast_cred_t *unpack_sbcast_cred(buf_t *buffer, uint16_t protocol_version);
+extern sbcast_cred_t *unpack_sbcast_cred(buf_t *buffer, void *msg,
+					 uint16_t protocol_version);
 extern void print_sbcast_cred(sbcast_cred_t *sbcast_cred);
 extern void sbcast_cred_arg_free(sbcast_cred_arg_t *arg);
 

src/plugins/cred/munge/cred_munge.c
@@ -406,7 +406,8 @@ extern sbcast_cred_t *sbcast_p_create(sbcast_cred_arg_t *cred_arg,
 	return cred;
 }
 
-extern sbcast_cred_t *sbcast_p_unpack(buf_t *buf, uint16_t protocol_version)
+extern sbcast_cred_t *sbcast_p_unpack(buf_t *buf, bool verify,
+				      uint16_t protocol_version)
 {
 	sbcast_cred_t *cred;
 	uint32_t cred_start = get_buf_offset(buf), siglen = 0;
@@ -416,7 +417,7 @@ extern sbcast_cred_t *sbcast_p_unpack(buf_t *buf, uint16_t protocol_version)
 		return NULL;
 	}
 
-	if (running_in_slurmd()) {
+	if (verify) {
 		if (_verify_signature(get_buf_data(buf) + cred_start,
 				      siglen, cred->signature)) {
 			delete_sbcast_cred(cred);

src/plugins/cred/none/cred_none.c
@@ -138,7 +138,8 @@ extern sbcast_cred_t *sbcast_p_create(sbcast_cred_arg_t *cred_arg,
 	return cred;
 }
 
-extern sbcast_cred_t *sbcast_p_unpack(buf_t *buf, uint16_t protocol_version)
+extern sbcast_cred_t *sbcast_p_unpack(buf_t *buf, bool verify,
+				      uint16_t protocol_version)
 {
 	uint32_t siglen;
 	sbcast_cred_t *cred;