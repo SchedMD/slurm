SchedMD/slurm

commit 75e787f65b0e19107ada83dd8e6dfd0dc28630dc

Factor out logic to _set_node_mixed_op()
This function handles the logic for updating the node state to mixed for
client display. Generally used by API when SHOW_MIXED flag is used.

Bug 17846

src/api/node_info.c
@@ -75,6 +75,9 @@ typedef struct load_node_resp_struct {
 	node_info_msg_t *new_msg;
 } load_node_resp_struct_t;
 
+/* Function declarations */
+static void _set_node_mixed_op(node_info_t *node_ptr);
+
 /*
  * slurm_print_node_info_msg - output information about all Slurm nodes
  *	based upon message as loaded using slurm_load_node
@@ -177,25 +180,14 @@ slurm_populate_node_partitions(node_info_msg_t *node_buffer_ptr,
  */
 char *slurm_sprint_node_table(node_info_t *node_ptr, int one_liner)
 {
-	uint32_t my_state = node_ptr->node_state;
 	char time_str[256];
 	char *out = NULL, *reason_str = NULL, *complete_state = NULL;
 	uint16_t alloc_cpus = 0;
-	int idle_cpus;
 	uint64_t alloc_memory;
 	char *node_alloc_tres = NULL;
 	char *line_end = (one_liner) ? " " : "\n   ";
 
-	slurm_get_select_nodeinfo(node_ptr->select_nodeinfo,
-				  SELECT_NODEDATA_SUBCNT,
-				  NODE_STATE_ALLOCATED,
-				  &alloc_cpus);
-	idle_cpus = node_ptr->cpus_efctv - alloc_cpus;
-
-	if (idle_cpus  && (idle_cpus != node_ptr->cpus_efctv)) {
-		my_state &= NODE_STATE_FLAGS;
-		my_state |= NODE_STATE_MIXED;
-	}
+	_set_node_mixed_op(node_ptr);
 
 	/****** Line 1 ******/
 	xstrfmtcat(out, "NodeName=%s ", node_ptr->name);
@@ -324,7 +316,7 @@ char *slurm_sprint_node_table(node_info_t *node_ptr, int one_liner)
 	}
 
 	/****** Line ******/
-	complete_state = node_state_string_complete(my_state);
+	complete_state = node_state_string_complete(node_ptr->node_state);
 	xstrfmtcat(out, "State=%s ThreadsPerCore=%u TmpDisk=%u Weight=%u ",
 		   complete_state, node_ptr->threads, node_ptr->tmp_disk,
 		   node_ptr->weight);
@@ -506,6 +498,24 @@ char *slurm_sprint_node_table(node_info_t *node_ptr, int one_liner)
 	return out;
 }
 
+static void _set_node_mixed_op(node_info_t *node_ptr)
+{
+	uint16_t alloc_cpus = 0;
+	uint16_t idle_cpus = 0;
+
+	xassert(node_ptr);
+
+	select_g_select_nodeinfo_get(node_ptr->select_nodeinfo,
+				     SELECT_NODEDATA_SUBCNT,
+				     NODE_STATE_ALLOCATED, &alloc_cpus);
+	idle_cpus = node_ptr->cpus_efctv - alloc_cpus;
+
+	if (idle_cpus && (idle_cpus < node_ptr->cpus_efctv)) {
+		node_ptr->node_state &= NODE_STATE_FLAGS;
+		node_ptr->node_state |= NODE_STATE_MIXED;
+	}
+}
+
 static void _set_node_mixed(node_info_msg_t *resp)
 {
 	node_info_t *node_ptr = NULL;
@@ -516,14 +526,7 @@ static void _set_node_mixed(node_info_msg_t *resp)
 
 	for (i = 0, node_ptr = resp->node_array;
 	     i < resp->record_count; i++, node_ptr++) {
-		uint16_t used_cpus = 0;
-		select_g_select_nodeinfo_get(node_ptr->select_nodeinfo,
-					     SELECT_NODEDATA_SUBCNT,
-					     NODE_STATE_ALLOCATED, &used_cpus);
-		if (used_cpus && (used_cpus != node_ptr->cpus_efctv)) {
-			node_ptr->node_state &= NODE_STATE_FLAGS;
-			node_ptr->node_state |= NODE_STATE_MIXED;
-		}
+		_set_node_mixed_op(node_ptr);
 	}
 }
 

commit 2bbb52a71ac0e758a9eb057d7eb94136d9bac4a7

Fix regression which caused CPUAlloc to always display zero
Introduced by commit 75e787f6.

Source of regression was bad refactoring of slurm_sprint_node_table().
Calling select_g_select_nodeinfo_get() once to get alloc_cpus is still
required.

Bug 18052

src/api/node_info.c
@@ -201,6 +201,9 @@ char *slurm_sprint_node_table(node_info_t *node_ptr, int one_liner)
 	xstrcat(out, line_end);
 
 	/****** Line ******/
+	select_g_select_nodeinfo_get(node_ptr->select_nodeinfo,
+				     SELECT_NODEDATA_SUBCNT,
+				     NODE_STATE_ALLOCATED, &alloc_cpus);
 	xstrfmtcat(out, "CPUAlloc=%u CPUEfctv=%u CPUTot=%u ",
 		   alloc_cpus, node_ptr->cpus_efctv, node_ptr->cpus);
 