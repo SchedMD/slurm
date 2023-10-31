91a65d3646b87164378891667d8101ec8c6565bf
Albert Gil <albert.gil@schedmd.com>
2023-10-31 09:36:36 +0100

    Testsuite - Adapt test_105_1 to new output from 53ec8d6697
    Bug 18057

--- testsuite/python/tests/test_105_1.py
+++ testsuite/python/tests/test_105_1.py
@@ -81,13 +81,13 @@ def test_gpus_per_cpu(init_gpu_vars):
     job_id = atf.submit_job_sbatch(f"--cpus-per-gpu={cpus_per_gpu} --gpu-bind={gpu_bind} --gpu-freq={gpu_freq} --gpus={gpu_count} --gpus-per-node={gpus_per_node} --gpus-per-task={gpus_per_task} --mem-per-gpu={memory_per_gpu} --nodes={node_count} --ntasks={task_count} -t1 --wrap \"true\"", fatal=True)
     job_dict = atf.get_job(job_id)
 
-    assert job_dict['CpusPerTres'] == f"gres:gpu:{cpus_per_gpu}"
-    assert job_dict['MemPerTres'] == f"gres:gpu:{memory_per_gpu}"
+    assert job_dict['CpusPerTres'] == f"gres/gpu:{cpus_per_gpu}"
+    assert job_dict['MemPerTres'] == f"gres/gpu:{memory_per_gpu}"
     assert job_dict['TresBind'] == f"gpu:{gpu_bind}"
     assert job_dict['TresFreq'] == f"gpu:{gpu_freq}"
-    assert job_dict['TresPerJob'] == f"gres:gpu:{gpu_count}"
-    assert job_dict['TresPerNode'] == f"gres:gpu:{gpus_per_node}"
-    assert job_dict['TresPerTask'] == f"gres:gpu:{gpus_per_task}"
+    assert job_dict['TresPerJob'] == f"gres/gpu:{gpu_count}"
+    assert job_dict['TresPerNode'] == f"gres/gpu:{gpus_per_node}"
+    assert job_dict['TresPerTask'] == f"gres/gpu:{gpus_per_task}"
 
 
 def test_gpus_per_socket(init_gpu_vars):
@@ -97,4 +97,4 @@ def test_gpus_per_socket(init_gpu_vars):
     job_id = atf.submit_job_sbatch(f"--cpus-per-gpu={cpus_per_gpu} --gpus-per-socket={gpus_per_socket} --sockets-per-node={sockets_per_node} --nodes={node_count} --ntasks={task_count} -t1 --wrap \"true\"", fatal=True)
     job_dict = atf.get_job(job_id)
 
-    assert job_dict['TresPerSocket'] == f"gres:gpu:{gpus_per_socket}"
+    assert job_dict['TresPerSocket'] == f"gres/gpu:{gpus_per_socket}"
