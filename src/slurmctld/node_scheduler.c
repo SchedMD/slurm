SchedMD/slurm

commit 9f4ecc22d4d5dce6f3a65b3be84f3dbde67fbc67

Change handling for jobs whose extra constraints cannot be satisfied
Change the state reason from "BadConstraints" to "Constraints".
Do not hold the job so that future node updates may allow the job to run
without needing to first explicitly release the job.

Bug 18064

src/slurmctld/node_scheduler.c
@@ -3542,8 +3542,21 @@ static int _build_node_list(job_record_t *job_ptr,
 		usable_node_mask = node_conf_get_active_bitmap();
 	}
 
-	if (!test_only && job_ptr->extra_constraints)
+	if (!test_only && job_ptr->extra_constraints) {
 		_apply_extra_constraints(job_ptr, usable_node_mask);
+		if (!bit_set_count(usable_node_mask)) {
+			rc = ESLURM_REQUESTED_NODE_CONFIG_UNAVAILABLE;
+			debug("%s: No nodes satisfy %pJ extra constraints in partition %s",
+				__func__, job_ptr, job_ptr->part_ptr->name);
+			xfree(job_ptr->state_desc);
+			job_ptr->state_reason = FAIL_CONSTRAINTS;
+			debug2("%s: setting %pJ to \"%s\" (%s)",
+			       __func__, job_ptr,
+			       job_reason_string(job_ptr->state_reason),
+			       slurm_strerror(rc));
+			return rc;
+		}
+	}
 
 	if ((rc = valid_feature_counts(job_ptr, false, usable_node_mask,
 				       &has_mor))) {

src/slurmctld/job_scheduler.c
@@ -1921,6 +1921,15 @@ skip_start:
 			debug("%pJ non-runnable in partition %s: %s",
 			      job_ptr, job_ptr->part_ptr->name,
 			      slurm_strerror(error_code));
+		} else if ((error_code ==
+			    ESLURM_REQUESTED_NODE_CONFIG_UNAVAILABLE) &&
+			   (job_ptr->state_reason == FAIL_CONSTRAINTS)) {
+			sched_info("%pJ current node constraints not satisfied",
+				   job_ptr);
+			/*
+			 * Future node updates may satisfy the constraints, so
+			 * do not hold the job.
+			 */
 		} else if (error_code == ESLURM_ACCOUNTING_POLICY) {
 			sched_debug3("%pJ delayed for accounting policy",
 				     job_ptr);

commit 30a9ddae575fa1af8a821543ff14c2028b2d9efa

Fix memory leak introduced by commit 9f4ecc22d4d
CID 330960

Bug 18064

src/slurmctld/node_scheduler.c
@@ -3554,6 +3554,7 @@ static int _build_node_list(job_record_t *job_ptr,
 			       __func__, job_ptr,
 			       job_reason_string(job_ptr->state_reason),
 			       slurm_strerror(rc));
+			FREE_NULL_BITMAP(usable_node_mask);
 			return rc;
 		}
 	}