SchedMD/slurm

commit 74e6ad0659aa552f0357ff75be980b71ee2a0016

Fix memleak when adding account to multiple clusters
Bug 17969

src/plugins/accounting_storage/mysql/as_mysql_assoc.c
@@ -3411,6 +3411,8 @@ static int _add_assoc_cond_acct(void *x, void *arg)
 
 end_it:
 	xfree(add_assoc_cond->add_assoc->assoc.lineage);
+	xfree(add_assoc_cond->old_cluster);
+	xfree(add_assoc_cond->old_parent);
 
 	add_assoc_cond->add_assoc->assoc.acct = NULL;
 	if (add_assoc_cond->rc != SLURM_SUCCESS)

commit c4638bf432c3cade00462281fcba66ed099e563b

Fix creating assocs with incorrect hierarchy for non-23.11 clusters
This fixes a regression in 23.11.0rc1 caused by 74e6ad0659 by moving the
xfrees to _add_assoc_cond_cluster instead. That way if there are
multiple calls to _add_assoc_cond_acct in a loop, each call can still
use old_parent and old_cluster.

Bug 18063

src/plugins/accounting_storage/mysql/as_mysql_assoc.c
@@ -3413,8 +3413,6 @@ static int _add_assoc_cond_acct(void *x, void *arg)
 
 end_it:
 	xfree(add_assoc_cond->add_assoc->assoc.lineage);
-	xfree(add_assoc_cond->old_cluster);
-	xfree(add_assoc_cond->old_parent);
 
 	add_assoc_cond->add_assoc->assoc.acct = NULL;
 	if (add_assoc_cond->rc != SLURM_SUCCESS)
@@ -3486,6 +3484,8 @@ static int _add_assoc_cond_cluster(void *x, void *arg)
 	_post_add_assoc_cond_cluster(add_assoc_cond);
 
 end_it:
+	xfree(add_assoc_cond->old_parent);
+	xfree(add_assoc_cond->old_cluster);
 	add_assoc_cond->add_assoc->assoc.cluster = NULL;
 	if (add_assoc_cond->rc != SLURM_SUCCESS)
 		return -1;