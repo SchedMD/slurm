2ee4226227f1caada75685bc7f8bbe11c759b706
Brian Christiansen <brian@schedmd.com>
2023-11-20 09:16:49 -0700

    Fix NULL dereference
    Continuation of cb06d1e731
    
    Checking for just !(*count) is enough and prevents checking sp_hl if
    count happened to have something.
    
    CID 331023
    
    Bug 18160

--- src/interfaces/topology.c
+++ src/interfaces/topology.c
@@ -221,8 +221,7 @@ extern int topology_g_split_hostlist(hostlist_t *hl,
 		tree_width = slurm_conf.tree_width;
 
 	rc = (*(ops.split_hostlist))(hl, sp_hl, count, tree_width);
-	if (!rc &&
-	    (!(*count) || !(*sp_hl)))
+	if (!rc && !(*count))
 		rc = SLURM_ERROR;
 
 	if (slurm_conf.debug_flags & DEBUG_FLAG_ROUTE) {
