SchedMD/slurm

commit 51e3a609238cbae9de22e06673a88078e73d437b

Fix memory leak, and a slight optimization
Fix a memory leak. Also do not copy the job's node bitmap unless needed.
Continuation of commit 782d1c1939.

CID 320950

Bug 16691

src/slurmctld/read_config.c
@@ -3250,14 +3250,12 @@ static int _sync_nodes_to_active_job(job_record_t *job_ptr)
 	int cnt = 0;
 	uint32_t node_flags;
 	node_record_t *node_ptr;
-	bitstr_t *node_bitmap, *orig_job_node_bitmap;
+	bitstr_t *node_bitmap, *orig_job_node_bitmap = NULL;
 
 	if (job_ptr->node_bitmap_cg) /* job completing */
 		node_bitmap = job_ptr->node_bitmap_cg;
 	else
 		node_bitmap = job_ptr->node_bitmap;
-
-	orig_job_node_bitmap = bit_copy(job_ptr->job_resrcs->node_bitmap);
 
 	job_ptr->node_cnt = bit_set_count(node_bitmap);
 	for (int i = 0; (node_ptr = next_node_bitmap(node_bitmap, &i)); i++) {
@@ -3306,6 +3304,16 @@ static int _sync_nodes_to_active_job(job_record_t *job_ptr)
 			int save_accounting_enforce;
 			info("Removing failed node %s from %pJ",
 			     node_ptr->name, job_ptr);
+
+			/*
+			 * Save the original node bitmap, which is needed for
+			 * rebuild_step_bitmaps below
+			 */
+			if (!orig_job_node_bitmap)
+				orig_job_node_bitmap =
+					bit_copy(job_ptr->job_resrcs->
+						 node_bitmap);
+
 			/*
 			 * Disable accounting here. Accounting reset for all
 			 * jobs in _restore_job_accounting()
@@ -3334,8 +3342,8 @@ static int _sync_nodes_to_active_job(job_record_t *job_ptr)
 	/* If the job was resized then resize the bitmaps of the job's steps */
 	if (job_ptr->bit_flags & JOB_RESIZED) {
 		rebuild_step_bitmaps(job_ptr, orig_job_node_bitmap);
-		FREE_NULL_BITMAP(orig_job_node_bitmap);
 	}
+	FREE_NULL_BITMAP(orig_job_node_bitmap);
 
 	if ((IS_JOB_RUNNING(job_ptr) || IS_JOB_SUSPENDED(job_ptr)) &&
 	    (job_ptr->front_end_ptr != NULL))

commit b9dd7c4a5dae62d41e4b6f236faf5660a6228d17

Fix rebuilding step bitmaps of resized jobs
When a job is resized and an scontrol reconfigure follows the resize a
segfault happens since a null bitmap is passed to rebuild_step_bitmaps.
We need to copy and pass the original job node bitmap.

This fixes a regression from 51e3a60923.

Bug 18281

src/slurmctld/read_config.c
@@ -3271,12 +3271,14 @@ static int _sync_nodes_to_active_job(job_record_t *job_ptr)
 	int cnt = 0;
 	uint32_t node_flags;
 	node_record_t *node_ptr;
-	bitstr_t *node_bitmap, *orig_job_node_bitmap = NULL;
+	bitstr_t *node_bitmap, *orig_job_node_bitmap;
 
 	if (job_ptr->node_bitmap_cg) /* job completing */
 		node_bitmap = job_ptr->node_bitmap_cg;
 	else
 		node_bitmap = job_ptr->node_bitmap;
+
+	orig_job_node_bitmap = bit_copy(job_ptr->job_resrcs->node_bitmap);
 
 	job_ptr->node_cnt = bit_set_count(node_bitmap);
 	for (int i = 0; (node_ptr = next_node_bitmap(node_bitmap, &i)); i++) {
@@ -3325,15 +3327,6 @@ static int _sync_nodes_to_active_job(job_record_t *job_ptr)
 			int save_accounting_enforce;
 			info("Removing failed node %s from %pJ",
 			     node_ptr->name, job_ptr);
-
-			/*
-			 * Save the original node bitmap, which is needed for
-			 * rebuild_step_bitmaps below
-			 */
-			if (!orig_job_node_bitmap)
-				orig_job_node_bitmap =
-					bit_copy(job_ptr->job_resrcs->
-						 node_bitmap);
 
 			/*
 			 * Disable accounting here. Accounting reset for all