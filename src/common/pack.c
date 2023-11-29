6e59eedffa80c9699d748c60ac10f62b5194dc8f
Tim Wickberg <tim@schedmd.com>
2023-11-28 23:16:40 -0700

    Initialize *valp earlier.

--- src/common/pack.c
+++ src/common/pack.c
@@ -1059,6 +1059,7 @@ int unpackstr_array(char ***valp, uint32_t *size_valp, buf_t *buffer)
 	int i;
 	uint32_t uint32_tmp;
 
+	*valp = NULL;
 	safe_unpack32(size_valp, buffer);
 
 	if (*size_valp > 0) {
@@ -1070,8 +1071,7 @@ int unpackstr_array(char ***valp, uint32_t *size_valp, buf_t *buffer)
 				return SLURM_ERROR;
 			}
 		}
-	} else
-		*valp = NULL;
+	}
 	return SLURM_SUCCESS;
 
 unpack_error:
