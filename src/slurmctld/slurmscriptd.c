SchedMD/slurm

commit f3385b5cc589ea0cce4db1ec1771215a1006a9b2

Wait for scriptd to finish running scripts before killing the scriptd
Bug 14010

src/slurmctld/slurmscriptd.c
@@ -1082,11 +1082,23 @@ static int _script_cnt(void)
 static void _kill_slurmscriptd(void)
 {
 	int status;
+	int pc, last_pc = 0;
 
 	if (slurmscriptd_pid <= 0) {
 		error("%s: slurmscriptd_pid < 0, we don't know the PID of slurmscriptd.",
 		      __func__);
 		return;
+	}
+
+	slurmscriptd_flush();
+
+	/* Wait until all script complete messages have been processed. */
+	while ((pc = _script_cnt()) > 0) {
+		if ((last_pc != 0) && (last_pc != pc)) {
+			info("waiting for %d running processes", pc);
+		}
+		last_pc = pc;
+		usleep(100000);
 	}
 
 	/* Tell slurmscriptd to shutdown, then wait for it to finish. */
@@ -1536,19 +1548,8 @@ extern int slurmscriptd_init(int argc, char **argv)
 
 extern int slurmscriptd_fini(void)
 {
-	int pc, last_pc = 0;
-
 	debug("%s starting", __func__);
 	_kill_slurmscriptd();
-
-	/* Wait until all script complete messages have been processed. */
-	while ((pc = _script_cnt()) > 0) {
-		if ((last_pc != 0) && (last_pc != pc)) {
-			info("waiting for %d running processes", pc);
-		}
-		last_pc = pc;
-		usleep(100000);
-	}
 
 	/* Now shutdown communications. */
 	eio_signal_shutdown(msg_handle);

commit 419d798fc47619a2f687d02237aa01cb4fb5a045

Fix waiting on powersave scripts before shutting down
Fix a regression from commit f3385b5cc5:
slurmctld was waiting until powersave scripts were completely finished
before shutting down instead of waiting for a maximum of 10 seconds.

Explanation of the regression:
After slurmscriptd_flush() is called, the slurmctld waits for all scripts to
finish before telling slurmscriptd to shutdown. Waiting for scripts to
finish includes the powersave scripts. However, when slurmscriptd received
the SLURMSCRIPTD_REQUEST_FLUSH RPC, it called track_script_flush() which
does not affect the powersave scripts. So, the powersave scripts were never
told to finish or be orphaned (_wait_for_powersave_scripts), so the
slurmctld waited for the powersave scripts to finish.

The solution is to issue the SLURMSCRIPTD_REQUEST_SHUTDOWN RPC before
waiting for scripts. This RPC calls _wait_for_powersave_scripts, which
appropriately waits for up to 10 seconds and then orphans any remaining
powersave scripts.

Bug 14010

src/slurmctld/slurmscriptd.c
@@ -561,6 +561,11 @@ static int _handle_flush(slurmscriptd_msg_t *recv_msg)
 	log_flag(SCRIPT, "Handling %s", rpc_num2string(recv_msg->msg_type));
 	/* Kill all running scripts */
 	track_script_flush();
+	/*
+	 * DO NOT CALL _wait_for_powersave_scripts HERE. That would result in
+	 * reconfigure waiting for up to MAX_SHUTDOWN_DELAY seconds, which is
+	 * an unacceptably long time for reconfigure.
+	 */
 
 	/* We need to respond to slurmctld that we are done */
 	_respond_to_slurmctld(recv_msg->key, 0, NULL,
@@ -1112,13 +1117,16 @@ static void _kill_slurmscriptd(void)
 	shutting_down = true;
 	slurmscriptd_flush();
 
+	/* Tell slurmscriptd to shutdown, then wait for it to finish. */
+	rc = _send_to_slurmscriptd(SLURMSCRIPTD_SHUTDOWN, NULL, false, NULL,
+				   NULL);
 	/*
 	 * Wait until all script complete messages have been processed or until
 	 * the readfd is closed, in which case we know we'll never get more
 	 * messages from slurmscriptd.
 	 */
 	slurm_mutex_lock(&script_count_mutex);
-	while (slurmctld_readfd > 0) {
+	while ((rc == SLURM_SUCCESS) && (slurmctld_readfd > 0)) {
 		if (!script_count)
 			break;
 		if (last_pc != script_count)
@@ -1130,9 +1138,6 @@ static void _kill_slurmscriptd(void)
 	}
 	slurm_mutex_unlock(&script_count_mutex);
 
-	/* Tell slurmscriptd to shutdown, then wait for it to finish. */
-	rc = _send_to_slurmscriptd(SLURMSCRIPTD_SHUTDOWN, NULL, false, NULL,
-				  NULL);
 	if (rc != SLURM_SUCCESS) {
 		/* Shutdown signal failed. Try to reap slurmscriptd now. */
 		if (waitpid(slurmscriptd_pid, &status, WNOHANG) == 0) {