7ba85d424bbeaf8c1bbd13fa8641e5d6ec09312b
Nathan Rini <nate@schedmd.com>
2023-12-22 20:25:09 -0700

    data_parser/v0.0.41 - Remove pointer resolution in _set_openapi_parse()
    Pointer resolution already handled by _set_ref().
    
    Bug 17916

--- src/plugins/data_parser/v0.0.41/openapi.c
+++ src/plugins/data_parser/v0.0.41/openapi.c
@@ -255,6 +255,7 @@ static data_t *_set_openapi_parse(data_t *obj, const parser_t *parser,
 	xassert(sargs->magic == MAGIC_SPEC_ARGS);
 	xassert(sargs->args->magic == MAGIC_ARGS);
 	xassert(parser->model != PARSER_MODEL_ARRAY_SKIP_FIELD);
+	xassert(!parser->pointer_type);
 
 	if ((parser->model ==
 	     PARSER_MODEL_ARRAY_LINKED_EXPLODED_FLAG_ARRAY_FIELD) ||
@@ -264,10 +265,6 @@ static data_t *_set_openapi_parse(data_t *obj, const parser_t *parser,
 	} else if (parser->model == PARSER_MODEL_ARRAY_LINKED_FIELD) {
 		/* find all parsers that should be references */
 		_set_ref(obj, parser, find_parser_by_type(parser->type), sargs);
-		return NULL;
-	} else if (parser->pointer_type) {
-		_set_ref(obj, parser, find_parser_by_type(parser->pointer_type),
-			 sargs);
 		return NULL;
 	}
 
