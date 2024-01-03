SchedMD/slurm

commit 21c6f51dbb48aa25912cc294ee9916f665abff55

Change node_record_table_ptr to array of pointers
Bug 13679

src/slurmctld/node_mgr.c
@@ -156,8 +156,8 @@ int dump_all_node_state ( void )
 
 	/* write node records to buffer */
 	lock_slurmctld (node_read_lock);
-	for (inx = 0, node_ptr = node_record_table_ptr; inx < node_record_count;
-	     inx++, node_ptr++) {
+	for (inx = 0; inx < node_record_count; inx++) {
+		node_ptr = node_record_table_ptr[inx];
 		xassert (node_ptr->magic == NODE_MAGIC);
 		xassert (node_ptr->config_ptr->magic == CONFIG_MAGIC);
 		_dump_node_state (node_ptr, buffer);
@@ -824,7 +824,7 @@ extern void pack_all_node(char **buffer_ptr, int *buffer_size,
 	uint32_t nodes_packed, tmp_offset;
 	buf_t *buffer;
 	time_t now = time(NULL);
-	node_record_t *node_ptr = node_record_table_ptr;
+	node_record_t *node_ptr;
 	bool hidden, privileged = validate_operator(uid);
 	pack_node_info_t pack_info = {
 		.uid = uid,
@@ -846,7 +846,8 @@ extern void pack_all_node(char **buffer_ptr, int *buffer_size,
 		pack_time(now, buffer);
 
 		/* write node records */
-		for (inx = 0; inx < node_record_count; inx++, node_ptr++) {
+		for (inx = 0; inx < node_record_count; inx++) {
+			node_ptr = node_record_table_ptr[inx];
 			xassert(node_ptr->magic == NODE_MAGIC);
 			xassert(node_ptr->config_ptr->magic == CONFIG_MAGIC);
 
@@ -942,7 +943,7 @@ extern void pack_one_node (char **buffer_ptr, int *buffer_size,
 		if (node_name)
 			node_ptr = find_node_record(node_name);
 		else
-			node_ptr = node_record_table_ptr;
+			node_ptr = node_record_table_ptr[0];
 		if (node_ptr) {
 			hidden = false;
 			if (((show_flags & SHOW_ALL) == 0) &&
@@ -1860,8 +1861,8 @@ extern void restore_node_features(int recover)
 	node_record_t *node_ptr;
 
 	node_features_plugin_cnt = node_features_g_count();
-	for (i = 0, node_ptr = node_record_table_ptr; i < node_record_count;
-	     i++, node_ptr++) {
+	for (i = 0; i < node_record_count; i++) {
+		node_ptr = node_record_table_ptr[i];
 		if (node_ptr->weight != node_ptr->config_ptr->weight) {
 			error("Node %s Weight(%u) differ from slurm.conf",
 			      node_ptr->name, node_ptr->weight);
@@ -2197,7 +2198,7 @@ static int _update_node_gres(char *node_names, char *gres)
 		for (i = i_first; i <= i_last; i++) {
 			if (!bit_test(tmp_bitmap, i))
 				continue;	/* Not this node */
-			node_ptr = node_record_table_ptr + i;
+			node_ptr = node_record_table_ptr[i];
 			rc2 = gres_node_reconfig(
 				node_ptr->name,
 				gres, &node_ptr->gres,
@@ -2282,7 +2283,7 @@ static void _update_config_ptr(bitstr_t *bitmap, config_record_t *config_ptr)
 	for (i = 0; i < node_record_count; i++) {
 		if (!bit_test(bitmap, i))
 			continue;
-		node_record_table_ptr[i].config_ptr = config_ptr;
+		node_record_table_ptr[i]->config_ptr = config_ptr;
 	}
 }
 
@@ -3128,7 +3129,7 @@ extern int validate_nodes_via_front_end(
 	bool update_node_state = false;
 	job_record_t *job_ptr;
 	config_record_t *config_ptr;
-	node_record_t *node_ptr;
+	node_record_t *node_ptr = NULL;
 	time_t now = time(NULL);
 	ListIterator job_iterator;
 	hostlist_t reg_hostlist = NULL;
@@ -3174,10 +3175,9 @@ extern int validate_nodes_via_front_end(
 		}
 
 		job_ptr = find_job_record(reg_msg->step_id[i].job_id);
-		node_ptr = node_record_table_ptr;
 		if (job_ptr && job_ptr->node_bitmap &&
 		    ((j = bit_ffs(job_ptr->node_bitmap)) >= 0))
-			node_ptr += j;
+			node_ptr = node_record_table_ptr[j];
 
 		if (job_ptr == NULL) {
 			error("Orphan %ps reported on node %s",
@@ -3259,11 +3259,11 @@ extern int validate_nodes_via_front_end(
 	list_iterator_destroy(job_iterator);
 
 	(void) gres_node_config_unpack(reg_msg->gres_info,
-				       node_record_table_ptr->name);
-	for (i = 0, node_ptr = node_record_table_ptr; i < node_record_count;
-	     i++, node_ptr++) {
+				       node_record_table_ptr[i]->name);
+	for (i = 0; i < node_record_count; i++) {
 		bool acct_updated = false;
 
+		node_ptr = node_record_table_ptr[i];
 		config_ptr = node_ptr->config_ptr;
 		node_ptr->last_response = now;
 
@@ -3644,7 +3644,7 @@ extern void node_no_resp_msg(void)
 	hostlist_t no_resp_hostlist = NULL;
 
 	for (i = 0; i < node_record_count; i++) {
-		node_ptr = &node_record_table_ptr[i];
+		node_ptr = node_record_table_ptr[i];
 		if (!node_ptr->not_responding ||
 		    IS_NODE_POWERED_DOWN(node_ptr) ||
 		    IS_NODE_POWERING_DOWN(node_ptr) ||
@@ -3778,7 +3778,7 @@ node_record_t *find_first_node_record(bitstr_t *node_bitmap)
 	if (inx < 0)
 		return NULL;
 	else
-		return &node_record_table_ptr[inx];
+		return node_record_table_ptr[inx];
 }
 
 /*
@@ -3821,8 +3821,8 @@ void msg_to_slurmd (slurm_msg_type_t msg_type)
 		kill_agent_args->node_count++;
 	}
 #else
-	node_ptr = node_record_table_ptr;
-	for (i = 0; i < node_record_count; i++, node_ptr++) {
+	for (i = 0; i < node_record_count; i++) {
+		node_ptr = node_record_table_ptr[i];
 		if (IS_NODE_FUTURE(node_ptr))
 			continue;
 		if (IS_NODE_CLOUD(node_ptr) &&
@@ -3830,9 +3830,9 @@ void msg_to_slurmd (slurm_msg_type_t msg_type)
 		     IS_NODE_POWERING_DOWN(node_ptr)))
 			continue;
 		if (kill_agent_args->protocol_version >
-		    node_record_table_ptr[i].protocol_version)
+		    node_record_table_ptr[i]->protocol_version)
 			kill_agent_args->protocol_version =
-				node_record_table_ptr[i].protocol_version;
+				node_record_table_ptr[i]->protocol_version;
 		hostlist_push_host(kill_agent_args->hostlist, node_ptr->name);
 		kill_agent_args->node_count++;
 	}
@@ -3899,8 +3899,8 @@ void push_reconfig_to_slurmd(char **slurmd_config_files)
 	load_config_response_msg(old_config, CONFIG_REQUEST_SLURMD);
 	old_args->msg_args = old_config;
 
-	node_ptr = node_record_table_ptr;
-	for (int i = 0; i < node_record_count; i++, node_ptr++) {
+	for (int i = 0; i < node_record_count; i++) {
+		node_ptr = node_record_table_ptr[i];
 		if (IS_NODE_FUTURE(node_ptr))
 			continue;
 		if (IS_NODE_CLOUD(node_ptr) &&
@@ -4282,8 +4282,8 @@ extern int send_nodes_to_accounting(time_t event_time)
 
  	lock_slurmctld(node_read_lock);
 	/* send nodes not in 'up' state */
-	node_ptr = node_record_table_ptr;
-	for (i = 0; i < node_record_count; i++, node_ptr++) {
+	for (i = 0; i < node_record_count; i++) {
+		node_ptr = node_record_table_ptr[i];
 		if (!node_ptr->name)
 			continue;
 		if (node_ptr->reason)
@@ -4385,7 +4385,7 @@ extern void check_reboot_nodes()
 	}
 
 	for (i = 0; i < node_record_count; i++) {
-		node_ptr = &node_record_table_ptr[i];
+		node_ptr = node_record_table_ptr[i];
 
 		if ((IS_NODE_REBOOT_ISSUED(node_ptr) ||
 		     (!power_save_on && IS_NODE_POWERING_UP(node_ptr))) &&

src/common/gres.c
@@ -6106,7 +6106,7 @@ extern int gres_job_revalidate2(uint32_t job_id, List job_gres_list,
 	for (i = i_first; i <= i_last; i++) {
 		if (!bit_test(node_bitmap, i))
 			continue;
-		node_ptr = node_record_table_ptr + i;
+		node_ptr = node_record_table_ptr[i];
 		node_inx++;
 		if (!_validate_node_gres_cnt(job_id, job_gres_list, node_inx,
 					     node_ptr->gres_list,

src/common/job_resources.c
@@ -73,19 +73,19 @@ extern job_resources_t *create_job_resources(void)
  * rc = build_job_resources(job_resrcs_ptr, node_record_table_ptr);
  */
 extern int build_job_resources(job_resources_t *job_resrcs,
-			       void *node_rec_table)
+			       node_record_t **node_rec_table)
 {
 	int i, bitmap_len;
 	int core_cnt = 0, sock_inx = -1;
 	uint32_t cores, socks;
-	node_record_t *node_ptr, *node_record_table;
+	node_record_t *node_ptr, **node_record_table;
 
 	if (job_resrcs->node_bitmap == NULL) {
 		error("build_job_resources: node_bitmap is NULL");
 		return SLURM_ERROR;
 	}
 
-	node_record_table = (node_record_t *) node_rec_table;
+	node_record_table = node_rec_table;
 	xfree(job_resrcs->sockets_per_node);
 	xfree(job_resrcs->cores_per_socket);
 	xfree(job_resrcs->sock_core_rep_count);
@@ -100,7 +100,7 @@ extern int build_job_resources(job_resources_t *job_resrcs,
 	for (i=0; i<bitmap_len; i++) {
 		if (!bit_test(job_resrcs->node_bitmap, i))
 			continue;
-		node_ptr = node_record_table + i;
+		node_ptr = node_record_table[i];
 
 		socks = node_ptr->config_ptr->tot_sockets;
 		cores = node_ptr->config_ptr->cores;
@@ -270,13 +270,13 @@ extern int reset_node_bitmap(void *void_job_ptr)
 }
 
 extern int valid_job_resources(job_resources_t *job_resrcs,
-			       void *node_rec_table)
+			       node_record_t **node_rec_table)
 {
 	int i, bitmap_len;
 	int sock_inx = 0, sock_cnt = 0;
 	int total_job_cores, total_node_cores;
 	uint32_t cores, socks;
-	node_record_t *node_ptr, *node_record_table;
+	node_record_t *node_ptr, **node_record_table;
 
 	if (job_resrcs->node_bitmap == NULL) {
 		error("valid_job_resources: node_bitmap is NULL");
@@ -289,12 +289,12 @@ extern int valid_job_resources(job_resources_t *job_resrcs,
 		return SLURM_ERROR;
 	}
 
-	node_record_table = (node_record_t *) node_rec_table;
+	node_record_table = node_rec_table;
 	bitmap_len = bit_size(job_resrcs->node_bitmap);
 	for (i=0; i<bitmap_len; i++) {
 		if (!bit_test(job_resrcs->node_bitmap, i))
 			continue;
-		node_ptr = node_record_table + i;
+		node_ptr = node_record_table[i];
 
 		socks = node_ptr->config_ptr->tot_sockets;
 		cores = node_ptr->config_ptr->cores;
@@ -1981,8 +1981,8 @@ extern uint16_t job_resources_get_node_cpu_cnt(job_resources_t *job_resrcs_ptr,
 	if (((job_resrcs_ptr->cr_type & CR_CORE) ||
 	     (job_resrcs_ptr->cr_type & CR_SOCKET)) &&
 	    (job_resrcs_ptr->threads_per_core <
-	     node_record_table_ptr[sys_node_inx].vpus)) {
-		cpu_count /= node_record_table_ptr[sys_node_inx].vpus;
+	     node_record_table_ptr[sys_node_inx]->vpus)) {
+		cpu_count /= node_record_table_ptr[sys_node_inx]->vpus;
 		cpu_count *= job_resrcs_ptr->threads_per_core;
 	}
 

src/common/job_resources.h
@@ -165,7 +165,7 @@ extern job_resources_t *create_job_resources(void);
  * rc = build_job_resources(job_resrcs_ptr, node_record_table_ptr);
  */
 extern int build_job_resources(job_resources_t *job_resrcs_ptr,
-			       void *node_rec_table);
+			       node_record_t **node_rec_table);
 
 /* Rebuild cpu_array_cnt, cpu_array_value, and cpu_array_reps based upon the
  * values of cpus in an existing data structure
@@ -188,7 +188,7 @@ extern int build_job_resources_cpus_array(job_resources_t *job_resrcs_ptr);
  * rc = valid_job_resources(job_resrcs_ptr, node_record_table_ptr);
  */
 extern int valid_job_resources(job_resources_t *job_resrcs_ptr,
-			       void *node_rec_table);
+			       node_record_t **node_rec_table);
 
 /* Make a copy of a job_resources data structure,
  * free using free_job_resources() */

src/common/node_conf.c
@@ -83,8 +83,9 @@ strong_alias(hostlist2bitmap, slurm_hostlist2bitmap);
 List config_list  = NULL;	/* list of config_record entries */
 List front_end_list = NULL;	/* list of slurm_conf_frontend_t entries */
 time_t last_node_update = (time_t) 0;	/* time of last update */
-node_record_t *node_record_table_ptr = NULL;	/* node records */
+node_record_t **node_record_table_ptr = NULL;	/* node records */
 xhash_t* node_hash_table = NULL;
+int node_record_table_size = 0;		/* size of node_record_table_ptr */
 int node_record_count = 0;		/* count in node_record_table_ptr */
 uint16_t *cr_node_num_cores = NULL;
 uint32_t *cr_node_cores_offset = NULL;
@@ -196,7 +197,7 @@ hostlist_t bitmap2hostlist (bitstr_t *bitmap)
 	for (i = first; i <= last; i++) {
 		if (bit_test(bitmap, i) == 0)
 			continue;
-		hostlist_push_host(hl, node_record_table_ptr[i].name);
+		hostlist_push_host(hl, node_record_table_ptr[i]->name);
 	}
 	return hl;
 
@@ -734,6 +735,18 @@ static void _init_node_record(node_record_t *node_ptr,
 		(node_ptr->core_spec_cnt * node_ptr->vpus);
 }
 
+extern void grow_node_record_table_ptr(void)
+{
+	node_record_table_size = node_record_count + 100;
+	xrealloc(node_record_table_ptr,
+		 node_record_table_size * sizeof(node_record_t *));
+	/*
+	 * You need to rehash the hash after we realloc or we will have
+	 * only bad memory references in the hash.
+	 */
+	rehash_node();
+}
+
 /*
  * create_node_record - create a node record and set its values to defaults
  * IN config_ptr - pointer to node's configuration information
@@ -746,31 +759,16 @@ extern node_record_t *create_node_record(config_record_t *config_ptr,
 					 char *node_name)
 {
 	node_record_t *node_ptr;
-	int old_buffer_size, new_buffer_size;
 
 	last_node_update = time (NULL);
 	xassert(config_ptr);
 	xassert(node_name);
 
-	/* round up the buffer size to reduce overhead of xrealloc */
-	old_buffer_size = (node_record_count) * sizeof(node_record_t);
-	old_buffer_size =
-		((int) ((old_buffer_size / BUF_SIZE) + 1)) * BUF_SIZE;
-	new_buffer_size =
-		(node_record_count + 1) * sizeof(node_record_t);
-	new_buffer_size =
-		((int) ((new_buffer_size / BUF_SIZE) + 1)) * BUF_SIZE;
-	if (!node_record_table_ptr) {
-		node_record_table_ptr = xmalloc(new_buffer_size);
-	} else if (old_buffer_size != new_buffer_size) {
-		xrealloc (node_record_table_ptr, new_buffer_size);
-		/*
-		 * You need to rehash the hash after we realloc or we will have
-		 * only bad memory references in the hash.
-		 */
-		rehash_node();
-	}
-	node_ptr = node_record_table_ptr + node_record_count;
+	if (node_record_count >= node_record_table_size)
+		grow_node_record_table_ptr();
+
+	node_record_table_ptr[node_record_count] = xmalloc(sizeof(*node_ptr));
+	node_ptr = node_record_table_ptr[node_record_count];
 	node_ptr->index = node_record_count++;
 	node_ptr->name = xstrdup(node_name);
 	if (!node_hash_table)
@@ -844,8 +842,8 @@ static node_record_t *_find_node_record(char *name, bool test_alias,
 	}
 
 	if ((node_record_count == 1) &&
-	    (xstrcmp(node_record_table_ptr[0].name, "localhost") == 0))
-		return (&node_record_table_ptr[0]);
+	    (xstrcmp(node_record_table_ptr[0]->name, "localhost") == 0))
+		return (node_record_table_ptr[0]);
 
 	if (log_missing)
 		error("%s: lookup failure for node \"%s\"",
@@ -880,9 +878,10 @@ extern void init_node_conf(void)
 	int i;
 	node_record_t *node_ptr;
 
-	node_ptr = node_record_table_ptr;
-	for (i = 0; i < node_record_count; i++, node_ptr++)
+	for (i = 0; i < node_record_count; i++) {
+		node_ptr = node_record_table_ptr[i];
 		purge_node_rec(node_ptr);
+	}
 
 	node_record_count = 0;
 	xfree(node_record_table_ptr);
@@ -909,9 +908,10 @@ extern void node_fini2 (void)
 	}
 
 	xhash_free(node_hash_table);
-	node_ptr = node_record_table_ptr;
-	for (i = 0; i < node_record_count; i++, node_ptr++)
+	for (i = 0; i < node_record_count; i++) {
+		node_ptr = node_record_table_ptr[i];
 		purge_node_rec(node_ptr);
+	}
 
 	xfree(node_record_table_ptr);
 	node_record_count = 0;
@@ -1054,11 +1054,12 @@ extern void purge_node_rec(node_record_t *node_ptr)
 extern void rehash_node (void)
 {
 	int i;
-	node_record_t *node_ptr = node_record_table_ptr;
+	node_record_t *node_ptr;
 
 	xhash_free (node_hash_table);
 	node_hash_table = xhash_init(_node_record_hash_identity, NULL);
-	for (i = 0; i < node_record_count; i++, node_ptr++) {
+	for (i = 0; i < node_record_count; i++) {
+		node_ptr = node_record_table_ptr[i];
 		if ((node_ptr->name == NULL) ||
 		    (node_ptr->name[0] == '\0'))
 			continue;	/* vestigial record */
@@ -1102,7 +1103,7 @@ extern int state_str2int(const char *state_str, char *node_name)
 }
 
 /* (re)set cr_node_num_cores arrays */
-extern void cr_init_global_core_data(node_record_t *node_ptr, int node_cnt)
+extern void cr_init_global_core_data(node_record_t **node_ptr, int node_cnt)
 {
 	uint32_t n;
 
@@ -1112,8 +1113,8 @@ extern void cr_init_global_core_data(node_record_t *node_ptr, int node_cnt)
 	cr_node_cores_offset = xmalloc((node_cnt+1) * sizeof(uint32_t));
 
 	for (n = 0; n < node_cnt; n++) {
-		uint16_t cores = node_ptr[n].config_ptr->cores;
-		cores *= node_ptr[n].config_ptr->tot_sockets;
+		uint16_t cores = node_ptr[n]->config_ptr->cores;
+		cores *= node_ptr[n]->config_ptr->tot_sockets;
 
 		cr_node_num_cores[n] = cores;
 		if (n > 0) {

src/common/node_conf.h
@@ -187,7 +187,7 @@ struct node_record {
 					 * save/restore, DO NOT use for
 					 * scheduling purposes. */
 };
-extern node_record_t *node_record_table_ptr;  /* ptr to node records */
+extern node_record_t **node_record_table_ptr;  /* ptr to node records */
 extern int node_record_count;		/* count in node_record_table_ptr */
 extern xhash_t* node_hash_table;	/* hash table for node records */
 extern time_t last_node_update;		/* time of last node record update */
@@ -266,6 +266,11 @@ extern void check_nodeline_info(slurm_conf_node_t *node_ptr,
  *	_delete_config_record() is called to delete all configuration records
  */
 extern config_record_t *create_config_record(void);
+
+/*
+ * Grow the node_record_table_ptr.
+ */
+extern void grow_node_record_table_ptr();
 
 /*
  * create_node_record - create a node record and set its values to defaults
@@ -352,7 +357,7 @@ extern void rehash_node (void);
 extern int state_str2int(const char *state_str, char *node_name);
 
 /* (re)set cr_node_num_cores arrays */
-extern void cr_init_global_core_data(node_record_t *node_ptr, int node_cnt);
+extern void cr_init_global_core_data(node_record_t **node_ptr, int node_cnt);
 
 extern void cr_fini_global_core_data(void);
 

src/common/select.c
@@ -458,7 +458,7 @@ extern int select_g_job_init(List job_list)
  * IN node_ptr - current node data
  * IN node_count - number of node entries
  */
-extern int select_g_node_init(node_record_t *node_ptr, int node_cnt)
+extern int select_g_node_init(node_record_t **node_ptr, int node_cnt)
 {
 	if (select_g_init(0) < 0)
 		return SLURM_ERROR;

src/common/select.h
@@ -69,7 +69,7 @@ typedef struct {
 	int		(*state_save)		(char *dir_name);
 	int		(*state_restore)	(char *dir_name);
 	int		(*job_init)		(List job_list);
-	int		(*node_init)		(node_record_t *node_ptr,
+	int		(*node_init)		(node_record_t **node_ptr,
 						 int node_cnt);
 	int		(*job_test)		(job_record_t *job_ptr,
 						 bitstr_t *bitmap,
@@ -217,7 +217,7 @@ extern int select_g_state_restore(char *dir_name);
  * IN node_ptr - current node data
  * IN node_count - number of node entries
  */
-extern int select_g_node_init(node_record_t *node_ptr, int node_cnt);
+extern int select_g_node_init(node_record_t **node_ptr, int node_cnt);
 
 /*
  * Note the initialization of job records, issued upon restart of

commit bf8bde7ca6508adf354f8a789629ca83090409dd

Fix segv in node validation on frontend node
Wrong node_record_table_ptr table index was used to initialize gres_info
structure leading to frequent SEGV when validating nodes with a frontend
node. At this stage, the value of i is a number of jobs. Bug was
introduced in 21c6f51.

Bug 18555

src/slurmctld/node_mgr.c
@@ -3770,7 +3770,7 @@ extern int validate_nodes_via_front_end(
 	list_iterator_destroy(job_iterator);
 
 	(void) gres_node_config_unpack(reg_msg->gres_info,
-				       node_record_table_ptr[i]->name);
+				       node_record_table_ptr[0]->name);
 	for (i = 0; (node_ptr = next_node(&i)); i++) {
 		bool acct_updated = false;
 