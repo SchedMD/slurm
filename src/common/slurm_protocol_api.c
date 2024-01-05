71f643a720d0bac64c0ba2e749c864e4964e299f
Marcin Stolarek <cinek@schedmd.com>
2024-01-04 17:00:28 -0700

    Limit scope for loop variable.
    Bug 18280.

--- src/common/slurm_protocol_api.c
+++ src/common/slurm_protocol_api.c
@@ -131,7 +131,6 @@ static slurm_protocol_config_t *_slurm_api_get_comm_config(void)
 	slurm_protocol_config_t *proto_conf = NULL;
 	slurm_addr_t controller_addr;
 	slurm_conf_t *conf;
-	int i;
 
 	conf = slurm_conf_lock();
 
@@ -164,7 +163,7 @@ static slurm_protocol_config_t *_slurm_api_get_comm_config(void)
 	memcpy(&proto_conf->controller_addr[0], &controller_addr,
 	       sizeof(slurm_addr_t));
 
-	for (i = 1; i < proto_conf->control_cnt; i++) {
+	for (int i = 1; i < proto_conf->control_cnt; i++) {
 		if (conf->control_addr[i]) {
 			slurm_set_addr(&proto_conf->controller_addr[i],
 				       conf->slurmctld_port,
