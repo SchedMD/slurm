SchedMD/slurm

commit 94e7f8731eda689be05e357bb58553e3bbf2c7f3

Manage the packed job info as a buf_t instead.

src/plugins/burst_buffer/lua/burst_buffer_lua.c
@@ -928,6 +928,7 @@ static int _start_lua_script(char *func, uint32_t job_id, uint32_t argc,
 static int _run_lua_script(run_lua_args_t *args)
 {
 	int rc;
+	buf_t *info_buf = NULL;
 	int job_buf_size = 0;
 	char *job_buf = NULL;
 	List job_ids = NULL;
@@ -963,6 +964,7 @@ static int _run_lua_script(run_lua_args_t *args)
 		pack_spec_jobs(&job_buf, &job_buf_size, job_ids, SHOW_DETAIL,
 			       slurm_conf.slurm_user_id, NO_VAL,
 			       SLURM_PROTOCOL_VERSION);
+		info_buf = create_buf(job_buf, job_buf_size);
 
 		if (!args->have_job_lock)
 			unlock_slurmctld(job_read_lock);
@@ -975,32 +977,22 @@ static int _run_lua_script(run_lua_args_t *args)
 					     args->argc,
 					     args->argv,
 					     args->timeout,
-					     job_buf,
-					     job_buf_size,
+					     info_buf->head,
+					     info_buf->processed,
 					     args->resp_msg,
 					     args->track_script_signal);
 	} else {
 		job_info_msg_t *job_info = NULL;
 
-		if (job_buf) {
-			buf_t *info_buf;
+		if (info_buf) {
 			slurm_msg_t *info_msg = xmalloc(sizeof *info_msg);
 
 			slurm_msg_t_init(info_msg);
 			info_msg->protocol_version = SLURM_PROTOCOL_VERSION;
 			info_msg->msg_type = RESPONSE_JOB_INFO;
-			info_buf = create_buf(job_buf, job_buf_size);
 			unpack_msg(info_msg, info_buf);
 			job_info = info_msg->data;
 			info_msg->data = NULL;
-
-			/*
-			 * create_buf() does not duplicate the data, just
-			 * points to it.  So just NULL it out here. It will get
-			 * free'd later.
-			 */
-			info_buf->head = NULL;
-			FREE_NULL_BUFFER(info_buf);
 			slurm_free_msg(info_msg);
 		}
 		rc = _start_lua_script(args->lua_func, args->job_id, args->argc,
@@ -1010,7 +1002,7 @@ static int _run_lua_script(run_lua_args_t *args)
 	_decr_lua_thread_cnt();
 
 	FREE_NULL_LIST(job_ids);
-	xfree(job_buf);
+	FREE_NULL_BUFFER(info_buf);
 
 	return rc;
 }

commit 1b79427932dda7c7a8ec6f3933db8686cb052074

burst_buffer/lua - Fix passing job_info directly from slurmctld
This is a regression from commit 94e7f8731ed, which was part of a series
of refactoring commits:

94e7f8731ed
3203a0ed13a
17e808a3f86

Before commit 94e7f8731ed, the buffer was recreated from the data with
create_buf(). That call initializes the buffer's "processed" to zero,
allowing the buffer to be unpacked from the beginning.
After commit 94e7f8731ed, when calling burst_buffer.lua directly from
slurmctld (not through slurmscriptd), the buffer was reused and unpacked
immediately after being packed without resetting "processed" back to zero.
This resulted in unpacking from the end of the packed data in the buffer
because unpacking happens from "head + processed". The job_info was
therefore invalid and attempts to access it from burst_buffer.lua resulted
in a slurmctld segfault.

We need to reset the buffer's "processed" field to zero before unpacking it.

Bug 18603

src/plugins/burst_buffer/lua/burst_buffer_lua.c
@@ -986,6 +986,12 @@ static int _run_lua_script(run_lua_args_t *args)
 			slurm_msg_t_init(info_msg);
 			info_msg->protocol_version = SLURM_PROTOCOL_VERSION;
 			info_msg->msg_type = RESPONSE_JOB_INFO;
+			/*
+			 * Since we are directly unpacking the buffer that we
+			 * just packed, we need to reset the "processed" value
+			 * of the buffer because unpacking starts from there.
+			 */
+			set_buf_offset(info_buf, 0);
 			unpack_msg(info_msg, info_buf);
 			job_info = info_msg->data;
 			info_msg->data = NULL;