SchedMD/slurm

commit c87d89c988071e3772a7fdb887d41385c57fd86b

slurmstepd - catch spank_task_post_fork() failure in _spawn_job_container()
Cancel spawning the job if the SPANK spank_task_post_fork() function
fails instead of ignoring it.

Bug 7573

src/slurmd/slurmstepd/mgr.c
@@ -1178,6 +1178,8 @@ static int _spawn_job_container(stepd_step_rec_t *step)
 
 	if (spank_task_post_fork(step, -1) < 0)
 		error("spank extern task post-fork failed");
+		goto fail1;
+	}
 
 	/* let the slurmd know we actually are done with the setup */
 	close_slurmd_conn(rc);

commit 0986de140c0902c76ec19d040206d93404638780

Fix extern step cleaning up after slurm_spank_task_post_fork failure
The previous "goto fail1" would skip the proctrack plugin calls that killed
the sleep process, as well as other function calls that did cleanup work.
This was a regression from commit c87d89c988.  The changes in commit
c87d89c988 make the additional goto unnecessary, and simply removing it
allows the step to clean up correctly.

Bug 18611

src/slurmd/slurmstepd/mgr.c
@@ -1194,13 +1194,13 @@ static int _spawn_job_container(stepd_step_rec_t *step)
 		for (uint32_t i = 0; i < step->node_tasks; i++)
 			if (step->task[i]->estatus <= 0)
 				step->task[i]->estatus = W_EXITCODE(1, 0);
-
-		/* let the slurmd know the setup failed */
-		close_slurmd_conn(rc);
-		goto fail1;
 	}
 
-	/* let the slurmd know we actually are done with the setup */
+	/*
+	 * Tell slurmd the setup status; slurmd will handle a failure and
+	 * cleanup the sleep task launched above, so we do not need to do
+	 * anything special here to handle a setup failure.
+	 */
 	close_slurmd_conn(rc);
 
 	while ((wait4(pid, &status, 0, &rusage) < 0) && (errno == EINTR)) {