SchedMD/slurm

commit bb86a63fbda76104469fcc1e185a11387b09aed0

Allocate shared version of gres on --exclusive requests if it requested
Bug 18590

src/slurmctld/gres_ctld.c
@@ -999,12 +999,27 @@ extern int gres_ctld_job_select_whole_node(
 		if (!gres_ns->gres_cnt_config)
 			continue;
 
-		/* Never allocate any shared GRES. */
-		if (gres_id_shared(gres_state_node->config_flags))
-			continue;
-
 		if (gres_state_node->config_flags & GRES_CONF_EXPLICIT)
 			continue;
+
+		/* Select shared GRES if requested */
+		if (gres_id_shared(gres_state_node->config_flags)) {
+			/*
+			 * If we find it, delete it and add back to the list as
+			 * a whole node selection.
+			 * This is because we didn't delete it in
+			 * _handle_explicit_req() in node_scheduler.c
+			 */
+			if (!list_delete_first(*job_gres_list, gres_find_id,
+					       &gres_state_node->plugin_id))
+				continue;
+		}
+		/* If we select the shared gres don't select sharing gres */
+		if (gres_id_sharing(gres_state_node->plugin_id)) {
+			if (list_find_first(*job_gres_list, gres_find_id,
+					    &(gres_ns->alt_gres->plugin_id)))
+				continue;
+		}
 
 		job_search_key.config_flags = gres_state_node->config_flags;
 		job_search_key.plugin_id = gres_state_node->plugin_id;
@@ -1183,9 +1198,18 @@ extern int gres_ctld_job_alloc_whole_node(
 		if (!gres_ns->gres_cnt_config)
 			continue;
 
-		/* Never allocate any shared GRES. */
-		if (gres_id_shared(gres_state_node->config_flags))
-			continue;
+		/* Allocate shared GRES if requested */
+		if (gres_id_shared(gres_state_node->config_flags)) {
+			if (!list_find_first(job_gres_list, gres_find_id,
+					       &gres_state_node->plugin_id))
+				continue;
+		}
+		/* If we allocate the shared gres don't allocate sharing gres */
+		if (gres_id_sharing(gres_state_node->plugin_id)) {
+			if (list_find_first(job_gres_list, gres_find_id,
+					    &(gres_ns->alt_gres->plugin_id)))
+				continue;
+		}
 
 		if (gres_state_node->config_flags & GRES_CONF_EXPLICIT) {
 			if (job_gres_list) {

src/slurmctld/node_scheduler.c
@@ -2264,7 +2264,8 @@ static void _handle_explicit_req(void *x, void *arg)
 	list_t **ret_gres_list = arg;
 
 	/* Copy over the explicit gres, skip others */
-	if (!(gres_state_job->config_flags & GRES_CONF_EXPLICIT))
+	if (!(gres_state_job->config_flags & GRES_CONF_EXPLICIT) &&
+	    !gres_id_shared(gres_state_job->config_flags))
 		return;
 
 	if (!*ret_gres_list)

commit 7111021d4efe2b464d18f99b4d196813a8dbb439

Fix segfault when exclusively allocating gpus with no shards
This bug was introduced in bb86a63fbd

Bug 18857

src/slurmctld/gres_ctld.c
@@ -1015,7 +1015,8 @@ extern int gres_ctld_job_select_whole_node(
 				continue;
 		}
 		/* If we select the shared gres don't select sharing gres */
-		if (gres_id_sharing(gres_state_node->plugin_id)) {
+		if (gres_ns->alt_gres &&
+		    gres_id_sharing(gres_state_node->plugin_id)) {
 			if (list_find_first(*job_gres_list, gres_find_id,
 					    &(gres_ns->alt_gres->plugin_id)))
 				continue;
@@ -1205,7 +1206,8 @@ extern int gres_ctld_job_alloc_whole_node(
 				continue;
 		}
 		/* If we allocate the shared gres don't allocate sharing gres */
-		if (gres_id_sharing(gres_state_node->plugin_id)) {
+		if (gres_ns->alt_gres &&
+		    gres_id_sharing(gres_state_node->plugin_id)) {
 			if (list_find_first(job_gres_list, gres_find_id,
 					    &(gres_ns->alt_gres->plugin_id)))
 				continue;