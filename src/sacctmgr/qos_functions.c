6258c14cd2298391a66fca6ea026f3e4a3359f5b
Ben Glines <ben.glines@schedmd.com>
2024-02-16 16:08:01 -0700

    Fix parsing for MaxTRESRunMinsPU string in sacctmgr
    Rather than attempt to accept abbreviated inputs for MaxTRESRunMinsPU,
    match the entire length of the string.
    
    Bug 17896

--- src/sacctmgr/qos_functions.c
+++ src/sacctmgr/qos_functions.c
@@ -590,7 +590,7 @@ static int _set_rec(int *start, int argc, char **argv,
 			} else
 				exit_code = 1;
 		} else if (!xstrncasecmp(argv[i], "MaxTRESRunMinsPU",
-					 MAX(command_len, 8))) {
+					 MAX(command_len, 16))) {
 			sacctmgr_initialize_g_tres_list();
 
 			if ((tmp_char = slurmdb_format_tres_str(
