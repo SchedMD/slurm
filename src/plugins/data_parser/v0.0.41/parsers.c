SchedMD/slurm

commit fde5365bbb1e3b295a5c39c03d7f17eb72223e96

data_parser/v0.0.41 - disable DATA_PARSER_SELECT_PLUGIN_ID
Continuation of 74a635fef3.

Bug 18435

src/plugins/data_parser/v0.0.41/parsers.c
@@ -1493,34 +1493,9 @@ static int DUMP_FUNC(RPC_ID)(const parser_t *const parser, void *obj,
 	return SLURM_SUCCESS;
 }
 
-static int PARSE_FUNC(SELECT_PLUGIN_ID)(const parser_t *const parser, void *obj,
-					data_t *src, args_t *args,
-					data_t *parent_path)
-{
-	int *id = obj;
 
-	if (data_get_type(src) == DATA_TYPE_NULL)
-		return ESLURM_PLUGIN_INVALID;
-	else if (data_convert_type(src, DATA_TYPE_STRING) == DATA_TYPE_STRING &&
-		 (*id = select_string_to_plugin_id(data_get_string(src)) > 0))
-		return SLURM_SUCCESS;
-
-	return ESLURM_DATA_CONV_FAILED;
-}
-
-static int DUMP_FUNC(SELECT_PLUGIN_ID)(const parser_t *const parser, void *obj,
-				       data_t *dst, args_t *args)
-{
-	int *id = obj;
-	char *s = select_plugin_id_to_string(*id);
-
-	if (s) {
-		data_set_string(dst, s);
-	} else
-		data_set_string(dst, "");
-
-	return SLURM_SUCCESS;
-}
+PARSE_DISABLED(SELECT_PLUGIN_ID)
+DUMP_DISABLED(SELECT_PLUGIN_ID)
 
 PARSE_DISABLED(TASK_DISTRIBUTION)
 

commit 08688e93cf81d3d6ac3528e74fff23f8ecf6b4e3

data_parser/v0.0.41 - Avoid aborting when a invoked DUMP_DISABLED()
Fixing regression in commit fde5365bbb.

Bug 18619

src/plugins/data_parser/v0.0.41/parsers.c
@@ -103,8 +103,7 @@
 	static int DUMP_FUNC(type)(const parser_t *const parser, void *src,  \
 				    data_t *dst, args_t *args)               \
 	{                                                                    \
-		fatal_abort("dumping of DATA_PARSER_%s is not implemented",  \
-			    XSTRINGIFY(type));                               \
+		return DUMP_FUNC(disabled)(parser, src, dst, args);	     \
 	}
 
 #define parse_error(parser, args, parent_path, error, fmt, ...)    \
@@ -127,6 +126,16 @@ static int PARSE_FUNC(disabled)(const parser_t *const parser, void *src,
 
 	xfree(path);
 	return rc;
+}
+
+static int DUMP_FUNC(disabled)(const parser_t *const parser, void *src,
+			       data_t *dst, args_t *args)
+{
+	on_warn(DUMPING, parser->type, args, NULL, __func__,
+		"data_parser/v0.0.41 does not support parser %u for dumping. Output may be incomplete.",
+		parser->type);
+
+	return SLURM_SUCCESS;
 }
 
 static int _parse_error_funcname(const parser_t *const parser, args_t *args,