SchedMD/slurm

commit e34277f15d3af929720bf9af6c96e5f6ccc55161

Simplify checks in _validate_and_set_defaults
Set the default just once and not after 3 conditionals.

Bug 17677

src/common/read_config.c
@@ -4386,14 +4386,14 @@ static int _validate_and_set_defaults(slurm_conf_t *conf,
 
 	(void) s_p_get_string(&conf->licenses, "Licenses", hashtbl);
 
+	/* Default log format */
+	conf->log_fmt = LOG_FMT_ISO8601_MS;
 	if (s_p_get_string(&temp_str, "LogTimeFormat", hashtbl)) {
 		/*
 		 * If adding to this please update src/api/config_log.c to do
 		 * the reverse translation.
 		 */
-		if (xstrcasestr(temp_str, "iso8601_ms"))
-			conf->log_fmt = LOG_FMT_ISO8601_MS;
-		else if (xstrcasestr(temp_str, "iso8601"))
+		if (xstrcasestr(temp_str, "iso8601"))
 			conf->log_fmt = LOG_FMT_ISO8601;
 		else if (xstrcasestr(temp_str, "rfc5424_ms"))
 			conf->log_fmt = LOG_FMT_RFC5424_MS;
@@ -4407,15 +4407,10 @@ static int _validate_and_set_defaults(slurm_conf_t *conf,
 			conf->log_fmt = LOG_FMT_SHORT;
 		else if (xstrcasestr(temp_str, "thread_id"))
 			conf->log_fmt = LOG_FMT_THREAD_ID;
-
-		if (xstrcasestr(temp_str, "format_stderr")) {
-			if (!conf->log_fmt)
-				conf->log_fmt = LOG_FMT_ISO8601_MS;
+		if (xstrcasestr(temp_str, "format_stderr"))
 			conf->log_fmt |= LOG_FMT_FORMAT_STDERR;
-		}
 		xfree(temp_str);
-	} else
-		conf->log_fmt = LOG_FMT_ISO8601_MS;
+	}
 
 	(void) s_p_get_string(&conf->mail_domain, "MailDomain", hashtbl);
 

src/slurmdbd/read_config.c
@@ -347,10 +347,10 @@ extern int read_slurmdbd_conf(void)
 			xfree(temp_str);
 		}
 
+		/* Default log time format */
+		slurm_conf.log_fmt = LOG_FMT_ISO8601_MS;
 		if (s_p_get_string(&temp_str, "LogTimeFormat", tbl)) {
-			if (xstrcasestr(temp_str, "iso8601_ms"))
-				slurm_conf.log_fmt = LOG_FMT_ISO8601_MS;
-			else if (xstrcasestr(temp_str, "iso8601"))
+			if (xstrcasestr(temp_str, "iso8601"))
 				slurm_conf.log_fmt = LOG_FMT_ISO8601;
 			else if (xstrcasestr(temp_str, "rfc5424_ms"))
 				slurm_conf.log_fmt = LOG_FMT_RFC5424_MS;
@@ -364,15 +364,10 @@ extern int read_slurmdbd_conf(void)
 				slurm_conf.log_fmt = LOG_FMT_SHORT;
 			else if (xstrcasestr(temp_str, "thread_id"))
 				slurm_conf.log_fmt = LOG_FMT_THREAD_ID;
-
-			if (xstrcasestr(temp_str, "format_stderr")) {
-				if (!slurm_conf.log_fmt)
-					slurm_conf.log_fmt = LOG_FMT_ISO8601_MS;
+			if (xstrcasestr(temp_str, "format_stderr"))
 				slurm_conf.log_fmt |= LOG_FMT_FORMAT_STDERR;
-			}
 			xfree(temp_str);
-		} else
-			slurm_conf.log_fmt = LOG_FMT_ISO8601_MS;
+		}
 
 		if (s_p_get_string(&temp_str, "MaxQueryTimeRange", tbl)) {
 			slurmdbd_conf->max_time_range = time_str2secs(temp_str);

commit da8d3e83ab73a65b81875c79a7dd2667e2a0aab4

Fix regression in 23.11.0 when parsing LogTimeFormat=iso8601_ms
Commit e34277f15d3 introduced a regression where iso8601_ms would
instead be parsed as iso8601, setting the option incorrectly.

Bug 19273

src/common/read_config.c
@@ -4409,7 +4409,9 @@ static int _validate_and_set_defaults(slurm_conf_t *conf,
 		 * If adding to this please update src/api/config_log.c to do
 		 * the reverse translation.
 		 */
-		if (xstrcasestr(temp_str, "iso8601"))
+		if (xstrcasestr(temp_str, "iso8601_ms"))
+			conf->log_fmt = LOG_FMT_ISO8601_MS;
+		else if (xstrcasestr(temp_str, "iso8601"))
 			conf->log_fmt = LOG_FMT_ISO8601;
 		else if (xstrcasestr(temp_str, "rfc5424_ms"))
 			conf->log_fmt = LOG_FMT_RFC5424_MS;

src/slurmdbd/read_config.c
@@ -352,7 +352,9 @@ extern int read_slurmdbd_conf(void)
 		/* Default log time format */
 		slurm_conf.log_fmt = LOG_FMT_ISO8601_MS;
 		if (s_p_get_string(&temp_str, "LogTimeFormat", tbl)) {
-			if (xstrcasestr(temp_str, "iso8601"))
+			if (xstrcasestr(temp_str, "iso8601_ms"))
+				slurm_conf.log_fmt = LOG_FMT_ISO8601_MS;
+			else if (xstrcasestr(temp_str, "iso8601"))
 				slurm_conf.log_fmt = LOG_FMT_ISO8601;
 			else if (xstrcasestr(temp_str, "rfc5424_ms"))
 				slurm_conf.log_fmt = LOG_FMT_RFC5424_MS;