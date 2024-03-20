8f4dd455f04fa9fb23e7a3234d622d28335d8376
Albert Gil <albert.gil@schedmd.com>
2024-03-20 16:33:09 +0100

    Testsuite - Improve test_138_2 using command instead of lines
    All tests have the same bash script, so make the function more useful.
    
    Bug 18390

--- testsuite/python/tests/test_138_2.py
+++ testsuite/python/tests/test_138_2.py
@@ -4,27 +4,36 @@
 import pytest
 import atf
 
+script_name = "./script.sh"
+script_out = "./script.out"
+
 
 @pytest.fixture(scope="module", autouse=True)
 def setup():
     atf.require_slurm_running()
 
 
-def make_and_run_bash(lines: list[str]) -> None:
-    """Make and run the bash script.
+def make_and_run_bash(command) -> None:
+    """Make and run the bash script to run a command 1000 times.
     Input is a list of lines to be run as bash script."""
-    script_name = "script.sh"
-    script = "\n".join(lines)
-    atf.make_bash_script(script_name, script)
-    atf.run_command(f"bash {script_name}")
+    atf.make_bash_script(
+        script_name,
+        f"""
+        for i in $(seq 1 1000)
+            do {command} &
+        done > {script_out}
+        wait
+        """,
+    )
+    atf.run_command(script_name)
 
 
 @pytest.mark.parametrize(
     "command, phrase",
     [
-	("sinfo --Format=NODELIST", "^NODELIST"),
-	("scontrol show node | head -1", "^NodeName="),
-	("squeue --only-job-state --Format=NODELIST", "^NODELIST"),
+        ("sinfo --Format=NODELIST", "^NODELIST"),
+        ("scontrol show node | head -1", "^NodeName="),
+        ("squeue --only-job-state --Format=NODELIST", "^NODELIST"),
     ],
 )
 def test_parallel(command, phrase):
@@ -32,17 +41,10 @@ def test_parallel(command, phrase):
     1000 user commands to slurm to make sure that it doesn't crash. We then
     check the output that the correct number of commands were run."""
 
-    script_out = str(atf.module_tmp_path / f"{command[:5]}.out")
     # Cancel all jobs so that the the queue is empty
     atf.cancel_all_jobs()
 
-    script_lines = [
-        "for i in $(seq 1 1000)",
-        f"    do {command} &",
-        f"done > {script_out}",
-        "wait",
-    ]
-    make_and_run_bash(script_lines)
+    make_and_run_bash(command)
 
     output = atf.run_command_output(f"cat {script_out} | grep -c '{phrase}'")
     assert (
@@ -55,20 +57,13 @@ def test_squeue_parallel():
     correctly. To test this we submit 100 jobs, then we run 1000 squeue
     commands and make sure that all 1000 worked and produced output."""
 
-    script_out = str(atf.module_tmp_path / "squeue.out")
     # Cancel all jobs so that the the queue is empty
     atf.cancel_all_jobs()
     # Submit 100 jobs to fill up the queue
     for i in range(100):
         atf.submit_job_sbatch("--wrap='sleep 100'")
 
-    script_lines = [
-        "for i in $(seq 1 1000)",
-        "    do squeue &",
-        f"done > {script_out}",
-        "wait",
-    ]
-    make_and_run_bash(script_lines)
+    make_and_run_bash("squeue")
 
     output = atf.run_command_output(f"cat {script_out} | grep -c 'JOBID'")
     assert (
@@ -81,18 +76,11 @@ def test_show_jobs_parallel():
     then run 1000 'scontrol show job {job_id} &' commands to make sure that all
     of them provide the correct output"""
 
-    script_out = str(atf.module_tmp_path / "job.out")
     # Cancel all jobs so that the the queue is empty
     atf.cancel_all_jobs()
     job_id = atf.submit_job_srun("true")
 
-    script_lines = [
-        "for i in $(seq 1 1000)",
-        f"    do scontrol show job {job_id} &",
-        f"done > {script_out}",
-        "wait",
-    ]
-    make_and_run_bash(script_lines)
+    make_and_run_bash(f"scontrol show job {job_id}")
 
     output = atf.run_command_output(f'cat {script_out} | grep -c "JobId="')
     assert (
