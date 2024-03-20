124f8c5a4ad9f616d8c1ee6a6bbddf86cc203ebf
Tim Wickberg <tim@schedmd.com>
2024-03-20 16:31:25 -0600

    Use add_key_pair().

--- src/plugins/acct_gather_profile/hdf5/acct_gather_profile_hdf5.c
+++ src/plugins/acct_gather_profile/hdf5/acct_gather_profile_hdf5.c
@@ -642,22 +642,9 @@ extern int acct_gather_profile_p_add_sample_data(int table_id, void *data,
 
 extern void acct_gather_profile_p_conf_values(List *data)
 {
-	config_key_pair_t *key_pair;
-
-	xassert(*data);
-
-	key_pair = xmalloc(sizeof(config_key_pair_t));
-	key_pair->name = xstrdup("ProfileHDF5Dir");
-	key_pair->value = xstrdup(hdf5_conf.dir);
-	list_append(*data, key_pair);
-
-	key_pair = xmalloc(sizeof(config_key_pair_t));
-	key_pair->name = xstrdup("ProfileHDF5Default");
-	key_pair->value = xstrdup(acct_gather_profile_to_string(hdf5_conf.def));
-	list_append(*data, key_pair);
-
-	return;
-
+	add_key_pair(*data, "ProfileHDF5Dir", "%s", hdf5_conf.dir);
+	add_key_pair(*data, "ProfileHDF5Default", "%s",
+		     acct_gather_profile_to_string(hdf5_conf.def));
 }
 
 extern bool acct_gather_profile_p_is_active(uint32_t type)
