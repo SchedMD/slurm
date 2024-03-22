9da117eaba1a724688a99a95d48e45f5ef19aa3e
Oriol Vilarrubi <jvilarru@schedmd.com>
2024-03-22 07:43:52 +0100

    slurmstepd - Call reclaim_privileges in _set_xauthority after error
    If we returned before reclaiming privileges then we would be left without
    privileges so further operations on the slurmstepd could fail.
    
    Bug 19390

--- src/slurmd/slurmstepd/mgr.c
+++ src/slurmd/slurmstepd/mgr.c
@@ -1003,6 +1003,7 @@ static void *_x11_signal_handler(void *arg)
 static int _set_xauthority(stepd_step_rec_t *step)
 {
 	struct priv_state sprivs = { 0 };
+	int rc = SLURM_SUCCESS;
 
 	if (drop_privileges(step, true, &sprivs, false) < 0) {
 		error("%s: Unable to drop privileges before xauth", __func__);
@@ -1012,7 +1013,7 @@ static int _set_xauthority(stepd_step_rec_t *step)
 	if (x11_set_xauth(step->x11_xauthority, step->x11_magic_cookie,
 			  step->x11_display)) {
 		error("%s: failed to run xauth", __func__);
-		return SLURM_ERROR;
+		rc =  SLURM_ERROR;
 	}
 
 	if (reclaim_privileges(&sprivs) < 0) {
@@ -1020,7 +1021,7 @@ static int _set_xauthority(stepd_step_rec_t *step)
 		return SLURM_ERROR;
 	}
 
-	return SLURM_SUCCESS;
+	return rc;
 }
 
 static int _spawn_job_container(stepd_step_rec_t *step)
