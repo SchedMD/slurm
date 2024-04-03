e7016f6d53319dc778704151e5f0537e3597b462
Dominik Bartkiewicz <bart@schedmd.com>
2024-04-03 23:09:35 +0100

    Add topology_p_whole_topo() for topology/block
    Ticket 18792

--- src/plugins/topology/block/topology_block.c
+++ src/plugins/topology/block/topology_block.c
@@ -172,6 +172,12 @@ extern int topology_p_eval_nodes(topology_eval_t *topo_eval)
 
 extern int topology_p_whole_topo(bitstr_t *node_mask)
 {
+	for (int i = 0; i < block_record_cnt; i++) {
+		if (bit_overlap_any(block_record_table[i].node_bitmap,
+				    node_mask)) {
+			bit_or(node_mask, block_record_table[i].node_bitmap);
+		}
+	}
 	return SLURM_SUCCESS;
 }
 
