SchedMD/slurm

commit 8cf81f1ef6b3bfe8bfd45566c76e117f9ccefb97

Make master 24.08

src/interfaces/select.c
@@ -939,16 +939,6 @@ extern int select_g_select_jobinfo_pack(dynamic_plugin_data_t *jobinfo,
 	} else
 		plugin_id = select_context_default;
 
-	/*
-	 * Remove 2 versions after 23.02 -- 23.02 doesn't pack jobinfo anymore
-	 * AND the select plugin isn't loaded in the slurmd in 23.11.
-	 */
-	if (!running_in_slurmctld() &&
-	    (protocol_version <= SLURM_23_02_PROTOCOL_VERSION)) {
-		pack32(plugin_id, buffer);
-		return SLURM_SUCCESS;
-	}
-
 	xassert(select_context_cnt >= 0);
 
 	if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
@@ -972,25 +962,6 @@ extern int select_g_select_jobinfo_unpack(dynamic_plugin_data_t **jobinfo,
 					  uint16_t protocol_version)
 {
 	dynamic_plugin_data_t *jobinfo_ptr = NULL;
-
-	/*
-	 * Remove 2 versions after 23.02 -- 23.02 doesn't pack jobinfo anymore
-	 * AND the select plugin isn't loaded in the slurmd in 23.11.
-	 */
-	if (!running_in_slurmctld() &&
-	    (protocol_version <= SLURM_23_02_PROTOCOL_VERSION)) {
-		uint32_t plugin_id;
-		safe_unpack32(&plugin_id, buffer);
-		/*
-		 * srun will unpack this and then repack things later when
-		 * sending to the slurmd. Instead of the context here we have
-		 * the actual plugin id.
-		 * This hack works for all systems that aren't cray_aries.
-		 */
-		select_context_default = plugin_id;
-		*jobinfo = NULL;
-		return SLURM_SUCCESS;
-	}
 
 	xassert(select_context_cnt >= 0);
 

src/interfaces/switch.c
@@ -369,12 +369,8 @@ extern int switch_g_pack_jobinfo(dynamic_plugin_data_t *jobinfo, buf_t *buffer,
 
 	xassert(switch_context_cnt >= 0);
 
-	if (!switch_context_cnt) {
-		if (protocol_version <= SLURM_23_02_PROTOCOL_VERSION) {
-			pack32(SWITCH_PLUGIN_NONE, buffer);
-		}
+	if (!switch_context_cnt)
 		return SLURM_SUCCESS;
-	}
 
 	if (jobinfo) {
 		data      = jobinfo->data;
@@ -400,14 +396,8 @@ extern int switch_g_unpack_jobinfo(dynamic_plugin_data_t **jobinfo,
 
 	xassert(switch_context_cnt >= 0);
 
-	if (!switch_context_cnt) {
-		if (protocol_version <= SLURM_23_02_PROTOCOL_VERSION) {
-			uint32_t plugin_id;
-			safe_unpack32(&plugin_id, buffer);
-			*jobinfo = NULL;
-		}
+	if (!switch_context_cnt)
 		return SLURM_SUCCESS;
-	}
 
 	jobinfo_ptr = xmalloc(sizeof(dynamic_plugin_data_t));
 	*jobinfo = jobinfo_ptr;

commit 152cdf88a9721727d3e2deab4224c80f6172fcfc

Fix 23.02 srun operation against 24.05 slurmctld.
Regression from 8cf81f1ef6.

src/interfaces/select.c
@@ -931,6 +931,13 @@ extern int select_g_select_jobinfo_pack(dynamic_plugin_data_t *jobinfo,
 	} else
 		plugin_id = select_context_default;
 
+	/* Remove when 23.02 is no longer supported. */
+	if (!running_in_slurmctld() &&
+	    (protocol_version <= SLURM_23_02_PROTOCOL_VERSION)) {
+		pack32(plugin_id, buffer);
+		return SLURM_SUCCESS;
+	}
+
 	xassert(select_context_cnt >= 0);
 
 	if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
@@ -954,6 +961,22 @@ extern int select_g_select_jobinfo_unpack(dynamic_plugin_data_t **jobinfo,
 					  uint16_t protocol_version)
 {
 	dynamic_plugin_data_t *jobinfo_ptr = NULL;
+
+	/* Remove when 23.02 is no longer supported. */
+	if (!running_in_slurmctld() &&
+	    (protocol_version <= SLURM_23_02_PROTOCOL_VERSION)) {
+		uint32_t plugin_id;
+		safe_unpack32(&plugin_id, buffer);
+		/*
+		 * srun will unpack this and then repack things later when
+		 * sending to the slurmd. Instead of the context here we have
+		 * the actual plugin id.
+		 * This hack works for all systems that aren't cray_aries.
+		 */
+		select_context_default = plugin_id;
+		*jobinfo = NULL;
+		return SLURM_SUCCESS;
+	}
 
 	xassert(select_context_cnt >= 0);
 

src/interfaces/switch.c
@@ -349,8 +349,11 @@ extern int switch_g_pack_jobinfo(dynamic_plugin_data_t *jobinfo, buf_t *buffer,
 
 	xassert(switch_context_cnt >= 0);
 
-	if (!switch_context_cnt)
+	if (!switch_context_cnt) {
+		if (protocol_version <= SLURM_23_02_PROTOCOL_VERSION)
+			pack32(SWITCH_PLUGIN_NONE, buffer);
 		return SLURM_SUCCESS;
+	}
 
 	if (jobinfo) {
 		data      = jobinfo->data;
@@ -376,8 +379,14 @@ extern int switch_g_unpack_jobinfo(dynamic_plugin_data_t **jobinfo,
 
 	xassert(switch_context_cnt >= 0);
 
-	if (!switch_context_cnt)
+	if (!switch_context_cnt) {
+		if (protocol_version <= SLURM_23_02_PROTOCOL_VERSION) {
+			uint32_t plugin_id;
+			safe_unpack32(&plugin_id, buffer);
+			*jobinfo = NULL;
+		}
 		return SLURM_SUCCESS;
+	}
 
 	jobinfo_ptr = xmalloc(sizeof(dynamic_plugin_data_t));
 	*jobinfo = jobinfo_ptr;