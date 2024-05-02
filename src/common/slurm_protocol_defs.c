SchedMD/slurm

commit 55759a05dcf2a2cd719ac93bcdd3c575d0facd4d

Add array_bitmap to slurm_selected_step_t
Bug 18464

Signed-off-by: Nathan Rini <nate@schedmd.com>

src/common/slurm_protocol_defs.c
@@ -857,6 +857,7 @@ extern int unfmt_job_id_string(const char *src, slurm_selected_step_t *id)
 	 */
 
 	/* reset to default of NO_VAL */
+	id->array_bitmap = NULL;
 	id->array_task_id = NO_VAL;
 	id->het_job_offset = NO_VAL;
 	id->step_id.job_id = NO_VAL;
@@ -1329,6 +1330,7 @@ extern const char *slurm_container_status_to_str(
 extern void slurm_destroy_selected_step(void *object)
 {
 	slurm_selected_step_t *step = (slurm_selected_step_t *)object;
+	FREE_NULL_BITMAP(step->array_bitmap);
 	xfree(step);
 }
 

slurm/slurm.h
@@ -2409,9 +2409,11 @@ typedef struct job_alloc_info_msg {
 } job_alloc_info_msg_t;
 
 #define SLURM_SELECTED_STEP_INITIALIZER \
-	{ NO_VAL, NO_VAL, { NO_VAL, NO_VAL, NO_VAL } }
+	{ NULL, NO_VAL, NO_VAL, { NO_VAL, NO_VAL, NO_VAL } }
 
 typedef struct {
+	bitstr_t *array_bitmap; /* Set with slurm_array_str2bitmap().
+				 * NOT PACKED */
 	uint32_t array_task_id;		/* task_id of a job array or NO_VAL */
 	uint32_t het_job_offset;	/* het_job_offset or NO_VAL */
 	slurm_step_id_t step_id;

src/slurmctld/job_mgr.c
@@ -6030,6 +6030,7 @@ static void _slurm_selected_step_init(job_record_t *job_ptr,
 {
 	xassert(job_ptr);
 
+	id->array_bitmap = NULL;
 	id->array_task_id = job_ptr->array_task_id;
 	if (job_ptr->array_task_id != NO_VAL)
 		id->step_id.job_id = job_ptr->array_job_id;

commit fd71d090f1d8113c1776eb45ae9c2082d800adc1

Avoid NULL dereference in slurm_destroy_selected_step()
Regression from 55759a05dc (Ticket 18464).

Ticket 19761, 18464

src/common/slurm_protocol_defs.c
@@ -1348,6 +1348,10 @@ extern const char *slurm_container_status_to_str(
 extern void slurm_destroy_selected_step(void *object)
 {
 	slurm_selected_step_t *step = (slurm_selected_step_t *)object;
+
+	if (!step)
+		return;
+
 	FREE_NULL_BITMAP(step->array_bitmap);
 	xfree(step);
 }