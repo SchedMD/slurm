f10717684526e33a3c74c5f932e0fdecec621144
Danny Auble <da@schedmd.com>
2024-05-07 16:18:10 -0600

    Replace logic with slurm_sort_int_list_asc.
    Continuation of 338c671fe4
    
    Ticket 19819

--- src/plugins/gres/gpu/gres_gpu.c
+++ src/plugins/gres/gpu/gres_gpu.c
@@ -369,12 +369,7 @@ static int _sort_gpu_by_links_order(void *x, void *y)
 	if (index_x < -1 || index_y < -1)
 		error("%s: invalid links value found", __func__);
 
-	if (index_x < index_y)
-		return -1;
-	else if (index_x > index_y)
-		return 1;
-
-	return 0;
+	return slurm_sort_int_list_asc(&index_x, &index_y);
 }
 
 /*
