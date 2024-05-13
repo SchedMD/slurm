SchedMD/slurm

commit 9a949a7a7807a9751984963c3fc8c368c2fb7c05

Fold switch_g_alloc_jobinfo() into switch_g_build_jobinfo().
Always paired up, just handle both steps simultaneously.

src/interfaces/switch.c
@@ -55,9 +55,7 @@ typedef struct slurm_switch_ops {
 	uint32_t     (*plugin_id);
 	int          (*state_save)        ( void );
 	int          (*state_restore)     ( bool recover );
-	int          (*alloc_jobinfo)     ( switch_jobinfo_t **jobinfo,
-					    uint32_t job_id, uint32_t step_id );
-	int          (*build_jobinfo)     ( switch_jobinfo_t *jobinfo,
+	int          (*build_jobinfo)     ( switch_jobinfo_t **jobinfo,
 					    slurm_step_layout_t *step_layout,
 					    step_record_t *step_ptr );
 	int          (*duplicate_jobinfo) ( switch_jobinfo_t *source,
@@ -88,7 +86,6 @@ static const char *syms[] = {
 	"plugin_id",
 	"switch_p_save",
 	"switch_p_restore",
-	"switch_p_alloc_jobinfo",
 	"switch_p_build_jobinfo",
 	"switch_p_duplicate_jobinfo",
 	"switch_p_free_jobinfo",
@@ -247,42 +244,22 @@ extern int switch_g_restore(bool recover)
 	return (*(ops[switch_context_default].state_restore))(recover);
 }
 
-extern int  switch_g_alloc_jobinfo(dynamic_plugin_data_t **jobinfo,
-				   uint32_t job_id, uint32_t step_id)
-{
-	dynamic_plugin_data_t *jobinfo_ptr = NULL;
-
-	xassert(switch_context_cnt >= 0);
-
-	if (!switch_context_cnt)
-		return SLURM_SUCCESS;
-
-	jobinfo_ptr = _create_dynamic_plugin_data(switch_context_default);
-	*jobinfo    = jobinfo_ptr;
-
-	return (*(ops[jobinfo_ptr->plugin_id].alloc_jobinfo))
-		((switch_jobinfo_t **)&jobinfo_ptr->data, job_id, step_id);
-}
-
-extern int switch_g_build_jobinfo(dynamic_plugin_data_t *jobinfo,
+extern int switch_g_build_jobinfo(dynamic_plugin_data_t **jobinfo,
 				  slurm_step_layout_t *step_layout,
 				  step_record_t *step_ptr)
 {
 	void *data = NULL;
-	uint32_t plugin_id;
+	uint32_t plugin_id = switch_context_default;
 
 	xassert(switch_context_cnt >= 0);
 
 	if (!switch_context_cnt)
 		return SLURM_SUCCESS;
 
-	if (jobinfo) {
-		data      = jobinfo->data;
-		plugin_id = jobinfo->plugin_id;
-	} else
-		plugin_id = switch_context_default;
+	*jobinfo = _create_dynamic_plugin_data(plugin_id);
 
-	return (*(ops[plugin_id].build_jobinfo))(data, step_layout, step_ptr);
+	return (*(ops[plugin_id].build_jobinfo))((switch_jobinfo_t **) &data,
+						 step_layout, step_ptr);
 }
 
 extern int  switch_g_duplicate_jobinfo(dynamic_plugin_data_t *source,

src/interfaces/switch.h
@@ -83,16 +83,6 @@ extern int switch_g_restore(bool recover);
  * JOB-SPECIFIC SWITCH CREDENTIAL MANAGEMENT FUNCIONS *
 \******************************************************/
 
-/* allocate storage for a switch job credential
- * OUT jobinfo - storage for a switch job credential
- * IN job_id   - job id of the job this is for.
- * IN step_id  - step id of the job this is for.
- * RET         - slurm error code
- * NOTE: storage must be freed using switch_g_free_jobinfo
- */
-extern int  switch_g_alloc_jobinfo (dynamic_plugin_data_t **jobinfo,
-				    uint32_t job_id, uint32_t step_id);
-
 /* fill a job's switch credential
  * OUT jobinfo  - storage for a switch job credential
  * IN  step_layout - the layout of the step with at least the nodes,
@@ -101,7 +91,7 @@ extern int  switch_g_alloc_jobinfo (dynamic_plugin_data_t **jobinfo,
  * NOTE: step_ptr will be NULL for "srun --no-allocate" calls
  * NOTE: storage must be freed using switch_g_free_jobinfo
  */
-extern int switch_g_build_jobinfo(dynamic_plugin_data_t *jobinfo,
+extern int switch_g_build_jobinfo(dynamic_plugin_data_t **jobinfo,
 				  slurm_step_layout_t *step_layout,
 				  step_record_t *step_ptr);
 

src/plugins/switch/hpe_slingshot/switch_hpe_slingshot.c
@@ -336,22 +336,6 @@ error:
 	return SLURM_ERROR;
 }
 
-/*
- * switch functions for job step specific credential
- */
-extern int switch_p_alloc_jobinfo(switch_jobinfo_t **switch_job,
-				  uint32_t job_id, uint32_t step_id)
-{
-	slingshot_jobinfo_t *new = xcalloc(1, sizeof(*new));
-
-	xassert(switch_job);
-
-	new->version = SLURM_PROTOCOL_VERSION;
-	*switch_job = (switch_jobinfo_t *)new;
-
-	return SLURM_SUCCESS;
-}
-
 static void _copy_jobinfo(slingshot_jobinfo_t *old, slingshot_jobinfo_t *new)
 {
 	/* Copy static (non-malloced) fields */
@@ -509,11 +493,11 @@ static uint32_t _get_het_job_node_cnt(step_record_t *step_ptr)
 	return node_cnt;
 }
 
-extern int switch_p_build_jobinfo(switch_jobinfo_t *switch_job,
+extern int switch_p_build_jobinfo(switch_jobinfo_t **switch_job,
 				  slurm_step_layout_t *step_layout,
 				  step_record_t *step_ptr)
 {
-	slingshot_jobinfo_t *job = (slingshot_jobinfo_t *) switch_job;
+	slingshot_jobinfo_t *job = NULL;
 	job_record_t *job_ptr;
 	uint32_t job_id;
 	uint32_t node_cnt;
@@ -528,11 +512,9 @@ extern int switch_p_build_jobinfo(switch_jobinfo_t *switch_job,
 		step_ptr->step_id.job_id, step_ptr->step_id.step_id,
 		job_ptr->user_id, step_ptr->network);
 
-	if (!job) {
-		debug("switch_job was NULL");
-		return SLURM_SUCCESS;
-	}
-	xassert(job->version == SLURM_PROTOCOL_VERSION);
+	job = xmalloc(sizeof(*job));
+	job->version = SLURM_PROTOCOL_VERSION;
+	*switch_job = (switch_jobinfo_t *) job;
 
 	/*
 	 * If this is a homogeneous step, or the first component in a

src/srun/step_ctx.c
@@ -264,11 +264,7 @@ extern slurm_step_ctx_t *step_ctx_create_no_alloc(
 		step_req->num_tasks,
 		0);
 
-	if (switch_g_alloc_jobinfo(&step_resp->switch_job,
-				   step_req->step_id.job_id,
-				   step_resp->job_step_id) < 0)
-		fatal("switch_g_alloc_jobinfo: %m");
-	if (switch_g_build_jobinfo(step_resp->switch_job,
+	if (switch_g_build_jobinfo(&step_resp->switch_job,
 				   step_resp->step_layout,
 				   NULL) < 0)
 		fatal("switch_g_build_jobinfo: %m");

src/stepmgr/stepmgr.c
@@ -3214,13 +3214,8 @@ static int _switch_setup(job_record_t *job_ptr, step_record_t *step_ptr)
 	if (!step_ptr->step_layout)
 		return SLURM_SUCCESS;
 
-	if (switch_g_alloc_jobinfo(&step_ptr->switch_job,
-				   job_ptr->job_id,
-				   step_ptr->step_id.step_id) < 0)
-		fatal("%s: switch_g_alloc_jobinfo error", __func__);
-
 	errno = 0;
-	if (switch_g_build_jobinfo(step_ptr->switch_job,
+	if (switch_g_build_jobinfo(&step_ptr->switch_job,
 				   step_ptr->step_layout,
 				   step_ptr) < 0) {
 		if (errno == ESLURM_INTERCONNECT_BUSY)

commit 1408c85027e234a15908ee94af8c10ef5dc117f7

Fix data argument handling in switch_g_build_stepinfo().
Regression from 9a949a7a78.

src/interfaces/switch.c
@@ -327,7 +327,7 @@ extern int switch_g_build_stepinfo(dynamic_plugin_data_t **stepinfo,
 				   slurm_step_layout_t *step_layout,
 				   step_record_t *step_ptr)
 {
-	void *data = NULL;
+	void **data = NULL;
 	uint32_t plugin_id = switch_context_default;
 
 	xassert(switch_context_cnt >= 0);
@@ -336,8 +336,9 @@ extern int switch_g_build_stepinfo(dynamic_plugin_data_t **stepinfo,
 		return SLURM_SUCCESS;
 
 	*stepinfo = _create_dynamic_plugin_data(plugin_id);
+	data = &(*stepinfo)->data;
 
-	return (*(ops[plugin_id].build_stepinfo))((switch_stepinfo_t **) &data,
+	return (*(ops[plugin_id].build_stepinfo))((switch_stepinfo_t **) data,
 						  step_layout, step_ptr);
 }
 