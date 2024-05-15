a65eb0bec37dcda72a514a8e301f71dde17813e1
Nathan Rini <nate@schedmd.com>
2024-05-15 10:22:11 -0600

    data_parser/v0.0.42 - Add IS_FLAG_BIT_DEPRECATED() macro
    Add macro to make forcing deprecated cleaner.
    
    Ticket 19881

--- src/plugins/data_parser/v0.0.42/openapi.c
+++ src/plugins/data_parser/v0.0.42/openapi.c
@@ -49,6 +49,7 @@
 #define REF_PATH OPENAPI_PATH_REL OPENAPI_SCHEMAS_PATH
 #define TYPE_PREFIX "DATA_PARSER_"
 #define KEY_PREFIX XSTRINGIFY(DATA_VERSION) "_"
+#define IS_FLAG_BIT_DEPRECATED(bit) (bit->deprecated)
 
 typedef struct {
 	int magic; /* MAGIC_SPEC_ARGS */
