0e98ed4308be1714dbd370261079a975fd77fd7c
Nathan Rini <nate@schedmd.com>
2024-05-15 10:41:56 -0600

    data_parser/v0.0.40 - Fix argument order for _add_param() call in _add_param_linked()
    Required and deprecated were swapped. Not updating NEWS or RELEASE_NOTES
    as these changes will be effectively reverted by the next set of commits.
    
    Ticket 19881, 18470

--- src/plugins/data_parser/v0.0.40/openapi.c
+++ src/plugins/data_parser/v0.0.40/openapi.c
@@ -580,7 +580,7 @@ static void _add_param_linked(data_t *params, const parser_t *fp,
 	schema = _add_param(data_set_dict(data_list_append(params)), fp->key,
 			    OPENAPI_FORMAT_STRING,
 			    (p->obj_openapi == OPENAPI_FORMAT_BOOL),
-			    fp->obj_desc, fp->required, fp->deprecated, args);
+			    fp->obj_desc, fp->deprecated, fp->required, args);
 
 	if (fp->model == PARSER_MODEL_ARRAY_LINKED_FIELD)
 		fp = find_parser_by_type(fp->type);
