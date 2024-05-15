SchedMD/slurm

commit 92de01ca2d3fcfaeb50025e703d33977f2697e60

Truly sleep at least a second
time() doesn't return current nanoseconds and can cause looping multiple
times per second.

Bug 19117

src/slurmctld/power_save.c
@@ -1148,8 +1148,8 @@ static void *_power_save_thread(void *arg)
 
 	while (!slurmctld_config.shutdown_time) {
 		slurm_mutex_lock(&power_mutex);
-		ts.tv_sec = time(NULL) + 1;
-		slurm_cond_timedwait(&power_cond, &power_mutex, &ts);
+		clock_gettime(CLOCK_REALTIME, &ts);
+		ts.tv_sec += 1;
 		slurm_mutex_unlock(&power_mutex);
 
 		if (slurmctld_config.shutdown_time)

commit e3827294b5fd3a09601afe5c52fe0eed1e74c47e

Fix power_save thread spinning too fast
Regression caused by 92de01ca2d3. The removal ended up in a debug commit
and was lost when the debug commit was removed.

Bug 19117

Signed-off-by: Broderick Gardner <broderick@schedmd.com>

src/slurmctld/power_save.c
@@ -1225,6 +1225,7 @@ static void *_power_save_thread(void *arg)
 		slurm_mutex_lock(&power_mutex);
 		clock_gettime(CLOCK_REALTIME, &ts);
 		ts.tv_sec += 1;
+		slurm_cond_timedwait(&power_cond, &power_mutex, &ts);
 		slurm_mutex_unlock(&power_mutex);
 
 		if (slurmctld_config.shutdown_time)