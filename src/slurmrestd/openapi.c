d49cada828f4fa67dbe1d278a831d10f06321268
Nathan Rini <nate@schedmd.com>
2024-05-16 11:51:15 -0600

    slurmrestd - Add OP_BIND_NO_SLURMDBD
    Allow paths to skip getting slurmdbd connection.
    
    Ticket 19899

--- src/slurmrestd/openapi.c
+++ src/slurmrestd/openapi.c
@@ -2376,8 +2376,10 @@ extern int wrap_openapi_ctxt_callback(const char *context_id,
 	      __func__, context_id, get_http_method_string(method),
 	      data_parser_get_plugin(ctxt.parser));
 
-	if (slurm_conf.accounting_storage_type &&
-	    !(ctxt.db_conn = openapi_get_db_conn(auth))) {
+	if (op_path->flags & OP_BIND_NO_SLURMDBD) {
+		; /* Do not attempt to open a connection to slurmdbd */
+	} else if (slurm_conf.accounting_storage_type &&
+		   !(ctxt.db_conn = openapi_get_db_conn(auth))) {
 		openapi_resp_error(&ctxt, (rc = ESLURM_DB_CONNECTION), __func__,
 				   "openapi_get_db_conn() failed to open slurmdb connection");
 	} else {
