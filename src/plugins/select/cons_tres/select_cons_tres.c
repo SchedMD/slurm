3f0311cd77af4e3e47371fdc8531ff80033dd656
Tim Wickberg <tim@schedmd.com>
2024-05-29 12:16:27 -0600

    Remove unused pstate_version variable.

--- src/plugins/select/cons_tres/select_cons_tres.c
+++ src/plugins/select/cons_tres/select_cons_tres.c
@@ -132,7 +132,6 @@ const char plugin_name[] = "Trackable RESources (TRES) Selection plugin";
 const char plugin_type[] = "select/cons_tres";
 const uint32_t plugin_id      = SELECT_PLUGIN_CONS_TRES;
 const uint32_t plugin_version = SLURM_VERSION_NUMBER;
-const uint32_t pstate_version = 7;	/* version control on saved state */
 const uint16_t nodeinfo_magic = 0x8a5d;
 
 /* Global variables */
