29fae57b7a3309e6551775b0ef1bfde1a055a717
Nathan Rini <nate@schedmd.com>
2024-05-28 22:34:07 -0600

    data_parser/v0.0.42 - Assert PARSER_MODEL_NT_PTR_ARRAY are composed of pointers
    Ticket 19842

--- src/plugins/data_parser/v0.0.42/parsers.c
+++ src/plugins/data_parser/v0.0.42/parsers.c
@@ -812,6 +812,12 @@ extern void check_parser_funcname(const parser_t *const parser,
 		xassert(!parser->obj_openapi);
 	} else if ((parser->model == PARSER_MODEL_NT_ARRAY) ||
 		   (parser->model == PARSER_MODEL_NT_PTR_ARRAY)) {
+		if (parser->model == PARSER_MODEL_NT_PTR_ARRAY) {
+			const parser_t *const eparser =
+				find_parser_by_type(parser->array_type);
+			xassert(eparser->pointer_type);
+		}
+
 		xassert(!parser->pointer_type);
 		xassert(parser->array_type > DATA_PARSER_TYPE_INVALID);
 		xassert(parser->array_type < DATA_PARSER_TYPE_MAX);
