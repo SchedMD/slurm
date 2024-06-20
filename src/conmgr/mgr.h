d5c3fc5e7bee6c338654c3d06440d2db3f788c58
Nathan Rini <nate@schedmd.com>
2024-06-19 22:37:21 -0600

    conmgr - Move struct declarations to mgr.h
    Ticket 20114

--- src/conmgr/mgr.h
+++ src/conmgr/mgr.h
@@ -36,7 +36,248 @@
 #ifndef _CONMGR_MGR_H
 #define _CONMGR_MGR_H
 
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <time.h>
+
+#include "slurm/slurm.h"
+
+#include "src/common/pack.h"
+
 #include "src/conmgr/conmgr.h"
 #include "src/conmgr/workq.h"
+
+typedef struct {
+#define MAGIC_WORK 0xD231444A
+	int magic; /* MAGIC_WORK */
+	conmgr_fd_t *con;
+	conmgr_work_func_t func;
+	void *arg;
+	const char *tag;
+	conmgr_work_status_t status;
+	conmgr_work_type_t type;
+	struct {
+		/* absolute time when to work can begin */
+		time_t seconds;
+		long nanoseconds; /* offset from seconds */
+	} begin;
+} work_t;
+
+/*
+ * Connection tracking structure
+ */
+struct conmgr_fd_s {
+#define MAGIC_CON_MGR_FD 0xD23444EF
+	int magic; /* MAGIC_CON_MGR_FD */
+	conmgr_con_type_t type;
+	/* input and output may be a different fd to inet mode */
+	int input_fd;
+	int output_fd;
+	/* arg handed to on_connection */
+	void *new_arg;
+	/* arg returned from on_connection */
+	void *arg;
+	/* name of connection for logging */
+	char *name;
+	/* address for connection */
+	slurm_addr_t address;
+	/* call backs on events */
+	conmgr_events_t events;
+	/* buffer holding incoming already read data */
+	buf_t *in;
+	/* has on_data already tried to parse data */
+	bool on_data_tried;
+	/* list of buf_t to write (in order) */
+	list_t *out;
+	/* socket maximum segment size (MSS) or NO_VAL if not known */
+	int mss;
+	/* this is a socket fd */
+	bool is_socket;
+	/* path to unix socket if it is one */
+	char *unix_socket;
+	/* this is a listen only socket */
+	bool is_listen;
+	/* connection is waiting for on_finish() to complete */
+	bool wait_on_finish;
+	/* poll has indicated write is possible */
+	bool can_write;
+	/* poll has indicated read is possible */
+	bool can_read;
+	/* has this connection received read EOF */
+	bool read_eof;
+	/* has this connection called on_connection */
+	bool is_connected;
+	/* incoming msg length - CON_TYPE_RPC only */
+	uint32_t msglen;
+	/*
+	 * has pending work:
+	 * there must only be 1 thread at a time working on this connection
+	 * directly.
+	 *
+	 * While this is true, the following must not be changed except by the
+	 * callback thread:
+	 * 	in
+	 * 	out
+	 * 	name (will never change for life of connection)
+	 * 	mgr (will not be moved)
+	 * 	con (will not be moved)
+	 * 	arg
+	 *	on_data_tried
+	 *	msglen
+	 *
+	 */
+	bool work_active;
+	/*
+	 * list of non-IO work pending
+	 * type: work_t*
+	 */
+	list_t *work;
+	/*
+	 * list of non-IO work pending out buffer being full sent
+	 * type: work_t*
+	 */
+	list_t *write_complete_work;
+};
+
+typedef struct {
+#define MAGIC_SIGNAL_HANDLER 0xC20A444A
+	int magic; /* MAGIC_SIGNAL_HANDLER */
+	struct sigaction prior;
+	struct sigaction new;
+	int signal;
+} signal_handler_t;
+
+typedef struct {
+#define MAGIC_SIGNAL_WORK 0xA201444A
+	int magic; /* MAGIC_SIGNAL_WORK */
+	int signal;
+	conmgr_work_func_t func;
+	void *arg;
+	const char *tag;
+} signal_work_t;
+
+typedef struct {
+#define MAGIC_DEFERRED_FUNC 0xA230403A
+	int magic; /* MAGIC_DEFERRED_FUNC */
+	work_func_t func;
+	void *arg;
+	const char *tag;
+} deferred_func_t;
+
+/*
+ * Global instance of conmgr
+ */
+typedef struct {
+	/* Max number of connections at any one time allowed */
+	int max_connections;
+	/*
+	 * list of all connections to process
+	 * type: conmgr_fd_t
+	 */
+	list_t *connections;
+	/*
+	 * list of connections that only listen
+	 * type: conmgr_fd_t
+	 */
+	list_t *listen_conns;
+	/*
+	 * list of complete connections pending cleanup
+	 * type: conmgr_fd_t
+	 */
+	list_t *complete_conns;
+	/*
+	 * True after conmgr_init() is called, false after conmgr_fini() is
+	 * called.
+	 */
+	bool initialized;
+	/*
+	 * True if _watch() is running
+	 * Changes protected by watch_mutex
+	 */
+	bool watching;
+	/*
+	 * True if there is a thread for listen queued or running
+	 */
+	bool listen_active;
+	/*
+	 * True if there is a thread for poll queued or running
+	 */
+	bool poll_active;
+	/*
+	 * True if there is a thread reading signal_fd[0]
+	 */
+	bool read_signals_active;
+	/*
+	 * Is trying to shutdown?
+	 */
+	bool shutdown_requested;
+	/*
+	 * Is mgr currently quiesced?
+	 * Sends all new work to deferred_funcs() while true
+	 */
+	bool quiesced;
+	/* at fork handler installed */
+	bool at_fork_installed;
+	/* will inspect connections (not listeners */
+	bool inspecting;
+	/* if an event signal has already been sent */
+	int event_signaled;
+	/* Event PIPE used to break out of poll */
+	int event_fd[2];
+	/* Signal PIPE to catch POSIX signals */
+	int signal_fd[2];
+
+	/* track when there is a pending signal to read */
+	bool signaled;
+	/* Caller requests finish on error */
+	bool exit_on_error;
+	/* First observed error */
+	int error;
+	/* list of work_t */
+	list_t *delayed_work;
+	/* last time clock was queried */
+	struct timespec last_time;
+	/* monotonic timer */
+	timer_t timer;
+	/* list of deferred_func_t */
+	list_t *deferred_funcs;
+
+	/* list of all registered signal handlers */
+	signal_handler_t *signal_handlers;
+	int signal_handler_count;
+	/* list of all registered signal work */
+	signal_work_t *signal_work;
+	int signal_work_count;
+
+	/* functions to handle host/port parsing */
+	conmgr_callbacks_t callbacks;
+
+	pthread_mutex_t mutex;
+	/* called after events or changes to wake up _watch */
+	pthread_cond_t cond;
+
+	/* use mutex to wait for watch to finish */
+	pthread_mutex_t watch_mutex;
+	pthread_cond_t watch_cond;
+} conmgr_t;
+
+#define CONMGR_DEFAULT \
+	(conmgr_t) {\
+		.mutex = PTHREAD_MUTEX_INITIALIZER,\
+		.cond = PTHREAD_COND_INITIALIZER,\
+		.watch_mutex = PTHREAD_MUTEX_INITIALIZER,\
+		.watch_cond = PTHREAD_COND_INITIALIZER,\
+		.max_connections = -1,\
+		.event_fd = { -1, -1 },\
+		.signal_fd = { -1, -1 },\
+		.error = SLURM_SUCCESS,\
+		.quiesced = true,\
+		.shutdown_requested = true,\
+	}
+
+extern conmgr_t mgr;
 
 #endif /* _CONMGR_MGR_H */
