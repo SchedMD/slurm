SchedMD/slurm

commit bb987a7f61cbb483a1fe448e5c1d3e395b02fd30

Move free_job_record() to common/job_record.h
And rename functions to job_record.

Ticket 19123

src/common/job_record.c
@@ -112,3 +112,195 @@ extern void free_step_record(void *x)
 	step_ptr->magic = ~STEP_MAGIC;
 	xfree(step_ptr);
 }
+
+/* Clear a job's GRES details per node strings, rebuilt later on demand */
+static void _clear_job_gres_details(job_record_t *job_ptr)
+{
+	int i;
+
+	xfree(job_ptr->gres_used);
+	for (i = 0; i < job_ptr->gres_detail_cnt; i++)
+		xfree(job_ptr->gres_detail_str[i]);
+	xfree(job_ptr->gres_detail_str);
+	job_ptr->gres_detail_cnt = 0;
+}
+
+/*
+ * _delete_job_details - delete a job's detail record and clear it's pointer
+ * IN job_entry - pointer to job_record to clear the record of
+ */
+static void _delete_job_details(job_record_t *job_entry)
+{
+	int i;
+
+	if (job_entry->details == NULL)
+		return;
+
+	xassert (job_entry->details->magic == DETAILS_MAGIC);
+
+	xfree(job_entry->details->acctg_freq);
+	for (i=0; i<job_entry->details->argc; i++)
+		xfree(job_entry->details->argv[i]);
+	xfree(job_entry->details->argv);
+	xfree(job_entry->details->cpu_bind);
+	free_cron_entry(job_entry->details->crontab_entry);
+	FREE_NULL_LIST(job_entry->details->depend_list);
+	xfree(job_entry->details->dependency);
+	xfree(job_entry->details->orig_dependency);
+	xfree(job_entry->details->env_hash);
+	for (i=0; i<job_entry->details->env_cnt; i++)
+		xfree(job_entry->details->env_sup[i]);
+	xfree(job_entry->details->env_sup);
+	xfree(job_entry->details->std_err);
+	FREE_NULL_BITMAP(job_entry->details->exc_node_bitmap);
+	xfree(job_entry->details->exc_nodes);
+	FREE_NULL_LIST(job_entry->details->feature_list);
+	xfree(job_entry->details->features);
+	xfree(job_entry->details->cluster_features);
+	FREE_NULL_BITMAP(job_entry->details->job_size_bitmap);
+	xfree(job_entry->details->std_in);
+	xfree(job_entry->details->mc_ptr);
+	xfree(job_entry->details->mem_bind);
+	FREE_NULL_LIST(job_entry->details->prefer_list);
+	xfree(job_entry->details->prefer);
+	xfree(job_entry->details->req_context);
+	xfree(job_entry->details->std_out);
+	xfree(job_entry->details->submit_line);
+	FREE_NULL_BITMAP(job_entry->details->req_node_bitmap);
+	xfree(job_entry->details->req_nodes);
+	xfree(job_entry->details->script);
+	xfree(job_entry->details->script_hash);
+	xfree(job_entry->details->arbitrary_tpn);
+	xfree(job_entry->details->work_dir);
+	xfree(job_entry->details->x11_magic_cookie);
+	xfree(job_entry->details->x11_target);
+	xfree(job_entry->details);	/* Must be last */
+}
+
+extern void job_record_delete(void *job_entry)
+{
+	job_record_t *job_ptr = job_entry;
+
+	if (!job_entry)
+		return;
+
+	xassert(job_ptr->magic == JOB_MAGIC);
+
+	_delete_job_details(job_ptr);
+	xfree(job_ptr->account);
+	xfree(job_ptr->admin_comment);
+	xfree(job_ptr->alias_list);
+	xfree(job_ptr->alloc_node);
+	job_record_free_null_array_recs(job_ptr);
+	if (job_ptr->array_recs) {
+		FREE_NULL_BITMAP(job_ptr->array_recs->task_id_bitmap);
+		xfree(job_ptr->array_recs->task_id_str);
+		xfree(job_ptr->array_recs);
+	}
+	xfree(job_ptr->batch_features);
+	xfree(job_ptr->batch_host);
+	xfree(job_ptr->burst_buffer);
+	xfree(job_ptr->burst_buffer_state);
+	xfree(job_ptr->comment);
+	xfree(job_ptr->container);
+	xfree(job_ptr->clusters);
+	xfree(job_ptr->cpus_per_tres);
+	xfree(job_ptr->extra);
+	FREE_NULL_EXTRA_CONSTRAINTS(job_ptr->extra_constraints);
+	xfree(job_ptr->failed_node);
+	job_record_free_fed_details(&job_ptr->fed_details);
+	free_job_resources(&job_ptr->job_resrcs);
+	_clear_job_gres_details(job_ptr);
+	xfree(job_ptr->gres_used);
+	FREE_NULL_LIST(job_ptr->gres_list_req);
+	FREE_NULL_LIST(job_ptr->gres_list_req_accum);
+	FREE_NULL_LIST(job_ptr->gres_list_alloc);
+	FREE_NULL_IDENTITY(job_ptr->id);
+	xfree(job_ptr->licenses);
+	xfree(job_ptr->lic_req);
+	FREE_NULL_LIST(job_ptr->license_list);
+	xfree(job_ptr->limit_set.tres);
+	xfree(job_ptr->mail_user);
+	xfree(job_ptr->mcs_label);
+	xfree(job_ptr->mem_per_tres);
+	xfree(job_ptr->name);
+	xfree(job_ptr->network);
+	xfree(job_ptr->node_addrs);
+	FREE_NULL_BITMAP(job_ptr->node_bitmap);
+	FREE_NULL_BITMAP(job_ptr->node_bitmap_cg);
+	FREE_NULL_BITMAP(job_ptr->node_bitmap_pr);
+	xfree(job_ptr->nodes);
+	xfree(job_ptr->nodes_completing);
+	xfree(job_ptr->nodes_pr);
+	xfree(job_ptr->origin_cluster);
+	if (job_ptr->het_details && job_ptr->het_job_id) {
+		/* xfree struct if hetjob leader and NULL ptr otherwise. */
+		if (job_ptr->het_job_offset == 0)
+			xfree(job_ptr->het_details);
+		else
+			job_ptr->het_details = NULL;
+	}
+	xfree(job_ptr->het_job_id_set);
+	FREE_NULL_LIST(job_ptr->het_job_list);
+	xfree(job_ptr->partition);
+	FREE_NULL_LIST(job_ptr->part_ptr_list);
+	if (job_ptr->part_prio) {
+		xfree(job_ptr->part_prio->priority_array);
+		xfree(job_ptr->part_prio->priority_array_parts);
+		xfree(job_ptr->part_prio);
+	}
+	slurm_destroy_priority_factors(job_ptr->prio_factors);
+	xfree(job_ptr->resp_host);
+	FREE_NULL_LIST(job_ptr->resv_list);
+	xfree(job_ptr->resv_name);
+	xfree(job_ptr->sched_nodes);
+	for (int i = 0; i < job_ptr->spank_job_env_size; i++)
+		xfree(job_ptr->spank_job_env[i]);
+	xfree(job_ptr->spank_job_env);
+	xfree(job_ptr->state_desc);
+	FREE_NULL_LIST(job_ptr->step_list);
+	xfree(job_ptr->system_comment);
+	xfree(job_ptr->tres_alloc_cnt);
+	xfree(job_ptr->tres_alloc_str);
+	xfree(job_ptr->tres_bind);
+	xfree(job_ptr->tres_freq);
+	xfree(job_ptr->tres_fmt_alloc_str);
+	xfree(job_ptr->tres_per_job);
+	xfree(job_ptr->tres_per_node);
+	xfree(job_ptr->tres_per_socket);
+	xfree(job_ptr->tres_per_task);
+	xfree(job_ptr->tres_req_cnt);
+	xfree(job_ptr->tres_req_str);
+	xfree(job_ptr->tres_fmt_req_str);
+	select_g_select_jobinfo_free(job_ptr->select_jobinfo);
+	xfree(job_ptr->user_name);
+	xfree(job_ptr->wckey);
+
+	job_ptr->job_id = 0;
+	/* make sure we don't delete record twice */
+	job_ptr->magic = ~JOB_MAGIC;
+	xfree(job_ptr);
+}
+
+extern void job_record_free_null_array_recs(job_record_t *job_ptr)
+{
+	if (!job_ptr || !job_ptr->array_recs)
+		return;
+
+	FREE_NULL_BITMAP(job_ptr->array_recs->task_id_bitmap);
+	xfree(job_ptr->array_recs->task_id_str);
+	xfree(job_ptr->array_recs);
+}
+
+extern void job_record_free_fed_details(job_fed_details_t **fed_details_pptr)
+{
+	job_fed_details_t *fed_details_ptr = *fed_details_pptr;
+
+	if (fed_details_ptr) {
+		xfree(fed_details_ptr->origin_str);
+		xfree(fed_details_ptr->siblings_active_str);
+		xfree(fed_details_ptr->siblings_viable_str);
+		xfree(fed_details_ptr);
+		*fed_details_pptr = NULL;
+	}
+}

src/common/job_record.h
@@ -622,4 +622,22 @@ extern job_record_t *create_job_record(void);
 /* free_step_record - delete a step record's data structures */
 extern void free_step_record(void *x);
 
+/*
+ *  Delete a job record and its corresponding job_details,
+ *
+ * IN job_entry - pointer to job_record to delete
+ */
+extern void job_record_delete(void *job_entry);
+
+/*
+ * Free an xmalloc'd job_array_struct_t structure inside of a job_record_t and
+ * set job_ptr->array_recs to NULL.
+ */
+extern void job_record_free_null_array_recs(job_record_t *array_recs);
+
+/*
+ * Free job's fed_details ptr.
+ */
+extern void job_record_free_fed_details(job_fed_details_t **fed_details_pptr);
+
 #endif /* _SLURM_JOB_RECORD_H */

src/slurmctld/job_mgr.c
@@ -235,7 +235,6 @@ static void _signal_pending_job_array_tasks(job_record_t *job_ptr, bitstr_t
 static void _add_job_hash(job_record_t *job_ptr);
 static void _add_job_array_hash(job_record_t *job_ptr);
 static void _handle_requeue_limit(job_record_t *job_ptr, const char *caller);
-static void _clear_job_gres_details(job_record_t *job_ptr);
 static int  _copy_job_desc_to_file(job_desc_msg_t * job_desc,
 				   uint32_t job_id);
 static int  _copy_job_desc_to_job_record(job_desc_msg_t * job_desc,
@@ -245,7 +244,6 @@ static int  _copy_job_desc_to_job_record(job_desc_msg_t * job_desc,
 static char *_copy_nodelist_no_dup(char *node_list);
 static int _calc_arbitrary_tpn(job_record_t *job_ptr);
 static job_record_t *_create_job_record(uint32_t num_jobs, bool list_add);
-static void _delete_job_details(job_record_t *job_entry);
 static slurmdb_qos_rec_t *_determine_and_validate_qos(
 	char *resv_name, slurmdb_assoc_rec_t *assoc_ptr,
 	bool operator, slurmdb_qos_rec_t *qos_rec, int *error_code,
@@ -673,7 +671,7 @@ static job_array_resp_msg_t *_resp_array_xlate(resp_array_struct_t *resp,
  *    > 1 - job array create with the task count as num_jobs
  * IN list_add - add to the joblist or not.
  * RET pointer to the record or NULL if error
- * NOTE: allocates memory that should be xfreed with free_job_record
+ * NOTE: allocates memory that should be xfreed with job_record_delete
  */
 static job_record_t *_create_job_record(uint32_t num_jobs, bool list_add)
 {
@@ -690,69 +688,6 @@ static job_record_t *_create_job_record(uint32_t num_jobs, bool list_add)
 	}
 
 	return job_ptr;
-}
-
-/*
- * _delete_job_details - delete a job's detail record and clear it's pointer
- * IN job_entry - pointer to job_record to clear the record of
- */
-static void _delete_job_details(job_record_t *job_entry)
-{
-	int i;
-
-	if (job_entry->details == NULL)
-		return;
-
-	xassert (job_entry->details->magic == DETAILS_MAGIC);
-
-	/*
-	 * Queue up job to have the batch script and environment deleted.
-	 * This is handled by a separate thread to limit the amount of
-	 * time purge_old_job needs to spend holding locks.
-	 */
-	if (IS_JOB_FINISHED(job_entry)) {
-		uint32_t *job_id = xmalloc(sizeof(uint32_t));
-		*job_id = job_entry->job_id;
-		list_enqueue(purge_files_list, job_id);
-	}
-
-	xfree(job_entry->details->acctg_freq);
-	for (i=0; i<job_entry->details->argc; i++)
-		xfree(job_entry->details->argv[i]);
-	xfree(job_entry->details->argv);
-	xfree(job_entry->details->cpu_bind);
-	free_cron_entry(job_entry->details->crontab_entry);
-	FREE_NULL_LIST(job_entry->details->depend_list);
-	xfree(job_entry->details->dependency);
-	xfree(job_entry->details->orig_dependency);
-	xfree(job_entry->details->env_hash);
-	for (i=0; i<job_entry->details->env_cnt; i++)
-		xfree(job_entry->details->env_sup[i]);
-	xfree(job_entry->details->env_sup);
-	xfree(job_entry->details->std_err);
-	FREE_NULL_BITMAP(job_entry->details->exc_node_bitmap);
-	xfree(job_entry->details->exc_nodes);
-	FREE_NULL_LIST(job_entry->details->feature_list);
-	xfree(job_entry->details->features);
-	xfree(job_entry->details->cluster_features);
-	FREE_NULL_BITMAP(job_entry->details->job_size_bitmap);
-	xfree(job_entry->details->std_in);
-	xfree(job_entry->details->mc_ptr);
-	xfree(job_entry->details->mem_bind);
-	FREE_NULL_LIST(job_entry->details->prefer_list);
-	xfree(job_entry->details->prefer);
-	xfree(job_entry->details->req_context);
-	xfree(job_entry->details->std_out);
-	xfree(job_entry->details->submit_line);
-	FREE_NULL_BITMAP(job_entry->details->req_node_bitmap);
-	xfree(job_entry->details->req_nodes);
-	xfree(job_entry->details->script);
-	xfree(job_entry->details->script_hash);
-	xfree(job_entry->details->arbitrary_tpn);
-	xfree(job_entry->details->work_dir);
-	xfree(job_entry->details->x11_magic_cookie);
-	xfree(job_entry->details->x11_target);
-	xfree(job_entry->details);	/* Must be last */
 }
 
 /*
@@ -2536,7 +2471,7 @@ free_it:
 	xfree(comment);
 	xfree(gres_used);
 	xfree(het_job_id_set);
-	free_job_fed_details(&job_fed_details);
+	job_record_free_fed_details(&job_fed_details);
 	free_job_resources(&job_resources);
 	xfree(resp_host);
 	xfree(licenses);
@@ -4173,19 +4108,6 @@ extern bool partition_in_use(char *part_name)
 	return false;
 }
 
-/* Clear a job's GRES details per node strings, rebuilt later on demand */
-static void _clear_job_gres_details(job_record_t *job_ptr)
-{
-	int i;
-
-	xfree(job_ptr->gres_used);
-	for (i = 0; i < job_ptr->gres_detail_cnt; i++)
-		xfree(job_ptr->gres_detail_str[i]);
-	xfree(job_ptr->gres_detail_str);
-	job_ptr->gres_detail_cnt = 0;
-}
-
-
 static bool _job_node_test(job_record_t *job_ptr, int node_inx)
 {
 	if (job_ptr->node_bitmap &&
@@ -4675,7 +4597,7 @@ void init_job_conf(void)
 	}
 
 	if (!purge_jobs_list)
-		purge_jobs_list = list_create(free_job_record);
+		purge_jobs_list = list_create(job_record_delete);
 }
 
 /*
@@ -10865,16 +10787,6 @@ static bool _validate_min_mem_partition(job_desc_msg_t *job_desc_msg,
 	return cc;
 }
 
-extern void free_null_array_recs(job_record_t *job_ptr)
-{
-	if (!job_ptr || !job_ptr->array_recs)
-		return;
-
-	FREE_NULL_BITMAP(job_ptr->array_recs->task_id_bitmap);
-	xfree(job_ptr->array_recs->task_id_str);
-	xfree(job_ptr->array_recs);
-}
-
 static void _delete_job_common(job_record_t *job_ptr)
 {
 	if (!job_ptr->job_id)
@@ -10925,112 +10837,6 @@ static void _move_to_purge_jobs_list(void *job_entry)
 
 	list_append(purge_jobs_list, job_ptr);
 }
-
-extern void free_job_record(void *job_entry)
-{
-	job_record_t *job_ptr = job_entry;
-
-	if (!job_entry)
-		return;
-
-	xassert(job_ptr->magic == JOB_MAGIC);
-
-	_delete_job_details(job_ptr);
-	xfree(job_ptr->account);
-	xfree(job_ptr->admin_comment);
-	xfree(job_ptr->alias_list);
-	xfree(job_ptr->alloc_node);
-	free_null_array_recs(job_ptr);
-	if (job_ptr->array_recs) {
-		FREE_NULL_BITMAP(job_ptr->array_recs->task_id_bitmap);
-		xfree(job_ptr->array_recs->task_id_str);
-		xfree(job_ptr->array_recs);
-	}
-	xfree(job_ptr->batch_features);
-	xfree(job_ptr->batch_host);
-	xfree(job_ptr->burst_buffer);
-	xfree(job_ptr->burst_buffer_state);
-	xfree(job_ptr->comment);
-	xfree(job_ptr->container);
-	xfree(job_ptr->clusters);
-	xfree(job_ptr->cpus_per_tres);
-	xfree(job_ptr->extra);
-	FREE_NULL_EXTRA_CONSTRAINTS(job_ptr->extra_constraints);
-	xfree(job_ptr->failed_node);
-	free_job_fed_details(&job_ptr->fed_details);
-	free_job_resources(&job_ptr->job_resrcs);
-	_clear_job_gres_details(job_ptr);
-	xfree(job_ptr->gres_used);
-	FREE_NULL_LIST(job_ptr->gres_list_req);
-	FREE_NULL_LIST(job_ptr->gres_list_req_accum);
-	FREE_NULL_LIST(job_ptr->gres_list_alloc);
-	FREE_NULL_IDENTITY(job_ptr->id);
-	xfree(job_ptr->licenses);
-	xfree(job_ptr->lic_req);
-	FREE_NULL_LIST(job_ptr->license_list);
-	xfree(job_ptr->limit_set.tres);
-	xfree(job_ptr->mail_user);
-	xfree(job_ptr->mcs_label);
-	xfree(job_ptr->mem_per_tres);
-	xfree(job_ptr->name);
-	xfree(job_ptr->network);
-	xfree(job_ptr->node_addrs);
-	FREE_NULL_BITMAP(job_ptr->node_bitmap);
-	FREE_NULL_BITMAP(job_ptr->node_bitmap_cg);
-	FREE_NULL_BITMAP(job_ptr->node_bitmap_pr);
-	xfree(job_ptr->nodes);
-	xfree(job_ptr->nodes_completing);
-	xfree(job_ptr->nodes_pr);
-	xfree(job_ptr->origin_cluster);
-	if (job_ptr->het_details && job_ptr->het_job_id) {
-		/* xfree struct if hetjob leader and NULL ptr otherwise. */
-		if (job_ptr->het_job_offset == 0)
-			xfree(job_ptr->het_details);
-		else
-			job_ptr->het_details = NULL;
-	}
-	xfree(job_ptr->het_job_id_set);
-	FREE_NULL_LIST(job_ptr->het_job_list);
-	xfree(job_ptr->partition);
-	FREE_NULL_LIST(job_ptr->part_ptr_list);
-	if (job_ptr->part_prio) {
-		xfree(job_ptr->part_prio->priority_array);
-		xfree(job_ptr->part_prio->priority_array_parts);
-		xfree(job_ptr->part_prio);
-	}
-	slurm_destroy_priority_factors(job_ptr->prio_factors);
-	xfree(job_ptr->resp_host);
-	FREE_NULL_LIST(job_ptr->resv_list);
-	xfree(job_ptr->resv_name);
-	xfree(job_ptr->sched_nodes);
-	for (int i = 0; i < job_ptr->spank_job_env_size; i++)
-		xfree(job_ptr->spank_job_env[i]);
-	xfree(job_ptr->spank_job_env);
-	xfree(job_ptr->state_desc);
-	FREE_NULL_LIST(job_ptr->step_list);
-	xfree(job_ptr->system_comment);
-	xfree(job_ptr->tres_alloc_cnt);
-	xfree(job_ptr->tres_alloc_str);
-	xfree(job_ptr->tres_bind);
-	xfree(job_ptr->tres_freq);
-	xfree(job_ptr->tres_fmt_alloc_str);
-	xfree(job_ptr->tres_per_job);
-	xfree(job_ptr->tres_per_node);
-	xfree(job_ptr->tres_per_socket);
-	xfree(job_ptr->tres_per_task);
-	xfree(job_ptr->tres_req_cnt);
-	xfree(job_ptr->tres_req_str);
-	xfree(job_ptr->tres_fmt_req_str);
-	select_g_select_jobinfo_free(job_ptr->select_jobinfo);
-	xfree(job_ptr->user_name);
-	xfree(job_ptr->wckey);
-
-	job_ptr->job_id = 0;
-	/* make sure we don't delete record twice */
-	job_ptr->magic = ~JOB_MAGIC;
-	xfree(job_ptr);
-}
-
 
 /*
  * find specific job_id entry in the job list, key is job_id_ptr
@@ -12412,7 +12218,7 @@ extern void free_old_jobs(void)
 	 * Delete records one-by-one to avoid blocking purge_job_record().
 	 */
 	while ((job_ptr = list_pop(purge_jobs_list)))
-		free_job_record(job_ptr);
+		job_record_delete(job_ptr);
 }
 
 /*
@@ -19567,19 +19373,6 @@ static job_fed_details_t *_dup_job_fed_details(job_fed_details_t *src)
 	return dst;
 }
 
-extern void free_job_fed_details(job_fed_details_t **fed_details_pptr)
-{
-	job_fed_details_t *fed_details_ptr = *fed_details_pptr;
-
-	if (fed_details_ptr) {
-		xfree(fed_details_ptr->origin_str);
-		xfree(fed_details_ptr->siblings_active_str);
-		xfree(fed_details_ptr->siblings_viable_str);
-		xfree(fed_details_ptr);
-		*fed_details_pptr = NULL;
-	}
-}
-
 static void _dump_job_fed_details(job_fed_details_t *fed_details_ptr,
 				  buf_t *buffer)
 {
@@ -19626,7 +19419,7 @@ static int _load_job_fed_details(job_fed_details_t **fed_details_pptr,
 	return SLURM_SUCCESS;
 
 unpack_error:
-	free_job_fed_details(fed_details_pptr);
+	job_record_free_fed_details(fed_details_pptr);
 	*fed_details_pptr = NULL;
 
 	return SLURM_ERROR;

src/slurmctld/slurmctld.h
@@ -866,12 +866,6 @@ extern step_record_t *find_step_record(job_record_t *job_ptr,
 				       slurm_step_id_t *step_id);
 
 /*
- * free_null_array_recs - free an xmalloc'd job_array_struct_t structure inside
- *                        of a job_record_t and set job_ptr->array_recs to NULL.
- */
-extern void free_null_array_recs(job_record_t *array_recs);
-
-/*
  * get_job_env - return the environment variables and their count for a
  *	given job
  * IN job_ptr - pointer to job for which data is required
@@ -2241,11 +2235,6 @@ extern void
 set_remote_working_response(resource_allocation_response_msg_t *resp,
 			    job_record_t *job_ptr,
 			    const char *req_cluster);
-
-/*
- * Free job's fed_details ptr.
- */
-extern void free_job_fed_details(job_fed_details_t **fed_details_pptr);
 
 /*
  * Calculate billable TRES based on partition's defined BillingWeights. If none

src/slurmctld/fed_mgr.c
@@ -1336,7 +1336,7 @@ static void _destroy_dep_job(void *object)
 			FREE_NULL_LIST(job_ptr->details->depend_list);
 			xfree(job_ptr->details);
 		}
-		free_null_array_recs(job_ptr);
+		job_record_free_null_array_recs(job_ptr);
 		job_ptr->magic = 0;
 		job_ptr->job_id = 0;
 		job_ptr->user_id = 0;
@@ -1457,7 +1457,7 @@ static void _cleanup_removed_origin_jobs(void)
 			running_remotely = true;
 
 		/* free fed_job_details so it can't call home. */
-		free_job_fed_details(&job_ptr->fed_details);
+		job_record_free_fed_details(&job_ptr->fed_details);
 
 		/* allow running/completing jobs to finish. */
 		if (IS_JOB_COMPLETED(job_ptr) ||
@@ -1563,7 +1563,7 @@ static void _cleanup_removed_cluster_jobs(slurmdb_cluster_rec_t *cluster)
 				continue;
 
 			/* free fed_job_details so it can't call home. */
-			free_job_fed_details(&job_ptr->fed_details);
+			job_record_free_fed_details(&job_ptr->fed_details);
 
 			/*
 			 * If this job originated from the origin (which is

src/slurmctld/reservation.c
@@ -3181,7 +3181,7 @@ extern int create_resv(resv_desc_msg_t *resv_desc_ptr, char **err_msg)
 		resv_ptr->gres_list_alloc = job_ptr->gres_list_req;
 		gres_job_state_log(resv_ptr->gres_list_alloc, 0);
 		job_ptr->gres_list_req = NULL; /* Nothing left to free */
-		free_job_record(resv_desc_ptr->job_ptr);
+		job_record_delete(resv_desc_ptr->job_ptr);
 		resv_desc_ptr->job_ptr = NULL; /* Nothing left to free */
 	}
 
@@ -3258,7 +3258,7 @@ extern int create_resv(resv_desc_msg_t *resv_desc_ptr, char **err_msg)
 	return SLURM_SUCCESS;
 
  bad_parse:
-	free_job_record(resv_desc_ptr->job_ptr);
+	job_record_delete(resv_desc_ptr->job_ptr);
 	resv_desc_ptr->job_ptr = NULL;
 	for (i = 0; i < account_cnt; i++)
 		xfree(account_list[i]);
@@ -4561,7 +4561,7 @@ static void _resv_node_replace(slurmctld_resv_t *resv_ptr)
 			gres_job_state_log(resv_ptr->gres_list_alloc, 0);
 			job_ptr->gres_list_req = NULL;
 
-			free_job_record(resv_desc.job_ptr);
+			job_record_delete(resv_desc.job_ptr);
 			resv_desc.job_ptr = NULL;
 
 			if (log_it ||
@@ -4592,7 +4592,7 @@ static void _resv_node_replace(slurmctld_resv_t *resv_ptr)
 			}
 			break;
 		}
-		free_job_record(resv_desc.job_ptr);
+		job_record_delete(resv_desc.job_ptr);
 		add_nodes /= 2;	/* Try to get idle nodes as possible */
 		if (log_it ||
 		    (slurm_conf.debug_flags & DEBUG_FLAG_RESERVATION)) {
@@ -4678,7 +4678,7 @@ static void _validate_node_choice(slurmctld_resv_t *resv_ptr)
 		resv_ptr->gres_list_alloc = job_ptr->gres_list_req;
 		gres_job_state_log(resv_ptr->gres_list_alloc, 0);
 		job_ptr->gres_list_req = NULL;
-		free_job_record(resv_desc.job_ptr);
+		job_record_delete(resv_desc.job_ptr);
 		resv_desc.job_ptr = NULL;
 		info("modified reservation %s due to unusable nodes, "
 		     "new nodes: %s", resv_ptr->name, resv_ptr->node_list);
@@ -4689,7 +4689,7 @@ static void _validate_node_choice(slurmctld_resv_t *resv_ptr)
 		debug("reservation %s contains unusable nodes, "
 		      "can't reallocate now", resv_ptr->name);
 	}
-	free_job_record(resv_desc.job_ptr);
+	job_record_delete(resv_desc.job_ptr);
 	_free_resv_select_members(&resv_select);
 }
 
@@ -5077,10 +5077,10 @@ static int  _resize_resv(slurmctld_resv_t *resv_ptr, uint32_t node_cnt)
 		resv_ptr->gres_list_alloc = job_ptr->gres_list_req;
 		gres_job_state_log(resv_ptr->gres_list_alloc, 0);
 		job_ptr->gres_list_req = NULL;
-		free_job_record(resv_desc.job_ptr);
+		job_record_delete(resv_desc.job_ptr);
 		resv_desc.job_ptr = NULL;
 	}
-	free_job_record(resv_desc.job_ptr);
+	job_record_delete(resv_desc.job_ptr);
 
 	return i;
 }

commit 7b773835bc49104735f6ba43ae93f7e95560c217

Fix controller regression not deleting job hash dirs after MinJobAge
A regression was caused by bb987a7f61 inadvertently removed the
code that put the jobid on the purge_files_list which would delete the
job hash dirs. The problem is that purge_files_list is only available to
the controller. So make it local to job_record_.h and only use it when
the controller is deleting a job_record_t. The controller through
_copy_job_desc_to_file() is the only one writing the job script and
environment to the file system.

Ticket 20238

src/common/job_record.c
@@ -46,6 +46,8 @@
 
 #define DETAILS_FLAG 0xdddd
 
+list_t *purge_files_list = NULL; /* job files to delete, only used by ctld */
+
 typedef struct {
 	int node_index;
 	int node_count;
@@ -149,6 +151,17 @@ static void _delete_job_details(job_record_t *job_entry)
 		return;
 
 	xassert (job_entry->details->magic == DETAILS_MAGIC);
+
+	/*
+	 * Queue up job to have the batch script and environment deleted.
+	 * This is handled by a separate thread to limit the amount of
+	 * time purge_old_job needs to spend holding locks.
+	 */
+	if (IS_JOB_FINISHED(job_entry) && purge_files_list) {
+		uint32_t *job_id = xmalloc(sizeof(uint32_t));
+		*job_id = job_entry->job_id;
+		list_enqueue(purge_files_list, job_id);
+	}
 
 	xfree(job_entry->details->acctg_freq);
 	for (i=0; i<job_entry->details->argc; i++)

src/common/job_record.h
@@ -46,6 +46,7 @@ typedef struct slurmctld_resv slurmctld_resv_t;
 #endif
 
 extern time_t last_job_update;	/* time of last update to job records */
+extern list_t *purge_files_list; /* job files to delete, only used by ctld */
 
 #define DETAILS_MAGIC	0xdea84e7
 #define JOB_MAGIC	0xf0b7392c

src/slurmctld/job_mgr.c
@@ -202,7 +202,6 @@ typedef struct {
 List   job_list = NULL;		/* job_record list */
 time_t last_job_update;		/* time of last update to job records */
 
-List purge_files_list = NULL;	/* job files to delete */
 list_t *purge_jobs_list = NULL;	/* job_record_t entries to free */
 
 /* Local variables */

src/slurmctld/slurmctld.h
@@ -424,7 +424,6 @@ extern time_t last_resv_update;	/* time of last resv_list update */
  *  Job lists
 \*****************************************************************************/
 extern List job_list;			/* list of job_record entries */
-extern List purge_files_list;		/* list of job ids to purge files of */
 extern list_t *purge_jobs_list;		/* list of job_record_t to free */
 
 /*****************************************************************************\