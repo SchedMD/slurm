SchedMD/slurm

commit c9789fe0c4ea758cff3815e52ce7340d4c6c6a3f

Limit the number of threads in _rpc_acct_gather_energy() to 10.
Bug 17294

src/slurmd/slurmd/req.c
@@ -3378,6 +3378,8 @@ static void _rpc_acct_gather_energy(slurm_msg_t *msg)
 {
 	int        rc = SLURM_SUCCESS;
 	static bool first_msg = true;
+	static uint32_t req_cnt = 0;
+	static pthread_mutex_t req_cnt_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 	if (!_slurm_authorized_user(msg->auth_uid)) {
 		error("Security violation, acct_gather_update RPC from uid %u",
@@ -3389,6 +3391,20 @@ static void _rpc_acct_gather_energy(slurm_msg_t *msg)
 		rc = ESLURM_USER_ID_MISSING;	/* or bad in this case */
 	}
 	first_msg = false;
+
+	/*
+	 * Avoid tying up too many slurmd threads if the IPMI (or similar)
+	 * interface is locked up. The request would likely eventually fail
+	 * anyways, so dying early isn't much worse here.
+	 */
+	slurm_mutex_lock(&req_cnt_mutex);
+	if (req_cnt < 10) {
+		req_cnt++;
+	} else {
+		error("%s: Too many pending requests", __func__);
+		rc = SLURM_ERROR;
+	}
+	slurm_mutex_unlock(&req_cnt_mutex);
 
 	if (rc != SLURM_SUCCESS) {
 		if (slurm_send_rc_msg(msg, rc) < 0)
@@ -3417,7 +3433,7 @@ static void _rpc_acct_gather_energy(slurm_msg_t *msg)
 
 		if (!sensor_cnt) {
 			error("Can't get energy data. No power sensors are available. Try later.");
-			return;
+			goto fini;
 		}
 
 		/* If we polled later than delta seconds then force a
@@ -3442,6 +3458,11 @@ static void _rpc_acct_gather_energy(slurm_msg_t *msg)
 
 		acct_gather_energy_destroy(acct_msg.energy);
 	}
+
+fini:
+	slurm_mutex_lock(&req_cnt_mutex);
+	req_cnt--;
+	slurm_mutex_unlock(&req_cnt_mutex);
 }
 
 static int _signal_jobstep(slurm_step_id_t *step_id, uint16_t signal,

commit be746cf4918b7a965409efed21b693fb150522b5

slurmd - Fix request counter underflow for energy gathering
This is a regression introduced by commit c9789fe0c4e.

When the number of threads requesting energy exceeds 10 it is possible to
reach a condition where some threads do not increment req_cnt but all of
them decrement the variable. This ends up in an underflow of this variable,
making it impossible to gather energy for any thread until a slurmd reboot.

Ticket 19779.

src/slurmd/slurmd/req.c
@@ -3307,6 +3307,7 @@ static void _rpc_acct_gather_energy(slurm_msg_t *msg)
 	static bool first_msg = true;
 	static uint32_t req_cnt = 0;
 	static pthread_mutex_t req_cnt_mutex = PTHREAD_MUTEX_INITIALIZER;
+	bool req_added = false;
 
 	if (!_slurm_authorized_user(msg->auth_uid)) {
 		error("Security violation, acct_gather_update RPC from uid %u",
@@ -3327,6 +3328,7 @@ static void _rpc_acct_gather_energy(slurm_msg_t *msg)
 	slurm_mutex_lock(&req_cnt_mutex);
 	if (req_cnt < 10) {
 		req_cnt++;
+		req_added = true;
 	} else {
 		error("%s: Too many pending requests", __func__);
 		rc = ESLURMD_TOO_MANY_RPCS;
@@ -3348,7 +3350,7 @@ static void _rpc_acct_gather_energy(slurm_msg_t *msg)
 			rc = SLURM_PROTOCOL_VERSION_ERROR;
 			if (slurm_send_rc_msg(msg, rc) < 0)
 				error("Error responding to energy request: %m");
-			return;
+			goto end;
 		}
 
 		acct_gather_energy_g_get_data(req->context_id,
@@ -3386,10 +3388,12 @@ static void _rpc_acct_gather_energy(slurm_msg_t *msg)
 
 		acct_gather_energy_destroy(acct_msg.energy);
 	}
-
-	slurm_mutex_lock(&req_cnt_mutex);
-	req_cnt--;
-	slurm_mutex_unlock(&req_cnt_mutex);
+end:
+	if (req_added) {
+		slurm_mutex_lock(&req_cnt_mutex);
+		req_cnt--;
+		slurm_mutex_unlock(&req_cnt_mutex);
+	}
 }
 
 static int _signal_jobstep(slurm_step_id_t *step_id, uint16_t signal,