SchedMD/slurm

commit edf41cf901d2ffa0a9c30a45de7cafdfae3a4af8

Join a new mount namespace in clone and mount /proc for get-user-env
Getting the user environment e.g. by setting --get-user-env or export=
in a batch script spawns a child which runs containerized into a pid
namespace, and which later executes a su or sudo to log in as the user
and get his environment. Some user environment scripts may require
reading /proc, and since these programs are run into its own pid
namespace starting from pid 1, they will also require its specific
/proc mountpoint or they would access the /proc related data from the host
and not from the container.

With this fix the cloned child is started in a new mount namespace.
In essence this emulates what "unshare --mount-proc" does, and fixes
issues where /proc/<pid> incorrectly resolved to processes from the host
namespace, which could make external init scripts to not behave as
they should (like when using lmod).

If the fork() approach is used instead of clone() (FreeBSD only), then we
don't need to mount /proc since we will not be working with namespaces.

Ticket 19160.

src/common/env.c
@@ -50,6 +50,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <sys/mount.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -105,6 +106,7 @@ typedef struct {
 	int rlimit;
 	char **tmp_env;
 	const char *username;
+	bool setup_namespaces;
 } child_args_t;
 
 /*
@@ -2001,6 +2003,20 @@ static int _child_fn(void *arg)
 	cmdstr = child_args->cmdstr;
 	tmp_env = child_args->tmp_env;
 
+	/*
+	 * Setting propagation and mounting our own /proc for this namespace.
+	 * This is done to ensure that this cloned process and its children
+	 * have coherent /proc contents with their virtual PIDs.
+	 * Check _clone_env_child to see namespace flags used in clone.
+	 */
+	if (child_args->setup_namespaces) {
+		if (mount("none", "/proc", NULL, MS_PRIVATE|MS_REC, NULL))
+			_exit(1);
+		if (mount("proc", "/proc", "proc",
+			  MS_NOSUID|MS_NOEXEC|MS_NODEV, NULL))
+			_exit(1);
+	}
+
 	if ((devnull = open("/dev/null", O_RDWR)) != -1) {
 		dup2(devnull, STDIN_FILENO);
 		dup2(devnull, STDERR_FILENO);
@@ -2047,8 +2063,9 @@ static int _clone_env_child(child_args_t *child_args)
 	 * Killing the 'child' pid will kill all the namespace, since in the
 	 * namespace, this 'child' is pid 1.
 	 */
+	child_args->setup_namespaces = true;
 	return clone(_child_fn, child_stack + STACK_SIZE,
-		     (SIGCHLD|CLONE_NEWPID), child_args);
+		     (SIGCHLD|CLONE_NEWPID|CLONE_NEWNS), child_args);
 }
 #endif
 
@@ -2124,6 +2141,7 @@ char **env_array_user_default(const char *username, int timeout, int mode,
 	child_args.fildes = fildes;
 	child_args.username = username;
 	child_args.cmdstr = cmdstr;
+	child_args.setup_namespaces = false;
 	child_args.tmp_env = env_array_create();
 	env_array_overwrite(&child_args.tmp_env, "ENVIRONMENT", "BATCH");
 	if (getrlimit(RLIMIT_NOFILE, &rlim) < 0) {

commit 839f8e7b89b8a5e0775e67606a32991fe362aa18

Fix compilation issue on FreeBSD
Commit edf41cf901 introduced a change that relied on the mount syscall
with flags that FreeBSD does not support, breaking compilation.

Since these mount operatives are only done in cases where the host
is not FreeBSD/NetBSD/Apple, use an ifdef instead of the conditionals
to avoid compiling them altogether in these cases.

Ticket 20331

src/common/env.c
@@ -103,7 +103,6 @@ typedef struct {
 	int rlimit;
 	char **tmp_env;
 	const char *username;
-	bool setup_namespaces;
 } child_args_t;
 
 /*
@@ -2095,19 +2094,19 @@ static int _child_fn(void *arg)
 	cmdstr = child_args->cmdstr;
 	tmp_env = child_args->tmp_env;
 
+#if !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(__NetBSD__)
 	/*
 	 * Setting propagation and mounting our own /proc for this namespace.
 	 * This is done to ensure that this cloned process and its children
 	 * have coherent /proc contents with their virtual PIDs.
 	 * Check _clone_env_child to see namespace flags used in clone.
 	 */
-	if (child_args->setup_namespaces) {
-		if (mount("none", "/proc", NULL, MS_PRIVATE|MS_REC, NULL))
-			_exit(1);
-		if (mount("proc", "/proc", "proc",
-			  MS_NOSUID|MS_NOEXEC|MS_NODEV, NULL))
-			_exit(1);
-	}
+	if (mount("none", "/proc", NULL, MS_PRIVATE|MS_REC, NULL))
+		_exit(1);
+	if (mount("proc", "/proc", "proc",
+		  MS_NOSUID|MS_NOEXEC|MS_NODEV, NULL))
+		_exit(1);
+#endif
 
 	if ((devnull = open("/dev/null", O_RDWR)) != -1) {
 		dup2(devnull, STDIN_FILENO);
@@ -2155,7 +2154,6 @@ static int _clone_env_child(child_args_t *child_args)
 	 * Killing the 'child' pid will kill all the namespace, since in the
 	 * namespace, this 'child' is pid 1.
 	 */
-	child_args->setup_namespaces = true;
 	return clone(_child_fn, child_stack + STACK_SIZE,
 		     (SIGCHLD|CLONE_NEWPID|CLONE_NEWNS), child_args);
 }
@@ -2233,7 +2231,6 @@ char **env_array_user_default(const char *username, int timeout, int mode,
 	child_args.fildes = fildes;
 	child_args.username = username;
 	child_args.cmdstr = cmdstr;
-	child_args.setup_namespaces = false;
 	child_args.tmp_env = env_array_create();
 	env_array_overwrite(&child_args.tmp_env, "ENVIRONMENT", "BATCH");
 	if (getrlimit(RLIMIT_NOFILE, &rlim) < 0) {