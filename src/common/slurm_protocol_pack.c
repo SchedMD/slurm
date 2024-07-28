b12d43691319a099d64684f6ca590c7360f73025
Brian Christiansen <brian@schedmd.com>
2024-07-27 22:37:46 -0600

    Fix resource leak
    If safe_xcalloc() failed and jumped to the label unpack_error then
    hidden_nodes could be leaked.
    
    Continuation of d193b5b71b8
    
    CID 397542

--- src/common/slurm_protocol_pack.c
+++ src/common/slurm_protocol_pack.c
@@ -1498,6 +1498,7 @@ static int _unpack_node_info_msg(node_info_msg_t **msg, buf_t *buffer,
 {
 	int i;
 	node_info_msg_t *tmp_ptr;
+	bitstr_t *hidden_nodes = NULL;
 
 	xassert(msg);
 	tmp_ptr = xmalloc(sizeof(node_info_msg_t));
@@ -1505,8 +1506,6 @@ static int _unpack_node_info_msg(node_info_msg_t **msg, buf_t *buffer,
 
 	/* load buffer's header (data structure version and time) */
 	if (protocol_version >= SLURM_24_11_PROTOCOL_VERSION) {
-		bitstr_t *hidden_nodes = NULL;
-
 		safe_unpack32(&tmp_ptr->record_count, buffer);
 		safe_unpack_time(&tmp_ptr->last_update, buffer);
 		unpack_bit_str_hex(&hidden_nodes, buffer);
@@ -1548,6 +1547,7 @@ static int _unpack_node_info_msg(node_info_msg_t **msg, buf_t *buffer,
 	return SLURM_SUCCESS;
 
 unpack_error:
+	FREE_NULL_BITMAP(hidden_nodes);
 	slurm_free_node_info_msg(tmp_ptr);
 	*msg = NULL;
 	return SLURM_ERROR;
