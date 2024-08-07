89d2a362c80e8c4b2550d07d0032880ba13c81c6
Marshall Garey <marshall@schedmd.com>
2024-08-07 10:47:13 -0600

    Revert "slurmdbd - Convert _commit_handler() to conmgr"
    This reverts commit fd82ff4e6f08f882e14d89629236d20850f88fad.

--- src/slurmdbd/slurmdbd.c
+++ src/slurmdbd/slurmdbd.c
@@ -76,8 +76,6 @@
 #include "src/slurmdbd/proc_req.h"
 #include "src/slurmdbd/backup.h"
 
-#define DEFAULT_COMMIT_DELAY 5
-
 typedef struct {
 #define RUN_ARGS_MAGIC 0xfeb0afb9
 	int magic; /* RUN_ARGS_MAGIC */
@@ -102,6 +100,7 @@ static log_options_t log_opts = 	/* Log to stderr & syslog */
 static int	 new_nice = 0;
 static pthread_t rpc_handler_thread = 0; /* thread ID for RPC hander */
 static pthread_t rollup_handler_thread = 0; /* thread ID for rollup hander */
+static pthread_t commit_handler_thread = 0; /* thread ID for commit hander */
 static pthread_mutex_t rollup_lock = PTHREAD_MUTEX_INITIALIZER;
 static bool running_rollup = 0;
 static bool running_commit = 0;
@@ -110,7 +109,8 @@ static bool reset_lft_rgt = 0;
 static List lft_rgt_list = NULL;
 
 /* Local functions */
-static void _commit_handler(conmgr_callback_args_t conmgr_args, void *arg);
+static void  _commit_handler_cancel(void);
+static void *_commit_handler(void *no_data);
 static void  _daemonize(void);
 static void  _init_config(void);
 static void  _init_pidfile(void);
@@ -286,8 +286,7 @@ static void _run(conmgr_callback_args_t conmgr_args, void *arg)
 
 	registered_clusters = list_create(NULL);
 
-	conmgr_add_work_delayed_fifo(_commit_handler, args,
-				     DEFAULT_COMMIT_DELAY, 0);
+	slurm_thread_create(&commit_handler_thread, _commit_handler, NULL);
 
 	memset(&assoc_init_arg, 0, sizeof(assoc_init_args_t));
 
@@ -304,7 +303,7 @@ static void _run(conmgr_callback_args_t conmgr_args, void *arg)
 	if (assoc_mgr_init(args->db_conn, &assoc_init_arg, errno) == SLURM_ERROR) {
 		error("Problem getting cache of data");
 		acct_storage_g_close_connection(&args->db_conn);
-		return;
+		goto end_it;
 	}
 
 	if (reset_lft_rgt) {
@@ -387,6 +386,13 @@ static void _run(conmgr_callback_args_t conmgr_args, void *arg)
 		if (shutdown_time)
 			break;
 	}
+	/* Daemon termination handled here */
+
+end_it:
+
+	slurm_thread_join(commit_handler_thread);
+
+	conmgr_add_work_fifo(_run_fini, args);
 }
 
 static void _run_fini(conmgr_callback_args_t conmgr_args, void *arg)
@@ -497,6 +503,7 @@ extern void shutdown_threads(void)
 	/* End commit before rpc_mgr_wake.  It will do the final
 	   commit on the connection.
 	*/
+	_commit_handler_cancel();
 	rpc_mgr_wake();
 	_rollup_handler_cancel();
 
@@ -885,6 +892,16 @@ static void *_rollup_handler(void *db_conn)
 	return NULL;
 }
 
+static void _commit_handler_cancel()
+{
+	if (running_commit)
+		debug("Waiting for commit thread to finish.");
+	slurm_mutex_lock(&registered_lock);
+	if (commit_handler_thread)
+		pthread_cancel(commit_handler_thread);
+	slurm_mutex_unlock(&registered_lock);
+}
+
 static void _try_apply_commit(void)
 {
 	slurmdbd_conn_t *slurmdbd_conn = NULL;
@@ -907,33 +924,23 @@ static void _try_apply_commit(void)
 }
 
 /* _commit_handler - Process commit's of registered clusters */
-static void _commit_handler(conmgr_callback_args_t conmgr_args, void *arg)
+static void *_commit_handler(void *db_conn)
 {
-	const time_t delay = slurmdbd_conf->commit_delay ?
-		slurmdbd_conf->commit_delay : DEFAULT_COMMIT_DELAY;
-	run_args_t *args = arg;
+	(void) pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+	(void) pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 
-	xassert(args->magic == RUN_ARGS_MAGIC);
+	while (!shutdown_time) {
+		/* Commit each slurmctld's info */
+		_try_apply_commit();
 
-	if (shutdown_time) {
-		debug3("%s: shutdown requested", __func__);
-		conmgr_add_work_fifo(_run_fini, args);
-		return;
+		/* This really doesn't need to be synconized so just
+		 * sleep for a bit and do it again.
+		 */
+		sleep(slurmdbd_conf->commit_delay ?
+		      slurmdbd_conf->commit_delay : 5);
 	}
 
-	/* Commit each slurmctld's info */
-	_try_apply_commit();
-
-	if (conmgr_args.status == CONMGR_WORK_STATUS_CANCELLED) {
-		debug3("%s: cancelled", __func__);
-		return;
-	}
-
-	/*
-	 * This really doesn't need to be synchronized so just
-	 * sleep for a bit and do it again.
-	 */
-	conmgr_add_work_delayed_fifo(_commit_handler, args, delay, 0);
+	return NULL;
 }
 
 /*
