SchedMD/slurm

commit be80ffc0de7a162866e39ab5d5bbc989f454767f

openapi/slurmctld - Port openapi.json entries for `DELETE /slurm/v0.0.40/jobs/state` to path bindings
Continuation of abee69f705.

Bug 18464

src/slurmrestd/plugins/openapi/slurmctld/api.c
@@ -225,6 +225,16 @@ const openapi_path_binding_t openapi_paths[] = {
 				},
 				.query = DATA_PARSER_OPENAPI_JOB_INFO_QUERY,
 			},
+			{
+				.method = HTTP_REQUEST_DELETE,
+				.tags = tags,
+				.summary = "send signal to list of jobs",
+				.response = {
+					.type = DATA_PARSER_OPENAPI_KILL_JOBS_RESP,
+					.description = "description of jobs to signal",
+				},
+				.query = DATA_PARSER_KILL_JOBS_MSG,
+			},
 			{0}
 		},
 		.flags = OP_FLAGS,

commit d3c85c40fe45595e4043524bc10fc51fba360539

Use body and not query for `DELETE /slurm/v0.0.40/jobs/`
Regression from be80ffc0de.

Ticket 20538

doc/html/openapi_release_notes/slurm-24.05.3.html
@@ -1,1 +1,109 @@
 <h2 id="24053">Slurm 24.05.3<a class="slurm_link" href="#24053"></a></h2>
+
+<h3>data_parser/v0.0.41</h3>
+
+<h4>Modified Fields</h4>
+<table class="tchange">
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.components.schemas["v0.0.41_kill_jobs_msg"] = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.components.schemas["v0.0.41_kill_jobs_msg"].required = [];</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.components.schemas["v0.0.41_kill_jobs_msg"].type = "object";</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Removed</td>
+	<td class="tdchange">.paths["/slurm/v0.0.41/jobs/"]["delete"].parameters = [];</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Replaced</td>
+	<td class="tdchange">.paths["/slurm/v0.0.41/jobs/"]["delete"].requestBody = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.41/jobs/"]["delete"].requestBody.content = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.41/jobs/"]["delete"].requestBody.content["application/json"] = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.41/jobs/"]["delete"].requestBody.content["application/json"].schema = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.41/jobs/"]["delete"].requestBody.content["application/json"].schema.$ref = "#/components/schemas/v0.0.41_kill_jobs_msg";</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.41/jobs/"]["delete"].requestBody.content["application/x-yaml"] = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.41/jobs/"]["delete"].requestBody.content["application/x-yaml"].schema = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.41/jobs/"]["delete"].requestBody.content["application/x-yaml"].schema.$ref = "#/components/schemas/v0.0.41_kill_jobs_msg";</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.41/jobs/"]["delete"].requestBody.description = "Signal or cancel jobs";</td>
+</tr>
+</table>
+
+<h3>data_parser/v0.0.40</h3>
+
+<h4>Modified Fields</h4>
+<table class="tchange">
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.components.schemas["v0.0.40_kill_jobs_msg"] = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Removed</td>
+	<td class="tdchange">.paths["/slurm/v0.0.40/jobs/"]["delete"].parameters = [];</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Replaced</td>
+	<td class="tdchange">.paths["/slurm/v0.0.40/jobs/"]["delete"].requestBody = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.40/jobs/"]["delete"].requestBody.content = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.40/jobs/"]["delete"].requestBody.content["application/json"] = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.40/jobs/"]["delete"].requestBody.content["application/json"].schema = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.40/jobs/"]["delete"].requestBody.content["application/json"].schema.$ref = "#/components/schemas/v0.0.40_kill_jobs_msg";</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.40/jobs/"]["delete"].requestBody.content["application/x-yaml"] = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.40/jobs/"]["delete"].requestBody.content["application/x-yaml"].schema = {};</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.40/jobs/"]["delete"].requestBody.content["application/x-yaml"].schema.$ref = "#/components/schemas/v0.0.40_kill_jobs_msg";</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">.paths["/slurm/v0.0.40/jobs/"]["delete"].requestBody.description = "Signal or cancel jobs";</td>
+</tr>
+</table>

src/slurmrestd/plugins/openapi/slurmctld/api.c
@@ -255,7 +255,10 @@ const openapi_path_binding_t openapi_paths[] = {
 					.type = DATA_PARSER_OPENAPI_KILL_JOBS_RESP,
 					.description = "description of jobs to signal",
 				},
-				.query = DATA_PARSER_KILL_JOBS_MSG,
+				.body = {
+					.type = DATA_PARSER_KILL_JOBS_MSG,
+					.description = "Signal or cancel jobs",
+				},
 			},
 			{0}
 		},