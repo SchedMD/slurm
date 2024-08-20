c3150c30a6c71d6bd80c0c4e605c0883010d835e
Nathan Rini <nate@schedmd.com>
2024-08-20 13:12:09 -0600

    data_parser/v0.0.40 - Set obj_openapi in add_fa()
    Required for add_removed() to work.
    
    Ticket 20718

--- src/plugins/data_parser/v0.0.40/parsers.c
+++ src/plugins/data_parser/v0.0.40/parsers.c
@@ -526,7 +526,7 @@ extern void check_parser_funcname(const parser_t *const parser,
 		xassert(parser->ptr_offset == NO_VAL);
 		xassert(!parser->pointer_type);
 		xassert(!parser->array_type);
-		xassert(!parser->obj_openapi);
+		xassert(parser->obj_openapi == OPENAPI_FORMAT_ARRAY);
 	} else if (parser->model == PARSER_MODEL_LIST) {
 		/* parser of a List */
 		xassert(parser->list_type > DATA_PARSER_TYPE_INVALID);
@@ -9565,6 +9565,7 @@ static const parser_t PARSER_ARRAY(OPENAPI_JOB_STATE_RESP)[] = {
 		.type = DATA_PARSER_##typev,                                   \
 		.type_string = XSTRINGIFY(DATA_PARSER_ ## typev),              \
 		.obj_type_string = XSTRINGIFY(typet),                          \
+		.obj_openapi = OPENAPI_FORMAT_ARRAY,                           \
 		.size = sizeof(typet),                                         \
 		.needs = NEED_NONE,                                            \
 		.flag_bit_array = PARSER_FLAG_ARRAY(typev),                    \
