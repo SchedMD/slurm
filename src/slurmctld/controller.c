SchedMD/slurm

commit 489f39c2c4c27e51bf9c5c739054981534dedb74

Re-exec slurmctld to run slurmscriptd
conmgr_init() creates threads and is called before slurmscriptd_init().
slurmscriptd_init() calls fork() and the slurmscriptd runs in the child
process. fork(2) says:

       After a fork() in a multithreaded program, the child can safely call
       only async-signal-safe functions (see signal-safety(7)) until such
       time as it calls execve(2).

In order to comply with this, re-exec slurmctld after the fork() in
slurmscriptd mode which is denoted by an environment variable. The newly
exec'd slurmctld re-initializes its configuration and then runs the
slurmscriptd main loop.

Ticket 20114

Signed-off-by: Nathan Rini <nate@schedmd.com>

src/slurmctld/controller.c
@@ -430,6 +430,7 @@ int main(int argc, char **argv)
 	};
 	bool create_clustername_file;
 	bool backup_has_control = false;
+	bool slurmscriptd_mode = false;
 	char *conf_file;
 	stepmgr_ops_t stepmgr_ops = {0};
 
@@ -446,12 +447,14 @@ int main(int argc, char **argv)
 
 	if (getenv("SLURMCTLD_RECONF"))
 		original = false;
+	if (getenv(SLURMSCRIPTD_MODE_ENV))
+		slurmscriptd_mode = true;
 
 	/*
 	 * Make sure we have no extra open files which
 	 * would be propagated to spawned tasks.
 	 */
-	if (original)
+	if (original || slurmscriptd_mode)
 		closeall(3);
 
 	/*
@@ -474,6 +477,13 @@ int main(int argc, char **argv)
 	lock_slurmctld(config_write_lock);
 	update_logging();
 	unlock_slurmctld(config_write_lock);
+
+	if (slurmscriptd_mode) {
+		/* Cleanup env */
+		(void) unsetenv(SLURMSCRIPTD_MODE_ENV);
+		/* Execute in slurmscriptd mode. */
+		slurmscriptd_run_slurmscriptd(argc, argv, binary);
+	}
 
 	memset(&slurmctld_diag_stats, 0, sizeof(slurmctld_diag_stats));
 	/*

src/slurmctld/slurmscriptd.c
@@ -1067,11 +1067,15 @@ static void _setup_eio(int fd)
 }
 
 __attribute__((noreturn))
-static void _slurmscriptd_mainloop(int argc, char **argv, char *binary_path)
+extern void slurmscriptd_run_slurmscriptd(int argc, char **argv,
+					  char *binary_path)
 {
 	ssize_t i;
 	int rc = SLURM_ERROR, ack;
 	char *failed_plugin = NULL;
+
+	slurmscriptd_writefd = STDOUT_FILENO;
+	slurmscriptd_readfd = STDIN_FILENO;
 
 	_change_proc_name(argc, argv, "slurmscriptd");
 
@@ -1689,8 +1693,17 @@ extern int slurmscriptd_init(int argc, char **argv, char *binary_path)
 			      __func__);
 			_exit(1);
 		}
-		/* Main loop */
-		_slurmscriptd_mainloop(argc, argv, binary_path);
+		/*
+		 * Dup needed file descriptors and re-exec self.
+		 * We do not need to closeall() here because it will happen on
+		 * the re-exec.
+		 */
+		dup2(slurmscriptd_readfd, STDIN_FILENO);
+		dup2(slurmscriptd_writefd, STDOUT_FILENO);
+		dup2(slurmscriptd_writefd, STDERR_FILENO);
+		setenv(SLURMSCRIPTD_MODE_ENV, "1", 1);
+		execv(binary_path, argv);
+		fatal("%s: execv() failed: %m", __func__);
 		/* Never returns */
 	}
 

src/slurmctld/slurmscriptd.h
@@ -36,6 +36,16 @@
 #ifndef _HAVE_SLURMSCRIPTD_H
 #define _HAVE_SLURMSCRIPTD_H
 
+#define SLURMSCRIPTD_MODE_ENV "SLURMSCRIPTD_MODE"
+
+/*
+ * Run the the slurmscriptd main loop.
+ * Does not return - calls exit.
+ */
+__attribute__((noreturn))
+extern void slurmscriptd_run_slurmscriptd(int argc, char **argv,
+					  char *binary_path);
+
 extern int slurmscriptd_init(int argc, char **argv, char *binary_path);
 
 extern int slurmscriptd_fini(void);

commit b82b1bd30ed5e96196ae4c14d528fa2eb9c3bfee

Ensure that slurmscriptd runs as SlurmUser
Regression from commit 489f39c2c4.

Ticket 20114

Signed-off-by: Nathan Rini <nate@schedmd.com>

src/slurmctld/controller.c
@@ -484,6 +484,7 @@ int main(int argc, char **argv)
 		/* Cleanup env */
 		(void) unsetenv(SLURMSCRIPTD_MODE_ENV);
 		/* Execute in slurmscriptd mode. */
+		become_slurm_user();
 		slurmscriptd_run_slurmscriptd(argc, argv, binary);
 	}
 