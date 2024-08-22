SchedMD/slurm

commit 942aa4c7c614682f4394bdca291083b6743f8b1e

slurmctld - Replace rpc_mgr with conmgr
Unify all listening sockets in conmgr and then direct to relevant RPC
processing based on if process is running as primary or backup.

Remove slurmctld_config.thread_id_rpc entirely. Maintain sending signals
as they interrupt I/O that is not conmgr controlled.

Ticket 20114

src/slurmctld/controller.c
@@ -182,8 +182,6 @@ bool cloud_dns = false;
 uint32_t cluster_cpus = 0;
 time_t	control_time = 0;
 bool disable_remote_singleton = false;
-int listen_nports = 0;
-struct pollfd *listen_fds = NULL;
 int max_depend_depth = 10;
 time_t	last_proc_req_start = 0;
 uint32_t max_powered_nodes = NO_VAL;
@@ -241,6 +239,12 @@ static pthread_mutex_t shutdown_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t shutdown_cond = PTHREAD_COND_INITIALIZER;
 static bool under_systemd = false;
 
+/* Array of listening sockets */
+static struct {
+	int count;
+	int *fd;
+} listeners;
+
 typedef struct primary_thread_arg {
 	pid_t cpid;
 	char *prog_type;
@@ -250,7 +254,6 @@ static int          _accounting_cluster_ready();
 static int          _accounting_mark_all_nodes_down(char *reason);
 static void *       _assoc_cache_mgr(void *no_data);
 static int          _controller_index(void);
-static void _close_ports(void);
 static void         _create_clustername_file(void);
 static void _flush_rpcs(void);
 static void         _get_fed_updates();
@@ -268,11 +271,11 @@ static void         _remove_qos(slurmdb_qos_rec_t *rec);
 static void         _restore_job_dependencies(void);
 static void         _run_primary_prog(bool primary_on);
 static void         _send_future_cloud_to_db();
-static void *       _service_connection(void *arg);
+static void _service_connection(conmgr_callback_args_t conmgr_args,
+				int input_fd, int output_fd, void *arg);
 static void         _set_work_dir(void);
 static int          _shutdown_backup_controller(void);
 static void *       _slurmctld_background(void *no_data);
-static void *       _slurmctld_rpc_mgr(void *no_data);
 static void         _test_thread_limit(void);
 static int _try_to_reconfig(void);
 static void         _update_assoc(slurmdb_assoc_rec_t *rec);
@@ -283,7 +286,6 @@ static void _update_pidfile(void);
 static void         _update_qos(slurmdb_qos_rec_t *rec);
 static void _usage(void);
 static bool         _verify_clustername(void);
-static bool         _wait_for_server_thread(void);
 static void *       _wait_primary_prog(void *arg);
 
 static void _try_to_reconfig_deferred(conmgr_callback_args_t conmgr_args,
@@ -557,6 +559,9 @@ int main(int argc, char **argv)
 		become_slurm_user();
 	}
 
+	rate_limit_init();
+	rpc_queue_init();
+
 	/* open ports must happen after become_slurm_user() */
 	 _open_ports();
 
@@ -824,12 +829,6 @@ int main(int argc, char **argv)
 			fatal("failed to initialize mcs plugin");
 
 		/*
-		 * create attached thread to process RPCs
-		 */
-		slurm_thread_create(&slurmctld_config.thread_id_rpc,
-				    _slurmctld_rpc_mgr, NULL);
-
-		/*
 		 * create attached thread for state save
 		 */
 		slurm_thread_create(&slurmctld_config.thread_id_save,
@@ -876,8 +875,6 @@ int main(int argc, char **argv)
 		agent_fini();
 
 		/* termination of controller */
-		slurm_thread_join(slurmctld_config.thread_id_rpc);
-		slurmctld_config.thread_id_rpc = (pthread_t) 0;
 		switch_g_save();
 		priority_g_fini();
 		shutdown_state_save();
@@ -1014,9 +1011,10 @@ int main(int argc, char **argv)
 
 	conmgr_request_shutdown();
 
-	_close_ports();
-
 	conmgr_fini();
+
+	rate_limit_shutdown();
+	rpc_queue_shutdown();
 	log_fini();
 	sched_log_fini();
 
@@ -1144,7 +1142,6 @@ static void  _init_config(void)
 	slurm_mutex_init(&slurmctld_config.thread_count_lock);
 	slurm_cond_init(&slurmctld_config.thread_count_cond, NULL);
 	slurmctld_config.thread_id_main    = (pthread_t) 0;
-	slurmctld_config.thread_id_rpc     = (pthread_t) 0;
 }
 
 static int _try_to_reconfig(void)
@@ -1166,13 +1163,13 @@ static int _try_to_reconfig(void)
 		setenvf(&child_env, "SLURMCTLD_RECONF_PIDFD", "%d", pidfd);
 		fd_set_noclose_on_exec(pidfd);
 	}
-	if (listen_nports) {
+	if (listeners.count) {
 		char *ports = NULL, *pos = NULL;
 		setenvf(&child_env, "SLURMCTLD_RECONF_LISTEN_COUNT", "%d",
-			listen_nports);
-		for (int i = 0; i < listen_nports; i++) {
-			xstrfmtcatat(ports, &pos, "%d,", listen_fds[i].fd);
-			fd_set_noclose_on_exec(listen_fds[i].fd);
+			listeners.count);
+		for (int i = 0; i < listeners.count; i++) {
+			xstrfmtcatat(ports, &pos, "%d,", listeners.fd[i]);
+			fd_set_noclose_on_exec(listeners.fd[i]);
 		}
 		setenvf(&child_env, "SLURMCTLD_RECONF_LISTEN_FDS", "%s", ports);
 		xfree(ports);
@@ -1240,8 +1237,8 @@ start_child:
 			continue;
 		if (fd == pidfd)
 			continue;
-		for (int i = 0; i < listen_nports; i++) {
-			if (fd == listen_fds[i].fd) {
+		for (int i = 0; i < listeners.count; i++) {
+			if (fd == listeners.fd[i]) {
 				match = true;
 				break;
 			}
@@ -1332,121 +1329,120 @@ extern void queue_job_scheduler(void)
 	slurm_mutex_unlock(&sched_cnt_mutex);
 }
 
+static void *_on_listen_connect(conmgr_fd_t *con, void *arg)
+{
+	debug3("%s: [%s] Successfully opened RPC listener",
+	       __func__, conmgr_fd_get_name(con));
+
+	return con;
+}
+
+static void _on_listen_finish(conmgr_fd_t *con, void *arg)
+{
+	xassert(con == arg);
+
+	debug3("%s: [%s] Closed RPC listener",
+	       __func__, conmgr_fd_get_name(con));
+}
+
+static void *_on_primary_connection(conmgr_fd_t *con, void *arg)
+{
+	int rc;
+
+	debug3("%s: [%s] PRIMARY: New RPC connection",
+	       __func__, conmgr_fd_get_name(con));
+
+	if (slurmctld_primary)
+
+	if ((rc = conmgr_queue_extract_con_fd(con, _service_connection,
+					      XSTRINGIFY(_service_connection),
+					      NULL))) {
+		error("%s: [%s] Extracting FDs failed: %s",
+		      __func__, conmgr_fd_get_name(con), slurm_strerror(rc));
+		return NULL;
+	}
+
+	return con;
+}
+
+static void _on_primary_finish(conmgr_fd_t *con, void *arg)
+{
+	xassert(arg == con);
+
+	debug3("%s: [%s] PRIMARY: RPC connection closed",
+	       __func__, conmgr_fd_get_name(con));
+}
+
+static int _on_primary_msg(conmgr_fd_t *con, slurm_msg_t *msg, void *arg)
+{
+	fatal_abort("should never happen");
+}
+
+static void *_on_connection(conmgr_fd_t *con, void *arg)
+{
+	if (slurmctld_primary)
+		return _on_primary_connection(con, arg);
+	else
+		return on_backup_connection(con, arg);
+}
+
+static void _on_finish(conmgr_fd_t *con, void *arg)
+{
+	if (slurmctld_primary)
+		return _on_primary_finish(con, arg);
+	else
+		return on_backup_finish(con, arg);
+}
+
+static int _on_msg(conmgr_fd_t *con, slurm_msg_t *msg, void *arg)
+{
+	if (slurmctld_primary)
+		return _on_primary_msg(con, msg, arg);
+	else
+		return on_backup_msg(con, msg, arg);
+}
+
 /*
  * _open_ports - Open all ports for the slurmctld to listen on.
  */
 static void _open_ports(void)
 {
-	slurm_addr_t srv_addr;
+	static const conmgr_events_t events = {
+		.on_listen_connect = _on_listen_connect,
+		.on_listen_finish = _on_listen_finish,
+		.on_connection = _on_connection,
+		.on_msg = _on_msg,
+		.on_finish = _on_finish,
+	};
 
 	/* initialize ports for RPCs */
 	if (original) {
-		if (!(listen_nports = slurm_conf.slurmctld_port_count))
+		if (!(listeners.count = slurm_conf.slurmctld_port_count))
 			fatal("slurmctld port count is zero");
-		listen_fds = xcalloc(listen_nports, sizeof(struct pollfd));
-		for (int i = 0; i < listen_nports; i++) {
-			listen_fds[i].fd = slurm_init_msg_engine_port(
+		listeners.fd = xcalloc(listeners.count, sizeof(*listeners.fd));
+		for (int i = 0; i < listeners.count; i++) {
+			listeners.fd[i] = slurm_init_msg_engine_port(
 				slurm_conf.slurmctld_port + i);
-			listen_fds[i].events = POLLIN;
-			if (listen_fds[i].fd == SLURM_ERROR)
-				fatal("slurm_init_msg_engine_port: error %m");
-			if (slurm_get_stream_addr(listen_fds[i].fd,
-						  &srv_addr)) {
-				error("slurm_get_stream_addr error %m");
-			} else {
-				debug2("slurmctld listening on %pA", &srv_addr);
-			}
 		}
 	} else {
 		char *pos = getenv("SLURMCTLD_RECONF_LISTEN_FDS");
-		listen_nports = atoi(getenv("SLURMCTLD_RECONF_LISTEN_COUNT"));
-		listen_fds = xcalloc(listen_nports, sizeof(struct pollfd));
-		for (int i = 0; i < listen_nports; i++) {
-			listen_fds[i].fd = strtol(pos, &pos, 10);
-			listen_fds[i].events = POLLIN;
+		listeners.count = atoi(getenv("SLURMCTLD_RECONF_LISTEN_COUNT"));
+		listeners.fd = xcalloc(listeners.count, sizeof(*listeners.fd));
+		for (int i = 0; i < listeners.count; i++) {
+			listeners.fd[i] = strtol(pos, &pos, 10);
 			pos++; /* skip comma */
 		}
 	}
-}
 
-static void _close_ports(void)
-{
-	for (int i = 0; i < listen_nports; i++)
-		close(listen_fds[i].fd);
-	xfree(listen_fds);
-}
+	for (int i = 0; i < listeners.count; i++) {
+		int rc;
 
-
-/*
- * _slurmctld_rpc_mgr - Read incoming RPCs and create pthread for each
- */
-static void *_slurmctld_rpc_mgr(void *no_data)
-{
-	int *newsockfd;
-	slurm_addr_t cli_addr;
-	int fd_next = 0, i;
-	/* Locks: Read config */
-
-#if HAVE_SYS_PRCTL_H
-	if (prctl(PR_SET_NAME, "rpcmgr", NULL, NULL, NULL) < 0) {
-		error("%s: cannot set my name to %s %m", __func__, "rpcmgr");
+		if ((rc = conmgr_process_fd_listen(listeners.fd[i],
+						   CON_TYPE_RPC, events, NULL,
+						   0, NULL)))
+			fatal("%s: unable to process fd:%d error:%s",
+			      __func__, listeners.fd[i], slurm_strerror(rc));
 	}
-#endif
-
-	debug3("%s pid = %u", __func__, getpid());
-
-	rate_limit_init();
-	rpc_queue_init();
-
-	/*
-	 * Process incoming RPCs until told to shutdown
-	 */
-	while (_wait_for_server_thread()) {
-		if (poll(listen_fds, listen_nports, -1) == -1) {
-			if (errno != EINTR)
-				error("slurm_accept_msg_conn poll: %m");
-			server_thread_decr();
-			continue;
-		}
-
-		/* find one to process */
-		for (i = 0; i < listen_nports; i++) {
-			if (listen_fds[(fd_next + i) % listen_nports].revents) {
-				i = (fd_next + i) % listen_nports;
-				break;
-			}
-		}
-		fd_next = (i + 1) % listen_nports;
-
-		newsockfd = xmalloc(sizeof(*newsockfd));
-		if ((*newsockfd = slurm_accept_msg_conn(listen_fds[i].fd,
-							&cli_addr))
-		    == SLURM_ERROR) {
-			if (errno != EINTR)
-				error("slurm_accept_msg_conn: %m");
-			server_thread_decr();
-			xfree(newsockfd);
-			continue;
-		}
-
-		log_flag(PROTOCOL, "%s: accept() connection from %pA",
-			 __func__, &cli_addr);
-
-		if (slurmctld_config.shutdown_time) {
-			slurmctld_diag_stats.proc_req_raw++;
-			_service_connection(newsockfd);
-		} else {
-			slurm_thread_create_detached(_service_connection,
-						     newsockfd);
-		}
-	}
-
-	debug3("%s shutting down", __func__);
-
-	rate_limit_shutdown();
-	rpc_queue_shutdown();
-
-	return NULL;
 }
 
 /*
@@ -1455,17 +1451,15 @@ static void *_slurmctld_rpc_mgr(void *no_data)
  *	upon completion
  * RET - NULL
  */
-static void *_service_connection(void *arg)
+static void _service_connection(conmgr_callback_args_t conmgr_args,
+				int input_fd, int output_fd, void *arg)
 {
-	int fd = *((int *) arg);
+	int fd = input_fd;
 	slurm_msg_t *msg = xmalloc(sizeof *msg);
 	xfree(arg);
 
-#if HAVE_SYS_PRCTL_H
-	if (prctl(PR_SET_NAME, "srvcn", NULL, NULL, NULL) < 0) {
-		error("%s: cannot set my name to %s %m", __func__, "srvcn");
-	}
-#endif
+	xassert(input_fd == output_fd);
+
 	slurm_msg_t_init(msg);
 	msg->flags |= SLURM_MSG_KEEP_BUFFER;
 	/*
@@ -1501,8 +1495,7 @@ static void *_service_connection(void *arg)
 		int rc = rpc_enqueue(msg);
 
 		if (rc == SLURM_SUCCESS) {
-			server_thread_decr();
-			return NULL;
+			return;
 		} else if (rc == SLURMCTLD_COMMUNICATIONS_BACKOFF) {
 			slurm_send_rc_msg(msg, SLURMCTLD_COMMUNICATIONS_BACKOFF);
 		} else if (rc == SLURMCTLD_COMMUNICATIONS_HARD_DROP) {
@@ -1518,51 +1511,6 @@ static void *_service_connection(void *arg)
 
 cleanup:
 	slurm_free_msg(msg);
-	server_thread_decr();
-
-	return NULL;
-}
-
-/* Increment slurmctld_config.server_thread_count and don't return
- * until its value is no larger than MAX_SERVER_THREADS,
- * RET true unless shutdown in progress */
-static bool _wait_for_server_thread(void)
-{
-	bool print_it = true;
-	bool rc = true;
-
-	slurm_mutex_lock(&slurmctld_config.thread_count_lock);
-	while (1) {
-		if (slurmctld_config.shutdown_time) {
-			rc = false;
-			break;
-		}
-		if (slurmctld_config.server_thread_count < max_server_threads) {
-			slurmctld_config.server_thread_count++;
-			break;
-		}
-
-		/*
-		 * Wait for state change and retry, this is just a delay and
-		 * not an error. This can happen when the epilog completes on
-		 * a bunch of nodes at the same time, which can easily happen
-		 * for highly parallel jobs.
-		 */
-		if (print_it) {
-			static time_t last_print_time = 0;
-			time_t now = time(NULL);
-			if (difftime(now, last_print_time) > 2) {
-				verbose("server_thread_count over limit (%d), waiting",
-					slurmctld_config.server_thread_count);
-				last_print_time = now;
-			}
-			print_it = false;
-		}
-		slurm_cond_wait(&slurmctld_config.thread_count_cond,
-				&slurmctld_config.thread_count_lock);
-	}
-	slurm_mutex_unlock(&slurmctld_config.thread_count_lock);
-	return rc;
 }
 
 /* Decrement slurmctld thread count (as applies to thread limit) */
@@ -2762,13 +2710,8 @@ int slurmctld_shutdown(void)
 	sched_debug("slurmctld terminating");
 	slurmctld_config.shutdown_time = time(NULL);
 	slurm_cond_signal(&shutdown_cond);
-	if (slurmctld_config.thread_id_rpc) {
-		pthread_kill(slurmctld_config.thread_id_rpc, SIGUSR1);
-		return SLURM_SUCCESS;
-	} else {
-		error("thread_id_rpc not set");
-		return SLURM_ERROR;
-	}
+	pthread_kill(pthread_self(), SIGUSR1);
+	return SLURM_SUCCESS;
 }
 
 /*

src/slurmctld/backup.c
@@ -75,8 +75,6 @@
 #define _DEBUG		0
 #define SHUTDOWN_WAIT	2	/* Time to wait for primary server shutdown */
 
-static int          _background_process_msg(slurm_msg_t * msg);
-static void *       _background_rpc_mgr(void *no_data);
 static void         _backup_reconfig(void);
 static int          _shutdown_primary_controller(int wait_time);
 static void *       _trigger_slurmctld_event(void *arg);
@@ -148,12 +146,6 @@ void run_backup(void)
 	slurm_mutex_lock(&slurmctld_config.backup_finish_lock);
 	slurm_cond_broadcast(&slurmctld_config.backup_finish_cond);
 	slurm_mutex_unlock(&slurmctld_config.backup_finish_lock);
-
-	/*
-	 * create attached thread to process RPCs
-	 */
-	slurm_thread_create(&slurmctld_config.thread_id_rpc,
-			    _background_rpc_mgr, NULL);
 
 	slurm_thread_create_detached(_trigger_slurmctld_event, NULL);
 
@@ -280,7 +272,6 @@ void run_backup(void)
 	trigger_backup_ctld_as_ctrl();
 
 	pthread_kill(pthread_self(), SIGTERM);
-	slurm_thread_join(slurmctld_config.thread_id_rpc);
 
 	/*
 	 * Expressly shutdown the agent. The agent can in whole or in part
@@ -344,81 +335,29 @@ void run_backup(void)
 	unlock_slurmctld(config_write_lock);
 }
 
-/*
- * _background_rpc_mgr - Read and process incoming RPCs to the background
- *	controller (that's us)
- */
-static void *_background_rpc_mgr(void *no_data)
+extern void *on_backup_connection(conmgr_fd_t *con, void *arg)
 {
-	int newsockfd;
-	int fd_next = 0, i;
-	slurm_addr_t cli_addr;
-	slurm_msg_t msg;
+	debug3("%s: [%s] BACKUP: New RPC connection",
+	       __func__, conmgr_fd_get_name(con));
 
-	debug3("_background_rpc_mgr pid = %lu", (unsigned long) getpid());
-
-	/*
-	 * Process incoming RPCs indefinitely
-	 */
-	while (slurmctld_config.shutdown_time == 0) {
-		if (poll(listen_fds, listen_nports, -1) == -1) {
-			if (errno != EINTR)
-				error("slurm_accept_msg_conn poll: %m");
-			continue;
-		}
-
-		/* find one to process */
-		for (i = 0; i < listen_nports; i++) {
-			if (listen_fds[(fd_next + i) % listen_nports].revents) {
-				i = (fd_next + i) % listen_nports;
-				break;
-			}
-		}
-		fd_next = (i + 1) % listen_nports;
-
-		if ((newsockfd = slurm_accept_msg_conn(listen_fds[i].fd,
-						       &cli_addr))
-		    == SLURM_ERROR) {
-			if (errno != EINTR)
-				error("slurm_accept_msg_conn: %m");
-			continue;
-		}
-
-		log_flag(PROTOCOL, "%s: accept() connection from %pA",
-			 __func__, &cli_addr);
-
-		slurm_msg_t_init(&msg);
-		if (slurm_receive_msg(newsockfd, &msg, 0) != 0)
-			error("slurm_receive_msg: %m");
-		else {
-			if (slurm_conf.debug_flags & DEBUG_FLAG_AUDIT_RPCS) {
-				slurm_addr_t cli_addr;
-				(void) slurm_get_peer_addr(newsockfd, &cli_addr);
-				log_flag(AUDIT_RPCS, "msg_type=%s uid=%u client=[%pA] protocol=%u",
-					 rpc_num2string(msg.msg_type),
-					 msg.auth_uid, &cli_addr,
-					 msg.protocol_version);
-			}
-
-			_background_process_msg(&msg);
-		}
-
-		slurm_free_msg_members(&msg);
-
-		close(newsockfd);	/* close new socket */
-	}
-
-	debug3("_background_rpc_mgr shutting down");
-	return NULL;
+	return con;
 }
 
-/*
- * _background_process_msg - process an RPC to the backup_controller
- */
-static int _background_process_msg(slurm_msg_t *msg)
+extern void on_backup_finish(conmgr_fd_t *con, void *arg)
+{
+	xassert(arg == con);
+
+	debug3("%s: [%s] BACKUP: finish RPC connection",
+	       __func__, conmgr_fd_get_name(con));
+}
+
+/* process an RPC to the backup_controller */
+extern int on_backup_msg(conmgr_fd_t *con, slurm_msg_t *msg, void *arg)
 {
 	int error_code = SLURM_SUCCESS;
 	bool send_rc = true;
+
+	xassert(arg == con);
 
 	if (!msg->auth_ids_set) {
 		error("%s: received message without previously validated auth",
@@ -426,19 +365,9 @@ static int _background_process_msg(slurm_msg_t *msg)
 		return SLURM_ERROR;
 	}
 
-	if (slurm_conf.debug_flags & DEBUG_FLAG_PROTOCOL) {
-		const char *p = rpc_num2string(msg->msg_type);
-		if (msg->conn) {
-			info("%s: received opcode %s from persist conn on (%s)%s uid %u",
-			     __func__, p, msg->conn->cluster_name,
-			     msg->conn->rem_host, msg->auth_uid);
-		} else {
-			slurm_addr_t cli_addr;
-			(void) slurm_get_peer_addr(msg->conn_fd, &cli_addr);
-			info("%s: received opcode %s from %pA uid %u",
-			     __func__, p, &cli_addr, msg->auth_uid);
-		}
-	}
+	log_flag(PROTOCOL, "%s: [%s] Received opcode %s from uid %u",
+	     __func__, conmgr_fd_get_name(con), rpc_num2string(msg->msg_type),
+	     msg->auth_uid);
 
 	if (msg->msg_type != REQUEST_PING) {
 		bool super_user = false;

src/slurmctld/slurmctld.h
@@ -154,7 +154,6 @@ typedef struct slurmctld_config {
 	pthread_t thread_id_main;
 	pthread_t thread_id_save;
 	pthread_t thread_id_purge_files;
-	pthread_t thread_id_rpc;
 } slurmctld_config_t;
 
 typedef enum {
@@ -255,7 +254,6 @@ extern bool cloud_dns;
 extern uint32_t   cluster_cpus;
 extern bool disable_remote_singleton;
 extern int listen_nports;
-extern struct pollfd *listen_fds;
 extern int max_depend_depth;
 extern uint32_t max_powered_nodes;
 extern bool node_features_updated;
@@ -1685,6 +1683,11 @@ extern void restore_node_features(int recover);
 /* run_backup - this is the backup controller, it should run in standby
  *	mode, assuming control when the primary controller stops responding */
 extern void run_backup(void);
+
+/* conmgr RPC connection callbacks */
+extern void *on_backup_connection(conmgr_fd_t *con, void *arg);
+extern void on_backup_finish(conmgr_fd_t *con, void *arg);
+extern int on_backup_msg(conmgr_fd_t *con, slurm_msg_t *msg, void *arg);
 
 /*
  * ping_controllers - ping other controllers in HA configuration.

commit 8de6387232b413285e6e1a5893b9eae168e57d22

Restore tracking server_thread_count
Regression from commit 942aa4c7.

Ticket 20114

Signed-off-by: Nathan Rini <nate@schedmd.com>

src/slurmctld/controller.c
@@ -1485,6 +1485,7 @@ static void _service_connection(conmgr_callback_args_t conmgr_args,
 	slurm_msg_t *msg = xmalloc(sizeof *msg);
 	xfree(arg);
 
+	server_thread_incr();
 	xassert(input_fd == output_fd);
 
 	slurm_msg_t_init(msg);
@@ -1522,6 +1523,7 @@ static void _service_connection(conmgr_callback_args_t conmgr_args,
 		int rc = rpc_enqueue(msg);
 
 		if (rc == SLURM_SUCCESS) {
+			server_thread_decr();
 			return;
 		} else if (rc == SLURMCTLD_COMMUNICATIONS_BACKOFF) {
 			slurm_send_rc_msg(msg, SLURMCTLD_COMMUNICATIONS_BACKOFF);
@@ -1537,6 +1539,7 @@ static void _service_connection(conmgr_callback_args_t conmgr_args,
 		error("close(%d): %m", msg->conn_fd);
 
 cleanup:
+	server_thread_decr();
 	slurm_free_msg(msg);
 }
 