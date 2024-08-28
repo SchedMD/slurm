SchedMD/slurm

commit 0a07c53c69a4dc468545998974e7e8a15a768c01

slurmrestd - reduce time spent holding higher permissions
Swap to lower permissions, including requested user and group,
immediately after processing the command line args and environment.

Late swapping to user and group when slurmrestd was run as root would
cause the listening socket(s) to end up being owned and only usable by
root user which was counter productive to setup a system service.

Bug 15392

src/slurmrestd/slurmrestd.c
@@ -387,6 +387,10 @@ int main(int argc, char **argv)
 
 	_parse_env();
 	_parse_commandline(argc, argv);
+
+	/* attempt to release all unneeded permissions */
+	_lock_down();
+
 	_examine_stdin();
 	_examine_stderr();
 	_examine_stdout();
@@ -504,9 +508,6 @@ int main(int argc, char **argv)
 		FREE_NULL_LIST(socket_listen);
 		debug("%s: server listen mode activated", __func__);
 	}
-
-	/* attempt to release all unneeded permissions before talking to clients */
-	_lock_down();
 
 	rc = con_mgr_run(conmgr);
 

commit 019ab8f5a17d80b73b94c1a517c89164b86a9766

slurmrestd - Check for SlurmUser after slurm.conf loads
Check slurmrestd is not running as SlurmUser after load of Slurm
configuration with SlurmUser.

Regression from 0a07c53c69.

Ticket 20773

src/slurmrestd/slurmrestd.c
@@ -551,19 +551,28 @@ static void _lock_down(void)
 
 	if (become_user && getuid())
 		fatal("slurmrestd must run as root in become_user mode");
-	else if (check_user && (slurm_conf.slurm_user_id == getuid()))
-		fatal("slurmrestd should not be run as SlurmUser");
 
 	if (become_user && getgid())
 		fatal("slurmrestd must run as root in become_user mode");
-	else if (check_user &&
-		 (gid_from_uid(slurm_conf.slurm_user_id) == getgid()))
-		fatal("slurmrestd should not be run with SlurmUser's group.");
 
 #ifdef PR_SET_DUMPABLE
 	if (prctl(PR_SET_DUMPABLE, 1) < 0)
 		error("%s: Unable to set process as dumpable: %m", __func__);
 #endif
+}
+
+/*
+ * Check slurmrestd is not running as SlurmUser unless check_user is false.
+ */
+static void _check_user(void)
+{
+	if (!check_user)
+		return;
+
+	if (slurm_conf.slurm_user_id == getuid())
+		fatal("slurmrestd should not be run as SlurmUser");
+	if (gid_from_uid(slurm_conf.slurm_user_id) == getgid())
+		fatal("slurmrestd should not be run with SlurmUser's group.");
 }
 
 /* simple wrapper to hand over operations router in http context */
@@ -638,6 +647,7 @@ int main(int argc, char **argv)
 	run_mode.listen = !list_is_empty(socket_listen);
 
 	slurm_init(slurm_conf_filename);
+	_check_user();
 
 	if (thread_count < 2)
 		fatal("Request at least 2 threads for processing");