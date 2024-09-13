SchedMD/slurm

commit f1047b28722a70f331f0634afdcea11c58bf83e9

Pack NO_VAL16 instead of job_core_spec.

src/common/stepd_api.c
@@ -996,7 +996,7 @@ stepd_suspend(int fd, uint16_t protocol_version,
 
 	if (phase == 0) {
 		safe_write(fd, &req, sizeof(int));
-		safe_write(fd, &susp_req->job_core_spec, sizeof(uint16_t));
+		safe_write(fd, NO_VAL16, sizeof(uint16_t));
 	} else {
 		/* Receive the return code and errno */
 		safe_read(fd, &rc, sizeof(int));
@@ -1027,7 +1027,7 @@ stepd_resume(int fd, uint16_t protocol_version,
 
 	if (phase == 0) {
 		safe_write(fd, &req, sizeof(int));
-		safe_write(fd, &susp_req->job_core_spec, sizeof(uint16_t));
+		safe_write(fd, NO_VAL16, sizeof(uint16_t));
 	} else {
 		/* Receive the return code and errno */
 		safe_read(fd, &rc, sizeof(int));

commit 78e674911830dfbd8a3f50ff775ea784f012a88f

Fix suspend/resume operation with 23.02 slurmstepd.
This fixes a regression from f1047b2872 in the 23.11 release.

Ticket 20610

src/common/stepd_api.c
@@ -1018,8 +1018,9 @@ stepd_suspend(int fd, uint16_t protocol_version,
 		}
 	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		if (phase == 0) {
+			uint16_t tmp = NO_VAL16;
 			safe_write(fd, &req, sizeof(int));
-			safe_write(fd, NO_VAL16, sizeof(uint16_t));
+			safe_write(fd, &tmp, sizeof(uint16_t));
 		} else {
 			/* Receive the return code and errno */
 			safe_read(fd, &rc, sizeof(int));
@@ -1060,8 +1061,9 @@ stepd_resume(int fd, uint16_t protocol_version,
 		}
 	} else if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		if (phase == 0) {
+			uint16_t tmp = NO_VAL16;
 			safe_write(fd, &req, sizeof(int));
-			safe_write(fd, NO_VAL16, sizeof(uint16_t));
+			safe_write(fd, &tmp, sizeof(uint16_t));
 		} else {
 			/* Receive the return code and errno */
 			safe_read(fd, &rc, sizeof(int));