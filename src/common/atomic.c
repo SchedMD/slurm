847a5de4de2ce1ba1f217b7fda1fd8e1c7e01b9d
Nathan Rini <nate@schedmd.com>
2024-09-17 18:04:16 -0600

    atomic - Add atomic_int32_t, atomic_bool_t, atomic_pthread_t
    Ticket 20114

--- src/common/atomic.c
+++ src/common/atomic.c
@@ -34,3 +34,184 @@
 \*****************************************************************************/
 
 #include "src/common/atomic.h"
+#include "src/common/log.h"
+
+#ifdef __STDC_NO_ATOMICS__
+
+#include "src/common/macros.h"
+
+extern bool atomic_bool_ptr_set_true(atomic_bool_t *target)
+{
+	bool value;
+
+	slurm_mutex_lock(&target->mutex);
+
+	value = target->value;
+	target->value = true;
+
+	slurm_mutex_unlock(&target->mutex);
+
+	return value;
+}
+
+extern bool atomic_bool_ptr_set_true_from_false(atomic_bool_t *target)
+{
+	bool changed = false;
+
+	slurm_mutex_lock(&target->mutex);
+
+	if (!target->value) {
+		target->value = true;
+		changed = true;
+	}
+
+	slurm_mutex_unlock(&target->mutex);
+
+	return changed;
+}
+
+extern bool atomic_bool_ptr_set_false_from_true(atomic_bool_t *target)
+{
+	bool changed = false;
+
+	slurm_mutex_lock(&target->mutex);
+
+	if (target->value) {
+		target->value = false;
+		changed = true;
+	}
+
+	slurm_mutex_unlock(&target->mutex);
+
+	return changed;
+}
+
+extern bool atomic_bool_ptr_get(atomic_bool_t *target)
+{
+	bool value;
+
+	slurm_mutex_lock(&target->mutex);
+
+	value = target->value;
+
+	slurm_mutex_unlock(&target->mutex);
+
+	return value;
+}
+
+extern int32_t atomic_int32_ptr_add(atomic_int32_t *target, int32_t value)
+{
+	int32_t prior;
+
+	slurm_mutex_lock(&target->mutex);
+
+	prior = target->value;
+	target->value += value;
+
+	slurm_mutex_unlock(&target->mutex);
+
+	return prior;
+}
+
+extern void atomic_int32_ptr_increment(atomic_int32_t *target)
+{
+	slurm_mutex_lock(&target->mutex);
+
+	target->value++;
+
+	slurm_mutex_unlock(&target->mutex);
+}
+
+extern void atomic_int32_ptr_decrement(atomic_int32_t *target)
+{
+	slurm_mutex_lock(&target->mutex);
+
+	target->value--;
+
+	slurm_mutex_unlock(&target->mutex);
+}
+
+extern int32_t atomic_int32_ptr_get(atomic_int32_t *target)
+{
+	int32_t value;
+
+	slurm_mutex_lock(&target->mutex);
+
+	value = target->value;
+
+	slurm_mutex_unlock(&target->mutex);
+
+	return value;
+}
+
+#else /* !__STDC_NO_ATOMICS__ */
+
+#include <stdatomic.h>
+
+extern bool atomic_bool_ptr_set_true(atomic_bool_t *target)
+{
+	return atomic_exchange(&target->value, true);
+}
+
+extern bool atomic_bool_ptr_set_false(atomic_bool_t *target)
+{
+	return atomic_exchange(&target->value, false);
+}
+
+extern bool atomic_bool_ptr_set_true_from_false(atomic_bool_t *target)
+{
+	bool expected = false;
+
+	if (atomic_compare_exchange_strong(&target->value, &expected, true))
+		return true;
+
+	if (expected == true)
+		return false;
+
+	fatal_abort("should never happen: expected=%c", BOOL_CHARIFY(expected));
+}
+
+extern bool atomic_bool_ptr_set_false_from_true(atomic_bool_t *target)
+{
+	bool expected = true;
+
+	if (atomic_compare_exchange_strong(&target->value, &expected, false))
+		return true;
+
+	if (expected == false)
+		return false;
+
+	fatal_abort("should never happen: expected=%c", BOOL_CHARIFY(expected));
+}
+
+extern bool atomic_bool_ptr_get(atomic_bool_t *target)
+{
+	return atomic_load(&target->value);
+}
+
+extern int32_t atomic_int32_ptr_add(atomic_int32_t *target, int32_t value)
+{
+	return atomic_fetch_add(&target->value, value);
+}
+
+extern void atomic_int32_ptr_increment(atomic_int32_t *target)
+{
+	(void) atomic_fetch_add(&target->value, 1);
+}
+
+extern void atomic_int32_ptr_decrement(atomic_int32_t *target)
+{
+	(void) atomic_fetch_sub(&target->value, 1);
+}
+
+extern int32_t atomic_int32_ptr_get(atomic_int32_t *target)
+{
+	return atomic_load(&target->value);
+}
+
+extern int32_t atomic_int32_ptr_set(atomic_int32_t *target, int32_t value)
+{
+	return atomic_exchange(&target->value, value);
+}
+
+#endif /* !__STDC_NO_ATOMICS__ */
--- src/common/atomic.h
+++ src/common/atomic.h
@@ -36,4 +36,155 @@
 #ifndef _SLURM_ATOMICS_H
 #define _SLURM_ATOMICS_H
 
+/*
+ * Need at least C11 for atomics and guarentee __STDC_NO_ATOMICS__ is always
+ * defined if not even if not by a non-compliant complier.
+ */
+#if (!defined(__STDC_VERSION__) || (__STDC_VERSION__ <= 201112L)) && \
+	!defined(__STDC_NO_ATOMICS__)
+#define __STDC_NO_ATOMICS__
+#endif
+
+#ifndef __STDC_NO_ATOMICS__
+#include <stdatomic.h>
+#endif /* __STDC_NO_ATOMICS__ */
+
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifndef __STDC_NO_ATOMICS__
+
+/* Always use helper functions to access and ATOMIC_INT32_INITIALIZER() */
+typedef struct {
+	_Atomic int32_t value;
+} atomic_int32_t;
+
+#define ATOMIC_INT32_INITIALIZER(init) \
+	((atomic_int32_t) { \
+		.value = init, \
+	})
+
+/* Always helper functions to access and ATOMIC_BOOL_INITIALIZER() */
+typedef struct {
+	_Atomic bool value;
+} atomic_bool_t;
+
+#define ATOMIC_BOOL_INITIALIZER(init) \
+	((atomic_bool_t) { \
+		.value = init, \
+	})
+
+/* Always init with ATOMIC_PTHREAD_INITIALIZER () */
+typedef _Atomic pthread_t atomic_pthread_t;
+
+#define ATOMIC_PTHREAD_INITIALIZER(init) ((atomic_pthread_t) init)
+
+#else /* __STDC_NO_ATOMICS__ */
+
+typedef struct {
+	pthread_mutex_t mutex;
+	int32_t value;
+} atomic_int32_t;
+
+#define ATOMIC_INT32_INITIALIZER(init) \
+	((atomic_int32_t) { \
+		.mutex = PTHREAD_MUTEX_INITIALIZER, \
+		.value = init, \
+	})
+
+typedef struct {
+	pthread_mutex_t mutex;
+	bool value;
+} atomic_bool_t;
+
+#define ATOMIC_BOOL_INITIALIZER(init) \
+	((atomic_bool_t) { \
+		.mutex = PTHREAD_MUTEX_INITIALIZER, \
+		.value = init, \
+	})
+
+#endif /* __STDC_NO_ATOMICS__ */
+
+/* Use atomic_bool_set_true() instead */
+extern bool atomic_bool_ptr_set_true(atomic_bool_t *target);
+
+/*
+ * Set target bool to true
+ * RET prior value of target
+ */
+#define atomic_bool_set_true(target) atomic_bool_ptr_set_true(&target)
+
+/* Use atomic_bool_set_false() instead */
+extern bool atomic_bool_ptr_set_false(atomic_bool_t *target);
+
+/*
+ * Set target bool to false
+ * RET prior value of target
+ */
+#define atomic_bool_set_false(target) atomic_bool_ptr_set_false(&target)
+
+/* Use atomic_bool_set_true_from_false() instead */
+extern bool atomic_bool_ptr_set_true_from_false(atomic_bool_t *target);
+
+/*
+ * Set target bool to true if target was false
+ * RET
+ *	true: if target was false and changed to true
+ *	false: if target was true (and not changed)
+ */
+#define atomic_bool_set_true_from_false(target) \
+	atomic_bool_ptr_set_true_from_false(&target)
+
+/* Use atomic_bool_set_false_from_true() instead */
+extern bool atomic_bool_ptr_set_false_from_true(atomic_bool_t *target);
+
+/*
+ * Set target bool to false if target was true
+ * RET
+ *	true: if target was true and changed to false
+ *	false: if target was false (and not changed)
+ */
+#define atomic_bool_set_false_from_true(target) \
+	atomic_bool_ptr_set_false_from_true(&target)
+
+/* Use atomic_bool_get() instead */
+extern bool atomic_bool_ptr_get(atomic_bool_t *target);
+
+/* Get value of target */
+#define atomic_bool_get(target) atomic_bool_ptr_get(&target)
+
+/* Use atomic_int32_add() instead */
+extern int32_t atomic_int32_ptr_add(atomic_int32_t *target, int32_t value);
+
+/* Increment target by value and return prior value */
+#define atomic_int32_add(target, value) atomic_int32_ptr_add(&target, value)
+
+/* Use atomic_int32_increment() instead */
+extern void atomic_int32_ptr_increment(atomic_int32_t *target);
+
+/* Increment target by 1 */
+#define atomic_int32_increment(target) atomic_int32_ptr_increment(&target)
+
+/* Use atomic_int32_decrement() instead */
+extern void atomic_int32_ptr_decrement(atomic_int32_t *target);
+
+/* Decrement target by 1 */
+#define atomic_int32_decrement(target) atomic_int32_ptr_decrement(&target)
+
+/* use atomic_int32_get() instead */
+extern int32_t atomic_int32_ptr_get(atomic_int32_t *target);
+
+/* get value of target */
+#define atomic_int32_get(target) atomic_int32_ptr_get(&target)
+
+/* use atomic_int32_set() instead */
+extern int32_t atomic_int32_ptr_set(atomic_int32_t *target, int32_t value);
+
+/* Set target to value and return value */
+#define atomic_int32_set(target, value) atomic_int32_ptr_set(&target, value)
+
+/* Set value of target to zero and return value */
+#define atomic_int32_set_zero(target) atomic_int32_ptr_set(&target, 0)
+
 #endif /* _SLURM_ATOMICS_H */
