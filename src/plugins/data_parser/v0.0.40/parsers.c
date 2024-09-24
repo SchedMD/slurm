6082ccf761522e1fb4597a06b2294e664e6038fc
Nathan Rini <nate@schedmd.com>
2024-09-24 09:43:03 -0600

    data_parser/v0.0.40 - Catch NaN and infinite in DUMP_FUNC(FLOAT64_NO_VAL)()
    Ticket 20817

--- src/plugins/data_parser/v0.0.40/parsers.c
+++ src/plugins/data_parser/v0.0.40/parsers.c
@@ -2086,11 +2086,11 @@ static int DUMP_FUNC(FLOAT64_NO_VAL)(const parser_t *const parser, void *obj,
 	inf = data_key_set(dst, "infinite");
 	num = data_key_set(dst, "number");
 
-	if ((uint32_t) *src == INFINITE) {
+	if ((((uint32_t) *src) == INFINITE) || isinf(*src)) {
 		data_set_bool(set, false);
 		data_set_bool(inf, true);
 		data_set_float(num, 0);
-	} else if ((uint32_t) *src == NO_VAL) {
+	} else if ((((uint32_t) *src) == NO_VAL) || isnan(*src)) {
 		data_set_bool(set, false);
 		data_set_bool(inf, false);
 		data_set_float(num, 0);
