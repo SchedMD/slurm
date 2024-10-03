SchedMD/slurm

commit b2eb504bbb3c82382d9e957173c8b3be89277932

Fix case where the primary and backup dbds would both be performing rollup.
If the backup dbd happened to be doing rollup at the time the primary resumed
both the primary and the backup would be doing rollups and causing contention on
the database tables. The backup would wait for the rollup handler to finish
before giving up control.

The fix is to cancel the rollup_handler and let the backup begin to shutdown so
that it will close an existing connections and then re-exec itself. The re-exec
helps because the rollup handler spawns a thread for each cluster to rollup and
just cancelling the rollup handler doesn't cancel the spawned threads from the
rollup handler. This cleans up the dbd and locks. The re-exec only happens in
the backup if the primary resumed and a rollup was happening.

Bug 1988

src/slurmdbd/slurmdbd.c
@@ -93,6 +93,7 @@ static pthread_t commit_handler_thread;	/* thread ID for commit hander */
 static pthread_mutex_t rollup_lock = PTHREAD_MUTEX_INITIALIZER;
 static bool running_rollup = 0;
 static bool running_commit = 0;
+static bool restart_backup = false;
 
 /* Local functions */
 static void  _become_slurm_user(void);
@@ -113,6 +114,7 @@ static void *_signal_handler(void *no_data);
 static void  _update_logging(bool startup);
 static void  _update_nice(void);
 static void  _usage(char *prog_name);
+static void  _restart_self(int argc, char **argv);
 
 /* main - slurmctld main function, start various threads and process RPCs */
 int main(int argc, char *argv[])
@@ -265,7 +267,7 @@ int main(int argc, char *argv[])
 		if (rpc_handler_thread)
 			pthread_join(rpc_handler_thread, NULL);
 
-		if (backup && primary_resumed) {
+		if (backup && primary_resumed && !restart_backup) {
 			shutdown_time = 0;
 			info("Backup has given up control");
 		}
@@ -277,7 +279,7 @@ int main(int argc, char *argv[])
 
 end_it:
 
-	if (signal_handler_thread)
+	if (signal_handler_thread && (!backup || !restart_backup))
 		pthread_join(signal_handler_thread, NULL);
 	if (commit_handler_thread)
 		pthread_join(commit_handler_thread, NULL);
@@ -292,6 +294,13 @@ end_it:
 	}
 
 	FREE_NULL_LIST(registered_clusters);
+
+	if (backup && restart_backup) {
+		info("Primary has come back but backup is "
+		     "running the rollup. To avoid contention, "
+		     "the backup dbd will now restart.");
+		_restart_self(argc, argv);
+	}
 
 	assoc_mgr_fini(NULL);
 	slurm_acct_storage_fini();
@@ -563,12 +572,23 @@ static void _request_registrations(void *db_conn)
 
 static void _rollup_handler_cancel()
 {
-	if (running_rollup)
-		debug("Waiting for rollup thread to finish.");
-	slurm_mutex_lock(&rollup_lock);
-	if (rollup_handler_thread)
-		pthread_cancel(rollup_handler_thread);
-	slurm_mutex_unlock(&rollup_lock);
+	if (running_rollup) {
+		if (backup && running_rollup && primary_resumed)
+			debug("Hard cancelling rollup thread");
+		else
+			debug("Waiting for rollup thread to finish.");
+	}
+
+	if (rollup_handler_thread) {
+		if (backup && running_rollup && primary_resumed) {
+			pthread_cancel(rollup_handler_thread);
+			restart_backup = true;
+		} else {
+			slurm_mutex_lock(&rollup_lock);
+			pthread_cancel(rollup_handler_thread);
+			slurm_mutex_unlock(&rollup_lock);
+		}
+	}
 }
 
 /* _rollup_handler - Process rollup duties */
@@ -809,4 +829,11 @@ static void _become_slurm_user(void)
 		fatal("Can not set uid to SlurmUser(%u): %m",
 		      slurmdbd_conf->slurm_user_id);
 	}
+}
+
+extern void _restart_self(int argc, char **argv)
+{
+	info("Restarting self");
+	if (execvp(argv[0], argv))
+		fatal("failed to restart the dbd: %m");
 }

commit fc5b02ead0dcc3110c264966c4e9a3f210471d5c

Fix race in slurmdbd while issuing several SIGINTs
Logic introduced in b2eb504bbb3c82382d9e957 fixed the case where primary and
backup dbds would both be performing rollup. After this logic, when several
interrupts were sent to the dbd, several threads were created that called
_rollup_handler_cancel. There was a segfault fixed in c6d41c1086 by this fact,
but this fix uncovered the possible race condition when several threads read
running_rollup env var.

This commit fixes this race.

Continuation of c6d41c1086.
CID 423371.

Ticket 21050.

src/slurmdbd/slurmdbd.c
@@ -787,6 +787,7 @@ static void _request_registrations(void *db_conn)
 
 static void _rollup_handler_cancel(void)
 {
+	slurm_mutex_lock(&rollup_lock);
 	if (running_rollup) {
 		if (backup && running_rollup && primary_resumed)
 			debug("Hard cancelling rollup thread");
@@ -794,7 +795,6 @@ static void _rollup_handler_cancel(void)
 			debug("Waiting for rollup thread to finish.");
 	}
 
-	slurm_mutex_lock(&rollup_lock);
 	if (rollup_handler_thread) {
 		if (backup && running_rollup && primary_resumed) {
 			pthread_cancel(rollup_handler_thread);