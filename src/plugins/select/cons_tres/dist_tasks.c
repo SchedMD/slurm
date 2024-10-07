SchedMD/slurm

commit b5a07f7cb1ed61500381770a2fc4294307bd8b16

Remove the ability for dist_tasks to allocate more tasks than allowed
Return error code instead

Ticket 20244

src/plugins/select/cons_tres/dist_tasks.c
@@ -190,9 +190,8 @@ static int _set_task_dist_internal(job_record_t *job_ptr)
 	uint32_t n, i, tid = 0, maxtasks;
 	uint16_t *avail_cpus;
 	job_resources_t *job_res = job_ptr->job_resrcs;
-	bool log_over_subscribe = true;
 	char *err_msg = NULL;
-	int plane_size = 1;
+	int rc = SLURM_SUCCESS, plane_size = 1;
 
 	if (!job_res)
 		err_msg = "job_res is NULL";
@@ -278,30 +277,11 @@ static int _set_task_dist_internal(job_record_t *job_ptr)
 			break;
 	}
 
-	/* If more tasks than resources, distribute them evenly */
-	if (!job_ptr->details->overcommit)
-		log_over_subscribe = true;
-	while (maxtasks > tid) {
-		if (log_over_subscribe) {
-			/*
-			 * 'over_subscribe' is a relief valve that guards
-			 * against an infinite loop, and it *should* never
-			 * come into play because maxtasks should never be
-			 * greater than the total number of available CPUs
-			 */
-			error("oversubscribe for %pJ",
-			      job_ptr);
-			log_over_subscribe = false;
-		}
-		for (n = 0; n < job_res->nhosts; n++) {
-			i = MIN(plane_size, maxtasks - tid);
-			job_res->tasks_per_node[n] += i;
-			tid += i;
-		}
-	}
+	if (maxtasks > tid)
+		rc = ESLURM_BAD_TASK_COUNT;
 	xfree(avail_cpus);
 
-	return SLURM_SUCCESS;
+	return rc;
 }
 
 static int _set_task_dist(job_record_t *job_ptr, const uint16_t cr_type)
@@ -343,12 +323,11 @@ static int _set_task_dist(job_record_t *job_ptr, const uint16_t cr_type)
 static int _compute_plane_dist(job_record_t *job_ptr,
 			       uint32_t *gres_task_limit)
 {
-	bool over_subscribe = false;
 	uint32_t n, i, p, tid, maxtasks, l;
 	uint16_t *avail_cpus, plane_size = 1;
 	job_resources_t *job_res = job_ptr->job_resrcs;
-	bool log_over_subscribe = true;
 	bool test_tres_tasks = true;
+	int rc = SLURM_SUCCESS;
 
 	if (!job_res || !job_res->cpus || !job_res->nhosts) {
 		error("invalid allocation for %pJ",
@@ -371,21 +350,8 @@ static int _compute_plane_dist(job_record_t *job_ptr,
 
 	job_res->cpus = xcalloc(job_res->nhosts, sizeof(uint16_t));
 	job_res->tasks_per_node = xcalloc(job_res->nhosts, sizeof(uint16_t));
-	if (job_ptr->details->overcommit)
-		log_over_subscribe = false;
 	for (tid = 0, i = 0; (tid < maxtasks); i++) { /* cycle counter */
 		bool space_remaining = false;
-		if (over_subscribe && log_over_subscribe) {
-			/*
-			 * 'over_subscribe' is a relief valve that guards
-			 * against an infinite loop, and it *should* never
-			 * come into play because maxtasks should never be
-			 * greater than the total number of available CPUs
-			 */
-			error("oversubscribe for %pJ",
-			      job_ptr);
-			log_over_subscribe = false;	/* Log once per job */;
-		}
 		for (n = 0; ((n < job_res->nhosts) && (tid < maxtasks)); n++) {
 			bool more_tres_tasks = false;
 			for (p = 0; p < plane_size && (tid < maxtasks); p++) {
@@ -394,8 +360,7 @@ static int _compute_plane_dist(job_record_t *job_ptr,
 					    gres_task_limit, job_res, n))
 					continue;
 				more_tres_tasks = true;
-				if ((job_res->cpus[n] < avail_cpus[n]) ||
-				    over_subscribe) {
+				if (job_res->cpus[n] < avail_cpus[n]) {
 					tid++;
 					job_res->tasks_per_node[n]++;
 					for (l = 0;
@@ -412,11 +377,13 @@ static int _compute_plane_dist(job_record_t *job_ptr,
 			if (job_res->cpus[n] < avail_cpus[n])
 				space_remaining = true;
 		}
-		if (!space_remaining)
-			over_subscribe = true;
+		if (!space_remaining && (tid < maxtasks)) {
+			rc = ESLURM_BAD_TASK_COUNT;
+			break;
+		}
 	}
 	xfree(avail_cpus);
-	return SLURM_SUCCESS;
+	return rc;
 }
 
 /*
@@ -1174,15 +1141,12 @@ static int _at_tpn_limit(const uint32_t n, const job_record_t *job_ptr,
 static int _dist_tasks_compute_c_b(job_record_t *job_ptr,
 				   uint32_t *gres_task_limit)
 {
-	bool over_subscribe = false;
 	uint32_t n, tid, t, maxtasks, l;
 	uint16_t *avail_cpus;
 	job_resources_t *job_res = job_ptr->job_resrcs;
-	bool log_over_subscribe = true;
 	char *err_msg = NULL;
 	uint16_t *vpus;
-	bool space_remaining;
-	int rem_cpus, rem_tasks;
+	int rc = SLURM_SUCCESS, rem_cpus, rem_tasks;
 	uint16_t cpus_per_task;
 	node_record_t *node_ptr;
 
@@ -1233,10 +1197,7 @@ static int _dist_tasks_compute_c_b(job_record_t *job_ptr,
 		      job_ptr);
 		maxtasks = 1;
 	}
-	if (job_ptr->details->overcommit)
-		log_over_subscribe = false;
 	/* Start by allocating one task per node */
-	space_remaining = false;
 	tid = 0;
 	for (n = 0; ((n < job_res->nhosts) && (tid < maxtasks)); n++) {
 		if (avail_cpus[n]) {
@@ -1247,12 +1208,8 @@ static int _dist_tasks_compute_c_b(job_record_t *job_ptr,
 				if (job_res->cpus[n] < avail_cpus[n])
 					job_res->cpus[n]++;
 			}
-			if (job_res->cpus[n] < avail_cpus[n])
-				space_remaining = true;
 		}
 	}
-	if (!space_remaining)
-		over_subscribe = true;
 
 	/* Next fill out the CPUs on the cores already allocated to this job */
 	for (n = 0; ((n < job_res->nhosts) && (tid < maxtasks)); n++) {
@@ -1292,37 +1249,20 @@ static int _dist_tasks_compute_c_b(job_record_t *job_ptr,
 		maxtasks = 0;	/* Allocate have one_task_per_node */
 	while (tid < maxtasks) {
 		bool space_remaining = false;
-		int over_limit = -1;
-		if (over_subscribe && log_over_subscribe && (over_limit > 0)) {
-			/*
-			 * 'over_subscribe' is a relief valve that guards
-			 * against an infinite loop, and it *should* never
-			 * come into play because maxtasks should never be
-			 * greater than the total number of available CPUs
-			 */
-			error("oversubscribe for %pJ",
-			      job_ptr);
-			log_over_subscribe = false;	/* Log once per job */;
-		}
 		for (n = 0; ((n < job_res->nhosts) && (tid < maxtasks)); n++) {
 			rem_tasks = vpus[n] / cpus_per_task;
 			rem_tasks = MAX(rem_tasks, 1);
 			for (t = 0; ((t < rem_tasks) && (tid < maxtasks)); t++){
-				if (!over_subscribe) {
-					if ((avail_cpus[n] - job_res->cpus[n]) <
-					    cpus_per_task)
-						break;
-					if (!dist_tasks_tres_tasks_avail(
-						    gres_task_limit,
-						    job_res, n))
-						break;
-					over_limit = _at_tpn_limit(
-						n, job_ptr,
-						"fill additional",
-						false);
-					if (over_limit >= 0)
-						break;
-				}
+				if ((avail_cpus[n] - job_res->cpus[n]) <
+					cpus_per_task)
+					break;
+				if (!dist_tasks_tres_tasks_avail(
+						gres_task_limit,
+						job_res, n))
+					break;
+				if (_at_tpn_limit(n, job_ptr, "fill allocated",
+						  false) >= 0)
+					break;
 
 				tid++;
 				job_res->tasks_per_node[n]++;
@@ -1336,13 +1276,15 @@ static int _dist_tasks_compute_c_b(job_record_t *job_ptr,
 					space_remaining = true;
 			}
 		}
-		if (!space_remaining)
-			over_subscribe = true;
+		if (!space_remaining && (tid < maxtasks)) {
+			rc = ESLURM_BAD_TASK_COUNT;
+			break;
+		}
 	}
 	xfree(avail_cpus);
 	xfree(vpus);
 
-	return SLURM_SUCCESS;
+	return rc;
 }
 
 /*

src/plugins/select/cons_tres/job_test.c
@@ -1899,7 +1899,7 @@ alloc_job:
 	job_ptr->job_resrcs = job_res;
 	if (job_ptr->gres_list_req && (error_code == SLURM_SUCCESS)) {
 		bool have_gres_per_task, task_limit_set = false;
-
+		int task_cnt = 0;
 		/*
 		 * Determine if any job gres_per_task specification here
 		 * to avoid calling gres_get_task_limit unless needed
@@ -1930,13 +1930,20 @@ alloc_job:
 			node_gres_list[j] = node_ptr->gres_list;
 			sock_gres_list[j] =
 				avail_res_array[i]->sock_gres_list;
-			if (task_limit_set)
+			if (task_limit_set) {
 				log_flag(SELECT_TYPE, "%pJ: Node=%s: gres_task_limit[%d]=%u",
 					 job_ptr,
 					 node_ptr->name,
 					 i, gres_task_limit[j]);
+				task_cnt += gres_task_limit[j];
+			}
 			j++;
 		}
+		/* Ensure gres_task_limit doesn't limit total cpu cnt */
+		job_ptr->details->cpus_per_task = MAX(
+			job_ptr->details->cpus_per_task,
+			job_res->ncpus / task_cnt);
+
 		if (!task_limit_set)
 			xfree(gres_task_limit);
 	}

commit 7fa4c6b36c00206467f03950b0f7b5ff2128460c

Allow extra tasks to be allocated beyond gres-per-node
If gres_task_limit is not associated with gres_per_task, it is a soft limit.

This fixes a regression from b5a07f7cb1

Ticket 20835

src/plugins/select/cons_tres/dist_tasks.c
@@ -34,6 +34,7 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
+#include "gres_select_util.h"
 #include "select_cons_tres.h"
 #include "dist_tasks.h"
 
@@ -378,8 +379,20 @@ static int _compute_plane_dist(job_record_t *job_ptr,
 				space_remaining = true;
 		}
 		if (!space_remaining && (tid < maxtasks)) {
-			rc = ESLURM_BAD_TASK_COUNT;
-			break;
+			/*
+			 * If gres_task_limit is not associated with
+			 * gres_per_task, it is a soft limit.
+			 */
+			if (gres_task_limit &&
+			    !gres_select_util_job_tres_per_task(
+				    job_ptr->gres_list_req)) {
+				/* Try again without limit */
+				gres_task_limit = NULL;
+				info("Function:%s LineNumber:%d", __func__, __LINE__);
+			} else {
+				rc = ESLURM_BAD_TASK_COUNT;
+				break;
+			}
 		}
 	}
 	xfree(avail_cpus);
@@ -1277,8 +1290,19 @@ static int _dist_tasks_compute_c_b(job_record_t *job_ptr,
 			}
 		}
 		if (!space_remaining && (tid < maxtasks)) {
-			rc = ESLURM_BAD_TASK_COUNT;
-			break;
+			/*
+			 * If gres_task_limit is not associated with
+			 * gres_per_task, it is a soft limit.
+			 */
+			if (gres_task_limit &&
+			    !gres_select_util_job_tres_per_task(
+				    job_ptr->gres_list_req)) {
+				/* Try again without limit */
+				gres_task_limit = NULL;
+			} else {
+				rc = ESLURM_BAD_TASK_COUNT;
+				break;
+			}
 		}
 	}
 	xfree(avail_cpus);

src/plugins/select/cons_tres/job_test.c
@@ -1906,7 +1906,6 @@ alloc_job:
 	job_ptr->job_resrcs = job_res;
 	if (job_ptr->gres_list_req && (error_code == SLURM_SUCCESS)) {
 		bool have_gres_per_task, task_limit_set = false;
-		int task_cnt = 0;
 		/*
 		 * Determine if any job gres_per_task specification here
 		 * to avoid calling gres_get_task_limit unless needed
@@ -1937,19 +1936,13 @@ alloc_job:
 			node_gres_list[j] = node_ptr->gres_list;
 			sock_gres_list[j] =
 				avail_res_array[i]->sock_gres_list;
-			if (task_limit_set) {
+			if (task_limit_set)
 				log_flag(SELECT_TYPE, "%pJ: Node=%s: gres_task_limit[%d]=%u",
 					 job_ptr,
 					 node_ptr->name,
 					 i, gres_task_limit[j]);
-				task_cnt += gres_task_limit[j];
-			}
 			j++;
 		}
-		/* Ensure gres_task_limit doesn't limit total cpu cnt */
-		job_ptr->details->cpus_per_task = MAX(
-			job_ptr->details->cpus_per_task,
-			job_res->ncpus / task_cnt);
 
 		if (!task_limit_set)
 			xfree(gres_task_limit);