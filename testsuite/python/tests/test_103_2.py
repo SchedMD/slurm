411e285ea0d7c1e3a420bd60f1cd8bb60f06917e
Albert Gil <albert.gil@schedmd.com>
2024-10-28 17:53:38 +0100

    Testsuite - Adapt test_103_2 to new/fixed behavior
    This is a continuation of previous commit for test_103_2.
    
    Ticket 21218

--- testsuite/python/tests/test_103_2.py
+++ testsuite/python/tests/test_103_2.py
@@ -1,12 +1,15 @@
 ############################################################################
 # Copyright (C) SchedMD LLC.
 ############################################################################
+import re
+
 import atf
 import pytest
 
 
 @pytest.fixture(scope="module", autouse=True)
 def setup():
+    atf.require_nodes(1, [("CPUs", 1)])
     atf.require_slurm_running()
 
 
@@ -16,7 +19,7 @@ def test_salloc_normal():
     The normal allocations should have no error output with the -Q flag."""
 
     # Test that normal salloc works correctly
-    result = atf.run_command("salloc -Q &")
+    result = atf.run_command("salloc -Q true")
     assert result["exit_code"] == 0, "Exit code was not 0!"
     assert (
         result["stderr"] == ""
@@ -28,7 +31,7 @@ def test_salloc_normal():
     atf.cancel_all_jobs()
 
     # Test that salloc -n1 works correctly
-    result = atf.run_command("salloc -Q -n1 &")
+    result = atf.run_command("salloc -Q -n1 true")
     assert result["exit_code"] == 0, "Exit code was not 0!"
     assert (
         result["stderr"] == ""
@@ -39,12 +42,12 @@ def test_salloc_normal():
 
     atf.cancel_all_jobs()
 
-    # Test that salloc -n2 will wait for resources correctly with only one node
-    result = atf.run_command("salloc -Q -n2 &", timeout=3)
-    assert result["exit_code"] == 110, "Exit code was not 110 (timeout)!"
-    assert (
-        result["stderr"] == ""
-    ), f"There should be no error messages from the salloc command. Got: {result['stderr']}"
+    # Test that salloc -n2 is rejected with only one node
+    result = atf.run_command("salloc -Q -n2 true", timeout=3)
+    assert result["exit_code"] != 0, "Verify salloc failed"
+    assert re.search(
+        "Requested node configuration is not available", result["stderr"]
+    ), f"Error message should contain 'Requested node configuration is not available'. Got: {result['stderr']}"
     assert (
         result["stdout"] == ""
     ), f"There should be no stdout from the salloc command. Got: {result['stdout']}"
