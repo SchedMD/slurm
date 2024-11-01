SchedMD/slurm

commit 194d774c8fe7578529026cec9e4453b9234ddc02

slurmstepd - Split _forward_return_code() from _forkexec_slurmstepd()
Ticket 20114

src/slurmd/slurmd/req.c
@@ -820,6 +820,46 @@ fail:
 	return errno;
 }
 
+#if (SLURMSTEPD_MEMCHECK != 1)
+
+static int _handle_return_code(int to_slurmd, int to_stepd, int *rc_ptr)
+{
+	int rc = SLURM_SUCCESS;
+	int i;
+	time_t start_time = time(NULL);
+
+	i = read(to_slurmd, &rc_ptr, sizeof(int));
+	if (i < 0) {
+		error("%s: Can not read return code from slurmstepd "
+		      "got %d: %m", __func__, i);
+		return SLURM_ERROR;
+	} else if (i != sizeof(int)) {
+		error("%s: slurmstepd failed to send return code "
+		      "got %d: %m", __func__, i);
+		return SLURM_ERROR;
+	} else {
+		int delta_time = time(NULL) - start_time;
+		int cc;
+		if (delta_time > 5) {
+			warning("slurmstepd startup took %d sec, possible file system problem or full memory",
+				delta_time);
+		}
+		if (rc != SLURM_SUCCESS)
+			error("slurmstepd return code %d: %s",
+			      rc, slurm_strerror(rc));
+
+		cc = SLURM_SUCCESS;
+		cc = write(to_stepd, &cc, sizeof(int));
+		if (cc != sizeof(int)) {
+			error("%s: failed to send ack to stepd %d: %m",
+			      __func__, cc);
+		}
+	}
+
+	return SLURM_SUCCESS;
+}
+
+#endif /* SLURMSTEPD_MEMCHECK != 1 */
 
 /*
  * Fork and exec the slurmstepd, then send the slurmstepd its
@@ -859,11 +899,7 @@ _forkexec_slurmstepd(uint16_t type, void *req, slurm_addr_t *cli,
 		_remove_starting_step(type, req);
 		return SLURM_ERROR;
 	} else if (pid > 0) {
-		int rc = SLURM_SUCCESS;
-#if (SLURMSTEPD_MEMCHECK != 1)
-		int i;
-		time_t start_time = time(NULL);
-#endif
+		int rc = SLURM_SUCCESS, rc2;
 		/*
 		 * Parent sends initialization data to the slurmstepd
 		 * over the to_stepd pipe, and waits for the return code
@@ -886,33 +922,8 @@ _forkexec_slurmstepd(uint16_t type, void *req, slurm_addr_t *cli,
 		 * so just skip it.
 		 */
 #if (SLURMSTEPD_MEMCHECK != 1)
-		i = read(to_slurmd[0], &rc, sizeof(int));
-		if (i < 0) {
-			error("%s: Can not read return code from slurmstepd "
-			      "got %d: %m", __func__, i);
-			rc = SLURM_ERROR;
-		} else if (i != sizeof(int)) {
-			error("%s: slurmstepd failed to send return code "
-			      "got %d: %m", __func__, i);
-			rc = SLURM_ERROR;
-		} else {
-			int delta_time = time(NULL) - start_time;
-			int cc;
-			if (delta_time > 5) {
-				warning("slurmstepd startup took %d sec, possible file system problem or full memory",
-					delta_time);
-			}
-			if (rc != SLURM_SUCCESS)
-				error("slurmstepd return code %d: %s",
-				      rc, slurm_strerror(rc));
-
-			cc = SLURM_SUCCESS;
-			cc = write(to_stepd[1], &cc, sizeof(int));
-			if (cc != sizeof(int)) {
-				error("%s: failed to send ack to stepd %d: %m",
-				      __func__, cc);
-			}
-		}
+		if ((rc2 = _handle_return_code(to_slurmd[0], to_stepd[1], &rc)))
+			return rc2;
 #endif
 	done:
 		if (_remove_starting_step(type, req))

commit 15d5363a7f60214a0b04c014c579dc7cdafbd9cf

slurmd - Properly clean up after an early step failure.
This fixes a regression from commit 194d774c8f that would cause the
slurmd to hang on shutdown if a step exits without returning an error
code.

Signed-off-by: Tim Wickberg <wickberg@schedmd.com>

src/slurmd/slurmd/req.c
@@ -954,8 +954,11 @@ static int _forkexec_slurmstepd(uint16_t type, void *req, slurm_addr_t *cli,
 		 * so just skip it.
 		 */
 #if (SLURMSTEPD_MEMCHECK != 1)
-		if ((rc2 = _handle_return_code(to_slurmd[0], to_stepd[1], &rc)))
-			return rc2;
+		if ((rc2 = _handle_return_code(to_slurmd[0],
+					       to_stepd[1], &rc))) {
+			rc = rc2;
+			goto done;
+		}
 #endif
 	done:
 		if (_remove_starting_step(type, req))