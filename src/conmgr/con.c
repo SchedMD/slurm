SchedMD/slurm

commit c37cda0668d2c469fa1973c2a2316c76e9f6d184

Set type via fd_change_mode() in add_connection()
Use single place for setting conmgr_fd_t->type.

Issue 50123

src/conmgr/con.c
@@ -515,7 +515,7 @@ extern int add_connection(conmgr_con_type_t type,
 		.work = list_create(NULL),
 		.write_complete_work = list_create(NULL),
 		.new_arg = arg,
-		.type = type,
+		.type = CON_TYPE_INVALID,
 		.polling_input_fd = PCTL_TYPE_NONE,
 		.polling_output_fd = PCTL_TYPE_NONE,
 		/* Set flags not related to connection state tracking */
@@ -568,7 +568,7 @@ extern int add_connection(conmgr_con_type_t type,
 
 	_set_connection_name(con, &in_stat, &out_stat);
 
-	_check_con_type(con, type);
+	fd_change_mode(con, type);
 
 	if (con_flag(con, FLAG_WATCH_CONNECT_TIMEOUT))
 		con->last_read = timespec_now();

commit 1b94e04f899361e8a16e6765767c258da5ddeb35

conmgr - Prevent NULL pointer dereference in log_flag CONMGR
If a connection is a listener, then con->in is not initialized in
add_connection(). This is a regression from commit c37cda06 because
fd_change_mode() was not called prior to that commit.

Continuation of c822ba3e87.

Issue #50132, 50135

Signed-off-by: Nathan Rini <nate@schedmd.com>

src/conmgr/con.c
@@ -408,7 +408,8 @@ extern int fd_change_mode(conmgr_fd_t *con, conmgr_con_type_t type)
 
 	log_flag(CONMGR, "%s: [%s] changing type: %s->%s pending_reads=%u pending_writes=%u",
 		 __func__, con->name, conmgr_con_type_string(con->type),
-		 conmgr_con_type_string(type), get_buf_offset(con->in),
+		 conmgr_con_type_string(type),
+		 con->in ? get_buf_offset(con->in) : 0,
 		 list_count(con->out));
 
 	/* Always set TCP_NODELAY for Slurm RPC connections */