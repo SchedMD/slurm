90ec1fecd21c680b42133deccc7ef11bfad6eeff
Danny Auble <da@schedmd.com>
2024-11-12 11:53:58 -0700

    Move open closer to where it is used for easier readability.
    Issue 50152

--- src/plugins/gpu/nvidia/gpu_nvidia.c
+++ src/plugins/gpu/nvidia/gpu_nvidia.c
@@ -109,9 +109,9 @@ static void _set_cpu_affinity(node_config_load_t *node_conf, char *bus_id,
 	}
 
 	path = xstrdup_printf(NVIDIA_CPULIST_PREFIX, bus_id);
-	f = fopen(path, "r");
 	cpus_bitmap = bit_alloc(MAX_CPUS);
 
+	f = fopen(path, "r");
 	while (fgets(buffer, sizeof(buffer), f)) {
 		if (bit_unfmt(cpus_bitmap, buffer))
 			error("Unable to parse cpu list in %s", path);
