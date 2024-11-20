SchedMD/slurm

commit fecabd3cc7f6c1caf262bc20929e6575be310c28

Add parameter --oom-kill-step
If set, this parameter will override the TaskPluginParam OOMKillStep.
This commit adds all the plumbing necessary to set the parameter and
make it reach the step struct.

Issue #50069

src/slurmctld/job_mgr.c
@@ -8686,6 +8686,8 @@ static int _copy_job_desc_to_job_record(job_desc_msg_t *job_desc,
 	if (job_desc->pn_min_tmp_disk != NO_VAL)
 		detail_ptr->pn_min_tmp_disk = job_desc->pn_min_tmp_disk;
 
+	detail_ptr->oom_kill_step = job_desc->oom_kill_step;
+
 	detail_ptr->segment_size = job_desc->segment_size;
 	detail_ptr->std_err = xstrdup(job_desc->std_err);
 	detail_ptr->std_in = xstrdup(job_desc->std_in);
@@ -11224,6 +11226,8 @@ static void _pack_pending_job_details(job_details_t *detail_ptr, buf_t *buffer,
 			pack64(detail_ptr->pn_min_memory, buffer);
 			pack32(detail_ptr->pn_min_tmp_disk, buffer);
 
+			pack16(detail_ptr->oom_kill_step, buffer);
+
 			packstr(detail_ptr->req_nodes, buffer);
 			pack_bit_str_hex(detail_ptr->req_node_bitmap, buffer);
 			packstr(detail_ptr->exc_nodes, buffer);
@@ -11243,6 +11247,8 @@ static void _pack_pending_job_details(job_details_t *detail_ptr, buf_t *buffer,
 
 			pack64((uint64_t) 0, buffer);
 			pack32((uint32_t) 0, buffer);
+
+			pack16((uint16_t) 0, buffer);
 
 			packnull(buffer);
 			packnull(buffer);
@@ -18344,6 +18350,7 @@ extern job_desc_msg_t *copy_job_record_to_job_desc(job_record_t *job_ptr)
 	job_desc->work_dir          = xstrdup(details->work_dir);
 	job_desc->pn_min_cpus       = details->pn_min_cpus;
 	job_desc->pn_min_memory     = details->pn_min_memory;
+	job_desc->oom_kill_step     = details->oom_kill_step;
 	job_desc->pn_min_tmp_disk   = details->pn_min_tmp_disk;
 	job_desc->min_cpus          = details->min_cpus;
 	job_desc->max_cpus          = details->max_cpus;

slurm/slurm.h
@@ -1670,6 +1670,9 @@ typedef struct job_descriptor {	/* For submit, allocate, and update requests */
 				 * NICE_OFFSET == no change */
 	uint32_t num_tasks;	/* number of tasks to be started,
 				 * for batch only */
+	uint16_t oom_kill_step;	/* 1 if kill whole step on task OOM,
+				 * 0 do nothing on task OOM
+				 * NO_VAL16 use taskpluginparam option */
 	uint8_t open_mode;	/* out/err open mode truncate or append,
 				 * see OPEN_MODE_* */
 	char *origin_cluster;	/* cluster name that initiated the job. */
@@ -1869,6 +1872,7 @@ typedef struct job_info {
 	uint32_t num_cpus;	/* minimum number of cpus required by job */
 	uint32_t num_nodes;	/* minimum number of nodes required by job */
 	uint32_t num_tasks;	/* requested task count */
+	uint16_t oom_kill_step;	/* kill whole step in case of task oom */
 	char *partition;	/* name of assigned partition */
 	char *prefer;		/* comma separated list of soft features */
 	uint64_t pn_min_memory; /* minimum real memory per node, default=0 */
@@ -2222,6 +2226,7 @@ typedef struct {
 	uint32_t spank_job_env_size;	/* element count in spank_env */
 	char *tres_bind;
 	char *tres_freq;
+	uint16_t oom_kill_step;
 } slurm_step_launch_params_t;
 
 typedef struct {

src/api/step_launch.c
@@ -353,6 +353,11 @@ extern int slurm_step_launch(slurm_step_ctx_t *ctx,
 	launch.ofname = params->remote_output_filename;
 	launch.efname = params->remote_error_filename;
 	launch.ifname = params->remote_input_filename;
+	if (params->oom_kill_step != NO_VAL16)
+		launch.oom_kill_step = (params->oom_kill_step == 1);
+	else
+		launch.oom_kill_step =
+			slurm_conf.task_plugin_param & OOM_KILL_STEP;
 	if (params->buffered_stdio)
 		launch.flags |= LAUNCH_BUFFERED_IO;
 	if (params->labelio)

src/common/job_record.c
@@ -566,6 +566,7 @@ static void _dump_job_details(job_details_t *detail_ptr, buf_t *buffer,
 
 		pack32(detail_ptr->orig_pn_min_cpus, buffer);
 		pack64(detail_ptr->orig_pn_min_memory, buffer);
+		pack16(detail_ptr->oom_kill_step, buffer);
 		pack32(detail_ptr->pn_min_tmp_disk, buffer);
 
 		packstr(detail_ptr->req_nodes, buffer);
@@ -1291,6 +1292,7 @@ static int _load_job_details(job_record_t *job_ptr, buf_t *buffer,
 	uint32_t min_cpus = 1, max_cpus = NO_VAL;
 	uint32_t pn_min_cpus, pn_min_tmp_disk;
 	uint64_t pn_min_memory;
+	uint16_t oom_kill_step = NO_VAL16;
 	uint32_t cpu_freq_min = NO_VAL;
 	uint32_t cpu_freq_max = NO_VAL;
 	uint32_t cpu_freq_gov = NO_VAL, nice = 0;
@@ -1355,6 +1357,7 @@ static int _load_job_details(job_record_t *job_ptr, buf_t *buffer,
 
 		safe_unpack32(&pn_min_cpus, buffer);
 		safe_unpack64(&pn_min_memory, buffer);
+		safe_unpack16(&oom_kill_step, buffer);
 		safe_unpack32(&pn_min_tmp_disk, buffer);
 
 		safe_unpackstr(&req_nodes, buffer);
@@ -1635,6 +1638,7 @@ static int _load_job_details(job_record_t *job_ptr, buf_t *buffer,
 	job_ptr->details->pn_min_cpus = pn_min_cpus;
 	job_ptr->details->orig_pn_min_cpus = pn_min_cpus;
 	job_ptr->details->pn_min_memory = pn_min_memory;
+	job_ptr->details->oom_kill_step = oom_kill_step;
 	job_ptr->details->orig_pn_min_memory = pn_min_memory;
 	job_ptr->details->pn_min_tmp_disk = pn_min_tmp_disk;
 	job_ptr->details->max_cpus = max_cpus;

src/common/job_record.h
@@ -144,6 +144,7 @@ typedef struct {
 					 * memory per allocated
 					 * CPU | MEM_PER_CPU */
 	uint64_t orig_pn_min_memory;	/* requested value of pn_min_memory */
+	uint16_t oom_kill_step;		/* Kill whole step in case of OOM */
 	uint32_t pn_min_tmp_disk;	/* minimum tempdisk per node, MB */
 	list_t *prefer_list;		/* soft features with node counts */
 	char *prefer;			/* soft features */

src/common/slurm_opt.c
@@ -2209,6 +2209,40 @@ static slurm_cli_opt_t slurm_opt_mcs_label = {
 	.reset_func = arg_reset_mcs_label,
 };
 
+static int arg_set_oom_kill_step(slurm_opt_t *opt, const char *arg)
+{
+	uint16_t res;
+	if (!arg) {
+		opt->oom_kill_step = 1;
+		return SLURM_SUCCESS;
+	}
+	if (!parse_uint16((char *)arg, &res) && (res <= 1)) {
+		opt->oom_kill_step = res;
+		return SLURM_SUCCESS;
+	}
+
+	error("Invalid --oom-kill-step specification");
+	return SLURM_ERROR;
+}
+
+static char *arg_get_oom_kill_step(slurm_opt_t *opt)
+{
+	if (opt->oom_kill_step == NO_VAL16)
+		return xstrdup("unset");
+
+	return xstrdup_printf("%u", opt->oom_kill_step);
+}
+
+COMMON_OPTION_RESET(oom_kill_step, NO_VAL16);
+static slurm_cli_opt_t slurm_opt_oom_kill_step = {
+	.name = "oom-kill-step",
+	.has_arg = optional_argument,
+	.val = LONG_OPT_OOMKILLSTEP,
+	.set_func = arg_set_oom_kill_step,
+	.get_func = arg_get_oom_kill_step,
+	.reset_func = arg_reset_oom_kill_step,
+};
+
 static int arg_set_mem(slurm_opt_t *opt, const char *arg)
 {
 	if ((opt->pn_min_memory = str_to_mbytes(arg)) == NO_VAL64) {
@@ -4146,6 +4180,7 @@ static const slurm_cli_opt_t *common_options[] = {
 	&slurm_opt_ntasks_per_node,
 	&slurm_opt_ntasks_per_socket,
 	&slurm_opt_ntasks_per_tres,
+	&slurm_opt_oom_kill_step,
 	&slurm_opt_open_mode,
 	&slurm_opt_output,
 	&slurm_opt_overcommit,
@@ -5617,6 +5652,7 @@ extern job_desc_msg_t *slurm_opt_create_job_desc(slurm_opt_t *opt_local,
 
 	job_desc->submit_line = opt_local->submit_line;
 	job_desc->task_dist = opt_local->distribution;
+	job_desc->oom_kill_step = opt_local->oom_kill_step;
 
 	if (opt_local->time_limit != NO_VAL)
 		job_desc->time_limit = opt_local->time_limit;

src/common/slurm_opt.h
@@ -149,6 +149,7 @@ enum {
 	LONG_OPT_NTASKSPERNODE,
 	LONG_OPT_NTASKSPERSOCKET,
 	LONG_OPT_NTASKSPERTRES,
+	LONG_OPT_OOMKILLSTEP,
 	LONG_OPT_OPEN_MODE,
 	LONG_OPT_OVERLAP,
 	LONG_OPT_HET_GROUP,
@@ -379,6 +380,7 @@ typedef struct {
 	uint64_t mem_per_cpu;		/* --mem-per-cpu		*/
 	uint64_t mem_per_gpu;		/* --mem-per-gpu		*/
 	uint64_t pn_min_memory;		/* --mem			*/
+	uint16_t oom_kill_step;		/* --oom-kill-step=0,1		*/
 	uint64_t pn_min_tmp_disk;	/* --tmp			*/
 	char *prefer;			/* --prefer			*/
 	char *constraint;		/* --constraint			*/

src/common/slurm_protocol_defs.h
@@ -753,6 +753,7 @@ typedef struct launch_tasks_request_msg {
 	part_record_t *part_ptr;
 
 	char *stepmgr; /* Hostname of stepmgr */
+	bool oom_kill_step;
 } launch_tasks_request_msg_t;
 
 typedef struct partition_info partition_desc_msg_t;
@@ -944,6 +945,7 @@ typedef struct batch_job_launch_msg {
 	char *tres_bind;	/* task binding to TRES (e.g. GPUs),
 				 * included for possible future use */
 	char *tres_freq;	/* frequency/power for TRES (e.g. GPUs) */
+	bool oom_kill_step;
 } batch_job_launch_msg_t;
 
 typedef struct job_id_request_msg {

commit 6b6b773760dc79b5144be0dfd4ec47f458985efd

Remove oom_kill_steps from the packing code
This new field was mistakenly added to the old protocol block, which breaks
backwards compatibility. This is a regression from commit fecabd3cc7f. This
only needs to be removed from the pack code. This field was added to a 24.11
protocol block in the unpack code.

Ticket 21468

src/slurmctld/job_mgr.c
@@ -11223,8 +11223,6 @@ static void _pack_pending_job_details(job_details_t *detail_ptr, buf_t *buffer,
 			pack64(detail_ptr->pn_min_memory, buffer);
 			pack32(detail_ptr->pn_min_tmp_disk, buffer);
 
-			pack16(detail_ptr->oom_kill_step, buffer);
-
 			packstr(detail_ptr->req_nodes, buffer);
 			pack_bit_str_hex(detail_ptr->req_node_bitmap, buffer);
 			packstr(detail_ptr->exc_nodes, buffer);
@@ -11244,8 +11242,6 @@ static void _pack_pending_job_details(job_details_t *detail_ptr, buf_t *buffer,
 
 			pack64((uint64_t) 0, buffer);
 			pack32((uint32_t) 0, buffer);
-
-			pack16((uint16_t) 0, buffer);
 
 			packnull(buffer);
 			packnull(buffer);