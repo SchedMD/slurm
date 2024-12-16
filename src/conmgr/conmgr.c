SchedMD/slurm

commit 9cc4c4f038b6f375bfbd1f95c0a22ead93b3931f

Set process name for conmgr watch() thread
Issue 50111

src/conmgr/watch.c
@@ -34,6 +34,12 @@
 \*****************************************************************************/
 
 #define _GNU_SOURCE
+#include "config.h"
+
+#if HAVE_SYS_PRCTL_H
+#include <sys/prctl.h>
+#endif
+
 #include <limits.h>
 #include <stdint.h>
 #include <sys/un.h>
@@ -1320,6 +1326,17 @@ static bool _watch_loop(void)
 
 extern void *watch(void *arg)
 {
+#if HAVE_SYS_PRCTL_H
+	{
+		static char title[] = "watch";
+
+		if (prctl(PR_SET_NAME, title, NULL, NULL, NULL)) {
+			error("%s: cannot set process name to %s %m",
+			      __func__, title);
+		}
+	}
+#endif
+
 	slurm_mutex_lock(&mgr.mutex);
 
 	xassert(mgr.watch_thread == pthread_self());

commit 0c980f1fe7250749947e9ceac37d62fe297f6944

Avoid changing process name for daemons that block on conmgr_run()
Only set thread name to "watch" if watch is running as a background thread.
Setting the thread name to "watch" when watch() is running on the primary
process thread will result in the entire process's name changing to "watch"
which can break existing monitoring scripts.

This change in behavior only affected slurmd and slurmrestd which call
`conmgr_run(true)`.

Regression from 9cc4c4f038.

Changelog: Avoid changing process name to "watch" from original daemon name.
 This could potentially breaking some monitoring scripts.
Ticket: 21572

src/conmgr/conmgr.c
@@ -225,7 +225,7 @@ extern int conmgr_run(bool blocking)
 	if (mgr.watch_thread)
 		running = true;
 	else if (!blocking)
-		slurm_thread_create(&mgr.watch_thread, watch, NULL);
+		slurm_thread_create(&mgr.watch_thread, watch_thread, NULL);
 	else
 		mgr.watch_thread = pthread_self();
 

src/conmgr/mgr.h
@@ -431,6 +431,7 @@ extern void handle_work(bool locked, work_t *work);
  * Poll all connections and handle any events
  */
 extern void *watch(void *arg);
+extern void *watch_thread(void *arg);
 
 /*
  * Wait for _watch() to finish

src/conmgr/watch.c
@@ -1330,17 +1330,6 @@ static bool _watch_loop(void)
 
 extern void *watch(void *arg)
 {
-#if HAVE_SYS_PRCTL_H
-	{
-		static char title[] = "watch";
-
-		if (prctl(PR_SET_NAME, title, NULL, NULL, NULL)) {
-			error("%s: cannot set process name to %s %m",
-			      __func__, title);
-		}
-	}
-#endif
-
 	slurm_mutex_lock(&mgr.mutex);
 
 	xassert(mgr.watch_thread == pthread_self());
@@ -1391,4 +1380,18 @@ extern void *watch(void *arg)
 	slurm_mutex_unlock(&mgr.mutex);
 
 	return NULL;
+}
+
+extern void *watch_thread(void *arg)
+{
+#if HAVE_SYS_PRCTL_H
+	static char title[] = "watch";
+
+	if (prctl(PR_SET_NAME, title, NULL, NULL, NULL)) {
+		error("%s: cannot set process name to %s %m",
+		      __func__, title);
+	}
+#endif
+
+	return watch(NULL);
 }