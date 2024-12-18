SchedMD/slurm

commit 892539ae62a22dc0d782067ca6add91f5485fca3

Make LogTimeFormat to always apply to stderr and the logfile.
Now messages printed to stderr and to logfile will have the exact
same format.

Issue: 50180

src/common/log.c
@@ -1393,8 +1393,9 @@ static void _log_msg(log_level_t level, bool sched, bool spank, bool warn,
 		if (spank) {
 			_log_printf(log, log->buf, stderr, "%s%s", buf, eol);
 		} else {
-			_log_printf(log, log->buf, stderr, "%s: %s%s%s",
-			            log->argv0, pfx, buf, eol);
+			xlogfmtcat(&msgbuf, "[%M] %s%s%s", pfx, buf, eol);
+			_log_printf(log, log->buf, stderr, msgbuf);
+			xfree(msgbuf);
 		}
 		fflush(stderr);
 	}

commit de48c636e185bacb6f2508c2aabe8a19a73aad8b

Fix passing bad string to xlogfmtcat when formatting stderr
We called xlogfmtcat with variables that could contain %n. This
made glibc unhappy and caused segfaults. Regression caused in
commit 892539ae62a22dc0d.

Issue: 50180
Ticket: 21679

src/common/log.c
@@ -1393,8 +1393,10 @@ static void _log_msg(log_level_t level, bool sched, bool spank, bool warn,
 		if (spank) {
 			_log_printf(log, log->buf, stderr, "%s%s", buf, eol);
 		} else {
-			xlogfmtcat(&msgbuf, "[%M] %s%s%s", pfx, buf, eol);
-			_log_printf(log, log->buf, stderr, msgbuf);
+			xlogfmtcat(&msgbuf, "[%M]");
+			_log_printf(log, log->buf, stderr,
+				    "%s %s: %s%s%s",
+				    msgbuf, log->argv0, pfx, buf, eol);
 			xfree(msgbuf);
 		}
 		fflush(stderr);