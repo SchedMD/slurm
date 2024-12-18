712cda9b2ea6280ee08524782972ada382868e55
Felip Moll <felip.moll@schedmd.com>
2024-12-18 20:28:14 +0100

    Do not print formatted log in user commands
    Regression introduced in 892539ae62a22dc0d.
    
    Issue: 50180
    Ticket: 21679

--- src/common/log.c
+++ src/common/log.c
@@ -1392,12 +1392,15 @@ static void _log_msg(log_level_t level, bool sched, bool spank, bool warn,
 		fflush(stdout);
 		if (spank) {
 			_log_printf(log, log->buf, stderr, "%s%s", buf, eol);
-		} else {
+		} else if (running_in_daemon()) {
 			xlogfmtcat(&msgbuf, "[%M]");
 			_log_printf(log, log->buf, stderr,
 				    "%s %s: %s%s%s",
 				    msgbuf, log->argv0, pfx, buf, eol);
 			xfree(msgbuf);
+		} else {
+			_log_printf(log, log->buf, stderr, "%s: %s%s%s",
+				    log->argv0, pfx, buf, eol);
 		}
 		fflush(stderr);
 	}
