SchedMD/slurm

commit 37a3fcdf7ba7b9b89bac32fd9ffe6f8e1e21bbaf

job_container/tmpfs - Add EntireStepInNS option.
Ticket 20844.

src/plugins/job_container/tmpfs/job_container_tmpfs.c
@@ -697,6 +697,16 @@ extern int container_p_join(uint32_t job_id, uid_t uid)
 		return SLURM_SUCCESS;
 
 	/*
+	 * Handle EntireStepInNS setting. If set, the join needs to happen
+	 * during the fork+exec chain that creates the slurmstepd process, and
+	 * all successive calls within slurmstepd need to be skipped. If not
+	 * set, do the opposite.
+	 */
+	if ((!jc_conf->entire_step_in_ns && running_in_slurmd()) ||
+	    (jc_conf->entire_step_in_ns && running_in_slurmstepd()))
+		return SLURM_SUCCESS;
+
+	/*
 	 * Jobid 0 means we are not a real job, but a script running instead we
 	 * do not need to handle this request.
 	 */

doc/man/man5/job_container.conf.5
@@ -1,4 +1,4 @@
-.TH "job_container.conf" "5" "Slurm Configuration File" "November 2022" "Slurm Configuration File"
+.TH "job_container.conf" "5" "Slurm Configuration File" "October 2024" "Slurm Configuration File"
 
 .SH "NAME"
 job_container.conf \- Slurm configuration file for job_container/tmpfs plugin
@@ -77,6 +77,14 @@ This parameter is optional and if not specified it defaults to "/tmp,/dev/shm".
 
 \fBNOTE\fR: /dev/shm has special handling, and instead of a bind mount is always
 a fresh tmpfs filesystem.
+.IP
+
+.TP
+\fBEntireStepInNS\fR
+Specifying fBEntireStepInNS=true will pivot all slurmstepd processes (excluding
+the external step, which is tasked with creating the namespace) into the
+constructed namespace. This will cause issues if certain paths such as
+SlurmdSpoolDir as inaccessible. This parameter is optional.
 .IP
 
 .TP

src/plugins/job_container/tmpfs/read_jcconf.c
@@ -55,6 +55,7 @@ static buf_t *slurm_jc_conf_buf = NULL;
 static bool slurm_jc_conf_inited = false;
 static bool auto_basepath_set = false;
 static bool shared_set = false;
+static bool entire_step_in_ns_set = false;
 
 static s_p_hashtbl_t *_create_ns_hashtbl(void)
 {
@@ -62,6 +63,7 @@ static s_p_hashtbl_t *_create_ns_hashtbl(void)
 		{"AutoBasePath", S_P_BOOLEAN},
 		{"BasePath", S_P_STRING},
 		{"Dirs", S_P_STRING},
+		{"EntireStepInNS", S_P_BOOLEAN},
 		{"InitScript", S_P_STRING},
 		{"Shared", S_P_BOOLEAN},
 		{NULL}
@@ -79,6 +81,7 @@ static void _pack_slurm_jc_conf_buf(void)
 	packbool(slurm_jc_conf.auto_basepath, slurm_jc_conf_buf);
 	packstr(slurm_jc_conf.basepath, slurm_jc_conf_buf);
 	packstr(slurm_jc_conf.dirs, slurm_jc_conf_buf);
+	packbool(slurm_jc_conf.entire_step_in_ns, slurm_jc_conf_buf);
 	packstr(slurm_jc_conf.initscript, slurm_jc_conf_buf);
 	packbool(slurm_jc_conf.shared, slurm_jc_conf_buf);
 }
@@ -114,6 +117,10 @@ static int _parse_jc_conf_internal(void **dest, slurm_parser_enum_t type,
 
 	if (!s_p_get_string(&slurm_jc_conf.dirs, "Dirs", tbl))
 		debug3("empty Dirs detected");
+
+	if (s_p_get_boolean(&slurm_jc_conf.entire_step_in_ns, "EntireStepInNS",
+			    tbl))
+		entire_step_in_ns_set = true;
 
 	if (!s_p_get_string(&slurm_jc_conf.initscript, "InitScript", tbl))
 		debug3("empty init script detected");
@@ -163,6 +170,7 @@ static int _read_slurm_jc_conf(void)
 		{"AutoBasePath", S_P_BOOLEAN},
 		{"BasePath", S_P_ARRAY, _parse_jc_conf_internal, NULL},
 		{"Dirs", S_P_STRING},
+		{"EntireStepInNS", S_P_BOOLEAN},
 		{"NodeName", S_P_ARRAY, _parse_jc_conf, NULL},
 		{"Shared", S_P_BOOLEAN},
 		{NULL}
@@ -202,6 +210,10 @@ static int _read_slurm_jc_conf(void)
 		debug("Plugin is disabled on this node per %s.",
 		      tmpfs_conf_file);
 	}
+
+	if (!entire_step_in_ns_set)
+		s_p_get_boolean(&slurm_jc_conf.entire_step_in_ns,
+				"EntireStepInNS", tbl);
 
 	if (!shared_set)
 		s_p_get_boolean(&slurm_jc_conf.shared, "Shared", tbl);
@@ -253,6 +265,7 @@ extern slurm_jc_conf_t *set_slurm_jc_conf(buf_t *buf)
 	safe_unpackbool(&slurm_jc_conf.auto_basepath, buf);
 	safe_unpackstr(&slurm_jc_conf.basepath, buf);
 	safe_unpackstr(&slurm_jc_conf.dirs, buf);
+	safe_unpackbool(&slurm_jc_conf.entire_step_in_ns, buf);
 	safe_unpackstr(&slurm_jc_conf.initscript, buf);
 	safe_unpackbool(&slurm_jc_conf.shared, buf);
 	slurm_jc_conf_inited = true;

src/plugins/job_container/tmpfs/read_jcconf.h
@@ -50,6 +50,7 @@ typedef struct slurm_jc_conf {
 	bool auto_basepath;
 	char *basepath;
 	char *dirs;
+	bool entire_step_in_ns;
 	char *initscript;
 	bool shared;
 } slurm_jc_conf_t;

src/slurmd/slurmd/req.c
@@ -1025,6 +1025,15 @@ static int _forkexec_slurmstepd(uint16_t type, void *req, slurm_addr_t *cli,
 			error("%s: setsid: %m", __func__);
 			failed = 1;
 		}
+
+		if (step_id != SLURM_EXTERN_CONT) {
+			if (container_g_join(job_id, uid)) {
+				error("%s container_g_join(%u): %m",
+				      __func__, job_id);
+				_exit(SLURM_ERROR);
+			}
+		}
+
 		if ((pid = fork()) < 0) {
 			error("%s: Unable to fork grandchild: %m", __func__);
 			failed = 2;

commit 956e1db0dfb781dd5fae7365393e8ba0e711930c

Fix sbcast transferring files to the job container
Fix a regression in 24.11 from commit 37a3fcdf which caused file transfers
to a job with sbcast to not join the job container namespace.

Ticket: 21634

src/plugins/job_container/tmpfs/job_container_tmpfs.c
@@ -737,7 +737,8 @@ extern int container_p_join(uint32_t job_id, uid_t uid, bool step_create)
 	 * all successive calls within slurmstepd need to be skipped. If not
 	 * set, do the opposite.
 	 */
-	if ((!jc_conf->entire_step_in_ns && running_in_slurmd()) ||
+	if ((!jc_conf->entire_step_in_ns && running_in_slurmd() &&
+	     step_create) ||
 	    (jc_conf->entire_step_in_ns && running_in_slurmstepd()))
 		return SLURM_SUCCESS;
 