c5bb52907b835018f811f2d2f258d4f408ffd143
Megan Dahl <megan@schedmd.com>
2025-01-08 10:57:40 -0700

    Don't skip setting cpus_per_task from tres_per_task in client command
    This fixes a regression added by commit be63f8438e.
    
    Ticket 21764

--- src/common/slurm_opt.c
+++ src/common/slurm_opt.c
@@ -5219,8 +5219,9 @@ static void _set_tres_per_task_from_sibling_opt(slurm_opt_t *opt, int optval)
 
 		/* Set cpus_per_task based off of tres_per_task */
 		if (opt->tres_per_task &&
-		    slurm_get_next_tres(&tres_type, opt->tres_per_task, &name,
-					&type, &cnt, &save_ptr)) {
+		    (slurm_get_next_tres(&tres_type, opt->tres_per_task, &name,
+					 &type, &cnt,
+					 &save_ptr) == SLURM_SUCCESS)) {
 			opt->cpus_per_task = cnt;
 			opt->cpus_set = true;
 
