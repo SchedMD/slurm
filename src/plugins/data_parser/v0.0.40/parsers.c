SchedMD/slurm

commit 1c265ec8a28527fccc1f5be58689d182faffc486

data_parser/v0.0.40 - Resync PARSER_ARRAY(ASSOC)
Bug 16960

src/plugins/data_parser/v0.0.40/parsers.c
@@ -6086,33 +6086,36 @@ static const flag_bit_t PARSER_FLAG_ARRAY(ASSOC_FLAGS)[] = {
 	add_parser(slurmdb_assoc_rec_t, mtype, true, field, 0, path, desc)
 /* should mirror the structure of slurmdb_assoc_rec_t */
 static const parser_t PARSER_ARRAY(ASSOC)[] = {
-	add_skip(accounting_list),
+	add_parse(ACCOUNTING_LIST, accounting_list, "accounting", "Usage accounting"),
 	add_parse(STRING, acct, "account", NULL),
 	add_skip(assoc_next),
 	add_skip(assoc_next_id),
 	add_skip(bf_usage),
 	add_parse(STRING, cluster, "cluster", NULL),
+	add_parse(STRING, comment, "comment", "comment for the association"),
 	add_parse(QOS_ID, def_qos_id, "default/qos", NULL),
 	add_parse_bit_flag_array(slurmdb_assoc_rec_t, ASSOC_FLAGS, false, flags, "flags", NULL),
-	add_skip(lft),
 	add_parse(UINT32_NO_VAL, grp_jobs, "max/jobs/per/count", NULL),
 	add_parse(UINT32_NO_VAL, grp_jobs_accrue, "max/jobs/per/accruing", NULL),
 	add_parse(UINT32_NO_VAL, grp_submit_jobs, "max/jobs/per/submitted", NULL),
 	add_parse(TRES_STR, grp_tres, "max/tres/total", NULL),
-	add_parse(TRES_STR, max_tres_mins_pj, "max/tres/minutes/per/job", NULL),
+	add_skip(grp_tres_ctld),
 	add_parse(TRES_STR, grp_tres_mins, "max/tres/group/minutes", NULL),
 	add_skip(grp_tres_mins_ctld),
 	add_parse(TRES_STR, grp_tres_run_mins, "max/tres/group/active", NULL),
-	add_skip(id),
+	add_skip(grp_tres_run_mins_ctld),
+	add_parse(TRES_STR, max_tres_run_mins, "max/tres/minutes/total", NULL),
+	add_parse(UINT32_NO_VAL, grp_wall, "max/per/account/wall_clock", NULL),
+	add_parse(ASSOC_ID, id, "id", NULL),
 	add_parse(BOOL16, is_def, "is_default", NULL),
+	add_skip(leaf_usage),
+	add_skip(lft),
 	add_parse(STRING, lineage, "lineage", "Complete path up the hierarchy to the root association"),
 	add_parse(UINT32_NO_VAL, max_jobs, "max/jobs/active", NULL),
 	add_parse(UINT32_NO_VAL, max_jobs_accrue, "max/jobs/accruing", NULL),
 	add_parse(UINT32_NO_VAL, max_submit_jobs, "max/jobs/total", NULL),
+	add_parse(TRES_STR, max_tres_mins_pj, "max/tres/minutes/per/job", NULL),
 	add_skip(max_tres_mins_ctld),
-	add_parse(TRES_STR, max_tres_run_mins, "max/tres/minutes/total", NULL),
-	add_skip(grp_tres_run_mins_ctld),
-	add_parse(UINT32_NO_VAL, grp_wall, "max/per/account/wall_clock", NULL),
 	add_parse(TRES_STR, max_tres_pj, "max/tres/per/job", NULL),
 	add_skip(max_tres_ctld),
 	add_parse(TRES_STR, max_tres_pn, "max/tres/per/node", NULL),
@@ -6130,7 +6133,7 @@ static const parser_t PARSER_ARRAY(ASSOC)[] = {
 	add_skip(uid),
 	add_parse(ASSOC_USAGE_PTR, usage, "usage", NULL),
 	add_parse_req(STRING, user, "user", NULL),
-	add_skip(user_rec ),
+	add_skip(user_rec),
 };
 #undef add_parse
 #undef add_parse_req

doc/html/openapi_release_notes/slurm-23.11.0.html
@@ -80,6 +80,28 @@ The openapi/slurmdbd plugin forked from the openapi/dbv0.0.39 plugin.
 </tr>
 </table>
 
+<h4>Add fields to assocations</h4>
+<table class="tchange">
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">
+		.components.schemas."v0.0.40_assoc".properties.accounting
+	</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">
+		.components.schemas."v0.0.40_assoc".properties.id
+	</td>
+</tr>
+<tr class="trchange">
+	<td class="tdfield">Added</td>
+	<td class="tdchange">
+		.components.schemas."v0.0.40_assoc".properties.comment
+	</td>
+</tr>
+</table>
+
 <h3>openapi/dbv0.0.38 (src/plugins/openapi/dbv0.0.38/openapi.json)</h3>
 
 <h4>Deprecation notice</h4>

commit 2b1bde3a90814811a97da156c7dac0152bb4371b

data_parser/v0.0.40 - Avoid loading all assocations for ASSOC_ID
Avoid performance hit during DUMP or PARSE of DATA_PARSER_ASSOC_ID from
having NEED_ASSOC set for parser.

Performance regression from 1c265ec8a28. DATA_PARSER_ASSOC_ID has always
had NEED_ASSOC but only added DATA_PARSER_ASSOC by that commit.

Ticket: 21654
Changelog: Avoid expensive lookup of all associations when dumping or
  parsing for v0.0.40 endpoints.

src/plugins/data_parser/v0.0.40/parsers.c
@@ -1044,6 +1044,16 @@ static int DUMP_FUNC(QOS_PREEMPT_LIST)(const parser_t *const parser, void *obj,
 	return SLURM_SUCCESS;
 }
 
+/* Force loading of associations via NEED_ASSOC */
+static int _load_all_assocs(const parser_t *const parser, args_t *args)
+{
+	parser_t p = *parser;
+
+	p.needs |= NEED_ASSOC;
+
+	return load_prereqs(PARSING, &p, args);
+}
+
 static int PARSE_FUNC(ASSOC_ID)(const parser_t *const parser, void *obj,
 				data_t *src, args_t *args, data_t *parent_path)
 {
@@ -1068,6 +1078,19 @@ static int PARSE_FUNC(ASSOC_ID)(const parser_t *const parser, void *obj,
 		if ((rc = PARSE(ASSOC_SHORT, assoc_short, src, parent_path,
 				args)))
 			goto cleanup;
+
+		if (!args->assoc_list) {
+			/*
+			 * WARNING: This is a work around to always load the
+			 * associations when resolving an Association via
+			 * PARSE_FUNC(ASSOC_ID)() without having to rewrite the
+			 * association lookup code in slurmdb_helpers.[ch].
+			 */
+			int rc;
+
+			if ((rc = _load_all_assocs(parser, args)))
+				return rc;
+		}
 
 		if ((match = list_find_first(args->assoc_list,
 					     (ListFindF) compare_assoc,
@@ -9700,7 +9723,7 @@ static const parser_t parsers[] = {
 	addpsp(JOB_DESC_MSG_CRON_ENTRY, CRON_ENTRY_PTR, cron_entry_t *, NEED_NONE, "crontab entry"),
 
 	/* Complex type parsers */
-	addpcp(ASSOC_ID, ASSOC_SHORT, slurmdb_assoc_rec_t, NEED_ASSOC, "Association ID"),
+	addpcp(ASSOC_ID, ASSOC_SHORT, slurmdb_assoc_rec_t, NEED_NONE, "Association ID"),
 	addpcp(JOB_ASSOC_ID, ASSOC_SHORT_PTR, slurmdb_job_rec_t, NEED_ASSOC, NULL),
 	addpca(QOS_PREEMPT_LIST, STRING, slurmdb_qos_rec_t, NEED_QOS, NULL),
 	addpcp(STEP_NODES, HOSTLIST, slurmdb_step_rec_t, NEED_TRES, NULL),