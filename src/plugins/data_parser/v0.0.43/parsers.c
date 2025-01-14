SchedMD/slurm

commit 9652c493b319d571d935491aa117250252129663

data_parser/v0.0.43 - Avoid always loading associations for JOB_ASSOC_ID
Avoid performance hit during DUMP of DATA_PARSER_JOB_ASSOC_ID from
having NEED_ASSOC unless needed to search for the association.

Ticket: 21654

src/plugins/data_parser/v0.0.43/parsers.c
@@ -1408,10 +1408,17 @@ static int DUMP_FUNC(JOB_ASSOC_ID)(const parser_t *const parser, void *obj,
 
 	xassert(args->assoc_list);
 
-	if (!job->associd || (job->associd == NO_VAL) ||
-	    !(assoc = list_find_first(args->assoc_list,
-				      (ListFindF) compare_assoc,
-				      &assoc_key))) {
+	if (job->associd && (job->associd != NO_VAL)) {
+		int rc;
+
+		if ((rc = _load_all_assocs(parser, args)))
+			return rc;
+
+		assoc = list_find_first(args->assoc_list,
+					(ListFindF) compare_assoc, &assoc_key);
+	}
+
+	if (!assoc) {
 		/*
 		 * The association is either invalid or unknown or deleted.
 		 * Since this is coming from Slurm internally, issue a warning
@@ -1423,9 +1430,9 @@ static int DUMP_FUNC(JOB_ASSOC_ID)(const parser_t *const parser, void *obj,
 			job->associd);
 		data_set_dict(dst);
 		return SLURM_SUCCESS;
-	} else {
-		return DUMP(ASSOC_SHORT_PTR, assoc, dst, args);
 	}
+
+	return DUMP(ASSOC_SHORT_PTR, assoc, dst, args);
 }
 
 static void _fill_job_stp(job_std_pattern_t *job_stp, slurmdb_job_rec_t *job)
@@ -10190,7 +10197,7 @@ static const parser_t parsers[] = {
 	addpcp(JOB_STDIN, STRING, slurmdb_job_rec_t, NEED_NONE, NULL),
 	addpcp(JOB_STDOUT, STRING, slurmdb_job_rec_t, NEED_NONE, NULL),
 	addpcp(JOB_STDERR, STRING, slurmdb_job_rec_t, NEED_NONE, NULL),
-	addpcp(JOB_ASSOC_ID, ASSOC_SHORT_PTR, slurmdb_job_rec_t, NEED_ASSOC, NULL),
+	addpcp(JOB_ASSOC_ID, ASSOC_SHORT_PTR, slurmdb_job_rec_t, NEED_NONE, NULL),
 	addpca(QOS_PREEMPT_LIST, STRING, slurmdb_qos_rec_t, NEED_QOS, NULL),
 	addpcp(STEP_NODES, HOSTLIST, slurmdb_step_rec_t, NEED_TRES, NULL),
 	addpca(STEP_TRES_REQ_MAX, TRES, slurmdb_step_rec_t, NEED_TRES, NULL),

commit 8c07fde37fda682005fadf4068846c360ca63109

data_parser/v0.0.43 - Fix incorrect parser type
Need to use non-PTR version of parser for DUMP() to avoid treating the
contents of the association as a pointer.

Regression from 9652c493b3

Ticket: 21810

src/plugins/data_parser/v0.0.43/parsers.c
@@ -1317,7 +1317,7 @@ static int DUMP_FUNC(JOB_ASSOC_ID)(const parser_t *const parser, void *obj,
 		.id = job->associd,
 	};
 
-	return DUMP(ASSOC_SHORT_PTR, assoc, dst, args);
+	return DUMP(ASSOC_SHORT, assoc, dst, args);
 }
 
 static void _fill_job_stp(job_std_pattern_t *job_stp, slurmdb_job_rec_t *job)