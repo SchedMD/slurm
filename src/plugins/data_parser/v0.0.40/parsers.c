SchedMD/slurm

commit 820a7ff7f42e91cf63ff1cefdfc31ef7a7696f6a

data_parser/v0.0.40 - Avoid always loading associations for JOB_ASSOC_ID
Avoid performance hit during DUMP of DATA_PARSER_JOB_ASSOC_ID from
having NEED_ASSOC unless needed to search for the association.

Ticket: 21654

src/plugins/data_parser/v0.0.40/parsers.c
@@ -1166,10 +1166,17 @@ static int DUMP_FUNC(JOB_ASSOC_ID)(const parser_t *const parser, void *obj,
 
 	xassert(args->assoc_list);
 
-	if (!job->associd || (job->associd == NO_VAL) ||
-	    !(assoc = list_find_first(args->assoc_list,
-				      (ListFindF) compare_assoc,
-				      &assoc_key))) {
+	if (job->associd && (job->associd != NO_VAL)) {
+		int rc;
+
+		if ((rc = _load_all_assocs(parser, args)))
+			return rc;
+
+		assoc = list_find_first(args->assoc_list,
+					(ListFindF) compare_assoc, &assoc_key);
+	}
+
+	if (!assoc) {
 		/*
 		 * The association is either invalid or unknown or deleted.
 		 * Since this is coming from Slurm internally, issue a warning
@@ -1181,9 +1188,9 @@ static int DUMP_FUNC(JOB_ASSOC_ID)(const parser_t *const parser, void *obj,
 			job->associd);
 		data_set_dict(dst);
 		return SLURM_SUCCESS;
-	} else {
-		return DUMP(ASSOC_SHORT_PTR, assoc, dst, args);
 	}
+
+	return DUMP(ASSOC_SHORT_PTR, assoc, dst, args);
 }
 
 static int _foreach_resolve_tres_id(void *x, void *arg)
@@ -9724,7 +9731,7 @@ static const parser_t parsers[] = {
 
 	/* Complex type parsers */
 	addpcp(ASSOC_ID, ASSOC_SHORT, slurmdb_assoc_rec_t, NEED_NONE, "Association ID"),
-	addpcp(JOB_ASSOC_ID, ASSOC_SHORT_PTR, slurmdb_job_rec_t, NEED_ASSOC, NULL),
+	addpcp(JOB_ASSOC_ID, ASSOC_SHORT_PTR, slurmdb_job_rec_t, NEED_NONE, NULL),
 	addpca(QOS_PREEMPT_LIST, STRING, slurmdb_qos_rec_t, NEED_QOS, NULL),
 	addpcp(STEP_NODES, HOSTLIST, slurmdb_step_rec_t, NEED_TRES, NULL),
 	addpca(STEP_TRES_REQ_MAX, TRES, slurmdb_step_rec_t, NEED_TRES, NULL),

commit bb4d4067727e5688bd24923f46f283dfacb0061a

data_parser/v0.0.40 - Remove assert
args->assoc_list will only be populated after _load_all_assocs().

Regression from 820a7ff7f42

Ticket: 21810

src/plugins/data_parser/v0.0.40/parsers.c
@@ -1166,8 +1166,6 @@ static int DUMP_FUNC(JOB_ASSOC_ID)(const parser_t *const parser, void *obj,
 		.cluster = job->cluster, .id = job->associd
 	};
 
-	xassert(args->assoc_list);
-
 	if (job->associd && (job->associd != NO_VAL)) {
 		int rc;
 