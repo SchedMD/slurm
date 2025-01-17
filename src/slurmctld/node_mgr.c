9fa46825eeb4a2b94396d997deb186c568574cfb
Nathan Rini <nate@schedmd.com>
2025-01-17 12:56:24 -0700

    Fix include order
    Issue: 50113

--- src/slurmctld/node_mgr.c
+++ src/slurmctld/node_mgr.c
@@ -48,8 +48,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <time.h>
 
 #include "src/common/bitstring.h"
