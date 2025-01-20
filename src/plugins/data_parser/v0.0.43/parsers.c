f28743fd6f5bb0f471d0bc0625cb0f0bb25c5e65
Megan Dahl <megan@schedmd.com>
2025-01-20 15:01:23 -0700

    data_parser/v0.0.43 - Add segment_size to PARSER_ARRAY(JOB)[]
    Changelog: data_parser/v0.0.43 - Added field '.jobs[].segment_size' to the
     following endpoints:
     'GET /slurmdb/v0.0.43/jobs'
     'GET /slurmdb/v0.0.43/job/{job_id}'
    Issue: 50220

--- src/plugins/data_parser/v0.0.43/parsers.c
+++ src/plugins/data_parser/v0.0.43/parsers.c
@@ -7019,6 +7019,7 @@ static const parser_t PARSER_ARRAY(JOB)[] = {
 	add_parse(STRING, resv_name, "reservation/name", "Name of reservation to use"),
 	add_cparse(JOB_PLANNED_TIME, "time/planned", "Time required to start job after becoming eligible to run in seconds"),
 	add_parse(STRING, script, "script", "Job batch script; only the first component in a HetJob is populated or honored"),
+	add_parse(UINT16, segment_size, "segment_size", "Requested segment size"),
 	add_cparse(JOB_STDIN, "stdin_expanded", "Job stdin with expanded fields"),
 	add_cparse(JOB_STDOUT, "stdout_expanded", "Job stdout with expanded fields"),
 	add_cparse(JOB_STDERR, "stderr_expanded", "Job stderr with expanded fields"),
