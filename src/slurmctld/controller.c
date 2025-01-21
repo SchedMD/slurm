SchedMD/slurm

commit 7948c505ae7d3219a003e2d371c10b228a0a32be

Honor keep_msg in _service_connection().
Skip close()ing connection and freeing the msg when keep_msg is active for
a given msg_type.

src/slurmctld/controller.c
@@ -1660,11 +1660,13 @@ static void _service_connection(conmgr_callback_args_t conmgr_args,
 		slurm_send_rc_msg(msg, EINVAL);
 	}
 
-	if ((msg->conn_fd >= 0) && (close(msg->conn_fd) < 0))
-		error("close(%d): %m", msg->conn_fd);
+	if (this_rpc && !this_rpc->keep_msg) {
+		if ((msg->conn_fd >= 0) && (close(msg->conn_fd) < 0))
+			error("close(%d): %m", msg->conn_fd);
+		slurm_free_msg(msg);
+	}
 
 	server_thread_decr();
-	slurm_free_msg(msg);
 }
 
 /* Decrement slurmctld thread count (as applies to thread limit) */

commit 6c17888a231ec888da710bae1874efa27add6a59

Fix memory leak in _service_connection()
Avoid leaking file descriptors or msg for invalid msg_types for
slurmctld.

Regression from 7948c505ae.

Changelog: slurmctld - Fix memory or fd leak if an RPC is recieved that
 is not registered for processing.
Issue: 50113

src/slurmctld/controller.c
@@ -1730,7 +1730,7 @@ static void _service_connection(conmgr_callback_args_t conmgr_args,
 		slurm_send_rc_msg(msg, EINVAL);
 	}
 
-	if (this_rpc && !this_rpc->keep_msg) {
+	if (!this_rpc || !this_rpc->keep_msg) {
 		if ((msg->conn_fd >= 0) && (close(msg->conn_fd) < 0))
 			error("close(%d): %m", msg->conn_fd);
 		slurm_free_msg(msg);