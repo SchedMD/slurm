6d0cae7e978f1a168e505febf2567e48000a32ff
Felip Moll <felip.moll@schedmd.com>
2025-01-21 16:30:19 +0100

    slurmctld - Do not catch SIGTTIN
    This signal is delivered to a background process if it tries to read from
    the terminal. The default behavior for that signal is to stop the program.
    
    Ignoring this signal is not desired as slurmctld in the background trying
    to read from the console must be treated. Moreover it was not documented
    as being explicitly ignored by us.
    
    This was introduced in 74b6c5f5a97 and this commit reverts this change.
    
    Ticket: 21828

--- src/slurmctld/controller.c
+++ src/slurmctld/controller.c
@@ -417,11 +417,6 @@ static void _on_sigpipe(conmgr_callback_args_t conmgr_args, void *arg)
 	debug5("Caught SIGPIPE. Ignoring.");
 }
 
-static void _on_sigttin(conmgr_callback_args_t conmgr_args, void *arg)
-{
-	debug5("Caught SIGTTIN. Ignoring.");
-}
-
 static void _on_sigxcpu(conmgr_callback_args_t conmgr_args, void *arg)
 {
 	debug5("Caught SIGXCPU. Ignoring.");
@@ -446,7 +441,6 @@ static void _register_signal_handlers(conmgr_callback_args_t conmgr_args,
 	conmgr_add_work_signal(SIGUSR1, _on_sigusr1, NULL);
 	conmgr_add_work_signal(SIGUSR2, _on_sigusr2, NULL);
 	conmgr_add_work_signal(SIGPIPE, _on_sigpipe, NULL);
-	conmgr_add_work_signal(SIGTTIN, _on_sigttin, NULL);
 	conmgr_add_work_signal(SIGXCPU, _on_sigxcpu, NULL);
 	conmgr_add_work_signal(SIGABRT, _on_sigabrt, NULL);
 }
