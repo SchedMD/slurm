SchedMD/slurm

commit 079c83ced67d741240ad827fecd600f7093e4c93

Send x11_authority back to parent when joining the container.
Ticket 20844

src/slurmd/slurmstepd/mgr.c
@@ -1289,27 +1289,55 @@ static int _run_prolog_epilog(stepd_step_rec_t *step, bool is_epilog)
 	return rc;
 }
 
-static void _setup_x11_child(uint32_t jobid, stepd_step_rec_t *step)
+static void _setup_x11_child(int to_parent[2], uint32_t jobid,
+			     stepd_step_rec_t *step)
 {
+	uint32_t len = 0;
+
 	if (container_g_join(jobid, step->uid) != SLURM_SUCCESS)
 		_exit(1);
 
 	if (_set_xauthority(step) != SLURM_SUCCESS)
 		_exit(1);
 
+	len = strlen(step->x11_xauthority);
+	safe_write(to_parent[1], &len, sizeof(len));
+	safe_write(to_parent[1], step->x11_xauthority, len);
+
 	_exit(0);
+
+rwfail:
+	error("%s: failed to write to parent: %m", __func__);
+	_exit(1);
 }
 
-static int _setup_x11_parent(pid_t pid)
+static int _setup_x11_parent(int to_parent[2], pid_t pid, char **tmp)
 {
+	uint32_t len = 0;
 	int status = 0;
+
+	safe_read(to_parent[0], &len, sizeof(len));
+
+	if (len) {
+		*tmp = xcalloc(len, sizeof(char));
+		safe_read(to_parent[0], *tmp, len);
+	}
 
 	if ((waitpid(pid, &status, 0) != pid) || WEXITSTATUS(status)) {
 		error("%s: Xauthority setup failed", __func__);
+		xfree(*tmp);
 		return SLURM_ERROR;
 	}
 
 	return SLURM_SUCCESS;
+
+rwfail:
+	error("%s: failed to read from child: %m", __func__);
+	xfree(*tmp);
+	waitpid(pid, &status, 0);
+	debug2("%s: status from child %d", __func__, status);
+
+	return SLURM_ERROR;
 }
 
 static int _spawn_job_container(stepd_step_rec_t *step)
@@ -1369,6 +1397,13 @@ static int _spawn_job_container(stepd_step_rec_t *step)
 		 * in /tmp from inside the job.
 		 */
 		if (_need_join_container()) {
+			int to_parent[2] = {-1, -1};
+
+			if (pipe(to_parent) < 0) {
+				error("%s: pipe failed: %m", __func__);
+				rc = SLURM_ERROR;
+				goto x11_fail;
+			}
 			/*
 			 * The fork is necessary because we cannot join a
 			 * namespace if we are multithreaded. Also we need to
@@ -1378,17 +1413,24 @@ static int _spawn_job_container(stepd_step_rec_t *step)
 			 */
 			pid = fork();
 			if (pid == 0) {
-				_setup_x11_child(jobid, step);
+				_setup_x11_child(to_parent, jobid, step);
 			} else if (pid > 0) {
-				rc = _setup_x11_parent(pid);
+				char *tmp = NULL;
+				rc = _setup_x11_parent(to_parent, pid, &tmp);
+				if (tmp) {
+					xfree(step->x11_xauthority);
+					step->x11_xauthority = tmp;
+				}
 			} else {
 				error("fork: %m");
 				rc = SLURM_ERROR;
 			}
+			close(to_parent[0]);
+			close(to_parent[1]);
 		} else {
 			rc = _set_xauthority(step);
 		}
-
+x11_fail:
 		if (rc != SLURM_SUCCESS) {
 			set_job_state(step, SLURMSTEPD_STEP_ENDING);
 			close_slurmd_conn(rc);

commit 5b7dd4caf54313bfcd5386841c4671ed458954b5

Fix potential hang in extern step on errors from setting up x11.
This fixes a regression from commit 079c83ced6

Changelog: Fix a job potentially getting stuck in CG on permissions
 errors while setting up X11 forwarding.
Ticket: 17826

src/slurmd/slurmstepd/mgr.c
@@ -1292,11 +1292,16 @@ static void _setup_x11_child(int to_parent[2], stepd_step_rec_t *step)
 {
 	uint32_t len = 0;
 
-	if (container_g_join(&step->step_id, step->uid, false) != SLURM_SUCCESS)
+	if (container_g_join(&step->step_id, step->uid, false) !=
+	    SLURM_SUCCESS) {
+		safe_write(to_parent[1], &len, sizeof(len));
 		_exit(1);
+	}
 
-	if (_set_xauthority(step) != SLURM_SUCCESS)
+	if (_set_xauthority(step) != SLURM_SUCCESS) {
+		safe_write(to_parent[1], &len, sizeof(len));
 		_exit(1);
+	}
 
 	len = strlen(step->x11_xauthority);
 	safe_write(to_parent[1], &len, sizeof(len));