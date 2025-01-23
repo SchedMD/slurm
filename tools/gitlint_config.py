460ed7a16dd4ff89eec553dc3b98233d49dc4bea
Brian Christiansen <brian@schedmd.com>
2025-01-23 14:52:46 -0700

    Add gitlint configuration rule to ignore default rules
    While we transition, we want to only validate our own trailer rules.
    You can do ignore="all" but there isn't a way to add specific rules back
    in. This is easier than blacklisting each rule with ignore as well.
    
    Issue: slurm-qa/114

---
+++ tools/gitlint_config.py
@@ -0,0 +1,21 @@
+from gitlint.rules import ConfigurationRule
+
+
+class IgnoreConfigurationRule(ConfigurationRule):
+    """
+    This rules allows us to ignore all but the user defined rules for now.
+    """
+
+    # A rule MUST have a human friendly name
+    name = "ignore-default-configuration-rule"
+
+    # A rule MUST have a *unique* id
+    # We recommend starting with UCR (for User-defined Configuration-Rule)
+    id = "UCR1"
+
+    def apply(self, config, commit):
+        for rule in config.rules:
+            self.log.debug(f"rule id:{rule.id} name:{rule.name}")
+            if not rule.id.startswith("UC"):
+                self.log.debug(f"ignoring rule id:{rule.id} name:{rule.name}")
+                config.ignore.append(rule.name)
