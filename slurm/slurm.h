dad1e66a1ac0258ef590e4fed96d5f9ab415c94c
Felip Moll <felip.moll@schedmd.com>
2025-01-24 11:25:56 +0100

    Remove renamed and unused field from struct
    This field has been renamed in previous commits and is not used anymore.
    
    Ticket: 21052

--- slurm/slurm.h
+++ slurm/slurm.h
@@ -2288,7 +2288,6 @@ typedef struct {
 				 * string */
 	uint32_t task_dist;	/* see enum task_dist_state */
 	uint32_t time_limit;	/* step time limit */
-	char *tres_alloc_str;   /* tres used in the job */
 	char *tres_bind;	/* Task to TRES binding directives */
 	char *tres_fmt_alloc_str; /* formatted tres string for step */
 	char *tres_freq;	/* TRES frequency directives */
