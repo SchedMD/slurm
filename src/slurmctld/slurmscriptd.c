85d98ea69b170c05fbc0efc3f9cf5526bc29fc14
Marshall Garey <marshall@schedmd.com>
2025-02-03 16:05:05 -0700

    slurmscriptd - Catch and ignore the same signals ignored by slurmctld
    Continuation of commits c883105f5b8 and 74b6c5f5a9.
    
    A process signal mask is preserved after fork(). Before the conversion to
    conmgr for signal handling in commit 74b6c5f5a9, slurmscriptd inherited the
    signal mask from slurmctld. After that commit, slurmscriptd no longer
    ignored some signals.
    
    This was particularly problematic for SIGTSTP. If slurmctld runs in the
    foreground and receives a SIGTSTP, then slurmctld catches and ignores that
    signal. However, because slurmscriptd is in the same process group the shell
    also sends SIGTSTP to slurmscriptd, which does not catch or mask SIGTSTP and
    therefore is suspended. This results in a non-functional slurmscriptd, and
    therefore a largely non-functional slurmctld that cannot even shut down
    because it waits forever for slurmscriptd to respond that it is shutting
    down. This is fixed by catching and ignoring SIGTSTP.
    
    Commit c883105f5b8 attempted to revert behavior back to before commit
    74b6c5f5a9. This commit completes that work.
    
    Ticket: 21828

--- src/slurmctld/slurmscriptd.c
+++ src/slurmctld/slurmscriptd.c
@@ -1084,14 +1084,29 @@ static void _on_sigterm(conmgr_callback_args_t conmgr_args, void *arg)
 	log_flag(SCRIPT, "Caught SIGTERM. Ignoring.");
 }
 
+static void _on_sigchld(conmgr_callback_args_t conmgr_args, void *arg)
+{
+	log_flag(SCRIPT, "Caught SIGCHLD. Ignoring");
+}
+
 static void _on_sigquit(conmgr_callback_args_t conmgr_args, void *arg)
 {
 	log_flag(SCRIPT, "Caught SIGQUIT. Ignoring.");
 }
 
+static void _on_sigtstp(conmgr_callback_args_t conmgr_args, void *arg)
+{
+	log_flag(SCRIPT, "Caught SIGTSTP. Ignoring.");
+}
+
 static void _on_sighup(conmgr_callback_args_t conmgr_args, void *arg)
 {
 	log_flag(SCRIPT, "Caught SIGHUP. Ignoring.");
+}
+
+static void _on_sigusr1(conmgr_callback_args_t conmgr_args, void *arg)
+{
+	log_flag(SCRIPT, "Caught SIGUSR1. Ignoring.");
 }
 
 static void _on_sigusr2(conmgr_callback_args_t conmgr_args, void *arg)
@@ -1105,6 +1120,15 @@ static void _on_sigpipe(conmgr_callback_args_t conmgr_args, void *arg)
 	debug5("Caught SIGPIPE. Ignoring.");
 }
 
+static void _on_sigxcpu(conmgr_callback_args_t conmgr_args, void *arg)
+{
+	log_flag(SCRIPT, "Caught SIGXCPU. Ignoring.");
+}
+
+static void _on_sigabrt(conmgr_callback_args_t conmgr_args, void *arg)
+{
+	log_flag(SCRIPT, "Caught SIGABRT. Ignoring.");
+}
 
 static void _init_slurmscriptd_conmgr(void)
 {
@@ -1121,10 +1145,15 @@ static void _init_slurmscriptd_conmgr(void)
 	 */
 	conmgr_add_work_signal(SIGINT, _on_sigint, NULL);
 	conmgr_add_work_signal(SIGTERM, _on_sigterm, NULL);
+	conmgr_add_work_signal(SIGCHLD, _on_sigchld, NULL);
 	conmgr_add_work_signal(SIGQUIT, _on_sigquit, NULL);
+	conmgr_add_work_signal(SIGTSTP, _on_sigtstp, NULL);
 	conmgr_add_work_signal(SIGHUP, _on_sighup, NULL);
+	conmgr_add_work_signal(SIGUSR1, _on_sigusr1, NULL);
 	conmgr_add_work_signal(SIGUSR2, _on_sigusr2, NULL);
 	conmgr_add_work_signal(SIGPIPE, _on_sigpipe, NULL);
+	conmgr_add_work_signal(SIGXCPU, _on_sigxcpu, NULL);
+	conmgr_add_work_signal(SIGABRT, _on_sigabrt, NULL);
 
 	conmgr_run(false);
 }
