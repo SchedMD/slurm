SchedMD/slurm

commit ccff92007b264003a69302dd068ed9151a4cff28

Cleanup of lua functions
Consolidate wrappers around slurm_lua_loadscript. No functional change.

Bug 9345

src/lua/slurm_lua.c
@@ -630,14 +630,14 @@ extern void slurm_lua_stack_dump(const char *plugin, char *header, lua_State *L)
 #endif
 }
 
-extern lua_State *slurm_lua_loadscript(lua_State *curr, const char *plugin,
-				       const char *script_path,
-				       const char **req_fxns,
-				       time_t *load_time,
-				       void (*local_options)(lua_State *L))
+extern int slurm_lua_loadscript(lua_State **L, const char *plugin,
+				const char *script_path,
+				const char **req_fxns,
+				time_t *load_time,
+				void (*local_options)(lua_State *L))
 {
-
 	lua_State *new = NULL;
+	lua_State *curr = *L;
 	struct stat st;
 	int rc = 0;
 
@@ -645,17 +645,17 @@ extern lua_State *slurm_lua_loadscript(lua_State *curr, const char *plugin,
 		if (curr) {
 			(void) error("%s: Unable to stat %s, using old script: %s",
 			             plugin, script_path, strerror(errno));
-			return curr;
+			return SLURM_SUCCESS;
 		}
 		(void) error("%s: Unable to stat %s: %s",
 		             plugin, script_path, strerror(errno));
-		return NULL;
+		return SLURM_ERROR;
 	}
 
 	if (st.st_mtime <= *load_time) {
 		debug3("%s: %s: skipping loading Lua script: %s", plugin,
 		       __func__, script_path);
-		return curr;
+		return SLURM_SUCCESS;
 	}
 	debug3("%s: %s: loading Lua script: %s", __func__, plugin, script_path);
 
@@ -665,7 +665,7 @@ extern lua_State *slurm_lua_loadscript(lua_State *curr, const char *plugin,
 	if (!(new = luaL_newstate())) {
 		error("%s: %s: luaL_newstate() failed to allocate.",
 		      plugin, __func__);
-		return curr;
+		return SLURM_SUCCESS;
 	}
 
 	luaL_openlibs(new);
@@ -675,13 +675,13 @@ extern lua_State *slurm_lua_loadscript(lua_State *curr, const char *plugin,
 			      plugin, script_path,
 			      lua_tostring(new, -1));
 			lua_close(new);
-			return curr;
+			return SLURM_SUCCESS;
 		}
 		error("%s: %s: %s", plugin, script_path,
 		      lua_tostring(new, -1));
 		lua_pop(new, 1);
 		lua_close(new);
-		return NULL;
+		return SLURM_ERROR;
 	}
 
 	/*
@@ -704,13 +704,13 @@ extern lua_State *slurm_lua_loadscript(lua_State *curr, const char *plugin,
 			      plugin, script_path,
 			      lua_tostring(new, -1));
 			lua_close(new);
-			return curr;
+			return SLURM_SUCCESS;
 		}
 		error("%s: %s: %s", plugin, script_path,
 		      lua_tostring(new, -1));
 		lua_pop(new, 1);
 		lua_close(new);
-		return NULL;
+		return SLURM_ERROR;
 	}
 
 	/*
@@ -722,13 +722,13 @@ extern lua_State *slurm_lua_loadscript(lua_State *curr, const char *plugin,
 			(void) error("%s: %s: returned %d on load, using previous script",
 			             plugin, script_path, rc);
 			lua_close(new);
-			return curr;
+			return SLURM_SUCCESS;
 		}
 		(void) error("%s: %s: returned %d on load", plugin,
 			     script_path, rc);
 		lua_pop(new, 1);
 		lua_close(new);
-		return NULL;
+		return SLURM_ERROR;
 	}
 
 	/*
@@ -740,14 +740,17 @@ extern lua_State *slurm_lua_loadscript(lua_State *curr, const char *plugin,
 			(void) error("%s: %s: required function(s) not present, using previous script",
 			             plugin, script_path);
 			lua_close(new);
-			return curr;
+			return SLURM_SUCCESS;
 		}
 		lua_close(new);
-		return NULL;
+		return SLURM_ERROR;
 	}
 
 	*load_time = st.st_mtime;
-	return new;
+	if (curr)
+		lua_close(curr);
+	*L = new;
+	return SLURM_SUCCESS;
 }
 #endif
 

src/lua/slurm_lua.h
@@ -43,20 +43,22 @@
 #include <lauxlib.h>
 #include <lualib.h>
 #include "src/slurmctld/slurmctld.h"
+#include "slurm/slurm_errno.h"
 
 /* Generic stack dump function for debugging purposes */
 extern void slurm_lua_stack_dump(const char *plugin,
 				 char *header, lua_State *L);
 
 /*
- * function for create a new lua state object, loading a lua script, setting up
- * basic slurm/lua integration and returning the lua state to the caller.
- * If the script mtime is greater than *load_time, a new lua state will be
- * allocated and configured, the caller should close the old one after
- * completing any remaining setup.
+ * This function loads a new lua state object.
+ *
+ * The new lua state object will be initialized and assigned to *L depending
+ * on its mtime vs *load_time and whether the new script is succesfully loaded.
+ * If it cannot load *L won't be touched and SLURM_ERROR or SUCCESS will be
+ * returned depending on if *L was NULL or the old script can still be in use.
  *
  * Parameters:
- * curr (in)   - current lua state object, should be NULL on first call
+ * L (in/out)   - current lua state object, should be NULL on first call
  * plugin (in) - string identifying the calling plugin, e.g. "job_submit/lua"
  * script_path (in) - path to script file
  * req_fxns (in) - NULL terminated array of functions that must exist in the
@@ -64,18 +66,15 @@ extern void slurm_lua_stack_dump(const char *plugin,
  * load_time (in/out) - mtime of script from the curr lua state object
  *
  * Returns:
- * pointer to new lua_State object - the caller should complete setup of the
- *                                   new environment, and possibly free any
- *                                   existing.
- * NULL -- an error occured, the caller should continue using the current object
- * same value as curr - no error, or a strong suggestion that the caller should
- *                      continue using the same state obj, with no further setup
+ * SLURM_SUCCESS - if a correct Lua object is set.
+ * SLURM_ERROR - an error occured, and there's no lua state loaded.
  */
-extern lua_State *slurm_lua_loadscript(lua_State *curr, const char *plugin,
-				       const char *script_path,
-				       const char **req_fxns,
-				       time_t *load_time,
-				       void (*local_options)(lua_State *L));
+extern int slurm_lua_loadscript(lua_State **L, const char *plugin,
+				const char *script_path,
+				const char **req_fxns,
+				time_t *load_time,
+				void (*local_options)(lua_State *L));
+
 extern void slurm_lua_table_register(lua_State *L, const char *libname,
 				     const luaL_Reg *l);
 

src/plugins/cli_filter/lua/cli_filter_lua.c
@@ -89,13 +89,20 @@ static lua_State *L = NULL;
 static char **stored_data = NULL;
 static size_t stored_n = 0;
 static size_t stored_sz = 0;
-static time_t last_load_time = 0;
+static time_t lua_script_last_loaded = 0;
+
+static const char *req_fxns[] = {
+	"slurm_cli_setup_defaults",
+	"slurm_cli_pre_submit",
+	"slurm_cli_post_submit",
+	NULL
+};
 
 static int _lua_cli_json(lua_State *st);
 static int _lua_cli_json_env(lua_State *st);
 static int _store_data(lua_State *st);
 static int _retrieve_data(lua_State *st);
-static int _load_script(void);
+static void _loadscript_extra(lua_State *st);
 
 static const struct luaL_Reg slurm_functions[] = {
 	{ "json_cli_options",_lua_cli_json },
@@ -120,7 +127,9 @@ int init(void)
 	stored_data = xmalloc(sizeof(char *) * 24);
 	stored_sz = 24;
 
-        return _load_script();
+	return slurm_lua_loadscript(&L, "cli_filter/lua",
+				    lua_script_path, req_fxns,
+				    &lua_script_last_loaded, _loadscript_extra);
 }
 
 int fini(void)
@@ -348,36 +357,13 @@ static void _loadscript_extra(lua_State *st)
 	lua_setglobal(st, "slurm");
 }
 
-static int _load_script(void)
-{
-        lua_State *load = NULL;
-        time_t load_time = last_load_time;
-        const char *req_fxns[] = {
-		"slurm_cli_setup_defaults",
-		"slurm_cli_pre_submit",
-		"slurm_cli_post_submit",
-		NULL
-        };
-
-        load = slurm_lua_loadscript(L, "cli_filter/lua",
-				    lua_script_path, req_fxns, &load_time,
-				    _loadscript_extra);
-        if (!load)
-                return SLURM_ERROR;
-        if (load == L)
-                return SLURM_SUCCESS;
-
-        /* since complete finished error free, swap the states */
-        if (L)
-                lua_close(L);
-        L = load;
-	last_load_time = load_time;
-        return SLURM_SUCCESS;
-}
-
 extern int cli_filter_p_setup_defaults(slurm_opt_t *opt, bool early)
 {
-	int rc = _load_script();
+	int rc;
+
+	rc = slurm_lua_loadscript(&L, "cli_filter/lua",
+				  lua_script_path, req_fxns,
+				  &lua_script_last_loaded, _loadscript_extra);
 
 	if (rc != SLURM_SUCCESS)
 		goto out;
@@ -410,11 +396,14 @@ out:
 
 extern int cli_filter_p_pre_submit(slurm_opt_t *opt, int offset)
 {
-	int rc = _load_script();
+	int rc;
+
+	rc = slurm_lua_loadscript(&L, "cli_filter/lua",
+				  lua_script_path, req_fxns,
+				  &lua_script_last_loaded, _loadscript_extra);
 
 	if (rc != SLURM_SUCCESS)
 		goto out;
-
 	/*
 	 *  All lua script functions should have been verified during
 	 *   initialization:
@@ -451,7 +440,11 @@ out:
 extern void cli_filter_p_post_submit(
 	int offset, uint32_t jobid, uint32_t stepid)
 {
-	int rc = _load_script();
+	int rc;
+
+	rc = slurm_lua_loadscript(&L, "cli_filter/lua",
+				  lua_script_path, req_fxns,
+				  &lua_script_last_loaded, _loadscript_extra);
 
 	if (rc != SLURM_SUCCESS)
 		goto out;

src/plugins/job_submit/lua/job_submit_lua.c
@@ -98,10 +98,13 @@ static const char lua_script_path[] = DEFAULT_SCRIPT_DIR "/job_submit.lua";
 static time_t lua_script_last_loaded = (time_t) 0;
 static lua_State *L = NULL;
 static char *user_msg = NULL;
-
 time_t last_lua_jobs_update = (time_t) 0;
 time_t last_lua_resv_update = (time_t) 0;
-
+static const char *req_fxns[] = {
+	"slurm_job_submit",
+	"slurm_job_modify",
+	NULL
+};
 /*
  *  Mutex for protecting multi-threaded access to this plugin.
  *   (Only 1 thread at a time should be in here)
@@ -1223,32 +1226,6 @@ static void _loadscript_extra(lua_State *st)
 	_register_lua_slurm_struct_functions(st);
 }
 
-static int _load_script(void)
-{
-	lua_State *load = NULL;
-	time_t load_time = lua_script_last_loaded;
-	const char *req_fxns[] = {
-		"slurm_job_submit",
-		"slurm_job_modify",
-		NULL
-	};
-
-	load = slurm_lua_loadscript(L, "job_submit/lua",
-				    lua_script_path, req_fxns, &load_time,
-				    _loadscript_extra);
-	if (!load)
-		return SLURM_ERROR;
-	if (load == L)
-		return SLURM_SUCCESS;
-
-	/* since complete finished error free, swap the states */
-	if (L)
-		lua_close(L);
-	L = load;
-	lua_script_last_loaded = load_time;
-	return SLURM_SUCCESS;
-}
-
 /*
  *  NOTE: The init callback should never be called multiple times,
  *   let alone called from multiple threads. Therefore, locking
@@ -1261,7 +1238,10 @@ int init(void)
 	if ((rc = slurm_lua_init()) != SLURM_SUCCESS)
 		return rc;
 
-	return _load_script();
+	return slurm_lua_loadscript(&L, "job_submit/lua",
+				    lua_script_path, req_fxns,
+				    &lua_script_last_loaded,
+				    _loadscript_extra);
 }
 
 int fini(void)
@@ -1283,10 +1263,14 @@ int fini(void)
 extern int job_submit(job_desc_msg_t *job_desc, uint32_t submit_uid,
 		      char **err_msg)
 {
-	int rc = SLURM_ERROR;
+	int rc;
 	slurm_mutex_lock (&lua_lock);
 
-	if ((rc = _load_script()))
+	rc = slurm_lua_loadscript(&L, "job_submit/lua",
+				  lua_script_path, req_fxns,
+				  &lua_script_last_loaded, _loadscript_extra);
+
+	if (rc != SLURM_SUCCESS)
 		goto out;
 
 	/*
@@ -1333,10 +1317,14 @@ out:	slurm_mutex_unlock (&lua_lock);
 extern int job_modify(job_desc_msg_t *job_desc, job_record_t *job_ptr,
 		      uint32_t submit_uid)
 {
-	int rc = SLURM_ERROR;
+	int rc;
 	slurm_mutex_lock (&lua_lock);
 
-	if ((rc = _load_script()))
+	rc = slurm_lua_loadscript(&L, "job_submit/lua",
+				  lua_script_path, req_fxns,
+				  &lua_script_last_loaded, _loadscript_extra);
+
+	if (rc == SLURM_ERROR)
 		goto out;
 
 	/*

src/plugins/jobcomp/lua/jobcomp_lua.c
@@ -84,35 +84,16 @@ static int _job_rec_field_index(lua_State *L);
 static void _push_job_rec(job_record_t *job_ptr);
 static int _set_job_rec_field_index(lua_State *L);
 
+static const char *req_fxns[] = {
+	"slurm_jobcomp_log_record",
+	NULL
+};
+
 /*
  *  Mutex for protecting multi-threaded access to this plugin.
  *   (Only 1 thread at a time should be in here)
  */
 static pthread_mutex_t lua_lock = PTHREAD_MUTEX_INITIALIZER;
-
-static int _load_script(void)
-{
-	lua_State *load = NULL;
-	time_t load_time = lua_script_last_loaded;
-	const char *req_fxns[] = {
-		"slurm_jobcomp_log_record",
-		NULL
-	};
-
-	load = slurm_lua_loadscript(L, "jobcomp/lua", lua_script_path,
-				    req_fxns, &load_time, NULL);
-	if (!load)
-		return SLURM_ERROR;
-	if (load == L)
-		return SLURM_SUCCESS;
-
-	/* since complete finished error free, swap the states */
-	if (L)
-		lua_close(L);
-	L = load;
-	lua_script_last_loaded = load_time;
-	return SLURM_SUCCESS;
-}
 
 static void _push_job_rec(job_record_t *job_ptr)
 {
@@ -179,7 +160,9 @@ extern int init(void)
 		return rc;
 
 	slurm_mutex_lock(&lua_lock);
-	rc = _load_script();
+	rc = slurm_lua_loadscript(&L, "job_comp/lua",
+				  lua_script_path, req_fxns,
+				  &lua_script_last_loaded, NULL);
 	slurm_mutex_unlock(&lua_lock);
 
 	return rc;
@@ -210,17 +193,19 @@ extern int slurm_jobcomp_set_location(char * location)
 
 extern int slurm_jobcomp_log_record(job_record_t *job_ptr)
 {
-	int rc = SLURM_ERROR;
+	int rc;
 	slurm_mutex_lock(&lua_lock);
 
-	if ((rc = _load_script()))
+	rc = slurm_lua_loadscript(&L, "jobcomp/lua", lua_script_path,
+				  req_fxns, &lua_script_last_loaded, NULL);
+
+	if (rc != SLURM_SUCCESS)
 		goto out;
 
 	/*
 	 *  All lua script functions should have been verified during
 	 *   initialization:
 	 */
-
 	lua_getglobal(L, "slurm_jobcomp_log_record");
 	if (lua_isnil(L, -1))
 		goto out;

commit 4b5224579352eddb9a0c3543e1933ad9da129099

slurm_lua_loadscript() - Fix error handling for luaL_newstate()
Regression from commit ccff92007b2. Prior to that commit, if luaL_newstate()
failed the previous lua_State pointer was returned if it existed, or NULL if
not. Commit ccff92007b2 changed this to always return SLURM_SUCCESS if
luaL_newstate() failed.

Change the error handling to match the rest of this function: if a previous
lua_State exists, return success. Otherwise, return error.

Signed-off-by: Michael Norris <michael.norris@schedmd.com>
Ticket: 21911

src/lua/slurm_lua.c
@@ -740,9 +740,14 @@ extern int slurm_lua_loadscript(lua_State **L, const char *plugin,
 
 	/* Initialize lua */
 	if (!(new = luaL_newstate())) {
+		if (curr) {
+			error("%s: %s: luaL_newstate() failed to allocate, using previous script.",
+			      plugin, __func__);
+			return SLURM_SUCCESS;
+		}
 		error("%s: %s: luaL_newstate() failed to allocate.",
 		      plugin, __func__);
-		return SLURM_SUCCESS;
+		return SLURM_ERROR;
 	}
 
 	luaL_openlibs(new);