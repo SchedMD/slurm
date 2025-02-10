6bd3f9a819ab71e3fb7ed39aaeb1641a6339636a
Marcin Stolarek <cinek@schedmd.com>
2025-02-10 13:40:57 +0100

    _on_primary_msg - return SLURM_SUCCESS when rate limit exceeded
    In case of rate_limit_exceeded() we need to send
    SLURMCTLD_COMMUNICATIONS_BACKOFF to the client, but we shouldn't return
    an error back to wrap_on_data. The logging is handled internally by
    rate_limit_exceeded(), (with implementation of rl_log_freq=) and it's
    not an error from slurmctld perspective.
    
    Instead set rc to the value returned by slurm_send_rc_msg(), so we treat
    as error the case when we can't send the message back to the client.
    
    Ticket: 22009
    Changelog: Prevent redundant "on_data returned rc: Rate limit exceeded,
     please retry momentarily" error message from being printed in
     slurmctld logs.

--- src/slurmctld/controller.c
+++ src/slurmctld/controller.c
@@ -1538,8 +1538,7 @@ static int _on_primary_msg(conmgr_fd_t *con, slurm_msg_t *msg, void *arg)
 	 * to minimize controller disruption.
 	 */
 	if (rate_limit_exceeded(msg)) {
-		rc = SLURMCTLD_COMMUNICATIONS_BACKOFF;
-		slurm_send_rc_msg(msg, rc);
+		rc = slurm_send_rc_msg(msg, SLURMCTLD_COMMUNICATIONS_BACKOFF);
 		slurm_free_msg(msg);
 	} else if ((rc = conmgr_queue_extract_con_fd(
 			    con, _service_connection,
