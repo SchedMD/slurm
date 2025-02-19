042ace2fc5353e7562a5eacda498d3962a8d926e
Tim Wickberg <tim@schedmd.com>
2025-02-19 14:25:21 -0700

    Remove unnecessary include

--- src/sacct/sacct.c
+++ src/sacct/sacct.c
@@ -35,8 +35,6 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
-#include "src/interfaces/auth.h"
-
 #include "sacct.h"
 
 /*
