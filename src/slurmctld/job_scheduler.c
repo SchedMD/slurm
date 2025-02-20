SchedMD/slurm

commit da827d454931eebb0238753882925c6f133d6c4b

Fix considering "resuming" nodes in scheduling
Continuation of 324404deadb

Bug 6433

src/slurmctld/job_scheduler.c
@@ -1491,6 +1491,7 @@ static int _schedule(uint32_t job_limit)
 	failed_resv = xmalloc(sizeof(struct slurmctld_resv*) * MAX_FAILED_RESV);
 	save_avail_node_bitmap = bit_copy(avail_node_bitmap);
 	bit_and_not(avail_node_bitmap, booting_node_bitmap);
+	bit_or(avail_node_bitmap, rs_node_bitmap);
 
 	/* Avoid resource fragmentation if important */
 	if (reduce_completing_frag) {

src/plugins/sched/backfill/backfill.c
@@ -1399,7 +1399,11 @@ static int _attempt_backfill(void)
 	node_space[0].begin_time = sched_start;
 	window_end = sched_start + backfill_window;
 	node_space[0].end_time = window_end;
+
 	node_space[0].avail_bitmap = bit_copy(avail_node_bitmap);
+	/* Make "resuming" nodes available to be scheduled in backfill */
+	bit_or(node_space[0].avail_bitmap, rs_node_bitmap);
+
 	node_space[0].next = 0;
 	node_space_recs = 1;
 	if (debug_flags & DEBUG_FLAG_BACKFILL_MAP)

src/slurmctld/controller.c
@@ -1846,12 +1846,8 @@ static void _queue_reboot_msg(void)
 		node_ptr->node_state &=  NODE_STATE_FLAGS;
 		node_ptr->node_state |=  NODE_STATE_DOWN;
 
-		if (node_ptr->next_state != NODE_RESUME) {
-			bit_clear(avail_node_bitmap, i);
-			bit_clear(idle_node_bitmap, i);
-		} else {
-			bit_set(rs_node_bitmap, i);
-		}
+		bit_clear(avail_node_bitmap, i);
+		bit_clear(idle_node_bitmap, i);
 
 		node_ptr->boot_req_time = now;
 		node_ptr->last_response = now + slurm_get_resume_timeout();

src/slurmctld/job_mgr.c
@@ -4607,6 +4607,10 @@ static int _select_nodes_parts(struct job_record *job_ptr, bool test_only,
 	ListIterator iter;
 	int rc = ESLURM_REQUESTED_PART_CONFIG_UNAVAILABLE;
 	int best_rc = -1, part_limits_rc = WAIT_NO_REASON;
+	bitstr_t *save_avail_node_bitmap = NULL;
+
+	save_avail_node_bitmap = bit_copy(avail_node_bitmap);
+	bit_or(avail_node_bitmap, rs_node_bitmap);
 
 	if (job_ptr->part_ptr_list) {
 		list_sort(job_ptr->part_ptr_list, priority_sort_part_tier);
@@ -4727,6 +4731,10 @@ static int _select_nodes_parts(struct job_record *job_ptr, bool test_only,
 		job_ptr->state_reason = WAIT_POWER_RESERVED;
 	else if (rc == ESLURM_PARTITION_DOWN)
 		job_ptr->state_reason = WAIT_PART_DOWN;
+
+	FREE_NULL_BITMAP(avail_node_bitmap);
+	avail_node_bitmap = save_avail_node_bitmap;
+
 	return rc;
 }
 

src/slurmctld/node_scheduler.c
@@ -1965,6 +1965,7 @@ _pick_best_nodes(struct node_set *node_set_ptr, int node_set_size,
 			FREE_NULL_BITMAP(avail_bitmap);
 			avail_bitmap = bit_copy(job_ptr->details->
 						req_node_bitmap);
+			bit_and_not(avail_bitmap, rs_node_bitmap);
 		}
 		for (i = 0; i < node_set_size; i++) {
 			int count1 = 0, count2 = 0;
@@ -2003,8 +2004,6 @@ _pick_best_nodes(struct node_set *node_set_ptr, int node_set_size,
 						share_node_bitmap);
 					bit_and_not(node_set_ptr[i].my_bitmap,
 						    cg_node_bitmap);
-					bit_and_not(node_set_ptr[i].my_bitmap,
-						    rs_node_bitmap);
 				} else {
 					bit_and(node_set_ptr[i].my_bitmap,
 						idle_node_bitmap);
@@ -2013,9 +2012,11 @@ _pick_best_nodes(struct node_set *node_set_ptr, int node_set_size,
 			} else {
 				bit_and_not(node_set_ptr[i].my_bitmap,
 					    cg_node_bitmap);
-				bit_and_not(node_set_ptr[i].my_bitmap,
-					    rs_node_bitmap);
 			}
+
+			bit_and_not(node_set_ptr[i].my_bitmap,
+				    rs_node_bitmap);
+
 			if (!nodes_busy) {
 				count2 = bit_set_count(node_set_ptr[i].
 						       my_bitmap);
@@ -2282,6 +2283,10 @@ _pick_best_nodes(struct node_set *node_set_ptr, int node_set_size,
 		error_code = ESLURM_REQUESTED_NODE_CONFIG_UNAVAILABLE;
 	} else if (!runable_avail && !nodes_busy) {
 		error_code = ESLURM_NODE_NOT_AVAIL;
+	} else if (job_ptr->details->req_node_bitmap &&
+		   bit_overlap(job_ptr->details->req_node_bitmap,
+			       rs_node_bitmap)) {
+		error_code = ESLURM_NODES_BUSY;
 	} else if (!preempt_flag && job_ptr->details->req_node_bitmap) {
 		/* specific nodes required */
 		if (shared) {
@@ -2293,20 +2298,14 @@ _pick_best_nodes(struct node_set *node_set_ptr, int node_set_size,
 					cg_node_bitmap)) {
 				error_code = ESLURM_NODES_BUSY;
 			}
-			if (bit_overlap(job_ptr->details->req_node_bitmap,
-					rs_node_bitmap)) {
-				error_code = ESLURM_NODES_BUSY;
-			}
 		} else if (!bit_super_set(job_ptr->details->req_node_bitmap,
 					  idle_node_bitmap)) {
 			error_code = ESLURM_NODES_BUSY;
 			/* Note: IDLE nodes are not COMPLETING */
 		}
 	} else if (job_ptr->details->req_node_bitmap &&
-		   (bit_overlap(job_ptr->details->req_node_bitmap,
-			       cg_node_bitmap) ||
-		    bit_overlap(job_ptr->details->req_node_bitmap,
-			       rs_node_bitmap))) {
+		   bit_overlap(job_ptr->details->req_node_bitmap,
+			       cg_node_bitmap)) {
 		error_code = ESLURM_NODES_BUSY;
 	}
 

src/slurmctld/proc_req.c
@@ -6076,6 +6076,9 @@ inline static void _slurm_rpc_reboot_nodes(slurm_msg_t * msg)
 		node_ptr->last_response = now + slurmctld_conf.resume_timeout;
 		if (reboot_msg) {
 			node_ptr->next_state = reboot_msg->next_state;
+			if (node_ptr->next_state == NODE_RESUME)
+				bit_set(rs_node_bitmap, i);
+
 			if (reboot_msg->reason) {
 				xfree(node_ptr->reason);
 				node_ptr->reason = xstrdup(reboot_msg->reason);

src/slurmctld/read_config.c
@@ -431,7 +431,10 @@ static int _build_bitmaps(void)
 			bit_set(booting_node_bitmap, i);
 		if (IS_NODE_COMPLETING(node_ptr))
 			bit_set(cg_node_bitmap, i);
-		if (IS_NODE_IDLE(node_ptr) || IS_NODE_ALLOCATED(node_ptr)) {
+		if (IS_NODE_IDLE(node_ptr) ||
+		    IS_NODE_ALLOCATED(node_ptr) ||
+		    (IS_NODE_REBOOT(node_ptr) &&
+		     (node_ptr->next_state == NODE_RESUME))) {
 			if ((drain_flag == 0) &&
 			    (!IS_NODE_NO_RESPOND(node_ptr)))
 				make_node_avail(i);
@@ -441,6 +444,10 @@ static int _build_bitmaps(void)
 			bit_set(power_node_bitmap, i);
 		if (IS_NODE_FUTURE(node_ptr))
 			bit_set(future_node_bitmap, i);
+
+		if (IS_NODE_REBOOT(node_ptr) &&
+		    (node_ptr->next_state == NODE_RESUME))
+			bit_set(rs_node_bitmap, i);
 	}
 
 	return error_code;

commit f9f1b4f3090b16fa2f1f3406da010c77bde73055

Fix not removing a node from rs_node_bitmap when it is being rebooted
This caused jobs to be scheduled and started R while the node was
rebooting. This happened when the flag nextstate=resume was used.

The rs_node_bitmap was or'ed with avail_node_bitmap in _schedule()
allowing the main scheduler to schedule on the resuming nodes. We need
to not schedule on the resuming nodes and just adjust the message.

This bug was introduced in commit da827d45493.

Changelog: Do not allow new jobs to start on a node which is being rebooted
 with the flag nextstate=resume.
Ticket: 18087

src/slurmctld/job_scheduler.c
@@ -1496,7 +1496,6 @@ static int _schedule(bool full_queue)
 	(void) list_for_each(resv_list, _foreach_setup_resv_sched, NULL);
 
 	save_avail_node_bitmap = bit_copy(avail_node_bitmap);
-	bit_or(avail_node_bitmap, rs_node_bitmap);
 
 	/* Avoid resource fragmentation if important */
 	if (reduce_completing_frag) {
@@ -1807,7 +1806,11 @@ next_task:
 			 */
 			job_ptr->state_reason = WAIT_RESOURCES;
 			xfree(job_ptr->state_desc);
-			job_ptr->state_desc = xstrdup("Nodes required for job are DOWN, DRAINED or reserved for jobs in higher priority partitions");
+			job_ptr->state_desc =
+				xstrdup_printf("Nodes required for job are DOWN, DRAINED%s or reserved for jobs in higher priority partitions",
+					       bit_overlap(rs_node_bitmap,
+							   job_ptr->part_ptr->
+							   node_bitmap) ? ", REBOOTING" : "");
 			last_job_update = now;
 			sched_debug3("%pJ. State=%s. Reason=%s. Priority=%u. Partition=%s.",
 				     job_ptr,