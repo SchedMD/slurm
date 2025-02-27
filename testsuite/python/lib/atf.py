234b3bc36e2cedb028129660bbabbb303a1b5646
Albert Gil <albert.gil@schedmd.com>
2025-02-27 19:14:24 +0100

    Testsuite - Minor logging improvement
    Cherry-picked: d9646979c2

--- testsuite/python/lib/atf.py
+++ testsuite/python/lib/atf.py
@@ -535,6 +535,8 @@ def start_slurmctld(clean=False, quiet=False):
     if not properties["auto-config"]:
         require_auto_config("wants to start slurmctld")
 
+    logging.debug("Starting slurmctld...")
+
     if not is_slurmctld_running(quiet=quiet):
         # Start slurmctld
         command = f"{properties['slurm-sbin-dir']}/slurmctld"
@@ -550,7 +552,9 @@ def start_slurmctld(clean=False, quiet=False):
         if not repeat_command_until(
             "scontrol ping", lambda results: re.search(r"is UP", results["stdout"])
         ):
-            pytest.fail(f"Slurmctld is not running")
+            pytest.fail("Slurmctld is not running")
+        else:
+            logging.debug("Slurmctldd started successfully")
 
 
 def start_slurmdbd(clean=False, quiet=False):
