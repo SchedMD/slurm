70cc5c3088a94e56b793789f2412a671b0257aa2
Nathan Rini <nate@schedmd.com>
2025-03-11 13:04:46 -0600

    Promote debug() in slurm_recv_timeout()
    Promote debug() on recv()=0 in slurm_recv_timeout() to log_flag(NET)
    
    Ticket: 22312
    Issue: 50319
    Changelog: Improve debug log message in slurm_recv_timeout() when EOF
      encountered from remote socket.

--- src/common/slurm_protocol_socket.c
+++ src/common/slurm_protocol_socket.c
@@ -460,8 +460,8 @@ extern int slurm_recv_timeout(int fd, char *buffer, size_t size, int timeout)
 			}
 		}
 		if (rc == 0) {
-			debug("%s at %d of %zu, recv zero bytes",
-			      __func__, recvlen, size);
+			log_flag(NET, "%s: recv(fd:%d)=0 at %d/%zu bytes",
+				 __func__, fd, recvlen, size);
 			errno = SLURM_PROTOCOL_SOCKET_ZERO_BYTES_SENT;
 			recvlen = SLURM_ERROR;
 			goto done;
