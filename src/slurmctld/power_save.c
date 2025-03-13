SchedMD/slurm

commit 1d212fe277066c70aedb1ce76873f79588cc0c89

Don't print error w/ResumeProgram if slurmctld not compiled with JSON-c
If Slurm is built without JSON-c, attempts to create job-node mapping in
JSON for the ResumeProgram's SLURM_RESUME_FILE will always fail and
print an error every time ResumeProgram is run. The job-node mapping
isn't always necessary, so avoid printing the error message.

Ticket: 22086
Changelog: Avoid repeated "failed to generate json for resume job/node
 list" error with ResumeProgram when slurmctld was built without JSON-c.

src/slurmctld/power_save.c
@@ -744,7 +744,8 @@ static void _do_power_work(time_t now)
 				nodes);
 		if (serialize_g_data_to_string(&json, NULL, resume_json_data,
 					       MIME_TYPE_JSON,
-					       SER_FLAGS_COMPACT))
+					       SER_FLAGS_COMPACT) !=
+		    ESLURM_DATA_UNKNOWN_MIME_TYPE)
 			error("failed to generate json for resume job/node list");
 
 		if (nodes)

commit d3e86c8989ff19aa0deb20131a097aa3ce55ddb0

Fix bad checking of error code
Regression from 1d212fe277, which I caused by my changes and not testing
with a valid serializer.

Ticket: 22086

src/slurmctld/power_save.c
@@ -736,16 +736,18 @@ static void _do_power_work(time_t now)
 	}
 
 	if (wake_node_bitmap) {
+		int rc;
 		char *nodes, *json = NULL;
 		nodes = bitmap2node_name(wake_node_bitmap);
 
 		data_set_string(data_key_set(resume_json_data,
 					     "all_nodes_resume"),
 				nodes);
-		if (serialize_g_data_to_string(&json, NULL, resume_json_data,
-					       MIME_TYPE_JSON,
-					       SER_FLAGS_COMPACT) !=
-		    ESLURM_DATA_UNKNOWN_MIME_TYPE)
+		rc = serialize_g_data_to_string(&json, NULL, resume_json_data,
+						MIME_TYPE_JSON,
+						SER_FLAGS_COMPACT);
+		if ((rc != SLURM_SUCCESS) &&
+		    (rc != ESLURM_DATA_UNKNOWN_MIME_TYPE))
 			error("failed to generate json for resume job/node list");
 
 		if (nodes)