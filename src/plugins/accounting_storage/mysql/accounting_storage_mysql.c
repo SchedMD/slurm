8b8bd1f2f1a55cf807033104852eb127b9425916
Danny Auble <da@schedmd.com>
2025-03-21 11:00:30 -0600

    Combine like functions
    After 0ed4d763ba _check_jobs_before_remove_assoc() and
    _check_jobs_before_remove() are no longer different.
    
    fb9eaa1de3 did cause unneeded divergence. This combines the 2 functions
    into the same function.
    
    Ticket: 20854

--- src/plugins/accounting_storage/mysql/accounting_storage_mysql.c
+++ src/plugins/accounting_storage/mysql/accounting_storage_mysql.c
@@ -417,76 +417,6 @@ static bool _check_jobs_before_remove(mysql_conn_t *mysql_conn,
 			(*already_flushed) = 1;
 			reset_mysql_conn(mysql_conn);
 		}
-		if (ret_list)
-			_process_running_jobs_result(cluster_name, result,
-						     ret_list);
-	}
-
-	mysql_free_result(result);
-	return rc;
-}
-
-/* Same as above but for associations instead of other tables */
-static bool _check_jobs_before_remove_assoc(mysql_conn_t *mysql_conn,
-					    char *cluster_name,
-					    char *assoc_char,
-					    list_t *ret_list,
-					    bool *already_flushed)
-{
-	char *query = NULL, *object = NULL;
-	bool rc = 0;
-	int i;
-	MYSQL_RES *result = NULL;
-
-	/* if this changes you will need to edit the corresponding
-	 * enum above in the global settings */
-	static char *jassoc_req_inx[] = {
-		"t1.id_job",
-		"t2.acct",
-		"t2.user",
-		"t2.partition"
-	};
-
-	if (ret_list) {
-		xstrcat(object, jassoc_req_inx[0]);
-		for(i=1; i<JASSOC_COUNT; i++)
-			xstrfmtcat(object, ", %s", jassoc_req_inx[i]);
-
-		query = xstrdup_printf("select %s "
-				       "from \"%s_%s\" as t1, \"%s_%s\" as t2 "
-				       "where (%s) and t1.id_assoc=t2.id_assoc "
-				       "and t1.time_end=0 && t1.state<%d;",
-				       object, cluster_name, job_table,
-				       cluster_name, assoc_table,
-				       assoc_char, JOB_COMPLETE);
-		xfree(object);
-	} else {
-		query = xstrdup_printf(
-			"select t1.id_assoc from \"%s_%s\" as t1, "
-			"\"%s_%s\" as t2 where (%s) "
-			"and t1.id_assoc=t2.id_assoc limit 1;",
-			cluster_name, job_table,
-			cluster_name, assoc_table,
-			assoc_char);
-	}
-
-	DB_DEBUG(DB_ASSOC, mysql_conn->conn, "query\n%s", query);
-
-	if (!(result = mysql_db_query_ret(
-		      mysql_conn, query, 0))) {
-		xfree(query);
-		return rc;
-	}
-	xfree(query);
-
-	if (mysql_num_rows(result)) {
-		debug4("We have jobs for this combo");
-		rc = true;
-		if (ret_list && !(*already_flushed)) {
-			list_flush(ret_list);
-			(*already_flushed) = 1;
-			reset_mysql_conn(mysql_conn);
-		}
 	}
 
 	if (ret_list)
@@ -2343,13 +2273,13 @@ extern int remove_common(mysql_conn_t *mysql_conn,
 		}
 	} else {
 		/* first check to see if we are running jobs now */
-		if (_check_jobs_before_remove_assoc(
+		if (_check_jobs_before_remove(
 			    mysql_conn, cluster_name, name_char,
 			    ret_list, jobs_running) || (*jobs_running))
 			return SLURM_SUCCESS;
 
 		/* now check to see if any jobs were ever run. */
-		has_jobs = _check_jobs_before_remove_assoc(
+		has_jobs = _check_jobs_before_remove(
 			mysql_conn, cluster_name, name_char,
 			NULL, NULL);
 	}
