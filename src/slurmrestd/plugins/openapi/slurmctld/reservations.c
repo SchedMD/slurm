75723399de0dddd6477bb86d92ae799b0a9586dc
Megan Dahl <megan@schedmd.com>
2025-03-21 19:52:29 -0600

    Unify create and update reservation endpoints
    Change the following endpoints:
    'POST /slurm/v0.0.43/reservation/{reservation_name}'
    'POST /slurm/v0.0.43/reservations/create'
    'POST /slurm/v0.0.43/reservations/update'
    So that they are:
    'POST /slurm/v0.0.43/reservation'
    'POST /slurm/v0.0.43/reservations'
    and each endpoint allows you to create and update reservations depending on
    if the specified reservation name already exists.
    
    Issue: 50233
    Changelog: slurmrestd - The following endpoints were removed:
     'POST /slurm/v0.0.43/reservation/{reservation_name}'
     'POST /slurm/v0.0.43/reservations/create'
     'POST /slurm/v0.0.43/reservations/update'
    Changelog: slurmrestd - The following endpoints were added:
     'POST /slurm/v0.0.43/reservation'
     'POST /slurm/v0.0.43/reservations'

--- src/slurmrestd/plugins/openapi/slurmctld/reservations.c
+++ src/slurmrestd/plugins/openapi/slurmctld/reservations.c
@@ -45,6 +45,83 @@
 
 #include "api.h"
 
+typedef struct {
+	openapi_ctxt_t *ctxt;
+	reserve_info_msg_t *res_info_ptr;
+} foreach_mod_resvs_args_t;
+
+/* validate resv create resv_desc_msg */
+static int _validate_each_resv_create_desc(resv_desc_msg_t *resv_msg,
+					   openapi_ctxt_t *ctxt)
+{
+	char *error_msg;
+
+	if (validate_resv_create_desc(resv_msg, &error_msg))
+		return resp_error(ctxt, ESLURM_RESERVATION_INVALID,
+				  "validate_resv_create_desc", "%s", error_msg);
+
+	return SLURM_SUCCESS;
+}
+
+/* Create reservation from desc */
+static int _create_resv(resv_desc_msg_t *resv_msg, openapi_ctxt_t *ctxt)
+{
+	char *new_res_name = NULL;
+	int rc = SLURM_SUCCESS;
+
+	if (!(new_res_name = slurm_create_reservation(resv_msg))) {
+		rc = errno;
+		if (((rc == ESLURM_REQUESTED_NODE_CONFIG_UNAVAILABLE) ||
+		     (rc == ESLURM_NODES_BUSY)) && !resv_msg->node_list) {
+			resp_error(ctxt, rc, "slurm_create_reservation",
+				   "Error creating reservation %s. Note, unless nodes are directly requested a reservation must exist in a single partition. If no partition is requested the default partition is assumed.",
+				   resv_msg->name);
+		} else {
+			resp_error(ctxt, rc, "slurm_create_reservation",
+				   "Error creating reservation  %s",
+				   resv_msg->name);
+		}
+		return rc;
+	}
+
+	free(new_res_name);
+	return rc;
+}
+
+/* Update reservation from desc */
+static int _update_resv(resv_desc_msg_t *resv_msg, openapi_ctxt_t *ctxt)
+{
+	int rc;
+
+	if ((rc = slurm_update_reservation(resv_msg))) {
+		if (errno)
+			rc = errno;
+		resp_error(ctxt, rc, "slurm_update_reservation",
+			   "Error updating reservation %s", resv_msg->name);
+	}
+
+	return rc;
+}
+
+static int _load_reservations(reserve_info_msg_t **res_info_ptr,
+			      openapi_ctxt_t *ctxt)
+{
+	int rc = SLURM_SUCCESS;
+
+	xassert(res_info_ptr && !*res_info_ptr);
+
+	errno = 0;
+	if ((rc = slurm_load_reservations(0, res_info_ptr)) || !*res_info_ptr) {
+		if (rc == SLURM_ERROR && errno)
+			rc = errno;
+
+		resp_error(ctxt, rc, "slurm_load_reservations()",
+			   "Unable to query reservations");
+	}
+
+	return rc;
+}
+
 static int _set_unused_flag(void *x, void *args)
 {
 	resv_desc_msg_t *resv_msg = x;
@@ -90,72 +167,77 @@ static int _parse_resv_desc_list(openapi_ctxt_t *ctxt,
 	return rc;
 }
 
-/*
- * Create or update reservations
- * IN: validate_func - function for use in list_find_first() to validate
- *		       list of parsed resv_desc_msg_t *
- * IN: mod_func - function for use in list_find_first() to send the requests to
- *		  the controller
- */
-static int _mod_reservations(openapi_ctxt_t *ctxt, ListFindF validate_func,
-			     ListFindF mod_func)
+/* return true on error else false - meant for use in list_find_first */
+static int _check_resv_name(void *x, void *args)
+{
+	resv_desc_msg_t *resv_msg = x;
+	openapi_ctxt_t *ctxt = args;
+
+	if (resv_msg->name == NULL) {
+		resp_error(ctxt, ESLURM_RESERVATION_INVALID, __func__,
+			   "Reservation must be given");
+		return true;
+	}
+	return false;
+}
+
+static bool _does_resv_exist(char *resv_name, reserve_info_msg_t *res_info_ptr)
+{
+	for (int i = 0; i < res_info_ptr->record_count; i++) {
+		const char *name = res_info_ptr->reservation_array[i].name;
+		if (!xstrcasecmp(resv_name, name))
+			return true;
+	}
+	return false;
+}
+
+static int _create_or_update_each_resv(void *x, void *arg)
+{
+	resv_desc_msg_t *resv_msg = x;
+	foreach_mod_resvs_args_t *args = arg;
+	int rc = SLURM_SUCCESS;
+
+	/* Check if the resv already exists - if so update it else create it */
+	if (_does_resv_exist(resv_msg->name, args->res_info_ptr))
+		rc = _update_resv(resv_msg, args->ctxt);
+	else if (!(rc = _validate_each_resv_create_desc(resv_msg, args->ctxt)))
+		rc = _create_resv(resv_msg, args->ctxt);
+
+	rc = rc ? rc : args->ctxt->rc;
+	args->ctxt->rc = args->ctxt->rc ? args->ctxt->rc : rc;
+
+	return rc;
+}
+
+/* Create or update reservations */
+static int _mod_reservations(openapi_ctxt_t *ctxt)
 {
 	openapi_reservation_mod_request_t resv_req = { 0 };
 	int rc = SLURM_SUCCESS;
+	foreach_mod_resvs_args_t args = {
+		.ctxt = ctxt,
+	};
 
 	if ((rc = _parse_resv_desc_list(ctxt, &resv_req)))
 		return rc;
 
-	if (!list_find_first(resv_req.reservations, validate_func, ctxt) &&
-	    !list_find_first(resv_req.reservations, mod_func, ctxt)) {
+	if (list_find_first(resv_req.reservations, _check_resv_name, ctxt)) {
+		FREE_NULL_LIST(resv_req.reservations);
+		return ctxt->rc;
+	}
+
+	if (!(rc = _load_reservations(&args.res_info_ptr, ctxt)))
+		list_for_each(resv_req.reservations,
+			      _create_or_update_each_resv, &args);
+
+	if (!rc && !ctxt->rc) {
 		DUMP_OPENAPI_RESP_SINGLE(OPENAPI_RESERVATION_MOD_RESP,
 					 resv_req.reservations, ctxt);
 	}
 
+	slurm_free_reservation_info_msg(args.res_info_ptr);
 	FREE_NULL_LIST(resv_req.reservations);
-	return ctxt->rc;
-}
-
-/* return true on error else false, meant for use in list_find_first() */
-static int _validate_each_resv_desc_msg(void *x, void *args)
-{
-	resv_desc_msg_t *resv_msg = x;
-	openapi_ctxt_t *ctxt = args;
-	char *error_msg;
-
-	if (validate_resv_create_desc(resv_msg, &error_msg)) {
-		resp_error(ctxt, ESLURM_RESERVATION_INVALID,
-			   "validate_resv_create_desc", "%s", error_msg);
-		return true;
-	}
-
-	return false;
-}
-
-/* Create reservation from desc - return true on error, list_find_first func */
-static int _create_each_resv(void *x, void *args)
-{
-	resv_desc_msg_t *resv_msg = x;
-	openapi_ctxt_t *ctxt = args;
-	char *new_res_name = NULL;
-
-	if (!(new_res_name = slurm_create_reservation(resv_msg))) {
-		int rc = errno;
-		if (((rc == ESLURM_REQUESTED_NODE_CONFIG_UNAVAILABLE) ||
-		     (rc == ESLURM_NODES_BUSY)) && !resv_msg->node_list) {
-			resp_error(ctxt, rc, "slurm_create_reservation",
-				   "Error creating reservation %s. Note, unless nodes are directly requested a reservation must exist in a single partition. If no partition is requested the default partition is assumed.",
-				   resv_msg->name);
-		} else {
-			resp_error(ctxt, rc, "slurm_create_reservation",
-				   "Error creating reservation  %s",
-				   resv_msg->name);
-		}
-		return true;
-	}
-
-	free(new_res_name);
-	return false;
+	return rc ? rc : ctxt->rc;
 }
 
 static int _get_reservations(openapi_ctxt_t *ctxt)
@@ -202,8 +284,7 @@ extern int op_handler_reservations(openapi_ctxt_t *ctxt)
 	if (ctxt->method == HTTP_REQUEST_GET) {
 		rc = _get_reservations(ctxt);
 	} else if (ctxt->method == HTTP_REQUEST_POST) {
-		rc = _mod_reservations(ctxt, _validate_each_resv_desc_msg,
-				       _create_each_resv);
+		rc = _mod_reservations(ctxt);
 	} else {
 		resp_error(ctxt, ESLURM_REST_INVALID_QUERY, __func__,
 			   "Unsupported HTTP method requested: %s",
@@ -211,48 +292,6 @@ extern int op_handler_reservations(openapi_ctxt_t *ctxt)
 	}
 
 	return rc;
-}
-
-/* return true on error else false */
-static int _check_resv_name(void *x, void *args)
-{
-	resv_desc_msg_t *resv_msg = x;
-	openapi_ctxt_t *ctxt = args;
-
-	if (resv_msg->name == NULL) {
-		resp_error(ctxt, ESLURM_RESERVATION_INVALID, __func__,
-			   "Reservation must be given.  No reservation update.");
-		return true;
-	}
-	return false;
-}
-
-/* Update reservation from desc - return true on error, list_find_first func */
-static int _update_each_resv(void *x, void *args)
-{
-	resv_desc_msg_t *resv_msg = x;
-	openapi_ctxt_t *ctxt = args;
-	int rc;
-
-	if ((rc = slurm_update_reservation(resv_msg))) {
-		if (errno)
-			rc = errno;
-		resp_error(ctxt, rc, "slurm_update_reservation",
-			   "Error updating reservation %s", resv_msg->name);
-		return true;
-	}
-
-	return false;
-}
-
-extern int op_handler_reservations_update(openapi_ctxt_t *ctxt)
-{
-	if (ctxt->method != HTTP_REQUEST_POST)
-		return resp_error(ctxt, ESLURM_REST_INVALID_QUERY, __func__,
-				  "Unsupported HTTP method requested: %s",
-				  get_http_method_string(ctxt->method));
-
-	return _mod_reservations(ctxt, _check_resv_name, _update_each_resv);
 }
 
 static int _parse_resv_name_param(openapi_ctxt_t *ctxt,
@@ -362,42 +401,45 @@ static int _parse_resv_desc(openapi_ctxt_t *ctxt, resv_desc_msg_t *resv_msg)
 	return rc;
 }
 
-extern int _reservation_update(openapi_ctxt_t *ctxt)
+/* Create or update reservations */
+static int _mod_reservation(openapi_ctxt_t *ctxt)
 {
-	openapi_reservation_param_t params = { 0 };
 	resv_desc_msg_t resv_msg = { 0 };
 	int rc = SLURM_SUCCESS;
+	foreach_mod_resvs_args_t args = {
+		.ctxt = ctxt,
+	};
 
 	if (ctxt->method != HTTP_REQUEST_POST)
 		return resp_error(ctxt, ESLURM_REST_INVALID_QUERY, __func__,
 				  "Unsupported HTTP method requested: %s",
 				  get_http_method_string(ctxt->method));
 
-	if ((rc = _parse_resv_name_param(ctxt, &params)) ||
-	    (rc = _parse_resv_desc(ctxt, &resv_msg)))
+	if ((rc = _parse_resv_desc(ctxt, &resv_msg)))
 		return rc;
 
-	xfree(resv_msg.name);
-	SWAP(resv_msg.name, params.reservation_name);
-
 	if (resv_msg.name == NULL) {
-		resp_error(ctxt, ESLURM_RESERVATION_INVALID, __func__,
-			   "Reservation must be given.  No reservation update.");
-	} else if ((rc = slurm_update_reservation(&resv_msg))) {
-		if (errno)
-			rc = errno;
-		resp_error(ctxt, rc, "slurm_update_reservation",
-			   "Error updating the reservation");
-	} else {
+		rc = resp_error(ctxt, ESLURM_RESERVATION_INVALID, __func__,
+				"Reservation must be given.");
+		slurm_free_resv_desc_members(&resv_msg);
+		return rc;
+	}
+
+	if (!(rc = _load_reservations(&args.res_info_ptr, ctxt)))
+		rc = _create_or_update_each_resv(&resv_msg, &args);
+
+	if (!rc) {
 		list_t *resv_list = list_create(NULL);
 		list_append(resv_list, &resv_msg);
 		DUMP_OPENAPI_RESP_SINGLE(OPENAPI_RESERVATION_MOD_RESP,
 					 resv_list, ctxt);
 		FREE_NULL_LIST(resv_list);
+		rc = ctxt->rc;
 	}
 
 	slurm_free_resv_desc_members(&resv_msg);
-	return rc ? rc : ctxt->rc;
+	slurm_free_reservation_info_msg(args.res_info_ptr);
+	return rc;
 }
 
 static int _delete_resv(openapi_ctxt_t *ctxt)
@@ -428,7 +470,7 @@ extern int op_handler_reservation(openapi_ctxt_t *ctxt)
 	if (ctxt->method == HTTP_REQUEST_GET) {
 		rc = _get_single_reservation(ctxt);
 	} else if (ctxt->method == HTTP_REQUEST_POST) {
-		rc = _reservation_update(ctxt);
+		rc = _mod_reservation(ctxt);
 	} else if (ctxt->method == HTTP_REQUEST_DELETE) {
 		rc = _delete_resv(ctxt);
 	} else {
