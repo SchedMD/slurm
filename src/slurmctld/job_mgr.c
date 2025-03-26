f168ab1cd47a97b47d68366f0b8d6c78c2406460
Joel Criado <joel@schedmd.com>
2025-03-26 11:40:49 +0100

    Set CPU values for jobs requesting tasks and --mem
    When a job requests --mem and MaxMemPerCPU is in place, we potentially
    adjust the CPU values in _valid_pn_min_mem. The commit updates the CPUs
    per task for jobs requesting tasks and nodes, so the job does not
    violate the MaxMemPerCPU in place. It aims to produce a job with the
    least amount of CPUs possible.
    
    Ticket: 22018
    Cherry-picked: d358dbbee1

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -8903,6 +8903,32 @@ static bool _valid_pn_min_mem(job_desc_msg_t *job_desc_msg,
 					MAX(job_desc_msg->cpus_per_task *
 					    job_desc_msg->ntasks_per_node,
 					    job_desc_msg->pn_min_cpus);
+			} else if (job_desc_msg->num_tasks &&
+				   (job_desc_msg->num_tasks != NO_VAL) &&
+				   job_desc_msg->min_nodes &&
+				   (job_desc_msg->min_nodes != NO_VAL)) {
+				/*
+				 * Calculate a new value of cpus/task given the
+				 * current nodes and tasks values:
+				 * CPUs/Task = (min_cpus_per_node * min_nodes) / num_tasks
+				 */
+				uint32_t cpus =
+					min_cpus * job_desc_msg->min_nodes;
+				job_desc_msg->cpus_per_task =
+					ROUNDUP(cpus, job_desc_msg->num_tasks);
+				/*
+				 * Recalculate pn_min_cpus based on the new
+				 * CPUs/task. This formula aims to get
+				 * an allocation with the least amount of
+				 * CPUs combining all the nodes from the job.
+				 */
+				min_cpus = (job_desc_msg->cpus_per_task *
+					    job_desc_msg->num_tasks) /
+					   job_desc_msg->min_nodes;
+				job_desc_msg->pn_min_cpus = min_cpus;
+				job_desc_msg->min_cpus =
+					MAX(min_cpus,
+					    job_desc_msg->pn_min_cpus);
 			}
 			debug("JobId=%u: Setting job's pn_min_cpus to %u due to memory limit",
 			      job_desc_msg->job_id,
