SchedMD/slurm

commit 214c824f7fbf9450256002f693ce3170c4a3525c

data_parser/v0.0.42 - Set obj_openapi in add_fa()
Required for add_removed() to work.

Ticket 20718

src/plugins/data_parser/v0.0.42/parsers.c
@@ -639,7 +639,7 @@ extern void check_parser_funcname(const parser_t *const parser,
 		xassert(parser->ptr_offset == NO_VAL);
 		xassert(!parser->pointer_type);
 		xassert(!parser->array_type);
-		xassert(!parser->obj_openapi);
+		xassert(parser->obj_openapi == OPENAPI_FORMAT_ARRAY);
 	} else if (parser->model == PARSER_MODEL_LIST) {
 		/* parser of a List */
 		xassert(parser->list_type > DATA_PARSER_TYPE_INVALID);
@@ -9750,6 +9750,7 @@ static const parser_t PARSER_ARRAY(OPENAPI_JOB_ALLOC_RESP)[] = {
 		.type = DATA_PARSER_##typev,                                   \
 		.type_string = XSTRINGIFY(DATA_PARSER_ ## typev),              \
 		.obj_type_string = XSTRINGIFY(typet),                          \
+		.obj_openapi = OPENAPI_FORMAT_ARRAY,                           \
 		.size = sizeof(typet),                                         \
 		.needs = NEED_NONE,                                            \
 		.flag_bit_array = PARSER_FLAG_ARRAY(typev),                    \

commit 20841419f071163b27f32383cda402483c21a45c

data_parser/v0.0.42 - Add FLAG_INLINE_ENUMS to not $ref enum arrays
In 24.05, enum arrays were always inlined in the generated OpenAPI spec.
However, commit 214c824f caused enum arrays to be referenced. This was an
issue for oapi-codegen because it has some issues currently where
referenced objects that contain enums are not generating in the Golang
client correctly. This patch adds the data_parser flag inline_enums to
v0.0.42, which can be used to inline enum arrays in the spec.

Issue: 50297
Changelog: data_parser/v0.0.42 - Added +inline_enums flag which modifies
 the output when generating OpenAPI specification. It causes enum arrays to
 not be defined in their own schema with references ($ref) to them. Instead
 they will be dumped inline.

doc/man/man8/slurmrestd.8
@@ -1,4 +1,4 @@
-.TH slurmrestd "8" "Slurm REST Daemon" "February 2025" "Slurm REST Daemon"
+.TH slurmrestd "8" "Slurm REST Daemon" "April 2025" "Slurm REST Daemon"
 
 .SH "NAME"
 slurmrestd \- Interface to Slurm via REST API.
@@ -127,7 +127,7 @@ OpenAPI specifications if the schema is only referenced once.
 .IP
 
 .TP
-\fB[data_parser/]v0.0.42[+fast][+minimize_refs]\fR
+\fB[data_parser/]v0.0.42[+fast][+minimize_refs][+inline_enums]\fR
 Load data_parser/]v0.0.42 plugin to for formatting of data. Only compatible
 with \fBopenapi/slurmctld\fR and \fBopenapi/slurmdbd\fR content plugins.
 .RS
@@ -143,6 +143,11 @@ values.
 \fB+minimize_refs\fR
 Avoid inline expansion of referenced schemas (via \fI$ref\fR) in generated
 OpenAPI specifications if the schema is only referenced once.
+.IP
+.TP
+\fB+inline_enums\fR
+Avoid inline expansion of referenced schemas (via \fI$ref\fR) in generated
+OpenAPI specifications for enum arrays.
 .IP
 .RE
 .IP

src/plugins/data_parser/v0.0.42/api.h
@@ -48,6 +48,7 @@
 #define is_fast_mode(args) (args->flags & FLAG_FAST)
 #define is_complex_mode(args) (args->flags & FLAG_COMPLEX_VALUES)
 #define is_prefer_refs_mode(args) (~args->flags & FLAG_MINIMIZE_REFS)
+#define is_inline_enums_mode(args) (args->flags & FLAG_INLINE_ENUMS)
 
 typedef enum {
 	FLAG_NONE = 0,
@@ -66,6 +67,8 @@ typedef enum {
 	FLAG_PREFER_REFS = SLURM_BIT(3),
 	/* Prefer to inline $ref into schemas when possible in OpenAPI specification */
 	FLAG_MINIMIZE_REFS = SLURM_BIT(4),
+	/* Prefer to inline $ref into schema for flag arrays */
+	FLAG_INLINE_ENUMS = SLURM_BIT(5),
 } data_parser_flags_t;
 
 typedef struct {

src/plugins/data_parser/v0.0.42/openapi.c
@@ -203,7 +203,9 @@ static bool _should_be_ref(const parser_t *parser, spec_args_t *sargs)
 	}
 
 	if ((parser->obj_openapi == OPENAPI_FORMAT_OBJECT) ||
-	    (parser->obj_openapi == OPENAPI_FORMAT_ARRAY))
+	    ((parser->obj_openapi == OPENAPI_FORMAT_ARRAY) &&
+	     !((is_inline_enums_mode(sargs->args)) &&
+	       (parser->model == PARSER_MODEL_FLAG_ARRAY))))
 		return true;
 
 	if (parser->array_type || parser->pointer_type || parser->list_type ||

src/plugins/data_parser/v0.0.42/parsers.c
@@ -8945,6 +8945,7 @@ static const flag_bit_t PARSER_FLAG_ARRAY(FLAGS)[] = {
 	add_flag_bit(FLAG_COMPLEX_VALUES, "COMPLEX"),
 	add_flag_bit(FLAG_PREFER_REFS, "PREFER_REFS"),
 	add_flag_bit(FLAG_MINIMIZE_REFS, "MINIMIZE_REFS"),
+	add_flag_bit(FLAG_INLINE_ENUMS, "INLINE_ENUMS"),
 };
 
 #define add_flag(flag_value, mask, flag_string, hidden, desc)               \