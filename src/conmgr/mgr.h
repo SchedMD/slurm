SchedMD/slurm

commit 9e7682b58129a4ac0f4b4aebe16e69383e7f350b

Add missing flags to FLAGS_MASK_STATE
Avoid inheriting state flags on newly accepted sockets from listener
socket.

In the event that a listener socket has been quiesced, then the child
connection should not inherit the quiesced state as that will not be
expected by the conmgr caller.

Issue 50094

src/conmgr/mgr.h
@@ -141,7 +141,9 @@ typedef enum {
 #define FLAGS_MASK_STATE \
 	( FLAG_ON_DATA_TRIED | FLAG_IS_SOCKET | FLAG_IS_LISTEN | \
 	  FLAG_WAIT_ON_FINISH | FLAG_CAN_WRITE | FLAG_CAN_READ | \
-	  FLAG_READ_EOF | FLAG_IS_CONNECTED | FLAG_WORK_ACTIVE )
+	  FLAG_READ_EOF | FLAG_IS_CONNECTED | FLAG_WORK_ACTIVE | \
+	  FLAG_QUIESCE | FLAG_CAN_QUERY_OUTPUT_BUFFER | \
+	  FLAG_IS_FIFO | FLAG_IS_CHR )
 
 /* con_flags_t macro helpers to test, set, and unset flags */
 #define con_flag(con, flag) ((con)->flags & (flag))

commit e882089be64a8619d913acb4f5ee4fff83f344f2

conmgr - Remove FLAG_QUIESCE from FLAGS_MASK_STATE
Avoid removing FLAG_QUIESCE in add_connection() which can result in new
connection that should be quiesced running immediately.

Before c0b27b883d, having FLAG_QUIESCE in FLAGS_MASK_STATE didn't affect
new connections as it wasn't possible to pass a set of flags to a new
connection negating the regression from 9e7682b581.

Regression from 9e7682b581 and c0b27b883d but is not trigger able with any
existing code paths.

Ticket: 22222

src/conmgr/mgr.h
@@ -156,8 +156,7 @@ typedef enum {
 	( FLAG_ON_DATA_TRIED | FLAG_IS_SOCKET | FLAG_IS_LISTEN | \
 	  FLAG_WAIT_ON_FINISH | FLAG_CAN_WRITE | FLAG_CAN_READ | \
 	  FLAG_READ_EOF | FLAG_IS_CONNECTED | FLAG_WORK_ACTIVE | \
-	  FLAG_QUIESCE | FLAG_CAN_QUERY_OUTPUT_BUFFER | \
-	  FLAG_IS_FIFO | FLAG_IS_CHR )
+	  FLAG_CAN_QUERY_OUTPUT_BUFFER | FLAG_IS_FIFO | FLAG_IS_CHR )
 
 /* con_flags_t macro helpers to test, set, and unset flags */
 #define con_flag(con, flag) ((con)->flags & (flag))