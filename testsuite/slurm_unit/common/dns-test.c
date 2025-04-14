778c02353eed008e326172fc4727c2dd0554a764
Nathan Rini <nate@schedmd.com>
2025-04-14 12:25:49 -0600

    Testsuite - Test sockaddr_to_string() in dns-test
    Test the variations of addresses supported by sockaddr_to_string().
    
    Issue: 50377

--- testsuite/slurm_unit/common/dns-test.c
+++ testsuite/slurm_unit/common/dns-test.c
@@ -31,16 +31,119 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
+#include <arpa/inet.h>
 #include <check.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include "slurm/slurm.h"
 
 #include "src/common/log.h"
+#include "src/common/net.h"
 #include "src/common/read_config.h"
+#include "src/common/util-net.h"
+#include "src/common/xmalloc.h"
 
-START_TEST(test_dns) {}
+static void _test_addr_to_string(const slurm_addr_t *addr,
+				 const socklen_t addrlen, const char *dst)
+{
+	char *str = sockaddr_to_string(addr, addrlen);
+
+	if (!dst) {
+		ck_assert(str == NULL);
+		goto cleanup;
+	}
+
+	ck_assert(str != NULL);
+	ck_assert_str_eq(str, dst);
+
+cleanup:
+	xfree(str);
+}
+
+#define taddr(addr, dst) \
+	_test_addr_to_string((slurm_addr_t *) &addr, sizeof(addr), dst)
+#define tunix(unix_path, dst)                     \
+	do {                                      \
+		const struct sockaddr_un addr = { \
+			.sun_family = AF_UNIX,    \
+			.sun_path = unix_path,    \
+		};                                \
+		taddr(addr, dst);                 \
+	} while (false)
+#define tip4(address, port, dst)                           \
+	do {                                               \
+		const struct in_addr inaddr = { address }; \
+		const uint16_t nport = htons(port);        \
+		const struct sockaddr_in addr = {          \
+			.sin_family = AF_INET,             \
+			.sin_port = nport,                 \
+			.sin_addr = inaddr,                \
+		};                                         \
+		taddr(addr, dst);                          \
+	} while (false)
+#define tip6(address, port, dst)                        \
+	do {                                            \
+		const struct in6_addr inaddr = address; \
+		const uint16_t nport = htons(port);     \
+		const struct sockaddr_in6 addr = {      \
+			.sin6_family = AF_INET6,        \
+			.sin6_port = nport,             \
+			.sin6_addr = inaddr,            \
+		};                                      \
+		taddr(addr, dst);                       \
+	} while (false)
+
+START_TEST(test_dns)
+{
+	{
+		slurm_addr_t addr = { 0 };
+		taddr(addr, NULL);
+	}
+	{
+		slurm_addr_t addr = {
+			.ss_family = AF_UNSPEC,
+		};
+		taddr(addr, NULL);
+	}
+
+	getnameinfo_cache_purge();
+
+	tunix({ 0 }, "unix:");
+	tunix("\0abstract", "unix:@abstract");
+	tunix("/tmp/unix", "unix:/tmp/unix");
+
+	tip4(INADDR_LOOPBACK, 10, "127.0.0.1:10");
+	tip4(INADDR_ANY, 20, "0.0.0.0:20");
+	tip4(0x0, 23, "0.0.0.0:23");
+	tip4(INADDR_BROADCAST, 30, "255.255.255.255:30");
+#ifdef INADDR_DUMMY
+	tip4(INADDR_DUMMY, 40, "192.0.0.8:40");
+#endif /* INADDR_DUMMY */
+	tip4(0xee10eea1, 44, "161.238.16.238:44");
+	tip4(0xee10eea1, 0, "161.238.16.238");
+
+	tip6({ { { 0 } } }, 0, "[::]");
+	tip6({ { { 0 } } }, 50, "[::]:50");
+	tip6(IN6ADDR_LOOPBACK_INIT, 60, "[::1]:60");
+	tip6(IN6ADDR_ANY_INIT, 70, "[::]:70");
+	tip6(in6addr_any, 80, "[::]:80");
+#define IP6_10102E                                           \
+	{                                                    \
+		{{0x10,0x10,0,0,0,0,0,0,0,0,0,0,0,0,0,0x2e}} \
+	}
+	tip6(IP6_10102E, 90, "[1010::2e]:90");
+	tip6(IP6_10102E, 0, "[1010::2e]");
+}
 
 END_TEST
+#undef taddr
+#undef tunix
+#undef tip4
+#undef tip6
 
 extern Suite *suite_dns(void)
 {
@@ -83,5 +186,8 @@ extern int main(void)
 	srunner_free(sr);
 
 	log_fini();
+
+	getnameinfo_cache_purge();
+
 	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
