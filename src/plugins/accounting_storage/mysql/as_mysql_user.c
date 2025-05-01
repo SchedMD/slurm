f416f8cd08d308f878fdc56de42e5a7aef36a97b
Danny Auble <da@schedmd.com>
2025-05-01 06:11:01 -0600

    Fix potential deadlock when starting of a DBD with no clusters
    Continuation of 28573c5cc0a
    
    Ticket: 22355

--- src/plugins/accounting_storage/mysql/as_mysql_user.c
+++ src/plugins/accounting_storage/mysql/as_mysql_user.c
@@ -2125,6 +2125,10 @@ extern int as_mysql_user_create_user_coords_list(mysql_conn_t *mysql_conn)
 	(void) list_for_each(as_mysql_cluster_list, _get_accts_coords,
 			     &create_string);
 
+	/* Empty as_mysql_cluster_list */
+	if (!create_string.query)
+		goto end_it;
+
 	DB_DEBUG(DB_ASSOC, mysql_conn->conn, "query\n%s", create_string.query);
 	result = mysql_db_query_ret(mysql_conn, create_string.query, 0);
 	xfree(create_string.query);
@@ -2177,6 +2181,8 @@ extern int as_mysql_user_create_user_coords_list(mysql_conn_t *mysql_conn)
 		mysql_free_result(result2);
 	}
 	mysql_free_result(result);
+
+end_it:
 	slurm_rwlock_unlock(&as_mysql_cluster_list_lock);
 
 	return SLURM_SUCCESS;
