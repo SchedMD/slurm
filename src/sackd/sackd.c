SchedMD/slurm

commit 235339c0d12699d73a377cf3336c11c2e1aee801

sackd - Add --port option
And corresponding SACKD_PORT environment variable.

Issue: 50276

src/sackd/sackd.c
@@ -72,6 +72,7 @@ static bool under_systemd = false;
 static char *conf_file = NULL;
 static char *conf_server = NULL;
 static char *dir = NULL;
+static uint16_t port = 0;
 
 static char **main_argv = NULL;
 static int listen_fd = -1;
@@ -95,11 +96,13 @@ static void _parse_args(int argc, char **argv)
 	enum {
 		LONG_OPT_ENUM_START = 0x100,
 		LONG_OPT_CONF_SERVER,
+		LONG_OPT_PORT,
 		LONG_OPT_SYSTEMD,
 	};
 
 	static struct option long_options[] = {
 		{"conf-server", required_argument, 0, LONG_OPT_CONF_SERVER},
+		{ "port", required_argument, 0, LONG_OPT_PORT },
 		{"systemd", no_argument, 0, LONG_OPT_SYSTEMD},
 		{NULL, no_argument, 0, 'v'},
 		{NULL, 0, 0, 0}
@@ -115,6 +118,12 @@ static void _parse_args(int argc, char **argv)
 		if (logopt.syslog_level == NO_VAL16)
 			fatal("Invalid env SACKD_DEBUG: %s", str);
 	}
+
+	if ((str = getenv("SACKD_PORT"))) {
+		if (parse_uint16(str, &port))
+			fatal("Invalid SACKD_PORT=%s", str);
+	} else
+		port = slurm_conf.slurmd_port;
 
 	if ((str = getenv("SACKD_SYSLOG_DEBUG"))) {
 		logopt.syslog_level = log_string2num(str);
@@ -163,6 +172,10 @@ static void _parse_args(int argc, char **argv)
 		case LONG_OPT_CONF_SERVER:
 			xfree(conf_server);
 			conf_server = xstrdup(optarg);
+			break;
+		case LONG_OPT_PORT:
+			if (parse_uint16(optarg, &port))
+				fatal("Invalid port '%s'", optarg);
 			break;
 		case LONG_OPT_SYSTEMD:
 			under_systemd = true;

commit 3571f8a53d38b6fcf0888e1edd44701c19dd34a5

sackd - Fix binding to SlurmdPort instead of an ephemeral port by default
When --port / SACKD_PORT is not specified, SlurmdPort should be the port
used by default to listen for reconfiguration updates.

Commit 235339c0d12 introduced a regression which caused sackd to listen
for such updates on an ephemeral port instead of SlurmdPort, resulting
in slurmctld sending REQUEST_RECONFIGURE_SACKD to SlurmdPort while sackd
was listening on the ephemeral port instead, resulting in the loss of
such RPC requests.

Ticket: 22700

src/sackd/sackd.c
@@ -133,8 +133,7 @@ static void _parse_args(int argc, char **argv)
 	if ((str = getenv("SACKD_PORT"))) {
 		if (parse_uint16(str, &port))
 			fatal("Invalid SACKD_PORT=%s", str);
-	} else
-		port = slurm_conf.slurmd_port;
+	}
 
 	if ((str = getenv("SACKD_SYSLOG_DEBUG"))) {
 		logopt.syslog_level = log_string2num(str);
@@ -298,6 +297,16 @@ static void _establish_config_source(void)
 	if (disable_reconfig)
 		fetch_type = CONFIG_REQUEST_SLURM_CONF;
 
+	/*
+	 * If --port / SACKD_PORT is not specified the default is to register
+	 * sackd for ctld reconfig updates with SlurmdPort, but at this point
+	 * the configuration hasn't been parsed yet so we pass 0 which will be
+	 * interpreted as SlurmdPort by slurmctld.
+	 *
+	 * This agreement needs to be in sync both in:
+	 * slurmctld/sack_mgr.c sackd_mgr_add_node() and
+	 * sackd/sackd.c _listen_for_reconf().
+	 */
 	while (!(configs = fetch_config(conf_server, fetch_type, port,
 					ca_cert_file))) {
 		error("Failed to load configs from slurmctld. Retrying in 10 seconds.");
@@ -357,13 +366,14 @@ static int _on_msg(conmgr_fd_t *con, slurm_msg_t *msg, int unpack_rc, void *arg)
 static void _listen_for_reconf(void)
 {
 	int rc = SLURM_SUCCESS;
+	uint16_t listen_port = port ? port : slurm_conf.slurmd_port;
 	static const conmgr_events_t events = {
 		.on_msg = _on_msg,
 	};
 
 	if (getenv("SACKD_RECONF_LISTEN_FD")) {
 		listen_fd = atoi(getenv("SACKD_RECONF_LISTEN_FD"));
-	} else if ((listen_fd = slurm_init_msg_engine_port(port)) < 0) {
+	} else if ((listen_fd = slurm_init_msg_engine_port(listen_port)) < 0) {
 		error("%s: failed to open port: %m", __func__);
 		return;
 	}