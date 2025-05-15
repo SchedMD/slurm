SchedMD/slurm

commit 426b931f9d272c3383a368f31477b1b129058b77

Remove node_prefix from slurm_conf_t

src/common/slurm_protocol_pack.c
@@ -3865,7 +3865,6 @@ _pack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t * build_ptr, buf_t *buffer,
 		                               protocol_version, buffer);
 
 		packstr(build_ptr->node_features_plugins, buffer);
-		packstr(build_ptr->node_prefix, buffer);
 
 		pack16(build_ptr->over_time_limit, buffer);
 
@@ -4126,7 +4125,7 @@ _pack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t * build_ptr, buf_t *buffer,
 		                               protocol_version, buffer);
 
 		packstr(build_ptr->node_features_plugins, buffer);
-		packstr(build_ptr->node_prefix, buffer);
+		packnull(buffer); /* was node_prefix */
 
 		pack16(build_ptr->over_time_limit, buffer);
 
@@ -4395,7 +4394,7 @@ _pack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t * build_ptr, buf_t *buffer,
 		                               protocol_version, buffer);
 
 		packstr(build_ptr->node_features_plugins, buffer);
-		packstr(build_ptr->node_prefix, buffer);
+		packnull(buffer); /* was node_prefix */
 
 		pack16(build_ptr->over_time_limit, buffer);
 
@@ -4689,8 +4688,6 @@ _unpack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t **build_buffer_ptr,
 
 		safe_unpackstr(&build_ptr->node_features_plugins, buffer);
 
-		safe_unpackstr(&build_ptr->node_prefix, buffer);
-
 		safe_unpack16(&build_ptr->over_time_limit, buffer);
 
 		safe_unpackstr(&build_ptr->plugindir, buffer);
@@ -4971,7 +4968,7 @@ _unpack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t **build_buffer_ptr,
 
 		safe_unpackstr(&build_ptr->node_features_plugins, buffer);
 
-		safe_unpackstr(&build_ptr->node_prefix, buffer);
+		safe_skipstr(buffer); /* was node_prefix */
 
 		safe_unpack16(&build_ptr->over_time_limit, buffer);
 
@@ -5277,7 +5274,7 @@ _unpack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t **build_buffer_ptr,
 
 		safe_unpackstr(&build_ptr->node_features_plugins, buffer);
 
-		safe_unpackstr(&build_ptr->node_prefix, buffer);
+		safe_skipstr(buffer); /* was node_prefix */
 
 		safe_unpack16(&build_ptr->over_time_limit, buffer);
 

contribs/perlapi/libslurm/perl/conf.c
@@ -184,9 +184,6 @@ int slurm_ctl_conf_to_hv(slurm_conf_t *conf, HV *hv)
 
 	STORE_FIELD(hv, conf, next_job_id, uint32_t);
 
-	if (conf->node_prefix)
-		STORE_FIELD(hv, conf, node_prefix, charp);
-
 	STORE_FIELD(hv, conf, over_time_limit, uint16_t);
 
 	if (conf->plugindir)
@@ -476,7 +473,6 @@ int hv_to_slurm_ctl_conf(HV *hv, slurm_conf_t *conf)
 	FETCH_FIELD(hv, conf, msg_timeout, uint16_t, TRUE);
 	FETCH_FIELD(hv, conf, next_job_id, uint32_t, TRUE);
 
-	FETCH_FIELD(hv, conf, node_prefix, charp, FALSE);
 	FETCH_FIELD(hv, conf, over_time_limit, uint16_t, TRUE);
 	FETCH_FIELD(hv, conf, plugindir, charp, FALSE);
 	FETCH_FIELD(hv, conf, plugstack, charp, FALSE);

slurm/slurm.h
@@ -3053,8 +3053,6 @@ typedef struct {
 	uint32_t next_job_id;	/* next slurm generated job_id to assign */
 	void *node_features_conf; /* Node Features Plugin config file */
 	char *node_features_plugins; /* List of node_features plugins to use */
-	char *node_prefix;      /* prefix of nodes in partition, only set in
-				   bluegene clusters NULL otherwise */
 	uint16_t over_time_limit; /* job's time limit can be exceeded by this
 				   * number of minutes before cancellation */
 	char *plugindir;	/* pathname to plugins */

src/common/read_config.c
@@ -506,42 +506,6 @@ static int _defunct_option(void **dest, slurm_parser_enum_t type,
 	return 0;
 }
 
-/* Used to get the general name of the machine, used primarily
- * for bluegene systems.  Not in general use because some systems
- * have multiple prefix's such as foo[1-1000],bar[1-1000].
- */
-/* Caller must be holding slurm_conf_lock() */
-static void _set_node_prefix(const char *nodenames)
-{
-	int i;
-	char *tmp;
-
-	xassert(nodenames != NULL);
-	for (i = 1; nodenames[i] != '\0'; i++) {
-		if ((nodenames[i-1] == '[')
-		   || (nodenames[i-1] <= '9'
-		       && nodenames[i-1] >= '0'))
-			break;
-	}
-
-	if (i == 1) {
-		error("In your Node definition in your slurm.conf you "
-		      "gave a nodelist '%s' without a prefix.  "
-		      "Please try something like bg%s.", nodenames, nodenames);
-	}
-
-	xfree(conf_ptr->node_prefix);
-	if (nodenames[i] == '\0')
-		conf_ptr->node_prefix = xstrdup(nodenames);
-	else {
-		tmp = xmalloc(i + 1);
-		snprintf(tmp, i, "%s", nodenames);
-		conf_ptr->node_prefix = tmp;
-		tmp = NULL;
-	}
-	debug3("Prefix is %s %s %d", conf_ptr->node_prefix, nodenames, i);
-}
-
 static int _parse_frontend(void **dest, slurm_parser_enum_t type,
 			   const char *key, const char *value,
 			   const char *line, char **leftover)
@@ -719,9 +683,6 @@ static int _parse_nodename(void **dest, slurm_parser_enum_t type,
 		dflt = default_nodename_tbl;
 
 		n->nodenames = xstrdup(value);
-		if ((slurmdb_setup_cluster_dims() > 1)
-		    && conf_ptr->node_prefix == NULL)
-			_set_node_prefix(n->nodenames);
 
 		if (!s_p_get_string(&n->hostnames, "NodeHostname", tbl))
 			n->hostnames = xstrdup(n->nodenames);
@@ -2390,9 +2351,6 @@ static void _init_slurmd_nodehash(void)
 	count = slurm_conf_nodename_array(&ptr_array);
 	for (i = 0; i < count; i++) {
 		expand_nodeline_info(ptr_array[i], NULL, NULL, _check_callback);
-		if ((slurmdb_setup_cluster_dims() > 1) &&
-		    !conf_ptr->node_prefix)
-			_set_node_prefix(ptr_array[i]->nodenames);
 	}
 
 	count = slurm_conf_frontend_array(&ptr_front_end);
@@ -2965,7 +2923,6 @@ extern void free_slurm_conf(slurm_conf_t *ctl_conf_ptr, bool purge_node_hash)
 	xfree (ctl_conf_ptr->mpi_params);
 	FREE_NULL_LIST(ctl_conf_ptr->node_features_conf);
 	xfree (ctl_conf_ptr->node_features_plugins);
-	xfree (ctl_conf_ptr->node_prefix);
 	xfree (ctl_conf_ptr->plugindir);
 	xfree (ctl_conf_ptr->plugstack);
 	xfree(ctl_conf_ptr->preempt_params);
@@ -3141,7 +3098,6 @@ void init_slurm_conf(slurm_conf_t *ctl_conf_ptr)
 	ctl_conf_ptr->msg_timeout		= NO_VAL16;
 	ctl_conf_ptr->next_job_id		= NO_VAL;
 	xfree(ctl_conf_ptr->node_features_plugins);
-	xfree (ctl_conf_ptr->node_prefix);
 	ctl_conf_ptr->over_time_limit           = 0;
 	xfree (ctl_conf_ptr->plugindir);
 	xfree (ctl_conf_ptr->plugstack);

src/slurmctld/proc_req.c
@@ -411,7 +411,6 @@ static void _fill_ctld_conf(slurm_conf_t *conf_ptr)
 	conf_ptr->next_job_id         = next_job_id;
 	conf_ptr->node_features_conf  = node_features_g_get_config();
 	conf_ptr->node_features_plugins = xstrdup(conf->node_features_plugins);
-	conf_ptr->node_prefix         = xstrdup(conf->node_prefix);
 
 	conf_ptr->over_time_limit     = conf->over_time_limit;
 

src/slurmd/common/slurmstepd_init.c
@@ -243,7 +243,6 @@ extern void pack_slurm_conf_lite(buf_t *buffer)
 	/* next_job_id */
 	/* node_features_conf */
 	/* node_features_plugins */
-	/* node_prefix */
 	/* over_time_limit */
 	packstr(slurm_conf.plugindir, buffer);
 	packstr(slurm_conf.plugstack, buffer);
@@ -476,7 +475,6 @@ extern int unpack_slurm_conf_lite_no_alloc(buf_t *buffer)
 	/* next_job_id */
 	/* node_features_conf */
 	/* node_features_plugins */
-	/* node_prefix */
 	/* over_time_limit */
 	safe_unpackstr(&slurm_conf.plugindir, buffer);
 	safe_unpackstr(&slurm_conf.plugstack, buffer);

commit 779707b46a9a3176ce68979887f169d2435cf68b

Fix backwards compatibility for RESPONSE_BUILD_INFO RPC
Regression in 426b931f9d.

Changelog: Fix backwards compatibility for RESPONSE_BUILD_INFO RPC
 ("scontrol show config").
Ticket: 22794

src/common/slurm_protocol_pack.c
@@ -4566,6 +4566,7 @@ _pack_slurm_ctl_conf_msg(slurm_ctl_conf_info_msg_t * build_ptr, buf_t *buffer,
 		                               protocol_version, buffer);
 
 		packstr(build_ptr->node_features_plugins, buffer);
+		packnull(buffer); /* was node_prefix */
 
 		pack16(build_ptr->over_time_limit, buffer);
 