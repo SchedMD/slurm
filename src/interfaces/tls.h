b6f04db467ec472fa71ca6e74e4440c16dbf33dd
Nathan Rini <nate@schedmd.com>
2025-05-25 20:31:20 -0600

    interfaces/tls - Add implementation
    Issue: 50293

--- src/interfaces/tls.h
+++ src/interfaces/tls.h
@@ -36,4 +36,80 @@
 #ifndef _INTERFACES_TLS_H
 #define _INTERFACES_TLS_H
 
+#include "src/common/slurm_time.h"
+
+#include "src/interfaces/conn.h"
+
+/*
+ * WARNING: interfaces/tls is a simplified alias for interfaces/conn and must
+ * be kept in sync with interfaces/tls as it reuses the same plugins.
+ */
+
+extern int tls_g_init(void);
+extern int tls_g_fini(void);
+
+/*
+ * Get absolute time that next tls_g_*() should be delayed until after any
+ * failure
+ * NOTE: returned timespec may be {0,0} indicating no delay required
+ */
+extern timespec_t tls_g_get_delay(void *conn);
+
+/*
+ * Create new TLS connection
+ * IN tls_conn_args - ptr to tls_conn_args_t
+ * RET ptr to TLS state
+ */
+extern void *tls_g_create_conn(const conn_args_t *tls_conn_args);
+extern void tls_g_destroy_conn(void *conn, bool close_fds);
+
+/*
+ * Attempt TLS connection negotiation
+ * NOTE: Only to be called at start of connection and if defer_negotiation=true
+ * RET SLURM_SUCCESS or EWOULDBLOCK or error
+ */
+extern int tls_g_negotiate_conn(void *conn);
+
+extern ssize_t tls_g_recv(void *conn, void *buf, size_t n);
+
+extern ssize_t tls_g_send(void *conn, const void *buf, size_t n);
+
+/*
+ * Set read/write fd's on TLS connection
+ * NOTE: This resets send/recv callbacks/contexts in TLS connection
+ * IN conn - TLS connection to reconfigure
+ * IN input_fd - new read fd
+ * IN output_fd - new write fd
+ * RET SLURM_SUCCESS or error
+ */
+extern int tls_g_set_conn_fds(void *conn, int input_fd, int output_fd);
+
+/*
+ * Set read/write fd's on TLS connection
+ * NOTE: This resets read/write fd's in TLS connection
+ * IN conn - TLS connection to reconfigure
+ * IN input_fd - new read fd
+ * IN output_fd - new write fd
+ * RET SLURM_SUCCESS or error
+ */
+extern int tls_g_set_conn_callbacks(void *conn, conn_callbacks_t *callbacks);
+
+/*
+ * Load own certificate into store
+ *
+ * This is useful when certificate is not known on startup, and must be loaded
+ * later (e.g. slurmd getting a signed certificate from slurmctld)
+ *
+ * Set 'cert' to NULL to try to load certificate from file. This is only
+ * relevant to Slurm daemons that have statically configured certificates.
+ * If 'cert' is NULL, all other arguments will be ignored.
+ *
+ * IN cert - certificate PEM, or NULL if loading from file.
+ * IN cert_len - length of cert
+ * IN key - key PEM
+ * IN key_len - length of key
+ */
+extern int tls_g_load_own_cert(char *cert, uint32_t cert_len, char *key,
+			       uint32_t key_len);
+
 #endif
