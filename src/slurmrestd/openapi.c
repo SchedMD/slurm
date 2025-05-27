SchedMD/slurm

commit 3434f99fece84bc5815f9a219a7d30885bc37f64

Switch to serializer_required()

src/slurmrestd/openapi.c
@@ -929,9 +929,7 @@ extern int init_openapi(const char *plugin_list, plugrack_foreach_t listf,
 	paths = list_create(_list_delete_path_t);
 
 	/* must have JSON plugin to parse the openapi.json */
-	if ((rc = serializer_g_init(MIME_TYPE_JSON_PLUGIN, NULL)))
-		fatal("Plugin serializer/json failed to load: %s",
-		      slurm_strerror(rc));
+	serializer_required(MIME_TYPE_JSON);
 
 	if ((rc = _bind_paths(openapi_paths, NULL)))
 		fatal("Unable to bind openapi specification paths: %s",

src/slurmrestd/slurmrestd.c
@@ -367,8 +367,7 @@ static void dump_spec(int argc, char **argv)
 		      slurm_conf_filename, slurm_strerror(rc));
 	}
 
-	if (serializer_g_init(MIME_TYPE_JSON_PLUGIN, NULL))
-		fatal("Unable to initialize JSON serializer");
+	serializer_required(MIME_TYPE_JSON);
 
 	if (!(parsers = data_parser_g_new_array(NULL, NULL, NULL, NULL, NULL,
 						NULL, NULL, NULL,
@@ -723,7 +722,7 @@ int main(int argc, char **argv)
 				 getenv("SLURMRESTD_JSON"));
 	(void) serializer_g_init(MIME_TYPE_YAML_PLUGIN,
 				 getenv("SLURMRESTD_YAML"));
-	(void) serializer_g_init(MIME_TYPE_URL_ENCODED_PLUGIN, NULL);
+	serializer_required(MIME_TYPE_URL_ENCODED);
 
 	/* This checks if slurmrestd is running in inetd mode */
 	conmgr_init((run_mode.listen ? thread_count : CONMGR_THREAD_COUNT_MIN),

src/common/data.h
@@ -65,7 +65,7 @@
  * Example usage:
  *
  * //Global init requiring JSON serializer
- * if (serializer_g_init(MIME_TYPE_JSON_PLUGIN, NULL)) fatal("failed");
+ * serializer_required(MIME_TYPE_JSON);
  * //Create root data entry:
  * data_t *ex = data_new();
  * //Set data entry to be a dictionary type

src/interfaces/data_parser.c
@@ -277,8 +277,7 @@ static int _load_plugins(plugin_param_t *pparams, plugrack_foreach_t listf,
 
 	slurm_mutex_lock(&init_mutex);
 
-	if ((rc = serializer_g_init(MIME_TYPE_JSON_PLUGIN, NULL)))
-		fatal("JSON plugin loading failed: %s", slurm_strerror(rc));
+	serializer_required(MIME_TYPE_JSON);
 
 	xassert(sizeof(parse_funcs_t) ==
 		(sizeof(void *) * ARRAY_SIZE(parse_syms)));

src/interfaces/topology.c
@@ -208,7 +208,7 @@ static int _parse_yaml(char *topo_conf)
 		.tctx_num = -1,
 	};
 
-	serializer_g_init(MIME_TYPE_YAML_PLUGIN, NULL);
+	serializer_required(MIME_TYPE_YAML);
 
 	if (!(conf_buf = create_mmap_buf(topo_conf))) {
 		error("could not load %s, and thus cannot create topo contexts",

src/plugins/auth/jwt/auth_jwt.c
@@ -180,8 +180,7 @@ static void _init_jwks(void)
 
 	_check_key_permissions(key_file, S_IWOTH);
 
-	if (serializer_g_init(MIME_TYPE_JSON_PLUGIN, NULL))
-		fatal("%s: serializer_g_init() failed", __func__);
+	serializer_required(MIME_TYPE_JSON);
 
 	debug("loading jwks file `%s`", key_file);
 	if (!(buf = create_mmap_buf(key_file))) {

src/plugins/auth/slurm/auth_slurm.c
@@ -112,8 +112,7 @@ extern int init(void)
 		return SLURM_SUCCESS;
 	init_run = true;
 
-	if (serializer_g_init(MIME_TYPE_JSON_PLUGIN, NULL))
-		fatal("%s: serializer_g_init() failed", __func__);
+	serializer_required(MIME_TYPE_JSON);
 
 	internal = run_in_daemon(IS_SACKD | IS_SLURMD | IS_SLURMCTLD |
 				 IS_SLURMDBD);

src/plugins/auth/slurm/internal.c
@@ -179,8 +179,7 @@ static void _read_keys_file(char *key_file)
 	buf_t *jwks = NULL;
 	data_t *keys = NULL;
 
-	if (serializer_g_init(MIME_TYPE_JSON_PLUGIN, NULL))
-		fatal("%s: serializer_g_init() failed", __func__);
+	serializer_required(MIME_TYPE_JSON);
 
 	debug("loading keys file `%s`", key_file);
 

commit f8d6c33b12e54b1e53b09437dc667e17aa435e4b

slurmrestd - Only dump JSON with --generate-openapi-spec
Regression from 3434f99fec.

src/slurmrestd/openapi.c
@@ -126,6 +126,7 @@ typedef struct {
 #define MAGIC_OAS 0x1218eeee
 typedef struct {
 	int magic; /* MAGIC_OAS */
+	const char **mime_types;
 	data_t *spec;
 	data_t *tags;
 	data_t *paths;
@@ -1114,7 +1115,7 @@ static char *_get_method_operationId(openapi_spec_t *spec, path_t *path,
 static int _populate_method(path_t *path, openapi_spec_t *spec, data_t *dpath,
 			    const openapi_path_binding_method_t *method)
 {
-	const char **mime_types = get_mime_type_array();
+	const char **mime_types = spec->mime_types;
 	void *refs = &spec->references[_resolve_parser_index(path->parser)];
 	data_t *dmethod = data_set_dict(data_key_set(dpath,
 		get_http_method_string_lc(method->method)));
@@ -1257,6 +1258,7 @@ static int _foreach_count_path(void *x, void *arg)
 {
 	path_t *path = x;
 	openapi_spec_t *spec = arg;
+	const char **mime_types = spec->mime_types;
 	const openapi_path_binding_t *bound = path->bound;
 	void *refs;
 
@@ -1271,7 +1273,6 @@ static int _foreach_count_path(void *x, void *arg)
 	for (int i = 0; path->methods[i].method; i++) {
 		const openapi_path_binding_method_t *method =
 			path->methods[i].bound;
-		const char **mime_types = get_mime_type_array();
 
 		if (method->parameters &&
 		    data_parser_g_increment_reference(path->parser,
@@ -1310,11 +1311,12 @@ static int _foreach_count_path(void *x, void *arg)
 	return SLURM_SUCCESS;
 }
 
-extern int generate_spec(data_t *dst)
+extern int generate_spec(data_t *dst, const char **mime_types)
 {
 	openapi_spec_t spec = {
 		.magic = MAGIC_OAS,
 		.spec = dst,
+		.mime_types = mime_types,
 	};
 	data_t *security1, *security2, *security3;
 	data_t *openapi_plugins, *data_parsers, *slurm_version;
@@ -1467,7 +1469,7 @@ extern int generate_spec(data_t *dst)
 
 static int _op_handler_openapi(openapi_ctxt_t *ctxt)
 {
-	return generate_spec(ctxt->resp);
+	return generate_spec(ctxt->resp, get_mime_type_array());
 }
 
 static bool _on_error(void *arg, data_parser_type_t type, int error_code,

src/slurmrestd/openapi.h
@@ -213,9 +213,10 @@ extern void openapi_resp_warn(openapi_ctxt_t *ctxt, const char *source,
 /*
  * Generate OpenAPI specification
  * IN/OUT dst - data_t to populate with specification
+ * IN mime_types - NULL terminated array of mime types to dump
  * RET SLURM_SUCCESS or error
  */
-extern int generate_spec(data_t *dst);
+extern int generate_spec(data_t *dst, const char **mime_types);
 
 /*
  * True if only generating an OAS

src/slurmrestd/slurmrestd.c
@@ -347,6 +347,7 @@ static void _usage(void)
 __attribute__((noreturn))
 static void dump_spec(int argc, char **argv)
 {
+	const char *dump_mime_types[] = { MIME_TYPE_JSON, NULL };
 	int rc = SLURM_SUCCESS;
 	data_t *spec = data_new();
 	char *output = NULL;
@@ -382,7 +383,7 @@ static void dump_spec(int argc, char **argv)
 	if (init_openapi(oas_specs, NULL, parsers, response_status_codes))
 		fatal("Unable to initialize OpenAPI structures");
 
-	if ((rc = generate_spec(spec)))
+	if ((rc = generate_spec(spec, dump_mime_types)))
 		fatal("Unable to generate OpenAPI Specification: %s",
 		      slurm_strerror(rc));
 