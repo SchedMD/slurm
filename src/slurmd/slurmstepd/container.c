SchedMD/slurm

commit 39fdfdc20849d42bfb29b653475aa84c4d81d932

slurmstepd - Avoid recursive ContainerPath generation
Use absolute path for ContainerPath's '%m' replacement in oci.conf and for
the generation of the spool directories which is now possible due to the
per-task and per-step tracking of the spool directories.

Issue: 50236

src/slurmd/slurmstepd/container.c
@@ -63,7 +63,6 @@
 #define SLURM_CONTAINER_STDOUT "/tmp/slurm/stdout"
 #define SLURM_CONTAINER_STDERR "/tmp/slurm/stderr"
 
-#define SLURM_CONTAINER_SPOOL_DIR_PATTERN "%m"
 #define SLURM_CONTAINER_BATCH_STEP_PATTERN "oci-job%j-batch"
 #define SLURM_CONTAINER_INTERACT_STEP_PATTERN "oci-job%j-interactive"
 #define SLURM_CONTAINER_EXTERN_STEP_PATTERN "oci-job%j-extern"
@@ -600,12 +599,21 @@ static char *_generate_spooldir_pattern(stepd_step_rec_t *step,
 {
 	const char *step_id = NULL;
 	const char *task_id = NULL;
-	const char *parent = NULL;
+	char *parent = NULL;
 
-	if (oci_conf->container_path)
-		return xstrdup(oci_conf->container_path);
-
-	parent = SLURM_CONTAINER_SPOOL_DIR_PATTERN;
+	if (oci_conf->container_path) {
+		parent = xstrdup(oci_conf->container_path);
+	} else if (conf->spooldir) {
+#ifdef MULTIPLE_SLURMD
+		parent = slurm_conf_expand_slurmd_path(conf->spooldir,
+						       conf->node_name,
+						       conf->hostname);
+#else /* !MULTIPLE_SLURMD */
+		parent = xstrdup(conf->spooldir);
+#endif /* !MULTIPLE_SLURMD */
+	} else {
+		parent = xstrdup(DEFAULT_SPOOLDIR);
+	}
 
 	if (step->step_id.step_id == SLURM_BATCH_SCRIPT) {
 		step_id = SLURM_CONTAINER_BATCH_STEP_PATTERN;

commit 71544b91db038ad7ca0ef30d3330be99342d4df1

Avoid memory leak in _generate_spooldir_pattern()
Regression from 39fdfdc208.

CID: 524459
Issue: 50246
Ticket: 22872
Changelog: slurmstepd - Correct memory leak for --container steps before
 executing job.

src/slurmd/slurmstepd/container.c
@@ -599,7 +599,7 @@ static char *_generate_spooldir_pattern(stepd_step_rec_t *step,
 {
 	const char *step_id = NULL;
 	const char *task_id = NULL;
-	char *parent = NULL;
+	char *parent = NULL, *pattern = NULL;
 
 	if (oci_conf->container_path) {
 		parent = xstrdup(oci_conf->container_path);
@@ -630,8 +630,10 @@ static char *_generate_spooldir_pattern(stepd_step_rec_t *step,
 	else
 		task_id = "";
 
-	return xstrdup_printf("%s%s%s%s%s", parent, (step_id[0] ? "/" : ""),
-			      step_id, (task_id[0] ? "/" : ""), task_id);
+	pattern = xstrdup_printf("%s%s%s%s%s", parent, (step_id[0] ? "/" : ""),
+				 step_id, (task_id[0] ? "/" : ""), task_id);
+	xfree(parent);
+	return pattern;
 }
 
 static char *_generate_spooldir(stepd_step_rec_t *step,