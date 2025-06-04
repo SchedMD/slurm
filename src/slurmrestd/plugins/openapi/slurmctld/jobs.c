SchedMD/slurm

commit d085fd0b4d1edcf42fa0c2ec88f9209ff572d624

slurmrestd - Don't require script to be populated for external jobs
Changelog: slurmrestd - Don't require script to be populated for
 external jobs.
Issue: 50475

src/slurmrestd/plugins/openapi/slurmctld/jobs.c
@@ -356,7 +356,8 @@ static void _job_post_submit(ctxt_t *ctxt, job_desc_msg_t *job, char *script)
 		job->script = xstrdup(script);
 	}
 
-	if (!job->script || !job->script[0]) {
+	if ((!job->script || !job->script[0]) &&
+	    !(job->bitflags & EXTERNAL_JOB)) {
 		resp_error(ctxt, ESLURM_JOB_SCRIPT_MISSING, "script",
 			   "Batch job script empty or missing");
 	} else if (slurm_submit_batch_job(job, &resp) || !resp) {
@@ -473,7 +474,8 @@ static void _job_post(ctxt_t *ctxt)
 		goto cleanup;
 
 	if (!req.jobs && (!req.script || !req.script[0]) &&
-	    (!req.job || !req.job->script)) {
+	    (!req.job || !req.job->script) &&
+	    !(req.job->bitflags & EXTERNAL_JOB)) {
 		resp_error(ctxt, ESLURM_REST_INVALID_QUERY, __func__,
 			   "Populated \"script\" field is required for job submission");
 		goto cleanup;

commit 24a203f4de427e59437fb0da6167a5c233430a06

openapi/slurmctld - Avoid NULL dereference when job not populated
Regression from d085fd0b4d.

Issue: 50475
Issue: 50518
Changelog: slurmrestd - Correct crash when empty request submitted to
 'POST slurm/*/job/submit' endpoints.

src/slurmrestd/plugins/openapi/slurmctld/jobs.c
@@ -474,8 +474,8 @@ static void _job_post(ctxt_t *ctxt)
 		goto cleanup;
 
 	if (!req.jobs && (!req.script || !req.script[0]) &&
-	    (!req.job || !req.job->script) &&
-	    !(req.job->bitflags & EXTERNAL_JOB)) {
+	    (!req.job ||
+	     (!req.job->script && !(req.job->bitflags & EXTERNAL_JOB)))) {
 		resp_error(ctxt, ESLURM_REST_INVALID_QUERY, __func__,
 			   "Populated \"script\" field is required for job submission");
 		goto cleanup;