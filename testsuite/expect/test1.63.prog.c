13937d6ba4cf6256b4f7059b8a1c8aca3939411b
Albert Gil <albert.gil@schedmd.com>
2025-06-06 13:29:18 +0200

    Testsuite - Improve test1.63 avoiding more false failures
    Avoid using "exp_continue" that may skip expected output.
    Also cleanup/refactor the test improving its style and logging.
    
    Signed-off-by: Oscar Hernández <oscar.hernandez@schedmd.com>
    Ticket: 22305

--- testsuite/expect/test1.63.prog.c
+++ testsuite/expect/test1.63.prog.c
@@ -1,14 +1,16 @@
 #include <signal.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
 
-void ouch(int sig)
+static const char *prefix = "TEST_PROCESS";
+
+void signal_handler(int sig)
 {
 	static int logged = 0;
 
 	if (logged == 0) {
 		logged = 1;
-		printf("OUCH! - I got signal %d\n", sig);
+		printf("%s: Signal received: %s\n", prefix, strsignal(sig));
 		fflush(stdout);
 	}
 }
@@ -16,17 +18,15 @@ void ouch(int sig)
 int main()
 {
 	struct sigaction act = {
-		.sa_handler = ouch,
+		.sa_handler = signal_handler,
 		.sa_flags = SA_NODEFER,
 	};
 	sigemptyset(&act.sa_mask);
 	sigaction(SIGINT, &act, 0);
 
-	printf("Hello World!\n");
+	printf("%s: Signal handler ready.\n", prefix);
 	fflush(stdout);
 	while (1) {
-		printf("Sleeping\n");
-		fflush(stdout);
 		sleep(1);
 	}
 }
