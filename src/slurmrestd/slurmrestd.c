SchedMD/slurm

commit 019ab8f5a17d80b73b94c1a517c89164b86a9766

slurmrestd - Check for SlurmUser after slurm.conf loads
Check slurmrestd is not running as SlurmUser after load of Slurm
configuration with SlurmUser.

Regression from 0a07c53c69.

Ticket 20773

src/slurmrestd/slurmrestd.c
@@ -551,19 +551,28 @@ static void _lock_down(void)
 
 	if (become_user && getuid())
 		fatal("slurmrestd must run as root in become_user mode");
-	else if (check_user && (slurm_conf.slurm_user_id == getuid()))
-		fatal("slurmrestd should not be run as SlurmUser");
 
 	if (become_user && getgid())
 		fatal("slurmrestd must run as root in become_user mode");
-	else if (check_user &&
-		 (gid_from_uid(slurm_conf.slurm_user_id) == getgid()))
-		fatal("slurmrestd should not be run with SlurmUser's group.");
 
 #ifdef PR_SET_DUMPABLE
 	if (prctl(PR_SET_DUMPABLE, 1) < 0)
 		error("%s: Unable to set process as dumpable: %m", __func__);
 #endif
+}
+
+/*
+ * Check slurmrestd is not running as SlurmUser unless check_user is false.
+ */
+static void _check_user(void)
+{
+	if (!check_user)
+		return;
+
+	if (slurm_conf.slurm_user_id == getuid())
+		fatal("slurmrestd should not be run as SlurmUser");
+	if (gid_from_uid(slurm_conf.slurm_user_id) == getgid())
+		fatal("slurmrestd should not be run with SlurmUser's group.");
 }
 
 /* simple wrapper to hand over operations router in http context */
@@ -638,6 +647,7 @@ int main(int argc, char **argv)
 	run_mode.listen = !list_is_empty(socket_listen);
 
 	slurm_init(slurm_conf_filename);
+	_check_user();
 
 	if (thread_count < 2)
 		fatal("Request at least 2 threads for processing");

commit badcb6aed3f46512e8937e0006301cf217145e68

slurmrestd - Skip root check if become_user active
Avoid checking if the user or group is root when become_user mode is
activated as that is a requirement.  This was a regression from commit
019ab8f5a1.

Ticket: 50389
Changelog: slurmrestd - Remove need to set both become_user and
 disable_user_check in SLURMRESTD_SECURITY when running slurmrestd as
 root in  become_user mode.

src/slurmrestd/slurmrestd.c
@@ -614,9 +614,9 @@ static void _check_user(void)
 	if (gid_from_uid(slurm_conf.slurm_user_id) == getgid())
 		fatal("slurmrestd should not be run with SlurmUser's group.");
 
-	if (!getuid())
+	if (!getuid() && !become_user)
 		fatal("slurmrestd should not be run as the root user.");
-	if (!getgid())
+	if (!getgid() && !become_user)
 		fatal("slurmrestd should not be run with the root group.");
 
 	if ((gid_count = getgroups(0, NULL)) > 0) {
@@ -629,7 +629,7 @@ static void _check_user(void)
 			if (list[i] == slurm_conf.slurm_user_id)
 				fatal("slurmrestd should not be run with SlurmUser's group.");
 
-			if (!list[i])
+			if (!list[i] && !become_user)
 				fatal("slurmrestd should not be run with the root group.");
 
 			if (list[i] == SLURM_AUTH_NOBODY)