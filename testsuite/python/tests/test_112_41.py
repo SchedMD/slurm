3a25ddd7588dc6fec34193a0a401051de73029db
Albert Gil <albert.gil@schedmd.com>
2025-06-11 16:49:08 +0200

    Testsuite - Add test for DELETE /slurm/v0.0.41/reservation
    Issue: 50267
    Cherry-picked: 54dbe85af5
    Cherry-picked: e4c41174c6

--- testsuite/python/tests/test_112_41.py
+++ testsuite/python/tests/test_112_41.py
@@ -1172,7 +1172,7 @@ def reservation(setup):
     )
 
 
-def test_resv(slurm, reservation):
+def test_resv(slurm, reservation, admin_level):
     resp = slurm.slurm_v0041_get_reservation(resv_name)
     assert len(resp.warnings) == 0
     assert len(resp.errors) == 0
@@ -1184,6 +1184,13 @@ def test_resv(slurm, reservation):
     assert len(resp.warnings) == 0
     assert len(resp.errors) == 0
     assert resp.reservations
+
+    # Delete reservation
+    resp = slurm.slurm_v0041_delete_reservation(resv_name)
+    assert not resp.warnings and not resp.errors
+    assert resv_name not in [
+        r.name for r in slurm.slurm_v0041_get_reservations().reservations
+    ], f"Reservation {resv_name} should be deleted"
 
 
 def test_partitions(slurm):
