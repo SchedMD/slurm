d67114674a34de675746ba747cdca34a0340eb16
Dominik Bartkiewicz <bart@schedmd.com>
2025-06-17 21:30:55 +0100

    Add ESLURM_RETRY_EVAL

--- src/common/slurm_errno.c
+++ src/common/slurm_errno.c
@@ -488,6 +488,8 @@ slurm_errtab_t slurm_errtab[] = {
 	  "Invalid job state" },
 	{ ERRTAB_ENTRY(ESLURM_BREAK_EVAL),
 	  "Break common_topo_choose_nodes" },
+	{ ERRTAB_ENTRY(ESLURM_RETRY_EVAL),
+	  "Remove nodes and retry eval_nodes" },
 
 	/* SPANK errors */
 	{ ERRTAB_ENTRY(ESPANK_ERROR),
