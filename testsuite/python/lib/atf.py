6e47c98dadba7841f83f793b73f171dc1bf84038
Albert Gil <albert.gil@schedmd.com>
2025-06-25 15:47:01 +0200

    Testsuite - Improve testsuite creating SlurmdSpoolDir and TmpFS per node
    Ticket: 20952
    Signed-off-by: Nathan Prisbrey <nathan@schedmd.com>
    Cherry-picked: 33e8709837

--- testsuite/python/lib/atf.py
+++ testsuite/python/lib/atf.py
@@ -3740,7 +3740,7 @@ def create_node(node_dict):
     node_line = ""
     if "NodeName" in node_dict:
         node_line = f"NodeName={node_dict['NodeName']}"
-        node_dict.pop("NodeName")
+        node_range = node_dict.pop("NodeName")
     if "Port" in node_dict:
         node_line += f" Port={node_dict['Port']}"
         node_dict.pop("Port")
@@ -3759,6 +3759,24 @@ def create_node(node_dict):
         fatal=True,
         quiet=True,
     )
+
+    # Ensure that slurm-spool-dir and slurm-tmpfs are set.
+    if "slurm-spool-dir" not in properties:
+        properties["slurm-spool-dir"] = get_config_parameter(
+            "SlurmdSpoolDir", live=False, quiet=True
+        )
+    if "slurm-tmpfs" not in properties:
+        properties["slurm-tmpfs"] = get_config_parameter(
+            "TmpFS", live=False, quiet=True
+        )
+
+    # Create the required node directories
+    for node_name in node_range_to_list(node_range):
+        spool_dir = properties["slurm-spool-dir"].replace("%n", node_name)
+        tmpfs_dir = properties["slurm-tmpfs"].replace("%n", node_name)
+
+        run_command(f"sudo mkdir -p {spool_dir}", fatal=True)
+        run_command(f"sudo mkdir -p {tmpfs_dir}", fatal=True)
 
     # Restart slurm if it is already running
     if is_slurmctld_running(quiet=True):
