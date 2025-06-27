SchedMD/slurm

commit 6e56eadf2450846ad6fe686219941f07e570c89f

Add exe_fname to libdir_rec_t
Bug 18562

src/slurmd/slurmd/req.c
@@ -125,6 +125,7 @@ typedef struct {
 	uint32_t uid;
 	uint32_t job_id;
 	uint32_t step_id;
+	char *exe_fname;
 	char *directory;
 	time_t last_update;
 } libdir_rec_t;
@@ -1406,6 +1407,8 @@ static int _find_libdir_record(void *x, void *arg)
 	if (l->job_id != key->job_id)
 		return 0;
 	if (l->step_id != key->step_id)
+		return 0;
+	if (xstrcmp(l->exe_fname, key->exe_fname))
 		return 0;
 
 	return 1;
@@ -4036,6 +4039,7 @@ static void _free_libdir_rec_t(void *x)
 		return;
 
 	xfree(l->directory);
+	xfree(l->exe_fname);
 	xfree(l);
 }
 
@@ -4158,6 +4162,7 @@ static void _rpc_file_bcast(slurm_msg_t *msg)
 			.uid = key.uid,
 			.job_id = key.job_id,
 			.step_id = key.step_id,
+			.exe_fname = req->exe_fname,
 		};
 		char *fname = NULL;
 
@@ -4323,6 +4328,7 @@ static int _file_bcast_register_file(slurm_msg_t *msg,
 		libdir->job_id = key->job_id;
 		libdir->step_id = key->step_id;
 		libdir->directory = directory;
+		libdir->exe_fname = xstrdup(key->fname);
 		libdir->last_update = time(NULL);
 	}
 

commit a7e544c038953a5fc7d50c5f7678e31de1d8bba8

Fix 'srun --send-libs --bcast=<path>' when <path> ends with a slash
When broadcasting a shared object, fixup the executable name in
libdir_args if necessary so the .so will be associated with the correct
executable.

Regression from 6e56eadf245.

Ticket: 22967
Changelog: Fix regression introduced in 24.05 for srun --bcast=<path> when
 libraries are also sent and <path> ends with a '/' (slash).

src/slurmd/slurmd/req.c
@@ -4461,14 +4461,26 @@ static void _rpc_file_bcast(slurm_msg_t *msg)
 			.uid = key.uid,
 			.job_id = key.job_id,
 			.step_id = key.step_id,
-			.exe_fname = req->exe_fname,
+			.exe_fname = xstrdup(req->exe_fname),
 		};
 		char *fname = NULL;
+		size_t exe_fname_len = strlen(libdir_args.exe_fname);
+
+		if (libdir_args.exe_fname[exe_fname_len - 1] == '/')
+			/*
+			 * Append the default filename to the executable path in
+			 * the search key so this shared object is associated
+			 * with the correct libdir entry.
+			 */
+			xstrfmtcat(libdir_args.exe_fname,
+				   "slurm_bcast_%u.%u_%s", cred_arg->job_id,
+				   cred_arg->step_id, conf->node_name);
 
 		slurm_rwlock_rdlock(&file_bcast_lock);
-		if (!(libdir = list_find_first(bcast_libdir_list,
-					       _find_libdir_record,
-					       &libdir_args))) {
+		libdir = list_find_first(bcast_libdir_list, _find_libdir_record,
+					 &libdir_args);
+		xfree(libdir_args.exe_fname);
+		if (!libdir) {
 			error("Could not find library directory for transfer from uid %u",
 			      key.uid);
 			slurm_rwlock_unlock(&file_bcast_lock);