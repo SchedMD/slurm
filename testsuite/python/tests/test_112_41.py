97c9ace5115830714e9c60eaaac9406b5d76f096
Nathan Rini <nate@schedmd.com>
2025-06-30 16:54:24 -0600

    Testsuite - Add test case for openapi.json changes in 112_41
    Generated on tag slurm-24-05-0-1 @ 1caf6e131e.
    
    Issue: 50536

--- testsuite/python/tests/test_112_41.py
+++ testsuite/python/tests/test_112_41.py
@@ -5,6 +5,7 @@ import atf
 import getpass
 import pytest
 import random
+import os
 
 # import json
 # import logging
@@ -222,6 +223,11 @@ def test_loaded_versions():
     # verify current plugins are loaded
     assert "/slurm/v0.0.41/jobs/" in spec["paths"].keys()
     assert "/slurmdb/v0.0.41/jobs/" in spec["paths"].keys()
+
+
+@pytest.mark.xfail(reason="Ticket 23807: Schema changed")
+def test_specification():
+    atf.assert_openapi_spec(os.path.splitext(__file__)[0] + ".json")
 
 
 def test_db_accounts(slurm, slurmdb, create_wckeys, admin_level):
