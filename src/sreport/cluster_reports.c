SchedMD/slurm

commit 5092dae5f17d6a7a61076f7235941ed2c8da9a98

sreport - moved 'planned' format field higher so it can be recognized
Ticket 19809

src/sreport/cluster_reports.c
@@ -447,6 +447,19 @@ static int _setup_print_fields_list(List format_list)
 			else
 				field->len = 9;
 			field->print_routine = print_fields_str;
+		} else if (!xstrncasecmp("planned", object,
+					 MAX(command_len, 4)) ||
+			   !xstrncasecmp("reserved", object,
+					 MAX(command_len, 3))) {
+			field->type = PRINT_CLUSTER_TRES_PLAN;
+			field->name = xstrdup("Planned");
+			if (time_format == SLURMDB_REPORT_TIME_SECS_PER
+			    || time_format == SLURMDB_REPORT_TIME_MINS_PER
+			    || time_format == SLURMDB_REPORT_TIME_HOURS_PER)
+				field->len = 18;
+			else
+				field->len = 9;
+			field->print_routine = print_fields_str;
 		} else if (!xstrncasecmp("PlannedDown", object,
 					 MAX(command_len, 2))) {
 			field->type = PRINT_CLUSTER_TRES_PLAN_DOWN;
@@ -474,19 +487,6 @@ static int _setup_print_fields_list(List format_list)
 				field->len = 20;
 			else
 				field->len = 12;
-			field->print_routine = print_fields_str;
-		} else if (!xstrncasecmp("reserved", object,
-					 MAX(command_len, 3)) ||
-			   !xstrncasecmp("planned", object,
-					 MAX(command_len, 4))) {
-			field->type = PRINT_CLUSTER_TRES_PLAN;
-			field->name = xstrdup("Planned");
-			if (time_format == SLURMDB_REPORT_TIME_SECS_PER
-			   || time_format == SLURMDB_REPORT_TIME_MINS_PER
-			   || time_format == SLURMDB_REPORT_TIME_HOURS_PER)
-				field->len = 18;
-			else
-				field->len = 9;
 			field->print_routine = print_fields_str;
 		} else if (!xstrncasecmp("TresCount", object,
 					 MAX(command_len, 5)) ||

commit dce66015084528510952cad15f45bf9accf49335

Correctly print PLNDDown in sreport cluster utilization
Regression from 5092dae5f1.

Ticket: 23146
Changelog: sreport - Fix Planned being printed instead of Planned Down
 by default in the cluster utilization report.

src/sreport/cluster_reports.c
@@ -1676,10 +1676,10 @@ extern int cluster_utilization(int argc, char **argv)
 	if (!list_count(format_list)) {
 		if (tres_str) {
 			slurm_addto_char_list(format_list,
-					      "Cl,TresName,al,d,planned,i,res,rep");
+					      "Cl,TresName,al,d,PLNDDown,i,res,rep");
 		} else {
 			slurm_addto_char_list(format_list,
-					      "Cl,al,d,planned,i,res,rep");
+					      "Cl,al,d,PLNDDown,i,res,rep");
 		}
 	}
 