e38651c779e14e94fbb5e4e51c2573dff9ea77cc
Brian Gregory <brian.gregory@schedmd.com>
2025-07-23 11:20:32 -0400

    Add 25.11 protocol block for _[un]pack_register_ctld_msg()
    Ticket: 23097

--- src/common/slurmdbd_pack.c
+++ src/common/slurmdbd_pack.c
@@ -1000,7 +1000,11 @@ unpack_error:
 static void _pack_register_ctld_msg(dbd_register_ctld_msg_t *msg,
 				    uint16_t rpc_version, buf_t *buffer)
 {
-	if (rpc_version >= SLURM_MIN_PROTOCOL_VERSION) {
+	if (rpc_version >= SLURM_25_11_PROTOCOL_VERSION) {
+		pack16(msg->dimensions, buffer);
+		pack32(msg->flags, buffer);
+		pack16(msg->port, buffer);
+	} else if (rpc_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		pack16(msg->dimensions, buffer);
 		pack32(msg->flags, buffer);
 		pack16(msg->port, buffer);
@@ -1014,7 +1018,11 @@ static int _unpack_register_ctld_msg(dbd_register_ctld_msg_t **msg,
 		sizeof(dbd_register_ctld_msg_t));
 	*msg = msg_ptr;
 
-	if (rpc_version >= SLURM_MIN_PROTOCOL_VERSION) {
+	if (rpc_version >= SLURM_25_11_PROTOCOL_VERSION) {
+		safe_unpack16(&msg_ptr->dimensions, buffer);
+		safe_unpack32(&msg_ptr->flags, buffer);
+		safe_unpack16(&msg_ptr->port, buffer);
+	} else if (rpc_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		safe_unpack16(&msg_ptr->dimensions, buffer);
 		safe_unpack32(&msg_ptr->flags, buffer);
 		safe_unpack16(&msg_ptr->port, buffer);
