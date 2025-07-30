SchedMD/slurm

commit 64f49258b3126ab512224bd6939782b6dc820b66

Add step launch flag for --wait-for-children
Issue: 50219

src/common/slurm_protocol_defs.h
@@ -649,6 +649,7 @@ typedef struct job_step_create_response_msg {
 #define LAUNCH_OVERCOMMIT 	SLURM_BIT(7)
 #define LAUNCH_NO_SIG_FAIL 	SLURM_BIT(8)
 #define LAUNCH_GRES_ALLOW_TASK_SHARING SLURM_BIT(9)
+#define LAUNCH_WAIT_FOR_CHILDREN SLURM_BIT(9)
 
 typedef struct launch_tasks_request_msg {
 	uint32_t  het_job_node_offset;	/* Hetjob node offset or NO_VAL */

src/api/step_launch.c
@@ -330,6 +330,8 @@ extern int slurm_step_launch(slurm_step_ctx_t *ctx,
 		launch.flags |= LAUNCH_EXT_LAUNCHER;
 	if (ctx->step_req->flags & SSF_GRES_ALLOW_TASK_SHARING)
 		launch.flags |= LAUNCH_GRES_ALLOW_TASK_SHARING;
+	if (ctx->step_req->flags & SSF_WAIT_FOR_CHILDREN)
+		launch.flags |= LAUNCH_WAIT_FOR_CHILDREN;
 
 	launch.task_dist	= params->task_dist;
 	if (params->pty)

commit b493d809ca03cb588a7951181b02d3bad382d417

Fix LAUNCH_WAIT_FOR_CHILDREN SLURM_BIT
LAUNCH_WAIT_FOR_CHILDREN was never meant to overlap with
LAUNCH_GRES_ALLOW_TASK_SHARING, rather it was meant to be a new
SLURM_BIT value.

Fixes regression introduced in 64f49258b31.

Ticket: 23346
Changelog: Fix issue with --wait-for-children incorrectly implying
 --gres-flags=allow-task-sharing and vice-versa. These options are now
 handled independently as originally intended. Note that upgraded
 daemons will not honor the --wait-for-children option from older
 clients, and clients will need to be upgraded immediately alongside
 daemons in order to use --wait-for-children.

src/common/slurm_protocol_defs.h
@@ -652,7 +652,7 @@ typedef struct job_step_create_response_msg {
 #define LAUNCH_OVERCOMMIT 	SLURM_BIT(7)
 #define LAUNCH_NO_SIG_FAIL 	SLURM_BIT(8)
 #define LAUNCH_GRES_ALLOW_TASK_SHARING SLURM_BIT(9)
-#define LAUNCH_WAIT_FOR_CHILDREN SLURM_BIT(9)
+#define LAUNCH_WAIT_FOR_CHILDREN SLURM_BIT(10)
 
 typedef struct launch_tasks_request_msg {
 	uint32_t  het_job_node_offset;	/* Hetjob node offset or NO_VAL */