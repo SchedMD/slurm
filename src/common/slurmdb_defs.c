SchedMD/slurm

commit 928a18e165bd02e062ba912c8e73556034a2d969

Move boolean variables into a 'flags' variable for slurmdb_account_cond_t
Ticket 18258

src/common/slurmdb_defs.c
@@ -1893,6 +1893,73 @@ extern char *slurmdb_acct_flags_2_str(slurmdb_acct_flags_t flags)
 	return acct_flags;
 }
 
+#define T(flag, str) { flag, XSTRINGIFY(flag), str }
+static const struct {
+	slurmdb_assoc_flags_t flag;
+	char *flag_str;
+	char *str;
+} slurmdb_assoc_flags_map[] = {
+	T(ASSOC_FLAG_DELETED, "Deleted"),
+	T(ASSOC_FLAG_NO_UPDATE, "NoUpdate"),
+	T(ASSOC_FLAG_EXACT, "Exact"),
+	T(ASSOC_FLAG_INVALID, "INVALID"),
+};
+#undef T
+
+static slurmdb_assoc_flags_t _str_2_assoc_flag(char *flag_in)
+{
+	if (!flag_in || !flag_in[0])
+		return ASSOC_FLAG_NONE;
+
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_assoc_flags_map); i++)
+		if (!xstrncasecmp(flag_in, slurmdb_assoc_flags_map[i].str,
+				  strlen(flag_in)))
+			return slurmdb_assoc_flags_map[i].flag;
+
+	debug("%s: Unable to match %s to a slurmdbd_assoc_flags_t flag",
+	      __func__, flag_in);
+	return ASSOC_FLAG_INVALID;
+}
+
+extern slurmdb_assoc_flags_t str_2_slurmdb_assoc_flags(char *flags_in)
+{
+	slurmdb_assoc_flags_t assoc_flags = 0;
+	char *token, *my_flags, *last = NULL;
+
+	my_flags = xstrdup(flags_in);
+	token = strtok_r(my_flags, ",", &last);
+	while (token) {
+		slurmdb_assoc_flags_t f = _str_2_assoc_flag(token);
+
+		if (f == ASSOC_FLAG_INVALID)
+			return ASSOC_FLAG_INVALID;
+
+		assoc_flags |= f;
+		token = strtok_r(NULL, ",", &last);
+	}
+	xfree(my_flags);
+
+	return assoc_flags;
+}
+
+extern char *slurmdb_assoc_flags_2_str(slurmdb_assoc_flags_t flags)
+{
+	char *assoc_flags = NULL, *at = NULL;
+
+	if (flags == ASSOC_FLAG_NONE)
+		return xstrdup("None");
+
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_assoc_flags_map); i++) {
+		if ((slurmdb_assoc_flags_map[i].flag & flags) ==
+		    slurmdb_assoc_flags_map[i].flag)
+			xstrfmtcatat(assoc_flags, &at, "%s%s",
+				     (assoc_flags ? "," : ""),
+				     slurmdb_assoc_flags_map[i].str);
+	}
+
+	return assoc_flags;
+}
+
 static uint32_t _str_2_federation_flags(char *flags)
 {
 	return 0;

src/common/slurmdb_defs.h
@@ -125,6 +125,8 @@ extern slurmdb_qos_usage_t *slurmdb_create_qos_usage(int tres_cnt);
 
 extern char *slurmdb_acct_flags_2_str(slurmdb_acct_flags_t flags);
 extern slurmdb_acct_flags_t str_2_slurmdb_acct_flags(char *flag_str);
+extern char *slurmdb_assoc_flags_2_str(slurmdb_assoc_flags_t flags);
+extern slurmdb_assoc_flags_t str_2_slurmdb_assoc_flags(char *flag_str);
 extern char *slurmdb_cluster_fed_states_str(uint32_t states);
 extern uint32_t str_2_cluster_fed_states(char *states);
 extern char *slurmdb_federation_flags_str(uint32_t flags);

slurm/slurmdb.h
@@ -245,12 +245,21 @@ enum cluster_fed_states {
 #define CLUSTER_FLAG_EXT    SLURM_BIT(12) /* This cluster is external */
 
 /* Assoc flags */
-#define ASSOC_FLAG_DELETED  SLURM_BIT(0)
-#define ASSOC_FLAG_NO_UPDATE SLURM_BIT(1)
-#define ASSOC_FLAG_EXACT SLURM_BIT(2) /* If looking for a partition based
-				       * association don't return SUCCESS for a
-				       * non-partition based association when
-				       * calling assoc_mgr_fill_in_assoc() */
+typedef enum {
+	ASSOC_FLAG_NONE = 0,
+	ASSOC_FLAG_DELETED = SLURM_BIT(0),
+	ASSOC_FLAG_NO_UPDATE = SLURM_BIT(1),
+	ASSOC_FLAG_EXACT = SLURM_BIT(2), /* If looking for a partition based
+					  * association don't return SUCCESS for
+					  * a non-partition based association
+					  * when calling
+					  * assoc_mgr_fill_in_assoc() */
+
+	/* Anything above this (0-15) will not be stored in the database. */
+	ASSOC_FLAG_BASE = 0x0000ffff,
+
+	ASSOC_FLAG_INVALID
+} slurmdb_assoc_flags_t;
 
 /* Event condition flags */
 #define SLURMDB_EVENT_COND_OPEN SLURM_BIT(0) /* Return only open events */
@@ -494,7 +503,7 @@ typedef struct slurmdb_assoc_rec {
 
 	uint32_t def_qos_id;       /* Which QOS id is this
 				    * associations default */
-	uint32_t flags;            /* various flags see ASSOC_FLAG_* */
+	slurmdb_assoc_flags_t flags; /* various flags see ASSOC_FLAG_* */
 	uint32_t grp_jobs;	   /* max number of jobs the
 				    * underlying group of associations can run
 				    * at one time */

src/interfaces/data_parser.h
@@ -125,7 +125,7 @@ typedef enum {
 	DATA_PARSER_ASSOC_SHORT_PTR, /* slurmdb_assoc_rec_t* (for id only) */
 	DATA_PARSER_ASSOC, /* slurmdb_assoc_rec_t */
 	DATA_PARSER_ASSOC_PTR, /* slurmdb_assoc_rec_t* */
-	DATA_PARSER_ASSOC_FLAGS, /* slurmdb_assoc_rec_t->flags & ASSOC_FLAG_* */
+	DATA_PARSER_ASSOC_FLAGS, /* slurmdb_assoc_flags_t */
 	DATA_PARSER_ASSOC_USAGE, /* slurmdb_assoc_usage_t */
 	DATA_PARSER_ASSOC_USAGE_PTR, /* slurmdb_assoc_usage_t* */
 	DATA_PARSER_ASSOC_REC_SET, /* slurmdb_assoc_rec_t */

src/plugins/data_parser/v0.0.39/parsers.c
@@ -6598,7 +6598,7 @@ static const parser_t parsers[] = {
 	addpa(UPDATE_NODE_MSG, update_node_msg_t),
 
 	/* Flag bit arrays */
-	addfa(ASSOC_FLAGS, uint16_t),
+	addfa(ASSOC_FLAGS, slurmdb_assoc_flags_t),
 	addfa(USER_FLAGS, uint32_t),
 	addfa(SLURMDB_JOB_FLAGS, uint32_t),
 	addfa(ACCOUNT_FLAGS, uint32_t),

src/plugins/data_parser/v0.0.40/parsers.c
@@ -9613,7 +9613,7 @@ static const parser_t parsers[] = {
 	addoar(OPENAPI_KILL_JOBS_RESP),
 
 	/* Flag bit arrays */
-	addfa(ASSOC_FLAGS, uint16_t),
+	addfa(ASSOC_FLAGS, slurmdb_assoc_flags_t),
 	addfa(USER_FLAGS, uint32_t),
 	addfa(SLURMDB_JOB_FLAGS, uint32_t),
 	addfa(ACCOUNT_FLAGS, uint32_t),

src/plugins/data_parser/v0.0.41/parsers.c
@@ -6608,7 +6608,7 @@ static const parser_t PARSER_ARRAY(ASSOC)[] = {
 	add_parse(STRING, cluster, "cluster", NULL),
 	add_parse(STRING, comment, "comment", "comment for the association"),
 	add_parse(QOS_ID, def_qos_id, "default/qos", NULL),
-	add_parse_bit_flag_array(slurmdb_assoc_rec_t, ASSOC_FLAGS, false, flags, "flags", NULL),
+	add_parse(ASSOC_FLAGS, flags, "flags", "Active flags"),
 	add_parse(UINT32_NO_VAL, grp_jobs, "max/jobs/per/count", NULL),
 	add_parse(UINT32_NO_VAL, grp_jobs_accrue, "max/jobs/per/accruing", NULL),
 	add_parse(UINT32_NO_VAL, grp_submit_jobs, "max/jobs/per/submitted", NULL),
@@ -9953,7 +9953,7 @@ static const parser_t parsers[] = {
 	addpap(OPENAPI_JOB_ALLOC_RESP, openapi_job_alloc_response_t, NULL, NULL),
 
 	/* Flag bit arrays */
-	addfa(ASSOC_FLAGS, uint16_t),
+	addfa(ASSOC_FLAGS, slurmdb_assoc_flags_t),
 	addfa(USER_FLAGS, uint32_t),
 	addfa(SLURMDB_JOB_FLAGS, uint32_t),
 	addfa(ACCOUNT_FLAGS, uint32_t),

commit d7c39ede364759b05ec0b0630429e14e2ec953ff

Capture errors with str_2_slurmdb_assoc_flags()
Change around prototype for str_2_slurmdb_assoc_flags() to provide status
code on parsing of flags instead attempting to use ASSOC_FLAG_INVALID as
flag.

Regression from 928a18e165.

Ticket: 23127

src/common/slurmdb_defs.c
@@ -1892,41 +1892,41 @@ static const struct {
 };
 #undef T
 
-static slurmdb_assoc_flags_t _str_2_assoc_flag(char *flag_in)
+static int _str_2_assoc_flag(const char *flag_in,
+			     slurmdb_assoc_flags_t *flags_ptr)
 {
 	if (!flag_in || !flag_in[0])
-		return ASSOC_FLAG_NONE;
+		return SLURM_SUCCESS;
 
-	for (int i = 0; i < ARRAY_SIZE(slurmdb_assoc_flags_map); i++)
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_assoc_flags_map); i++) {
 		if (!xstrncasecmp(flag_in, slurmdb_assoc_flags_map[i].str,
-				  strlen(flag_in)))
+				  strlen(flag_in))) {
+			*flags_ptr |= slurmdb_assoc_flags_map[i].flag;
 			return slurmdb_assoc_flags_map[i].flag;
+		}
+	}
 
 	debug("%s: Unable to match %s to a slurmdbd_assoc_flags_t flag",
 	      __func__, flag_in);
-	return ASSOC_FLAG_INVALID;
+	return EINVAL;
 }
 
-extern slurmdb_assoc_flags_t str_2_slurmdb_assoc_flags(char *flags_in)
+extern int str_2_slurmdb_assoc_flags(const char *str,
+				     slurmdb_assoc_flags_t *flags_ptr)
 {
-	slurmdb_assoc_flags_t assoc_flags = 0;
 	char *token, *my_flags, *last = NULL;
+	int rc = SLURM_SUCCESS;
 
-	my_flags = xstrdup(flags_in);
+	/* Always reset flags */
+	*flags_ptr = ASSOC_FLAG_NONE;
+
+	my_flags = xstrdup(str);
 	token = strtok_r(my_flags, ",", &last);
-	while (token) {
-		slurmdb_assoc_flags_t f = _str_2_assoc_flag(token);
-
-		if (f == ASSOC_FLAG_INVALID) {
-			assoc_flags = ASSOC_FLAG_INVALID;
-			break;
-		}
-		assoc_flags |= f;
+	while (token && !(rc = _str_2_assoc_flag(token, flags_ptr)))
 		token = strtok_r(NULL, ",", &last);
-	}
-	xfree(my_flags);
 
-	return assoc_flags;
+	xfree(my_flags);
+	return rc;
 }
 
 extern char *slurmdb_assoc_flags_2_str(slurmdb_assoc_flags_t flags)

src/common/slurmdb_defs.h
@@ -129,7 +129,15 @@ extern char *slurmdb_acct_flags_2_str(slurmdb_acct_flags_t flags);
 extern int str_2_slurmdb_acct_flags(const char *str,
 				    slurmdb_acct_flags_t *flags_ptr);
 extern char *slurmdb_assoc_flags_2_str(slurmdb_assoc_flags_t flags);
-extern slurmdb_assoc_flags_t str_2_slurmdb_assoc_flags(char *flag_str);
+/*
+ * Parse CSV of associations flags
+ *
+ * IN str - CSV of associations flags to parse
+ * OUT flags_ptr - pointer to populate based on parsed flags (ASSOC_FLAG_*)
+ * RET SLURM_SUCCESS or error
+ */
+extern int str_2_slurmdb_assoc_flags(const char *str,
+				     slurmdb_assoc_flags_t *flags_ptr);
 extern char *slurmdb_cluster_fed_states_str(uint32_t states);
 extern uint32_t str_2_cluster_fed_states(char *states);
 extern char *slurmdb_federation_flags_str(uint32_t flags);