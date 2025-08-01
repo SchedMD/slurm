SchedMD/slurm

commit 8b96d710cd765fb90d986bb4d7a57ff91ec31ee6

Add functions to deal with account flags.
Ticket 18258

src/common/slurmdb_defs.c
@@ -1824,6 +1824,71 @@ extern char *slurmdb_federation_flags_str(uint32_t flags)
 	return federation_flags;
 }
 
+#define T(flag, str) { flag, XSTRINGIFY(flag), str }
+static const struct {
+	slurmdb_acct_flags_t flag;
+	char *flag_str;
+	char *str;
+} slurmdb_acct_flags_map[] = {
+	T(SLURMDB_ACCT_FLAG_DELETED, "Deleted"),
+	T(SLURMDB_ACCT_FLAG_INVALID, "INVALID"),
+};
+#undef T
+
+static slurmdb_acct_flags_t _str_2_acct_flag(char *flag_in)
+{
+	if (!flag_in || !flag_in[0])
+		return SLURMDB_ACCT_FLAG_NONE;
+
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_acct_flags_map); i++)
+		if (!xstrncasecmp(flag_in, slurmdb_acct_flags_map[i].str,
+				  strlen(flag_in)))
+			return slurmdb_acct_flags_map[i].flag;
+
+	debug("%s: Unable to match %s to a slurmdbd_acct_flags_t flag",
+	      __func__, flag_in);
+	return SLURMDB_ACCT_FLAG_INVALID;
+}
+
+extern slurmdb_acct_flags_t str_2_slurmdb_acct_flags(char *flags_in)
+{
+	slurmdb_acct_flags_t acct_flags = 0;
+	char *token, *my_flags, *last = NULL;
+
+	my_flags = xstrdup(flags_in);
+	token = strtok_r(my_flags, ",", &last);
+	while (token) {
+		slurmdb_acct_flags_t f = _str_2_acct_flag(token);
+
+		if (f == SLURMDB_ACCT_FLAG_INVALID)
+			return SLURMDB_ACCT_FLAG_INVALID;
+
+		acct_flags |= f;
+		token = strtok_r(NULL, ",", &last);
+	}
+	xfree(my_flags);
+
+	return acct_flags;
+}
+
+extern char *slurmdb_acct_flags_2_str(slurmdb_acct_flags_t flags)
+{
+	char *acct_flags = NULL, *at = NULL;
+
+	if (flags == SLURMDB_ACCT_FLAG_NONE)
+		return xstrdup("None");
+
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_acct_flags_map); i++) {
+		if ((slurmdb_acct_flags_map[i].flag & flags) ==
+		    slurmdb_acct_flags_map[i].flag)
+			xstrfmtcatat(acct_flags, &at, "%s%s",
+				     (acct_flags ? "," : ""),
+				     slurmdb_acct_flags_map[i].str);
+	}
+
+	return acct_flags;
+}
+
 static uint32_t _str_2_federation_flags(char *flags)
 {
 	return 0;

src/common/slurmdb_defs.h
@@ -123,6 +123,8 @@ extern slurmdb_step_rec_t *slurmdb_create_step_rec(void);
 extern slurmdb_assoc_usage_t *slurmdb_create_assoc_usage(int tres_cnt);
 extern slurmdb_qos_usage_t *slurmdb_create_qos_usage(int tres_cnt);
 
+extern char *slurmdb_acct_flags_2_str(slurmdb_acct_flags_t flags);
+extern slurmdb_acct_flags_t str_2_slurmdb_acct_flags(char *flag_str);
 extern char *slurmdb_cluster_fed_states_str(uint32_t states);
 extern uint32_t str_2_cluster_fed_states(char *states);
 extern char *slurmdb_federation_flags_str(uint32_t flags);

slurm/slurmdb.h
@@ -373,6 +373,13 @@ typedef struct {
 
 /************** alphabetical order of structures **************/
 
+typedef enum {
+	SLURMDB_ACCT_FLAG_NONE = 0,
+	SLURMDB_ACCT_FLAG_DELETED = SLURM_BIT(0),
+
+	SLURMDB_ACCT_FLAG_INVALID
+} slurmdb_acct_flags_t;
+
 typedef struct {
 	slurmdb_assoc_cond_t *assoc_cond;/* use acct_list here for
 						  names */
@@ -383,16 +390,11 @@ typedef struct {
 	uint16_t with_deleted;
 } slurmdb_account_cond_t;
 
-enum {
-	SLURMDB_ACCT_FLAG_NONE          = 0,
-	SLURMDB_ACCT_FLAG_DELETED       = SLURM_BIT(0),
-};
-
 typedef struct {
 	List assoc_list; /* list of slurmdb_assoc_rec_t *'s */
 	List coordinators; /* list of slurmdb_coord_rec_t *'s */
 	char *description;
-	uint32_t flags; /* SLURMDB_ACCT_FLAG_* */
+	slurmdb_acct_flags_t flags; /* SLURMDB_ACCT_FLAG_* */
 	char *name;
 	char *organization;
 } slurmdb_account_rec_t;

commit 5bf78fdb70287d12003dd5c459aad0b08099b4d3

Capture errors with str_2_slurmdb_acct_flags()
Change around prototype for str_2_slurmdb_acct_flags() to provide status
code on parsing of flags instead attempting to use
SLURMDB_ACCT_FLAG_INVALID as flag.

Regression from 8b96d710cd.

Ticket: 23127
Changelog: sacctmgr - Catch and reject attempts to pass invalid account
 flags= arguments.

src/common/slurmdb_defs.c
@@ -1823,42 +1823,41 @@ static const struct {
 };
 #undef T
 
-static slurmdb_acct_flags_t _str_2_acct_flag(char *flag_in)
+static int _str_2_acct_flag(const char *flag_in,
+			    slurmdb_acct_flags_t *flags_ptr)
 {
 	if (!flag_in || !flag_in[0])
-		return SLURMDB_ACCT_FLAG_NONE;
+		return SLURM_SUCCESS;
 
-	for (int i = 0; i < ARRAY_SIZE(slurmdb_acct_flags_map); i++)
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_acct_flags_map); i++) {
 		if (!xstrncasecmp(flag_in, slurmdb_acct_flags_map[i].str,
-				  strlen(flag_in)))
-			return slurmdb_acct_flags_map[i].flag;
+				  strlen(flag_in))) {
+			*flags_ptr |= slurmdb_acct_flags_map[i].flag;
+			return SLURM_SUCCESS;
+		}
+	}
 
 	debug("%s: Unable to match %s to a slurmdbd_acct_flags_t flag",
 	      __func__, flag_in);
-	return SLURMDB_ACCT_FLAG_INVALID;
+	return EINVAL;
 }
 
-extern slurmdb_acct_flags_t str_2_slurmdb_acct_flags(char *flags_in)
+extern int str_2_slurmdb_acct_flags(const char *str,
+				    slurmdb_acct_flags_t *flags_ptr)
 {
-	slurmdb_acct_flags_t acct_flags = 0;
 	char *token, *my_flags, *last = NULL;
+	int rc = SLURM_SUCCESS;
 
-	my_flags = xstrdup(flags_in);
+	/* Always reset flags */
+	*flags_ptr = SLURMDB_ACCT_FLAG_NONE;
+
+	my_flags = xstrdup(str);
 	token = strtok_r(my_flags, ",", &last);
-	while (token) {
-		slurmdb_acct_flags_t f = _str_2_acct_flag(token);
-
-		if (f == SLURMDB_ACCT_FLAG_INVALID) {
-			acct_flags = SLURMDB_ACCT_FLAG_INVALID;
-			break;
-		}
-
-		acct_flags |= f;
+	while (token && !(rc = _str_2_acct_flag(token, flags_ptr)))
 		token = strtok_r(NULL, ",", &last);
-	}
-	xfree(my_flags);
 
-	return acct_flags;
+	xfree(my_flags);
+	return rc;
 }
 
 extern char *slurmdb_acct_flags_2_str(slurmdb_acct_flags_t flags)

src/common/slurmdb_defs.h
@@ -118,7 +118,16 @@ extern slurmdb_assoc_usage_t *slurmdb_create_assoc_usage(int tres_cnt);
 extern slurmdb_qos_usage_t *slurmdb_create_qos_usage(int tres_cnt);
 
 extern char *slurmdb_acct_flags_2_str(slurmdb_acct_flags_t flags);
-extern slurmdb_acct_flags_t str_2_slurmdb_acct_flags(char *flag_str);
+
+/*
+ * Parse CSV of accounting flags
+ *
+ * IN str - CSV of accounting flags to parse
+ * OUT flags_ptr - pointer to populate based on parsed flags (SLURMDB_ACCT_FLAG_*)
+ * RET SLURM_SUCCESS or error
+ */
+extern int str_2_slurmdb_acct_flags(const char *str,
+				    slurmdb_acct_flags_t *flags_ptr);
 extern char *slurmdb_assoc_flags_2_str(slurmdb_assoc_flags_t flags);
 extern slurmdb_assoc_flags_t str_2_slurmdb_assoc_flags(char *flag_str);
 extern char *slurmdb_cluster_fed_states_str(uint32_t states);

src/sacctmgr/account_functions.c
@@ -129,9 +129,19 @@ static int _set_cond(int *start, int argc, char **argv,
 				cond_set |= SA_SET_USER;
 		} else if (!xstrncasecmp(argv[i], "Flags",
 					 MAX(command_len, 2))) {
-			acct_cond->flags |= str_2_slurmdb_acct_flags(
-				argv[i]+end);
-			cond_set |= SA_SET_USER;
+			const char *flag_str = (argv[i] + end);
+			slurmdb_acct_flags_t *flags_ptr = &acct_cond->flags;
+			int rc;
+
+			if ((rc = str_2_slurmdb_acct_flags(flag_str,
+							   flags_ptr))) {
+				fprintf(stderr, " Unknown Flag(s)=%s\n",
+					flag_str);
+				exit_code = 1;
+				break;
+			} else {
+				cond_set |= SA_SET_USER;
+			}
 		} else if (!xstrncasecmp(argv[i], "Format",
 					 MAX(command_len, 2))) {
 			if (format_list)
@@ -228,8 +238,18 @@ static int _set_rec(int *start, int argc, char **argv,
 
 		} else if (!xstrncasecmp(argv[i], "Flags",
 					 MAX(command_len, 2))) {
-			acct->flags = str_2_slurmdb_acct_flags(argv[i]+end);
-			rec_set |= SA_SET_USER;
+			const char *flag_str = (argv[i] + end);
+			int rc;
+
+			if ((rc = str_2_slurmdb_acct_flags(flag_str,
+							   &acct->flags))) {
+				fprintf(stderr, " Unknown Flag(s)=%s\n",
+					flag_str);
+				exit_code = 1;
+				break;
+			} else {
+				rec_set |= SA_SET_USER;
+			}
 		} else if (!xstrncasecmp(argv[i], "Organization",
 					 MAX(command_len, 1))) {
 			acct->organization = strip_quotes(argv[i]+end, NULL, 1);