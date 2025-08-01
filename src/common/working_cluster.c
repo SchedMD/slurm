SchedMD/slurm

commit f1ff474f3c974048464208ab093bc0ab3d59ecb6

Move #defines to a typedef enum for cluster flags
Ticket: 21657

src/common/working_cluster.c
@@ -77,21 +77,38 @@ extern uint32_t slurmdb_setup_cluster_flags(void)
 	return cluster_flags;
 }
 
-static uint32_t _str_2_cluster_flags(char *flags_in)
+#define T(flag, str) { flag, XSTRINGIFY(flag), str }
+static const struct {
+	slurmdb_cluster_flags_t flag;
+	char *flag_str;
+	char *str;
+} slurmdb_cluster_flags_map[] = {
+	T(CLUSTER_FLAG_EXT, "External"),
+	T(CLUSTER_FLAG_FE, "FrontEnd"),
+	T(CLUSTER_FLAG_MULTSD, "MultipleSlurmd"),
+	T(CLUSTER_FLAG_INVALID, "INVALID"),
+};
+#undef T
+
+static slurmdb_cluster_flags_t _str_2_cluster_flags(char *flags_in)
 {
-	if (xstrcasestr(flags_in, "FrontEnd"))
-		return CLUSTER_FLAG_FE;
+	if (!flags_in || !flags_in[0])
+		return CLUSTER_FLAG_NONE;
 
-	if (xstrcasestr(flags_in, "MultipleSlurmd"))
-		return CLUSTER_FLAG_MULTSD;
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_cluster_flags_map); i++)
+		if (!xstrncasecmp(flags_in, slurmdb_cluster_flags_map[i].str,
+				  strlen(flags_in)))
+			return slurmdb_cluster_flags_map[i].flag;
 
-	return (uint32_t) 0;
+	debug("%s: Unable to match %s to a slurmdbd_cluster_flags_t flag",
+	      __func__, flags_in);
+	return CLUSTER_FLAG_INVALID;
 }
 
 
-extern uint32_t slurmdb_str_2_cluster_flags(char *flags_in)
+extern slurmdb_cluster_flags_t slurmdb_str_2_cluster_flags(char *flags_in)
 {
-	uint32_t cluster_flags = 0;
+	slurmdb_cluster_flags_t cluster_flags = 0;
 	char *token, *my_flags, *last = NULL;
 
 	my_flags = xstrdup(flags_in);
@@ -106,30 +123,20 @@ extern uint32_t slurmdb_str_2_cluster_flags(char *flags_in)
 }
 
 /* must xfree() returned string */
-extern char *slurmdb_cluster_flags_2_str(uint32_t flags_in)
+extern char *slurmdb_cluster_flags_2_str(slurmdb_cluster_flags_t flags_in)
 {
-	char *cluster_flags = NULL;
+	char *cluster_flags = NULL, *at = NULL;
 
-	if (flags_in & CLUSTER_FLAG_FE) {
-		if (cluster_flags)
-			xstrcat(cluster_flags, ",");
-		xstrcat(cluster_flags, "FrontEnd");
+	if (!flags_in)
+		return xstrdup("None");
+
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_cluster_flags_map); i++) {
+		if ((slurmdb_cluster_flags_map[i].flag & flags_in) ==
+		    slurmdb_cluster_flags_map[i].flag)
+			xstrfmtcatat(cluster_flags, &at, "%s%s",
+				     (cluster_flags ? "," : ""),
+				     slurmdb_cluster_flags_map[i].str);
 	}
-
-	if (flags_in & CLUSTER_FLAG_MULTSD) {
-		if (cluster_flags)
-			xstrcat(cluster_flags, ",");
-		xstrcat(cluster_flags, "MultipleSlurmd");
-	}
-
-	if (flags_in & CLUSTER_FLAG_EXT) {
-		if (cluster_flags)
-			xstrcat(cluster_flags, ",");
-		xstrcat(cluster_flags, "External");
-	}
-
-	if (!cluster_flags)
-		cluster_flags = xstrdup("None");
 
 	return cluster_flags;
 }

src/common/working_cluster.h
@@ -45,7 +45,7 @@ extern uint16_t slurmdb_setup_cluster_dims(void);
 extern uint32_t slurmdb_setup_cluster_flags(void);
 
 /* Translate architecture flag strings to their equivalent bitmaps */
-extern uint32_t slurmdb_str_2_cluster_flags(char *flags_in);
+extern slurmdb_cluster_flags_t slurmdb_str_2_cluster_flags(char *flags_in);
 
 /*
  * Translate architecture flag bitmaps to their equivalent comma-delimited
@@ -53,7 +53,7 @@ extern uint32_t slurmdb_str_2_cluster_flags(char *flags_in);
  *
  * NOTE: Call xfree() to release memory allocated to the return value
  */
-extern char *slurmdb_cluster_flags_2_str(uint32_t flags_in);
+extern char *slurmdb_cluster_flags_2_str(slurmdb_cluster_flags_t flags_in);
 
 /*
  * Setup the working_cluster_rec with the working_cluster_rec and node_addrs

slurm/slurmdb.h
@@ -239,20 +239,23 @@ enum cluster_fed_states {
 #define SLURMDB_CLASS_BASE      0x00ff
 
 /* Cluster flags */
-#define CLUSTER_FLAG_REGISTER SLURM_BIT(0) /* If the cluster is registering
-					    * right now or not */
-#define CLUSTER_FLAG_A2     SLURM_BIT(1) /* UNUSED */
-#define CLUSTER_FLAG_A3     SLURM_BIT(2) /* UNUSED */
-#define CLUSTER_FLAG_A4     SLURM_BIT(3) /* UNUSED */
-#define CLUSTER_FLAG_A5     SLURM_BIT(4) /* UNUSED */
-#define CLUSTER_FLAG_A6     SLURM_BIT(5) /* UNUSED */
-#define CLUSTER_FLAG_A7     SLURM_BIT(6) /* UNUSED */
-#define CLUSTER_FLAG_MULTSD SLURM_BIT(7) /* This cluster is multiple slurmd */
-#define CLUSTER_FLAG_A9     SLURM_BIT(8) /* UNUSED */
-#define CLUSTER_FLAG_FE     SLURM_BIT(9) /* This cluster is a front end system*/
-/*                          SLURM_BIT(10)   UNUSED */
-#define CLUSTER_FLAG_FED    SLURM_BIT(11) /* This cluster is in a federation. */
-#define CLUSTER_FLAG_EXT    SLURM_BIT(12) /* This cluster is external */
+typedef enum {
+	CLUSTER_FLAG_NONE = 0,
+	CLUSTER_FLAG_REGISTER = SLURM_BIT(0), /* Cluster is registering now */
+	/* SLURM_BIT(1) empty */
+	/* SLURM_BIT(2) empty */
+	/* SLURM_BIT(3) empty */
+	/* SLURM_BIT(4) empty */
+	/* SLURM_BIT(5) empty */
+	/* SLURM_BIT(6) empty */
+	CLUSTER_FLAG_MULTSD = SLURM_BIT(7), /* Cluster is multiple slurmd */
+	/* SLURM_BIT(8) empty */
+	CLUSTER_FLAG_FE = SLURM_BIT(9), /* This cluster is a front end system */
+	/* SLURM_BIT(10) empty */
+	CLUSTER_FLAG_FED = SLURM_BIT(11), /* This cluster is in a federation. */
+	CLUSTER_FLAG_EXT = SLURM_BIT(12), /* This cluster is external */
+	CLUSTER_FLAG_INVALID
+}  slurmdb_cluster_flags_t;
 
 /* Assoc flags */
 typedef enum {
@@ -715,7 +718,7 @@ typedef struct {
 	uint16_t classification; /* how this machine is classified */
 	list_t *cluster_list; /* list of char * */
 	list_t *federation_list; /* list of char */
-	uint32_t flags;
+	slurmdb_cluster_flags_t flags;
 	list_t *format_list; 	/* list of char * */
 	list_t *rpc_version_list; /* list of char * */
 	time_t usage_end;
@@ -753,7 +756,7 @@ struct slurmdb_cluster_rec {
 			* PACKED, is set up in slurmdb_get_info_cluster */
 	uint16_t id; /* unique id */
 	slurmdb_cluster_fed_t fed; /* Federation information */
-	uint32_t flags;      /* set of CLUSTER_FLAG_* */
+	slurmdb_cluster_flags_t flags;
 	pthread_mutex_t lock; /* For convenience only. DOESN"T GET PACKED */
 	char *name;
 	char *nodes;

src/plugins/data_parser/v0.0.40/parsers.c
@@ -9919,7 +9919,7 @@ static const parser_t parsers[] = {
 	addfa(WCKEY_FLAGS, uint32_t),
 	addfa(QOS_FLAGS, slurmdb_qos_flags_t),
 	addfa(QOS_PREEMPT_MODES, uint16_t),
-	addfa(CLUSTER_REC_FLAGS, uint32_t),
+	addfa(CLUSTER_REC_FLAGS, slurmdb_cluster_flags_t),
 	addfa(NODE_STATES, uint32_t),
 	addfa(PARTITION_STATES, uint16_t),
 	addfa(JOB_FLAGS, uint64_t),

src/plugins/data_parser/v0.0.41/parsers.c
@@ -10290,7 +10290,7 @@ static const parser_t parsers[] = {
 	addfa(WCKEY_FLAGS, uint32_t),
 	addfa(QOS_FLAGS, slurmdb_qos_flags_t),
 	addfa(QOS_PREEMPT_MODES, uint16_t),
-	addfa(CLUSTER_REC_FLAGS, uint32_t),
+	addfa(CLUSTER_REC_FLAGS, slurmdb_cluster_flags_t),
 	addfa(NODE_STATES, uint32_t),
 	addfa(PARTITION_STATES, uint16_t),
 	addfa(JOB_FLAGS, uint64_t),

src/plugins/data_parser/v0.0.42/parsers.c
@@ -10224,7 +10224,7 @@ static const parser_t parsers[] = {
 	addfa(QOS_FLAGS, slurmdb_qos_flags_t),
 	addfa(QOS_CONDITION_FLAGS, uint16_t),
 	addfa(QOS_PREEMPT_MODES, uint16_t),
-	addfa(CLUSTER_REC_FLAGS, uint32_t),
+	addfa(CLUSTER_REC_FLAGS, slurmdb_cluster_flags_t),
 	addfa(NODE_STATES, uint32_t),
 	addfa(PARTITION_STATES, uint16_t),
 	addfa(JOB_FLAGS, uint64_t),

src/plugins/data_parser/v0.0.43/parsers.c
@@ -10358,7 +10358,7 @@ static const parser_t parsers[] = {
 	addfa(QOS_FLAGS, slurmdb_qos_flags_t),
 	addfa(QOS_CONDITION_FLAGS, uint16_t),
 	addfa(QOS_PREEMPT_MODES, uint16_t),
-	addfa(CLUSTER_REC_FLAGS, uint32_t),
+	addfa(CLUSTER_REC_FLAGS, slurmdb_cluster_flags_t),
 	addfa(NODE_STATES, uint32_t),
 	addfa(PARTITION_STATES, uint16_t),
 	addfa(JOB_FLAGS, uint64_t),

commit cb18550712c1e7c8ec06dae0c2e8de895fccd34a

Capture errors with slurmdb_str_2_cluster_flags()
Change around prototype for slurmdb_str_2_cluster_flags() to provide
status code on parsing of flags instead attempting to use
CLUSTER_FLAG_INVALID as flag.

Regression from f1ff474f3c.

Ticket: 23127
Changelog: sacctmgr - Catch and reject attempts to pass invalid Cluster
 flags= arguments.

src/common/working_cluster.c
@@ -92,36 +92,41 @@ static const struct {
 };
 #undef T
 
-static slurmdb_cluster_flags_t _str_2_cluster_flags(char *flags_in)
+static int _str_2_cluster_flags(const char *flags_in,
+				slurmdb_cluster_flags_t *flags_ptr)
 {
 	if (!flags_in || !flags_in[0])
-		return CLUSTER_FLAG_NONE;
+		return SLURM_SUCCESS;
 
-	for (int i = 0; i < ARRAY_SIZE(slurmdb_cluster_flags_map); i++)
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_cluster_flags_map); i++) {
 		if (!xstrncasecmp(flags_in, slurmdb_cluster_flags_map[i].str,
-				  strlen(flags_in)))
-			return slurmdb_cluster_flags_map[i].flag;
+				  strlen(flags_in))) {
+			*flags_ptr |= slurmdb_cluster_flags_map[i].flag;
+			return SLURM_SUCCESS;
+		}
+	}
 
 	debug("%s: Unable to match %s to a slurmdbd_cluster_flags_t flag",
 	      __func__, flags_in);
-	return CLUSTER_FLAG_INVALID;
+	return EINVAL;
 }
 
-
-extern slurmdb_cluster_flags_t slurmdb_str_2_cluster_flags(char *flags_in)
+extern int slurmdb_str_2_cluster_flags(const char *flags_in,
+				       slurmdb_cluster_flags_t *flags_ptr)
 {
-	slurmdb_cluster_flags_t cluster_flags = 0;
 	char *token, *my_flags, *last = NULL;
+	int rc = SLURM_SUCCESS;
+
+	/* Always reset flags */
+	*flags_ptr = CLUSTER_FLAG_NONE;
 
 	my_flags = xstrdup(flags_in);
 	token = strtok_r(my_flags, ",", &last);
-	while (token) {
-		cluster_flags |= _str_2_cluster_flags(token);
+	while (token && !(rc = _str_2_cluster_flags(token, flags_ptr)))
 		token = strtok_r(NULL, ",", &last);
-	}
-	xfree(my_flags);
 
-	return cluster_flags;
+	xfree(my_flags);
+	return rc;
 }
 
 /* must xfree() returned string */

src/common/working_cluster.h
@@ -44,9 +44,15 @@ extern uint16_t slurmdb_setup_cluster_dims(void);
 /* Return the architecture flags in the current working cluster */
 extern uint32_t slurmdb_setup_cluster_flags(void);
 
-/* Translate architecture flag strings to their equivalent bitmaps */
-extern slurmdb_cluster_flags_t slurmdb_str_2_cluster_flags(char *flags_in);
-
+/*
+ * Translate architecture flag strings to their equivalent bitmaps
+ *
+ * IN str - CSV of cluster flags to parse
+ * OUT flags_ptr - pointer to populate based on parsed flags (CLUSTER_FLAG_*)
+ * RET SLURM_SUCCESS or error
+ */
+extern int slurmdb_str_2_cluster_flags(const char *flags_in,
+				       slurmdb_cluster_flags_t *flags_ptr);
 /*
  * Translate architecture flag bitmaps to their equivalent comma-delimited
  * string

src/sacctmgr/cluster_functions.c
@@ -112,9 +112,18 @@ static int _set_cond(int *start, int argc, char **argv,
 				cond_set |= SA_SET_CLUST;
 		} else if (!xstrncasecmp(argv[i], "flags",
 					 MAX(command_len, 2))) {
-			cluster_cond->flags = slurmdb_str_2_cluster_flags(
-				argv[i]+end);
-			cond_set |= SA_SET_CLUST;
+			const char *flag_str = (argv[i] + end);
+			int rc;
+
+			if ((rc = slurmdb_str_2_cluster_flags(
+				     flag_str, &cluster_cond->flags))) {
+				fprintf(stderr, " Unknown Flag(s)=%s\n",
+					flag_str);
+				exit_code = 1;
+			} else {
+				cond_set |= SA_SET_CLUST;
+			}
+			break;
 		} else if (!xstrncasecmp(argv[i], "Format",
 					 MAX(command_len, 2))) {
 			if (format_list)