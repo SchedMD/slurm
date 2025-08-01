SchedMD/slurm

commit 67c7268dc9677c6d6a5005008fe5a30b725b183f

Move #defines to a typedef enum for QOS flags
Ticket: 21657

src/common/slurmdb_defs.c
@@ -393,42 +393,40 @@ extern void slurmdb_job_cond_def_start_end(slurmdb_job_cond_t *job_cond)
 		job_cond->usage_end++;
 }
 
-static uint32_t _str_2_qos_flags(char *flags)
+#define T(flag, str) { flag, XSTRINGIFY(flag), str }
+static const struct {
+	slurmdb_qos_flags_t flag;
+	char *flag_str;
+	char *str;
+} slurmdb_qos_flags_map[] = {
+	T(QOS_FLAG_DENY_LIMIT, "DenyOnLimit"),
+	T(QOS_FLAG_ENFORCE_USAGE_THRES, "EnforceUsageThreshold"),
+	T(QOS_FLAG_PART_MIN_NODE, "PartitionMinNodes"),
+	T(QOS_FLAG_PART_MAX_NODE, "PartitionMaxNodes"),
+	T(QOS_FLAG_PART_TIME_LIMIT, "PartitionTimeLimit"),
+	T(QOS_FLAG_REQ_RESV, "RequiresReservation"),
+	T(QOS_FLAG_OVER_PART_QOS, "OverPartQOS"),
+	T(QOS_FLAG_NO_RESERVE, "NoReserve"),
+	T(QOS_FLAG_NO_DECAY, "NoDecay"),
+	T(QOS_FLAG_RELATIVE, "Relative"),
+	T(QOS_FLAG_USAGE_FACTOR_SAFE, "UsageFactorSafe"),
+	T(QOS_FLAG_INVALID, "INVALID"),
+};
+#undef T
+
+static slurmdb_qos_flags_t _str_2_qos_flags(char *flag_in)
 {
-	if (xstrcasestr(flags, "DenyOnLimit"))
-		return QOS_FLAG_DENY_LIMIT;
+	if (!flag_in || !flag_in[0])
+		return QOS_FLAG_NONE;
 
-	if (xstrcasestr(flags, "EnforceUsageThreshold"))
-		return QOS_FLAG_ENFORCE_USAGE_THRES;
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_qos_flags_map); i++)
+		if (!xstrncasecmp(flag_in, slurmdb_qos_flags_map[i].str,
+				  strlen(flag_in)))
+			return slurmdb_qos_flags_map[i].flag;
 
-	if (xstrcasestr(flags, "PartitionMinNodes"))
-		return QOS_FLAG_PART_MIN_NODE;
-
-	if (xstrcasestr(flags, "PartitionMaxNodes"))
-		return QOS_FLAG_PART_MAX_NODE;
-
-	if (xstrcasestr(flags, "PartitionTimeLimit"))
-		return QOS_FLAG_PART_TIME_LIMIT;
-
-	if (xstrcasestr(flags, "RequiresReservation"))
-		return QOS_FLAG_REQ_RESV;
-
-	if (xstrcasestr(flags, "OverPartQOS"))
-		return QOS_FLAG_OVER_PART_QOS;
-
-	if (xstrcasestr(flags, "NoReserve"))
-		return QOS_FLAG_NO_RESERVE;
-
-	if (xstrcasestr(flags, "NoDecay"))
-		return QOS_FLAG_NO_DECAY;
-
-	if (xstrcasestr(flags, "Relative"))
-		return QOS_FLAG_RELATIVE;
-
-	if (xstrcasestr(flags, "UsageFactorSafe"))
-		return QOS_FLAG_USAGE_FACTOR_SAFE;
-
-	return 0;
+	debug("%s: Unable to match %s to a slurmdbd_qos_flags_t flag",
+	      __func__, flag_in);
+	return QOS_FLAG_INVALID;
 }
 
 static uint32_t _str_2_res_flags(char *flags)
@@ -2108,49 +2106,27 @@ extern uint32_t str_2_job_flags(char *flags)
 	return job_flags;
 }
 
-extern char *slurmdb_qos_flags_str(uint32_t flags)
+extern char *slurmdb_qos_flags_str(slurmdb_qos_flags_t flags)
 {
-	char *qos_flags = NULL;
+	char *qos_flags = NULL, *at = NULL;
 
-	if (flags & QOS_FLAG_NOTSET)
+	if (flags == QOS_FLAG_NOTSET)
 		return xstrdup("NotSet");
 
-	if (flags & QOS_FLAG_ADD)
-		xstrcat(qos_flags, "Add,");
-	if (flags & QOS_FLAG_REMOVE)
-		xstrcat(qos_flags, "Remove,");
-	if (flags & QOS_FLAG_DENY_LIMIT)
-		xstrcat(qos_flags, "DenyOnLimit,");
-	if (flags & QOS_FLAG_ENFORCE_USAGE_THRES)
-		xstrcat(qos_flags, "EnforceUsageThreshold,");
-	if (flags & QOS_FLAG_NO_RESERVE)
-		xstrcat(qos_flags, "NoReserve,");
-	if (flags & QOS_FLAG_PART_MAX_NODE)
-		xstrcat(qos_flags, "PartitionMaxNodes,");
-	if (flags & QOS_FLAG_PART_MIN_NODE)
-		xstrcat(qos_flags, "PartitionMinNodes,");
-	if (flags & QOS_FLAG_OVER_PART_QOS)
-		xstrcat(qos_flags, "OverPartQOS,");
-	if (flags & QOS_FLAG_PART_TIME_LIMIT)
-		xstrcat(qos_flags, "PartitionTimeLimit,");
-	if (flags & QOS_FLAG_REQ_RESV)
-		xstrcat(qos_flags, "RequiresReservation,");
-	if (flags & QOS_FLAG_NO_DECAY)
-		xstrcat(qos_flags, "NoDecay,");
-	if (flags & QOS_FLAG_RELATIVE)
-		xstrcat(qos_flags, "Relative,");
-	if (flags & QOS_FLAG_USAGE_FACTOR_SAFE)
-		xstrcat(qos_flags, "UsageFactorSafe,");
-
-	if (qos_flags)
-		qos_flags[strlen(qos_flags)-1] = '\0';
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_qos_flags_map); i++) {
+		if ((slurmdb_qos_flags_map[i].flag & flags) ==
+		    slurmdb_qos_flags_map[i].flag)
+			xstrfmtcatat(qos_flags, &at, "%s%s",
+				     (qos_flags ? "," : ""),
+				     slurmdb_qos_flags_map[i].str);
+	}
 
 	return qos_flags;
 }
 
-extern uint32_t str_2_qos_flags(char *flags, int option)
+extern slurmdb_qos_flags_t str_2_qos_flags(char *flags, int option)
 {
-	uint32_t qos_flags = 0;
+	slurmdb_qos_flags_t qos_flags = 0;
 	char *token, *my_flags, *last = NULL;
 
 	if (!flags) {

slurm/slurmdb.h
@@ -121,24 +121,30 @@ typedef enum {
 } slurmdb_update_type_t;
 
 /* Define QOS flags */
-#define	QOS_FLAG_BASE                0x0fffffff
-#define	QOS_FLAG_NOTSET              0x10000000
-#define	QOS_FLAG_ADD                 0x20000000
-#define	QOS_FLAG_REMOVE              0x40000000
+typedef enum {
+	QOS_FLAG_NONE = 0,
+	QOS_FLAG_PART_MIN_NODE = SLURM_BIT(0),
+	QOS_FLAG_PART_MAX_NODE = SLURM_BIT(1),
+	QOS_FLAG_PART_TIME_LIMIT = SLURM_BIT(2),
+	QOS_FLAG_ENFORCE_USAGE_THRES = SLURM_BIT(3),
+	QOS_FLAG_NO_RESERVE = SLURM_BIT(4),
+	QOS_FLAG_REQ_RESV = SLURM_BIT(5),
+	QOS_FLAG_DENY_LIMIT = SLURM_BIT(6),
+	QOS_FLAG_OVER_PART_QOS = SLURM_BIT(7),
+	QOS_FLAG_NO_DECAY = SLURM_BIT(8),
+	QOS_FLAG_USAGE_FACTOR_SAFE = SLURM_BIT(9),
+	QOS_FLAG_RELATIVE = SLURM_BIT(10),
+	QOS_FLAG_RELATIVE_SET = SLURM_BIT(11),
+	QOS_FLAG_PART_QOS = SLURM_BIT(12),
 
-#define	QOS_FLAG_PART_MIN_NODE       SLURM_BIT(0)
-#define	QOS_FLAG_PART_MAX_NODE       SLURM_BIT(1)
-#define	QOS_FLAG_PART_TIME_LIMIT     SLURM_BIT(2)
-#define	QOS_FLAG_ENFORCE_USAGE_THRES SLURM_BIT(3)
-#define	QOS_FLAG_NO_RESERVE          SLURM_BIT(4)
-#define	QOS_FLAG_REQ_RESV            SLURM_BIT(5)
-#define	QOS_FLAG_DENY_LIMIT          SLURM_BIT(6)
-#define	QOS_FLAG_OVER_PART_QOS       SLURM_BIT(7)
-#define	QOS_FLAG_NO_DECAY            SLURM_BIT(8)
-#define	QOS_FLAG_USAGE_FACTOR_SAFE   SLURM_BIT(9)
-#define	QOS_FLAG_RELATIVE            SLURM_BIT(10)
-#define	QOS_FLAG_RELATIVE_SET        SLURM_BIT(11)
-#define	QOS_FLAG_PART_QOS            SLURM_BIT(12)
+	/* 28+ are operators and will not be on a normal QOS rec. */
+	QOS_FLAG_BASE = 0x0fffffff,
+	QOS_FLAG_NOTSET = SLURM_BIT(28),
+	QOS_FLAG_ADD = SLURM_BIT(29),
+	QOS_FLAG_REMOVE = SLURM_BIT(30),
+
+	QOS_FLAG_INVALID,
+} slurmdb_qos_flags_t;
 
 /* Define QOS Cond flags */
 #define	QOS_COND_FLAG_WITH_DELETED SLURM_BIT(0)
@@ -970,8 +976,8 @@ typedef struct {
 	time_t blocked_until; /* internal use only, DON'T PACK  */
 	char *description;
 	uint32_t id;
-	uint32_t flags; /* flags for various things to enforce or
-			   override other limits */
+	slurmdb_qos_flags_t flags; /* flags for various things to enforce or
+				      override other limits */
 	uint32_t grace_time; /* preemption grace time */
 	uint32_t grp_jobs_accrue; /* max number of jobs this qos can
 				   * have accruing priority time

src/common/slurmdb_defs.h
@@ -135,8 +135,8 @@ extern char *slurmdb_job_flags_str(uint32_t flags);
 extern uint32_t str_2_job_flags(char *flags);
 extern char *slurmdb_qos_str(list_t *qos_list, uint32_t level);
 extern uint32_t str_2_slurmdb_qos(list_t *qos_list, char *level);
-extern char *slurmdb_qos_flags_str(uint32_t flags);
-extern uint32_t str_2_qos_flags(char *flags, int option);
+extern char *slurmdb_qos_flags_str(slurmdb_qos_flags_t flags);
+extern slurmdb_qos_flags_t str_2_qos_flags(char *flags, int option);
 extern char *slurmdb_res_flags_str(uint32_t flags);
 extern uint32_t str_2_res_flags(char *flags, int option);
 extern char *slurmdb_res_type_str(slurmdb_resource_type_t type);

src/plugins/data_parser/v0.0.40/parsers.c
@@ -9917,7 +9917,7 @@ static const parser_t parsers[] = {
 	addfa(SLURMDB_JOB_FLAGS, uint32_t),
 	addfa(ACCOUNT_FLAGS, uint32_t),
 	addfa(WCKEY_FLAGS, uint32_t),
-	addfa(QOS_FLAGS, uint32_t),
+	addfa(QOS_FLAGS, slurmdb_qos_flags_t),
 	addfa(QOS_PREEMPT_MODES, uint16_t),
 	addfa(CLUSTER_REC_FLAGS, uint32_t),
 	addfa(NODE_STATES, uint32_t),

src/plugins/data_parser/v0.0.41/parsers.c
@@ -10288,7 +10288,7 @@ static const parser_t parsers[] = {
 	addfa(SLURMDB_JOB_FLAGS, uint32_t),
 	addfa(ACCOUNT_FLAGS, uint32_t),
 	addfa(WCKEY_FLAGS, uint32_t),
-	addfa(QOS_FLAGS, uint32_t),
+	addfa(QOS_FLAGS, slurmdb_qos_flags_t),
 	addfa(QOS_PREEMPT_MODES, uint16_t),
 	addfa(CLUSTER_REC_FLAGS, uint32_t),
 	addfa(NODE_STATES, uint32_t),

src/plugins/data_parser/v0.0.42/parsers.c
@@ -10221,7 +10221,7 @@ static const parser_t parsers[] = {
 	addfa(SLURMDB_JOB_FLAGS, uint32_t),
 	addfa(ACCOUNT_FLAGS, uint32_t),
 	addfa(WCKEY_FLAGS, uint32_t),
-	addfa(QOS_FLAGS, uint32_t),
+	addfa(QOS_FLAGS, slurmdb_qos_flags_t),
 	addfa(QOS_CONDITION_FLAGS, uint16_t),
 	addfa(QOS_PREEMPT_MODES, uint16_t),
 	addfa(CLUSTER_REC_FLAGS, uint32_t),

src/plugins/data_parser/v0.0.43/parsers.c
@@ -10353,7 +10353,7 @@ static const parser_t parsers[] = {
 	addfa(SLURMDB_JOB_FLAGS, uint32_t),
 	addfa(ACCOUNT_FLAGS, uint32_t),
 	addfa(WCKEY_FLAGS, uint32_t),
-	addfa(QOS_FLAGS, uint32_t),
+	addfa(QOS_FLAGS, slurmdb_qos_flags_t),
 	addfa(QOS_CONDITION_FLAGS, uint16_t),
 	addfa(QOS_PREEMPT_MODES, uint16_t),
 	addfa(CLUSTER_REC_FLAGS, uint32_t),

commit a2d4faaac57794f6c3cec020c3303e1c02bc75ee

Capture errors with str_2_qos_flags()
Change _str_2_qos_flags() prototype to return an error on a bad qos flag instead
of attempting to use QOS_FLAG_INVALID as a flag.

Regression from 67c7268dc9.

Changelog: sacctmgr - Catch and reject attempts to pass invalid QOS flags
Ticket: 23127

src/common/slurmdb_defs.c
@@ -417,19 +417,22 @@ static const struct {
 };
 #undef T
 
-static slurmdb_qos_flags_t _str_2_qos_flags(char *flag_in)
+static int _str_2_qos_flags(char *flag_in, slurmdb_qos_flags_t *flags_ptr)
 {
 	if (!flag_in || !flag_in[0])
-		return QOS_FLAG_NONE;
+		return SLURM_SUCCESS;
 
-	for (int i = 0; i < ARRAY_SIZE(slurmdb_qos_flags_map); i++)
+	for (int i = 0; i < ARRAY_SIZE(slurmdb_qos_flags_map); i++) {
 		if (!xstrncasecmp(flag_in, slurmdb_qos_flags_map[i].str,
-				  strlen(flag_in)))
-			return slurmdb_qos_flags_map[i].flag;
+				  strlen(flag_in))) {
+			*flags_ptr |= slurmdb_qos_flags_map[i].flag;
+			return SLURM_SUCCESS;
+		}
+	}
 
 	debug("%s: Unable to match %s to a slurmdbd_qos_flags_t flag",
 	      __func__, flag_in);
-	return QOS_FLAG_INVALID;
+	return EINVAL;
 }
 
 static uint32_t _str_2_res_flags(char *flags)
@@ -2106,6 +2109,7 @@ extern slurmdb_qos_flags_t str_2_qos_flags(char *flags, int option)
 {
 	slurmdb_qos_flags_t qos_flags = 0;
 	char *token, *my_flags, *last = NULL;
+	int rc = SLURM_SUCCESS;
 
 	if (!flags) {
 		error("We need a qos flags string to translate");
@@ -2120,13 +2124,11 @@ extern slurmdb_qos_flags_t str_2_qos_flags(char *flags, int option)
 
 	my_flags = xstrdup(flags);
 	token = strtok_r(my_flags, ",", &last);
-	while (token) {
-		qos_flags |= _str_2_qos_flags(token);
+	while (token && !(rc = _str_2_qos_flags(token, &qos_flags)))
 		token = strtok_r(NULL, ",", &last);
-	}
 	xfree(my_flags);
 
-	if (!qos_flags)
+	if (rc || !qos_flags)
 		qos_flags = QOS_FLAG_NOTSET;
 	else if (option == '+')
 		qos_flags |= QOS_FLAG_ADD;