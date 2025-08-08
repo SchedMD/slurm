SchedMD/slurm

commit dd6890ab06f8153d9c35a8a0e24b99427280c844

slurmctld - Always generate tls_conn

src/slurmctld/controller.c
@@ -1774,7 +1774,15 @@ static void _service_connection(conmgr_callback_args_t conmgr_args,
 	 */
 	msg->conmgr_fd = NULL;
 	msg->conn_fd = input_fd;
-	msg->tls_conn = tls_conn;
+	if (tls_conn) {
+		msg->tls_conn = tls_conn;
+	} else {
+		tls_conn_args_t tls_args = {
+			.input_fd = input_fd,
+			.output_fd = input_fd,
+		};
+		msg->tls_conn = tls_g_create_conn(&tls_args);
+	}
 
 	server_thread_incr();
 
@@ -1796,13 +1804,10 @@ static void _service_connection(conmgr_callback_args_t conmgr_args,
 	}
 
 	if (!this_rpc || !this_rpc->keep_msg) {
-		if (msg->tls_conn) {
-			tls_g_destroy_conn(msg->tls_conn, true);
-			msg->tls_conn = NULL;
-			log_flag(TLS, "Destroyed server TLS connection for incoming RPC on fd %d->%d",
-				 input_fd, output_fd);
-		} else if ((msg->conn_fd >= 0) && (close(msg->conn_fd) < 0))
-			error("close(%d): %m", msg->conn_fd);
+		tls_g_destroy_conn(msg->tls_conn, true);
+		msg->tls_conn = NULL;
+		log_flag(TLS, "Destroyed server TLS connection for incoming RPC on fd %d->%d",
+			 input_fd, output_fd);
 		msg->conn_fd = -1;
 		slurm_free_msg(msg);
 	}

commit 7abe555e53e46e3d96c517b4844362f7c1303bcf

slurmctld - Use output_fd in _service_connection()
No existing code paths can trigger this bug as _open_ports() only creates
unified sockets for listening for incoming RPCs.

Avoid only using input_fd in _service_connection() when calling
tls_g_create_conn() which would break any connection that has different
input_fd and output_fd.

Regression from dd6890ab06.

Issue: 50602

src/slurmctld/controller.c
@@ -1785,7 +1785,7 @@ static void _service_connection(conmgr_callback_args_t conmgr_args,
 	} else {
 		conn_args_t tls_args = {
 			.input_fd = input_fd,
-			.output_fd = input_fd,
+			.output_fd = output_fd,
 		};
 		msg->tls_conn = conn_g_create(&tls_args);
 	}