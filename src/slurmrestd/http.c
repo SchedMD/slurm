0f53543dda20d6d4b7a240c3ac4b5d47a2b20575
Nathan Rini <nate@schedmd.com>
2025-08-21 13:09:16 -0600

    slurmrestd - Use context->ref in _on_header()
    Prepare for context->con to be removed.
    
    Issue: 50189

--- src/slurmrestd/http.c
+++ src/slurmrestd/http.c
@@ -272,7 +272,7 @@ static int _on_header(const http_parser_header_t *header, void *arg)
 			request->keep_alive = ibuffer;
 		} else {
 			error("%s: [%s] invalid Keep-Alive value %s",
-			      __func__, conmgr_fd_get_name(context->con),
+			      __func__, conmgr_con_get_name(context->ref),
 			      header->value);
 			return _send_reject(
 				context, HTTP_STATUS_CODE_ERROR_NOT_ACCEPTABLE,
