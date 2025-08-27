SchedMD/slurm

commit 669e20e29756a1d890c16228289c32bda91bec0d

Avoid multiple calls to openlog() and closelog()
Ticket 19942, 19866

src/common/log.c
@@ -144,7 +144,7 @@ char *slurm_prog_name = NULL;
 static pthread_mutex_t  log_lock = PTHREAD_MUTEX_INITIALIZER;
 static log_t            *log = NULL;
 static log_t            *sched_log = NULL;
-
+static bool syslog_open = false;
 static volatile log_level_t highest_log_level = LOG_LEVEL_END;
 static volatile log_level_t highest_sched_log_level = LOG_LEVEL_QUIET;
 
@@ -345,8 +345,16 @@ _log_init(char *prog, log_options_t opt, log_facility_t fac, char *logfile )
 		log->fbuf = cbuf_create(128, 8192);
 	}
 
-	if (log->opt.syslog_level > LOG_LEVEL_QUIET)
+	if (syslog_open) {
+		closelog();
+		syslog_open = false;
+	}
+
+	if (log->opt.syslog_level > LOG_LEVEL_QUIET) {
 		log->facility = fac;
+		openlog(log->argv0, LOG_PID, log->facility);
+		syslog_open = true;
+	}
 
 	if (logfile && (log->opt.logfile_level > LOG_LEVEL_QUIET)) {
 		int mode = O_CREAT | O_WRONLY | O_APPEND | O_CLOEXEC;
@@ -527,6 +535,10 @@ void log_fini(void)
 		cbuf_destroy(log->fbuf);
 	if (log->logfp)
 		fclose(log->logfp);
+	if (syslog_open) {
+		closelog();
+		syslog_open = false;
+	}
 	xfree(log);
 	xfree(slurm_prog_name);
 	slurm_mutex_unlock(&log_lock);
@@ -1420,9 +1432,7 @@ static void _log_msg(log_level_t level, bool sched, bool spank, bool warn,
 		/* Avoid changing errno if syslog fails */
 		int orig_errno = slurm_get_errno();
 		xlogfmtcat(&msgbuf, "%s%s%s", log->prefix, pfx, buf);
-		openlog(log->argv0, LOG_PID, log->facility);
 		syslog(priority, "%.500s", msgbuf);
-		closelog();
 		slurm_seterrno(orig_errno);
 
 		xfree(msgbuf);

commit 043884bdded5f34239b1df7f3068a015b8d4b624

Fix use-after-free issue with the ident argument to openlog()
The syslog subsystem apparently stores this as a pointer, and not as an
independent copy. When log_set_prefix() is called in slurmscript or
slurmstepd we need to call closelog() to invalidate that pointer, and
ensure that the ident string will be updated correctly in successive
logging calls.

Regression from 669e20e297.

Changelog: slurmscriptd,slurmstepd - Fix use-after-free issue with the
 "ident" string when logging to syslog.
Ticket: 23586

src/common/log.c
@@ -308,6 +308,11 @@ _log_init(char *prog, log_options_t opt, log_facility_t fac, char *logfile )
 		atfork_install_handlers();
 	}
 
+	if (syslog_open) {
+		closelog();
+		syslog_open = false;
+	}
+
 	if (prog) {
 		if (log->argv0)
 			xfree(log->argv0);
@@ -338,11 +343,6 @@ _log_init(char *prog, log_options_t opt, log_facility_t fac, char *logfile )
 	if (log->opt.buffered) {
 		log->buf  = cbuf_create(128, 8192);
 		log->fbuf = cbuf_create(128, 8192);
-	}
-
-	if (syslog_open) {
-		closelog();
-		syslog_open = false;
 	}
 
 	if (log->opt.syslog_level > LOG_LEVEL_QUIET) {
@@ -522,6 +522,10 @@ void log_fini(void)
 
 	slurm_mutex_lock(&log_lock);
 	_log_flush(log);
+	if (syslog_open) {
+		closelog();
+		syslog_open = false;
+	}
 	xfree(log->argv0);
 	xfree(log->prefix);
 	if (log->buf)
@@ -530,10 +534,6 @@ void log_fini(void)
 		cbuf_destroy(log->fbuf);
 	if (log->logfp)
 		fclose(log->logfp);
-	if (syslog_open) {
-		closelog();
-		syslog_open = false;
-	}
 	xfree(log);
 	slurm_mutex_unlock(&log_lock);
 }
@@ -578,6 +578,10 @@ void log_set_prefix(char **prefix)
 void log_set_argv0(char *argv0)
 {
 	slurm_mutex_lock(&log_lock);
+	if (syslog_open) {
+		closelog();
+		syslog_open = false;
+	}
 	if (log->argv0)
 		xfree(log->argv0);
 	if (!argv0)