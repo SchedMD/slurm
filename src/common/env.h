9424897d15d414ecbb56e0ede3607d431f2df7f1
Ben Glines <ben.glines@schedmd.com>
2025-08-27 12:16:48 -0600

    Add SLURM_SRUN_TLS_CERT to job environment
    This environment variable can be used by user programs that need to
    connect to and trust a listening socket opened by srun, e.g.:
    - PMI_Barrier->slurm_pmi_get_kvs_comm_set
    - PMI_KVS_Commit->slurm_pmi_send_kvs_comm_set
    
    Ticket: 23273

--- src/common/env.h
+++ src/common/env.h
@@ -94,6 +94,7 @@ typedef struct env_options {
 	char *job_licenses;	/* job's licenses */
 	time_t job_start_time;  /* job's start time */
 	uint16_t oom_kill_step;   /* --oom-kill-step */
+	char *tls_cert;		/* srun TLS certificate */
 } env_t;
 
 
