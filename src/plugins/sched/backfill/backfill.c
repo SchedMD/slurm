SchedMD/slurm

commit 2655ec414a43edcf8ac3f6450b0447daf268b8d0

Fix leak of TRESRunMins with --time-min flag
When the time limit of a job was decreased to fit in a space
with the --time-min flag, it caused it to leak used time for
the account that had to be recovered with a reconfigure.

Bug 9356

src/plugins/sched/backfill/backfill.c
@@ -2063,13 +2063,15 @@ next_task:
 		}
 		if (deadline_time_limit)
 			comp_time_limit = MIN(time_limit, deadline_time_limit);
-		else
+		else if (job_ptr->time_min &&
+			 (job_ptr->time_min < time_limit)) {
+			time_limit = job_ptr->time_limit = job_ptr->time_min;
+			comp_time_limit = time_limit;
+		} else
 			comp_time_limit = time_limit;
 		if ((qos_flags & QOS_FLAG_NO_RESERVE) &&
 		    slurm_get_preempt_mode())
 			time_limit = job_ptr->time_limit = 1;
-		else if (job_ptr->time_min && (job_ptr->time_min < time_limit))
-			time_limit = job_ptr->time_limit = job_ptr->time_min;
 
 		later_start = now;
 

commit dedc92b367f0f97071e87140278efc173dba3dbe

sched/backfill - Fix incorrectly resetting --time to --time-min
Regression from 2655ec414a.

Resetting comp_time_limit to time_min so early makes the whole logic
around --time-min non-functional and causes lower-priority jobs
to be scheduled with a --time limit of exactly --time-min, even when
existing allocations and planned jobs would allow for a longer execution
time.

Changelog: Fix bug where the backfill scheduler changed the specified --time
 of a job and incorrectly reset it to --time-min.
Ticket: 23350

src/plugins/sched/backfill/backfill.c
@@ -2651,8 +2651,8 @@ next_task:
 			comp_time_limit = MIN(time_limit, deadline_time_limit);
 		else if (job_ptr->time_min &&
 			 (job_ptr->time_min < time_limit)) {
-			time_limit = job_ptr->time_limit = job_ptr->time_min;
 			comp_time_limit = time_limit;
+			time_limit = job_ptr->time_limit = job_ptr->time_min;
 		} else
 			comp_time_limit = time_limit;
 		if ((qos_flags & QOS_FLAG_NO_RESERVE) &&