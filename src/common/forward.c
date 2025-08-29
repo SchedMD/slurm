9f352dee9b489f730bdf5ac1ec9de5c628644248
Nathan Rini <nate@schedmd.com>
2025-08-29 17:05:55 -0600

    Switch to FREE_NULL_FORWARD_STRUCT() in forward_wait()
    Issue: 50643

--- src/common/forward.c
+++ src/common/forward.c
@@ -944,8 +944,7 @@ extern void forward_wait(slurm_msg_t * msg)
 		}
 		debug2("Got them all");
 		slurm_mutex_unlock(&msg->forward_struct->forward_mutex);
-		destroy_forward_struct(msg->forward_struct);
-		msg->forward_struct = NULL;
+		FREE_NULL_FORWARD_STRUCT(msg->forward_struct);
 	}
 	return;
 }
