d8902be7c2828ac24265ee6ab63f5043da617769
Tim McMullan <mcmullan@schedmd.com>
2025-09-16 08:35:29 -0400

    Add stub namespace/linux plugin
    Changelog: Add new namespace/linux plugin
    Issue: 50542
    Ticket: 21718

---
+++ src/plugins/namespace/linux/namespace_linux.c
@@ -0,0 +1,101 @@
+/*****************************************************************************\
+ *  namespace_linux.c - Define namespace plugin for creating temporary linux
+ *  		        namespaces for the job to provde some isolation between
+ *  		        jobs on the same node.
+ *****************************************************************************
+ *  Copyright (C) SchedMD LLC.
+ *
+ *  This file is part of Slurm, a resource management program.
+ *  For details, see <https://slurm.schedmd.com/>.
+ *  Please also read the included file: DISCLAIMER.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and
+ *  distribute linked combinations including the two. You must obey the GNU
+ *  General Public License in all respects for all of the code used other than
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this
+ *  exception to your version of the file(s), but you are not obligated to do
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in
+ *  the program, then also delete it here.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+#define _GNU_SOURCE
+#include "src/common/slurm_xlator.h"
+
+#include "src/slurmd/slurmstepd/slurmstepd_job.h"
+
+#include "read_nsconf.h"
+
+#if defined(__APPLE__)
+extern slurmd_conf_t *conf __attribute__((weak_import));
+#else
+slurmd_conf_t *conf = NULL;
+#endif
+
+const char plugin_name[] = "namespace linux plugin";
+const char plugin_type[] = "namespace/linux";
+const uint32_t plugin_version = SLURM_VERSION_NUMBER;
+
+
+extern int init(void)
+{
+	debug("%s loaded", plugin_name);
+
+	return SLURM_SUCCESS;
+}
+
+extern void fini(void)
+{
+	debug("%s unloaded", plugin_name);
+}
+
+extern int namespace_p_restore(char *dir_name, bool recover)
+{
+	return SLURM_SUCCESS;
+}
+
+extern int namespace_p_join_external(uint32_t job_id, list_t *ns_map)
+{
+	return SLURM_SUCCESS;
+}
+
+extern int namespace_p_join(slurm_step_id_t *step_id, uid_t uid,
+			    bool step_create)
+{
+	return SLURM_SUCCESS;
+}
+
+extern int namespace_p_stepd_create(stepd_step_rec_t *step)
+{
+	return SLURM_SUCCESS;
+}
+
+extern int namespace_p_stepd_delete(slurm_step_id_t *step_id)
+{
+	return SLURM_SUCCESS;
+}
+
+extern int namespace_p_send_stepd(int fd)
+{
+	return SLURM_SUCCESS;
+}
+
+extern int namespace_p_recv_stepd(int fd)
+{
+	return SLURM_SUCCESS;
+}
