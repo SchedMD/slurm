59b579239db114b510c0790ff6f87c31295092a6
Nathan Rini <nate@schedmd.com>
2025-10-09 13:03:44 -0600

    probes - Add implementation
    Issue: 50538

--- src/common/probes.h
+++ src/common/probes.h
@@ -36,4 +36,65 @@
 #ifndef SLURM_PROBES_H
 #define SLURM_PROBES_H
 
+#include "src/common/pack.h"
+
+typedef enum {
+	PROBE_RC_INVALID = 0,
+	PROBE_RC_UNKNOWN, /* service state is unknown */
+	PROBE_RC_DOWN, /* service is down or failed */
+	PROBE_RC_ONLINE, /* service is online */
+	PROBE_RC_BUSY, /* service is too busy for requests */
+	PROBE_RC_READY, /* service is ready for requests */
+	PROBE_RC_INVALID_MAX,
+} probe_status_t;
+
+/* Opaque struct to handle probe verbose logging */
+typedef struct probe_log_s probe_log_t;
+
+/*
+ * Callback to query service status
+ * IN log -
+ *	!Null: pass to probe_log() for logging verbose status
+ *	Null: logging not requested
+ * RET status of service
+ */
+typedef probe_status_t (*probe_query_t)(probe_log_t *log);
+
+/*
+ * Register probe query function
+ * IN name - name of service to log
+ * IN query - callback function to query to poll status
+ */
+extern void probe_register(const char *name, probe_query_t query);
+
+/*
+ * Log verbose status for service
+ * IN log - log pointer from probe_query_t log arg
+ * NOTE: Call probe_log() macro instead
+ */
+extern void probe_logger(probe_log_t *log, const char *caller, const char *fmt,
+			 ...) __attribute__((format(printf, 3, 4)));
+
+#define probe_log(log, fmt, ...) \
+	do { \
+		if (log) \
+			probe_logger(log, __func__, fmt, ##__VA_ARGS__); \
+	} while (0)
+
+extern void probe_init(void);
+extern void probe_fini(void);
+
+/*
+ * Run probes
+ * IN name - name of probe to run or NULL for all probes
+ * IN verbose - True to enable verbose logging
+ * IN/OUT output - (ignored if verbose=false)
+ *	!NULL: buffer to populate with verbose logs
+ *	NULL: log to info()
+ * caller - __func__ from caller
+ * RET (lowest) status of all probes run
+ */
+extern probe_status_t probe_run(bool verbose, const char *name, buf_t *output,
+				const char *caller);
+
 #endif
