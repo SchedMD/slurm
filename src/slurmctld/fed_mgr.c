SchedMD/slurm

commit 8f3392573cfc2deee14ec74bef0ea7c5221bbe9b

Fix dependency on array jobs preventing dependency on more than one task.
Caused by commit b16230c35a7.

Fix regression in 20.02 where a dependency on multiple jobs in a single
array (using the <array_job_id>_<array_task_id> format) would only add the
first job. However, using the non-array job id's directly for those jobs was
still accepted.

For example, this only added a dependency on the first job listed:

$ sbatch -Dtmp --depend=afterok:50_2,afterok:50_3 --wrap="srun whereami 10"
Submitted batch job 59

$ scontrol show job 59 |egrep -i "jobid|depend"
JobId=59 JobName=wrap
   JobState=PENDING Reason=Dependency Dependency=afterok:50_2(unfulfilled)

But this added a dependency on both jobs:

$ sbatch -Dtmp --depend=afterok:52,afterok:53 --wrap="srun whereami 10"
Submitted batch job 60
$ scontrol show job 60 |egrep -i "jobid|depend"
JobId=60 JobName=wrap
   JobState=PENDING Reason=Dependency \
	    Dependency=afterok:52(unfulfilled),afterok:53(unfulfilled)

Bug 9137.

src/slurmctld/job_scheduler.c
@@ -3184,6 +3184,7 @@ static int _find_dependency(void *arg, void *key)
 	depend_spec_t *dep_ptr = (depend_spec_t *)arg;
 	depend_spec_t *new_dep = (depend_spec_t *)key;
 	return (dep_ptr->job_id == new_dep->job_id) &&
+		(dep_ptr->array_task_id == new_dep->array_task_id) &&
 		(dep_ptr->depend_type == new_dep->depend_type);
 }
 

commit 1a4d91ccf70d3defed3a78622991bc56a7830555

slurmctld - Fix dependencies on remote array jobs
Fixes a regression in test37.17 introduced by 8f3392573c, which
added a check that array_task_ids are equal in _find_dependency.
Since _find_dependent_job_ptr sets array_task_id of NO_VAL to
INFINITE when an array job is found, the remote cluster would
respond with a different array_task_id than the one in the request.
This caused remote dependency updates to arrays to be ignored
if the dependency string was specified without "_*". For example,

sbatch --dependency=<job_id> <prog>

would never be fulfilled if <job_id> referred to a remote array job.

The local cluster has no way to know if a remote job id without
"_*" is an array. So the remote cluster responds with the
parsed array_task_id, not the resolved array_task_id in
dep_update_origin messages.

Ticket: 19658

src/slurmctld/fed_mgr.c
@@ -2217,11 +2217,19 @@ static int _foreach_fed_job_update_info(fed_job_update_info_t *job_update_info)
 	return SLURM_SUCCESS;
 }
 
+static int _restore_array_task_id(void *x, void *arg)
+{
+	depend_spec_t *dep_ptr = x;
+	dep_ptr->array_task_id = dep_ptr->parsed_array_task_id;
+	return SLURM_SUCCESS;
+}
+
 static void _update_origin_job_dep(job_record_t *job_ptr,
 				   slurmdb_cluster_rec_t *origin)
 {
 	slurm_msg_t req_msg;
 	dep_update_origin_msg_t dep_update_msg = { 0 };
+	list_t *depend_list;
 
 	xassert(job_ptr);
 	xassert(job_ptr->details);
@@ -2234,7 +2242,11 @@ static void _update_origin_job_dep(job_record_t *job_ptr,
 		return;
 	}
 
-	dep_update_msg.depend_list = job_ptr->details->depend_list;
+	/* Respond with the original array_task_id */
+	depend_list = depended_list_copy(job_ptr->details->depend_list);
+	list_for_each(depend_list, _restore_array_task_id, NULL);
+
+	dep_update_msg.depend_list = depend_list;
 	dep_update_msg.job_id = job_ptr->job_id;
 
 	slurm_msg_t_init(&req_msg);
@@ -2244,6 +2256,8 @@ static void _update_origin_job_dep(job_record_t *job_ptr,
 	if (_queue_rpc(origin, &req_msg, 0, false))
 		error("%s: Failed to send dependency update for %pJ",
 		      __func__, job_ptr);
+
+	FREE_NULL_LIST(depend_list);
 }
 
 static int _find_local_dep(void *arg, void *key)

src/slurmctld/job_scheduler.c
@@ -3695,6 +3695,7 @@ static void _parse_dependency_jobid_new(list_t *new_depend_list, char **sep_ptr,
 		dep_ptr->depend_type = depend_type;
 		dep_ptr->depend_time = depend_time;
 		dep_ptr->depend_state = depend_state;
+		dep_ptr->parsed_array_task_id = array_task_id;
 		list_append(new_depend_list, dep_ptr);
 		if (tmp[0] != ':')
 			break;
@@ -3726,6 +3727,7 @@ static void _parse_dependency_jobid_old(list_t *new_depend_list, char **sep_ptr,
 	dep_ptr->job_id = job_id;
 	dep_ptr->array_task_id = array_task_id;
 	dep_ptr->depend_type = SLURM_DEPEND_AFTER_ANY;
+	dep_ptr->parsed_array_task_id = array_task_id;
 
 	*sep_ptr = tmp;
 	list_append(new_depend_list, dep_ptr);