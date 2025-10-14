SchedMD/slurm

commit 0c46c43c42c07876ae9887978edf788916e7ae4d

Set array_task_id when the last job record is split from the job array
Make sure that the meta record for a job array gets array_task_id set
correctly after splitting off the last record in the array. This fixes
issues with updating job dependencies for that job within the job array.

If we don't do this, then array_task_id would stay at NO_VAL for the
last job record of the array until the job got scheduled.

This is a continuation of the previous commit.

Bug 8492

src/slurmctld/job_mgr.c
@@ -4644,11 +4644,33 @@ extern job_record_t *job_array_split(job_record_t *job_ptr)
 	xfree(job_ptr_pend->array_recs->task_id_str);
 	if (job_ptr_pend->array_recs->task_cnt) {
 		job_ptr_pend->array_recs->task_cnt--;
+		if (job_ptr_pend->array_recs->task_cnt <= 1) {
+			/*
+			 * This is the last task of the job array, so we need to
+			 * set array_task_id to a specific task id. We also
+			 * need to call job_array_post_sched() to do cleanup
+			 * on the array, specifically how job_array_post_sched()
+			 * handles adding the job to the array_hash, otherwise
+			 * we'll get errors.
+			 */
+			i = bit_ffs(job_ptr_pend->array_recs->task_id_bitmap);
+			if (i < 0) {
+				error("%s: No tasks in task_id_bitmap for %pJ",
+				      __func__, job_ptr_pend);
+				job_ptr_pend->array_task_id = NO_VAL;
+			} else {
+				job_ptr_pend->array_task_id = i;
+				job_array_post_sched(job_ptr_pend);
+			}
+		} else {
+			/* Still have tasks left to split off in the array */
+			job_ptr_pend->array_task_id = NO_VAL;
+		}
 	} else {
 		error("%pJ array_recs->task_cnt underflow",
 		      job_ptr);
+		job_ptr_pend->array_task_id = NO_VAL;
 	}
-	job_ptr_pend->array_task_id = NO_VAL;
 
 	job_ptr_pend->batch_host = NULL;
 	job_ptr_pend->burst_buffer = xstrdup(job_ptr->burst_buffer);

commit e1fa21034d79880ecb04fe10ba297e3a825db323

slurmcltd - Fix job_split_array decrement error
Caused by 0c46c43c42

Test 37.17 was failing due to an unregistered remote dependency on
the final array job. The symptom was that the last step of any array
with more that one task would stay pending forever.

Fixed a double decrement in job_array_split when task_cnt = 2.
Now the final REQUEST_SEND_DEP is sent to the remote cluster.

Ticket: 19658

src/slurmctld/job_mgr.c
@@ -3410,7 +3410,7 @@ extern job_record_t *job_array_split(job_record_t *job_ptr, bool list_add)
 	}
 	xfree(job_ptr_pend->array_recs->task_id_str);
 	if (job_ptr_pend->array_recs->task_cnt) {
-		job_ptr_pend->array_recs->task_cnt--;
+		/* Note job_array_post_sched decrements task_cnt if non-zero */
 		if (job_ptr_pend->array_recs->task_cnt <= 1) {
 			/*
 			 * This is the last task of the job array, so we need to
@@ -3431,6 +3431,7 @@ extern job_record_t *job_array_split(job_record_t *job_ptr, bool list_add)
 			}
 		} else {
 			/* Still have tasks left to split off in the array */
+			job_ptr_pend->array_recs->task_cnt--;
 			job_ptr_pend->array_task_id = NO_VAL;
 		}
 	} else {