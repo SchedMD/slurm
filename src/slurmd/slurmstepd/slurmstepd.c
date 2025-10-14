SchedMD/slurm

commit 1c065cb9083b4df9fca062d946359f853c2b7f64

Fix x11 cleanup race condition
The thread used to clean the x11 files was dropping privileges. If in the
meantime a cgroup or other tasks from step cleanup were taking place and
needed privileges, they would fail with EPERM. By adding locking with
auth_setuid_lock we guarantee that while doing these cleanups the
privileges will not be dropped.

Other parts of the code like the removal of the domain socket were also
affected by this, so the locking has been extended also to these places.

Changelog: Fix race condition on x11 shutdown which caused other privileged
 cleanup operations to fail and leave stray cgroup or spool directories and
 errors in the logs.
Ticket: 22736

src/slurmd/slurmstepd/slurmstepd.c
@@ -492,10 +492,18 @@ extern void stepd_cleanup(slurm_msg_t *msg, stepd_step_rec_t *step,
 		goto done;
 	}
 
-	if (!only_mem) {
-		if (step->batch)
-			batch_finish(step, rc); /* sends batch complete message */
+	if (!only_mem && step->batch)
+		batch_finish(step, rc); /* sends batch complete message */
 
+	/*
+	 * Call auth_setuid_lock after sending the batch_finish message as in
+	 * there the auth_g_create function is called, and that function uses
+	 * the lock. The lock is needed to ensure that the privileges are not
+	 * dropped from a different thread, like X11 shutdown thread.
+	 */
+	auth_setuid_lock();
+
+	if (!only_mem) {
 		/* signal the message thread to shutdown, and wait for it */
 		if (step->msg_handle)
 			eio_signal_shutdown(step->msg_handle);
@@ -519,6 +527,7 @@ extern void stepd_cleanup(slurm_msg_t *msg, stepd_step_rec_t *step,
 			      step->step_id.job_id);
 	}
 
+	auth_setuid_unlock();
 	run_command_shutdown();
 
 	/*

src/slurmd/slurmstepd/mgr.c
@@ -1536,6 +1536,9 @@ x11_fail:
 		if (task_g_post_term(step, step->task[i]) == ENOMEM)
 			step->oom_error = true;
 
+	/* Lock to not collide with the _x11_signal_handler thread. */
+	auth_setuid_lock();
+
 	/*
 	 * This function below calls jobacct_gather_fini(). For the case of
 	 * jobacct_gather/cgroup, it ends up doing the cgroup hierarchy cleanup
@@ -1543,8 +1546,9 @@ x11_fail:
 	 * children processes from the step are gone.
 	 */
 	acct_gather_profile_fini();
-
 	task_g_post_step(step);
+
+	auth_setuid_unlock();
 
 fail1:
 	conmgr_add_work_fifo(_x11_signal_handler, step);

commit 051e162957bce12a4bb2879ec14033b93b9cd910

mpi/pmix - Prevent deadlock in slurmstepd when cancelling a PMIx job
At stepd_cleanup(), if the PMIx server receives an event that triggers the
error handler, it will spawn a new thread which call slurm_kill_job_step().
That function tries to acquire the setuid_lock which is already taken by
stepd_cleanup(), and if at that time PMIx_Finalize() is called from
stepd_cleanup(), it will wait indefinitely for the error handler thread
to complete.

This fixes a regression introduced in 1c065cb9083b4df9fca062d946359f853c2b7f64.

Ticket: 23688
Changelog: slurmstepd - Fix deadlock when PMIx receives an event during
 step termination, which caused stuck stepd processes after job completion.

src/slurmd/slurmstepd/slurmstepd.c
@@ -535,6 +535,14 @@ extern void stepd_cleanup(slurm_msg_t *msg, slurm_addr_t *cli, int rc,
 		batch_finish(rc); /* sends batch complete message */
 
 	/*
+	 * We must de-register any error handlers that might call
+	 * slurm_kill_jobstep() before auth_setuid_lock(), or we could get
+	 * deadlocked with an incoming PMIx error, as PMIx_Finalize() will
+         * wait for any thread.
+         */
+	mpi_fini();
+
+	/*
 	 * Call auth_setuid_lock after sending the batch_finish message as in
 	 * there the auth_g_create function is called, and that function uses
 	 * the lock. The lock is needed to ensure that the privileges are not
@@ -548,8 +556,6 @@ extern void stepd_cleanup(slurm_msg_t *msg, slurm_addr_t *cli, int rc,
 			eio_signal_shutdown(step->msg_handle);
 		slurm_thread_join(step->msgid);
 	}
-
-	mpi_fini();
 
 	/*
 	 * This call is only done once per step since stepd_cleanup is protected