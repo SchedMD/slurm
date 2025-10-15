4db1e91fd4fab3c0b816bb60a74c4d6ce739a654
Tim Wickberg <tim@schedmd.com>
2025-10-15 12:08:06 -0600

    Convert _unpack_suspend_int_msg() to new form

--- src/common/slurm_protocol_pack.c
+++ src/common/slurm_protocol_pack.c
@@ -11391,24 +11391,19 @@ static void _pack_suspend_int_msg(const slurm_msg_t *smsg, buf_t *buffer)
 	}
 }
 
-static int  _unpack_suspend_int_msg(suspend_int_msg_t **msg_ptr, buf_t *buffer,
-				    uint16_t protocol_version)
+static int _unpack_suspend_int_msg(slurm_msg_t *smsg, buf_t *buffer)
 {
-	suspend_int_msg_t * msg;
-	xassert(msg_ptr);
+	suspend_int_msg_t *msg = xmalloc(sizeof(*msg));
 
-	msg = xmalloc ( sizeof (suspend_int_msg_t) );
-	*msg_ptr = msg ;
-
-	if (protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
+	if (smsg->protocol_version >= SLURM_MIN_PROTOCOL_VERSION) {
 		safe_unpack32(&msg->job_id, buffer);
 		safe_unpack16(&msg->op, buffer);
 	}
 
+	smsg->data = msg;
 	return SLURM_SUCCESS;
 
 unpack_error:
-	*msg_ptr = NULL;
 	slurm_free_suspend_int_msg(msg);
 	return SLURM_ERROR;
 }
@@ -14503,8 +14498,7 @@ unpack_msg(slurm_msg_t * msg, buf_t *buffer)
 					 msg->protocol_version);
 		break;
 	case REQUEST_SUSPEND_INT:
-		rc = _unpack_suspend_int_msg((suspend_int_msg_t **) &msg->data,
-					     buffer, msg->protocol_version);
+		rc = _unpack_suspend_int_msg(msg, buffer);
 		break;
 	case REQUEST_TOP_JOB:
 		rc = _unpack_top_job_msg((top_job_msg_t **) &msg->data, buffer,
