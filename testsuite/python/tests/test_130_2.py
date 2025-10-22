412ee1d2dd97d5859edae1aca3530be0de721be7
Brian Christiansen <brian@schedmd.com>
2025-10-21 19:37:51 -0600

    Testsuite - Adapt test_130_2 as in ce4741b891 for test24.1

--- testsuite/python/tests/test_130_2.py
+++ testsuite/python/tests/test_130_2.py
@@ -30,33 +30,8 @@ def test_shares(compiled_program):
     """Verify that shares return the expected values"""
     output = atf.run_command_output(compiled_program, fatal=True)
     assert (
-        re.search(r"root\|\|\|1.000000\|210\|\|1.000000\|0.500000\|\|cpu=0\|", output)
-        is not None
-    )
-    assert (
-        re.search(
-            r"AccountA\|\|40\|0.266667\|45\|0.214286\|0.214286\|0.572929\|\|cpu=0\|",
-            output,
-        )
-        is not None
-    )
-    assert (
-        re.search(
-            r"AccountB\|\|30\|0.200000\|20\|0.095238\|0.184524\|0.527550\|\|cpu=0\|",
-            output,
-        )
-        is not None
-    )
-    assert (
         re.search(
             r"AccountB\|User1\|1\|0.200000\|20\|0.095238\|0.184524\|0.527550\|\|cpu=0\|",
-            output,
-        )
-        is not None
-    )
-    assert (
-        re.search(
-            r"AccountC\|\|10\|0.066667\|25\|0.119048\|0.142857\|0.226431\|\|cpu=0\|",
             output,
         )
         is not None
@@ -77,28 +52,7 @@ def test_shares(compiled_program):
     )
     assert (
         re.search(
-            r"AccountD\|\|60\|0.400000\|25\|0.119048\|0.119048\|0.813594\|\|cpu=0\|",
-            output,
-        )
-        is not None
-    )
-    assert (
-        re.search(
-            r"AccountE\|\|25\|0.166667\|25\|0.119048\|0.119048\|0.609507\|\|cpu=0\|",
-            output,
-        )
-        is not None
-    )
-    assert (
-        re.search(
             r"AccountE\|User4\|1\|0.166667\|25\|0.119048\|0.119048\|0.609507\|\|cpu=0\|",
-            output,
-        )
-        is not None
-    )
-    assert (
-        re.search(
-            r"AccountF\|\|35\|0.233333\|0\|0.000000\|0.069444\|0.813594\|\|cpu=0\|",
             output,
         )
         is not None
@@ -112,21 +66,7 @@ def test_shares(compiled_program):
     )
     assert (
         re.search(
-            r"AccountG\|\|0\|0.000000\|30\|0.142857\|0.142857\|0.000000\|\|cpu=0\|",
-            output,
-        )
-        is not None
-    )
-    assert (
-        re.search(
             r"AccountG\|User6\|0\|0.000000\|30\|0.142857\|0.142857\|0.000000\|\|cpu=0\|",
-            output,
-        )
-        is not None
-    )
-    assert (
-        re.search(
-            r"AccountH\|\|50\|0.333333\|110\|0.523810\|0.523810\|0.336475\|\|cpu=0\|",
             output,
         )
         is not None
@@ -148,13 +88,6 @@ def test_shares(compiled_program):
     assert (
         re.search(
             r"AccountH\|UHRA3\|50\|0.098039\|25\|0.119048\|0.238095\|0.185749\|\|cpu=0\|",
-            output,
-        )
-        is not None
-    )
-    assert (
-        re.search(
-            r"AccountHTA\|\|parent\|0.333333\|45\|0.214286\|0.523810\|0.336475\|\|cpu=0\|",
             output,
         )
         is not None
