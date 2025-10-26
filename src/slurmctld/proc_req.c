SchedMD/slurm

commit 3a17d4be3aef69995b8df9b0c8ceba7c3132a2c2

Change argument to job_epilog_complete()
Send job_ptr in directly as it's already available on two of three calling
paths. Adjust the agent code to handle the job_ptr lookup itself, and log
a suitable error if it cannot be found.

src/slurmctld/proc_req.c
@@ -1834,11 +1834,11 @@ static void _slurm_rpc_epilog_complete(slurm_msg_t *msg)
 	log_flag(ROUTE, "%s: node_name = %s, %pI",
 		 __func__, epilog_msg->node_name, &epilog_msg->step_id);
 
-	if (job_epilog_complete(epilog_msg->step_id.job_id,
-				epilog_msg->node_name, epilog_msg->return_code))
-		run_scheduler = true;
-
 	job_ptr = find_job_record(epilog_msg->step_id.job_id);
+
+	if (job_epilog_complete(job_ptr, epilog_msg->node_name,
+				epilog_msg->return_code))
+		run_scheduler = true;
 
 	if (epilog_msg->return_code)
 		error("%s: epilog error %pJ Node=%s Err=%s %s",

src/slurmctld/agent.c
@@ -1076,10 +1076,16 @@ static void *_thread_per_group_rpc(void *args)
 			kill_job_msg_t *kill_job;
 			kill_job = (kill_job_msg_t *)
 				task_ptr->msg_args_ptr;
+			job_record_t *job_ptr = NULL;
 			rc = SLURM_SUCCESS;
 			lock_slurmctld(job_write_lock);
-			if (job_epilog_complete(kill_job->step_id.job_id,
-						ret_data_info->node_name, rc))
+			if (!(job_ptr = find_job(&kill_job->step_id)))
+				debug("%s: unable to find %pI to mark epilog completed on node=%s with return_code=%u",
+				      __func__, &kill_job->step_id,
+				      ret_data_info->node_name, rc);
+			else if (job_epilog_complete(job_ptr,
+						     ret_data_info->node_name,
+						     rc))
 				run_scheduler = true;
 			unlock_slurmctld(job_write_lock);
 		}

src/slurmctld/job_mgr.c
@@ -16546,22 +16546,15 @@ extern uint64_t job_get_tres_mem(struct job_resources *job_res,
 /*
  * job_epilog_complete - Note the completion of the epilog script for a
  *	given job
- * IN job_id      - id of the job for which the epilog was executed
+ * IN job_ptr
  * IN node_name   - name of the node on which the epilog was executed
  * IN return_code - return code from epilog script
  * RET true if job is COMPLETED, otherwise false
  */
-extern bool job_epilog_complete(uint32_t job_id, char *node_name,
+extern bool job_epilog_complete(job_record_t *job_ptr, char *node_name,
 				uint32_t return_code)
 {
-	job_record_t *job_ptr = find_job_record(job_id);
 	node_record_t *node_ptr;
-
-	if (job_ptr == NULL) {
-		debug("%s: unable to find JobId=%u for node=%s with return_code=%u.",
-		      __func__, job_id, node_name, return_code);
-		return true;
-	}
 
 	log_flag(TRACE_JOBS, "%s: enter %pJ", __func__, job_ptr);
 

src/slurmctld/node_scheduler.c
@@ -408,8 +408,7 @@ extern void deallocate_nodes(job_record_t *job_ptr, bool timeout,
 				     (node_ptr = next_node_bitmap(
 					     job_ptr->node_bitmap_cg, &i));
 			     i++) {
-				job_epilog_complete(job_ptr->job_id,
-						    node_ptr->name, 0);
+				job_epilog_complete(job_ptr, node_ptr->name, 0);
 			}
 		}
 

src/slurmctld/slurmctld.h
@@ -965,13 +965,13 @@ extern uint64_t job_get_tres_mem(struct job_resources *job_res,
 /*
  * job_epilog_complete - Note the completion of the epilog script for a
  *	given job
- * IN job_id      - id of the job for which the epilog was executed
+ * IN job_ptr
  * IN node_name   - name of the node on which the epilog was executed
  * IN return_code - return code from epilog script
  * RET true if job is COMPLETED, otherwise false
  */
-extern bool job_epilog_complete(uint32_t job_id, char *node_name,
-		uint32_t return_code);
+extern bool job_epilog_complete(job_record_t *job_ptr, char *node_name,
+				uint32_t return_code);
 
 /*
  * job_end_time - Process JOB_END_TIME

commit 2d97be20953c86e805f5c060b25ccf072137da59

Avoid passing NULL job_ptr to job_epilog_complete()
Regression from 3a17d4be3a.

src/slurmctld/proc_req.c
@@ -1834,10 +1834,10 @@ static void _slurm_rpc_epilog_complete(slurm_msg_t *msg)
 	log_flag(ROUTE, "%s: node_name = %s, %pI",
 		 __func__, epilog_msg->node_name, &epilog_msg->step_id);
 
-	job_ptr = find_job_record(epilog_msg->step_id.job_id);
-
-	if (job_epilog_complete(job_ptr, epilog_msg->node_name,
-				epilog_msg->return_code))
+	if (!(job_ptr = find_job_record(epilog_msg->step_id.job_id)))
+		error("%s: could not find %pI", __func__, &epilog_msg->step_id);
+	else if (job_epilog_complete(job_ptr, epilog_msg->node_name,
+				     epilog_msg->return_code))
 		run_scheduler = true;
 
 	if (epilog_msg->return_code)