SchedMD/slurm

commit 91d008fa1cce9aa0f599a6576e1579830499004b

Change argument to job_complete()

src/slurmctld/job_mgr.c
@@ -6281,7 +6281,7 @@ static int _foreach_het_job_complete(void *x, void *arg)
 
 /*
  * job_complete - note the normal termination the specified job
- * IN job_id - id of the job which completed
+ * IN step_id - id of the job which completed
  * IN uid - user id of user issuing the RPC
  * IN requeue - job should be run again if possible
  * IN node_fail - true if job terminated due to node failure
@@ -6290,7 +6290,7 @@ static int _foreach_het_job_complete(void *x, void *arg)
  * global: job_list - pointer global job list
  *	last_job_update - time of last job table update
  */
-extern int job_complete(uint32_t job_id, uid_t uid, bool requeue,
+extern int job_complete(slurm_step_id_t *step_id, uid_t uid, bool requeue,
 			bool node_fail, uint32_t job_return_code)
 {
 	job_record_t *job_ptr;
@@ -6299,9 +6299,8 @@ extern int job_complete(uint32_t job_id, uid_t uid, bool requeue,
 	xassert(verify_lock(JOB_LOCK, WRITE_LOCK));
 	xassert(verify_lock(FED_LOCK, READ_LOCK));
 
-	job_ptr = find_job_record(job_id);
-	if (job_ptr == NULL) {
-		info("%s: invalid JobId=%u", __func__, job_id);
+	if (!(job_ptr = find_job_record(step_id->job_id))) {
+		info("%s: invalid %pI", __func__, &step_id);
 		return ESLURM_INVALID_JOB_ID;
 	}
 
@@ -15939,6 +15938,9 @@ static int _foreach_purge_missing_jobs(void *x, void *arg)
 	     find_node_record(job_ptr->batch_host))) {
 		bool requeue = false;
 		char *requeue_msg = "";
+		slurm_step_id_t step_id = {
+			.job_id = job_ptr->job_id,
+		};
 		if (job_ptr->details && job_ptr->details->requeue) {
 			requeue = true;
 			requeue_msg = ", Requeuing job";
@@ -15947,8 +15949,8 @@ static int _foreach_purge_missing_jobs(void *x, void *arg)
 		     job_ptr, job_ptr->batch_host, requeue_msg);
 		xfree(job_ptr->failed_node);
 		job_ptr->failed_node = xstrdup(job_ptr->batch_host);
-		job_complete(job_ptr->job_id, slurm_conf.slurm_user_id,
-			     requeue, true, 1);
+		job_complete(&step_id, slurm_conf.slurm_user_id, requeue, true,
+			     1);
 	} else {
 		foreach_purge_missing_jobs->job_ptr = job_ptr;
 
@@ -19687,9 +19689,12 @@ extern void sort_all_jobs_partition_lists()
 
 extern void job_mgr_handle_cred_failure(job_record_t *job_ptr)
 {
+	slurm_step_id_t step_id = {
+		.job_id = job_ptr->job_id,
+	};
 	job_ptr->priority = 0; /* Hold job */
 	xfree(job_ptr->system_comment);
 	job_ptr->system_comment =
 		xstrdup("slurm_cred_create failure, holding job.");
-	job_complete(job_ptr->job_id, slurm_conf.slurm_user_id, true, false, 0);
+	job_complete(&step_id, slurm_conf.slurm_user_id, true, false, 0);
 }

src/slurmctld/agent.c
@@ -769,7 +769,7 @@ static void _notify_slurmctld_nodes(agent_info_t *agent_ptr,
 				  .fed  = READ_LOCK };
 
 			lock_slurmctld(job_write_lock);
-			job_complete(launch_msg_ptr->step_id.job_id,
+			job_complete(&launch_msg_ptr->step_id,
 				     slurm_conf.slurm_user_id, true, false, 0);
 			unlock_slurmctld(job_write_lock);
 		}
@@ -1105,7 +1105,7 @@ static void *_thread_per_group_rpc(void *args)
 			thread_state = DSH_DONE;
 			ret_data_info->err = thread_state;
 			lock_slurmctld(job_write_lock);
-			job_complete(launch_msg_ptr->step_id.job_id,
+			job_complete(&launch_msg_ptr->step_id,
 				     slurm_conf.slurm_user_id, false, false,
 				     _wif_status());
 			unlock_slurmctld(job_write_lock);
@@ -1117,13 +1117,13 @@ static void *_thread_per_group_rpc(void *args)
 			 * behind on the allocated nodes. */
 			resource_allocation_response_msg_t *msg_ptr =
 				task_ptr->msg_args_ptr;
-			job_id = msg_ptr->step_id.job_id;
-			info("Killing interactive JobId=%u: %s",
-			     job_id, slurm_strerror(rc));
+			info("Killing interactive %pI: %s",
+			     &msg_ptr->step_id, slurm_strerror(rc));
 			thread_state = DSH_FAILED;
 			lock_slurmctld(job_write_lock);
-			job_complete(job_id, slurm_conf.slurm_user_id,
-			             false, false, _wif_status());
+			job_complete(&msg_ptr->step_id,
+				     slurm_conf.slurm_user_id, false, false,
+				     _wif_status());
 			unlock_slurmctld(job_write_lock);
 			continue;
 		} else if ((msg_type == RESPONSE_HET_JOB_ALLOCATION) &&
@@ -1137,13 +1137,13 @@ static void *_thread_per_group_rpc(void *args)
 			    (list_count(het_alloc_list) == 0))
 				continue;
 			msg_ptr = list_peek(het_alloc_list);
-			job_id = msg_ptr->step_id.job_id;
-			info("Killing interactive JobId=%u: %s",
-			     job_id, slurm_strerror(rc));
+			info("Killing interactive %pI: %s",
+			     &msg_ptr->step_id, slurm_strerror(rc));
 			thread_state = DSH_FAILED;
 			lock_slurmctld(job_write_lock);
-			job_complete(job_id, slurm_conf.slurm_user_id,
-			             false, false, _wif_status());
+			job_complete(&msg_ptr->step_id,
+				     slurm_conf.slurm_user_id, false, false,
+				     _wif_status());
 			unlock_slurmctld(job_write_lock);
 			continue;
 		}

src/slurmctld/job_scheduler.c
@@ -2343,6 +2343,9 @@ static batch_job_launch_msg_t *_build_launch_job_msg(job_record_t *job_ptr,
 {
 	char *fail_why = NULL;
 	batch_job_launch_msg_t *launch_msg_ptr;
+	slurm_step_id_t step_id = {
+		.job_id = job_ptr->job_id,
+	};
 
 	/* Initialization of data structures */
 	launch_msg_ptr = (batch_job_launch_msg_t *)
@@ -2473,8 +2476,7 @@ job_failed:
 	last_job_update = time(NULL);
 	slurm_free_job_launch_msg(launch_msg_ptr);
 	/* ignore the return as job is in an unknown state anyway */
-	job_complete(job_ptr->job_id, slurm_conf.slurm_user_id, false, false,
-	             1);
+	job_complete(&step_id, slurm_conf.slurm_user_id, false, false, 1);
 	return NULL;
 }
 

src/slurmctld/proc_req.c
@@ -720,11 +720,14 @@ static void _kill_job_on_msg_fail(uint32_t job_id)
 	/* Locks: Write job, write node */
 	slurmctld_lock_t job_write_lock = {
 		NO_LOCK, WRITE_LOCK, WRITE_LOCK, NO_LOCK, READ_LOCK };
+	slurm_step_id_t step_id = {
+		.job_id = job_id,
+	};
 
 	error("Job allocate response msg send failure, killing JobId=%u",
 	      job_id);
 	lock_slurmctld(job_write_lock);
-	job_complete(job_id, slurm_conf.slurm_user_id, false, false, SIGTERM);
+	job_complete(&step_id, slurm_conf.slurm_user_id, false, false, SIGTERM);
 	unlock_slurmctld(job_write_lock);
 }
 
@@ -1918,8 +1921,8 @@ static void _slurm_rpc_complete_job_allocation(slurm_msg_t *msg)
 	log_flag(TRACE_JOBS, "%s: enter %pJ", __func__, job_ptr);
 
 	/* Mark job and/or job step complete */
-	error_code = job_complete(comp_msg->step_id.job_id, msg->auth_uid,
-				  false, false, comp_msg->job_rc);
+	error_code = job_complete(&comp_msg->step_id, msg->auth_uid, false,
+				  false, comp_msg->job_rc);
 	if (error_code) {
 		if (error_code == ESLURM_INVALID_JOB_ID) {
 			info("%s: %pI error %s",
@@ -2134,8 +2137,8 @@ static void _slurm_rpc_complete_batch_script(slurm_msg_t *msg)
 		job_requeue = true;
 
 	/* Mark job allocation complete */
-	i = job_complete(comp_msg->step_id.job_id, msg->auth_uid, job_requeue,
-			 false, comp_msg->job_rc);
+	i = job_complete(&comp_msg->step_id, msg->auth_uid, job_requeue, false,
+			 comp_msg->job_rc);
 	error_code = MAX(error_code, i);
 	if (!(msg->flags & CTLD_QUEUE_PROCESSING)) {
 		unlock_slurmctld(job_write_lock);

src/slurmctld/slurmctld.h
@@ -1079,7 +1079,7 @@ extern int job_suspend2(slurm_msg_t *msg, suspend_msg_t *sus_ptr, uid_t uid,
 
 /*
  * job_complete - note the normal termination the specified job
- * IN job_id - id of the job which completed
+ * IN step_id - id of the job which completed
  * IN uid - user id of user issuing the RPC
  * IN requeue - job should be run again if possible
  * IN node_fail - true if job terminated due to node failure
@@ -1088,7 +1088,7 @@ extern int job_suspend2(slurm_msg_t *msg, suspend_msg_t *sus_ptr, uid_t uid,
  * global: job_list - pointer global job list
  *	last_job_update - time of last job table update
  */
-extern int job_complete(uint32_t job_id, uid_t uid, bool requeue,
+extern int job_complete(slurm_step_id_t *step_id, uid_t uid, bool requeue,
 			bool node_fail, uint32_t job_return_code);
 
 /*

commit ee2f25d49b6e7fe17ca71802eb29a9ab2c013767

Fix log message in job_complete()
Regression from 91d008fa1c.

src/slurmctld/job_mgr.c
@@ -6311,7 +6311,7 @@ extern int job_complete(slurm_step_id_t *step_id, uid_t uid, bool requeue,
 	xassert(verify_lock(FED_LOCK, READ_LOCK));
 
 	if (!(job_ptr = find_job_record(step_id->job_id))) {
-		info("%s: invalid %pI", __func__, &step_id);
+		info("%s: invalid %pI", __func__, step_id);
 		return ESLURM_INVALID_JOB_ID;
 	}
 