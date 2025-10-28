SchedMD/slurm

commit ece9b0bc463e425463d491ac6ed5b09610239ce8

conmgr - Enforce quiesce timeout
Issue: 50314

src/conmgr/watch.c
@@ -1519,6 +1519,35 @@ static int _get_quiesced_waiter_count(void)
 	return waiters;
 }
 
+/* NOTE: must hold mgr.mutex */
+static int _close_con_for_each(void *x, void *arg)
+{
+	conmgr_fd_t *con = x;
+	close_con(true, con);
+	return 1;
+}
+
+static void _on_quiesce_timeout(void)
+{
+	log_flag(CONMGR, "%s: Quiesce timed out. Closing all connections.",
+		 __func__);
+
+	/* Close all connections but not listeners */
+	list_for_each(mgr.connections, _close_con_for_each, NULL);
+}
+
+static void _quiesce_max_sleep(void)
+{
+	handle_connection_args_t args = {
+		.magic = MAGIC_HANDLE_CONNECTION,
+	};
+	args.time = timespec_now();
+
+	if (_handle_time_limit(&args, mgr.quiesce.start,
+			       mgr.quiesce.conf_timeout))
+		_on_quiesce_timeout();
+}
+
 static bool _watch_loop(void)
 {
 	if (mgr.shutdown_requested) {
@@ -1529,6 +1558,12 @@ static bool _watch_loop(void)
 
 	if (mgr.quiesce.requested) {
 		int waiters;
+
+		/*
+		 * Limit amount of time watch() will sleep to ensure that the
+		 * quiesce timeout is enforced
+		 */
+		_quiesce_max_sleep();
 
 		if (signal_mgr_has_incoming()) {
 			/*

commit 0757b13c30e8cc51a08250f5033cda3a67415252

conmgr - Avoid closing signal connection in _close_con_for_each()
_on_quiesce_timeout() is called when quiesce times out which should not
close() the signal connection before shutdown.

Regression from ece9b0bc46.

Changelog: slurmctld - Avoid regression that caused POSIX signals to be
 ignored after quiesce timeout triggers.
Ticket: 23549

src/conmgr/watch.c
@@ -1591,11 +1591,14 @@ static int _get_quiesced_waiter_count(void)
 	return waiters;
 }
 
-/* NOTE: must hold mgr.mutex */
+/* NOTE: must hold mgr.mutex except signal connection */
 static int _close_con_for_each(void *x, void *arg)
 {
 	conmgr_fd_t *con = x;
-	close_con(true, con);
+
+	if (!is_signal_connection(con))
+		close_con(true, con);
+
 	return 1;
 }
 