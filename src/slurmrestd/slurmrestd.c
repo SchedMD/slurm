1271fedd093f4fd897337ac0908c8e089fd484ae
Nathan Rini <nate@schedmd.com>
2025-10-31 16:34:25 -0600

    slurmrestd - Swap to probe_run() in _on_sigprof()
    Swap to using probes instead of calling conmgr_log_diagnostics() directly
    upon SIGPROF.
    
    Issue: 50538

--- src/slurmrestd/slurmrestd.c
+++ src/slurmrestd/slurmrestd.c
@@ -163,7 +163,7 @@ static void _on_sigprof(conmgr_callback_args_t conmgr_args, void *arg)
 	if (conmgr_args.status == CONMGR_WORK_STATUS_CANCELLED)
 		return;
 
-	conmgr_log_diagnostics();
+	(void) probe_run(true, NULL, NULL, __func__);
 }
 
 static void _set_max_connections(const char *buffer)
