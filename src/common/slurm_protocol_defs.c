52778dc74f7203e43afa0bd8a7ec6b21cb0715e0
Tim Wickberg <tim@schedmd.com>
2025-11-03 13:35:55 -0700

    Add supporting structs and free functions
    Cherry-picked: 7b4be532e2

--- src/common/slurm_protocol_defs.c
+++ src/common/slurm_protocol_defs.c
@@ -1935,6 +1935,43 @@ extern void slurm_free_acct_gather_energy_req_msg(
 	}
 }
 
+extern void slurm_free_node_gres_layout(void *in)
+{
+	node_gres_layout_t *msg = in;
+
+	if (!msg)
+		return;
+
+	xfree(msg->name);
+	xfree(msg->type);
+	FREE_NULL_BITMAP(msg->index);
+	xfree(msg);
+}
+
+extern void slurm_free_node_resource_layout(void *in)
+{
+	node_resource_layout_t *msg = in;
+
+	if (!msg)
+		return;
+
+	xfree(msg->node);
+	xfree(msg->core_bitmap);
+	FREE_NULL_LIST(msg->gres);
+	xfree(msg);
+}
+
+extern void slurm_free_resource_layout_msg(void *in)
+{
+	resource_layout_msg_t *msg = in;
+
+	if (!msg)
+		return;
+
+	FREE_NULL_LIST(msg->nodes);
+	xfree(msg);
+}
+
 extern void slurm_free_node_registration_status_msg(
 	slurm_node_registration_status_msg_t * msg)
 {
