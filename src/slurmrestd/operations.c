SchedMD/slurm

commit db479b620800fd230c0dd3a2a7a275ec99676bc8

slurmrestd - Move long error string to ESLURM_REST_UNKNOWN_URL
Issue: 50651

src/slurmrestd/operations.c
@@ -277,10 +277,8 @@ static int _resolve_path(on_http_request_args_t *args, int *path_tag,
 	FREE_NULL_DATA(path);
 
 	if (*path_tag == -1)
-		return _operations_router_reject(
-			args,
-			"Unable to find requested URL endpoint. Please query the '/openapi/v3' endpoint or visit 'https://slurm.schedmd.com/rest_api.html' for the OpenAPI specification which includes a list of all possible slurmrestd endpoints.",
-			ESLURM_URL_INVALID_PATH, NULL);
+		return _operations_router_reject(args, NULL,
+						 ESLURM_URL_INVALID_PATH, NULL);
 	else if (*path_tag == -2)
 		return _operations_router_reject(args, NULL,
 						 ESLURM_REST_UNKNOWN_URL_METHOD,

src/common/slurm_errno.c
@@ -690,7 +690,7 @@ slurm_errtab_t slurm_errtab[] = {
 	{ ERRTAB_ENTRY(ESLURM_REST_MISSING_GID),
 	  "Missing UNIX group in the system"},
 	{ ERRTAB_ENTRY(ESLURM_REST_UNKNOWN_URL),
-	  "Unknown URL endpoint requested in query"},
+	  "Unable to find requested URL endpoint. Please query the '/openapi/v3' endpoint or visit 'https://slurm.schedmd.com/rest_api.html' for the OpenAPI specification which includes a list of all possible slurmrestd endpoints."},
 	{ ERRTAB_ENTRY(ESLURM_REST_UNKNOWN_URL_METHOD),
 	  "Requested HTTP query method is not supported at URL endpoint. Please query the '/openapi/v3' endpoint or visit 'https://slurm.schedmd.com/rest_api.html' for the OpenAPI specification which includes a list of all possible slurmrestd endpoints."},
 

commit 2be5be6fdcde10bef5e9c26257aae5a9464d5e37

slurmrestd - Use ESLURM_REST_UNKNOWN_URL in _resolve_path()
Return ESLURM_REST_UNKNOWN_URL instead of ESLURM_URL_INVALID_PATH when
path is not found for slurmrestd to have a descriptive error for the user.

Regression from db479b6208.

Ticket: 24052

src/slurmrestd/operations.c
@@ -281,7 +281,7 @@ static int _resolve_path(on_http_request_args_t *args, int *path_tag,
 
 	if (*path_tag == -1)
 		return _operations_router_reject(args, NULL,
-						 ESLURM_URL_INVALID_PATH, NULL);
+						 ESLURM_REST_UNKNOWN_URL, NULL);
 	else if (*path_tag == -2)
 		return _operations_router_reject(args, NULL,
 						 ESLURM_REST_UNKNOWN_URL_METHOD,