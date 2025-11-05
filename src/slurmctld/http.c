SchedMD/slurm

commit 9691798d5877b5a4d3abec27ed0641d6d55dd05c

slurmctld - Add basic HTTP implementation
Issue: 50538, 50189

src/slurmctld/http.c
@@ -33,4 +33,64 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
 
+#include "src/common/http.h"
+#include "src/common/http_con.h"
+#include "src/common/http_router.h"
+#include "src/common/pack.h"
+#include "src/common/xmalloc.h"
+#include "src/common/xstring.h"
+
 #include "src/slurmctld/http.h"
+
+static int _reply_error(http_con_t *hcon, const char *name,
+			const http_con_request_t *request, int err)
+{
+	char *body = NULL, *at = NULL;
+	int rc = EINVAL;
+
+	xstrfmtcatat(body, &at, "slurmctld HTTP server request for '%s %s':\n",
+		     get_http_method_string(request->method),
+		     request->url.path);
+
+	if (err)
+		xstrfmtcatat(body, &at, "Failed: %s\n", slurm_strerror(err));
+
+	rc = http_con_send_response(hcon, http_status_from_error(err), NULL,
+				    true,
+				    &SHADOW_BUF_INITIALIZER(body, strlen(body)),
+				    MIME_TYPE_TEXT);
+
+	xfree(body);
+	return rc;
+}
+
+static int _req_not_found(http_con_t *hcon, const char *name,
+			  const http_con_request_t *request, void *arg)
+{
+	return _reply_error(hcon, name, request, ESLURM_REST_UNKNOWN_URL);
+}
+
+extern void http_init(void)
+{
+	http_router_init(_req_not_found);
+}
+
+extern void http_fini(void)
+{
+	http_router_fini();
+}
+
+extern int on_http_connection(conmgr_fd_t *con)
+{
+	static const http_con_server_events_t events = {
+		.on_request = http_router_on_request,
+	};
+	int rc = EINVAL;
+	conmgr_fd_ref_t *ref = conmgr_fd_new_ref(con);
+
+	rc = http_con_assign_server(ref, NULL, &events, NULL);
+
+	conmgr_fd_free_ref(&ref);
+
+	return rc;
+}

src/slurmctld/http.h
@@ -39,4 +39,14 @@
 #ifndef _SLURMCTLD_HTTP_H
 #define _SLURMCTLD_HTTP_H
 
+#include <stdbool.h>
+
+#include "src/conmgr/conmgr.h"
+
+/* Setup incoming HTTP connection */
+extern int on_http_connection(conmgr_fd_t *con);
+
+extern void http_init(void);
+extern void http_fini(void);
+
 #endif

commit 5e4df90f4a7c98a07ae5a7e11430508949cbdeef

slurmctld - Provide non-slurmrestd error for URL path not found
Regression from 9691798d58.

Ticket: 24052

src/slurmctld/http.c
@@ -73,7 +73,7 @@ static int _reply_error(http_con_t *hcon, const char *name,
 static int _req_not_found(http_con_t *hcon, const char *name,
 			  const http_con_request_t *request, void *arg)
 {
-	return _reply_error(hcon, name, request, ESLURM_REST_UNKNOWN_URL);
+	return _reply_error(hcon, name, request, ESLURM_URL_INVALID_PATH);
 }
 
 static int _req_metrics(http_con_t *hcon, const char *name,