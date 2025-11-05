SchedMD/slurm

commit b3ceee66c94f722809d0e7dbf2506b24c167c592

slurmrestd - Use Slurm error in _operations_router_reject()
Use Slurm error string in _operations_router_reject() when err arg is not
provided by caller.

Issue: 50651

src/slurmrestd/operations.c
@@ -231,7 +231,6 @@ static int _operations_router_reject(on_http_request_args_t *args,
 		.headers = list_create(NULL),
 		.http_major = args->http_major,
 		.http_minor = args->http_minor,
-		.body = err,
 		.body_encoding = (body_encoding ? body_encoding : "text/plain"),
 		.body_length = (err ? strlen(err) : 0),
 	};
@@ -243,6 +242,11 @@ static int _operations_router_reject(on_http_request_args_t *args,
 
 	send_args.con = conmgr_fd_get_ref(args->con);
 	send_args.status_code = http_status_from_error(error_code);
+
+	if (!err)
+		send_args.body = slurm_strerror(error_code);
+	else
+		send_args.body = err;
 
 	/* Always warn that connection will be closed after the body is sent */
 	list_append(send_args.headers, &close);

commit b29778f6f1912eae0cc9755a8d54ec695117e2ec

slurmrestd - Always Set body_length in _operations_router_reject()
Regression from b3ceee66c9.

Changelog: slurmrestd - Fix regression that caused requests for unknown or
 unsupported URL paths to not include a descriptive error.
Ticket: 24052

src/slurmrestd/operations.c
@@ -234,7 +234,6 @@ static int _operations_router_reject(on_http_request_args_t *args,
 		.http_minor = args->http_minor,
 		.body_encoding =
 			(body_encoding ? body_encoding : MIME_TYPE_TEXT),
-		.body_length = (err ? strlen(err) : 0),
 	};
 	http_header_t close = {
 		.magic = HTTP_HEADER_MAGIC,
@@ -249,6 +248,8 @@ static int _operations_router_reject(on_http_request_args_t *args,
 		send_args.body = slurm_strerror(error_code);
 	else
 		send_args.body = err;
+
+	send_args.body_length = strlen(send_args.body);
 
 	/* Always warn that connection will be closed after the body is sent */
 	list_append(send_args.headers, &close);