SchedMD/slurm

commit 280a4546f84e9531cf6cefadc29937d1c778f37b

conmgr - Pass conmgr_callback_args_t to all conmgr_events_t callbacks
Pass conmgr_callback_args_t to all conmgr_events_t callbacks to make the
callbacks uniform and prepare for con to replaced by references.

Issue: 50528

src/scrun/anchor.c
@@ -580,8 +580,9 @@ static void _catch_sigchld(conmgr_callback_args_t conmgr_args, void *arg)
 	} while (pid > 0);
 }
 
-static void *_on_cs_connection(conmgr_fd_t *con, void *arg)
+static void *_on_cs_connection(conmgr_callback_args_t conmgr_args, void *arg)
 {
+	conmgr_fd_t *con = conmgr_args.con;
 	int tty;
 	xassert(!arg);
 
@@ -618,7 +619,7 @@ static void *_on_cs_connection(conmgr_fd_t *con, void *arg)
 	return &state;
 }
 
-static int _on_cs_data(conmgr_fd_t *con, void *arg)
+static int _on_cs_data(conmgr_callback_args_t conmgr_args, void *arg)
 {
 	xassert(!arg);
 
@@ -633,7 +634,7 @@ static int _on_cs_data(conmgr_fd_t *con, void *arg)
 	return EINVAL;
 }
 
-static void _on_cs_finish(conmgr_fd_t *con, void *arg)
+static void _on_cs_finish(conmgr_callback_args_t conmgr_args, void *arg)
 {
 	xassert(arg == &state);
 	check_state();
@@ -1139,8 +1140,10 @@ extern void on_allocation(conmgr_callback_args_t conmgr_args, void *arg)
 		_try_start();
 }
 
-static void *_on_connection(conmgr_fd_t *con, void *arg)
+static void *_on_connection(conmgr_callback_args_t conmgr_args, void *arg)
 {
+	conmgr_fd_t *con = conmgr_args.con;
+
 	/* may or may not need to be locked for this one */
 	check_state();
 	xassert(!arg);
@@ -1150,7 +1153,7 @@ static void *_on_connection(conmgr_fd_t *con, void *arg)
 	return &state;
 }
 
-static void _on_connection_finish(conmgr_fd_t *con, void *arg)
+static void _on_connection_finish(conmgr_callback_args_t conmgr_args, void *arg)
 {
 	xassert(arg == &state);
 	check_state();
@@ -1194,9 +1197,10 @@ static int _send_state(conmgr_fd_t *con, slurm_msg_t *req_msg)
 	return rc;
 }
 
-static int _on_connection_msg(conmgr_fd_t *con, slurm_msg_t *msg, int unpack_rc,
-			      void *arg)
+static int _on_connection_msg(conmgr_callback_args_t conmgr_args,
+			      slurm_msg_t *msg, int unpack_rc, void *arg)
 {
+	conmgr_fd_t *con = conmgr_args.con;
 	int rc;
 	uid_t user_id;
 
@@ -1312,7 +1316,7 @@ static void _open_pty(void)
 	state.pts = pts;
 }
 
-static int _on_startup_con_data(conmgr_fd_t *con, void *arg)
+static int _on_startup_con_data(conmgr_callback_args_t conmgr_args, void *arg)
 {
 	xassert(arg == &state);
 	check_state();
@@ -1320,8 +1324,9 @@ static int _on_startup_con_data(conmgr_fd_t *con, void *arg)
 	fatal("%s: unexpected data", __func__);
 }
 
-static void *_on_startup_con(conmgr_fd_t *con, void *arg)
+static void *_on_startup_con(conmgr_callback_args_t conmgr_args, void *arg)
 {
+	conmgr_fd_t *con = conmgr_args.con;
 	bool queue = false;
 
 	xassert(!arg);
@@ -1349,8 +1354,9 @@ static void *_on_startup_con(conmgr_fd_t *con, void *arg)
 	return &state;
 }
 
-static void _on_startup_con_fin(conmgr_fd_t *con, void *arg)
+static void _on_startup_con_fin(conmgr_callback_args_t conmgr_args, void *arg)
 {
+	conmgr_fd_t *con = conmgr_args.con;
 	xassert(arg == &state);
 
 	write_lock_state();

src/common/http_con.c
@@ -682,8 +682,9 @@ static int _on_content_complete(void *arg)
 	return rc;
 }
 
-extern int _on_data(conmgr_fd_t *con, void *arg)
+extern int _on_data(conmgr_callback_args_t conmgr_args, void *arg)
 {
+	conmgr_fd_t *con = conmgr_args.con;
 	http_con_t *hcon = arg;
 	static const http_parser_callbacks_t callbacks = {
 		.on_request = _on_request,
@@ -745,8 +746,9 @@ cleanup:
 	return rc;
 }
 
-static void _on_finish(conmgr_fd_t *con, void *arg)
+static void _on_finish(conmgr_callback_args_t conmgr_args, void *arg)
 {
+	conmgr_fd_t *con = conmgr_args.con;
 	http_con_t *hcon = arg;
 	void *hcon_arg = hcon->arg;
 	conmgr_fd_ref_t *hcon_con = NULL;

src/common/http_switch.c
@@ -129,9 +129,10 @@ static int _on_match_rpc(conmgr_fd_t *con)
 	return rc;
 }
 
-extern int http_switch_on_data(conmgr_fd_t *con,
+extern int http_switch_on_data(conmgr_callback_args_t conmgr_args,
 			       int (*on_http)(conmgr_fd_t *con))
 {
+	conmgr_fd_t *con = conmgr_args.con;
 	buf_t *buffer = conmgr_fd_shadow_in_buffer(con);
 	rpc_fingerprint_t status = rpc_fingerprint(buffer);
 	int rc = SLURM_SUCCESS;

src/common/http_switch.h
@@ -58,11 +58,11 @@ extern conmgr_con_flags_t http_switch_con_flags(void);
  * Changes mode to CON_TYPE_RPC when fingerprints as Slurm RPC.
  * Calls on_http() when fingerprint matches HTTP.
  *
- * IN con - connection to fingerprint
+ * IN conmgr_args - Args containing connection to fingerprint
  * IN on_http - callback to call when fingerprint for HTTP matches
  * RET SLURM_SUCCESS or error
  */
-extern int http_switch_on_data(conmgr_fd_t *con,
+extern int http_switch_on_data(conmgr_callback_args_t conmgr_args,
 			       int (*on_http)(conmgr_fd_t *con));
 
 /* Initialize all plugins and state needed for HTTP switching */

src/common/stepd_proxy.c
@@ -153,8 +153,9 @@ static int _slurmd_send_recv_msg(conmgr_fd_t *con, slurm_msg_t *req,
 	return rc;
 }
 
-static int _on_data_local_socket(conmgr_fd_t *con, void *arg)
+static int _on_data_local_socket(conmgr_callback_args_t conmgr_args, void *arg)
 {
+	conmgr_fd_t *con = conmgr_args.con;
 	buf_t *in = NULL;
 	uint32_t length, timeout, r_uid;
 	uint16_t msg_type, protocol_version, proxy_type;

src/conmgr/con.c
@@ -680,7 +680,7 @@ extern void wrap_on_connection(conmgr_callback_args_t conmgr_args, void *arg)
 			 __func__, con->name,
 			 (uintptr_t) con->events->on_listen_connect);
 
-		arg = con->events->on_listen_connect(con, con->new_arg);
+		arg = con->events->on_listen_connect(conmgr_args, con->new_arg);
 
 		log_flag(CONMGR, "%s: [%s] END func=0x%"PRIxPTR" arg=0x%"PRIxPTR,
 			 __func__, con->name,
@@ -691,7 +691,7 @@ extern void wrap_on_connection(conmgr_callback_args_t conmgr_args, void *arg)
 			 __func__, con->name,
 			 (uintptr_t) con->events->on_connection);
 
-		arg = con->events->on_connection(con, con->new_arg);
+		arg = con->events->on_connection(conmgr_args, con->new_arg);
 
 		log_flag(CONMGR, "%s: [%s] END func=0x%"PRIxPTR" arg=0x%"PRIxPTR,
 			 __func__, con->name,

src/conmgr/conmgr.h
@@ -137,49 +137,50 @@ typedef struct {
 	/*
 	 * Call back for new listener for setup
 	 *
-	 * IN con - connection handler
+	 * IN conmgr_args - Args relaying conmgr callback state
 	 * IN arg - arg ptr handed to fd processing functions
 	 * RET arg ptr to hand to events
 	 */
-	void *(*on_listen_connect)(conmgr_fd_t *con, void *arg);
+	void *(*on_listen_connect)(conmgr_callback_args_t conmgr_args,
+				   void *arg);
 
 	/*
 	 * Call back when listener ended.
 	 * Called once per connection right before connection is xfree()ed.
 	 *
-	 * IN con - connection handler
+	 * IN conmgr_args - Args relaying conmgr callback state
 	 * IN arg - ptr to be handed return of on_connection().
 	 * 	Ownership of arg pointer returned to caller as it will not be
 	 * 	used anymore.
 	 */
-	void (*on_listen_finish)(conmgr_fd_t *con, void *arg);
+	void (*on_listen_finish)(conmgr_callback_args_t conmgr_args, void *arg);
 
 	/*
 	 * Call back for new connection for setup
 	 *
-	 * IN fd file descriptor of new connection
+	 * IN conmgr_args - Args relaying conmgr callback state
 	 * IN arg - arg ptr handed to fd processing functions
 	 * RET arg ptr to hand to events
 	 */
-	void *(*on_connection)(conmgr_fd_t *con, void *arg);
+	void *(*on_connection)(conmgr_callback_args_t conmgr_args, void *arg);
 
 	/*
 	 * Call back when there is data ready in "in" buffer
 	 * This may be called several times in the same connection.
 	 * Only called when type = CON_TYPE_RAW.
 	 *
-	 * IN con connection handler
+	 * IN conmgr_args - Args relaying conmgr callback state
 	 * IN arg ptr to be handed return of on_connection() callback.
 	 * RET SLURM_SUCCESS or error to kill connection
 	 */
-	int (*on_data)(conmgr_fd_t *con, void *arg);
+	int (*on_data)(conmgr_callback_args_t conmgr_args, void *arg);
 
 	/*
 	 * Call back when there is new RPC msg ready
 	 * This may be called several times in the same connection.
 	 * Only called when type = CON_TYPE_RPC.
 	 *
-	 * IN con connection handler
+	 * IN conmgr_args - Args relaying conmgr callback state
 	 * IN msg ptr to new msg (call must slurm_free_msg())
 	 * IN unpack_rc return code from unpacking RPC
 	 * WARNING: always check unpack_rc and msg->auth_ids_set before
@@ -187,19 +188,19 @@ typedef struct {
 	 * IN arg ptr to be handed return of on_connection() callback.
 	 * RET SLURM_SUCCESS or error to kill connection
 	 */
-	int (*on_msg)(conmgr_fd_t *con, slurm_msg_t *msg, int unpack_rc,
-		      void *arg);
+	int (*on_msg)(conmgr_callback_args_t conmgr_args, slurm_msg_t *msg,
+		      int unpack_rc, void *arg);
 
 	/*
 	 * Call back when connection ended.
 	 * Called once per connection right before connection is xfree()ed.
 	 *
-	 * IN con - connection handler
+	 * IN conmgr_args - Args relaying conmgr callback state
 	 * IN arg - ptr to be handed return of on_connection().
 	 * 	Ownership of arg pointer returned to caller as it will not be
 	 * 	used anymore.
 	 */
-	void (*on_finish)(conmgr_fd_t *con, void *arg);
+	void (*on_finish)(conmgr_callback_args_t conmgr_args, void *arg);
 
 	/*
 	 * Call back when read timeout occurs
@@ -208,11 +209,11 @@ typedef struct {
 	 * If on_read_timeout=NULL is treated same as returning
 	 *	SLURM_PROTOCOL_SOCKET_IMPL_TIMEOUT
 	 *
-	 * IN con - connection handler
+	 * IN conmgr_args - Args relaying conmgr callback state
 	 * IN arg ptr to be handed return of on_connection() callback.
 	 * RET SLURM_SUCCESS to wait timeout again or error to kill connection
 	 */
-	int (*on_read_timeout)(conmgr_fd_t *con, void *arg);
+	int (*on_read_timeout)(conmgr_callback_args_t conmgr_args, void *arg);
 
 	/*
 	 * Call back when write timeout occurs
@@ -221,11 +222,11 @@ typedef struct {
 	 * If on_read_timeout=NULL is treated same as returning
 	 *	SLURM_PROTOCOL_SOCKET_IMPL_TIMEOUT
 	 *
-	 * IN con - connection handler
+	 * IN conmgr_args - Args relaying conmgr callback state
 	 * IN arg ptr to be handed return of on_connection() callback.
 	 * RET SLURM_SUCCESS to wait timeout again or error to kill connection
 	 */
-	int (*on_write_timeout)(conmgr_fd_t *con, void *arg);
+	int (*on_write_timeout)(conmgr_callback_args_t conmgr_args, void *arg);
 
 	/*
 	 * Call back when connect timeout occurs
@@ -234,11 +235,12 @@ typedef struct {
 	 * If on_read_timeout=NULL is treated same as returning
 	 *	SLURM_PROTOCOL_SOCKET_IMPL_TIMEOUT
 	 *
-	 * IN con - connection handler
+	 * IN conmgr_args - Args relaying conmgr callback state
 	 * IN arg - arg ptr handed to fd processing functions
 	 * RET SLURM_SUCCESS to wait timeout again or error to kill connection
 	 */
-	int (*on_connect_timeout)(conmgr_fd_t *con, void *arg);
+	int (*on_connect_timeout)(conmgr_callback_args_t conmgr_args,
+				  void *arg);
 } conmgr_events_t;
 
 typedef enum {

src/conmgr/io.c
@@ -331,7 +331,7 @@ extern void wrap_on_data(conmgr_callback_args_t conmgr_args, void *arg)
 {
 	conmgr_fd_t *con = conmgr_args.con;
 	int rc = EINVAL;
-	int (*callback)(conmgr_fd_t *con, void *arg) = NULL;
+	STRUCT_FIELD_TYPEOF(conmgr_events_t, on_data) callback = NULL;
 	const char *callback_string = NULL;
 	void *con_arg = NULL;
 	buf_t shadow_buffer = { 0 };
@@ -384,7 +384,7 @@ extern void wrap_on_data(conmgr_callback_args_t conmgr_args, void *arg)
 		 __func__, con->name, callback_string, (uintptr_t) con_arg,
 		 (uintptr_t) callback);
 
-	rc = callback(con, con_arg);
+	rc = callback(conmgr_args, con_arg);
 
 	log_flag(CONMGR, "%s: [%s] END func=%s(arg=0x%"PRIxPTR")@0x%"PRIxPTR"=[%d]%s",
 		 __func__, con->name, callback_string, (uintptr_t) con_arg,

commit fe9aba504bb20853919cb9f94203b07204f89ab0

scrun - Remove con from _on_startup_con_fin()
Avoid NDEBUG builds have compiler warning about con being unused.

Regression from 280a4546f8.

Issue: 50705

src/scrun/anchor.c
@@ -1356,11 +1356,10 @@ static void *_on_startup_con(conmgr_callback_args_t conmgr_args, void *arg)
 
 static void _on_startup_con_fin(conmgr_callback_args_t conmgr_args, void *arg)
 {
-	conmgr_fd_t *con = conmgr_args.con;
 	xassert(arg == &state);
 
 	write_lock_state();
-	xassert(state.startup_con == con);
+	xassert(state.startup_con == conmgr_args.con);
 	debug4("%s: [%s] create command parent notified of start",
 	       __func__, conmgr_fd_get_name(state.startup_con));
 	xassert(state.startup_con);