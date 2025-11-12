0eb9f9f44dfab4b5de4d08596a025da77f8c981d
Nathan Rini <nate@schedmd.com>
2025-11-11 21:19:46 -0700

    slurmctld - Convert _slurm_rpc_resv_delete() to TIMER_STR()
    Issue: 50113

--- src/slurmctld/proc_req.c
+++ src/slurmctld/proc_req.c
@@ -4460,7 +4460,7 @@ static void _slurm_rpc_resv_delete(slurm_msg_t *msg)
 		slurm_send_rc_msg(msg, error_code);
 	} else {
 		info("%s complete for %s %s",
-		     __func__, resv_desc_ptr->name, TIME_STR);
+		     __func__, resv_desc_ptr->name, TIMER_STR());
 		slurm_send_rc_msg(msg, SLURM_SUCCESS);
 
 		queue_job_scheduler();
