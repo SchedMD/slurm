7fccc98f62e017d357e4738029ff736b797bf400
Nathan Rini <nate@schedmd.com>
2025-11-11 21:36:45 -0700

    Convert to TIMER_DURATION_USEC() in job_time_limit()
    Avoid accessing timer's tv1 variable directly.
    
    Issue: 50113

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -9502,7 +9502,7 @@ void job_time_limit(void)
 		 */
 	time_check:
 		/* Use a hard-coded 3 second timeout, with a 1 second sleep. */
-		if (slurm_delta_tv(&tv1) >= 3000000 &&
+		if ((TIMER_DURATION_USEC() >= 3000000) &&
 		    list_peek_next(job_iterator)) {
 			END_TIMER;
 			debug("%s: yielding locks after testing %d jobs, %s",
