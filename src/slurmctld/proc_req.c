0ebc0b64c7dd80eebe350bd86de22e245ca8cfed
Nathan Rini <nate@schedmd.com>
2025-11-11 21:19:01 -0700

    slurmctld - Convert _slurm_rpc_delete_partition() to TIMER_STR()
    Issue: 50113

--- src/slurmctld/proc_req.c
+++ src/slurmctld/proc_req.c
@@ -4303,7 +4303,7 @@ static void _slurm_rpc_delete_partition(slurm_msg_t *msg)
 		slurm_send_rc_msg(msg, error_code);
 	} else {
 		info("%s complete for %s %s",
-		     __func__, part_desc_ptr->name, TIME_STR);
+		     __func__, part_desc_ptr->name, TIMER_STR());
 		slurm_send_rc_msg(msg, SLURM_SUCCESS);
 
 		save_all_state();	/* Has own locking */
