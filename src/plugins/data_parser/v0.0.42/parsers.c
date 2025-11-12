SchedMD/slurm

commit 700de4a2eb1602ad887a22d81390726de43c3d9e

Remove handling for --mem-bind=sort
Was only supported on KNL which has been removed.

src/plugins/data_parser/v0.0.42/parsers.c
@@ -8249,7 +8249,6 @@ static const flag_bit_t PARSER_FLAG_ARRAY(MEMORY_BINDING_TYPE)[] = {
 	add_flag_equal(MEM_BIND_MASK, MEM_BIND_TYPE_MASK, "MASK"),
 	add_flag_equal(MEM_BIND_LOCAL, MEM_BIND_TYPE_MASK, "LOCAL"),
 	add_flag_masked_bit(MEM_BIND_VERBOSE, MEM_BIND_VERBOSE, "VERBOSE"),
-	add_flag_masked_bit(MEM_BIND_SORT, MEM_BIND_TYPE_FLAGS_MASK, "SORT"),
 	add_flag_masked_bit(MEM_BIND_PREFER, MEM_BIND_TYPE_FLAGS_MASK, "PREFER"),
 };
 

src/plugins/data_parser/v0.0.43/parsers.c
@@ -8984,7 +8984,6 @@ static const flag_bit_t PARSER_FLAG_ARRAY(MEMORY_BINDING_TYPE)[] = {
 	add_flag_equal(MEM_BIND_MASK, MEM_BIND_TYPE_MASK, "MASK"),
 	add_flag_equal(MEM_BIND_LOCAL, MEM_BIND_TYPE_MASK, "LOCAL"),
 	add_flag_masked_bit(MEM_BIND_VERBOSE, MEM_BIND_VERBOSE, "VERBOSE"),
-	add_flag_masked_bit(MEM_BIND_SORT, MEM_BIND_TYPE_FLAGS_MASK, "SORT"),
 	add_flag_masked_bit(MEM_BIND_PREFER, MEM_BIND_TYPE_FLAGS_MASK, "PREFER"),
 };
 

src/plugins/data_parser/v0.0.41/parsers.c
@@ -8356,7 +8356,6 @@ static const flag_bit_t PARSER_FLAG_ARRAY(MEMORY_BINDING_TYPE)[] = {
 	add_flag_equal(MEM_BIND_MASK, MEM_BIND_TYPE_MASK, "MASK"),
 	add_flag_equal(MEM_BIND_LOCAL, MEM_BIND_TYPE_MASK, "LOCAL"),
 	add_flag_masked_bit(MEM_BIND_VERBOSE, MEM_BIND_VERBOSE, "VERBOSE"),
-	add_flag_masked_bit(MEM_BIND_SORT, MEM_BIND_TYPE_FLAGS_MASK, "SORT"),
 	add_flag_masked_bit(MEM_BIND_PREFER, MEM_BIND_TYPE_FLAGS_MASK, "PREFER"),
 };
 

src/plugins/data_parser/v0.0.44/parsers.c
@@ -9009,7 +9009,6 @@ static const flag_bit_t PARSER_FLAG_ARRAY(MEMORY_BINDING_TYPE)[] = {
 	add_flag_equal(MEM_BIND_MASK, MEM_BIND_TYPE_MASK, "MASK"),
 	add_flag_equal(MEM_BIND_LOCAL, MEM_BIND_TYPE_MASK, "LOCAL"),
 	add_flag_masked_bit(MEM_BIND_VERBOSE, MEM_BIND_VERBOSE, "VERBOSE"),
-	add_flag_masked_bit(MEM_BIND_SORT, MEM_BIND_TYPE_FLAGS_MASK, "SORT"),
 	add_flag_masked_bit(MEM_BIND_PREFER, MEM_BIND_TYPE_FLAGS_MASK, "PREFER"),
 };
 

doc/man/man1/salloc.1
@@ -1303,7 +1303,6 @@ The following informational environment variables are set when
 .nf
 	SLURM_MEM_BIND_LIST
 	SLURM_MEM_BIND_PREFER
-	SLURM_MEM_BIND_SORT
 	SLURM_MEM_BIND_TYPE
 	SLURM_MEM_BIND_VERBOSE
 .fi

doc/man/man1/sbatch.1
@@ -1478,7 +1478,6 @@ The following informational environment variables are set when
 .nf
    SLURM_MEM_BIND_LIST
    SLURM_MEM_BIND_PREFER
-   SLURM_MEM_BIND_SORT
    SLURM_MEM_BIND_TYPE
    SLURM_MEM_BIND_VERBOSE
 .fi

doc/man/man1/srun.1
@@ -1770,7 +1770,6 @@ The following informational environment variables are set when
 .nf
 	SLURM_MEM_BIND_LIST
 	SLURM_MEM_BIND_PREFER
-	SLURM_MEM_BIND_SORT
 	SLURM_MEM_BIND_TYPE
 	SLURM_MEM_BIND_VERBOSE
 .fi

src/common/env.c
@@ -498,7 +498,6 @@ int setup_env(env_t *env, bool preserve_env)
 	if (env->mem_bind_type && (env->stepid != SLURM_INTERACTIVE_STEP)) {
 		char *str_verbose, *str_bind_type = NULL, *str_bind_list;
 		char *str_prefer = NULL, *str_bind = NULL;
-		char *str_bind_sort = NULL;
 
 		if (env->batch_flag) {
 			unsetenvp(env->env, "SBATCH_MEM_BIND");
@@ -510,7 +509,6 @@ int setup_env(env_t *env, bool preserve_env)
 			unsetenvp(env->env, "SLURM_MEM_BIND");
 			unsetenvp(env->env, "SLURM_MEM_BIND_LIST");
 			unsetenvp(env->env, "SLURM_MEM_BIND_PREFER");
-			unsetenvp(env->env, "SLURM_MEM_BIND_SORT");
 			unsetenvp(env->env, "SLURM_MEM_BIND_TYPE");
 			unsetenvp(env->env, "SLURM_MEM_BIND_VERBOSE");
 		}
@@ -532,9 +530,6 @@ int setup_env(env_t *env, bool preserve_env)
 		} else if (env->mem_bind_type & MEM_BIND_LOCAL) {
 			str_bind_type = "local";
 		}
-
-		if (env->mem_bind_type & MEM_BIND_SORT)
-			str_bind_sort = "sort";
 
 		if (env->mem_bind)
 			str_bind_list = env->mem_bind;
@@ -569,12 +564,6 @@ int setup_env(env_t *env, bool preserve_env)
 				error("Unable to set SBATCH_MEM_BIND_PREFER");
 				rc = SLURM_ERROR;
 			}
-			if (str_bind_sort &&
-			    setenvf(&env->env, "SBATCH_MEM_BIND_SORT", "%s",
-				    str_bind_sort)) {
-				error("Unable to set SBATCH_MEM_BIND_SORT");
-				rc = SLURM_ERROR;
-			}
 			if (setenvf(&env->env, "SBATCH_MEM_BIND_TYPE", "%s",
 				    str_bind_type)) {
 				error("Unable to set SBATCH_MEM_BIND_TYPE");
@@ -599,12 +588,6 @@ int setup_env(env_t *env, bool preserve_env)
 			    setenvf(&env->env, "SLURM_MEM_BIND_PREFER", "%s",
 				    str_prefer)) {
 				error("Unable to set SLURM_MEM_BIND_PREFER");
-				rc = SLURM_ERROR;
-			}
-			if (str_bind_sort &&
-			    setenvf(&env->env, "SLURM_MEM_BIND_SORT", "%s",
-				    str_bind_sort)) {
-				error("Unable to set SLURM_MEM_BIND_SORT");
 				rc = SLURM_ERROR;
 			}
 			if (setenvf(&env->env, "SLURM_MEM_BIND_TYPE", "%s",

commit a5a96060b5db93f034025278df8c3609215ea5d2

data_parser/v0.0.4[2,3,4]/parsers.c - mark SORT as a removed flag
The SORT flag was removed, but that broke the openapi specs
that have been snapshotted and expect the flag to be there. This adds
SORT back in and marks it as "removed" so that the tests pass, but the
functionality is still gone.

Regression from 700de4a2eb.

Ticket: 23807

src/plugins/data_parser/v0.0.42/parsers.c
@@ -8255,6 +8255,7 @@ static const flag_bit_t PARSER_FLAG_ARRAY(MEMORY_BINDING_TYPE)[] = {
 	add_flag_equal(MEM_BIND_MASK, MEM_BIND_TYPE_MASK, "MASK"),
 	add_flag_equal(MEM_BIND_LOCAL, MEM_BIND_TYPE_MASK, "LOCAL"),
 	add_flag_masked_bit(MEM_BIND_VERBOSE, MEM_BIND_VERBOSE, "VERBOSE"),
+	add_flag_removed("SORT", SLURM_25_11_PROTOCOL_VERSION),
 	add_flag_masked_bit(MEM_BIND_PREFER, MEM_BIND_TYPE_FLAGS_MASK, "PREFER"),
 };
 

src/plugins/data_parser/v0.0.43/parsers.c
@@ -8987,6 +8987,7 @@ static const flag_bit_t PARSER_FLAG_ARRAY(MEMORY_BINDING_TYPE)[] = {
 	add_flag_equal(MEM_BIND_MASK, MEM_BIND_TYPE_MASK, "MASK"),
 	add_flag_equal(MEM_BIND_LOCAL, MEM_BIND_TYPE_MASK, "LOCAL"),
 	add_flag_masked_bit(MEM_BIND_VERBOSE, MEM_BIND_VERBOSE, "VERBOSE"),
+	add_flag_removed("SORT", SLURM_25_11_PROTOCOL_VERSION),
 	add_flag_masked_bit(MEM_BIND_PREFER, MEM_BIND_TYPE_FLAGS_MASK, "PREFER"),
 };
 