b1eaf37a29a2c23e36dcdb5f9ded8345556c64f8
Albert Gil <albert.gil@schedmd.com>
2025-11-19 14:42:11 +0100

    Testsuite - Improve test_116_46 avoiding false failures
    Sometimes the list of running and/or killed tasks may not be received
    in a single message, but we may get it split.
    Improve the test to support getting it split too.

--- testsuite/python/tests/test_116_46.py
+++ testsuite/python/tests/test_116_46.py
@@ -37,9 +37,29 @@ sleep 20
     assert (
         re.search(r"First task exited", run_error) is not None
     ), "First task did not exit"
+
+    # Sometimes the list of running and/or killed tasks may not be received
+    # in a single message, but we may get it split.
+    # E.g.:
+    # srun: error: node0: tasks 0,2-6,8-9: Killed
+    # srun: error: node0: task 7: Killed
+    #
+    # We should support receiving it in any combination of ranges
+
+    expected_tasks = sorted(map(int, atf.node_range_to_list("[0,2-9]")))
+
+    # Assert running tasks
+    matches = re.findall(r"task[s]?\s+([0-9,\-]+): running", run_error)
+    trange = "[" + ",".join(matches) + "]"
+    tlist = sorted(map(int, atf.node_range_to_list(trange)))
     assert (
-        re.search(r"tasks 0,2-9: running", run_error) is not None
-    ), "Other tasks not running"
+        tlist == expected_tasks
+    ), f"Running tasks should be {expected_tasks}, but got {tlist}"
+
+    # Assert killed tasks
+    matches = re.findall(r"task[s]?\s+([0-9,\-]+): Killed", run_error)
+    trange = "[" + ",".join(matches) + "]"
+    tlist = sorted(map(int, atf.node_range_to_list(trange)))
     assert (
-        re.search(r"tasks 0,2-9: Killed", run_error) is not None
-    ), "Other tasks not killed"
+        tlist == expected_tasks
+    ), f"Killed tasks should be {expected_tasks}, but got {tlist}"
