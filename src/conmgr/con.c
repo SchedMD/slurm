ba49e3c21cf2bde449b50f8f0f1078eee06f9672
Ben Glines <bglines@nvidia.com>
2025-12-05 20:56:51 -0700

    conmgr - Add tls_cert to conmgr_create_connect_socket()
    Allow tls_cert to be specified for client connections
    
    Issue: 50762

--- src/conmgr/con.c
+++ src/conmgr/con.c
@@ -1219,7 +1219,7 @@ extern int conmgr_create_connect_socket(conmgr_con_type_t type,
 					conmgr_con_flags_t flags,
 					slurm_addr_t *addr, socklen_t addrlen,
 					const conmgr_events_t *events,
-					void *arg)
+					char *tls_cert, void *arg)
 {
 	int fd = -1, rc = SLURM_ERROR;
 	//socklen_t bindlen = 0;
@@ -1285,7 +1285,7 @@ again:
 	}
 
 	return add_connection(type, NULL, fd, fd, events, flags, addr, addrlen,
-			      false, NULL, NULL, NULL, arg);
+			      false, NULL, NULL, tls_cert, arg);
 }
 
 /* WARNING: caller must not hold mgr.mutex lock */
