a1b592723389bcc6e262b4e6d6a733dd0fbedc43
Nathan Rini <nate@schedmd.com>
2025-12-08 14:08:59 -0700

    http_auth/jwt - Add implementation
    Issue: 50707

--- src/plugins/http_auth/jwt/http_auth_jwt.c
+++ src/plugins/http_auth/jwt/http_auth_jwt.c
@@ -32,3 +32,120 @@
  *  with Slurm; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
+
+#include "config.h"
+
+#include <stdint.h>
+
+#include "src/common/http_con.h"
+#include "src/common/read_config.h"
+#include "src/common/slurm_protocol_defs.h"
+#include "src/common/xmalloc.h"
+#include "src/common/xstring.h"
+
+#include "slurm/slurm_errno.h"
+
+#include "src/interfaces/auth.h"
+#include "src/interfaces/http_auth.h"
+
+/* Required Slurm plugin symbols: */
+const char plugin_name[] = "HTTP JWT authentication";
+const char plugin_type[] = "http_auth/jwt";
+const uint32_t plugin_version = SLURM_VERSION_NUMBER;
+
+/* Required for http_auth plugins: */
+const uint32_t plugin_id = HTTP_AUTH_PLUGIN_JWT;
+
+#define HTTP_HEADER_USER_TOKEN "X-SLURM-USER-TOKEN"
+#define HTTP_HEADER_AUTH "Authorization"
+#define HTTP_HEADER_AUTH_BEARER "Bearer "
+#define HTTP_HEADER_USER_NAME "X-SLURM-USER-NAME"
+
+extern int http_auth_p_init(void)
+{
+	return SLURM_SUCCESS;
+}
+
+extern void http_auth_p_fini(void)
+{
+	/* do nothing */
+}
+
+static int _headers(const char **token_ptr, const char **user_name_ptr,
+		    const char *name, const http_con_request_t *request)
+{
+	const char *token = NULL, *user_name = NULL, *bearer = NULL;
+
+	token = find_http_header(request->headers, HTTP_HEADER_USER_TOKEN);
+	bearer = find_http_header(request->headers, HTTP_HEADER_AUTH);
+	user_name = find_http_header(request->headers, HTTP_HEADER_USER_NAME);
+
+	if (!token && !user_name && !bearer) {
+		debug3("%s: [%s] skipping token authentication",
+		       __func__, name);
+		return ESLURM_AUTH_SKIP;
+	}
+
+	if (!token && !bearer) {
+		error("%s: [%s] missing header user token: %s",
+		      __func__, name, HTTP_HEADER_USER_TOKEN);
+		return ESLURM_AUTH_CRED_INVALID;
+	} else if (token && bearer) {
+		error("%s: [%s] mutually exclusive headers %s and %s found. Rejecting ambiguous authentication request.",
+		      __func__, name, HTTP_HEADER_USER_TOKEN, HTTP_HEADER_AUTH);
+		return ESLURM_AUTH_CRED_INVALID;
+	}
+
+	if (bearer) {
+		if (!xstrncmp(HTTP_HEADER_AUTH_BEARER, bearer,
+			      strlen(HTTP_HEADER_AUTH_BEARER))) {
+			token = (bearer + strlen(HTTP_HEADER_AUTH_BEARER));
+		} else {
+			error("%s: [%s] unexpected format for %s header: %s",
+			      __func__, name, HTTP_HEADER_AUTH, bearer);
+			return ESLURM_AUTH_CRED_INVALID;
+		}
+	}
+
+	*token_ptr = token;
+	*user_name_ptr = user_name;
+	return SLURM_SUCCESS;
+}
+
+extern int http_auth_p_authenticate(uid_t *uid_ptr, http_con_t *hcon,
+				    const char *name,
+				    const http_con_request_t *request)
+{
+	const char *token = NULL, *user_name = NULL;
+	void *cred = NULL;
+	int rc = ESLURM_AUTH_CRED_INVALID;
+	uid_t uid = SLURM_AUTH_NOBODY;
+
+	if ((rc = _headers(&token, &user_name, name, request)))
+		return rc;
+
+	if ((cred = auth_g_cred_generate(AUTH_PLUGIN_JWT, token, user_name)) &&
+	    !(rc = auth_g_verify(cred, slurm_conf.authinfo)))
+		uid = auth_g_get_uid(cred);
+
+	if (uid == SLURM_AUTH_NOBODY)
+		rc = ESLURM_AUTH_NOBODY;
+
+	if (uid_ptr)
+		*uid_ptr = uid;
+
+	auth_g_destroy(cred);
+	return rc;
+}
+
+extern int http_auth_p_proxy_token(http_con_t *hcon, const char *name,
+				   const http_con_request_t *request)
+{
+	const char *token = NULL, *user_name = NULL;
+	int rc = ESLURM_AUTH_CRED_INVALID;
+
+	if ((rc = _headers(&token, &user_name, name, request)))
+		return rc;
+
+	return auth_g_thread_config(token, user_name);
+}
