be99ef090e99dbcdd0a22e4e5931863b65fee318
Nathan Rini <nate@schedmd.com>
2026-01-08 09:45:14 -0700

    Always increment active_threads in _increment_thd_count()
    Change _increment_thd_count() to always increment active_threads, even
    when returning EWOULDBLOCK due to thread limit being reached. This makes
    the function behavior consistent with its name and pairs correctly with
    the _decrement_thd_count() call added in fd70f0ed4b.
    
    This caused a thread miscount due to calling _decrement_thd_count() in
    _try_service_msg() on EWOULDBLOCK.
    
    While this creates an increment-then-decrement pattern, it maintains
    consistency in function naming and correct accounting.
    
    Changelog: slurmd - Fix regression that could cause thread limits to not
     be enforced for handling incoming RPCs.
    Ticket: 24387

--- src/slurmd/slurmd/slurmd.c
+++ src/slurmd/slurmd/slurmd.c
@@ -662,6 +662,7 @@ static void _decrement_thd_count(void)
  */
 static int _increment_thd_count(bool block)
 {
+	int rc = SLURM_SUCCESS;
 	bool logged = false;
 
 	slurm_mutex_lock(&active_mutex);
@@ -675,13 +676,13 @@ static int _increment_thd_count(bool block)
 		if (block)  {
 			slurm_cond_wait(&active_cond, &active_mutex);
 		} else {
-			slurm_mutex_unlock(&active_mutex);
-			return EWOULDBLOCK;
+			rc = EWOULDBLOCK;
+			break;
 		}
 	}
 	active_threads++;
 	slurm_mutex_unlock(&active_mutex);
-	return SLURM_SUCCESS;
+	return rc;
 }
 
 /* secs IN - wait up to this number of seconds for all threads to complete */
