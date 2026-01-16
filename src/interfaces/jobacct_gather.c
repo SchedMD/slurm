SchedMD/slurm

commit af2c0bd43055e4486e4d455bbeda39b514a2b294

slurmstepd,jobacct_gather - Fix how extern step tracks energy
With commit 0f533f44 the extern step stopped tracking consumption for
TRES. However, stopping the energy gathering for the extern step also
disabled the job consumed energy metric. This commit restores the
energy tracking while avoiding to track other TRES, without having
to revert the mentioned commit, so that both the extern step and job
have the consumed energy metric again.

Changelog: Restore the energy consumption tracking for the extern step
 that was removed in commit 0f533f44. This re-enables the consumed
 energy metric for both the extern step and the job.
Co-authored-by: Oriol Vilarrubi <jvilarru@schedmd.com>
Ticket: 20207

src/interfaces/jobacct_gather.c
@@ -672,7 +672,7 @@ extern int jobacct_gather_add_task(pid_t pid, jobacct_id_t *jobacct_id,
 	jobacct = jobacctinfo_create(jobacct_id);
 
 	slurm_mutex_lock(&task_list_lock);
-	if (pid <= 0) {
+	if (pid < 0) {
 		error("invalid pid given (%d) for task acct", pid);
 		goto error;
 	} else if (!task_list) {
@@ -1223,19 +1223,21 @@ extern void jobacctinfo_aggregate(jobacctinfo_t *dest, jobacctinfo_t *from)
 	if (!from)
 		return;
 
-	dest->user_cpu_sec	+= from->user_cpu_sec;
-	dest->user_cpu_usec	+= from->user_cpu_usec;
-	if (dest->user_cpu_usec >= 1E6) {
-		dest->user_cpu_sec += dest->user_cpu_usec / 1E6;
-		dest->user_cpu_usec = dest->user_cpu_usec % (int)1E6;
+	if (from->pid) {
+		dest->user_cpu_sec += from->user_cpu_sec;
+		dest->user_cpu_usec += from->user_cpu_usec;
+		if (dest->user_cpu_usec >= 1E6) {
+			dest->user_cpu_sec += dest->user_cpu_usec / 1E6;
+			dest->user_cpu_usec = dest->user_cpu_usec % (int) 1E6;
+		}
+		dest->sys_cpu_sec += from->sys_cpu_sec;
+		dest->sys_cpu_usec += from->sys_cpu_usec;
+		if (dest->sys_cpu_usec >= 1E6) {
+			dest->sys_cpu_sec += dest->sys_cpu_usec / 1E6;
+			dest->sys_cpu_usec = dest->sys_cpu_usec % (int) 1E6;
+		}
+		dest->act_cpufreq += from->act_cpufreq;
 	}
-	dest->sys_cpu_sec	+= from->sys_cpu_sec;
-	dest->sys_cpu_usec	+= from->sys_cpu_usec;
-	if (dest->sys_cpu_usec >= 1E6) {
-		dest->sys_cpu_sec += dest->sys_cpu_usec / 1E6;
-		dest->sys_cpu_usec = dest->sys_cpu_usec % (int)1E6;
-	}
-	dest->act_cpufreq 	+= from->act_cpufreq;
 	if (dest->energy.consumed_energy != NO_VAL64) {
 		if (from->energy.consumed_energy == NO_VAL64)
 			dest->energy.consumed_energy = NO_VAL64;

src/plugins/jobacct_gather/cgroup/jobacct_gather_cgroup.c
@@ -253,6 +253,14 @@ extern int jobacct_gather_p_add_task(pid_t pid, jobacct_id_t *jobacct_id)
 {
 	int rc = SLURM_SUCCESS;
 
+	/*
+	 * If we are the extern step, then our PID is 0. In that case, we want
+	 * to return and not create any cgroups, as we do not exist as a real
+	 * process.
+	 */
+	if (!pid)
+		return rc;
+
 	if (is_first_task) {
 		/* Only do once in this plugin */
 		if (cgroup_g_step_create(CG_CPUACCT, jobacct_id->step)

src/plugins/jobacct_gather/common/common_jag.c
@@ -1089,70 +1089,87 @@ extern void jag_common_poll_data(list_t *task_list, uint64_t cont_id,
 		double cpu_calc;
 		double last_total_cputime;
 		jag_prec_t *permanent_anc;
-		if (!(prec = list_find_first(prec_list, _find_prec,
-					     &jobacct->pid)))
-			continue;
-		/*
-		 * We can't use the prec from the list as we need to keep it in
-		 * the original state without offspring since we reuse this list
-		 * keeping around precs after they end.
-		 */
-		memcpy(&tmp_prec, prec, sizeof(*prec));
-		permanent_anc = prec;
-		prec = &tmp_prec;
-
-		if (acct_gather_filesystem_g_get_data(prec->tres_data) < 0) {
-			log_flag(JAG, "problem retrieving filesystem data");
-		}
-
-		if (acct_gather_interconnect_g_get_data(prec->tres_data) < 0) {
-			log_flag(JAG, "problem retrieving interconnect data");
-		}
-		/* find all my descendents */
-		if (callbacks->get_offspring_data)
-			(*(callbacks->get_offspring_data))
-				(prec_list, prec, prec->pid, permanent_anc);
-
-		/*
-		 * Only jobacct_gather/cgroup uses prec_extra, and we want to
-		 * make sure we call it once per task, so call it here as we
-		 * iterate through the tasks instead of in get_precs.
-		 */
-		if (callbacks->prec_extra) {
-			if (last_taskid == jobacct->id.taskid) {
-				log_flag(JAG, "skipping prec_extra() call against nodeid:%u taskid:%u",
-					 jobacct->id.nodeid,
-					 jobacct->id.taskid);
+		if (jobacct->pid) {
+			if (!(prec = list_find_first(prec_list, _find_prec,
+						     &jobacct->pid)))
 				continue;
-			} else {
-				log_flag(JAG, "calling prec_extra() call against nodeid:%u taskid:%u",
-					 jobacct->id.nodeid,
-					 jobacct->id.taskid);
+			/*
+			 * We can't use the prec from the list as we need to
+			 * keep it in the original state without offspring since
+			 * we reuse this list keeping around precs after they
+			 * end.
+			 */
+			memcpy(&tmp_prec, prec, sizeof(*prec));
+			permanent_anc = prec;
+			prec = &tmp_prec;
+
+			if (acct_gather_filesystem_g_get_data(prec->tres_data) <
+			    0) {
+				log_flag(JAG, "problem retrieving filesystem data");
 			}
 
-			last_taskid = jobacct->id.taskid;
-			(*(callbacks->prec_extra))(prec, jobacct->id.taskid);
+			if (acct_gather_interconnect_g_get_data(
+				    prec->tres_data) < 0) {
+				log_flag(JAG, "problem retrieving interconnect data");
+			}
+			/* find all my descendents */
+			if (callbacks->get_offspring_data)
+				(*(callbacks->get_offspring_data))(
+					prec_list, prec, prec->pid,
+					permanent_anc);
+
+			/*
+			 * Only jobacct_gather/cgroup uses prec_extra, and we
+			 * want to make sure we call it once per task, so call
+			 * it here as we iterate through the tasks instead of
+			 * in get_precs.
+			 */
+			if (callbacks->prec_extra) {
+				if (last_taskid == jobacct->id.taskid) {
+					log_flag(JAG, "skipping prec_extra() call against nodeid:%u taskid:%u",
+						 jobacct->id.nodeid,
+						 jobacct->id.taskid);
+					continue;
+				} else {
+					log_flag(JAG, "calling prec_extra() call against nodeid:%u taskid:%u",
+						 jobacct->id.nodeid,
+						 jobacct->id.taskid);
+				}
+
+				last_taskid = jobacct->id.taskid;
+				(*(callbacks->prec_extra))(prec,
+							   jobacct->id.taskid);
+			}
+
+			log_flag(JAG, "pid:%u ppid:%u %s:%" PRIu64 " B",
+				 prec->pid, prec->ppid,
+				 (xstrcasestr(slurm_conf.job_acct_gather_params,
+					      "UsePss") ?  "pss" : "rss"),
+				 prec->tres_data[TRES_ARRAY_MEM].size_read);
+
+			last_total_cputime =
+				(double) jobacct->
+				tres_usage_in_tot[TRES_ARRAY_CPU];
+
+			cpu_calc =
+				(prec->ssec + prec->usec) / (double) conv_units;
+
+			/*
+			 * Since we are not storing things as a double anymor
+			 * make it bigger so we don't loose precision.
+			 */
+			cpu_calc *= CPU_TIME_ADJ;
+
+			prec->tres_data[TRES_ARRAY_CPU].size_read =
+				(uint64_t) cpu_calc;
+		} else {
+			prec = xmalloc(sizeof(jag_prec_t));
+			prec->tres_count = jobacct->tres_count;
+			prec->tres_data = xcalloc(prec->tres_count,
+						  sizeof(acct_gather_data_t));
+			_init_tres(prec, NULL);
+			cpu_calc = last_total_cputime = 0;
 		}
-
-		log_flag(JAG, "pid:%u ppid:%u %s:%" PRIu64 " B",
-			 prec->pid, prec->ppid,
-			 (xstrcasestr(slurm_conf.job_acct_gather_params,
-				      "UsePss") ?  "pss" : "rss"),
-			 prec->tres_data[TRES_ARRAY_MEM].size_read);
-
-
-		last_total_cputime =
-			(double)jobacct->tres_usage_in_tot[TRES_ARRAY_CPU];
-
-		cpu_calc = (prec->ssec + prec->usec) / (double) conv_units;
-
-		/*
-		 * Since we are not storing things as a double anymore make it
-		 * bigger so we don't loose precision.
-		 */
-		cpu_calc *= CPU_TIME_ADJ;
-
-		prec->tres_data[TRES_ARRAY_CPU].size_read = (uint64_t)cpu_calc;
 
 		/* get energy consumption
 		 * only once is enough since we
@@ -1294,6 +1311,8 @@ extern void jag_common_poll_data(list_t *task_list, uint64_t cont_id,
 
 			jobacct->last_time = jobacct->cur_time;
 		}
+		if (!jobacct->pid)
+			xfree(prec);
 	}
 	list_iterator_destroy(itr);
 

src/slurmd/slurmstepd/mgr.c
@@ -266,24 +266,26 @@ static void _local_jobacctinfo_aggregate(
 {
 	int gpumem_pos = -1, gpuutil_pos = -1;
 
-	gpu_get_tres_pos(&gpumem_pos, &gpuutil_pos);
+	if (from->pid) {
+		gpu_get_tres_pos(&gpumem_pos, &gpuutil_pos);
 
-	/*
-	 * Here to make any sense for some variables we need to move the
-	 * Max to the total (i.e. Mem, VMem, gpumem, gpuutil) since the total
-	 * might be incorrect data, this way the total/ave will be of the Max
-	 * values.
-	 */
-	from->tres_usage_in_tot[TRES_ARRAY_MEM] =
-		from->tres_usage_in_max[TRES_ARRAY_MEM];
-	from->tres_usage_in_tot[TRES_ARRAY_VMEM] =
-		from->tres_usage_in_max[TRES_ARRAY_VMEM];
-	if (gpumem_pos != -1)
-		from->tres_usage_in_tot[gpumem_pos] =
-			from->tres_usage_in_max[gpumem_pos];
-	if (gpuutil_pos != -1)
-		from->tres_usage_in_tot[gpuutil_pos] =
-			from->tres_usage_in_max[gpuutil_pos];
+		/*
+		 * Here to make any sense for some variables we need to move the
+		 * Max to the total (i.e. Mem, VMem, gpumem, gpuutil) since the
+		 * total might be incorrect data, this way the total/ave will be
+		 * of the Max values.
+		 */
+		from->tres_usage_in_tot[TRES_ARRAY_MEM] =
+			from->tres_usage_in_max[TRES_ARRAY_MEM];
+		from->tres_usage_in_tot[TRES_ARRAY_VMEM] =
+			from->tres_usage_in_max[TRES_ARRAY_VMEM];
+		if (gpumem_pos != -1)
+			from->tres_usage_in_tot[gpumem_pos] =
+				from->tres_usage_in_max[gpumem_pos];
+		if (gpuutil_pos != -1)
+			from->tres_usage_in_tot[gpuutil_pos] =
+				from->tres_usage_in_max[gpuutil_pos];
+	}
 
 	/*
 	 * Here ave_watts stores the ave of the watts collected so store that
@@ -1345,6 +1347,7 @@ static int _spawn_job_container(stepd_step_rec_t *step)
 {
 	jobacctinfo_t *jobacct = NULL;
 	struct rusage rusage;
+	jobacct_id_t jobacct_id;
 	int rc = SLURM_SUCCESS;
 	uint32_t jobid = step->step_id.job_id;
 
@@ -1440,7 +1443,11 @@ x11_fail:
 		      step->x11_xauthority);
 	}
 
+	jobacct_id.nodeid = step->nodeid;
+	jobacct_id.taskid = step->nodeid; /* Treat node ID as global task ID */
+	jobacct_id.step = step;
 	jobacct_gather_set_proctrack_container_id(step->cont_id);
+	jobacct_gather_add_task(0, &jobacct_id, 1);
 
 	set_job_state(step, SLURMSTEPD_STEP_RUNNING);
 	if (!slurm_conf.job_acct_gather_freq)
@@ -1490,9 +1497,9 @@ x11_fail:
 
 	/* remove all tracked tasks */
 	while ((jobacct = jobacct_gather_remove_task(0))) {
-		jobacctinfo_setinfo(jobacct,
-				    JOBACCT_DATA_RUSAGE, &rusage,
-				    SLURM_PROTOCOL_VERSION);
+		if (jobacct->pid)
+			jobacctinfo_setinfo(jobacct, JOBACCT_DATA_RUSAGE,
+					    &rusage, SLURM_PROTOCOL_VERSION);
 		step->jobacct->energy.consumed_energy = 0;
 		_local_jobacctinfo_aggregate(step->jobacct, jobacct);
 		jobacctinfo_destroy(jobacct);

commit a8f6d010aa93e291db81514427b3bf48ce859b43

Fix missing step cputime stats
Usercpu and systemcpu stats were missing for job steps started with
srun. This was caused by a regression introduced in af2c0bd.

Ticket: 24287
Changelog: Fix regression from af2c0bd which caused usercpu and
 systemcpu to be missing for job steps.

src/interfaces/jobacct_gather.c
@@ -917,6 +917,8 @@ extern jobacctinfo_t *jobacctinfo_create(jobacct_id_t *jobacct_id)
 	jobacct->sys_cpu_usec = 0;
 	jobacct->user_cpu_sec = 0;
 	jobacct->user_cpu_usec = 0;
+	/* Default PID -1 will be used to aggregate metrics from other PIDs */
+	jobacct->pid = -1;
 
 	_jobacctinfo_create_tres_usage(jobacct_id, jobacct);
 	return jobacct;
@@ -1144,9 +1146,10 @@ extern int jobacctinfo_unpack(jobacctinfo_t **jobacct, uint16_t rpc_version,
 
 	xassert(jobacct);
 
-	if (alloc)
+	if (alloc) {
 		*jobacct = xmalloc(sizeof(struct jobacctinfo));
-	else {
+		(*jobacct)->pid = -1;
+	} else {
 		xassert(*jobacct);
 		_free_tres_usage(*jobacct);
 	}