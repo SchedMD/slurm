SchedMD/slurm

commit 1c065cb9083b4df9fca062d946359f853c2b7f64

Fix x11 cleanup race condition
The thread used to clean the x11 files was dropping privileges. If in the
meantime a cgroup or other tasks from step cleanup were taking place and
needed privileges, they would fail with EPERM. By adding locking with
auth_setuid_lock we guarantee that while doing these cleanups the
privileges will not be dropped.

Other parts of the code like the removal of the domain socket were also
affected by this, so the locking has been extended also to these places.

Changelog: Fix race condition on x11 shutdown which caused other privileged
 cleanup operations to fail and leave stray cgroup or spool directories and
 errors in the logs.
Ticket: 22736

src/slurmd/slurmstepd/mgr.c
@@ -1536,6 +1536,9 @@ x11_fail:
 		if (task_g_post_term(step, step->task[i]) == ENOMEM)
 			step->oom_error = true;
 
+	/* Lock to not collide with the _x11_signal_handler thread. */
+	auth_setuid_lock();
+
 	/*
 	 * This function below calls jobacct_gather_fini(). For the case of
 	 * jobacct_gather/cgroup, it ends up doing the cgroup hierarchy cleanup
@@ -1543,8 +1546,9 @@ x11_fail:
 	 * children processes from the step are gone.
 	 */
 	acct_gather_profile_fini();
-
 	task_g_post_step(step);
+
+	auth_setuid_unlock();
 
 fail1:
 	conmgr_add_work_fifo(_x11_signal_handler, step);

src/slurmd/slurmstepd/slurmstepd.c
@@ -492,10 +492,18 @@ extern void stepd_cleanup(slurm_msg_t *msg, stepd_step_rec_t *step,
 		goto done;
 	}
 
-	if (!only_mem) {
-		if (step->batch)
-			batch_finish(step, rc); /* sends batch complete message */
+	if (!only_mem && step->batch)
+		batch_finish(step, rc); /* sends batch complete message */
 
+	/*
+	 * Call auth_setuid_lock after sending the batch_finish message as in
+	 * there the auth_g_create function is called, and that function uses
+	 * the lock. The lock is needed to ensure that the privileges are not
+	 * dropped from a different thread, like X11 shutdown thread.
+	 */
+	auth_setuid_lock();
+
+	if (!only_mem) {
 		/* signal the message thread to shutdown, and wait for it */
 		if (step->msg_handle)
 			eio_signal_shutdown(step->msg_handle);
@@ -519,6 +527,7 @@ extern void stepd_cleanup(slurm_msg_t *msg, stepd_step_rec_t *step,
 			      step->step_id.job_id);
 	}
 
+	auth_setuid_unlock();
 	run_command_shutdown();
 
 	/*

commit a9a7397d2e6725aea9990ed1a766272620f048d7

Avoid potential deadlock in auth_setuid_lock() calls
Fix a potential deadlock introduced in 1c065cb that may happen if the
_x11_signal_handler() forks in between the auth_setuid_lock/unlock
calls inside stepd_cleanup().

The child will try to drop_privileges, attempting to acquire the lock
twice because externally_locked is set to true. Fix introduces a new
pair auth_context_lock/unlock() which should be used when lock does
not need to persist across fork().

Ticket: 24375
Changelog: Fix potential deadlock in _x11_signal_handler() during
 stepd_cleanup().

src/interfaces/auth.c
@@ -312,6 +312,10 @@ extern int auth_index(void *cred)
 	return 0;
 }
 
+/*
+ * For drop_privileges/reclaim_privileges pairs.
+ * Sets externally_locked to maintain lock state across fork().
+ */
 extern void auth_setuid_lock(void)
 {
 	slurm_rwlock_wrlock(&context_lock);
@@ -326,6 +330,21 @@ extern void auth_setuid_lock(void)
 extern void auth_setuid_unlock(void)
 {
 	externally_locked = false;
+	slurm_rwlock_unlock(&context_lock);
+}
+
+/*
+ * For protecting against concurrent privilege changes without fork
+ * implications. Does NOT set externally_locked, so lock state will not
+ * persist across fork().
+ */
+extern void auth_context_lock(void)
+{
+	slurm_rwlock_wrlock(&context_lock);
+}
+
+extern void auth_context_unlock(void)
+{
 	slurm_rwlock_unlock(&context_lock);
 }
 

src/interfaces/auth.h
@@ -94,6 +94,9 @@ extern bool auth_is_plugin_type_inited(auth_plugin_type_t plugin_id);
  */
 extern void auth_setuid_lock(void);
 extern void auth_setuid_unlock(void);
+extern void auth_context_lock(void);
+extern void auth_context_unlock(void);
+
 /*
  * Static bindings for the global authentication context.
  */

src/slurmd/slurmstepd/mgr.c
@@ -1507,7 +1507,7 @@ x11_fail:
 			step->oom_error = true;
 
 	/* Lock to not collide with the _x11_signal_handler thread. */
-	auth_setuid_lock();
+	auth_context_lock();
 
 	/*
 	 * This function below calls jobacct_gather_fini(). For the case of
@@ -1518,7 +1518,7 @@ x11_fail:
 	acct_gather_profile_fini();
 	task_g_post_step(step);
 
-	auth_setuid_unlock();
+	auth_context_unlock();
 
 fail1:
 	conmgr_add_work_fifo(_x11_signal_handler, NULL);

src/slurmd/slurmstepd/slurmstepd.c
@@ -569,7 +569,7 @@ extern void stepd_cleanup(slurm_msg_t *msg, slurm_addr_t *cli, int rc,
 	 * the lock. The lock is needed to ensure that the privileges are not
 	 * dropped from a different thread, like X11 shutdown thread.
 	 */
-	auth_setuid_lock();
+	auth_context_lock();
 
 	if (!only_mem) {
 		/* signal the message thread to shutdown, and wait for it */
@@ -593,7 +593,7 @@ extern void stepd_cleanup(slurm_msg_t *msg, slurm_addr_t *cli, int rc,
 			      &step->step_id);
 	}
 
-	auth_setuid_unlock();
+	auth_context_unlock();
 	run_command_shutdown();
 
 	/*