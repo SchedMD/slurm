SchedMD/slurm

commit 4f216124762762662b35229f6534d2dbd3b3564b

sprio - Swap from slurm_xlate_job_id() to unfmt_job_id_string()
Save an RPC call: slurm_load_jobs() within slurm_xlate_job_id(). We only
need the base job id from the user for sprio to compare against the
priority_factors_response_msg_t returned from slurm_load_job_prio().

Bug 18559

src/sprio/opts.c
@@ -456,7 +456,6 @@ static list_t *_build_job_list(char *str)
 {
 	list_t *my_list;
 	char *job = NULL, *tmp_char = NULL, *my_job_list = NULL;
-	int i;
 	uint32_t *job_id = NULL;
 
 	if ( str == NULL)
@@ -466,13 +465,14 @@ static list_t *_build_job_list(char *str)
 	my_job_list = xstrdup(str);
 	job = strtok_r(my_job_list, ",", &tmp_char);
 	while (job) {
-		i = slurm_xlate_job_id(job);
-		if (i <= 0) {
+		slurm_selected_step_t sel_step;
+		if (unfmt_job_id_string(str, &sel_step)) {
 			error("Invalid job id: %s", job);
 			exit(1);
-		}
+		} else if (sel_step.het_job_offset != NO_VAL)
+			sel_step.step_id.job_id += sel_step.het_job_offset;
 		job_id = xmalloc(sizeof(uint32_t));
-		*job_id = (uint32_t) i;
+		*job_id = sel_step.step_id.job_id;
 		list_append(my_list, job_id);
 		job = strtok_r(NULL, ",", &tmp_char);
 	}

commit 80beef6a2233a343214482865edbc577e543cd4d

Fix sprio regression not handling comma separated list of jobids
sprio was unable to parse a comma-separated list of jobs despite
documentation stating that this was possible. The entire job list was
passed to the unfmt_job_id_string() function, but this function is
unable to parse job lists. Switching to only pass the single job to
unfmt_job_id_string() fixes this issue.

Regression introduced in 4f21612476.

Changelog: Fix sprio regression not handling comma separated list of
 jobids.
Ticket: 24625

src/sprio/opts.c
@@ -466,7 +466,7 @@ static list_t *_build_job_list(char *str)
 	job = strtok_r(my_job_list, ",", &tmp_char);
 	while (job) {
 		slurm_selected_step_t sel_step;
-		if (unfmt_job_id_string(str, &sel_step, NO_VAL)) {
+		if (unfmt_job_id_string(job, &sel_step, NO_VAL)) {
 			error("Invalid job id: %s", job);
 			exit(1);
 		} else if (sel_step.het_job_offset != NO_VAL)