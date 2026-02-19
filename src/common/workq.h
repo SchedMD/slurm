f57fe1a1baab9db328e7dc313432e55a9ec66221
Nathan Rini <nrini@nvidia.com>
2026-02-19 15:22:39 -0700

    workq - Add implementation
    Issue: 50528

--- src/common/workq.h
+++ src/common/workq.h
@@ -36,4 +36,83 @@
 #ifndef _COMMON_WORKQ_H
 #define _COMMON_WORKQ_H
 
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef struct workq_s workq_t;
+typedef struct workq_allocator_s workq_allocator_t;
+
+typedef enum {
+	WORKQ_PRIORITY_INVALID = 0,
+	WORKQ_PRIORITY_IDLE,
+	WORKQ_PRIORITY_NORMAL,
+	WORKQ_PRIORITY_INVALID_MAX
+} workq_priority_t;
+
+/*
+ * Prototype for all work callbacks
+ * IN shutdown - True if workq is shutting down
+ * IN arg - Arbitrary pointer handed to workq_enqueue()
+ */
+typedef void (*work_func_t)(const bool shutdown, void *arg);
+
+/*
+ * Create new work to run in workq
+ * IN workq - Pointer to work queue to enqueue the work
+ * IN alloc - Pointer to allocator for the work request
+ * IN priority - which priority queue for work
+ * IN func - function to call
+ * IN func_name - function name for logging
+ * IN caller - __func__ from caller
+ * IN arg - Arbitrary pointer to hand to func()
+ */
+extern void workq_enqueue(workq_t *workq, workq_allocator_t *alloc,
+			  const workq_priority_t priority, work_func_t func,
+			  const char *func_name, const char *caller, void *arg);
+
+/*
+ * Run pending work in workq
+ * IN workq - Run all work from this workq
+ * IN blocking
+ *	True: Wait for new work until workq_fini()
+ *	False: Run all pending work and return
+ */
+extern void workq_run(workq_t *workq, const bool blocking);
+
+/* Get number of bytes required to store a workq instance */
+extern const size_t workq_bytes(void);
+
+/*
+ * Initialize a new workq
+ * IN workq - Pointer with workq_bytes() bytes of allocated memory
+ * IN name - NULL or Name of workq probe
+ * RET pointer to workq
+ */
+extern workq_t *workq_init(workq_t *workq, const char *name);
+
+/*
+ * Shutdown and release workq
+ * NOTE: Use FREE_NULL_WORKQ() instead of calling directly
+ * IN workq - Pointer to release workq state
+ */
+extern void workq_fini(workq_t *workq);
+
+#define FREE_NULL_WORKQ(workq) \
+	do { \
+		if (workq) { \
+			workq_fini(workq); \
+			(workq) = NULL; \
+		} \
+	} while (0)
+
+/*
+ * Initialize allocator for work requests
+ * NOTE: Allocator will be owned by workq
+ * IN count - Number of work requests to preallocate
+ * IN name - Name to use for logging (must be valid life of allocator)
+ * RET pointer to allocator
+ */
+extern workq_allocator_t *workq_allocator(workq_t *workq, const int count,
+					  const char *name);
+
 #endif
