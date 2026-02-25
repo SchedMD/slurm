d68d5f2027b850a83efd8cb0d02b176ffa541025
Dominik Bartkiewicz <dbartkiewicz@nvidia.com>
2026-02-25 21:43:12 +0000

    Implement auth_p_get_time() for auth/munge
    Issue: 50781

--- src/plugins/auth/munge/auth_munge.c
+++ src/plugins/auth/munge/auth_munge.c
@@ -409,7 +409,10 @@ extern int auth_p_get_data(auth_credential_t *cred, char **data, uint32_t *len)
 
 extern time_t auth_p_get_time(auth_credential_t *cred)
 {
-	return 0;
+	if (!cred)
+		return 0;
+
+	return cred->ctime;
 }
 
 extern void *auth_p_get_identity(auth_credential_t *cred)
