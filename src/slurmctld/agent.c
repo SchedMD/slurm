2306346896bb71d07e6f94cc95e71b325a551c94
Dominik Bartkiewicz <dbartkiewicz@nvidia.com>
2026-02-25 21:43:13 +0000

    Use SLUID in _thread_per_group_rpc()
    Issue: 50781

--- src/slurmctld/agent.c
+++ src/slurmctld/agent.c
@@ -980,7 +980,6 @@ static void *_thread_per_group_rpc(void *args)
 	slurmctld_lock_t node_write_lock = {
 		.node = WRITE_LOCK,
 	};
-	uint32_t job_id;
 
 	xassert(args != NULL);
 	is_kill_msg = (	(msg_type == REQUEST_KILL_TIMELIMIT)	||
@@ -1228,12 +1227,11 @@ static void *_thread_per_group_rpc(void *args)
 
 			if ((msg_ptr->signal == SIGCONT) ||
 			    (msg_ptr->signal == SIGSTOP)) {
-				job_id = msg_ptr->step_id.job_id;
 				lock_slurmctld(job_write_lock);
-				job_ptr = find_job_record(job_id);
+				job_ptr = find_job(&msg_ptr->step_id);
 				if (job_ptr == NULL) {
-					info("%s: invalid JobId=%u",
-					     __func__, job_id);
+					info("%s: invalid %pI",
+					     __func__, &msg_ptr->step_id);
 				} else if (rc == SLURM_SUCCESS) {
 					if (msg_ptr->signal == SIGSTOP) {
 						job_state_set_flag(job_ptr,
@@ -1329,9 +1327,8 @@ cleanup:
 			task_ptr->msg_args_ptr;
 		if ((msg_ptr->signal == SIGCONT) ||
 		    (msg_ptr->signal == SIGSTOP)) {
-			job_id = msg_ptr->step_id.job_id;
 			lock_slurmctld(job_write_lock);
-			job_ptr = find_job_record(job_id);
+			job_ptr = find_job(&msg_ptr->step_id);
 			if (job_ptr)
 				job_state_unset_flag(job_ptr, JOB_SIGNALING);
 			unlock_slurmctld(job_write_lock);
