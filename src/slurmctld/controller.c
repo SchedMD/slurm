7e5bb7c370f23f760a36d409e329f7808c1d3a97
Nathan Rini <nrini@nvidia.com>
2026-03-10 10:32:35 -0600

    slurmctld - Call forward_init() and forward_fini()
    Changelog: slurmctld - Avoid race condition during shutdown that could
     cause a crash due to tree forwarding.
    Cherry-picked: fe90adde36
    Ticket: 24122

--- src/slurmctld/controller.c
+++ src/slurmctld/controller.c
@@ -64,6 +64,7 @@
 #include "src/common/daemonize.h"
 #include "src/common/extra_constraints.h"
 #include "src/common/fd.h"
+#include "src/common/forward.h"
 #include "src/common/group_cache.h"
 #include "src/common/hostlist.h"
 #include "src/common/http_switch.h"
@@ -754,6 +755,8 @@ int main(int argc, char **argv)
 	if (serializer_g_init() != SLURM_SUCCESS)
 		fatal("Failed to initialize serialization plugins.");
 
+	forward_init();
+
 	if (original && !under_systemd) {
 		/*
 		 * Need to create pidfile here in case we setuid() below
@@ -1174,6 +1177,7 @@ int main(int argc, char **argv)
 	}
 
 	conmgr_request_shutdown();
+	forward_fini();
 	http_fini();
 	http_switch_fini();
 	conmgr_fini();
