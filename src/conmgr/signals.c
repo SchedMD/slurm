939772d49e907c7938bc53189f348002ef421a09
Nathan Rini <nrini@nvidia.com>
2026-03-13 16:08:38 -0600

    conmgr - Disable timeouts in signal_mgr_start()
    Issue: 50453

--- src/conmgr/signals.c
+++ src/conmgr/signals.c
@@ -345,9 +345,9 @@ extern void signal_mgr_start(conmgr_callback_args_t conmgr_args, void *arg)
 
 	slurm_rwlock_unlock(&lock);
 
-	if (add_connection(CON_TYPE_RAW, NULL, NULL, fd[0], -1, &events,
-			   CON_FLAG_NONE, NULL, 0, false, NULL, NULL, NULL,
-			   NULL)) {
+	if (add_connection(CON_TYPE_RAW, &conmgr_timeouts_disabled, NULL, fd[0],
+			   -1, &events, CON_FLAG_NONE, NULL, 0, false, NULL,
+			   NULL, NULL, NULL)) {
 		fatal_abort("%s: [fd:%d] unable to a register new connection",
 			    __func__, fd[0]);
 	}
