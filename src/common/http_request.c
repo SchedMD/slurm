f68ac9e2ccceb74192799967bc076bcf9b580ebe
Nathan Rini <nrini@nvidia.com>
2026-03-26 17:47:44 -0600

    http_request - Add implementation
    Ticket: 24164

--- src/common/http_request.c
+++ src/common/http_request.c
@@ -34,3 +34,187 @@
 \*****************************************************************************/
 
 #include "src/common/http_request.h"
+#include "src/common/http.h"
+#include "src/common/http_con.h"
+#include "src/common/http_mime.h"
+#include "src/common/http_router.h"
+#include "src/common/openapi.h"
+#include "src/common/read_config.h"
+#include "src/common/serdes.h"
+#include "src/common/uid.h"
+#include "src/common/xassert.h"
+#include "src/common/xmalloc.h"
+#include "src/common/xstring.h"
+
+#include "src/interfaces/data_parser.h"
+#include "src/interfaces/http_auth.h"
+
+#define BOUND_REQUEST_MAGIC 0xab008800
+
+typedef struct {
+	int magic; /* BOUND_REQUEST_MAGIC  */
+	data_parser_t *parser;
+	char *path;
+	http_request_on_request_t on_request;
+	http_request_on_error_t on_error;
+	data_parser_type_t reply_type;
+	void *arg;
+} bound_request_t;
+
+#define HTTP_REQUEST_EVENT_MAGIC 0xbf0f8830
+
+typedef struct http_request_event_s {
+	int magic; /* HTTP_REQUEST_EVENT_MAGIC */
+	bound_request_t *breq;
+	http_con_t *hcon;
+	const char *name;
+	const char *read_mime;
+	const char *write_mime;
+	const http_con_request_t *request;
+} http_request_event_t;
+
+static int _on_request(http_con_t *hcon, const char *name,
+		       const http_con_request_t *request, void *arg,
+		       void *path_arg)
+{
+	bound_request_t *breq = path_arg;
+	http_request_event_t event = {
+		.magic = HTTP_REQUEST_EVENT_MAGIC,
+		.breq = breq,
+		.hcon = hcon,
+		.name = name,
+		.request = request,
+	};
+	int rc = EINVAL;
+	uid_t uid = SLURM_AUTH_NOBODY;
+
+	xassert(breq->magic == BOUND_REQUEST_MAGIC);
+
+	if ((rc = http_auth_g_authenticate(HTTP_AUTH_PLUGIN_ANY, &uid, hcon,
+					   name, request))) {
+		error("%s: [%s] Rejecting HTTP authentication: %s",
+		      __func__, name, slurm_strerror(rc));
+		return breq->on_error(&event, hcon, name, request, rc);
+	}
+
+	if ((rc = http_resolve_mime_types(name, request, &event.read_mime,
+					  &event.write_mime))) {
+		error("%s: [%s] Rejecting HTTP mime headers: %s",
+		      __func__, name, slurm_strerror(rc));
+		return breq->on_error(&event, hcon, name, request, rc);
+	}
+
+	log_flag(NET, "%s: [%s] Accepted HTTP request: method=%s path=%s uid[%d]:%s read_mime:%s write_mime:%s",
+		 __func__, name, get_http_method_string(request->method),
+		 request->url.path, uid, uid_to_string_or_null(uid),
+		 event.read_mime, event.write_mime);
+	return breq->on_request(&event, hcon, name, uid, request->method,
+				request, breq->arg);
+}
+
+static void _breq_free(http_router_on_request_event_t on_request,
+		       void *path_arg)
+{
+	bound_request_t *breq = path_arg;
+
+	xassert(breq->magic == BOUND_REQUEST_MAGIC);
+	xassert(on_request == _on_request);
+
+	breq->magic = ~BOUND_REQUEST_MAGIC;
+	xfree(breq->path);
+	xfree(breq);
+}
+
+static void _bind(data_parser_t *parser, http_request_method_t method,
+		  const char *path, http_request_on_request_t on_request,
+		  http_request_on_error_t on_error,
+		  data_parser_type_t reply_type, void *arg)
+{
+	bound_request_t *breq = xmalloc(sizeof(*breq));
+	*breq = (bound_request_t) {
+		.magic = BOUND_REQUEST_MAGIC,
+		.on_request = on_request,
+		.on_error = on_error,
+		.reply_type = reply_type,
+		.arg = arg,
+		.path = xstrdup(path),
+		.parser = parser,
+	};
+
+	if (parser)
+		xstrsubstitute(breq->path, OPENAPI_DATA_PARSER_PARAM,
+			       data_parser_get_plugin_version(parser));
+
+	http_router_bind(method, breq->path, _on_request, _breq_free, breq);
+}
+
+extern void http_request_bind(data_parser_t **parsers,
+			      http_request_method_t method, const char *path,
+			      http_request_on_request_t on_request,
+			      http_request_on_error_t on_error,
+			      data_parser_type_t reply_type, void *arg)
+{
+	if (reply_type == DATA_PARSER_TYPE_INVALID) {
+		_bind(NULL, method, path, on_request, on_error, reply_type,
+		      arg);
+		return;
+	}
+
+	xassert(reply_type > DATA_PARSER_TYPE_INVALID);
+	xassert(reply_type < DATA_PARSER_TYPE_MAX);
+
+	for (int i = 0; parsers[i]; i++)
+		if (data_parser_g_resolve_type_string(parsers[i], reply_type))
+			_bind(parsers[i], method, path, on_request, on_error,
+			      reply_type, arg);
+}
+
+extern int http_request_reply(http_request_event_t *event, int rc,
+			      list_t *headers, bool close_header, void *reply,
+			      size_t reply_bytes)
+{
+	bound_request_t *breq = event->breq;
+	data_parser_t *parser = breq->parser;
+	http_con_t *hcon = event->hcon;
+	const char *name = event->name;
+	const http_con_request_t *request = event->request;
+	const char *write_mime = event->write_mime;
+	http_status_code_t status = http_status_from_error(rc);
+
+	xassert(event->magic == HTTP_REQUEST_EVENT_MAGIC);
+	xassert(event->breq->magic == BOUND_REQUEST_MAGIC);
+
+	if (reply && parser) {
+		buf_t *buffer = NULL;
+
+		xassert(breq->reply_type > DATA_PARSER_TYPE_INVALID);
+		xassert(breq->reply_type < DATA_PARSER_TYPE_MAX);
+
+		if (!(buffer = try_init_buf(BUF_SIZE)))
+			return breq->on_error(event, hcon, name, request,
+					      ENOMEM);
+
+		if ((rc = serdes_dump_buf(parser, breq->reply_type, reply,
+					  reply_bytes, buffer, write_mime,
+					  SER_FLAGS_NONE)))
+			rc = breq->on_error(event, hcon, name, request, rc);
+		else
+			rc = http_con_send_response(hcon, status, headers,
+						    close_header, buffer,
+						    write_mime);
+
+		FREE_NULL_BUFFER(buffer);
+	} else if (reply && !parser) {
+		const buf_t body = SHADOW_BUF_INITIALIZER(reply, reply_bytes);
+
+		xassert(breq->reply_type == DATA_PARSER_TYPE_INVALID);
+
+		rc = http_con_send_response(hcon, status, headers, close_header,
+					    &body, MIME_TYPE_TEXT);
+	} else {
+		rc = http_con_send_response(hcon, status, headers, close_header,
+					    NULL, MIME_TYPE_TEXT);
+	}
+
+	return rc;
+}
--- src/common/http_request.h
+++ src/common/http_request.h
@@ -36,4 +36,83 @@
 #ifndef SLURM_HTTP_REQUEST_H
 #define SLURM_HTTP_REQUEST_H
 
+#include "src/common/http.h"
+#include "src/common/http_con.h"
+
+#include "src/interfaces/data_parser.h"
+
+/* Opaque event state passed between _on_request() and http_request_reply() */
+typedef struct http_request_event_s http_request_event_t;
+
+/*
+ * Called after successful HTTP authentication and MIME negotiation.
+ * IN event - opaque request event state (pass to http_request_reply())
+ * IN hcon - pointer to HTTP connection
+ * IN name - connection name for logging
+ * IN uid - authenticated user id
+ * IN method - HTTP request method
+ * IN request - pointer to parsed HTTP request
+ * IN arg - arbitrary pointer handed to http_request_bind()
+ * RET SLURM_SUCCESS or error
+ */
+typedef int (*http_request_on_request_t)(http_request_event_t *event,
+					 http_con_t *hcon, const char *name,
+					 const uid_t uid,
+					 http_request_method_t method,
+					 const http_con_request_t *request,
+					 void *arg);
+/*
+ * Called when HTTP request processing encounters an error (e.g.
+ * authentication failure or unsupported MIME type).
+ * IN event - opaque request event state
+ * IN hcon - pointer to HTTP connection
+ * IN name - connection name for logging
+ * IN request - pointer to parsed HTTP request
+ * IN err - error code describing the failure
+ * RET SLURM_SUCCESS or error
+ */
+typedef int (*http_request_on_error_t)(http_request_event_t *event,
+				       http_con_t *hcon, const char *name,
+				       const http_con_request_t *request,
+				       int err);
+
+/*
+ * Bind an HTTP path to request and error callbacks. Handles authentication
+ * and MIME negotiation before invoking on_request. When reply_type is not
+ * DATA_PARSER_TYPE_INVALID, the path is registered once per parser version
+ * that supports the given type (substituting OPENAPI_DATA_PARSER_PARAM in
+ * path with the parser version string).
+ * IN parsers - NULL-terminated array of data parsers
+ * IN method - HTTP method to bind
+ * IN path - URL path to bind (may contain OPENAPI_DATA_PARSER_PARAM)
+ * IN on_request - callback for authenticated requests
+ * IN on_error - callback for request processing errors
+ * IN reply_type - data parser type for serializing replies, or
+ *	DATA_PARSER_TYPE_INVALID for untyped replies
+ * IN arg - arbitrary pointer passed through to on_request callback
+ */
+extern void http_request_bind(data_parser_t **parsers,
+			      http_request_method_t method, const char *path,
+			      http_request_on_request_t on_request,
+			      http_request_on_error_t on_error,
+			      data_parser_type_t reply_type, void *arg);
+
+/*
+ * Send an HTTP response for a bound request. When a parser is associated with
+ * the binding and reply is non-NULL, the reply is serialized using the bound
+ * data parser type and the negotiated write MIME type. When no parser is
+ * associated, the raw reply bytes are sent as text. When reply is NULL, an
+ * empty response with the appropriate status code is sent.
+ * IN event - opaque request event state from on_request callback
+ * IN rc - result code (mapped to HTTP status via http_status_from_error())
+ * IN headers - optional list of additional HTTP headers (or NULL)
+ * IN close_header - if true, include Connection: close header
+ * IN reply - pointer to response data to serialize (or NULL)
+ * IN reply_bytes - size of reply data in bytes
+ * RET SLURM_SUCCESS or error
+ */
+extern int http_request_reply(http_request_event_t *event, int rc,
+			      list_t *headers, bool close_header, void *reply,
+			      size_t reply_bytes);
+
 #endif
