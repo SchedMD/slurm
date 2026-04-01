71ce1f5678193d9bf1ceebfb7ee3d55dca2a5457
Nathan Rini <nrini@nvidia.com>
2026-04-01 14:12:24 -0600

    Reject --gid=99 in arg_set_gid()
    Avoid submitting group as SLURM_AUTH_NOBODY.
    
    Changelog: sbatch,srun,salloc - Reject arg --gid=99.
    Ticket: 24813
    Cherry-picked: 35d6127cce

--- src/common/slurm_opt.c
+++ src/common/slurm_opt.c
@@ -1541,6 +1541,11 @@ static int arg_set_gid(slurm_opt_t *opt, const char *arg)
 		return SLURM_ERROR;
 	}
 
+	if (opt->gid == SLURM_AUTH_NOBODY) {
+		error("Rejecting --gid as nobody specification");
+		return SLURM_ERROR;
+	}
+
 	return SLURM_SUCCESS;
 }
 COMMON_INT_OPTION_GET(gid);
