6953188d3b9349ee111ee492ea19ce77b423009a
Nathan Rini <nrini@nvidia.com>
2026-04-20 16:21:55 -0600

    slurmdbd - Drain conmgr workers before acct_storage fini
    Move conmgr_fini() to run before acct_storage_g_fini() during
    shutdown. conmgr_fini() -> workers_shutdown() is the only call that
    guarantees all persist_conn service threads have drained, including
    invocations of rpc_mgr's _connection_fini_callback(), which calls
    acct_storage_g_close_connection() from worker context.
    
    Changelog: slurmdbd - Fix crash from race condition during shutdown when a
     persistent connection closes its database connection after the
     accounting_storage plugin has already unloaded.
    Ticket: 24562
    Cherry-picked: 0fa0f8321e

--- src/slurmdbd/slurmdbd.c
+++ src/slurmdbd/slurmdbd.c
@@ -358,6 +358,7 @@ int main(int argc, char **argv)
 	/* Daemon termination handled here */
 
 end_it:
+	conmgr_request_shutdown();
 
 	slurm_thread_join(commit_handler_thread);
 
@@ -381,6 +382,7 @@ end_it:
 		_restart_self(argc, argv);
 	}
 
+	conmgr_fini();
 	assoc_mgr_fini(0);
 	acct_storage_g_fini();
 	auth_g_fini();
@@ -392,7 +394,6 @@ end_it:
 	rpc_stats = NULL;
 	slurm_mutex_unlock(&rpc_mutex);
 
-	conmgr_fini();
 	log_fini();
 	return SLURM_SUCCESS;
 }
