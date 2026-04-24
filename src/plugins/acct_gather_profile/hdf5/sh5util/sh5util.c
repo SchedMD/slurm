SchedMD/slurm

commit b296c409b4c3c4b97f7ed0aed07bdf636b15a5b4

sh5util - Update sh5util to reject het/array component in arguments
The h5 files that acct_gather produces do not have het/array index appended
to them, so there is no simple way for sh5util to know which file
corresponds to a particular component of a het/array job.

Instead, the user should determine what the raw JobId of the het/array
component is, and use that as input to sh5util.

For example, with an array job of 2 components:

$ sacct -X -j 27 --format JobId,JobIdRaw
JobID        JobIDRaw
------------ ------------
27_1         28
27_2         27

$ sh5util -j 28 # works for 27_1
$ sh5util -j 27 # works for 27_2

Changelog: Alter sh5util -j to not allow array or het job ids.
Ticket: 17714

src/plugins/acct_gather_profile/hdf5/sh5util/sh5util.c
@@ -339,7 +339,6 @@ static int _set_options(const int argc, char **argv)
 	int option_index = 0;
 	int cc;
 	log_options_t logopt = LOG_OPTS_STDERR_ONLY;
-	char *next_str = NULL;
 	uid_t u;
 
 	static struct option long_options[] = {
@@ -402,11 +401,30 @@ static int _set_options(const int argc, char **argv)
 			params.input = xstrdup(optarg);
 			break;
 		case 'j':
-			params.job_id = strtol(optarg, &next_str, 10);
-			if (next_str[0] == '.')
-				params.step_id =
-					strtol(next_str + 1, NULL, 10);
+		{
+			slurm_selected_step_t selected_step;
+			int rc = unfmt_job_id_string(optarg, &selected_step, 0);
+
+			switch (rc) {
+			case SLURM_SUCCESS:
+				if (selected_step.array_task_id != NO_VAL) {
+					error("Job array IDs not supported, use regular job ID instead");
+					return -1;
+				}
+				if (selected_step.het_job_offset != NO_VAL) {
+					error("Het job IDs not supported, use regular job ID instead");
+					return -1;
+				}
+				break;
+			default:
+				error("Failed to parse job ID \"%s\": %s",
+				      optarg, slurm_strerror(rc));
+				return -1;
+			}
+			params.job_id = selected_step.step_id.job_id;
+			params.step_id = selected_step.step_id.step_id;
 			break;
+		}
 		case 'l':
 			xfree(params.level);
 			params.level = xstrdup(optarg);

doc/man/man1/sh5util.1
@@ -93,6 +93,22 @@ Name of data item in series (See note below).
 Format is <\fIjob\fR[.\fIstep\fR]>. Merge this job/step
 (or a comma\-separated list of job steps). This option is required.
 Not specifying a step will result in all steps found to be processed.
+
+Array task IDs and het component IDs are not supported. Use regular job IDs
+instead:
+.IP
+.nf
+$ sacct -X -j 5077 --format=JobId,JobIdRaw
+JobID        JobIDRaw
+------------ ------------
+5077_1       5078
+5077_2       5079
+5077_3       5077
+
+$ sh5util -S -j 5079 # for merging 5077_2 only
+sh5util: Merging node-step files into ./job_5079.h5
+.fi
+
 .IP
 
 .TP

commit e29d81eadceec317cb3feb2fd9950152f7a0d22d

Fix sh5util stepid parsing regression
Regression from b296c409b4. If unfmt_job_id_string() is parsing just a
jobid, then step_id_t.job_id will be filled with the jobid and
step_id.step_id will be left initialized to NO_VAL. However,
_set_options() initializes params.step_id to -1. params.step_id was
being set to NO_VAL when just asking for a jobid. So only set
params.step_id if step_id_t.step_id is not NO_VAL.

Ticket: 24888

src/plugins/acct_gather_profile/hdf5/sh5util/sh5util.c
@@ -422,7 +422,8 @@ static int _set_options(const int argc, char **argv)
 				return -1;
 			}
 			params.job_id = selected_step.step_id.job_id;
-			params.step_id = selected_step.step_id.step_id;
+			if (selected_step.step_id.step_id != NO_VAL)
+				params.step_id = selected_step.step_id.step_id;
 			break;
 		}
 		case 'l':