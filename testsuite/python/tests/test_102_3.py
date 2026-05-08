657e1eb4ebca04576dc4260031f33bfd849610c1
Megan Dahl <mdahl@nvidia.com>
2026-05-08 14:28:13 -0600

    Testsuite - test_102_3.py validate .meta.command field
    Ticket: 18108

--- testsuite/python/tests/test_102_3.py
+++ testsuite/python/tests/test_102_3.py
@@ -19,7 +19,12 @@ def setup():
     "entry", ["accounts", "associations", "clusters", "qos", "wckeys", "users", "tres"]
 )
 def test_json(entry):
-    """Verify sacctmgr --json has the correct format"""
+    """Verify sacctmgr --json has the correct format and meta data command"""
+
+    expected_command = ["sacctmgr", "--json", "show", entry]
 
     output = atf.run_command_output(f"sacctmgr --json show {entry}", fatal=True)
-    assert json.loads(output) is not None
+    json_data = json.loads(output)
+    assert json_data is not None
+    if atf.get_version("bin/sacctmgr") >= (26, 5):
+        assert json_data["meta"]["command"] == expected_command
