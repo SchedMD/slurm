0de1c06f73744e4b5d5baf08d2ec343b83025f39
Megan Dahl <mdahl@nvidia.com>
2026-05-08 15:01:04 -0600

    Testsuite - Add test_101_4.py to test sacct --json
    Ticket: 18108

---
+++ testsuite/python/tests/test_101_4.py
@@ -0,0 +1,27 @@
+############################################################################
+# Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+############################################################################
+import atf
+import pytest
+
+# import re
+import json
+
+
+# Setup
+@pytest.fixture(scope="module", autouse=True)
+def setup():
+    atf.require_accounting()
+    atf.require_slurm_running()
+
+
+def test_json():
+    """Verify sacct --json has the correct format and meta data command"""
+
+    expected_command = ["sacct", "--json"]
+
+    output = atf.run_command_output("sacct --json", fatal=True)
+    json_data = json.loads(output)
+    assert json_data is not None
+    if atf.get_version("bin/sacct") >= (26, 5):
+        assert json_data["meta"]["command"] == expected_command
