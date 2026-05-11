2278eb18622b33255b2e83ddc6358083b98e1ec0
Henrik Klasen <henrik.klasen.001@student.uni.lu>
2026-05-11 20:48:43 +0200

    I hope this fixes it

--- src/plugins/acct_gather_energy/gpu/acct_gather_energy_gpu.c
+++ src/plugins/acct_gather_energy/gpu/acct_gather_energy_gpu.c
@@ -192,15 +192,15 @@ static void _update_energy(gpu_status_t *gpu, uint32_t readings)
 					e->current_watts);
 		e->previous_consumed_energy = e->consumed_energy;
 		e->consumed_energy += e->base_consumed_energy;
-		debug2("gpu: %d, current watts: %u, previous watts: %u, consumed %"PRIu64" Joules %"PRIu64" new, ave watts %u",
-		       gpu->dv_ind, e->current_watts, prev_watts,
-		       e->consumed_energy, e->base_consumed_energy, e->ave_watts);
+		debug2("consumed energy: %"PRIu64" Joules, base consumed energy: %"PRIu64" Joules, ave watts: %u W, current watts: %u W",
+		       e->consumed_energy, e->base_consumed_energy,
+		       e->ave_watts, e->current_watts);
 	} else {
 		e->consumed_energy = 0;
 		e->ave_watts = 0;
 		e->current_watts = gpu->last_update_watt;
-		debug2("gpu: %d, current watts: %u, no previous watts, can't calculate energy",
-		       gpu->dv_ind, e->current_watts);
+		debug2("current watts: %u, no previous watts, can't calculate energy",
+		        e->current_watts);
 	}
 	e->poll_time = time(NULL);
 }
