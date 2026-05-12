e71acccef28c368968e88027fc1e9b1a754ad4c5
Oscar Hernández Saborit <ohernandezsa@nvidia.com>
2026-05-12 18:29:11 +0200

    Testsuite - Fix test 123_7 to correctly reflect behavior in <=25.05
    Ticket: 24838

--- testsuite/python/tests/test_123_7.py
+++ testsuite/python/tests/test_123_7.py
@@ -184,22 +184,22 @@ def _bring_node_up(node: str):
             True,
         ),
         # REOCCURRING vs MAINT - replacement must NOT overlap
-        (
+        pytest.param(
             ("resv_a9", "MAINT"),
             ("resv_b9", "DAILY"),
             ("now+1day"),
             False,
+            marks=pytest.mark.xfail(
+                atf.get_version() < (25, 11),
+                reason="Ticket 23547: Pre 25.11 reservations could take nodes from a maintenance",
+            ),
         ),
         # REOCCURRING vs MAINT - replacement overlap is allowed
-        pytest.param(
+        (
             ("resv_a10", "MAINT"),
             ("resv_b10", "DAILY"),
             ("now+8day"),
             True,
-            marks=pytest.mark.xfail(
-                atf.get_version() < (25, 11),
-                reason="Ticket 24838: Pre 25.11 we were not distinguishing based on start date",
-            ),
         ),
     ],
 )
