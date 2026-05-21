7b62cea7ef7dd050ad4d055b744d7ce1ab78fd8a
Nathan Rini <nrini@nvidia.com>
2026-05-20 20:46:47 -0600

    interfaces/http_auth - Convert to load auth/jwt plugin
    Issue: 50898

--- src/interfaces/http_auth.c
+++ src/interfaces/http_auth.c
@@ -66,40 +66,47 @@ static const char *syms[] = {
 	"http_auth_p_proxy_token",
 };
 
-static plugins_t *plugins = NULL;
+static plugins_t *http_plugins = NULL;
+static plugins_t *auth_plugins = NULL;
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void _init_plugins(plugins_t *plugins, const char *auth_info)
+{
+	for (int i = 0; i < plugins->count; i++) {
+		int rc = EINVAL;
+		const ops_t *ops = plugins->functions[i];
+
+		if ((rc = ops->init(auth_info)))
+			fatal("%s: %s loaded failed: %s", __func__,
+			      plugins->types[i], slurm_strerror(rc));
+	}
+}
 
 static int _load(const char *auth_info, const char *plugin_type,
 		 plugrack_foreach_t listf)
 {
 	int rc;
 
-	if (plugins)
+	if (http_plugins || auth_plugins)
 		return SLURM_SUCCESS;
 
 	_Static_assert(sizeof(ops_t) == (sizeof(void *) * ARRAY_SIZE(syms)),
 		       "Check symbol table is correct size");
 
-	if ((rc = load_plugins(&plugins, HTTP_AUTH_PLUGIN_TYPE, NULL, listf,
+	if ((rc = load_plugins(&http_plugins, HTTP_AUTH_PLUGIN_TYPE, NULL,
+			       listf, syms, ARRAY_SIZE(syms))))
+		return rc;
+
+	if ((rc = load_plugins(&auth_plugins, AUTH_PLUGIN_TYPE,
+			       auth_get_plugin_name(AUTH_PLUGIN_JWT), listf,
 			       syms, ARRAY_SIZE(syms))))
 		return rc;
 
-	if (plugins->count <= 0)
+	if ((http_plugins->count <= 0) && (auth_plugins->count <= 0))
 		return ESLURM_PLUGIN_NOT_LOADED;
 
-	for (int i = 0; i < plugins->count; i++) {
-		const ops_t *ops = plugins->functions[i];
-
-		if ((rc = ops->init(auth_info))) {
-			/* Cleanup initialized plugins */
-			for (; --i >= 0;) {
-				ops = plugins->functions[i];
-				ops->fini();
-			}
-			FREE_NULL_PLUGINS(plugins);
-			return rc;
-		}
-	}
+	_init_plugins(http_plugins, auth_info);
+	_init_plugins(auth_plugins, auth_info);
 
 	return SLURM_SUCCESS;
 }
@@ -121,38 +128,36 @@ extern int http_auth_g_init(const char *auth_info, const char *plugin_type,
 	return rc;
 }
 
+static void _fini_plugins(plugins_t *plugins)
+{
+	if (!plugins || (plugins->count <= 0))
+		return;
+
+	for (int i = 0; i < plugins->count; i++) {
+		const ops_t *ops = plugins->functions[i];
+
+		ops->fini();
+	}
+}
+
 extern void http_auth_g_fini(void)
 {
 	slurm_mutex_lock(&mutex);
 
-	if (plugins && (plugins->count > 0)) {
-		for (int i = 0; i < plugins->count; i++) {
-			const ops_t *ops = plugins->functions[i];
-
-			ops->fini();
-		}
-	}
-
-	FREE_NULL_PLUGINS(plugins);
+	_fini_plugins(http_plugins);
+	FREE_NULL_PLUGINS(http_plugins);
+	_fini_plugins(auth_plugins);
+	FREE_NULL_PLUGINS(auth_plugins);
 
 	slurm_mutex_unlock(&mutex);
 }
 
-extern int http_auth_g_authenticate(http_auth_plugin_id_t plugin_id,
-				    uid_t *uid_ptr, http_con_t *hcon,
-				    const char *name,
-				    const http_con_request_t *request)
+static int _authenticate(plugins_t *plugins, http_auth_plugin_id_t plugin_id,
+			 uid_t *uid_ptr, http_con_t *hcon, const char *name,
+			 const http_con_request_t *request)
 {
-	xassert(plugin_id > HTTP_AUTH_PLUGIN_INVALID);
-	xassert((plugin_id < HTTP_AUTH_PLUGIN_INVALID_MAX) ||
-		(plugin_id == HTTP_AUTH_PLUGIN_ANY));
-
-	/* Always populate the UID with SLURM_AUTH_NOBODY */
-	if (uid_ptr)
-		*uid_ptr = SLURM_AUTH_NOBODY;
-
 	if (!plugins || (plugins->count <= 0))
-		return ESLURM_NOT_SUPPORTED;
+		return ESLURM_AUTH_SKIP;
 
 	/*
 	 * Intentionally not holding mutex to avoid performance penalty.
@@ -175,22 +180,39 @@ extern int http_auth_g_authenticate(http_auth_plugin_id_t plugin_id,
 		return rc;
 	}
 
-	return ESLURM_AUTH_CRED_INVALID;
+	return ESLURM_AUTH_SKIP;
 }
 
-extern int http_auth_g_proxy_token(http_auth_plugin_id_t plugin_id,
-				   http_con_t *hcon, const char *name,
-				   const http_con_request_t *request)
+extern int http_auth_g_authenticate(http_auth_plugin_id_t plugin_id,
+				    uid_t *uid_ptr, http_con_t *hcon,
+				    const char *name,
+				    const http_con_request_t *request)
 {
-	xassert(plugin_id >= HTTP_AUTH_PLUGIN_INVALID);
+	int rc = EINVAL;
+
+	xassert(plugin_id > HTTP_AUTH_PLUGIN_INVALID);
 	xassert((plugin_id < HTTP_AUTH_PLUGIN_INVALID_MAX) ||
 		(plugin_id == HTTP_AUTH_PLUGIN_ANY));
 
-	if (plugin_id == HTTP_AUTH_PLUGIN_INVALID) {
-		auth_g_thread_clear();
-		return SLURM_SUCCESS;
-	}
+	/* Always populate the UID with SLURM_AUTH_NOBODY */
+	if (uid_ptr)
+		*uid_ptr = SLURM_AUTH_NOBODY;
 
+	if ((rc = _authenticate(http_plugins, plugin_id, uid_ptr, hcon, name,
+				request)) != ESLURM_AUTH_SKIP)
+		return rc;
+
+	if ((rc = _authenticate(auth_plugins, plugin_id, uid_ptr, hcon, name,
+				request)) != ESLURM_AUTH_SKIP)
+		return rc;
+
+	return ESLURM_AUTH_CRED_INVALID;
+}
+
+static int _proxy_token(plugins_t *plugins, http_auth_plugin_id_t plugin_id,
+			http_con_t *hcon, const char *name,
+			const http_con_request_t *request)
+{
 	if (!plugins || (plugins->count <= 0))
 		return ESLURM_NOT_SUPPORTED;
 
@@ -208,6 +230,32 @@ extern int http_auth_g_proxy_token(http_auth_plugin_id_t plugin_id,
 
 		return rc;
 	}
+
+	return ESLURM_AUTH_SKIP;
+}
+
+extern int http_auth_g_proxy_token(http_auth_plugin_id_t plugin_id,
+				   http_con_t *hcon, const char *name,
+				   const http_con_request_t *request)
+{
+	int rc = EINVAL;
+
+	xassert(plugin_id >= HTTP_AUTH_PLUGIN_INVALID);
+	xassert((plugin_id < HTTP_AUTH_PLUGIN_INVALID_MAX) ||
+		(plugin_id == HTTP_AUTH_PLUGIN_ANY));
+
+	if (plugin_id == HTTP_AUTH_PLUGIN_INVALID) {
+		auth_g_thread_clear();
+		return SLURM_SUCCESS;
+	}
+
+	if ((rc = _proxy_token(http_plugins, plugin_id, hcon, name, request)) !=
+	    ESLURM_AUTH_SKIP)
+		return rc;
+
+	if ((rc = _proxy_token(auth_plugins, plugin_id, hcon, name, request)) !=
+	    ESLURM_AUTH_SKIP)
+		return rc;
 
 	return ESLURM_AUTH_CRED_INVALID;
 }
