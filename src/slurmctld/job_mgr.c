SchedMD/slurm

commit cc38f513205e14c4f2b225ba7e0139f337827ef6

Implement logic for license OR'ing in job requests
Allocate any one of the licenses that were requested. Track the allocated
licenses in a separate field and update job_ptr->license_list to the
allocated license(s) when the job is allocated. This makes it easy to get
and return the correct licenses from cluster_license_list when the job is
allocated, completed, or state saved/restored. This also automatically gives
us the correct values in ReqTres and AllocTres if licenses are tracked in
AccountingStorageTres.

There is an important limitation in this implementation:

We always attempt to allocate the first license that is available in the
order that they were requested. This has important implications for
reservations that have licenses. For example:

* License A: 10
* License B: 20
* A reservation has 5 of license A and 10 of license B
* A job requests the reservation and --licenses='A:5|B:10'. The job will be
  allocated 5 of license A.
* The same job request comes in. The job will be allocated 5 of license A,
  even though there are 10 of license B in the reservation that are unused.
  This results in unoptimal use of licenses: jobs outside of the reservation
  are unable to use license A, while there are unused license B in the
  reservation.

This limitation occurs because of how licenses are implemented in
reservations: We track how many licenses are reserved (if someone calls
scontrol show licenses, although that can easily be fixed). However, we
don't track how many licenses are used from within a reservation. We only
track how many licenses are used in total. Therefore, when we checking if
licenses are available to the job, we currently can't distinguish between
licenses inside and outside of a reservation.

Issue: 50203
Ticket: 20934

src/slurmctld/job_mgr.c
@@ -16773,6 +16773,8 @@ void batch_requeue_fini(job_record_t *job_ptr)
 	xfree(job_ptr->alias_list);
 	xfree(job_ptr->batch_host);
 	free_job_resources(&job_ptr->job_resrcs);
+	FREE_NULL_LIST(job_ptr->license_list);
+	xfree(job_ptr->licenses_allocated);
 	xfree(job_ptr->nodes);
 	xfree(job_ptr->node_addrs);
 	xfree(job_ptr->nodes_completing);

src/slurmctld/licenses.c
@@ -152,6 +152,15 @@ static int _license_find_rec_by_id(void *x, void *key)
 	return 0;
 }
 
+/*
+ * Find a license_t record that does NOT match license id. This is the inverse
+ * of _license_find_rec_by_id
+ */
+static int _license_find_rec_by_id_not(void *x, void *key)
+{
+	return !_license_find_rec_by_id(x, key);
+}
+
 static int _license_find_rec_in_list_by_id(void *x, void *key)
 {
 	licenses_t *license_entry = x;
@@ -808,6 +817,11 @@ static int _foreach_license_job_test(void *x, void *arg)
 				_add_license(job_ptr->licenses_to_preempt,
 					     license_entry);
 			test_args->rc = EAGAIN;
+		} else if (license_entry->op_or) {
+			test_args->rc = SLURM_SUCCESS;
+			FREE_NULL_LIST(job_ptr->licenses_to_preempt);
+			/* Stop list_for_each */
+			return -1;
 		}
 	}
 	return 0;
@@ -832,13 +846,17 @@ extern int license_job_test_with_list(job_record_t *job_ptr, time_t when,
 		.reboot = reboot,
 		.when = when,
 	};
+	licenses_t *license_entry;
 	bool use_licenses_to_preempt;
 
 	if (!job_ptr->license_list)	/* no licenses needed */
 		return SLURM_SUCCESS;
 
+	/* reclaim_licenses is disabled with OR'd licenses */
+	license_entry = list_peek(job_ptr->license_list);
 	use_licenses_to_preempt = preempt_for_licenses &&
-		check_preempt_licenses;
+				  check_preempt_licenses &&
+				  !license_entry->op_or;
 	if (!job_ptr->licenses_to_preempt && use_licenses_to_preempt)
 		job_ptr->licenses_to_preempt = list_create(NULL);
 
@@ -940,6 +958,45 @@ extern list_t *cluster_license_copy(void)
 
 	return license_list_dest;
 }
+
+/*
+ * We need to track the allocated licenses separately, so that:
+ *
+ * - when the job is state saved and then restored, or
+ * - when the job completes,
+ *
+ * we update the license counts in cluster_license_list using only the licenses
+ * that were allocated.
+ */
+static int _set_licenses_alloc(job_record_t *job_ptr, bool lic_or,
+			       licenses_t *license_entry)
+{
+	if (lic_or) {
+		if (!license_entry) {
+			/*
+			 * We tested that there were enough licenses available
+			 * but then there weren't enough when we tried to
+			 * allocate. This indicates faulty logic.
+			 */
+			error("Could not allocate licenses %s for %pJ",
+			      job_ptr->licenses, job_ptr);
+			return SLURM_ERROR;
+		}
+
+		/* Remove all other licenses besides the one we allocated. */
+		list_delete_all(job_ptr->license_list,
+				_license_find_rec_by_id_not,
+				&license_entry->lic_id);
+		xassert(list_count(job_ptr->license_list) == 1);
+		xassert(license_entry ==
+			(licenses_t *) list_peek(job_ptr->license_list));
+	}
+	job_ptr->licenses_allocated =
+		license_list_to_string(job_ptr->license_list);
+
+	return SLURM_SUCCESS;
+}
+
 /*
  * license_job_get - Get the licenses required for a job
  * IN job_ptr - job identification
@@ -950,6 +1007,7 @@ extern int license_job_get(job_record_t *job_ptr, bool restore)
 	list_itr_t *iter;
 	licenses_t *license_entry, *match;
 	int rc = SLURM_SUCCESS;
+	bool lic_or = false;
 
 	if (!job_ptr->license_list)	/* no licenses needed */
 		return rc;
@@ -959,10 +1017,30 @@ extern int license_job_get(job_record_t *job_ptr, bool restore)
 	slurm_mutex_lock(&license_mutex);
 	iter = list_iterator_create(job_ptr->license_list);
 	while ((license_entry = list_next(iter))) {
+		lic_or = license_entry->op_or;
 		match = list_find_first(cluster_license_list,
 					_license_find_rec_by_id,
 					&license_entry->lic_id);
 		if (match) {
+			/*
+			 * With OR, we only know that at least one of the job's
+			 * requested licenses are available, so we need to test
+			 * for availability again. With AND we know that all
+			 * licenses are available so we don't need to check.
+			 */
+			if (lic_or) {
+				int resv_blk_lic_cnt =
+					job_test_lic_resv(job_ptr, match->name,
+							  last_license_update,
+							  false);
+
+				if (!_sufficient_licenses(license_entry, match,
+							  resv_blk_lic_cnt)) {
+					/* Not enough of this license */
+					continue;
+				}
+			}
+
 			match->used += license_entry->total;
 			license_entry->used += license_entry->total;
 			if (match->remote && restore) {
@@ -972,6 +1050,9 @@ extern int license_job_get(job_record_t *job_ptr, bool restore)
 					match->last_deficit -=
 						license_entry->total;
 			}
+			if (lic_or) {
+				break;
+			}
 		} else {
 			error("could not find license %s for job %u",
 			      license_entry->name, job_ptr->job_id);
@@ -979,6 +1060,11 @@ extern int license_job_get(job_record_t *job_ptr, bool restore)
 		}
 	}
 	list_iterator_destroy(iter);
+
+	/* When restoring, allocated licenses is already set */
+	if (!rc && !restore)
+		rc = _set_licenses_alloc(job_ptr, lic_or, license_entry);
+
 	_licenses_print("acquire_license", cluster_license_list, job_ptr);
 	slurm_mutex_unlock(&license_mutex);
 	return rc;

src/slurmctld/node_scheduler.c
@@ -2960,6 +2960,12 @@ extern int select_nodes(job_node_select_t *job_node_select,
 
 	job_end_time_reset(job_ptr);
 
+	/*
+	 * job_array_post_sched() must happen before allocate_nodes() because
+	 * we need the pending job array state to be copied. For example,
+	 * allocate_nodes() calls license_job_get() which can modify the job's
+	 * license_list if the job requested OR'd licenses.
+	 */
 	tmp_job = job_array_post_sched(job_ptr, true);
 	if (tmp_job && (tmp_job != job_ptr) && (orig_resv_port_cnt == NO_VAL16))
 		tmp_job->resv_port_cnt = orig_resv_port_cnt;

src/slurmctld/read_config.c
@@ -2269,16 +2269,31 @@ static void _sync_nodes_to_suspended_job(job_record_t *job_ptr)
 
 static void _restore_job_licenses(job_record_t *job_ptr)
 {
-	list_t *license_list = NULL;
-	bool valid = true;
+	list_t *license_list = NULL, *license_list_alloc = NULL;
+	bool valid = true, alloc_valid = true;
 
 	license_list = license_validate(job_ptr->licenses, false, false,
 					job_ptr->tres_req_cnt, &valid);
+	license_list_alloc = license_validate(job_ptr->licenses_allocated,
+					      false, false, NULL, &alloc_valid);
 	FREE_NULL_LIST(job_ptr->license_list);
+
 	if (valid) {
 		job_ptr->license_list = license_list;
 		xfree(job_ptr->licenses);
 		job_ptr->licenses = license_list_to_string(license_list);
+	}
+
+	/*
+	 * If there are allocated licenses, then set job_ptr->license_list to
+	 * that so we get the correct licenses from the cluster.
+	 */
+	if (license_list_alloc && alloc_valid) {
+		FREE_NULL_LIST(job_ptr->license_list);
+		job_ptr->license_list = license_list_alloc;
+		xfree(job_ptr->licenses_allocated);
+		job_ptr->licenses_allocated =
+			license_list_to_string(job_ptr->license_list);
 	}
 
 	if (IS_JOB_RUNNING(job_ptr) || IS_JOB_SUSPENDED(job_ptr))

commit 0a0131ad7662aadc218a2cef1dccd8d1e8c3390c

Rebuild license list after requeue
After clearing license_list here the next run will not allocate licenses in
license_job_get(). Need to rebuild because the allocated on the previous run
could be wrong due to HRES or license "OR". This was a regression from
commit cc38f513205e.

Ticket: 25226
Changelog: Fix a regression in slurm 25.05 that caused requeued jobs to lose
 their license/HRES requests, which results in Slurm allowing the job to run
 without having sufficient licenses/HRES.

src/slurmctld/job_mgr.c
@@ -16606,6 +16606,13 @@ void batch_requeue_fini(job_record_t *job_ptr)
 	FREE_NULL_LIST(job_ptr->gres_list_alloc);
 	xfree(job_ptr->resv_ports);
 
+	/*
+	 * Rebuild license_list from the request (licenses_allocated was cleared
+	 * above). validate=true checks configured licenses and holds if the
+	 * request is invalid; busy licenses pend later with WAIT_LICENSES.
+	 */
+	restore_job_licenses(job_ptr, true);
+
 	job_resv_clear_magnetic_flag(job_ptr);
 	job_ptr->epilog_failed = false;
 