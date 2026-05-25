ede2e38a181395ec8877a3b4002e7c350aaac134
Albert Gil <agilmoreno@nvidia.com>
2026-05-25 18:47:28 +0200

    Testsuite - Improve test12.2 making it faster

--- testsuite/python/expect/test_12_2.py
+++ testsuite/python/expect/test_12_2.py
@@ -13,6 +13,7 @@ def setup():
 
     atf.require_accounting()
     atf.require_config_parameter("JobAcctGatherType", "jobacct_gather/cgroup")
+    atf.require_config_parameter("JobAcctGatherFrequency", "task=1")
     atf.require_nodes(1, [("CPUS", 4), ("RealMemory", 1024)])
     atf.require_slurm_running()
 
