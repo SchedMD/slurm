e31ee3ede7c8eb02db9b38f48bc6a26094045cbb
Felip Moll <fmoll@nvidia.com>
2026-06-04 13:19:27 +0200

    mysql_jobcomp_process_get_jobs - Replace SQL || with or
    As of MySQL 8.0 the || is deprecated and we should replace it by the
    standard SQL or operator.
    
    Issue: 50959

--- src/plugins/jobcomp/mysql/mysql_jobcomp_process.c
+++ src/plugins/jobcomp/mysql/mysql_jobcomp_process.c
@@ -69,7 +69,7 @@ extern list_t *mysql_jobcomp_process_get_jobs(slurmdb_job_cond_t *job_cond)
 		itr = list_iterator_create(job_cond->step_list);
 		while ((selected_step = list_next(itr))) {
 			if (set)
-				xstrcat(extra, " || ");
+				xstrcat(extra, " or ");
 			tmp = xstrdup_printf("jobid=%u",
 					     selected_step->step_id.job_id);
 			xstrcat(extra, tmp);
@@ -90,7 +90,7 @@ extern list_t *mysql_jobcomp_process_get_jobs(slurmdb_job_cond_t *job_cond)
 		itr = list_iterator_create(job_cond->partition_list);
 		while ((selected_part = list_next(itr))) {
 			if (set)
-				xstrcat(extra, " || ");
+				xstrcat(extra, " or ");
 			tmp = xstrdup_printf("`partition`='%s'",
 					      selected_part);
 			xstrcat(extra, tmp);
