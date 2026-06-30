bd5758460bf5c811c3d20e9609aaf60ca41edec2
Brian Christiansen <bchristianse@nvidia.com>
2026-06-30 16:33:56 -0600

    Factor configuring-job launch into a helper
    Extract the "configuration complete -> launch" logic from job_time_limit()
    into _test_job_config_complete() so it can be reused by other callers.

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -9443,6 +9443,29 @@ extern bool test_job_nodes_ready(job_record_t *job_ptr)
 	return true;
 }
 
+/*
+ * Finish a configuring job's setup and launch it once all of its nodes are
+ * ready.
+ * IN job_ptr - the (potentially) configuring job to test
+ */
+static void _test_job_config_complete(job_record_t *job_ptr)
+{
+	uint8_t prolog = 0;
+
+	if (job_ptr->details)
+		prolog = job_ptr->details->prolog_running;
+
+	if (!prolog && IS_JOB_CONFIGURING(job_ptr) &&
+	    !pick_batch_host(job_ptr) && test_job_nodes_ready(job_ptr)) {
+		info("%s: Configuration for %pJ complete", __func__, job_ptr);
+		job_config_fini(job_ptr);
+		if (job_ptr->batch_flag)
+			launch_job(job_ptr);
+		else if (job_ptr->het_job_id)
+			launch_het_job_leader(job_ptr);
+	}
+}
+
 static int _foreach_het_job_configuring_test(void *x, void *arg)
 {
 	job_record_t *het_job = x;
@@ -9495,7 +9518,6 @@ void job_time_limit(void)
 	                    slurm_conf.msg_timeout + 1);
 	time_t over_run;
 	uint16_t over_time_limit;
-	uint8_t prolog;
 	int job_test_count = 0;
 	uint32_t resv_over_run = slurm_conf.resv_over_run;
 
@@ -9527,21 +9549,7 @@ void job_time_limit(void)
 		xassert (job_ptr->magic == JOB_MAGIC);
 		job_test_count++;
 
-		if (job_ptr->details)
-			prolog = job_ptr->details->prolog_running;
-		else
-			prolog = 0;
-		if ((prolog == 0) && IS_JOB_CONFIGURING(job_ptr) &&
-		    !pick_batch_host(job_ptr) &&
-		    test_job_nodes_ready(job_ptr)) {
-			info("%s: Configuration for %pJ complete",
-			     __func__, job_ptr);
-			job_config_fini(job_ptr);
-			if (job_ptr->batch_flag)
-				launch_job(job_ptr);
-			else if (job_ptr->het_job_id)
-				launch_het_job_leader(job_ptr);
-		}
+		_test_job_config_complete(job_ptr);
 
 		/*
 		 * Features have been changed on some node, make job eligiable
