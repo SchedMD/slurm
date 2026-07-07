SchedMD/slurm

commit c6eed2f29b11e4175c79e2f77c6ffc6fb4087af9

Fix race condition in stepmgr step completion handling
Ticket 21122

Signed-Off-By: Scott Hilton <scott@schedmd.com>

src/slurmd/slurmstepd/req.c
@@ -2163,31 +2163,39 @@ _handle_completion(int fd, stepd_step_rec_t *step, uid_t uid)
 		goto rwfail;
 	FREE_NULL_BUFFER(buffer);
 
-	if (job_step_ptr && do_stepmgr) {
-		int rem = 0;
-		uint32_t max_rc;
-		slurm_step_id_t temp_id = {
-			.job_id = job_step_ptr->job_id,
-			.step_het_comp = NO_VAL,
-			.step_id = step_id
-		};
+	if (do_stepmgr) {
+		slurm_mutex_lock(&stepmgr_mutex);
+		if (job_step_ptr) {
+			int rem = 0;
+			uint32_t max_rc;
+			slurm_step_id_t temp_id = {
+				.job_id = job_step_ptr->job_id,
+				.step_het_comp = NO_VAL,
+				.step_id = step_id
+			};
 
-		step_complete_msg_t req = {
-			.range_first = first,
-			.range_last = last,
-			.step_id = temp_id,
-			.step_rc = step_rc,
-			.jobacct = jobacct
-		};
+			step_complete_msg_t req = {
+				.range_first = first,
+				.range_last = last,
+				.step_id = temp_id,
+				.step_rc = step_rc,
+				.jobacct = jobacct
+			};
 
-		step_partial_comp(&req, uid, true, &rem, &max_rc);
+			step_partial_comp(&req, uid, true, &rem, &max_rc);
 
-		safe_write(fd, &rc, sizeof(int));
-		safe_write(fd, &errnum, sizeof(int));
+			safe_write(fd, &rc, sizeof(int));
+			safe_write(fd, &errnum, sizeof(int));
 
-		jobacctinfo_destroy(jobacct);
+			jobacctinfo_destroy(jobacct);
 
-		return SLURM_SUCCESS;
+			rc = SLURM_SUCCESS;
+		} else {
+			error("Asked to complete a stepmgr step but we don't have a job_step_ptr. This should never happen.");
+			rc = SLURM_ERROR;
+		}
+		slurm_mutex_unlock(&stepmgr_mutex);
+		return rc;
 	}
 
 	/*

commit 2e962834127c07b34be6d583aab768cfad414462

slurmstepd - Fix jobacct leak and mutex deadlock on step completion
The stepmgr path in _handle_completion leaked jobacct when job_step_ptr
was NULL, and a failed response write jumped to rwfail with stepmgr_mutex
still held and jobacct leaked. Free jobacct on all paths and send the
response after releasing the mutex.

Regression from commit c6eed2f29b.

Changelog: Fix a memory leak and a potential deadlock in slurmstepd
 stepmgr step completion handling.

src/slurmd/slurmstepd/req.c
@@ -2294,19 +2294,19 @@ static int _handle_completion(int fd, uid_t uid, pid_t remote_pid)
 			};
 
 			step_partial_comp(&req, uid, true, &rem, &max_rc);
-
-			safe_write(fd, &rc, sizeof(int));
-			safe_write(fd, &errnum, sizeof(int));
-
-			jobacctinfo_destroy(jobacct);
-
-			rc = SLURM_SUCCESS;
 		} else {
 			error("Asked to complete a stepmgr step but we don't have a job_step_ptr. This should never happen.");
 			rc = SLURM_ERROR;
 		}
 		slurm_mutex_unlock(&stepmgr_mutex);
-		return rc;
+		jobacctinfo_destroy(jobacct);
+
+		if (rc != SLURM_SUCCESS)
+			return rc;
+
+		safe_write(fd, &rc, sizeof(int));
+		safe_write(fd, &errnum, sizeof(int));
+		return SLURM_SUCCESS;
 	}
 
 	/*