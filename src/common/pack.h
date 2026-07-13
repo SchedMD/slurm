8dd8578c8e1a22fa1a682db2d59f69deb37d4de5
Nathan Rini <nrini@nvidia.com>
2026-07-13 13:43:48 -0600

    Add parentheses to size_buf() macro

--- src/common/pack.h
+++ src/common/pack.h
@@ -78,7 +78,7 @@ typedef struct {
 #define get_buf_offset(__buf) ((__buf)->processed)
 #define set_buf_offset(__buf, __val) ((__buf)->processed = (__val))
 #define remaining_buf(__buf) ((__buf)->size - (__buf)->processed)
-#define size_buf(__buf) (__buf->size)
+#define size_buf(__buf) ((__buf)->size)
 
 /* Initialize shadow buffer to point at data with size of bytes */
 #define SHADOW_BUF_INITIALIZER(data, bytes) \
