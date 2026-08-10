SchedMD/slurm

commit 5779369cd4c8e5c18db5dcde5d1a913b5a701bf7

sched/backfill - Change an info to a log_flag(BACKFILL)
Issue #8

src/plugins/sched/backfill/backfill.c
@@ -306,8 +306,9 @@ static void _dump_job_sched(job_record_t *job_ptr, time_t end_time,
 	slurm_make_time_str(&job_ptr->start_time, begin_buf, sizeof(begin_buf));
 	slurm_make_time_str(&end_time, end_buf, sizeof(end_buf));
 	node_list = bitmap2node_name(avail_bitmap);
-	info("%pJ to start at %s, end at %s on nodes %s in partition %s",
-	     job_ptr, begin_buf, end_buf, node_list, job_ptr->part_ptr->name);
+	log_flag(BACKFILL, "%pJ to start at %s, end at %s on nodes %s in partition %s",
+		 job_ptr, begin_buf, end_buf, node_list,
+		 job_ptr->part_ptr->name);
 	xfree(node_list);
 }
 
@@ -331,8 +332,8 @@ static void _dump_job_test(job_record_t *job_ptr, bitstr_t *avail_bitmap,
 	slurm_make_time_str(&later_start, end_buf, sizeof(end_buf));
 
 	node_list = bitmap2node_name(avail_bitmap);
-	info("Test %pJ at %s to %s (later_start: %s) on %s",
-	     job_ptr, begin_buf, end_buf, later_buf, node_list);
+	log_flag(BACKFILL, "Test %pJ at %s to %s (later_start: %s) on %s",
+		 job_ptr, begin_buf, end_buf, later_buf, node_list);
 	xfree(node_list);
 }
 
@@ -342,7 +343,7 @@ static void _dump_node_space_table(node_space_map_t *node_space_ptr)
 	int i = 0;
 	char begin_buf[256], end_buf[256], *node_list, *licenses;
 
-	info("=========================================");
+	log_flag(BACKFILL, "=========================================");
 	while (1) {
 		slurm_make_time_str(&node_space_ptr[i].begin_time,
 				    begin_buf, sizeof(begin_buf));
@@ -350,15 +351,15 @@ static void _dump_node_space_table(node_space_map_t *node_space_ptr)
 				    end_buf, sizeof(end_buf));
 		node_list = bitmap2node_name(node_space_ptr[i].avail_bitmap);
 		licenses = bf_licenses_to_string(node_space_ptr[i].licenses);
-		info("Begin:%s End:%s Nodes:%s Licenses:%s Fragmentation:%u",
-		     begin_buf, end_buf, node_list, licenses,
-		     node_space_ptr[i].fragmentation);
+		log_flag(BACKFILL, "Begin:%s End:%s Nodes:%s Licenses:%s Fragmentation:%u",
+			 begin_buf, end_buf, node_list, licenses,
+			 node_space_ptr[i].fragmentation);
 		xfree(node_list);
 		xfree(licenses);
 		if ((i = node_space_ptr[i].next) == 0)
 			break;
 	}
-	info("=========================================");
+	log_flag(BACKFILL, "=========================================");
 }
 
 static void _set_job_time_limit(job_record_t *job_ptr, uint32_t new_limit)
@@ -2674,10 +2675,10 @@ TRY_LATER:
 			_set_job_time_limit(job_ptr, orig_time_limit);
 			if (slurm_conf.debug_flags & DEBUG_FLAG_BACKFILL) {
 				END_TIMER;
-				info("yielding locks after testing "
-				     "%u(%d) jobs tested, %u time slots, %s",
-				     slurmctld_diag_stats.bf_last_depth,
-				     job_test_count, test_time_count, TIME_STR);
+				log_flag(BACKFILL, "yielding locks after testin %u(%d) jobs tested, %u time slots, %s",
+					 slurmctld_diag_stats.bf_last_depth,
+					 job_test_count, test_time_count,
+					 TIME_STR);
 			}
 			/* Sync planned nodes before yielding locks */
 			nodes_planned = true;

commit ef2d46558bed6dc10d68b5625cf809c09aea30e3

sched/backfill - Restore BackfillMap output without Backfill
These dumps are gated on BACKFILL_MAP at each call site but logged under
BACKFILL, so DebugFlags=BackfillMap alone printed nothing.
Regression from 5779369cd4.

Changelog: Fix DebugFlags=BackfillMap logging nothing unless
 DebugFlags=Backfill was also set.

src/plugins/sched/backfill/backfill.c
@@ -368,7 +368,7 @@ static void _dump_job_test(job_record_t *job_ptr, bitstr_t *avail_bitmap,
 	slurm_make_time_str(&later_start, end_buf, sizeof(end_buf));
 
 	node_list = bitmap2node_name(avail_bitmap);
-	log_flag(BACKFILL, "Test %pJ at %s to %s (later_start: %s) on %s",
+	log_flag(BACKFILL_MAP, "Test %pJ at %s to %s (later_start: %s) on %s",
 		 job_ptr, begin_buf, end_buf, later_buf, node_list);
 	xfree(node_list);
 }
@@ -379,7 +379,7 @@ static void _dump_node_space_table(node_space_map_t *node_space_ptr)
 	int i = 0;
 	char begin_buf[256], end_buf[256], *node_list, *licenses;
 
-	log_flag(BACKFILL, "=========================================");
+	log_flag(BACKFILL_MAP, "=========================================");
 	while (1) {
 		slurm_make_time_str(&node_space_ptr[i].begin_time,
 				    begin_buf, sizeof(begin_buf));
@@ -387,7 +387,7 @@ static void _dump_node_space_table(node_space_map_t *node_space_ptr)
 				    end_buf, sizeof(end_buf));
 		node_list = bitmap2node_name(node_space_ptr[i].avail_bitmap);
 		licenses = bf_licenses_to_string(node_space_ptr[i].licenses);
-		log_flag(BACKFILL, "Begin:%s End:%s Nodes:%s Licenses:%s Fragmentation:%u",
+		log_flag(BACKFILL_MAP, "Begin:%s End:%s Nodes:%s Licenses:%s Fragmentation:%u",
 			 begin_buf, end_buf, node_list, licenses,
 			 node_space_ptr[i].fragmentation);
 		xfree(node_list);
@@ -395,7 +395,7 @@ static void _dump_node_space_table(node_space_map_t *node_space_ptr)
 		if ((i = node_space_ptr[i].next) == 0)
 			break;
 	}
-	log_flag(BACKFILL, "=========================================");
+	log_flag(BACKFILL_MAP, "=========================================");
 }
 
 static void _set_job_time_limit(job_record_t *job_ptr, uint32_t new_limit)