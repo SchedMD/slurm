0f14f9f05b43cdbb490f8769af7c06d1fc07d897
Tim McMullan <tmcmullan@nvidia.com>
2026-08-18 13:29:36 -0400

    slurmstepd - Guard against invalid lengths in the attach handler
    Cherry-picked: 30fa32cbd1

--- src/slurmd/slurmstepd/req.c
+++ src/slurmd/slurmstepd/req.c
@@ -1365,6 +1365,11 @@ static int _handle_attach(int fd, uid_t uid, pid_t remote_pid)
 	srun       = xmalloc(sizeof(srun_info_t));
 
 	safe_read(fd, &cert_len, sizeof(uint32_t));
+	if (cert_len > MAX_STEPD_REQ_STR_LEN) {
+		error("%s: rejecting invalid cert length %u",
+		      __func__, cert_len);
+		goto rwfail;
+	}
 	if (cert_len) {
 		srun->tls_cert = xmalloc(cert_len);
 		safe_read(fd, srun->tls_cert, cert_len);
@@ -1372,6 +1377,10 @@ static int _handle_attach(int fd, uid_t uid, pid_t remote_pid)
 	safe_read(fd, &srun->ioaddr, sizeof(slurm_addr_t));
 	safe_read(fd, &srun->resp_addr, sizeof(slurm_addr_t));
 	safe_read(fd, &key_len, sizeof(uint32_t));
+	if (key_len > MAX_STEPD_REQ_STR_LEN) {
+		error("%s: rejecting invalid key length %u", __func__, key_len);
+		goto rwfail;
+	}
 	srun->key = xmalloc(key_len);
 	safe_read(fd, srun->key, key_len);
 	safe_read(fd, &srun->uid, sizeof(uid_t));
